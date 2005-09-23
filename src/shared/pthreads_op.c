1bec2664fe3491128381e9b63d397f373f9718d6
dcid <none@none>
2005-09-23 19:06:25 +0000

    Adding pthreads_op to the shared libraries

---
+++ src/shared/pthreads_op.c
@@ -0,0 +1,53 @@
+/*      $OSSEC, pthreads_op.c, v0.1, 2005/09/23, Daniel B. Cid$      */
+
+/* Copyright (C) 2005 Daniel B. Cid <dcid@ossec.net>
+ * All rights reserved.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
+
+#include <pthread.h>
+
+#include "headers/debug_op.h"
+#include "error_messages/error_messages.h"
+
+
+/* CreateThread(void v0.1
+ * Creates a new thread and gives the argument passed to the function
+ * Return 0 on success or -1 on error
+ */
+int CreateThread(void *function_pointer(void *data), void *data)
+{
+    pthread_t lthread;
+    int ret = 0;
+
+    ret = pthread_create(&lthread, NULL, function_pointer, (void*)data);
+    if(ret != 0)
+    {
+        merror(THREAD_ERROR,ARGV0);
+        return (-1);
+    }
+
+    return(0);
+}
+
+/*
+void *mylala(int *a)
+{
+    printf("Printf thread example: %d\n", *a);
+    return (0);
+}
+
+int main()
+{
+    int abc = 10;
+    CreateThread((void *)mylala, (void *)&abc);
+    return(0);
+}
+*/
+
+/* EOF */
