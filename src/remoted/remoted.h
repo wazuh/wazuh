d1c525ad769f26a305d2795388f89556f5b4e944
dcid <none@none>
2005-09-25 04:04:54 +0000

    Multiple changes. -Changed "loc" on the SendUnixMsg from being an int to being a char. -Added first prototypes and functions for managing the agent-server connections -Fixed some functions on sec.c and os_net.c. Performance improvements. -More error messages added. -Added get_uname function to return a char * with the uname info.

--- src/remoted/remoted.h
+++ src/remoted/remoted.h
@@ -1,3 +1,14 @@
+/*   $OSSEC, remoted.h, v0.3, 2005/02/09, Daniel B. Cid$   */
+
+/* Copyright (C) 2003,2004,2005 Daniel B. Cid <dcid@ossec.net>
+ * All right reserved.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
 
 #ifndef __LOGREMOTE_H
 
@@ -5,6 +16,8 @@
 
 #define SYSLOG_CONN 1   
 #define SECURE_CONN 2
+
+#define MGR_PORT    1514
 
 typedef struct _remoted
 {
@@ -16,4 +29,9 @@ typedef struct _remoted
 }remoted;
 
 int BindConf(char *cfgfile, remoted *logr);
+
+/* Shared keys */
+#include "headers/sec.h"
+keystruct keys;
+
 #endif
