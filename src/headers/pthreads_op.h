d862ec2297a68c0b1d60a18e390c3c03826598b0
dcid <none@none>
2005-09-25 04:06:45 +0000

    forgot the add the header file for pthreads_op

---
+++ src/headers/pthreads_op.h
@@ -0,0 +1,19 @@
+/*   $OSSEC, pthreads_op.h, v0.1, 2005/09/23, Daniel B. Cid$   */
+
+/* Copyright (C) 2005 Daniel B. Cid <dcid@ossec.net>
+ * All rights reserved.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
+
+#ifndef PTHREADS_OP_H
+#define PTHREADS_OP_H
+
+int CreateThread(void *function_pointer(void *data), void *data);
+
+
+#endif
