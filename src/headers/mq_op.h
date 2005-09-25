f2621d5a1e62c4887036a3c9ad227f1101f2a725
dcid <none@none>
2005-09-25 04:04:53 +0000

    Multiple changes. -Changed "loc" on the SendUnixMsg from being an int to being a char. -Added first prototypes and functions for managing the agent-server connections -Fixed some functions on sec.c and os_net.c. Performance improvements. -More error messages added. -Added get_uname function to return a char * with the uname info.

--- src/headers/mq_op.h
+++ src/headers/mq_op.h
@@ -23,22 +23,19 @@
 
 #define _MQ__H
 
-#define LOCALFILE_MQ 	1
+#define LOCALFILE_MQ 	'1'
 
-#define SYSLOG_MQ	    2
+#define SYSLOG_MQ	    '2'
 
-#define SECURE_MQ	    4
+#define SECURE_MQ	    '4'
 
-#define SNORT_MQ_FULL   5
-#define SNORT_MQ_FULLC '5'
+#define SNORT_MQ_FULL   '5'
 
-#define SNORT_MQ_FAST   6
-#define SNORT_MQ_FASTC '6'
+#define SNORT_MQ_FAST   '6'
 
-#define APACHERR_MQ	    7
+#define APACHERR_MQ	    '7'
 
-#define SYSCHECK_MQ     8
-#define SYSCHECK_MQ_C  '8'
+#define SYSCHECK_MQ     '8'
 
 
 int StartMQ(char * key,short int type);
@@ -46,6 +43,6 @@ int StartMQ(char * key,short int type);
 int FinishMQ();
 
 int SendMSG(int queue, char * message, char *locmsg, 
-                       char *logroup, unsigned short int loc);
+                       char *logroup, char loc);
 
 #endif
