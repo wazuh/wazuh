5e88a51e92f81b200bf03eaab77fd4791d84eec5
dcid <none@none>
2005-09-26 21:30:58 +0000

    Adding support for the manager on the client side. Using TCP and pthreads.

--- src/client-agent/manager.c
+++ src/client-agent/manager.c
@@ -18,8 +18,10 @@
 #include "os_net/os_net.h"
 #include "headers/defs.h"
 #include "headers/debug_op.h"
+#include "headers/file_op.h"
 #include "error_messages/error_messages.h"
 
+#include "agentd.h"
 
 
 /* start_mgr: Start manager thread */
@@ -27,22 +29,61 @@ void *start_mgr(void *arg)
 {
     int sock;
     int *port = (int *)arg;
+    int msg_size;
+
     
+    char tmp_msg[1024];
+    char *crypt_msg;
+    char *uname;
     
     printf("Starting manager thread on port %d..\n", *port);
 
 
     /* Connect to the server */
-    if((sock =  OS_ConnectTCP(port, serverip)) < 0)
+    if((sock =  OS_ConnectTCP(*port, logr->rip)) < 0)
     {
-        merror(CONNS_ERROR, ARGV0, serverip);
+        merror(CONNS_ERROR, ARGV0, logr->rip);
         return(NULL);
     }
     
+    /* Send the message.
+     * Message is going to be the 
+     * uname : file checksum : file checksum :
+     */   
+    
+    /* Getting uname */
+    uname = getuname();
+    if(!uname)
+    {
+        uname = strdup("No system info available");
+        if(!uname)
+        {
+            merror(MEM_ERROR,ARGV0);
+            return(NULL);
+        }
+    }
     
     printf("connected \n");
-
     
+    /* creating message */
+    snprintf(tmp_msg, 1024, "%s:",uname);
+    
+    crypt_msg = CreateSecMSG(&keys, tmp_msg, 0, &msg_size);
+    
+    if(crypt_msg == NULL)
+    {
+        merror(SEC_ERROR,ARGV0);
+        return(NULL);
+    }
+   
+    /* sending message */
+    if(write(sock, crypt_msg, msg_size) < msg_size)
+    {
+        merror("%s: Error sending message to server (write)",ARGV0);
+    }
+                                                                                     
+    printf("message sent!\n");
+    free(crypt_msg);
     return(NULL);
 }
 
