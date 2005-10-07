0bf73f77a9db49ed9aeb58f042c9e3439f908536
dcid <none@none>
2005-10-07 16:20:45 +0000

    Small changes..

--- src/rootcheck/check_rc_pids.c
+++ src/rootcheck/check_rc_pids.c
@@ -9,15 +9,15 @@
  * Foundation
  */
 
- 
-#include <stdio.h>       
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/stat.h>
+ 
+#include <stdio.h>       
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include <signal.h>
 #include <errno.h>
 
