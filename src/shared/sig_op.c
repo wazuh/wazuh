335cc102b1b10ce2d1cd08b21f5c5b4562b38922
dcid <none@none>
2005-10-28 03:11:10 +0000

    A lot of changes. Improving the way we handle the header files and debug messages...

--- src/shared/sig_op.c
+++ src/shared/sig_op.c
@@ -17,33 +17,27 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#include "headers/sig_op.h"
-#include "headers/file_op.h"
+#include "sig_op.h"
+#include "file_op.h"
+#include "debug_op.h"
+
+#include "error_messages/error_messages.h"
 
 char *pidfile = NULL;
 
 void HandleSIG()
 {
-    (void)fprintf(stderr, "%s: SIGNAL Received. Exit Cleaning...\n",
-                  pidfile);
+    merror(SIGNAL_RECV, pidfile);
     
     DeletePID(pidfile);
     
     exit(1);
 }
 
+
 /* To avoid client-server communication problems */
 void HandleSIGPIPE()
 {
-    /*(void)fprintf(stderr, "SIGPIPE Received. Ignoring ..\n");*/
-    /* Should not use printf if we are going to back to the previous
-     * point in the code
-     */
-    #ifdef DEBUG
-    (void)fprintf(stderr, "%s: SIGPIPE Received. Ignoring...\n",
-                    pidfile); 
-    #endif
-    
     return;
 }
 
@@ -52,7 +46,8 @@ void StartSIG(char *process_name)
     /* Signal Manipulation
        go to HandleSIG() */
     pidfile = process_name;
-    
+
+    signal(SIGHUP, SIG_IGN);    
     signal(SIGINT, HandleSIG);
     signal(SIGQUIT, HandleSIG);
     signal(SIGTERM, HandleSIG);
