907097634bec47f7e9434c7b31d1d9cb13c2234c
dcid <none@none>
2005-11-11 22:40:19 +0000

    s/deb\/null/dev/null :)

--- active-response/iptables-drop.sh
+++ active-response/iptables-drop.sh
@@ -4,13 +4,24 @@
 # Author: Daniel B. Cid
 # Last modified: Nov 11, 2005
 
-
+UNAME=`uname`
 IPTABLES="/sbin/iptables"
 USER=$1
 IP=$2
 
+# We should only run on linux
+if [ "X${UNAME}" != "XLinux" ]; then
+   exit 0;
+fi
+
+# Checking if iptables is present
+ls ${IPTABLES} >> /dev/null 2>&1
+if [ $? != 0 ]; then
+   exit 0;
+fi    
+       
 if [ "x${IP}" = "x" ]; then
-   echo "$0: Missing argument (ip)" 
+   echo "$0: <username> <ip>" 
    exit 1;
 fi
 
