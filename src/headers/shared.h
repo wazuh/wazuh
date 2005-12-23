ac775c4365425c4ffb2897ffe0604a13a7bdb7e0
dcid <none@none>
2005-12-23 19:31:46 +0000

    Adding u_int8/16 for Solaris.

--- src/headers/shared.h
+++ src/headers/shared.h
@@ -1,4 +1,4 @@
-/*   $OSSEC, shared.h, v0.1, 2005/10/27, Daniel B. Cid$   */
+/*   $OSSEC, shared.h, v0.2, 2005/12/23, Daniel B. Cid$   */
 
 /* Copyright (C) 2004 Daniel B. Cid <dcid@ossec.net>
  * All rights reserved.
@@ -9,10 +9,17 @@
  * Foundation
  */
 
+/* v0.2 (2005/12/23): Adding 'u_int16_t' for Solaris.
+ * v0.1 (2005/10/27): first version.
+ */
 
 #ifndef __SHARED_H
 
 #define __SHARED_H
+
+/* Global headers */
+#include <sys/types.h>
+#include <stdio.h>
 
 #include "defs.h"
 #include "help.h"
@@ -32,4 +39,13 @@
 
 #include "error_messages/error_messages.h"
 
-#endif
+
+#ifdef SOLARIS
+typedef uint16_t u_int16_t
+typedef uint8_t u_int8_t
+#endif /* SOLARIS */
+
+
+#endif /* __SHARED_H */
+
+/* EOF */
