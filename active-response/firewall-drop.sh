968aa0e3d8d74edbad4df4728f88f815b88a0d7b
dcid <none@none>
2006-02-02 03:39:31 +0000

    Multiple fixes... Fixing active response for ipfilter, and a lot of more stuff..

--- active-response/firewall-drop.sh
+++ active-response/firewall-drop.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 # Adds an IP to the iptables drop list (if linux)
-# Adds an IP to the ipfilter drop list (if solaris or freebsd)
+# Adds an IP to the ipfilter drop list (if solaris, freebsd or netbsd)
 # Requirements: Linux with iptables or Solaris/FreeBSD/NetBSD with ipfilter
 # Expect: srcip
 # Author: Daniel B. Cid
-# Last modified: Jan 30, 2006
+# Author: Ahmet Ozturk
+# Last modified: Feb 01, 2006
 
 UNAME=`uname`
 ECHO="/bin/echo"
 IPTABLES="/sbin/iptables"
 IPFILTER="/sbin/ipf"
-COMMAND=""
 ARG1=""
 ARG2=""
 ACTION=$1
 USER=$2
 IP=$3
 
+
 echo "`date` $0 $1 $2 $3" >> /tmp/ossec-hids-responses.log
+
 
 # Checking for an IP
 if [ "x${IP}" = "x" ]; then
-   echo "$0: <username> <ip>" 
+   echo "$0: <action> <username> <ip>" 
    exit 1;
 fi
+
+
 
 # Blocking IP
 if [ "x${ACTION}" != "xadd" -a "x${ACTION}" != "xdelete" ]; then
@@ -31,9 +35,9 @@ if [ "x${ACTION}" != "xadd" -a "x${ACTION}" != "xdelete" ]; then
 fi
 
 
-# We should only run on linux
+
+# We should run on linux
 if [ "X${UNAME}" = "XLinux" ]; then
-   COMMAND=${IPTABLES}
    if [ "x${ACTION}" = "xadd" ]; then
       ARG1="-I INPUT -s ${IP} -j DROP"
       ARG2="-I FORWARD -s ${IP} -j DROP"
@@ -41,46 +45,54 @@ if [ "X${UNAME}" = "XLinux" ]; then
       ARG1="-D INPUT -s ${IP} -j DROP"
       ARG2="-D FORWARD -s ${IP} -j DROP"
    fi
-# FreeBSD or SunON with ipfilter
-elif [ "X${UNAME}" = "XFreeBSD" -o "X${UNAME}" = "XSunOS" ]; then
    
-   COMMAND=${IPFILTER}
+   # Checking if iptables is present
+   ls ${IPTABLES} >> /dev/null 2>&1
+   if [ $? != 0 ]; then
+      IPTABLES="/usr"${IPTABLES}
+      ls ${IPTABLES} >> /dev/null 2>&1
+      if [ $? != 0 ]; then
+         exit 0;
+      fi
+   fi
+
+   # Executing and exiting
+   ${IPTABLES} ${ARG1}
+   ${IPTABLES} ${ARG2}                             
+
+   exit 0;
+   
+# FreeBSD, SunOS or NetBSD with ipfilter
+elif [ "X${UNAME}" = "XFreeBSD" -o "X${UNAME}" = "XSunOS" -o "X${UNAME}" = "XNetBSD" ]; then
    
    # Checking if ipfilter is present
    ls ${IPFILTER} >> /dev/null 2>&1
    if [ $? != 0 ]; then
       exit 0;
    fi    
+
+   # Checking if echo is present
+   ls ${ECHO} >> /dev/null 2>&1
+   if [ $? != 0 ]; then
+       exit 0;
+   fi    
    
    if [ "x${ACTION}" = "xadd" ]; then
-      ARG1="\"@1 block out quick on any from any to ${IP}\"|${COMMAND} -f -"
-      ARG1="\"@1 block in quick on any from ${IP} to any\"|${COMMAND} -f -"
+      ARG1="\"@1 block out quick from any to ${IP}\""
+      ARG2="\"@1 block in quick from ${IP} to any\""
+      IPFARG="${IPFILTER} -f -"
    else
-      ARG1="\"@1 block out quick on any from any to ${IP}\"|${COMMAND} -rf -"
-      ARG1="\"@1 block in quick on any from ${IP} to any\"|${COMMAND} -rf -"
+      ARG1="\"@1 block out quick from any to ${IP}\""
+      ARG2="\"@1 block in quick from ${IP} to any\""
+      IPFARG="${IPFILTER} -rf -"
    fi
   
-   # Setting command to echo 
-   COMMAND=${ECHO}        
+   # Executing it 
+   eval ${ECHO} ${ARG1}| ${IPFARG}       
+   eval ${ECHO} ${ARG2}| ${IPFARG}
+   
+   exit 0;
+         
 else
     exit 0;
 fi
-
-
-# Checking if COMMAND is present (also at /usr)
-ls ${COMMAND} >> /dev/null 2>&1
-if [ $? != 0 ]; then
-   COMMAND="/usr"${COMMAND}
-   ls ${COMMAND} >> /dev/null 2>&1
-   if [ $? != 0 ]; then
-      exit 0;
-   fi
-fi    
-
-
-# Issuing the commands
-${COMMAND} ${ARG1}
-${COMMAND} ${ARG2}
-
-       
-exit 0;
