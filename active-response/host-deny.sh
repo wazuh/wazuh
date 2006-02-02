28a4674463912fbee119da792472369f43efd0dd
dcid <none@none>
2006-02-02 03:39:31 +0000

    Multiple fixes... Fixing active response for ipfilter, and a lot of more stuff..

--- active-response/host-deny.sh
+++ active-response/host-deny.sh
@@ -13,20 +13,23 @@ echo "`date` $0 $1 $2 $3" >> /tmp/ossec-hids-responses.log
 
 # IP Address must be provided
 if [ "x${IP}" = "x" ]; then
-   echo "$0: Missing argument (ip)" 
+   echo "$0: Missing argument <action> <user> (ip)" 
    exit 1;
 fi
+
 
 # Adding the ip to hosts.deny
 if [ "x${ACTION}" = "xadd" ]; then
    echo "ALL:${IP}" >> /etc/hosts.deny
    exit 0;
 
+
 # Deleting from hosts.deny   
 elif [ "x${ACTION}" = "xdelete" ]; then   
    cat /etc/hosts.deny | grep -v "ALL:${IP}"> /tmp/hosts.deny.$$
    mv /tmp/hosts.deny.$$ /etc/hosts.deny
    exit 0;
+
 
 # Invalid action   
 else
