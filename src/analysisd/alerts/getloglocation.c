39c1da4d60844ee3582adc0381767113e26e0d20
dcid <none@none>
2006-03-05 00:53:31 +0000

    Description: Lot of changes for stats. Addition of some performance improvements. Reviewed by: dcid Bug:

--- src/analysisd/alerts/getloglocation.c
+++ src/analysisd/alerts/getloglocation.c
@@ -43,130 +43,127 @@ void OS_InitLog()
 /* OS_GetLogLocation: v0.1, 2005/04/25 */
 int OS_GetLogLocation(Eventinfo *lf)
 {
-
-    /* Checking what directories to create */
-    /* Checking if the year directory is there.
+    /* Checking what directories to create 
+     * Checking if the year directory is there.
      * If not, create it. Same for the month directory.
      */
-    if(__crt_day != lf->day)
+     
+    /* For the events */
+    snprintf(__elogfile,OS_FLSIZE,"%s/%d/", EVENTS, lf->year);
+    if(IsDir(__elogfile) == -1)
+        if(mkdir(__elogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__elogfile);
+            return (-1);
+        }
+
+    snprintf(__elogfile,OS_FLSIZE,"%s/%d/%s", EVENTS, lf->year,lf->mon);
+
+    if(IsDir(__elogfile) == -1)
+        if(mkdir(__elogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__elogfile);
+            return (-1);
+        }
+
+
+    /* Creating the logfile name */
+    snprintf(__elogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
+            EVENTS,
+            lf->year,
+            lf->mon,
+            "archive",
+            lf->day);
+
+    if(_eflog)
     {
-        /* For the events */
-        snprintf(__elogfile,OS_FLSIZE,"%s/%d/", EVENTS, lf->year);
-        if(IsDir(__elogfile) == -1)
-            if(mkdir(__elogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__elogfile);
-                return (-1);
-            }
-
-        snprintf(__elogfile,OS_FLSIZE,"%s/%d/%s", EVENTS, lf->year,lf->mon);
-
-        if(IsDir(__elogfile) == -1)
-            if(mkdir(__elogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__elogfile);
-                return (-1);
-            }
-
-
-        /* Creating the logfile name */
-        snprintf(__elogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
-                EVENTS,
-                lf->year,
-                lf->mon,
-                "archive",
-                lf->day);
-
-        if(_eflog)
-        {
-            fclose(_eflog);
-        }
-        
-        _eflog = fopen(__elogfile,"a");
-        
-        if(!_eflog)
-            merror("%s: Error opening logfile: '%s'",ARGV0,__elogfile);
-       
-        
-        /* for the alerts logs */
-        snprintf(__alogfile,OS_FLSIZE,"%s/%d/", ALERTS, lf->year);
-        if(IsDir(__alogfile) == -1)
-            if(mkdir(__alogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__alogfile);
-                return (-1);
-            }
-
-        snprintf(__alogfile,OS_FLSIZE,"%s/%d/%s", ALERTS, lf->year,lf->mon);
-
-        if(IsDir(__alogfile) == -1)
-            if(mkdir(__alogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__alogfile);
-                return (-1);
-            }
-
-
-        /* Creating the logfile name */
-        snprintf(__alogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
-                ALERTS,
-                lf->year,
-                lf->mon,
-                "alerts",
-                lf->day);
-
-         if(_aflog)
-         {
-             fclose(_aflog);
-         }
-        
-        _aflog = fopen(__alogfile,"a");
-        
-        if(!_aflog)
-            merror("%s: Error opening logfile: '%s'",ARGV0,__alogfile);
-            
-        
-        /* For the firewall events */
-        snprintf(__flogfile,OS_FLSIZE,"%s/%d/", FWLOGS, lf->year);
-        if(IsDir(__flogfile) == -1)
-            if(mkdir(__flogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__flogfile);
-                return (-1);
-            }
-
-        snprintf(__flogfile,OS_FLSIZE,"%s/%d/%s", FWLOGS, lf->year,lf->mon);
-
-        if(IsDir(__flogfile) == -1)
-            if(mkdir(__flogfile,0770) == -1)
-            {
-                merror(MKDIR_ERROR,ARGV0,__flogfile);
-                return (-1);
-            }
-
-
-        /* Creating the logfile name */
-        snprintf(__flogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
-                FWLOGS,
-                lf->year,
-                lf->mon,
-                "firewall",
-                lf->day);
-
-        if(_fflog)
-        {
-            fclose(_fflog);
-        }
-        
-        _fflog = fopen(__flogfile,"a");
-        
-        if(!_fflog)
-            merror("%s: Error opening logfile: '%s'",ARGV0,__flogfile);
-        
-        
-        /* Setting the new day */        
-        __crt_day = lf->day;
+        fclose(_eflog);
     }
+
+    _eflog = fopen(__elogfile,"a");
+
+    if(!_eflog)
+        merror("%s: Error opening logfile: '%s'",ARGV0,__elogfile);
+
+
+    /* for the alerts logs */
+    snprintf(__alogfile,OS_FLSIZE,"%s/%d/", ALERTS, lf->year);
+    if(IsDir(__alogfile) == -1)
+        if(mkdir(__alogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__alogfile);
+            return (-1);
+        }
+
+    snprintf(__alogfile,OS_FLSIZE,"%s/%d/%s", ALERTS, lf->year,lf->mon);
+
+    if(IsDir(__alogfile) == -1)
+        if(mkdir(__alogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__alogfile);
+            return (-1);
+        }
+
+
+    /* Creating the logfile name */
+    snprintf(__alogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
+            ALERTS,
+            lf->year,
+            lf->mon,
+            "alerts",
+            lf->day);
+
+    if(_aflog)
+    {
+        fclose(_aflog);
+    }
+
+    _aflog = fopen(__alogfile,"a");
+
+    if(!_aflog)
+        merror("%s: Error opening logfile: '%s'",ARGV0,__alogfile);
+
+
+    /* For the firewall events */
+    snprintf(__flogfile,OS_FLSIZE,"%s/%d/", FWLOGS, lf->year);
+    if(IsDir(__flogfile) == -1)
+        if(mkdir(__flogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__flogfile);
+            return (-1);
+        }
+
+    snprintf(__flogfile,OS_FLSIZE,"%s/%d/%s", FWLOGS, lf->year,lf->mon);
+
+    if(IsDir(__flogfile) == -1)
+        if(mkdir(__flogfile,0770) == -1)
+        {
+            merror(MKDIR_ERROR,ARGV0,__flogfile);
+            return (-1);
+        }
+
+
+    /* Creating the logfile name */
+    snprintf(__flogfile,OS_FLSIZE,"%s/%d/%s/ossec-%s-%02d.log",
+            FWLOGS,
+            lf->year,
+            lf->mon,
+            "firewall",
+            lf->day);
+
+    if(_fflog)
+    {
+        fclose(_fflog);
+    }
+
+    _fflog = fopen(__flogfile,"a");
+
+    if(!_fflog)
+        merror("%s: Error opening logfile: '%s'",ARGV0,__flogfile);
+
+
+    /* Setting the new day */        
+    __crt_day = lf->day;
 
     return(0);
 }
