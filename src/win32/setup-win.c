c15822a8f8df684ea9e19e0f90feec14a4977da8
dcid <none@none>
2006-07-21 13:21:50 +0000

    Description: Ignoring more directories in windows and removing new lines on the syscheck alerts. Reviewed by: dcid Bug:

--- src/win32/setup-win.c
+++ src/win32/setup-win.c
@@ -93,6 +93,7 @@ int config_syscheck()
     "    <ignore>C:\\Program Files/ossec-agent</ignore>\r\n"
     "    <ignore>C:\\WINDOWS/Temp/</ignore>\r\n"
     "    <ignore>C:\\WINDOWS/system32/config/systemprofile/Local Settings</ignore>\r\n"
+    "    <ignore>C:\\WINDOWS/SchedLgU.Txt</ignore>\r\n"
     "  </syscheck>\r\n"
     "</ossec_config>\r\n");
 
