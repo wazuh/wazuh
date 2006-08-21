bbf30865f4e46acf1b80e9a7896d7362c2c12b98
dcid <none@none>
2006-08-21 18:34:46 +0000

    Description: Added init scripts for macos and gentoo. Reviewed by: dcid Bug:

---
+++ src/init/darwin-init.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# Darwin init script.
+# by Lorenzo Costanzia di Costigliole <mummie@tin.it>
+
+mkdir -p /Library/StartupItems/OSSEC
+cat <<EOF >/Library/StartupItems/OSSEC/StartupParameters.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://
+www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+       <key>Description</key>
+       <string>OSSEC Host-based Intrusion Detection System</string>
+       <key>Messages</key>
+       <dict>
+               <key>start</key>
+               <string>Starting OSSEC</string>
+               <key>stop</key>
+               <string>Stopping OSSEC</string>
+       </dict>
+       <key>Provides</key>
+       <array>
+               <string>OSSEC</string>
+       </array>
+       <key>Requires</key>
+       <array>
+               <string>IPFilter</string>
+       </array>
+</dict>
+</plist>
+EOF
+
+cat <<EOF >/Library/StartupItems/OSSEC/OSSEC
+#!/bin/sh
+
+. /etc/rc.common
+. /etc/ossec-init.conf
+if [ "X${DIRECTORY}" = "X" ]; then
+    DIRECTORY="/var/ossec"
+fi
+
+
+StartService ()
+{
+        ${DIRECTORY}/bin/ossec-control start
+}
+
+StopService ()
+{
+        ${DIRECTORY}/bin/ossec-control stop
+}
+
+RestartService ()
+{
+        ${DIRECTORY}/bin/ossec-control restart
+}
+
+RunService "$1"
+EOF
+chmod 755 /Library/StartupItems/OSSEC
+chmod 644 /Library/StartupItems/OSSEC/StartupParameters.plist
+chmod 755 /Library/StartupItems/OSSEC/OSSEC
--- src/init/init.sh
+++ src/init/init.sh
@@ -13,7 +13,7 @@ runInit()
     echo ""
     echo ""
     # Checking if it is a Redhat system.
-    if [ -r /etc/redhat-release ]; then
+    if [ -r "/etc/redhat-release" ]; then
         if [ -d /etc/rc.d/init.d ]; then
             echo " - ${systemis} Redhat Linux."
             echo " - ${modifiedinit}"
@@ -21,9 +21,30 @@ runInit()
             chmod 555 /etc/rc.d/init.d/ossec
             chown root:ossec /etc/rc.d/init.d/ossec
             /sbin/chkconfig --add ossec
-            return 0
+            return 0;
         fi
     fi
+    # Checking for Gentoo
+    if [ -r "/etc/gentoo-release" ]; then
+        echo " - ${systemis} Gentoo Linux."
+        echo " - ${modifiedinit}"
+        cp -pr ./src/init/ossec-hids-gentoo.init /etc/init.d/ossec
+        chmod 555 /etc/init.d/ossec
+        chown root:ossec /etc/init.d/ossec
+        rc-update add ossec default
+        return 0;
+    fi    
+    
+    # Darwin init script (by Lorenzo Costanzia di Costigliole <mummie@tin.it>)
+    if [ "X${NUNAME}" = "Darwin" ]; then
+        # Generating darwin init script.
+
+        echo " - ${systemis} Darwin."
+        echo " - ${modifiedinit}"
+        sh ./src/init/darwin-init.sh
+        return 0;
+    fi
+         
     if [ "X${UN}" = "XOpenBSD" -o "X${UN}" = "XNetBSD" -o "X${UN}" = "XFreeBSD" ]; then
         # Checking for the presence of ossec-control on rc.local
         grep ossec-control /etc/rc.local > /dev/null 2>&1
@@ -54,12 +75,12 @@ runInit()
             return 0;
         # Taken from Stephen Bunn ossec howto.    
         elif [ -d "/etc/init.d" -a -f "/usr/sbin/update-rc.d" ]; then
-            echo " - ${systemis} Linux (SysV)."
+            echo " - ${systemis} Ubuntu Linux."
             echo " - ${modifiedinit}"
             cp -pr ./src/init/ossec-hids.init  /etc/init.d/ossec
             chmod +x /etc/init.d/ossec
             chmod go-w /etc/init.d/ossec
-            chown root /etc/init.d/ossec
+            chown root:ossec /etc/init.d/ossec
             update-rc.d ossec defaults
             return 0;    
         else
