28687c5c8bdcd929bce23ce0a3aeebec42d480a2
dcid <none@none>
2009-04-16 20:02:47 +0000

    Description: Adding mergetest. Reviewed by: dcid Bug:

---
+++ src/shared/tests/merge_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "file_op.h"
+
+
+int main(int argc, char **argv)
+{
+    if(!argv[1])
+    {
+        printf("%s [mu] <merged file> <file to merge1> <file to merge2> ..\n", argv[0]);
+        exit(1);
+    }
+
+    if(strcmp(argv[1], "m") == 0)
+    {
+        MergeFiles(argv[2], argv + 3);
+    }
+    else if(strcmp(argv[1], "u") == 0)
+    {
+        UnmergeFiles(argv[2]);
+    }
+    else
+    {
+        printf("ERROR\n");
+    }
+
+    return(0);
+}
+
+
+/* EOF */
