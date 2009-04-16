5ca85ab793fdc3cf574af8cf11ca66511af50bb1
dcid <none@none>
2009-04-16 20:06:52 +0000

    Description: Adding ip_test. Reviewed by: dcid

---
+++ src/shared/tests/ip_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "validate_op.h"
+
+
+int main(int argc, char **argv)
+{
+    os_ip myip;
+
+    if(!argv[1])
+        return(1);
+
+    if(!OS_IsValidIP(argv[1], &myip))
+    {
+        printf("Invalid ip\n");
+    }
+    
+    if(OS_IPFound(argv[2], &myip))
+    {
+        printf("IP MATCHED!\n");
+    }
+    return(0);
+}
+
+
+/* EOF */
