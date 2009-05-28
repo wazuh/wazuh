36a3856a7457a3c52be8386d5ce591604e94159c
dcid <none@none>
2009-05-28 19:54:33 +0000

    Description: Adding active-response to restart ossec. Reviewed by: dcid Bug:

---
+++ active-response/restart-ossec.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Restarts ossec.
+# Requirements: none
+# Author: Daniel B. Cid
+
+ACTION=$1
+USER=$2
+IP=$3
+
+LOCAL=`dirname $0`;
+cd $LOCAL
+cd ../
+PWD=`pwd`
+UNAME=`uname`
+
+
+# Logging the call
+echo "`date` $0 $1 $2 $3 $4 $5" >> ${PWD}/../logs/active-responses.log
+
+
+
+# Adding the ip to hosts.deny
+if [ "x${ACTION}" = "xadd" ]; then
+   ${PWD}/../bin/ossec-control restart
+   exit 0;
+
+
+# Deleting from hosts.deny   
+elif [ "x${ACTION}" = "xdelete" ]; then   
+   exit 0;
+
+
+# Invalid action   
+else
+   echo "$0: invalid action: ${ACTION}"
+fi       
+
+exit 1;
