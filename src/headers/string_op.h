49872aee8d3961a38a960b490978111bdf3f217e
Christopher Moraes <cmoraes.dev@gmail.com>
2011-07-05 15:17:38 -0400

    Added string_op.h.  Missed from earlier commits.

---
+++ src/headers/string_op.h
@@ -0,0 +1,28 @@
+/* @(#) $Id$ */
+
+/* Copyright (C) 2009 Trend Micro Inc.
+ * All rights reserved.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ *
+ * License details at the LICENSE file included with OSSEC or
+ * online at: http://www.ossec.net/en/licensing.html
+ */
+
+
+#ifndef H_STRINGOP_OS
+#define H_STRINGOP_OS
+
+
+/** os_trimcrlf
+ * Trims the cr and/or LF from the last positions of a string
+ */
+void os_trimcrlf(char *str);
+
+
+#endif
+
+/* EOF */
