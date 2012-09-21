3285829bedb989dd8344eafbf8c2f71455009887
jbcheng <none@none>
2012-09-21 12:45:21 -0700

    [2.7-beta0-rootcheck] cgzones fix jbcheng#20 normalize_string overflow

--- src/rootcheck/common.c
+++ src/rootcheck/common.c
@@ -509,27 +509,22 @@ int pt_matches(char *str, char *pattern)
 char *normalize_string(char *str)
 {
     int str_sz = strlen(str) -1;
-    
-    while(*str != '\0')
-    {
-        if(*str == ' ' || *str == '\t')
-        {
+    while(str[str_sz] == ' ' || str[str_sz] == '\t') {
+        str[str_sz--] = '\0';
+    }
+
+    while(*str != '\0') {
+        if(*str == ' ' || *str == '\t') {
             str++;
-        }
-        else
-        {
+        } else {
             break;
         }
     }
 
-    while(str[str_sz] == ' ' || str[str_sz] == '\t')
-    {
-        str[str_sz] = '\0';
-        str_sz--;
-    }
-
     return(str);
 }
+
+
 
 
 
