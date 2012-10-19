f2253c23c99cb0883710300f4d9b8d76279d2675
jbcheng <none@none>
2012-10-19 11:20:39 -0700

    Change version to 2.7-beta3 in src/scripts, to 2.7 in DOC

--- src/headers/defs.h
+++ src/headers/defs.h
@@ -52,7 +52,7 @@
 
 /* Some Global names */
 #define __name      "OSSEC HIDS"
-#define __version   "v2.7-beta2"
+#define __version   "v2.7-beta3"
 #define __author    "Trend Micro Inc."
 #define __contact   "contact@ossec.net"
 #define __site      "http://www.ossec.net"
