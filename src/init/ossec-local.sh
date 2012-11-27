5d7cfa1d6418ba9cf167157917c173b527098ca7
jbcheng <none@none>
2012-11-26 18:47:32 -0800

    [2.7+] change version to 2.7+

--- src/init/ossec-local.sh
+++ src/init/ossec-local.sh
@@ -22,7 +22,7 @@ fi
 
 
 NAME="OSSEC HIDS"
-VERSION="v2.7"
+VERSION="v2.7+"
 AUTHOR="Trend Micro Inc."
 DAEMONS="ossec-monitord ossec-logcollector ossec-syscheckd ossec-analysisd ossec-maild ossec-execd ${DB_DAEMON} ${CSYSLOG_DAEMON} ${AGENTLESS_DAEMON}"
 
