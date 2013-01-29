732c6e22a207ade95c9f9ec85d3ab41433192a78
ddpbsd <ddpbsd@gmail.com>
2013-01-29 14:35:29 -0500

    Add dns lookups to agent-auth. This is fairly untested, and probably missing some error checks.

--- src/os_auth/main-client.c
+++ src/os_auth/main-client.c
@@ -206,6 +206,43 @@ int main(int argc, char **argv)
     }
 
 
+    /* XXX char *OS_GetHost(char *host, int attempts) XXX */ 
+    /* Check to see if manager is an IP */
+    int is_ip = 1, it = 0;
+    struct sockaddr_in iptest;
+    memset(&iptest, 0, sizeof(iptest));
+
+    if(inet_pton(AF_INET, manager, &iptest.sin_addr) != 1)
+    {
+      is_ip = 0;	/* This is not an IPv4 address */
+    }
+
+    /* Not IPv4, IPv6 maybe? */
+    if(is_ip == 0)
+    {
+
+        struct sockaddr_in6 iptest6;
+        memset(&iptest6, 0, sizeof(iptest6));
+        if(inet_pton(AF_INET6, manager, &iptest6.sin6_addr) != 1)
+            is_ip = 0;
+        else
+            is_ip = 1;	/* This is an IPv6 address */
+
+    }
+    
+
+    /* If it isn't an ip, try to resolve the IP */
+    if(is_ip == 0)
+    {
+        char *ipaddress;
+        ipaddress = OS_GetHost(manager, 3);
+        printf("XXX IP: %s\n", ipaddress);
+
+        strncpy(manager, ipaddress, 16);
+
+    }
+
+
     /* Connecting via TCP */
     sock = OS_ConnectTCP(port, manager, 0);
     if(sock <= 0)
