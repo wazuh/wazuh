6290f58d74aa96f593d28fcd4cd024ea49142483
jbcheng <none@none>
2013-03-27 18:23:08 -0700

    [2.7-install] ddpbsd - fix Solaris not supporting "local" var in shell script

--- src/init/update.sh
+++ src/init/update.sh
@@ -46,10 +46,10 @@ doUpdatecleanup()
     fi
 
     # Checking if the directory is valid.
-    local _dir_pattern="^/[-a-zA-Z0-9/\.-]{3,128}$"
-    echo $DIRECTORY | grep -E "$_dir_pattern" > /dev/null 2>&1
+    _dir_pattern_update="^/[-a-zA-Z0-9/\.-]{3,128}$"
+    echo $DIRECTORY | grep -E "$_dir_pattern_update" > /dev/null 2>&1
     if [ ! $? = 0 ]; then
-        echo "# ($FUNCNAME) ERROR: directory name ($DIRECTORY) doesn't match the pattern $_dir_pattern" 1>&2
+        echo "# ($FUNCNAME) ERROR: directory name ($DIRECTORY) doesn't match the pattern $_dir_pattern_update" 1>&2
         echo "${FALSE}"
         return 1;
     fi
