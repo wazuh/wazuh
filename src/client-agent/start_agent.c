7cef15e51a88410195ec9020f8683979036b6a56
aquerubin <none@none>
2013-11-17 00:49:19 -1000

    Fixed CIDR detection bug. Removed IP version dependent INFO since the IP address is already displayed.

--- src/client-agent/start_agent.c
+++ src/client-agent/start_agent.c
@@ -98,17 +98,7 @@ int connect_server(int initial_id)
                 logr->rip[rc],
                 logr->port);
 
-        /* IPv6 address: */
-        if(strchr(tmp_str,':') != NULL)
-        {
-            verbose("%s: INFO: Using IPv6 for: %s .", ARGV0, tmp_str);
-            logr->sock = OS_ConnectUDP(logr->port, tmp_str);
-        }
-        else
-        {
-            verbose("%s: INFO: Using IPv4 for: %s .", ARGV0, tmp_str);
-            logr->sock = OS_ConnectUDP(logr->port, tmp_str);
-        }
+        logr->sock = OS_ConnectUDP(logr->port, tmp_str);
 
         if(logr->sock < 0)
         {
