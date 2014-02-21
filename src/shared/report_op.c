ba62d6fe0a5322bf63b258f76f11f2c7db621f3d
cgzones <cgzones@googlemail.com>
2014-02-21 18:02:24 +0100

    surround binary expression with parenthesis

--- src/shared/report_op.c
+++ src/shared/report_op.c
@@ -343,13 +343,13 @@ int _os_report_print_related(int print_related, OSList *st_data)
                 l_print_out("   group: '%s'", saved_aldata->group);
             else if(print_related & REPORT_REL_RULE)
                 l_print_out("   rule: '%d'", saved_aldata->rule);
-            else if(print_related & REPORT_REL_SRCIP && saved_aldata->srcip)
+            else if((print_related & REPORT_REL_SRCIP) && saved_aldata->srcip)
                 l_print_out("   srcip: '%s'", saved_aldata->srcip);
-            else if(print_related & REPORT_REL_USER && saved_aldata->user)
+            else if((print_related & REPORT_REL_USER) && saved_aldata->user)
                 l_print_out("   user: '%s'", saved_aldata->user);
             else if(print_related & REPORT_REL_LEVEL)
                 l_print_out("   level: '%d'", saved_aldata->level);
-            else if(print_related & REPORT_REL_FILE && saved_aldata->filename)
+            else if((print_related & REPORT_REL_FILE) && saved_aldata->filename)
                 l_print_out("   filename: '%s'", saved_aldata->filename);
         }
 
@@ -482,7 +482,7 @@ void os_ReportdStart(report_filter *r_filter)
 
     /* Getting current time before starting */
     tm = time(NULL);
-    p = localtime(&tm);	
+    p = localtime(&tm);
 
 
 
