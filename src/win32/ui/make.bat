7c1ad9dad6bb77a08800ab7381620ebb2a578669
awiddersheim <awiddersheim@hotmail.com>
2014-03-05 22:16:50 -0500

    Fix win32 ARGV0 names
    The ARGV0 names of manage-agents and the win32ui needed more clarity.
    Using 'ossec-agent' doesn't really makes sense. This will help in
    figuring out what is doing what in the log file for example a little
    easier.

--- src/win32/ui/make.bat
+++ src/win32/ui/make.bat
@@ -1,6 +1,6 @@
 echo Making windows agent UI
 
 "C:\MinGW\bin\windres.exe" -o resource.o win32ui.rc
-"C:\MinGW\bin\gcc.exe" -o "os_win32ui" -Wall -DARGV0=\"ossec-agent\" -DCLIENT -DWIN32 resource.o ../os_net/*.c ../os_xml/*.c ../addagent/b64.c ../shared/validate_op.c ../shared/debug_op.c ../win_service.c *.c -I../headers/ -I../ -lcomctl32 -mwindows -lwsock32
+"C:\MinGW\bin\gcc.exe" -o "os_win32ui" -Wall -DARGV0=\"ossec-win32ui\" -DCLIENT -DWIN32 resource.o ../os_net/*.c ../os_xml/*.c ../addagent/b64.c ../shared/validate_op.c ../shared/debug_op.c ../win_service.c *.c -I../headers/ -I../ -lcomctl32 -mwindows -lwsock32
 copy os_win32ui.exe ..\
 cd ../
