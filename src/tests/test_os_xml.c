f0f95ad2fc4e93eed297923f7a5bd11bd9455224
cgzones <cgzones@googlemail.com>
2014-03-11 11:53:04 +0100

    simplyfy os_xml testing code

--- src/tests/test_os_xml.c
+++ src/tests/test_os_xml.c
@@ -24,72 +24,110 @@ static void create_xml_file(const char *str, char file_name[], size_t length)
 	close(fd);
 }
 
+static void nodecat(XML_NODE node, OS_XML *xml, char *buffer) {
+	int i = 0;
+	/* write node */
+	while(node[i])
+	{
+		strncat(buffer, "<", 1);
+		ck_assert_ptr_ne(node[i]->element, NULL);
+		strncat(buffer, node[i]->element, strlen(node[i]->element));
+		/* write attributes */
+		if(node[i]->attributes)
+		{
+			ck_assert_ptr_ne(node[i]->values, NULL);
+			int j = 0;
+			while(node[i]->attributes[j])
+			{
+				strncat(buffer, " ", 1);
+				ck_assert_ptr_ne(node[i]->values[j], NULL);
+				strncat(buffer, node[i]->attributes[j], strlen(node[i]->attributes[j]));
+				strncat(buffer, "=", 1);
+				strncat(buffer, "\"", 1);
+				strncat(buffer, node[i]->values[j], strlen(node[i]->values[j]));
+				strncat(buffer, "\"", 1);
+				j++;
+			}
+			ck_assert_ptr_eq(node[i]->values[j], NULL);
+		}
+		else
+		{
+			ck_assert_ptr_eq(node[i]->values, NULL);
+		}
+		strncat(buffer, ">", 1);
+		ck_assert_ptr_ne(node[i]->content, NULL);
+		strncat(buffer, node[i]->content, strlen(node[i]->content));
 
-START_TEST(test_singleroot)
+		/* write children */
+		XML_NODE child = OS_GetElementsbyNode(xml, node[i]);
+		if(child != NULL)
+		{
+			nodecat(child, xml, buffer);
+			OS_ClearNode(child);
+		}
+
+		/* close node */
+		strncat(buffer, "</", 2);
+		strncat(buffer, node[i]->element, strlen(node[i]->element));
+		strncat(buffer, ">", 1);
+		i++;
+	}
+}
+
+static void assert_os_xml_eq_str(OS_XML *xml, const char *xml_str)
+{
+	XML_NODE node = OS_GetElementsbyNode(xml, NULL);
+	if(node == NULL)
+	{
+		ck_assert_str_eq(xml_str, "");
+		return;
+	}
+
+	char *buffer = malloc(6144 * sizeof(char));
+	buffer[0] = '\0';
+	nodecat(node, xml, buffer);
+	OS_ClearNode(node);
+
+	ck_assert_str_eq(buffer, xml_str);
+}
+
+static void assert_os_xml_eq(const char *parse_str, const char *xml_str)
 {
 	char xml_file_name[256];
-	create_xml_file(
-			"<root></root>",
-			xml_file_name, 256);
+	create_xml_file(parse_str, xml_file_name, 256);
 	OS_XML xml;
-	XML_NODE node;
 	ck_assert_int_eq(OS_ReadXML(xml_file_name, &xml), 0);
 	ck_assert_int_eq(OS_ApplyVariables(&xml), 0);
-	ck_assert_ptr_ne(node = OS_GetElementsbyNode(&xml, NULL), NULL);
+	assert_os_xml_eq_str(&xml, xml_str);
 
-	ck_assert_ptr_ne(node[0], NULL);
-	ck_assert_str_eq(node[0]->element, "root");
-	ck_assert_str_eq(node[0]->content, "");
-	ck_assert_ptr_eq(node[0]->attributes, NULL);
-	ck_assert_ptr_eq(node[0]->values, NULL);
-	ck_assert_ptr_eq(node[1], NULL);
-
-	OS_ClearNode(node);
 	OS_ClearXML(&xml);
 	unlink(xml_file_name);
+}
+
+START_TEST(test_simplenodes)
+{
+	assert_os_xml_eq("", "");
+	assert_os_xml_eq("<root></root>", "<root></root>");
+	assert_os_xml_eq("<root/>", "<root></root>");
 }
 END_TEST
 
 
-START_TEST(test_multipleroot)
+START_TEST(test_multiplenodes)
 {
-	char xml_file_name[256];
-	create_xml_file(
+	assert_os_xml_eq(
 			"<root1></root1>"
 			"<root2></root2>"
 			"<root3/>",
-			xml_file_name, 256);
-	OS_XML xml;
-	XML_NODE node;
-	ck_assert_int_eq(OS_ReadXML(xml_file_name, &xml), 0);
-	ck_assert_int_eq(OS_ApplyVariables(&xml), 0);
-	ck_assert_ptr_ne(node = OS_GetElementsbyNode(&xml, NULL), NULL);
-
-	ck_assert_ptr_ne(node[0], NULL);
-	ck_assert_str_eq(node[0]->element, "root1");
-	ck_assert_str_eq(node[0]->content, "");
-	ck_assert_ptr_eq(node[0]->attributes, NULL);
-	ck_assert_ptr_eq(node[0]->values, NULL);
-	ck_assert_str_eq(node[1]->element, "root2");
-	ck_assert_str_eq(node[1]->content, "");
-	ck_assert_ptr_eq(node[1]->attributes, NULL);
-	ck_assert_ptr_eq(node[1]->values, NULL);
-	ck_assert_str_eq(node[2]->element, "root3");
-	ck_assert_str_eq(node[2]->content, "");
-	ck_assert_ptr_eq(node[2]->attributes, NULL);
-	ck_assert_ptr_eq(node[2]->values, NULL);
-	ck_assert_ptr_eq(node[3], NULL);
-
-	OS_ClearNode(node);
-	OS_ClearXML(&xml);
-	unlink(xml_file_name);
+			"<root1></root1>"
+			"<root2></root2>"
+			"<root3></root3>");
 }
 END_TEST
 
 START_TEST(test_children)
 {
-	char xml_file_name[256];
-	create_xml_file(
+	assert_os_xml_eq(
 			"<root1>"
 			"<child1></child1>"
 			"</root1>"
@@ -101,169 +139,40 @@ START_TEST(test_children)
 			"</child3>"
 			"</root2>"
 			"<root3></root3>",
-			xml_file_name, 256);
-	OS_XML xml;
-	XML_NODE node, child, grandchild;
-	ck_assert_int_eq(OS_ReadXML(xml_file_name, &xml), 0);
-	ck_assert_int_eq(OS_ApplyVariables(&xml), 0);
-	ck_assert_ptr_ne(node = OS_GetElementsbyNode(&xml, NULL), NULL);
-
-	ck_assert_ptr_ne(node[0], NULL);
-	ck_assert_str_eq(node[0]->element, "root1");
-	ck_assert_str_eq(node[0]->content, "");
-	ck_assert_ptr_eq(node[0]->attributes, NULL);
-	ck_assert_ptr_eq(node[0]->values, NULL);
-
-	ck_assert_ptr_ne(child = OS_GetElementsbyNode(&xml, node[0]), NULL);
-	ck_assert_ptr_ne(child[0], NULL);
-	ck_assert_str_eq(child[0]->element, "child1");
-	ck_assert_str_eq(child[0]->content, "");
-	ck_assert_ptr_eq(child[0]->attributes, NULL);
-	ck_assert_ptr_eq(child[0]->values, NULL);
-	ck_assert_ptr_eq(child[1], NULL);
-	OS_ClearNode(child);
-
-
-	ck_assert_str_eq(node[1]->element, "root2");
-	ck_assert_str_eq(node[1]->content, "");
-	ck_assert_ptr_eq(node[1]->attributes, NULL);
-	ck_assert_ptr_eq(node[1]->values, NULL);
-
-	ck_assert_ptr_ne(child = OS_GetElementsbyNode(&xml, node[1]), NULL);
-	ck_assert_ptr_ne(child[0], NULL);
-	ck_assert_str_eq(child[0]->element, "child1");
-	ck_assert_str_eq(child[0]->content, "");
-	ck_assert_ptr_eq(child[0]->attributes, NULL);
-	ck_assert_ptr_eq(child[0]->values, NULL);
-
-	ck_assert_ptr_ne(child[1], NULL);
-	ck_assert_str_eq(child[1]->element, "child2");
-	ck_assert_str_eq(child[1]->content, "");
-	ck_assert_ptr_eq(child[1]->attributes, NULL);
-	ck_assert_ptr_eq(child[1]->values, NULL);
-
-	ck_assert_ptr_ne(child[2], NULL);
-	ck_assert_str_eq(child[2]->element, "child3");
-	ck_assert_str_eq(child[2]->content, "");
-	ck_assert_ptr_eq(child[2]->attributes, NULL);
-	ck_assert_ptr_eq(child[2]->values, NULL);
-
-	ck_assert_ptr_ne(grandchild = OS_GetElementsbyNode(&xml, child[2]), NULL);
-	ck_assert_ptr_ne(grandchild[0], NULL);
-	ck_assert_str_eq(grandchild[0]->element, "child3.1");
-	ck_assert_str_eq(grandchild[0]->content, "");
-	ck_assert_ptr_eq(grandchild[0]->attributes, NULL);
-	ck_assert_ptr_eq(grandchild[0]->values, NULL);
-	ck_assert_ptr_eq(grandchild[1], NULL);
-	OS_ClearNode(grandchild);
-
-	ck_assert_ptr_eq(child[3], NULL);
-	OS_ClearNode(child);
-
-	ck_assert_str_eq(node[2]->element, "root3");
-	ck_assert_str_eq(node[2]->content, "");
-	ck_assert_ptr_eq(node[2]->attributes, NULL);
-	ck_assert_ptr_eq(node[2]->values, NULL);
-	ck_assert_ptr_eq(OS_GetElementsbyNode(&xml, node[2]), NULL);
-
-	ck_assert_ptr_eq(node[3], NULL);
-
-
-	OS_ClearNode(node);
-	OS_ClearXML(&xml);
-	unlink(xml_file_name);
+			"<root1>"
+			"<child1></child1>"
+			"</root1>"
+			"<root2>"
+			"<child1></child1>"
+			"<child2></child2>"
+			"<child3>"
+			"<child3.1></child3.1>"
+			"</child3>"
+			"</root2>"
+			"<root3></root3>");
 }
 END_TEST
 
 
 START_TEST(test_attributes)
 {
-	char xml_file_name[256];
-	create_xml_file(
+	assert_os_xml_eq(
 			"<root attr1=\"test\" attr2=\"1\"></root>",
-			xml_file_name, 256);
-	OS_XML xml;
-	XML_NODE node;
-	ck_assert_int_eq(OS_ReadXML(xml_file_name, &xml), 0);
-	ck_assert_int_eq(OS_ApplyVariables(&xml), 0);
-	ck_assert_ptr_ne(node = OS_GetElementsbyNode(&xml, NULL), NULL);
-
-	ck_assert_ptr_ne(node[0], NULL);
-	ck_assert_str_eq(node[0]->element, "root");
-	ck_assert_str_eq(node[0]->content, "");
-	ck_assert_ptr_ne(node[0]->attributes, NULL);
-	ck_assert_str_eq(node[0]->attributes[0], "attr1");
-	ck_assert_str_eq(node[0]->attributes[1], "attr2");
-	ck_assert_ptr_eq(node[0]->attributes[2], NULL);
-	ck_assert_ptr_ne(node[0]->values, NULL);
-	ck_assert_str_eq(node[0]->values[0], "test");
-	ck_assert_str_eq(node[0]->values[1], "1");
-	ck_assert_ptr_eq(node[0]->values[2], NULL);
-	ck_assert_ptr_eq(node[1], NULL);
-
-	OS_ClearNode(node);
-	OS_ClearXML(&xml);
-	unlink(xml_file_name);
+			"<root attr1=\"test\" attr2=\"1\"></root>");
 }
 END_TEST
 
 START_TEST(test_variables)
 {
-	char xml_file_name[256];
-	create_xml_file(
+	assert_os_xml_eq(
 			"<var name=\"var1\">value1</var>"
 			"<var name=\"var2\">value2</var>"
 			"<root attr1=\"$var1\" attr2=\"1\">$var2</root>"
 			"<root attr1=\"blah$var1\" attr2=\"1\">blah$var2</root>"
 			"<root attr1=\"blah$var1$var2 blah\" attr2=\"1\">blah$var2$var1 blah</root>",
-			xml_file_name, 256);
-	OS_XML xml;
-	XML_NODE node;
-	ck_assert_int_eq(OS_ReadXML(xml_file_name, &xml), 0);
-	ck_assert_int_eq(OS_ApplyVariables(&xml), 0);
-	ck_assert_ptr_ne(node = OS_GetElementsbyNode(&xml, NULL), NULL);
-
-	ck_assert_ptr_ne(node[0], NULL);
-	ck_assert_str_eq(node[0]->element, "root");
-	ck_assert_str_eq(node[0]->content, "value2");
-	ck_assert_ptr_ne(node[0]->attributes, NULL);
-	ck_assert_str_eq(node[0]->attributes[0], "attr1");
-	ck_assert_str_eq(node[0]->attributes[1], "attr2");
-	ck_assert_ptr_eq(node[0]->attributes[2], NULL);
-	ck_assert_ptr_ne(node[0]->values, NULL);
-	ck_assert_str_eq(node[0]->values[0], "value1");
-	ck_assert_str_eq(node[0]->values[1], "1");
-	ck_assert_ptr_eq(node[0]->values[2], NULL);
-
-	ck_assert_ptr_ne(node[1], NULL);
-	ck_assert_str_eq(node[1]->element, "root");
-	ck_assert_str_eq(node[1]->content, "blahvalue2");
-	ck_assert_ptr_ne(node[1]->attributes, NULL);
-	ck_assert_str_eq(node[1]->attributes[0], "attr1");
-	ck_assert_str_eq(node[1]->attributes[1], "attr2");
-	ck_assert_ptr_eq(node[1]->attributes[2], NULL);
-	ck_assert_ptr_ne(node[1]->values, NULL);
-	ck_assert_str_eq(node[1]->values[0], "blahvalue1");
-	ck_assert_str_eq(node[1]->values[1], "1");
-	ck_assert_ptr_eq(node[1]->values[2], NULL);
-
-	ck_assert_ptr_ne(node[2], NULL);
-	ck_assert_str_eq(node[2]->element, "root");
-	ck_assert_str_eq(node[2]->content, "blahvalue2value1 blah");
-	ck_assert_ptr_ne(node[2]->attributes, NULL);
-	ck_assert_str_eq(node[2]->attributes[0], "attr1");
-	ck_assert_str_eq(node[2]->attributes[1], "attr2");
-	ck_assert_ptr_eq(node[2]->attributes[2], NULL);
-	ck_assert_ptr_ne(node[2]->values, NULL);
-	ck_assert_str_eq(node[2]->values[0], "blahvalue1value2 blah");
-	ck_assert_str_eq(node[2]->values[1], "1");
-	ck_assert_ptr_eq(node[2]->values[2], NULL);
-
-	ck_assert_ptr_eq(node[3], NULL);
-
-	OS_ClearNode(node);
-	OS_ClearXML(&xml);
-	unlink(xml_file_name);
+			"<root attr1=\"value1\" attr2=\"1\">value2</root>"
+			"<root attr1=\"blahvalue1\" attr2=\"1\">blahvalue2</root>"
+			"<root attr1=\"blahvalue1value2 blah\" attr2=\"1\">blahvalue2value1 blah</root>");
 }
 END_TEST
 
@@ -274,8 +183,8 @@ Suite *test_os_xml_suite(void)
 
 	/* Core test case */
 	TCase *tc_core = tcase_create("Core");
-	tcase_add_test(tc_core, test_singleroot);
-	tcase_add_test(tc_core, test_multipleroot);
+	tcase_add_test(tc_core, test_simplenodes);
+	tcase_add_test(tc_core, test_multiplenodes);
 	tcase_add_test(tc_core, test_children);
 	tcase_add_test(tc_core, test_attributes);
 	tcase_add_test(tc_core, test_variables);
