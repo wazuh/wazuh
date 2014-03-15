464289043bd8e4310093327c842cfe5ab5f7e7ed
cgzones <cgzones@googlemail.com>
2014-03-15 11:29:46 +0100

    Revert "[os_xml] astyle format"
    This reverts commit 1e94f634f35ceb54abb4d8941eb48bc80caf1793.

--- src/os_xml/os_xml_variables.c
+++ src/os_xml/os_xml_variables.c
@@ -28,53 +28,55 @@ int OS_ApplyVariables(OS_XML *_lxml)
     char **var = NULL;
     char **value = NULL;
     char **tmp = NULL;
-    char *p2 = NULL;
+    char *p2= NULL;
     char *var_placeh = NULL;
 
 
     /* No variables. */
-    if(!_lxml->cur) {
+    if(!_lxml->cur)
         return(0);
-    }
 
 
     /* Getting all variables */
-    for(; i < _lxml->cur; i++) {
-        if(_lxml->tp[i] == XML_VARIABLE_BEGIN) {
+    for(;i<_lxml->cur;i++)
+    {
+        if(_lxml->tp[i] == XML_VARIABLE_BEGIN)
+        {
             int _found_var = 0;
 
-            j = i + 1;
-            for(; j < _lxml->cur; j++) {
-                if(_lxml->rl[j] < _lxml->rl[i]) {
+            j = i+1;
+            for(;j<_lxml->cur;j++)
+            {
+                if(_lxml->rl[j] < _lxml->rl[i])
                     break;
-                }
 
-                else if(_lxml->tp[j] == XML_ATTR) {
-                    if((_lxml->el[j]) && (strcasecmp(_lxml->el[j], "name") == 0)) {
-                        if(!_lxml->ct[j]) {
+                else if(_lxml->tp[j] == XML_ATTR)
+                {
+                    if((_lxml->el[j])&&(strcasecmp(_lxml->el[j],"name") == 0))
+                    {
+                        if(!_lxml->ct[j])
                             break;
-                        }
 
                         /* If not used, it will be cleaned latter */
                         snprintf(_lxml->err, 128, "XML_ERR: Memory error");
 
-                        tmp = (char **)realloc(var, (s + 1) * sizeof(char *));
-                        if(tmp == NULL) {
+                        tmp = (char**)realloc(var,(s+1)*sizeof(char *));
+                        if(tmp == NULL)
                             goto fail;
-                        }
                         var = tmp;
 
                         var[s] = strdup(_lxml->ct[j]);
-                        if(var[s] == NULL) {
+                        if(var[s] == NULL)
                             goto fail;
-                        }
 
                         /* Cleaning the lxml->err */
-                        strncpy(_lxml->err, " ", 3);
+                        strncpy(_lxml->err," ", 3);
 
                         _found_var = 1;
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         snprintf(_lxml->err, 128,
                                  "XML_ERR: Only \"name\" is allowed"
                                  " as an attribute for a variable");
@@ -84,104 +86,110 @@ int OS_ApplyVariables(OS_XML *_lxml)
             } /* Attribute FOR */
 
 
-            if((_found_var == 0) || (!_lxml->ct[i])) {
-                snprintf(_lxml->err, 128,
+            if((_found_var == 0)||(!_lxml->ct[i]))
+            {
+                snprintf(_lxml->err,128,
                          "XML_ERR: Bad formed variable. No value set");
                 goto fail;
             }
 
 
-            snprintf(_lxml->err, 128, "XML_ERR: Memory error");
+            snprintf(_lxml->err,128, "XML_ERR: Memory error");
 
-            tmp = (char **)realloc(value, (s + 1) * sizeof(char *));
-            if (tmp == NULL) {
+            tmp = (char**)realloc(value,(s+1)*sizeof(char *));
+            if (tmp == NULL)
                 goto fail;
-            }
             value = tmp;
 
             value[s] = strdup(_lxml->ct[i]);
-            if(value[s] == NULL) {
+            if(value[s] == NULL)
                 goto fail;
-            }
 
-            strncpy(_lxml->err, " ", 3);
+            strncpy(_lxml->err," ", 3);
             s++;
         }
     } /* initial FOR to get the variables  */
 
 
     /* No variable */
-    if(s == 0) {
+    if(s == 0)
         return(0);
-    }
 
 
     /* Looping again and modifying where found the variables */
     i = 0;
-    for(; i < _lxml->cur; i++) {
-        if(((_lxml->tp[i] == XML_ELEM) || (_lxml->tp[i] == XML_ATTR)) &&
-                (_lxml->ct[i])) {
+    for(;i<_lxml->cur;i++)
+    {
+        if(((_lxml->tp[i] == XML_ELEM) || (_lxml->tp[i] == XML_ATTR))&&
+            (_lxml->ct[i]))
+        {
             unsigned int tp = 0;
             size_t init = 0;
             char *p = NULL;
             char lvar[256]; /* MAX Var size */
 
 
-            if(strlen(_lxml->ct[i]) <= 2) {
+            if(strlen(_lxml->ct[i]) <= 2)
                 continue;
-            }
 
 
             /* Duplicating string */
             p = strdup(_lxml->ct[i]);
-            p2 = p;
+            p2= p;
 
-            if(p == NULL) {
+            if(p == NULL)
+            {
                 snprintf(_lxml->err, 128, "XML_ERR: Memory error");
                 goto fail;
             }
 
 
             /* Reading the whole string */
-            while(*p != '\0') {
-                if(*p == XML_VARIABLE_BEGIN) {
+            while(*p != '\0')
+            {
+                if(*p == XML_VARIABLE_BEGIN)
+                {
                     tp = 0;
                     p++;
                     memset(lvar, '\0', 256);
 
-                    while(1) {
+                    while(1)
+                    {
                         if((*p == XML_VARIABLE_BEGIN)
-                                || (*p == '\0')
-                                || (*p == '.')
-                                || (*p == '|')
-                                || (*p == ',')
-                                || (*p == ' ')) {
-                            lvar[tp] = '\0';
+                            ||(*p == '\0')
+                            ||(*p == '.')
+                            ||(*p == '|')
+                            ||(*p == ',')
+                            ||(*p == ' '))
+                        {
+                            lvar[tp]='\0';
 
                             /* Looking for var */
-                            for(j = 0; j < s; j++) {
-                                if(var[j] == NULL) {
+                            for(j=0; j<s; j++)
+                            {
+                                if(var[j] == NULL)
                                     break;
-                                }
 
-                                if(strcasecmp(var[j], lvar) != 0) {
+                                if(strcasecmp(var[j], lvar) != 0)
+                                {
                                     continue;
                                 }
 
 
                                 size_t tsize = strlen(_lxml->ct[i]) +
-                                               strlen(value[j]) - tp + 1;
+                                        strlen(value[j]) - tp + 1;
 
                                 var_placeh = strdup(_lxml->ct[i]);
 
                                 free(_lxml->ct[i]);
 
-                                _lxml->ct[i] = (char *)calloc(tsize + 2,
-                                                              sizeof(char));
+                                _lxml->ct[i] = (char*)calloc(tsize +2,
+                                                             sizeof(char));
 
-                                if(_lxml->ct[i] == NULL || var_placeh == NULL) {
-                                    snprintf(_lxml->err, 128, "XML_ERR: Memory "
-                                             "error");
+                                if(_lxml->ct[i] == NULL || var_placeh == NULL)
+                                {
+                                    snprintf(_lxml->err,128, "XML_ERR: Memory "
+                                                             "error");
                                     goto fail;
                                 }
 
@@ -190,12 +198,12 @@ int OS_ApplyVariables(OS_XML *_lxml)
 
 
                                 _lxml->ct[i][init] = '\0';
-                                strncat(_lxml->ct[i], value[j], tsize - init);
+                                strncat(_lxml->ct[i], value[j],tsize - init);
 
 
                                 init = strlen(_lxml->ct[i]);
                                 strncat(_lxml->ct[i], p,
-                                        tsize - strlen(_lxml->ct[i]));
+                                         tsize - strlen(_lxml->ct[i]));
 
 
                                 free(var_placeh);
@@ -205,12 +213,15 @@ int OS_ApplyVariables(OS_XML *_lxml)
                             }
 
                             /* Variale not found */
-                            if((j == s) && (strlen(lvar) >= 1)) {
-                                snprintf(_lxml->err, 128,
-                                         "XML_ERR: Unknown variable"
-                                         ": %s", lvar);
+                            if((j == s) && (strlen(lvar) >= 1))
+                            {
+                                snprintf(_lxml->err,128,
+                                                "XML_ERR: Unknown variable"
+                                                ": %s", lvar);
                                 goto fail;
-                            } else if(j == s) {
+                            }
+                            else if(j == s)
+                            {
                                 init++;
                             }
 
@@ -218,9 +229,10 @@ int OS_ApplyVariables(OS_XML *_lxml)
                         }
 
                         /* Maximum size for a variable */
-                        if(tp >= 255) {
-                            snprintf(_lxml->err, 128, "XML_ERR: Invalid "
-                                     "variable size.");
+                        if(tp >= 255)
+                        {
+                            snprintf(_lxml->err,128, "XML_ERR: Invalid "
+                                                     "variable size.");
                             goto fail;
 
                         }
@@ -234,12 +246,13 @@ int OS_ApplyVariables(OS_XML *_lxml)
                 p++;
                 init++;
 
-go_next:
+                go_next:
                 continue;
 
             } /* WHILE END */
 
-            if(p2 != NULL) {
+            if(p2 != NULL)
+            {
                 free(p2);
                 p2 = NULL;
                 p = NULL;
@@ -249,16 +262,19 @@ go_next:
 
     goto cleanup;
 
-fail:
+    fail:
     retval = -1;
 
-cleanup:
+    cleanup:
     /* Cleaning the variables */
-    for(i = 0; i < s; i++) {
-        if((var) && (var[i])) {
+    for(i=0;i<s;i++)
+    {
+        if((var)&&(var[i]))
+        {
             free(var[i]);
         }
-        if((value) && (value[i])) {
+        if((value)&&(value[i]))
+        {
             free(value[i]);
         }
     }
@@ -266,7 +282,7 @@ cleanup:
     free(var);
     free(value);
     free(p2);
-    free(var_placeh);
+	free(var_placeh);
 
     return(retval);
 }
