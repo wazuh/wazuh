43f6ced03d21fc98692225276099d6fb8df86ac4
JB Cheng <jjoobbcc@gmail.com>
2014-04-04 16:06:03 -0700

    Fix windows agents compile error and warnings #define ENOBUFS, #define ALERT_SYSTEM_ERR

--- src/os_net/os_net.c
+++ src/os_net/os_net.c
@@ -37,8 +37,12 @@ socklen_t us_l = sizeof(n_us);
 		                      + strlen ((ptr)->sun_path))
 #endif /* Sun_LEN */
 
-/*#else*/
+#else /* WIN32 */
 /*int ENOBUFS = 0;*/
+# ifndef ENOBUFS
+# define ENOBUFS 0
+# endif
+
 #endif /* WIN32*/
 
 
