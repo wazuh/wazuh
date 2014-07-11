6cd9996efe423b839cda99602f45b387c29bafcc
ddpbsd <ddpbsd@gmail.com>
2014-07-11 15:24:36 -0400

    Revert a small change I had made a while back. Let's see if that crash(maybe? can't remember for sure) returns.

--- src/analysisd/decoders/plugins/ossecalert_decoder.c
+++ src/analysisd/decoders/plugins/ossecalert_decoder.c
@@ -168,8 +168,7 @@ void *OSSECAlert_Decoder_Exec(Eventinfo *lf)
 
 
     /* Creating new full log. */
-    /*free(lf->full_log);*/	/* Use after free? */
-    lf->full_log = '\0';
+    free(lf->full_log);		/* XXX I think I was seeing a crash here at some point - keep an on on it */
     os_strdup(tmp_str, lf->full_log);
     lf->log = lf->full_log;
 
