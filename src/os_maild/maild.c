40af29caaccab5480e930890792dc099ce699a62
awiddersheim <awiddersheim@hotmail.com>
2014-08-22 15:07:20 -0400

    Remove shared help from ossec-maild
    Implement fixes for #207. Stop using shared help in ossec-maild.

--- src/os_maild/maild.c
+++ src/os_maild/maild.c
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
                 print_version();
                 break;
             case 'h':
-                help(ARGV0);
+                help_local();
                 break;
             case 'd':
                 nowDebug();
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
                 test_config = 1;
                 break;
             default:
-                help(ARGV0);
+                help_local();
                 break;
         }
 
@@ -476,6 +476,26 @@ void OS_Run(MailConfig *mail)
         }
 
     }
+}
+
+/* print help statement */
+void help_local()
+{
+    print_header();
+    print_out("  %s: -[Vhdt] [-u user] [-g group] [-c config] [-D dir]", ARGV0);
+    print_out("    -V          Version and license message");
+    print_out("    -h          This help message");
+    print_out("    -d          Execute in debug mode. This parameter");
+    print_out("                can be specified up to two times");
+    print_out("                to increase the debug level.");
+    print_out("    -t          Test configuration");
+    print_out("    -f          Run in foreground");
+    print_out("    -u <user>   Run as 'user'");
+    print_out("    -g <group>  Run as 'group'");
+    print_out("    -c <config> Read the 'config' file");
+    print_out("    -D <dir>    Chroot to 'dir'");
+    print_out(" ");
+    exit(1);
 }
 
 /* EOF */
