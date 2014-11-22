9f282f0616fd4515ba071d56a439346379717c43
awiddersheim <awiddersheim@hotmail.com>
2014-11-22 08:56:16 -0500

    Fix formatting of event channel code
    Remove excess whitespace among other things. No code changes made.

--- src/logcollector/read_win_event_channel.c
+++ src/logcollector/read_win_event_channel.c
@@ -73,9 +73,9 @@ char *convert_windows_string(LPCWSTR string)
 
 	if (string == NULL)
 		return (NULL);
-	
+
 	wcstombs_s(&len, new_value, OS_MAXSTR, string, OS_MAXSTR - 1);
-	
+
 	return (strdup(new_value));
 }
 
@@ -92,15 +92,15 @@ void get_username_and_domain(os_event *event)
 	DWORD user_length = 0;
 	DWORD domain_length = 0;
 	SID_NAME_USE account_type;
-	
+
 	LookupAccountSid(NULL, event->uid, NULL, &user_length,
 					 NULL, &domain_length, &account_type);
-	
+
 	if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
 	{
 		event->user = calloc(user_length, sizeof (char));
 		event->domain = calloc(domain_length, sizeof (char));
-		
+
 		if ((event->user != NULL) && (event->domain != NULL))
 			LookupAccountSid(NULL, event->uid, event->user, &user_length,
 							 event->domain, &domain_length, &account_type);
@@ -117,15 +117,15 @@ void get_messages(os_event *event, EVT_HANDLE evt, LPCWSTR publisher_name)
 	EVT_HANDLE publisher;
 	DWORD size = 0;
 	wchar_t *buffer = NULL;
-	
+
 	publisher = EvtOpenPublisherMetadata(NULL, publisher_name, NULL, 0, 0);
-	
+
 	EvtFormatMessage(publisher, evt, 0, 0, NULL, EvtFormatMessageEvent, 0, NULL, &size);
 	buffer = calloc(size, sizeof (wchar_t));
 	EvtFormatMessage(publisher, evt, 0, 0, NULL, EvtFormatMessageEvent, size, buffer, &size);
 	event->message = convert_windows_string(buffer);
 	free(buffer);
-	
+
 	EvtFormatMessage(publisher, evt, 0, 0, NULL, EvtFormatMessageLevel, 0, NULL, &size);
 	buffer = calloc(size, sizeof (wchar_t));
 	EvtFormatMessage(publisher, evt, 0, 0, NULL, EvtFormatMessageLevel, size, buffer, &size);
@@ -139,10 +139,10 @@ void update_bookmark(EVT_HANDLE evt, os_channel *context)
 	DWORD count = 0;
 	wchar_t *buffer = NULL;
 	int i = 0;
-	
+
 	EvtUpdateBookmark(context->bookmark, evt);
 	EvtRender(NULL, context->bookmark, EvtRenderBookmark, 0, NULL, &size, &count);
-	
+
 	buffer = calloc(size, 1);
 	if (buffer == NULL)
 	{
@@ -156,11 +156,11 @@ void update_bookmark(EVT_HANDLE evt, os_channel *context)
 		fseek(context->bookmark_file, 0, SEEK_SET);
 		if (fwrite(buffer, 1, size, context->bookmark_file) < size)
 			merror("%s: could not save bookmark (%ld)", ARGV0, GetLastError());
-			
+
 		// Write spaces to be certain to overwrite previous content
 		for (i = 0; i < size; ++i)
-			fputc(' ', context->bookmark_file); 
-	
+			fputc(' ', context->bookmark_file);
+
 		fflush(context->bookmark_file);
 	}
 }
@@ -183,7 +183,7 @@ char *WinEvtTimeToString(ULONGLONG ulongTime)
 
 	/* Convert from ULONGLONG to usable FILETIME value */
 	ulargeTime.QuadPart = ulongTime;
-	
+
 	fTime.dwLowDateTime = ulargeTime.LowPart;
 	fTime.dwHighDateTime = ulargeTime.HighPart;
 
@@ -214,8 +214,10 @@ char *WinEvtTimeToString(ULONGLONG ulongTime)
 	return (result);
 
 error:
-	if (result) free(result);
-	return NULL; 
+	if (result)
+		free(result);
+
+	return NULL;
 }
 
 void send_channel_event(EVT_HANDLE evt, os_channel *channel)
@@ -231,30 +233,30 @@ void send_channel_event(EVT_HANDLE evt, os_channel *channel)
 		L"Event/System/Provider/@Name",
 		L"Event/System/TimeCreated/@SystemTime"
 	};
-    DWORD count = sizeof(properties)/sizeof(LPWSTR);
+    	DWORD count = sizeof(properties)/sizeof(LPWSTR);
 	EVT_HANDLE context = NULL;
 	os_event event;
 	char final_msg[OS_MAXSTR];
 	char *timestamp;
-	
+
 	context = EvtCreateRenderContext(count, properties, EvtRenderContextValues);
-	
+
 	EvtRender(context, evt, EvtRenderEventValues, 0, NULL, &buffer_length, &count);
-	
+
 	if (NULL == (properties_values = malloc(buffer_length))) {
 		merror("%s: Not enough memory, could not process event", ARGV0);
 		return;
 	}
-	
+
 	EvtRender(context, evt, EvtRenderEventValues, buffer_length, properties_values, &buffer_length, &count);
-	
+
 	event.name = get_property_value(&properties_values[0]);
 	event.id = properties_values[1].UInt16Val;
 	event.source = get_property_value(&properties_values[2]);
 	event.uid = properties_values[3].Type == EvtVarTypeNull ? NULL : properties_values[3].SidVal;
 	event.computer = get_property_value(&properties_values[4]);
 	event.time_created = properties_values[6].FileTimeVal;
-	
+
 	get_username_and_domain(&event);
 	get_messages(&event, evt, properties_values[5].StringVal);
 
@@ -273,26 +275,24 @@ void send_channel_event(EVT_HANDLE evt, os_channel *channel)
 	free(timestamp);
 
 	if(SendMSG(logr_queue, final_msg, "WinEvtLog", LOCALFILE_MQ) < 0)
-    {
+    	{
 		merror(QUEUE_SEND, ARGV0);
-    }
-    
-    if (channel->bookmark_enabled)
+    	}
+
+    	if (channel->bookmark_enabled)
 		update_bookmark(evt, channel);
-		
+
 	free(properties_values);
 	free_event(&event);
 }
 
-DWORD WINAPI event_channel_callback(EVT_SUBSCRIBE_NOTIFY_ACTION action,
-									os_channel *context,
-									EVT_HANDLE evt)
-{	
-    if (action == EvtSubscribeActionDeliver)
+DWORD WINAPI event_channel_callback(EVT_SUBSCRIBE_NOTIFY_ACTION action, os_channel *context, EVT_HANDLE evt)
+{
+	if (action == EvtSubscribeActionDeliver)
 	{
 		send_channel_event(evt, context);
 	}
-	
+
 	return (0);
 }
 
@@ -301,20 +301,20 @@ void win_start_event_channel(char *evt_log, char future, char *query)
 	wchar_t		*channel = NULL;
 	wchar_t		*wquery = NULL;
 	size_t		size = 0;
-	os_channel  *context = NULL;
+	os_channel  	*context = NULL;
 	DWORD		flags = EvtSubscribeToFutureEvents;
 	EVT_HANDLE	bookmark = NULL;
-	
+
 	size = strlen(evt_log) + 1;
 
 	channel = calloc(size, sizeof (wchar_t));
-	if(channel == NULL) 
+	if(channel == NULL)
 	{
 		merror("%s: Not enough memory, skipping %s", ARGV0, evt_log);
 		goto error;
         }
 	context = calloc(1, sizeof (os_channel));
-	
+
 	if (context == NULL)
 	{
 		merror("%s: Not enough memory, skipping %s", ARGV0, evt_log);
@@ -323,7 +323,7 @@ void win_start_event_channel(char *evt_log, char future, char *query)
 
 	// Convert 'evt_log' to windows string
 	mbstowcs_s(&size, channel, size, evt_log, size - 1);
-	
+
 	// Convert 'query' to windows string
 	if (query)
 	{
@@ -332,28 +332,28 @@ void win_start_event_channel(char *evt_log, char future, char *query)
 		if (wquery)
 			mbstowcs_s(&size, wquery, size, query, size - 1);
 	}
-	
+
 	context->bookmark_enabled = !future;
 
 	if (context->bookmark_enabled)
 	{
 		char        file_name[OS_MAXSTR];
 		wchar_t     bookmark_xml[OS_MAXSTR];
-	
-		snprintf(file_name, OS_MAXSTR, "%s/%s", BOOKMARKS_DIR, evt_log); 
+
+		snprintf(file_name, OS_MAXSTR, "%s/%s", BOOKMARKS_DIR, evt_log);
 		// Replace '/' by ' ' in the channel name
 		if (strchr(evt_log, '/'))
 			*(strrchr(file_name, '/')) = ' ';
-	
+
 		// If we have a stored bookmark, start from it
 		if (access(file_name, F_OK) != -1)
-		{	
+		{
 			// Open the file and read storet bookmark
 			context->bookmark_file = fopen(file_name, "r+");
 			fseek(context->bookmark_file, 0, SEEK_SET);
 			size = fread(bookmark_xml, sizeof (wchar_t), OS_MAXSTR, context->bookmark_file);
 			bookmark_xml[size] = L'\0';
-		
+
 			// Create bookmark from saved xml
 			context->bookmark = EvtCreateBookmark(bookmark_xml);
 			if (context->bookmark == NULL)
@@ -377,7 +377,7 @@ void win_start_event_channel(char *evt_log, char future, char *query)
 				merror("%s: could not create bookmark file %s (%ld)", ARGV0, file_name, GetLastError());
 		}
 	}
-	
+
 	if (EvtSubscribe(NULL, NULL, channel, wquery, bookmark, context,
 					 (EVT_SUBSCRIBE_CALLBACK)event_channel_callback,
 					 flags) == NULL)
@@ -390,17 +390,20 @@ void win_start_event_channel(char *evt_log, char future, char *query)
 							 EvtSubscribeToFutureEvents) == NULL)
 				merror("%s: Subscription error: %ld", ARGV0, GetLastError());
 		}
-		else 
+		else
 			merror("%s: Subscription error: %ld", ARGV0, GetLastError());
 	}
-	
+
 	free(channel);
 	return;
 
-error: 
-	if(channel) free(channel); 
-	if(context) free(context); 
-	return; 
+error:
+	if(channel)
+		free(channel);
+	if(context)
+		free(context);
+
+	return;
 }
 
 #endif
