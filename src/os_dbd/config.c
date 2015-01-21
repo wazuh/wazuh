39898f51014287bf0350b3e31246f6e75d9648a8
Wouter Clarie <wclarie@gmail.com>
2015-01-21 16:06:42 +0100

    os_dbd: Formatting

--- src/os_dbd/config.c
+++ src/os_dbd/config.c
@@ -1,6 +1,3 @@
-/* @(#) $Id: ./src/os_dbd/config.c, 2011/09/08 dcid Exp $
- */
-
 /* Copyright (C) 2009 Trend Micro Inc.
  * All rights reserved.
  *
@@ -8,36 +5,27 @@
  * and/or modify it under the terms of the GNU General Public
  * License (version 2) as published by the FSF - Free Software
  * Foundation.
- *
- * License details at the LICENSE file included with OSSEC or
- * online at: http://www.ossec.net/en/licensing.html
  */
-
 
 #include "dbd.h"
 #include "config/global-config.h"
 #include "config/config.h"
 
 
-/** int OS_ReadDBConf(int test_config, char *cfgfile, DBConfig *db_config)
- * Reads database configuration.
- */
+/* Read database configuration */
 int OS_ReadDBConf(__attribute__((unused)) int test_config, const char *cfgfile, DBConfig *db_config)
 {
     int modules = 0;
     _Config *tmp_config;
 
-
     /* Modules for the configuration */
-    modules|= CDBD;
-    modules|= CRULES;
+    modules |= CDBD;
+    modules |= CRULES;
 
-
-    /* Allocating config just to get the rules. */
+    /* Allocate config just to get the rules */
     os_calloc(1, sizeof(_Config), tmp_config);
 
-
-    /* Clearing configuration variables */
+    /* Clear configuration variables */
     tmp_config->includes = NULL;
     db_config->includes = NULL;
     db_config->host = NULL;
@@ -49,93 +37,76 @@ int OS_ReadDBConf(__attribute__((unused)) int test_config, const char *cfgfile, 
     db_config->db_type = 0;
     db_config->maxreconnect = 0;
 
+    /* Read configuration */
+    if (ReadConfig(modules, cfgfile, tmp_config, db_config) < 0) {
+        return (OS_INVALID);
+    }
 
-    /* Reading configuration */
-    if(ReadConfig(modules, cfgfile, tmp_config, db_config) < 0)
-        return(OS_INVALID);
-
-
-    /* Here, we assign the rules to db_config and free the rest
-     * of the Config.
-     */
+    /* Assign the rules to db_config and free the rest of the Config */
     db_config->includes = tmp_config->includes;
     free(tmp_config);
 
-
-    /* Checking if dbd isn't supposed to run. */
-    if(!db_config->host &&
-       !db_config->user &&
-       !db_config->pass &&
-       !db_config->db &&
-       !db_config->sock &&
-       !db_config->port &&
-       !db_config->db_type)
-    {
-        return(0);
+    /* Check if dbd isn't supposed to run */
+    if (!db_config->host &&
+            !db_config->user &&
+            !db_config->pass &&
+            !db_config->db &&
+            !db_config->sock &&
+            !db_config->port &&
+            !db_config->db_type) {
+        return (0);
     }
 
-
-    /* Checking for a valid config. */
-    if(!db_config->host ||
-       !db_config->user ||
-       !db_config->pass ||
-       !db_config->db ||
-       !db_config->db_type)
-    {
+    /* Check for a valid config */
+    if (!db_config->host ||
+            !db_config->user ||
+            !db_config->pass ||
+            !db_config->db ||
+            !db_config->db_type) {
         merror(DB_MISS_CONFIG, ARGV0);
-        return(OS_INVALID);
+        return (OS_INVALID);
     }
 
     osdb_connect = NULL;
 
-    /* Assigning the proper location for the function calls */
-    #ifdef MYSQL_DATABASE_ENABLED
-    if(db_config->db_type == MYSQLDB)
-    {
+    /* Assign the proper location for the function calls */
+
+#ifdef MYSQL_DATABASE_ENABLED
+    if (db_config->db_type == MYSQLDB) {
         osdb_connect = mysql_osdb_connect;
         osdb_query_insert = mysql_osdb_query_insert;
         osdb_query_select = mysql_osdb_query_select;
         osdb_close = mysql_osdb_close;
     }
-    #endif
+#endif
 
-    #ifdef PGSQL_DATABASE_ENABLED
-    if(db_config->db_type == POSTGDB)
-    {
+#ifdef PGSQL_DATABASE_ENABLED
+    if (db_config->db_type == POSTGDB) {
         osdb_connect = postgresql_osdb_connect;
         osdb_query_insert = postgresql_osdb_query_insert;
         osdb_query_select = postgresql_osdb_query_select;
         osdb_close = postgresql_osdb_close;
     }
-    #endif
+#endif
 
-
-
-    /* Checking for config errros (moving from config.c).
-     */
-    if(db_config->db_type == MYSQLDB)
-    {
-        #ifndef MYSQL_DATABASE_ENABLED
+    /* Check for config errros */
+    if (db_config->db_type == MYSQLDB) {
+#ifndef MYSQL_DATABASE_ENABLED
         merror(DB_COMPILED, ARGV0, "mysql");
-        return(OS_INVALID);
-        #endif
-    }
-    else if(db_config->db_type == POSTGDB)
-    {
-        #ifndef PGSQL_DATABASE_ENABLED
+        return (OS_INVALID);
+#endif
+    } else if (db_config->db_type == POSTGDB) {
+#ifndef PGSQL_DATABASE_ENABLED
         merror(DB_COMPILED, ARGV0, "postgresql");
-        return(OS_INVALID);
-        #endif
+        return (OS_INVALID);
+#endif
     }
 
-
-    if(osdb_connect == NULL)
-    {
+    if (osdb_connect == NULL) {
         merror("%s: Invalid DB configuration (Internal error?). ", ARGV0);
-        return(OS_INVALID);
+        return (OS_INVALID);
     }
 
-    return(1);
+    return (1);
 }
 
-/* EOF */
--- src/os_dbd/main.c
+++ src/os_dbd/main.c
@@ -1,6 +1,3 @@
-/* @(#) $Id: ./src/os_dbd/main.c, 2011/09/08 dcid Exp $
- */
-
 /* Copyright (C) 2009 Trend Micro Inc.
  * All rights reserved.
  *
@@ -8,40 +5,37 @@
  * and/or modify it under the terms of the GNU General Public
  * License (version 2) as published by the FSF - Free Software
  * Foundation.
- *
- * License details at the LICENSE file included with OSSEC or
- * online at: http://www.ossec.net/en/licensing.html
  */
-
-
-#ifndef ARGV0
-   #define ARGV0 "ossec-dbd"
-#endif
 
 #include "shared.h"
 #include "dbd.h"
 
+#ifndef ARGV0
+#define ARGV0 "ossec-dbd"
+#endif
+
+/* Prototypes */
 static void print_db_info(void);
 static void help_dbd(void) __attribute__((noreturn));
 
 
-/* Prints information regarding enabled databases */
+/* Print information regarding enabled databases */
 static void print_db_info()
 {
-    #ifdef MYSQL_DATABASE_ENABLED
+#ifdef UMYSQL_DATABASE_ENABLED
     print_out("    Compiled with MySQL support");
-    #endif
+#endif
 
-    #ifdef PGSQL_DATABASE_ENABLED
+#ifdef PGSQL_DATABASE_ENABLED
     print_out("    Compiled with PostgreSQL support");
-    #endif
+#endif
 
-    #if !defined(MYSQL_DATABASE_ENABLED) && !defined(PGSQL_DATABASE_ENABLED)
+#if !defined(MYSQL_DATABASE_ENABLED) && !defined(PGSQL_DATABASE_ENABLED)
     print_out("    Compiled without any database support");
-    #endif
+#endif
 }
 
-/* print help statement */
+/* Print help statement */
 static void help_dbd()
 {
     print_header();
@@ -71,24 +65,21 @@ int main(int argc, char **argv)
     gid_t gid;
     unsigned int d;
 
-    /* Using MAILUSER (read only) */
+    /* Use MAILUSER (read only) */
     const char *dir  = DEFAULTDIR;
     const char *user = MAILUSER;
     const char *group = GROUPGLOBAL;
     const char *cfg = DEFAULTCPATH;
 
-
     /* Database Structure */
     DBConfig db_config;
     db_config.error_count = 0;
 
-
-    /* Setting the name */
+    /* Set the name */
     OS_SetName(ARGV0);
 
-
-    while((c = getopt(argc, argv, "Vdhtfu:g:D:c:")) != -1){
-        switch(c){
+    while ((c = getopt(argc, argv, "Vdhtfu:g:D:c:")) != -1) {
+        switch (c) {
             case 'V':
                 print_version();
                 break;
@@ -102,23 +93,27 @@ int main(int argc, char **argv)
                 run_foreground = 1;
                 break;
             case 'u':
-                if(!optarg)
-                    ErrorExit("%s: -u needs an argument",ARGV0);
-                user=optarg;
+                if (!optarg) {
+                    ErrorExit("%s: -u needs an argument", ARGV0);
+                }
+                user = optarg;
                 break;
             case 'g':
-                if(!optarg)
-                    ErrorExit("%s: -g needs an argument",ARGV0);
-                group=optarg;
+                if (!optarg) {
+                    ErrorExit("%s: -g needs an argument", ARGV0);
+                }
+                group = optarg;
                 break;
             case 'D':
-                if(!optarg)
-                    ErrorExit("%s: -D needs an argument",ARGV0);
-                dir=optarg;
+                if (!optarg) {
+                    ErrorExit("%s: -D needs an argument", ARGV0);
+                }
+                dir = optarg;
                 break;
             case 'c':
-                if(!optarg)
-                    ErrorExit("%s: -c needs an argument",ARGV0);
+                if (!optarg) {
+                    ErrorExit("%s: -c needs an argument", ARGV0);
+                }
                 cfg = optarg;
                 break;
             case 't':
@@ -128,78 +123,61 @@ int main(int argc, char **argv)
                 help_dbd();
                 break;
         }
-
     }
 
-
-    /* Starting daemon */
+    /* Start daemon */
     debug1(STARTED_MSG, ARGV0);
-
 
     /* Check if the user/group given are valid */
     uid = Privsep_GetUser(user);
     gid = Privsep_GetGroup(group);
-    if(uid == (uid_t)-1 || gid == (gid_t)-1)
-    {
+    if (uid == (uid_t) - 1 || gid == (gid_t) - 1) {
         ErrorExit(USER_ERROR, ARGV0, user, group);
     }
 
-
-    /* Reading configuration */
-    if((c = OS_ReadDBConf(test_config, cfg, &db_config)) < 0)
-    {
+    /* Read configuration */
+    if ((c = OS_ReadDBConf(test_config, cfg, &db_config)) < 0) {
         ErrorExit(CONFIG_ERROR, ARGV0, cfg);
     }
 
-
     /* Exit here if test config is set */
-    if(test_config)
+    if (test_config) {
         exit(0);
+    }
 
-
-    if(!run_foreground)
-    {
+    if (!run_foreground) {
         /* Going on daemon mode */
         nowDaemon();
         goDaemon();
     }
 
-
-
     /* Not configured */
-    if(c == 0)
-    {
+    if (c == 0) {
         verbose("%s: Database not configured. Clean exit.", ARGV0);
         exit(0);
     }
 
-
     /* Maybe disable this debug? */
     debug1("%s: DEBUG: Connecting to '%s', using '%s', '%s', '%s', %d,'%s'.",
            ARGV0, db_config.host, db_config.user,
-           db_config.pass, db_config.db,db_config.port,db_config.sock);
+           db_config.pass, db_config.db, db_config.port, db_config.sock);
 
-
-    /* Setting config pointer */
+    /* Set config pointer */
     osdb_setconfig(&db_config);
 
-
-    /* Getting maximum reconned attempts */
+    /* Get maximum reconnect attempts */
     db_config.maxreconnect = (unsigned int) getDefine_Int("dbd",
-                                           "reconnect_attempts", 1, 9999);
+                             "reconnect_attempts", 1, 9999);
 
-
-    /* Connecting to the database */
+    /* Connect to the database */
     d = 0;
-    while(d <= (db_config.maxreconnect * 10))
-    {
+    while (d <= (db_config.maxreconnect * 10)) {
         db_config.conn = osdb_connect(db_config.host, db_config.user,
                                       db_config.pass, db_config.db,
-                                      db_config.port,db_config.sock);
+                                      db_config.port, db_config.sock);
 
         /* If we are able to reconnect, keep going */
-        if(db_config.conn)
-        {
+        if (db_config.conn) {
             break;
         }
 
@@ -208,74 +186,60 @@ int main(int argc, char **argv)
 
     }
 
-
-    /* If after the maxreconnect attempts, it still didn't work, exit here. */
-    if(!db_config.conn)
-    {
+    /* If after the maxreconnect attempts, it still didn't work, exit here */
+    if (!db_config.conn) {
         merror(DB_CONFIGERR, ARGV0);
         ErrorExit(CONFIG_ERROR, ARGV0, cfg);
     }
-
 
     /* We must notify that we connected -- easy debugging */
     verbose("%s: Connected to database '%s' at '%s'.",
             ARGV0, db_config.db, db_config.host);
 
-
     /* Privilege separation */
-    if(Privsep_SetGroup(gid) < 0)
-        ErrorExit(SETGID_ERROR,ARGV0,group, errno, strerror(errno));
+    if (Privsep_SetGroup(gid) < 0) {
+        ErrorExit(SETGID_ERROR, ARGV0, group, errno, strerror(errno));
+    }
 
+    /* chroot */
+    if (Privsep_Chroot(dir) < 0) {
+        ErrorExit(CHROOT_ERROR, ARGV0, dir, errno, strerror(errno));
+    }
 
-    /* chrooting */
-    if(Privsep_Chroot(dir) < 0)
-        ErrorExit(CHROOT_ERROR,ARGV0,dir, errno, strerror(errno));
-
-
-    /* Now on chroot */
+    /* Now in chroot */
     nowChroot();
 
-
-    /* Inserting server info into the db */
+    /* Insert server info into the db */
     db_config.server_id = OS_Server_ReadInsertDB(&db_config);
-    if(db_config.server_id <= 0)
-    {
+    if (db_config.server_id <= 0) {
         ErrorExit(CONFIG_ERROR, ARGV0, cfg);
     }
-
 
     /* Read rules and insert into the db */
-    if(OS_InsertRulesDB(&db_config) < 0)
-    {
+    if (OS_InsertRulesDB(&db_config) < 0) {
         ErrorExit(CONFIG_ERROR, ARGV0, cfg);
     }
 
+    /* Change user */
+    if (Privsep_SetUser(uid) < 0) {
+        ErrorExit(SETUID_ERROR, ARGV0, user, errno, strerror(errno));
+    }
 
-    /* Changing user */
-    if(Privsep_SetUser(uid) < 0)
-        ErrorExit(SETUID_ERROR,ARGV0,user, errno, strerror(errno));
-
-
-    /* Basic start up completed. */
-    debug1(PRIVSEP_MSG,ARGV0,dir,user);
-
+    /* Basic start up completed */
+    debug1(PRIVSEP_MSG, ARGV0, dir, user);
 
     /* Signal manipulation */
     StartSIG(ARGV0);
 
-
-    /* Creating PID files */
-    if(CreatePID(ARGV0, getpid()) < 0)
-        ErrorExit(PID_ERROR,ARGV0);
-
+    /* Create PID files */
+    if (CreatePID(ARGV0, getpid()) < 0) {
+        ErrorExit(PID_ERROR, ARGV0);
+    }
 
     /* Start up message */
     verbose(STARTUP_MSG, ARGV0, (int)getpid());
 
-
-    /* the real daemon now */
+    /* The real daemon now */
     OS_DBD(&db_config);
 }
 
-
-/* EOF */
