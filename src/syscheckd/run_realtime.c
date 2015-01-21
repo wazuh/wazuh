91b1f46480821c1ad4641885736aabc28dcd419c
Wouter Clarie <wclarie@gmail.com>
2015-01-21 16:15:33 +0100

    syscheckd: Formatting

--- src/syscheckd/run_realtime.c
+++ src/syscheckd/run_realtime.c
@@ -1,6 +1,3 @@
-/* @(#) $Id: ./src/syscheckd/run_realtime.c, 2011/09/08 dcid Exp $
- */
-
 /* Copyright (C) 2009 Trend Micro Inc.
  * All right reserved.
  *
@@ -10,7 +7,6 @@
  * Foundation
  */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,13 +15,11 @@
 #include <limits.h>
 #include <errno.h>
 
-
 #ifdef WIN32
 #define sleep(x) Sleep(x * 1000)
 #define os_calloc(x,y,z) (z = calloc(x,y))?(void)1:ErrorExit(MEM_ERROR, ARGV0, errno, strerror(errno))
 #define os_strdup(x,y) (y = strdup(x))?(void)1:ErrorExit(MEM_ERROR, ARGV0, errno, strerror(errno))
 #endif
-
 
 #ifdef INOTIFY_ENABLED
 #include <sys/inotify.h>
@@ -35,152 +29,119 @@
 #include "shared.h"
 #endif
 
-
 #include "hash_op.h"
 #include "debug_op.h"
 #include "syscheck.h"
 #include "error_messages/error_messages.h"
 
-//static int realtime_checksumfile(const char *file_name) __attribute__((nonnull));
+/* Prototypes */
 int realtime_checksumfile(const char *file_name) __attribute__((nonnull));
 
-/* Checking sum of the realtime file being monitored. */
-//static int realtime_checksumfile(const char *file_name)
+
+/* Checksum of the realtime file being monitored */
 int realtime_checksumfile(const char *file_name)
 {
     char *buf;
 
     buf = (char *) OSHash_Get(syscheck.fp, file_name);
-    if(buf != NULL)
-    {
-        char c_sum[256 +2];
+    if (buf != NULL) {
+        char c_sum[256 + 2];
 
         c_sum[0] = '\0';
         c_sum[255] = '\0';
 
+        /* If it returns < 0, we have already alerted */
+        if (c_read_file(file_name, buf, c_sum) < 0) {
+            return (0);
+        }
 
-         /* If it returns < 0, we will already have alerted. */
-         if(c_read_file(file_name, buf, c_sum) < 0)
-         {
-             return(0);
-         }
+        if (strcmp(c_sum, buf + 6) != 0) {
+            char *fullalert = NULL;
+            char alert_msg[OS_MAXSTR + 1];
 
+            alert_msg[OS_MAXSTR] = '\0';
 
-         if(strcmp(c_sum, buf+6) != 0)
-         {
-             char *fullalert = NULL;
-             char alert_msg[OS_MAXSTR +1];
-             alert_msg[OS_MAXSTR] = '\0';
-             if(buf[5] == 's' || buf[5] == 'n')
-             {
-                 fullalert = seechanges_addfile(file_name);
-                 if(fullalert)
-                 {
+            if (buf[5] == 's' || buf[5] == 'n') {
+                fullalert = seechanges_addfile(file_name);
+                if (fullalert) {
                     snprintf(alert_msg, OS_MAXSTR, "%s %s\n%s", c_sum, file_name, fullalert);
                     free(fullalert);
                     fullalert = NULL;
-                 }
-                 else
-                 {
-                     snprintf(alert_msg, 912, "%s %s", c_sum, file_name);
-                 }
-             }
-             else
-             {
-                 snprintf(alert_msg, 912, "%s %s", c_sum, file_name);
-             }
-             send_syscheck_msg(alert_msg);
+                } else {
+                    snprintf(alert_msg, 912, "%s %s", c_sum, file_name);
+                }
+            } else {
+                snprintf(alert_msg, 912, "%s %s", c_sum, file_name);
+            }
+            send_syscheck_msg(alert_msg);
 
-             return(1);
-         }
-
-         return(0);
-
+            return (1);
+        }
+        return (0);
     }
-
-    return(0);
+    return (0);
 }
-
-
-
 
 #ifdef INOTIFY_ENABLED
 #include <sys/inotify.h>
-
 
 #define REALTIME_MONITOR_FLAGS  IN_MODIFY|IN_ATTRIB|IN_MOVED_FROM|IN_MOVED_TO|IN_CREATE|IN_DELETE|IN_DELETE_SELF
 #define REALTIME_EVENT_SIZE     (sizeof (struct inotify_event))
 #define REALTIME_EVENT_BUFFER   (2048 * (REALTIME_EVENT_SIZE + 16))
 
-
-
-/* Starts real time monitoring using inotify. */
+/* Start real time monitoring using inotify */
 int realtime_start()
 {
     verbose("%s: INFO: Initializing real time file monitoring (not started).", ARGV0);
 
     syscheck.realtime = (rtfim *) calloc(1, sizeof(rtfim));
-    if(syscheck.realtime == NULL)
-    {
+    if (syscheck.realtime == NULL) {
         ErrorExit(MEM_ERROR, ARGV0, errno, strerror(errno));
     }
     syscheck.realtime->dirtb = OSHash_Create();
     syscheck.realtime->fd = -1;
 
-    #ifdef INOTIFY_ENABLED
+#ifdef INOTIFY_ENABLED
     syscheck.realtime->fd = inotify_init();
-    if(syscheck.realtime->fd < 0)
-    {
+    if (syscheck.realtime->fd < 0) {
         merror("%s: ERROR: Unable to initialize inotify.", ARGV0);
-        return(-1);
+        return (-1);
     }
-    #endif
+#endif
 
-    return(1);
+    return (1);
 }
 
-
-
-/* Adds a directory to real time checking. */
+/* Add a directory to real time checking */
 int realtime_adddir(const char *dir)
 {
-    if(!syscheck.realtime)
-    {
+    if (!syscheck.realtime) {
         realtime_start();
     }
 
-
-    /* Checking if it is ready to use. */
-    if(syscheck.realtime->fd < 0)
-    {
-        return(-1);
-    }
-    else
-    {
+    /* Check if it is ready to use */
+    if (syscheck.realtime->fd < 0) {
+        return (-1);
+    } else {
         int wd = 0;
 
         wd = inotify_add_watch(syscheck.realtime->fd,
                                dir,
                                REALTIME_MONITOR_FLAGS);
-        if(wd < 0)
-        {
+        if (wd < 0) {
             merror("%s: ERROR: Unable to add directory to real time "
                    "monitoring: '%s'. %d %d", ARGV0, dir, wd, errno);
-        }
-        else
-        {
-            char wdchar[32 +1];
+        } else {
+            char wdchar[32 + 1];
             wdchar[32] = '\0';
             snprintf(wdchar, 32, "%d", wd);
 
-            /* Entry not present. */
-            if(!OSHash_Get(syscheck.realtime->dirtb, wdchar))
-            {
+            /* Entry not present */
+            if (!OSHash_Get(syscheck.realtime->dirtb, wdchar)) {
                 char *ndir;
 
                 ndir = strdup(dir);
-                if(ndir == NULL)
-                {
+                if (ndir == NULL) {
                     ErrorExit("%s: ERROR: Out of memory. Exiting.", ARGV0);
                 }
 
@@ -191,36 +152,29 @@ int realtime_adddir(const char *dir)
         }
     }
 
-    return(1);
+    return (1);
 }
 
-
-/* Process events in the real time queue. */
+/* Process events in the real time queue */
 int realtime_process()
 {
     ssize_t len;
     size_t i = 0;
-    char buf[REALTIME_EVENT_BUFFER +1];
+    char buf[REALTIME_EVENT_BUFFER + 1];
     struct inotify_event *event;
 
     buf[REALTIME_EVENT_BUFFER] = '\0';
 
-
     len = read(syscheck.realtime->fd, buf, REALTIME_EVENT_BUFFER);
-    if (len < 0)
-    {
+    if (len < 0) {
         merror("%s: ERROR: Unable to read from real time buffer.", ARGV0);
-    }
-    else if (len > 0)
-    {
-        while (i < (size_t) len)
-        {
+    } else if (len > 0) {
+        while (i < (size_t) len) {
             event = (struct inotify_event *) (void *) &buf[i];
 
-            if(event->len)
-            {
-                char wdchar[32 +1];
-                char final_name[MAX_LINE +1];
+            if (event->len) {
+                char wdchar[32 + 1];
+                char final_name[MAX_LINE + 1];
 
                 wdchar[32] = '\0';
                 final_name[MAX_LINE] = '\0';
@@ -237,20 +191,17 @@ int realtime_process()
         }
     }
 
-    return(0);
+    return (0);
 }
 
-
-
 #elif defined(WIN32)
-typedef struct _win32rtfim
-{
+typedef struct _win32rtfim {
     HANDLE h;
     OVERLAPPED overlap;
 
     char *dir;
     TCHAR buffer[12288];
-}win32rtfim;
+} win32rtfim;
 
 int realtime_win32read(win32rtfim *rtlocald);
 
@@ -258,45 +209,35 @@ void CALLBACK RTCallBack(DWORD dwerror, DWORD dwBytes, LPOVERLAPPED overlap)
 {
     int lcount;
     size_t offset = 0;
-
     char *ptfile;
-    char wdchar[32 +1];
-    char final_path[MAX_LINE +1];
-
+    char wdchar[32 + 1];
+    char final_path[MAX_LINE + 1];
     win32rtfim *rtlocald;
-
     PFILE_NOTIFY_INFORMATION pinfo;
     TCHAR finalfile[MAX_PATH];
 
-    if(dwBytes == 0)
-    {
+    if (dwBytes == 0) {
         merror("%s: ERROR: real time call back called, but 0 bytes.", ARGV0);
         return;
     }
 
-    if(dwerror != ERROR_SUCCESS)
-    {
+    if (dwerror != ERROR_SUCCESS) {
         merror("%s: ERROR: real time call back called, but error is set.",
                ARGV0);
         return;
     }
 
-
-    /* Getting hash to parse the data. */
+    /* Get hash to parse the data */
     wdchar[32] = '\0';
     snprintf(wdchar, 32, "%d", (int)overlap->Offset);
     rtlocald = OSHash_Get(syscheck.realtime->dirtb, wdchar);
-    if(rtlocald == NULL)
-    {
+    if (rtlocald == NULL) {
         merror("%s: ERROR: real time call back called, but hash is empty.",
                ARGV0);
         return;
     }
 
-
-
-    do
-    {
+    do {
         pinfo = (PFILE_NOTIFY_INFORMATION) &rtlocald->buffer[offset];
         offset += pinfo->NextEntryOffset;
 
@@ -305,11 +246,9 @@ void CALLBACK RTCallBack(DWORD dwerror, DWORD dwBytes, LPOVERLAPPED overlap)
                                      finalfile, MAX_PATH - 1, NULL, NULL);
         finalfile[lcount] = TEXT('\0');
 
-
-        /* Change forward slashes to backslashes on finalfile. */
+        /* Change forward slashes to backslashes on finalfile */
         ptfile = strchr(finalfile, '\\');
-        while(ptfile)
-        {
+        while (ptfile) {
             *ptfile = '/';
             ptfile++;
 
@@ -319,30 +258,14 @@ void CALLBACK RTCallBack(DWORD dwerror, DWORD dwBytes, LPOVERLAPPED overlap)
         final_path[MAX_LINE] = '\0';
         snprintf(final_path, MAX_LINE, "%s/%s", rtlocald->dir, finalfile);
 
-
-        /* Checking the change. */
+        /* Check the change */
         realtime_checksumfile(final_path);
-
-
-        /*
-        if(pinfo->Action == FILE_ACTION_ADDED)
-        else if(pinfo->Action == FILE_ACTION_REMOVED)
-        else if(pinfo->Action == FILE_ACTION_MODIFIED)
-        else if(pinfo->Action == FILE_ACTION_RENAMED_OLD_NAME)
-        else if(pinfo->Action == FILE_ACTION_RENAMED_NEW_NAME)
-        else
-        */
-
-    }while(pinfo->NextEntryOffset != 0);
-
+    } while (pinfo->NextEntryOffset != 0);
 
     realtime_win32read(rtlocald);
 
-
     return;
 }
-
-
 
 int realtime_start()
 {
@@ -352,7 +275,8 @@ int realtime_start()
     syscheck.realtime->dirtb = (void *)OSHash_Create();
     syscheck.realtime->fd = -1;
     syscheck.realtime->evt = CreateEvent(NULL, TRUE, FALSE, NULL);
-    return(0);
+
+    return (0);
 }
 
 int realtime_win32read(win32rtfim *rtlocald)
@@ -363,117 +287,98 @@ int realtime_win32read(win32rtfim *rtlocald)
                                rtlocald->buffer,
                                sizeof(rtlocald->buffer) / sizeof(TCHAR),
                                TRUE,
-                               FILE_NOTIFY_CHANGE_FILE_NAME|FILE_NOTIFY_CHANGE_DIR_NAME|FILE_NOTIFY_CHANGE_SIZE|FILE_NOTIFY_CHANGE_LAST_WRITE,
+                               FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE,
                                0,
                                &rtlocald->overlap,
                                RTCallBack);
-    if(rc == 0)
-    {
+    if (rc == 0) {
         merror("%s: ERROR: Unable to set directory for monitoring: %s",
                ARGV0, rtlocald->dir);
         sleep(2);
     }
 
-    return(0);
+    return (0);
 }
 
 int realtime_adddir(const char *dir)
 {
-    char wdchar[32 +1];
+    char wdchar[32 + 1];
     win32rtfim *rtlocald;
 
-
-    if(!syscheck.realtime)
-    {
+    if (!syscheck.realtime) {
         realtime_start();
     }
 
-
-    /* Maximum limit for realtime on Windows. */
-    if(syscheck.realtime->fd > 256)
-    {
+    /* Maximum limit for realtime on Windows */
+    if (syscheck.realtime->fd > 256) {
         merror("%s: ERROR: Unable to add directory to real time "
                "monitoring: '%s' - Maximum size permitted.", ARGV0, dir);
-        return(0);
+        return (0);
     }
-
 
     os_calloc(1, sizeof(win32rtfim), rtlocald);
 
-
     rtlocald->h = CreateFile(dir,
                              FILE_LIST_DIRECTORY,
-                             FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE,
+                             FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
                              NULL,
                              OPEN_EXISTING,
-                             FILE_FLAG_BACKUP_SEMANTICS|FILE_FLAG_OVERLAPPED,
+                             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
                              NULL);
 
 
-    if(rtlocald->h == INVALID_HANDLE_VALUE ||
-       rtlocald->h == NULL)
-    {
+    if (rtlocald->h == INVALID_HANDLE_VALUE ||
+            rtlocald->h == NULL) {
         free(rtlocald);
         rtlocald = NULL;
         merror("%s: ERROR: Unable to add directory to real time "
                "monitoring: '%s'.", ARGV0, dir);
-        return(0);
+        return (0);
     }
 
     rtlocald->overlap.Offset = ++syscheck.realtime->fd;
 
-
-
-    /* Setting key for hash. */
+    /* Set key for hash */
     wdchar[32] = '\0';
     snprintf(wdchar, 32, "%d", (int)rtlocald->overlap.Offset);
 
-
-    if(OSHash_Get(syscheck.realtime->dirtb, wdchar))
-    {
+    if (OSHash_Get(syscheck.realtime->dirtb, wdchar)) {
         merror("%s: ERROR: Entry already in the real time hash: %s",
                ARGV0, wdchar);
         CloseHandle(rtlocald->overlap.hEvent);
         free(rtlocald);
         rtlocald = NULL;
-        return(0);
+        return (0);
     }
 
-
-    /* Adding final elements to the hash. */
+    /* Add final elements to the hash */
     os_strdup(dir, rtlocald->dir);
-
     OSHash_Add(syscheck.realtime->dirtb, strdup(wdchar), rtlocald);
 
-
-    /* Adding directory to be monitored. */
+    /* Add directory to be monitored */
     realtime_win32read(rtlocald);
 
-
-    return(1);
+    return (1);
 }
 
+#else /* !WIN32 */
 
-
-
-
-#else
 int realtime_start()
 {
     verbose("%s: ERROR: Unable to initalize real time file monitoring.", ARGV0);
-    return(0);
+
+    return (0);
 }
 
 int realtime_adddir(__attribute__((unused)) const char *dir)
 {
-    return(0);
+    return (0);
 }
-
 
 int realtime_process()
 {
-    return(0);
+    return (0);
 }
 
-#endif
-/* EOF */
+#endif /* WIN32 */
+
--- src/syscheckd/seechanges.c
+++ src/syscheckd/seechanges.c
@@ -1,6 +1,3 @@
-/* @(#) $Id: ./src/syscheckd/seechanges.c, 2011/09/08 dcid Exp $
- */
-
 /* Copyright (C) 2009 Trend Micro Inc.
  * All rights reserved.
  *
@@ -10,47 +7,48 @@
  * Foundation
  */
 
-
-
 #include "shared.h"
 #include "os_crypto/md5/md5_op.h"
 #include "syscheck.h"
 
+/* Prototypes */
 static char *gen_diff_alert(const char *filename, time_t alert_diff_time) __attribute__((nonnull));
 static int seechanges_dupfile(const char *old, const char *new) __attribute__((nonnull));
 static int seechanges_createpath(const char *filename) __attribute__((nonnull));
 
 #ifdef USE_MAGIC
 #include <magic.h>
+
+/* Global variables */
 extern magic_t magic_cookie;
 
-int is_text(magic_t cookie, const void* buf, size_t len)
+
+int is_text(magic_t cookie, const void *buf, size_t len)
 {
-    const char* magic = magic_buffer(cookie, buf, len);
+    const char *magic = magic_buffer(cookie, buf, len);
 
-    if(!magic)
-    {
-        const char* err = magic_error(cookie);
+    if (!magic) {
+        const char *err = magic_error(cookie);
         merror("%s: ERROR: magic_buffer: %s", ARGV0, err ? err : "unknown");
-        return(1); // TODO default to true?
-    }
-    else
-    {
-        if(strncmp(magic, "text/", 5) == 0) return(1);
+        return (1); // TODO default to true?
+    } else {
+        if (strncmp(magic, "text/", 5) == 0) {
+            return (1);
+        }
     }
 
-    return(0);
+    return (0);
 }
 #endif
 
-/* Generate diffs alerts. */
+/* Generate diffs alerts */
 static char *gen_diff_alert(const char *filename, time_t alert_diff_time)
 {
     size_t n = 0;
     FILE *fp;
     char *tmp_str;
-    char buf[OS_MAXSTR +1];
-    char diff_alert[OS_MAXSTR +1];
+    char buf[OS_MAXSTR + 1];
+    char diff_alert[OS_MAXSTR + 1];
 
     buf[OS_MAXSTR] = '\0';
     diff_alert[OS_MAXSTR] = '\0';
@@ -59,51 +57,40 @@ static char *gen_diff_alert(const char *filename, time_t alert_diff_time)
              DIFF_DIR_PATH, filename,  (int)alert_diff_time);
 
     fp = fopen(buf, "r");
-    if(!fp)
-    {
+    if (!fp) {
         merror("%s: ERROR: Unable to generate diff alert.", ARGV0);
-        return(NULL);
+        return (NULL);
     }
 
-    n = fread(buf, 1, 4096 -1, fp);
-    if(n <= 0)
-    {
+    n = fread(buf, 1, 4096 - 1, fp);
+    if (n <= 0) {
         merror("%s: ERROR: Unable to generate diff alert (fread).", ARGV0);
         fclose(fp);
-        return(NULL);
-    }
-    else if(n >= 4000)
-    {
-        /* We need to clear the last new line. */
+        return (NULL);
+    } else if (n >= 4000) {
+        /* Clear the last newline */
         buf[n] = '\0';
         tmp_str = strrchr(buf, '\n');
-        if(tmp_str)
+        if (tmp_str) {
             *tmp_str = '\0';
-        else
-        {
-            /* Weird diff with only one large line. */
+        } else {
+            /* Weird diff with only one large line */
             buf[256] = '\0';
         }
-    }
-    else
-    {
+    } else {
         buf[n] = '\0';
     }
 
     n = 0;
 
-
-    /* Getting up to 20 line changes. */
+    /* Get up to 20 line changes */
     tmp_str = buf;
 
-
-    while(tmp_str && (*tmp_str != '\0'))
-    {
+    while (tmp_str && (*tmp_str != '\0')) {
         tmp_str = strchr(tmp_str, '\n');
-        if(!tmp_str)
+        if (!tmp_str) {
             break;
-        else if(n >= 19)
-        {
+        } else if (n >= 19) {
             *tmp_str = '\0';
             break;
         }
@@ -111,64 +98,55 @@ static char *gen_diff_alert(const char *filename, time_t alert_diff_time)
         tmp_str++;
     }
 
-
-    /* Creating alert. */
-    snprintf(diff_alert, 4096 -1, "%s%s",
-             buf, n>=19?
-             "\nMore changes..":
+    /* Create alert */
+    snprintf(diff_alert, 4096 - 1, "%s%s",
+             buf, n >= 19 ?
+             "\nMore changes.." :
              "");
 
-
     fclose(fp);
-    return(strdup(diff_alert));
+    return (strdup(diff_alert));
 }
-
 
 static int seechanges_dupfile(const char *old, const char *new)
 {
     size_t n;
     FILE *fpr;
     FILE *fpw;
-    unsigned char buf[2048 +1];
+    unsigned char buf[2048 + 1];
 
     buf[2048] = '\0';
 
-    fpr = fopen(old,"r");
-    if(!fpr)
-    {
+    fpr = fopen(old, "r");
+    if (!fpr) {
         return (0);
     }
 
-    fpw = fopen(new,"w");
-    if(!fpw)
-    {
+    fpw = fopen(new, "w");
+    if (!fpw) {
         fclose(fpr);
-        return(0);
+        return (0);
     }
 
     n = fread(buf, 1, 2048, fpr);
-    #ifdef USE_MAGIC
-    if(is_text(magic_cookie, buf, n) == 0)
-    {
+#ifdef USE_MAGIC
+    if (is_text(magic_cookie, buf, n) == 0) {
         goto cleanup;
     }
-    #endif
+#endif
 
-    do
-    {
+    do {
         buf[n] = '\0';
         fwrite(buf, n, 1, fpw);
-    }
-    while((n = fread(buf, 1, 2048, fpr)) > 0);
+    } while ((n = fread(buf, 1, 2048, fpr)) > 0);
 
 #ifdef USE_MAGIC
 cleanup:
 #endif
     fclose(fpr);
     fclose(fpw);
-    return(1);
+    return (1);
 }
-
 
 static int seechanges_createpath(const char *filename)
 {
@@ -176,45 +154,38 @@ static int seechanges_createpath(const char *filename)
     char *tmpstr = NULL;
     char *newdir = NULL;
 
-
     os_strdup(filename, buffer);
     newdir = buffer;
-    tmpstr = strchr(buffer +1, '/');
-    if(!tmpstr)
-    {
+    tmpstr = strchr(buffer + 1, '/');
+    if (!tmpstr) {
         merror("%s: ERROR: Invalid path name: '%s'", ARGV0, filename);
         free(buffer);
-        return(0);
+        return (0);
     }
     *tmpstr = '\0';
     tmpstr++;
 
-
-    while(1)
-    {
-        if(IsDir(newdir) != 0)
-        {
-            #ifndef WIN32
-            if(mkdir(newdir, 0770) == -1)
-            #else
-            if(mkdir(newdir) == -1)
-            #endif
+    while (1) {
+        if (IsDir(newdir) != 0) {
+#ifndef WIN32
+            if (mkdir(newdir, 0770) == -1)
+#else
+            if (mkdir(newdir) == -1)
+#endif
             {
                 merror(MKDIR_ERROR, ARGV0, newdir, errno, strerror(errno));
                 free(buffer);
-                return(0);
+                return (0);
             }
         }
 
-        if(*tmpstr == '\0')
-        {
+        if (*tmpstr == '\0') {
             break;
         }
 
         tmpstr[-1] = '/';
         tmpstr = strchr(tmpstr, '/');
-        if(!tmpstr)
-        {
+        if (!tmpstr) {
             break;
         }
         *tmpstr = '\0';
@@ -222,18 +193,16 @@ static int seechanges_createpath(const char *filename)
     }
 
     free(buffer);
-    return(1);
+    return (1);
 }
 
-
-/* Checks if the file has changed */
+/* Check if the file has changed */
 char *seechanges_addfile(const char *filename)
 {
     time_t date_of_change;
-    char old_location[OS_MAXSTR +1];
-    char tmp_location[OS_MAXSTR +1];
-    char diff_cmd[OS_MAXSTR +1];
-
+    char old_location[OS_MAXSTR + 1];
+    char tmp_location[OS_MAXSTR + 1];
+    char diff_cmd[OS_MAXSTR + 1];
     os_md5 md5sum_old;
     os_md5 md5sum_new;
 
@@ -243,75 +212,56 @@ char *seechanges_addfile(const char *filename)
     md5sum_new[0] = '\0';
     md5sum_old[0] = '\0';
 
-
-    snprintf(old_location, OS_MAXSTR, "%s/local/%s/%s", DIFF_DIR_PATH, filename +1,
+    snprintf(old_location, OS_MAXSTR, "%s/local/%s/%s", DIFF_DIR_PATH, filename + 1,
              DIFF_LAST_FILE);
 
-
-    /* If the file is not there, rename new location to last location. */
-    if(OS_MD5_File(old_location, md5sum_old) != 0)
-    {
+    /* If the file is not there, rename new location to last location */
+    if (OS_MD5_File(old_location, md5sum_old) != 0) {
         seechanges_createpath(old_location);
-        if(seechanges_dupfile(filename, old_location) != 1)
-        {
+        if (seechanges_dupfile(filename, old_location) != 1) {
             merror(RENAME_ERROR, ARGV0, filename, old_location, errno, strerror(errno));
         }
-        return(NULL);
+        return (NULL);
     }
 
-
-    /* Get md5sum of the new file. */
-    if(OS_MD5_File(filename, md5sum_new) != 0)
-    {
-        //merror("%s: ERROR: Invalid internal state (missing '%s').",
-        //       ARGV0, filename);
-        return(NULL);
+    /* Get md5sum of the new file */
+    if (OS_MD5_File(filename, md5sum_new) != 0) {
+        return (NULL);
     }
 
-
-    /* If they match, keep the old file and remove the new. */
-    if(strcmp(md5sum_new, md5sum_old) == 0)
-    {
-        return(NULL);
+    /* If they match, keep the old file and remove the new */
+    if (strcmp(md5sum_new, md5sum_old) == 0) {
+        return (NULL);
     }
 
-
-    /* Saving the old file at timestamp and renaming new to last. */
+    /* Save the old file at timestamp and rename new to last */
     date_of_change = File_DateofChange(old_location);
-    snprintf(tmp_location, OS_MAXSTR, "%s/local/%s/state.%d", DIFF_DIR_PATH, filename +1,
+    snprintf(tmp_location, OS_MAXSTR, "%s/local/%s/state.%d", DIFF_DIR_PATH, filename + 1,
              (int)date_of_change);
 
-    if(rename(old_location, tmp_location) == -1)
-    {
+    if (rename(old_location, tmp_location) == -1) {
         merror(RENAME_ERROR, ARGV0, old_location, tmp_location, errno, strerror(errno));
         return (NULL);
     }
 
-    if(seechanges_dupfile(filename, old_location) != 1)
-    {
-        merror("%s: ERROR: Unable to create snapshot for %s",ARGV0, filename);
-        return(NULL);
+    if (seechanges_dupfile(filename, old_location) != 1) {
+        merror("%s: ERROR: Unable to create snapshot for %s", ARGV0, filename);
+        return (NULL);
     }
 
-
-    /* Run diff. */
+    /* Run diff */
     date_of_change = File_DateofChange(old_location);
     snprintf(diff_cmd, 2048, "diff \"%s\" \"%s\" > \"%s/local/%s/diff.%d\" "
              "2>/dev/null",
              tmp_location, old_location,
-             DIFF_DIR_PATH, filename +1, (int)date_of_change);
-    if(system(diff_cmd) != 256)
-    {
+             DIFF_DIR_PATH, filename + 1, (int)date_of_change);
+    if (system(diff_cmd) != 256) {
         merror("%s: ERROR: Unable to run diff for %s",
                ARGV0,  filename);
-        return(NULL);
+        return (NULL);
     }
 
-
-    /* Generate alert. */
-    return(gen_diff_alert(filename, date_of_change));
+    /* Generate alert */
+    return (gen_diff_alert(filename, date_of_change));
 }
 
-
-
-/* EOF */
