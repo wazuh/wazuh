ccba4cac52cb574ab0dffda7aed7a6aecc5889b0
Wouter Clarie <wclarie@gmail.com>
2015-01-21 16:10:58 +0100

    os_zlib: Formatting

--- src/os_zlib/os_zlib.c
+++ src/os_zlib/os_zlib.c
@@ -9,37 +9,35 @@
 
 #include "os_zlib.h"
 
-unsigned long int os_zlib_compress(const char *src, char *dst, unsigned long int src_size,
-		unsigned long int dst_size)
+
+unsigned long int os_zlib_compress(const char *src, char *dst,
+                                   unsigned long int src_size,
+                                   unsigned long int dst_size)
 {
-    if(compress2((Bytef *)dst,
-                 &dst_size,
-                 (const Bytef *)src,
-                 src_size,
-                 Z_BEST_COMPRESSION) == Z_OK)
-    {
-        dst[dst_size] = '\0';
-        return(dst_size);
-    }
-
-    return(0);
-}
-
-
-unsigned long int os_zlib_uncompress(const char *src, char *dst, unsigned long int src_size,
-		unsigned long int dst_size)
-{
-    if(uncompress((Bytef *)dst,
+    if (compress2((Bytef *)dst,
                   &dst_size,
                   (const Bytef *)src,
-                  src_size) == Z_OK)
-    {
+                  src_size,
+                  Z_BEST_COMPRESSION) == Z_OK) {
         dst[dst_size] = '\0';
-        return(dst_size);
+        return (dst_size);
     }
 
-    return(0);
+    return (0);
 }
 
+unsigned long int os_zlib_uncompress(const char *src, char *dst,
+                                     unsigned long int src_size,
+                                     unsigned long int dst_size)
+{
+    if (uncompress((Bytef *)dst,
+                   &dst_size,
+                   (const Bytef *)src,
+                   src_size) == Z_OK) {
+        dst[dst_size] = '\0';
+        return (dst_size);
+    }
 
-/* EOF */
+    return (0);
+}
+
