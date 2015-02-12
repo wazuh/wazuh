fee68f3835938ac6f4b8743bdad43f462fa1a8f8
cgzones <cgzones@googlemail.com>
2015-02-12 13:09:36 +0100

    fix regressions intorduced by https://github.com/ossec/ossec-hids/commit/178dfed55e7e1efbada3d6ad0e307794ebc696fd

--- src/shared/report_op.c
+++ src/shared/report_op.c
@@ -411,13 +411,27 @@ void os_ReportdStart(report_filter *r_filter)
             || !r_filter->top_group || !r_filter->top_location || !r_filter->top_files) {
         merror(MEM_ERROR, __local_name, errno, strerror((errno)));
 
-        OSStore_Free(r_filter->top_user);
-        OSStore_Free(r_filter->top_srcip);
-        OSStore_Free(r_filter->top_level);
-        OSStore_Free(r_filter->top_rule);
-        OSStore_Free(r_filter->top_group);
-        OSStore_Free(r_filter->top_location);
-        OSStore_Free(r_filter->top_files);
+        if (r_filter->top_user) {
+            OSStore_Free(r_filter->top_user);
+        }
+        if (r_filter->top_srcip) {
+            OSStore_Free(r_filter->top_srcip);
+        }
+        if (r_filter->top_level) {
+            OSStore_Free(r_filter->top_level);
+        }
+        if (r_filter->top_rule) {
+            OSStore_Free(r_filter->top_rule);
+        }
+        if (r_filter->top_group) {
+            OSStore_Free(r_filter->top_group);
+        }
+        if (r_filter->top_location) {
+            OSStore_Free(r_filter->top_location);
+        }
+        if (r_filter->top_files) {
+            OSStore_Free(r_filter->top_files);
+        }
 
         goto cleanup;
     }
