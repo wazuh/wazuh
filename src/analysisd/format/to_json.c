0dfa2e51bdcfea1743f878317e082f238fcf01fa
root <admin@wazuh.com>
2015-08-18 03:40:26 -0700

    JSON Improvements - wazuh_external_functions

--- src/analysisd/format/to_json.c
+++ src/analysisd/format/to_json.c
@@ -4,7 +4,7 @@
  * This program is a free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public
  * License (version 2) as published by the FSF - Free Software
- * Foundation. 
+ * Foundation.  
  */
 
 #include "to_json.h"
