b7e9ebe0acd7bf3b50d8884021d3408d5c04488f
Doke Scott <doke@udel.edu>
2015-08-19 17:26:10 -0400

    include glob.h on unix

--- src/headers/shared.h
+++ src/headers/shared.h
@@ -60,9 +60,9 @@
 #include <dirent.h>
 #include <ctype.h>
 #include <signal.h>
+#include <glob.h>
 
 #ifndef WIN32
-#include <glob.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
