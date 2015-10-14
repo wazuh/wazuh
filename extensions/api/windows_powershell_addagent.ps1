c653b86ab21541cc0bdc3345e5c0d794a8c06760
Pedro S <pedro@wazuh.com>
2015-10-13 23:33:55 -0700

    API: Powershell script now through SSL/TLS. Fully functional.

--- extensions/api/windows_powershell_addagent.ps1
+++ extensions/api/windows_powershell_addagent.ps1
@@ -13,22 +13,50 @@ if ((Test-Admin) -eq $false)  {
 	   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 	   Exit
 }
-	  
+
+# Certs functions
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    
+    public class PolicyCert : ICertificatePolicy {
+        public PolicyCert() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = new-object PolicyCert 
+
+#API SETTINGS
+$api_port = "8080"
+$api_ip = "192.168.73.145"
+$username = "foo"
+$password = "bar"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+
 #Installing Agent Executable
 $exe = "C:\Users\snaow\Desktop\ossec-agent-win32-2.8.exe"
 $AllArgs = @('/S')
 $check = Start-Process $exe $AllArgs -Wait -Verb runAs
 "OSSEC Installed"
+
+# Temp adding ip to ossec.conf
+$ossec_server_ip = "<ossec_config><client> <server-ip>192.168.73.145</server-ip> </client></ossec_config>"
+Add-Content "C:\Program Files\ossec-agent\ossec.conf" $ossec_server_ip
+
 # API: Add Agent +  Get Key
-$url = "http://192.168.73.145:8080"
+$url = "https://" + $api_ip + ":" + $api_port;
 $resource = $url + "/agents/add/" + $env:computername
-$response = Invoke-RestMethod -Method Get -Uri $resource
+$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
 $ID = $response[0].ID
 $ID
 if ($ID) {
 	$resource = $url + "/agents/"+$ID+"/key"
 	$resource
-	$response = Invoke-RestMethod -Method Get -Uri $resource
+	$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
 	$key = $response[0].key
 	$key
 }
