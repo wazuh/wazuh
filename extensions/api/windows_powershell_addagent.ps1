431952b81e240b4b5c2fd40bc1c9e82f0a2939a8
Pedro S <pedro@wazuh.com>
2015-10-14 18:26:09 -0700

    API: Powershell windows error management

--- extensions/api/windows_powershell_addagent.ps1
+++ extensions/api/windows_powershell_addagent.ps1
@@ -1,4 +1,15 @@
 param([switch]$Elevated)
+
+# Create Log file
+$path = $PSScriptRoot
+$file_log = "\ossec_deploy.txt"
+
+if(!(Test-Path -Path $path$file_log)){
+	New-Item -Path $path$file_log -ItemType File
+}else{
+	Add-Content -Path $path$file_log -Value "Starting"
+}
+
 # Opening powershell as Administrator
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
@@ -8,12 +19,13 @@ function Test-Admin {
   $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 if ((Test-Admin) -eq $false)  {
+	   Add-Content -Path $path$file_log -Value "`nERROR: Administrator privilegies failed"
        "This script requires Administrator privileges"
 	   Write-Host "Press any key to continue ..."
 	   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 	   Exit
 }
-
+Add-Content -Path $path$file_log -Value "Privileges OK"
 # Certs functions
 add-type @"
     using System.Net;
@@ -29,7 +41,7 @@ add-type @"
     }
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = new-object PolicyCert 
-
+Add-Content -Path $path$file_log -Value "Certify OK"
 #API SETTINGS
 $api_port = "8080"
 $api_ip = "192.168.73.145"
@@ -41,25 +53,45 @@ $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 $exe = "C:\Users\snaow\Desktop\ossec-agent-win32-2.8.exe"
 $AllArgs = @('/S')
 $check = Start-Process $exe $AllArgs -Wait -Verb runAs
-"OSSEC Installed"
-
+Add-Content -Path $path$file_log -Value "OSSEC Installed OK"
 # Temp adding ip to ossec.conf
-$ossec_server_ip = "<ossec_config><client> <server-ip>192.168.73.145</server-ip> </client></ossec_config>"
-Add-Content "C:\Program Files\ossec-agent\ossec.conf" $ossec_server_ip
+# $ossec_server_ip = "<ossec_config><client> <server-ip>192.168.73.145</server-ip> </client></ossec_config>"
+# Add-Content "C:\Program Files\ossec-agent\ossec.conf" $ossec_server_ip
 
 # API: Add Agent +  Get Key
 $url = "https://" + $api_ip + ":" + $api_port;
 $resource = $url + "/agents/add/" + $env:computername
-$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
-$ID = $response[0].ID
-$ID
+try{
+	$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
+}catch{
+	$exceptionDetails = $_.Exception
+	Add-Content -Path $path$file_log -Value "$($exceptionDetails)"
+	exit 1001
+}
+if($response.error -eq 0){
+	$ID = $response.response.ID
+}else{
+	Add-Content -Path $path$file_log -Value "$($response.description)"
+	exit 1001
+}
+Add-Content -Path $path$file_log -Value "Adding Agent OK"
 if ($ID) {
 	$resource = $url + "/agents/"+$ID+"/key"
-	$resource
-	$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
-	$key = $response[0].key
-	$key
+	try{
+		$response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -Uri $resource
+	}catch{
+		$exceptionDetails = $_.Exception
+		Add-Content -Path $path$file_log -Value "$($exceptionDetails)"
+		exit 1001
+	}
+	if($response.error -eq 0){
+		$key = $response.response.key
+	}else{
+		Add-Content -Path $path$file_log -Value "$($response.description)"
+		exit 1001
+	}
 }
+Add-Content -Path $path$file_log -Value "Getting KEY OK"
 # API: Import key
 if($key) {
     $psi = New-Object System.Diagnostics.ProcessStartInfo;
@@ -72,6 +104,10 @@ if($key) {
     $p.StandardInput.WriteLine("y");
     Start-Sleep -s 2 #wait 2 seconds so that the process can be up and running
     $p.StandardInput.WriteLine("ENTER");
+	Add-Content -Path $path$file_log -Value "Import Key OK"
+	# Start OSSEC Service
+	net start OssecSvc
+	Add-Content -Path $path$file_log -Value "OSSEC SERVICE OK"
 }
-# Start OSSEC Service
-net start OssecSvc
+
+
