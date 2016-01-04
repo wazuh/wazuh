ae5c4c89895b82104bc27d1e3a2967a18b6335be
Pedro S <snaowxt@gmail.com>
2016-01-04 16:20:07 +0100

    Windows deployment script improvements

--- extensions/deployment/add_agent.ps1
+++ extensions/deployment/add_agent.ps1
@@ -1,38 +1,106 @@
-param([switch]$Elevated)
+# WAZUH Windows Add Agent Script
+
+# v2.0 2015/12/30
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+param (
+	#Mandatory
+	[switch]$Elevated,
+    [string]$api_ip = "",
+    [string]$username = "",
+    [string]$password = "",
+	
+	#Optionals
+	[string]$api_port = "55000",
+    [string]$server_ip = $api_ip,
+    [string]$agent_name = $env:computername,
+	[string]$ossec_path = $env:SystemDrive+"\ossec-agent\",
+	[string]$ossec_exe = "ossec-win32-agent.exe",
+	[Int]$prompt_agent_name = 0,
+	[switch]$help
+	
+	)
+
+if(($help.isPresent)) {
+	"Wazuh Add OSSEC Agent Windows
+Github repository: http://github.com/wazuh/ossec-wazuh
+API Documentation: http://documentation.wazuh.com/en/latest/ossec_api.html
+Site: http://www.wazuh.com"
+""
+""
+	"Usage: add_agent.exe -Arguments -api_ip IP -username USERNAME -password PASSWORD"
+	"Arguments description:
+	Mandatory:
+		-api_ip Wazuh API IP
+		-username Wazuh API auth https username
+		-password Wazuh API auth https password
+	Optionals:
+		-api_port Wazuh API port [Default 55000]
+		-server_ip OSSEC Manager IP [Default -api_ip]
+		-agent_name OSSEC Agent Name [Default windows hostname]
+		-ossec_path OSSEC Agent installation path [Default Sysdrive:\ossec-agent]
+		-ossec_exe OSSEC Agent executable name [Default ossec-win32-agent.exe]
+		-prompt_agent_name [0/1] In case agent name already exists on OSSEC Manager, prompt to ask Agent Name [default 0]
+		-help Display help
+	"
+	Exit
+}	
+
+
+# Opening powershell as Administrator
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+       "This script requires Administrator privileges"
+	   Write-Host "Press any key to continue ..."
+	   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	   Exit
+}
+
+# Checking Administrator privilegies
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Test-Admin) -eq $false)  {
+       "This script requires Administrator privileges"
+	   Write-Host "Press any key to continue ..."
+	   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	   Exit
+}
+
+if($api_ip -eq ""){
+	"-api_ip argument is required. Try -Arguments -help to display arguments list"
+	Write-Host "Press any key to continue ..."
+	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	Exit
+}
+if($username -eq ""){
+	"-username argument is required. Try -Arguments -help to display arguments list"
+	Write-Host "Press any key to continue ..."
+	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")	
+	Exit
+}
+if($password -eq ""){
+	"-password argument is required. Try -Arguments -help to display arguments list"
+	Write-Host "Press any key to continue ..."
+	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")	
+	Exit
+}
+
 
 # Create Log file
+# ps1
+# $path = $PSScriptRoot
+# executable
 $path = $PSScriptRoot
-$file_log = "\ossec_deploy.txt"
+
+$file_log = "\add_agent.log"
 $slash = "\"
 
 # Ossec service name
 $ossec_service = 'OssecSvc'
 
-############################
-#SETTINGS
-############################
-
-#API SETTINGS
-$api_port = "55000"
-$api_ip = ""
-$username = ""
-$password = ""
-
-#OSSEC Manager server_ip
-$server_ip = $api_ip
-
-# AGENT NAME
-$agent_name = $env:computername
-
-# ASK FOR AGENT NAME
-$prompt_agent_name = 1
-
-# Installation Default Path (letter and patch must be the same where ossec agent is installed)
-#$ossec_path = $env:SystemDrive"\Program Files\ossec-agent\"
-$ossec_path = $env:SystemDrive+"\Program Files (x86)\ossec-agent\"
-
-# Executable name
-$exe = "ossec-win32-agent.exe"
+#Executable name
+$exe = $ossec_exe
 $exe = $slash+$exe
 
 if(!(Test-Path -Path $path$file_log)){
@@ -40,6 +108,7 @@ if(!(Test-Path -Path $path$file_log)){
 }else{
     Clear-Content $path$file_log
 	Add-Content -Path $path$file_log -Value "Starting"
+	"Starting"
 }
 
 #################
@@ -54,33 +123,20 @@ function AgentName
     $read_agent_name
 }
 
-# Opening powershell as Administrator
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-
-# Checking Administrator privilegies
-function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-if ((Test-Admin) -eq $false)  {
-	   Add-Content -Path $path$file_log -Value "`nERROR: Administrator privilegies failed"
-       "This script requires Administrator privileges"
-	   Write-Host "Press any key to continue ..."
-	   $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-	   Exit
-}
 Add-Content -Path $path$file_log -Value "Privileges OK"
 
 # If OSSEC service already exits, do not install.
 Get-Service -Name $ossec_service -ErrorAction SilentlyContinue | Restart-Service -ErrorAction SilentlyContinue
 if ($? -eq $true) {
-	Add-Content -Path $path$file_log -Value "ERROR: OSSEC SERVICE already installed."
+	Add-Content -Path $path$file_log -Value "ERROR: OSSEC SERVICE already installed. Please uninstall OSSEC and run again."
+	"ERROR: OSSEC SERVICE already installed."
     Exit
 }
 
 # Verifying executable path
 if(!(Test-Path -Path $path$exe)){
 	Add-Content -Path $path$file_log -Value "OSSEC Executable does not exists: $path$exe"
+	"OSSEC Executable does not exists: $path$exe"
     Exit
 }
 
@@ -100,15 +156,18 @@ add-type @"
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = new-object PolicyCert 
 Add-Content -Path $path$file_log -Value "Certify OK"
+"Certify OK"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 
 
 ############################
 #Installing Agent Executable
 ############################
-$AllArgs = @('/S')
+$AllArgs = @('/S /D='+$env:SystemDrive+'\ossec-agent\')
 $check = Start-Process $path$exe $AllArgs -Wait -Verb runAs
 Add-Content -Path $path$file_log -Value "OSSEC Installed OK"
+"OSSEC Installed OK"
+
 
 ############################
 #Server ip to ossec.conf
@@ -117,9 +176,11 @@ if((Test-Path -Path $ossec_path"ossec.conf")){
     if(!(select-string -Quiet -path $ossec_path"ossec.conf" -pattern '<server-ip>.*[^1.2.3.4].*</server-ip>')){
         Add-Content -Path $ossec_path"ossec.conf" -Value "<ossec_config><client><server-ip>$server_ip</server-ip></client></ossec_config>"
         Add-Content -Path $path$file_log -Value "Added server-ip to ossec.conf"
+		"Added server-ip to ossec.conf"
     }
 }else{
     Add-Content -Path $path$file_log -Value "ERROR: OSSEC conf not found at $ossec_path"+"ossec.conf"
+	"ERROR: OSSEC conf not found at "+$ossec_path+"ossec.conf"
 }
 
 ############################
@@ -140,6 +201,7 @@ while($addedOK -eq 0){
     }catch{
         $exceptionDetails = $_.Exception
         Add-Content -Path $path$file_log -Value "$($exceptionDetails)"
+		$exceptionDetails
         exit 1001
     }
     if($response.error -eq 0){
@@ -151,11 +213,13 @@ while($addedOK -eq 0){
             $response.description
             $agent_name = AgentName
         }else{
+			$response.description
             exit 1001
         }
     }
 }
 Add-Content -Path $path$file_log -Value "Adding Agent OK"
+"Adding Agent OK"
 
 ##################
 # API: Getting key
@@ -167,12 +231,14 @@ if ($ID) {
 	}catch{
 		$exceptionDetails = $_.Exception
 		Add-Content -Path $path$file_log -Value "$($exceptionDetails)"
+		$exceptionDetails
 		exit 1001
 	}
 	if($response.error -eq 0){
 		$key = $response.response.key
 	}else{
 		Add-Content -Path $path$file_log -Value "$($response.description)"
+		$response.description
 		exit 1001
 	}
 }
@@ -186,6 +252,7 @@ if($key) {
     #Verifying manage_agent path
     if(!(Test-Path -Path $psi.FileName)){
         Add-Content -Path $path$file_log -Value "$psi.FileName does not exists"
+		"$psi.FileName does not exists"
         Exit
     }
     $psi.UseShellExecute = $false; #start the process from it's own executable file
@@ -201,8 +268,8 @@ if($key) {
 	net start OssecSvc
 	Add-Content -Path $path$file_log -Value "OSSEC SERVICE OK"
     # Restart Service
-    Start-Sleep -s 5 #wait 5s and restart OSSEC Agent (Better way to send a notification to OSSEC Manager)
+    Start-Sleep -s 3 #wait 5s and restart OSSEC Agent (Better way to send a notification to OSSEC Manager)
+    Get-Service -Name $ossec_service -ErrorAction SilentlyContinue | Restart-Service -ErrorAction SilentlyContinue
+	Start-Sleep -s 3 #wait 5s and restart OSSEC Agent (Better way to send a notification to OSSEC Manager)
     Get-Service -Name $ossec_service -ErrorAction SilentlyContinue | Restart-Service -ErrorAction SilentlyContinue
 }
-
-
