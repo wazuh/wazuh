3ba5cd2ae89dd29d8adbd487088bc4450087811d
Víctor Manuel Fernández Castro <vmfdez90@gmail.com>
2016-03-23 10:24:00 +0100

    Implements Reply-To for OS_Sendmail()
    Picked from commit by calve

--- src/os_maild/sendmail.c
+++ src/os_maild/sendmail.c
@@ -425,6 +425,13 @@ int OS_Sendmail(MailConfig *mail, struct tm *p)
     snprintf(snd_msg, 127, FROM, mail->from);
     OS_SendTCP(socket, snd_msg);
 
+    /* Send reply-to if set */
+    if (mail->reply_to){
+        memset(snd_msg, '\0', 128);
+        snprintf(snd_msg, 127, REPLYTO, mail->reply_to);
+        OS_SendTCP(socket, snd_msg);
+    }
+
     /* Add CCs */
     if (mail->to[1]) {
         i = 1;
