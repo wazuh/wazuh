ed5b497ca6b165c0e169c746d6f70b2915c31619
ddp <ddp@ix.example.com>
2016-04-01 13:18:19 -0400

    Check for a NULL from malloc.

--- src/analysisd/lists_list.c
+++ src/analysisd/lists_list.c
@@ -224,6 +224,9 @@ static int OS_DBSearchKeyAddressValue(ListRule *lrule, char *key)
             vpos = cdb_datapos(&lrule->db->cdb);
             vlen = cdb_datalen(&lrule->db->cdb);
             val = (char *) malloc(vlen);
+            if(val == NULL) {
+                return(0);
+            }
             cdb_read(&lrule->db->cdb, val, vlen, vpos);
             result = OSMatch_Execute(val, vlen, lrule->matcher);
             free(val);
