93082c41b39d500e8036e010016ec8874ef2d128
Scott R. Shinn <scott@atomicorp.com>
2016-05-24 08:49:47 -0400

    - Add ifdef for libgeo
    Signed-off-by: Scott R. Shinn <scott@atomicorp.com>

--- src/analysisd/config.h
+++ src/analysisd/config.h
@@ -12,7 +12,10 @@
 
 #include "config/config.h"
 #include "config/global-config.h"
+#ifdef LIBGEOIP_ENABLED
 #include "GeoIP.h"
+#endif
+
 
 extern long int __crt_ftell; /* Global ftell pointer */
 extern _Config Config;       /* Global Config structure */
