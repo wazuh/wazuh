fd67047ab5954c8e7e02a0ba73100f92ad166380
Pedro S <snaowxt@gmail.com>
2016-06-10 03:26:22 -0700

    Preparing sqlite tables

---
+++ src/os_db/db_fim.h
@@ -0,0 +1,15 @@
+/*
+ * Wazuh SQLite integration
+ * Copyright (C) 2016 Wazuh Inc.
+ * June 06, 2016.
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+ 
+#include "db_sqlite.h"
+
+
+int db_insert_fim(sqlite3 *db, char * path, char * event, char * md5, char * sha1, char * size, char * uid, char * gid, int perm);
