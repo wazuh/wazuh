69ce94da0f91c08db38c848da6bd198704acfde9
Vikman Fdez-Castro <vmfdez90@gmail.com>
2016-06-18 23:04:29 +0200

    Adding FIM events to SQLite database

--- src/os_db/db_fim.c
+++ src/os_db/db_fim.c
@@ -8,12 +8,92 @@
  * License (version 2) as published by the FSF - Free Software
  * Foundation.
  */
- 
-#include "db_fim.h"
 
-int db_insert_fim(sqlite3 *db, char * path, char * event, char * md5, char * sha1, char * size, char * uid, char * gid, int perm){
-	
-	merror("Database: %s, %s, %s, %s, %s, %s, %s, %d", path, event, md5, sha1, size, uid, gid, perm);
-	
-	return 1;
+#include "db.h"
+#include "sqlite3.h"
+
+static const char *SQL_INSERT_FIM = "INSERT INTO fim_event (id_agent, id_file, event, date, size, perm, uid, gid, md5, sha1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+static const char *SQL_INSERT_FILE = "INSERT INTO fim_file (path) VALUES (?);";
+static const char *SQL_FIND_FILE = "SELECT id FROM fim_file WHERE path = ?;";
+
+/* Find file: returns ID, or 0 if it doesn't exists, or -1 on error. */
+int db_insert_file(const char *path) {
+    static sqlite3_stmt *stmt = NULL;
+    int result;
+
+    if (!(stmt || sqlite3_prepare_v2(db, SQL_INSERT_FILE, -1, &stmt, NULL)))
+        return -1;
+
+    sqlite3_bind_text(stmt, 1, path, -1, NULL);
+    result = sqlite3_step(stmt) == SQLITE_DONE ? (int)sqlite3_last_insert_rowid(db) : -1;
+    sqlite3_reset(stmt);
+
+     return result;
+}
+
+/* Insert file, Returns -1 on error. */
+int db_find_file(const char *path) {
+    static sqlite3_stmt *stmt = NULL;
+    int result;
+
+    if (!(stmt || sqlite3_prepare_v2(db, SQL_FIND_FILE, -1, &stmt, NULL)))
+        return -1;
+
+    sqlite3_bind_text(stmt, 1, path, -1, NULL);
+
+    switch (sqlite3_step(stmt)) {
+    case SQLITE_ROW:
+        result = sqlite3_column_int(stmt, 0);
+    case SQLITE_DONE:
+        result = 0;
+    default:
+        result = -1;
+    }
+
+    sqlite3_reset(stmt);
+    return result;
+}
+
+/* Insert FIM entry. Returns -1 on error. */
+int db_insert_fim(int id_agent, const char *f_name, const char *event, const SyscheckSum *sum, long int time) {
+    sqlite3_stmt *stmt = NULL;
+    int id_file;
+    int result;
+
+    if (!(stmt || sqlite3_prepare_v2(db, SQL_INSERT_FIM, -1, &stmt, NULL)))
+        return -1;
+
+    switch ((id_file = db_find_file(f_name))) {
+    case -1:
+        return -1;
+
+    case 0:
+        if ((id_file = db_insert_file(f_name)) < 0)
+            return -1;
+    }
+
+    sqlite3_bind_int(stmt, 1, id_agent);
+    sqlite3_bind_int(stmt, 2, id_file);
+    sqlite3_bind_text(stmt, 3, event, -1, NULL);
+    sqlite3_bind_int(stmt, 4, time);
+
+    if (sum) {
+        sqlite3_bind_int(stmt, 5, atoi(sum->size));
+        sqlite3_bind_int(stmt, 6, sum->perm);
+        sqlite3_bind_int(stmt, 7, atoi(sum->uid));
+        sqlite3_bind_int(stmt, 8, atoi(sum->gid));
+        sqlite3_bind_text(stmt, 9, sum->md5, -1, NULL);
+        sqlite3_bind_text(stmt, 10, sum->sha1, -1, NULL);
+    } else {
+        sqlite3_bind_null(stmt, 5);
+        sqlite3_bind_null(stmt, 6);
+        sqlite3_bind_null(stmt, 7);
+        sqlite3_bind_null(stmt, 8);
+        sqlite3_bind_null(stmt, 9);
+        sqlite3_bind_null(stmt, 10);
+    }
+
+    result = sqlite3_step(stmt) == SQLITE_DONE ? (int)sqlite3_last_insert_rowid(db) : -1;
+    sqlite3_reset(stmt);
+    return result;
 }
