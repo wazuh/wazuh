58fbc642c89c1affb87246ce613d8592d70ef2b0
Pedro S <pedro@wazuh.com>
2016-07-20 22:06:26 +0000

    Removed without-rowid from databases for compatibility

--- src/wazuh_db/schema_global.sql
+++ src/wazuh_db/schema_global.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS agent (
     version TEXT,
     date_add NUMERIC DEFAULT CURRENT_TIMESTAMP,
     enabled INTEGER DEFAULT 1
-) WITHOUT ROWID;
+);
 
 CREATE INDEX IF NOT EXISTS agent_name ON agent (name);
 CREATE INDEX IF NOT EXISTS agent_ip ON agent (ip);
