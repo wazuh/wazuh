48b15c5b7ec7643102e8b701a1cb11f2813082ce
Vikman Fdez-Castro <victor@wazuh.com>
2016-11-14 15:32:17 +0100

    Set agent's maximum connection time value to notification time

--- src/headers/defs.h
+++ src/headers/defs.h
@@ -75,7 +75,6 @@ http://www.ossec.net/main/license/\n"
 
 /* Notify the manager */
 #define NOTIFY_TIME     600 /* ... every 600 seconds (10 minutes) */
-#define MAX_CONN_SLEEP  60
 
 /* User Configuration */
 #ifndef MAILUSER
