f3f96819c0c7fc4bafe9d840ee539657c8eb82cf
Vikman Fdez-Castro <victor@wazuh.com>
2017-02-15 19:08:07 -0800

    Fixed compile error on Windows version.

--- src/win32/win_agent.c
+++ src/win32/win_agent.c
@@ -202,7 +202,7 @@ int local_start()
     /* Read keys */
     verbose(ENC_READ, ARGV0);
 
-    OS_ReadKeys(&keys, 1);
+    OS_ReadKeys(&keys, 1, 0);
     OS_StartCounter(&keys);
     os_write_agent_info(keys.keyentries[0]->name, NULL, keys.keyentries[0]->id, agt->profile);
 
