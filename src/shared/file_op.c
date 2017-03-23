2883ca88f0a28ba5356fa4f45805bc35355f9d4d
Vikman Fdez-Castro <victor@wazuh.com>
2017-03-22 18:44:27 -0700

    Fix TOCTOU race condition when removing directories recursively.

--- src/shared/file_op.c
+++ src/shared/file_op.c
@@ -1550,54 +1550,55 @@ char *getuname()
 
 // Delete directory recorsively
 
-int rmdir_ex(const char *path) {
-    char filestr[PATH_MAX + 1];
+int rmdir_ex(const char *name) {
+    DIR *dir;
     struct dirent *dirent;
-    struct stat statbuf;
-    DIR *dir = opendir(path);
+    char path[PATH_MAX + 1];
 
-    if (!dir)
-        return -1;
+    if (rmdir(name) == 0) {
+        return 0;
+    }
 
-    while ((dirent = readdir(dir))) {
+    switch (errno) {
+    case ENOTDIR:   // Not a directory
+        return unlink(name);
 
-        // Skip "." and ".." (avoid strcmp)
+    case ENOTEMPTY: // Directory not empty
 
-        if (dirent->d_name[0] == '.') {
-            switch (dirent->d_name[1]) {
-            case '\0':
-                continue;
-            case '.':
-                if (dirent->d_name[2] == '\0')
-                    continue;
-                break;
-            default:
-                break;
-            }
-        }
+        // Erase content
 
-        snprintf(filestr, PATH_MAX + 1, "%s/%s", path, dirent->d_name);
+        dir = opendir(name);
 
-        if (stat(filestr, &statbuf) < 0) {
-            closedir(dir);
+        if (!dir) {
             return -1;
         }
 
-        if (S_ISREG(statbuf.st_mode)) {
-            if (unlink(filestr) < 0) {
+        while (dirent = readdir(dir), dirent) {
+            // Skip "." and ".."
+            if (dirent->d_name[0] == '.' && (dirent->d_name[1] == '\0' || (dirent->d_name[1] == '.' && dirent->d_name[2] == '\0'))) {
+                continue;
+            }
+
+            if (snprintf(path, PATH_MAX + 1, "%s/%s", name, dirent->d_name) > PATH_MAX) {
                 closedir(dir);
                 return -1;
             }
-        } else if (S_ISDIR(statbuf.st_mode)) {
-            if (rmdir_ex(filestr) < 0) {
+
+            if (rmdir_ex(path) < 0) {
                 closedir(dir);
                 return -1;
             }
         }
-    }
 
-    closedir(dir);
-    return rmdir(path);
+        closedir(dir);
+
+        // Try to erase again
+
+        return rmdir(name);
+
+    default:
+        return -1;
+    }
 }
 
 int TempFile(File *file, const char *source, int copy) {
