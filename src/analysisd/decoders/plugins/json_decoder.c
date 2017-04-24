95736f8b83bc913f0892c351edc91ce422e392d4
Alberto Marín <amnbaza@hotmail.com>
2017-04-24 13:40:56 +0200

    Added parsing JSON boolean arrays and improve code indentation.

--- src/analysisd/decoders/plugins/json_decoder.c
+++ src/analysisd/decoders/plugins/json_decoder.c
@@ -15,256 +15,280 @@
 #include "../../config.h"
 #include "../../external/cJSON/cJSON.h"
 
-static void fillData(Eventinfo *lf, char *key, char *value){
+static void fillData(Eventinfo *lf, char *key, char *value)
+{
 
-  if (strcmp(key, "srcip") == 0){
+    if (strcmp(key, "srcip") == 0){
+        lf->srcip = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       srcip: '%s'", lf->srcip);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "dstip") == 0){
+        lf->dstip = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       dstip: '%s'", lf->dstip);
+        }
+#endif
+    return;
+    }
+
+    if (strcmp(key, "dstport") == 0){
+        lf->dstport = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       dstport: '%s'", lf->dstport);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "srcport") == 0){
+        lf->srcport = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       srcport: '%s'", lf->srcport);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "protocol") == 0){
+        lf->protocol = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       protocol: '%s'", lf->protocol);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "action") == 0){
+        lf->action = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       action: '%s'", lf->action);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "srcuser") == 0){
+        lf->srcuser = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       srcuser: '%s'", lf->srcuser);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "dstuser") == 0){
+        lf->dstuser = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       dstuser: '%s'", lf->dstuser);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "id") == 0){
+        lf->id = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       id: '%s'", lf->id);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "status") == 0){
+        lf->status = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       status: '%s'", lf->status);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "command") == 0){
+        lf->command = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       command: '%s'", lf->command);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "url") == 0){
+        lf->url = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       url: '%s'", lf->url);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "data") == 0){
+        lf->data = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       data: '%s'", lf->data);
+        }
+#endif
+        return;
+    }
+
+    if (strcmp(key, "systemname") == 0){
+        lf->systemname = strdup(value);
+#ifdef TESTRULE
+        if (!alert_only) {
+            print_out("       systemname: '%s'", lf->systemname);
+        }
+#endif
+        return;
+    }
+
+    // Dynamic fields
+    if (lf->nfields >= Config.decoder_order_size) {
+        merror(ARGV0 ": ERROR: too many fields for JSON decoder.");
+        return;
+    }
 #ifdef TESTRULE
     if (!alert_only) {
-        print_out("       srcip: '%s'", value);
+        print_out("       %s: '%s'", key, value);
     }
 #endif
-    lf->srcip = strdup(value);
-    return;
-  }
-  if (strcmp(key, "dstip") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       dstip: '%s'", value);
-    }
-#endif
-    lf->dstip = strdup(value);
-    return;
-  }
-  if (strcmp(key, "dstport") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       dstport: '%s'", value);
-    }
-#endif
-    lf->dstport = strdup(value);
-    return;
-  }
-  if (strcmp(key, "srcport") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       srcport: '%s'", value);
-    }
-#endif
-    lf->srcport = strdup(value);
-    return;
-  }
-  if (strcmp(key, "protocol") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       protocol: '%s'", value);
-    }
-#endif
-    lf->protocol = strdup(value);
-    return;
-  }
-  if (strcmp(key, "action") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       action: '%s'", value);
-    }
-#endif
-    lf->action = strdup(value);
-    return;
-  }
-  if (strcmp(key, "srcuser") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       srcuser: '%s'", value);
-    }
-#endif
-    lf->srcuser = strdup(value);
-    return;
-  }
-  if (strcmp(key, "dstuser") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       dstuser: '%s'", value);
-    }
-#endif
-    lf->dstuser = strdup(value);
-    return;
-  }
-  if (strcmp(key, "id") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       id: '%s'", value);
-    }
-#endif
-    lf->id = strdup(value);
-    return;
-  }
-  if (strcmp(key, "status") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       status: '%s'", value);
-    }
-#endif
-    lf->status = strdup(value);
-    return;
-  }
-  if (strcmp(key, "command") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       command: '%s'", value);
-    }
-#endif
-    lf->command = strdup(value);
-    return;
-  }
-  if (strcmp(key, "url") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       url: '%s'", value);
-    }
-#endif
-    lf->url = strdup(value);
-    return;
-  }
-  if (strcmp(key, "data") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       data: '%s'", value);
-    }
-#endif
-    lf->data = strdup(value);
-    return;
-  }
-  if (strcmp(key, "systemname") == 0){
-#ifdef TESTRULE
-    if (!alert_only) {
-        print_out("       systemname: '%s'", value);
-    }
-#endif
-    lf->systemname = strdup(value);
-    return;
-  }
-  // Dynamic fields
-  if (lf->nfields >= Config.decoder_order_size) {
-		merror(ARGV0 ": ERROR: too many fields for JSON decoder.");
-		return;
-	}
-#ifdef TESTRULE
-  if (!alert_only) {
-      print_out("       %s: '%s'", key, value);
-  }
-#endif
-	lf->fields[lf->nfields].key = strdup(key);
-	lf->fields[lf->nfields].value = strdup(value);
-	lf->nfields++;
+    lf->fields[lf->nfields].key = strdup(key);
+    lf->fields[lf->nfields].value = strdup(value);
+    lf->nfields++;
 }
 
-static void readJSON (cJSON *logJSON, char *parent, Eventinfo *lf){
-  cJSON *next, *array;
-  char *key = NULL;
-  char *value = NULL;
-  size_t  n;
+static void readJSON (cJSON *logJSON, char *parent, Eventinfo *lf)
+{
+    cJSON *next, *array;
+    char *key = NULL;
+    char *value = NULL;
+    size_t  n;
 
-  while(logJSON){
-    next = logJSON->next;
-    if (logJSON->string){
-      if(parent){
-        n = strlen(parent);
-        key = malloc(n+strlen(logJSON->string)+2);
-        strcpy(key, parent);
-        key[n++] = '.';
-        strcpy(key + n, logJSON->string);
-      }
-      else{
-        key = strdup(logJSON->string);
-      }
-    }
-    switch((logJSON->type)&255){
-
-      case cJSON_String:
-        fillData(lf, key, logJSON->valuestring);
-        break;
-
-      case cJSON_Number:
-        if ((double)logJSON->valueint == logJSON->valuedouble){
-          char value_char[16];
-          snprintf(value_char, 16, "%i", logJSON->valueint);
-          fillData(lf, key, value_char);
-        }
-        else{
-          char value_char[16];
-          snprintf(value_char, 16, "%f", logJSON->valuedouble);
-          fillData(lf, key, value_char);
-        }
-        break;
-
-      case cJSON_Array:
-        array = logJSON->child;
-        value = calloc(256,sizeof(char));
-        while (array){
-          if (array->type == cJSON_String){
-            strcat(value, array->valuestring);
-          }
-          else if (array->type == cJSON_Number){
-            if ((double)array->valueint == array->valuedouble){
-              char value_char[16];
-              snprintf(value_char, 16, "%i", array->valueint);
-              strcat(value, value_char);
+    while (logJSON) {
+        next = logJSON->next;
+        if (logJSON->string) {
+            if (parent) {
+                n = strlen(parent);
+                key = malloc(n + strlen(logJSON->string) + 2);
+                strcpy(key, parent);
+                key[n++] = '.';
+                strcpy(key + n, logJSON->string);
             }
-            else{
-              char value_char[16];
-              snprintf(value_char, 16, "%f", array->valuedouble);
-              strcat(value, value_char);
+            else {
+                key = strdup(logJSON->string);
             }
-          }
-          strcat(value, ",");
-          array = array->next;
         }
-        fillData(lf, key, value);
-        cJSON_Delete(array);
-        free(value);
-        break;
 
-      case cJSON_NULL:
-        fillData(lf, key, "NULL");
-        break;
+        switch ((logJSON->type)&255) {
 
-      case cJSON_True:
-        fillData(lf, key, "true");
-        break;
+            case cJSON_String:
+                fillData(lf, key, logJSON->valuestring);
+                break;
 
-      case cJSON_False:
-        fillData(lf, key, "false");
-        break;
+            case cJSON_Number:
+                if ((double)logJSON->valueint == logJSON->valuedouble){
+                    char value_char[16];
+                    snprintf(value_char, 16, "%i", logJSON->valueint);
+                    fillData(lf, key, value_char);
+                }
+                else{
+                    char value_char[16];
+                    snprintf(value_char, 16, "%f", logJSON->valuedouble);
+                    fillData(lf, key, value_char);
+                }
+                break;
 
-      case cJSON_Object:
-        readJSON (logJSON->child, key, lf);
-        break;
+            case cJSON_Array:
+                array = logJSON->child;
+                value = calloc(256,sizeof(char));
+                while (array){
+                    if (array->type == cJSON_String) {
+                        strcat(value, array->valuestring);
+                    }
+                    else if (array->type == cJSON_Number) {
+                        if ((double)array->valueint == array->valuedouble){
+                            char value_char[16];
+                            snprintf(value_char, 16, "%i", array->valueint);
+                            strcat(value, value_char);
+                        }
+                        else {
+                            char value_char[16];
+                            snprintf(value_char, 16, "%f", array->valuedouble);
+                            strcat(value, value_char);
+                        }
+                    }
+                    else if (array->type == cJSON_NULL) {
+                        strcat(value, "null");
+                    }
+                    else if (array->type == cJSON_True) {
+                        strcat(value, "true");
+                    }
+                    else if (array->type == cJSON_False) {
+                        strcat(value, "false");
+                    }
+                    strcat(value, ",");
+                    array = array->next;
+                }
+                fillData(lf, key, value);
+                cJSON_Delete(array);
+                free(value);
+                break;
 
-    } // switch
-    logJSON = next;
-    free(key);
-  } // while
-  cJSON_Delete(next);
+            case cJSON_NULL:
+                fillData(lf, key, "null");
+                break;
+
+            case cJSON_True:
+                fillData(lf, key, "true");
+                break;
+
+            case cJSON_False:
+                fillData(lf, key, "false");
+                break;
+
+            case cJSON_Object:
+                readJSON (logJSON->child, key, lf);
+                break;
+
+        } // switch
+        logJSON = next;
+        free(key);
+    } // while
+    cJSON_Delete(next);
 }
-
 
 void *JSON_Decoder_Init()
 {
-  debug1("%s: Initializing JSON decoder.", ARGV0);
-  return (NULL);
+    debug1 ("%s: Initializing JSON decoder.", ARGV0);
+    return (NULL);
 }
 
 void *JSON_Decoder_Exec(Eventinfo *lf)
 {
-  cJSON *logJSON;
-  logJSON = cJSON_Parse(lf->log);
-  if (!logJSON)
-    merror("%s: ERROR: JSON parsing. %s", ARGV0, cJSON_GetErrorPtr());
-  else
-  {
-    readJSON (logJSON, NULL, lf);
-    cJSON_Delete(logJSON);
-  }
-
-  return (NULL);
+    cJSON *logJSON;
+    logJSON = cJSON_Parse(lf->log);
+    if (!logJSON)
+        debug2 ("%s: ERROR: Error parsing JSON string. %s", ARGV0, cJSON_GetErrorPtr());
+    else
+    {
+        readJSON (logJSON, NULL, lf);
+        cJSON_Delete (logJSON);
+    }
+    return (NULL);
 }
