d0b787f66e4dd4e26d7aa9edab02546c85a7921e
Jesus Linares <jesuslinares90@gmail.com>
2017-05-05 09:08:49 +0000

    Increase framework version

--- framework/setup.py
+++ framework/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 # Install the package dev: python setup.py develop
 
 setup(name='wazuh',
-      version='3.0',
+      version='3.0.0-alpha1',
       description='Wazuh control with Python',
       url='https://github.com/wazuh',
       author='Wazuh',
