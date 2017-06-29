60ed5547b3da3143e4f5b2313f4cbdcec3253585
Alberto Marín <amnbaza@hotmail.com>
2017-06-29 19:36:51 +0200

    Update script

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(sleep 5 && /var/ossec/var/incoming/wazuh_pkg/install.sh >> /var/ossec/var/incoming/wazuh_pkg/upgrade.log 2>&1) >/dev/null 2>&1 &
+(chmod +x /var/ossec/var/incoming/wazuh_pkg/install.sh && chmod +x /var/ossec/var/incoming/wazuh_pkg/src/init/*.sh && sleep 5 && /var/ossec/var/incoming/wazuh_pkg/install.sh >> /var/ossec/var/incoming/wazuh_pkg/upgrade.log 2>&1) >/dev/null 2>&1 &
