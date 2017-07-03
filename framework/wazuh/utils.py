2fe1fe7f9a59a6f7aee78ff2bff2613e039a339e
Jesus Linares <jesuslinares90@gmail.com>
2017-07-03 18:12:32 +0000

    Framework: Update merged.mg information

--- framework/wazuh/utils.py
+++ framework/wazuh/utils.py
@@ -9,6 +9,7 @@ from tempfile import mkstemp
 from subprocess import call, CalledProcessError
 from os import remove, chmod, chown, path, listdir, close as close
 from datetime import datetime, timedelta
+from hashlib import md5
 import json
 import stat
 
@@ -321,3 +322,10 @@ def chown_r(filepath, uid, gid):
                 chown(itempath, uid, gid)
             elif path.isdir(itempath):
                 chown_r(itempath, uid, gid)
+
+def get_md5(fname):
+    hash_md5 = md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
