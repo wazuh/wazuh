3666582bf48db103682438f0329c5ca273619c55
Alberto Marín <alberto.marin@wazuh.com>
2017-08-10 12:36:42 +0200

    Add Windows upgrading script

---
+++ src/win32/do_upgrade.ps1
@@ -0,0 +1,85 @@
+function backup
+{
+    New-Item -ItemType directory -Path .\backup -ErrorAction SilentlyContinue
+    New-Item -ItemType directory -Path $env:temp\backup -ErrorAction SilentlyContinue
+    Copy-Item .\* $env:temp\backup -force
+    Remove-Item $env:temp\backup\backup -recurse -ErrorAction SilentlyContinue
+    Copy-Item $env:temp\backup\* .\backup -force
+}
+
+function install
+{
+    kill -processname win32ui -ErrorAction SilentlyContinue
+    Remove-Item .\upgrade\upgrade_result -ErrorAction SilentlyContinue
+    Start-Process -FilePath (Get-ChildItem ".\upgrade\wazuh-agent*.exe") -ArgumentList '/S'
+}
+
+function restore
+{
+    Copy-Item .\backup\* .\ -force
+}
+
+
+# Get current version
+$current_version = (Get-Content VERSION.txt).split(" ")[1] -replace "^."
+$current_file_date = (Get-Item ".\ossec-agent.exe").LastWriteTime
+echo "$(Get-Date -format u) - Current version: $($current_version)" > .\upgrade\upgrade.log
+
+# Generating backup
+echo "$(Get-Date -format u) - Generating backup." >> .\upgrade\upgrade.log
+backup
+
+# Install
+echo "$(Get-Date -format u) - Installing" >> .\upgrade\upgrade.log
+install
+
+# Wait for installation completion
+Start-Sleep 5
+$new_file_date = (Get-Item ".\ossec-agent.exe").LastWriteTime
+$counter = 5
+while($current_file_date -ne $new_file_date -And $counter -gt 0)
+{
+    $counter--
+    Start-Sleep 2
+    $new_file_date = (Get-Item ".\ossec-agent.exe").LastWriteTime
+}
+echo "$(Get-Date -format u) - Installation finished." >> .\upgrade\upgrade.log
+
+# Check process status
+$process_id = (Get-Process ossec-agent -ErrorAction SilentlyContinue).id
+$counter = 5
+while($process_id -eq $null -And $counter -gt 0)
+{
+    $counter--
+    Start-Service -Name "Wazuh" -ErrorAction SilentlyContinue
+    Start-Sleep 2
+    $process_id = (Get-Process ossec-agent -ErrorAction SilentlyContinue).id
+}
+echo "$(Get-Date -format u) - Process ID: $($process_id)" >> .\upgrade\upgrade.log
+
+# Check status file
+$status = Get-Content .\ossec-agent.state | select-string "status='connected'" -SimpleMatch
+$counter = 5
+while($status -eq $null -And $counter -gt 0)
+{
+    $counter--
+    Start-Sleep 2
+    $status = Get-Content .\ossec-agent.state | select-string "status='connected'" -SimpleMatch
+}
+echo "$(Get-Date -format u) - Reading status file: $($status)" >> .\upgrade\upgrade.log
+
+If ($status -eq $null)
+{
+    echo "2" > .\upgrade\upgrade_result
+    restore
+    echo "$(Get-Date -format u) - Upgrade failed: Restoring." >> .\upgrade\upgrade.log
+    .\ossec-agent.exe install-service >> .\upgrade\upgrade.log
+    Start-Service -Name "ossec-agent" -ErrorAction SilentlyContinue
+}
+Else
+{
+    echo "0" > .\upgrade\upgrade_result
+    echo "$(Get-Date -format u) - Upgrade finished successfully." >> .\upgrade\upgrade.log
+    $new_version = (Get-Content VERSION.txt).split(" ")[1] -replace "^."
+    echo "$(Get-Date -format u) - New version: $($new_version)" >> .\upgrade\upgrade.log
+}
--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -2,15 +2,10 @@
 
 IF "%1"=="B" GOTO background
 
-CD upgrade
-TASKKILL /F /IM win32ui.exe > nul 2>&1
-START /B upgrade.bat B
+START /B upgrade\upgrade.bat B
 GOTO end
 
 :background
-DEL /Q upgrade_result 2> nul
-SLEEP 5 2> NUL || ping -n 5 127.0.0.1 > NUL
-FOR %%f IN (wazuh-agent-*.exe) DO (%%~nf /S)
-ECHO 0 > upgrade_result
+powershell -ExecutionPolicy ByPass -File upgrade\do_upgrade.ps1
 
 :end
