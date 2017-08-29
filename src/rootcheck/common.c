956779b528b976284f135334b1a802502f6c7f6f
Josh Soref <jsoref@users.noreply.github.com>
2017-08-29 02:31:08 -0400

    spelling: preceded

--- src/rootcheck/common.c
+++ src/rootcheck/common.c
@@ -272,7 +272,7 @@ int pt_check_negate(const char *pattern)
 }
 
 /* Checks if the specific pattern is present on str.
- * A pattern can be preceeded by:
+ * A pattern can be preceded by:
  *                                =: (for equal) - default - strcasecmp
  *                                r: (for ossec regexes)
  *                                >: (for strcmp greater)
