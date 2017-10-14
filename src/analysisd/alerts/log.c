b333d4f4d02f4663d7a47b2431e9c0f52afa6732
Vikman Fdez-Castro <victor@wazuh.com>
2017-10-13 21:23:54 -0700

    Fix Syscheck changes reporting in Maild

--- src/analysisd/alerts/log.c
+++ src/analysisd/alerts/log.c
@@ -239,9 +239,6 @@ void OS_LogOutput(Eventinfo *lf)
             printf("Old inode: %ld\n", lf->inode_before);
         if (lf->inode_after)
             printf("New inode: %ld\n", lf->inode_after);
-
-        if (lf->diff)
-            printf("What changed: %s\n", lf->diff);
     }
 
     // Dynamic fields, except for syscheck events
@@ -410,9 +407,6 @@ void OS_Log(Eventinfo *lf)
             fprintf(_aflog, "Old inode: %ld\n", lf->inode_before);
         if (lf->inode_after)
             fprintf(_aflog, "New inode: %ld\n", lf->inode_after);
-
-        if (lf->diff)
-            fprintf(_aflog, "What changed: %s\n", lf->diff);
     }
 
     // Dynamic fields, except for syscheck events
