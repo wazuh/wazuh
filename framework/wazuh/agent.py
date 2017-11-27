5958a039923c11183eea6c90b417897d943b2647
psanchezr <pablo.sanchez@wazuh.com>
2017-11-27 13:17:23 +0100

    change condition in _load_info_from_DB

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+ç#!/usr/bin/env python
 
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
@@ -85,9 +85,7 @@ class Agent:
         """
         Calculates state based on last keep alive
         """
-	if not last_keep_alive:
-            return None
-        elif last_keep_alive == 0:
+        if last_keep_alive == 0 or not last_keep_alive:
             return "Never connected"
         else:
             limit_seconds = 600*3 + 30
@@ -164,8 +162,11 @@ class Agent:
                 pending = False if self.version != "" else True
             if field == 'dateAdd' and value != None:
                 self.dateAdd = value
-            if field == 'last_keepalive' and value != None:
-                self.lastKeepAlive = value
+            if field == 'last_keepalive':
+		if value != None:
+                    self.lastKeepAlive = value
+		else
+		    self.lastKeepAlive = 0
             if field == 'configSum' and value != None:
                 self.configSum = value
             if field == 'mergedSum' and value != None:
