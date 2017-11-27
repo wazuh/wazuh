55ecaa1a1b07e012485db070217e7fe07c2f91c0
Chema Martinez <chema.martinez@wazuh.com>
2017-11-27 12:23:16 +0100

    Set bin directory as root group for agents

--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -585,7 +585,12 @@ InstallCommon(){
 	${INSTALL} -m 0660 -o ${OSSEC_USER} -g ${OSSEC_GROUP} /dev/null ${PREFIX}/logs/ossec.json
 	${INSTALL} -m 0660 -o ${OSSEC_USER} -g ${OSSEC_GROUP} /dev/null ${PREFIX}/logs/active-responses.log
 
-	${INSTALL} -d -m 0750 -o root -g ${OSSEC_GROUP} ${PREFIX}/bin
+    if [ ${INSTYPE} = 'agent' ]; then
+        ${INSTALL} -d -m 0750 -o root -g 0 ${PREFIX}/bin
+    else
+        ${INSTALL} -d -m 0750 -o root -g ${OSSEC_GROUP} ${PREFIX}/bin
+    fi
+
 	${INSTALL} -d -m 0750 -o root -g 0 ${PREFIX}/lua
 	${INSTALL} -d -m 0750 -o root -g 0 ${PREFIX}/lua/native
 	${INSTALL} -d -m 0750 -o root -g 0 ${PREFIX}/lua/compiled
