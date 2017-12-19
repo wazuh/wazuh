081b8d13b9d1e4e077a7b108b12b5b952ab1df3a
psanchezr <pablo.sanchez@wazuh.com>
2017-12-19 10:12:45 +0100

    Fix defect 1378546

--- src/client-agent/receiver-win.c
+++ src/client-agent/receiver-win.c
@@ -103,9 +103,10 @@ void *receiver_thread(__attribute__((unused)) void *none)
                     os_delwait();
                     update_status(GA_STATUS_ACTIVE);
                     break;
-                }
-
-                if (length > OS_MAXSTR) {
+                }else if (length == 0) {
+                    merror("Empty message from manager");
+                    break;
+                }else if (length > OS_MAXSTR) {
                     merror("Too big message size from manager.");
                     break;
                 }
