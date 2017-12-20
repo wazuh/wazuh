wazuh/wazuh

commit a82e86c7622231e61036cfa9f85ac169aaa033e8

Fix syslog format pre-decoding for logs without space after tag
Such whitespace is optional, according to RFC3164, sections 4.1.3 and 5.3.

Thanks to @rossengeorgiev

src/analysisd/cleanevent.c
@@ -194,9 +194,12 @@ int OS_CleanMSG(char *msg, Eventinfo *lf)
             }
 
             /* Check for the first format: p_name: */
-            if ((*pieces == ':') && (pieces[1] == ' ')) {
+            if (*pieces == ':') {
                 *pieces = '\0';
-                pieces += 2;
+                pieces++;
+
+                // The space after ':' is optional
+                pieces += pieces[1] == ' ';
             }
 
             /* Check for the second format: p_name[pid]: */
@@ -207,8 +210,11 @@ int OS_CleanMSG(char *msg, Eventinfo *lf)
                     pieces++;
                 }
 
-                if ((*pieces == ']') && (pieces[1] == ':') && (pieces[2] == ' ')) {
-                    pieces += 3;
+                if ((*pieces == ']') && (pieces[1] == ':')) {
+                    pieces += 2;
+
+                    // The space after ':' is optional
+                    pieces += pieces[1] == ' ';
                 }
                 /* Some systems are not terminating the program name with
                  * a ':'. Working around this in here...

commit a2678782fa3b4c9e7cef11a08f5e2d6e6bc248c5

Fix bug in pre-decoder for syslog events containing a space after tag
This issue was introduced in commit a82e86c7622231e61036cfa9f85ac169aaa033e8

src/analysisd/cleanevent.c
@@ -199,7 +199,7 @@ int OS_CleanMSG(char *msg, Eventinfo *lf)
                 pieces++;
 
                 // The space after ':' is optional
-                pieces += pieces[1] == ' ';
+                pieces += *pieces == ' ';
             }
 
             /* Check for the second format: p_name[pid]: */
@@ -214,7 +214,7 @@ int OS_CleanMSG(char *msg, Eventinfo *lf)
                     pieces += 2;
 
                     // The space after ':' is optional
-                    pieces += pieces[1] == ' ';
+                    pieces += *pieces == ' ';
                 }
                 /* Some systems are not terminating the program name with
                  * a ':'. Working around this in here...