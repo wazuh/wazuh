7f497591c4983df1fc51eba0a5f64890176d76a8
Marta Gómez Macías <marta@wazuh.com>
2018-02-05 11:26:01 +0100

    Fixes incorrect limit when offset=0 and selecting fields

--- framework/wazuh/wdb.py
+++ framework/wazuh/wdb.py
@@ -42,7 +42,8 @@ class WazuhDBConnection():
             (query_elements[sql_first_index] == 'sql', "Incorrect WDB request type."),
             (query_elements[0] == 'agent' or query_elements[0] == 'global', "The {} database is not valid".format(query_elements[0])),
             (query_elements[1].isdigit() if query_elements[0] == 'agent' else True, "Incorrect agent ID {}".format(query_elements[1])),
-            (query_elements[sql_first_index+1] == 'select', "The API can only send select requests to WDB")
+            (query_elements[sql_first_index+1] == 'select', "The API can only send select requests to WDB"),
+            (not ';' in query, "Found a not valid symbol in database query: ;")
         ]
 
         for check, error_text in input_val_errors:
@@ -74,24 +75,24 @@ class WazuhDBConnection():
         offset = 0
         if 'offset' in query_lower:
             offset = int(re.compile(r".* offset (\d)+").match(query_lower).group(1))
-            query_lower = query_lower.replace("offset {}".format(offset), "")
+            query_lower = query_lower.replace(" offset {}".format(offset), "")
 
         if 'count' not in query_lower:
             lim = 0
             if'limit' in query_lower:
                 lim  = int(re.compile(r".* limit (\d)+").match(query_lower).group(1))
-                query_lower = query_lower.replace("limit {}".format(lim), "")
+                query_lower = query_lower.replace(" limit {}".format(lim), "")
 
             regex  = re.compile(r"\w+ \d+? sql select ([a-z0-9,* ]+) from")
             select = regex.match(query_lower).group(1)
-            countq = query_lower.replace(select, "count({})".format(select))
+            countq = query_lower.replace(select, "count(*)")
             total  = self.__send(countq)[0].values()[0]
 
             limit = total if lim == 0  else lim
 
             response = []
             step = limit if limit < self.request_slice  else self.request_slice
-            for off in range(offset, limit+1, step):
+            for off in range(offset, limit+offset, step):
                 request = "{} limit {} offset {}".format(query_lower, step, off)
                 response.extend(self.__send(request))
             return response
