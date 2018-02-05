d29824a5be88077e874609cfe66ec45f9353c0af
Chema Martinez <chema.martinez@wazuh.com>
2018-02-05 12:14:38 -0800

    Prevent to show an error when the state file is opened for the first time

--- src/wazuh_modules/wmodules.c
+++ src/wazuh_modules/wmodules.c
@@ -203,7 +203,6 @@ int wm_state_io(const char * tag, int op, void *state, size_t size) {
     #endif
 
     if (!(file = fopen(path, op == WM_IO_WRITE ? "wb" : "rb"))) {
-        merror("Unable to open the file: %s. %s", path, strerror(errno));
         return -1;
     }
 
