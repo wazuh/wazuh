6c91b9d91add182621e2c883d43caaa3b9c560c5
Vikman Fernandez-Castro <victor@wazuh.com>
2018-02-07 21:03:54 +0100

    Let remote upgrades execute in dynamic installation directory

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-(sleep 5 && chmod +x /var/ossec/var/upgrade/src/init/*.sh && /var/ossec/var/upgrade/src/init/pkg_installer.sh) >/dev/null 2>&1 &
+. /etc/ossec-init.conf 2> /dev/null || exit 1
+(sleep 5 && chmod +x $DIRECTORY/var/upgrade/src/init/*.sh && $DIRECTORY/var/upgrade/src/init/pkg_installer.sh) >/dev/null 2>&1 &
