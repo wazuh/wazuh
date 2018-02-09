ac4d5a227e7c3f99649509b094f47840f2ab01ed
Lifka <javierizquierdovera@gmail.com>
2018-02-09 11:19:54 +0100

    Add syscollector.py

---
+++ framework/wazuh/syscollector.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+from wazuh import common
+from wazuh.exception import WazuhException
+from wazuh.agent import Agent
+from wazuh import Wazuh
