410fdb70930f70b661e736cfc8de1c727c3ce399
Marta Gómez Macías <marta@wazuh.com>
2018-02-12 23:20:37 +0100

    Improve syscollector API calls and refactor code

--- framework/wazuh/syscollector.py
+++ framework/wazuh/syscollector.py
@@ -10,25 +10,7 @@ from wazuh import Wazuh
 from wazuh.utils import plain_dict_to_nested_dict, cut_array, sort_array, search_array
 from operator import itemgetter
 
-def get_os_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}):
-    """
-    Get info about an agent's OS
-    """
-    # The osinfo fields in database are different in Windows and Linux
-    agent_obj = Agent(agent_id)
-    offset = int(offset)
-    limit = int(limit)
-
-    os_name = agent_obj.get_agent_attr('os_name')
-    windows_fields = {'hostname', 'os_version', 'os_name', 'architecture',
-                      'scan_time', 'scan_id'}
-    linux_fields   = windows_fields | {'sysname', 'version', 'release'}
-
-    valid_select_fields = windows_fields if 'Windows' in os_name else linux_fields
-
-    allowed_sort_fields = {'sysname', 'os_name', 'hostname',
-                           'version', 'architecture','release', 'os_version'}
-                           
+def get_item_agent(agent_id, offset, limit, select, search, sort, filters, valid_select_fields, allowed_sort_fields, table, nested=True, array=False):
     if select:
         select_fields = list(set(select['fields']) & set(windows_fields)) \
                         if 'Windows' in os_name else \
@@ -47,54 +29,63 @@ def get_os_agent(agent_id, offset=0, limit=common.database_limit, select={}, sea
     if sort and sort['fields']:
         # Check if every element in sort['fields'] is in allowed_sort_fields.
         if not set(sort['fields']).issubset(allowed_sort_fields):
-            raise WazuhException(1403, 'Allowed sort fields: {0}. Fields: {1}'.format(allowed_sort_fields, sort['fields']))
-               
-    response, total = Agent(agent_id)._load_info_from_agent_db(table='sys_osinfo',
-                            offset=offset, limit=limit, select=select_fields,
-                            count=True, sort=sort, search=search, filters=filters)
-    return {'totalItems':total, 'items':response}
+            raise WazuhException(1403, 'Allowed sort fields: {0}. Fields: {1}'.format(
+                        ', '.join(allowed_sort_fields), ','.join(sort['fields'])))
+
+    response, total = Agent(agent_id)._load_info_from_agent_db(table=table,
+                        offset=offset, limit=limit, select=select_fields,
+                        count=True, sort=sort, search=search, filters=filters)
+
+    if array:
+        return_data = response
+    elif not response:
+        return_data = {}
+    else:
+        return_data = response[0] if not nested else plain_dict_to_nested_dict(response[0])
+
+    return {'items': return_data, 'totalItems': total} if array else return_data
 
 
-def get_hardware_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}):
+def get_os_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}, nested=True):
+    """
+    Get info about an agent's OS
+    """
+    agent_obj = Agent(agent_id)
+    offset = int(offset)
+    limit = int(limit)
+
+    # The osinfo fields in database are different in Windows and Linux
+    os_name = agent_obj.get_agent_attr('os_name')
+    windows_fields = {'hostname', 'os_version', 'os_name', 'architecture',
+                      'scan_time', 'scan_id'}
+    linux_fields   = windows_fields | {'sysname', 'version', 'release'}
+
+    valid_select_fields = windows_fields if 'Windows' in os_name else linux_fields
+
+    allowed_sort_fields = {'sysname', 'os_name', 'hostname',
+                           'version', 'architecture','release', 'os_version'}
+                           
+    return get_item_agent(agent_id=agent_id, offset=offset, limit=limit, select=select, 
+                         search=search, sort=sort, filters=filters, allowed_sort_fields=None,
+                         valid_select_fields=valid_select_fields, table='sys_osinfo', nested=nested)
+
+
+def get_hardware_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}, nested=True):
     """
     Get info about an agent's OS
     """
     offset = int(offset)
     limit = int(limit)
-
-    agent_obj = Agent(agent_id)
     
     valid_select_fields = ['board_serial', 'cpu_name', 'cpu_cores', 'cpu_mhz',
                            'ram_total', 'ram_free', 'scan_id', 'scan_time']
-
-    allowed_sort_fields = {'board_serial', 'cpu_name', 'cpu_cores',
-                           'cpu_mhz', 'ram_total','scan_id', 'scan_time'}
-                           
-    if select:
-        if not set(select['fields']).issubset(valid_select_fields):
-            uncorrect_fields = map(lambda x: str(x), set(select['fields']) - set(valid_select_fields))
-            raise WazuhException(1724, "Allowed select fields: {0}. Fields {1}".\
-                    format(', '.join(valid_select_fields), ','.join(uncorrect_fields)))
-        select_fields = select['fields']
-    else:
-        select_fields = valid_select_fields
-
-    if search:
-        search['fields'] = select_fields
-        
-    # Sorting
-    if sort and sort['fields']:
-        # Check if every element in sort['fields'] is in allowed_sort_fields.
-        if not set(sort['fields']).issubset(allowed_sort_fields):
-            raise WazuhException(1403, 'Allowed sort fields: {0}. Fields: {1}'.format(allowed_sort_fields, sort['fields']))
               
-    response, total = agent_obj._load_info_from_agent_db(table='sys_hwinfo',
-                            offset=offset, limit=limit, select=select_fields,
-                            count=True, sort=sort, search=search, filters=filters)
-    return {'totalItems':total, 'items':response}
+    return get_item_agent(agent_id=agent_id, offset=offset, limit=limit, select=select, 
+                         search=search, sort=sort, filters=filters, allowed_sort_fields=None,
+                         valid_select_fields=valid_select_fields, table='sys_hwinfo', nested=nested)
 
 
-def get_packages_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}):
+def get_packages_agent(agent_id, offset=0, limit=common.database_limit, select={}, search={}, sort={}, filters={}, nested=False):
     """
     Get info about an agent's programs
     """
@@ -102,35 +93,13 @@ def get_packages_agent(agent_id, offset=0, limit=common.database_limit, select={
     limit = int(limit)
     valid_select_fields = {'scan_id', 'scan_time', 'format', 'name',
                            'vendor', 'version', 'architecture', 'description'}
-    allowed_sort_fields = {'scan_id', 'scan_time', 'format', 'name',
-                           'vendor', 'version', 'architecture', 'description'}
 
-    if select:
-        if not set(select['fields']).issubset(valid_select_fields):
-            uncorrect_fields = map(lambda x: str(x), set(select['fields']) - set(valid_select_fields))
-            raise WazuhException(1724, "Allowed select fields: {0}. Fields {1}".\
-                    format(', '.join(valid_select_fields), ','.join(uncorrect_fields)))
-        select_fields = select['fields']
-    else:
-        select_fields = valid_select_fields
-
-    if search:
-        search['fields'] = select_fields
-
-    # Sorting
-    if sort and sort['fields']:
-        # Check if every element in sort['fields'] is in allowed_sort_fields.
-        if not set(sort['fields']).issubset(allowed_sort_fields):
-            raise WazuhException(1403, 'Allowed sort fields: {0}. Fields: {1}'.format(allowed_sort_fields, sort['fields']))
-
-    response, total = Agent(agent_id)._load_info_from_agent_db(table='sys_programs',
-                                offset=offset, limit=limit, select=select_fields,
-                                count=True, sort=sort, search=search, filters=filters)
-
-    return {'totalItems':total, 'items':response}
+    return get_item_agent(agent_id=agent_id, offset=offset, limit=limit, select=select, 
+                         search=search, sort=sort, filters=filters, allowed_sort_fields=valid_select_fields,
+                         valid_select_fields=valid_select_fields, table='sys_programs', array=True, nested=nested)
 
 
-def _get_agent_items(func, offset, limit, select, filters, search, sort):
+def _get_agent_items(func, offset, limit, select, filters, search, sort, array=False):
     agents, result = Agent.get_agents_overview(select={'fields':['id']})['items'], []
 
     limit = int(limit)
@@ -138,7 +107,11 @@ def _get_agent_items(func, offset, limit, select, filters, search, sort):
     found_limit = False
 
     for agent in agents:
-        items = func(agent_id = agent['id'], select = select, filters = filters, limit = limit, offset = offset, search = search)['items']
+        items = func(agent_id = agent['id'], select = select, filters = filters, limit = limit, offset = offset, search = search, nested=False)
+        if items == {}:
+            continue
+
+        items = [items] if not array else items['items']
 
         for item in items:
             item['agent_id'] = agent['id']
@@ -154,7 +127,7 @@ def _get_agent_items(func, offset, limit, select, filters, search, sort):
 
 def get_packages(offset=0, limit=common.database_limit, select=None, filters={}, search={}, sort={}):
     return _get_agent_items(func=get_packages_agent, offset=offset, limit=limit, select=select, 
-                            filters=filters, search=search, sort=sort)
+                            filters=filters, search=search, sort=sort, array=True)
 
 
 def get_os(filters={}, offset=0, limit=common.database_limit, select={}, search={}, sort={}):
