82d125e1f5847b47d8dda822328f392f41b5ccfe
Alberto Marin <alberto.marin@wazuh.com>
2018-03-14 15:48:46 -0700

    Add remote requests for wmodules config section

--- src/wazuh_modules/wmodules.h
+++ src/wazuh_modules/wmodules.h
@@ -175,4 +175,12 @@ int wm_get_path(const char *binary, char **validated_comm);
 */
 int wm_validate_command(const char *command, const char *digest, crypto_type ctype);
 
+#ifndef WIN32
+// Com request thread dispatcher
+void * wmcom_main(void * arg);
+#endif
+
+size_t wmcom_dispatch(char *command, size_t length __attribute__ ((unused)), char *output);
+size_t wmcom_getconfig(const char * section, char * output);
+
 #endif // W_MODULES
