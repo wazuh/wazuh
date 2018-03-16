5232c0d0e2a4b502dbe47c537be24688e53b6afb
Alberto Marin <alberto.marin@wazuh.com>
2018-03-15 17:29:04 -0700

    Fix compilation warnings in Windows

--- src/os_execd/wcom.c
+++ src/os_execd/wcom.c
@@ -16,6 +16,10 @@
 #include "os_crypto/signature/signature.h"
 #include "wazuh_modules/wmodules.h"
 #include "external/zlib/zlib.h"
+#include "client-agent/agentd.h"
+#include "logcollector/logcollector.h"
+#include "syscheckd/syscheck.h"
+#include "rootcheck/rootcheck.h"
 
 // Current opened file
 
