54fc188cd12229944c81df9bf633fa0e899e2536
shazaum <shazaum@gmail.com>
2018-04-10 15:14:23 -0300

    changed function that checks a host that has already been added, and has no effect for when it is deleted

--- active-response/host-deny.sh
+++ active-response/host-deny.sh
@@ -42,27 +42,27 @@ lock()
         C_PID=`cat ${LOCK_PID} 2>/dev/null`
         if [ "x" = "x${S_PID}" ]; then
             S_PID=${C_PID}
-        fi    
+        fi
 
         # Breaking out of the loop after X attempts
         if [ "x${C_PID}" = "x${S_PID}" ]; then
             i=`expr $i + 1`;
         fi
-   
+
         sleep $i;
-             
+
         i=`expr $i + 1`;
-        
+
         # So i increments 2 by 2 if the pid does not change.
         # If the pid keeps changing, we will increments one
         # by one and fail after MAX_ITERACTION
         if [ "$i" = "${MAX_ITERATION}" ]; then
             echo "`date` Unable to execute. Locked: $0" \
                         >> ${PWD}/ossec-hids-responses.log
-            
+
             # Unlocking and exiting
             unlock;
-            exit 1;                
+            exit 1;
         fi
     done
 }
@@ -70,7 +70,7 @@ lock()
 # Unlock function
 unlock()
 {
-   rm -rf ${LOCK} 
+   rm -rf ${LOCK}
 }
 
 
@@ -80,7 +80,7 @@ echo "`date` $0 $1 $2 $3 $4 $5" >> ${PWD}/../logs/active-responses.log
 
 # IP Address must be provided
 if [ "x${IP}" = "x" ]; then
-   echo "$0: Missing argument <action> <user> (ip)" 
+   echo "$0: Missing argument <action> <user> (ip)"
    exit 1;
 fi
 #
@@ -89,8 +89,10 @@ fi
 IPKEY=$(grep -w "${IP}" /etc/hosts.deny)
 if [ ! -z "$IPKEY" ]
 then
-        echo "`date` Duplicate ip/hostname entry: ${IP}" >> ${PWD}/../logs/active-responses.log
-        exit 1
+    echo "`date` Duplicate ip/hostname entry: ${IP}" >> ${PWD}/../logs/active-responses.log
+	IPKEY="1"
+else
+	IPKEY="0"
 fi
 
 # Checking for invalid entries (lacking "." or ":", etc)
@@ -103,26 +105,30 @@ fi
 
 # Adding the ip to hosts.deny
 if [ "x${ACTION}" = "xadd" ]; then
-   lock;     
+   if [ "$IPKEY" -eq "1" ]; then
+      echo "IP ${IP} already exists on host.deny..." >> ${PWD}/../logs/active-responses.log
+      exit 1
+   fi
+   lock;
    echo "${IP}" | grep "\:" > /dev/null 2>&1
    if [ $? = 0 ]; then
     IP="[${IP}]"
    fi
    if [ "X$UNAME" = "XFreeBSD" ]; then
     echo "ALL : ${IP} : deny" >> /etc/hosts.allow
-   else    
+   else
     echo "ALL:${IP}" >> /etc/hosts.deny
-   fi 
+   fi
    unlock;
    exit 0;
 
 
-# Deleting from hosts.deny   
-elif [ "x${ACTION}" = "xdelete" ]; then   
+# Deleting from hosts.deny
+elif [ "x${ACTION}" = "xdelete" ]; then
    lock;
-   TMP_FILE=`mktemp /var/ossec/ossec-hosts.XXXXXXXXXX` 
-   if [ "X${TMP_FILE}" = "X" ]; then 
-     # Cheap fake tmpfile, but should be harder then no random data 
+   TMP_FILE=`mktemp /var/ossec/ossec-hosts.XXXXXXXXXX`
+   if [ "X${TMP_FILE}" = "X" ]; then
+     # Cheap fake tmpfile, but should be harder then no random data
      TMP_FILE="/var/ossec/ossec-hosts.`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -1 `"
    fi
    echo "${IP}" | grep "\:" > /dev/null 2>&1
@@ -136,14 +142,14 @@ elif [ "x${ACTION}" = "xdelete" ]; then
     cat /etc/hosts.deny | grep -v "ALL:${IP}$"> ${TMP_FILE}
     cat ${TMP_FILE} > /etc/hosts.deny
     rm ${TMP_FILE}
-   fi 
+   fi
    unlock;
    exit 0;
 
 
-# Invalid action   
+# Invalid action
 else
    echo "$0: invalid action: ${ACTION}"
-fi       
+fi
 
 exit 1;
