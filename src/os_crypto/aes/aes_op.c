d52c6808fc40fd55cf9262c58da7e0141e0a828b
Vikman Fernandez-Castro <victor@wazuh.com>
2018-04-27 02:30:21 -0700

    Prevent Remoted from crashing on agent authentication failure

--- src/os_crypto/aes/aes_op.c
+++ src/os_crypto/aes/aes_op.c
@@ -1,14 +1,11 @@
-/* Copyright (C) 2009 Trend Micro Inc.
- * All right reserved.
+/* OS_crypto/AES Library
+ * Copyright (C) 2018 Wazuh Inc.
+ * March 12, 2018.
  *
  * This program is a free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public
  * License (version 2) as published by the FSF - Free Software
- * Foundation
- */
-
-/* OS_crypto/aes Library
- * APIs for many crypto operations
+ * Foundation.
  */
 
 #include <stdio.h>
@@ -34,10 +31,8 @@ int OS_AES_Str(const char *input, char *output, const char *charkey,
     }
     else
     {
-        decrypt_AES((const uchar *)input, (int)size,(uchar *)charkey, iv,(uchar *)output);
+        return decrypt_AES((const uchar *)input, (int)size,(uchar *)charkey, iv,(uchar *)output);
     }
-
-    return (1);
 }
 
 int encrypt_AES(const unsigned char *plaintext, int plaintext_len, unsigned char *key,
@@ -45,22 +40,31 @@ int encrypt_AES(const unsigned char *plaintext, int plaintext_len, unsigned char
   {
 	EVP_CIPHER_CTX *ctx;
 	int len;
-	int ciphertext_len;
-  
-	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-  
-	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-	  handleErrors();
-  
-	if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-	  handleErrors();
+	int ciphertext_len = 0;
+
+	if (!(ctx = EVP_CIPHER_CTX_new())) {
+        goto end;
+    }
+
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {
+        goto end;
+    }
+
+	if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) {
+        goto end;
+    }
+
 	ciphertext_len = len;
-  
-	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+
+	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) {
+        ciphertext_len = 0;
+        goto end;
+    }
+
 	ciphertext_len += len;
-  
+
+end:
 	EVP_CIPHER_CTX_free(ctx);
-  
 	return ciphertext_len;
 }
 
@@ -69,27 +73,30 @@ int decrypt_AES(const unsigned char *ciphertext, int ciphertext_len, unsigned ch
   {
 	EVP_CIPHER_CTX *ctx;
 	int len;
-	int plaintext_len;
-  
-	if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-  
-	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-	  handleErrors();
-  
-	if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-	  handleErrors();
+	int plaintext_len = 0;
+
+	if (!(ctx = EVP_CIPHER_CTX_new())) {
+        goto end;
+    }
+
+	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {
+        goto end;
+    }
+
+	if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) {
+        goto end;
+    }
+
 	plaintext_len = len;
-  
-	if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+
+	if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) {
+        plaintext_len = 0;
+        goto end;
+    }
+
 	plaintext_len += len;
-  
+
+end:
 	EVP_CIPHER_CTX_free(ctx);
-
 	return plaintext_len;
-}
-
-void handleErrors(void)
-{
-  ERR_print_errors_fp(stderr);
-  abort();
 }
