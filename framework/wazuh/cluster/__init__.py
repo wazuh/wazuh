3efe5a9d1b01d9dcd2f872443eda9fa19cc67052
Marta Gómez Macías <marta@wazuh.com>
2018-04-30 11:36:46 +0200

    Add version and revision to the framework cluster subpackage

---
+++ framework/wazuh/cluster/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+__version__ = '3.2.3'
+__revision__ = '3201'
