393e15f1724a08ea4183f1c77fe4d0e2b2a95d31
Jesus Linares <jesuslinares90@gmail.com>
2018-05-04 06:13:17 -0400

    Bump version: v3.2.3

--- framework/wazuh/__init__.py
+++ framework/wazuh/__init__.py
@@ -18,7 +18,7 @@ Wazuh is a python package to manage OSSEC.
 
 """
 
-__version__ = '3.2.2'
+__version__ = '3.2.3'
 
 
 msg = "\n\nPython 2.7 or newer not found."
--- framework/wazuh/cluster/__init__.py
+++ framework/wazuh/cluster/__init__.py
@@ -4,4 +4,4 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '3.2.3'
-__revision__ = '3201'
+__revision__ = '0900'
