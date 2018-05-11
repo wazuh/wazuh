6fb493210d4183b389c0b408ea7f59fbbba4a699
Borja Arroba <borja.arroba@wazuh.com>
2018-05-11 12:41:02 +0200

    Avoid sending uid and gid fields to windows agents

--- src/syscheckd/create_db.c
+++ src/syscheckd/create_db.c
@@ -179,6 +179,27 @@ static int read_file(const char *file_name, int opts, OSMatch *restriction)
                 alertdump = seechanges_addfile(file_name);
             }
 
+#ifdef WIN32
+            snprintf(alert_msg, 1172, "%c%c%c%c%c%c%c%c%c%ld:%d:::%s:%s:%s:%s:%s:%ld:%ld",
+                     opts & CHECK_SIZE ? '+' : '-',
+                     opts & CHECK_PERM ? '+' : '-',
+                     opts & CHECK_OWNER ? '+' : '-',
+                     opts & CHECK_GROUP ? '+' : '-',
+                     opts & CHECK_MD5SUM ? '+' : '-',
+                     sha1s,
+                     sha256s,
+                     opts & CHECK_MTIME ? '+' : '-',
+                     opts & CHECK_INODE ? '+' : '-',
+                     opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
+                     opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
+                     opts & CHECK_MD5SUM ? mf_sum : "xxx",
+                     opts & CHECK_SHA1SUM ? sf_sum : "xxx",
+                     opts & CHECK_SHA256SUM ? sf256_sum : "xxx",
+                     opts & CHECK_OWNER ? get_user(file_name, statbuf.st_uid) : "",
+                     opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
+                     opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
+                     opts & CHECK_INODE ? (long)statbuf.st_ino : 0);
+#else
             snprintf(alert_msg, 1172, "%c%c%c%c%c%c%c%c%c%ld:%d:%d:%d:%s:%s:%s:%s:%s:%ld:%ld",
                      opts & CHECK_SIZE ? '+' : '-',
                      opts & CHECK_PERM ? '+' : '-',
@@ -200,6 +221,7 @@ static int read_file(const char *file_name, int opts, OSMatch *restriction)
                      opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
                      opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
                      opts & CHECK_INODE ? (long)statbuf.st_ino : 0);
+#endif
 
             if (OSHash_Add(syscheck.fp, file_name, strdup(alert_msg)) <= 0) {
                 merror("Unable to add file to db: %s", file_name);
@@ -208,6 +230,21 @@ static int read_file(const char *file_name, int opts, OSMatch *restriction)
             /* Send the new checksum to the analysis server */
             alert_msg[1172] = '\0';
 
+#ifdef WIN32
+            snprintf(alert_msg, 1172, "%ld:%d:::%s:%s:%s:%s:%ld:%ld:%s %s%s%s",
+                opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
+                opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
+                opts & CHECK_MD5SUM ? mf_sum : "xxx",
+                opts & CHECK_SHA1SUM ? sf_sum : "xxx",
+                opts & CHECK_OWNER ? get_user(file_name, statbuf.st_uid) : "",
+                opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
+                opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
+                opts & CHECK_INODE ? (long)statbuf.st_ino : 0,
+                opts & CHECK_SHA256SUM ? sf256_sum : "xxx",
+                file_name,
+                alertdump ? "\n" : "",
+                alertdump ? alertdump : "");
+#else
             snprintf(alert_msg, 1172, "%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s %s%s%s",
                 opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
                 opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
@@ -223,6 +260,7 @@ static int read_file(const char *file_name, int opts, OSMatch *restriction)
                 file_name,
                 alertdump ? "\n" : "",
                 alertdump ? alertdump : "");
+#endif
             send_syscheck_msg(alert_msg);
             free(alertdump);
         } else {
