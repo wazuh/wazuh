38008b505ff13ac44cd6649d48388b2e0a508ca8
Vikman Fernandez-Castro <victor@wazuh.com>
2018-05-15 20:49:10 -0700

    Remoted: fix implicit agent assignation to default group

--- src/remoted/manager.c
+++ src/remoted/manager.c
@@ -568,6 +568,7 @@ static void read_controlmsg(const char *agent_id, char *msg)
     file_sum **f_sum = NULL;
     os_md5 tmp_sum;
     char *end;
+    agent_group *agt_group;
 
     if (!groups) {
         /* Nothing to share with agent */
@@ -653,15 +654,17 @@ static void read_controlmsg(const char *agent_id, char *msg)
                     return;
                 }
             }
+
+            set_agent_group(agent_id, group);
         }
 
         // Check if the group is set in the yaml file
-        agent_group *agt_group = w_parser_get_agent(agent_id);
-
         // Check if the group we want to set is "default", apply yaml configuration
-        if(agt_group && !strncmp(group,"default",7)){
-            if(set_agent_group(agent_id, agt_group->group) == -1){
-                merror("Could not set group '%s' specified in the yaml file for agent '%s'",agt_group->group,agent_id);
+        if (!strcmp(group, "default")) {
+            if (agt_group = w_parser_get_agent(agent_id), agt_group) {
+                if(set_agent_group(agent_id, agt_group->group) == -1){
+                    merror("Could not set group '%s' specified in the yaml file for agent '%s'",agt_group->group,agent_id);
+                }
             }
         }
 
