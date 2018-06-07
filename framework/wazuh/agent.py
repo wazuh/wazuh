241bd8115ee32931573c6c011a19a70c706b9852
Javier Izquierdo Vera <javierizquierdovera@gmail.com>
2018-06-07 19:56:10 +0200

    Fix limit 500 in remove agents (#740)

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1209,7 +1209,7 @@ class Agent:
         :return: Dictionary with affected_agents (agents removed), timeframe applied, failed_ids if it necessary (agents that cannot been removed), and a message.
         """
 
-        agents = Agent.get_agents_overview(status = status, older_than = older_than)
+        agents = Agent.get_agents_overview(status = status, older_than = older_than, limit = None)
         id_purgeable_agents = [agent['id'] for agent in agents['items']]
 
         failed_ids = []
