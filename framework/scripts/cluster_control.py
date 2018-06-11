91afa5ca5bf8e5144edb0b6eca5f237dc76a4cd0
Lifka <javierizquierdovera@gmail.com>
2018-06-11 12:01:20 +0200

    Check python version in cluter control

--- framework/scripts/cluster_control.py
+++ framework/scripts/cluster_control.py
@@ -3,8 +3,13 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
+from sys import argv, exit, path, stdout, version_info
+
+if version_info[0] == 2 and version_info[1] < 7:
+    print("Error: Minimal Python version required is 2.7. Found version is {0}.{1}.".format(version_info[0], version_info[1]))
+    exit()
+
 from os.path import dirname, basename
-from sys import argv, exit, path, stdout
 from itertools import chain
 import argparse
 import logging
