f5a3c1fad498ae523121c2589e87456f30e8ee5b
Vikman Fernandez-Castro <victor@wazuh.com>
2018-06-26 16:10:48 +0200

    Get who-data in Syscheck for new files

--- src/analysisd/decoders/syscheck.c
+++ src/analysisd/decoders/syscheck.c
@@ -19,6 +19,8 @@
 /* Compare the first common fields between sum strings */
 static int SumCompare(const char *s1, const char *s2);
 
+static void InsertWhodata(Eventinfo * lf, const sk_sum_t * sum);
+
 /* Initialize the necessary information to process the syscheck information */
 void SyscheckInit()
 {
@@ -366,56 +368,7 @@ static int DB_Search(const char *f_name, char *c_sum, char *w_sum, Eventinfo *lf
         fflush(fp);
 
         if (result = sk_decode_sum(&newsum, c_sum, w_sum), result != -1) {
-            /* Whodata user */
-            if(newsum.wdata.user_id && newsum.wdata.user_name) {
-                snprintf(sdb.user_name, OS_FLSIZE, "(Audit) User: '%s (%s)'\n", newsum.wdata.user_name, newsum.wdata.user_id);
-                os_strdup(newsum.wdata.user_id, lf->user_id);
-                os_strdup(newsum.wdata.user_name, lf->user_name);
-            } else {
-                *sdb.user_name = '\0';
-            }
-
-            /* Whodata effective user */
-            if(newsum.wdata.effective_uid && newsum.wdata.effective_name && *newsum.wdata.effective_uid != '\0') {
-                snprintf(sdb.effective_name, OS_FLSIZE, "(Audit) Effective user: '%s (%s)'\n", newsum.wdata.effective_name, newsum.wdata.effective_uid);
-                os_strdup(newsum.wdata.effective_uid, lf->effective_uid);
-                os_strdup(newsum.wdata.effective_name, lf->effective_name);
-            } else {
-                *sdb.effective_name = '\0';
-            }
-
-            /* Whodata Audit user */
-            if(newsum.wdata.audit_uid && newsum.wdata.audit_name && *newsum.wdata.audit_uid != '\0') {
-                snprintf(sdb.audit_name, OS_FLSIZE, "(Audit) Login user: '%s (%s)'\n", newsum.wdata.audit_name, newsum.wdata.audit_uid);
-                os_strdup(newsum.wdata.audit_uid, lf->audit_uid);
-                os_strdup(newsum.wdata.audit_name, lf->audit_name);
-            } else {
-                *sdb.audit_name = '\0';
-            }
-
-            /* Whodata Group */
-            if(newsum.wdata.group_id && newsum.wdata.group_name && *newsum.wdata.group_id != '\0') {
-                snprintf(sdb.group_name, OS_FLSIZE, "(Audit) Group: '%s (%s)'\n", newsum.wdata.group_name, newsum.wdata.group_id);
-                os_strdup(newsum.wdata.group_id, lf->group_id);
-                os_strdup(newsum.wdata.group_name, lf->group_name);
-            } else {
-                *sdb.group_name = '\0';
-            }
-
-            /* Whodata process */
-            if(newsum.wdata.process_id) {
-                snprintf(sdb.process_id, OS_FLSIZE, "(Audit) Process id: '%s'\n", newsum.wdata.process_id);
-                os_strdup(newsum.wdata.process_id, lf->process_id);
-            } else {
-                *sdb.process_id = '\0';
-            }
-
-            if(newsum.wdata.process_name) {
-                snprintf(sdb.process_name, OS_FLSIZE, "(Audit) Process name: '%s'\n", newsum.wdata.process_name);
-                os_strdup(newsum.wdata.process_name, lf->process_name);
-            } else {
-                *sdb.process_name = '\0';
-            }
+            InsertWhodata(lf, &newsum);
         }
 
         switch (result) {
@@ -679,12 +632,20 @@ static int DB_Search(const char *f_name, char *c_sum, char *w_sum, Eventinfo *lf
             /* Alert if configured to notify on new files */
             if ((Config.syscheck_alert_new == 1) && DB_IsCompleted(agent_id)) {
                 sdb.syscheck_dec->id = sdb.idn;
+                InsertWhodata(lf, &newsum);
                 sk_fill_event(lf, f_name, &newsum);
 
                 /* New file message */
                 snprintf(sdb.comment, OS_MAXSTR,
                          "New file '%.756s' "
-                         "added to the file system.", f_name);
+                         "added to the file system.\n%s%s%s%s%s%s",
+                         f_name,
+                         sdb.user_name,
+                         sdb.audit_name,
+                         sdb.effective_name,
+                         sdb.group_name,
+                         sdb.process_id,
+                         sdb.process_name);
 
                 /* Create a new log message */
                 free(lf->full_log);
@@ -793,4 +754,57 @@ int SumCompare(const char *s1, const char *s2) {
     size2 = ptr2 ? (size_t)(ptr2 - s2) : strlen(s2);
 
     return size1 == size2 ? strncmp(s1, s2, size1) : 1;
+}
+
+void InsertWhodata(Eventinfo * lf, const sk_sum_t * sum) {
+    /* Whodata user */
+    if(sum->wdata.user_id && sum->wdata.user_name) {
+        snprintf(sdb.user_name, OS_FLSIZE, "(Audit) User: '%s (%s)'\n", sum->wdata.user_name, sum->wdata.user_id);
+        os_strdup(sum->wdata.user_id, lf->user_id);
+        os_strdup(sum->wdata.user_name, lf->user_name);
+    } else {
+        *sdb.user_name = '\0';
+    }
+
+    /* Whodata effective user */
+    if(sum->wdata.effective_uid && sum->wdata.effective_name && *sum->wdata.effective_uid != '\0') {
+        snprintf(sdb.effective_name, OS_FLSIZE, "(Audit) Effective user: '%s (%s)'\n", sum->wdata.effective_name, sum->wdata.effective_uid);
+        os_strdup(sum->wdata.effective_uid, lf->effective_uid);
+        os_strdup(sum->wdata.effective_name, lf->effective_name);
+    } else {
+        *sdb.effective_name = '\0';
+    }
+
+    /* Whodata Audit user */
+    if(sum->wdata.audit_uid && sum->wdata.audit_name && *sum->wdata.audit_uid != '\0') {
+        snprintf(sdb.audit_name, OS_FLSIZE, "(Audit) Login user: '%s (%s)'\n", sum->wdata.audit_name, sum->wdata.audit_uid);
+        os_strdup(sum->wdata.audit_uid, lf->audit_uid);
+        os_strdup(sum->wdata.audit_name, lf->audit_name);
+    } else {
+        *sdb.audit_name = '\0';
+    }
+
+    /* Whodata Group */
+    if(sum->wdata.group_id && sum->wdata.group_name && *sum->wdata.group_id != '\0') {
+        snprintf(sdb.group_name, OS_FLSIZE, "(Audit) Group: '%s (%s)'\n", sum->wdata.group_name, sum->wdata.group_id);
+        os_strdup(sum->wdata.group_id, lf->group_id);
+        os_strdup(sum->wdata.group_name, lf->group_name);
+    } else {
+        *sdb.group_name = '\0';
+    }
+
+    /* Whodata process */
+    if(sum->wdata.process_id) {
+        snprintf(sdb.process_id, OS_FLSIZE, "(Audit) Process id: '%s'\n", sum->wdata.process_id);
+        os_strdup(sum->wdata.process_id, lf->process_id);
+    } else {
+        *sdb.process_id = '\0';
+    }
+
+    if(sum->wdata.process_name) {
+        snprintf(sdb.process_name, OS_FLSIZE, "(Audit) Process name: '%s'\n", sum->wdata.process_name);
+        os_strdup(sum->wdata.process_name, lf->process_name);
+    } else {
+        *sdb.process_name = '\0';
+    }
 }
