92eeef88e56cb0de4452b0a71bf21d3ebbd46d7f
Borja Arroba <b.arroba.h@gmail.com>
2018-06-29 11:28:59 +0200

    Prevent to insert '/' or '\' at the end of directories path

--- src/config/syscheck-config.c
+++ src/config/syscheck-config.c
@@ -38,17 +38,6 @@ int dump_syscheck_entry(syscheck_config *syscheck, const char *entry, int vals, 
     }
 
     else {
-#ifdef WIN32
-        char *ptfile;
-        /* Change forward slashes to backslashes on entry */
-        ptfile = strchr(entry, '/');
-        while (ptfile) {
-            *ptfile = '\\';
-            ptfile++;
-
-            ptfile = strchr(ptfile, '/');
-        }
-#endif
         if (syscheck->dir == NULL) {
             os_calloc(2, sizeof(char *), syscheck->dir);
             syscheck->dir[pl + 1] = NULL;
@@ -634,6 +623,23 @@ int Read_Syscheck(XML_NODE node, void *configp, __attribute__((unused)) void *ma
         /* Get directories */
         else if (strcmp(node[i]->element, xml_directories) == 0) {
             char dirs[OS_MAXSTR];
+
+            char *ptfile;
+#ifdef WIN32
+            /* Change backslashes to forwardslashes on entry */
+            ptfile = strchr(node[i]->content, '/');
+            while (ptfile) {
+                *ptfile = '\\';
+                ptfile++;
+
+                ptfile = strchr(ptfile, '/');
+            }
+#endif
+            ptfile = node[i]->content;
+            ptfile += strlen(node[i]->content + 1);
+            if (*ptfile == '/' || *ptfile == '\\') {
+                *ptfile = '\0';
+            }
 
 #ifdef WIN32
             ExpandEnvironmentStrings(node[i]->content, dirs, sizeof(dirs) - 1);
