7744aacdc7f8567ab41a8240d863cee85ca496e3
Marta Gómez Macías <marta@wazuh.com>
2018-07-03 11:50:29 -0700

    Revert "Add 'client' as valid node_type and show a warning if it is used"
    This reverts commit 5b9edad96d94aa76845fe236bc0a968c8252bcba.

--- framework/scripts/wazuh-clusterd.py
+++ framework/scripts/wazuh-clusterd.py
@@ -279,7 +279,7 @@ if __name__ == '__main__':
                 else:
                     logger.error("{0}".format(str(e)))
 
-        elif cluster_config['node_type'] == "worker" or cluster_config['node_type'] == "client":
+        elif cluster_config['node_type'] == "worker":
             manager_tag = "Worker"
             logger.info("[{0}] PID: {1}".format(manager_tag, getpid()))
 
