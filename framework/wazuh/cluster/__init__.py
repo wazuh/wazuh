ae50fd4abcddb09f2f6a295273bff1efb6687dd9
Vikman Fernandez-Castro <victor@wazuh.com>
2018-07-18 16:59:28 +0000

    Bump revision to 3408

--- framework/wazuh/cluster/__init__.py
+++ framework/wazuh/cluster/__init__.py
@@ -4,7 +4,7 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '3.4.0'
-__revision__ = '3407'
+__revision__ = '3408'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
--- src/win32/ossec-installer.nsi
+++ src/win32/ossec-installer.nsi
@@ -21,7 +21,7 @@
 !define MUI_ICON install.ico
 !define MUI_UNICON uninstall.ico
 !define VERSION "3.4.0"
-!define REVISION "3407"
+!define REVISION "3408"
 !define NAME "Wazuh"
 !define SERVICE "OssecSvc"
 
