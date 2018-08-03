842ace1b57ec98d94525dbb6dd9231cf67a08e9d
Marta Gómez Macías <marta@wazuh.com>
2018-08-02 20:40:42 -0700

    Fix bug when installing framework package

--- framework/setup.py
+++ framework/setup.py
@@ -3,7 +3,7 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Install the package locally: python setup.py install
 # Install the package dev: python setup.py develop
@@ -15,6 +15,6 @@ setup(name='wazuh',
       author='Wazuh',
       author_email='hello@wazuh.com',
       license='GPLv2',
-      packages=['wazuh'],
+      packages=find_packages(),
       install_requires=[],
       zip_safe=False)
