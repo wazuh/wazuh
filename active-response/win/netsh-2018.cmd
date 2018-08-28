1b8d01f102943b66c6003dcf53309fa7b267a4aa
frgv <36547287+frgv@users.noreply.github.com>
2018-08-28 19:20:52 +0200

    Create netsh-2018.cmd
    Create script for blocking IP's using netsh-advfirewall.

---
+++ active-response/win/netsh-2018.cmd
@@ -0,0 +1,30 @@
+:: Simple script to block an ip using netsh-advfirewall. Created by Wazuh, Inc. <support@wazuh.com>.
+@ECHO OFF
+ECHO.
+
+
+:: Logging it all
+FOR /F "TOKENS=1* DELIMS= " %%A IN ('DATE/T') DO SET DATE=%%B
+FOR /F "TOKENS=1* DELIMS= " %%A IN ('TIME/T') DO SET TIME=%%A
+ECHO %DATE% %TIME% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9 >> active-response/active-responses.log
+
+
+IF "%1"=="add" GOTO ADD
+IF "%1"=="delete" GOTO DEL
+:ERROR
+
+ECHO "Invalid argument. %1"
+GOTO Exit;
+
+
+:: Adding to the blocked.
+
+:ADD
+:: Extracts last ip address from ipconfig.
+netsh advfirewall firewall add rule name="WAZUH ACTIVE RESPONSE BLOCKED IP" interface=any dir=in action=block remoteip=%3/32
+GOTO Exit;
+
+:DEL
+netsh advfirewall firewall delete rule name="WAZUH ACTIVE RESPONSE BLOCKED IP" remoteip=%3/32
+
+:Exit
