50028ee431f18bead53ac82b2653b5d2dd498b91
Vikman Fernandez-Castro <victor@wazuh.com>
2018-09-03 19:31:46 +0200

    Replace space in pthreads_op.c

--- src/shared/pthreads_op.c
+++ src/shared/pthreads_op.c
@@ -15,7 +15,7 @@
  * Returns 0 on success or -1 on error
  */
 
-#if defined(__MACH__) || defined(AIX) || defined(HPUX)
+#if defined(__MACH__) || defined(AIX) || defined(HPUX)
 
 #include <sys/resource.h>
 
