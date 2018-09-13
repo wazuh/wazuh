fc10bc846e5bc1159db5e9580074cd9b094a9c1f
druizz90 <demetrio.ruiz@wazuh.com>
2018-09-13 09:32:51 +0200

    Docker woodle can send messages to Wazuh socket.

--- wodles/docker-listener/EventListener.py
+++ wodles/docker-listener/EventListener.py
@@ -11,36 +11,70 @@ import sys
 class DockerListener:
 
     def __init__(self, interval=1):
+        """"
+        Docker constructor
+
+        """
         # socket variables
         self.wazuh_path = open('/etc/ossec-init.conf').readline().split('"')[1]
         self.wazuh_queue = '{0}/queue/ossec/queue'.format(self.wazuh_path)
+        self.msg_header = "1:Wazuh-Docker:"
         # docker variables
         self.interval = interval
         self.client = docker.from_env()
-        self.event_list = []
+        self.event_list = []  # could be removed
         thread = threading.Thread(target=self.listen)
         thread.daemon = True
         thread.start()
 
     def listen(self):
+        """
+        Listens Docker events
+
+        """
         for event in self.client.events():
-            self.event_list.append(event)
-            # print(event)
+            print(event)
             self.process(event)
 
     def process(self, event):
+        """"
+        Processes a main Docker event
+
+        :param event: Docker full event.
+        """
         if("\"status\":\"unpause\"" in str(event)):
             print("One container was restarted")
+            self.event_list.append(event)
+            # sends the event to the socket
+            self.send_msg(str(event))
         elif("\"status\":\"pause\"" in str(event)):
             print("One container was paused")
+            self.event_list.append(event)
+            # sends the event to the socket
+            self.send_msg(str(event))
         elif("\"status\":\"start\"" in str(event)):
             print("One container was started")
+            self.event_list.append(event)
+            # sends the event to the socket
+            self.send_msg(str(event))
         elif("\"status\":\"stop\"" in str(event)):
             print("One container was stopped")
+            self.event_list.append(event)
+            # sends the event to the socket
+            self.send_msg(str(event))
 
-    def debug(msg, msg_level):
+    def debug(self, msg, msg_level):
         # if debug_level >= msg_level:
         print('DEBUG: {debug_msg}'.format(debug_msg=msg))
+
+    def format_msg(self, msg):
+        """
+        Formats a Docker event
+
+        :param msg: message to be formatted.
+        :return: formatted message.
+        """
+        return {'integration': 'docker', 'data': msg}
 
     def send_msg(self, msg):
         """
@@ -49,8 +83,8 @@ class DockerListener:
         :param msg: JSON message to be sent.
         """
         try:
-            json_msg = json.dumps(msg)
-            self.debug(json_msg, 3)
+            json_msg = json.dumps(self.format_msg(msg))
+            # self.debug(json_msg, 3)
             s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
             s.connect(self.wazuh_queue)
             s.send("{header}{msg}".format(header=self.msg_header,
@@ -71,18 +105,4 @@ class DockerListener:
 if __name__ == "__main__":
     dl = DockerListener()
     while True:
-        time.sleep(5)
-
-"""
-msg = {
-            'integration': 'aws',
-            'aws': {
-                'log_info': {
-                    'aws_account_alias': self.account_alias,
-                    'log_file': log_key,
-                    's3bucket': self.bucket
-                }
-            }
-        }
-must have -> {'integration':, 'data':}
-"""
+        time.sleep(60)
