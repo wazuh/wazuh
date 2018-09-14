8217e7caf63ebd6f2e9b3cc118248ee8432dace5
druizz90 <demetrio.ruiz@wazuh.com>
2018-09-14 09:34:24 +0200

    Key is hidden now.

--- framework/wazuh/configuration.py
+++ framework/wazuh/configuration.py
@@ -413,6 +413,8 @@ def get_ossec_conf(section=None, field=None):
     except Exception as e:
         raise WazuhException(1101, str(e))
 
+    data["cluster"].pop("key")  # removes key field
+
     if section:
         try:
             data = data[section]
