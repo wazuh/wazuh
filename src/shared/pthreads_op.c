afaac0d83580efeeea014cbe344f617654423542
Chema Martinez <chema.martinez@wazuh.com>
2018-09-18 01:24:30 -0700

    Ignore system default stack size

--- src/shared/pthreads_op.c
+++ src/shared/pthreads_op.c
@@ -19,40 +19,29 @@
 int CreateThreadJoinable(pthread_t *lthread, void * (*function_pointer)(void *), void *data)
 {
     pthread_attr_t attr;
-    struct rlimit lim;
     size_t read_size = 0;
     size_t stacksize = 0;
     int ret = 0;
 
-    if (getrlimit(RLIMIT_STACK, &lim)) {
-        merror_exit("At getrlimit(RLIMIT_STACK): %s (%d)", strerror(errno), errno);
+    if (pthread_attr_init(&attr)) {
+        merror(THREAD_ERROR);
+        return -1;
     }
 
-    if (lim.rlim_cur != RLIM_INFINITY && lim.rlim_cur >= PTHREAD_STACK_MIN) {
-        if (pthread_attr_init(&attr)) {
-            merror(THREAD_ERROR);
-            return -1;
-        }
+    read_size = 1024 * (size_t)getDefine_Int("wazuh", "thread_stack_size", 2048, 65536);
 
-        read_size = 1024 * (size_t)getDefine_Int("wazuh", "thread_stack_size", 2048, 65536);
-
-        if (lim.rlim_cur < read_size) {
-            read_size = lim.rlim_cur;
-        }
-
-        /* Set the maximum stack limit to new threads */
-        if (pthread_attr_setstacksize(&attr, read_size)) {
-            merror(THREAD_ERROR);
-            return -1;
-        }
-
-        if (pthread_attr_getstacksize(&attr, &stacksize)) {
-            merror(THREAD_ERROR);
-            return -1;
-        }
-
-        mdebug2("Thread stack size set to: %d", (int)stacksize);
+    /* Set the maximum stack limit to new threads */
+    if (pthread_attr_setstacksize(&attr, read_size)) {
+        merror(THREAD_ERROR);
+        return -1;
     }
+
+    if (pthread_attr_getstacksize(&attr, &stacksize)) {
+        merror(THREAD_ERROR);
+        return -1;
+    }
+
+    mdebug2("Thread stack size set to: %d KiB", (int)stacksize / 1024);
 
     ret = pthread_create(lthread, &attr, function_pointer, (void *)data);
     if (ret != 0) {
