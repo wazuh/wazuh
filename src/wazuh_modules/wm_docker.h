347cb9a31647f908e1f347ad9113cd6bd0b5ad1f
Alberto Marin <alberto.marin@wazuh.com>
2018-10-03 18:10:24 +0200

    Fix compilation issue

--- src/wazuh_modules/wm_docker.h
+++ src/wazuh_modules/wm_docker.h
@@ -9,10 +9,9 @@
  * Foundation.
  */
 
-#ifndef CLIENT
-#ifndef WIN32
 #ifndef WM_DOCKER
 #define WM_DOCKER
+#ifndef WIN32
 
 #define WM_DOCKER_LOGTAG ARGV0 ":docker-listener"
 #define WM_DOCKER_SCRIPT_PATH WM_DEFAULT_DIR "/docker/DockerListener"
@@ -35,6 +34,5 @@ extern const wm_context WM_DOCKER_CONTEXT;   // Context
 // Parse XML configuration
 int wm_docker_read(xml_node **nodes, wmodule *module);
 
-#endif
 #endif
 #endif
