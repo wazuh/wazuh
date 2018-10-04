b5e68cdd2b3f93ce9f4134cc299b1a0823ebfeb8
Mitch Anderson <manderson@e7440-slibby.imm.corp>
2018-10-04 10:53:28 -0600

    initial Abuse IPDB alert commit

---
+++ active-response/abuseipdb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+PWD=$(pwd)
+# Enter API Key from AbuseIPDB
+API_KEY=""
+REPORT_URL="https://www.abuseipdb.com/report/json?"
+# If you want to change the message, you have to encode it
+COMMENT="Automatic%20report%20generated%20by%20OSSEC"
+IP="$3"
+# Web Attack
+CATEGORY="21"
+FULL_URL="${REPORT_URL}key=${API_KEY}&category=${CATEGORY}&comment=${COMMENT}&ip=${IP}"
+LOG_FILE="${PWD}/../logs/active-responses.log"
+curl "${FULL_URL}" | tee -a "${LOG_FILE}"
+# Sample <command>
+#   <command>
+#    <name>abuse-ipdb</name>
+#    <executable>abuseipdb.sh</executable>
+#    <expect>srcip</expect>
+#    <timeout_allowed>no</timeout_allowed>
+# </command>
+# Sample active-response
+#  <active-response>
+#    <command>abuse-ipdb</command>
+#    <location>server</location>
+#    <rules_group>appsec</rules_group>
+#  </active-response>
