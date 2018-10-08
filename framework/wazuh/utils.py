1e162273edf916701c7a5bcef690f694ddb2620e
druizz90 <demetrio.ruiz@wazuh.com>
2018-10-08 08:47:59 +0200

    Fix for decoding errors in tail function.

--- framework/wazuh/utils.py
+++ framework/wazuh/utils.py
@@ -280,32 +280,31 @@ def tail(filename, n=20):
     :param n: number of lines.
     :return: Array of last lines.
     """
-    f = open(filename, 'rb')
-    total_lines_wanted = n
+    with open(filename, 'rb') as f:
+        total_lines_wanted = n
 
-    BLOCK_SIZE = 1024
-    f.seek(0, 2)
-    block_end_byte = f.tell()
-    lines_to_go = total_lines_wanted
-    block_number = -1
-    blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting from the end of the file
-    while lines_to_go > 0 and block_end_byte > 0:
-        if (block_end_byte - BLOCK_SIZE > 0):
-            # read the last block we haven't yet read
-            f.seek(block_number*BLOCK_SIZE, 2)
-            blocks.append(f.read(BLOCK_SIZE).decode('utf-8'))
-        else:
-            # file too small, start from beginning
-            f.seek(0,0)
-            # only read what was not read
-            blocks.append(f.read(block_end_byte).decode('utf-8'))
-        lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
-        block_end_byte -= BLOCK_SIZE
-        block_number -= 1
-    all_read_text = ''.join(reversed(blocks))
+        BLOCK_SIZE = 1024
+        f.seek(0, 2)
+        block_end_byte = f.tell()
+        lines_to_go = total_lines_wanted
+        block_number = -1
+        blocks = []  # blocks of size BLOCK_SIZE, in reverse order starting from the end of the file
+        while lines_to_go > 0 and block_end_byte > 0:
+            if (block_end_byte - BLOCK_SIZE > 0):
+                # read the last block we haven't yet read
+                f.seek(block_number * BLOCK_SIZE, 2)
+                blocks.append(f.read(BLOCK_SIZE).decode('utf-8', errors='replace'))
+            else:
+                # file too small, start from beginning
+                f.seek(0, 0)
+                # only read what was not read
+                blocks.append(f.read(block_end_byte).decode('utf-8', errors='replace'))
+            lines_found = blocks[-1].count('\n')
+            lines_to_go -= lines_found
+            block_end_byte -= BLOCK_SIZE
+            block_number -= 1
+        all_read_text = ''.join(reversed(blocks))
 
-    f.close()
     #return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
     return all_read_text.splitlines()[-total_lines_wanted:]
 
