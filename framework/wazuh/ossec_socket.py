27abfd31c1c5b4fb6068183fc75acb6e65292897
druizz90 <demetrio.ruiz@wazuh.com>
2018-10-16 12:08:40 +0200

    Refactor in send function.

--- framework/wazuh/ossec_socket.py
+++ framework/wazuh/ossec_socket.py
@@ -27,11 +27,12 @@ class OssecSocket:
     def close(self):
         self.s.close()
 
-    def send(self, msg):
-        if isinstance(msg, str):
-            msg = msg.encode()
+    def send(self, msg_bytes):
+        if not isinstance(msg_bytes, bytes):
+            raise WazuhException(1104, "Type must be bytes")
+
         try:
-            sent = self.s.send(pack("<I", len(msg)) + msg)
+            sent = self.s.send(pack("<I", len(msg_bytes)) + msg_bytes)
             if sent == 0:
                 raise WazuhException(1014, self.path)
             return sent
@@ -58,7 +59,7 @@ class OssecSocketJSON(OssecSocket):
         OssecSocket.__init__(self, path)
 
     def send(self, msg):
-        return OssecSocket.send(self, dumps(msg))
+        return OssecSocket.send(self, dumps(msg).encode())
 
     def receive(self):
         response = loads(OssecSocket.receive(self).decode())
