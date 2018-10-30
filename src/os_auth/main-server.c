26d7fb5e08484e4d56a7b40670b76eb666e2bc88
Vikman Fernandez-Castro <victor@wazuh.com>
2018-10-30 12:20:26 +0100

    Let Authd log the client IP on timeout

--- src/os_auth/main-server.c
+++ src/os_auth/main-server.c
@@ -658,14 +658,15 @@ void* run_dispatcher(__attribute__((unused)) void *arg) {
 
         buf[0] = '\0';
         ret = SSL_read(ssl, buf, OS_SIZE_65536 + OS_SIZE_4096);
-        if (ssl_error(ssl, ret)) {
-            merror("SSL Error (%d)", ret);
-            SSL_free(ssl);
-            close(client.socket);
-            free(buf);
-            continue;
-        } else if (ret <= 0) {
-            minfo("Client was disconnected, or network timeout.");
+        if (ret <= 0) {
+            switch (ssl_error(ssl, ret)) {
+            case 0:
+                minfo("Client timeout from %s", srcip);
+                break;
+            default:
+                merror("SSL Error (%d)", ret);
+            }
+
             SSL_free(ssl);
             close(client.socket);
             free(buf);
