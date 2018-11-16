b8d258d1a834642eb57f6904b042f71574444b5e
Borja Arroba <borja.arroba@wazuh.com>
2018-11-16 16:32:38 +0100

    Fix bug configuring empty blocks in FIM

--- src/config/syscheck-config.c
+++ src/config/syscheck-config.c
@@ -787,15 +787,7 @@ int Read_Syscheck(const OS_XML *xml, XML_NODE node, void *configp, __attribute__
     syscheck = (syscheck_config *)configp;
     unsigned int nodiff_size = 0;
 
-    /* If no options are defined, disable it */
-    if (!node) {
-        syscheck->disabled = 1;
-        return 0;
-    } else {
-        syscheck->disabled = 0;
-    }
-
-    while (node[i]) {
+    while (node && node[i]) {
         if (!node[i]->element) {
             merror(XML_ELEMNULL);
             return (OS_INVALID);
