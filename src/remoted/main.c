60329ef2e230a71fec8777afdc9bb06b2b238c3e
DaveVG1 <vgdavid91@gmail.com>
2018-11-19 08:40:53 +0100

    Fixed cluster error message Not showing error message when cluster configuration is diabled or non-existent. Change in error message when there's a real error (node type not valid).

--- src/remoted/main.c
+++ src/remoted/main.c
@@ -136,7 +136,7 @@ int main(int argc, char **argv)
 
     switch (is_worker){
         case -1:
-            merror("Cannot read cluster config %s", strerror(errno));
+            merror("Cluster node type is not valid [client/worker/master]. Cluster not configured %s (%d)", strerror(errno), errno);
             break;
         case 1:
             mdebug1("Cluster client node: Disabling the merged.mg creation");
