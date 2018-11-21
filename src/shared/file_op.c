6b314b8384fab4025462d4e32864f13dce78f7a1
Vikman Fernandez-Castro <victor@wazuh.com>
2018-11-21 12:45:11 +0100

    Let Auth client change to the installation directory

--- src/shared/file_op.c
+++ src/shared/file_op.c
@@ -1861,6 +1861,41 @@ const char *getuname()
     return (ret);
 }
 
+// Move to the directory where this executable lives in
+
+void w_ch_exec_dir() {
+    TCHAR path[2048];
+    DWORD last_error;
+    int ret;
+
+    /* Get full path to the directory this executable lives in */
+    ret = GetModuleFileName(NULL, path, sizeof(path));
+
+    /* Check for errors */
+    if (!ret) {
+        merror_exit(GMF_ERROR);
+    }
+
+    /* Get last error */
+    last_error = GetLastError();
+
+    /* Look for errors */
+    if (last_error != ERROR_SUCCESS) {
+        if (last_error == ERROR_INSUFFICIENT_BUFFER) {
+            merror_exit(GMF_BUFF_ERROR, ret, sizeof(path));
+        } else {
+            merror_exit(GMF_UNKN_ERROR, last_error);
+        }
+    }
+
+    /* Remove file name from path */
+    PathRemoveFileSpec(path);
+
+    /* Move to correct directory */
+    if (chdir(path)) {
+        merror_exit(CHDIR_ERROR, path, errno, strerror(errno));
+    }
+}
 
 #endif /* WIN32 */
 
@@ -2077,7 +2112,7 @@ int w_copy_file(const char *src, const char *dst,char mode,char * message) {
     else {
         fp_dst = fopen(dst, "w");
     }
-    
+
 
     if (!fp_dst) {
         merror("At w_copy_file(): Couldn't open file '%s'", dst);
@@ -2362,7 +2397,7 @@ char ** wreaddir(const char * name) {
 
         files = realloc(files, (i + 2) * sizeof(char *));
         if(!files){
-           merror_exit(MEM_ERROR, errno, strerror(errno)); 
+           merror_exit(MEM_ERROR, errno, strerror(errno));
         }
         files[i++] = strdup(dirent->d_name);
     }
@@ -2474,7 +2509,7 @@ int w_remove_line_from_file(char *file,int line){
 
         if(i != line){
             count_w = fwrite(buffer, 1, strlen(buffer) , fp_dst);
-           
+
             if (count_w != strlen(buffer) || ferror(fp_dst)) {
                 merror("At remove_line_from_file(): Couldn't write file '%s'", destination);
                 break;
