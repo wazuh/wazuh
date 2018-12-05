a65c0b620aaa45a5fb89b93a7c114d3aff960db7
juan70 <juan70@users.noreply.github.com>
2018-12-05 12:59:52 +0100

    Allow strings up to 64KB long for log difference analysis (#2032)

--- src/analysisd/dodiff.c
+++ src/analysisd/dodiff.c
@@ -69,11 +69,11 @@ int doDiff(RuleInfo *rule, Eventinfo *lf)
     time_t date_of_change;
     char *htpt = NULL;
     char flastfile[OS_SIZE_2048 + 1];
-    char flastcontent[OS_SIZE_8192 + 1];
+    char flastcontent[OS_SIZE_65536 + 1];
 
     /* Clean up global */
     flastcontent[0] = '\0';
-    flastcontent[OS_SIZE_8192] = '\0';
+    flastcontent[OS_SIZE_65536] = '\0';
 
     if (lf->hostname[0] == '(') {
         htpt = strchr(lf->hostname, ')');
@@ -99,7 +99,7 @@ int doDiff(RuleInfo *rule, Eventinfo *lf)
     }
 
     /* lf->size can't be too long */
-    if (lf->size >= OS_SIZE_8192) {
+    if (lf->size >= OS_SIZE_65536) {
         merror("Event size (%zd) too long for diff.", lf->size);
         return (0);
     }
@@ -121,7 +121,7 @@ int doDiff(RuleInfo *rule, Eventinfo *lf)
             return (0);
         }
 
-        n = fread(flastcontent, 1, OS_SIZE_8192, fp);
+        n = fread(flastcontent, 1, OS_SIZE_65536, fp);
         if (n > 0) {
             flastcontent[n] = '\0';
         } else {
