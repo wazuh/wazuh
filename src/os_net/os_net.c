0b4e092c26831ceeef949f0e1f0456f1286bbd2a
Vikman Fernandez-Castro <victor@wazuh.com>
2018-12-30 17:32:12 +0100

    Fix socket timeout setup in agent running on Windows

--- src/os_net/os_net.c
+++ src/os_net/os_net.c
@@ -518,14 +518,24 @@ int OS_CloseSocket(int socket)
 
 int OS_SetRecvTimeout(int socket, long seconds, long useconds)
 {
+#ifdef WIN32
+    DWORD ms = seconds * 1000 + useconds / 1000;
+    return setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, (const void *)&ms, sizeof(ms));
+#else
     struct timeval tv = { seconds, useconds };
     return setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, (const void *)&tv, sizeof(tv));
+#endif
 }
 
 int OS_SetSendTimeout(int socket, int seconds)
 {
+#ifdef WIN32
+    DWORD ms = seconds * 1000;
+    return setsockopt(socket, SOL_SOCKET, SO_SNDTIMEO, (const void *)&ms, sizeof(ms));
+#else
     struct timeval tv = { seconds, 0 };
     return setsockopt(socket, SOL_SOCKET, SO_SNDTIMEO, (const void *)&tv, sizeof(tv));
+#endif
 }
 
 /* Send secure TCP message
