c9f32c9ce145f5e4649cdf20c527de5d208e3897
Alberto Marin <alberto.marin@wazuh.com>
2019-01-09 11:25:51 +0100

    Add follow_symbolic_link option to info message

--- src/config/syscheck-config.c
+++ src/config/syscheck-config.c
@@ -1309,6 +1309,7 @@ char *syscheck_opts2str(char *buf, int buflen, int opts) {
         CHECK_INODE,
         CHECK_WHODATA,
         CHECK_ATTRS,
+        CHECK_FOLLOW,
 	0
 	};
     char *check_strings[] = {
@@ -1325,6 +1326,7 @@ char *syscheck_opts2str(char *buf, int buflen, int opts) {
         "inode",
         "whodata",
         "attributes",
+        "follow_symbolic_link",
 	NULL
 	};
 
