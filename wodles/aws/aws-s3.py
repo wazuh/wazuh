04b1b2082f8221d8f3f273d2b210aa25a05986e5
druizz90 <demetrio.ruiz@wazuh.com>
2019-01-14 08:59:52 +0100

    New column was added for Inspector table (account ID).

--- wodles/aws/aws-s3.py
+++ wodles/aws/aws-s3.py
@@ -1939,8 +1939,8 @@ def main(argv):
             if not options.regions:
                 debug("+++ Warning: No regions were specified, trying to get events from all regions", 1)
                 options.regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-                    'ap-northeast-2', 'ap-southeast-2', 'ap-south-1', 'eu-central-1',
-                    'eu-west-1']
+                    'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-2', 'ap-south-1',
+                    'eu-central-1', 'eu-west-1']
 
             for region in options.regions:
                 service = service_type(reparse=options.reparse, access_key=options.access_key,
