3da7f7686edc323f2d092d6aefa26ffba94a1c2a
Marta Gómez Macías <marta@wazuh.com>
2019-01-15 17:31:21 +0100

    Fix bug in cluster_control -a when there is never connected agents

--- framework/wazuh/cluster/control.py
+++ framework/wazuh/cluster/control.py
@@ -32,16 +32,20 @@ async def get_health(filter_node=None):
 async def get_agents(filter_node=None, filter_status=None):
     filter_status = ["all"] if not filter_status else filter_status
     filter_node = ["all"] if not filter_node else filter_node
+    select_fields = {'id', 'ip', 'name', 'status', 'node_name', 'version'}
 
     input_json = {'function': '/agents', 'from_cluster': False,
                   'arguments': {
                       'filters': {'status': ','.join(filter_status), 'node_name': ','.join(filter_node)},
                       'limit': None, 'wait_for_complete': False,
-                      'select': {'fields': ['id', 'ip', 'name', 'status', 'node_name', 'version']}}}
+                      'select': {'fields': list(select_fields)}}}
 
     result = json.loads(await local_client.execute(command=b'dapi', data=json.dumps(input_json).encode(),
                                                    wait_for_complete=False))
+    # add unknown value to unfilled variables in result. For example, never connected agents will miss the 'version'
+    # variable.
+    filled_result = [{**r, **{key: 'unknown' for key in select_fields - r.keys()}} for r in result['data']['items']]
+    result['data']['items'] = filled_result
     if result['error'] > 0:
         raise exception.WazuhException(result['error'], result['message'])
     return result
-
