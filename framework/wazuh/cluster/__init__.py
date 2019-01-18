688157e869f371271395da30cc490d45cd6d5753
Pedro de Castro <pedro@wazuh.com>
2019-01-18 20:38:33 +0000

    Update revision to 3805

--- framework/wazuh/cluster/__init__.py
+++ framework/wazuh/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '3.8.0'
-__revision__ = '3804'
+__revision__ = '3805'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
