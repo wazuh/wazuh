e80caf2735ec923874cbe228baa205ec50627b41
Cerv1 <cervick13@gmail.com>
2019-01-23 16:27:08 +0100

    Prevent realtime from alerting with hot-added symlinks

--- src/syscheckd/run_realtime.c
+++ src/syscheckd/run_realtime.c
@@ -131,15 +131,15 @@ int realtime_checksumfile(const char *file_name, whodata_evt *evt)
             struct stat statbuf;
             if (lstat(file_name, &statbuf) < 0) {
                 mdebug2("Stat() function failed on: %s. File may have been deleted", file_name);
-                return -1;
             }
-            if S_ISLNK(statbuf.st_mode) {
+            if (S_ISLNK(statbuf.st_mode) && (syscheck.opts[pos] & CHECK_FOLLOW)) {
                 read_dir(file_name, pos, evt, depth, 1);
-            } else
-#endif
-            {
-                read_dir(file_name, pos, evt, depth, 0);
+                return 0;
+            } else if (S_ISLNK(statbuf.st_mode) && !(syscheck.opts[pos] & CHECK_FOLLOW)) {
+                return 0;
             }
+#endif
+            read_dir(file_name, pos, evt, depth, 0);
         }
 
     }
