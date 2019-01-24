97c8c61da5b32f6198e7c7848a8deaffbaff4dfc
Chema Martinez <chema.martinez@wazuh.com>
2019-01-24 09:28:55 -0800

    Bump revision 3811

--- framework/wazuh/cluster/__init__.py
+++ framework/wazuh/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '3.8.1'
-__revision__ = '3810'
+__revision__ = '3811'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
