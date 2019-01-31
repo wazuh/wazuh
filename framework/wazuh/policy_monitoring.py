2273d6b3743db23c60ab56086ab33869711ba4a8
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-01-31 19:29:55 +0100

    Get compliance data

--- framework/wazuh/policy_monitoring.py
+++ framework/wazuh/policy_monitoring.py
@@ -5,30 +5,54 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 from glob import glob
+from itertools import groupby
+
 
 from wazuh import common
 from wazuh.agent import Agent
 from wazuh.exception import WazuhException
-from wazuh.utils import WazuhDBQuery, WazuhDBQueryDistinct
+from wazuh.utils import WazuhDBQuery
 from wazuh.wdb import WazuhDBConnection
 
 
 # API field -> DB field
-fields_translation = {'scan_id': 'pmg.scan_id',
-                      'profile': 'pmg.profile',
-                      'pass': 'pmg.pass',
-                      'failed': 'pmg.failed',
-                      'unknown': 'pmg.unknown',
-                      'score': 'pmg.score',
-                      'end_scan': 'si.pm_end_scan',
-                      'start_scan': 'si.pm_start_scan'}
+fields_translation_pm = {'scan_id': 'scan_id',
+                         'name': 'name',
+                         'description': 'description',
+                         'os_required': 'os_required',
+                         'pass': 'pass',
+                         'failed': 'failed',
+                         'score': 'score',
+                         'end_scan': 'pm_end_scan',
+                         'start_scan': 'pm_start_scan'}
+fields_translation_pm_check = {'name': 'name',
+                               'id': 'id',
+                               'cis_control': 'cis_control',
+                               'title': 'title',
+                               'description': 'description',
+                               'rationale': 'rationale',
+                               'remediation': 'remediation',
+                               'default_value': 'default_value',
+                               'file': 'file',
+                               'process': 'process',
+                               'directory': 'directory',
+                               'registry': 'registry',
+                               'reference': 'reference',
+                               'result': 'result'}
+fields_translation_pm_check_compliance = {'key': 'key',
+                                          'value': 'value'}
+
+default_query_pm = 'SELECT {0} FROM pm_global pmg INNER JOIN scan_info si ON pmg.scan_id=si.pm_scan_id'
+default_query_pm_check = 'SELECT {0} FROM pm_check INNER JOIN pm_check_compliance ON id=id_check'
 
 
 class WazuhDBQueryPM(WazuhDBQuery):
 
     def __init__(self, agent_id, offset, limit, sort, search, select, query, count,
-                 get_data, default_sort_field='date_last', filters={}, fields=fields_translation):
+                 get_data, default_sort_field='name', filters={}, fields=fields_translation_pm,
+                 default_query=default_query_pm):
         self.agent_id = agent_id
+        self._default_query_str = default_query
         Agent(agent_id).get_basic_information()  # check if the agent exists
         db_path = glob('{0}/{1}.db'.format(common.database_path_agents, agent_id))
         if not db_path:
@@ -38,33 +62,79 @@ class WazuhDBQueryPM(WazuhDBQuery):
                               select=select, fields=fields, default_sort_field=default_sort_field,
                               default_sort_order='DESC', filters=filters, query=query, db_path=db_path[0],
                               min_select_fields=set(), count=count, get_data=get_data,
-                              date_fields={'si.pm_end_scan', 'si.pm_start_scan'})
+                              date_fields={'pm_end_scan', 'pm_start_scan'})
         self.conn = WazuhDBConnection()
 
     def _default_query(self):
-        return 'SELECT {0} FROM pm_global pmg INNER JOIN scan_info si ON pmg.scan_id=si.pm_scan_id'
+        return self._default_query_str
+
+    def _substitute_params(self):
+        for k, v in self.request.items():
+            self.query = self.query.replace(f':{k}', str(v))
 
     def _get_total_items(self):
-        self.total_items = self.conn.execute(f'agent {self.agent_id} sql ' + self.query.format(self._default_count_query()), self.request)
+        self._substitute_params()
+        self.total_items = self.conn.execute(f'agent {self.agent_id} sql ' + self.query.format(self._default_count_query()))
 
     def _get_data(self):
-        self._data = self.conn.execute(f'agent {self.agent_id} sql ' + self.query.format(','.join(map(lambda x: self.fields[x], self.select['fields'] | self.min_select_fields))), self.request)
+        self._substitute_params()
+        self._data = self.conn.execute(f'agent {self.agent_id} sql ' + self.query.format(','.join(map(lambda x: self.fields[x], self.select['fields'] | self.min_select_fields))))
 
     def _format_data_into_dictionary(self):
         return self._data
 
+    def run(self):
 
-class WazuhDBQueryPMDistinct(WazuhDBQueryDistinct, WazuhDBQueryPM):
-    pass
+        self._add_select_to_query()
+        self._add_filters_to_query()
+        self._add_search_to_query()
+        if self.count:
+            self._get_total_items()
+        self._add_sort_to_query()
+        self._add_limit_to_query()
+        if self.data:
+            self._get_data()
+            return self._format_data_into_dictionary()
 
 
 def get_pm_list(agent_id=None, q="", offset=0, limit=common.database_limit,
                 sort=None, search=None, select=None, filters={}):
     if select is None:
-        select = {'fields': ['scan_id', 'profile', 'pass', 'failed', 'unknown',
-                             'score', 'end_scan', 'start_scan']
-                  }
+        select = {'fields': list(fields_translation_pm.keys())}
 
     db_query = WazuhDBQueryPM(agent_id=agent_id, offset=offset, limit=limit, sort=sort, search=search,
                               select=select, count=True, get_data=True, query=q, filters=filters)
     return db_query.run()
+
+
+def get_pm_checks(name, agent_id=None, q="", offset=0, limit=common.database_limit,
+                  sort=None, search=None, select=None, filters={}):
+    fields_translation = {**fields_translation_pm_check,
+                          **fields_translation_pm_check_compliance}
+    if select is None:
+        select = {'fields': (list(fields_translation_pm_check.keys()) +
+                             list(fields_translation_pm_check_compliance.keys()))
+                  }
+    else:
+        if 'name' not in select['fields']:
+            select['fields'].append('name')
+
+    db_query = WazuhDBQueryPM(agent_id=agent_id, offset=offset, limit=limit, sort=sort, search=search,
+                              select=select, count=True, get_data=True,
+                              query=f'name={name}' if q == "" else f'name={name};{q}',
+                              filters=filters, default_query=default_query_pm_check,
+                              fields=fields_translation)
+
+    checks = db_query.run()
+    groups = groupby(checks, key=lambda row: row['name'])
+    result = []
+    # Rearrange check and compliance fields
+    for _, group in groups:
+        check_dict = {k: v for k, v in group[0].items() if k in fields_translation_pm_check.values()}
+        check_dict['compliance'] = [{k: v for k, v in elem.items()
+                                     if k in fields_translation_pm_check_compliance.values()}
+                                    for elem in group
+                                    ]
+        result.append(check_dict)
+
+    return result
--- framework/wazuh/tests/test_policy_monitoring.py
+++ framework/wazuh/tests/test_policy_monitoring.py
@@ -5,21 +5,36 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import os
+import re
+import sqlite3
 from unittest import TestCase
 from unittest.mock import patch
 
 from wazuh import WazuhException
-from wazuh.policy_monitoring import WazuhDBQueryPM, get_pm_list
+from wazuh.policy_monitoring import WazuhDBQueryPM, get_pm_list, fields_translation_pm,\
+    get_pm_checks, fields_translation_pm_check
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
 
-def get_fake_pm_data():
-    pass
+def get_fake_pm_data(*args, **kwargs):
+    assert(isinstance(args[0], str))
+    query = re.search(r'^agent \d{3} sql (.+)$', args[0]).group(1)
+    try:
+        conn = sqlite3.connect(os.path.join(test_data_path, '000.db'))
+        conn.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        rows = conn.execute(query).fetchall()
+        return rows
+    finally:
+        conn.close()
+
 
 class TestPolicyMonitoring(TestCase):
 
     def test_wazuhdbquerypm(self):
+        """
+        Checks exception is raised when db not found
+        """
         with self.assertRaises(WazuhException):
             WazuhDBQueryPM('000', 0, 500, None, None, None, '', True, True)
         with patch('wazuh.common.database_path_agents', test_data_path):
@@ -27,8 +42,67 @@ class TestPolicyMonitoring(TestCase):
             query = WazuhDBQueryPM('000', 0, 500, None, None, None, '', True, True)
             assert(isinstance(query, WazuhDBQueryPM))
 
-    @patch('wazuh.policy_monitoring.WazuhDBConnection.__send', side_effect=get_fake_pm_data)
     @patch('wazuh.common.database_path_agents', test_data_path)
-    def test_get_pm_list(self, mock1, mock2):
-        result = get_pm_list('000')
-        assert(result, list)
+    def test_get_pm_list(self):
+        """
+        Checks data are properly loaded from database
+        """
+        with patch('wazuh.policy_monitoring.WazuhDBConnection') as mock_wdb:
+            mock_wdb.return_value.execute.side_effect = get_fake_pm_data
+            result = get_pm_list('000')
+            assert(isinstance(result, list))
+            assert(len(result) > 0)
+            pm = result[0]
+            assert(isinstance(pm, dict))
+            assert(set(pm.keys()) == set(fields_translation_pm.values()))
+
+    @patch('wazuh.common.database_path_agents', test_data_path)
+    def test_get_pm_list_select_param(self):
+        """
+        Checks only selected fields are loaded from database
+        """
+        with patch('wazuh.policy_monitoring.WazuhDBConnection') as mock_wdb:
+            mock_wdb.return_value.execute.side_effect = get_fake_pm_data
+            fields = {'fields': ['scan_id', 'name']}
+            result = get_pm_list('000', select=fields)
+            assert (isinstance(result, list))
+            assert (len(result) > 0)
+            pm = result[0]
+            assert (isinstance(pm, dict))
+            assert (set(pm.keys()) == set(fields['fields']))
+
+    @patch('wazuh.common.database_path_agents', test_data_path)
+    def test_get_pm_list_search_param(self):
+        """
+        Checks only selected fields are loaded from database
+        """
+        with patch('wazuh.policy_monitoring.WazuhDBConnection') as mock_wdb:
+            mock_wdb.return_value.execute.side_effect = get_fake_pm_data
+            search = {'value': 'Apache', 'negation': False}
+            result = get_pm_list('000', search=search)
+            assert (isinstance(result, list))
+            assert (len(result) > 0)
+
+            search = {'value': 'foo', 'negation': False}
+            result = get_pm_list('000', search=search)
+            assert (isinstance(result, list))
+            assert (len(result) == 0)
+
+            search = {'value': 'foo', 'negation': True}
+            result = get_pm_list('000', search=search)
+            assert (isinstance(result, list))
+            assert (len(result) > 0)
+
+    @patch('wazuh.common.database_path_agents', test_data_path)
+    def test_get_pm_checks(self):
+        """
+        Checks pm checks data are properly loaded from database
+        """
+        with patch('wazuh.policy_monitoring.WazuhDBConnection') as mock_wdb:
+            mock_wdb.return_value.execute.side_effect = get_fake_pm_data
+            result = get_pm_checks("'CIS Checks for Apache Https Server'", agent_id='000')
+            assert(isinstance(result, list))
+            assert(len(result) > 0)
+            pm = result[0]
+            assert(isinstance(pm, dict))
+            assert(set(pm.keys()) == set(fields_translation_pm_check.values()))
