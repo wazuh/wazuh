cba6bb553b17a2b1ce3fe410958bc9764dd02e8c
BraulioV <braulio@wazuh.com>
2019-02-01 18:44:31 +0100

    Added a header to the wrappers and improved how WAZUH_PATH is calculated

--- framework/wrappers/generic_wrapper.sh
+++ framework/wrappers/generic_wrapper.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 PYTHON_BIN="python/bin/python3.7"
 FRAMEWORK_PATH="framework/scripts"
@@ -9,8 +12,14 @@ SCRIPT_PATH_NAME="$0"
 SCRIPT_NAME=$(echo ${SCRIPT_PATH_NAME##*/})
 
 # If WAZUH_PATH variable is not defined
+# the script will calculate it by remoning
+# the relative path of the script in bin
+# from the WAZUH_PATH.
+#
+# Ex: /var/ossec/bin/wazuh-clusterd => /var/ossec
 if [ -z "${WAZUH_PATH}" ]; then
-    WAZUH_PATH="$( cd $(dirname ${SCRIPT_PATH_NAME}) ; pwd -P )/.."
+    WAZUH_PATH="$( cd $(dirname ${SCRIPT_PATH_NAME}) ; pwd -P )"
+    WAZUH_PATH=${WAZUH_PATH%%/bin*}
 fi
 
 # If WPYTHON_PATH variable is not defined
