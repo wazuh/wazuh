ecd3472c7a6d0520056e47b59d691d1bb58efbbe
Alberto Marin <alberto.marin@wazuh.com>
2019-02-04 20:01:25 +0100

    Fix memory leak

--- src/syscheckd/syscheck_audit.c
+++ src/syscheckd/syscheck_audit.c
@@ -375,7 +375,7 @@ int audit_init(void) {
             return -1;
         }
     } else {
-        mwarn("Audit health check is disabled. Whodata could not work correctly.");
+        minfo("Audit health check is disabled. Whodata could not work correctly.");
     }
 
     // Add Audit rules
@@ -1098,6 +1098,7 @@ void audit_read_events(int *audit_sock, int mode) {
 
     }
 
+    free(cache_id);
     free(cache);
     free(buffer);
 }
