3c40d3b1b2e6d0307310de17f86fa342ff6286ce
Cerv1 <cervick13@gmail.com>
2019-02-05 13:30:51 +0100

    Refactoring tests for os_zlib

--- src/tests/tap_os_zlib.c
+++ src/tests/tap_os_zlib.c
@@ -14,20 +14,13 @@ int test_success_compress_string() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
 
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     char buffer2[BUFFER_LENGTH];
     unsigned long int i2 = os_zlib_uncompress(buffer, buffer2, i1, BUFFER_LENGTH);
 
-    if(i2 == 0){
-        return 0;
-    }
-
-    if(strcmp(buffer2, TEST_STRING_1)){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
+    w_assert_str_eq(buffer2, TEST_STRING_1);
     return 1;
 }
 
@@ -36,106 +29,77 @@ int test_success_compress_special_string() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_2, buffer, strlen(TEST_STRING_2), BUFFER_LENGTH);
 
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     char buffer2[BUFFER_LENGTH];
     unsigned long int i2 = os_zlib_uncompress(buffer, buffer2, i1, BUFFER_LENGTH);
 
-    if(i2 == 0){
-        return 0;
-    }
-
-    if(strcmp(buffer2, TEST_STRING_2)){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
+    w_assert_str_eq(buffer2, TEST_STRING_2);
     return 1;
 }
 
 int test_fail_compress_null_src() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(NULL, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
     return 1;
 }
 
 int test_fail_compress_no_dest() {
 
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, NULL, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
     return 1;
 }
 
 int test_fail_compress_no_dest_size() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), 0);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
     return 1;
 }
 
 int test_fail_uncompress_null_src() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     char buffer2[BUFFER_LENGTH];
     unsigned long int i2 = os_zlib_uncompress(NULL, buffer2, i1, BUFFER_LENGTH);
-    if(i2 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
     return 1;
 }
 
 int test_fail_uncompress_null_dst() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     unsigned long int i2 = os_zlib_uncompress(buffer, NULL, i1, BUFFER_LENGTH);
-    if(i2 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
     return 1;
 }
 
 int test_fail_uncompress_no_src_size() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     char buffer2[BUFFER_LENGTH];
     unsigned long int i2 = os_zlib_uncompress(buffer, buffer2, 0, BUFFER_LENGTH);
-    if(i2 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
     return 1;
 }
 
 int test_fail_uncompress_no_dest_size() {
     char buffer[BUFFER_LENGTH];
     unsigned long int i1 = os_zlib_compress(TEST_STRING_1, buffer, strlen(TEST_STRING_1), BUFFER_LENGTH);
-    if(i1 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i1, 0);
 
     char buffer2[BUFFER_LENGTH];
     unsigned long int i2 = os_zlib_uncompress(buffer, buffer2, i1, 0);
-    if(i2 == 0){
-        return 0;
-    }
+    w_assert_uint_ne(i2, 0);
     return 1;
 }
 
