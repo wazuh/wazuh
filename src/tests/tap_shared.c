9004874c5b853e269283e4efb6931d9f0cebdbf3
Cerv1 <cervick13@gmail.com>
2019-02-05 08:57:42 +0100

    Migrating shared tests to TAP

---
+++ src/tests/tap_shared.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../headers/custom_output_search.h"
+#include "tap.h"
+
+int test_search_and_replace(){
+
+    int i;
+    const char *tests[][4] = {
+        {"testMe", "nomatch", "", "testMe"},
+        {"test me", "ME", "me", "test me"},
+        {"test me", "me", "ME", "test ME"},
+        {"testMe", "test", "Tested", "TestedMe"},
+        {"Metest", "test", "Tested", "MeTested"},
+        {"A B CTeStD E F", "TeSt", "tEsT", "A B CtEsTD E F"},
+        {"TeStA B CTeStD E F", "TeSt", "tEsT", "tEsTA B CtEsTD E F"},
+        {"TeSt TeStA B CTeStD E F", "TeSt", "tEsT", "tEsT tEsTA B CtEsTD E F"},
+        {"A B CTeStD E FTeSt", "TeSt", "tEsT", "A B CtEsTD E FtEsT"},
+        {"A B CTeStD E FTeSt TeSt", "TeSt", "tEsT", "A B CtEsTD E FtEsT tEsT"},
+        {"TeSt++ TeSt++A B CTeSt++D E F", "TeSt++", "tEsT", "tEsT tEsTA B CtEsTD E F"},
+        {"A B CTeStD E FTeSt TeSt", "TeSt", "tEsT++", "A B CtEsT++D E FtEsT++ tEsT++"},
+        {NULL, NULL, NULL, NULL}
+    };
+
+    for (i = 0; tests[i][0] != NULL ; i++) {
+        char *result = searchAndReplace(tests[i][0], tests[i][1], tests[i][2]);
+        if(strcmp(result, tests[i][3])){
+            return 0;
+        }
+        free(result);
+    }
+    return 1;
+}
+
+int main(void) {
+    printf(CYELLOW"\n\n   STARTING TEST - OS_SHARED   \n\n" CEND);
+
+    // Search and replace strings test
+    TAP_TEST_MSG(test_search_and_replace(), "Search and replace strings test.");
+
+    TAP_PLAN;
+    TAP_SUMMARY;
+    printf(CYELLOW "\n   ENDING TEST  - OS_SHARED   \n\n" CEND);
+    return 0;
+}
