5aa46958f93232e065226241cd736dff97032f23
Cerv1 <cervick13@gmail.com>
2019-02-05 12:45:00 +0100

    Refactoring tests for os_net

--- src/tests/tap_os_net.c
+++ src/tests/tap_os_net.c
@@ -22,46 +22,25 @@ int test_tcpv4_local() {
     char *msg;
     char ipbuffer[BUFFERSIZE];
 
+    w_assert_int_ge((server_root_socket = OS_Bindporttcp(PORT, IPV4, 0)), 0);
 
-    if ((server_root_socket = OS_Bindporttcp(PORT, IPV4, 0)), server_root_socket < 0){
-        return 0;
-    }
-    
-    if ((client_socket = OS_ConnectTCP(PORT, IPV4, 0)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectTCP(PORT, IPV4, 0)) , 0);
 
-    if ((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), server_client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), 0);
 
-    if (strcmp(ipbuffer, IPV4) != 0){
-        return 0;
-    }
+    w_assert_str_eq(ipbuffer, IPV4);
 
-    if (OS_SendTCP(client_socket, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCP(client_socket, SENDSTRING), 0);
 
-    if (OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE) != 0){
-        return 0;
-    }
-    
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE), 0);
 
-    if (OS_SendTCPbySize(server_client_socket, 5, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
 
-    if(msg = OS_RecvTCP(client_socket, BUFFERSIZE), msg == NULL){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCPbySize(server_client_socket, 5, SENDSTRING), 0);
 
-    if (strcmp(msg, "Hello") != 0){
-        return 0;
-    }
+    w_assert_ptr_ne((msg = OS_RecvTCP(client_socket, BUFFERSIZE)), NULL);
+
+    w_assert_str_eq(msg, "Hello"); /* only 5 bytes send */
 
     free(msg);
 
@@ -78,46 +57,25 @@ int test_tcpv4_inet() {
     char *msg;
     char ipbuffer[BUFFERSIZE];
 
+    w_assert_int_ge((server_root_socket = OS_Bindporttcp(PORT, NULL, 0)), 0);
 
-    if ((server_root_socket = OS_Bindporttcp(PORT, NULL, 0)), server_root_socket < 0){
-        return 0;
-    }
-    
-    if ((client_socket = OS_ConnectTCP(PORT, IPV4, 0)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectTCP(PORT, IPV4, 0)) , 0);
 
-    if ((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), server_client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), 0);
 
-    if (strcmp(ipbuffer, IPV4) != 0){
-        return 0;
-    }
+    w_assert_str_eq(ipbuffer, IPV4);
 
-    if (OS_SendTCP(client_socket, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCP(client_socket, SENDSTRING), 0);
 
-    if (OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE) != 0){
-        return 0;
-    }
-    
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE), 0);
 
-    if (OS_SendTCPbySize(server_client_socket, 5, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
 
-    if(msg = OS_RecvTCP(client_socket, BUFFERSIZE), msg == NULL){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCPbySize(server_client_socket, 5, SENDSTRING), 0);
 
-    if (strcmp(msg, "Hello") != 0){
-        return 0;
-    }
+    w_assert_ptr_ne((msg = OS_RecvTCP(client_socket, BUFFERSIZE)), NULL);
+
+    w_assert_str_eq(msg, "Hello"); /* only 5 bytes send */
 
     free(msg);
 
@@ -133,45 +91,26 @@ int test_tcpv6() {
     char *msg;
     char ipbuffer[BUFFERSIZE];
 
-    if ((server_root_socket = OS_Bindporttcp(PORT, IPV6, 1)), server_root_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_root_socket = OS_Bindporttcp(PORT, IPV6, 1)), 0);
 
-    if ((client_socket = OS_ConnectTCP(PORT, IPV6, 1)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectTCP(PORT, IPV6, 1)) , 0);
 
-    if ((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), server_client_socket < 0){
-        return 0;
-    }
-    
-    if (strcmp(ipbuffer, "0.0.0.0") != 0){
-        return 0;
-    }
+    w_assert_int_ge((server_client_socket = OS_AcceptTCP(server_root_socket, ipbuffer, BUFFERSIZE)), 0);
 
-    if (OS_SendTCP(client_socket, SENDSTRING) != 0){
-        return 0;
-    }
+    //TODO: ipv6 ip
+    w_assert_str_eq(ipbuffer, "0.0.0.0");
 
-    if (OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE) != 0){
-        return 0;
-    }
-    
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCP(client_socket, SENDSTRING), 0);
 
-    if (OS_SendTCPbySize(server_client_socket, 5, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_RecvTCPBuffer(server_client_socket, buffer, BUFFERSIZE), 0);
 
-    if(msg = OS_RecvTCP(client_socket, BUFFERSIZE), msg == NULL){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
 
-    if (strcmp(msg, "Hello") != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendTCPbySize(server_client_socket, 5, SENDSTRING), 0);
+
+    w_assert_ptr_ne((msg = OS_RecvTCP(client_socket, BUFFERSIZE)), NULL);
+
+    w_assert_str_eq(msg, "Hello"); /* only 5 bytes send */
 
     free(msg);
 
@@ -184,25 +123,15 @@ int test_tcpv6() {
 int test_tcp_invalid_sockets() {
     char buffer[BUFFERSIZE];
 
-    if (OS_SendTCP(-1, SENDSTRING) != OS_SOCKTERR){
-        return 1;
-    }
+    w_assert_int_eq(OS_SendTCP(-1, SENDSTRING), OS_SOCKTERR);
 
-    if (OS_SendTCPbySize(-1, strlen(SENDSTRING), SENDSTRING) != OS_SOCKTERR){
-        return 1;
-    }
+    w_assert_int_eq(OS_SendTCPbySize(-1, strlen(SENDSTRING), SENDSTRING), OS_SOCKTERR);
 
-    if (OS_RecvTCP(-1, BUFFERSIZE) != NULL){
-        return 1;
-    }
+    w_assert_ptr_eq(OS_RecvTCP(-1, BUFFERSIZE), NULL);
 
-    if (OS_RecvTCPBuffer(-1, buffer, BUFFERSIZE) != -1){
-        return 1;
-    }
+    w_assert_int_eq(OS_RecvTCPBuffer(-1, buffer, BUFFERSIZE), -1);
 
-    if (OS_AcceptTCP(-1, buffer, BUFFERSIZE) != -1){
-        return 1;
-    }
+    w_assert_int_eq(OS_AcceptTCP(-1, buffer, BUFFERSIZE), -1);
 
     return 0;
 }
@@ -212,37 +141,23 @@ int test_udpv4() {
     char buffer[BUFFERSIZE];
     char *msg;
 
-    if ((server_socket = OS_Bindportudp(PORT, IPV4, 0)), server_socket < 0){
-        return 0;
-    }
-    
-    if ((client_socket = OS_ConnectUDP(PORT, IPV4, 0)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_socket = OS_Bindportudp(PORT, IPV4, 0)), 0);
 
-    if(OS_SendUDPbySize(client_socket, strlen(SENDSTRING), SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectUDP(PORT, IPV4, 0)) , 0);
 
-    if(OS_RecvConnUDP(server_socket, buffer, BUFFERSIZE) != strlen(SENDSTRING)){
-        return 0;
-    }
+    //TODO: w_assert_int_eq(OS_SendUDP(client_socket, SENDSTRING), 0);
+    w_assert_int_eq(OS_SendUDPbySize(client_socket, strlen(SENDSTRING), SENDSTRING), 0);
 
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    //TODO: not null-terminated
+    w_assert_int_eq(OS_RecvConnUDP(server_socket, buffer, BUFFERSIZE), strlen(SENDSTRING));
 
-    if (OS_SendUDPbySize(client_socket, 5, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
 
-    if(msg = OS_RecvUDP(server_socket, BUFFERSIZE), msg == NULL){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendUDPbySize(client_socket, 5, SENDSTRING), 0);
 
-    if (strcmp(msg, "Hello") != 0){
-        return 0;
-    }
+    w_assert_ptr_ne((msg = OS_RecvUDP(server_socket, BUFFERSIZE)), NULL);
+
+    w_assert_str_eq(msg, "Hello"); /* only 5 bytes send */
 
     free(msg);
 
@@ -257,37 +172,23 @@ int test_udpv6() {
     char buffer[BUFFERSIZE];
     char *msg;
 
-    if ((server_socket = OS_Bindportudp(PORT, IPV6, 1)), server_socket < 0){
-        return 0;
-    }
-    
-    if ((client_socket = OS_ConnectUDP(PORT, IPV6, 1)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_socket = OS_Bindportudp(PORT, IPV6, 1)), 0);
 
-    if(OS_SendUDPbySize(client_socket, strlen(SENDSTRING), SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectUDP(PORT, IPV6, 1)) , 0);
 
-    if(OS_RecvConnUDP(server_socket, buffer, BUFFERSIZE) != strlen(SENDSTRING)){
-        return 0;
-    }
+    //TODO: w_assert_int_eq(OS_SendUDP(client_socket, SENDSTRING), 0);
+    w_assert_int_eq(OS_SendUDPbySize(client_socket, strlen(SENDSTRING), SENDSTRING), 0);
 
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    //TODO: not null-terminated
+    w_assert_int_eq(OS_RecvConnUDP(server_socket, buffer, BUFFERSIZE), strlen(SENDSTRING));
 
-    if (OS_SendUDPbySize(client_socket, 5, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
 
-    if(msg = OS_RecvUDP(server_socket, BUFFERSIZE), msg == NULL){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendUDPbySize(client_socket, 5, SENDSTRING), 0);
 
-    if (strcmp(msg, "Hello") != 0){
-        return 0;
-    }
+    w_assert_ptr_ne((msg = OS_RecvUDP(server_socket, BUFFERSIZE)), NULL);
+
+    w_assert_str_eq(msg, "Hello"); /* only 5 bytes send */
 
     free(msg);
 
@@ -300,17 +201,11 @@ int test_udpv6() {
 int test_udp_invalid_sockets() {
     char buffer[BUFFERSIZE];
 
-    if (OS_SendUDPbySize(-1, strlen(SENDSTRING), SENDSTRING) != OS_SOCKTERR){
-        return 1;
-    }
+    w_assert_int_eq(OS_SendUDPbySize(-1, strlen(SENDSTRING), SENDSTRING), OS_SOCKTERR);
 
-    if (OS_RecvUDP(-1, BUFFERSIZE) != NULL){
-        return 1;
-    }
+    w_assert_ptr_eq(OS_RecvUDP(-1, BUFFERSIZE), NULL);
 
-    if (OS_RecvConnUDP(-1, buffer, BUFFERSIZE) != 0){
-        return 1;
-    }
+    w_assert_int_eq(OS_RecvConnUDP(-1, buffer, BUFFERSIZE), 0);
 
     return 0;
 }
@@ -328,41 +223,28 @@ int test_unix() {
     const int msg_size = 2048;
     char buffer[BUFFERSIZE];
 
-    if ((server_socket = OS_BindUnixDomain(socket_path, SOCK_DGRAM, msg_size)), server_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((server_socket = OS_BindUnixDomain(socket_path, SOCK_DGRAM, msg_size)), 0);
 
-    if (OS_getsocketsize(server_socket) < msg_size){
-        return 0;
-    }
+    w_assert_int_ge(OS_getsocketsize(server_socket), msg_size);
 
-    if ((client_socket = OS_ConnectUnixDomain(socket_path, SOCK_DGRAM, msg_size)), client_socket < 0){
-        return 0;
-    }
+    w_assert_int_ge((client_socket = OS_ConnectUnixDomain(socket_path, SOCK_DGRAM, msg_size)), 0);
 
-    if(OS_SendUnix(client_socket, SENDSTRING, 5) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendUnix(client_socket, SENDSTRING, 5), 0);
 
-    if(OS_RecvUnix(server_socket, BUFFERSIZE, buffer) != 5){
-        return 0;
-    }
+    w_assert_int_eq(OS_RecvUnix(server_socket, BUFFERSIZE, buffer), 5);
 
-    if (strcmp(buffer, "Hello") != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, "Hello");
 
-    if(OS_SendUnix(client_socket, SENDSTRING, 0) != 0){
-        return 0;
-    }
+    w_assert_int_eq(OS_SendUnix(client_socket, SENDSTRING, 0), 0);
 
-    if(OS_RecvUnix(server_socket, BUFFERSIZE, buffer) != strlen(SENDSTRING) + 1){
-        return 0;
-    }
+    w_assert_int_eq(OS_RecvUnix(server_socket, BUFFERSIZE, buffer), strlen(SENDSTRING) + 1);
 
-    if (strcmp(buffer, SENDSTRING) != 0){
-        return 0;
-    }
+    w_assert_str_eq(buffer, SENDSTRING);
+
+    OS_CloseSocket(client_socket);
+    OS_CloseSocket(server_socket);
+
+    unlink(socket_path);
 
     return 1;
 }
@@ -370,13 +252,9 @@ int test_unix() {
 int test_unix_invalid_sockets() {
     char buffer[BUFFERSIZE];
 
-    if(OS_SendUnix(-1, SENDSTRING, strlen(SENDSTRING)) != OS_SOCKTERR){
-        return 1;
-    }
+    w_assert_int_eq(OS_SendUnix(-1, SENDSTRING, strlen(SENDSTRING)), OS_SOCKTERR);
 
-    if(OS_RecvUnix(-1, BUFFERSIZE, buffer) != 0){
-        return 1;
-    }
+    w_assert_int_eq(OS_RecvUnix(-1, BUFFERSIZE, buffer), 0);
 
     return 0;
 }
@@ -384,34 +262,21 @@ int test_unix_invalid_sockets() {
 int test_gethost_success() {
     char *ret;
 
-    if(ret = OS_GetHost("google-public-dns-a.google.com", 2), ret == NULL){
-        return 0;
-    }
+    w_assert_ptr_ne((ret = OS_GetHost("google-public-dns-a.google.com", 2)), NULL);
+    w_assert_str_eq(ret, "8.8.8.8");
 
-    if(strcmp(ret,"8.8.8.8") != 0){
-        return 0;
-    }
-    
     free(ret);
     
     return 1;
 }
 
 int test_gethost_null() {
-
-    if(!OS_GetHost(NULL, 2)){
-        return 0;
-    }
-
+    w_assert_ptr_ne(OS_GetHost(NULL, 2), NULL);
     return 1;
 }
 
 int test_gethost_not_exists() {
-
-    if(!OS_GetHost("this.should.not.exist", 2)){
-        return 0;
-    }
-
+    w_assert_ptr_ne(OS_GetHost("this.should.not.exist", 2), NULL);
     return 1;
 }
 
