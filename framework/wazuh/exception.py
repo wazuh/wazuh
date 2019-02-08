04f7d4a87facbebeef346d87f49fb55f57122d1a
druizz90 <demetrio.ruiz@wazuh.com>
2019-02-08 08:47:46 +0100

    Tests for manager.py were updated.

--- framework/wazuh/exception.py
+++ framework/wazuh/exception.py
@@ -144,8 +144,6 @@ class WazuhException(Exception):
         # Manager:
         1900: 'Error restarting manager',
         1901: '\'execq\' socket has not been created',
-        1902: 'Could not connect to \'execq\' socket',
-        1903: 'Could not connect to \'execa\' socket',
 
         # Database:
         2000: 'No such database file',
