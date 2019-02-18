23940c86ca201036b0b3fccf6338b260b7325f45
Marta Gómez Macías <marta@wazuh.com>
2019-02-18 12:53:11 +0100

    Fix bug when running distributed requests to all nodes from a worker

--- framework/wazuh/cluster/master.py
+++ framework/wazuh/cluster/master.py
@@ -132,10 +132,13 @@ class MasterHandler(server.AbstractServerHandler, c_common.WazuhCommon):
         if command == b'dapi_forward':
             client, request = data.split(b' ', 1)
             client = client.decode()
-            if not client in self.server.clients:
-                raise WazuhException(3022, client)
-            else:
+            if client == 'fw_all_nodes':
+                for worker in self.server.clients.values():
+                    result = (await worker.send_request(b'dapi', request_id.encode() + b' ' + request)).decode()
+            elif client in self.server.clients:
                 result = (await self.server.clients[client].send_request(b'dapi', request_id.encode() + b' ' + request)).decode()
+            else:
+                raise WazuhException(3022, client)
         else:
             result = (await self.send_request(b'dapi', request_id.encode() + b' ' + data)).decode()
         if result.startswith('Error'):
