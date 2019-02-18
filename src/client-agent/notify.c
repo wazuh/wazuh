123d760893c325c9bab76aab0c1fa7b3994a5b2a
Pablo Navarro <pablo.navarro@wazuh.com>
2019-02-18 12:30:24 +0100

    Fix memory leaks in the control module (#2623)

--- src/client-agent/notify.c
+++ src/client-agent/notify.c
@@ -73,9 +73,6 @@ void run_notify()
     char *shared_files;
     os_md5 md5sum;
     time_t curr_time;
-    int sock;
-    char label_ip[50];
-    int i;
 
     keep_alive_random[0] = '\0';
     curr_time = time(0);
@@ -135,15 +132,18 @@ void run_notify()
 
 #ifdef __linux__
     char *agent_ip;
+    int sock;
+    char label_ip[50];
+    int i;
     os_calloc(16,sizeof(char),agent_ip);
 
     for (i = SOCK_ATTEMPTS; i > 0; --i) {
         if (sock = control_check_connection(), sock >= 0) {
-            if (OS_SendUnix(sock, agent_ip, 16) < 0) {
+            if (OS_SendUnix(sock, agent_ip, IPSIZE) < 0) {
                 merror("Error sending msg to control socket (%d) %s", errno, strerror(errno));
             }
             else{
-                if(OS_RecvUnix(sock, 16, agent_ip) == 0){
+                if(OS_RecvUnix(sock, IPSIZE - 1, agent_ip) == 0){
                     merror("Error receiving msg from control socket (%d) %s", errno, strerror(errno));
                 }
                 else{
