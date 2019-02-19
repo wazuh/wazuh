d71c2b183c5c4dd90a4ce82caae255a9a88afc05
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-02-19 08:56:25 +0100

    Configuration assessment for framework (#2630)
    Adds configuration assessment capabilities to Wazuh framework

--- framework/wazuh/cluster/master.py
+++ framework/wazuh/cluster/master.py
@@ -204,7 +204,6 @@ class MasterHandler(server.AbstractServerHandler, c_common.WazuhCommon):
             cmd, res = self.get_nodes(api_call_info['arguments'])
             if api_call_info['function'] == '/cluster/nodes/:node_name':
                 res = res['items'][0] if len(res['items']) > 0 else {}
-
         return cmd, json.dumps({'error': 0, 'data': res}).encode()
 
     def get_nodes(self, arguments: Dict) -> Tuple[bytes, Dict]:
@@ -460,6 +459,8 @@ class MasterHandler(server.AbstractServerHandler, c_common.WazuhCommon):
         try:
             for filename, data in files_checksums.items():
                 update_file(data=data, name=filename)
+
+            shutil.rmtree(decompressed_files_path)
 
         except Exception as e:
             self.logger.error("Error updating worker files: '{}'.".format(e))
