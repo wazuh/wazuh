b739eab7481d6cf043efdd6f10424f02bf310ccb
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-02-20 10:33:32 +0100

    Fix python path

--- api/scripts/wazuh-apid.py
+++ api/scripts/wazuh-apid.py
@@ -1,4 +1,4 @@
-#!/var/ossec/python/bin/python3
+#!/var/ossec/framework/python/bin/python3
 # Copyright (C) 2015-2019, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
