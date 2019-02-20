6fcdcad86da977978d8ce5dc2f168a1cf99e7e62
Demetrio Ruiz <32929872+druizz90@users.noreply.github.com>
2019-02-20 10:50:43 +0100

    Fix for restart.sh script (#2642)
    Fix for restarting Wazuh with 'service' command.

--- active-response/restart.sh
+++ active-response/restart.sh
@@ -44,10 +44,8 @@ fi
 # Restart Wazuh
 if command -v systemctl > /dev/null 2>&1; then        
     systemctl restart wazuh-$TYPE
-elif command -v service > /dev/null 2>&1; then        
-    service restart wazuh-$TYPE
-elif command -v update-rc.d > /dev/null 2>&1; then        
-    update-rc.d restart wazuh-$TYPE
+elif command -v service > /dev/null 2>&1; then
+    service wazuh-$TYPE restart
 else
     ${PWD}/bin/ossec-control restart
 fi
