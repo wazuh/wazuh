ac7e645d35ec41b7497a5354b9aa8adf1855a960
druizz90 <demetrio.ruiz@wazuh.com>
2019-02-26 13:03:41 +0100

    Unit tests for validator.py were updated.

--- api/api/test/test_validator.py
+++ api/api/test/test_validator.py
@@ -1,4 +1,7 @@
-# coding: utf-8
+#!/usr/bin/env python
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import pytest
 
