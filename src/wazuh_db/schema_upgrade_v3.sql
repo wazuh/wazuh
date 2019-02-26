87f10e31f98cc357f3163d44f5bf30d188779b5b
Borja Arroba <borja.arroba@wazuh.com>
2019-02-26 18:25:27 +0100

    Update database version from wazuh-db to add support to vulnerability detector in windows

---
+++ src/wazuh_db/schema_upgrade_v3.sql
@@ -0,0 +1,15 @@
+/*
+ * SQL Schema for upgrading databases
+ * Copyright (C) 2015-2019, Wazuh Inc.
+ * December, 2018.
+ * This program is a free software, you can redistribute it
+ * and/or modify it under the terms of GPLv2.
+*/
+
+BEGIN;
+
+ALTER TABLE sys_programs ADD COLUMN cpe TEXT DEFAULT NULL;
+
+UPDATE metadata SET value = 3 WHERE key = 'db_version';
+
+END;
