8d20af3213f705eb646861a7adbb318fc53fd8cb
Marta Gómez Macías <marta@wazuh.com>
2019-02-27 18:01:38 +0100

    Add missing processes and sort the list by alphabetical order
    GET/manager/status API call wasn't returning all daemons statuses but only a few of them.

--- framework/wazuh/manager.py
+++ framework/wazuh/manager.py
@@ -31,10 +31,9 @@ def status():
     :return: Array of dictionaries (keys: status, daemon).
     """
 
-    processes = ['ossec-monitord', 'ossec-logcollector', 'ossec-remoted',
-                 'ossec-syscheckd', 'ossec-analysisd', 'ossec-maild',
-                 'ossec-execd', 'wazuh-modulesd', 'ossec-authd',
-                 'wazuh-clusterd']
+    processes = ['ossec-agentlessd', 'ossec-analysisd', 'ossec-authd', 'ossec-csyslogd', 'ossec-dbd', 'ossec-monitord',
+                 'ossec-execd', 'ossec-integratord', 'ossec-logcollector', 'ossec-maild', 'ossec-remoted',
+                 'ossec-reportd', 'ossec-syscheckd', 'wazuh-clusterd', 'wazuh-modulesd']
 
     data = {}
     for process in processes:
