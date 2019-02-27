6939a15ffc1865059fbc234869be667d96415f34
druizz90 <demetrio.ruiz@wazuh.com>
2019-02-27 12:36:36 +0100

    'overwrite' parameter was added for uploading files.

--- framework/wazuh/manager.py
+++ framework/wazuh/manager.py
@@ -180,31 +180,44 @@ def ossec_log_summary(months=3):
     return categories
 
 
-def upload_file(tmp_file, path, content_type):
+def upload_file(tmp_file, path, content_type, overwrite=False):
     """
     Updates a group file
 
-    :param file: Relative path of file name from origin
+    :param tmp_file: Relative path of file name from origin
     :param path: Path of destination of the new file
+    :parm content_type: Content type of file from origin
+    :param overwrite: True for updating existing files, False otherwise
     :return: Confirmation message in string
     """
     try:
-        with open(join(common.ossec_path, tmp_file)) as f:
-            file_data = f.read()
-    except IOError:
-        raise WazuhException(1005)
-    except Exception:
-        raise WazuhException(1000)
+        # if file already exists and overwrite is False, raise exception
+        if not overwrite and exists(join(common.ossec_path, path)):
+            raise WazuhException(1905)
 
-    if len(file_data) == 0:
-        raise WazuhException(1112)
+        try:
+            with open(join(common.ossec_path, tmp_file)) as f:
+                file_data = f.read()
+        except IOError:
+            raise WazuhException(1005)
+        except Exception:
+            raise WazuhException(1000)
 
-    if content_type == 'application/xml':
-        return upload_xml(file_data, path)
-    elif content_type == 'application/octet-stream':
-        return upload_list(file_data, path)
-    else:
-        raise WazuhException(1016)
+        if len(file_data) == 0:
+            raise WazuhException(1112)
+
+        if content_type == 'application/xml':
+            return upload_xml(file_data, path)
+        elif content_type == 'application/octet-stream':
+            return upload_list(file_data, path)
+        else:
+            raise WazuhException(1016)
+    finally:
+        # delete temporary file from API
+        try:
+            remove(join(common.ossec_path, tmp_file))
+        except OSError:
+            raise WazuhException(1903)
 
 
 def upload_xml(xml_file, path):
