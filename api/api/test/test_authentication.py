b96fa3fd63bb124328cc325edb2f19945aed08a8
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-03-04 14:52:36 +0100

    Added unit tests

--- api/api/test/test_authentication.py
+++ api/api/test/test_authentication.py
@@ -5,9 +5,62 @@
 import os
 from unittest.mock import patch
 
+import pytest
+
 test_path = os.path.dirname(os.path.realpath(__file__))
+test_data_path = os.path.join(test_path, 'data')
 
 
-@patch('wazuh.common.ossec_path', '/var/ossec')
-def test_importing_module():
-    import api.authentication
+@pytest.fixture(scope='module')
+def import_api_auth():
+    with patch('wazuh.common.ossec_path', new=test_data_path):
+        import api.authentication as auth
+        db_path = os.path.join(test_data_path, 'api', 'users.db')
+        secret_path = os.path.join(test_data_path, 'api', 'jwt_secret')
+        assert (os.path.exists(db_path))
+        assert (os.path.exists(secret_path))
+        yield auth
+        os.unlink(db_path)
+        os.unlink(secret_path)
+
+
+def test_database_init(import_api_auth):
+    """
+    Checks users db is properly initialized
+    """
+    with import_api_auth.AuthenticationManager() as am:
+        assert(am.check_user('wazuh', 'wazuh'))
+        assert(am.check_user('wazuh-app', 'wazuh-app'))
+
+
+def test_add_user(import_api_auth):
+    """
+    Checks users are added to database
+    """
+    with import_api_auth.AuthenticationManager() as am:
+        # New user
+        am.add_user('newuser', 'passwd')
+        assert(am.check_user('newuser', 'passwd'))
+
+        # Conflicting previously created user
+        assert(not am.add_user('newuser', 'other_passwd'))
+
+
+def test_check_user(import_api_auth):
+    """
+    Checks users are rejected if password does not match
+    """
+    with import_api_auth.AuthenticationManager() as am:
+        assert(not am.check_user('wazuh', 'wrong_pass'))
+
+
+def test_login_user(import_api_auth):
+    """
+    Checks tokens are generated properly
+    """
+    with import_api_auth.AuthenticationManager() as am:
+        token = am.login_user('wazuh', 'wazuh')
+        decoded_token = import_api_auth.decode_token(token)
+        assert(isinstance(decoded_token, dict))
+
+        assert(am.login_user('nonexists', 'pass') is None)
