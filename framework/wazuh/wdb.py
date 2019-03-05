f4bbcee1bcc0b4d823b88e0b70135ee88b98562d
Marta Gómez Macías <marta@wazuh.com>
2019-03-05 11:11:13 +0100

    Rename WazuhDBConnection __send to _send
    If the method name begins with two _ characters, the test won't be able to find it and therefore it couldnt be tested.

--- framework/wazuh/wdb.py
+++ framework/wazuh/wdb.py
@@ -52,7 +52,7 @@ class WazuhDBConnection:
                 raise WazuhException(2004, error_text)
 
 
-    def __send(self, msg):
+    def _send(self, msg):
         """
         Sends a message to the wdb socket
         """
@@ -102,7 +102,7 @@ class WazuhDBConnection:
         def send_request_to_wdb(query_lower, step, off, response):
             try:
                 request = "{} limit {} offset {}".format(query_lower, step, off)
-                response.extend(self.__send(request))
+                response.extend(self._send(request))
             except ValueError:
                 # if the step is already 1, it can't be divided
                 if step == 1:
@@ -119,7 +119,7 @@ class WazuhDBConnection:
             regex = re.compile(r"\w+ \d+? sql delete from ([a-z0-9,_ ]+)")
             if regex.match(query_lower) is None:
                 raise WazuhException(2004, "Delete query is wrong")
-            return self.__send(query_lower)
+            return self._send(query_lower)
 
         # only for update queries
         if update:
@@ -128,7 +128,7 @@ class WazuhDBConnection:
                                r" '([a-z0-9,*_%\- ]+)'")
             if regex.match(query_lower) is None:
                 raise WazuhException(2004, "Update query is wrong")
-            return self.__send(query_lower)
+            return self._send(query_lower)
 
         # if the query has already a parameter limit / offset, divide using it
         offset = 0
@@ -145,7 +145,7 @@ class WazuhDBConnection:
             regex = re.compile(r"\w+ \d+? sql select ([A-Z a-z0-9,*_` \.\-%\(\):\']+) from")
             select = regex.match(query_lower).group(1)
             countq = query_lower.replace(select, "count(*)", 1)
-            total = list(self.__send(countq)[0].values())[0]
+            total = list(self._send(countq)[0].values())[0]
 
             limit = lim if lim != 0 else total
 
@@ -164,4 +164,4 @@ class WazuhDBConnection:
             else:
                 return response
         else:
-            return list(self.__send(query_lower)[0].values())[0]
+            return list(self._send(query_lower)[0].values())[0]
