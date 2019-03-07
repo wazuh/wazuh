e8a5abb638e07182385c3ede7173b9c2a53c0d13
Jesus Jaldo <jm.jaldo@wazuh.com>
2019-03-07 11:10:22 +0100

    Add decoders endpoints

---
+++ api/api/controllers/decoders_controller.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+
+def get_decoders():
+    pass
+
+def get_decoders_by_name():
+    pass
+
+def get_decoders_files():
+    pass
+
+def get_decoders_parets():
+    pass
