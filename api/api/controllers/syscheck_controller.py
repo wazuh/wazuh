8eaf5670a1ff315dafae2fec3cd943a9c4de5766
Marta Gómez Macías <marta@wazuh.com>
2019-03-08 12:32:10 +0100

    Add spec for /syscheck endpoint

---
+++ api/api/controllers/syscheck_controller.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+def put_syscheck(pretty=False, wait_for_complete=False):
+    """
+
+    :param pretty: Show results in human-readable format 
+    :type pretty: bool
+    :param wait_for_complete: Disable timeout response 
+    :type wait_for_complete: bool
+    """
+    pass
