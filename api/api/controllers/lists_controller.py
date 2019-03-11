c1a32ac008bf1cea99155e664d2df633c7386101
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-03-11 15:13:38 +0100

    Added lists endpoints (#2792)
    Added lists endpoints

---
+++ api/api/controllers/lists_controller.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+
+def get_lists():
+    pass
+
+
+def get_lists_files():
+    pass
