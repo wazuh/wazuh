0f28ccd4f80187711fb05bd77bbf0a5094a68466
Marta Gómez Macías <marta@wazuh.com>
2019-03-11 12:16:38 +0100

    Add unit tests for #2793

--- framework/wazuh/tests/test_wdb.py
+++ framework/wazuh/tests/test_wdb.py
@@ -4,6 +4,7 @@
 
 from unittest.mock import patch
 import pytest
+import itertools
 
 from wazuh import exception
 from wazuh.wdb import WazuhDBConnection
@@ -24,8 +25,8 @@ def test_failed_connection():
             WazuhDBConnection()
 
 
-@patch('wazuh.wdb.WazuhDBConnection')
-def test_wrong_character_encodings_wdb(wdb_mock):
+@patch('socket.socket')
+def test_wrong_character_encodings_wdb(socket_mock):
     """
     Tests receiving a text with a bad character encoding from wazuh db
     """
@@ -33,7 +34,47 @@ def test_wrong_character_encodings_wdb(wdb_mock):
         bad_string = b' {"bad": "\x96bad"}'
         return bytes(len(bad_string)) if size_to_receive == 4 else bad_string
 
-    with patch('socket.socket.recv', side_effect=recv_mock):
+    socket_mock.return_value.recv.side_effect = recv_mock
+    mywdb = WazuhDBConnection()
+    received = mywdb._send("test")
+    assert received == {"bad": "bad"}
+
+
+@patch('socket.socket')
+def test_receive_long_message(socket_mock):
+    """
+    Tests receiving a message so big it can't be sliced and, therefore an exception will be raised.
+    """
+    # return as many wrong JSON items as needed using itertools.cycle
+    socket_mock.return_value.recv.side_effect = itertools.chain([b'0001 ', b'ok [{"count(*)":1000}]'],
+                                                                itertools.cycle([b'1234', b'ok [{"long": "long"']))
+    with pytest.raises(exception.WazuhException, match=".* 2007 .*"):
         mywdb = WazuhDBConnection()
-        received = mywdb._send("test")
-        assert received == {"bad": "bad"}
+        received = mywdb.execute("agent 000 sql select * from test")
+
+    # 5 calls to setup the socket (create object, connect, make count(*) request...) +
+    # 8 failed requests (180, 90, 45, 22, 11, 5, 2 and 1) * 3 necessary calls per request
+    # (send, recv size and recv data)
+    assert len(socket_mock.mock_calls) == 5 + 3 * 8
+
+
+@patch('socket.socket')
+def test_slice_message(socket_mock):
+    """
+    Tests receiving a message so big it can't be sliced the first time, but after requesting half of elements
+    its correctly returned
+    """
+    socket_mock.return_value.recv.side_effect = itertools.chain([b'0001 ', b'ok [{"count(*)":1000}]',
+                                                                 b'1234', b'ok [{"long": "long"'],
+                                                                itertools.cycle([b'1234', b'ok [{"long": "long"}]']))
+
+    mywdb = WazuhDBConnection()
+    received = mywdb.execute("agent 000 sql select * from test")
+    # 1000 elements / 180 step -> 5.5 -> 6 + 1 element from the double failed request (the one returning the
+    # incomplete JSON).
+    assert received == [{"long": "long"}]*7
+
+    # 5 calls to setup the socket (create object, connect, make count(*) request...) +
+    # 6 requests (1000 elements sliced in 180 items per request) + 1 failed request
+    # + 2 extra request consequence of the failed one
+    assert len(socket_mock.mock_calls) == 5 + (6 + 2) * 3
