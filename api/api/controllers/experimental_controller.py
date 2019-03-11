a20e11e8e0b6dc8c4f268f96f07edaf90e2f5f6c
Jesús Jaldo <jm.jaldo@wazuh.com>
2019-03-11 13:45:58 +0100

    Add spec for /experimental/ciscat/:agent_id/results endpoint

---
+++ api/api/controllers/experimental_controller.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+
+def clear_syscheck_database():
+    pass
+def get_cis_cat_results():
+    pass
