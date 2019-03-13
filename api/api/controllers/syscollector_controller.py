632360aafa294d0e6db36febf2dd3fd9b4c96b85
Jesús Jaldo <jm.jaldo@wazuh.com>
2019-03-13 12:07:21 +0100

    Add /syscollector controllers

---
+++ api/api/controllers/syscollector_controller.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+def get_hardware_info():
+    pass
+
+def get_network_address_info():
+    pass
+
+def get_network_interface_info():
+    pass
+
+def get_network_protocol_info():
+    pass
+
+def get_os_info():
+    pass
+
+def get_packages_info():
+    pass
+
+def get_packages_info():
+    pass
+
+def get_processes_info():
+    pass
