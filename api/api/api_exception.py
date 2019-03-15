a67883d9cea069deb8a1a3d9f7edfb1ab3fd240d
druizz90 <demetrio.ruiz@wazuh.com>
2019-03-15 10:35:28 +0100

    Function for getting relative paths from Wazuh base directory was added.

--- api/api/api_exception.py
+++ api/api/api_exception.py
@@ -2,10 +2,8 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-import os
-
 from api.constants import CONFIG_FILE_PATH, SECURITY_PATH
-from wazuh.common import ossec_path as WAZUH_PATH
+from api.util import to_relative_path
 
 
 class APIException(Exception):
@@ -23,10 +21,10 @@ class APIException(Exception):
         # show relative paths in exceptions
         self.exceptions = {
             2000: 'Some parameters are not expected in the configuration file '
-                  f'(WAZUH_PATH/{os.path.relpath(CONFIG_FILE_PATH, WAZUH_PATH)})',
+                  f'(WAZUH_PATH/{to_relative_path(CONFIG_FILE_PATH)})',
             2001: 'Error creating or reading secrets file. Please, ensure '
                   'there is enough disk space and permission to write in '
-                  f'WAZUH_PATH/{os.path.relpath(SECURITY_PATH, WAZUH_PATH)}',
+                  f'WAZUH_PATH/{to_relative_path(SECURITY_PATH)}',
             2002: 'Error migrating configuration from old API version. '
                   'Default configuration will be applied',
             2003: 'Error loading SSL/TLS certificates'
