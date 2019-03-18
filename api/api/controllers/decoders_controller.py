2e5999ee121237f17a0a913de1e19327159b089a
Marta Gómez Macías <marta@wazuh.com>
2019-03-18 15:50:10 +0100

    Add endpoint implementation for /decoders/{decoder_name}

--- api/api/controllers/decoders_controller.py
+++ api/api/controllers/decoders_controller.py
@@ -2,7 +2,6 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-from typing import List
 import asyncio
 import logging
 
@@ -49,8 +48,35 @@ def get_decoders(pretty: bool = False, wait_for_complete: bool = False, offset: 
     return data, 200
 
 
-def get_decoders_by_name():
-    pass
+def get_decoders_by_name(pretty: bool = False, wait_for_complete: bool = False, offset: int = 0, limit: int = None,
+                         sort: str = None, search: str = None, decoder_name = None):
+    """Get decoders by name
+
+    Returns information about decoders with a specified name. This information include decoder's route, decoder's name,
+    decoder's file among others.
+
+    :param pretty: Show results in human-readable format
+    :param wait_for_complete: Disable timeout response
+    :param offset: First element to return in the collection
+    :param limit: Maximum number of elements to return
+    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in
+                 ascending or descending order.
+    :param search: Looks for elements with the specified string
+    :param decoder_name: Decoder name.
+    """
+    f_kwargs = {'offset': offset, 'limit': limit, 'sort': sort, 'search': search, 'name': decoder_name}
+
+    dapi = DistributedAPI(f=Decoder.get_decoders,
+                          f_kwargs=remove_nones_to_dict(f_kwargs),
+                          request_type='local_any',
+                          is_async=False,
+                          wait_for_complete=wait_for_complete,
+                          pretty=pretty,
+                          logger=logger
+                          )
+    data = loop.run_until_complete(dapi.distribute_function())
+
+    return data, 200
 
 
 def get_decoders_files():
