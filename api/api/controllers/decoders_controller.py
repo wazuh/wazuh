a4f08df2f6fb6e7fcc66756705bf2128e2420240
Marta Gómez Macías <marta@wazuh.com>
2019-03-18 16:24:09 +0100

    Add implementation for /decoders/files endpoint

--- api/api/controllers/decoders_controller.py
+++ api/api/controllers/decoders_controller.py
@@ -79,7 +79,41 @@ def get_decoders_by_name(pretty: bool = False, wait_for_complete: bool = False, 
     return data, 200
 
 
-def get_decoders_files():
+def get_decoders_files(pretty: bool = False, wait_for_complete: bool = False, offset: int = 0, limit: int = None,
+                       sort: str = None, search: str = None, file: str = None, path: str = None,
+                       status: str = None):
+    """Get all decoders files
+
+    Returns information about all decoders files used in Wazuh. This information include decoder's file, decoder's route
+    and decoder's status among others
+
+    :param pretty: Show results in human-readable format
+    :param wait_for_complete: Disable timeout response
+    :param offset: First element to return in the collection
+    :param limit: Maximum number of elements to return
+    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in
+                 ascending or descending order.
+    :param search: Looks for elements with the specified string
+    :param file: Filters by filename.
+    :param path: Filters by path
+    :param status: Filters by list status.
+    """
+    f_kwargs = {'offset': offset, 'limit': limit, 'sort': sort, 'search': search, 'file': file, 'path': path,
+                'status': status}
+
+    dapi = DistributedAPI(f=Decoder.get_decoders_files,
+                          f_kwargs=remove_nones_to_dict(f_kwargs),
+                          request_type='local_any',
+                          is_async=False,
+                          wait_for_complete=wait_for_complete,
+                          pretty=pretty,
+                          logger=logger
+                          )
+    data = loop.run_until_complete(dapi.distribute_function())
+
+    return data, 200
+
+
     pass
 
 
