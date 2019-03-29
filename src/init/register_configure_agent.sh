1f3b25f35c05ad7f78dd5d80417f0c3135728060
alberpilot <alberto.rodriguez@wazuh.com>
2019-03-29 13:21:27 +0100

    Added multi manager option

--- src/init/register_configure_agent.sh
+++ src/init/register_configure_agent.sh
@@ -20,6 +20,23 @@ edit_value_tag() {
     fi
 }
 
+add_adress_block() {
+    
+    SET_ADDRESSES=("$@")
+    LAST=$((${#SET_ADDRESSES[@]}-1))
+
+    for i in "${SET_ADDRESSES[@]}";
+    do
+        if [ "$i" == "${SET_ADDRESSES[${LAST}]}" ]; then
+            NEW="<address>$i</address>      "
+        else
+            NEW="<address>$i</address>\n      "
+        fi
+        BLOCK=${BLOCK}${NEW}
+    done
+    sed -ri "s#<address>MANAGER_IP</address>#${BLOCK}#g" "ossec.conf" > /dev/null
+}
+
 add_parameter () {
     if [ ! -z "$3" ]; then
         OPTIONS="$1 $2 $3" 
@@ -35,16 +52,26 @@ if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_MANAGER_IP} ]; then
         chown root:ossec ${DIRECTORY}/logs/ossec.log
     fi
 
+    # Check if multiples IPs are defined in variable WAZUH_MANAGER_IP
+    ADDRESSES=(${WAZUH_MANAGER_IP//,/ })
+    if [ ${#ADDRESSES[@]} -gt 1 ]; then
+        # Get uniques values
+        ADDRESSES=($(echo "${ADDRESSES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+        add_adress_block "${ADDRESSES[@]}" 
+    else
+        # Single address
+        edit_value_tag "address" ${WAZUH_MANAGER_IP}	
+    fi
+
     # Options to be modified in ossec.conf
-    edit_value_tag "address" ${WAZUH_MANAGER_IP}
     edit_value_tag "protocol" ${WAZUH_PROTOCOL}
     edit_value_tag "port" ${WAZUH_SERVER_PORT}
     edit_value_tag "notify_time" ${WAZUH_NOTIFY_TIME}
     edit_value_tag "time-reconnect" ${WAZUH_TIME_RECONNECT}
+fi
 
+if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_AUTHD_SERVER} ]; then
     # Options to be used in register time. 
-    if [ ! -z ${WAZUH_AUTHD_SERVER} ]; then
-
         OPTIONS="-m ${WAZUH_AUTHD_SERVER}"
         OPTIONS=$(add_parameter "${OPTIONS}" "-p" "${WAZUH_AUTHD_PORT}")
         OPTIONS=$(add_parameter "${OPTIONS}" "-P" "${WAZUH_PASSWORD}")
@@ -54,7 +81,21 @@ if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_MANAGER_IP} ]; then
         OPTIONS=$(add_parameter "${OPTIONS}" "-k" "${WAZUH_KEY}")
         OPTIONS=$(add_parameter "${OPTIONS}" "-x" "${WAZUH_PEM}")
         ${DIRECTORY}/bin/agent-auth ${OPTIONS} >> ${DIRECTORY}/logs/ossec.log 2>/dev/null
+        # This avoid message: Unable to send message to 'server': Broken pipe
+        # sleep 10
+        if [ -s ${DIR}/etc/client.keys ]; then
+
+            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-ip>).*(?=</server-ip>)' | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$' > /dev/null 2>&1; then
+                service wazuh-agent restart || :
+            fi
         
-    fi
+            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-hostname>).*(?=</server-hostname>)' > /dev/null 2>&1; then
+                service wazuh-agent restart || :
+            fi
+        
+            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<address>)(?!MANAGER_IP).*(?=</address>)' > /dev/null 2>&1; then
+                service wazuh-agent restart || :
+            fi
+        fi
 
 fi
