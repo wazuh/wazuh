b77a29808cddd0504f0c3320931741b4ce8c6a3b
druizz90 <demetrio.ruiz@wazuh.com>
2019-04-02 16:25:46 +0200

    Add AWS unit tests.

--- wodles/aws/tests/data/schema_cloudtrail_test.sql
+++ wodles/aws/tests/data/schema_cloudtrail_test.sql
@@ -6,6 +6,34 @@
  * and/or modify it under the terms of GPLv2.
  */
 
+INSERT INTO 'cloudtrail' (
+    bucket_path,
+    aws_account_id,
+    aws_region,
+    log_key,
+    processed_date,
+    created_date) VALUES (
+    'test-bucket/',
+    '123456789',
+    'us-east-1',
+    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0030Z_aaaa.json.gz',
+    DATETIME('now'),
+    '');
+
+INSERT INTO 'cloudtrail' (
+    bucket_path,
+    aws_account_id,
+    aws_region,
+    log_key,
+    processed_date,
+    created_date) VALUES (
+    'test-bucket/',
+    '123456789',
+    'us-east-1',
+    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0000Z_aaab.json.gz',
+    DATETIME('now'),
+    '');
+
 CREATE TABLE 'cloudtrail' (
     bucket_path 'text' NOT NULL,
     aws_account_id 'text' NOT NULL,
@@ -39,49 +67,7 @@ INSERT INTO 'cloudtrail' (
     'test-bucket/',
     '123456789',
     'us-east-1',
-    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0000Z_aaab.json.gz',
-    DATETIME('now'),
-    '');
-
-INSERT INTO 'cloudtrail' (
-    bucket_path,
-    aws_account_id,
-    aws_region,
-    log_key,
-    processed_date,
-    created_date) VALUES (
-    'test-bucket/',
-    '123456789',
-    'us-east-1',
     'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0005Z_aaaa.json.gz',
-    DATETIME('now'),
-    '');
-
-INSERT INTO 'cloudtrail' (
-    bucket_path,
-    aws_account_id,
-    aws_region,
-    log_key,
-    processed_date,
-    created_date) VALUES (
-    'test-bucket/',
-    '123456789',
-    'us-east-1',
-    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0010Z_aaaa.json.gz',
-    DATETIME('now'),
-    '');
-
-INSERT INTO 'cloudtrail' (
-    bucket_path,
-    aws_account_id,
-    aws_region,
-    log_key,
-    processed_date,
-    created_date) VALUES (
-    'test-bucket/',
-    '123456789',
-    'us-east-1',
-    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T00015Z_aaaa.json.gz',
     DATETIME('now'),
     '');
 
@@ -109,6 +95,20 @@ INSERT INTO 'cloudtrail' (
     'test-bucket/',
     '123456789',
     'us-east-1',
+    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0010Z_aaaa.json.gz',
+    DATETIME('now'),
+    '');
+
+INSERT INTO 'cloudtrail' (
+    bucket_path,
+    aws_account_id,
+    aws_region,
+    log_key,
+    processed_date,
+    created_date) VALUES (
+    'test-bucket/',
+    '123456789',
+    'us-east-1',
     'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0025Z_aaaa.json.gz',
     DATETIME('now'),
     '');
@@ -123,7 +123,7 @@ INSERT INTO 'cloudtrail' (
     'test-bucket/',
     '123456789',
     'us-east-1',
-    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0030Z_aaaa.json.gz',
+    'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T00015Z_aaaa.json.gz',
     DATETIME('now'),
     '');
 
