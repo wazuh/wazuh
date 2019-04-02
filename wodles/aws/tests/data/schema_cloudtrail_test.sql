578ab19991426b21cebab5c37b8bd4b698190414
druizz90 <demetrio.ruiz@wazuh.com>
2019-04-02 16:27:25 +0200

    Add AWS unit tests.

--- wodles/aws/tests/data/schema_cloudtrail_test.sql
+++ wodles/aws/tests/data/schema_cloudtrail_test.sql
@@ -6,6 +6,15 @@
  * and/or modify it under the terms of GPLv2.
  */
 
+ CREATE TABLE 'cloudtrail' (
+    bucket_path 'text' NOT NULL,
+    aws_account_id 'text' NOT NULL,
+    aws_region 'text' NOT NULL,
+    log_key 'text' NOT NULL,
+    processed_date 'text' NOT NULL,
+    created_date 'integer' NOT NULL,
+    PRIMARY KEY (bucket_path, aws_account_id, aws_region, log_key));
+
 INSERT INTO 'cloudtrail' (
     bucket_path,
     aws_account_id,
@@ -33,15 +42,6 @@ INSERT INTO 'cloudtrail' (
     'AWSLogs/123456789/CloudTrail/us-east-1/2019/04/01/123456789_CloudTrail-us-east-1_20190401T0000Z_aaab.json.gz',
     DATETIME('now'),
     '');
-
-CREATE TABLE 'cloudtrail' (
-    bucket_path 'text' NOT NULL,
-    aws_account_id 'text' NOT NULL,
-    aws_region 'text' NOT NULL,
-    log_key 'text' NOT NULL,
-    processed_date 'text' NOT NULL,
-    created_date 'integer' NOT NULL,
-    PRIMARY KEY (bucket_path, aws_account_id, aws_region, log_key));
 
 INSERT INTO 'cloudtrail' (
     bucket_path,
