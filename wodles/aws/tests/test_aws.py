7fa52a0cc5956c79a0540de36bccf6688dc60016
druizz90 <demetrio.ruiz@wazuh.com>
2019-04-02 11:24:32 +0200

    Tests for AWS wodle were updated.

--- wodles/aws/tests/test_aws.py
+++ wodles/aws/tests/test_aws.py
@@ -6,45 +6,41 @@
 
 import os
 import re
-import sqlite3
+from sqlite3 import connect
 import sys
 from unittest import TestCase
 from unittest.mock import patch
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
-from aws_s3 import AWSCloudTrailBucket
+import aws_s3
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
 
 def get_fake_s3_db(*args, **kwargs):
-    s3_db = sqlite3.connect(':memory:')
+    s3_db = connect(':memory:')
     cur = s3_db.cursor()
     with open(os.path.join(test_data_path, 'schema_metadata_test.sql')) as f:
         cur.executescript(f.read())
-    #s3_db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+
     return s3_db
 
 
 class TestAWS(TestCase):
 
-    @patch.object(AWSCloudTrailBucket, 'get_client')
-    def test_get_metadata_version(self, mocked_method):
+    @patch('sqlite3.connect', side_effect=get_fake_s3_db)
+    @patch('aws_s3.AWSCloudTrailBucket.get_client')
+    def test_metadata_version(self, mocked_method, mocked_db):
         """
-        Checks metadata version
+        Checks if metadata version has been updated
         """
-        #with patch('aws_s3.WazuhIntegration') as mock_db:
-            #wi = WazuhIntegration(access_key, secret_key, aws_profile, iam_role_arn,
-            #    service_name=None, region=None, bucket=None)
-        #mocked_method.return_value = None
+        ct = aws_s3.AWSCloudTrailBucket(**{'reparse': False, 'access_key': None, 'secret_key': None,
+                                    'profile': None, 'iam_role_arn': None, 'bucket': 'test',
+                                    'only_logs_after': '19700101', 'skip_on_error': True,
+                                    'account_alias': None, 'prefix': 'test',
+                                    'delete_file': False, 'aws_organization_id': None})
 
-        with patch('sqlite3.connect') as mock_db:
-            mock_db.return_value = get_fake_s3_db()
-            ct = AWSCloudTrailBucket(**{'reparse': False, 'access_key': None, 'secret_key': None,
-                                        'profile': None, 'iam_role_arn': None, 'bucket': 'test',
-                                        'only_logs_after': '19700101', 'skip_on_error': True,
-                                        'account_alias': None, 'prefix': 'test',
-                                        'delete_file': False, 'aws_organization_id': None})
-            query_version = ct.db_connector.execute(ct.sql_get_metadata_version)
-            metadata_version = query_version.fetchone()[0]
-            assert(metadata_version == '3.8')
+        query_version = ct.db_connector.execute(ct.sql_get_metadata_version)
+        metadata_version = query_version.fetchone()[0]
+
+        assert(metadata_version == ct.wazuh_version)
