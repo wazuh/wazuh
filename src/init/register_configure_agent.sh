d86f88501c4fba5c340dd7e47d624fffe2f1d87d
BraulioV <braulio@wazuh.com>
2019-04-08 13:22:03 +0200

    Read the env variables using launchctl getenv for macos

--- src/init/register_configure_agent.sh
+++ src/init/register_configure_agent.sh
@@ -9,8 +9,8 @@
 # Foundation.
 
 . /etc/ossec-init.conf
-  
-edit_value_tag() { 
+
+edit_value_tag() {
     if [ "$#" == "2" ] && [ ! -z "$2" ]; then
         sed -ri "s#<$1>.+</$1>#<$1>$2</$1>#g" "${DIRECTORY}/etc/ossec.conf" > /dev/null
     fi
@@ -21,7 +21,7 @@ edit_value_tag() {
 }
 
 add_adress_block() {
-    
+
     SET_ADDRESSES=("$@")
     LAST=$((${#SET_ADDRESSES[@]}-1))
 
@@ -39,62 +39,88 @@ add_adress_block() {
 
 add_parameter () {
     if [ ! -z "$3" ]; then
-        OPTIONS="$1 $2 $3" 
+        OPTIONS="$1 $2 $3"
     fi
     echo ${OPTIONS}
-} 
+}
 
-if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_MANAGER_IP} ]; then
+set_vars () {
+    export WAZUH_PROTOCOL=$(launchctl getenv WAZUH_PROTOCOL)
+    export WAZUH_SERVER_PORT=$(launchctl getenv WAZUH_SERVER_PORT)
+    export WAZUH_NOTIFY_TIME=$(launchctl getenv WAZUH_NOTIFY_TIME)
+    export WAZUH_TIME_RECONNECT=$(launchctl getenv WAZUH_TIME_RECONNECT)
+    export WAZUH_AUTHD_SERVER=$(launchctl getenv WAZUH_AUTHD_SERVER)
+    export WAZUH_AUTHD_PORT=$(launchctl getenv WAZUH_AUTHD_PORT)
+    export WAZUH_PASSWORD=$(launchctl getenv WAZUH_PASSWORD)
+    export WAZUH_AGENT_NAME=$(launchctl getenv WAZUH_AGENT_NAME)
+    export WAZUH_GROUP=$(launchctl getenv WAZUH_GROUP)
+    export WAZUH_CERTIFICATE=$(launchctl getenv WAZUH_CERTIFICATE)
+    export WAZUH_KEY=$(launchctl getenv WAZUH_KEY)
+    export WAZUH_PEM=$(launchctl getenv WAZUH_PEM)
+}
 
-    if [ ! -f ${DIRECTORY}/logs/ossec.log ]; then
-        touch -f ${DIRECTORY}/logs/ossec.log
-        chmod 660 ${DIRECTORY}/logs/ossec.log
-        chown root:ossec ${DIRECTORY}/logs/ossec.log
+main () {
+
+    uname_s=$(uname -s)
+
+    if [ "${uname_s}" = "Darwin" ]; then
+        set_vars
     fi
 
-    # Check if multiples IPs are defined in variable WAZUH_MANAGER_IP
-    ADDRESSES=(${WAZUH_MANAGER_IP//,/ })
-    if [ ${#ADDRESSES[@]} -gt 1 ]; then
-        # Get uniques values
-        ADDRESSES=($(echo "${ADDRESSES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-        add_adress_block "${ADDRESSES[@]}" 
-    else
-        # Single address
-        edit_value_tag "address" ${WAZUH_MANAGER_IP}	
-    fi
+    if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_MANAGER_IP} ]; then
 
-    # Options to be modified in ossec.conf
-    edit_value_tag "protocol" ${WAZUH_PROTOCOL}
-    edit_value_tag "port" ${WAZUH_SERVER_PORT}
-    edit_value_tag "notify_time" ${WAZUH_NOTIFY_TIME}
-    edit_value_tag "time-reconnect" ${WAZUH_TIME_RECONNECT}
-fi
-
-if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_AUTHD_SERVER} ]; then
-    # Options to be used in register time. 
-        OPTIONS="-m ${WAZUH_AUTHD_SERVER}"
-        OPTIONS=$(add_parameter "${OPTIONS}" "-p" "${WAZUH_AUTHD_PORT}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-P" "${WAZUH_PASSWORD}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-A" "${WAZUH_AGENT_NAME}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-G" "${WAZUH_GROUP}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-v" "${WAZUH_CERTIFICATE}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-k" "${WAZUH_KEY}")
-        OPTIONS=$(add_parameter "${OPTIONS}" "-x" "${WAZUH_PEM}")
-        ${DIRECTORY}/bin/agent-auth ${OPTIONS} >> ${DIRECTORY}/logs/ossec.log 2>/dev/null
-
-        if [ -s ${DIR}/etc/client.keys ]; then
-
-            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-ip>).*(?=</server-ip>)' | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$' > /dev/null 2>&1; then
-                service wazuh-agent restart || :
-            fi
-        
-            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-hostname>).*(?=</server-hostname>)' > /dev/null 2>&1; then
-                service wazuh-agent restart || :
-            fi
-        
-            if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<address>)(?!MANAGER_IP).*(?=</address>)' > /dev/null 2>&1; then
-                service wazuh-agent restart || :
-            fi
+        if [ ! -f ${DIRECTORY}/logs/ossec.log ]; then
+            touch -f ${DIRECTORY}/logs/ossec.log
+            chmod 660 ${DIRECTORY}/logs/ossec.log
+            chown root:ossec ${DIRECTORY}/logs/ossec.log
         fi
 
-fi
+        # Check if multiples IPs are defined in variable WAZUH_MANAGER_IP
+        ADDRESSES=(${WAZUH_MANAGER_IP//,/ })
+        if [ ${#ADDRESSES[@]} -gt 1 ]; then
+            # Get uniques values
+            ADDRESSES=($(echo "${ADDRESSES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+            add_adress_block "${ADDRESSES[@]}"
+        else
+            # Single address
+            edit_value_tag "address" ${WAZUH_MANAGER_IP}
+        fi
+
+        # Options to be modified in ossec.conf
+        edit_value_tag "protocol" ${WAZUH_PROTOCOL}
+        edit_value_tag "port" ${WAZUH_SERVER_PORT}
+        edit_value_tag "notify_time" ${WAZUH_NOTIFY_TIME}
+        edit_value_tag "time-reconnect" ${WAZUH_TIME_RECONNECT}
+    fi
+
+    if [ ! -s ${DIRECTORY}/etc/client.keys ] && [ ! -z ${WAZUH_AUTHD_SERVER} ]; then
+        # Options to be used in register time.
+            OPTIONS="-m ${WAZUH_AUTHD_SERVER}"
+            OPTIONS=$(add_parameter "${OPTIONS}" "-p" "${WAZUH_AUTHD_PORT}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-P" "${WAZUH_PASSWORD}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-A" "${WAZUH_AGENT_NAME}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-G" "${WAZUH_GROUP}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-v" "${WAZUH_CERTIFICATE}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-k" "${WAZUH_KEY}")
+            OPTIONS=$(add_parameter "${OPTIONS}" "-x" "${WAZUH_PEM}")
+            ${DIRECTORY}/bin/agent-auth ${OPTIONS} >> ${DIRECTORY}/logs/ossec.log 2>/dev/null
+
+            if [ -s ${DIR}/etc/client.keys ]; then
+
+                if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-ip>).*(?=</server-ip>)' | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$' > /dev/null 2>&1; then
+                    service wazuh-agent restart || :
+                fi
+
+                if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<server-hostname>).*(?=</server-hostname>)' > /dev/null 2>&1; then
+                    service wazuh-agent restart || :
+                fi
+
+                if cat ${DIR}/etc/ossec.conf | grep -o -P '(?<=<address>)(?!MANAGER_IP).*(?=</address>)' > /dev/null 2>&1; then
+                    service wazuh-agent restart || :
+                fi
+            fi
+
+    fi
+}
+
+main
