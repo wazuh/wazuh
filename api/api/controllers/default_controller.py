0e37e157221d3ee49d24e8621305c80cb6cfa135
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-04-12 17:30:24 +0200

    Add model to controller response

--- api/api/controllers/default_controller.py
+++ api/api/controllers/default_controller.py
@@ -10,6 +10,7 @@ import time
 import yaml
 
 from api import __path__ as api_path
+from api.models.basic_info import BasicInfo
 
 logger = logging.getLogger('wazuh')
 
@@ -21,15 +22,16 @@ def default_info():
     """
     with open(os.path.join(api_path[0], 'spec', 'spec.yaml'), 'r') as stream:
         info_data = yaml.load(stream)
-        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime())
-        data = {
-            'title': info_data['info']['title'],
-            'api_version': info_data['info']['version'],
-            'revision': info_data['info']['x-revision'],
-            'license_name': info_data['info']['license']['name'],
-            'license_url': info_data['info']['license']['url'],
-            'hostname': socket.gethostname(),
-            'timestamp': timestamp
-        }
+    timestamp = time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime())
+    data = {
+        'title': info_data['info']['title'],
+        'api_version': info_data['info']['version'],
+        'revision': info_data['info']['x-revision'],
+        'license_name': info_data['info']['license']['name'],
+        'license_url': info_data['info']['license']['url'],
+        'hostname': socket.gethostname(),
+        'timestamp': timestamp
+    }
+    response = BasicInfo.from_dict(data)
 
-    return data, 200
+    return response, 200
