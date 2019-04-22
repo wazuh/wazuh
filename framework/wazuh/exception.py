70e31ae5996731a8bcf752ec7be71bf2bf6c9651
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-04-22 18:28:20 +0200

    Update error 1403

--- framework/wazuh/exception.py
+++ framework/wazuh/exception.py
@@ -72,7 +72,9 @@ class WazuhException(Exception):
         1400: 'Invalid offset',
         1401: 'Invalid limit',
         1402: 'Invalid order. Order must be \'asc\' or \'desc\'',
-        1403: 'Sort field invalid',  # Also, in DB
+        1403: {'message': 'Not a valid sort field ',
+               'remediation': 'Please, use only allowed sort fields'
+               },
         1404: 'A field must be specified to order the data',
         1405: 'Specified limit exceeds maximum allowed (1000)',
         1406: '0 is not a valid limit',
