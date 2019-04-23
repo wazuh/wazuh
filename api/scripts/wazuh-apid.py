2f5a3ce36933db5de44b6de36f573a3ad9bd4dfa
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-04-23 18:34:16 +0200

    Refactor wazuh-apid to load app using uwsgi

--- api/scripts/wazuh-apid.py
+++ api/scripts/wazuh-apid.py
@@ -2,67 +2,22 @@
 # Copyright (C) 2015-2019, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
 import argparse
 import os
-import ssl
+import subprocess
 import sys
 
-import connexion
-from api.api_exception import APIException
-from api.constants import CONFIG_FILE_PATH
-from api.util import to_relative_path
-from flask_caching import Cache
-from flask_cors import CORS
-from wazuh import common, pyDaemonModule, Wazuh
+from api import configuration
+from wazuh import pyDaemonModule, common
 from wazuh.cluster import __version__, __author__, __ossec_name__, __licence__
-
-from api import alogging, encoder, configuration, __path__ as api_path
-from api import validator  # To register custom validators (do not remove)
-
-#
-# Aux functions
-#
-def set_logging(foreground_mode=False, debug_mode='info'):
-    api_logger = alogging.APILogger(foreground_mode=foreground_mode, debug_level=debug_mode)
-    api_logger.setup_logger()
-    return api_logger
 
 
 def print_version():
     print("\n{} {} - {}\n\n{}".format(__ossec_name__, __version__, __author__, __licence__))
 
 
-app = connexion.App(__name__, specification_dir=os.path.join(api_path[0], 'spec'))
-app.app.json_encoder = encoder.JSONEncoder
-app.add_api('spec.yaml', arguments={'title': 'Wazuh API'})
-
-# def main(cache_conf, cors, port, host, ssl_context, main_logger):
-#     app = connexion.App(__name__, specification_dir=os.path.join(api_path[0], 'spec'))
-#     app.app.json_encoder = encoder.JSONEncoder
-#     app.add_api('spec.yaml', arguments={'title': 'Wazuh API'})
-#     # app.app.logger = main_logger
-#     # app.app.before_request(alogging.set_request_user_logs)
-#     if cors:
-#         # add CORS support
-#         CORS(app.app)
-#     # add Cache support
-#     if cache_conf['enabled']:
-#         app.app.config['CACHE_TYPE'] = 'simple'
-#         app.app.config['CACHE_DEFAULT_TIMEOUT'] = cache_conf['time']/1000
-#     else:
-#         app.app.config['CACHE_TYPE'] = 'null'
-#     app.app.cache = Cache(app.app)
-#     try:
-#         app.run(port=port, host=host, ssl_context=ssl_context)
-#     except Exception as e:
-#         main_logger.error("Error starting API server: {}".format(e))
-
-
-#
-# Main
-#
 if __name__ == '__main__':
-    my_wazuh = Wazuh(get_init=True)
 
     parser = argparse.ArgumentParser()
     ####################################################################################################################
@@ -74,6 +29,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     configuration = configuration.read_config()
+
     if args.test_config:
         sys.exit(0)
 
@@ -90,38 +46,5 @@ if __name__ == '__main__':
         os.chown('{0}/logs/api.log'.format(common.ossec_path), common.ossec_uid, common.ossec_gid)
         os.chmod('{0}/logs/api.log'.format(common.ossec_path), 0o660)
 
-    if configuration['https']['enabled']:
-        try:
-            ssl_context = ssl.SSLContext()
-            if configuration['https']['use_ca']:
-                ssl_context.verify_mode = ssl.CERT_REQUIRED
-                ssl_context.load_verify_locations(configuration['https']['ca'])
-            ssl_context.load_cert_chain(certfile=configuration['https']['cert'], keyfile=configuration['https']['key'])
-        except ssl.SSLError as e:
-            raise APIException(2003, details='Private key does not match with the certificate')
-        except IOError as e:
-            raise APIException(2003, details='Please, ensure '
-                               'if path to certificates is correct in '
-                               'the configuration file '
-                               f'(WAZUH_PATH/{to_relative_path(CONFIG_FILE_PATH)})')
-    else:
-        ssl_context = None
-
-    # Drop privileges to ossec
-    if configuration['drop_privileges']:
-        os.setgid(common.ossec_gid)
-        os.setuid(common.ossec_uid)
-
-    main_logger = set_logging(args.foreground, configuration['logs']['level'])
-
-    pyDaemonModule.create_pid('wazuh-apid', os.getpid())
-
-    try:
-        app.run(ssl_context=ssl_context)
-    except Exception as e:
-        main_logger.error("Error starting API server: {}".format(e))
-
-    # try:
-    #     main(configuration['cache'], configuration['cors'], configuration['port'], configuration['host'], ssl_context, main_logger)
-    # except KeyboardInterrupt:
-    #     main_logger.info("SIGINT received. Bye!")
+    proc = subprocess.Popen(["/var/ossec/framework/python/bin/uwsgi", "--yaml", "/var/ossec/api/configuration/api.yml"])
+    pyDaemonModule.create_pid('wazuh-apid', proc.pid)
