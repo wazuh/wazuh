03da088aedd7f2f7dcd0b111461f8a8cdf268e59
Chema Martinez <chema.martinez@wazuh.com>
2019-04-24 04:22:36 -0700

    Fix database version

--- src/wazuh_db/wdb.h
+++ src/wazuh_db/wdb.h
@@ -139,7 +139,6 @@ extern char *schema_global_sql;
 extern char *schema_agents_sql;
 extern char *schema_upgrade_v1_sql;
 extern char *schema_upgrade_v2_sql;
-extern char *schema_upgrade_v3_sql;
 
 extern wdb_config config;
 extern pthread_mutex_t pool_mutex;
