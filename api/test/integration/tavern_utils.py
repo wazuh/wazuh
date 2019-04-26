d0c919ea263d5dcdd2bbe8031b22d3d98ef88f50
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-04-26 17:09:04 +0200

    Fix test_select_key tavern helper function

--- api/test/integration/tavern_utils.py
+++ api/test/integration/tavern_utils.py
@@ -19,5 +19,5 @@ def test_select_key(response, select_key):
     :param select_key: Parametrized key used for select param in request
     :return: True if request response item key matches used select param
     """
-    assert list(response.json()["items"][0])[0] == select_key
+    assert list(response.json()["data"]["items"][0])[0] == select_key
     return
