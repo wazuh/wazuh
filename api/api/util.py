e95dba2ca141adca4b39ec3ded4a55454a402c62
AdriiiPRodri <adrianjpr@gmail.com>
2019-04-26 11:57:53 +0200

    Deleted unused import

--- api/api/util.py
+++ api/api/util.py
@@ -9,7 +9,6 @@ from connexion import problem
 from flask import current_app
 from wazuh.common import ossec_path as WAZUH_PATH
 from wazuh.exception import WazuhException, WazuhInternalError, WazuhError
-import wazuh.results as wresults
 
 
 def _deserialize(data, klass):
