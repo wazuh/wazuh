f04711b3b6ee95f57d604fe2d56838f0d19c9522
druizz90 <demetrio.ruiz@wazuh.com>
2019-05-06 10:21:33 +0200

    Add tavern test for SCA.

--- api/api/controllers/sca_controller.py
+++ api/api/controllers/sca_controller.py
@@ -25,17 +25,17 @@ def get_sca_agent(agent_id=None, pretty=False, wait_for_complete=False,
 
     Returns the security SCA database of an agent
 
-    :param agent_id:
+    :param agent_id: Agent ID. All posible values since 000 onwards.
     :param pretty: Show results in human-readable format
     :param wait_for_complete: Disable timeout response
-    :param name:
-    :param description:
-    :param references:
-    :param offset:
-    :param limit:
-    :param sort:
-    :param search:
-    :param query:
+    :param name: Filters by policy name
+    :param description: Filters by policy description
+    :param references: Filters by references
+    :param offset:First element to return in the collection
+    :param limit: Maximum number of elements to return
+    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order
+    :param search: Looks for elements with the specified string
+    :param query: Query to filter results by. This is specially useful to filter by total checks passed, failed or total score (fields pass, fail, score)
     """
     f_kwargs = {'agent_id': agent_id,
                 'name': name,
@@ -70,25 +70,25 @@ def get_sca_checks(agent_id=None, pretty=False, wait_for_complete=False,
 
     Returns the policy monitoring alerts for a given policy
 
-    :param agent_id:
+    :param agent_id: Agent ID. All posible values since 000 onwards
     :param pretty: Show results in human-readable format
     :param wait_for_complete: Disable timeout response
-    :param policy_id:
-    :param title
-    :param description:
-    :param rationale:
-    :param remediation:
-    :param file
-    :param process
-    :param directory
-    :param registry
-    :param references:
-    :param result:
-    :param offset:
-    :param limit:
-    :param sort:
-    :param search:
-    :param query:
+    :param policy_id: Filters by policy id
+    :param title: Filters by title
+    :param description: Filters by policy description
+    :param rationale: Filters by rationale
+    :param remediation: Filters by remediation
+    :param file: Filters by file
+    :param process: Filters by process
+    :param directory: Filters by directory
+    :param registry: Filters by registry
+    :param references: Filters by references
+    :param result: Filters by result
+    :param offset:First element to return in the collection
+    :param limit: Maximum number of elements to return
+    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order
+    :param search: Looks for elements with the specified string
+    :param query: Query to filter results by. This is specially useful to filter by total checks passed, failed or total score (fields pass, fail, score)
     """
     # get file parameter from query
     file_ = connexion.request.args.get('file', None)
