f452908504c894ce6bc6164ec142a7616df41e7f
JuantAldea <JuantAldea@users.noreply.github.com>
2019-05-07 17:34:08 +0200

    Enforce constantness of w_is_file(file) parameter

--- src/headers/os_utils.h
+++ src/headers/os_utils.h
@@ -26,7 +26,7 @@ char *w_os_get_runps(const char *ps, int mpid);
 /* Get list of Unix processes */
 OSList *w_os_get_process_list();
 /* Check if a file exists */
-int w_is_file(char *file);
+int w_is_file(const char * const file);
 /* Delete the process list */
 int w_del_plist(OSList *p_list);
 
