f1a1787328b8748b566d594aea613270c5bd32af
Cristobal Lopez <crolopezp@gmail.com>
2019-05-08 14:54:34 +0200

    Change a information message in Vulnerability Detector

--- src/error_messages/debug_messages.h
+++ src/error_messages/debug_messages.h
@@ -53,7 +53,7 @@
 #define VU_AG_NEVER_CON       "(5488): Agent '%s' never connected."
 #define VU_API_REQ_RETRY      "(5490): There was no valid response to '%s'. Retrying in %d seconds..."
 #define VU_UNEXP_JSON_KEY     "(5492): Unexpected JSON key: '%s'."
-#define VU_ENDING_UPDATE      "(5494): The update of the feeds ended successfully."
+#define VU_ENDING_UPDATE      "(5494): The update of the feeds finished successfully."
 #define VU_DOWNLOAD_FAIL      "(5495): The download can not be completed. Retrying in %d seconds."
 #define VU_ANAL_ACC_REPORTS   "(5501): Analyzing accumulated reports for %s in %s."
 #define VU_DISC_ACC_REPORTS   "(5502): Discarding report %s with check %s because %s is higher."
