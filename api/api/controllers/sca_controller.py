63f289629207413d1871cb45e280c060c76409a5
David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
2019-05-08 14:00:24 +0200

    Split a large string in api/api/controllers/sca_controller.py
    Co-Authored-By: druizz90 <32929872+druizz90@users.noreply.github.com>

--- api/api/controllers/sca_controller.py
+++ api/api/controllers/sca_controller.py
@@ -34,7 +34,8 @@ def get_sca_agent(agent_id=None, pretty=False, wait_for_complete=False,
     :param references: Filters by references
     :param offset:First element to return in the collection
     :param limit: Maximum number of elements to return
-    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order
+    :param sort: Sorts the collection by a field or fields (separated by comma). 
+    Use +/- at the beginning to list in ascending or descending order
     :param search: Looks for elements with the specified string
     :param query: Query to filter results by. This is specially useful to filter by total checks passed, failed or total score (fields pass, fail, score)
     """
