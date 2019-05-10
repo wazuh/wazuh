cd64fd1e73c75ea4beddad44566b9026918dc35e
Jesús Jaldo <jm.jadlo@wazuh.com>
2019-05-10 14:01:08 +0200

    Change response tavern test

--- api/test/integration/tavern_utils.py
+++ api/test/integration/tavern_utils.py
@@ -9,3 +9,14 @@ def calc_agents(response, total):
     :return: Number - 1
     """
     return {"totalAgents": str(int(total)-1)}
+
+def test_affected_items_response(response, affected_items):
+    """
+    :param response: Request response
+    :param affected_items: List of agent
+    :return: True if request response have this items
+    """
+    print(list(affected_items))
+    print(response.json()["data"]["affected_items"])
+    assert list(response.json()["data"]["affected_items"]) == list(affected_items)
+    return
