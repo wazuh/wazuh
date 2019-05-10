92b622b410864eaa387a584512a2f02383f2bd63
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-05-10 18:23:56 +0200

    Implement lists controllers from scratch

--- api/api/controllers/lists_controller.py
+++ api/api/controllers/lists_controller.py
@@ -5,12 +5,10 @@
 import asyncio
 import logging
 
-from connexion.lifecycle import ConnexionResponse
-
 from api.models.base_model_ import Data
 from api.util import remove_nones_to_dict, exception_handler, parse_api_param, raise_if_exc
-from wazuh.cluster.dapi.dapi import DistributedAPI
 from wazuh import cdb_list
+from wazuh.cluster.dapi.dapi import DistributedAPI
 
 loop = asyncio.get_event_loop()
 logger = logging.getLogger('wazuh')
@@ -18,27 +16,30 @@ logger = logging.getLogger('wazuh')
 
 @exception_handler
 def get_lists(pretty: bool = False, wait_for_complete: bool = False, offset: int = 0, limit: int = None,
-              sort: str = None, search: str = None, path: str = None, status: str = None):
-    """
-    :param pretty: Show results in human-readable format
+              sort: str = None, search: str = None, path: str = None):
+    """ Get all CDB lists
+
+    Returns the contents of all CDB lists. Optionally, the result can be filtered by several criteria.
+    See available parameters for more details.
+
+    :param pretty: Show results in human-readable format.
     :type pretty: bool
-    :param wait_for_complete: Disable timeout response
+    :param wait_for_complete: Disable timeout response.
     :type wait_for_complete: bool
-    :param offset: First element to return in the collection
+    :param offset: First element to return in the collection.
     :type offset: int
-    :param limit: Maximum number of elements to return
+    :param limit: Maximum number of elements to return.
     :type limit: int
-    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order.
+    :param sort: Sorts the collection by a field or fields (separated by comma).
+    Use +/- at the beginning to list in ascending or descending order.
     :type sort: str
-    :param search: Looks for elements with the specified string
+    :param search: Looks for elements with the specified string.
     :type search: str
-    :param path: Filters by rule path.
+    :param path: Filters by list path.
     :type path: str
-    :param status: Filters by rules status.
-    :type status: List[str]
     """
     f_kwargs = {'offset': offset, 'limit': limit, 'sort': parse_api_param(sort, 'sort'),
-                'search': parse_api_param(search, 'search'),  'path': path, 'status': status}
+                'search': parse_api_param(search, 'search'),  'path': path}
 
     dapi = DistributedAPI(f=cdb_list.get_lists,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -50,9 +51,66 @@ def get_lists(pretty: bool = False, wait_for_complete: bool = False, offset: int
                           )
 
     data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+    response = Data(data)
 
-    return data, 200
+    return response, 200
 
 
-def get_lists_files():
-    pass
+@exception_handler
+def get_list(pretty: bool = False, wait_for_complete: bool = False, path: str = None):
+    """ Get CBD list from a specific file path
+
+    Returns the contents of specified CDB list.
+
+    :param pretty: Show results in human-readable format.
+    :type pretty: bool
+    :param wait_for_complete: Disable timeout response.
+    :type wait_for_complete: bool
+    :param path: File path to load list from
+    :type path: str
+    """
+    f_kwargs = {'file_path': path}
+
+    dapi = DistributedAPI(f=cdb_list.get_list_from_file,
+                          f_kwargs=remove_nones_to_dict(f_kwargs),
+                          request_type='local_master',
+                          is_async=False,
+                          wait_for_complete=wait_for_complete,
+                          pretty=pretty,
+                          logger=logger
+                          )
+
+    data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+    response = Data(data)
+
+    return response, 200
+
+
+@exception_handler
+def get_lists_files(pretty: bool = False, wait_for_complete: bool = False):
+    """ Get paths from all CDB lists
+
+    Returns the path from all CDB lists. Use this method to know all the CDB lists and
+    their location in the filesystem relative to Wazuh installation folder
+
+    :param pretty: Show results in human-readable format.
+    :type pretty: bool
+    :param wait_for_complete: Disable timeout response.
+    :type wait_for_complete: bool
+    """
+
+    f_kwargs = {}
+
+    dapi = DistributedAPI(f=cdb_list.get_path_lists,
+                          f_kwargs=remove_nones_to_dict(f_kwargs),
+                          request_type='local_master',
+                          is_async=False,
+                          wait_for_complete=wait_for_complete,
+                          pretty=pretty,
+                          logger=logger
+                          )
+
+    data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+    response = Data(data)
+
+    return response, 200
