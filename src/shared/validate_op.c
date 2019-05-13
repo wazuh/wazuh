e907d114153684b1aa22d837809d5279ac664290
DaveVG1 <vgdavid91@gmail.com>
2019-05-13 08:42:50 +0000

    Change 'getDefine_Int' definition in order to ignore internal options file when option is not defined

--- src/shared/validate_op.c
+++ src/shared/validate_op.c
@@ -209,10 +209,12 @@ int getDefine_Int(const char *high_name, const char *low_name, int min, int max)
     if (!value) {
         value = _read_file(high_name, low_name, OSSEC_DEFINES);
         if (!value) {
-            merror_exit(DEF_NOT_FOUND, high_name, low_name);
+            mwarn("Option '%s.%s' not defined in any internal options file", high_name, low_name);
+            return -99999;
         }
     }
 
+    mwarn("internal_options and local_internal_options files are being deprecated. We strongly recommend to set this option '%s.%s' in 'ossec.conf", high_name, low_name);
     pt = value;
     while (*pt != '\0') {
         if (!isdigit((int)*pt)) {
