bd41b1693e86e9482bfae233026a1012b50f7d0e
Cerv1 <cervick13@gmail.com>
2019-05-15 13:42:51 +0200

    Fixing files format and adding documentation for rotation_op

--- src/headers/rotation_op.h
+++ src/headers/rotation_op.h
@@ -11,6 +11,14 @@
 #define _W_ROTATION
 #define PATH_SEP_ROT '/'
 
+/*  Rotation node
+ * - first_value: The day of the log
+ * - second_value: The counter of the log
+ * - string_value: The path of the log
+ * - next: Next node with the next log
+ *
+ * - prev: Previous node with the previous log
+ */
 typedef struct rotation_node {
     int first_value;
     int second_value;
@@ -19,14 +27,24 @@ typedef struct rotation_node {
     struct rotation_node *prev;
 } rotation_node;
 
+/*  Rotation list
+ * - count: The number of elements of the list
+ * - first: The oldest log
+ * - last: The newest log
+ */
 typedef struct rotation_list {
     int count;
     rotation_node *first;
     rotation_node *last;
 } rotation_list;
 
+// Generate the rotation list for the given tag and extension
 rotation_list *get_rotation_list(char *tag, char *ext);
+
+// Purge the rotation list to 'keep_files' number of files if it's enabled
 void purge_rotation_list(rotation_list *list, int keep_files);
+
+// Add a new node to the rotation list
 void add_new_rotation_node(rotation_list *list, char *value, int keep_files);
 
 #endif
--- src/monitord/compress_log.c
+++ src/monitord/compress_log.c
@@ -1,47 +1,47 @@
 /* Copyright (C) 2015-2019, Wazuh Inc.
- * Copyright (C) 2009 Trend Micro Inc.
- * All right reserved.
- *
- * This program is a free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation
- */
+* Copyright (C) 2009 Trend Micro Inc.
+* All right reserved.
+*
+* This program is a free software; you can redistribute it
+* and/or modify it under the terms of the GNU General Public
+* License (version 2) as published by the FSF - Free Software
+* Foundation
+*/
 
- #include "shared.h"
+#include "shared.h"
 #include "monitord.h"
 #include "../external/zlib/zlib.h"
 
 
- /* gzip a log file */
+/* gzip a log file */
 void OS_CompressLog(const char *logfile)
 {
     FILE *log;
     gzFile zlog;
 
-     char logfileGZ[OS_FLSIZE + 1];
+    char logfileGZ[OS_FLSIZE + 1];
     int len, err;
 
-     char buf[OS_MAXSTR + 1];
+    char buf[OS_MAXSTR + 1];
 
-     /* Clear memory */
+    /* Clear memory */
     memset(logfileGZ, '\0', OS_FLSIZE + 1);
     memset(buf, '\0', OS_MAXSTR + 1);
 
-     /* Set umask */
+    /* Set umask */
     umask(0027);
 
-     /* Create the gzip file name */
+    /* Create the gzip file name */
     snprintf(logfileGZ, OS_FLSIZE, "%s.gz", logfile);
 
-     /* Read log file */
+    /* Read log file */
     log = fopen(logfile, "r");
     if (!log) {
         /* Do not warn in here, since the alert file may not exist */
         return;
     }
 
-     /* Open compressed file */
+    /* Open compressed file */
     zlog = gzopen(logfileGZ, "w");
     if (!zlog) {
         fclose(log);
@@ -49,7 +49,7 @@ void OS_CompressLog(const char *logfile)
         return;
     }
 
-     for (;;) {
+    for (;;) {
         len = (int) fread(buf, 1, OS_MAXSTR, log);
         if (len <= 0) {
             break;
@@ -59,12 +59,12 @@ void OS_CompressLog(const char *logfile)
         }
     }
 
-     fclose(log);
+    fclose(log);
     gzclose(zlog);
 
-     /* Remove uncompressed file */
+    /* Remove uncompressed file */
     if ( unlink(logfile) == -1)
         merror("Unable to delete '%s' due to '%s'", logfile, strerror(errno));
 
-     return;
+    return;
 }
