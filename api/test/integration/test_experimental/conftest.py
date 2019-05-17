b7d0c96fefb4e295faaed9a947a083a563a571b9
druizz90 <demetrio.ruiz@wazuh.com>
2019-05-17 10:54:17 +0200

    Add new environment for testing API.

--- api/test/integration/test_experimental/conftest.py
+++ api/test/integration/test_experimental/conftest.py
@@ -4,9 +4,9 @@ import os
 
 @pytest.fixture(name="experimental_test", scope="session")
 def fix_test():
-    os.system("docker-compose -p ./environment_experimental/docker-compose.yml up --build -d")
-    time.sleep(60)
+    os.system("docker-compose -f ./environment_experimental/docker-compose.yml up --build -d")
+    time.sleep(20)
     print('Environment ready, starting tests')
     yield
-    print('Tests finished')
-    os.system("docker-compose down")
+    print('\nTests finished')
+    os.system("docker-compose -f ./environment_experimental/docker-compose.yml down")
