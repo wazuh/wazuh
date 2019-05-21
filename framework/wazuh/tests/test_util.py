e25b5540ba3874da8569b858ee94216f7217379c
druizz90 <demetrio.ruiz@wazuh.com>
2019-05-21 08:20:30 +0200

    Add unit tests for 'WazuhVersion' class.

---
+++ framework/wazuh/tests/test_util.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+
+import pytest
+import os
+
+from wazuh.utils import WazuhVersion
+
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
+
+@pytest.mark.parametrize('version1', 'version2' [
+('Wazuh v3.5.0','Wazuh v3.5.2'),
+('Wazuh v3.6.1', 'Wazuh v3.6.3'),
+('Wazuh v3.7.2', 'Wazuh v3.8.0'),
+('Wazuh v3.8.0', 'Wazuh v3.8.1'),
+('Wazuh v3.9.0', 'Wazuh v3.9.2'),
+('Wazuh v3.9.10', 'Wazuh v3.9.14'),
+('Wazuh v3.10.1', 'Wazuh v3.10.10'),
+('Wazuh v4.10.10', 'Wazuh v4.11.0'),
+('Wazuh v5.1.15', 'Wazuh v5.2.0')
+])
+def test_version(version1, version2):
+    """
+    Test WazuhVersion class
+    """
+    current_version = WazuhVersion(version1)
+    new_version = WazuhVersion(version2)
+
+    assert current_version < new_version, True
+    assert new_version > current_version, True
+    assert current_version != new_version, True
+    assert current_version == new_version, False
