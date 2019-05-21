9549466464134713d60c25393c3d1f4b0449d68b
druizz90 <demetrio.ruiz@wazuh.com>
2019-05-21 11:01:10 +0200

    Fix for updating agents to Wazuh 3.10 or higher.

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1915,7 +1915,7 @@ class Agent:
             raise WazuhException(1713, error)
 
         protocol = self._get_protocol(wpk_repo, use_http)
-        if (version is None or version[:4] >= "v3.4") and self.os['platform'] != "windows":
+        if (version is None or WazuhVersion(version) >= WazuhVersion("v3.4.0")) and self.os['platform'] != "windows":
             versions_url = protocol + wpk_repo + "linux/" + self.os['arch'] + "/versions"
         else:
             if self.os['platform']=="windows":
@@ -1947,21 +1947,21 @@ class Agent:
         # Get manager version
         manager = Agent(id=0)
         manager._load_info_from_DB()
-        manager_ver = manager.version
+        manager_ver = WazuhVersion(manager.version)
         if debug:
-            print("Manager version: {0}".format(manager_ver.split(" ")[1]))
+            print("Manager version: {0}".format(manager_ver))
 
         agent_new_ver = None
         versions = self._get_versions(wpk_repo=wpk_repo, version=version, use_http=use_http)
         if not version:
             for versions in versions:
-                if versions[0] == manager_ver.split(" ")[1]:
+                if WazuhVersion(versions[0]) == manager_ver:
                     agent_new_ver = versions[0]
                     agent_new_shasum = versions[1]
                     break
         else:
             for versions in versions:
-                if versions[0] == version:
+                if WazuhVersion(versions[0]) == version:
                     agent_new_ver = versions[0]
                     agent_new_shasum = versions[1]
                     break
@@ -1971,14 +1971,14 @@ class Agent:
         # Comparing versions
         agent_ver = self.version
 
-        if (WazuhVersion(manager_ver.split(" ")[1]) < WazuhVersion(agent_new_ver) and not force):
-            raise WazuhException(1717, "Manager: {0} / Agent: {1} -> {2}".format(manager_ver.split(" ")[1], agent_ver.split(" ")[1], agent_new_ver))
+        if (WazuhVersion(manager_ver) < WazuhVersion(agent_new_ver) and not force):
+            raise WazuhException(1717, "Manager: {0} / Agent: {1} -> {2}".format(manager_ver, agent_new_ver))
 
-        if (WazuhVersion(agent_ver.split(" ")[1]) >= WazuhVersion(agent_new_ver) and not force):
-            raise WazuhException(1749, "Agent: {0} -> {1}".format(agent_ver.split(" ")[1], agent_new_ver))
+        if (WazuhVersion(agent_ver) >= WazuhVersion(agent_new_ver) and not force):
+            raise WazuhException(1749, "Agent: {0} -> {1}".format(agent_ver, agent_new_ver))
 
         if debug:
-            print("Agent version: {0}".format(agent_ver.split(" ")[1]))
+            print("Agent version: {0}".format(agent_ver))
             print("Agent new version: {0}".format(agent_new_ver))
 
         protocol = self._get_protocol(wpk_repo, use_http)
@@ -1988,7 +1988,7 @@ class Agent:
             wpk_url = protocol + wpk_repo + "windows/" + wpk_file
 
         else:
-            if version is None or version[:4] >= "v3.4":
+            if version is None or WazuhVersion(version) >= WazuhVersion("v3.4.0"):
                 wpk_file = "wazuh_agent_{0}_linux_{1}.wpk".format(agent_new_ver, self.os['arch'])
                 wpk_url = protocol + wpk_repo + "linux/" + self.os['arch'] + "/" + wpk_file
 
@@ -2172,7 +2172,7 @@ class Agent:
             raise WazuhException(1720)
 
         # Check if remote upgrade is available for the selected agent version
-        if WazuhVersion(self.version.split(' ')[1]) < WazuhVersion("3.0.0-alpha4"):
+        if WazuhVersion(self.version) < WazuhVersion("3.0.0-alpha4"):
             raise WazuhException(1719, version)
 
         if self.os['platform']=="windows" and int(self.os['major']) < 6:
