23e685f750cb75b8702770a21097c8ac7614fec2
druizz90 <demetrio.ruiz@wazuh.com>
2019-05-22 13:22:52 +0200

    Update unit tests for 'WazuhVersion' class.

--- framework/wazuh/tests/test_util.py
+++ framework/wazuh/tests/test_util.py
@@ -21,9 +21,15 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
     ('Wazuh v3.9.10', 'Wazuh v3.9.14'),
     ('Wazuh v3.10.1', 'Wazuh v3.10.10'),
     ('Wazuh v4.10.10', 'Wazuh v4.11.0'),
-    ('Wazuh v5.1.15', 'Wazuh v5.2.0')
+    ('Wazuh v5.1.15', 'Wazuh v5.2.0'),
+    ('v3.6.0', 'v3.6.1'),
+    ('v3.9.1', 'v3.9.2'),
+    ('v4.0.0', 'v4.0.1'),
+    ('3.6.0', '3.6.1'),
+    ('3.9.0', '3.9.2'),
+    ('4.0.0', '4.0.1')
 ])
-def test_version(version1, version2):
+def test_version_ok(version1, version2):
     """
     Test WazuhVersion class
     """
@@ -39,3 +45,27 @@ def test_version(version1, version2):
 
     assert isinstance(current_version.to_array(), list)
     assert isinstance(new_version.to_array(), list)
+
+
+@pytest.mark.parametrize('version1, version2', [
+    ('v3.6.0', 'v.3.6.1'),
+    ('Wazuh v4', 'Wazuh v5'),
+    ('Wazuh v3.9', 'Wazuh v3.10'),
+    ('ABC v3.10.1', 'ABC v3.10.12'),
+    ('Wazuhv3.9.0', 'Wazuhv3.9.2'),
+    ('3.9', '3.10'),
+    ('3.9.0', '3.10'),
+    ('3.10', '4.2'),
+    ('3', '3.9.1')
+])
+def test_version_ko(version1, version2):
+    """
+    Test WazuhVersion class
+    """
+    try:
+        current_version = WazuhVersion(version1)
+        new_version = WazuhVersion(version2)
+    except ValueError:
+        return
+
+    raise Exception
