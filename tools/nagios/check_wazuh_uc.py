0c52e2e1912053e7596dc1f53ec1f1830f9f7771
Laurence Gill <laurencegill@users.noreply.github.com>
2019-05-24 16:08:27 +0100

    tidy up a little

--- tools/nagios/check_wazuh_uc.py
+++ tools/nagios/check_wazuh_uc.py
@@ -6,7 +6,10 @@ import sqlite3
 db_dir = '/var/ossec/var/db/integrations/'
 db_file = 'alerts.db'
 db_name = (db_dir + db_file)
-alert_level = 15
+threshold = 15
+sql = 'SELECT COUNT(*) from alert WHERE\
+ alert_level >= {} AND classification IS\
+ NULL'.format(threshold)
 OK = 0
 WARNING = 1
 CRITICAL = 2
@@ -14,12 +17,9 @@ UNKNOWN = 3
 
 
 def check_alert_db():
-    level = (alert_level,)
     conn = sqlite3.connect(db_name)
     cur = conn.cursor()
-    cur.execute('SELECT COUNT(*) \
-      from alert WHERE alert_level >= ? \
-      AND classification IS NULL', level)
+    cur.execute(sql)
     result = cur.fetchone()
     conn.close()
     result_tuple = result
@@ -37,9 +37,9 @@ def print_alert(exitcode=UNKNOWN, exitmsg='UNKNOWN - Status is unknown'):
 def main():
     cur_alerts = get_alert_count()
     if cur_alerts == 0:
-        print_alert(exitcode=OK, exitmsg='OK')
+        print_alert(exitcode=OK, exitmsg='OK - {} unclassified alerts'.format(cur_alerts))
     elif cur_alerts >= 1:
-        print_alert(exitcode=CRITICAL, exitmsg='CRITICAL - There are unclassified alerts')
+        print_alert(exitcode=CRITICAL, exitmsg='CRITICAL - There are {} unclassified alerts'.format(cur_alerts))
     else:
         print_alert()
 
