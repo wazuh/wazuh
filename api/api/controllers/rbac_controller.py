69dd394e13ad366b388770eaccf64788581de933
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-05-28 13:20:14 +0200

    WIP: Added Role object

--- api/api/controllers/rbac_controller.py
+++ api/api/controllers/rbac_controller.py
@@ -12,7 +12,7 @@ from connexion.lifecycle import ConnexionResponse
 from api.models.base_model_ import Data
 from api.util import remove_nones_to_dict, exception_handler, parse_api_param, raise_if_exc
 from wazuh.cluster.dapi.dapi import DistributedAPI
-from wazuh.rbac import *
+from wazuh.rbac import Role
 from wazuh.exception import WazuhError, WazuhInternalError
 
 
@@ -21,19 +21,15 @@ logger = logging.getLogger('wazuh')
 
 
 @exception_handler
-def get_roles(role_name=None, pretty=False, wait_for_complete=False):
+def get_roles(pretty=False, wait_for_complete=False):
     """
-    :param role_name: Specified role
-    :type role_name: str
     :param pretty: Show results in human-readable format
     :type pretty: bool
     :param wait_for_complete: Disable timeout response
     :type wait_for_complete: bool
     """
-    f_kwargs = {'role_name': role_name}
 
-    dapi = DistributedAPI(f=get_roles,
-                          f_kwargs=remove_nones_to_dict(f_kwargs),
+    dapi = DistributedAPI(f=Role.get_roles,
                           request_type='local_master',
                           is_async=False,
                           wait_for_complete=wait_for_complete,
@@ -44,3 +40,29 @@ def get_roles(role_name=None, pretty=False, wait_for_complete=False):
     response = Data(data)
 
     return response, 200
+
+
+# @exception_handler
+# def get_role(role_name=None, pretty=False, wait_for_complete=False):
+#     """
+#     :param role_name: Specified role
+#     :type role_name: str
+#     :param pretty: Show results in human-readable format
+#     :type pretty: bool
+#     :param wait_for_complete: Disable timeout response
+#     :type wait_for_complete: bool
+#     """
+#     f_kwargs = {'role_name': role_name}
+#
+#     dapi = DistributedAPI(f=get_role,
+#                           f_kwargs=remove_nones_to_dict(f_kwargs),
+#                           request_type='local_master',
+#                           is_async=False,
+#                           wait_for_complete=wait_for_complete,
+#                           pretty=pretty,
+#                           logger=logger
+#                           )
+#     data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+#     response = Data(data)
+#
+#     return response, 200
--- framework/wazuh/rbac.py
+++ framework/wazuh/rbac.py
@@ -3,33 +3,103 @@
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 from wazuh.RBAC import RBAC
+from wazuh.exception import WazuhException, WazuhInternalError, WazuhError
 
 
-def get_role(role_name):
+class Role:
     """
-    Here we will be able to obtain a certain role, as well as to
-    know that it exists or to obtain all the roles of the system.
-
-    :param role_name: Return the information of a role
-    :return: Message.
+    Role Object.
     """
 
-    role = None
-    with RBAC.RolesManager as rm:
-        role = rm.get_role(name=role_name)
+    SORT_FIELDS = ['name']
 
-    return role
+    def __init__(self):
+        self.id = None
+        self.name = None
+        self.rule = None
+        self.policies = list()
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def __lt__(self, other):
+        if isinstance(other, Role):
+            return self.id < other.id
+        else:
+            raise WazuhInternalError(1204)
+
+    def __le__(self, other):
+        if isinstance(other, Role):
+            return self.id <= other.id
+        else:
+            raise WazuhInternalError(1204)
+
+    def __gt__(self, other):
+        if isinstance(other, Role):
+            return self.id > other.id
+        else:
+            raise WazuhInternalError(1204)
+
+    def __ge__(self, other):
+        if isinstance(other, Role):
+            return self.id >= other.id
+        else:
+            raise WazuhInternalError(1204)
+
+    def to_dict(self):
+        return {'id': self.id, 'name': self.name, 'rule': self.rule, 'policies': self.policies}
+
+    def add_policy(self, policy):
+        """
+        Adds a policy to the policies list.
+        :param policy: Policy to add (string or list)
+        """
+
+        Role.__add_unique_element(self.policies, policy)
+
+    @staticmethod
+    def __add_unique_element(src_list, element):
+        new_list = []
+
+        if type(element) in [list, tuple]:
+            new_list.extend(element)
+        else:
+            new_list.append(element)
+
+        for item in new_list:
+            if item is not None and item != '':
+                i = item.strip()
+                if i not in src_list:
+                    src_list.append(i)
+
+    @staticmethod
+    def get_role(role_name):
+        """
+        Here we will be able to obtain a certain role, as well as to
+        know that it exists or to obtain all the roles of the system.
+
+        :param role_name: Return the information of a role
+        :return: Message.
+        """
+
+        role = None
+        with RBAC.RolesManager as rm:
+            role = rm.get_role(name=role_name)
+
+        return role
 
 
-def get_roles():
-    """
-    Here we will be able to obtain all roles
+    @staticmethod
+    def get_roles():
+        """
+        Here we will be able to obtain all roles
 
-    :return: Message.
-    """
+        :return: Message.
+        """
+        print('Here')
 
-    roles = None
-    with RBAC.RolesManager as rm:
-        roles = rm.get_roles()
+        roles = None
+        with RBAC.RolesManager as rm:
+            roles = rm.get_roles()
 
-    return roles
+        return roles
