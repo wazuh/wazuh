c5b36de0e1b813f93532fa7e607ad1af10340b63
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-05-28 17:01:37 +0200

    Add gpg13 unit tests

--- framework/wazuh/tests/test_rules.py
+++ framework/wazuh/tests/test_rules.py
@@ -26,6 +26,7 @@ rule_contents = '''
 </group>
     '''
 
+mocked_items = {'items': [], 'totalItems': 0}
 
 def rules_files(file_path):
     """
@@ -175,3 +176,8 @@ def test_get_rules_file_search(mock_config, mock_glob, search, func):
             d_files['items'] = list(map(lambda x: x.to_dict(), d_files['items']))
         if search is not None:
             assert d_files['items'][0]['file'] == f"rules{'0' if search['negation'] else '1'}.xml"
+
+
+@patch('wazuh.rule.Rule._get_requirement', return_value = mocked_items)
+def test_get_gpg13(mocked):
+    assert isinstance(Rule.get_gpg13(), dict)
