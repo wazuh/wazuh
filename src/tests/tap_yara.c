597791f7061a81a3bed7c3351257e26157af73c2
TJOSERAFAEL <rafael.cenit@wazuh.com>
2019-06-06 18:44:37 +0200

    Added yara unit test squeleton

---
+++ src/tests/tap_yara.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../wazuh_modules/wmodules.h"
+#include "../wazuh_modules/wm_yara.h"
+#include "../wazuh_modules/wm_yara.c"
+
+#include "tap.h"
+
+int main(void) {
+    printf("\n\n   STARTING TEST - YARA MODULE  \n\n");
+
+    TAP_PLAN;
+    TAP_SUMMARY;
+    printf("\n   ENDING TEST  - YARA MODULE   \n\n");
+    return 0;
+}
