214990c205330967a74050bc0704df629acbcbac
druizz90 <demetrio.ruiz@wazuh.com>
2019-06-10 16:51:03 +0200

    Change id parameter to keyword argument.

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1157,7 +1157,7 @@ class Agent:
 
 
     @staticmethod
-    def insert_agent(name, id, key, ip='any', force=-1):
+    def insert_agent(name, key, id=None, ip='any', force=-1):
         """
         Create a new agent providing the id, name, ip and key to the Manager.
 
