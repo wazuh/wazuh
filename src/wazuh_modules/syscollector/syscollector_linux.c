cd41cd309e855a87fa5f5cddbc4ee3e88f321c6d
Sergio Vela Pelegrina <sergio.vela@wazuh.com>
2019-06-14 11:30:28 +0000

    Prevent Modulesd from spawning processes with unused inherited file descriptors

--- src/wazuh_modules/syscollector/syscollector_linux.c
+++ src/wazuh_modules/syscollector/syscollector_linux.c
@@ -636,6 +636,7 @@ char * sys_deb_packages(int queue_fd, const char* LOCATION, int random_id){
     memset(read_buff, 0, OS_MAXSTR);
 
     if ((fp = fopen(file, "r"))) {
+        w_file_cloexec(fp);
 
         while(fgets(read_buff, OS_MAXSTR, fp) != NULL){
 
