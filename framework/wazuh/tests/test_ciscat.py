f4a40ddffb47b675d6de4382343d07f767cb2af3
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-06-24 09:25:31 +0200

    Update unit test

--- framework/wazuh/tests/test_ciscat.py
+++ framework/wazuh/tests/test_ciscat.py
@@ -3,26 +3,18 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-from unittest import TestCase, mock
+from unittest import mock
 
-from wazuh.ciscat import get_results_agent, get_ciscat_results
+import wazuh.ciscat
 
 
 def mocked_get_item_agent(**kwargs):
     return {'totalItems': 4, 'items': []}
 
 
-class TestCiscat(TestCase):
-    @mock.patch('wazuh.ciscat.get_item_agent', side_effect=mocked_get_item_agent)
-    def test_get_results_agent(self, get_function):
-        result = get_results_agent('001')
-        self.assertIsInstance(result, dict)
-        self.assertIsInstance(result['totalItems'], int)
-        self.assertIsInstance(result['items'], list)
-
-    @mock.patch('wazuh.ciscat.get_item_agent', side_effect=mocked_get_item_agent)
-    def test_get_ciscat_results(self, get_function):
-        result = get_ciscat_results()
-        self.assertIsInstance(result, dict)
-        self.assertIsInstance(result['totalItems'], int)
-        self.assertIsInstance(result['items'], list)
+@mock.patch('wazuh.ciscat.get_item_agent', side_effect=mocked_get_item_agent)
+def test_get_ciscat_results(*mocked_args):
+    result = wazuh.ciscat.get_ciscat_results()
+    assert isinstance(result, dict)
+    assert isinstance(result['totalItems'], int)
+    assert isinstance(result['items'], list)
