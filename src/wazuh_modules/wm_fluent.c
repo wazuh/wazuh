b5b71ae13d3341532f19ab325a99f75fcae1ff6e
Skeptor <pablo.navarro@wazuh.com>
2019-07-05 17:26:01 +0200

    Fix Array to NULL comparison

--- src/wazuh_modules/wm_fluent.c
+++ src/wazuh_modules/wm_fluent.c
@@ -671,7 +671,8 @@ static int wm_fluent_send(wm_fluent_t * fluent, const char * str, size_t size) {
         assert(fluent->ssl);
         retval = SSL_write(fluent->ssl, sbuf.data, sbuf.size) == (ssize_t)sbuf.size ? 0 : -1;
     } else {
-        retval = send(fluent->client_sock, sbuf.data, sbuf.size, 0) == (ssize_t)sbuf.size ? 0 : -1;
+        if(sbuf.data)
+            retval = send(fluent->client_sock, sbuf.data, sbuf.size, 0) == (ssize_t)sbuf.size ? 0 : -1;
     }
 
     msgpack_sbuffer_destroy(&sbuf);
