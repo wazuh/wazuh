b4ed62b433f6ef18892a4f393315622ba493ccb9
Zenidd <jpsaezgutierrez@gmail.com>
2019-07-08 15:34:48 +0200

    mwarn to minfo for duplicated localfile entries

--- src/logcollector/logcollector.c
+++ src/logcollector/logcollector.c
@@ -1474,7 +1474,7 @@ static IT_control remove_duplicates(logreader *current, int i, int j) {
             }
 
             if (current != dup && dup->file && !strcmp(current->file, dup->file)) {
-                mwarn(DUP_FILE, current->file);
+                minfo(DUP_FILE, current->file);
                 int result;
                 if (j < 0) {
                     result = Remove_Localfile(&logff, i, 0, 1,NULL);
