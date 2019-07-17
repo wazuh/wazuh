e8ea453e54f6efa0976d031f879cb285c4afb58a
Jesús Jaldo <jesus.jalru@gmail.com>
2019-07-17 13:46:11 +0200

    Add new unit tests for utils

--- framework/wazuh/tests/test_util.py
+++ framework/wazuh/tests/test_util.py
@@ -5,11 +5,37 @@
 
 
 import pytest
-import os
 
-from wazuh.utils import WazuhVersion
+from wazuh.utils import *
+from unittest.mock import patch
+from wazuh import exception
+from sys import modules
+from subprocess import CalledProcessError
 
-test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+# MOCK DATA
+
+class ClassTest(object):
+    """__init__() functions as the class constructor"""
+
+    def __init__(self, name=None, job=None):
+        self.name = name
+        self.job = job
+
+    def to_dict(self):
+        return {'name': self.name, 'job': self.job}
+
+
+mock_array = [{'rx': {'bytes': 4005, 'packets': 30}, 'scan': {'id': 1999992193, 'time': '2019/05/29 07:25:26'},
+               'mac': '02:42:ac:14:00:05', 'agent_id': '000'},
+              {'rx': {'bytes': 447914, 'packets': 1077}, 'scan': {'id': 396115592, 'time': '2019/05/29 07:26:26'},
+               'mac': '02:42:ac:14:00:01', 'agent_id': '003'}]
+mock_sort_by = ['mac']
+mock_array_order_by_mac = [
+    {'rx': {'bytes': 447914, 'packets': 1077}, 'scan': {'id': 396115592, 'time': '2019/05/29 07:26:26'},
+     'mac': '02:42:ac:14:00:01', 'agent_id': '003'},
+    {'rx': {'bytes': 4005, 'packets': 30}, 'scan': {'id': 1999992193, 'time': '2019/05/29 07:25:26'},
+     'mac': '02:42:ac:14:00:05', 'agent_id': '000'}]
+mock_array_class = [ClassTest("Payne", "coach")]
 
 
 @pytest.mark.parametrize('version1, version2', [
@@ -93,3 +119,142 @@ def test_same_version(version1, version2):
 
     assert isinstance(current_version.to_array(), list)
     assert isinstance(new_version.to_array(), list)
+
+
+@pytest.mark.parametrize('month', [
+    1,
+    2,
+    -1
+])
+def test_previous_moth(month):
+    result = previous_month(month)
+
+    assert isinstance(result, datetime)
+
+
+def test_execute():
+    with patch('wazuh.utils.check_output', return_value='{"data":"Some data", "message":"Some message", "error":0}'):
+        result = execute('Something')
+
+        assert isinstance(result, str)
+
+
+@pytest.mark.parametrize('error_effect, expected_exception', [
+    (CalledProcessError(returncode=10000, cmd='Unspected error', output='{"data":"Some data", "message":"Error", "error":10000}'), 10000),
+    (Exception, 1002),
+    (CalledProcessError(returncode=1, cmd='Unspected error', output={}), 1003),
+    (CalledProcessError(returncode=1, cmd='Unspected error', output='{"error":10000}'), 1004),
+    (CalledProcessError(returncode=1, cmd='Unspected error', output='{"data":"Some data", "message":"Error"}'), 1004)
+])
+def test_failed_execute(error_effect, expected_exception):
+    with pytest.raises(exception.WazuhException, match=f'.* {expected_exception} .*'):
+        with patch('wazuh.utils.check_output', side_effect=error_effect):
+            execute('Something')
+
+
+@pytest.mark.parametrize('array, limit', [
+    (['one', 'two', 'three'], 2),
+    (['one', 'two', 'three'], None),
+    ([], 1)
+])
+def test_cut_array(array, limit):
+    result = cut_array(array=array, limit=limit, offset=0)
+
+    assert isinstance(result, list)
+
+
+@pytest.mark.parametrize('limit, offset, expected_exception', [
+    (11, 0, 1405),
+    (0, 0, 1406),
+    (5, -1, 1400),
+    (-1, 0, 1401)
+])
+@patch('wazuh.utils.common.maximum_database_limit', new=10)
+def test_failed_cut_array(limit, offset, expected_exception):
+    with pytest.raises(exception.WazuhException, match=f'.* {expected_exception} .*'):
+        cut_array(array=['one', 'two', 'three'], limit=limit, offset=offset)
+
+
+def test_sort_array_type():
+    """
+    Tests utils.sort_array() response type
+    """
+    assert isinstance(sort_array(mock_array, mock_sort_by), list)
+
+
+@pytest.mark.parametrize('array, sort_by, order, expected_exception', [
+    ([{'test':'test'}], None, 'asc', 1404),
+    ('{}', None, 'ramdom', 1402),
+    (mock_array, ['test'], 'asc', 1403)
+])
+def test_sort_array_error(array, sort_by, order, expected_exception):
+    """
+    Tests utils.sort_array() function for all exceptions cases:
+        * List with a dictionary and no sort parameter
+        * Order type different to 'asc' or 'desc'
+        * Sort parameter not allow
+    """
+    with pytest.raises(exception.WazuhException, match=f'.* {expected_exception} .*'):
+        sort_array(array, sort_by, order)
+
+
+@pytest.mark.parametrize('array, sort_by, order, allowed_sort_field, output', [
+    ('', None, 'asc', None, ''),
+    ([4005, 4006, 4019, 36], None, 'asc', None, [36, 4005, 4006, 4019]),
+    ([4005, 4006, 4019, 36], None, 'desc', None, [4019, 4006, 4005, 36]),
+    (mock_array, mock_sort_by, 'asc', mock_sort_by, mock_array_order_by_mac),
+    (mock_array_class, ['name'], 'desc', ['name'], mock_array_class)
+])
+def test_sort_array(array, sort_by, order, allowed_sort_field, output):
+    """
+    Tests utils.sort_array() function for different cases:
+        * Empty list
+        * Sorted list with values
+        * Sorted list with order parameter 'desc'
+        * Sorted list with dict, sorted by one nester parameter
+        * Sorted list with dict, sorted by different parameter
+        * Sorted list with class
+    """
+    assert sort_array(array, sort_by, order, allowed_sort_field) == output
+
+
+@pytest.mark.parametrize('object, fields', [
+    ({'test':'test'}, None),
+    ({'test':'test'}, ['test']),
+    (['test', 'name'], None),
+    (ClassTest("Payne", "coach"), None)
+])
+def test_get_values(object, fields):
+    result = get_values(o=object, fields=fields)
+
+    assert isinstance(result, list)
+    assert isinstance(result[0], str)
+
+
+
+@pytest.mark.parametrize('array, text, negation, length', [
+    (['test', 'name'], 'e', False, 2),
+    (['test', 'name'], 'name', False, 1),
+    (['test', 'name'], 'unknown', False, 0),
+    (['test', 'name'], 'test', True, 1),
+    (['test', 'name'], 'unknown', True, 2)
+])
+def test_search_array(array, text, negation, length):
+    result = search_array(array=array, text=text, negation=negation)
+
+    assert isinstance(result, list)
+    assert len(result) == length
+
+
+def test_filemode():
+    result = filemode(40960)
+
+    assert isinstance(result, str)
+
+
+#def test_failed_import():
+    #del modules['wazuh.utils']
+    #del modules['subprocess']
+    #with patch.dict('sys.modules', {'subprocess.check_output': None}):
+        #modules.items()
+        #import wazuh.utils
