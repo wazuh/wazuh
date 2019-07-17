c5a281df3aff0ca130f555406c4e29320d131f81
Laurence <laurencegill@users.noreply.github.com>
2019-07-17 17:16:17 +0100

    check for all unclassified alerts unless the user specifies a severity level

--- tools/nagios/check_wazuh_uc.py
+++ tools/nagios/check_wazuh_uc.py
@@ -2,21 +2,33 @@
 
 import sys
 import sqlite3
+import argparse
 
+argp = argparse.ArgumentParser()
+argp.add_argument('--level', help='severity level', type=int)
+arg = argp.parse_args()
 db_dir = '/var/ossec/var/db/integrations/'
 db_file = 'alerts.db'
 db_name = (db_dir + db_file)
 threshold = 15
-sql = 'SELECT COUNT(*) from alert WHERE\
- alert_level >= {} AND classification IS\
- NULL'.format(threshold)
 OK = 0
 WARNING = 1
 CRITICAL = 2
 UNKNOWN = 3
 
+def set_sql():
+    if arg.level:
+        threshold = arg.level
+        sql = 'SELECT COUNT(*) from alert WHERE\
+         alert_level >= {} AND classification IS\
+         NULL'.format(threshold)
+    else:
+        sql = 'SELECT COUNT(*) from alert WHERE\
+         classification IS NULL'
+    return sql
 
 def check_alert_db():
+    sql = set_sql()
     conn = sqlite3.connect(db_name)
     cur = conn.cursor()
     cur.execute(sql)
