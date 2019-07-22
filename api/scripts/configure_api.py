2800d20a00601472964a191cc2787e1edb19e654
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-07-22 16:34:58 +0200

    Added http enable-disable parameter

--- api/scripts/configure_api.py
+++ api/scripts/configure_api.py
@@ -159,28 +159,26 @@ def change_proxy(value):
 
 def change_http(line, value):
     match_split = line.split(':')
-    if value == 'yes' and '# ' not in ''.join(match_split):
-        comment = match_split[0].split('h')
-        if len(comment) > 1:
-            match_split[0] = comment[0] + '# h' + comment[1]
-            value = 'no'
-    elif value == 'no':
+    if value == 'yes':
         comment = match_split[0].split('# ')
         if len(comment) > 1:
             match_split[0] = comment[0] + comment[1]
-            value = 'yes'
+    elif value == 'no' and '# ' not in ''.join(match_split):
+        comment = match_split[0].split('h')
+        if len(comment) > 1:
+            match_split[0] = comment[0] + '# h' + comment[1]
 
     print('[INFO] HTTP changed correctly to \'{}\''.format(value))
     return ':'.join(match_split)
 
 
-def change_https(value):
+def change_https(value, https=True):
     with open(UWSGI_CONFIG_PATH, 'r+') as f:
         lines = f.readlines()
 
     value = convert_boolean_to_string(value)
     # Disable basic auth
-    if value == 'yes':
+    if value == 'yes' and https:
         change_basic_auth('no')
 
     new_file = ''
@@ -188,9 +186,10 @@ def change_https(value):
         match = re.search(_uwsgi_socket, line)
         match_cert = re.search(_uwsgi_certs, line)
         match_http = re.search(_ip_host, line)
-        if match_http:
+        if match_http and not https:
             line = change_http(line, value)
-        if match or match_cert:
+            new_file += line
+        elif https and (match or match_cert):
             match_split = line.split(':')
             if value == 'yes':
                 comment = match_split[0].split('# ')
@@ -211,21 +210,18 @@ def change_https(value):
     if new_file != '':
         with open(UWSGI_CONFIG_PATH, 'w') as f:
             f.write(new_file)
-        print('[INFO] HTTPS changed correctly to \'{}\''.format(value))
+        if https:
+            print('[INFO] HTTPS changed correctly to \'{}\''.format(value))
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
-    subparser = parser.add_subparsers()
-    https = subparser.add_parser('https')
-
-    parser.add_argument('-p',  '--port',     help="Change port number",                          type=int)
-    parser.add_argument('-i',  '--ip',       help="Change the host IP",                          type=str)
-    parser.add_argument('-b',  '--basic',    help="Configure basic authentication (true/false)", type=str)
-    parser.add_argument('-bp', '--proxy',    help="Yes to run API behind a proxy",               type=str)
-    parser.add_argument('-s',  '--https',    help="Enable https protocol (true/false)",          type=str)
-
+    parser.add_argument('-p', '--port',     help="Change port number",                          type=int)
+    parser.add_argument('-i', '--ip',       help="Change the host IP",                          type=str)
+    parser.add_argument('-b', '--basic',    help="Configure basic authentication (true/false)", type=str)
+    parser.add_argument('-x', '--proxy',    help="Yes to run API behind a proxy",               type=str)
+    parser.add_argument('-t', '--http',     help="Enable http protocol (true/false)",           type=str)
+    parser.add_argument('-s', '--https',    help="Enable https protocol (true/false)",          type=str)
     args = parser.parse_args()
 
     if check_uwsgi_config() and len(sys.argv) > 1:
@@ -243,6 +239,13 @@ if __name__ == '__main__':
 
         if check_boolean('https', args.https):
             change_https(args.https)
+
+        if check_boolean('http', args.http):
+            if args.http.lower() == 'true' or args.http.lower() == 'yes':
+                args.http = 'yes'
+            elif args.http.lower() == 'false' or args.http.lower() == 'no':
+                args.http = 'no'
+            change_https(args.http, https=False)
     elif len(sys.argv) == 1:
         parser.print_help()
     else:
