0e947e5b5723ced5b388cbd63f2ee39986ee98f0
Jesús Jaldo <jesus.jalru@gmail.com>
2019-07-29 09:44:35 +0200

    Add all unit test for ossec_queue module

--- framework/wazuh/tests/test_ossec_queue.py
+++ framework/wazuh/tests/test_ossec_queue.py
@@ -68,3 +68,58 @@ def test_OssecQueue_protected_send_ko(mock_send, mock_conn):
         queue._send('msg')
 
     mock_conn.assert_called_with('test_path')
+
+
+@patch('wazuh.ossec_queue.socket.socket.connect')
+@patch('wazuh.ossec_queue.socket.socket.close')
+def test_OssecQueue_close(mock_close, mock_conn):
+    """Tests OssecQueue.close function works"""
+
+    queue = OssecQueue('test_path')
+
+    queue.close()
+
+    mock_conn.assert_called_once_with('test_path')
+    mock_close.assert_called_once_with()
+
+
+@pytest.mark.parametrize('msg, agent_id, msg_type', [
+    ('test_msg', '000', 'ar-message'),
+    ('test_msg', '001', 'ar-message'),
+    ('test_msg', None, 'ar-message'),
+    ('syscheck restart', '000', None),
+    ('restart-ossec0', '001', None),
+    ('syscheck restart', None, None),
+    ('restart-ossec0', None, None)
+])
+@patch('wazuh.ossec_queue.socket.socket.connect')
+@patch('wazuh.ossec_queue.OssecQueue._send')
+def test_OssecQueue_send_msg_to_agent(mock_send, mock_conn, msg, agent_id, msg_type):
+    """Tests OssecQueue.send_msg_to_agent function works"""
+
+    queue = OssecQueue('test_path')
+
+    response = queue.send_msg_to_agent(msg, agent_id, msg_type)
+
+    assert isinstance(response, str)
+    mock_conn.assert_called_once_with('test_path')
+
+
+@pytest.mark.parametrize('msg, agent_id, msg_type, expected_exception', [
+    ('test_msg', '000', 'ar-message', 1652),
+    ('test_msg', '000', None, 1012),
+    ('syscheck restart', '001', None, 1601),
+    ('syscheck restart', None, None, 1601),
+    ('restart-ossec0', None, None, 1702)
+])
+@patch('wazuh.ossec_queue.socket.socket.connect')
+@patch('wazuh.ossec_queue.OssecQueue._send', side_effect=Exception)
+def test_OssecQueue_send_msg_to_agent_ko(mock_send, mock_conn, msg, agent_id, msg_type, expected_exception):
+    """Tests OssecQueue.send_msg_to_agent function exception works"""
+
+    queue = OssecQueue('test_path')
+
+    with pytest.raises(WazuhException, match=f'.* {expected_exception} .*'):
+        queue.send_msg_to_agent(msg, agent_id, msg_type)
+
+    mock_conn.assert_called_once_with('test_path')
