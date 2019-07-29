6970c09ff60f6c1f8b66ecf64eeb94fd198fb4f1
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-07-29 13:10:00 +0200

    Add agent test environment

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -1,27 +1,27 @@
 import pytest
 import time
 import os
+import subprocess
 
-
-@pytest.fixture(name="agents_test", scope="session")
-def fix_test():
-    os.chdir("./agent_enviroment")
-    os.system("docker-compose up --build -d")
-    time.sleep(60)
-    print('Entorno configurado - Comienzan los test')
-    yield
-    print('Test finalizados')
-    os.system("docker-compose down")
-
-
-@pytest.fixture(name="agent_tests", scope="session")
-def fix_test():
-    here = os.path.abspath(os.path.dirname(__file__))
-    test_path = os.path.join(here, 'environment', 'agents', 'docker-compose.yml')
-    os.system("docker-compose -f {} up --build -d".format(test_path))
-    time.sleep(90)
-    yield
-    os.system("docker-compose -f {} down".format(test_path))
+# @pytest.fixture(name="agents_test", scope="session")
+# def fix_test():
+#     os.chdir("./agent_enviroment")
+#     os.system("docker-compose up --build -d")
+#     time.sleep(60)
+#     print('Entorno configurado - Comienzan los test')
+#     yield
+#     print('Test finalizados')
+#     os.system("docker-compose down")
+#
+#
+# @pytest.fixture(name="agent_tests", scope="session")
+# def fix_test():
+#     here = os.path.abspath(os.path.dirname(__file__))
+#     test_path = os.path.join(here, 'environment', 'agents', 'docker-compose.yml')
+#     os.system("docker-compose -f {} up --build -d".format(test_path))
+#     time.sleep(90)
+#     yield
+#     os.system("docker-compose -f {} down".format(test_path))
 
 
 @pytest.fixture(name="ciscat_tests", scope="session")
@@ -33,3 +33,21 @@ def fix_test():
     yield
     os.system("docker-compose -f {0} down".format(test_path))
 
+
+@pytest.fixture(name="agent_tests", scope="session")
+def fix_test():
+    here = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(here, 'environment', 'agent', 'docker-compose.yml')
+    os.system("docker-compose -f {0} up --build -d".format(test_path))
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        health = subprocess.check_output(
+            "docker inspect wazuh-cluster-agents_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if health.startswith(b'"healthy"'):
+            yield
+        else:
+            retries += 1
+    os.system("docker-compose -f {0} down".format(test_path))
