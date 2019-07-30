5d3411b43e22ca984571f4b3fb6d8d5eb0ed9469
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-07-30 13:49:55 +0200

    Add active-response environment dependency

--- api/test/integration/environment/default/wazuh-agent/entrypoint.sh
+++ api/test/integration/environment/default/wazuh-agent/entrypoint.sh
@@ -8,6 +8,11 @@ sed -n "/$2 /p" /var/ossec/etc/test.keys > /var/ossec/etc/client.keys
 chown root:ossec /var/ossec/etc/client.keys
 rm -rf /var/ossec/etc/test.keys
 
+# Disable active-response for agent 003
+if [ "X$2" == "Xwazuh-agent3" ]; then
+      sed -i "/<active-response>/{n;s/no/yes/}" /var/ossec/etc/ossec.conf
+fi
+
 sleep 1
 
 /var/ossec/bin/ossec-control start
