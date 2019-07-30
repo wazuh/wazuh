de4d1a72a5707a67fcec260371ca7a336ccd7d54
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-07-30 10:03:32 +0200

    Update dockerCI launcher environment

--- api/test/integration/Docker-CI/CI/entrypoint.sh
+++ api/test/integration/Docker-CI/CI/entrypoint.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-     
-pytest /wazuh/api/test/integration/test_ciscat_endpoints.tavern.yaml 
+#!/usr/bin/env bash
+
+sleep 5
+
+pytest -vv /wazuh/api/test/integration/test_agentsGET_endpoints.tavern.yaml
+pytest -vv /wazuh/api/test/integration/test_agentsPOST_endpoints.tavern.yaml
+pytest -vv /wazuh/api/test/integration/test_agentsPUT_endpoints.tavern.yaml
+pytest -vv /wazuh/api/test/integration/test_agentsDELETE_endpoints.tavern.yaml
+...
+...
