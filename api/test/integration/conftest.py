9bdeafe78334b8795cda257e6739d2495b1c7e56
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-07-31 11:39:22 +0200

    Integrate ciscat environment requirements in default

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -43,13 +43,40 @@ def fix_test():
     interval = 10  # seconds
     retries = 0
     while retries < max_retries:
-        print(str(retries))
         time.sleep(interval)
         health = subprocess.check_output(
             "docker inspect default_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
         if health.startswith(b'"healthy"'):
             yield
             retries = max_retries
+        else:
+            retries += 1
+    os.system("docker-compose -f {0} down".format(test_path))
+
+
+@pytest.fixture(name="ciscat_tests", scope="session")
+def fix_test():
+    here = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(here, 'environment', 'default', 'docker-compose.yml')
+    os.system("docker-compose -f {0} up --build -d".format(test_path))
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        master_health = subprocess.check_output(
+            "docker inspect default_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if master_health.startswith(b'"healthy"'):
+            agents_healthy = True
+            for i in [1, 2, 3]:
+                state_str = "'{{json .State.Health.Status}}'"
+                health = subprocess.check_output(f"docker inspect default_wazuh-agent{i}_1 -f {state_str}", shell=True)
+                if not health.startswith(b'"healthy"'):
+                    agents_healthy = False
+                    break
+            if agents_healthy is True:
+                yield
+                retries = max_retries
         else:
             retries += 1
     os.system("docker-compose -f {0} down".format(test_path))
