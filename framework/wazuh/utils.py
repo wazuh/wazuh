b26f76d7739af59a13d702a0e2e128eb8945cc7d
Pedro de Castro <1519428+snaow@users.noreply.github.com>
2019-08-08 00:08:52 +0200

    Framework move: Keep only one move instance

--- framework/wazuh/utils.py
+++ framework/wazuh/utils.py
@@ -357,11 +357,8 @@ def safe_move(source, target, ownership=(common.ossec_uid, common.ossec_gid), ti
     :param time: tuple in the form (addition_timestamp, modified_timestamp)
     :param permissions: string mask in octal notation. I.e.: '0o640'
     """
-    # Create temp file
-    #tmp_target = f"{target}.tmp"
-    #shutil.move(source, tmp_target, copy_function=shutil.copyfile)
-
-    move(source, target, copy_function=copyfile)
+    # Move file
+    shutil.move(source, target, copy_function=shutil.copyfile)
 
     # Set up metadata
     chown(target, *ownership)
