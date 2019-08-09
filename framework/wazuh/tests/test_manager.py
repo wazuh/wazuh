0613ac6027b5179dffda1d9b42cd6b5fdeded838
druizz90 <demetrio.ruiz@wazuh.com>
2019-08-09 14:15:04 +0200

    Fix unit tests (manager) for 3.11 version.

--- framework/wazuh/tests/test_manager.py
+++ framework/wazuh/tests/test_manager.py
@@ -559,12 +559,11 @@ multiline log
 ])
 @patch("wazuh.manager.previous_month", return_value=datetime.strptime('2019-03-01 00:00:00', '%Y-%m-%d %H:%M:%S'))
 def test_ossec_log(mock_month, test_manager, category, type_log, totalItems, sort):
-    """
-    Tests reading ossec.log file contents
-    """
+    """Test reading ossec.log file contents."""
     with patch('wazuh.manager.tail') as tail_patch:
         tail_patch.return_value = ossec_log_file.splitlines()
-        logs = ossec_log(category=category, type_log=type_log, sort=sort)
+        filters = {'category': category, 'type_log': type_log}
+        logs = ossec_log(filters=filters, sort=sort)
         assert logs['totalItems'] == totalItems
         assert all(log['description'][-1] != '\n' for log in logs['items'])
         if category != 'all' and category != 'wazuh-modulesd:syscollector':
