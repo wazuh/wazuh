6162dc33763159b8a0ecdfff73ab531eaa9d89d5
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-08-20 14:51:35 +0200

    Removed ciscat data

--- api/test/integration/env/configurations/ciscat/wazuh-agent-ciscat/ciscat/sce/find_ciscat.ps1
+++
@@ -1,6 +0,0 @@
-﻿$paths = Get-PSDrive -PSProvider "FileSystem" | ForEach-Object { Get-ChildItem -Path $_.Root -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -eq "CIS-CAT.bat" } | Select-Object -First 1 FullName } | Select-Object -First 1
-if ( $paths -eq $null ) {
-    Exit $Env:XCCDF_RESULT_FAIL
-}
-Write-Output $paths.FullName
-Exit $Env:XCCDF_RESULT_PASS
