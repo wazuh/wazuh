23063dc631a94a21453b805ec9421a3196d638ab
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-08-28 13:54:17 +0200

    Add config ondemand tests

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -17,26 +17,11 @@ def down_env(test_path: str):
     current_process.wait()
 
 
-@pytest.fixture(scope="session")
-def environment(request):
-    marks = request.node.own_markers
-    mark_names = [m.name for m in marks]
+@pytest.fixture(name="default_tests", scope="session")
+def environment_base():
     pwd = os.path.abspath(os.path.dirname(__file__))
     test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
-
-    env = 'base'
-    if 'base' in mark_names:
-        env = 'base'
-    elif 'security' in mark_names:
-        env = 'security'
-    elif 'manager' in mark_names:
-        env = 'manager'
-    elif 'syscollector' in mark_names:
-        env = 'syscollector'
-    elif 'cluster' in mark_names:
-        env = 'cluster'
-
-    build_and_up(test_path, env)
+    build_and_up(test_path, "base")
     max_retries = 30
     interval = 10  # seconds
     retries = 0
@@ -45,21 +30,117 @@ def environment(request):
         health = subprocess.check_output(
             "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
         if health.startswith(b'"healthy"'):
-            if env == 'ciscat':
-                agents_healthy = True
-                for i in [1, 2, 3]:
-                    state_str = "'{{json .State.Health.Status}}'"
-                    health = subprocess.check_output('docker inspect env_wazuh-agent{}_1 -f {}'.format(
-                        i, state_str), shell=True)
-                    if not health.startswith(b'"healthy"'):
-                        agents_healthy = False
-                        break
-                if agents_healthy is True:
-                    yield
-                    retries = max_retries
-            else:
+            yield
+            retries = max_retries
+        else:
+            retries += 1
+    down_env(test_path)
+
+
+@pytest.fixture(name="security_tests", scope="session")
+def environment_security():
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
+    build_and_up(test_path, "security")
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        health = subprocess.check_output(
+            "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if health.startswith(b'"healthy"'):
+            yield
+            retries = max_retries
+        else:
+            retries += 1
+    down_env(test_path)
+
+
+@pytest.fixture(name="manager_tests", scope="session")
+def environment_manager():
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
+    build_and_up(test_path, "manager")
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        health = subprocess.check_output(
+            "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if health.startswith(b'"healthy"'):
+            yield
+            retries = max_retries
+        else:
+            retries += 1
+    down_env(test_path)
+
+
+@pytest.fixture(name="cluster_tests", scope="session")
+def environment_cluster():
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
+    build_and_up(test_path, "cluster")
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        health = subprocess.check_output(
+            "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if health.startswith(b'"healthy"'):
+            yield
+            retries = max_retries
+        else:
+            retries += 1
+    down_env(test_path)
+
+
+@pytest.fixture(name="ciscat_tests", scope="session")
+def environment_ciscat():
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
+    build_and_up(test_path, "ciscat")
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        master_health = subprocess.check_output(
+            "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if master_health.startswith(b'"healthy"'):
+            agents_healthy = True
+            for i in [1, 2, 3]:
+                state_str = "'{{json .State.Health.Status}}'"
+                health = subprocess.check_output('docker inspect env_wazuh-agent{}_1 -f {}'.format(
+                    i, state_str), shell=True)
+                if not health.startswith(b'"healthy"'):
+                    agents_healthy = False
+                    break
+            if agents_healthy is True:
                 yield
                 retries = max_retries
+        else:
+            retries += 1
+    down_env(test_path)
+
+
+@pytest.fixture(name="syscollector_tests", scope="session")
+def environment_syscollector():
+    pwd = os.path.abspath(os.path.dirname(__file__))
+    test_path = os.path.join(pwd, 'env', 'docker-compose.yml')
+    build_and_up(test_path, "syscollector")
+    max_retries = 30
+    interval = 10  # seconds
+    retries = 0
+    while retries < max_retries:
+        time.sleep(interval)
+        health = subprocess.check_output(
+            "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        if health.startswith(b'"healthy"'):
+            yield
+            break
         else:
             retries += 1
     down_env(test_path)
