922c1d7323961831eebd04c92f6f2412d2b6251e
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-08-29 13:21:24 +0200

    General improvements

--- api/test/integration/env/base/wazuh-manager/entrypoint_manager.sh
+++ api/test/integration/env/base/wazuh-manager/entrypoint_manager.sh
@@ -3,14 +3,18 @@
 sed -i "s:<key></key>:<key>9d273b53510fef702b54a92e9cffc82e</key>:g" /var/ossec/etc/ossec.conf
 sed -i "s:<node>NODE_IP</node>:<node>$1</node>:g" /var/ossec/etc/ossec.conf
 sed -i -e "/<cluster>/,/<\/cluster>/ s|<disabled>[a-z]\+</disabled>|<disabled>no</disabled>|g" /var/ossec/etc/ossec.conf
-sed -i "s:<node_name>node01</node_name>:<node_name>$2</node_name>:g" /var/ossec/etc/ossec.conf
+sed -i "s:<node_name>NODE_NAME</node_name>:<node_name>$2</node_name>:g" /var/ossec/etc/ossec.conf
 
 # Add this to configure with nginx load balancer
 sed -i "s:<use_source_ip>yes</use_source_ip>:<use_source_ip>no</use_source_ip>:g" /var/ossec/etc/ossec.conf
 sed -i "s:<protocol>udp</protocol>:<protocol>tcp</protocol>:g" /var/ossec/etc/ossec.conf
 
 if [ "X$3" != "Xmaster" ]; then
-    sed -i "s:<node_type>master</node_type>:<node_type>worker</node_type>:g" /var/ossec/etc/ossec.conf
+    sed -i "s:<node_type>NODE_TYPE</node_type>:<node_type>worker</node_type>:g" /var/ossec/etc/ossec.conf
+else
+
+    sed -i "s:<node_type>NODE_TYPE</node_type>:<node_type>master</node_type>:g" /var/ossec/etc/ossec.conf
+
 fi
 
 sleep 1
