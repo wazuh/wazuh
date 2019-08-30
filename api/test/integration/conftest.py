8f91929f64aecbe94b0f2407878b234652c8ca08
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-08-30 11:11:59 +0200

    Add delay after healthcheck

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -30,15 +30,15 @@ def down_env(test_path: str):
 def check_health(interval=10, node_type='master', agents=None):
     time.sleep(interval)
     if node_type == 'master':
-        return subprocess.check_output(
+        health = subprocess.check_output(
             "docker inspect env_wazuh-master_1 -f '{{json .State.Health.Status}}'", shell=True)
+        return False if not health.startswith(b'"healthy"') else True
     elif node_type == 'agent':
         for agent in agents:
             health = subprocess.check_output(
                 "docker inspect env_wazuh-agent{}_1 -f '{{json .State.Health.Status}}'".format(agent), shell=True)
             if not health.startswith(b'"healthy"'):
                 return False
-
         return True
 
 
@@ -47,7 +47,8 @@ def environment_base():
     values = build_and_up("base")
     while values['retries'] < values['max_retries']:
         health = check_health()
-        if health.startswith(b'"healthy"'):
+        if health:
+            time.sleep(10)
             yield
             break
         else:
@@ -60,7 +61,8 @@ def environment_security():
     values = build_and_up("security")
     while values['retries'] < values['max_retries']:
         health = check_health()
-        if health.startswith(b'"healthy"'):
+        if health:
+            time.sleep(10)
             yield
             break
         else:
@@ -73,7 +75,8 @@ def environment_manager():
     values = build_and_up("manager")
     while values['retries'] < values['max_retries']:
         health = check_health()
-        if health.startswith(b'"healthy"'):
+        if health:
+            time.sleep(10)
             yield
             break
         else:
@@ -86,7 +89,8 @@ def environment_cluster():
     values = build_and_up("cluster")
     while values['retries'] < values['max_retries']:
         health = check_health()
-        if health.startswith(b'"healthy"'):
+        if health:
+            time.sleep(10)
             yield
             break
         else:
@@ -99,7 +103,8 @@ def environment_syscollector():
     values = build_and_up("syscollector")
     while values['retries'] < values['max_retries']:
         health = check_health()
-        if health.startswith(b'"healthy"'):
+        if health:
+            time.sleep(10)
             yield
             break
         else:
@@ -112,9 +117,10 @@ def environment_ciscat():
     values = build_and_up("ciscat")
     while values['retries'] < values['max_retries']:
         master_health = check_health()
-        if master_health.startswith(b'"healthy"'):
+        if master_health:
             agents_healthy = check_health(node_type='agent', agents=[1, 2, 3])
             if agents_healthy is True:
+                time.sleep(10)
                 yield
                 break
         else:
