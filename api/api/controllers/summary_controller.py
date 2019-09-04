7ccb7a99c5a0195e6209bae2a4954e55f0e5a23e
druizz90 <demetrio.ruiz@wazuh.com>
2019-09-04 08:48:03 +0200

    Update model for overview controller in spec.yaml.

--- api/api/controllers/summary_controller.py
+++ api/api/controllers/summary_controller.py
@@ -8,10 +8,11 @@ import connexion
 import datetime
 import logging
 
-from wazuh.agent import Agent as Agent
 from api.models.base_model_ import Data
-from api.util import remove_nones_to_dict, exception_handler, parse_api_param, raise_if_exc
+from api.util import (exception_handler, parse_api_param, raise_if_exc,
+                      remove_nones_to_dict)
 from wazuh import common
+from wazuh.agent import Agent
 from wazuh.cluster.dapi.dapi import DistributedAPI
 
 loop = asyncio.get_event_loop()
