f5beec186eaa7a32e5c6d564fbb7faef68f14970
Daniel Melgarejo Garcia <danimegar@gmail.com>
2019-09-17 12:02:59 +0200

    Added header, examples and directory input in mitredb.py script
    python mitredb.py -> install mitre.db in /var/ossec/var/db
    
    python mitredb.py -d /other/directory/mitre.db  -> install mitre.db in other directory
    
    python mitredb.py -h -> Help

--- tools/mitre/mitredb.py
+++ tools/mitre/mitredb.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2015-2019, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+#
+# Example:
+#
+# python mitredb.py -> install mitre.db in /var/ossec/var/db
+# python mitredb.py -d /other/directory/mitre.db  -> install mitre.db in other directory
+# python mitredb.py -h -> Help 
+
 import json
 import sqlite3
 from sqlite3 import Error
+import os
+import argparse
  
  
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
+    """ 
+    Create a database connection to the SQLite database specified by db_file.
+
+    :param db_file: Database file. Examples: ('example.db'), ("/var/ossec/var/db/example.db"), etc.
     :return: Connection object or None
     """
     conn = None
@@ -19,23 +34,27 @@ def create_connection(db_file):
     return conn
  
  
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
+def create_table(conn, table_sql):
+    """ 
+    Create o delete a table from the table_sql statement.
+
     :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
+    :param table_sql: a CREATE TABLE or DELETE TABLE statement
     :return:
     """
     try:
         c = conn.cursor()
-        c.execute(create_table_sql)
+        c.execute(table_sql)
     except Error as e:
         print(e)
 
 def insert_attack_table(conn, id, json_object):
-    """ insert to Mitre 'attack' table from id techniquec and json object 
+    """ 
+    Insert to Mitre 'attack' table from Mitre ID technique and its JSON object. 
+
     :param conn: Connection object
-    :param id: Mitre External id
-    :param value: It can be a json object, a phase (Persistence, Evasion, etc) or platform (Windows, Linux, macOs)
+    :param id: Mitre ID technique (e.g. 'T1122')
+    :param json_object: JSON object with ID 'id' taken from the JSON file
     :return:
     """
     attack_sql = """INSERT INTO attack ('id', 'json') VALUES (?, ?);"""
@@ -49,10 +68,12 @@ def insert_attack_table(conn, id, json_object):
         print(e)
 
 def insert_phase_table(conn, attack_id, phase_name):
-    """ insert to a table from the insert_sql statement
+    """ 
+    Insert to Mitre 'phase' table from Mitre ID technique and its phase/tactic. It is posible that one ID has more than one phase/tactic associated.
+
     :param conn: Connection object
-    :param id: Mitre External id
-    :param value: It can be a json object, a phase (Persistence, Evasion, etc) or platform (Windows, Linux, macOs)
+    :param id: Mitre ID technique (e.g. 'T1122')
+    :param phase_name: A phase/tactic name ('persistence ', 'privilege-escalation', etc)
     :return:
     """
     phase_sql = """INSERT INTO has_phase ('attack_id', 'phase_name') VALUES (?, ?);"""
@@ -66,10 +87,12 @@ def insert_phase_table(conn, attack_id, phase_name):
         print(e)
         
 def insert_platform_table(conn, attack_id, platform_name):
-    """ insert to a table from the insert_sql statement
+    """ 
+    Insert to Mitre 'plaftform' table from Mitre ID technique and its platform. It is posible that one ID has more than one platform associated.
+
     :param conn: Connection object
-    :param id: Mitre External id
-    :param value: It can be a json object, a phase (Persistence, Evasion, etc) or platform (Windows, Linux, macOs)
+    :param id: Mitre ID technique (e.g. 'T1122')
+    :param platform_name: A platform name (e.g. Windows, Linux, macOs)
     :return:
     """
     platform_sql = """INSERT INTO has_platform ('attack_id', 'platform_name') VALUES (?, ?);"""
@@ -83,8 +106,18 @@ def insert_platform_table(conn, attack_id, platform_name):
         print(e)           
  
  
-def main():
-    database = r"/var/ossec/var/db/mitre.db"
+def main(database=None):
+    """
+    This function create the mitre database in a chosen directory. It deletes, creates and fills the mitre tables.
+
+    :param database: Directory where mitre.db is. Default: /var/ossec/var/db/mitre.db
+    :return:
+    """
+    if database is None:
+        database = "/var/ossec/var/db/mitre.db"
+    else:
+        if not os.path.isdir('/'.join((str(database).split('/')[0:-1]))):
+            raise Exception('Error: Directory not found.')
 
     sql_delete_attack = """DROP TABLE IF EXISTS attack;"""
  
@@ -116,13 +149,13 @@ def main():
 
     # Delete tables
     if conn is not None:
-        # Delete attack table
+        # Delete attack table if exists
         create_table(conn, sql_delete_attack)
  
-        # Delete has_phase table
+        # Delete has_phase table if exists
         create_table(conn, sql_delete_has_phase)
 
-        # Delete has_platform table
+        # Delete has_platform table if exists
         create_table(conn, sql_delete_has_platform)
     else:
         print("Error! cannot create the database connection.")
@@ -141,7 +174,7 @@ def main():
         print("Error! cannot create the database connection.")
     
     # Parse enterprise-attack.json file:
-    with open('enterprise-attack.json') as json_file:
+    with open('../../etc/mitre/enterprise-attack.json') as json_file:
         data = json.load(json_file)
         for data_object in data['objects']:
             if data_object['type'] == 'attack-pattern' and data_object['external_references'][0]['source_name'] == 'mitre-attack':
@@ -165,4 +198,7 @@ def main():
     conn.close()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='This script installs mitre.db in a directory.')
+    parser.add_argument('--database', '-d', help='-d /your/directory/mitre.db (default: /var/ossec/var/db/mitre.db')
+    args = parser.parse_args()
+    main(args.database)
