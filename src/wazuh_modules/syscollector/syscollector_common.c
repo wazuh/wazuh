56ba70aacbac1619e5a2a83c0032370195df1d61
Pablo Navarro <pablo.navarro@wazuh.com>
2019-09-17 18:18:41 +0200

    Add process scan to syscollector capabilities in Mac OS agents (#3322)

--- src/wazuh_modules/syscollector/syscollector_common.c
+++ src/wazuh_modules/syscollector/syscollector_common.c
@@ -147,6 +147,8 @@ void* wm_sys_main(wm_sys_t *sys) {
                 sys_proc_linux(queue_fd, WM_SYS_LOCATION);
             #elif defined(WIN32)
                 sys_proc_windows(WM_SYS_LOCATION);
+            #elif defined(__MACH__)
+                sys_proc_mac(queue_fd, WM_SYS_LOCATION);
             #else
                 sys->flags.procinfo = 0;
                 mtwarn(WM_SYS_LOGTAG, "Running processes inventory is not available for this OS version.");
