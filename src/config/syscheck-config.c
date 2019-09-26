120fde0d89c109b1828478c6840fa34d2d69abdd
Vikman Fernandez-Castro <victor@wazuh.com>
2019-09-26 12:28:34 +0200

    Let Syscheck start although it finds unknown configuration elements

--- src/config/syscheck-config.c
+++ src/config/syscheck-config.c
@@ -1408,8 +1408,7 @@ int Read_Syscheck(const OS_XML *xml, XML_NODE node, void *configp, __attribute__
                 syscheck->send_delay = 1000000 / value;
             }
         } else {
-            merror(XML_INVELEM, node[i]->element);
-            return (OS_INVALID);
+            mwarn(XML_INVELEM, node[i]->element);
         }
     }
 
