1f616e3eb11d2f0af43d396697a7a709aa4775a5
Eva López Ruiz <17710550+Lopuiz@users.noreply.github.com>
2019-09-26 16:34:16 +0200

    check if MANAGER_IP is a valid IP or hostname (#3951)

--- src/headers/shared.h
+++ src/headers/shared.h
@@ -69,6 +69,7 @@
 #include <dirent.h>
 #include <ctype.h>
 #include <signal.h>
+#include <stdbool.h>
 
 /* The mingw32 builder used by travis.ci can't find glob.h
  * Yet glob must work on actual win32.
