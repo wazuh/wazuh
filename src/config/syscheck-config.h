2bb1cdb4e3a05a07ffd71623d57d1850e81a9d22
Vikman Fernandez-Castro <victor@wazuh.com>
2019-10-15 18:24:57 +0200

    Let FIM permission field be always a string.
    Keep 'perm' field in both UNIX and Windows platforms.

--- src/config/syscheck-config.h
+++ src/config/syscheck-config.h
@@ -197,7 +197,6 @@ typedef struct fim_entry_data {
     fim_event_mode mode;
     time_t last_event;
     const char * entry_type;
-    char * win_perm_mask;
     unsigned long int dev;
     unsigned int scanned;
     int options;
