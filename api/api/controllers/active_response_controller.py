b77539b0aecbd75f9107b8507d58bd9797babfa3
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-10-18 16:20:22 +0200

    General fixes active response

--- api/api/controllers/active_response_controller.py
+++ api/api/controllers/active_response_controller.py
@@ -19,18 +19,17 @@ logger = logging.getLogger('wazuh')
 
 
 @exception_handler
-def run_command(agent_id, pretty=False, wait_for_complete=False):
+def run_command(list_agents='*', pretty=False, wait_for_complete=False):
     """Runs an Active Response command on a specified agent
 
-    :param agent_id: Agent ID. All possible values since 000 onwards
+    :param list_agents: List of Agents IDs. All possible values since 000 onwards
     :param pretty: Show results in human-readable format
     :param wait_for_complete: Disable timeout response
     :return: message
     """
     # Get body parameters
     active_response_model = ActiveResponse.from_dict(connexion.request.get_json())
-    rbac = get_permissions(connexion.request.headers['Authorization'])
-    f_kwargs = {'rbac': rbac, **{'agent_list': [agent_id]}, **active_response_model.to_dict()}
+    f_kwargs = {**{'agent_list': list_agents}, **active_response_model.to_dict()}
 
     dapi = DistributedAPI(f=active_response.run_command,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -38,39 +37,11 @@ def run_command(agent_id, pretty=False, wait_for_complete=False):
                           is_async=False,
                           wait_for_complete=wait_for_complete,
                           pretty=pretty,
-                          logger=logger
-                          )
-    data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
-    response = Data(data)
-
-    return response, 200
-
-
-@exception_handler
-def run_command_all(list_agents=None, pretty=False, wait_for_complete=False):
-    """Runs an Active Response command on a specified agent
-
-    :param list_agents: List of agents
-    :param pretty: Show results in human-readable format
-    :param wait_for_complete: Disable timeout response
-    :return: message
-    """
-    # Get body parameters
-    active_response_model = ActiveResponse.from_dict(connexion.request.get_json())
-    func = active_response.run_command_all if list_agents is None else active_response.run_command
-    rbac = get_permissions(connexion.request.headers['Authorization'])
-    f_kwargs = {'rbac': rbac, 'agent_list': list_agents,  **active_response_model.to_dict()}
-
-    dapi = DistributedAPI(f=func,
-                          f_kwargs=remove_nones_to_dict(f_kwargs),
-                          request_type='distributed_master',
-                          is_async=False,
-                          wait_for_complete=wait_for_complete,
-                          pretty=pretty,
                           logger=logger,
-                          broadcasting=list_agents is None
+                          broadcasting=list_agents == '*',
+                          rbac_permissions=get_permissions(connexion.request.headers['Authorization'])
                           )
     data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
-    response = Data(data)
 
-    return response, 200
+    return data, 200
+
--- framework/wazuh/active_response.py
+++ framework/wazuh/active_response.py
@@ -4,9 +4,10 @@
 
 from wazuh import common
 from wazuh.core import active_response
+from wazuh.exception import WazuhException
 from wazuh.ossec_queue import OssecQueue
-from wazuh.rbac.decorators import expose_resources, list_handler_no_denied, list_handler_with_denied
-from wazuh.exception import WazuhException, create_exception_dic
+from wazuh.rbac.decorators import expose_resources
+from wazuh.results import AffectedItemsWazuhResult
 
 
 @expose_resources(actions=['active_response:command'], resources=['agent:id:{agent_list}'])
@@ -18,51 +19,20 @@ def run_command(agent_list=None, command=None, arguments=None, custom=False):
     a command name
     :param custom: Whether the specified command is a custom command or not
     :param arguments: Command arguments
-    :return: WazuhResult.
+    :return: AffectedItemsWazuhResult.
     """
     msg_queue = active_response.create_message(command=command, arguments=arguments, custom=custom)
     oq = OssecQueue(common.ARQUEUE)
-    affected_items = list()
-    failed_items = list()
+    result = AffectedItemsWazuhResult(none_msg='Could not send command to any agent',
+                                      some_msg='Could not send command to some agents',
+                                      all_msg='Command sent to all agents')
     for agent_id in agent_list:
         try:
             active_response.send_command(msg_queue, oq, agent_id)
-            affected_items.append(agent_id)
+            result.affected_items.append(agent_id)
+            result.total_affected_items += 1
         except WazuhException as e:
-            failed_items.append(create_exception_dic(agent_id, e))
+            result.add_failed_item(id_=agent_id, error=e)
     oq.close()
 
-    return {'affected_items': affected_items,
-            'failed_items': failed_items,
-            'str_priority': ['Command sent to all agents',
-                             'Could not send command to some agents',
-                             'Could not send command to any agent']}
-
-
-@expose_resources(actions=['active_response:command'], resources=['agent:id:*'])
-def run_command_all(agent_list=None, command=None, arguments=None, custom=False):
-    """Run AR command in a specific agent
-
-    :param agent_list: Run AR command in the agent.
-    :param command: Command running in the agent. If this value starts by !, then it refers to a script name instead of
-    a command name
-    :param custom: Whether the specified command is a custom command or not
-    :param arguments: Command arguments
-    :return: WazuhResult.
-    """
-    msg_queue = active_response.create_message(command=command, arguments=arguments, custom=custom)
-    oq = OssecQueue(common.ARQUEUE)
-    affected_items = list()
-    for agent_id in agent_list:
-        try:
-            active_response.send_command(msg_queue, oq, agent_id)
-            affected_items.append(agent_id)
-        except WazuhException:
-            pass
-    oq.close()
-
-    return {'affected_items': affected_items,
-            'failed_items': list(),
-            'str_priority': ['Command sent to shown agents',
-                             '',
-                             'Could not send command to any agent']}
+    return result
