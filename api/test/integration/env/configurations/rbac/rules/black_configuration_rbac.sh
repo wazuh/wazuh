506e38c4a9e52fc3e66b30b8ca483656fa29fc1c
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-10-31 12:06:18 +0100

    Remove unused command

--- api/test/integration/env/configurations/rbac/rules/black_configuration_rbac.sh
+++ api/test/integration/env/configurations/rbac/rules/black_configuration_rbac.sh
@@ -5,5 +5,3 @@ sed -i "s:    # policies = RBAChecker.run_testing():    policies = RBAChecker.ru
 permissions='[{"actions":["rules:read"],"resources":["rule:file:0010-rules_config.xml","rule:file:0015-ossec_rules.xml","rule:file:0610-win-ms_logs_rules.xml"],"effect":"deny"}]'
 awk -v var="${permissions}" '{sub(/testing_policies = \[\]/, "testing_policies = " var)}1' /var/ossec/framework/python/lib/python3.7/site-packages/wazuh-3.11.0-py3.7.egg/wazuh/rbac/auth_context.py >> /var/ossec/framework/python/lib/python3.7/site-packages/wazuh-3.11.0-py3.7.egg/wazuh/rbac/auth_context1.py
 cat /var/ossec/framework/python/lib/python3.7/site-packages/wazuh-3.11.0-py3.7.egg/wazuh/rbac/auth_context1.py > /var/ossec/framework/python/lib/python3.7/site-packages/wazuh-3.11.0-py3.7.egg/wazuh/rbac/auth_context.py
-
-/var/ossec/bin/ossec-control restart
