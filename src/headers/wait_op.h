6c55b6cc1f36843a7a9225ff097aec3537e57185
Vikman Fernandez-Castro <victor@wazuh.com>
2019-11-11 16:37:02 -0800

    Let Logcollector queue file rotation and keepalive messages.
    This will prevent socket output threads from stopping if the agent gets disconnected.

--- src/headers/wait_op.h
+++ src/headers/wait_op.h
@@ -15,4 +15,12 @@ void os_setwait(void);
 void os_delwait(void);
 void os_wait(void);
 
+/**
+ * @brief Check whether the agent wait mark is on (manager is disconnected)
+ *
+ * @retval true The agent is blocked.
+ * @retval false The agent is not blocked.
+ */
+bool os_iswait();
+
 #endif /* WAIT_OP_H */
