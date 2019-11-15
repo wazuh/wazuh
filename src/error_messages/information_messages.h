08b031195e43bb0c6b512449a1ecd9ec53ca306d
Borja Arroba <borja.arroba@wazuh.com>
2019-11-15 09:45:31 +0100

    Changed fim audit message from info to debug

--- src/error_messages/information_messages.h
+++ src/error_messages/information_messages.h
@@ -21,11 +21,9 @@
 #define FIM_WAITING_QUEUE                   "(6005): Cannot connect to queue '%s' (%d)'%s'. Waiting %d seconds to reconnect."
 #define FIM_PRINT_IGNORE_ENTRY              "(6206): Ignore '%s' entry '%s'"
 #define FIM_PRINT_IGNORE_SREGEX             "(6207): Ignore '%s' sregex '%s'"
-
 #define FIM_FREQUENCY_STARTED               "(6008): File integrity monitoring scan started."
 #define FIM_FREQUENCY_ENDED                 "(6009): File integrity monitoring scan ended."
 #define FIM_FREQUENCY_TIME                  "(6010): File integrity monitoring scan frequency: %d seconds"
-
 #define FIM_REALTIME_STARTING               "(6011): Initializing real time file monitoring engine."
 #define FIM_REALTIME_STARTED                "(6012): Real-time file integrity monitoring started."
 #define FIM_REALTIME_PAUSED                 "(6013): Real-time file integrity monitoring paused."
@@ -38,22 +36,18 @@
 #define FIM_WHODATA_READDED                 "(6020): '%s' has been re-added. It will be monitored in real-time Whodata mode."
 #define FIM_WHODATA_SACL_CHANGED            "(6021): The SACL of '%s' has been modified and it is not valid for the real-time Whodata mode. Whodata will not be available for this file."
 #define FIM_WHODATA_DELETE                  "(6022): '%s' has been deleted. It will not be monitored in real-time Whodata mode."
-
 #define FIM_AUDIT_NOSOCKET                  "(6023): No socket found at '%s'. Restarting Auditd service."
 #define FIM_AUDIT_SOCKET                    "(6024): Generating Auditd socket configuration file: '%s'"
 #define FIM_AUDIT_RESTARTING                "(6025): Audit plugin configuration (%s) was modified. Restarting Auditd service."
 #define FIM_AUDIT_HEALTHCHECK_DISABLE       "(6026): Audit health check is disabled. Real-time Whodata could not work correctly."
 #define FIM_AUDIT_REMOVE_RULE               "(6027): Monitored directory '%s' was removed: Audit rule removed."
-#define FIM_AUDIT_INVALID_AUID              "(6028): Audit: Invalid 'auid' value read. Check Audit configuration (PAM)."
+
 #define FIM_AUDIT_RECONNECT                 "(6029): Audit: reconnecting... (%i)"
 #define FIM_AUDIT_CONNECT                   "(6030): Audit: connected."
-
 #define FIM_WINREGISTRY_START               "(6031): Registry integrity monitoring scan started"
 #define FIM_WINREGISTRY_ENDED               "(6032): Registry integrity monitoring scan ended"
-
 #define FIM_LINKCHECK_START                 "(6033): Starting symbolic link updater. Interval '%d'."
 #define FIM_LINKCHECK_CHANGED               "(6034): Updating the symbolic link from '%s': '%s' to '%s'."
-
 #define FIM_WHODATA_VOLUMES                 "(6036): Analyzing Windows volumes"
 
 #endif /* INFO_MESSAGES_H */
