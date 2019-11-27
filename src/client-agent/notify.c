c0c30c008b2d2e70f16b819a5879666dd4ea3e54
Vikman Fernandez-Castro <victor@wazuh.com>
2019-11-26 19:57:23 -0800

    Let Agentd attempt to get the agent IP once per notification
    10-second sleep per notification impacts:
    - Notification interval.
    - Requests (WPK, remote configuration and Syscheck synchronization).
    - Active Response.
    - Merged file reception.

--- src/client-agent/notify.c
+++ src/client-agent/notify.c
@@ -134,34 +134,23 @@ void run_notify()
     char *agent_ip;
     int sock;
     char label_ip[60];
-    int i;
     os_calloc(IPSIZE + 1,sizeof(char),agent_ip);
 
-    for (i = SOCK_ATTEMPTS; i > 0; --i) {
-        if (sock = control_check_connection(), sock >= 0) {
-            if (OS_SendUnix(sock, agent_ip, IPSIZE) < 0) {
-                merror("Error sending msg to control socket (%d) %s", errno, strerror(errno));
-            }
-            else{
-                if(OS_RecvUnix(sock, IPSIZE, agent_ip) == 0){
-                    merror("Error receiving msg from control socket (%d) %s", errno, strerror(errno));
-                    *agent_ip = '\0';
-                }
-                else{
-                    snprintf(label_ip,sizeof label_ip,"#\"_agent_ip\":%s", agent_ip);
-                }
-            }
-
-            close(sock);
-            break;
+    if (sock = control_check_connection(), sock >= 0) {
+        if (OS_SendUnix(sock, agent_ip, IPSIZE) < 0) {
+            merror("Error sending msg to control socket (%d) %s", errno, strerror(errno));
         } else {
-            mdebug2("Control module not yet available. Remaining attempts: %d", i - 1);
-            sleep(1);
+            if (OS_RecvUnix(sock, IPSIZE, agent_ip) == 0) {
+                merror("Error receiving msg from control socket (%d) %s", errno, strerror(errno));
+                *agent_ip = '\0';
+            } else {
+                snprintf(label_ip,sizeof label_ip,"#\"_agent_ip\":%s", agent_ip);
+            }
         }
-    }
 
-    if(sock < 0) {
-        merror("Unable to bind to socket '%s': (%d) %s.", CONTROL_SOCK, errno, strerror(errno));
+        close(sock);
+    } else {
+        mdebug2("Control module is not yet available. Check that wazuh-modulesd is running.");
     }
 
     /* Create message */
