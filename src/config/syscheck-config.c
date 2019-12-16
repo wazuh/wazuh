ce6e2287ad9b4a23b7b88c05dd451bb8a3821e37
nicolas <nicolas@nicolas-Ubuntu19.04>
2019-12-16 14:44:59 -0300

    Remove queue_size and change max_interval

--- src/config/syscheck-config.c
+++ src/config/syscheck-config.c
@@ -813,7 +813,7 @@ out_free:
 static void parse_inventory(syscheck_config * syscheck, XML_NODE node) {
     const char *xml_enabled = "enabled";
     const char *xml_sync_interval = "interval";
-    const char *xml_max_sync_interval = "max_sync_interval";
+    const char *xml_max_sync_interval = "max_interval";
     const char *xml_response_timeout = "response_timeout";
     const char *xml_sync_queue_size = "queue_size";
 
