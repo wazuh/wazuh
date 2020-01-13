0c39b20cdcbd848352c3dcc36596d4b562f7fa59
Nicolas Papp <nicolaspapp@gmail.com>
2020-01-13 15:45:28 -0300

    Remove tab in wdb_agent.c

--- src/wazuh_db/wdb_agent.c
+++ src/wazuh_db/wdb_agent.c
@@ -438,7 +438,7 @@ int wdb_find_agent(const char *name, const char *ip) {
 
     sqlite3_bind_text(stmt, 1, name, -1, NULL);
     sqlite3_bind_text(stmt, 2, ip, -1, NULL);
-    
+
     if (result = wdb_step(stmt), result == SQLITE_ROW) {
         result = sqlite3_column_int(stmt, 0);
     } else if (result == SQLITE_DONE) {
