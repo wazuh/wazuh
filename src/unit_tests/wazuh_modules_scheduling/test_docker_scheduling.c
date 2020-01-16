c0f5b60d128814d7478ec770346005f6ebd35437
Nicolas Papp <nicolaspapp@gmail.com>
2020-01-16 17:12:24 -0300

    Refactor on docker tests

--- src/unit_tests/wazuh_modules_scheduling/test_docker_scheduling.c
+++ src/unit_tests/wazuh_modules_scheduling/test_docker_scheduling.c
@@ -14,7 +14,9 @@
 
 #define TEST_MAX_DATES 5
 
-static wmodule docker_module;
+static wmodule *docker_module;
+static OS_XML *lxml;
+
 static unsigned test_docker_date_counter = 0;
 static struct tm test_docker_date_storage[TEST_MAX_DATES];
 
@@ -22,16 +24,23 @@ int __wrap_wpclose(wfd_t * wfd) {
     if (wfd->file) {
         fclose(wfd->file);
     }
+    free(wfd);
     time_t current_time = time(NULL);
     struct tm *date = localtime(&current_time);
     test_docker_date_storage[test_docker_date_counter++] = *date;
     if(test_docker_date_counter >= TEST_MAX_DATES){
-        const wm_docker_t *ptr = (wm_docker_t *) docker_module.data;
+        const wm_docker_t *ptr = (wm_docker_t *) docker_module->data;
         check_function_ptr( &ptr->scan_config, &test_docker_date_storage[0], TEST_MAX_DATES);
         // Break infinite loop
         disable_forever_loop();
     }
     return 0;
+}
+
+wfd_t * __wrap_wpopenl(const char * path, int flags, ...) {
+    wfd_t * wfd;
+    os_calloc(1, sizeof(wfd_t), wfd);
+    return wfd;
 }
 
 char *__wrap_fgets (char *__restrict __s, int __n, FILE *__restrict __stream) {
@@ -47,11 +56,31 @@ static void set_up_test(void (*ptr)(const sched_scan_config *scan_config, struct
     check_function_ptr = ptr;
 }
 
-static void run_test_string(const char *string){
-    OS_XML lxml;
-    XML_NODE nodes = string_to_xml_node(string, &lxml);
-    assert_int_equal(wm_docker_read(nodes, &docker_module),0);
-    docker_module.context->start( (wm_docker_t *) docker_module.data);
+static void wmodule_cleanup(wmodule *module){
+    free(module->data);
+    free(module->tag);
+    free(module);
+}
+
+/***  SETUPS/TEARDOWNS  ******/
+static int setup_module() {
+    docker_module = calloc(1, sizeof(wmodule));
+    const char *string = 
+        "<interval>10m</interval>\n"
+        "<attempts>10</attempts>\n"
+        "<run_on_start>no</run_on_start>\n"
+        "<disabled>no</disabled>\n";
+    lxml = malloc(sizeof(OS_XML));
+    XML_NODE nodes = string_to_xml_node(string, lxml);
+    assert_int_equal(wm_docker_read(nodes, docker_module), 0);
+    OS_ClearNode(nodes);
+    return 0;
+}
+
+static int teardown_module(){
+    wmodule_cleanup(docker_module);
+    OS_ClearXML(lxml);
+    return 0;
 }
 
 /****************************************************************/
@@ -59,47 +88,52 @@ static void run_test_string(const char *string){
 /** Tests **/
 void test_interval_execution() {
     set_up_test(check_time_interval);
-    const char *string = 
-        "<interval>10m</interval>\n"
-        "<attempts>10</attempts>\n"
-        "<run_on_start>no</run_on_start>\n"
-        "<disabled>no</disabled>\n";
-    run_test_string(string);
+    wm_docker_t* module_data = (wm_docker_t *)docker_module->data;
+    module_data->scan_config.last_scan_time = 0;
+    module_data->scan_config.scan_day = 0;
+    module_data->scan_config.scan_wday = -1;
+    module_data->scan_config.interval = 60 * 25; // 25min
+    module_data->scan_config.month_interval = false;
+    docker_module->context->start(module_data);
 }
 
 void test_day_of_month() {
     set_up_test(check_day_of_month);
-    const char *string = 
-        "<day>11</day>\n"
-        "<time>12:30</time>\n"
-        "<interval>10m</interval>\n"
-        "<attempts>10</attempts>\n"
-        "<run_on_start>no</run_on_start>\n"
-        "<disabled>no</disabled>\n";
-    run_test_string(string);
+    wm_docker_t* module_data = (wm_docker_t *)docker_module->data;
+    module_data->scan_config.last_scan_time = 0;
+    module_data->scan_config.scan_day = 27;
+    module_data->scan_config.scan_wday = -1;
+    module_data->scan_config.scan_time = strdup("00:00");
+    module_data->scan_config.interval = 1; // 1 month
+    module_data->scan_config.month_interval = true;
+    docker_module->context->start(module_data);
+    free(module_data->scan_config.scan_time);
 }
 
 void test_day_of_week() {
     set_up_test(check_day_of_week);
-    const char *string =
-        "<wday>Monday</wday>\n"
-        "<time>10:00</time>\n"
-        "<interval>10m</interval>\n"
-        "<attempts>10</attempts>\n"
-        "<run_on_start>no</run_on_start>\n"
-        "<disabled>no</disabled>\n";
-    run_test_string(string);
+    wm_docker_t* module_data = (wm_docker_t *)docker_module->data;
+    module_data->scan_config.last_scan_time = 0;
+    module_data->scan_config.scan_day = 0;
+    module_data->scan_config.scan_wday = 0;
+    module_data->scan_config.scan_time = strdup("00:00");
+    module_data->scan_config.interval = 604800;  // 1 week
+    module_data->scan_config.month_interval = false;
+    docker_module->context->start(module_data);
+    free(module_data->scan_config.scan_time);
 }
 
 void test_time_of_day() {
     set_up_test(check_time_of_day);
-    const char *string =
-        "<time>19:55</time>\n"
-        "<interval>10m</interval>\n"
-        "<attempts>10</attempts>\n"
-        "<run_on_start>no</run_on_start>\n"
-        "<disabled>no</disabled>\n";
-    run_test_string(string);
+    wm_docker_t* module_data = (wm_docker_t *)docker_module->data;
+    module_data->scan_config.last_scan_time = 0;
+    module_data->scan_config.scan_day = 0;
+    module_data->scan_config.scan_wday = -1;
+    module_data->scan_config.scan_time = strdup("00:00");
+    module_data->scan_config.interval = WM_DEF_INTERVAL;  // 1 day
+    module_data->scan_config.month_interval = false;
+    docker_module->context->start(module_data);
+    free(module_data->scan_config.scan_time);
 }
 
 void test_fake_tag() {
@@ -111,19 +145,125 @@ void test_fake_tag() {
         "<run_on_start>no</run_on_start>\n"
         "<disabled>no</disabled>\n"
         "<extra-tag>extra</extra-tag>\n";
-    OS_XML lxml;
-    XML_NODE nodes = string_to_xml_node(string, &lxml);
-    assert_int_equal(wm_docker_read(nodes, &docker_module),-1);
+    wmodule *module = calloc(1, sizeof(wmodule));
+    OS_XML xml;
+    XML_NODE nodes = string_to_xml_node(string, &xml);
+    assert_int_equal(wm_docker_read(nodes, module),-1);
+    OS_ClearNode(nodes);
+    OS_ClearXML(&xml);
+    wm_docker_t* module_data = (wm_docker_t *)module->data;
+    free(module_data->scan_config.scan_time);
+    wmodule_cleanup(module);
 }
 
+void test_read_scheduling_monthday_configuration() {
+    const char *string = 
+        "<time>19:55</time>\n"
+        "<day>10</day>\n"
+        "<attempts>10</attempts>\n"
+        "<run_on_start>no</run_on_start>\n"
+        "<disabled>no</disabled>\n"
+    ;
+    wmodule *module = calloc(1, sizeof(wmodule));;
+    OS_XML xml;
+    XML_NODE nodes = string_to_xml_node(string, &xml);
+    assert_int_equal(wm_docker_read(nodes, module),0);
+    wm_docker_t *module_data = (wm_docker_t*)module->data;
+    assert_int_equal(module_data->scan_config.scan_day, 10);
+    assert_int_equal(module_data->scan_config.interval, 1);
+    assert_int_equal(module_data->scan_config.month_interval, true);
+    assert_int_equal(module_data->scan_config.scan_wday, -1);
+    assert_string_equal(module_data->scan_config.scan_time, "19:55");
+    OS_ClearNode(nodes);
+    OS_ClearXML(&xml);
+    free(module_data->scan_config.scan_time);
+    wmodule_cleanup(module);
+}
+
+void test_read_scheduling_weekday_configuration() {
+    const char *string = 
+        "<time>18:55</time>\n"
+        "<wday>Thursday</wday>\n"
+        "<attempts>10</attempts>\n"
+        "<run_on_start>no</run_on_start>\n"
+        "<disabled>no</disabled>\n"
+    ;
+    wmodule *module = calloc(1, sizeof(wmodule));
+    OS_XML xml;
+    XML_NODE nodes = string_to_xml_node(string, &xml);
+    assert_int_equal(wm_docker_read(nodes, module),0);
+    wm_docker_t *module_data = (wm_docker_t*)module->data;
+    assert_int_equal(module_data->scan_config.scan_day, 0);
+    assert_int_equal(module_data->scan_config.interval, 604800);
+    assert_int_equal(module_data->scan_config.month_interval, false);
+    assert_int_equal(module_data->scan_config.scan_wday, 4);
+    assert_string_equal(module_data->scan_config.scan_time, "18:55");
+    OS_ClearNode(nodes);
+    OS_ClearXML(&xml);
+    free(module_data->scan_config.scan_time);
+    wmodule_cleanup(module);
+}
+
+void test_read_scheduling_daytime_configuration() {
+    const char *string = 
+        "<time>17:20</time>\n"
+        "<attempts>10</attempts>\n"
+        "<run_on_start>no</run_on_start>\n"
+        "<disabled>no</disabled>\n"
+    ;
+    wmodule *module = calloc(1, sizeof(wmodule));;
+    OS_XML xml;
+    XML_NODE nodes = string_to_xml_node(string, &xml);
+    assert_int_equal(wm_docker_read(nodes, module),0);
+    wm_docker_t *module_data = (wm_docker_t*)module->data;
+    assert_int_equal(module_data->scan_config.scan_day, 0);
+    assert_int_equal(module_data->scan_config.interval, WM_DEF_INTERVAL);
+    assert_int_equal(module_data->scan_config.month_interval, false);
+    assert_int_equal(module_data->scan_config.scan_wday, -1);
+    assert_string_equal(module_data->scan_config.scan_time, "17:20");
+    OS_ClearNode(nodes);
+    OS_ClearXML(&xml);
+    free(module_data->scan_config.scan_time);
+    wmodule_cleanup(module);
+}
+
+void test_read_scheduling_interval_configuration() {
+    const char *string = 
+        "<interval>1d</interval>\n"
+        "<attempts>10</attempts>\n"
+        "<run_on_start>no</run_on_start>\n"
+        "<disabled>no</disabled>\n"
+    ;
+    wmodule *module = calloc(1, sizeof(wmodule));;
+    OS_XML xml;
+    XML_NODE nodes = string_to_xml_node(string, &xml);
+    assert_int_equal(wm_docker_read(nodes, module),0);
+    wm_docker_t *module_data = (wm_docker_t*)module->data;
+    assert_int_equal(module_data->scan_config.scan_day, 0);
+    assert_int_equal(module_data->scan_config.interval, WM_DEF_INTERVAL); // 1 day
+    assert_int_equal(module_data->scan_config.month_interval, false);
+    assert_int_equal(module_data->scan_config.scan_wday, -1);
+    OS_ClearNode(nodes);
+    OS_ClearXML(&xml);
+    wmodule_cleanup(module);
+}
 
 int main(void) {
-    const struct CMUnitTest tests[] = {
+    const struct CMUnitTest tests_with_startup[] = {
         cmocka_unit_test(test_interval_execution),
         cmocka_unit_test(test_day_of_month),
         cmocka_unit_test(test_day_of_week),
-        cmocka_unit_test(test_time_of_day),
-        cmocka_unit_test(test_fake_tag)
+        cmocka_unit_test(test_time_of_day)
     };
-    return cmocka_run_group_tests(tests, NULL, NULL);
+    const struct CMUnitTest tests_without_startup[] = {
+        cmocka_unit_test(test_fake_tag),
+        cmocka_unit_test(test_read_scheduling_monthday_configuration),
+        cmocka_unit_test(test_read_scheduling_weekday_configuration),
+        cmocka_unit_test(test_read_scheduling_daytime_configuration),
+        cmocka_unit_test(test_read_scheduling_interval_configuration)
+    };
+    int result;
+    result = cmocka_run_group_tests(tests_with_startup, setup_module, teardown_module);
+    result &= cmocka_run_group_tests(tests_without_startup, NULL, NULL);
+    return result;
 }
