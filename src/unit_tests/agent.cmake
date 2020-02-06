196b0121cb43e7a5c2be578d0de6afb3a279e472
Mauro Moltrasio <mauro.ezequiel@wazuh.com>
2020-02-06 13:02:25 +0100

    Adds agent unit tests compilation configuration

---
+++ src/unit_tests/agent.cmake
@@ -0,0 +1,12 @@
+# Find the wazuh shared library
+find_library(WAZUHEXT NAMES libwazuhext.so HINTS "${SRC_FOLDER}")
+
+if(NOT WAZUHEXT)
+    message(FATAL_ERROR "libwazuhext not found! Aborting...")
+endif()
+
+# Add compiling flags
+add_compile_options(-ggdb -O0 -g -coverage -DTEST_AGENT)
+
+# Set tests dependencies
+set(TEST_DEPS SYSCHECK_O -lcmocka -fprofile-arcs -ftest-coverage)
