47b7434762b7859a60e3f74d60bddc15ba38780f
Tomas Turina <tomas.turina@wazuh.com>
2020-02-13 18:57:20 -0300

    Some fixes in tests

--- src/unit_tests/test_create_db.c
+++ src/unit_tests/test_create_db.c
@@ -293,6 +293,8 @@ static int setup_group(void **state) {
     syscheck.max_depth = 256;
     syscheck.file_max_size = 1024;
 
+    nowDebug();
+
     return 0;
 }
 
--- src/unit_tests/test_seechanges.c
+++ src/unit_tests/test_seechanges.c
@@ -19,6 +19,19 @@
 
 /* redefinitons/wrapping */
 
+/* Setup/teardown */
+
+static int setup_group(void **state) {
+    (void) state;
+    Read_Syscheck_Config("test_syscheck.conf");
+    return 0;
+}
+
+static int teardown_group(void **state) {
+    (void) state;
+    Free_Syscheck(&syscheck);
+    return 0;
+}
 
 /* tests */
 
@@ -26,8 +39,6 @@ void test_is_nodiff_true(void **state)
 {
     (void) state;
     int ret;
-
-    Read_Syscheck_Config("test_syscheck.conf");
 
     const char * file_name = "/etc/ssl/private.key";
 
@@ -42,8 +53,6 @@ void test_is_nodiff_false(void **state)
     (void) state;
     int ret;
 
-    Read_Syscheck_Config("test_syscheck.conf");
-
     const char * file_name = "/dummy_file.key";
 
     ret = is_nodiff(file_name);
@@ -56,8 +65,6 @@ void test_is_nodiff_regex_true(void **state)
 {
     (void) state;
     int ret;
-
-    Read_Syscheck_Config("test_syscheck.conf");
 
     const char * file_name = "file.test";
 
@@ -72,8 +79,6 @@ void test_is_nodiff_regex_false(void **state)
     (void) state;
     int ret;
 
-    Read_Syscheck_Config("test_syscheck.conf");
-
     const char * file_name = "test.file";
 
     ret = is_nodiff(file_name);
@@ -86,7 +91,21 @@ void test_is_nodiff_no_nodiff(void **state)
 {
     (void) state;
     int ret;
+    int i;
 
+    if (syscheck.nodiff) {
+        for (i=0; syscheck.nodiff[i] != NULL; i++) {
+            free(syscheck.nodiff[i]);
+        }
+        free(syscheck.nodiff);
+    }
+    if (syscheck.nodiff_regex) {
+        for (i=0; syscheck.nodiff_regex[i] != NULL; i++) {
+            OSMatch_FreePattern(syscheck.nodiff_regex[i]);
+            free(syscheck.nodiff_regex[i]);
+        }
+        free(syscheck.nodiff_regex);
+    }
     syscheck.nodiff = NULL;
     syscheck.nodiff_regex = NULL;
 
@@ -107,5 +126,5 @@ int main(void) {
         cmocka_unit_test(test_is_nodiff_no_nodiff),
     };
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+    return cmocka_run_group_tests(tests, setup_group, teardown_group);
 }
