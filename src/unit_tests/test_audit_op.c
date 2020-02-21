d9f750ff1fdaddcf899e3528cf025da54db54fc2
Tomas Turina <tomas.turina@wazuh.com>
2020-02-21 11:02:47 -0300

    Add test_audit_print_reply test

--- src/unit_tests/test_audit_op.c
+++ src/unit_tests/test_audit_op.c
@@ -18,6 +18,8 @@
 #include "../headers/audit_op.h"
 #include "../headers/defs.h"
 
+extern w_audit_rules_list *_audit_rules_list;
+
 /* auxiliary structs */
 
 typedef struct __audit_replies {
@@ -29,6 +31,17 @@ typedef struct __audit_replies {
 /* redefinitons/wrapping */
 
 void __wrap__merror(const char * file, int line, const char * func, const char *msg, ...) {
+    char formatted_msg[OS_MAXSTR];
+    va_list args;
+
+    va_start(args, msg);
+    vsnprintf(formatted_msg, OS_MAXSTR, msg, args);
+    va_end(args);
+
+    check_expected(formatted_msg);
+}
+
+void __wrap__mdebug2(const char * file, int line, const char * func, const char *msg, ...) {
     char formatted_msg[OS_MAXSTR];
     va_list args;
 
@@ -107,6 +120,33 @@ static int test_teardown_kernel_get_reply(void **state) {
     return 0;
 }
 
+static int test_setup_print_reply(void **state) {
+    struct audit_reply *reply = calloc(1, sizeof(struct audit_reply));
+
+    reply->type = AUDIT_LIST_RULES;
+    reply->ruledata = calloc(1, sizeof(struct audit_rule_data));
+    reply->ruledata->field_count = 4;
+    reply->ruledata->fields[0] = AUDIT_DIR;
+    reply->ruledata->values[0] = 0;
+    reply->ruledata->fields[1] = AUDIT_FILTERKEY;
+    reply->ruledata->values[1] = 0;
+    reply->ruledata->fields[3] = AUDIT_PERM;
+    reply->ruledata->values[3] = AUDIT_PERM_EXEC | AUDIT_PERM_WRITE | AUDIT_PERM_READ | AUDIT_PERM_ATTR;
+
+    *state = reply;
+
+    return 0;
+}
+
+static int test_teardown_print_reply(void **state) {
+    struct audit_reply *reply = *state;
+
+    free(reply->ruledata);
+    free(reply);
+
+    return 0;
+}
+
 /* tests */
 
 static void test_audit_get_rule_list_error(void **state) {
@@ -173,12 +213,27 @@ static void test_kernel_get_reply(void **state) {
     errno = 0;
 }
 
+static void test_audit_print_reply(void **state) {
+    struct audit_reply *reply = *state;
+
+    expect_string(__wrap__mdebug2, formatted_msg, "Audit rule loaded: -w  -p rwxa -k ");
+
+    int ret = audit_print_reply(reply);
+
+    assert_int_equal(ret, 1);
+    assert_non_null(_audit_rules_list->list[0]);
+    assert_string_equal(_audit_rules_list->list[0]->path, "");
+    assert_string_equal(_audit_rules_list->list[0]->key, "");
+    assert_string_equal(_audit_rules_list->list[0]->perm, "rwxa");
+}
+
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_audit_get_rule_list_error),
         cmocka_unit_test(test_audit_get_rule_list_success),
         cmocka_unit_test_setup_teardown(test_kernel_get_reply, test_setup_kernel_get_reply, test_teardown_kernel_get_reply),
+        cmocka_unit_test_setup_teardown(test_audit_print_reply, test_setup_print_reply, test_teardown_print_reply),
     };
     return cmocka_run_group_tests(tests, NULL, group_teardown);
 }
