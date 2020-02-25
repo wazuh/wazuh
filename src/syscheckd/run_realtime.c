9e1c4843df6138cba737e7aac346b32743f0c7ee
Vikman Fernandez-Castro <victor@wazuh.com>
2020-02-25 09:56:04 +0100

    Let FIM parameter rt_delay apply on both realtime and whodata

--- src/syscheckd/run_realtime.c
+++ src/syscheckd/run_realtime.c
@@ -177,12 +177,6 @@ void realtime_process()
             }
         }
 
-        /* Need a sleep here to avoid triggering on vim
-         * (and finding the file removed)
-         */
-        struct timeval timeout = {0, syscheck.rt_delay * 1000};
-        select(0, NULL, NULL, NULL, &timeout);
-
         char ** paths = rbtree_keys(tree);
 
         for (int i = 0; paths[i] != NULL; i++) {
@@ -283,8 +277,6 @@ void CALLBACK RTCallBack(DWORD dwerror, DWORD dwBytes, LPOVERLAPPED overlap)
 
             int index = fim_configuration_directory(wdchar, "file");
             int file_index = fim_configuration_directory(final_path, "file");
-
-            Sleep(syscheck.rt_delay);
 
             if (index == file_index) {
                 item->mode = FIM_REALTIME;
