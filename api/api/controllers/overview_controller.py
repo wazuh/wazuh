2c725ebb66c46f34653b1f88359d726d97341162
Daniel Ruiz <daniel.ruiz@wazuh.com>
2020-03-10 16:26:06 +0100

    Adapt overview controller to aiohttp

--- api/api/controllers/overview_controller.py
+++ api/api/controllers/overview_controller.py
@@ -2,23 +2,23 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-import asyncio
 import logging
 
 import connexion
+from aiohttp import web
 
 from api.authentication import get_permissions
+from api.encoder import dumps
 from api.models.base_model_ import Data
 from api.util import exception_handler, raise_if_exc, remove_nones_to_dict
 from wazuh.agent import get_full_overview
 from wazuh.core.cluster.dapi.dapi import DistributedAPI
 
-loop = asyncio.get_event_loop()
 logger = logging.getLogger('wazuh')
 
 
 @exception_handler
-def get_overview_agents(pretty=False, wait_for_complete=False):
+async def get_overview_agents(pretty=False, wait_for_complete=False):
     """ Get full summary of agents.
 
     :param pretty: Show results in human-readable format
@@ -36,7 +36,7 @@ def get_overview_agents(pretty=False, wait_for_complete=False):
                           logger=logger,
                           rbac_permissions=get_permissions(connexion.request.headers['Authorization'])
                           )
-    data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+    data = raise_if_exc(await dapi.distribute_function())
     response = Data(data)
 
-    return response, 200
+    return web.json_response(data=response, status=200, dumps=dumps)
