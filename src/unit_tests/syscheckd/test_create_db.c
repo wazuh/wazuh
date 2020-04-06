3fd2ff79fc9058944503ad5efe6098e43c5364d9
jesusjimsa <jesusjimsa@icloud.com>
2020-04-06 12:25:44 +0200

    Reorder tests

--- src/unit_tests/syscheckd/test_create_db.c
+++ src/unit_tests/syscheckd/test_create_db.c
@@ -2998,10 +2998,6 @@ int main(void) {
         cmocka_unit_test(test_fim_file_no_attributes),
         cmocka_unit_test_setup(test_fim_file_error_on_insert, setup_fim_entry),
 
-        /* fim_scan */
-        cmocka_unit_test_teardown(test_fim_scan_no_realtime, teardown_fim_scan_realtime),
-        cmocka_unit_test_teardown(test_fim_scan_realtime_enabled, teardown_fim_scan_realtime),
-
         /* fim_checker */
         cmocka_unit_test(test_fim_checker_scheduled_configuration_directory_error),
         cmocka_unit_test(test_fim_checker_not_scheduled_configuration_directory_error),
@@ -3017,6 +3013,10 @@ int main(void) {
         cmocka_unit_test(test_fim_checker_fim_regular_ignore),
         cmocka_unit_test(test_fim_checker_fim_regular_restrict),
         cmocka_unit_test_setup_teardown(test_fim_checker_fim_directory, setup_struct_dirent, teardown_struct_dirent),
+
+        /* fim_scan */
+        cmocka_unit_test_teardown(test_fim_scan_no_realtime, teardown_fim_scan_realtime),
+        cmocka_unit_test_teardown(test_fim_scan_realtime_enabled, teardown_fim_scan_realtime),
 
         /* fim_directory */
         cmocka_unit_test_setup_teardown(test_fim_directory, setup_struct_dirent, teardown_struct_dirent),
