159fe6e919d00441ef8001e26576a5ed9a7605da
David Iglesias Lopez <davidj.iglesias@wazuh.com>
2020-04-22 13:07:17 +0200

    Fix several integration tests

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -369,13 +369,27 @@ def environment_black_syscollector_rbac():
     down_env()
 
 
+@pytest.fixture(name="active-response_tests", scope="session")
+def environment_active_response():
+    values = build_and_up("active-response")
+    while values['retries'] < values['max_retries']:
+        health = check_health()
+        if health:
+            time.sleep(30)
+            yield
+            break
+        else:
+            values['retries'] += 1
+    down_env()
+
+
 @pytest.fixture(name="active-response_white_rbac_tests", scope="session")
 def environment_white_active_response_rbac():
     values = build_and_up("active-response_white_rbac")
     while values['retries'] < values['max_retries']:
         health = check_health()
         if health:
-            time.sleep(10)
+            time.sleep(30)
             yield
             break
         else:
@@ -389,7 +403,7 @@ def environment_black_active_response_rbac():
     while values['retries'] < values['max_retries']:
         health = check_health()
         if health:
-            time.sleep(10)
+            time.sleep(30)
             yield
             break
         else:
