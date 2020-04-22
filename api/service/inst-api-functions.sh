94b85a03216cb82918a65b57631daf194425738b
CarlosRS <carlos.ridao@wazuh.com>
2020-04-22 10:03:45 +0200

    A few improvements for inst-api-functions.py
    - API_PATH_BACKUP is now always created
    - configuration/ssl folder is also created as intended
    - migration.py script is executed from the expected path

--- api/service/inst-api-functions.sh
+++ api/service/inst-api-functions.sh
@@ -19,18 +19,19 @@ backup_old_api() {
     # do backup only if config.js file exists
     if [ -f ${API_PATH}/configuration/config.js ]; then
 
+        # remove backup folder and its contents if exists
         if [ -e ${API_PATH_BACKUP} ]; then
             rm -rf ${API_PATH_BACKUP}
-        else
-            ${INSTALL} -d -m 0770 -o root -g ${OSSEC_GROUP} ${API_PATH_BACKUP}
-            chown root:ossec ${API_PATH_BACKUP}
         fi
 
-        ${INSTALL} -d -m 0770 -o root -g ${OSSEC_GROUP} ${API_PATH_BACKUP}/configuration
+        install -o root -g ossec -m 0770 -d ${API_PATH_BACKUP}
+        install -o root -g ossec -m 0770 -d ${API_PATH_BACKUP}/configuration
+
         cp -rLfp ${API_PATH}/configuration/config.js ${API_PATH_BACKUP}/configuration/config.js
 
+        # copy ssl contents if the folder exists
         if [ -d ${API_PATH}/configuration/ssl ]; then
-            ${INSTALL} -d -m 0770 -o root -g ${OSSEC_GROUP} ${API_PATH_BACKUP}/configuration/ssl
+            install -o root -g ossec -m 0770 -d ${API_PATH_BACKUP}/configuration/ssl
             cp -rLfp ${API_PATH}/configuration/ssl/* ${API_PATH_BACKUP}/configuration/ssl
         fi
 
@@ -46,13 +47,23 @@ restore_old_api() {
     API_PATH=${PREFIX}/api
     API_PATH_BACKUP=${PREFIX}/~api
 
+    # perform migration only if there is config.js file in the old api backup
     if [ -r ${API_PATH_BACKUP}/configuration/config.js ]; then
+
         # execute migration.py
-        ${PREFIX}/framework/python/bin/python3 ../api/migration.py
+        ${PREFIX}/framework/python/bin/python3 ../api/scripts/migration.py
+
+        # create ssl folder if it does not exists in the new api
         if [ ! -d ${API_PATH}/configuration/ssl ]; then
-            ${INSTALL} -d -m 0770 -o root -g ${OSSEC_GROUP} ${API_PATH}/configuration/ssl
+            install -o root -g ossec -m 0770 -d ${API_PATH}/configuration/ssl
         fi
-        cp -rLfp ${API_PATH_BACKUP}/configuration/ssl/* ${API_PATH}/configuration/ssl
+
+        # if the ssl folder exists in the api backup copy its content to new api's ssl folder
+        if [ -d ${API_PATH_BACKUP}/configuration/ssl ]; then
+            cp -rLfp ${API_PATH_BACKUP}/configuration/ssl/* ${API_PATH}/configuration/ssl
+        fi
+
+        # remove the old api backup
         rm -rf ${API_PATH_BACKUP}
     fi
 }
