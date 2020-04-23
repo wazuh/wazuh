2b37a53a8fbc1d83366444b8013ed59e3a412371
David Iglesias Lopez <davidj.iglesias@wazuh.com>
2020-04-23 12:54:01 +0200

    Fix Master execute

--- framework/wazuh/core/cluster/master.py
+++ framework/wazuh/core/cluster/master.py
@@ -224,12 +224,11 @@ class MasterHandler(server.AbstractServerHandler, c_common.WazuhCommon):
                 timeout = None if wait_for_complete \
                                else self.cluster_items['intervals']['communication']['timeout_api_request']
                 await asyncio.wait_for(self.server.pending_api_requests[request_id]['Event'].wait(), timeout=timeout)
-                request_result = json.loads(self.server.pending_api_requests[request_id]['Response'],
-                                            cls=c_common.as_wazuh_object)
+                request_result = self.server.pending_api_requests[request_id]['Response']
             except asyncio.TimeoutError:
                 raise exception.WazuhClusterError(3021)
         else:
-            request_result = json.loads(result, cls=c_common.as_wazuh_object)
+            request_result = result
         return request_result
 
     def hello(self, data: bytes) -> Tuple[bytes, bytes]:
