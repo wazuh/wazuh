481992a8f5a14cec292c802a7540e74b2c9f80ec
Mauro Moltrasio <mauro.ezequiel@wazuh.com>
2020-05-06 12:05:20 +0200

    Add wrappers needed to test audit_health_check()
    Fix tests failing due to the added wrappers.
    Removed audit_health_check() from lcov exclusion.

--- src/syscheckd/syscheck_audit.c
+++ src/syscheckd/syscheck_audit.c
@@ -1422,7 +1422,6 @@ int filterkey_audit_events(char *buffer) {
 }
 
 
-// LCOV_EXCL_START
 // Audit healthcheck before starting the main thread
 int audit_health_check(int audit_socket) {
     int retval;
@@ -1488,7 +1487,6 @@ exit_err:
     hc_thread_active = 0;
     return -1;
 }
-// LCOV_EXCL_STOP
 
 #endif
 #endif
