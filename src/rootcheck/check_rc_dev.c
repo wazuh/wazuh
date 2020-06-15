8898db41f0c547c7184c3d395d2b4220c8c588a4
Vikman Fernandez-Castro <victor@wazuh.com>
2020-06-15 11:17:37 +0200

    Bump version to 3.14.0

--- src/rootcheck/check_rc_dev.c
+++ src/rootcheck/check_rc_dev.c
@@ -31,9 +31,7 @@ static int read_dev_file(const char *file_name)
 
     /* Process directories recursively */
     if (S_ISDIR(statbuf.st_mode)) {
-#ifdef DEBUG
-        mtinfo(ARGV0, "Reading dir: %s\n", file_name);
-#endif
+        mtdebug2(ARGV0, "Reading dir: %s\n", file_name);
         return (read_dev_dir(file_name));
     }
 
