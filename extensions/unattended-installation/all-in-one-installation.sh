0bccf1e6371f868caedb8553dfad1377b5c4d2f4
David <hola@davidfmiranda.com>
2020-06-17 18:04:39 +0200

    Fixed JDK repository for Debian

--- extensions/unattended-installation/all-in-one-installation.sh
+++ extensions/unattended-installation/all-in-one-installation.sh
@@ -16,14 +16,14 @@ logger() {
 
 ## Install the required packages for the installation
 installPrerequisites() {
-    logger "Installing all necessary packages for the installation..."
+    logger "Installing all necessary packages before the installation..."
 
     if [ $sys_type == "yum" ] 
     then
         yum install java-11-openjdk-devel unzip wget curl libcap -y -q > /dev/null 2>&1
     elif [ $sys_type == "apt-get" ] 
     then
-        echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list > /dev/null 2>&1
+        echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list
         add-apt-repository ppa:openjdk-r/ppa -y > /dev/null 2>&1
         apt-get update -q > /dev/null 2>&1
         apt-get install openjdk-11-jdk apt-transport-https curl unzip wget libcap2-bin -y -q > /dev/null 2>&1
