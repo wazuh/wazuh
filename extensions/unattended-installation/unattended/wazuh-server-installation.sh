2ac04512f34519ba830107d3583c3daf958e2e5f
David <hola@davidfmiranda.com>
2020-06-18 13:25:29 +0200

    Wazuh server unattended installation single node script

---
+++ extensions/unattended-installation/unattended/wazuh-server-installation.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+## Check if system is based on yum or apt-get
+
+if [ -n "$(command -v yum)" ] 
+then
+    sys_type="yum"
+elif [ -n "$(command -v apt-get)" ] 
+then
+    sys_type="apt-get"
+fi
+
+logger() {
+    echo $1
+}
+
+## Install the required packages for the installation
+installPrerequisites() {
+    logger "Installing all necessary utilities for the installation..."
+
+    if [ $sys_type == "yum" ] 
+    then
+        yum install java-11-openjdk-devel unzip wget curl libcap -y -q > /dev/null 2>&1
+    elif [ $sys_type == "apt-get" ] 
+    then
+        if [ -n "$(command -v add-apt-repository)" ]
+        then
+            add-apt-repository ppa:openjdk-r/ppa -y > /dev/null 2>&1
+        else
+            echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list
+        fi
+        apt-get update -q > /dev/null 2>&1
+        apt-get install openjdk-11-jdk apt-transport-https curl unzip wget libcap2-bin -y -q > /dev/null 2>&1
+    fi
+
+    logger "Done"
+}
+
+## Add the Wazuh repository
+addWazuhrepo() {
+    logger "Adding the Wazuh repository..."
+
+    if [ $sys_type == "yum" ] 
+    then
+        rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH > /dev/null 2>&1
+        echo -e '[wazuh_trash]\ngpgcheck=1\ngpgkey=https://packages-dev.wazuh.com/key/GPG-KEY-WAZUH\nenabled=1\nname=EL-$releasever - Wazuh\nbaseurl=https://packages-dev.wazuh.com/trash/yum/\nprotect=1' | tee /etc/yum.repos.d/wazuh_pre.repo > /dev/null 2>&1
+    elif [ $sys_type == "apt-get" ] 
+    then
+        curl -s https://packages-dev.wazuh.com/key/GPG-KEY-WAZUH | apt-key add - > /dev/null 2>&1
+        echo "deb https://packages-dev.wazuh.com/trash/apt/ unstable main" | tee -a /etc/apt/sources.list.d/wazuh_trash.list > /dev/null 2>&1
+        apt-get update -q > /dev/null 2>&1
+    fi    
+
+    logger "Done" 
+}
+
+## Wazuh manager and API
+installWazuh() {
+    logger "Installing the Wazuh manager and the Wazuh API..."
+
+    if [ $sys_type == "yum" ] 
+    then
+        curl -sL https://rpm.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
+    elif [ $sys_type == "apt-get" ] 
+    then
+        curl -sL https://deb.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
+    fi 
+    $sys_type install wazuh-manager nodejs wazuh-api -y -q > /dev/null 2>&1
+
+    logger "Done"
+}
+
+configureFilebeat() {
+
+    awk '{sub(/127.0.0.1/,"'$wip'")}1' /etc/filebeat/filebeat.yml
+
+}
+
+## Filebeat
+installFilebeat() {
+    
+    logger "Installing Filebeat..."
+
+    $sys_type install filebeat -y -q  > /dev/null 2>&1
+    curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/wazuh/new-documentation-templates/extensions/filebeat/7.x/filebeat_all_in_one.yml > /dev/null 2>&1
+    curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/v3.12.0/extensions/elasticsearch/7.x/wazuh-template.json > /dev/null 2>&1
+    chmod go+r /etc/filebeat/wazuh-template.json > /dev/null 2>&1
+    curl -s https://packages.wazuh.com/3.x/filebeat/wazuh-filebeat-0.1.tar.gz | tar -xvz -C /usr/share/filebeat/module > /dev/null 2>&1
+    mkdir /etc/filebeat/certs > /dev/null 2>&1
+
+    logger "Done"
+}
+
+## Health check
+healthCheck() {
+    cores=$(cat /proc/cpuinfo | grep processor | wc -l)
+    ram_gb=$(free -g | awk '/^Mem:/{print $2}')
+
+    if [[ $cores < "2" ]] || [[ $ram_gb < "4" ]]
+    then
+        echo "The system must have at least 4Gb of RAM and 2 CPUs"
+        exit 1;
+    else
+        echo "Starting the installation..."
+    fi
+}
+
+## Main
+
+main() {
+
+    healthCheck
+    installPrerequisites
+    addWazuhrepo
+    installWazuh
+    installFilebeat
+    
+    if [ -n "$1" ] 
+    then    
+        while [ -n "$1" ]
+        do
+            case "$1" in
+            "-wip"|"--elasticsearch-ip")        
+                wip="$2"
+                configureFilebeat $wip
+                shift
+                shift
+                ;;
+            *)
+                exit 1
+            esac
+        done
+    fi
+}
+
+main "$@"
