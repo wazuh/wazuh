eb2117d3210572da334b14ac3abf90ede563fe92
Jotacarma <jotacarma90@gmail.com>
2020-06-19 12:04:28 +0200

    Restructuring of unit test wrappers, readproc wrappers unified

--- src/unit_tests/wrappers/externals/procpc/readproc_wrappers.c
+++ src/unit_tests/wrappers/externals/procpc/readproc_wrappers.c
@@ -13,3 +13,25 @@
 #include <stdarg.h>
 #include <setjmp.h>
 #include <cmocka.h>
+
+
+void __wrap_closeproc(PROCTAB* PT) {
+    check_expected(PT);
+}
+
+void __wrap_freeproc(proc_t* p) {
+    check_expected(p);
+}
+
+PROCTAB* __wrap_openproc(int flags, ...) {
+    check_expected(flags);
+    return mock_type(PROCTAB*);
+}
+
+proc_t* __wrap_readproc(PROCTAB *restrict const PT,
+                        proc_t *restrict p) {
+    check_expected(PT);
+    check_expected(p);
+
+    return mock_type(proc_t*);
+}
