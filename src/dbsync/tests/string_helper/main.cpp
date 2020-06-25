fee16ae45d392b08b73fa2224cde7b0dc1ad5cbc
Dword <octa.tala@gmail.com>
2020-06-25 12:45:37 -0300

    Coding style changes, const correct and pragma once in header

--- src/dbsync/tests/string_helper/main.cpp
+++ src/dbsync/tests/string_helper/main.cpp
@@ -3,6 +3,5 @@
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    int ret = RUN_ALL_TESTS();
-    return ret;
+    return RUN_ALL_TESTS();
 }
--- src/dbsync/tests/string_helper/string_helper_test.cpp
+++ src/dbsync/tests/string_helper/string_helper_test.cpp
@@ -5,32 +5,37 @@ void StringHelperTest::SetUp() {};
 
 void StringHelperTest::TearDown() {};
 
-TEST_F(StringHelperTest, CheckReplacement) {
-    std::string string_base("hello_world");
-    auto ret_val { StringHelper::replace_string(string_base, "hello_", "bye_") };
+TEST_F(StringHelperTest, CheckReplacement) 
+{
+    std::string string_base { "hello_world" };
+    const auto ret_val { StringHelper::replace_string(string_base, "hello_", "bye_") };
     EXPECT_EQ(string_base, "bye_world");
-    EXPECT_EQ(ret_val, true);
+    EXPECT_TRUE(ret_val);
 }
 
-TEST_F(StringHelperTest, CheckNotReplacement) {
-    std::string string_base("hello_world");
-    auto ret_val { StringHelper::replace_string(string_base, "nothing_", "bye_") };
+TEST_F(StringHelperTest, CheckNotReplacement) 
+{
+    std::string string_base {"hello_world" };
+    const auto ret_val { StringHelper::replace_string(string_base, "nothing_", "bye_") };
     EXPECT_EQ(string_base, "hello_world");
-    EXPECT_EQ(ret_val, false);
+    EXPECT_FALSE(ret_val);
 }
 
-TEST_F(StringHelperTest, SplitEmptyString) {
+TEST_F(StringHelperTest, SplitEmptyString) 
+{
     auto split_text_vector { StringHelper::split("", '.') };
     EXPECT_EQ(0ull, split_text_vector.size());
 }
 
-TEST_F(StringHelperTest, SplitDelimiterNoCoincidence) {
-    auto split_text_vector { StringHelper::split("hello_world", '.') };
+TEST_F(StringHelperTest, SplitDelimiterNoCoincidence) 
+{
+    const auto split_text_vector { StringHelper::split("hello_world", '.') };
     EXPECT_EQ(1ull, split_text_vector.size());
 }
 
-TEST_F(StringHelperTest, SplitDelimiterCoincidence) {
-    auto split_text_vector { StringHelper::split("hello.world", '.') };
+TEST_F(StringHelperTest, SplitDelimiterCoincidence) 
+{
+    const auto split_text_vector { StringHelper::split("hello.world", '.') };
     EXPECT_EQ(2ull, split_text_vector.size());
     EXPECT_EQ(split_text_vector[0], "hello");
     EXPECT_EQ(split_text_vector[1], "world");
