779a227ecc505f16bdf2e14307480e72bc316779
Martin Boretto <martinboretto@gmail.com>
2020-07-07 16:07:27 -0300

    Adding new dbsync APIs to be used for FIM use cases with dummy implementation. Adding doxygen comments too

--- src/dbsync/include/typedef.h
+++ src/dbsync/include/typedef.h
@@ -9,46 +9,64 @@
  * Foundation.
  */
 
-/**
- * @file typedef.h
- * @author Dwordcito
- * @date 17 May 2020
- * @brief File containing declaration for common types for the usage of this module
- *
- */
-
-#pragma once
+#ifndef _DBSYNC_TYPEDEF_H_
+#define _DBSYNC_TYPEDEF_H_
 
 #include "cJSON.h"
 
+/**
+ * @brief Represents the different host types to be used.
+ */
 typedef enum 
 {
-    MANAGER = 0,
-    AGENT = 1
+    MANAGER = 0,    /*< Manager host type. */
+    AGENT   = 1     /*< Agent host type.   */
 }HostType;
 
+/**
+ * @brief Represents the database type to be used.
+ */
 typedef enum 
 {
-    UNDEFINED = 0,
-    SQLITE3 = 1,
+    UNDEFINED = 0,  /*< Undefined database. */
+    SQLITE3   = 1,  /*< SQLite3 database.   */
 }DbEngineType;
 
+/**
+ * @brief Represents the database operation events.
+ */
 typedef enum 
 {
-    MODIFIED = 0,
-    DELETED = 1,
-    INSERTED = 2
+    MODIFIED = 0,   /*< Database modificaton operation. */
+    DELETED  = 1,   /*< Database deletion operation.    */
+    INSERTED = 2    /*< Database insertion operation.   */
 }ReturnTypeCallback;
 
+/**
+ * @brief Represents the handle associated with database creation.
+ */
 typedef void* DBSYNC_HANDLE;
 
 /**
- * \brief Callback function for results
+ * @brief Represents the transaction handle associated with database instance.
+ */
+typedef void* TXN_HANDLE;
+
+/**
+ * @brief Callback function for results
  *
- * This callback is called for each result obtained, after evaluating changes between two snapshots.
- * \param result_type Enumeration value indicating what action was taken.
- * \param result_json Json which describe the change.
+ * Callback called for each result obtained, after evaluating changes between two snapshots.
+ * @param result_type Enumeration value indicating what action was taken.
+ * @param result_json Json which describe the change.
  */
 typedef void((*result_callback)(ReturnTypeCallback result_type, cJSON* result_json));
 
+/**
+ * @brief Callback function for user defined logging.
+ *
+ * Callback called to get deeper information during the dbsync interaction.
+ * @param msg Message to be logged.
+ */
 typedef void((*log_fnc_t)(const char* msg));
+
+#endif // _DBSYNC_TYPEDEF_H_
