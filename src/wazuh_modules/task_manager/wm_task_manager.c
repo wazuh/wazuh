170ada7ff325527bf866470ea309c9e2d3dd0052
Tomas Turina <tomas.turina@wazuh.com>
2020-07-13 17:46:56 -0300

    Add socket /queue/tasks/task

--- src/wazuh_modules/task_manager/wm_task_manager.c
+++ src/wazuh_modules/task_manager/wm_task_manager.c
@@ -12,8 +12,9 @@
 #ifndef WIN32
 
 #include "../wmodules.h"
+#include "../os_net/os_net.h"
 
-static void wm_task_manager_init(wm_task_manager *task_config);
+static int wm_task_manager_init(wm_task_manager *task_config);
 static void* wm_task_manager_main(wm_task_manager* task_config);    // Module main function. It won't return
 static void wm_task_manager_destroy(wm_task_manager* task_config);
 static cJSON* wm_task_manager_dump(const wm_task_manager* task_config);
@@ -26,7 +27,9 @@ const wm_context WM_TASK_MANAGER_CONTEXT = {
     (cJSON * (*)(const void *))wm_task_manager_dump
 };
 
-void wm_task_manager_init(wm_task_manager *task_config) {
+int wm_task_manager_init(wm_task_manager *task_config) {
+    int sock = 0;
+
     // Check if module is enabled
     if (!task_config->enabled) {
         mtinfo(WM_TASK_MANAGER_LOGTAG, "Module disabled. Exiting...");
@@ -38,17 +41,83 @@ void wm_task_manager_init(wm_task_manager *task_config) {
         mterror(WM_TASK_MANAGER_LOGTAG, "DB integrity is invalid. Exiting...");
         pthread_exit(NULL);
     }
+
+    /* Set the queue */
+    if (sock = OS_BindUnixDomain(DEFAULTDIR TASK_QUEUE, SOCK_STREAM, OS_MAXSTR), sock < 0) {
+        mterror(WM_TASK_MANAGER_LOGTAG, "Queue '%s' not accesible: '%s'. Exiting...", TASK_QUEUE, strerror(errno));
+        pthread_exit(NULL);
+    }
+
+    return sock;
 }
 
 void* wm_task_manager_main(wm_task_manager* task_config) {
-    wm_task_manager_init(task_config);
+    int sock;
+    int peer;
+    char *buffer = NULL;
+    char *response = NULL;
+    ssize_t length;
+    fd_set fdset;
+
+    // Initial configuration
+    sock = wm_task_manager_init(task_config);
 
     mtinfo(WM_TASK_MANAGER_LOGTAG, "Module Task Manager started.");
 
     while (1) {
-        // Main loop
+        // Wait for socket
+        FD_ZERO(&fdset);
+        FD_SET(sock, &fdset);
+
+        switch (select(sock + 1, &fdset, NULL, NULL, NULL)) {
+        case -1:
+            if (errno != EINTR) {
+                mterror(WM_TASK_MANAGER_LOGTAG, "Error in select(): '%s'. Exiting...", strerror(errno));
+                pthread_exit(NULL);
+            }
+            continue;
+        case 0:
+            continue;
+        default:
+            break;
+        }
+
+        // Accept incomming connection
+        if (peer = accept(sock, NULL, NULL), peer < 0) {
+            if (errno != EINTR) {
+                mterror(WM_TASK_MANAGER_LOGTAG, "Error in accept(): '%s'", strerror(errno));
+            }
+            continue;
+        }
+
+        // Receive message from connection
+        os_calloc(OS_MAXSTR, sizeof(char), buffer);
+        switch (length = OS_RecvSecureTCP(peer, buffer, OS_MAXSTR), length) {
+        case OS_SOCKTERR:
+            mterror(WM_TASK_MANAGER_LOGTAG, "Response size is bigger than expected.");
+            break;
+        case -1:
+            mterror(WM_TASK_MANAGER_LOGTAG, "Error in recv(): '%s'", strerror(errno));
+            break;
+        case 0:
+            mtdebug1(WM_TASK_MANAGER_LOGTAG, "Empty message from local client.");
+            close(peer);
+            break;
+        case OS_MAXLEN:
+            mterror(WM_TASK_MANAGER_LOGTAG, "Received message > %i", MAX_DYN_STR);
+            close(peer);
+            break;
+        default:
+            //length = wm_task_manager_dispatch(buffer, &response);
+            // Send message to connection
+            //OS_SendSecureTCP(peer, length, response);
+            //os_free(response);
+            close(peer);
+        }
+        os_free(buffer);
     }
 
+    close(sock);
     return NULL;
 }
 
