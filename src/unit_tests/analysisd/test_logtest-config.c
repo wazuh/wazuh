2f9c0403fbf4de6d7cb1db86a1e2ecdabae04632
Juan Cabrera <jcabre88@gmail.com>
2020-07-13 16:41:48 +0200

    Added tests for getRuleTestConfig

--- src/unit_tests/analysisd/test_logtest-config.c
+++ src/unit_tests/analysisd/test_logtest-config.c
@@ -17,6 +17,7 @@
 #include "../../analysisd/logtest.h"
 
 int Read_Logtest(XML_NODE node);
+cJSON *getRuleTestConfig();
 
 /* setup/teardown */
 
@@ -61,11 +62,33 @@ int __wrap_get_nprocs (void) {
     return mock();
 }
 
+cJSON * __wrap_cJSON_CreateObject(void) {
+    return mock_type(cJSON *);
+}
+
+cJSON* __wrap_cJSON_AddStringToObject(cJSON * const object, const char * const name, const char * const string) {
+    if (name) check_expected(name);
+    if (string) check_expected(string);
+    return mock_type(cJSON *);
+}
+
+cJSON* __wrap_cJSON_AddNumberToObject(cJSON * const object, const char * const name, const double number) {
+    if (name) check_expected(name);
+    if (number) check_expected(number);
+    return mock_type(cJSON *);
+}
+
+
+void __wrap_cJSON_AddItemToObject(cJSON *object, const char *string, cJSON *item) {
+    if (string) check_expected(string);
+    if (item) check_expected(item);
+}
+
 /* tests */
 
 /* Read_Logtest */
 
-void test_Read_Logtest_invalid_element(void **state)
+void test_Read_Logtest_element_NULL(void **state)
 {
     xml_node **nodes;
     nodes = calloc(2, sizeof(xml_node*));
@@ -85,7 +108,7 @@ void test_Read_Logtest_invalid_element(void **state)
     os_free(nodes);
 }
 
-void test_Read_Logtest_invalid_content(void **state)
+void test_Read_Logtest_content_NULL(void **state)
 {
     xml_node **nodes;
     nodes = calloc(2, sizeof(xml_node*));
@@ -485,12 +508,70 @@ void test_Read_Logtest_valid_session_timeout(void **state)
     os_free(logtest_conf.enabled);
 }
 
+void test_Read_Logtest_invalid_element(void **state)
+{
+    xml_node **nodes;
+    nodes = calloc(2, sizeof(xml_node*));
+    nodes[0] = calloc(1, sizeof(xml_node));
+
+    os_calloc(4, sizeof(char), logtest_conf.enabled);
+
+    nodes[0]->element = strdup("test");
+    nodes[0]->content = strdup("unit_test");
+
+    expect_string(__wrap__merror, formatted_msg, "(1230): Invalid element in the configuration: 'test'.");
+
+    int ret = Read_Logtest(nodes);
+    assert_int_equal(ret, OS_INVALID);
+
+    os_free(nodes[0]->element);
+    os_free(nodes[0]->content);
+    os_free(nodes[0]);
+    os_free(nodes);
+    os_free(logtest_conf.enabled);
+}
+
+/* getRuleTestConfig */
+void test_getRuleTestConfig_OK(void **state)
+{
+    will_return(__wrap_cJSON_CreateObject, (cJSON *)1);
+    will_return(__wrap_cJSON_CreateObject, (cJSON *)1);
+
+    os_strdup("yes", logtest_conf.enabled);
+    logtest_conf.threads = LOGTEST_LIMIT_THREAD;
+    logtest_conf.max_sessions = LOGTEST_LIMIT_MAX_SESSIONS;
+    logtest_conf.session_timeout = LOGTEST_LIMIT_SESSION_TIMEOUT;
+
+    expect_string(__wrap_cJSON_AddStringToObject, name, "enabled");
+    expect_string(__wrap_cJSON_AddStringToObject, string, "yes");
+    will_return(__wrap_cJSON_AddStringToObject, (cJSON *)1);
+
+    expect_string(__wrap_cJSON_AddNumberToObject, name, "threads");
+    expect_value(__wrap_cJSON_AddNumberToObject, number, 128);
+    will_return(__wrap_cJSON_AddNumberToObject, (cJSON *)1);
+
+    expect_string(__wrap_cJSON_AddNumberToObject, name, "max_sessions");
+    expect_value(__wrap_cJSON_AddNumberToObject, number, 500);
+    will_return(__wrap_cJSON_AddNumberToObject, (cJSON *)1);
+
+    expect_string(__wrap_cJSON_AddNumberToObject, name, "session_timeout");
+    expect_value(__wrap_cJSON_AddNumberToObject, number, 31536000);
+    will_return(__wrap_cJSON_AddNumberToObject, (cJSON *)1);
+
+    expect_string(__wrap_cJSON_AddItemToObject, string, "rule_test");
+    expect_value(__wrap_cJSON_AddItemToObject, item, (cJSON *)1);
+
+    cJSON* ret = getRuleTestConfig();
+
+    os_free(logtest_conf.enabled);
+}
+
 int main(void)
 {
     const struct CMUnitTest tests[] = {
         // Tests Read_Logtest
-        cmocka_unit_test(test_Read_Logtest_invalid_element),
-        cmocka_unit_test(test_Read_Logtest_invalid_content),
+        cmocka_unit_test(test_Read_Logtest_element_NULL),
+        cmocka_unit_test(test_Read_Logtest_content_NULL),
         cmocka_unit_test(test_Read_Logtest_invalid_enabled),
         cmocka_unit_test(test_Read_Logtest_valid_enabled),
         cmocka_unit_test(test_Read_Logtest_invalid_threads),
@@ -507,7 +588,10 @@ int main(void)
         cmocka_unit_test(test_Read_Logtest_invalid_session_timeout),
         cmocka_unit_test(test_Read_Logtest_smaller_session_timeout),
         cmocka_unit_test(test_Read_Logtest_limit_session_timeout),
-        cmocka_unit_test(test_Read_Logtest_valid_session_timeout)
+        cmocka_unit_test(test_Read_Logtest_valid_session_timeout),
+        cmocka_unit_test(test_Read_Logtest_invalid_element),
+        // Tests getRuleTestConfig
+        cmocka_unit_test(test_getRuleTestConfig_OK),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
