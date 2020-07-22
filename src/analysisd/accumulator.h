8aa052b8b4ec37a9cdf720899bf55f15e23ce980
Lopuiz <lopezziur@gmail.com>
2020-07-22 10:20:26 +0200

    Modify accumulate functions and variables to works with wazuh-logtest

--- src/analysisd/accumulator.h
+++ src/analysisd/accumulator.h
@@ -13,9 +13,55 @@
 
 #include "eventinfo.h"
 
-/* Accumulator Functions */
-int Accumulate_Init(void);
-Eventinfo *Accumulate(Eventinfo *lf);
-void Accumulate_CleanUp(void);
+
+/**
+ * @brief Hash where save data which have same id
+ *
+ * Only for Analysisd use
+ */
+OSHash *os_analysisd_acm_store;
+
+/**
+ * @brief Counter of the number of times purge
+ *
+ * Only for Analysisd use
+ */
+int os_analysisd_acm_lookups;
+
+/**
+ * @brief Counter of interval time since the last purge
+ *
+ * Only for Analysisd use
+ */
+time_t os_analysisd_acm_purge_ts;
+
+/**
+ * @brief Initialize accumulator engine
+ *
+ * @param acm_store
+ * @param acm_purge_ts
+ * @return 1 on succes, otherwise 0
+ */
+int Accumulate_Init(OSHash **acm_store, int *acm_lookups, time_t *acm_purge_ts);
+
+/**
+ * @brief Accumulate data from events sharing the same ID
+ *
+ * @param lf EventInfo to proccess
+ * @param acm_store Hash where save data which have same ID
+ * @param acm_lookups
+ * @param acm_purge_ts
+ * @return EventInfo passed from input
+ */
+Eventinfo *Accumulate(Eventinfo *lf, OSHash **acm_store, int *acm_lookups, time_t *acm_purge_ts);
+
+/**
+ * @brief Purge the cache as needed
+ *
+ * @param acm_store
+ * @param acm_lookups
+ * @param acm_purge_ts
+ */
+void Accumulate_CleanUp(OSHash **acm_store, int *acm_lookups, time_t *acm_purge_ts);
 
 #endif /* ACCUMULATOR_H */
