49596b21393b973908ceda16e3df00d4d69fa31c
Lopuiz <lopezziur@gmail.com>
2020-07-24 09:20:44 +0200

    - Implement function to initialize and remove Logtest client session - Modify decoders and cdb list flow to work with wazuh-logtest - Document the CDB list, rules list, and decoders list

--- src/analysisd/decoders/decoder.h
+++ src/analysisd/decoders/decoder.h
@@ -30,7 +30,11 @@
 
 struct _Eventinfo;
 
-/* Decoder structure */
+/**
+ * @brief Decoder structure
+ *
+ * Allow saving the decoders information
+ */
 typedef struct {
     u_int8_t  get_next;
     u_int8_t  type;
@@ -56,6 +60,8 @@ typedef struct {
 
     void (*plugindecoder)(void *lf, void *rule_hash, void *decoder_match);
     void* (**order)(struct _Eventinfo *, char *, const char *);
+
+    bool internal_saving;      ///< Used to free decoderinfo structure in wazuh-logtest
 } OSDecoderInfo;
 
 /* List structure */
@@ -75,11 +81,22 @@ typedef struct dbsync_context_t {
     cJSON * data;
 } dbsync_context_t;
 
-/* Functions to Create the list, add a osdecoder to the
- * list and to get the first osdecoder
+/**
+ * @brief Initialize decoder lists to NULL
+ *
+ * Only used for analysisd decoder lists (os_analysisd_decoderlist_pn and os_analysisd_decoderlist_nopn)
  */
 void OS_CreateOSDecoderList(void);
+
+/**
+ * @brief Add decoders to decoder lists
+ * @param pi decoder to add in decoder list
+ * @param pn_osdecodernode decoder list for events with program name
+ * @param npn_osdecodernode decoder list for events without program name
+ * @return 1 on success, otherwise 0
+ */
 int OS_AddOSDecoder(OSDecoderInfo *pi, OSDecoderNode **pn_osdecodernode, OSDecoderNode **npn_osdecodernode);
+
 OSDecoderNode *OS_GetFirstOSDecoder(const char *pname);
 int getDecoderfromlist(const char *name);
 char *GetGeoInfobyIP(char *ip_addr);
@@ -91,6 +108,34 @@ void SyscollectorInit(void);
 void CiscatInit(void);
 void WinevtInit(void);
 void SecurityConfigurationAssessmentInit(void);
-int ReadDecodeXML(const char *file);
+int ReadDecodeXML(const char *file, OSDecoderNode **decoderlist_pn, OSDecoderNode **decoderlist_nopn);
+
+/**
+ * @brief Remove decoder information
+ * @param pi OSDecoderInfo struct to remove
+ */
+void FreeDecoderInfo(OSDecoderInfo *pi);
+
+/**
+ * @brief Remove decoder list
+ * @param decoderlist_pn list of decoders which have program_name
+ * @param decoderlist_npn ist of decoders which haven't program_name
+ * @param num_decoders number of decoder nodes on memory
+ */
+void os_remove_decoders_list(OSDecoderNode *decoderlist_pn, OSDecoderNode *decoderlist_npn);
+
+/**
+ * @brief Remove a decoder node
+ * @param node OSDecoderNode node to remove
+ * @param decoders hash to save the reference to decoder information
+ */
+void os_remove_decodernode(OSDecoderNode *node, OSDecoderInfo **decoders, int *pos, int *max_size);
+
+/**
+ * @brief Count the number of decoders in a list
+ * @param node the first node of the list
+ * @param num_decoders the number of decoders
+ */
+void os_count_decoders(OSDecoderNode *node, int *num_decoders);
 
 #endif /* DECODER_H */
--- src/analysisd/logtest.h
+++ src/analysisd/logtest.h
@@ -9,10 +9,16 @@
 
 #include "shared.h"
 #include "rules.h"
+#include "config.h"
 #include "decoders/decoder.h"
 #include "eventinfo.h"
+#include "lists.h"
+#include "lists_make.h"
+#include "fts.h"
+#include "accumulator.h"
 #include "../config/logtest-config.h"
 #include "../os_net/os_net.h"
+#include <time.h>
 
 
 /**
@@ -77,22 +83,22 @@ int w_logtest_init_parameters();
 void *w_logtest_main(w_logtest_connection_t * connection);
 
 /**
- * @brief Create resources necessary to service client
- * @param fd File descriptor which represents the client
- */
-void w_logtest_initialize_session(const char * token);
-
-/**
  * @brief Process client's request
  * @param fd File descriptor which represents the client
  */
-void w_logtest_process_log(const char * token);
+void w_logtest_process_log(char * token);
+
+/**
+ * @brief Create resources necessary to service client
+ * @param fd File descriptor which represents the client
+ */
+w_logtest_session_t *w_logtest_initialize_session(char * token, char ** msg_error);
 
 /**
  * @brief Free resources after client closes connection
  * @param fd File descriptor which represents the client
  */
-void w_logtest_remove_session(const char * token);
+void w_logtest_remove_session(char * token);
 
 /**
  * @brief Check the inactive logtest sessions
