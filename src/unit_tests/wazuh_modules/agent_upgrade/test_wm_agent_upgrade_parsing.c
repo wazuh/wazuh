a70fddc6248dd9abd3c035df7018e1bba3d63cfb
Tomas Turina <tomas.turina@wazuh.com>
2020-08-14 12:06:31 -0300

    Tests for wm_agent_upgrade_parse_task_module_request

--- src/unit_tests/wazuh_modules/agent_upgrade/test_wm_agent_upgrade_parsing.c
+++ src/unit_tests/wazuh_modules/agent_upgrade/test_wm_agent_upgrade_parsing.c
@@ -140,6 +140,44 @@ void test_wm_agent_upgrade_parse_response_message_without_agent_id(void **state)
     assert_string_equal(cJSON_GetObjectItem(response, "status")->valuestring, status);
 }
 
+void test_wm_agent_upgrade_parse_task_module_request_complete(void **state)
+{
+    int command = 1;
+    int agent_id = 10;
+    char *status = "Done";
+
+    cJSON *response = wm_agent_upgrade_parse_task_module_request(command, agent_id, status);
+
+    *state = response;
+
+    assert_non_null(cJSON_GetObjectItem(response, "module"));
+    assert_string_equal(cJSON_GetObjectItem(response, "module")->valuestring, "upgrade_module");
+    assert_non_null(cJSON_GetObjectItem(response, "command"));
+    assert_string_equal(cJSON_GetObjectItem(response, "command")->valuestring, "upgrade_custom");
+    assert_non_null(cJSON_GetObjectItem(response, "agent"));
+    assert_int_equal(cJSON_GetObjectItem(response, "agent")->valueint, agent_id);
+    assert_non_null(cJSON_GetObjectItem(response, "status"));
+    assert_string_equal(cJSON_GetObjectItem(response, "status")->valuestring, status);
+}
+
+void test_wm_agent_upgrade_parse_task_module_request_without_status(void **state)
+{
+    int command = 1;
+    int agent_id = 10;
+
+    cJSON *response = wm_agent_upgrade_parse_task_module_request(command, agent_id, NULL);
+
+    *state = response;
+
+    assert_non_null(cJSON_GetObjectItem(response, "module"));
+    assert_string_equal(cJSON_GetObjectItem(response, "module")->valuestring, "upgrade_module");
+    assert_non_null(cJSON_GetObjectItem(response, "command"));
+    assert_string_equal(cJSON_GetObjectItem(response, "command")->valuestring, "upgrade_custom");
+    assert_non_null(cJSON_GetObjectItem(response, "agent"));
+    assert_int_equal(cJSON_GetObjectItem(response, "agent")->valueint, agent_id);
+    assert_null(cJSON_GetObjectItem(response, "status"));
+}
+
 #endif
 
 int main(void) {
@@ -148,7 +186,9 @@ int main(void) {
         cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_response_message_complete, teardown_json),
         cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_response_message_without_status, teardown_json),
         cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_response_message_without_task_id, teardown_json),
-        cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_response_message_without_agent_id, teardown_json)
+        cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_response_message_without_agent_id, teardown_json),
+        cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_task_module_request_complete, teardown_json),
+        cmocka_unit_test_teardown(test_wm_agent_upgrade_parse_task_module_request_without_status, teardown_json)
 #endif
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
