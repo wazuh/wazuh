85e9e9ea841702f6f19fce62b3a49db746084188
Martin Boretto <martinboretto@gmail.com>
2020-08-18 12:45:05 -0300

    Adding RSync folder structure

---
+++ src/rsync/src/rsync.cpp
@@ -0,0 +1,23 @@
+/*
+ * Wazuh DBSYNC
+ * Copyright (C) 2015-2020, Wazuh Inc.
+ * August 18, 2020.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include "rsync.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void rsync_initialize()
+{}
+
+#ifdef __cplusplus
+}
+#endif
