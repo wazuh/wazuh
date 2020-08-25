701a086d323f9933450bc49feb91b285b47459f0
Tomas Turina <tomas.turina@wazuh.com>
2020-08-25 17:21:23 -0300

    Tests for tasks functions

--- src/unit_tests/wazuh_modules/agent_upgrade/test_wm_agent_upgrade_tasks.c
+++ src/unit_tests/wazuh_modules/agent_upgrade/test_wm_agent_upgrade_tasks.c
@@ -17,6 +17,8 @@
 #include "../../wazuh_modules/agent_upgrade/manager/wm_agent_upgrade_tasks.h"
 #include "../../headers/shared.h"
 
+#ifdef TEST_SERVER
+
 // Setup / teardown
 
 static int setup_agent_task(void **state) {
@@ -31,6 +33,21 @@ static int teardown_agent_task(void **state) {
     wm_agent_upgrade_free_agent_task(agent_task);
     return 0;
 }
+
+static int setup_node(void **state) {
+    OSHashNode *node = NULL;
+    os_calloc(1, sizeof(OSHashNode), node);
+    *state = (void *)node;
+    return 0;
+}
+
+static int teardown_node(void **state) {
+    OSHashNode *node = (OSHashNode *)*state;
+    os_free(node);
+    return 0;
+}
+
+#endif
 
 // Wrappers
 
@@ -67,6 +84,37 @@ int __wrap_OSHash_Add_ex(OSHash *self, const char *key, void *data) {
     return mock();
 }
 
+void *__wrap_OSHash_Get_ex(const OSHash *self, const char *key) {
+    check_expected(key);
+
+    return mock_type(void*);
+}
+
+int __wrap_OSHash_Update_ex(OSHash *self, const char *key, void *data) {
+    check_expected(key);
+    check_expected(data);
+
+    return mock();
+}
+
+void *__wrap_OSHash_Delete_ex(OSHash *self, const char *key) {
+    check_expected(key);
+
+    return mock_type(void*);
+}
+
+int __wrap_OSHash_Begin(unsigned int *index) {
+    check_expected(index);
+
+    return mock();
+}
+
+int __wrap_OSHash_Next(unsigned int *index, OSHashNode *current) {
+    check_expected(index);
+
+    return mock();
+}
+
 #ifdef TEST_SERVER
 
 // Tests
@@ -99,6 +147,83 @@ void test_wm_agent_upgrade_create_task_entry_duplicate(void **state)
     assert_int_equal(ret, OSHASH_DUPLICATED);
 }
 
+void test_wm_agent_upgrade_insert_task_id_ok(void **state)
+{
+    int agent_id = 8;
+    int task_id = 100;
+    wm_agent_task *agent_task = *state;
+
+    agent_task->task_info = wm_agent_upgrade_init_task_info();
+
+    expect_string(__wrap_OSHash_Get_ex, key, "8");
+    will_return(__wrap_OSHash_Get_ex, agent_task);
+
+    expect_string(__wrap_OSHash_Update_ex, key, "8");
+    expect_memory(__wrap_OSHash_Update_ex, data, agent_task, sizeof(agent_task));
+    will_return(__wrap_OSHash_Update_ex, OSHASH_SUCCESS);
+
+    wm_agent_upgrade_insert_task_id(agent_id, task_id);
+
+    assert_int_equal(agent_task->task_info->task_id, task_id);
+}
+
+void test_wm_agent_upgrade_insert_task_id_err(void **state)
+{
+    int agent_id = 8;
+    int task_id = 100;
+
+    expect_string(__wrap_OSHash_Get_ex, key, "8");
+    will_return(__wrap_OSHash_Get_ex, NULL);
+
+    wm_agent_upgrade_insert_task_id(agent_id, task_id);
+}
+
+void test_wm_agent_upgrade_remove_entry_ok(void **state)
+{
+    int agent_id = 10;
+    wm_agent_task *agent_task = *state;
+
+    expect_string(__wrap_OSHash_Delete_ex, key, "10");
+    will_return(__wrap_OSHash_Delete_ex, agent_task);
+
+    wm_agent_upgrade_remove_entry(agent_id);
+}
+
+void test_wm_agent_upgrade_remove_entry_err(void **state)
+{
+    int agent_id = 10;
+
+    expect_string(__wrap_OSHash_Delete_ex, key, "10");
+    will_return(__wrap_OSHash_Delete_ex, NULL);
+
+    wm_agent_upgrade_remove_entry(agent_id);
+}
+
+void test_wm_agent_upgrade_get_first_node(void **state)
+{
+    int index = 0;
+
+    expect_value(__wrap_OSHash_Begin, index, index);
+    will_return(__wrap_OSHash_Begin, 1);
+
+    OSHashNode* ret = wm_agent_upgrade_get_first_node(&index);
+
+    assert_int_equal(ret, 1);
+}
+
+void test_wm_agent_upgrade_get_next_node(void **state)
+{
+    int index = 0;
+    OSHashNode *node = *state;
+
+    expect_value(__wrap_OSHash_Next, index, index);
+    will_return(__wrap_OSHash_Next, 1);
+
+    OSHashNode* ret = wm_agent_upgrade_get_next_node(&index, node);
+
+    assert_int_equal(ret, 1);
+}
+
 #endif
 
 int main(void) {
@@ -107,6 +232,16 @@ int main(void) {
         // wm_agent_upgrade_upgrade_success_callback
         cmocka_unit_test_setup_teardown(test_wm_agent_upgrade_create_task_entry_ok, setup_agent_task, teardown_agent_task),
         cmocka_unit_test_setup_teardown(test_wm_agent_upgrade_create_task_entry_duplicate, setup_agent_task, teardown_agent_task),
+        // wm_agent_upgrade_insert_task_id
+        cmocka_unit_test_setup_teardown(test_wm_agent_upgrade_insert_task_id_ok, setup_agent_task, teardown_agent_task),
+        cmocka_unit_test(test_wm_agent_upgrade_insert_task_id_err),
+        // wm_agent_upgrade_remove_entry
+        cmocka_unit_test_setup(test_wm_agent_upgrade_remove_entry_ok, setup_agent_task),
+        cmocka_unit_test(test_wm_agent_upgrade_remove_entry_err),
+        // wm_agent_upgrade_get_first_node
+        cmocka_unit_test(test_wm_agent_upgrade_get_first_node),
+        // wm_agent_upgrade_get_next_node
+        cmocka_unit_test_setup_teardown(test_wm_agent_upgrade_get_next_node, setup_node, teardown_node),
 #endif
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
