0f0343d880c4a32690232247aa166e41dfa0e271
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-08-31 11:42:05 +0200

    Fix unit and integrated tests

--- api/test/integration/env/configurations/base/manager/security/base_security_test.sql
+++ api/test/integration/env/configurations/base/manager/security/base_security_test.sql
@@ -10,7 +10,7 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
 /* Testing user */
-INSERT INTO users VALUES(99,'testing','pbkdf2:sha256:150000$OMVAATei$cb30da77537eea26b964265dab6f403e9499f18522c7cc9e6ba2cb2d33694e1f',1,'1970-01-01 00:00:00');
+INSERT INTO users VALUES(99,'testing','pbkdf2:sha256:150000$OMVAATei$cb30da77537eea26b964265dab6f403e9499f18522c7cc9e6ba2cb2d33694e1f',0,'1970-01-01 00:00:00');
 
 /* Testing role */
 INSERT INTO roles VALUES(99,'testing','1970-01-01 00:00:00');
