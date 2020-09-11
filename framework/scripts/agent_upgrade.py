fd56e5da1d3cc6d0b47f44cb006b743c5543a1c7
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-09-11 14:06:26 +0200

    Add docstrings

--- framework/scripts/agent_upgrade.py
+++ framework/scripts/agent_upgrade.py
@@ -49,9 +49,20 @@ def list_outdated():
         print("\nTotal outdated agents: {0}".format(agents.total_affected_items))
 
 
-def get_agents_versions(affected_items):
+def get_agents_versions(agents):
+    """Get the current versions of the specified agents
+
+    Parameters
+    ----------
+    agents : list
+        List of agent's IDs
+
+    Returns
+    -------
+    Dictionary with the current version (prev_version)
+    """
     agents_versions = dict()
-    for agent_id in affected_items:
+    for agent_id in agents:
         agent = Agent(agent_id)
         agent.load_info_from_db()
         if agent.version:
@@ -64,21 +75,39 @@ def get_agents_versions(affected_items):
 
 
 def create_command():
+    """Create a custom command based on the CLI arguments
+
+    Returns
+    -------
+    Dictionary with upgrade command
+    """
     if not args.file and not args.execute:
-        f_kwargs = {'agent_list': args.agents,
-                    'wpk_repo': args.repository,
-                    'version': args.version,
-                    'use_http': args.http,
-                    'force': args.force}
+        f_kwargs = {'agent_list': args.agents, 'wpk_repo': args.repository, 'version': args.version,
+                    'use_http': args.http, 'force': args.force}
     else:
-        f_kwargs = {'agent_list': args.agents,
-                    'installer': args.execute,
-                    'file_path': args.file}
+        # Upgrade custom
+        f_kwargs = {'agent_list': args.agents, 'installer': args.execute, 'file_path': args.file}
 
     return f_kwargs
 
 
 def send_command(function, command, local_master=False):
+    """Send the command to the specified function.
+    If local_master is True, the request type must be local_master (upgrade_result)
+
+    Parameters
+    ----------
+    function : func
+        Upgrade function
+    command : dict
+        Arguments for the specified function
+    local_master : bool
+        True for get the upgrade results, False for send upgrade command
+
+    Returns
+    -------
+    Distributed API request result
+    """
     dapi = DistributedAPI(f=function, f_kwargs=command,
                           request_type='distributed_master' if not local_master else 'local_master',
                           is_async=False, wait_for_complete=True, logger=logger)
@@ -87,6 +116,15 @@ def send_command(function, command, local_master=False):
 
 
 def print_result(agents_versions, failed_agents):
+    """Print the operation's result
+
+    Parameters
+    ----------
+    agents_versions : dict
+        Dictionary with the previous version an the new one
+    failed_agents : dict
+        Contain the error's information
+    """
     len(agents_versions.keys()) > 0 and print('\nUpgraded agents:')
     for agent_id, versions in agents_versions.items():
         print(f"\tAgent {agent_id} upgraded: {versions['prev_version']} -> {versions['new_version']}")
@@ -96,24 +134,36 @@ def print_result(agents_versions, failed_agents):
         print(f"\tAgent {agent_id}. Status: {error}")
 
 
-def check_status(affected_items, result_dict, failed_items):
-    len(affected_items) and print('\nUpgrading...')
-    while len(affected_items):
-        task_results = send_command(function=get_upgrade_result, command={'agent_list': list(affected_items)},
+def check_status(affected_agents, result_dict, failed_agents):
+    """Check the agent's upgrade status
+
+    Parameters
+    ----------
+    affected_agents : list
+        Result of the upgrade task check
+    result_dict : dict
+        Dictionary with the previous version and the new one
+    failed_agents : dict
+        Contain the error's information
+    """
+    affected_agents = set(affected_agents)
+    len(affected_agents) and print('\nUpgrading...')
+    while len(affected_agents):
+        task_results = send_command(function=get_upgrade_result, command={'agent_list': list(affected_agents)},
                                     local_master=True)
         for task_result in task_results.affected_items.copy():
             if task_result['status'] == 'Updated' or 'Legacy upgrade' in task_result['status']:
                 agent = Agent(task_result['agent_id'])
                 agent.load_info_from_db()
                 result_dict[task_result['agent_id']]['new_version'] = args.version if args.version else agent.version
-                affected_items.discard(task_result['agent_id'])
+                affected_agents.discard(task_result['agent_id'])
             elif task_result['status'] == 'Error':
-                failed_items[task_result['agent_id']] = task_result['error_msg']
+                failed_agents[task_result['agent_id']] = task_result['error_msg']
                 result_dict.pop(task_result['agent_id'])
-                affected_items.discard(task_result['agent_id'])
+                affected_agents.discard(task_result['agent_id'])
         sleep(3)
 
-    print_result(agents_versions=result_dict, failed_agents=failed_items)
+    print_result(agents_versions=result_dict, failed_agents=failed_agents)
 
 
 def main():
@@ -138,11 +188,11 @@ def main():
     for agent_result, agent_ids in result.failed_items.items():
         print(f"\tAgent {', '.join(agent_ids)} upgrade failed. Status: {agent_result}")
 
-    result.affected_items = {task["agent_id"] for task in result.affected_items}
-    agents_versions = get_agents_versions(result.affected_items)
+    result.affected_items = [task["agent_id"] for task in result.affected_items]
+    agents_versions = get_agents_versions(agents=result.affected_items)
 
     failed_agents = dict()
-    check_status(affected_items=result.affected_items, result_dict=agents_versions, failed_items=failed_agents)
+    check_status(affected_agents=result.affected_items, result_dict=agents_versions, failed_agents=failed_agents)
 
 
 if __name__ == "__main__":
