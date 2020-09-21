7d761e4f5cbbf93db1787ef1b13d3172687efd39
Manuel Carmona <manuel.carmona@wazuh.com>
2020-09-21 13:35:55 +0200

    Update function descriptions to numpydoc

--- framework/wazuh/sca.py
+++ framework/wazuh/sca.py
@@ -22,15 +22,28 @@ def get_sca_list(agent_list=None, q="", offset=0, limit=common.database_limit, s
                  filters=None):
     """ Get a list of policies analyzed in the configuration assessment for a given agent
 
-    :param agent_list: agent id to get policies from
-    :param q: Defines query to filter in DB.
-    :param offset: First item to return.
-    :param limit: Maximum number of items to return.
-    :param sort: Sorts the items. Format: {"fields":["field1","field2"],"order":"asc|desc"}.
-    :param search: Looks for items with the specified string. Format: {"fields": ["field1","field2"]}
-    :param select: Select fields to return. Format: {"fields":["field1","field2"]}.
-    :param filters: Define field filters required by the user. Format: {"field1":"value1", "field2":["value2","value3"]}
-    :return: AffectedItemsWazuhResult
+    Parameters
+    ----------
+    agent_list : list
+        Agent ids to get policies from.
+    q : str
+        Defines query to filter in DB.
+    offset : int
+        First item to return.
+    limit : int
+        Maximum number of items to return.
+    sort : str
+        Sorts the items. Format: {"fields":["field1","field2"],"order":"asc|desc"}.
+    search : str
+        Looks for items with the specified string. Format: {"fields": ["field1","field2"]}
+    select : str
+        Select fields to return. Format: {"fields":["field1","field2"]}.
+    filters : str
+        Define field filters required by the user. Format: {"field1":"value1", "field2":["value2","value3"]}
+
+    Returns
+    -------
+    AffectedItemsWazuhResult
     """
     result = AffectedItemsWazuhResult(all_msg='All selected sca information was returned',
                                       some_msg='Some sca information was not returned',
@@ -57,16 +70,30 @@ def get_sca_checks(policy_id=None, agent_list=None, q="", offset=0, limit=common
                    select=None, filters=None):
     """ Get a list of checks analyzed for a policy
 
-    :param policy_id: policy id to get the checks from
-    :param agent_list: agent id to get the policies from
-    :param q: Defines query to filter in DB.
-    :param offset: First item to return.
-    :param limit: Maximum number of items to return.
-    :param sort: Sorts the items. Format: {"fields":["field1","field2"],"order":"asc|desc"}.
-    :param search: Looks for items with the specified string. Format: {"fields": ["field1","field2"]}
-    :param select: Select fields to return. Format: {"fields":["field1","field2"]}.
-    :param filters: Define field filters required by the user. Format: {"field1":"value1", "field2":["value2","value3"]}
-    :return: AffectedItemsWazuhResult
+    Parameters
+    ----------
+    policy_id : str
+        Policy id to get the checks from.
+    agent_list : list
+        Agent id to get the policies from
+    q : str
+        Defines query to filter in DB.
+    offset : int
+        First item to return.
+    limit : int
+        Maximum number of items to return.
+    sort : str
+        Sorts the items. Format: {"fields":["field1","field2"],"order":"asc|desc"}.
+    search : str
+        Looks for items with the specified string. Format: {"fields": ["field1","field2"]}
+    select : str
+        Select fields to return. Format: {"fields":["field1","field2"]}.
+    filters : str
+        Define field filters required by the user. Format: {"field1":"value1", "field2":["value2","value3"]}
+
+    Returns
+    -------
+    AffectedItemsWazuhResult
     """
     result = AffectedItemsWazuhResult(all_msg='All selected sca/policy information was returned',
                                       some_msg='Some sca/policy information was not returned',
