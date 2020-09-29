e4bc53fdf786f57bd6ba8413f5a4a85718ef667c
Diego Provinciani <diegoprovinciani@gmail.com>
2020-09-29 04:00:08 -0300

    Fixing issues found during exploratory testing

--- src/addagent/validate.c
+++ src/addagent/validate.c
@@ -182,11 +182,8 @@ int OS_RemoveAgent(const char *u_id) {
     if (sock >= 0) {
         close(sock);
     }
-    os_free(wdboutput);
 
-    if (wdb_remove_agent(atoi(u_id)) != OS_SUCCESS) {
-        merror("Could not remove the information stored in Wazuh DB of the agent %s.", u_id);
-    }
+    os_free(wdboutput);
 
     /* Remove counter for ID */
     OS_RemoveCounter(u_id);
