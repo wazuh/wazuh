223105be9ecc116b087fd51e679db235a75572de
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-10-09 15:46:23 +0200

    [API] Fix API integration tests in 4.0-RC2 (#6242)
    * Fix group files permissions on testing environment
    
    * Add agent_control in spec.yaml
    
    * Fix PUT /security/config. Update related tests
    
    * Fix for invalid command in entrypoint.sh
    
    * Fix reload configuration module.
    
    * Fix experimental bug with os_name parameter
    
    * Fix test_syscheck_endpoints in API.
    
    * Fixed bug that obscured non-internal errors in wdb
    
    * Fix wdb regex
    
    * Change get_agent_attr
    
    * Fix agent unittests for new get_agent_os_name method. (#6200)
    
    * Fix agent unittests for new get_agent_os_name method.
    
    * Update test_syscollector unittests.
    
    * Fix outdated test
    
    * Adjust count regex in wdb.py
    
    * Update agent ossec.conf for API tests.
    
    * Fix agent_upgrade CLI tool
    
    * Add node names to disconnected agents on testing environment
    
    * Fix get_token_login_api
    
    * Fix error code
    
    * Fix code key error on integration tests
    
    * Update API spec to expect an object response when revoking tokens
    
    * Fix extra key and code key erros in API integration tests.
    
    * Fix /security/config endpoints
    
    * Fix for GET agents and cluster tests
    
    * Fix for authorization_context test
    
    * Fix for test_manager
    
    * Fix security API tests.
    
    * [API] Remove error field from data field in API responses (#6224)
    
    * Remove error field from data field in API responses
    
    * Apply requested improvements
    
    * Add field error to API spec examples (#6229)
    
    * Add field error to API spec examples
    
    * Update api/api/spec/spec.yaml
    
    Co-authored-by: David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
    
    * Update api/api/spec/spec.yaml
    
    Co-authored-by: David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
    
    * Update api/api/spec/spec.yaml
    
    Co-authored-by: David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
    
    Co-authored-by: David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
    
    * Fix agents_put tests
    
    * Fix add_rules ID bug
    
    * Fix agents_PUT tests
    
    * Update GET agents integration tests.
    
    * Update agents test
    
    Co-authored-by: Víctor Fernández Poyatos <vicferpoy@gmail.com>
    Co-authored-by: Selutario <selutario@gmail.com>
    Co-authored-by: David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
    Co-authored-by: CarlosRS <carlos.ridao@wazuh.com>
    Co-authored-by: Carlos RS <54286452+CarlosRS9@users.noreply.github.com>
    Co-authored-by: David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>

--- api/test/integration/env/base/agent/entrypoint.sh
+++ api/test/integration/env/base/agent/entrypoint.sh
@@ -2,6 +2,9 @@
 
 # Modify ossec.conf
 python3 /scripts/xml_parser.py /var/ossec/etc/ossec.conf /scripts/xml_templates/ossec.conf
+if [ $3  == "4.x" ]; then
+  python3 /scripts/xml_parser.py /var/ossec/etc/ossec.conf /scripts/xml_templates/ossec_4.x.conf
+fi
 
 sed -i "s:<address>MANAGER_IP</address>:<address>$1</address>:g" /var/ossec/etc/ossec.conf
 sed -n "/$2 /p" /var/ossec/etc/test.keys > /var/ossec/etc/client.keys
