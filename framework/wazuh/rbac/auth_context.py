89d4d55f89dabbf695820b84cf1efadb57c1ef31
David Jose Iglesias Lopez <48716145+davidjiglesias@users.noreply.github.com>
2020-10-14 09:53:33 +0200

    Fix/6269 framework integration tests (#6286)
    * Add error field to framework integration tests
    
    * Update list_ parameters
    
    * Fix error field in black cluster and manager integration tests.
    
    * Fix error field on /manager/configuration
    
    * Fix validation functions and outdated integration tests
    
    * Fix overview endpoint
    
    * Minor improvement for overview endpoint
    
    * Fix tests
    
    Co-authored-by: Víctor Fernández Poyatos <vicferpoy@gmail.com>
    Co-authored-by: Selutario <selutario@gmail.com>
    Co-authored-by: CarlosRS <carlos.ridao@wazuh.com>

--- framework/wazuh/rbac/auth_context.py
+++ framework/wazuh/rbac/auth_context.py
@@ -288,7 +288,7 @@ class RBAChecker:
         list_roles = list()
         for role in self.roles_list:
             for rule in role['rules']:
-                if (rule['id'] not in orm.required_rules_for_role[1] or self.user_id in orm.admin_user_ids) and \
+                if (rule['id'] > orm.max_id_reserved or self.user_id < orm.max_id_reserved) and \
                         self.check_rule(rule['rule']):
                     list_roles.append(role['id'])
                     break
