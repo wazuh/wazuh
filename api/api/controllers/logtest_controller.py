397415e2e2fb50ab21b63f0490c6a38fb90b23ed
Manuel Carmona <manuel.carmona@wazuh.com>
2020-10-15 12:58:32 +0200

    Minor changes

--- api/api/controllers/logtest_controller.py
+++ api/api/controllers/logtest_controller.py
@@ -33,7 +33,7 @@ async def run_logtest_tool(request, pretty: bool = False, wait_for_complete: boo
     Body.validate_content_type(request, expected_content_type='application/json')
     f_kwargs = await LogtestModel.get_kwargs(request)
 
-    dapi = DistributedAPI(f=logtest.get_logtest_output,
+    dapi = DistributedAPI(f=logtest.run_logtest,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
                           request_type='local_master',
                           is_async=False,
