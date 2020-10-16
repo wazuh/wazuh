ac434427e473f23b261d16745da37f90886804d4
konangc87 <fedegarciacruz@gmail.com>
2020-10-15 21:52:43 -0700

    added hw info to macos

--- src/wazuh_modules/syscollector/sysInfoMac.cpp
+++ src/wazuh_modules/syscollector/sysInfoMac.cpp
@@ -11,16 +11,144 @@
 #include "sysInfo.hpp"
 #include "cmdHelper.h"
 #include "stringHelper.h"
+#include <sys/sysctl.h>
+
+static void getMemory(nlohmann::json& info)
+{
+    constexpr auto vmPageSize{"vm.pagesize"};
+    constexpr auto vmPageFreeCount{"vm.page_free_count"};
+    constexpr auto KByte{1024};
+    uint64_t ram{0};
+    const std::vector<int> mib{CTL_HW, HW_MEMSIZE};
+    size_t len{sizeof(ram)};
+    auto ret{sysctl(const_cast<int*>(mib.data()), mib.size(), &ram, &len, nullptr, 0)};
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error reading total RAM."
+        };
+    }
+    const auto ramTotal{ram/KByte};
+    info["ram_total"] = ramTotal;
+    u_int pageSize{0};
+    len = sizeof(pageSize);
+    ret = sysctlbyname(vmPageSize, &pageSize, &len, nullptr, 0);
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error reading page size."
+        };
+    }
+    uint64_t freePages{0};
+    len = sizeof(freePages);
+    ret = sysctlbyname(vmPageFreeCount, &freePages, &len, nullptr, 0);
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error reading free pages."
+        };
+    }
+    const auto ramFree{(freePages * pageSize)/KByte};
+    info["ram_free"] = ramFree;
+    info["ram_usage"] = 100 - (100 * ramFree / ramTotal);
+}
+
+static double getCpuMHz()
+{
+    constexpr auto MHz{1000000.0};
+    unsigned long cpuMHz{0};
+    constexpr auto clockRate{"hw.cpufrequency"};
+    size_t len{sizeof(cpuMHz)};
+    const auto ret{sysctlbyname(clockRate, &cpuMHz, &len, nullptr, 0)};
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error reading cpu frequency."
+        };
+    }
+    return static_cast<double>(cpuMHz)/MHz;
+}
+
+static unsigned int getCpuCores()
+{
+    unsigned int cores{0};
+    size_t len{sizeof(cores)};
+    const std::vector<int> mib{CTL_HW, HW_NCPU};
+    const auto ret{sysctl(const_cast<int*>(mib.data()), mib.size(), &cores, &len, nullptr, 0)};
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error reading cpu cores number."
+        };
+    }
+    return cores;
+}
+
+static std::string getCpuName()
+{
+    std::unique_ptr<char> spBuff;
+    const std::vector<int> mib{CTL_HW, HW_MODEL};
+    size_t len{0};
+    auto ret{sysctl(const_cast<int*>(mib.data()), mib.size(), nullptr, &len, nullptr, 0)};
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error getting cpu name size."
+        };
+    }
+    spBuff.reset(new char[len+1]);
+    if(!spBuff)
+    {
+        throw std::runtime_error
+        {
+            "Error allocating memory to read the cpu name."
+        };
+    }
+    ret = sysctl(const_cast<int*>(mib.data()), mib.size(), spBuff.get(), &len, nullptr, 0);
+    if(ret)
+    {
+        throw std::system_error
+        {
+            ret,
+            std::system_category(),
+            "Error getting cpu name"
+       };
+    }
+    spBuff.get()[len] = 0;
+    return std::string{reinterpret_cast<const char*>(spBuff.get())};
+}
 
 static std::string getSerialNumber()
 {
-    const auto rawData{Utils::exec("system_profiler SPHardwareDataType | grep Serial")}
-    return Utils::trim(rawData.substr(rawData.find(":")), " \t\r\n");
+    const auto rawData{Utils::exec("system_profiler SPHardwareDataType | grep Serial")};
+    return Utils::trim(rawData.substr(rawData.find(":")), " :\t\r\n");
 }
 
 nlohmann::json SysInfo::hardware()
 {
     nlohmann::json ret;
     ret["board_serial"] = getSerialNumber();
+    ret["cpu_name"] = getCpuName();
+    ret["cpu_cores"] = getCpuCores();
+    ret["cpu_MHz"] = getCpuMHz();
+    getMemory(ret);
     return ret;
 }
