60f36d727da1357a3d025633b92f84b8dcd00275
Diego Provinciani <diegoprovinciani@gmail.com>
2020-10-20 18:41:12 -0300

    Adding 100% of unit testing coverage got function wdb_parse_global_update_connection_status

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_global_wrappers.h
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_global_wrappers.h
@@ -44,6 +44,8 @@ int __wrap_wdb_global_set_agent_label(wdb_t *wdb, int id, char* key, char* value
 
 int __wrap_wdb_global_update_agent_keepalive(wdb_t *wdb, int id, char* status);
 
+int __wrap_wdb_global_update_agent_connection_status(wdb_t *wdb, int id, char* connection_status);
+
 int __wrap_wdb_global_delete_agent(wdb_t *wdb, int id);
 
 cJSON* __wrap_wdb_global_select_agent_name(wdb_t *wdb, int id);
