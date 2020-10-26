f62ee921a33603515aa67d8a995563de974a74f2
BraulioV <brauliovl95@gmail.com>
2020-10-26 11:39:54 +0100

    Improved the user check in shared.sh

--- src/init/shared.sh
+++ src/init/shared.sh
@@ -14,14 +14,13 @@ UNAME=`uname -snr`
 NUNAME=`uname`
 
 # If whoami does not exist, try id
-ls "`command -v whoami`" > /dev/null 2>&1
-if [ ! $? = 0 ]; then
+if command -v whoami > /dev/null 2>&1 ; then
+    ME=`whoami 2>/dev/null`
+else
     ME=`id | cut -d " " -f 1`
     if [ "X${ME}" = "Xuid=0(root)" ]; then
         ME="root"
     fi
-else
-    ME=`whoami 2>/dev/null`
 fi
 
 OSSEC_INIT="/etc/ossec-init.conf"
