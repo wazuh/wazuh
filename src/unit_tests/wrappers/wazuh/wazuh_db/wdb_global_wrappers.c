7ce5cae178b724e3879e9bf708dde85a7d9be704
Diego Provinciani <diegoprovinciani@gmail.com>
2020-10-27 23:18:41 -0300

    Restoring get_agents_by_keepalive to avoid a lack of functionality

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_global_wrappers.c
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_global_wrappers.c
@@ -264,9 +264,15 @@ int __wrap_wdb_global_sync_agent_info_set(__attribute__((unused)) wdb_t *wdb,
     return mock();
 }
 
-cJSON* __wrap_wdb_global_get_agents_to_disconnect(__attribute__((unused)) wdb_t *wdb,
-                                                  int keep_alive) {
+wdbc_result __wrap_wdb_global_get_agents_by_keepalive(__attribute__((unused)) wdb_t *wdb,
+                                                      int* last_agent_id,
+                                                      char comparator,
+                                                      int keep_alive,
+                                                      char **output) {
+    check_expected(*last_agent_id);
+    check_expected(comparator);
     check_expected(keep_alive);
+    os_strdup(mock_ptr_type(char*), *output);
     return mock();
 }
 
@@ -291,6 +297,12 @@ int __wrap_wdb_global_reset_agents_connection( __attribute__((unused)) wdb_t *wd
 cJSON* __wrap_wdb_global_get_agents_by_connection_status(__attribute__((unused)) wdb_t* wdb,
                                                          const char* status) {
     check_expected(status);
+    return mock_ptr_type(cJSON*);
+}
+
+cJSON* __wrap_wdb_global_get_agents_to_disconnect(__attribute__((unused)) wdb_t *wdb,
+                                                  int keep_alive) {
+    check_expected(keep_alive);
     return mock_ptr_type(cJSON*);
 }
 
