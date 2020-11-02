9ca45f935b7073f42435940ff5992bf91c8761bc
pereyra-m <matias.pereyra@wazuh.com>
2020-11-02 20:35:37 -0300

    Adding UT for time, date and trigger functions in monitord.c

--- src/unit_tests/server.cmake
+++ src/unit_tests/server.cmake
@@ -6,7 +6,7 @@ if(NOT WAZUHEXT)
 endif()
 
 # Add compiling flags
-add_compile_options(-ggdb -O0 -g -coverage -DTEST_SERVER -DENABLE_AUDIT -DINOTIFY_ENABLED)
+add_compile_options(-ggdb -O0 -g -coverage -DTEST_SERVER -DENABLE_AUDIT -DINOTIFY_ENABLED -Wall)
 
 # Set tests dependencies
 set(TEST_DEPS ${WAZUHLIB} ${WAZUHEXT} -lpthread -lcmocka -fprofile-arcs -ftest-coverage)
@@ -16,3 +16,4 @@ add_subdirectory(remoted)
 add_subdirectory(wazuh_db)
 add_subdirectory(os_auth)
 add_subdirectory(wazuh_modules)
+add_subdirectory(monitord)
