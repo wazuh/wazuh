32ff6ec2cf5d5e75129aab9aabd0bc0cc1dd1c2f
pereyra-m <matias.pereyra@wazuh.com>
2020-11-03 19:30:12 -0300

    Adding UT for monitord methods in monitor_actions.c

--- src/unit_tests/wrappers/wazuh/shared/hash_op_wrappers.h
+++ src/unit_tests/wrappers/wazuh/shared/hash_op_wrappers.h
@@ -25,6 +25,8 @@ OSHash *__wrap_OSHash_Create();
 
 void *__wrap_OSHash_Delete_ex(OSHash *self, const char *key);
 
+void *__wrap_OSHash_Delete(OSHash *self, const char *key);
+
 void *__wrap_OSHash_Get(const OSHash *self, const char *key);
 
 void *__wrap_OSHash_Get_ex(const OSHash *self, const char *key);
