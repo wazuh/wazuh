f9d06c8194ac842d201b222685d4f5a85e1e9250
Vikman Fernandez-Castro <victor@wazuh.com>
2020-11-19 21:56:39 +0100

    Update changelog and bump revision to 40008

--- framework/wazuh/core/cluster/__init__.py
+++ framework/wazuh/core/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '4.0.2'
-__revision__ = '40007'
+__revision__ = '40008'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
