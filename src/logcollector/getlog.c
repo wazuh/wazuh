ab3d8a91fb34458c0d7116c2b25df09299a98da7
Juan Nicolas Asselle <jnasselle@gmail.com>
2020-11-19 18:45:37 +0000

    Add support for multiline and singleline dispatcher at syslog

---
+++ src/logcollector/getlog.c
@@ -0,0 +1,21 @@
+/* Copyright (C) 2015-2020, Wazuh Inc.
+ * Copyright (C) 2009 Trend Micro Inc.
+ * All right reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
+#include "getlog.h"
+
+char *getlog_singleline(getlog_params_t * params){
+    return fgets(params->buffer,params->length,params->stream);
+}
+
+char *getlog_multiline(getlog_params_t * params){
+    // Handle multiline 
+    w_multiline_config_t * ml_cfg = (w_multiline_config_t *) params->ctxt;
+    return NULL;
+}
