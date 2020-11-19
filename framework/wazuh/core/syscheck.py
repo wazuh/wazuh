a3ade4db1c9e469fbe227456b0ee807d48db19b8
Manuel Carmona <manuel.carmona@wazuh.com>
2020-11-19 12:21:02 +0100

    Add min_select_fields to syscheck

--- framework/wazuh/core/syscheck.py
+++ framework/wazuh/core/syscheck.py
@@ -10,9 +10,14 @@ from wazuh.core.utils import WazuhDBQuery, WazuhDBBackend, get_fields_to_nest, p
 class WazuhDBQuerySyscheck(WazuhDBQuery):
     nested_fields = ['value']
 
-    def __init__(self, agent_id, nested=False, default_sort_field='mtime', *args, **kwargs):
-        super().__init__(backend=WazuhDBBackend(agent_id), default_sort_field=default_sort_field, count=True,
-                         get_data=True, date_fields={'mtime', 'date'}, *args, **kwargs)
+    def __init__(self, agent_id, nested=False, default_sort_field='mtime', min_select_fields=None, *args,
+                 **kwargs):
+        if min_select_fields is None:
+            min_select_fields = {'file'}
+        super().__init__(backend=WazuhDBBackend(agent_id), default_sort_field=default_sort_field,
+                         min_select_fields=min_select_fields, count=True,
+                         get_data=True, date_fields={'mtime', 'date'}, *args,
+                         **kwargs)
         self.nested = nested
 
     def _filter_date(self, date_filter, filter_db_name):
@@ -34,8 +39,7 @@ class WazuhDBQuerySyscheck(WazuhDBQuery):
             else:
                 return value
 
-        self._data = [{key: format_fields(key, value) for key, value in item.items() if key in self.select}
-                      for item in self._data]
+        self._data = [{key: format_fields(key, value) for key, value in item.items()} for item in self._data]
 
         if self.nested:
             fields_to_nest, non_nested = get_fields_to_nest(self.fields.keys(), self.nested_fields, '.')
