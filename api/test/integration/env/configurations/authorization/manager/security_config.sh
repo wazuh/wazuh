0f699d8b817111a0518f84add11ac429284806b2
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2020-11-23 12:04:41 +0100

    Rework authorization context integration tests

--- api/test/integration/env/configurations/authorization/manager/security_config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# RBAC configuration
-sqlite3 /var/ossec/api/configuration/security/rbac.db < /configuration_files/schema_security_test.sql
-chown ossec:ossec /var/ossec/api/configuration/security/rbac.db
