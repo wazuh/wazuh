8ce981249a834b6fff01c6898821a0350693668f
Víctor Fernández <vicferpoy@gmail.com>
2020-11-25 13:28:21 +0100

    [API][Framework] Rename framework files to adapt for automated testing structure (#6694)
    * Rename API/framework modules and tests
    
    * Update spec references
    
    * Rename list_controller to cdb_list_controller
    
    * Rename API models

--- api/api/models/__init__.py
+++ api/api/models/__init__.py
@@ -3,5 +3,5 @@
 # flake8: noqa
 from __future__ import absolute_import
 
-from api.models.basic_info import BasicInfo
-from api.models.token_response import TokenResponseModel
+from api.models.basic_info_model import BasicInfo
+from api.models.security_token_response_model import TokenResponseModel
