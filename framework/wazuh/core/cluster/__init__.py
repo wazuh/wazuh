a6e7efa55824676587434b8e55302161b761b63b
David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
2020-11-26 19:46:20 +0100

    Bump rev 40010

--- framework/wazuh/core/cluster/__init__.py
+++ framework/wazuh/core/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '4.0.2'
-__revision__ = '40009'
+__revision__ = '40010'
 __author__ = "Wazuh Inc"
 __ossec_name__ = "Wazuh"
 __licence__ = "\
--- src/win32/ossec-installer.nsi
+++ src/win32/ossec-installer.nsi
@@ -21,7 +21,7 @@
 !define MUI_ICON install.ico
 !define MUI_UNICON uninstall.ico
 !define VERSION "4.0.2"
-!define REVISION "40009"
+!define REVISION "40010"
 !define NAME "Wazuh"
 !define SERVICE "OssecSvc"
 
