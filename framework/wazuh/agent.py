89188a8e1e5d4d9f20ea51032be4af119dfa9bbc
jmv74211 <jonathan.martin@wazuh.com>
2020-11-30 10:37:28 +0100

    Add new test mark to test pr wazuh

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2015-2020, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+# Test mark
 
 import hashlib
 import logging
