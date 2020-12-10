d641ac8c0775c9196dff276e565a6efa967b52aa
Dword <octa.tala@gmail.com>
2020-12-10 02:18:27 -0300

    Add constraint to avoid rows issues.

--- src/wazuh_db/schema_upgrade_v6.sql
+++ src/wazuh_db/schema_upgrade_v6.sql
@@ -8,14 +8,14 @@
  * and/or modify it under the terms of GPLv2.
 */
 
-ALTER TABLE sys_osinfo ADD COLUMN os_patch TEXT DEFAULT NULL;
+ALTER TABLE sys_osinfo ADD COLUMN os_patch TEXT DEFAULT NOT NULL CHECK (checksum <> '');
 
-ALTER TABLE sys_netiface ADD COLUMN checksum TEXT DEFAULT NULL;
+ALTER TABLE sys_netiface ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
 ALTER TABLE sys_netproto ADD COLUMN checksum TEXT DEFAULT NULL;
-ALTER TABLE sys_netaddr ADD COLUMN checksum TEXT DEFAULT NULL;
-ALTER TABLE sys_ports ADD COLUMN checksum TEXT DEFAULT NULL;
-ALTER TABLE sys_programs ADD COLUMN checksum TEXT DEFAULT NULL;
-ALTER TABLE sys_hotfixes ADD COLUMN checksum TEXT DEFAULT NULL;
-ALTER TABLE sys_processes ADD COLUMN checksum TEXT DEFAULT NULL;
+ALTER TABLE sys_netaddr ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
+ALTER TABLE sys_ports ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
+ALTER TABLE sys_programs ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
+ALTER TABLE sys_hotfixes ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
+ALTER TABLE sys_processes ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
 
 INSERT OR REPLACE INTO metadata (key, value) VALUES ('db_version', 6);
