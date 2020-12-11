f15c0c08efca97caaf25b0955d2b5b25ea6a4ba8
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-12-11 14:10:31 +0100

    Provisional fix for experimental IT

--- api/test/integration/env/configurations/experimental/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/experimental/agent/healthcheck/healthcheck.py
@@ -1,9 +1,25 @@
+# import os
+#
+# output_ciscat = os.system("grep -q 'wazuh-modulesd:ciscat: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
+# output_sysc = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
+#
+# if output_ciscat == 0 and output_sysc == 0:
+#     exit(0)
+# else:
+#     exit(1)
+
+# Provisionally
 import os
 
-output_ciscat = os.system("grep -q 'wazuh-modulesd:ciscat: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
-output_sysc = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
+def get_health():
+    output = os.system("grep -q 'ossec-syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log")
+    output_syscollector = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
 
-if output_ciscat == 0 and output_sysc == 0:
-    exit(0)
-else:
-    exit(1)
+    if output == 0 and output_syscollector == 0:
+        return 0
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    exit(get_health())
