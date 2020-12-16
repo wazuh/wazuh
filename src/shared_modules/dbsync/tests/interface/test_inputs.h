d5b544d027a9c2524bba81910e3fe2813263de9a
Federico Garcia Cruz <fedegarciacruz@gmail.com>
2020-12-16 18:33:43 -0300

    added new test.h and removed old ones

--- src/shared_modules/dbsync/tests/interface/test_inputs.h
+++ src/shared_modules/dbsync/tests/interface/test_inputs.h
@@ -1,3 +1,6 @@
+#ifndef TEST_INPUTS_H
+#define TEST_INPUTS_H
+
 constexpr auto input1
 {
 R"(
@@ -17878,3 +17881,59 @@ R"(
 ])"
 };
 
+constexpr auto diffResult
+{
+R"(
+[
+	{
+		"pid": 380,
+		"stime": 2652
+	},
+	{
+		"pid": 1246,
+		"utime": 13568
+	},
+	{
+		"pid": 1280,
+		"processor": 1,
+		"stime": 37380,
+		"utime": 229205
+	},
+	{
+		"pid": 2259,
+		"stime": 35207,
+		"utime": 336505
+	},
+	{
+		"pid": 3412,
+		"processor": 1,
+		"stime": 20876,
+		"utime": 117829
+	},
+	{
+		"pid": 3472,
+		"processor": 0,
+		"stime": 5431,
+		"utime": 40365
+	},
+	{
+		"pid": 321811,
+		"processor": 1,
+		"stime": 13
+	},
+	{
+		"pid": 520740,
+		"processor": 0
+	},
+	{
+		"pid": 653689,
+		"processor": 1
+	},
+	{
+		"pid": 654011,
+		"stime": 41
+	}
+])"
+};
+
+#endif //TEST_INPUTS_H
