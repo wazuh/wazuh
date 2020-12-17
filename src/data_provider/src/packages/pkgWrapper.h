2d708e3bbde8d6e7e9e18cd6d692d530ea89aefe
Dwordcito <octa.tala@gmail.com>
2020-12-16 16:49:30 -0800

    Add factory to select differents types of raw providers

---
+++ src/data_provider/src/packages/pkgWrapper.h
@@ -0,0 +1,52 @@
+/*
+ * Wazuh SYSINFO
+ * Copyright (C) 2015-2020, Wazuh Inc.
+ * December 14, 2020.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _PKG_WRAPPER_H
+#define _PKG_WRAPPER_H
+
+#include "ipackageWrapper.h"
+#include "sharedDefs.h"
+
+class PKGWrapper final : public IPackageWrapper
+{
+    public:
+    explicit PKGWrapper(const std::string& /*fileName*/)
+    { }
+
+    ~PKGWrapper() = default;
+
+    std::string name() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+    std::string version() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+    std::string groups() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+    std::string description() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+    std::string architecture() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+    std::string format() const override
+    {
+        return DEFAULT_STRING_VALUE;
+    }
+};
+
+#endif //_PKG_WRAPPER_H
