001220bbebcf12394b7cb2e9647d07052f954469
Manuel Carmona <manuel.carmona@wazuh.com>
2020-12-21 12:30:35 +0100

    Minor changes to experimental agent healthcheck

--- api/test/integration/env/configurations/experimental/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/experimental/agent/healthcheck/healthcheck.py
@@ -1,9 +1,10 @@
 import os
 
-output_ciscat = os.system("grep -q 'wazuh-modulesd:ciscat: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
+output_ciscat_scan = os.system("grep -q 'wazuh-modulesd:ciscat: INFO: Scan finished successfully.' /var/ossec/logs/ossec.log")
+output_ciscat_evaluation = os.system("grep -q 'wazuh-modulesd:ciscat: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
 output_sysc = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
 
-if output_ciscat == 0 and output_sysc == 0:
+if output_ciscat_scan == 0 and output_ciscat_evaluation == 0 and output_sysc == 0:
     exit(0)
 else:
     exit(1)
