35d72d6e80d08665906716855dd72336a506f5be
José Luis López Sánchez <selutario@gmail.com>
2020-12-24 15:01:10 +0100

    Update healthcheck and conftest. (#7029)
    * Update healthcheck and conftest.
    
    * Fix security integration tests
    
    Co-authored-by: Víctor Fernández Poyatos <vicferpoy@gmail.com>

--- api/test/integration/env/configurations/tmp/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/tmp/agent/healthcheck/healthcheck.py
@@ -1,8 +1,7 @@
 import os
 
-
 def get_health():
-    output = os.system("grep -q 'syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log")
+    output = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
 
     if output == 0:
         return 0
