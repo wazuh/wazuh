bf1af75a801849bc7aa4e844f7d7223460c4aa53
Martin Boretto <martinboretto@gmail.com>
2020-12-29 16:21:17 -0300

    Adding UTs for valid and invalid timestamps

--- src/shared_modules/utils/tests/timeHelper_test.cpp
+++ src/shared_modules/utils/tests/timeHelper_test.cpp
@@ -9,6 +9,7 @@
  * Foundation.
  */
 
+#include <regex>
 #include "timeHelper_test.h"
 #include "timeHelper.h"
 
@@ -20,4 +21,18 @@ TEST_F(TimeUtilsTest, CheckTimestamp)
 {
     const auto timestamp { Utils::getTimestamp(std::time(nullptr)) };
     EXPECT_FALSE(timestamp.empty());
+}
+
+TEST_F(TimeUtilsTest, CheckTimestampValidFormat)
+{
+    const auto dateFormatRegexStr { "[0-9]{4}/([1-9]|1[0-2])/([1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|1[0-9]|[0-9]):([0-9]|[1-5][0-9]):([1-5][0-9]|[0-9])" };
+    const auto timestamp { Utils::getTimestamp(std::time(nullptr)) };
+    EXPECT_TRUE(std::regex_match(timestamp, std::regex(dateFormatRegexStr)));
+}
+
+TEST_F(TimeUtilsTest, CheckTimestampInvalidFormat)
+{
+    const auto dateFormatRegexStr { "[0-9]{4}/([1-9]|1[0-2])/([1-9]|[1-2][0-9]|3[0-1])(2[0-3]|1[0-9]|[0-9]):([0-9]|[1-5][0-9]):([1-5][0-9]|[0-9])" };
+    const auto timestamp { Utils::getTimestamp(std::time(nullptr)) };
+    EXPECT_FALSE(std::regex_match(timestamp, std::regex(dateFormatRegexStr)));
 }
