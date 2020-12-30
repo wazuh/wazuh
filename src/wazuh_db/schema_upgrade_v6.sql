ec524cd349d326397f63bbdfeaa42b454dfc14d2
root <root@localhost.localdomain>
2020-12-30 13:32:12 -0800

    added upgrade to version 7 code

--- src/wazuh_db/schema_upgrade_v6.sql
+++ src/wazuh_db/schema_upgrade_v6.sql
@@ -44,44 +44,4 @@ CREATE INDEX IF NOT EXISTS fim_date_index ON fim_entry (date);
 
 ALTER TABLE sys_osinfo ADD COLUMN os_patch TEXT DEFAULT NULL;
 
-DELETE FROM sys_osinfo;
-DELETE FROM sys_netiface;
-DELETE FROM sys_netproto;
-DELETE FROM sys_netaddr;
-DELETE FROM sys_programs;
-DELETE FROM sys_hotfixes;
-DELETE FROM sys_processes;
-DROP TABLE sys_ports;
-CREATE TABLE IF NOT EXISTS sys_ports (
-    scan_id INTEGER,
-    scan_time TEXT,
-    protocol TEXT,
-    local_ip TEXT,
-    local_port INTEGER CHECK (local_port >= 0),
-    remote_ip TEXT,
-    remote_port INTEGER CHECK (remote_port >= 0),
-    tx_queue INTEGER,
-    rx_queue INTEGER,
-    inode INTEGER,
-    state TEXT,
-    PID INTEGER,
-    process TEXT,
-    checksum TEXT NOT NULL CHECK (checksum <> ''),
-    PRIMARY KEY (protocol, local_ip, local_port, inode)
-);
-
-ALTER TABLE sys_netiface ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-ALTER TABLE sys_netproto ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-ALTER TABLE sys_netaddr ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-ALTER TABLE sys_programs ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-ALTER TABLE sys_hotfixes ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-ALTER TABLE sys_processes ADD COLUMN checksum TEXT DEFAULT NOT NULL CHECK (checksum <> '');
-
-ALTER TABLE sys_netiface ADD COLUMN item_id TEXT DEFAULT NULL;
-ALTER TABLE sys_netproto ADD COLUMN item_id TEXT DEFAULT NULL;
-ALTER TABLE sys_netaddr ADD COLUMN item_id TEXT DEFAULT NULL;
-ALTER TABLE sys_ports ADD COLUMN item_id TEXT DEFAULT NULL;
-ALTER TABLE sys_programs ADD COLUMN item_id TEXT DEFAULT NULL;
-ALTER TABLE sys_programs ADD COLUMN os_patch TEXT DEFAULT NULL;
-
 INSERT OR REPLACE INTO metadata (key, value) VALUES ('db_version', 6);
