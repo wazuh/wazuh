f238166e5df7dfb6a3e6b5787cdfeb447c2c0bdc
David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
2021-01-09 17:28:31 +0100

    Fix spec regexes

--- api/api/test/test_validator.py
+++ api/api/test/test_validator.py
@@ -11,8 +11,8 @@ from unittest.mock import patch
 from api.validator import check_cdb_list, check_exp, check_xml, _alphanumeric_param, \
     _array_numbers, _array_names, _boolean, _dates, _empty_boolean, _hashes,\
     _ips, _names, _numbers, _wazuh_key, _paths, _query_param, _ranges, _search_param,\
-    _sort_param, _timeframe_type, _type_format, _yes_no_boolean, format_etc_file_path, _etc_file_path, \
-    _etc_and_ruleset_file_path, _etc_and_ruleset_path, allowed_fields, is_safe_path
+    _sort_param, _timeframe_type, _type_format, _yes_no_boolean, format_edit_files_path, _edit_files_path, \
+    _delete_files_path, _get_files_path, _get_dirnames_path, allowed_fields, is_safe_path
 
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -60,9 +60,10 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
     ('/var/ossec/etc/internal_options', _paths),
     ('/var/ossec/etc/rules/local_rules.xml', _paths),
     # relative paths
-    ('etc/ossec.conf', _etc_file_path),
-    ('etc/rules/new_rules2.xml', _etc_and_ruleset_file_path),
-    ('etc/lists/new_lists3', _etc_and_ruleset_path),
+    ('etc/ossec.conf', _edit_files_path),
+    ('etc/rules/new_rules2.xml', _get_files_path),
+    ('etc/decoders/new_decoder3.xml', _get_files_path),
+    ('etc/lists/new_lists3', _get_dirnames_path),
 ])
 def test_validation_check_exp_ok(exp, regex_name):
     """Verify that check_exp() returns True with correct params"""
@@ -107,11 +108,17 @@ def test_validation_check_exp_ok(exp, regex_name):
     ('/var/ossec/etc/internal_options$', _paths),
     ('/var/ossec/etc/rules/local_rules.xml()', _paths),
     # relative paths
-    ('etc/internal_options', _etc_and_ruleset_path),
-    ('../../path', _etc_and_ruleset_path),
-    ('/var/ossec/etc/lists/new_lists3', _etc_and_ruleset_path),
-    ('../ossec', _etc_and_ruleset_path),
-    ('etc/rules/../../../dir', _etc_and_ruleset_path)
+    ('etc/internal_options', _get_dirnames_path),
+    ('../../path', _get_dirnames_path),
+    ('/var/ossec/etc/lists/new_lists3', _get_dirnames_path),
+    ('../ossec', _get_dirnames_path),
+    ('etc/rules/../../../dir', _get_dirnames_path),
+    ('ruleset/rules/0010-rules_config.xml', _delete_files_path),
+    ('etc/ossec.conf', _delete_files_path),
+    ('etc/lists/../../../../../../var/ossec/etc/client.keys', _get_files_path),
+    ('ruleset/decoders/../../../../../../var/ossec/ruleset/rules/0016-wazuh_rules.xml', _edit_files_path),
+    ('ruleset/decoders/0040-auditd_decoders.xml', _edit_files_path),
+    ('etc/rules/no_xml.something', _edit_files_path)
 ])
 def test_validation_check_exp_ko(exp, regex_name):
     """Verify that check_exp() returns False with incorrect params"""
@@ -125,8 +132,8 @@ def test_validation_check_exp_ko(exp, regex_name):
     ('etc/ossec.conf')
 ])
 def test_validation_paths_ok(relative_path):
-    """Verify that format_etc_file_path() returns True with correct params"""
-    assert format_etc_file_path(relative_path)
+    """Verify that format_edit_files_path() returns True with correct params"""
+    assert format_edit_files_path(relative_path)
 
 
 @pytest.mark.parametrize('relative_path', [
@@ -136,8 +143,8 @@ def test_validation_paths_ok(relative_path):
     ('etc/internal_options')
 ])
 def test_validation_paths_ko(relative_path):
-    """Verify that format_etc_file_path() returns False with incorrect params"""
-    assert not format_etc_file_path(relative_path)
+    """Verify that format_edit_files_path() returns False with incorrect params"""
+    assert not format_edit_files_path(relative_path)
 
 
 @pytest.mark.parametrize('cdb_list', [
@@ -196,10 +203,10 @@ def test_is_safe_path():
 @pytest.mark.parametrize('value, format', [
     ("test.33alphanumeric:", "alphanumeric"),
     ("cGVwZQ==", "base64"),
-    ("etc/lists/list.csv", "etc_file_path"),
-    ("etc/decoders/dec35.xml", "etc_and_ruleset_file_path"),
-    ("etc/decoders/test/dec35.xml", "etc_and_ruleset_file_path"),
-    ("etc/decoders", "etc_and_ruleset_path"),
+    ("etc/lists/list_me", "delete_files_path"),
+    ("etc/decoders/dec35.xml", "edit_files_path"),
+    ("etc/decoders/test/dec35.xml", "edit_files_path"),
+    ("etc/decoders", "get_dirnames_path"),
     ("AB0264EA00FD9BCDCF1A5B88BC1BDEA4", "hash"),
     ("file_test-33.xml", "names"),
     ("651403650840", "numbers"),
@@ -235,8 +242,13 @@ def test_validation_json_ok(value, format):
 @pytest.mark.parametrize('value, format', [
     ("~test.33alphanumeric:", "alphanumeric"),
     ("cGVwZQ===", "base64"),
-    ("etc/../lists/list.csv", "etc_and_ruleset_file_path"),
-    ("/etc/decoders/dec35.xml", "etc_and_ruleset_file_path"),
+    ("etc/../lists/list.csv", "edit_files_path"),
+    ("/etc/decoders/dec35.xml", "edit_files_path"),
+    ("/etc/decoders/../../dec35.xml", "edit_files_path"),
+    ("/etc/decoders./dec35.xml", "delete_files_path"),
+    ("/etc/ossec.conf", "delete_files_path"),
+    ("/etc/client.keys", "get_files_path"),
+    ("/etc/ossec.conf", "edit_files_path"),
     ("AB0264EA00FD9BCDCF1A5B88BC1BDEA4.", "hash"),
     ("../../file_test-33.xml", "names"),
     ("a651403650840", "numbers"),
--- api/api/validator.py
+++ api/api/validator.py
@@ -35,10 +35,11 @@ _wazuh_key = re.compile(r'[a-zA-Z0-9]+$')
 _paths = re.compile(r'^[\w\-\.\\\/:]+$')
 _query_param = re.compile(r"^(?:[\w\.\-]+(?:=|!=|<|>|~)[\w\.\- ]+)(?:(?:;|,)[\w\.\-]+(?:=|!=|<|>|~)[\w\.\- ]+)*$")
 _ranges = re.compile(r'[\d]+$|^[\d]{1,2}\-[\d]{1,2}$')
-_etc_file_path = re.compile(r'^etc\/(ossec\.conf|(rules|decoders)\/[\w\-\/]+\.xml|lists\/[\w\-\.\/]+)$')
-_etc_and_ruleset_file_path = re.compile(
-    r'(^etc\/ossec\.conf$)|(^(etc|ruleset)\/(decoders|rules)\/[\w\-\/]+\.{1}xml$)|(^etc\/lists\/[\w\-\.\/]+)$')
-_etc_and_ruleset_path = re.compile(r'(^(etc|ruleset)\/(decoders|rules|lists(\/[\w\-\_]+)*))$')
+_edit_files_path = re.compile(r'^etc\/(ossec\.conf|(rules|decoders)\/[\w\-\/]+\.xml|lists\/[\w\-\/]+)$')
+_delete_files_path = re.compile(r'^etc\/((rules|decoders)\/[\w\-\/]+\.xml|lists\/[\w\-\/]+)$')
+_get_files_path = re.compile(
+    r'(^etc\/ossec\.conf$)|(^(etc|ruleset)\/(decoders|rules)\/[\w\-]+\.{1}xml$)|(^etc\/lists\/[\w\-\/]+)$')
+_get_dirnames_path = re.compile(r'^(((etc|ruleset)\/(decoders|rules)[\w\-\/]*)|(etc\/lists[\w\-\/]*))$')
 _search_param = re.compile(r'^[^;\|&\^*>]+$')
 _sort_param = re.compile(r'^[\w_\-\,\s\+\.]+$')
 _timeframe_type = re.compile(r'^(\d{1,}[d|h|m|s]?){1}$')
@@ -108,7 +109,11 @@ def is_safe_path(path: str, basedir: str = common.ossec_path, follow_symlinks: b
     :param follow_symlinks: True if path is relative, False if it is absolute
     :return: True if path is correct, False otherwise
     """
-    # resolves symbolic links
+    # Protect path
+    if './' in path or '../' in path:
+        return False
+
+    # Resolve symbolic links
     if follow_symlinks:
         full_path = common.ossec_path + path
         return os.path.realpath(full_path).startswith(basedir)
@@ -131,43 +136,56 @@ def format_base64(value):
     return check_exp(value, _base64)
 
 
-@draft4_format_checker.checks("etc_file_path")
-def format_etc_file_path(relative_path):
-    """
-    Function to check if a relative path file is allowed
-    :param relative_path: file path string to check
-    :return: True if path is OK, False otherwise
-    """
-    if not is_safe_path(relative_path):
-        return False
-
-    return check_exp(relative_path, _etc_file_path)
-
-
-@draft4_format_checker.checks("etc_and_ruleset_file_path")
-def format_etc_and_ruleset_file_path(relative_path):
-    """
-    Function to check if a relative path file is allowed
-    :param relative_path: file path string to check
-    :return: True if path is OK, False otherwise
-    """
-    if not is_safe_path(relative_path):
-        return False
-
-    return check_exp(relative_path, _etc_and_ruleset_file_path)
-
-
-@draft4_format_checker.checks("etc_and_ruleset_path")
+@draft4_format_checker.checks("edit_files_path")
 def format_edit_files_path(relative_path):
     """
-    Function to check if a relative path is allowed
+    Function to check if a relative path file is allowed to be uploaded or edited
+    :param relative_path: file path string to check
+    :return: True if path is OK, False otherwise
+    """
+    if not is_safe_path(relative_path):
+        return False
+
+    return check_exp(relative_path, _edit_files_path)
+
+
+@draft4_format_checker.checks("delete_files_path")
+def format_delete_files_path(relative_path):
+    """
+    Function to check if a relative path file is allowed to be deleted
+    :param relative_path: file path string to check
+    :return: True if path is OK, False otherwise
+    """
+    if not is_safe_path(relative_path):
+        return False
+
+    return check_exp(relative_path, _delete_files_path)
+
+
+@draft4_format_checker.checks("get_files_path")
+def format_get_files_path(relative_path):
+    """
+    Function to check if a relative path file is allowed to be read
+    :param relative_path: file path string to check
+    :return: True if path is OK, False otherwise
+    """
+    if not is_safe_path(relative_path):
+        return False
+
+    return check_exp(relative_path, _get_files_path)
+
+
+@draft4_format_checker.checks("get_dirnames_path")
+def format_get_dirnames_path(relative_path):
+    """
+    Function to check if a relative path is allowed to be read
     :param relative_path: path string to check
     :return: True if path is OK, False otherwise
     """
     if not is_safe_path(relative_path):
         return False
 
-    return check_exp(relative_path, _etc_and_ruleset_path)
+    return check_exp(relative_path, _get_dirnames_path)
 
 
 @draft4_format_checker.checks("hash")
