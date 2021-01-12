d04c53bee0e630534e6e4cf9af80cc9f73211456
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-01-12 03:16:11 -0300

    Make ossec-eps.sh check wazuh-control for Wazuh env var

--- contrib/ossec-eps.sh
+++ contrib/ossec-eps.sh
@@ -3,21 +3,34 @@
 # Author Michael Starks ossec [at] michaelstarks [dot] com
 # License: GPLv3
 
-if [ ! -e /etc/ossec-init.conf ]; then
-  echo OSSEC does not appear to be installed on this system. Goodbye.
+WAZUH_HOME=${1}
+
+#Check syntax
+if [ "X${WAZUH_HOME}" = "X" ]; then
+  echo "Usage: $0 WAZUH_HOME"
   exit 1
-else
-  grep -q agent /etc/ossec-init.conf && echo This script can only be run on the manager. Goodbye. && exit 1
+fi
+
+eval $(${WAZUH_HOME}/bin/wazuh-control info 2>/dev/null)
+
+#Check if Wazuh is installed
+if [ "X${WAZUH_TYPE}" = "X" ]; then
+  echo "Wazuh does not appear to be installed on this system."
+  exit 1
+fi
+
+#Check if it is Wazuh Manager
+if [ "${WAZUH_TYPE}" = "agent" ]; then
+  echo "This script can only be run on the manager."
+  exit 1
 fi
 
 #Reset counters
 COUNT=0
 EPSSUM=0
 EPSAVG=0
-#Source OSSEC Dir
-. /etc/ossec-init.conf
 
-for i in $(grep 'Total events for day' ${DIRECTORY}/stats/totals/*/*/ossec-totals-*.log | cut -d: -f3); do
+for i in $(grep 'Total events for day' ${WAZUH_HOME}/stats/totals/*/*/ossec-totals-*.log | cut -d: -f3); do
   COUNT=$((COUNT+1))
   DAILYEVENTS=$i
   EPSSUM=$(($DAILYEVENTS+$EPSSUM))
