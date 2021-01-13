b657b67febff509faf9c6fb3ef7905d06dad287d
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2021-01-13 11:52:49 +0100

    Set TLSv1.2 as default TLS protocol

--- api/api/configuration.py
+++ api/api/configuration.py
@@ -33,7 +33,8 @@ default_api_configuration = {
         "key": "api/configuration/ssl/server.key",
         "cert": "api/configuration/ssl/server.crt",
         "use_ca": False,
-        "ca": "api/configuration/ssl/ca.crt"
+        "ca": "api/configuration/ssl/ca.crt",
+        "tls_protocol": "TLSv1.2"
     },
     "logs": {
         "level": "info",
