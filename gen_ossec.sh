13868e93ea404b3596baa236c5ae3a75728e5471
Diego Provinciani <diegoprovinciani@gmail.com>
2021-01-14 17:37:41 -0300

    Deprecate the ossec-init.conf generation option from the gen_ossec.sh script

--- gen_ossec.sh
+++ gen_ossec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Wazuh Configuration & Init Files Generator
-# Copyright (C) 2015-2020, Wazuh Inc.
+# Copyright (C) 2015-2021, Wazuh Inc.
 # November 24, 2016.
 #
 # This program is free software; you can redistribute it
@@ -9,60 +9,38 @@
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
 
-# File dependencies:
-# ./src/init/shared.sh
-# ./src/init/inst-functions.sh
-# ./src/init/template-select.sh
-# ./src/init/dist-detect.sh
-# ./src/VERSION
-# ./src/LOCATION
-# ./etc/templates/config
-
-
 # Looking up for the execution directory
 cd `dirname $0`
 
-Conf_Use()
+Use()
 {
-  echo " USE: ./gen_ossec.sh conf install_type distribution version [installation_path]"
+  echo " USE: ./gen_ossec.sh install_type distribution version [installation_path]"
   echo "   - install_type: manager, agent, local"
   echo "   - distribution: rhel, debian, ubuntu, ..."
   echo "   - version: 6, 7, 16.04, ..."
   echo "   - installation_path (optional): changes the default path '/var/ossec' "
 }
 
-Init_Use()
-{
-  echo " USE: ./gen_ossec.sh init install_type [installation_path]"
-  echo "   - install_type: manager, agent, local"
-  echo "   - installation_path (optional): changes the default path '/var/ossec' "
-}
-
 # Read script values
-if [ "$1" = "conf" ]; then
+if [ "$#" -ge "3" ]; then
 
   . ./src/init/shared.sh
   . ./src/init/inst-functions.sh
 
-  if [ "$#" -ge "4" ]; then
-    INSTYPE=$(echo $2 | tr '[:upper:]' '[:lower:]')
-    if [ "$INSTYPE" = "manager" ]; then
-        INSTYPE="server"
-    fi
-    DIST_NAME=$(echo $3 | tr '[:upper:]' '[:lower:]')
-    if [ $(echo $4 | grep "\.") ]; then
-      DIST_VER=$(echo $4 | cut -d\. -f1)
-      DIST_SUBVER=$(echo $4 | cut -d\. -f2)
-    else
-      DIST_VER="$4"
-      DIST_SUBVER="0"
-    fi
-    if [ "$#" = "5" ]; then
-      INSTALLDIR="$5"
-    fi
+  INSTYPE=$(echo $1 | tr '[:upper:]' '[:lower:]')
+  if [ "$INSTYPE" = "manager" ]; then
+      INSTYPE="server"
+  fi
+  DIST_NAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
+  if [ $(echo $3 | grep "\.") ]; then
+    DIST_VER=$(echo $3 | cut -d\. -f1)
+    DIST_SUBVER=$(echo $3 | cut -d\. -f2)
   else
-    Conf_Use
-    exit 1
+    DIST_VER="$3"
+    DIST_SUBVER="0"
+  fi
+  if [ "$#" = "4" ]; then
+    INSTALLDIR="$4"
   fi
 
   # Default values definition
@@ -86,10 +64,10 @@ if [ "$1" = "conf" ]; then
     WriteManager "no_localfiles"
   elif [ "$INSTYPE" = "agent" ]; then
     WriteAgent "no_localfiles"
-elif [ "$INSTYPE" = "local" ]; then
+  elif [ "$INSTYPE" = "local" ]; then
     WriteLocal "no_localfiles"
   else
-    Conf_Use
+    Use
     exit 1
   fi
 
@@ -97,39 +75,11 @@ elif [ "$INSTYPE" = "local" ]; then
   rm "$NEWCONFIG"
 
   exit 0
-
-elif [ "$1" = "init" ]; then
-
-  . ./src/init/inst-functions.sh
-  . ./src/init/shared.sh
-
-  # Read script values
-  if [ "$#" -ge "2" ]; then
-    INSTYPE=$(echo $2 | tr '[:upper:]' '[:lower:]')
-    if [ "$INSTYPE" = "manager" ]; then
-        INSTYPE="server"
-    fi
-    if [ "$#" = "3" ]; then
-      INSTALLDIR="$3"
-    fi
-  else
-    Init_Use
-    exit 1
-  fi
-
-  GenerateInitConf
-
-  exit 0
-
 else
   echo ""
-  echo "Wazuh Configuration & Init Files Generator"
+  echo "Wazuh Configuration Generator"
   echo ""
-  echo " Generate a default ossec.conf file."
-  Conf_Use
-  echo ""
-  echo " Generate a default ossec-init.conf file."
-  Init_Use
+  Use
   echo ""
   exit 1
 fi
