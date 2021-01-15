8e62709da828c5d010c2356a98265773e92f8d53
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-01-15 01:48:27 -0300

    Adapt gcloud tests to wazuh-control

--- wodles/gcloud/tests/test_tools.py
+++ wodles/gcloud/tests/test_tools.py
@@ -10,25 +10,19 @@
 
 import os
 import sys
-from unittest.mock import mock_open, patch
+from unittest.mock import patch
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))  # noqa: E501
-from tests.common import mock_ossec_init
-from tools import get_wazuh_paths
+from tools import get_wazuh_version
 
-test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                              'data')
+wazuh_control_info = 'WAZUH_VERSION="TEST_VERSION"\n\
+                      WAZUH_REVISION="TEST_REVISION"\n\
+                      WAZUH_TYPE="TEST_TYPE"\n'
 
+def test_get_wazuh_version():
+    """Test get_wazuh_version function."""
+        
+    with patch(f'tools.call_wazuh_control', return_value=wazuh_control_info):
+        wazuh_version = get_wazuh_version()
 
-@patch('tools.open', side_effect=mock_ossec_init())
-def test_get_wazuh_paths(mock_ossec_init):
-    """Test get_wazuh_paths function."""
-    expected_wazuh_path = os.path.join('/', 'var', 'ossec')
-    expected_wazuh_version = 'v3.11.0'
-    expected_wazuh_queue = os.path.join(expected_wazuh_path, 'queue', 'ossec',
-                                        'queue')
-    wazuh_path, wazuh_version, wazuh_queue = get_wazuh_paths()
-
-    assert expected_wazuh_path == wazuh_path
-    assert expected_wazuh_version == wazuh_version
-    assert expected_wazuh_queue == wazuh_queue
+    assert "TEST_VERSION" == wazuh_version
