b8be8abea59aa8f80a847a3f6d7244c89551b855
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-01-17 06:49:28 -0300

    Modify WPK upgrade scripts to use relative path and wazuh-control

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 # Copyright (C) 2015-2020, Wazuh Inc.
-. /etc/ossec-init.conf 2> /dev/null || exit 1
-(sleep 5 && chmod +x $DIRECTORY/var/upgrade/src/init/*.sh && $DIRECTORY/var/upgrade/src/init/pkg_installer.sh) >/dev/null 2>&1 &
+
+# Get Wazuh installation path
+SCRIPT=$(readlink -f "$0")
+WAZUH_HOME=$(dirname $(dirname $(dirname "$SCRIPT")))
+
+# Get Wazuh Info
+eval $(${WAZUH_HOME}/bin/wazuh-control info 2>/dev/null)
+if [ "X${WAZUH_VERSION}" = "X" ] ; then
+    . /etc/ossec-init.conf 2> /dev/null
+    if [ "X${VERSION}" = "X" ] ; then
+        exit 1
+    else
+        WAZUH_VERSION=${VERSION}
+    fi
+fi
+
+(sleep 5 && chmod +x ${WAZUH_HOME}/var/upgrade/src/init/*.sh && ${WAZUH_HOME}/var/upgrade/src/init/pkg_installer.sh ${WAZUH_HOME} ${WAZUH_VERSION}) >/dev/null 2>&1 &
