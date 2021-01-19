6c9bd123b8545942d3d118c6472183be9981bedf
Tomas Turina <tomas.turina@wazuh.com>
2021-01-19 16:37:04 -0300

    Modify firewall check script

--- src/init/wazuh/wazuh.sh
+++ src/init/wazuh/wazuh.sh
@@ -163,6 +163,7 @@ WazuhUpgrade()
 
     # Remove old Active Response scripts
 
+    rm -f $DIRECTORY/active-response/bin/firewall-drop.sh
     rm -f $DIRECTORY/active-response/bin/default-firewall-drop.sh
     rm -f $DIRECTORY/active-response/bin/disable-account.sh
     rm -f $DIRECTORY/active-response/bin/restart-ossec.sh
