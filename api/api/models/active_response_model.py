3dad6eeeac8541bcc3d3713c65ac942d2c5642e9
Manuel <manuel.carmona@wazuh.com>
2021-01-28 11:42:45 +0100

    Update active response model docstrings

--- api/api/models/active_response_model.py
+++ api/api/models/active_response_model.py
@@ -13,12 +13,14 @@ class ActiveResponseModel(Body):
     def __init__(self, command: str = None, custom: bool = None, arguments: List[str] = None, alert: dict = None):
         """ActiveResponseModel body model
 
-        :param command: Command running in the agent. If this value starts by !, then it refers to a script name instead of a command name
-        :type command: str
-        :param custom: Whether the specified command is a custom command or not
-        :type custom: bool
-        :param arguments: Command arguments
-        :type arguments: str
+        Parameters
+        ----------
+        command : str
+            Command running in the agent. If this value starts by !, then it refers to a script name instead of a command name
+        custom : bool
+            Whether the specified command is a custom command or not
+        arguments : List[str]
+            Command arguments
         """
         self.swagger_types = {
             'command': str,
@@ -42,62 +44,79 @@ class ActiveResponseModel(Body):
     @property
     def command(self) -> str:
         """
-        :return: Command to run in the agent
-        :rtype: str
+        Returns
+        -------
+        command : str
+            Command to run in the agent
         """
         return self._command
 
     @command.setter
     def command(self, command: str):
-        """Command running in the agent.
-
-        :param command: Command to run in the agent
+        """
+        Parameters
+        ----------
+        command : str
+            Command to run in the agent
         """
         self._command = command
 
     @property
     def custom(self) -> bool:
         """
-        :return: Whether the specified command is a custom command or not
-        :rtype: bool
+        Returns
+        -------
+        custom : bool
+            Whether the specified command is a custom command or not
         """
         return self._custom
 
     @custom.setter
     def custom(self, custom: bool):
         """
-        :param command: Whether the specified command is a custom command or not
+        Parameters
+        ----------
+        custom : bool
+            Whether the specified command is a custom command or not
         """
         self._custom = custom
 
     @property
     def arguments(self) -> List[str]:
         """
-        :return: Command arguments
-        :rtype: List[str]
+        Returns
+        -------
+        arguments : List[str]
+            Command arguments
         """
         return self._arguments
 
     @arguments.setter
     def arguments(self, arguments: List[str]):
-        """Command running in the agent.
-
-        :param arguments: Command arguments
+        """
+        Parameters
+        ----------
+        arguments : List[str]
+            Command arguments
         """
         self._arguments = arguments
 
     @property
     def alert(self) -> dict:
         """
-        :return: alert
-        :rtype: dict
+        Returns
+        -------
+        alert : dict
+            Alert data sent with the AR command
         """
         return self._alert
 
     @alert.setter
     def alert(self, alert: List[str]):
-        """Command running in the agent.
-
-        :param alert: Command arguments
+        """
+        Parameters
+        ----------
+        alert : dict
+            Alert data sent with the AR command
         """
         self._alert = alert
