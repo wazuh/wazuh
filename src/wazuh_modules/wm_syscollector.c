fbff0ede73818876fece1d67d8b4ddc13c80c76f
Dword <octa.tala@gmail.com>
2021-02-04 10:18:22 -0300

    HPUX shared modules compilation skip

--- src/wazuh_modules/wm_syscollector.c
+++ src/wazuh_modules/wm_syscollector.c
@@ -92,7 +92,11 @@ void* wm_sys_main(wm_sys_t *sys) {
         syscollector_stop_ptr = so_get_function_sym(syscollector_module, "syscollector_stop");
         syscollector_sync_message_ptr = so_get_function_sym(syscollector_module, "syscollector_sync_message");
     } else {
+#ifdef __hpux
+        mtinfo(WM_SYS_LOGTAG, "Not supported in HP-UX.");
+#else
         mterror(WM_SYS_LOGTAG, "Can't load syscollector.");
+#endif
         pthread_exit(NULL);
     }
     if (syscollector_start_ptr) {
