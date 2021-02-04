d7c4405e7f5740e68f8e2c732a0ed1d5b81b3343
Manuel <manuel.carmona@wazuh.com>
2021-02-04 08:33:26 +0100

    Fix Active Response model

--- api/api/models/active_response_model.py
+++ api/api/models/active_response_model.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 
 from datetime import date, datetime  # noqa: F401
+from typing import List
 
 from api.models.base_model_ import Body
 
@@ -27,7 +28,7 @@ class ActiveResponseModel(Body):
         self.swagger_types = {
             'command': str,
             'custom': bool,
-            'arguments': list,
+            'arguments': List[str],
             'alert': dict
         }
 
