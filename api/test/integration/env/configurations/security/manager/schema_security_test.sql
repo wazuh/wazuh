1f5d298310cd14098d743dd281db9e62a491527d
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2021-02-11 12:38:51 +0100

    Update and deprecate policies

--- api/test/integration/env/configurations/security/manager/schema_security_test.sql
+++ api/test/integration/env/configurations/security/manager/schema_security_test.sql
@@ -33,35 +33,6 @@ INSERT INTO rules VALUES(103,'rule4','{"MATCH": {"definition": "administratorRul
 INSERT INTO rules VALUES(104,'rule5','{"MATCH": {"definition": "normalRule"}}','1970-01-01 00:00:00');
 INSERT INTO rules VALUES(105,'rule6','{"MATCH": {"definition": "ossecRule"}}','1970-01-01 00:00:00');
 
-/* Default policies */
-INSERT INTO policies VALUES(1,'agents_all_resourceless','{"actions": ["agent:create", "group:create"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.630121');
-INSERT INTO policies VALUES(2,'agents_all_agents','{"actions": ["agent:read", "agent:delete", "agent:modify_group", "agent:restart", "agent:upgrade"], "resources": ["agent:id:*", "agent:group:*"], "effect": "allow"}','2020-06-16 14:34:31.640870');
-INSERT INTO policies VALUES(3,'agents_all_groups','{"actions": ["group:read", "group:delete", "group:update_config", "group:modify_assignments"], "resources": ["group:id:*"], "effect": "allow"}','2020-06-16 14:34:31.650961');
-INSERT INTO policies VALUES(4,'agents_read_agents','{"actions": ["agent:read"], "resources": ["agent:id:*", "agent:group:*"], "effect": "allow"}','2020-06-16 14:34:31.660302');
-INSERT INTO policies VALUES(5,'agents_read_groups','{"actions": ["group:read"], "resources": ["group:id:*"], "effect": "allow"}','2020-06-16 14:34:31.668318');
-INSERT INTO policies VALUES(6,'agents_commands_agents','{"actions": ["active-response:command"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.676223');
-INSERT INTO policies VALUES(7,'security_all_resourceless','{"actions": ["security:create", "security:create_user", "security:read_config", "security:update_config", "security:revoke"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.684085');
-INSERT INTO policies VALUES(8,'security_all_security','{"actions": ["security:read", "security:update", "security:delete"], "resources": ["role:id:*", "rule:id:*", "policy:id:*", "user:id:*"], "effect": "allow"}','2020-06-16 14:34:31.691953');
-INSERT INTO policies VALUES(9,'users_all_resourceless','{"actions": ["security:create_user", "security:revoke"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.699822');
-INSERT INTO policies VALUES(10,'users_all_users','{"actions": ["security:read", "security:update", "security:delete"], "resources": ["user:id:*"], "effect": "allow"}','2020-06-16 14:34:31.707888');
-INSERT INTO policies VALUES(11,'ciscat_read_ciscat','{"actions": ["ciscat:read"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.716023');
-INSERT INTO policies VALUES(12,'decoders_read_decoders','{"actions": ["decoders:read"], "resources": ["decoder:file:*"], "effect": "allow"}','2020-06-16 14:34:31.724054');
-INSERT INTO policies VALUES(13,'mitre_read_mitre','{"actions": ["mitre:read"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.732129');
-INSERT INTO policies VALUES(14,'lists_read_rules','{"actions": ["lists:read"], "resources": ["list:file:*"], "effect": "allow"}','2020-06-16 14:34:31.743865');
-INSERT INTO policies VALUES(15,'rules_read_rules','{"actions": ["rules:read"], "resources": ["rule:file:*"], "effect": "allow"}','2020-06-16 14:34:31.760616');
-INSERT INTO policies VALUES(16,'sca_read_sca','{"actions": ["sca:read"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.760616');
-INSERT INTO policies VALUES(17,'syscheck_read_syscheck','{"actions": ["syscheck:read"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.771236');
-INSERT INTO policies VALUES(18,'syscheck_all_syscheck','{"actions": ["syscheck:clear", "syscheck:read", "syscheck:run"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.793929');
-INSERT INTO policies VALUES(19,'syscollector_read_syscollector','{"actions": ["syscollector:read"], "resources": ["agent:id:*"], "effect": "allow"}','2020-06-16 14:34:31.809009');
-INSERT INTO policies VALUES(20,'cluster_all_resourceless','{"actions": ["cluster:status", "manager:read", "manager:read_api_config", "manager:upload_file", "manager:restart", "manager:delete_file"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.823710');
-INSERT INTO policies VALUES(21,'cluster_all_files','{"actions": ["manager:delete_file", "manager:read_file"], "resources": ["file:path:*"], "effect": "allow"}','2020-06-16 14:34:31.837608');
-INSERT INTO policies VALUES(22,'cluster_all_nodes','{"actions": ["cluster:delete_file", "cluster:read_api_config", "cluster:read", "cluster:read_api_config", "cluster:restart", "cluster:upload_file"], "resources": ["node:id:*"], "effect": "allow"}','2020-06-16 14:34:31.852327');
-INSERT INTO policies VALUES(23,'cluster_all_combination','{"actions": ["cluster:read_file", "cluster:delete_file"], "resources": ["node:id:*&file:path:*"], "effect": "allow"}','2020-06-16 14:34:31.866994');
-INSERT INTO policies VALUES(24,'cluster_read_resourceless','{"actions": ["cluster:status", "manager:read", "manager:read_api_config"], "resources": ["*:*:*"], "effect": "allow"}','2020-06-16 14:34:31.881676');
-INSERT INTO policies VALUES(25,'cluster_read_files','{"actions": ["manager:read", "manager:read_api_config"], "resources": ["file:path:*"], "effect": "allow"}','2020-06-16 14:34:31.896095');
-INSERT INTO policies VALUES(26,'cluster_read_nodes','{"actions": ["cluster:read_api_config", "cluster:read", "cluster:read_api_config"], "resources": ["node:id:*"], "effect": "allow"}','2020-06-16 14:34:31.911005');
-INSERT INTO policies VALUES(27,'cluster_read_combination','{"actions": ["cluster:read_file"], "resources": ["node:id:*&file:path:*"], "effect": "allow"}','2020-06-16 14:34:31.925851');
-
 /* Testing */
 INSERT INTO policies VALUES(100,'wazuhPolicy','{"actions": ["*:*"], "resources": ["*:*"], "effect": "allow"}','1970-01-01 00:00:00');
 INSERT INTO policies VALUES(101,'wazuh-wuiPolicy','{"actions": ["agent:create"], "effect": "allow", "resources": ["agent:id:001", "agent:id:002", "agent:id:003"]}','1970-01-01 00:00:00');
