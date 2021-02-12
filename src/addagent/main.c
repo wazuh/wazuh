e8890e9346281af7c35fb6ed36831429c285bbf0
Daniel Melgarejo Garcia <danimegar@gmail.com>
2021-02-12 09:20:10 +0100

    Adapted addagent file to work under relative paths

--- src/addagent/main.c
+++ src/addagent/main.c
@@ -87,8 +87,6 @@ int main(int argc, char **argv)
     const char *cmdimport = NULL;
     const char *cmdbulk = NULL;
 #ifndef WIN32
-    home_path = w_homedir(argv[0]);
-    const char *dir = HOMEDIR;
     const char *group = GROUPGLOBAL;
     gid_t gid;
 #else
@@ -97,6 +95,13 @@ int main(int argc, char **argv)
 
     /* Set the name */
     OS_SetName(ARGV0);
+#ifndef WIN32
+    char * home_path = w_homedir(argv[0]);
+    if (chdir(home_path) == -1) {
+        merror_exit(CHDIR_ERROR, home_path, errno, strerror(errno));
+    }
+    mdebug1(WAZUH_HOMEDIR, home_path);
+#endif
 
     while ((c = getopt(argc, argv, "Vhle:r:i:f:ja:n:F:L")) != -1) {
         switch (c) {
@@ -232,9 +237,11 @@ int main(int argc, char **argv)
     }
 
     /* Chroot to the default directory */
-    if (Privsep_Chroot(dir) < 0) {
-        merror_exit(CHROOT_ERROR, dir, errno, strerror(errno));
+    if (Privsep_Chroot(home_path) < 0) {
+        merror_exit(CHROOT_ERROR, home_path, errno, strerror(errno));
     }
+
+    os_free(home_path);
 
     /* Inside chroot now */
     nowChroot();
@@ -346,8 +353,5 @@ int main(int argc, char **argv)
         printf(EXIT);
     }
 
-#ifndef WIN32
-    os_free(home_path);
-#endif
     return (0);
 }
