eda69a17e5c468d0fb8119a63ea6b53ac8a71cc9
Daniel Melgarejo Garcia <danimegar@gmail.com>
2021-02-12 10:34:45 +0100

    Removed DEFAULTARPATH in active-resp config file

--- src/config/active-response.c
+++ src/config/active-response.c
@@ -52,9 +52,11 @@ int ReadActiveResponses(XML_NODE node, void *d1, void *d2)
     active_response *tmp_ar;
 
     /* Open shared ar file */
-    fp = fopen(DEFAULTARPATH, "a");
+    fp = fopen(DEFAULTAR, "a");
     if (!fp) {
-        merror(FOPEN_ERROR, DEFAULTARPATH, errno, strerror(errno));
+        char buffer[PATH_MAX] = {'\0'};
+        abspath(DEFAULTAR, buffer, PATH_MAX);
+        merror(FOPEN_ERROR, buffer, errno, strerror(errno));
         return (-1);
     }
 
@@ -67,7 +69,7 @@ int ReadActiveResponses(XML_NODE node, void *d1, void *d2)
         return OS_INVALID;
     }
 
-    if ((chown(DEFAULTARPATH, (uid_t) - 1, gid)) == -1) {
+    if ((chown(DEFAULTAR, (uid_t) - 1, gid)) == -1) {
         merror("Could not change the group to '%s': %d.", GROUPGLOBAL, errno);
         fclose(fp);
         return OS_INVALID;
@@ -75,7 +77,7 @@ int ReadActiveResponses(XML_NODE node, void *d1, void *d2)
 
 #endif
 
-    if ((chmod(DEFAULTARPATH, 0640)) == -1) {
+    if ((chmod(DEFAULTAR, 0640)) == -1) {
         merror("Could not chmod to 0640: '%d'", errno);
         fclose(fp);
         return (-1);
@@ -282,7 +284,7 @@ int ReadActiveResponses(XML_NODE node, void *d1, void *d2)
              tmp_ar->timeout);
 
     /* Add to shared file */
-    mdebug1("Writing command '%s' to '%s'", tmp_ar->command, DEFAULTARPATH);
+    mdebug1("Writing command '%s' to '%s'", tmp_ar->command, DEFAULTAR);
     fprintf(fp, "%s - %s - %d\n",
             tmp_ar->name,
             tmp_ar->ar_cmd->executable,
