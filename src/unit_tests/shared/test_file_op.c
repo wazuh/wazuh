e03b3cc0370dfd35f35fb9c56dd24ff527e05d53
Tomas Turina <tomas.turina@wazuh.com>
2021-02-18 17:33:28 -0300

    Translate shared tests to cmocka

--- src/unit_tests/shared/test_file_op.c
+++ src/unit_tests/shared/test_file_op.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2020, Wazuh Inc.
+ * Copyright (C) 2015-2021, Wazuh Inc.
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public
@@ -613,6 +613,35 @@ void test_get_UTC_modification_time_fail_get_filetime(void **state) {
 }
 #endif
 
+void test_get_file_content(void **state)
+{
+    int max_size = 300;
+    char * content;
+    const char * expected = "test string";
+    const char * file_name = "test_file.txt";
+
+    expect_string(__wrap_fopen, path, file_name);
+    expect_string(__wrap_fopen, mode, "r");
+    will_return(__wrap_fopen, 1);
+
+    will_return(__wrap_ftell, 0);
+    will_return(__wrap_fseek, 0);
+    will_return(__wrap_ftell, 11); // Content size
+    will_return(__wrap_fseek, 0);
+
+    will_return(__wrap_fread, expected);
+    will_return(__wrap_fread, strlen(expected));
+
+    expect_value(__wrap_fclose, _File, 1);
+    will_return(__wrap_fclose, 0);
+
+    content = w_get_file_content(file_name, max_size);
+
+    assert_string_equal(content, expected);
+
+    free(content);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
 #ifndef TEST_WINAGENT
@@ -638,14 +667,13 @@ int main(void) {
         cmocka_unit_test(test_w_uncompress_gzfile_gzopen_fail),
         cmocka_unit_test(test_w_uncompress_gzfile_first_read_fail),
         cmocka_unit_test(test_w_uncompress_gzfile_first_read_success),
-        cmocka_unit_test(test_w_uncompress_gzfile_success)
+        cmocka_unit_test(test_w_uncompress_gzfile_success),
 #else
         cmocka_unit_test(test_get_UTC_modification_time_success),
         cmocka_unit_test(test_get_UTC_modification_time_fail_get_handle),
-        cmocka_unit_test(test_get_UTC_modification_time_fail_get_filetime)
+        cmocka_unit_test(test_get_UTC_modification_time_fail_get_filetime),
 #endif
-
-
+        cmocka_unit_test(test_get_file_content),
     };
     return cmocka_run_group_tests(tests, setup_group, teardown_group);
 }
