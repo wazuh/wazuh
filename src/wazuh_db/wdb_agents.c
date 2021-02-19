1bfb168d11ee44307347997c0bc2b26036cc70f6
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-02-19 14:40:52 -0300

    Refactor vuln_cve functions

--- src/wazuh_db/wdb_agents.c
+++ src/wazuh_db/wdb_agents.c
@@ -1,22 +1,22 @@
 #include "wdb.h"
 
 int wdb_agents_insert_vuln_cve(wdb_t *wdb, const char* name, const char* version, const char* architecture, const char* cve) {
-    
+
     sqlite3_stmt* stmt = wdb_start_cached_transaction(wdb, WDB_STMT_VULN_CVE_INSERT);
     if (stmt == NULL) {
         return OS_INVALID;
-    }  
-    
+    }
+
     sqlite3_bind_text(stmt, 1, name, -1, NULL);
     sqlite3_bind_text(stmt, 2, version, -1, NULL);
-    sqlite3_bind_text(stmt, 3, architecture, -1, NULL); 
+    sqlite3_bind_text(stmt, 3, architecture, -1, NULL);
     sqlite3_bind_text(stmt, 4, cve, -1, NULL);
-     
+
     return wdb_exec_stmt_silent(stmt);
 }
 
 int wdb_agents_clear_vuln_cve(wdb_t *wdb) {
-    
+
     sqlite3_stmt* stmt = wdb_start_cached_transaction(wdb, WDB_STMT_VULN_CVE_CLEAR);
     if (stmt == NULL) {
         return OS_INVALID;
