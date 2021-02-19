21f447ec3bdfaac7b6c352d9954eb10d00d51b51
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-02-19 16:52:18 -0300

    Add Doxygen

--- src/wazuh_db/wdb_agents.h
+++ src/wazuh_db/wdb_agents.h
@@ -13,8 +13,25 @@
 #define WDB_AGENTS_H
 
 #include "wdb.h"
-//JJP: Doxygen
+
+/**
+ * @brief Function to clear whole data from agent vuln_cve table.
+ *
+ * @param [in] wdb The Global struct database.
+ * @return Returns 0 on success or -1 on error.
+ */
 int wdb_agents_clear_vuln_cve(wdb_t *wdb);
+
+/**
+ * @brief Function to insert a new entry into the agent vuln_cve table.
+ *
+ * @param [in] wdb The Global struct database.
+ * @param [in] name The vulnerable package name.
+ * @param [in] version The vulnerable package version.
+ * @param [in] architecture The vulnerable package architecture.
+ * @param [in] cve The CVE id of the vulnerability.
+ * @return Returns 0 on success or -1 on error.
+ */
 int wdb_agents_insert_vuln_cve(wdb_t *wdb, const char* name, const char* version, const char* architecture, const char* cve);
 
 #endif
