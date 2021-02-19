7d3e5e0fb715017327dbd3fbeda4d9c50e94ead5
Diego Provinciani <diegoprovinciani@gmail.com>
2021-02-19 10:58:47 -0300

    Moving wdb_agents_helpers.c to the helpers folder and adding header file

---
+++ src/wazuh_db/helpers/wdb_agents_helpers.h
@@ -0,0 +1,11 @@
+/*
+ * Wazuh SQLite integration
+ * Copyright (C) 2015-2021, Wazuh Inc.
+ * February 17, 2021.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
