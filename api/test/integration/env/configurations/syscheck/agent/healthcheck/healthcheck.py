a5763de0c6a0712d4f796cd7e7cbf7e6b90c5f03
Manuel Carmona <manuel.carmona@wazuh.com>
2021-02-23 15:45:35 +0100

    Fix API integration tests failing in master (#7545)
    * Fix test_decoder, error when using q parameter
    
    * Update decoder api integration tests
    
    * Fix rbac decoder API integration tests
    
    * Update syscheck API integration test descriptions
    
    * Use base_healthcheck with the specific ones in the test environment
    
    * Update sca api integration test
    
    * Fix test rootcheck and active response
    
    * Update rootcheck and sca API integration tests
    
    * Fix API integration tests errors due to merging 4.1 into master
    
    * Minor fix to test_rbac_white_all

--- api/test/integration/env/configurations/syscheck/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/syscheck/agent/healthcheck/healthcheck.py
@@ -1,8 +1,11 @@
 import os
 
+from base_healthcheck import get_agent_health_base
+
 
 def get_health():
-    output = os.system("grep -q 'syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log")
+    output = os.system(
+        "grep -q 'syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log")
 
     if output == 0:
         return 0
@@ -11,4 +14,4 @@ def get_health():
 
 
 if __name__ == "__main__":
-    exit(get_health())
+    exit(get_health() or get_agent_health_base())
