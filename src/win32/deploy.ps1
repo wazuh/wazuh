b3c92e77e136aca09f7e467cde1d6d39a94b8924
dfolcha <daniel.folch@wazuh.com>
2021-02-23 17:33:21 +0100

    Change example versions

--- src/win32/deploy.ps1
+++ src/win32/deploy.ps1
@@ -37,16 +37,16 @@ Tool to Install and configure Windows Wazuh Agent
  Wazuh agent group
 
  .example
- Install Wazuh-agent 4.0.1 and set the Wazuh manager address
-./deploy.ps1 -version 4.0.1 -address [MANAGER_ADDRESS]
+ Install Wazuh-agent 4.2.0 and set the Wazuh manager address
+./deploy.ps1 -version 4.2.0 -address [MANAGER_ADDRESS]
 
  .example
- Install Wazuh-agent 4.0.1 set the Wazuh Manager address and register to a different manager
-./deploy.ps1 -version 4.0.1 -address [MANAGER_ADDRESS] -registration-address [REGISTRATION_ADDRESS]
+ Install Wazuh-agent 4.2.0 set the Wazuh Manager address and register to a different manager
+./deploy.ps1 -version 4.2.0 -address [MANAGER_ADDRESS] -registration-address [REGISTRATION_ADDRESS]
 
  .example
- Install Wazuh-agent 4.0.1 set the Wazuh Manager address and register using password
-./deploy.ps1 -version 4.0.1 -address [MANAGER_ADDRESS] -token [PASSWORD]
+ Install Wazuh-agent 4.2.0 set the Wazuh Manager address and register using password
+./deploy.ps1 -version 4.2.0 -address [MANAGER_ADDRESS] -token [PASSWORD]
 #> 
 
 param (
@@ -111,7 +111,7 @@ if($address){
 if($address){
    $params.Add("-registration-key","${registration-key}")
 }
-
+   
 $url = "https://packages-dev.wazuh.com/warehouse/test/4.2/windows/wazuh-agent-4.2.0-wazuhctl.msi"
 $output = "wazuh-agent.msi"
 
