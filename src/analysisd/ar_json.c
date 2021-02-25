09f67bb8479282cdd20e4e9a599fb207c81348ac
Tomas Turina <tomas.turina@wazuh.com>
2021-02-25 15:42:44 -0300

    Fix missing wazuh_config tag in AR

--- src/analysisd/ar_json.c
+++ src/analysisd/ar_json.c
@@ -23,7 +23,7 @@ static char *get_node_name()
     char* node_name = NULL;
     OS_XML xml;
 
-    const char *(xml_node[]) = {"ossec_config", "cluster", "node_name", NULL};
+    const char *(xml_node[]) = {"wazuh_config", "cluster", "node_name", NULL};
     if (OS_ReadXML(DEFAULTCPATH, &xml) >= 0) {
         node_name = OS_GetOneContentforElement(&xml, xml_node);
     }
