3f80225642eb1124a0b1a2620ff59fc9fd5a0a07
Pedro Nicolas Gomez Palacios <nicogp2@gmail.com>
2021-03-02 19:30:15 +0000

    Add the tables techniques data_source defense_bypassed to mitredb

--- tools/mitre/mitredb.py
+++ tools/mitre/mitredb.py
@@ -18,8 +18,9 @@ import argparse
 import sys
 from datetime import datetime
 from sqlalchemy import create_engine, Column, DateTime, String, Integer, ForeignKey, Boolean
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
 
 Base = declarative_base()
 
@@ -42,6 +43,85 @@ class Metadata(Base):
         self.name = name
         self.description = description
 
+class Technique(Base):
+    __tablename__ = "techniques"
+
+    id = Column('id', String, primary_key=True)
+    name = Column('name', String, nullable=False)
+    description = Column('description', String)
+    created_time = Column('created_time', DateTime)
+    modified_time = Column('modified_time' ,DateTime)
+    mitre_version = Column('mitre_version', String)
+    mitre_detection = Column('mitre_detection', String)
+    network_requirements = Column('network_requirements', Boolean, default=False)
+    remote_support = Column('remote_support', Boolean, default=False)
+    revoked_by = Column('revoked_by', String)
+    deprecated = Column('deprecated', Boolean, default=False)
+    subtechnique_of = Column('subtechnique_of', String)
+
+    data_sources = relationship("DataSource", backref="techniques")
+    defenses_bypassed = relationship("DefenseByPasses", backref="techniques")
+
+class DataSource(Base):
+    __tablename__ = "data_source"
+
+    id = Column('id', String, ForeignKey("techniques.id", ondelete='CASCADE'), primary_key=True)
+    source = Column('source', String, primary_key=True)
+
+class DefenseByPasses(Base):
+    __tablename__ = "defense_bypassed"
+
+    id = Column('id', String, ForeignKey("techniques.id", ondelete='CASCADE'), primary_key=True)
+    defense = Column('defense', String, primary_key=True)
+
+
+def parse_json_techniques(technique_json):
+    technique = Technique()
+
+    if technique_json.get('id'):
+        technique.id = technique_json['id']
+    if technique_json.get('name'):
+        technique.name = technique_json['name']
+    if technique_json.get('description'):
+        technique.description = technique_json['description']
+    if technique_json.get('created'):
+        technique.created_time = datetime.strptime(technique_json['created'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if technique_json.get('modified'):
+        technique.modified_time = datetime.strptime(technique_json['modified'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    if technique_json.get('x_mitre_version'):
+        technique.mitre_version = technique_json['x_mitre_version']
+    if technique_json.get('x_mitre_detection'):
+        technique.mitre_detection = technique_json['x_mitre_detection']
+    if technique_json.get('x_mitre_network_requirements'):
+        technique.network_requirements = technique_json['x_mitre_network_requirements']
+    if technique_json.get('x_mitre_remote_support'):
+        technique.remote_support = technique_json['x_mitre_remote_support']
+    if technique_json.get('revoked_by'):
+        technique.revoked_by = technique_json['revoked_by']
+    if technique_json.get('x_mitre_deprecated'):
+        technique.deprecated = technique_json['x_mitre_deprecated']
+    if technique_json.get('subtechnique_of'):
+        technique.subtechnique_of = technique_json['subtechnique_of']
+    if technique_json.get('x_mitre_data_sources'):
+        for data_source in list(set(technique_json['x_mitre_data_sources'])):
+            technique.data_sources.append(DataSource(techniques=technique, source=data_source))
+    if technique_json.get('x_mitre_defense_bypassed'):
+        for defense in list(set(technique_json['x_mitre_defense_bypassed'])):
+            technique.defenses_bypassed.append(DefenseByPasses(techniques=technique, defense=defense))
+    return technique
+
+
+def parse_json_relationships(relationships_json, session):
+    if relationships_json.get('relationship_type') == 'subtechnique-of':
+        technique = session.query(Technique).get(relationships_json['source_ref'])
+        technique.subtechnique_of = relationships_json['target_ref']
+        session.commit()
+    elif relationships_json.get('relationship_type') == 'revoked-by' and \
+            relationships_json['source_ref'].startswith("attack-pattern"):
+        technique = session.query(Technique).get(relationships_json['source_ref'])
+        technique.revoked_by = relationships_json['target_ref']
+        session.commit()
+
 
 def parse_json(pathfile, session, database):
     """
@@ -62,6 +142,12 @@ def parse_json(pathfile, session, database):
                     metadata.name = data_object['name']
                 elif data_object['type'] == 'marking-definition':
                     metadata.description = data_object['definition']['statement']
+                elif data_object['type'] == 'attack-pattern':
+                    technique = parse_json_techniques(data_object)
+                    session.add(technique)
+                    session.commit()
+                elif data_object['type'] == 'relationship':
+                    parse_json_relationships(data_object, session)
         session.add(metadata)
         session.commit()
 
