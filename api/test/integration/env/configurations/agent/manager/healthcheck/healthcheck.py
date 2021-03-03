c741768b405030fffd7aabc35fb733b7cf88525f
Selutario <selutario@gmail.com>
2021-03-03 08:46:47 +0100

    Add CVE rbac_white_all_endpoints test

--- api/test/integration/env/configurations/agent/manager/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/agent/manager/healthcheck/healthcheck.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     # Workers are not needed in this test, so the exit code is set to 0 (healthy).
     # os.system can return 256 (wait status), so WEXITSTATUS translate it to 'exit' status.
     exit(WEXITSTATUS(system(
-        "grep -q 'wazuh-modulesd:vulnerability-detector: INFO: (5471): Finished vulnerability assessment for agent '\\''001'\\''' /var/ossec/logs/ossec.log"))
+        "grep -q 'wazuh-modulesd:vulnerability-detector: INFO: (5471): Finished vulnerability assessment for agent '\\''002'\\''' /var/ossec/logs/ossec.log"))
          or get_manager_health_base()) if socket.gethostname() == 'wazuh-master' else exit(0)
