3185b3236900a643f273e22496fb35e2c6f62496
MarcelKemp <marcel.kempt@wazuh.com>
2021-03-05 17:32:53 +0100

    Applying the requested changes

--- src/init/darwin-delete-oldusers.sh
+++ src/init/darwin-delete-oldusers.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Copyright (C) 2015-2020, Wazuh Inc.
+# Copyright (C) 2015-2021, Wazuh Inc.
 
 #####
 # This checks for an error and exits with a custom message
@@ -10,11 +10,10 @@
 
 function check_errm
 {
-   if  [[ ${?} != "0" ]]
-      then
+   if  [[ ${?} != "0" ]]; then
       echo "${1}";
       exit ${2};
-      fi
+   fi
 }
 
 USER=ossec
@@ -23,31 +22,28 @@ USER_REM=ossecr
 GROUP=$1
 
 if ! [ $# -eq 1 ]; then
-    echo $#
-    echo "Usage: ${0} GROUPNAME.";
-    exit 1;
+   echo $#
+   echo "Usage: ${0} GROUPNAME.";
+   exit 1;
 fi
 
-if [[ ! -f "/usr/bin/dscl" ]]
-  then
-  echo "Error, I have no dscl, dying here";
-  exit
+if [[ ! -f "/usr/bin/dscl" ]]; then
+   echo "Unable to find dscl. Exiting.";
+   exit
 fi
 
 DSCL="/usr/bin/dscl";
 
 # Removing the users.
-
 OSSECUSERS="${USER_REM} ${USER_MAIL} ${USER}"
 
 for U in ${OSSECUSERS}; do
-    if [[ $(dscl . -read /Users/${U} 2>/dev/null) ]]
-       then
-       sudo ${DSCL} localhost -delete /Local/Default/Users/${U}
-       check_errm "Error removing user ${U}" "87"
-    else
-       echo "${U} don't exists";
-    fi
+   if [[ $(dscl . -read /Users/${U} 2>/dev/null) ]]; then
+      sudo ${DSCL} localhost -delete /Local/Default/Users/${U}
+      check_errm "Error removing user ${U}" "87"
+   else
+      echo "${U} doesn't exist";
+   fi
 done
 
 # Removing the group.
