b56a9ad8b7a79cf3073994fb7d9d1697cb8491fe
pereyra-m <matias.pereyra@wazuh.com>
2021-03-11 15:03:42 -0300

    Fixing PoC functions

--- src/wazuh_db/wdb_syscollector.c
+++ src/wazuh_db/wdb_syscollector.c
@@ -473,24 +473,28 @@ int wdb_package_save(wdb_t * wdb, const char * scan_id, const char * scan_time, 
         return -1;
     }
 
-    /////////////////////////
     SHA_CTX shactx;
     unsigned char digest[SHA_DIGEST_LENGTH];
     int i=0,j=0;
     char* item_id_final= (char*) malloc (sizeof(char)*SHA_DIGEST_LENGTH*2+1);
 
-    SHA1_Init(&shactx);
-    SHA1_Update(&shactx, name, strlen(name));
-    SHA1_Update(&shactx, version, strlen(version));
-    SHA1_Update(&shactx, architecture, strlen(architecture));
-    SHA1_Final(digest, &shactx);
 
-    for (i=0; i<SHA_DIGEST_LENGTH; i++) {
-        char * test = (char*) malloc (sizeof(char) * 3);
-        snprintf (test, 2, "%02x",digest[i]);
-        strcpy(item_id_final+j,test);
-        j+=2;
-        os_free(test);
+    /////////////////////////
+    if(!item_id) {
+
+        SHA1_Init(&shactx);
+        SHA1_Update(&shactx, name, strlen(name));
+        SHA1_Update(&shactx, version, strlen(version));
+        SHA1_Update(&shactx, architecture, strlen(architecture));
+        SHA1_Final(digest, &shactx);
+
+        for (i=0; i<SHA_DIGEST_LENGTH; i++) {
+            char * test = (char*) malloc (sizeof(char) * 3);
+            snprintf (test, 2, "%02x",digest[i]);
+            strcpy(item_id_final+j,test);
+            j+=2;
+            os_free(test);
+        }
     }
     /////////////////////////
 
@@ -512,7 +516,7 @@ int wdb_package_save(wdb_t * wdb, const char * scan_id, const char * scan_time, 
         location,
         0,
         checksum,
-        item_id_final,
+        item_id ? item_id : item_id_final,
         replace) < 0) {
 
         return -1;
