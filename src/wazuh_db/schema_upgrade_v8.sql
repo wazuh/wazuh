55ed04a58dda7e05ad3c336160873589a0a5f4d9
pereyra-m <matias.pereyra@wazuh.com>
2021-03-16 16:10:31 -0300

    Updating agents DB schema to improve CVE alerts mechanism

--- src/wazuh_db/schema_upgrade_v8.sql
+++ src/wazuh_db/schema_upgrade_v8.sql
@@ -37,4 +37,26 @@ DROP TABLE IF EXISTS sys_programs;
 ALTER TABLE _sys_programs RENAME TO sys_programs;
 CREATE INDEX IF NOT EXISTS programs_id ON sys_programs (scan_id);
 
+ALTER TABLE sys_osinfo ADD COLUMN reference TEXT DEFAULT '' NOT NULL;
+ALTER TABLE sys_osinfo ADD COLUMN triaged INTEGER(1) DEFAULT 0;
+
+DROP TABLE IF EXISTS vuln_cves;
+
+CREATE TABLE IF NOT EXISTS vuln_cves (
+    name TEXT,
+    version TEXT,
+    architecture TEXT,
+    cve TEXT,
+    reference TEXT DEFAULT '' NOT NULL,
+    type TEXT DEFAULT 'UNDEFINED' NOT NULL CHECK (type IN ('OS', 'PACKAGE','UNDEFINED')),
+    status TEXT DEFAULT 'PENDING' NOT NULL CHECK (status IN ('VALID', 'PENDING', 'OBSOLETE')),
+    PRIMARY KEY (reference, cve)
+);
+CREATE INDEX IF NOT EXISTS packages_id ON vuln_cves (name);
+CREATE INDEX IF NOT EXISTS cves_id ON vuln_cves (cve);
+CREATE INDEX IF NOT EXISTS cve_type ON vuln_cves (type);
+CREATE INDEX IF NOT EXISTS cve_status ON vuln_cves (status);
+
+UPDATE vuln_metadata SET LAST_SCAN = '0';
+
 INSERT OR REPLACE INTO metadata (key, value) VALUES ('db_version', 8);
