38b1b97f0be4d065fa2b377c59d2c6b12f4d36ae
pereyra-m <matias.pereyra@wazuh.com>
2021-03-16 16:10:31 -0300

    Updating agents DB schema to improve CVE alerts mechanism

--- src/wazuh_db/wdb.h
+++ src/wazuh_db/wdb.h
@@ -285,6 +285,7 @@ extern char *schema_upgrade_v4_sql;
 extern char *schema_upgrade_v5_sql;
 extern char *schema_upgrade_v6_sql;
 extern char *schema_upgrade_v7_sql;
+extern char *schema_upgrade_v8_sql;
 extern char *schema_global_upgrade_v1_sql;
 extern char *schema_global_upgrade_v2_sql;
 
