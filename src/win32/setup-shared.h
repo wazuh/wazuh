2ee5d9aec915fd70f0ed99871571760119bd4aae
javabalm <javier.balmaceda@wazuh.com>
2021-03-17 22:10:59 +0100

    Renamed ossec.conf to ossec.conf.backup in windows

--- src/win32/setup-shared.h
+++ src/win32/setup-shared.h
@@ -19,7 +19,7 @@
 #include "os_regex/os_regex.h"
 #include "headers/file_op.h"
 
-#define OSSECCONF   "agent.conf"
+#define AGENTCONF   "agent.conf"
 #define OSSECDEF    "default-agent.conf"
 #define AGENT_CONF_LAST   "agent.conf.bak"
 #define CLIENTKEYS  "client.keys"
