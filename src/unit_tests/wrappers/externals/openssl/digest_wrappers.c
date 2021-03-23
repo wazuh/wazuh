eea7957f8e0ab3acbf992f24be034ceb33de5881
pereyra-m <matias.pereyra@wazuh.com>
2021-03-23 19:36:50 -0300

    Updating hash method UT in wdb_integrity

--- src/unit_tests/wrappers/externals/openssl/digest_wrappers.c
+++ src/unit_tests/wrappers/externals/openssl/digest_wrappers.c
@@ -12,9 +12,10 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include "../../common.h"
+#include <openssl/evp.h>
 
-extern int __real_EVP_DigestUpdate(__attribute__((unused)) EVP_MD_CTX *ctx,const void *data, size_t count);
-int __wrap_EVP_DigestUpdate(__attribute__((unused)) EVP_MD_CTX *ctx,
+extern int __real_EVP_DigestUpdate(EVP_MD_CTX *ctx,const void *data, size_t count);
+int __wrap_EVP_DigestUpdate(EVP_MD_CTX *ctx,
                             const void *data,
                             size_t count) {
    if (test_mode) {
