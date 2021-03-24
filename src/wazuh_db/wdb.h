bed86f85bc7847ae5f90a7df46711ab26494a4ca
pereyra-m <matias.pereyra@wazuh.com>
2021-03-24 00:43:24 -0300

    Updating hash method description

--- src/wazuh_db/wdb.h
+++ src/wazuh_db/wdb.h
@@ -1245,11 +1245,16 @@ int wdbi_query_clear(wdb_t * wdb, wdb_component_t component, const char * payloa
  * @retval -1 On error.
  */
 int wdb_journal_wal(sqlite3 *db);
+// Calculates SHA1 hash from a NULL terminated string array or strings as parameters
 
 /**
-*  @brief Calculates the SHA1 hash of all strings stored in a NULL terminated array
+*  @brief Calculates SHA1 hash from a NULL terminated string array or strings as parameters.
+*
+* For example, wdbi_sha_calculation(array, hexdigest, 0)
+*              wdbi_sha_calculation(NULL, hexdigest, parameters_count, "first", "second", ...)
 *
 * @param [in] strings_to_hash NULL Terminated array with strings to hash
+* @param [in] count Number of variable parameters sent
 * @param [out] hexdigest Final strings hashed
 */
  int wdbi_sha_calculation(const char ** strings_to_hash, os_sha1 hexdigest, unsigned int count, ...);
