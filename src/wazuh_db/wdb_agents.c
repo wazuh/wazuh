14a1a0369c88bd608c21ade27b71a9722f0ff30d
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-03-26 17:40:25 -0300

    Review fixes

--- src/wazuh_db/wdb_agents.c
+++ src/wazuh_db/wdb_agents.c
@@ -15,7 +15,7 @@ bool wdb_agents_find_package(wdb_t *wdb, const char* reference){
     case SQLITE_DONE:
         return FALSE;
     default:
-        merror("DB(%s) sqlite3_step(): %s", wdb->id, sqlite3_errmsg(wdb->db));
+        mdebug1("DB(%s) sqlite3_step(): %s", wdb->id, sqlite3_errmsg(wdb->db));
         return FALSE;
     }
 }
@@ -36,7 +36,7 @@ bool wdb_agents_find_cve(wdb_t *wdb, const char* cve, const char* reference){
     case SQLITE_DONE:
         return FALSE;
     default:
-        merror("DB(%s) sqlite3_step(): %s", wdb->id, sqlite3_errmsg(wdb->db));
+        mdebug1("DB(%s) sqlite3_step(): %s", wdb->id, sqlite3_errmsg(wdb->db));
         return FALSE;
     }
 }
