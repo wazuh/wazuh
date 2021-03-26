cec879dad89b10895411fb325e84295aa666a229
Diego Provinciani <diegoprovinciani@gmail.com>
2021-03-26 17:38:55 -0300

    Removing limit from sql statement

--- src/wazuh_db/wdb.c
+++ src/wazuh_db/wdb.c
@@ -212,7 +212,7 @@ static const char *SQL_STMT[] = {
     [WDB_STMT_VULN_CVES_UPDATE] = "UPDATE vuln_cves SET status = ? WHERE status = ?;",
     [WDB_STMT_VULN_CVES_UPDATE_ALL] = "UPDATE vuln_cves SET status = ?",
     [WDB_STMT_VULN_CVES_FIND_CVE] = "SELECT 1 FROM vuln_cves WHERE cve = ? AND reference = ?;",
-    [WDB_STMT_VULN_CVES_SELECT_BY_STATUS] = "SELECT * FROM vuln_cves WHERE status = ? LIMIT 1;",
+    [WDB_STMT_VULN_CVES_SELECT_BY_STATUS] = "SELECT * FROM vuln_cves WHERE status = ?;",
     [WDB_STMT_VULN_CVES_DELETE_ENTRY] = "DELETE FROM vuln_cves WHERE cve = ? AND reference = ?;"
 };
 
