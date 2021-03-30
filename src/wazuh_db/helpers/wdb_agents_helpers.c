0b4e960690f035e655ca2a481d5901d930bdf6bd
pereyra-m <matias.pereyra@wazuh.com>
2021-03-29 23:04:09 -0300

    Adding type filter to update status command for vuln_cves table

--- src/wazuh_db/helpers/wdb_agents_helpers.c
+++ src/wazuh_db/helpers/wdb_agents_helpers.c
@@ -132,6 +132,65 @@ int wdb_agents_vuln_cves_update_status(int id,
     return result;
 }
 
+int wdb_agents_vuln_cves_update_status_by_type(int id,
+                                               const char *type,
+                                               const char *new_status,
+                                               int *sock) {
+    int result = 0;
+    cJSON *data_in = NULL;
+    char *data_in_str = NULL;
+    char *wdbquery = NULL;
+    char *wdboutput = NULL;
+    char *payload = NULL;
+    int aux_sock = -1;
+
+    data_in = cJSON_CreateObject();
+
+    if (!data_in) {
+        mdebug1("Error creating data JSON for Wazuh DB.");
+        return OS_INVALID;
+    }
+
+    cJSON_AddStringToObject(data_in, "type", type);
+    cJSON_AddStringToObject(data_in, "new_status", new_status);
+
+    data_in_str = cJSON_PrintUnformatted(data_in);
+    os_malloc(WDBQUERY_SIZE, wdbquery);
+    snprintf(wdbquery, WDBQUERY_SIZE, agents_db_commands[WDB_AGENTS_VULN_CVES_UPDATE_STATUS], id, data_in_str);
+
+    os_malloc(WDBOUTPUT_SIZE, wdboutput);
+    result = wdbc_query_ex(sock?sock:&aux_sock, wdbquery, wdboutput, WDBOUTPUT_SIZE);
+
+    switch (result) {
+        case OS_SUCCESS:
+            if (WDBC_OK != wdbc_parse_result(wdboutput, &payload)) {
+                mdebug1("Agents DB (%d) Error reported in the result of the query", id);
+                result = OS_INVALID;
+            }
+            break;
+        case OS_INVALID:
+            mdebug1("Agents DB (%d) Error in the response from socket", id);
+            mdebug2("Agents DB (%d) SQL query: %s", id, wdbquery);
+            result = OS_INVALID;
+            break;
+        default:
+            mdebug1("Agents DB (%d) Cannot execute SQL query", id);
+            mdebug2("Agents DB (%d) SQL query: %s", id, wdbquery);
+            result = OS_INVALID;
+    }
+
+    if (!sock) {
+        wdbc_close(&aux_sock);
+    }
+
+    cJSON_Delete(data_in);
+    os_free(data_in_str);
+    os_free(wdbquery);
+    os_free(wdboutput);
+
+    return result;
+}
+
 int wdb_agents_vuln_cves_remove_entry(int id,
                                       const char *cve,
                                       const char *reference,
