98eaeaa4888e417182c023662bc4b4eaa543c7b7
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2021-03-31 13:28:02 -0300

    Fixed wdb insert command UTs

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_wrappers.c
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_wrappers.c
@@ -13,12 +13,24 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-int __wrap_wdb_agents_insert_vuln_cves(__attribute__((unused)) wdb_t *wdb, const char* name, const char* version, const char* architecture, const char* cve) {
+cJSON* __wrap_wdb_agents_insert_vuln_cves(__attribute__((unused)) wdb_t *wdb, 
+                                          const char* name, 
+                                          const char* version, 
+                                          const char* architecture, 
+                                          const char* cve,
+                                          const char* reference,
+                                          const char* type,
+                                          const char* status,
+                                          bool check_pkg_existance) {
     check_expected(name);
     check_expected(version);
     check_expected(architecture);
     check_expected(cve);
-    return mock();
+    check_expected(reference);
+    check_expected(type);
+    check_expected(status);
+    check_expected(check_pkg_existance);
+    return mock_ptr_type(cJSON*);
 }
 
 int __wrap_wdb_agents_update_status_vuln_cves(__attribute__((unused)) wdb_t *wdb, const char* old_status, const char* new_status) {
@@ -40,5 +52,16 @@ wdbc_result __wrap_wdb_agents_remove_by_status_vuln_cves(__attribute__((unused))
 }
 
 int __wrap_wdb_agents_clear_vuln_cves(__attribute__((unused)) wdb_t *wdb) {
+    return mock();
+}
+
+bool __wrap_wdb_agents_find_package(__attribute__((unused)) wdb_t *wdb, const char* reference){
+    check_expected(reference);
+    return mock();
+}
+
+bool __wrap_wdb_agents_find_cve(__attribute__((unused)) wdb_t *wdb, const char* cve, const char* reference){
+    check_expected(cve);
+    check_expected(reference);
     return mock();
 }
