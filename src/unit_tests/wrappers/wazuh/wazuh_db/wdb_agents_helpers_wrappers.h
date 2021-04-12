521774e2646738640208934e74cb4081f3385ceb
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-04-12 11:13:18 -0700

    Code review fix

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.h
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.h
@@ -8,26 +8,23 @@
  */
 
 
-#ifndef WDB_AGENTS_WRAPPERS_H
-#define WDB_AGENTS_WRAPPERS_H
+#ifndef WDB_AGENTS_HELPERS_WRAPPERS_H
+#define WDB_AGENTS_HELPERS_WRAPPERS_H
 
 #include "wazuh_db/wdb.h"
 
-cJSON* __wrap_wdb_agents_get_sys_osinfo(wdb_t *wdb);
-int __wrap_wdb_agents_set_sys_osinfo_triaged(wdb_t *wdb);
-cJSON* __wrap_wdb_agents_insert_vuln_cves(wdb_t *wdb,
-                                          const char* name,
-                                          const char* version,
-                                          const char* architecture,
-                                          const char* cve,
-                                          const char* reference,
-                                          const char* type,
-                                          const char* status,
-                                          bool check_pkg_existence);
-int __wrap_wdb_agents_update_status_vuln_cves(wdb_t *wdb, const char* old_status, const char* new_status, const char* type);
-int __wrap_wdb_agents_remove_vuln_cves(wdb_t *wdb, const char* cve, const char* reference);
-wdbc_result __wrap_wdb_agents_remove_by_status_vuln_cves(wdb_t *wdb, const char* status, char **output);
-int __wrap_wdb_agents_clear_vuln_cves(wdb_t *wdb);
-bool __wrap_wdb_agents_find_package(wdb_t *wdb, const char* reference);
-bool __wrap_wdb_agents_find_cve(wdb_t *wdb, const char* cve, const char* reference);
+int __wrap_wdb_agents_vuln_cves_insert(int id,
+                                      const char *name,
+                                      const char *version,
+                                      const char *architecture,
+                                      const char *cve,
+                                      const char *reference,
+                                      const char *type,
+                                      const char *status,
+                                      bool check_pkg_existence,
+                                      __attribute__((unused)) int *sock);
+
+int __wrap_wdb_agents_vuln_cves_clear(int id,
+                                     __attribute__((unused)) int *sock);
+
 #endif
