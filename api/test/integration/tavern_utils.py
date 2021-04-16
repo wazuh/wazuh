27f5c0942d0511d20c5d91274b25e7d42ede618b
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2021-04-16 16:47:17 +0200

    Add MITRE's techniques integration tests

--- api/test/integration/tavern_utils.py
+++ api/test/integration/tavern_utils.py
@@ -145,6 +145,22 @@ def test_save_response_data(response):
     return Box({'response_data': response.json()['data']})
 
 
+def test_save_response_data_mitre(response, fields):
+    response = response.json()['data']
+    fields_response = list()
+    for r in response['affected_items']:
+        fields_response.append({k: r[k] for k in fields})
+
+    return Box({'response_data': fields_response})
+
+
+def test_validate_mitre(response, data, index=0):
+    data = json.loads(data.replace("'", '"'))
+    for element in data:
+        for k, v in element.items():
+            assert v == response.json()['data']['affected_items'][index][k]
+
+
 def test_validate_restart_by_node(response, data):
     data = json.loads(data.replace("'", '"'))
     affected_items = list()
