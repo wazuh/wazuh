0045e9f58a8fd051c1c84950c617916274573f56
Enrique CL <kicalin@protonmail.com>
2021-04-16 15:03:51 +0200

    Added more filter cases for status and type in test_vulnerability.py

--- framework/wazuh/tests/data/schema_cve_test.sql
+++ framework/wazuh/tests/data/schema_cve_test.sql
@@ -21,8 +21,17 @@ CREATE INDEX IF NOT EXISTS cves_id ON vuln_cves (cve);
 BEGIN;
 
 -- Data from https://www.cvedetails.com/vulnerability-list/
-INSERT INTO vuln_cves (name,version,architecture,cve) VALUES ('Invenio-previewer', '0.1.0', 'ARM', 'CVE-2019-1020019');
-INSERT INTO vuln_cves (name,version,architecture,cve) VALUES ('Discourse', '0.8.0', 'PowerPC', 'CVE-2019-1020018');
-INSERT INTO vuln_cves (name,version,architecture,cve) VALUES ('Ash-aio', '2.0.0.0', 'x86', 'CVE-2019-1020016');
-INSERT INTO vuln_cves (name,version,architecture,cve) VALUES ('Credential Helpers', '0.1.0', 'x86', 'CVE-2019-1020014');
-INSERT INTO vuln_cves (name,version,architecture,cve) VALUES ('Smokedetector', '-', 'x86', 'CVE-2019-1020011');
+INSERT INTO vuln_cves (name,version,architecture,cve,type,status)
+VALUES ('Invenio-previewer', '0.1.0', 'ARM', 'CVE-2019-1020019','OS','VALID');
+
+INSERT INTO vuln_cves (name,version,architecture,cve,type,status)
+VALUES ('Discourse', '0.8.0', 'PowerPC', 'CVE-2019-1020018','OS','VALID');
+
+INSERT INTO vuln_cves (name,version,architecture,cve,type,status)
+VALUES ('Ash-aio', '2.0.0.0', 'x86', 'CVE-2019-1020016','OS','OBSOLETE');
+
+INSERT INTO vuln_cves (name,version,architecture,cve,type,status)
+VALUES ('Credential Helpers', '0.1.0', 'x86', 'CVE-2019-1020014','PACKAGE','OBSOLETE');
+
+INSERT INTO vuln_cves (name,version,architecture,cve,type,status)
+VALUES ('Smokedetector', '-', 'x86', 'CVE-2019-1020011','PACKAGE','PENDING');
