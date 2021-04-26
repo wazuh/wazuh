95d7f05b382a88422376c9eb19fd156de36e8829
Manuel <manuel.carmona@wazuh.com>
2021-04-26 16:29:28 +0200

    Minor changes after rebasing

--- api/test/integration/env/configurations/cluster/manager/configuration_files/entrypoint.sh
+++ api/test/integration/env/configurations/cluster/manager/configuration_files/entrypoint.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/ossec/stats/totals/2019/Aug/
 cp -rf /tmp/configuration_files/wazuh-totals-27.log /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
-chown -R ossec:ossec /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
+chown -R wazuh:wazuh /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
--- api/test/integration/env/configurations/manager/manager/configuration_files/manager.sh
+++ api/test/integration/env/configurations/manager/manager/configuration_files/manager.sh
@@ -5,5 +5,5 @@ if [ "$HOSTNAME" == "wazuh-master" ]; then
   rm -rf /var/ossec/stats/totals/*
   mkdir -p /var/ossec/stats/totals/2019/Aug/
   cp -rf /tmp/configuration_files/wazuh-totals-27.log /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
-  chown -R ossec:ossec /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
+  chown -R wazuh:wazuh /var/ossec/stats/totals/2019/Aug/wazuh-totals-27.log
 fi
