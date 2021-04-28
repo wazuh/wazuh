10dc9619bf1843199669d3ca337e7944b23de8de
Jotacarma <jotacarma90@gmail.com>
2021-04-28 11:53:51 +0200

    Change strdup to os_strdup, and check a string non empty

--- src/shared/version_op.c
+++ src/shared/version_op.c
@@ -747,13 +747,16 @@ os_info *get_unix_version()
                     pclose(cmd_output_ver);
                 }
             } else if (strcmp(strtok_r(buff, "\n", &save_ptr), "AIX") == 0) { // AIX
-                info->os_name = strdup("AIX");
-                info->os_platform = strdup("aix");
+                os_strdup("AIX", info->os_name);
+                os_strdup("aix", info->os_platform);
 
                 if (cmd_output_ver = popen("oslevel", "r"), cmd_output_ver) {
                     if (fgets(buff, sizeof(buff) - 1, cmd_output_ver)) {
-                        buff[strlen(buff) - 1] = '\0';
-                        os_strdup(buff, info->os_version);
+                        int buff_len = strlen(buff);
+                        if (buff_len > 0) {
+                            buff[buff_len - 1] = '\0';
+                            os_strdup(buff, info->os_version);
+                        }
                     } else {
                         mdebug1("Cannot read from command output (oslevel).");
                     }
