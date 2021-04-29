a7e501b90e84c691304e34beaeb6250f1790489f
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2021-04-29 17:15:58 +0000

    logcollector: log show execution added, several renaming and code improvements

--- src/logcollector/oslog.h
+++ src/logcollector/oslog.h
@@ -8,17 +8,41 @@
  * Foundation
  */
 
-#ifndef OSLOG_STREAM_H
-#define OSLOG_STREAM_H
+#ifndef OSLOG_H
+#define OSLOG_H
+
+/* ******************  INCLUDES  ****************** */
 
 #include "shared.h"
 #include "config/localfile-config.h"
 
+/* ******************  DEFINES  ****************** */
+
+///< macOS ULS milliseconds lenght i.e .123456
+#define OS_LOGCOLLECTOR_TIMESTAMP_MS_LEN        7
+///< macOS ULS timezone lenght i.e -0700
+#define OS_LOGCOLLECTOR_TIMESTAMP_TZ_LEN        5
+///< macOS ULS basic timestamp lenght i.e 2021-04-27 08:07:20
+#define OS_LOGCOLLECTOR_BASIC_TIMESTAMP_LEN     19
+///< macOS ULS short timestamp lenght i.e 2021-04-27 08:07:20-0700
+#define OS_LOGCOLLECTOR_SHORT_TIMESTAMP_LEN     OS_LOGCOLLECTOR_BASIC_TIMESTAMP_LEN + OS_LOGCOLLECTOR_TIMESTAMP_TZ_LEN
+///< macOS ULS full timestamp lenght i.e 2020-11-09 05:45:08.000000-0800
+#define OS_LOGCOLLECTOR_FULL_TIMESTAMP_LEN      OS_LOGCOLLECTOR_SHORT_TIMESTAMP_LEN + OS_LOGCOLLECTOR_TIMESTAMP_MS_LEN
+
+/* ******************  DATATYPES  ****************** */
+
+typedef struct {
+    pthread_mutex_t mutex;
+    char timestamp[OS_LOGCOLLECTOR_SHORT_TIMESTAMP_LEN + 1];
+} oslog_status_t;
+
+/* ******************  PROTOTYPES  ****************** */
+
 /**
  * @brief Creates the environment for collecting logs on MacOS Systems
- * @param oslog_array logreader structure with "log stream"'s input arguments and w_oslog_config_t structure to be set
+ * @param oslog_array logreader structure with `log`'s input arguments and w_oslog_config_t structure to be set
  */
-void w_logcollector_create_oslog_env(logreader * current);
+void w_oslog_create_env(logreader * current);
 
 
 #endif /* OSLOGSTREAM_H */
