601acd6ab25e2539fcfb30340574573480224ed9
Manuel <manuel.carmona@wazuh.com>
2021-05-04 17:06:02 +0200

    Refactor send_request usage with the new cluster msg chunk feature

--- framework/wazuh/core/cluster/common.py
+++ framework/wazuh/core/cluster/common.py
@@ -279,7 +279,7 @@ class Handler(asyncio.Protocol):
         # Stores incoming string information from string commands.
         self.in_str = {}
         # Maximum message length to send in a single request.
-        self.request_chunk = 5242880
+        self.request_chunk = 200
         # Object use to encrypt and decrypt requests.
         self.my_fernet = cryptography.fernet.Fernet(base64.b64encode(fernet_key.encode())) if fernet_key else None
         # Logging.Logger object used to write logs.
@@ -497,9 +497,9 @@ class Handler(asyncio.Protocol):
         # Send each chunk so it is updated in the destination.
         file_hash = hashlib.sha256()
         with open(filename, 'rb') as f:
-            for chunk in iter(lambda: f.read(self.request_chunk - len(relative_path) - 1), b''):
-                await self.send_request(command=b'file_upd', data=relative_path + b' ' + chunk)
-                file_hash.update(chunk)
+            data = f.read()
+            await self.send_request(command=b'file_upd', data=relative_path + b' ' + data)
+            file_hash.update(data)
 
         # Close the destination file descriptor so the file in memory is dumped to disk.
         await self.send_request(command=b'file_end', data=relative_path + b' ' + file_hash.digest())
@@ -523,10 +523,8 @@ class Handler(asyncio.Protocol):
         total = len(my_str)
         task_id = await self.send_request(command=b'new_str', data=str(total).encode())
 
-        # Send chunks of the string to the destination node, indicating the ID of the string.
-        local_req_chunk = self.request_chunk - len(task_id) - 1
-        for c in range(0, total, local_req_chunk):
-            await self.send_request(command=b'str_upd', data=task_id + b' ' + my_str[c:c + local_req_chunk])
+        # Send the string to the destination node, indicating the ID of the string.
+        await self.send_request(command=b'str_upd', data=task_id + b' ' + my_str)
 
         return task_id
 
@@ -747,12 +745,12 @@ class Handler(asyncio.Protocol):
         return b"ok ", b"Ready to receive new file"
 
     def update_file(self, data: bytes) -> Tuple[bytes, bytes]:
-        """Extend file content.
+        """Update file content.
 
         Parameters
         ----------
         data : bytes
-            Bytes containing filepath and chunk of data separated by ' '.
+            Bytes containing filepath and data separated by ' '.
 
         Returns
         -------
@@ -761,9 +759,9 @@ class Handler(asyncio.Protocol):
         bytes
             Response message.
         """
-        name, chunk = data.split(b' ', 1)
-        self.in_file[name]['fd'].write(chunk)
-        self.in_file[name]['checksum'].update(chunk)
+        name, file_content = data.split(b' ', 1)
+        self.in_file[name]['fd'].write(file_content)
+        self.in_file[name]['checksum'].update(file_content)
         return b"ok", b"File updated"
 
     def end_file(self, data: bytes) -> Tuple[bytes, bytes]:
@@ -815,7 +813,7 @@ class Handler(asyncio.Protocol):
         Parameters
         ----------
         data : bytes
-            Bytes containing string ID and chunk of data separated by ' '.
+            Bytes containing string ID and data separated by ' '.
 
         Returns
         -------
@@ -826,7 +824,7 @@ class Handler(asyncio.Protocol):
         """
         name, str_data = data.split(b' ', 1)
         self.in_str[name].receive_data(str_data)
-        return b"ok", b"Chunk received"
+        return b"ok", b"String updated"
 
     def process_unknown_cmd(self, command: bytes) -> Tuple[bytes, bytes]:
         """Define message when an unknown command is received.
