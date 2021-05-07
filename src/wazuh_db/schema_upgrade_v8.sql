530ca6854f22cdcd6626443af696ce0d2abe64b6
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2021-05-07 00:24:11 -0300

    Changes according to review

--- src/wazuh_db/schema_upgrade_v8.sql
+++ src/wazuh_db/schema_upgrade_v8.sql
@@ -40,7 +40,7 @@ CREATE INDEX IF NOT EXISTS programs_id ON sys_programs (scan_id);
 ALTER TABLE sys_osinfo ADD COLUMN os_display_version TEXT DEFAULT NULL;
 ALTER TABLE sys_osinfo ADD COLUMN reference TEXT NOT NULL DEFAULT '';
 ALTER TABLE sys_osinfo ADD COLUMN triaged INTEGER(1) DEFAULT 0;
-ALTER TABLE sync_info ADD COLUMN last_global_checksum TEXT NOT NULL DEFAULT 'legacy';
+ALTER TABLE sync_info ADD COLUMN last_agent_checksum TEXT NOT NULL DEFAULT ' ';
 
 
 DROP TABLE IF EXISTS vuln_cves;
