1f673bb5fb63526fc4f32cdfeafc41b66c872daa
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-05-10 12:40:37 -0700

    Add doxygen

--- src/wazuh_db/wdb_agents.h
+++ src/wazuh_db/wdb_agents.h
@@ -115,14 +115,66 @@ wdbc_result wdb_agents_remove_vuln_cves_by_status(wdb_t *wdb, const char* status
  */
 int wdb_agents_clear_vuln_cves(wdb_t *wdb);
 
-/* JJP TODO: Doxygen*/
-
+/**
+ * @brief Function to set all packages from the table as triaged.
+ *
+ * @param [in] wdb The 'agents' struct database.
+ * @return Returns OS_SUCCESS on success or OS_INVALID on error.
+ */
 int wdb_agents_set_packages_triaged(wdb_t *wdb);
 
+/**
+ * @brief Function to send every package from the table row by row using wdb->peer
+ *
+ * @param [in] wdb The 'agents' struct database.
+ * @param [in] not_triaged_only Flag to request packages not marked as triaged only.
+ *
+ * @return OS_SUCCESS on success.
+ *         OS_INVALID on errors executing SQL statement.
+ *         OS_SOCKTERR on errors handling the socket.
+ *         OS_SIZELIM on error trying to fit the row response into the socket buffer.
+ */
 int wdb_agents_send_packages(wdb_t *wdb, bool not_triaged_only);
 
+/**
+ * @brief Function to send every hotfix from the table row by row using wdb->peer
+ *
+ * @param [in] wdb The 'agents' struct database.
+ * @return OS_SUCCESS on success.
+ *         OS_INVALID on errors executing SQL statement.
+ *         OS_SOCKTERR on errors handling the socket.
+ *         OS_SIZELIM on error trying to fit the row response into the socket buffer.
+ */
+int wdb_agents_send_hotfixes(wdb_t *wdb);
+
+/**
+ * @brief Function to get every package from the table.
+ *        It checks if the table is already updated previous obtaining the data.
+ *        The packages will be responded in multiple commands with "due" prefix.
+ *        All the packages are set as triaged after this.
+ *
+ * @param [in] wdb The 'agents' struct database.
+ * @param [in] not_triaged_only Flag to request packages not marked as triaged only.
+ * @param [out] response A JSON structure with the status of the operation:
+ *                          {"status":"SUCCESS"} on success
+ *                          {"status":"ERROR"} on error
+ *                          {"status":"NOT_SYNCED"} if data wasn´t available because the table is being synced
+ * @return OS_SUCCESS on success. An error code on error.
+ */
 int wdb_agents_get_packages(wdb_t *wdb, bool not_triaged_only, cJSON** response);
 
+/**
+ * @brief Function to get every hotfix from the table.
+ *        It checks if the table is already updated previous obtaining the data.
+ *        The hotfixes will be responded in multiple commands with "due" prefix.
+ *
+ * @param [in] wdb The 'agents' struct database.
+ * @param [out] response A JSON structure with the status of the operation:
+ *                          {"status":"SUCCESS"} on success
+ *                          {"status":"ERROR"} on error
+ *                          {"status":"NOT_SYNCED"} if data wasn´t available because the table is being synced
+ * @return OS_SUCCESS on success. An error code on error.
+ */
 int wdb_agents_get_hotfixes(wdb_t *wdb, cJSON** response);
 
 #endif
