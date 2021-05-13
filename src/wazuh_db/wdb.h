9640f628a3aaf73ef19f86001e4b47fd11178fa5
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-05-13 06:49:31 -0700

    Code review changes

--- src/wazuh_db/wdb.h
+++ src/wazuh_db/wdb.h
@@ -872,7 +872,7 @@ int wdb_parse_hardware(wdb_t * wdb, char * input, char * output);
 /**
  * @brief Parses a packages command
  * Commands:
- * 1. delete: Deletes packages table
+ * 1. del: Deletes packages table
  * 2. save: Inserts the entry or updates if it already exists
  * 3. get: Obtain every package on the table.
  * @param wdb Database of an agent
@@ -884,7 +884,7 @@ int wdb_parse_packages(wdb_t * wdb, char * input, char * output);
 /**
  * @brief Parses a hotfixes command
  * Commands:
- * 1. delete: Deletes hotfixes table
+ * 1. del: Deletes hotfixes table
  * 2. save: Inserts the entry or updates if it already exists
  * 3. get: Obtain every hotfix on the table.
  * @param wdb Database of an agent
