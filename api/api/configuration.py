baf929e9805a3fc18b1a18261b95394ab47abe1b
Enrique <kicalin@protonmail.com>
2021-05-25 12:48:55 +0200

    Test custom configuration TLS/ECDHE

--- api/api/configuration.py
+++ api/api/configuration.py
@@ -38,8 +38,8 @@ default_api_configuration = {
         "cert": "api/configuration/ssl/server.crt",
         "use_ca": False,
         "ca": "api/configuration/ssl/ca.crt",
-        "ssl_protocol": "TLSv1.2",
-        "ssl_ciphers": ""
+        "ssl_protocol": "TLS",
+        "ssl_ciphers": "ECDHE+AESGCM:!ECDSA"
     },
     "logs": {
         "level": "info",
