dd4ba944f44f42d5640ab60570d0b7a64611d3e1
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2021-05-31 18:25:50 -0300

    Fixed UTs

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.c
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.c
@@ -15,20 +15,26 @@
 #include <stdlib.h>
 
 cJSON* __wrap_wdb_insert_vuln_cves(int id,
-                                          const char *name,
-                                          const char *version,
-                                          const char *architecture,
-                                          const char *cve,
-                                          const char *reference,
-                                          const char *type,
-                                          const char *status,
-                                          bool check_pkg_existence,
-                                          __attribute__((unused)) int *sock) {
+                                   const char *name,
+                                   const char *version,
+                                   const char *architecture,
+                                   const char *cve,
+                                   const char *severity,
+                                   double cvss2_score,
+                                   double cvss3_score,
+                                   const char *reference,
+                                   const char *type,
+                                   const char *status,
+                                   bool check_pkg_existence,
+                                   __attribute__((unused)) int *sock) {
     check_expected(id);
     check_expected(name);
     check_expected(version);
     check_expected(architecture);
     check_expected(cve);
+    check_expected(severity);
+    check_expected(cvss2_score);
+    check_expected(cvss3_score);
     check_expected(reference);
     check_expected(type);
     check_expected(status);
@@ -37,23 +43,23 @@ cJSON* __wrap_wdb_insert_vuln_cves(int id,
 }
 
 int __wrap_wdb_clear_vuln_cves(int id,
-                                     __attribute__((unused)) int *sock) {
+                               __attribute__((unused)) int *sock) {
     check_expected(id);
     return mock_type(int);
 }
 
 cJSON* __wrap_wdb_remove_vuln_cves_by_status(int id,
-                                                    const char *status,
-                                                    __attribute__((unused)) int *sock) {
+                                             const char *status,
+                                             __attribute__((unused)) int *sock) {
     check_expected(id);
     check_expected(status);
     return mock_ptr_type(cJSON*);
 }
 
 int __wrap_wdb_update_vuln_cves_status(int id,
-                                              const char *old_status,
-                                              const char *new_status,
-                                              __attribute__((unused)) int *sock) {
+                                       const char *old_status,
+                                       const char *new_status,
+                                       __attribute__((unused)) int *sock) {
     check_expected(id);
     check_expected(old_status);
     check_expected(new_status);
