f53e9501d73da245f0c5b45b60fc210521491764
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2021-05-31 18:25:50 -0300

    Fixed UTs

--- src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.h
+++ src/unit_tests/wrappers/wazuh/wazuh_db/wdb_agents_helpers_wrappers.h
@@ -14,26 +14,29 @@
 #include "wazuh_db/wdb.h"
 
 cJSON* __wrap_wdb_insert_vuln_cves(int id,
-                                          const char *name,
-                                          const char *version,
-                                          const char *architecture,
-                                          const char *cve,
-                                          const char *reference,
-                                          const char *type,
-                                          const char *status,
-                                          bool check_pkg_existence,
-                                          __attribute__((unused)) int *sock);
+                                   const char *name,
+                                   const char *version,
+                                   const char *architecture,
+                                   const char *cve,
+                                   const char *severity,
+                                   double cvss2_score,
+                                   double cvss3_score,
+                                   const char *reference,
+                                   const char *type,
+                                   const char *status,
+                                   bool check_pkg_existence,
+                                   __attribute__((unused)) int *sock);
 
 int __wrap_wdb_clear_vuln_cves(int id,
-                                     __attribute__((unused)) int *sock);
+                               __attribute__((unused)) int *sock);
 
 cJSON* __wrap_wdb_remove_vuln_cves_by_status(int id,
                                              const char *status,
                                              __attribute__((unused)) int *sock);
 
 int __wrap_wdb_update_vuln_cves_status(int id,
-                                              const char *old_status,
-                                              const char *new_status,
-                                              __attribute__((unused)) int *sock);
+                                       const char *old_status,
+                                       const char *new_status,
+                                       __attribute__((unused)) int *sock);
 
 #endif
