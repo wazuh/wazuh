7f35bccb61396386edc8f21c32a52ff8362825ae
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2021-06-08 09:52:08 +0200

    Remove timeout_api_exe from cluster.json

--- framework/wazuh/core/cluster/tests/test_utils.py
+++ framework/wazuh/core/cluster/tests/test_utils.py
@@ -153,8 +153,8 @@ def test_get_cluster_items():
                                               'max_failed_keepalive_attempts': 2},
                                    'master': {'recalculate_integrity': 8, 'check_worker_lastkeepalive': 60,
                                               'max_allowed_time_without_keepalive': 120},
-                                   'communication': {'timeout_cluster_request': 20, 'timeout_api_request': 200,
-                                                     'request_timeout': 10, 'timeout_receiving_file': 120}},
+                                   'communication': {'timeout_cluster_request': 20, 'timeout_dapi_request': 200,
+                                                     'timeout_receiving_file': 120}},
                      'sync_options': {'get_agentinfo_newer_than': 1800}, 'distributed_api': {'enabled': True}}
 
 
