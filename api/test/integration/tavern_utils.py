b7bc6090fd31909138e194de19ad398b504c33da
Selutario <selutario@gmail.com>
2021-06-14 16:36:53 +0200

    Update API integration tests

--- api/test/integration/tavern_utils.py
+++ api/test/integration/tavern_utils.py
@@ -214,7 +214,7 @@ def test_expected_value(response, key, expected_values):
     expected_values = set(expected_values.split(',')) if not isinstance(expected_values, list) else set(expected_values)
 
     for item in response.json()['data']['affected_items']:
-        response_set = set(item[key]) if isinstance(item[key], list) else {item[key]}
+        response_set = set(map(str, item[key])) if isinstance(item[key], list) else {str(item[key])}
         assert bool(expected_values.intersection(response_set)), \
             f'Expected values {expected_values} not found in {item[key]}'
 
