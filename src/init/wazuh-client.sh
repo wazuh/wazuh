92bfd0689fc782f987057f72a276108de7c3a629
Vikman Fernandez-Castro <victor@wazuh.com>
2021-06-25 18:31:19 +0200

    Bump revision to 40207

--- src/init/wazuh-client.sh
+++ src/init/wazuh-client.sh
@@ -12,7 +12,7 @@ DIR=`dirname $PWD`;
 
 # Installation info
 VERSION="v4.2.0"
-REVISION="40207"
+REVISION="40208"
 TYPE="agent"
 
 ###  Do not modify below here ###
--- src/init/wazuh-server.sh
+++ src/init/wazuh-server.sh
@@ -14,7 +14,7 @@ PLIST=${DIR}/bin/.process_list;
 
 # Installation info
 VERSION="v4.2.0"
-REVISION="40207"
+REVISION="40208"
 TYPE="server"
 
 ###  Do not modify below here ###
