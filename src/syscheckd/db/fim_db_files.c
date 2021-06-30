63d90c0dac77a7a38c9f7968748c160886f8cd6c
Jotacarma <jotacarma90@gmail.com>
2021-06-30 16:21:53 +0200

    Change how to Windows agent process permissions of a file, from char to cJSON

--- src/syscheckd/db/fim_db_files.c
+++ src/syscheckd/db/fim_db_files.c
@@ -181,6 +181,9 @@ fim_entry *fim_db_decode_full_row(sqlite3_stmt *stmt) {
     entry->file_entry.data->size = (unsigned int)sqlite3_column_int(stmt, 8);
     sqlite_strdup((char *)sqlite3_column_text(stmt, 9), entry->file_entry.data->perm);
     sqlite_strdup((char *)sqlite3_column_text(stmt, 10), entry->file_entry.data->attributes);
+#ifdef WIN32
+    entry->file_entry.data->perm_json = cJSON_Parse((char *)sqlite3_column_text(stmt, 10));
+#endif
     sqlite_strdup((char *)sqlite3_column_text(stmt, 11), entry->file_entry.data->uid);
     sqlite_strdup((char *)sqlite3_column_text(stmt, 12), entry->file_entry.data->gid);
     sqlite_strdup((char *)sqlite3_column_text(stmt, 13), entry->file_entry.data->user_name);
