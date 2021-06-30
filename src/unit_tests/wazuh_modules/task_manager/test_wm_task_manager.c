d16a6d2e7b06a76f67509cc4b4d226f78dd6d8e9
Mauro Moltrasio <mauro.ezequiel@wazuh.com>
2021-06-30 16:16:35 +0200

    Reimplement death test on wm_task_manager
    Readded a previously removed UTs on wm_task_manager.
    Implemented a rudimentary way to run death tests on wm_task_manager.

--- src/unit_tests/wazuh_modules/task_manager/test_wm_task_manager.c
+++ src/unit_tests/wazuh_modules/task_manager/test_wm_task_manager.c
@@ -147,11 +147,7 @@ void test_wm_task_manager_init_bind_err(void **state)
     expect_string(__wrap__mterror, tag, "wazuh-modulesd:task-manager");
     expect_string(__wrap__mterror, formatted_msg, "(8251): Queue 'queue/tasks/task' not accessible: 'Success'. Exiting...");
 
-    will_return(__wrap_pthread_exit, OS_INVALID);
-
-    int ret = wm_task_manager_init(config);
-
-    assert_int_equal(ret, OS_INVALID);
+    expect_assert_failure(wm_task_manager_init(config));
 }
 
 void test_wm_task_manager_init_disabled(void **state)
@@ -164,16 +160,7 @@ void test_wm_task_manager_init_disabled(void **state)
     expect_string(__wrap__mtinfo, tag, "wazuh-modulesd:task-manager");
     expect_string(__wrap__mtinfo, formatted_msg, "(8202): Module disabled. Exiting...");
 
-    will_return(__wrap_pthread_exit, OS_INVALID);
-
-    expect_string(__wrap_OS_BindUnixDomain, path, TASK_QUEUE);
-    expect_value(__wrap_OS_BindUnixDomain, type, SOCK_STREAM);
-    expect_value(__wrap_OS_BindUnixDomain, max_msg_size, OS_MAXSTR);
-    will_return(__wrap_OS_BindUnixDomain, sock);
-
-    int ret = wm_task_manager_init(config);
-
-    assert_int_equal(ret, sock);
+    expect_assert_failure(wm_task_manager_init(config));
 }
 
 void test_wm_task_manager_dispatch_ok(void **state)
@@ -957,58 +944,44 @@ void test_wm_task_manager_main_select_empty_err(void **state)
     wm_task_manager_main(config);
 }
 
-// void test_wm_task_manager_main_select_err(void **state)
-// {
-//     wm_task_manager *config = *state;
-//     int sock = 555;
-//     int peer = 1111;
+void test_wm_task_manager_main_select_err(void **state)
+{
+    wm_task_manager *config = *state;
+    int sock = 555;
+    int peer = 1111;
 
-//     config->enabled = 1;
+    config->enabled = 1;
 
-//     char *message = "{"
-//                     "  \"origin\": {"
-//                     "      \"name\": \"node05\","
-//                     "      \"module\": \"upgrade_module\""
-//                     "   },"
-//                     "  \"command\": \"upgrade\","
-//                     "  \"parameters\": {"
-//                     "      \"agents\": [1, 2]"
-//                     "   }"
-//                     "}";
+    char *message = "{"
+                    "  \"origin\": {"
+                    "      \"name\": \"node05\","
+                    "      \"module\": \"upgrade_module\""
+                    "   },"
+                    "  \"command\": \"upgrade\","
+                    "  \"parameters\": {"
+                    "      \"agents\": [1, 2]"
+                    "   }"
+                    "}";
 
-//     will_return(__wrap_w_is_worker, 0);
+    will_return(__wrap_w_is_worker, 0);
 
-//     // wm_task_manager_init
+    // wm_task_manager_init
 
-//     expect_string(__wrap_OS_BindUnixDomain, path, TASK_QUEUE);
-//     expect_value(__wrap_OS_BindUnixDomain, type, SOCK_STREAM);
-//     expect_value(__wrap_OS_BindUnixDomain, max_msg_size, OS_MAXSTR);
-//     will_return(__wrap_OS_BindUnixDomain, sock);
+    expect_string(__wrap_OS_BindUnixDomain, path, TASK_QUEUE);
+    expect_value(__wrap_OS_BindUnixDomain, type, SOCK_STREAM);
+    expect_value(__wrap_OS_BindUnixDomain, max_msg_size, OS_MAXSTR);
+    will_return(__wrap_OS_BindUnixDomain, sock);
 
-//     expect_string(__wrap__mtinfo, tag, "wazuh-modulesd:task-manager");
-//     expect_string(__wrap__mtinfo, formatted_msg, "(8200): Module Task Manager started.");
+    expect_string(__wrap__mtinfo, tag, "wazuh-modulesd:task-manager");
+    expect_string(__wrap__mtinfo, formatted_msg, "(8200): Module Task Manager started.");
 
-//     will_return(__wrap_select, -1);
+    will_return(__wrap_select, -1);
 
-//     expect_string(__wrap__mterror, tag, "wazuh-modulesd:task-manager");
-//     expect_string(__wrap__mterror, formatted_msg, "(8252): Error in select(): 'Success'. Exiting...");
+    expect_string(__wrap__mterror, tag, "wazuh-modulesd:task-manager");
+    expect_string(__wrap__mterror, formatted_msg, "(8252): Error in select(): 'Success'. Exiting...");
 
-//     will_return(__wrap_pthread_exit, OS_INVALID);
-
-//     will_return(__wrap_select, 1);
-
-//     will_return(__wrap_accept, peer);
-
-//     expect_value(__wrap_OS_RecvSecureTCP, sock, peer);
-//     expect_value(__wrap_OS_RecvSecureTCP, size, OS_MAXSTR);
-//     will_return(__wrap_OS_RecvSecureTCP, message);
-//     will_return(__wrap_OS_RecvSecureTCP, OS_SOCKTERR);
-
-//     expect_string(__wrap__mterror, tag, "wazuh-modulesd:task-manager");
-//     expect_string(__wrap__mterror, formatted_msg, "(8255): Response size is bigger than expected.");
-
-//     wm_task_manager_main(config);
-// }
+    expect_assert_failure(wm_task_manager_main(config));
+}
 
 void test_wm_task_manager_main_worker_err(void **state)
 {
@@ -1061,7 +1034,7 @@ int main(void) {
         cmocka_unit_test(test_wm_task_manager_main_sockterr_err),
         cmocka_unit_test(test_wm_task_manager_main_accept_err),
         cmocka_unit_test(test_wm_task_manager_main_select_empty_err),
-        // cmocka_unit_test(test_wm_task_manager_main_select_err),
+        cmocka_unit_test(test_wm_task_manager_main_select_err),
         cmocka_unit_test(test_wm_task_manager_main_worker_err)
     };
     return cmocka_run_group_tests(tests, setup_group, teardown_group);
