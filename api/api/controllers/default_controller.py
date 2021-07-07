3eea508b458df3a845b052813e150702d021e652
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2021-07-07 17:14:11 +0200

    Change default controller date format

--- api/api/controllers/default_controller.py
+++ api/api/controllers/default_controller.py
@@ -4,9 +4,10 @@
 
 import logging
 import socket
-import time
+from datetime import datetime
 
 from aiohttp import web
+
 from api.encoder import dumps, prettify
 from api.models.basic_info_model import BasicInfo
 from wazuh.core.results import WazuhResult
@@ -23,7 +24,6 @@ async def default_info(pretty=False):
     Returns various basic information about the API
     """
     info_data = load_spec()
-    timestamp = time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime())
     data = {
         'title': info_data['info']['title'],
         'api_version': info_data['info']['version'],
@@ -31,7 +31,7 @@ async def default_info(pretty=False):
         'license_name': info_data['info']['license']['name'],
         'license_url': info_data['info']['license']['url'],
         'hostname': socket.gethostname(),
-        'timestamp': timestamp
+        'timestamp': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     }
     response = WazuhResult({'data': BasicInfo.from_dict(data)})
 
