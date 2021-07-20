3670dbbb9fc8c8bf5994e5ab9c692fc0e6cb386d
Christian Borla <borlach12@gmail.com>
2021-07-20 13:07:39 -0300

    Removed wrappers/wazuh/os_crypto/shared folder

--- src/unit_tests/remoted/test_secure.c
+++ src/unit_tests/remoted/test_secure.c
@@ -25,7 +25,7 @@
 #include "../wrappers/posix/stat_wrappers.h"
 #include "../wrappers/posix/unistd_wrappers.h"
 #include "../wrappers/wazuh/shared/queue_linked_op_wrappers.h"
-#include "../wrappers/wazuh/os_crypto/shared/keys_wrappers.h"
+#include "../wrappers/wazuh/os_crypto/keys_wrappers.h"
 #include "../wrappers/linux/netbuffer_wrappers.h"
 #include "../wrappers/linux/netcounter_wrappers.h"
 
