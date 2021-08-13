4d1d2ec9c8f0d6eb2b160287ba380b33e349cb77
Christian Borla <borlach12@gmail.com>
2021-08-13 17:02:30 -0300

    Added macos to agent upgrade process

--- src/init/pkg_installer.sh
+++ src/init/pkg_installer.sh
@@ -2,43 +2,46 @@
 
 # Copyright (C) 2015-2021, Wazuh Inc.
 
-WAZUH_HOME=${1}
-WAZUH_VERSION=${2}
-
 # Installing upgrade
-echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade started." >> ${WAZUH_HOME}/logs/upgrade.log
-chmod +x ${WAZUH_HOME}/var/upgrade/install.sh
-${WAZUH_HOME}/var/upgrade/install.sh >> ${WAZUH_HOME}/logs/upgrade.log 2>&1
+echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade started." >> ./logs/upgrade.log
+
+if ["uname" = "Linux"] ; then
+    chmod +x ./var/upgrade/install.sh
+    ./var/upgrade/install.sh >> ./logs/upgrade.log 2>&1
+else
+    chmod +x ./var/upgrade/wazuh-agent*
+    installer -pkg ./var/upgrade/wazuh-agent* -target / >> ./logs/upgrade.log 2>&1
+fi
 
 # Check installation result
 RESULT=$?
-echo "$(date +"%Y/%m/%d %H:%M:%S") - Installation result = ${RESULT}" >> ${WAZUH_HOME}/logs/upgrade.log
+echo "$(date +"%Y/%m/%d %H:%M:%S") - Installation result = ${RESULT}" >> ./logs/upgrade.log
 
 # Wait connection
 status="pending"
 COUNTER=30
 while [ "$status" != "connected" -a $COUNTER -gt 0  ]; do
-    . ${WAZUH_HOME}/var/run/wazuh-agentd.state >> ${WAZUH_HOME}/logs/upgrade.log 2>&1
+    . ./var/run/wazuh-agentd.state >> ./logs/upgrade.log 2>&1
     sleep 1
     COUNTER=$[COUNTER - 1]
-    echo "$(date +"%Y/%m/%d %H:%M:%S") - Waiting connection... Status = "${status}". Remaining attempts: ${COUNTER}." >> ${WAZUH_HOME}/logs/upgrade.log
+    echo "$(date +"%Y/%m/%d %H:%M:%S") - Waiting connection... Status = "${status}". Remaining attempts: ${COUNTER}." >> ./logs/upgrade.log
 done
 
 # Check connection
 if [ "$status" = "connected" -a $RESULT -eq 0  ]; then
-    echo "$(date +"%Y/%m/%d %H:%M:%S") - Connected to manager." >> ${WAZUH_HOME}/logs/upgrade.log
-    echo -ne "0" > ${WAZUH_HOME}/var/upgrade/upgrade_result
-    echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade finished successfully." >> ${WAZUH_HOME}/logs/upgrade.log
+    echo "$(date +"%Y/%m/%d %H:%M:%S") - Connected to manager." >> ./logs/upgrade.log
+    echo -ne "0" > ./var/upgrade/upgrade_result
+    echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade finished successfully." >> ./logs/upgrade.log
 else
-    echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade failed..." >> ${WAZUH_HOME}/logs/upgrade.log
+    echo "$(date +"%Y/%m/%d %H:%M:%S") - Upgrade failed..." >> ./logs/upgrade.log
 
     CONTROL="$WAZUH_HOME/bin/wazuh-control"
     if [ ! -f $CONTROL ]; then
         CONTROL="$WAZUH_HOME/bin/ossec-control"
     fi
 
-    echo "$(date +"%Y/%m/%d %H:%M:%S") - Trying to start the agent on its current state..." >> ${WAZUH_HOME}/logs/upgrade.log
-    $CONTROL start >> ${WAZUH_HOME}/logs/upgrade.log 2>&1
-    echo -ne "2" > ${WAZUH_HOME}/var/upgrade/upgrade_result
+    echo "$(date +"%Y/%m/%d %H:%M:%S") - Trying to start the agent on its current state..." >> ./logs/upgrade.log
+    $CONTROL start >> ./logs/upgrade.log 2>&1
+    echo -ne "2" > ./var/upgrade/upgrade_result
 
 fi
--- upgrade.sh
+++ upgrade.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 # Copyright (C) 2015-2021, Wazuh Inc.
 
-# Get Wazuh installation path
-SCRIPT=$(readlink -f "$0")
-WAZUH_HOME=$(dirname $(dirname $(dirname "$SCRIPT")))
-
-# Get Wazuh Info
-eval $(${WAZUH_HOME}/bin/wazuh-control info 2>/dev/null)
-if [ "X${WAZUH_VERSION}" = "X" ] ; then
-    . /etc/ossec-init.conf 2> /dev/null
-    if [ "X${VERSION}" = "X" ] ; then
-        exit 1
-    else
-        WAZUH_VERSION=${VERSION}
-    fi
+# validate OS, linux or macos
+if ["uname" = "Linux"] ; then
+    (sleep 5 && chmod +x ./var/upgrade/src/init/*.sh && ./var/upgrade/src/init/pkg_installer.sh && find ./var/upgrade/* -not -name upgrade_result -delete) >/dev/null 2>&1 &
+else
+    (sleep 5 && chmod +x ./var/upgrade/*.sh && ./var/upgrade/pkg_installer.sh && find ./var/upgrade/ -mindepth 1 -not -name upgrade_result -delete) >/dev/null 2>&1 &
 fi
-
-(sleep 5 && chmod +x ${WAZUH_HOME}/var/upgrade/src/init/*.sh && ${WAZUH_HOME}/var/upgrade/src/init/pkg_installer.sh ${WAZUH_HOME} ${WAZUH_VERSION} && find ${WAZUH_HOME}/var/upgrade/* -not -name upgrade_result -delete) >/dev/null 2>&1 &
