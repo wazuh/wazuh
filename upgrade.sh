5679431bbcc2fbb41b3afa5c49bd6632ede2b1cc
Christian Borla <borlach12@gmail.com>
2021-08-17 11:03:09 -0300

    Added X character

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 # Copyright (C) 2015-2021, Wazuh Inc.
 
 # validate OS, linux or macos
-if [ "$(uname)" = "Linux" ] ; then
+if [ "X$(uname)" = "XLinux" ] ; then
     (sleep 5 && chmod +x ./var/upgrade/src/init/*.sh && ./var/upgrade/src/init/pkg_installer.sh && find ./var/upgrade/* -not -name upgrade_result -delete) >/dev/null 2>&1 &
 else
     (sleep 5 && chmod +x ./var/upgrade/*.sh && ./var/upgrade/pkg_installer.sh && find ./var/upgrade/ -mindepth 1 -not -name upgrade_result -delete) >/dev/null 2>&1 &
