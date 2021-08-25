0cba47cd8d1b5ebeb0e96e2e04cc9ab4e6daa3c4
alromero <alvaro.romero@wazuh.com>
2021-08-25 10:55:09 +0200

    Add debug message in SUSE pre-scan

--- src/error_messages/debug_messages.h
+++ src/error_messages/debug_messages.h
@@ -95,6 +95,7 @@
 #define VU_ERROR_CMP_VER      "(5487): Unknown relation '%s' between versions '%s' and '%s' for package '%s'"
 #define VU_DISCARD_CVE_ENTRY  "(5488): Package '%s' not affected by '%s' with misleading condition (%s '%s')."
 #define VU_INS_DEPS           "(5489): Inserting '%s' vulnerabilities dependencies."
+#define VU_DEP_FLAG           "(5490): Dependency '%s' of agent '%.3d' meets the vulnerability criteria: Version (%s) '%s' '%s'."
 
 /* File integrity monitoring debug messages */
 #define FIM_DIFF_SKIPPED                    "(6200): Diff execution skipped for containing insecure characters."
