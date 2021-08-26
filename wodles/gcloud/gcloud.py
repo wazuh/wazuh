bb6b698569ca51ab194fd185f595c5b1ccc15752
Víctor Fernández Poyatos <victor.f.poyatos@wazuh.com>
2021-08-26 12:29:05 +0200

    Add multithreading to Gcloud PubSub integration

--- wodles/gcloud/gcloud.py
+++ wodles/gcloud/gcloud.py
@@ -7,10 +7,16 @@
 # it and/or modify it under the terms of GPLv2
 
 """This module processes events from a Google Cloud subscription."""
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 import tools
-import os
 from integration import WazuhGCloudSubscriber
+
+try:
+    max_threads = os.cpu_count() * 5
+except TypeError:
+    max_threads = 5
 
 try:
     # get script arguments
@@ -20,18 +26,32 @@ try:
     credentials_file = arguments.credentials_file
     max_messages = arguments.max_messages
     log_level = arguments.log_level
+    n_threads = arguments.n_threads
 
     # get logger
     logger = tools.get_stdout_logger(tools.logger_name, log_level)
 
-    # get Google Cloud client
-    client = WazuhGCloudSubscriber(credentials_file, project, subscription_id)
+    if n_threads > max_threads:
+        n_threads = max_threads
+        logger.warning(f'Reached maximum number of threads. Truncating to {max_threads}')
 
-    # check permissions about subscription
-    client.check_permissions()
+    logger.debug(f"Setting {n_threads} thread{'s' if n_threads > 1 else ''} to pull {max_messages}" 
+                 f" message{'s' if max_messages > 1 else ''} each")
 
     # process messages
-    num_processed_messages = client.process_messages(max_messages)
+    with ThreadPoolExecutor() as executor:
+        futures = []
+
+        # check permissions
+        subscriber_client = WazuhGCloudSubscriber(credentials_file, project, subscription_id)
+        subscriber_client.check_permissions()
+        futures.append(executor.submit(subscriber_client.process_messages, max_messages // n_threads))
+
+        for _ in range(n_threads - 1):
+            client = WazuhGCloudSubscriber(credentials_file, project, subscription_id)
+            futures.append(executor.submit(client.process_messages, max_messages // n_threads))
+
+    num_processed_messages = sum([future.result() for future in futures])
 
 except Exception as e:
     # write the trace in the log file
