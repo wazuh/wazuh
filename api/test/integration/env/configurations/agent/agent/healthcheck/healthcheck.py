d53f5d09a83243cb20c381aa7f1cec4a7c7d71ef
Selutario <joseluis.lopez@wazuh.com>
2021-09-03 13:11:42 +0200

    Use legacy_healthcheck

--- api/test/integration/env/configurations/agent/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/agent/agent/healthcheck/healthcheck.py
@@ -2,12 +2,12 @@ import os
 import sys
 sys.path.append('/tools')
 
-from healthcheck_utils import get_agent_health_base, is_old_agent
+from healthcheck_utils import get_agent_health_base
 
 
 def get_health():
     stats_files = ['/var/ossec/var/run/wazuh-logcollector.state']
-    if all(os.path.exists(file) and os.path.getsize(file) > 0 for file in stats_files) or is_old_agent():
+    if all(os.path.exists(file) and os.path.getsize(file) > 0 for file in stats_files):
         return 0
     else:
         return 1
