a83db774ad690fd019a0b669b802d1c70c172d8c
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2021-09-21 11:54:14 -0300

    Code review changes

--- src/unit_tests/wrappers/wazuh/addagent/manage_agents_wrappers.h
+++ src/unit_tests/wrappers/wazuh/addagent/manage_agents_wrappers.h
@@ -11,10 +11,6 @@
 #ifndef MANAGE_AGENTS_WRAPPERS_H
 #define MANAGE_AGENTS_WRAPPERS_H
 
-#include <time.h>
-
-double __wrap_get_time_since_agent_disconnection(const char *id);
 void __wrap_OS_RemoveAgentGroup(const char *id);
-time_t __wrap_get_time_since_agent_registration(int id);
 
 #endif
