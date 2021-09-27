cee477bc37f50434fd23057f28ea577950b438d7
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-09-27 10:13:39 +0200

    Added all the necessary files to compile one trial test with gtest

---
+++ src/syscheckd/db/tests/testFolder/testPrueba.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
