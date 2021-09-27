dd6cb256964f68cfed102771516a005c2c724fd1
Jotacarma <jotacarma90@gmail.com>
2021-09-27 12:25:17 +0200

    Created file fimDB.cpp

---
+++ src/syscheckd/db/src/fimDB.cpp
@@ -0,0 +1,23 @@
+/*
+ * Wazuh SysCollector
+ * Copyright (C) 2015-2021, Wazuh Inc.
+ * September 27, 2020.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+#ifndef _FIMDB_CPP
+#define _FIMDB_CPP
+#include "fimDB.hpp"
+
+
+
+
+
+
+
+
+
+#endif
