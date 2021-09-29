59feedc03527899e47b7245215fcbe90e34a1ac6
juanmanuelug <juan.utrera@wazuh.com>
2021-09-29 10:15:58 +0200

    added new path to get Microsoft software hotfixes

--- src/data_provider/src/packages/packagesWindowsParserHelper.h
+++ src/data_provider/src/packages/packagesWindowsParserHelper.h
@@ -135,7 +135,7 @@ namespace PackageWindowsHelper
                 {
                     const auto callbackKey
                     {
-                        [&key, &subKey, &packageKey, &hotfixes](const std::string & package)
+                        [&key, &subKey, &packageKey,&hotfixes](const std::string & package)
                         {
                             auto hfValue { extractHFValue(package) };
 
@@ -157,12 +157,12 @@ namespace PackageWindowsHelper
                 nlohmann::json hotfixValue;
                 hotfixValue["hotfix"] = std::move(hotfix);
                 data.push_back(std::move(hotfixValue));
-            }
+            }  
         }
         catch (...)
         {
         }
-
+        
     }
 
     static void getHotFixFromRegProduct(const HKEY key, const std::string& subKey, nlohmann::json& data)
@@ -178,13 +178,12 @@ namespace PackageWindowsHelper
                     {
                         [&key, &subKey, &packageKey, &hotfixes](const std::string & package)
                         {
-
+                            
                             if (Utils::startsWith(package, "InstallProperties"))
                             {
-
+                                
                                 Utils::Registry packageReg{key, subKey + "\\" + packageKey + "\\" + package, KEY_WOW64_64KEY | KEY_ENUMERATE_SUB_KEYS | KEY_READ};
                                 std::string value;
-
                                 if (packageReg.string("DisplayName", value))
                                 {
                                     auto hfValue { extractHFValue(value) };
@@ -194,6 +193,28 @@ namespace PackageWindowsHelper
                                         hotfixes.insert(std::move(hfValue));
                                     }
                                 }
+                            }
+                            else if (Utils::startsWith(package, "Patches"))
+                            {
+                                const auto callbackPatch
+                                {
+                                    [&key, &subKey, &packageKey, &package, &hotfixes](const std::string & packagePatch)
+                                    {
+                                        Utils::Registry packageReg{key, subKey + "\\" + packageKey + "\\" + package + "\\" + packagePatch, KEY_WOW64_64KEY | KEY_ENUMERATE_SUB_KEYS | KEY_READ};
+                                        std::string value;
+                                        if (packageReg.string("DisplayName", value))
+                                        {
+                                            auto hfValue { extractHFValue(value) };
+
+                                            if (!hfValue.empty())
+                                            {
+                                                hotfixes.insert(std::move(hfValue));
+                                            }
+                                        }
+                                    }
+                                };
+                                Utils::Registry rootPatch{key, subKey + "\\" + packageKey + "\\" + package, KEY_WOW64_64KEY | KEY_ENUMERATE_SUB_KEYS | KEY_READ};
+                                rootPatch.enumerate(callbackPatch);
                             }
                         }
                     };
@@ -209,12 +230,12 @@ namespace PackageWindowsHelper
                 nlohmann::json hotfixValue;
                 hotfixValue["hotfix"] = std::move(hotfix);
                 data.push_back(std::move(hotfixValue));
-            }
+            }  
         }
         catch (...)
         {
         }
-
+        
     }
 };
 
