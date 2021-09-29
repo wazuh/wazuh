488af70862a41d7c54a3d05bdf30a07daf8716bd
Hanes Nahuel Sciarrone <hanessciarrone@gmail.com>
2021-09-29 18:00:56 -0300

    Unit test for wdb_syscollector.c with 100% coverage

--- src/unit_tests/wrappers/externals/sqlite/sqlite3_wrappers.c
+++ src/unit_tests/wrappers/externals/sqlite/sqlite3_wrappers.c
@@ -39,6 +39,19 @@ int __wrap_sqlite3_bind_int64(__attribute__((unused)) sqlite3_stmt *stmt,
     return mock();
 }
 
+int __wrap_sqlite3_bind_double(__attribute__((unused)) sqlite3_stmt *pStmt, int index, double value) {
+    check_expected(index);
+    check_expected(value);
+
+    return mock();
+}
+
+int __wrap_sqlite3_bind_null(__attribute__((unused)) sqlite3_stmt *pStmt, int index) {
+    check_expected(index);
+
+    return mock();
+}
+
 void expect_sqlite3_bind_int64_call(int idx, double val, int ret) {
     expect_value(__wrap_sqlite3_bind_int64, index, idx);
     expect_value(__wrap_sqlite3_bind_int64, value, val);
