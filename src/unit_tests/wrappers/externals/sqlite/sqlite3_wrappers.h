ee97ff69010fda42831fe5bcf646e042104b1e89
Hanes Sciarrone <58960358+HanesSciarrone@users.noreply.github.com>
2021-10-01 02:34:05 -0300

    Unit test for wdb_syscollector.c with 100% coverage (#10352)

--- src/unit_tests/wrappers/externals/sqlite/sqlite3_wrappers.h
+++ src/unit_tests/wrappers/externals/sqlite/sqlite3_wrappers.h
@@ -23,6 +23,10 @@ int __wrap_sqlite3_bind_int64(sqlite3_stmt *stmt,
                               int index,
                               sqlite3_int64 value);
 
+int __wrap_sqlite3_bind_double(sqlite3_stmt *pStmt, int index, double value);
+
+int __wrap_sqlite3_bind_null(sqlite3_stmt *pStmt, int index);
+
 void expect_sqlite3_bind_int64_call(int idx, double val, int ret);
 
 int __wrap_sqlite3_bind_text(sqlite3_stmt* pStmt,
