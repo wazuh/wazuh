b833a4ce5850913692af28cff9c61a7a91f6e2b5
juanmanuelug <juan.utrera@wazuh.com>
2021-10-08 16:57:54 +0200

    Added new settings to be readable by the API

--- src/os_auth/config.c
+++ src/os_auth/config.c
@@ -17,9 +17,6 @@
 int authd_read_config(const char *path) {
     config.port = DEFAULT_PORT;
     config.force_options.connection_time = -1;
-    config.key_request.compatibility_flag = 0;
-    config.key_request.exec_path = NULL;
-    config.key_request.socket = NULL;
 
     mdebug2("Reading configuration '%s'", path);
 
@@ -75,6 +72,12 @@ cJSON *getAuthdConfig(void) {
     if (config.agent_ca) cJSON_AddStringToObject(auth,"ssl_agent_ca",config.agent_ca);
     if (config.manager_cert) cJSON_AddStringToObject(auth,"ssl_manager_cert",config.manager_cert);
     if (config.manager_key) cJSON_AddStringToObject(auth,"ssl_manager_key",config.manager_key);
+    if (config.key_request.enabled) cJSON_AddStringToObject(auth,"enabled","yes"); else cJSON_AddStringToObject(authd,"enabled","no");
+    if (config.key_request.exec_path) cJSON_AddStringToObject(auth,"exec_path",config.key_request.exec_path);
+    if (config.key_request.socket) cJSON_AddStringToObject(auth,"socket",config.key_request.socket);
+    if (config.key_request.timeout) cJSON_AddNumberToObject(auth,"timeout",config.key_request.timeout);
+    if (config.key_request.threads) cJSON_AddNumberToObject(auth,"threads",config.key_request.threads);
+    if (config.key_request.queue_size) cJSON_AddNumberToObject(auth,"queue_size",config.key_request.queue_size);
 
     cJSON_AddItemToObject(root,"auth",auth);
 
