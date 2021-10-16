a6eede799c8592c9e2742b4a2046ffa292012f42
Rivero, Franco <franco.rivero@wazuh.com>
2021-10-16 14:43:59 -0300

    Add changes on tests folder, delete test example, and add a check to increase coverage

--- src/syscheckd/db/tests/testFolder/helloWorldTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
