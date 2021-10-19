484710c26026b1ba0c63d285881471eddc606c79
Rivero, Franco <franco.rivero@wazuh.com>
2021-10-19 11:02:54 -0300

    Add RegistryValue class with its methods implementation

--- src/syscheckd/db/include/dbRegistryValue.hpp
+++ src/syscheckd/db/include/dbRegistryValue.hpp
@@ -11,24 +11,90 @@
 
 #ifndef _REGISTRYVALUE_HPP
 #define _REGISTRYVALUE_HPP
-#include "shared.h"
+#include <nlohmann/json.hpp>
 #include "dbItem.hpp"
-#include "dbRegistryValue.hpp"
+#include <iostream>
 
+struct FimRegistryValueDeleter {
+    void operator()(fim_entry* address) {
+        if (address) {
+            if (address->registry_entry.value) {
+                std::free(address->registry_entry.value);
+            }
+            std::free(address);
+        }
+    }
+};
 class RegistryValue final : public DBItem {
 public:
-    RegistryValue();
-    ~RegistryValue();
-    fim_entry* toFimEntry();
-    nlohmann::json* toJSON();
+    RegistryValue(fim_entry * const fim)
+                : DBItem(std::string(fim->registry_entry.value->name)
+                , fim->registry_entry.value->scanned
+                , fim->registry_entry.value->last_event
+                , fim->registry_entry.value->checksum
+                , fim->registry_entry.value->mode) {
+                    m_keyUid = fim->registry_entry.value->id;
+                    m_registryKey = 0;
+                    m_size = fim->registry_entry.value->size;
+                    m_type = fim->registry_entry.value->type;
+                    m_md5 = std::string(fim->registry_entry.value->hash_md5);
+                    m_sha1 = std::string(fim->registry_entry.value->hash_sha1);
+                    m_sha256 = std::string(fim->registry_entry.value->hash_sha256);
+                    createJSON();
+                    createFimEntry();
+    }
+    RegistryValue(const std::string &name,
+                  const std::string &checksum,
+                  const time_t &lastEvent,
+                  const unsigned int &scanned,
+                  const fim_event_mode &mode,
+                  const unsigned int &registryKey,
+                  const unsigned int &rowID,
+                  const std::string &md5,
+                  const std::string &sha1,
+                  const std::string &sha256,
+                  const unsigned int &size,
+                  const unsigned int &type)
+                  : DBItem(name, scanned, lastEvent, checksum, mode)
+                  , m_keyUid( rowID )
+                  , m_registryKey ( registryKey )
+                  , m_size( size )
+                  , m_type( type )
+                  , m_md5( md5 )
+                  , m_sha1( sha1 )
+                  , m_sha256( sha256 ) {
+                    createFimEntry();
+                    createJSON();
+    }
+    RegistryValue(const nlohmann::json &fim)
+    : DBItem(fim.at("name"), fim.at("scanned"), fim.at("last_event"), fim.at("checksum"), fim.at("mode")) {
+        std::cout << fim.dump() << std::endl;
+        m_keyUid = fim.at("id");
+        m_registryKey = 0;
+        m_size = fim.at("size");
+        m_type = fim.at("type");
+        m_md5 = fim.at("hash_md5");
+        m_sha1 = fim.at("hash_sha1");
+        m_sha256 = fim.at("hash_sha256");
+        createFimEntry();
+        m_statementConf = std::make_unique<nlohmann::json>(fim);
+    }
+    ~RegistryValue() = default;
+    fim_entry* toFimEntry() { return m_fimEntry.get(); };
+    nlohmann::json* toJSON() { return m_statementConf.get(); };
 
 private:
-    int             m_type;
-    int             m_keyUid;
-    int             m_size;
-    std::string     m_registryKey;
-    std::string     m_md5;
-    std::string     m_sha1;
-    std::string     m_sha256;
+    unsigned int                                        m_keyUid;
+    unsigned int                                        m_registryKey;
+    unsigned int                                        m_size;
+    unsigned int                                        m_type;
+    std::string                                         m_md5;
+    std::string                                         m_sha1;
+    std::string                                         m_sha256;
+    std::unique_ptr<fim_entry, FimRegistryValueDeleter> m_fimEntry;
+    std::unique_ptr<nlohmann::json>                     m_statementConf;
+
+    void createFimEntry();
+    void createJSON();
 };
 #endif //_REGISTRYVALUE_HPP
---
+++ src/syscheckd/db/src/dbRegistryValue.cpp
@@ -0,0 +1,46 @@
+/*
+ * Wazuh Syscheckd
+ * Copyright (C) 2015-2021, Wazuh Inc.
+ * October 18, 2021.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+#include "dbRegistryValue.hpp"
+
+void RegistryValue::createFimEntry() {
+    fim_entry *fim = reinterpret_cast<fim_entry*>(std::calloc(1, sizeof(fim_entry)));;
+    fim_registry_value_data *value = reinterpret_cast<fim_registry_value_data*>(std::calloc(1, sizeof(fim_registry_value_data)));
+
+    fim->type = FIM_TYPE_REGISTRY;
+    value->size = m_size;
+    value->id = m_keyUid;
+    value->name = const_cast<char*>(m_identifier.c_str());
+    std::strncpy(value->hash_md5, m_md5.c_str(), sizeof(value->hash_md5));
+    std::strncpy(value->hash_sha1, m_sha1.c_str(), sizeof(value->hash_sha1));
+    std::strncpy(value->hash_sha256, m_sha256.c_str(), sizeof(value->hash_sha256));
+    value->mode = m_mode;
+    value->last_event = m_lastEvent;
+    value->scanned = m_scanned;
+    std::strncpy(value->checksum, m_checksum.c_str(), sizeof(value->checksum));
+    fim->registry_entry.value = value;
+    m_fimEntry = std::unique_ptr<fim_entry, FimRegistryValueDeleter>(fim);
+}
+
+void RegistryValue::createJSON() {
+    nlohmann::json conf = {};
+
+    conf.push_back(nlohmann::json::object_t::value_type("id", m_keyUid));
+    conf.push_back(nlohmann::json::object_t::value_type("mode", m_mode));
+    conf.push_back(nlohmann::json::object_t::value_type("last_event", m_lastEvent));
+    conf.push_back(nlohmann::json::object_t::value_type("scanned", m_scanned));
+    conf.push_back(nlohmann::json::object_t::value_type("name", m_identifier));
+    conf.push_back(nlohmann::json::object_t::value_type("checksum", m_checksum));
+    conf.push_back(nlohmann::json::object_t::value_type("size", m_size));
+    conf.push_back(nlohmann::json::object_t::value_type("hash_md5", m_md5));
+    conf.push_back(nlohmann::json::object_t::value_type("hash_sha1", m_sha1));
+    conf.push_back(nlohmann::json::object_t::value_type("hash_sha256", m_sha256));
+    m_statementConf = std::make_unique<nlohmann::json>(conf);
+}
