7eed138c65826e1b332b09c18985d87def4e4afd
alromero <alvaro.romero@wazuh.com>
2021-10-25 12:39:49 +0200

    Deprecate chroot usage in authd

--- src/os_auth/main-server.c
+++ src/os_auth/main-server.c
@@ -82,7 +82,7 @@ static void help_authd(char * home_path)
     print_out("    -t          Test configuration.");
     print_out("    -f          Run in foreground.");
     print_out("    -g <group>  Group to run as. Default: %s.", GROUPGLOBAL);
-    print_out("    -D <dir>    Directory to chroot into. Default: %s.", home_path);
+    print_out("    -D <dir>    Directory to chdir into. Default: %s.", home_path);
     print_out("    -p <port>   Manager port. Default: %d.", DEFAULT_PORT);
     print_out("    -P          Enable shared password authentication, at %s or random.", AUTHD_PASS);
     print_out("    -c          SSL cipher list (default: %s)", DEFAULT_CIPHERS);
@@ -470,7 +470,6 @@ int main(int argc, char **argv)
         }
     }
 
-    /* Before chroot */
     srandom_init();
     getuname();
 
@@ -479,12 +478,6 @@ int main(int argc, char **argv)
         shost[sizeof(shost) - 1] = '\0';
     }
 
-    /* Chroot */
-    if (Privsep_Chroot(home_path) < 0) {
-        merror_exit(CHROOT_ERROR, home_path, errno, strerror(errno));
-    }
-
-    nowChroot();
     os_free(home_path);
 
     /* Initialize queues */
