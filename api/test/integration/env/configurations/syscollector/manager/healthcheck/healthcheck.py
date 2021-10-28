e867352222b39be52ab076de0a9ab9d9fbeac71d
Víctor Fernández Poyatos <victor.f.poyatos@wazuh.com>
2021-10-28 12:31:42 +0200

    Change syscollector healthcheck for managers on AIT

--- api/test/integration/env/configurations/syscollector/manager/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/syscollector/manager/healthcheck/healthcheck.py
@@ -1,5 +1,4 @@
 import os
-import socket
 import sys
 sys.path.append('/tools')
 
@@ -9,4 +8,4 @@ if __name__ == "__main__":
     # Workers are not needed in this test, so the exit code is set to 0 (healthy).
     exit(os.system(
         "grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
-         or get_manager_health_base()) if socket.gethostname() == 'wazuh-master' else exit(0)
+         or get_manager_health_base())
