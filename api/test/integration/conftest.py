d6e876ef68f7006d76eb9e8daa25f51842dcc354
Manuel <manuel.carmona@wazuh.com>
2021-11-02 11:35:51 +0100

    Use pytest hook to add standalone and cluster marks to AIT

--- api/test/integration/conftest.py
+++ api/test/integration/conftest.py
@@ -25,9 +25,27 @@ login_headers = {'Content-Type': 'application/json',
                  'Authorization': f'Basic {b64encode(basic_auth).decode()}'}
 environment_status = None
 
+standalone_env_mode = 'standalone'
+cluster_env_mode = 'cluster'
+
 
 def pytest_addoption(parser):
     parser.addoption('--nobuild', action='store_false', help='Do not run docker-compose build.')
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]):
+    """Pytest hook used to add standalone and cluster marks to tests having none of them.
+
+    Parameters
+    ----------
+    items : list[pytest.Item]
+        List of pytest items collected in the pytest session.
+    """
+    for item in items:
+        item_marker_names = [m.name for m in item.own_markers]
+        if standalone_env_mode not in item_marker_names and cluster_env_mode not in item_marker_names:
+            item.add_marker(standalone_env_mode)
+            item.add_marker(cluster_env_mode)
 
 
 def get_token_login_api():
