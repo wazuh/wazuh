a884692ed671e91d2769356435d623b71c051c03
Manuel Carmona Pérez <manuel.carmona@wazuh.com>
2021-11-04 13:00:00 +0100

    Allow API integration tests to be passed in an environment with cluster disabled (standalone mode) (#10620)
    * Add AIT environment changes to pass tests in standalone mode
    
    * Add manager.Dockerfile changes to use the healthcheck with a parameter
    
    * Add changes to avoid using two nginx-lb folders to set the AIT env
    
    * Remove unnecessary ENV call from NGINX Dockerfile
    
    * Use only one entrypoint to change cluster configuration for standalone AIT
    
    * Remove daemons_check_no_cluster and check standalone daemons with the daemons_check.txt
    
    * Update API integration test mapping JSON
    
    * Add changes to use an only NGINX configuration file and docstrings changes
    
    * Use profiles in docker-compose.yml to avoid using 2 compose files
    
    * Update API integration test mapping JSON
    
    * Add docker profiles to all docker-compose services
    
    * Add requested changes to conftest and manager entrypoint

--- api/test/integration/env/base/nginx-lb/entrypoint.sh
+++ api/test/integration/env/base/nginx-lb/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $1 == "standalone" ]; then
+  # Remove workers upstream configurations (in upstream mycluster and upstream register)
+  sed -i -E '/wazuh-worker1|wazuh-worker2/d' /etc/nginx/nginx.conf;
+fi
+
 until service nginx start; do
   echo "nginx couldn´t start - sleeping for 1 second"
   sleep 1
