98641cd4873abab2c1d94a15e65a3cf4067be0b9
Juan Nicolas Asselle <jnasselle@gmail.com>
2021-11-16 12:46:04 +0000

    Add CVEs inventory fields to syscollecto delta handling structs

--- src/analysisd/decoders/syscollector.c
+++ src/analysisd/decoders/syscollector.c
@@ -196,6 +196,8 @@ static struct deltas_fields_match_list const OS_FIELDS[] = {
     { .current = { "version", "os.version" }, .next = &OS_FIELDS[14]},
     { .current = { "os_release", "os.os_release" }, .next = &OS_FIELDS[15]},
     { .current = { "os_display_version", "os.display_version" }, .next = &OS_FIELDS[16]},
+    { .current = { "triaged", "" }, .next = &OS_FIELDS[17]},
+    { .current = { "reference", "" }, .next = &OS_FIELDS[18]},
     { .current = { "checksum", "" }, .next = NULL},
 };
 
--- src/wazuh_db/wdb_parser.c
+++ src/wazuh_db/wdb_parser.c
@@ -166,9 +166,11 @@ static struct column_list const TABLE_OS[] = {
     { .value = { FIELD_TEXT, 15, false, false, "version" }, .next = &TABLE_OS[15] },
     { .value = { FIELD_TEXT, 16, false, false, "os_release" }, .next = &TABLE_OS[16] },
     { .value = { FIELD_TEXT, 17, false, false, "os_display_version" }, .next = &TABLE_OS[17] },
-    { .value = { FIELD_TEXT, 18, false, false, "checksum" }, .next = NULL }
+    { .value = { FIELD_INTEGER, 18, false, false, "triaged" }, .next = &TABLE_OS[18] },
+    { .value = { FIELD_TEXT, 19, false, false, "reference" }, .next = &TABLE_OS[19] },
+    { .value = { FIELD_TEXT, 20, false, false, "checksum" }, .next = NULL }
 };
-#define OS_FIELD_COUNT 17
+#define OS_FIELD_COUNT 19
 
 static struct column_list const TABLE_HARDWARE[] = {
     { .value = { FIELD_INTEGER, 1, true, false, "scan_id" }, .next = &TABLE_HARDWARE[1] },
