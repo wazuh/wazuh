0e4b52a75efc12196c657079b170577bf67e01a2
Manuel <manuel.carmona@wazuh.com>
2021-11-19 10:57:44 +0100

    Use the check function in os.system calls in AIT env

--- api/test/integration/env/configurations/experimental/agent/healthcheck/legacy_healthcheck.py
+++ api/test/integration/env/configurations/experimental/agent/healthcheck/legacy_healthcheck.py
@@ -1,18 +1,10 @@
 import os
 import sys
+
 sys.path.append('/tools')
 
-from healthcheck_utils import get_agent_health_base
-
-
-def get_health():
-    output = os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log")
-
-    if output == 0:
-        return 0
-    else:
-        return 1
-
+from healthcheck_utils import get_agent_health_base, check
 
 if __name__ == "__main__":
-    exit(get_health() or get_agent_health_base())
+    exit(check(os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log"))
+         or get_agent_health_base())
--- api/test/integration/env/configurations/syscheck/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/syscheck/agent/healthcheck/healthcheck.py
@@ -1,19 +1,11 @@
 import os
 import sys
+
 sys.path.append('/tools')
 
-from healthcheck_utils import get_agent_health_base
-
-
-def get_health():
-    output = os.system(
-        "grep -q 'syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log")
-
-    if output == 0:
-        return 0
-    else:
-        return 1
-
+from healthcheck_utils import get_agent_health_base, check
 
 if __name__ == "__main__":
-    exit(get_health() or get_agent_health_base())
+    exit(check(os.system(
+        "grep -q 'syscheckd: INFO: (6009): File integrity monitoring scan ended.' /var/ossec/logs/ossec.log"))
+         or get_agent_health_base())
