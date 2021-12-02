71fbaa8c255de774fadb9c0a8a2a7263f46e6277
Christian Borla <borlach12@gmail.com>
2021-12-02 20:05:11 -0300

    Fixed OS_IsValidIP when ip_address is 'any' and final_ip not null

--- src/shared/validate_op.c
+++ src/shared/validate_op.c
@@ -486,6 +486,7 @@ int OS_IsValidIP(const char *ip_address, os_ip *final_ip)
     else {
         /* any case */
         if (final_ip) {
+            os_calloc(1, sizeof(os_ipv4), final_ip->ipv4);
             final_ip->ipv4->ip_address = 0;
             final_ip->ipv4->netmask = 0;
         }
