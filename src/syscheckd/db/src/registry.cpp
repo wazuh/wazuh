90feb5d805059a54dd043ee517600f869f3e69c1
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-12-07 19:14:22 +0100

    Add definition and declaration for registry wrappers methods

--- src/syscheckd/db/src/registry.cpp
+++ src/syscheckd/db/src/registry.cpp
@@ -5,11 +5,25 @@
  *
  * @copyright Copyright (C) 2015-2021 Wazuh, Inc.
  */
+
+#include <iostream>
+#include <string.h>
+
+#ifdef WAZUH_UNIT_TESTING
+    #include "fimDBHelpersUTInterface.hpp"
+#else
+    #include "fimDBHelper.hpp"
+    #include "fimDB.hpp"
+#endif
+
+#include "dbRegistryValue.hpp"
+#include "dbRegistryKey.hpp"
+#include "fimCommonDefs.h"
+#include "registry.hpp"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "db.h"
 
 extern const char* SQL_STMT[];
 
@@ -19,62 +33,124 @@ const char* registry_arch[] =
     [ARCH_64BIT] = "[x64]"
 };
 
-int fim_db_remove_registry_key(fim_entry* entry)
+int fim_db_remove_registry_key(fim_registry_key* key_entry)
 {
-    /* TODO: Add c++ code to delete a registry from key_id
-    */
-    return FIMDB_OK;
+
+    std::string filter = "WHERE id = " + std::to_string(key_entry->id) + " AND arch = " +
+                         std::to_string(key_entry->arch);
+
+    try
+    {
+        FIMDBHelper::removeFromDB<FIMDB>(std::string(FIMDB_REGISTRY_KEY_TABLENAME), filter);
+        return FIMDB_OK;
+    } catch(std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 }
 
-int fim_db_remove_registry_value_data(fim_registry_value_data* entry)
+int fim_db_remove_registry_value_data(fim_registry_value_data* value_entry)
 {
-    /* TODO: Add c++ code to delete a registry from fim_registry_value_data
-    */
 
-    return FIMDB_OK;
-}
+    std::string filter = "WHERE key_id = " + std::to_string(value_entry->id) + " AND name = " +
+                         std::to_string(*value_entry->name);
 
-// Registry functions
-int fim_db_get_registry_key_rowid(const char* path, unsigned int arch, unsigned int* rowid)
-{
-    /* TODO: Add c++ code to get a registry key from path, arch and rowid
-    */
-
-    return FIMDB_OK;
+    try
+    {
+        FIMDBHelper::removeFromDB<FIMDB>(std::string(FIMDB_REGISTRY_VALUE_TABLENAME), filter);
+        return FIMDB_OK;
+    } catch(std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 }
 
 fim_registry_value_data* fim_db_get_registry_data(unsigned int key_id, const char* name)
 {
-    fim_registry_value_data* value = NULL;
-    /* TODO: Add c++ code to get a registry data from key_id and name
-    */
 
-    return value;
+    nlohmann::json dbItem;
+    std::string strName(name);
+
+    std::string rawFilter = "WHERE name = " + strName + " AND key_id = " + std::to_string(key_id);
+    nlohmann::json columnList;
+    columnList["column_list"] = "[*]";
+
+    nlohmann::json query = FIMDBHelper::dbQuery(std::string(FIMDB_REGISTRY_VALUE_TABLENAME), columnList, rawFilter, "");
+
+    try
+    {
+        FIMDBHelper::getDBItem<FIMDB>(dbItem, query);
+    }catch(std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return 0;
+    }
+
+    RegistryValue reg_value(dbItem);
+    return (reg_value.toFimEntry())->registry_entry.value;
 }
 
 fim_registry_key* fim_db_get_registry_key(const char* path, unsigned int arch)
 {
-    fim_registry_key* reg_key = NULL;
-    /* TODO: Add c++ code to get a registry key from path and arch
-    */
+    nlohmann::json dbItem;
+    std::string strPath(path);
 
-    return reg_key;
+    std::string rawFilter = "WHERE path = " + strPath + " AND arch = " + std::to_string(arch);
+    nlohmann::json columnList;
+    columnList["column_list"] = "[*]";
+
+    nlohmann::json query = FIMDBHelper::dbQuery(std::string(FIMDB_REGISTRY_KEY_TABLENAME), columnList, rawFilter, "");
+    try
+    {
+        FIMDBHelper::getDBItem<FIMDB>(dbItem, query);
+    }catch(std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return 0;
+    }
+
+    RegistryKey reg_key(dbItem);
+    return reg_key.toFimEntry()->registry_entry.key;
+
 }
 
-fim_registry_key* fim_db_get_registry_key_using_id(unsigned int id)
+fim_registry_key* fim_db_get_registry_key_using_id(unsigned int key_id)
 {
-    fim_registry_key* reg_key = NULL;
-    /* TODO: Add c++ code to get a registry key from id
-    */
+    nlohmann::json dbItem;
 
-    return reg_key;
+    std::string rawFilter = "WHERE item_id = " + std::to_string(key_id);
+    nlohmann::json columnList;
+    columnList["column_list"] = "[*]";
+
+    nlohmann::json query = FIMDBHelper::dbQuery(std::string(FIMDB_REGISTRY_KEY_TABLENAME), columnList, rawFilter, "");
+
+    try
+    {
+        FIMDBHelper::getDBItem<FIMDB>(dbItem, query);
+    }catch(std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return 0;
+    }
+
+    RegistryKey reg_key(dbItem);
+    return reg_key.toFimEntry()->registry_entry.key;
 }
 
 int fim_db_get_count_registry_key()
 {
     int res = 0;
-    /* TODO: Add c++ code to count registry keys
-    */
+
+    try
+    {
+        FIMDBHelper::getCount<FIMDB>(std::string(FIMDB_REGISTRY_KEY_TABLENAME), res);
+    } catch(std::exception& ex) // EMPTY_TABLE_METADATA
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 
     return res;
 }
@@ -82,32 +158,55 @@ int fim_db_get_count_registry_key()
 int fim_db_get_count_registry_data()
 {
     int res = 0;
-    /* TODO: Add c++ code to get a registry key from path and arch
-    */
+    try
+    {
+        FIMDBHelper::getCount<FIMDB>(std::string(FIMDB_REGISTRY_VALUE_TABLENAME), res);
+    } catch(std::exception& ex) // EMPTY_TABLE_METADATA
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 
     return res;
 }
 
-int fim_db_insert_registry_data(fim_registry_value_data* data,
-                                unsigned int key_id,
-                                unsigned int replace_entry)
+int fim_db_insert_registry_data(fim_registry_value_data* data)
 {
-    int res = 0;
-    /* TODO: Add c++ code to insert or update a registry data
-    */
+    fim_entry regValueEntry;
+    regValueEntry.registry_entry.value = data;
+    RegistryValue regValue(&regValueEntry);
+
+    try
+    {
+        FIMDBHelper::updateItem<FIMDB>(std::string(FIMDB_REGISTRY_VALUE_TABLENAME), *regValue.toJSON());
+    }
+    catch(const std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 
     return FIMDB_OK;
 }
 
-int fim_db_insert_registry_key(fim_registry_key* entry, unsigned int rowid)
+int fim_db_insert_registry_key(fim_registry_key* entry)
 {
-    int res = 0;
-    /* TODO: Add c++ code to insert or update a registry key
-    */
+
+    fim_entry regEntryKey;
+    regEntryKey.registry_entry.key = entry;
+    RegistryKey regKey(&regEntryKey);
+
+    try
+    {
+        FIMDBHelper::updateItem<FIMDB>(std::to_string(*FIMDB_REGISTRY_KEY_TABLENAME), *regKey.toJSON());
+    }
+    catch(const std::exception& ex)
+    {
+        FIMDB::getInstance().loggingFunction(LOG_ERROR, ex.what());
+        return FIMDB_ERR;
+    }
 
     return FIMDB_OK;
 }
 
-#ifdef __cplusplus
 }
-#endif
