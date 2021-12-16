e1c22c5ac1c884b8dc9328332dc4c5954c679656
Víctor Fernández Poyatos <victor.f.poyatos@wazuh.com>
2021-12-16 18:01:53 +0100

    Make process pool executors part of a context var

--- framework/wazuh/core/cluster/dapi/dapi.py
+++ framework/wazuh/core/cluster/dapi/dapi.py
@@ -31,8 +31,8 @@ from wazuh.core.cluster.cluster import check_cluster_status
 from wazuh.core.exception import WazuhException, WazuhClusterError, WazuhError
 from wazuh.core.wazuh_socket import wazuh_sendsync
 
-process_pool = ProcessPoolExecutor(max_workers=1)
-authentication_pool = ProcessPoolExecutor(max_workers=1)
+process_pool = common.mp_pools.get()['process_pool']
+authentication_pool = common.mp_pools.get()['authentication_pool']
 authentication_funcs = {'check_token', 'check_user_master', 'get_permissions', 'get_security_conf'}
 
 
