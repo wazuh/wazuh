3197f80abcf3c372ad07990d34787af96ec15fc3
Víctor Fernández Poyatos <victor.f.poyatos@wazuh.com>
2021-12-17 10:54:05 +0100

    Fix rbac.db permissions on API integration tests

--- api/test/integration/env/configurations/security/manager/configuration_files/security_config.sh
+++ api/test/integration/env/configurations/security/manager/configuration_files/security_config.sh
@@ -3,3 +3,5 @@
 # RBAC configuration
 sqlite3 /var/ossec/api/configuration/security/rbac.db < /tmp/configuration_files/schema_security_test.sql
 sqlite3 /var/ossec/api/configuration/security/rbac.db < /tmp/configuration_files/base_security_test.sql
+chown wazuh:wazuh /var/ossec/api/configuration/security/rbac.db
+chmod 640 /var/ossec/api/configuration/security/rbac.db
