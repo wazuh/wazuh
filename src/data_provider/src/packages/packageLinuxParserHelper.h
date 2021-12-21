b13a282806c60fe8082f65a38b0fcd1930323bc4
Ramiro Romero Dapozo <ramiroromero94@gmail.com>
2021-12-21 09:06:35 -0300

    Apply suggestions from code review
    Missing newlines at EOF and copyright notice
    
    Co-authored-by: Juan Nicolas Asselle <jnasselle@gmail.com>

--- src/data_provider/src/packages/packageLinuxParserHelper.h
+++ src/data_provider/src/packages/packageLinuxParserHelper.h
@@ -15,6 +15,15 @@
 #include <fstream>
 #include "sharedDefs.h"
 #include "cmdHelper.h"
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+ 
 #include "stringHelper.h"
 #include "json.hpp"
 #include "timeHelper.h"
