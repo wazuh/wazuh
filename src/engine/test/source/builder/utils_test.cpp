7520b1011e25ae384f5a5d08f116a24b7d728e7a
Javier Bejar Mendez <javier.bejar@wazuh.com>
2021-12-22 11:23:35 +0100

    Added condition_value operation and util JsonPath class
    - Added operation condition_value builder and test
    - Added namespace utils under builder
    - Added JsonPath class to traverse json path strings to utils
    - Updated CMakeLists

---
+++ src/engine/test/source/builder/utils_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+
+#include "builder.h"
+
+#define GTEST_COUT std::cerr << "[          ] [ INFO ] "
+
+
+using namespace std;
+
+TEST(BuilderUtils, JsonPath)
+{
+    string s = "this.is.some.string";
+    vector<string> expected{"this", "is", "some", "string"};
+    builder::utils::JsonPath jp(s);
+    auto i = 0;
+    for_each(jp.begin(), jp.end(), [&i, &expected](auto field)
+    {
+        ASSERT_EQ(field, expected[i]);
+        i++;
+    });
+
+}
