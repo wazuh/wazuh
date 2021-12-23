2ac983674d5257930ef5de9fa0792c971ca2328b
Julian Morales <jmorales@unc.edu.ar>
2021-12-23 06:52:00 +0000

    Adds interface for drivers storage

--- src/engine/source/catalog/storageDriver/disk/diskStorage.cpp
+++ src/engine/source/catalog/storageDriver/disk/diskStorage.cpp
@@ -45,47 +45,48 @@ std::vector<std::string> diskStorage::getAssetList(const AssetType type)
             std::cout << "Adding file: '" << entry.path() << "' . A yml file"  << std::endl;
             const string asset_name {entry.path().stem().string()};
             assetList.push_back(std::move(asset_name));
-        // } else
-        // {
-        //     std::cout << "ignoring file: '" << entry.path() << "' . Not a yml file"  << std::endl;
+            // } else
+            // {
+            //     std::cout << "ignoring file: '" << entry.path() << "' . Not a yml file"  << std::endl;
         }
     }
 
     return assetList;
 }
 
-nlohmann::ordered_json diskStorage::getAsset(const AssetType type, const std::string_view assetName) {
+nlohmann::ordered_json diskStorage::getAsset(const AssetType type, const std::string_view assetName)
+{
     using std::string;
     using std::string_view;
     using nlohmann::json;
 
     std::filesystem::path base_dir {this->path};
-//    std::cout << "Getting file | Type: ";
+    //    std::cout << "Getting file | Type: ";
 
     switch (type)
     {
         case AssetType::Decoder:
-  //          std::cout << "Decoder" << std::endl;
+            //          std::cout << "Decoder" << std::endl;
             base_dir /= "decoders";
             break;
 
         case AssetType::Rule:
-    //        std::cout << "Rules" << std::endl;
+            //        std::cout << "Rules" << std::endl;
             base_dir /= "rules";
             break;
 
         case AssetType::JSONSchema:
-    //        std::cout << "JSONSchema" << std::endl;
+            //        std::cout << "JSONSchema" << std::endl;
             base_dir /= "schemas";
             break;
 
         case AssetType::Filter:
-    //        std::cout << "Filter" << std::endl;
+            //        std::cout << "Filter" << std::endl;
             base_dir /= "filters";
             break;
 
         default:
-    //        std::cout << "Unknown" << std::endl;
+            //        std::cout << "Unknown" << std::endl;
             break;
     }
 
@@ -100,7 +101,8 @@ nlohmann::ordered_json diskStorage::getAsset(const AssetType type, const std::st
 
         nlohmann::ordered_json j {tojson::detail::loadyaml(file_path.string())};
         return j;
-    } else
+    }
+    else
     {
         std::cout << "File not found: " << file_path << std::endl;
         return json {};
