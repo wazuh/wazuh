c976c04efe2dd458ea2c0572c8f3bb2c3c332749
Javier Bejar Mendez <javier.bejar@wazuh.com>
2021-12-23 11:10:36 +0100

    Added helper_exists builder, condition_builder and syntax namespace with definitions

---
+++ src/engine/source/builder/syntax.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+
+/**
+* @brief Defines syntax elements.
+*
+* This namespace contains all anchors and syntax elements that identify
+* different objects.
+*/
+namespace builder::syntax
+{
+
+    const std::string REFERENCE_ANCHOR("$");
+    const std::string HELPER_ANCHOR("+");
+    const std::string HELPER_ARG_ANCHOR("/");
+
+}
