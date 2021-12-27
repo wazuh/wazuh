4a164336199a447510e71387a997630913e1c73f
Julian Morales <jmorales@unc.edu.ar>
2021-12-27 21:43:50 +0000

    Adds yaml to json using rapidjson

--- src/engine/source/catalog/catalogSharedDef.hpp
+++ src/engine/source/catalog/catalogSharedDef.hpp
@@ -2,13 +2,14 @@
 #define __CATALOGSHAREDDEF_H__
 
 
-enum class AssetType {
-                Decoder,
-                Rule,
-                Output,
-                Filter,
-                Schemas,
-                Environments
-            };
+enum class AssetType
+{
+    Decoder,
+    Rule,
+    Output,
+    Filter,
+    Schemas,
+    Environments
+};
 
 #endif // __CATALOGSHAREDDEF_H__
