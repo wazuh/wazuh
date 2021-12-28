796ca3e3143d9bd1c68af364b390673e739797a5
Julian Morales <jmorales@unc.edu.ar>
2021-12-28 19:23:55 +0000

    Deteles old yml2json lib with nlohmann

--- src/engine/test/source/catalog/storageDriver/disk/diskStorage_test.cpp
+++ src/engine/test/source/catalog/storageDriver/disk/diskStorage_test.cpp
@@ -1,8 +1,9 @@
 #include <filesystem>
 
-#include "diskStorage_test.hpp"
 #include "rapidjson/document.h"
-#include "catalog/storageDriver/yml_to_json.hpp"
+#include "rapidjson/schema.h"
+#include "diskStorage_test.hpp"
+
 
 // const std::string currentDir = std::filesystem::current_path().string();
 // const std::string testDir {currentDir + "/test/source/catalog/storageDriver/disk/db_test"};
@@ -12,17 +13,36 @@ const std::string db_dir_test {"/root/repos/wazuh/src/engine/test/source/catalog
 TEST(diskStorage, path)
 {
 
-    //  diskStorage ds(db_dir_test);
-    //    storageDriverInterface* dsi {&ds};
+    diskStorage ds(db_dir_test);
+    storageDriverInterface* dsi {&ds};
 
-    //dsi->getAssetList(AssetType::Decoder);
-    //dsi->getAsset(AssetType::Decoder, "syslog").dump();
-    //std::string json_dump {dsi->getAsset(AssetType::Decoder, "syslog").dump()};
-    //std::string json_dump {dsi->getAsset(AssetType::Schemas, "wazuh-decoders").dump()};
+    auto syslogDec {dsi->getAsset(AssetType::Decoder, "syslog")};
+    auto decSchema {dsi->getAsset(AssetType::Schemas, "wazuh-decoders")};
 
-    //rapidjson::Document schema_json {};
+    rapidjson::SchemaDocument schema(decSchema);
+    rapidjson::SchemaValidator validator(schema);
 
-    yml2json::loadyaml("/root/repos/wazuh/src/engine/test/source/catalog/storageDriver/disk/db_test/decoders/syslog.yml");
+    if (!syslogDec.Accept(validator)) {
+        // Input JSON is invalid according to the schema
+        // Output diagnostic information
+        rapidjson::StringBuffer sb;
+        validator.GetInvalidSchemaPointer().StringifyUriFragment(sb);
+        printf("Invalid schema: %s\n", sb.GetString());
+        printf("Invalid keyword: %s\n", validator.GetInvalidSchemaKeyword());
+        sb.Clear();
+        validator.GetInvalidDocumentPointer().StringifyUriFragment(sb);
+        printf("Invalid document: %s\n", sb.GetString());
+    } else {
+        printf("Valid\n");
+    }
+
+    // rapidjson::StringBuffer SB;
+
+    // SB.Clear();
+    // rapidjson::Writer<rapidjson::StringBuffer> writer(SB);
+
+    // syslogDec.Accept(writer);
+    // std::cout << SB.GetString() << std::endl;
 
 
     EXPECT_STREQ("test out --> ", "json_dump.c_str()");
