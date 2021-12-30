0fd28f1552a7a3ac801ce6fedda32bd61ba292c9
Gonzalo Abril Paniza <gonzalo.abril@wazuh.com>
2021-12-30 13:06:32 +0100

    Support the use of the Azure integration module in Linux agents (#10870)
    * Make Azure module work in agents (#10662)
    
    * Remove legacy comment (#10662)
    
    * Add requested changes (#10662)
    
    * Fix indentation (#10662)

--- src/wazuh_modules/wm_azure.c
+++ src/wazuh_modules/wm_azure.c
@@ -10,7 +10,6 @@
  */
 
 #ifndef WIN32
-#ifndef CLIENT
 
 #include "wmodules.h"
 #include "wm_azure.h"
@@ -546,5 +545,4 @@ cJSON *wm_azure_dump(const wm_azure_t * azure) {
     return root;
 }
 
-#endif
 #endif
