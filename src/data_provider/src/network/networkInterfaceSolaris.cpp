544a230bc718753cb648f39bb3535d87a8dbf47d
Hanes Nahuel Sciarrone <hanessciarrone@gmail.com>
2021-12-30 20:42:17 -0300

    License commentary added

--- src/data_provider/src/network/networkInterfaceSolaris.cpp
+++ src/data_provider/src/network/networkInterfaceSolaris.cpp
@@ -1,3 +1,14 @@
+/*
+ * Wazuh SYSINFO
+ * Copyright (C) 2015-2021, Wazuh Inc.
+ * December 21, 2021.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
 #include "networkInterfaceSolaris.h"
 #include "sys/socket.h"
 
--- src/data_provider/src/network/networkSolarisHelper.cpp
+++ src/data_provider/src/network/networkSolarisHelper.cpp
@@ -1,3 +1,14 @@
+/*
+ * Wazuh SYSINFO
+ * Copyright (C) 2015-2021, Wazuh Inc.
+ * December 16, 2021.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
 #include <sys/sockio.h>
 #include "networkSolarisHelper.hpp"
 #include "UtilsWrapperUnix.hpp"
