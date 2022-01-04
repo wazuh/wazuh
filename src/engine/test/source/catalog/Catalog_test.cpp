e7e3a9d19cb10ed858f48cf6e555bb620b927036
Julian Morales <jmorales@unc.edu.ar>
2022-01-04 17:11:06 +0000

    get_decoder_valid

--- src/engine/test/source/catalog/Catalog_test.cpp
+++ src/engine/test/source/catalog/Catalog_test.cpp
@@ -1,7 +1,33 @@
 #include "Catalog_test.hpp"
+#include "rapidjson/writer.h"
 
-
+// Test that the getDecoder method return a valid decoder
 TEST(Catalog, get_decoder_valid)
 {
-    EXPECT_STREQ("decVal", "dec_Val");
+
+    auto storageDriver = std::make_unique<fakeStorage>();
+    auto catalog = std::make_unique<Catalog>(std::move(storageDriver));
+
+    rapidjson::Document decoder = catalog->getDecoder("syslog2");
+
+    if (decoder.IsNull())
+    {
+        FAIL() << "The decoder is null";
+    }
+    else if (decoder.IsObject())
+    {
+        if (decoder.FindMember("name")->value.IsString())
+        {
+            EXPECT_STREQ("syslog2", decoder.FindMember("name")->value.GetString());
+        }
+        else
+        {
+            FAIL() << "The decoder name is not a string";
+        }
+    }
+    else
+    {
+        FAIL() << "The decoder is not an object";
+    }
+
 }
