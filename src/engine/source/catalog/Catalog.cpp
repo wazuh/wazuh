a40bf77751e0e4e0d342a252765522b2b9ce1c4f
Julian Morales <jmorales@unc.edu.ar>
2022-01-06 21:09:19 +0000

    Adds catalog getAssets tests

--- src/engine/source/catalog/Catalog.cpp
+++ src/engine/source/catalog/Catalog.cpp
@@ -1,44 +1,15 @@
 #include "Catalog.hpp"
 #include "rapidjson/schema.h"
 
-rapidjson::Document Catalog::getDecoder(std::string_view decoderName) {
 
-    using std::string;
-    using rapidjson::Document;
-
-    auto decoderStr = storageDriver->getAsset(AssetType::Decoder, decoderName);
-    auto jsonSchemaStr = storageDriver->getAsset(AssetType::Schemas, "wazuh-decoder");
-
-    Document decoder {yml2json::loadYMLfromString(std::move(decoderStr))};
-    Document jsonSchema {yml2json::loadYMLfromString(std::move(jsonSchemaStr))};
-
-    // Validate the decoder
-    auto errorStr = validateJSON(decoder, jsonSchema);
-    std::cout << "Validation result: " << errorStr.value_or("OK") << std::endl;
-
-    if (errorStr) {
-        return {};
-    }
-
-    return decoder;
-}
-
-std::vector<std::string_view> Catalog::getDecoderList() {
-    // #TODO
-    return {};
-}
-
-rapidjson::Document getJSONSchema(AssetType assetType) {
-    // #TODO
-    return {};
-}
-
-std::optional<std::string> Catalog::validateJSON(rapidjson::Document& json, rapidjson::Document& schema) {
+std::optional<std::string> Catalog::validateJSON(rapidjson::Document& json, rapidjson::Document& schema)
+{
 
     rapidjson::SchemaDocument schemaDocument(schema);
     rapidjson::SchemaValidator validator(schemaDocument);
 
-    if (!json.Accept(validator)) {
+    if (!json.Accept(validator))
+    {
         // Input JSON is invalid according to the schema
         std::ostringstream oss {};
         rapidjson::StringBuffer sb;
@@ -57,7 +28,8 @@ std::optional<std::string> Catalog::validateJSON(rapidjson::Document& json, rapi
     return {};
 }
 
-rapidjson::Document Catalog::getAsset(const AssetType type, std::string_view assetName) {
+rapidjson::Document Catalog::getAsset(const AssetType type, std::string_view assetName)
+{
 
     using std::string;
     using rapidjson::Document;
@@ -65,39 +37,81 @@ rapidjson::Document Catalog::getAsset(const AssetType type, std::string_view ass
     string assetStr {};
     string assetSchemaStr {};
 
-    switch (type) {
+    switch (type)
+    {
         case AssetType::Decoder:
         case AssetType::Rule:
         case AssetType::Output:
         case AssetType::Filter:
 
             assetStr = storageDriver->getAsset(type, assetName);
-            if (assetStr.empty()) {
+
+            if (assetStr.empty())
+            {
                 return {};
             }
 
             assetSchemaStr = storageDriver->getAsset(AssetType::Schemas, assetTypeToSchema.at(type));
-            if (assetSchemaStr.empty()) {
+
+            if (assetSchemaStr.empty())
+            {
                 throw std::runtime_error("Could not get the schema '" + assetTypeToSchema.at(type) + "' for the asset type.");
             }
 
             break;
+
         case AssetType::Schemas:
+
+        // #TODO add schemas
         case AssetType::Environments:
         default:
             throw std::runtime_error("Not implemented");
             break;
     }
 
-    // Throw a YML::ParserException if the asset is not valid
+    // Parse asset. Throw a YML::ParserException if the asset is not valid
     Document asset {yml2json::loadYMLfromString(assetStr)};
-    Document jsonSchema {yml2json::loadYMLfromString(assetSchemaStr)};
+
+    // Parse schema
+    Document jsonSchema {};
+    jsonSchema.Parse(assetSchemaStr.c_str());
+
+    if (jsonSchema.HasParseError())
+    {
+        throw std::runtime_error("Could not parse the schema for the asset type.");
+    }
 
     // Validate the asset
     auto errorStr = validateJSON(asset, jsonSchema);
-    if (errorStr) {
+
+    if (errorStr)
+    {
         throw std::runtime_error("The asset '" + string{assetName} + "' is invalid: " + errorStr.value());
     }
 
     return asset;
+}
+
+std::vector<std::string_view> Catalog::getAssetList(const AssetType type)
+{
+
+    std::vector<std::string_view> assetList {};
+
+    switch (type)
+    {
+        case AssetType::Decoder:
+        case AssetType::Rule:
+        case AssetType::Output:
+        case AssetType::Filter:
+        case AssetType::Schemas:
+            assetList = this->storageDriver->getAssetList(type);
+            break;
+
+        case AssetType::Environments:
+        default:
+            throw std::runtime_error("Not implemented");
+            break;
+    }
+
+    return assetList;
 }
