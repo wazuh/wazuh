1d6533f3167510570541ace29096bae756b1b998
Julian Morales <jmorales@unc.edu.ar>
2022-01-06 21:09:19 +0000

    Adds catalog getAssets tests

--- src/engine/source/catalog/Catalog.hpp
+++ src/engine/source/catalog/Catalog.hpp
@@ -19,7 +19,8 @@
  * ----------TODO Singleton
  * The Catalog class is a singleton. (@warning not implemented yet)
  */
-class Catalog {
+class Catalog
+{
 
     private:
 
@@ -35,7 +36,7 @@ class Catalog {
          */
         std::optional<std::string> validateJSON(rapidjson::Document& json, rapidjson::Document& schema);
 
-        /** @brief Mapping the assets and the schema validator */
+        /** @brief Mapping the assets types and their schemas validator. */
         static const inline std::map<AssetType, std::string> assetTypeToSchema
         {
             {AssetType::Decoder, "wazuh-decoders"},
@@ -46,13 +47,6 @@ class Catalog {
             {AssetType::Environments, ""}
         };
 
-        std::string jsonToString(rapidjson::Document& json) {
-            rapidjson::StringBuffer buffer;
-            rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-            json.Accept(writer);
-            return buffer.GetString();
-        }
-
     public:
         /**
          * @brief Create the catalog manager from the given driver to connect.
@@ -61,14 +55,16 @@ class Catalog {
          * The driver will be deleted when the catalog is destroyed.
          * @param storageDriver The storage driver to connect to. The driver is destroyed when the catalog is freed.
          */
-        Catalog(std::unique_ptr<StorageDriverInterface> storageDriver) {
+        Catalog(std::unique_ptr<StorageDriverInterface> storageDriver)
+        {
             this->storageDriver = std::move(storageDriver);
         }
 
         /**
          * @brief Dump pending changes and freed driver storage.
          */
-        ~Catalog() {
+        ~Catalog()
+        {
             storageDriver.reset();
         }
 
@@ -79,34 +75,20 @@ class Catalog {
          * @param assetName The name of the asset.
          * @return rapidjson::Document The asset object. If the asset is not found, the document is empty.
          * @throws std::runtime_error If the asset is corrupted or cannot get the json schema to validate against.
-         * @throws CatalogExeptions The storage driver throws an exception.
+         * @throws std::runtime_error If the asset is not valid.(i.e. Enviromenment, schemas are not supported yet)
+         * @throws YML::ParserException If the yaml in the storage is corrupted.
+         * @throws filesystem::filesystem_error if the storage driver fails to get the asset. Only if driver is diskDriver.
          *
          */
         rapidjson::Document getAsset(const AssetType type, std::string_view assetName);
 
         /**
-         * @brief Get the decoder for the given decoder name.
-         * @param decoderName The name of the decoder.
-         * @return The decoder or an empty document if could not be found.
-         * @throw std::runtime_error if the decoder its invalid or the storage driver failed to get the decoder.
-         */
-        rapidjson::Document getDecoder(std::string_view decoderName);
-
-        /**
-         * @brief Get the JSON schema for the given assetType
+         * @brief Get the list of assets of a given type.
          *
-         * Only decoder, rule, filter and output are valid assetType.
-         * @param assetType The type of the asset ()
-         * @return The JSON schema or an empty document if could not be found.
-         * @throws std::runtime_error if the assetType is invalid or the storage driver failed to get the JSON schema.
+         * @param type The type of the asset. Only decoder, rules, filter and schema are supported.
+         * @return std::vector<std::string_view> The list of assets.
+         * @throws filesystem::filesystem_error if the storage driver fails to get the asset. Only if driver is diskDriver.
          */
-        rapidjson::Document getJSONSchema(AssetType assetType);
-
-        /**
-         * @brief Get the Decoder list
-         * @return std::vector<std::string_view> the list of decoders.
-         * @throw std::runtime_error if the storage driver failed to get the decoder list.
-         */
-        std::vector<std::string_view> getDecoderList();
+        std::vector<std::string_view> getAssetList(const AssetType type);
 };
 #endif // __CATALOG_H__
