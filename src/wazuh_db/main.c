e383936c702ee954a0bee0b07b4fdf9e920893ab
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2022-01-12 20:55:48 -0300

    Added error log while doing backup by request and by interval

--- src/wazuh_db/main.c
+++ src/wazuh_db/main.c
@@ -452,8 +452,9 @@ void * run_backup(__attribute__((unused)) void * args) {
                         current_time = time(NULL);
                         if((current_time - last_global_backup_time) >= global_interval) {
                             wdb_t* wdb = wdb_open_global();
-                            wdb_global_create_backup(wdb, output, NULL);
-                            mdebug1("Backup creation result: %s", output);
+                            if (OS_SUCCESS != wdb_global_create_backup(wdb, output, NULL)) {
+                                merror("Creating Global DB snapshot by interval failed: %s", output);
+                            }
                             last_global_backup_time = current_time;
                             wdb_leave(wdb);
                         }
