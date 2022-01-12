42d45814f71a97f62bba2e11e36c23a855936698
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-12 10:29:46 +0100

    Refactored Registry as a heterogeneous map, this is achived using static meta maps. - Deleted cpp and defined source inside hpp - Updated tests - Updated code style to Wazuh guidelines

--- src/engine/test/source/builder/registry_test.cpp
+++ src/engine/test/source/builder/registry_test.cpp
@@ -1,47 +1,26 @@
 #include <gtest/gtest.h>
-#include <rxcpp/rx.hpp>
-#include <typeinfo>
 #include <string>
-#include <map>
 
 #include "registry.hpp"
 
-using json = nlohmann::json;
-using namespace rxcpp;
 using namespace std;
+using namespace builder::internals;
 
-namespace
-{
-    observable<json> decoder_build(const observable<json>& obs, const vector<json>& decoders)
-    {
-        return obs;
-    }
-    builder::MultiJsonBuilder decoder_builder("decoder_engine", &decoder_build);
+string g_builderId = "test builder";
+int g_builder = 0;
+
+TEST(Registry, RegisterNewBuilder) {
+  ASSERT_NO_THROW(Registry::instance().registerBuilder<int>(g_builderId, g_builder));
 }
 
-string builder_id = "test_decoder";
-builder::Registry &reg = builder::Registry::instance();
-
-TEST (RegistryTests, RegisterNewBuilder) {
-
-    ASSERT_NO_THROW(reg.register_builder(builder_id,decoder_builder));
-
+TEST(Registry, RegisterExistingBuilder) {
+  ASSERT_THROW(Registry::instance().registerBuilder<int>(g_builderId, g_builder), invalid_argument);
 }
 
-TEST (RegistryTests, RegisterExistingBuilder) {
-
-    ASSERT_THROW(reg.register_builder(builder_id,decoder_builder),invalid_argument);
-
+TEST(Registry, getExistingBuilder) {
+  ASSERT_NO_THROW(Registry::instance().builder<int>(g_builderId));
 }
 
-TEST (RegistryTests, getExistingBuilder) {
-
-    ASSERT_NO_THROW(reg.get_builder(builder_id));
-
-}
-
-TEST (RegistryTests, getNonExistingBuilder) {
-
-    ASSERT_THROW(reg.get_builder("none"),out_of_range);
-    
+TEST(Registry, getNonExistingBuilder) {
+  ASSERT_THROW(Registry::instance().builder<int>("none"), out_of_range);
 }
