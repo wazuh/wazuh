d15c27fc42e0863466a0d429f392b4c64936fe8f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-13 12:10:12 +0100

    Refactored syntax - Moved to builder::internals::syntax - Updated style to Wazuh guidelines

--- src/engine/source/builder/syntax.hpp
+++ src/engine/source/builder/syntax.hpp
@@ -1,19 +1,20 @@
-#pragma once
+#ifndef _SYNTAX_H
+#define _SYNTAX_H
 
 #include <string>
 
-
 /**
-* @brief Defines syntax elements.
-*
-* This namespace contains all anchors and syntax elements that identify
-* different objects.
-*/
-namespace builder::syntax
-{
+ * @brief Defines syntax elements.
+ *
+ * This namespace contains all anchors and syntax elements that identify
+ * different objects.
+ */
+namespace builder::internals::syntax {
 
-    const std::string REFERENCE_ANCHOR("$");
-    const std::string HELPER_ANCHOR("+");
-    const std::string HELPER_ARG_ANCHOR("/");
+const std::string REFERENCE_ANCHOR("$");
+const std::string HELPER_ANCHOR("+");
+const std::string HELPER_ARG_ANCHOR("/");
 
-}
+} // namespace builder::internals::syntax
+
+#endif // _SYNTAX_H
