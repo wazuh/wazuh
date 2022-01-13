13ea98a5fcf2d650d911bc30021503a4571455cf
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-13 12:07:12 +0100

    Refactored utils - Moved to namespace builder::internals::utils - Updated test - Updated style to Wazuh guidelines - Updated CMakeLists

--- test/source/builder/utils_test.cpp
+++ test/source/builder/utils_test.cpp
@@ -1,24 +1,18 @@
-#include <gtest/gtest.h>
 #include <algorithm>
+#include <gtest/gtest.h>
 
-#include "builder.hpp"
 #include "utils.hpp"
 
-#define GTEST_COUT std::cerr << "[          ] [ INFO ] "
-
-
 using namespace std;
+using namespace builder::internals;
 
-TEST(BuilderUtils, JsonPath)
-{
-    string s = "this.is.some.string";
-    vector<string> expected{"this", "is", "some", "string"};
-    builder::utils::JsonPath jp(s);
-    auto i = 0;
-    for_each(jp.begin(), jp.end(), [&i, &expected](auto field)
-    {
-        ASSERT_EQ(field, expected[i]);
-        i++;
-    });
-
+TEST(BuilderUtils, JsonPath) {
+  string s = "this.is.some.string";
+  vector<string> expected{"this", "is", "some", "string"};
+  utils::JsonPath jP(s);
+  auto i = 0;
+  for_each(jP.begin(), jP.end(), [&i, &expected](auto field) {
+    ASSERT_EQ(field, expected[i]);
+    i++;
+  });
 }
