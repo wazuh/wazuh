1f9927092b0e539f826bd5cd201ff7707c5e06c2
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-13 09:30:36 +0100

    Refactored Builder class as a meta-class that implements a wrapper over an arbitrary function. - Deleted builder.cpp - Deleted builder BaseBuilder class - Updated tests - Updated code style to Wazuh guidelines - Updated CMakelists

--- src/engine/source/builder/builder.hpp
+++ src/engine/source/builder/builder.hpp
@@ -1,171 +1,38 @@
-#pragma once
+#ifndef _BUILDER_H
+#define _BUILDER_H
 
+#include <functional>
 #include <string>
-#include <exception>
-#include <rxcpp/rx.hpp>
-#include <nlohmann/json.hpp>
 
+namespace builder::internals {
 
-/**
- * @brief Defines all functionality related to builders.
- *
- * It defines all classes and functions needed to construct runtime objects that implement the operations
- * described by an enviroment, with it engines (Decoders, Rules, Filters...).
- *
- */
-namespace builder
-{
+template <class> class Builder;
+template <class Operation, class... BuildArgs>
+class Builder<Operation(BuildArgs...)> {
+private:
+  std::string m_name;
+  std::function<Operation(BuildArgs...)> m_build;
 
-    /**
-     * @brief Get the enviroment object.
-     *
-     * It queries the Catalog in order to access the objects needed to build all observable operations
-     * and returns a composed observable with them.
-     *
-     * @param enviroment_id Name of the enviroment to be built.
-     * @return observable<json>
-     */
-    rxcpp::observable<nlohmann::json> get_enviroment(const std::string& enviroment_id);
+public:
+  Builder(){};
+  Builder(const std::string &name,
+          std::function<Operation(BuildArgs...)> buildFunction)
+      : m_name{name}, m_build{buildFunction} {}
 
-    /**
-     * @brief Base class to allow polymorphism in the Registry.
-     *
-     * This class implements the polimorphism needed to include various builder types in the Registry
-     * and implements the registration on all Builder classes.
-     *
-     * This class can not be instantiated directly.
-     */
-    class Builder
-    {
-        protected:
-            /**
-             * @brief Construct a new Builder object.
-             *
-             * @param builder_id Name with which it will be registered.
-             */
-            explicit Builder(const std::string& builder_id);
+  Builder(const Builder<Operation(BuildArgs...)> &other)
+      : m_name{other.m_name}, m_build{other.m_build} {}
 
-        public:
-            /**
-            * @brief Deleted for safety.
-            */
-            Builder() = delete;
+  Builder<Operation(BuildArgs...)> &
+  operator=(const Builder<Operation(BuildArgs...)> &other) {
+    this->m_name = other.m_name;
+    this->m_build = other.m_build;
+    return *this;
+  }
 
-            /**
-            * @brief Deleted for safety.
-            */
-            Builder(const Builder&) = delete;
-
-            /**
-            * @brief Deleted for safety.
-            */
-            Builder& operator = (const Builder&) = default;
-    };
-
-    /**
-    * @brief Implements json input builders.
-    *
-    * This class implements the builders wich need a json object as input.
-    */
-    class JsonBuilder: public Builder
-    {
-        public:
-            /**
-             * @brief Construct a new JsonBuilder object.
-             *
-             * @param builder_id Name with which it will be registered.
-             * @param build Function that implements the build operation.
-             */
-            JsonBuilder(const std::string& builder_id, rxcpp::observable<nlohmann::json> (*build)(const rxcpp::observable<nlohmann::json>&, const nlohmann::json&));
-
-            /**
-             * @brief Implepents build operation.
-             *
-             * @param input_observable Observable on which the transformations will be applied.
-             * @param input_json Json object with transformations definitions.
-             * @return Composed observable with transformations.
-             */
-            rxcpp::observable<nlohmann::json> (*build)(const rxcpp::observable<nlohmann::json>& input_observable, const nlohmann::json& input_json);
-
-            /**
-             * @brief Deleted for safety.
-             */
-            JsonBuilder() = delete;
-
-            /**
-            * @brief Deleted for safety.
-            */
-            JsonBuilder(const JsonBuilder&) = delete;
-
-            /**
-             * @brief Deleted for safety.
-             */
-            JsonBuilder& operator = (const JsonBuilder&) = delete;
-    };
-
-    /**
-    * @brief Implements multiple jsons input builders.
-    *
-    * This class implements the builders wich need multiple json objects as input.
-    */
-    class MultiJsonBuilder: public Builder
-    {
-        public:
-            /**
-            * @brief Construct a new MultiJsonBuilder object.
-            *
-            * @param builder_id Name with which it will be registered.
-            * @param build Function that implements the build operation.
-            */
-            MultiJsonBuilder(const std::string&, rxcpp::observable<nlohmann::json> (*)(const rxcpp::observable<nlohmann::json>&, const std::vector<nlohmann::json>&));
-
-
-            /**
-             * @brief Implepents build operation.
-             *
-             * @param input_observable Observable on which the transformations will be applied.
-             * @param input_json_vector Json objects with transformations definitions.
-             * @return Composed observable with transformations.
-             */
-            rxcpp::observable<nlohmann::json> (*build)(const rxcpp::observable<nlohmann::json>& input_observable, const std::vector<nlohmann::json>& input_json_vector);
-
-            /**
-            * @brief Deleted for safety.
-            */
-            MultiJsonBuilder() = delete;
-
-            /**
-            * @brief Deleted for safety.
-            */
-            MultiJsonBuilder(const MultiJsonBuilder&) = delete;
-
-            /**
-            * @brief Deleted for safety.
-            */
-            MultiJsonBuilder& operator = (const MultiJsonBuilder&) = delete;
-    };
-
-    /**
-     * @brief Exception thrown on building errors.
-     *
-     * Thrown by specific instances of Builder when method build encounters errors and the object
-     * can't be built.
-     *
-     */
-    class BuildError: public std::exception
-    {
-        public:
-            /**
-             * @brief Construct a new BuildError object.
-             *
-             * @param builder_name name of the Builder instance.
-             * @param message descriptive message of the error.
-             */
-            BuildError(const std::string& builder_name, const std::string& message);
-            const char* what() const noexcept override;
-
-        private:
-            std::string m_builder_name;
-            std::string m_message;
-    };
+  Operation operator()(BuildArgs... args) const {
+    return this->m_build(args...);
+  }
 };
+} // namespace builder::internals
+
+#endif // _BUILDER_H
--- src/engine/test/source/builder/builder_test.cpp
+++ src/engine/test/source/builder/builder_test.cpp
@@ -1,176 +1,19 @@
 #include <gtest/gtest.h>
-#include <nlohmann/json.hpp>
-#include <rxcpp/rx.hpp>
-#include <algorithm>
-#include <vector>
-#include <map>
 #include <string>
 
 #include "builder.hpp"
-#include "registry.hpp"
 
-#define GTEST_COUT std::cerr << "[          ] [ INFO ] "
-
-
-using json = nlohmann::json;
 using namespace std;
-using namespace rxcpp;
+using namespace builder::internals;
 
-// Fake engine class
-class FakeEngine
-{
-    public:
-        FakeEngine(const string& engine_id, const vector<json>& resources): engine_id(engine_id), resources(resources) {}
-        vector<json> resources;
-        string engine_id;
-};
+int fn(string strNumber, int number) { return number + stoi(strNumber); }
+typedef Builder<int(string, int)> BuilderType;
 
-// Fake catalog class
-typedef vector<FakeEngine> FakeConf;
-class FakeCatalog
-{
-    public:
-        map<string, FakeConf> catalog;
-        void add_enviroment(const string& enviroment_id, const vector<FakeEngine>& engines)
-        {
-            this->catalog[enviroment_id] = engines;
-        }
-        FakeConf get_conf(const string& enviroment_id)
-        {
-            return this->catalog[enviroment_id];
-        }
-};
-
-// entry_point as observable
-json generate(std::string name, std::string source)
-{
-    auto t = std::time(nullptr);
-    auto tm = *std::gmtime(&t);
-
-    std::string cstr(30, '\0');
-    auto len = std::strftime(&cstr[0], cstr.size(), "%FT%TZ%z", &tm);
-    cstr.erase(len, std::string::npos);
-    return json
-    {
-        {
-            "event", {
-                {"original", "::1 - - [26/Dec/2016:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209\n"},
-            }
-        },
-        {
-            "wazuh", {
-                {
-                    "agent", {
-                        {"id", "001"},
-                        {"name", "agentSim"},
-                        {"version", "PoC"},
-                    }
-                },
-                {
-                    "event", {
-                        {"format", "text"},
-                        {"id", "9aa69e7b-e1b0-530e-a710-49108e86019b"},
-                        {"ingested", cstr },
-                        {"kind", "event"},
-                    }
-                },
-                {
-                    "host", {
-                        {"architecture", "x86_64"},
-                        {"hostname", "hive"},
-                        {"ip", "127.0.1.1"},
-                        {"mac", "B0:7D:64:11:B3:13"},
-                        {
-                            "os",
-                            {
-                                {"kernel", "5.14.14-arch1-1"},
-                                {"name", "Linux"},
-                                {"type", "posix"},
-                                {"version", "#1 SMP PREEMPT Wed, 20 Oct 2021 21:35:18 +0000"},
-                            }
-                        },
-                    }
-                },
-            }
-        },
-        {
-            "module", {
-                {"name", name},
-                {"source", source},
-            }
-        }
-    };
-}
-auto handler = [](subscriber<json> s)
-{
-    s.on_next(generate("logcollector", "apache-access"));
-    s.on_next(generate("logcollector", "apache-error"));
-    s.on_next(generate("logcollector", "apache-access"));
-    s.on_next(generate("logcollector", "apache-error"));
-    s.on_completed();
-};
-
-// Dummy builders
-namespace
-{
-    observable<json> decoder_build(const observable<json>& obs, const vector<json>& decoders)
-    {
-        GTEST_COUT << "Decoder engine builder called" << endl;
-        return obs;
-    }
-    builder::MultiJsonBuilder decoder_builder("decoder_engine", &decoder_build);
+TEST(Builder, Constructs) {
+  ASSERT_NO_THROW(BuilderType test_builder("test_builder", fn));
 }
 
-
-
-// Acceptance test
-// Builder returns proper enviroment object when a configuration is given to it
-TEST(BuilderTest, BuilderBuilds)
-{
-    // Assumption registry is build and populated
-    builder::Registry& registry = builder::Registry::instance();
-    GTEST_COUT << "Registry instance retreived" << endl;
-    // Set up FakeCatalog
-    vector<json> decoders;
-
-    for (int i = 0; i < 10; i++)
-    {
-        decoders.push_back(json(
-        {
-            "name",
-            string("decoder_").append(to_string(i))
-        }));
-    }
-
-    FakeCatalog catalog;
-    FakeEngine decoder_engine("decoder_engine", decoders);
-    vector<FakeEngine> engines;
-    engines.push_back(decoder_engine);
-    catalog.add_enviroment("enviroment_test", engines);
-
-    // An enviroment id is passed to the builder
-    string enviroment_id = "enviroment_test";
-
-    // We request the configuration to the catalog
-    FakeConf conf = catalog.get_conf(enviroment_id);
-    GTEST_COUT << "Configuration retreived" << endl;
-
-    // Build observable entry point, where subsequents observable operations are concatenated
-    observable<json> entry_point = observable<>::create<json>(handler);
-    GTEST_COUT << "Entry point built" << endl;
-
-
-    // and iterate over each engine, building the appropiate object.
-    // When building the object an observable is passed, operations are applied to
-    // the observable, returning the combined observable
-    observable<json> current_point = entry_point;
-    for_each(begin(conf), end(conf), [&registry, &current_point](FakeEngine e)
-    {
-        GTEST_COUT << "Start building " << e.engine_id << endl;
-        const builder::MultiJsonBuilder* eng_ptr = (const builder::MultiJsonBuilder*)(registry.get_builder(e.engine_id));
-        GTEST_COUT << "Decoder engine builder retreived" << endl;
-        current_point = eng_ptr->build(current_point, e.resources);
-    });
-    GTEST_COUT << "Enviroment built" << endl;
-    // current_point holds now all operations combined
+TEST(Builder, Builds) {
+  BuilderType test_builder("test_builder", fn);
+  ASSERT_EQ(test_builder("1", 1), 2);
 }
