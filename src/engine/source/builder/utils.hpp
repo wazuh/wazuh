d2bfad29f1de93886ec823366adaa6fa331087e6
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-13 12:07:12 +0100

    Refactored utils - Moved to namespace builder::internals::utils - Updated test - Updated style to Wazuh guidelines - Updated CMakeLists

--- src/engine/source/builder/utils.hpp
+++ src/engine/source/builder/utils.hpp
@@ -1,52 +1,52 @@
-#pragma once
+#ifndef _UTILS_H
+#define _UTILS_H
 
 #include <string>
 #include <vector>
-
 
 /**
  * @brief defines helper classes and functions for builders.
  *
  */
-namespace builder::utils
-{
+namespace builder::internals::utils {
 
-    /**
-     * @brief Represents a json path string.
-     *
-     * This class splits json path strings to its individual components.
-     *
-     */
-    class JsonPath
-    {
-        public:
-            /**
-             * @brief Construct a new Json Path object.
-             *
-             * @param json_path
-             */
-            explicit JsonPath(const std::string& json_path);
-            /**
-             * @brief Construct a new Json Path object.
-             *
-             * @param o
-             */
-            JsonPath(const JsonPath& o);
-            /**
-             * @brief Return iterator at the begining.
-             *
-             * @return vector<string>::const_iterator
-             */
-            std::vector<std::string>::const_iterator begin() const noexcept;
-            /**
-             * @brief Return iterator at the end.
-             *
-             * @return vector<string>::const_iterator
-             */
-            std::vector<std::string>::const_iterator end() const noexcept;
+/**
+ * @brief Represents a json path string.
+ *
+ * This class splits json path strings to its individual components.
+ *
+ */
+class JsonPath {
+private:
+  std::vector<std::string> m_jsonPath;
 
-        private:
-            std::vector<std::string> json_path;
-    };
+public:
+  /**
+   * @brief Construct a new Json Path object.
+   *
+   * @param jsonPath
+   */
+  explicit JsonPath(const std::string &jsonPath);
+  /**
+   * @brief Construct a new Json Path object.
+   *
+   * @param o
+   */
+  JsonPath(const JsonPath &o);
+  /**
+   * @brief Return iterator at the begining.
+   *
+   * @return vector<string>::const_iterator
+   */
+  std::vector<std::string>::const_iterator begin() const noexcept;
+  /**
+   * @brief Return iterator at the end.
+   *
+   * @return vector<string>::const_iterator
+   */
+  std::vector<std::string>::const_iterator end() const noexcept;
+};
 
-}
+} // namespace builder::internals::utils
+
+#endif // _UTILS_H
