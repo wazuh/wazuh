b82da1b270b1541c632377b558d3435a6075e891
miguemedina11 <miguemedina2000@gmail.com>
2022-01-17 13:48:22 +0000

    [WIP] map_reference and map_value refactor. Removed unnecesary define on json.hpp

--- src/engine/source/builder/operations/map_value.cpp
+++ src/engine/source/builder/operations/map_value.cpp
@@ -1,56 +1,35 @@
 #include <string>
-#include <nlohmann/json.hpp>
 #include <rxcpp/rx.hpp>
 
 #include "builder.hpp"
 #include "utils.hpp"
+#include "json.hpp"
 
+#include "rapidjson/document.h"
+#include "rapidjson/pointer.h"
 
-using json = nlohmann::json;
 using namespace std;
 using namespace rxcpp;
 
 namespace
 {
     string name("map.value");
-    observable<json> build(const observable<json>& input_observable, const json& input_json)
+    observable<json::Document> build(const observable<json::Document> &input_observable,
+                                     const rapidjson::Value &input_json)
     {
-        // Check that input is as expected and throw exception otherwise
-        if (!input_json.is_object())
+        auto iter = input_json.MemberBegin();
+        auto output_observable = input_observable.map([iter](json::Document e)
         {
-            throw builder::BuildError(name, "build expects json with an object");
-        }
 
-        if (input_json.size() != 1)
-        {
-            throw builder::BuildError(name, "build expects json with only one key");
-        }
-
-        auto it = begin(input_json);
-        auto field =  builder::utils::JsonPath(it.key());
-        // Todo validate json path
-        auto value = it.value();
-        auto output_observable = input_observable.map([field, value](json e)
-        {
-            json* actual = &e;
-
-            for (auto field_it=field.begin() ; field_it<field.end()-1 ; ++field_it ){
-
-                string field_name = *field_it;
-
-                if (actual->contains(field_name))
-                {
-                    actual = &(*actual)[field_name];
-                }
-
-            }
-            
-            (*actual)[*(field.end()-1)] = value;
+            e.set(iter->name.GetString(),iter->value);
 
             return e;
+
         });
         return output_observable;
     }
 
-    builder::JsonBuilder map_value_builder(name, build);
+    builder::internals::Builder<observable<json::Document>(observable<json::Document>,
+                                       rapidjson::Value)>
+    map_value(name, build);
 }
