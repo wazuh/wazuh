37fe9a7652c2e5ce4a56dc64cb73941b88cb64f1
Gabriel Diaz <gabriel.diaz@wazuh.com>
2022-01-17 06:15:36 +0000

    update connectable class, add set method

--- src/engine/source/builder/connectable.hpp
+++ src/engine/source/builder/connectable.hpp
@@ -1,22 +1,88 @@
 #ifndef _CONNECTABLE_H
 #define _CONNECTABLE_H
 
+#include "rxcpp/rx.hpp"
 #include <string>
 
-template <class Input, class Output> class Connectable {
-private:
-  std::string m_name;
-  Input m_input;
-  Output m_output;
+namespace builder
+{
 
-public:
-  Connectable(std::string &name, Input input, Output output)
-      : m_name(name), m_input(input), m_output(output) {}
-  void connect(const Connectable<Input, Output> &other) {
-    this->m_output.subscribe(other.m_input);
-  }
-  Input input() { return this->m_input; }
-  Output output() { return this->m_output; }
-  std::string name() const { return this->m_name; }
-};
+  /**
+   * @brief a connectable has the property of being able to be connected
+   * with another connectable. A connectable does not know its childs, and only
+   * knows about the name of the parents it shoudl be connected to.
+   *
+   * @tparam T the type of the event which will flow between connected connectables
+   */
+  template <class T>
+  class Connectable
+  {
+  private:
+    std::string m_name;
+    std::vector<std::string> m_parents;
+
+    rxcpp::subjects::subject<T> m_subj;
+    rxcpp::observable<T> m_obs;
+
+  public:
+    /**
+     * @brief Construct a new Connectable object
+     *
+     * @param name unique id of the conectable
+     */
+    Connectable(std::string &name)
+        : m_name(name),
+          m_obs(m_subj.get_observable()) {}
+
+    /**
+     * @brief connects the ouput of this connectable to the input
+     * og the other connectable. This does not check against
+     * loops.
+     *
+     * @param other connectable to send our output
+     */
+    void connect(const Connectable<T> &other)
+    {
+      this->output().subscribe(other.input());
+    }
+
+    /**
+     * @brief set the observable of this connectable. Used to
+     * apply observers to this connectable oobservable.
+     *
+     * @param obs
+     */
+    void set(rxcpp::observer<T> obs) { this->m_obs = obs; }
+
+    /**
+     * @brief return our subscriber, which will receive the events we want
+     *
+     * @return rxcpp::subscriber<T> our subscriber
+     */
+
+    auto input() { return this->m_subj.get_subscriber(); }
+    /**
+     * @brief returns our observable, to whom other will connect to receive
+     * the events we have processed.
+     *
+     * @return rxcpp::observable<T> our observable
+     */
+    auto output() { return this->m_obs; }
+
+    /**
+     * @brief return the list of our parents
+     *
+     * @return std::vector<std::string>
+     */
+    auto parents() { return this->m_parents; }
+
+    /**
+     * @brief returns our nane
+     *
+     * @return std::string
+     */
+    auto name() const { return this->m_name; }
+  };
+
+} // namespace builder
 #endif // _CONNECTABLE_H
