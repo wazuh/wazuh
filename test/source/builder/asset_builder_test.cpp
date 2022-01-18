adcca48675acdd7597cee951e32f14a965d1d418
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-18 07:55:57 +0100

    Renamed ComponentBuilder to AssetBuilder

--- test/source/builder/asset_builder_test.cpp
+++ test/source/builder/asset_builder_test.cpp
@@ -1,19 +1,19 @@
 #include <gtest/gtest.h>
 #include <string>
 
-#include "component_builder.hpp"
+#include "asset_builder.hpp"
 
 using namespace std;
 using namespace builder::internals;
 
 int fn(string strNumber, int number) { return number + stoi(strNumber); }
-typedef ComponentBuilder<int(string, int)> ComponentBuilderType;
+using AssetBuilderType = AssetBuilder<int(string, int)>;
 
 TEST(ComponentBuilder, Constructs) {
-  ASSERT_NO_THROW(ComponentBuilderType test_builder("test_builder", fn));
+  ASSERT_NO_THROW(AssetBuilderType test_builder("test_builder", fn));
 }
 
 TEST(ComponentBuilder, Builds) {
-  ComponentBuilderType test_builder("test_builder", fn);
+  AssetBuilderType test_builder("test_builder", fn);
   ASSERT_EQ(test_builder("1", 1), 2);
 }
