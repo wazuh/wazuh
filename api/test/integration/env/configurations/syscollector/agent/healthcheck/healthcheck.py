7e61ce4e62e4320a2443e85a4c51662cd99ff5b9
Manuel <manuel.carmona@wazuh.com>
2022-01-18 15:38:17 +0100

    Update syscollector and experimental AIT healthchecks

--- api/test/integration/env/configurations/syscollector/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/syscollector/agent/healthcheck/healthcheck.py
@@ -8,10 +8,10 @@ from healthcheck_utils import get_agent_health_base, check
 if __name__ == "__main__":
     # Check that the syscollector differences are sent. This is part of the DBs synchronization process, and it happens
     # after the scan (modules with debug = 2 is needed).
-    components = ('syscollector_processes', 'syscollector_osinfo', 'syscollector_ports', 'syscollector_hwinfo',
-                  'syscollector_packages', 'syscollector_network_iface', 'syscollector_network_protocol',
-                  'syscollector_network_address')
-    strings_to_grep = ('DEBUG: Sync sent: {"component":' + f'"{component}"' for component in components)
+    sync_sent_logs = ('DEBUG: Sync sent: {"component":'f'"{component}"' for component in
+                      ('syscollector_processes', 'syscollector_osinfo', 'syscollector_ports', 'syscollector_hwinfo',
+                       'syscollector_packages', 'syscollector_network_iface', 'syscollector_network_protocol',
+                       'syscollector_network_address'))
 
-    checks = (check(os.system(f"grep -q '{s}' /var/ossec/logs/ossec.log")) for s in strings_to_grep)
-    exit(any(checks) or get_agent_health_base())
+    exit(any(check(os.system(f"grep -q '{log}' /var/ossec/logs/ossec.log")) for log in sync_sent_logs)
+         or get_agent_health_base())
