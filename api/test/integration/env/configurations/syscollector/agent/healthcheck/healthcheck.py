a0235eacd88abf50952ec53f7f1a15aa3b76c8e7
Manuel <manuel.carmona@wazuh.com>
2022-01-18 13:50:16 +0100

    Run modulesd in debug mode and update healthecks for the AIT env

--- api/test/integration/env/configurations/syscollector/agent/healthcheck/healthcheck.py
+++ api/test/integration/env/configurations/syscollector/agent/healthcheck/healthcheck.py
@@ -6,5 +6,12 @@ sys.path.append('/tools')
 from healthcheck_utils import get_agent_health_base, check
 
 if __name__ == "__main__":
-    exit(check(os.system("grep -q 'wazuh-modulesd:syscollector: INFO: Evaluation finished.' /var/ossec/logs/ossec.log"))
-         or get_agent_health_base())
+    # Check that the syscollector differences are sent. This is part of the DBs synchronization process, and it happens
+    # after the scan (modules with debug = 2 is needed).
+    components = ('syscollector_processes', 'syscollector_osinfo', 'syscollector_ports', 'syscollector_hwinfo',
+                  'syscollector_packages', 'syscollector_network_iface', 'syscollector_network_protocol',
+                  'syscollector_network_address')
+    strings_to_grep = ('DEBUG: Sync sent: {"component":' + f'"{component}"' for component in components)
+
+    checks = (check(os.system(f"grep -q '{s}' /var/ossec/logs/ossec.log")) for s in strings_to_grep)
+    exit(any(checks) or get_agent_health_base())
