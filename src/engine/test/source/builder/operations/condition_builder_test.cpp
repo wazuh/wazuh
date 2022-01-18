5b27779dd949761033d35c31cb77286a99f9cc97
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-18 15:41:59 +0100

    Refactored condition builders

--- src/engine/test/source/builder/operations/condition_builder_test.cpp
+++ src/engine/test/source/builder/operations/condition_builder_test.cpp
@@ -1,18 +1,83 @@
 #include <gtest/gtest.h>
+#include <rxcpp/rx.hpp>
 
-#include "builder.hpp"
+#include "asset_builder.hpp"
 #include "registry.hpp"
+#include "test_utils.hpp"
 
-#define GTEST_COUT std::cout << "[          ] [ INFO ] "
+using condition_builder_t =
+    AssetBuilder<observable<event>(observable<event>, value_t)>;
 
+TEST(ConditionBuilderTest, Initializes) {
+  ASSERT_NO_THROW(
+      Registry::instance().builder<condition_builder_t>("condition"));
+}
 
-using namespace std;
+TEST(ConditionBuilderTest, BuildsConditionValue) {
+  // Fake entry poing
+  auto entry_point = rxcpp::observable<>::create<event>(handler);
 
-TEST(ConditionValueTest, Initializes)
-{
-    // Get registry instance as all builders are only accesible by it
-    builder::Registry& registry = builder::Registry::instance();
+  // Fake input json
+  auto fake_jstring = R"(
+      {
+          "check": {
+              "field": 1
+          }
+      }
+  )";
+  json::Document fake_j{fake_jstring};
 
-    // Retreive builder
-    ASSERT_NO_THROW(auto builder = registry.get_builder("condition"));
+  // Retreive builder
+  auto _builder =
+      Registry::instance().builder<condition_builder_t>("condition");
+
+  // Build
+  ASSERT_NO_THROW(auto _observable =
+                      _builder(entry_point, fake_j.get(".check")));
+}
+
+TEST(ConditionBuilderTest, BuildsConditionExists) {
+  // Fake entry poing
+  auto entry_point = rxcpp::observable<>::create<event>(handler);
+
+  // Fake input json
+  auto fake_jstring = R"(
+      {
+          "check": {
+              "field": "+exists"
+          }
+      }
+  )";
+  json::Document fake_j{fake_jstring};
+
+  // Retreive builder
+  auto _builder =
+      Registry::instance().builder<condition_builder_t>("condition");
+
+  // Build
+  ASSERT_NO_THROW(auto _observable =
+                      _builder(entry_point, fake_j.get(".check")));
+}
+
+TEST(ConditionBuilderTest, BuildsConditionNotExists) {
+  // Fake entry poing
+  auto entry_point = rxcpp::observable<>::create<event>(handler);
+
+  // Fake input json
+  auto fake_jstring = R"(
+      {
+          "check": {
+              "field": "+not_exists"
+          }
+      }
+  )";
+  json::Document fake_j{fake_jstring};
+
+  // Retreive builder
+  auto _builder =
+      Registry::instance().builder<condition_builder_t>("condition");
+
+  // Build
+  ASSERT_NO_THROW(auto _observable =
+                      _builder(entry_point, fake_j.get(".check")));
 }
