3ca37cca749c476fb30a91d5db086216fda26a0d
Mariano Koremblum <nmkoremblum@gmail.com>
2022-01-19 11:28:02 -0300

    Code style fixed and tests added

--- test/source/server/engineserver_test.cpp
+++ test/source/server/engineserver_test.cpp
@@ -5,17 +5,14 @@
  * and/or modify it under the terms of the GNU General Public
  * License (version 2) as published by the FSF - Free Software
  * Foundation.
-*/
-
+ */
 
 #include "engineserver_test.hpp"
-
 
 using namespace engineserver;
 using namespace protocolhandler;
 
-
-TEST(EngineServer, parseEvent)
+TEST(EngineServer, parse_event)
 {
     MessageQueue queue = SYSLOG;
     std::string location = "/var/log/syslog";
@@ -28,39 +25,159 @@ TEST(EngineServer, parseEvent)
     ASSERT_EQ(object["message"], message);
 }
 
-// TEST(EngineServer, blocking_test_nc)
-// {
-//     EngineServer server;
+TEST(EngineServer, listen_TCP)
+{
+    const int tcpPort{5050};
+    EngineServer server;
 
-//     server.listenSocket("/tmp/test.sock");
+    server.listenTCP(tcpPort);
+    server.run();
 
-//     server.listenTCP(5050);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-//     server.listenUDP(5051);
+    server.close();
+}
 
-//     auto sockObs = server.getEndpointObservable(EndpointType::SOCKET, "/tmp/test.sock");
-//     if(sockObs)
-//     {
-//         sockObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
-//                                 [&server](nlohmann::json event){ std::cout << "SOCKET: " << event.at("message") << "\n"; },
-//                                 [](){printf("OnEnd?");}));
-//     }
+TEST(EngineServer, subscribe_TCP)
+{
+    const int tcpPort{5050};
+    EngineServer server;
 
-//     auto tcpObs = server.getEndpointObservable(EndpointType::TCP, 5050);
-//     if(tcpObs)
-//     {
-//         tcpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
-//                                 [&server](nlohmann::json event){ std::cout << "TCP (5050): " << event.at("message") << "\n"; },
-//                                 [](){printf("OnEnd?");}));
-//     }
+    server.listenTCP(tcpPort);
 
-//     auto udpObs = server.getEndpointObservable(EndpointType::UDP, 5051);
-//     if(udpObs)
-//     {
-//         udpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
-//                                 [&server](nlohmann::json event){ std::cout << "UDP (5051): " << event.at("message") << "\n"; },
-//                                 [](){printf("OnEnd?");}));
-//     }
+    try
+    {
+        auto tcpObs = server.getEndpointObservable(EndpointType::TCP, tcpPort);
 
-//     server.run();
-// }
+        ASSERT_TRUE(tcpObs);
+
+        tcpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+            [&server](nlohmann::json event)
+            { std::cout << "TCP (" << tcpPort << "): " << event.at("message") << "\n"; },
+            []() { printf("OnEnd?"); }));
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+TEST(EngineServer, listen_UDP)
+{
+    const int udpPort{5051};
+    EngineServer server;
+
+    server.listenUDP(udpPort);
+    server.run();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    server.close();
+}
+
+TEST(EngineServer, subscribe_UDP)
+{
+    const int udpPort{5050};
+    EngineServer server;
+
+    server.listenUDP(udpPort);
+
+    try
+    {
+        auto udpObs = server.getEndpointObservable(EndpointType::UDP, udpPort);
+
+        ASSERT_TRUE(udpObs);
+
+        udpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+            [&server](nlohmann::json event)
+            { std::cout << "UDP (" << udpPort << "): " << event.at("message") << "\n"; },
+            []() { printf("OnEnd?"); }));
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+TEST(EngineServer, listen_socket)
+{
+    const std::string sockPath{"/tmp/test.sock"};
+    EngineServer server;
+
+    server.listenSocket(sockPath);
+    server.run();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    server.close();
+}
+
+TEST(EngineServer, subscribe_socket)
+{
+    const std::string sockPath{"/tmp/test.sock"};
+    EngineServer server;
+
+    server.listenSocket(sockPath);
+
+    try
+    {
+        auto sockObs = server.getEndpointObservable(EndpointType::SOCKET, sockPath);
+
+        ASSERT_TRUE(sockObs);
+
+        sockObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+            [&server, &sockPath](nlohmann::json event)
+            { std::cout << "Socket (" << sockPath << "): " << event.at("message") << "\n"; },
+            []() { printf("OnEnd?"); }));
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+TEST(EngineServer, listen_and_subscribe_multiple_endpoints)
+{
+    const std::string sockPath{"/tmp/test.sock"};
+    const int tcpPort{5050};
+    const int udpPort{5051};
+
+    EngineServer server;
+
+    server.listenSocket(sockPath);
+
+    server.listenTCP(tcpPort);
+
+    server.listenUDP(udpPort);
+
+    auto sockObs = server.getEndpointObservable(EndpointType::SOCKET, sockPath);
+
+    ASSERT_TRUE(sockObs);
+
+    sockObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+        [&server, &sockPath](nlohmann::json event)
+        { std::cout << "SOCKET (" << sockPath << "): " << event.at("message") << "\n"; },
+        []() { printf("OnEnd?"); }));
+
+    auto tcpObs = server.getEndpointObservable(EndpointType::TCP, tcpPort);
+
+    ASSERT_TRUE(tcpObs);
+
+    tcpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+        [&server](nlohmann::json event) { std::cout << "TCP (" << tcpPort << "): " << event.at("message") << "\n"; },
+        []() { printf("OnEnd?"); }));
+
+    auto udpObs = server.getEndpointObservable(EndpointType::UDP, udpPort);
+
+    ASSERT_TRUE(udpObs);
+
+    udpObs.value().subscribe(rxcpp::make_subscriber<nlohmann::json>(
+        [&server](nlohmann::json event) { std::cout << "UDP (" << udpPort << "): " << event.at("message") << "\n"; },
+        []() { printf("OnEnd?"); }));
+
+    server.run();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    server.close();
+}
