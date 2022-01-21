83af7314e3f2e7219f8ecccb56de403af7b3a2e0
Gabriel Diaz <gabriel.diaz@wazuh.com>
2022-01-21 05:13:14 +0000

    Fix fakeConnectable and graph tests

--- src/engine/test/source/graph/graph_test.hpp
+++ src/engine/test/source/graph/graph_test.hpp
@@ -1,14 +1,12 @@
-#include <type_traits>
 #include <chrono>
 #include <thread>
+#include <type_traits>
 
 #include "rxcpp/rx.hpp"
 
 #define GTEST_COUT std::cerr << "[          ] [ INFO ] "
 
-
-template <typename E>
-class fakeConnectable
+template <typename E> class fakeConnectable
 {
 private:
     rxcpp::subjects::subject<E> m_subj;
@@ -18,14 +16,28 @@ public:
     std::string m_name;
 
     fakeConnectable(std::string name) : m_name(name), m_obs(m_subj.get_observable()){};
-    fakeConnectable(std::string name, rxcpp::subjects::subject<E> subj, rxcpp::observable<E> obs) : m_name(name), m_subj(subj), m_obs(obs){};
+    fakeConnectable(std::string name, rxcpp::subjects::subject<E> subj, rxcpp::observable<E> obs)
+        : m_name(name), m_subj(subj), m_obs(obs){};
 
-    void connect( std::shared_ptr<fakeConnectable<E>> other) {
+    void connect(std::shared_ptr<fakeConnectable<E>> other)
+    {
         this->m_obs.subscribe(other->input());
     }
 
-    auto set(rxcpp::observable<E> obs) { this->m_obs = obs; }
-    auto output() const { return this->m_obs; }
-    auto input() const { return this->m_subj.get_subscriber(); }
+    auto set(rxcpp::observable<E> obs)
+    {
+        this->m_obs = obs;
+    }
+    auto name()
+    {
+        return this->m_name;
+    }
+    auto output() const
+    {
+        return this->m_obs;
+    }
+    auto input() const
+    {
+        return this->m_subj.get_subscriber();
+    }
 };
-
