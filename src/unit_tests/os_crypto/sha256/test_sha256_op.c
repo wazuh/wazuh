b68cf12d940d0bd48ea96a886b4ebaa35e4fa263
Santiago Belluzzo <santiago.belluzzo@wazuh.com>
2022-01-21 16:42:11 -0300

    Unit tests - Fixing crypto unit tests to use mock functions to deal with files

--- src/unit_tests/os_crypto/sha256/test_sha256_op.c
+++ src/unit_tests/os_crypto/sha256/test_sha256_op.c
@@ -16,6 +16,17 @@
 #include "../../wrappers/common.h"
 #include "../headers/shared.h"
 
+/* setups/teardowns */
+static int setup_group(void **state) {
+    test_mode = 1;
+    return 0;
+}
+
+static int teardown_group(void **state) {
+    test_mode = 0;
+    return 0;
+}
+
 // Tests
 
 void test_sha256_string() {
@@ -41,33 +52,42 @@ void test_sha256_string_sized() {
 
 void test_sha256_file() {
     const char *string = "teststring";
-    const char *string_sha256 = "3c8727e019a42b444667a587b6001251becadabbb36bfed8087a92c18882d111";
+    const char *string_sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    char path[] = "path/to/file";
 
-    /* create tmp file */
-    char file_name[256];
-    strncpy(file_name, "/tmp/tmp_file-XXXXXX", 256);
-    int fd = mkstemp(file_name);
+    expect_value(__wrap_fopen, path, path);
+    expect_string(__wrap_fopen, mode, "r");
+    will_return(__wrap_fopen, 1);
 
-    write(fd, string, strlen(string));
-    close(fd);
+    will_return(__wrap_fread, string);
+    will_return(__wrap_fread, 0);
+
+    expect_value(__wrap_fclose, _File, 1);
+    will_return(__wrap_fclose, 1);
 
     os_sha256 buffer;
-    assert_int_equal(OS_SHA256_File(file_name, buffer, OS_TEXT), 0);
+    assert_int_equal(OS_SHA256_File(path, buffer, OS_TEXT), 0);
 
     assert_string_equal(buffer, string_sha256);
 }
 
 void test_sha256_file_fail() {
+    char path[] = "path/to/non-existing/file";
+
+    expect_value(__wrap_fopen, path, path);
+    expect_string(__wrap_fopen, mode, "r");
+    will_return(__wrap_fopen, 0);
+
     os_sha256 buffer;
-    assert_int_equal(OS_SHA256_File("not_existing_file", buffer, OS_TEXT), -1);
+    assert_int_equal(OS_SHA256_File(path, buffer, OS_TEXT), -1);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_sha256_string),
         cmocka_unit_test(test_sha256_string_sized),
-        cmocka_unit_test(test_sha256_file),
-        cmocka_unit_test(test_sha256_file_fail),
+        cmocka_unit_test_setup_teardown(test_sha256_file, setup_group, teardown_group),
+        cmocka_unit_test_setup_teardown(test_sha256_file_fail, setup_group, teardown_group),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
