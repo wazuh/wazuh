4a9dca6b733330bc0a2ad5ec4de96e0498214915
Gabriel Diaz <gabriel.diaz@wazuh.com>
2022-01-23 14:52:11 +0000

    Add filter support.

--- src/engine/test/source/builder/builder_test.cpp
+++ src/engine/test/source/builder/builder_test.cpp
@@ -1,13 +1,176 @@
-#include <gtest/gtest.h>
-#include <string>
-
-#include "builder.hpp"
 #include "builder_test.hpp"
+#include "builder.hpp"
 #include "connectable.hpp"
 #include "rxcpp/rx.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#define GTEST_COUT std::cerr << "[          ] [ INFO ] "
 
-TEST(Builder, Build)
+TEST(Builder, EnvironmentSingleDecoder)
 {
     auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
-    auto root = builder.build("environment_0");
+    auto root = builder.build("environment_1");
+}
+
+TEST(Builder, EnvironmentSingleDecoderSingleRule)
+{
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_2");
+}
+
+TEST(Builder, EnvironmentSingleDecoderSingleRuleSingleFilter)
+{
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_3");
+}
+
+TEST(Builder, EnvironmentOneofEachAsset)
+{
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_4");
+}
+
+TEST(Builder, EnvSingleDecoderProcess)
+{
+    using Event_t = json::Document;
+
+    using Subs_t = rxcpp::subscriber<Event_t>;
+    using Obs_t = rxcpp::observable<Event_t>;
+
+    using Con_t = builder::internals::Connectable;
+    using pCon_t = std::shared_ptr<Con_t>;
+
+    using Node_t = graph::Node<Con_t>;
+    using pNode_t = std::shared_ptr<Node_t>;
+
+    int expected{1};
+
+    // Prepare fake server/router
+    auto source = std::make_shared<Con_t>("source");
+    source->set(rxcpp::observable<>::create<Event_t>(
+        [expected](const Subs_t s)
+        {
+            for (int i = 0; i < expected; i++)
+            {
+                s.on_next(Event_t(R"({"field": "value" })"));
+            }
+            s.on_completed();
+        }));
+    auto pSourceNode = std::make_shared<Node_t>(source);
+
+    // Prepare the fake output
+    auto destination = std::make_shared<Con_t>("destination");
+    auto pDestNode = std::make_shared<Node_t>(destination);
+
+    int got{0};
+
+    // Build graph
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_1");
+    graph::visitLeaves<Con_t>(root, [&](auto leaf) { leaf->connect(pDestNode); });
+
+    destination->output().subscribe(
+        [&](const auto & e)
+        {
+            got++;
+            GTEST_COUT << e.str() << std::endl;
+        },
+        [](const std::exception_ptr & e)
+        {
+            try
+            {
+                std::rethrow_exception(e);
+            }
+            catch (const std::exception & ex)
+            {
+                GTEST_COUT << "OnError: " << ex.what() << std::endl;
+            }
+        },
+        []() { GTEST_COUT << "completed" << std::endl; });
+
+    pSourceNode->connect(root);
+
+    ASSERT_EQ(expected * 2, got);
+}
+
+std::string EachOfOneDiagraph = R"(digraph G {
+decoder_input -> decoder_input;
+decoder_input -> decoder_0;
+decoder_0 -> filter_0;
+filter_0 -> decoder_output;
+decoder_output -> output_input;
+output_input -> output_0;
+output_0 -> output_output;
+output_output -> destination;
+decoder_output -> rule_input;
+rule_input -> rule_0;
+rule_0 -> rule_output;
+rule_output -> output_input;
+}
+)";
+
+TEST(Builder, EnvironmentOneofEachAssetProcess)
+{
+    using Event_t = json::Document;
+
+    using Subs_t = rxcpp::subscriber<Event_t>;
+    using Obs_t = rxcpp::observable<Event_t>;
+
+    using Con_t = builder::internals::Connectable;
+    using pCon_t = std::shared_ptr<Con_t>;
+
+    using Node_t = graph::Node<Con_t>;
+    using pNode_t = std::shared_ptr<Node_t>;
+
+    int expected{1};
+
+    // Prepare fake server/router
+    auto source = std::make_shared<Con_t>("source");
+    source->set(rxcpp::observable<>::create<Event_t>(
+        [expected](const Subs_t s)
+        {
+            for (int i = 0; i < expected; i++)
+            {
+                s.on_next(Event_t(R"({"field": "value" })"));
+            }
+            s.on_completed();
+        }));
+    auto pSourceNode = std::make_shared<Node_t>(source);
+
+    // Prepare the fake output
+    auto destination = std::make_shared<Con_t>("destination");
+    auto pDestNode = std::make_shared<Node_t>(destination);
+
+    int got{0};
+
+    // Build graph
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_4");
+    graph::visitLeaves<Con_t>(root, [&](auto leaf) { leaf->connect(pDestNode); });
+
+    destination->output().subscribe(
+        [&](const auto & e)
+        {
+            got++;
+            GTEST_COUT << e.str() << std::endl;
+        },
+        [](const std::exception_ptr & e)
+        {
+            try
+            {
+                std::rethrow_exception(e);
+            }
+            catch (const std::exception & ex)
+            {
+                GTEST_COUT << "OnError: " << ex.what() << std::endl;
+            }
+        },
+        []() { GTEST_COUT << "completed" << std::endl; });
+
+    pSourceNode->connect(root);
+    auto diagraph = graph::print<Con_t>(root);
+    ASSERT_EQ(expected * 2, got);
+    ASSERT_EQ(diagraph.str(), EachOfOneDiagraph);
 }
