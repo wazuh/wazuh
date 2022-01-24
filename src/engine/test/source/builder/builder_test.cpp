805dd431bc1e2853dbadf2397bcf7024891d89b7
Gabriel Diaz <gabriel.diaz@wazuh.com>
2022-01-24 20:22:01 +0000

    Add new environment tests

--- src/engine/test/source/builder/builder_test.cpp
+++ src/engine/test/source/builder/builder_test.cpp
@@ -174,3 +174,130 @@ TEST(Builder, EnvironmentOneofEachAssetProcess)
     ASSERT_EQ(expected * 2, got);
     ASSERT_EQ(diagraph.str(), EachOfOneDiagraph);
 }
+
+TEST(Builder, Environment5)
+{
+    using Event_t = json::Document;
+
+    using Subs_t = rxcpp::subscriber<Event_t>;
+    using Obs_t = rxcpp::observable<Event_t>;
+
+    using Con_t = builder::internals::Connectable;
+    using pCon_t = std::shared_ptr<Con_t>;
+
+    using Node_t = graph::Node<Con_t>;
+    using pNode_t = std::shared_ptr<Node_t>;
+
+    int expected{1};
+
+    // Prepare fake server/router
+    auto source = std::make_shared<Con_t>("source");
+    source->set(rxcpp::observable<>::create<Event_t>(
+        [expected](const Subs_t s)
+        {
+            for (int i = 0; i < expected; i++)
+            {
+                s.on_next(Event_t(R"({"field": "value" })"));
+            }
+            s.on_completed();
+        }));
+    auto pSourceNode = std::make_shared<Node_t>(source);
+
+    // Prepare the fake output
+    auto destination = std::make_shared<Con_t>("destination");
+    auto pDestNode = std::make_shared<Node_t>(destination);
+
+    int got{0};
+
+    // Build graph
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_5");
+    graph::visitLeaves<Con_t>(root, [&](auto leaf) { leaf->connect(pDestNode); });
+
+    destination->output().subscribe(
+        [&](const auto & e)
+        {
+            got++;
+            GTEST_COUT << e.str() << std::endl;
+        },
+        [](const std::exception_ptr & e)
+        {
+            try
+            {
+                std::rethrow_exception(e);
+            }
+            catch (const std::exception & ex)
+            {
+                GTEST_COUT << "OnError: " << ex.what() << std::endl;
+            }
+        },
+        []() { GTEST_COUT << "completed" << std::endl; });
+
+    pSourceNode->connect(root);
+    auto diagraph = graph::print<Con_t>(root);
+    ASSERT_EQ(expected * 2, got);
+}
+
+TEST(Builder, Environment6)
+{
+    using Event_t = json::Document;
+
+    using Subs_t = rxcpp::subscriber<Event_t>;
+    using Obs_t = rxcpp::observable<Event_t>;
+
+    using Con_t = builder::internals::Connectable;
+    using pCon_t = std::shared_ptr<Con_t>;
+
+    using Node_t = graph::Node<Con_t>;
+    using pNode_t = std::shared_ptr<Node_t>;
+
+    int expected{1};
+
+    // Prepare fake server/router
+    auto source = std::make_shared<Con_t>("source");
+    source->set(rxcpp::observable<>::create<Event_t>(
+        [expected](const Subs_t s)
+        {
+            for (int i = 0; i < expected; i++)
+            {
+                s.on_next(Event_t(R"({"field": "value" })"));
+            }
+            s.on_completed();
+        }));
+    auto pSourceNode = std::make_shared<Node_t>(source);
+
+    // Prepare the fake output
+    auto destination = std::make_shared<Con_t>("destination");
+    auto pDestNode = std::make_shared<Node_t>(destination);
+
+    int got{0};
+
+    // Build graph
+    auto builder = builder::Builder<FakeCatalog>(FakeCatalog());
+    auto root = builder.build("environment_6");
+    graph::visitLeaves<Con_t>(root, [&](auto leaf) { leaf->connect(pDestNode); });
+
+    destination->output().subscribe(
+        [&](const auto & e)
+        {
+            got++;
+            GTEST_COUT << e.str() << std::endl;
+        },
+        [](const std::exception_ptr & e)
+        {
+            try
+            {
+                std::rethrow_exception(e);
+            }
+            catch (const std::exception & ex)
+            {
+                GTEST_COUT << "OnError: " << ex.what() << std::endl;
+            }
+        },
+        []() { GTEST_COUT << "completed" << std::endl; });
+
+    pSourceNode->connect(root);
+    auto diagraph = graph::print<Con_t>(root);
+    GTEST_COUT << diagraph.str() << std::endl;
+    ASSERT_EQ(expected * 4, got);
+}
