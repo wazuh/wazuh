344f17f1c7d8b948fcd87ebe042066a99f5c177f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-26 12:28:40 +0100

    Added skeleton tcp driver and created cpps

--- src/engine/source/server/endpoints/endpoint.hpp
+++ src/engine/source/server/endpoints/endpoint.hpp
@@ -7,6 +7,14 @@
 
 namespace server::endpoints
 {
+
+class Endpoint
+{
+protected:
+    std::function<void(const std::string &)> m_forward;
+    explicit Endpoint(std::function<void(const std::string &)> forward);
+};
+
 enum EndpointType
 {
     TCP,
@@ -14,35 +22,10 @@ enum EndpointType
     SOCKET
 };
 
-class Endpoint
-{
-protected:
-    std::function<void(const std::string &)> m_forward;
-};
+EndpointType stringToEndpoint(const std::string & endpointName);
 
-EndpointType stringToEndpoint(const std::string & endpointName)
-{
-    if (endpointName == "tcp")
-    {
-        return TCP;
-    }
-    else if (endpointName == "udp")
-    {
-        return UDP;
-    }
-    else if (endpointName == "socket")
-    {
-        return SOCKET;
-    }
-    else
-    {
-        throw std::invalid_argument("Error, endpoint " + endpointName + " not supported");
-    }
-}
-
-std::unique_ptr<Endpoint> create(const std::string & type, const std::string & config, std::function<void(const std::string &)> forward)
-{
-}
+std::unique_ptr<Endpoint> create(const std::string & type, const std::string & config,
+                                 std::function<void(const std::string &)> forward);
 
 } // namespace server::endpoints
 #endif // _ENDPOINT_H
