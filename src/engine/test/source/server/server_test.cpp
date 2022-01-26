8efc99e4625bcca2c5fa5026c70349460daf3f4f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-26 16:58:02 +0100

    Updated refactored server, not tested

--- src/engine/test/source/server/server_test.cpp
+++ src/engine/test/source/server/server_test.cpp
@@ -7,8 +7,40 @@
 using namespace server;
 using namespace std;
 
-TEST(Server, Initializes)
+// TEST(Server, Initializes)
+// {
+//     vector<string> config = {"tcp:localhost:5054"};
+//     Server server(config);
+//     server.run();
+//     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+//     server.stop();
+// }
+
+// TEST(Server, TcpInitializes)
+// {
+//     vector<string> config = {"tcp:localhost:5054", "tcp:localhost:5053"};
+//     Server server(config);
+//     server.output().subscribe([](nlohmann::json j) { cout << j.dump(2) << endl; });
+//     server.run();
+//     server.stop();
+// }
+
+// TEST(Server, UdpInitializes)
+// {
+//     vector<string> config = {"udp:localhost:5054"};
+//     Server server(config);
+//     server.output().subscribe([](nlohmann::json j) { cout << j.dump(2) << endl; });
+//     server.run();
+//     std::this_thread::sleep_for(std::chrono::milliseconds(50000));
+//     server.stop();
+// }
+
+TEST(Server, SocketInitializes)
 {
-    vector<string> config = {"config"};
+    vector<string> config = {"socket:/tmp/testsocket"};
     Server server(config);
+    server.output().subscribe([](nlohmann::json j) { cout << j.dump(2) << endl; });
+    server.run();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50000));
+    server.stop();
 }
