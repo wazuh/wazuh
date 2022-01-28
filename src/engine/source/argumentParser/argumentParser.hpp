7d6eee67b88be1b04c1d445935ccf224063db952
miguemedina11 <miguemedina2000@gmail.com>
2022-01-28 13:32:32 +0000

    Added Argument Parser module. Added folder structure. Added Cmake for test and argparse library

---
+++ src/engine/source/argumentParser/argumentParser.hpp
@@ -0,0 +1,26 @@
+#ifndef _ARGUMENT_PARSER_H
+#define _ARGUMENT_PARSER_H
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include "argparse/argparse.hpp"
+
+namespace argumentParser
+{
+
+/**
+ * @brief Document is a json class based on rapidjson library.
+ *
+ */
+class Parser
+{
+
+};
+
+}; // namespace argumentParser
+
+#endif // _ARGUMENT_PARSER_H
