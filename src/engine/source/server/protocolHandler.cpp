33722ce3e1d3b6d650de4adbe7d1d9ccbcdf801f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-01-31 16:52:01 +0100

    Fixed protocolHandler json object building and updated CMakeLists with rapidjson dependency

--- src/engine/source/server/protocolHandler.cpp
+++ src/engine/source/server/protocolHandler.cpp
@@ -13,7 +13,7 @@
 
 json::Document engineserver::protocolhandler::parseEvent(const std::string & event)
 {
-    json::Document object;
+    json::Document object(R"({})");
 
     auto separator_pos = event.find(":");
     auto event_slice = event.substr(separator_pos + 1);
@@ -33,15 +33,15 @@ json::Document engineserver::protocolhandler::parseEvent(const std::string & eve
     }
 
     separator_pos = event_slice.find(":");
-    auto location = event_slice.substr(0, separator_pos);
-
-    auto message = event_slice.substr(separator_pos + 1);
+    std::string location = event_slice.substr(0, separator_pos);
+    std::string message = event_slice.substr(separator_pos + 1);
 
     json::Value queueVal(queue);
     object.set("/queue", queueVal);
-    json::Value locationVal(location.c_str(), location.length());
+    json::Value locationVal;
+    locationVal.SetString(location.c_str(), location.size(), object.getAllocator());
     object.set("/location", locationVal);
-    json::Value messageVal(message.c_str(), message.length());
+    json::Value messageVal(message.c_str(), message.size(), object.getAllocator());
     object.set("/message", messageVal);
 
     return object;
