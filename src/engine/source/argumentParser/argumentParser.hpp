9711d6e8d684ebca69ade05a594c613da4f23c7a
“Migue <miguemedina2000#@gmail.com>
2022-02-01 12:07:51 +0000

    ArgumentParser Class and Tests added (Pending Formatting) Added argumentParser.cpp and argumentParser.hpp content Added test for Parser class

--- src/engine/source/argumentParser/argumentParser.hpp
+++ src/engine/source/argumentParser/argumentParser.hpp
@@ -1,5 +1,5 @@
-#ifndef _ARGUMENT_PARSER_H
-#define _ARGUMENT_PARSER_H
+#ifndef _PARSER_H
+#define _PARSER_H
 
 #include <algorithm>
 #include <chrono>
@@ -9,18 +9,57 @@
 
 #include "argparse/argparse.hpp"
 
-namespace argumentParser
+/**
+ * @brief Defines all parser functionality.
+ *
+ */
+namespace parser
 {
 
 /**
- * @brief Document is a json class based on rapidjson library.
+ * @brief Parser is a class made to parse the command line input.
  *
  */
 class Parser
 {
 
+private:
+    std::string m_endpoint_config;
+    std::string m_storage_path;
+
+public:
+    /**
+     * @brief Construct a new Parser object and extracts the arguments saving them into the class variables
+     *
+     * @param argc Number of arguments passed.
+     * @param argv List the arguments passed via console.
+     */
+    Parser(int argc, char *argv[]);
+
+    /**
+     * @brief Extracts the arguments saving them into the class variables.
+     *
+     * @param argc Number of arguments passed.
+     * @param argv List the arguments passed via console.
+     */
+    void parse(int argc, char *argv[]);
+
+    /**
+     * @brief Returns the endpoint configuration that has been previously parsed
+     *
+     * @return std::string m_endpoint_config
+     */
+    std::string getEndpointConfig();
+
+    /**
+     * @brief Returns the storage path that has been previously parsed
+     *
+     * @return std::string m_storage_path
+     */
+    std::string getStoragePath();
+
 };
 
-}; // namespace argumentParser
+} // namespace parser
 
-#endif // _ARGUMENT_PARSER_H
+#endif // _PARSER_H
