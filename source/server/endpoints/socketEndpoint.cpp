ad8d241bd3aacfe34126cfdcaae55ef09bb53345
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-03 13:21:18 +0100

    Refactored server to use observable of observables with flat_map

--- source/server/endpoints/socketEndpoint.cpp
+++ source/server/endpoints/socketEndpoint.cpp
@@ -12,81 +12,85 @@
 namespace engineserver::endpoints
 {
 
-SocketEndpoint::SocketEndpoint(const std::string & config)
-    : BaseEndpoint{config}, m_loop{uvw::Loop::getDefault()}, m_handle{m_loop->resource<uvw::PipeHandle>()}
+SocketEndpoint::SocketEndpoint(const std::string & config) : BaseEndpoint{config}
 {
-    this->m_path = config;
-
-    this->m_handle->on<uvw::ErrorEvent>(
-        [](const uvw::ErrorEvent & event, uvw::PipeHandle & socket)
+    std::string path = config;
+    this->m_out = rxcpp::observable<>::create<BaseEndpoint::observable_t>(
+        [=](BaseEndpoint::subscriber_t s)
         {
-            std::cerr << "FIFO Server (" << socket.sock().c_str() << ") error: code=" << event.code()
-                      << "; name=" << event.name() << "; message=" << event.what() << std::endl;
-        });
-
-    this->m_handle->on<uvw::ListenEvent>(
-        [this](const uvw::ListenEvent &, uvw::PipeHandle & handle)
-        {
-            auto client = handle.loop().resource<uvw::PipeHandle>();
-
-            client->on<uvw::ErrorEvent>(
-                [](const uvw::ErrorEvent & event, uvw::PipeHandle & socket)
+            s.on_next(rxcpp::observable<>::create<BaseEndpoint::event_t>(
+                [path](rxcpp::subscriber<BaseEndpoint::event_t> sInner)
                 {
-                    std::cerr << "FIFO Client (" << socket.peer().c_str() << ") error: code=" << event.code()
-                              << "; name=" << event.name() << "; message=" << event.what() << std::endl;
-                });
+                    auto loop = uvw::Loop::getDefault();
+                    auto handle = loop->resource<uvw::PipeHandle>();
 
-            client->on<uvw::DataEvent>(
-                [this](const uvw::DataEvent & event, uvw::PipeHandle & client)
-                {
-                    auto eventObject =
-                        engineserver::protocolhandler::parseEvent(std::string(event.data.get(), event.length));
-                    if (!eventObject.contains("error"))
+                    handle->on<uvw::ErrorEvent>(
+                        [](const uvw::ErrorEvent & event, uvw::PipeHandle & socket)
+                        {
+                            std::cerr << "FIFO Server (" << socket.sock().c_str() << ") error: code=" << event.code()
+                                      << "; name=" << event.name() << "; message=" << event.what() << std::endl;
+                        });
+
+                    handle->on<uvw::ListenEvent>(
+                        [sInner](const uvw::ListenEvent &, uvw::PipeHandle & handle)
+                        {
+                            auto client = handle.loop().resource<uvw::PipeHandle>();
+
+                            client->on<uvw::ErrorEvent>(
+                                [](const uvw::ErrorEvent & event, uvw::PipeHandle & socket)
+                                {
+                                    std::cerr << "FIFO Client (" << socket.peer().c_str()
+                                              << ") error: code=" << event.code() << "; name=" << event.name()
+                                              << "; message=" << event.what() << std::endl;
+                                });
+
+                            client->on<uvw::DataEvent>(
+                                [sInner](const uvw::DataEvent & event, uvw::PipeHandle & client)
+                                {
+                                    auto eventObject = engineserver::protocolhandler::parseEvent(
+                                        std::string(event.data.get(), event.length));
+                                    if (!eventObject.contains("error"))
+                                    {
+                                        sInner.on_next(eventObject);
+                                    }
+                                    else
+                                    {
+                                        // TODO: complete this case
+                                    }
+                                });
+
+                            client->on<uvw::CloseEvent>([&handle](const uvw::CloseEvent &, uvw::PipeHandle &)
+                                                        { handle.close(); });
+
+                            handle.accept(*client);
+                            client->read();
+                        });
+
                     {
-                        this->m_subscriber.on_next(eventObject);
+                        struct stat buffer;
+                        if (stat(path.c_str(), &buffer) == 0)
+                        {
+                            remove(path.c_str());
+                        }
                     }
-                    else
-                    {
-                        // TODO: complete this case
-                    }
-                });
 
-            client->on<uvw::CloseEvent>([&handle](const uvw::CloseEvent &, uvw::PipeHandle &) { handle.close(); });
-
-            handle.accept(*client);
-            client->read();
+                    handle->bind(path);
+                    handle->listen();
+                    loop->run();
+                }));
         });
-
-    {
-        struct stat buffer;
-        if (stat(this->m_path.c_str(), &buffer) == 0)
-        {
-            remove(this->m_path.c_str());
-        }
-    }
-
-    this->m_handle->bind(this->m_path);
-    this->m_handle->listen();
 }
 
 void SocketEndpoint::run(void)
 {
-    std::thread t(&uvw::Loop::run, this->m_loop.get());
-    t.detach();
 }
 
 void SocketEndpoint::close(void)
 {
-    m_loop->stop();                                                 /// Stops the loop
-    m_loop->walk([](uvw::BaseHandle & handle) { handle.close(); }); /// Triggers every handle's close callback
-    m_loop->run(); /// Runs the loop again, so every handle is able to receive its close callback
-    m_loop->clear();
-    m_loop->close();
 }
 
 SocketEndpoint::~SocketEndpoint()
 {
-    this->close();
 }
 
 } // namespace engineserver::endpoints
