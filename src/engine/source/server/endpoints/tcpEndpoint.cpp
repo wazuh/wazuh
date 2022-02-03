cec7570172238ad4032899b449fd5873195208d0
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-03 13:21:18 +0100

    Refactored server to use observable of observables with flat_map

--- src/engine/source/server/endpoints/tcpEndpoint.cpp
+++ src/engine/source/server/endpoints/tcpEndpoint.cpp
@@ -12,76 +12,86 @@
 namespace engineserver::endpoints
 {
 
-TCPEndpoint::TCPEndpoint(const std::string & config)
-    : BaseEndpoint{config}, m_loop{uvw::Loop::getDefault()}, m_handle{m_loop->resource<uvw::TCPHandle>()}
+TCPEndpoint::TCPEndpoint(const std::string & config) : BaseEndpoint{config}
 {
     auto pos = config.find(":");
     this->m_ip = config.substr(0, pos);
     this->m_port = stoi(config.substr(pos + 1));
-
-    m_handle->on<uvw::ErrorEvent>(
-        [](const uvw::ErrorEvent & event, uvw::TCPHandle & tcp)
+    this->m_out = rxcpp::observable<>::create<BaseEndpoint::observable_t>(
+        [ip = this->m_ip, port = this->m_port](BaseEndpoint::subscriber_t s)
         {
-            std::cerr << "TCP Server (" << tcp.sock().ip.c_str() << ":" << tcp.sock().port
-                      << ") error: code=" << event.code() << "; name=" << event.name() << "; message=" << event.what()
-                      << std::endl;
-        });
+            auto loop = uvw::Loop::getDefault();
+            auto handle = loop->resource<uvw::TCPHandle>();
 
-    m_handle->on<uvw::ListenEvent>(
-        [this](const uvw::ListenEvent &, uvw::TCPHandle & srv)
-        {
-            auto client = srv.loop().resource<uvw::TCPHandle>();
-
-            client->on<uvw::ErrorEvent>(
-                [](const uvw::ErrorEvent & event, uvw::TCPHandle & client)
+            handle->on<uvw::ErrorEvent>(
+                [](const uvw::ErrorEvent & event, uvw::TCPHandle & tcp)
                 {
-                    std::cerr << "TCP Client (" << client.peer().ip.c_str() << ":" << client.peer().port
+                    std::cerr << "TCP Server (" << tcp.sock().ip.c_str() << ":" << tcp.sock().port
                               << ") error: code=" << event.code() << "; name=" << event.name()
                               << "; message=" << event.what() << std::endl;
                 });
 
-            client->on<uvw::DataEvent>(
-                [this, &srv](const uvw::DataEvent & event, uvw::TCPHandle & client)
+            handle->on<uvw::ListenEvent>(
+                [s, ip, port](const uvw::ListenEvent &, uvw::TCPHandle & srv)
                 {
-                    auto eventObject =
-                        engineserver::protocolhandler::parseEvent(std::string(event.data.get(), event.length));
-                    if (!eventObject.contains("error"))
-                    {
-                        this->m_subscriber.on_next(eventObject);
-                    }
-                    else
-                    {
-                        // TODO: complete this case
-                    }
+                    s.on_next(rxcpp::observable<>::create<BaseEndpoint::event_t>(
+                        [ip, port, &srv](rxcpp::subscriber<BaseEndpoint::event_t> sInner)
+                        {
+                            auto client = srv.loop().resource<uvw::TCPHandle>();
+
+                            client->on<uvw::ErrorEvent>(
+                                [](const uvw::ErrorEvent & event, uvw::TCPHandle & client)
+                                {
+                                    std::cerr << "TCP Client (" << client.peer().ip.c_str() << ":" << client.peer().port
+                                              << ") error: code=" << event.code() << "; name=" << event.name()
+                                              << "; message=" << event.what() << std::endl;
+                                });
+
+                            client->on<uvw::DataEvent>(
+                                [sInner, &srv](const uvw::DataEvent & event, uvw::TCPHandle & client)
+                                {
+                                    auto eventObject = engineserver::protocolhandler::parseEvent(
+                                        std::string(event.data.get(), event.length));
+                                    if (!eventObject.contains("error"))
+                                    {
+                                        sInner.on_next(eventObject);
+                                    }
+                                    else
+                                    {
+                                        // TODO: complete this case
+                                    }
+                                });
+
+                            client->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::TCPHandle & client)
+                                                      { client.close(); });
+
+                            srv.accept(*client);
+                            client->read();
+                        }));
                 });
 
-            client->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::TCPHandle & client) { client.close(); });
-
-            srv.accept(*client);
-            client->read();
+            handle->bind(ip, port);
+            handle->listen();
+            loop->run();
         });
-
-    m_handle->bind(this->m_ip, this->m_port);
-    m_handle->listen();
 }
 
 void TCPEndpoint::run(void)
 {
-    this->m_loop->run();
 }
 
 void TCPEndpoint::close(void)
 {
-    m_loop->stop();                                                 /// Stops the loop
-    m_loop->walk([](uvw::BaseHandle & handle) { handle.close(); }); /// Triggers every handle's close callback
-    m_loop->run(); /// Runs the loop again, so every handle is able to receive its close callback
-    m_loop->clear();
-    m_loop->close();
+    // m_loop->stop();                                                 /// Stops the loop
+    // m_loop->walk([](uvw::BaseHandle & handle) { handle.close(); }); /// Triggers every handle's close callback
+    // m_loop->run(); /// Runs the loop again, so every handle is able to receive its close callback
+    // m_loop->clear();
+    // m_loop->close();
 }
 
 TCPEndpoint::~TCPEndpoint()
 {
-    this->close();
+    // this->close();
 }
 
 } // namespace engineserver::endpoints
