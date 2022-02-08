ac393540fad72dba32fa18c19e2a4b739716d1af
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-08 13:18:10 +0100

    Deleted unused code and format style

--- src/engine/source/cliParser/cliParser.cpp
+++ src/engine/source/cliParser/cliParser.cpp
@@ -3,32 +3,31 @@
 #include <iostream>
 #include <string>
 
-
 #include "cliParser.hpp"
 
 using namespace std;
 
 namespace cliparser
 {
-    
-CliParser::CliParser(int argc, char *argv[]){
-    parse(argc,argv);
+
+CliParser::CliParser(int argc, char * argv[])
+{
+    parse(argc, argv);
 }
 
-void CliParser::parse(int argc, char *argv[]){
+void CliParser::parse(int argc, char * argv[])
+{
     argparse::ArgumentParser serverParser("server");
-    serverParser.add_argument("--endpoint")
-        .help("Endpoint configuration string")
-        .required();
+    serverParser.add_argument("--endpoint").help("Endpoint configuration string").required();
 
-    serverParser.add_argument("--file_storage")
-        .help("Path to storage folder")
-        .required();
+    serverParser.add_argument("--file_storage").help("Path to storage folder").required();
 
-    try{
+    try
+    {
         serverParser.parse_args(argc, argv);
     }
-    catch (const std::runtime_error& err) {
+    catch (const std::runtime_error & err)
+    {
         std::cerr << err.what() << std::endl;
         cerr << serverParser;
     }
@@ -37,12 +36,14 @@ void CliParser::parse(int argc, char *argv[]){
     m_storage_path = serverParser.get("--file_storage");
 }
 
-string CliParser::getEndpointConfig(){
+string CliParser::getEndpointConfig()
+{
     return m_endpoint_config;
 }
 
-string CliParser::getStoragePath(){
+string CliParser::getStoragePath()
+{
     return m_storage_path;
 }
 
-}
+} // namespace cliparser
--- src/engine/source/server/protocolHandler.hpp
+++ src/engine/source/server/protocolHandler.hpp
@@ -21,29 +21,46 @@ namespace engineserver
 /**
  * @brief A handler which knows how to parse messages from the network
  * data chunks and send them to a subscriber.
- * 
+ *
  */
 class ProtocolHandler
 {
 private:
-    json::Document parse();
     std::vector<char> m_buff;
     int m_pending{0};
     int m_stage{0};
 
+    /**
+     * @brief generate a json::Document from internal state
+     *
+     * @return json::Document
+     */
+    json::Document parse();
+
+    /**
+     * @brief Update pending value and return true if we have enough data
+     * to calculate the message size.
+     *
+     * @return true
+     * @return false
+     */
     bool hasHeader();
 
-    std::string payload();
-
+    /**
+     * @brief Process a message, parsing it and sending it to s
+     *
+     * @param s a subscriber of this connection.
+     */
     void send(const rxcpp::subscriber<json::Document> s);
+
 public:
     /**
-     * @brief process the chunk of data and send messages to dst when. Return 
+     * @brief process the chunk of data and send messages to dst when. Return
      * true if all data was processed correctly, or false in case of error.
      * The error will be send to the dst.
-     * 
-     * @param data 
-     * @param length 
+     *
+     * @param data
+     * @param length
      * @param dst destination subscriber
      * @return true no errors
      * @return false errors in processing
@@ -51,6 +68,6 @@ public:
     bool process(char * data, std::size_t length, const rxcpp::subscriber<json::Document> dst);
 };
 
-} // namespace engineserver::protocolhandler
+} // namespace engineserver
 
 #endif // _PROTOCOL_HANDLER_H_
