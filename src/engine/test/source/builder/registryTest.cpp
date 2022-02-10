5651edca524d702c4d924804935b98561f795269
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-10 17:29:34 +0100

    Changed registry to be static

--- src/engine/test/source/builder/registryTest.cpp
+++ src/engine/test/source/builder/registryTest.cpp
@@ -24,41 +24,31 @@ TEST(Registry, RegisterBuilder)
 {
     Registry::BuildDocument b = build;
     Registry::BuildType c = b;
-    Registry reg;
-    ASSERT_NO_THROW(reg.registerBuilder("test", c));
+    ASSERT_NO_THROW(Registry::registerBuilder("test", c));
 }
 
 TEST(Registry, RegisterDuplicatedBuilder)
 {
     Registry::BuildDocument b = build;
     Registry::BuildType c = b;
-    Registry reg;
-    reg.registerBuilder("test", c);
-    ASSERT_THROW(reg.registerBuilder("test", c), invalid_argument);
+    ASSERT_THROW(Registry::registerBuilder("test", c), invalid_argument);
 }
 
 TEST(Registry, GetBuilder)
 {
     Registry::BuildDocument b = build;
     Registry::BuildType c = b;
-    Registry reg;
-    reg.registerBuilder("test", c);
-    ASSERT_NO_THROW(auto buildB = reg.getBuilder("test"));
+    ASSERT_NO_THROW(auto buildB = Registry::getBuilder("test"));
 }
 
 TEST(Registry, GetNonExistentBuilder)
 {
-    Registry reg;
-    ASSERT_THROW(auto buildB = reg.getBuilder("test"), invalid_argument);
+    ASSERT_THROW(auto buildB = Registry::getBuilder("error"), invalid_argument);
 }
 
 TEST(Registry, GetBuilderAndBuilds)
 {
-    Registry::BuildDocument b = build;
-    Registry::BuildType c = b;
-    Registry reg;
-    reg.registerBuilder("test", c);
-    auto buildB = std::get<Registry::BuildDocument>(reg.getBuilder("test"));
+    auto buildB = std::get<Registry::BuildDocument>(Registry::getBuilder("test"));
     Registry::Obs_t o = rxcpp::observable<>::empty<Registry::Event_t>();
     Registry::Obs_t expected = buildB(json::Document(R"({})"))(o);
     ASSERT_EQ(o, expected);
