4ed4d7a9e03cd45a663ceeac6558734ec09d7891
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-02-10 18:21:08 +0000

    New cpp files and copyrights added

--- src/engine/source/builder/builders/filter.hpp
+++ src/engine/source/builder/builders/filter.hpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
 #ifndef _BUILDERS_FILTER_H
 #define _BUILDERS_FILTER_H
 
