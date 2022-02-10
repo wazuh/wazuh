29578256bf898041c73c08ac4ef257dcf979b545
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-10 17:29:34 +0100

    Changed registry to be static

--- src/engine/source/builder/registry.cpp
+++ src/engine/source/builder/registry.cpp
@@ -3,29 +3,31 @@
 namespace builder::internals
 {
 
+std::map<std::string, Registry::BuildType> Registry::m_registry;
+
 void Registry::registerBuilder(const std::string & builderName, const Registry::BuildType & builder)
 {
-    if (this->m_registry.count(builderName) > 0)
+    if (Registry::m_registry.count(builderName) > 0)
     {
         LOG(ERROR) << "Tried to register duplicate builder " << builderName << std::endl;
         throw std::invalid_argument("Tried to register duplicate builder " + builderName);
     }
     else
     {
-        this->m_registry[builderName] = builder;
+        Registry::m_registry[builderName] = builder;
     }
 }
 
 Registry::BuildType Registry::getBuilder(const std::string & builderName)
 {
-    if (this->m_registry.count(builderName) == 0)
+    if (Registry::m_registry.count(builderName) == 0)
     {
         LOG(ERROR) << "Tried to obtain not registered builder " << builderName << std::endl;
         throw std::invalid_argument("Tried to obtain not registered builder " + builderName);
     }
     else
     {
-        return this->m_registry[builderName];
+        return Registry::m_registry[builderName];
     }
 }
 
