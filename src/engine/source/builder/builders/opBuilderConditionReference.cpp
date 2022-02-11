69f2b84f6d31ce495645041a7b407150312768ed
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-11 13:00:48 +0100

    Refactored condition builder to use new registry - Renamed builders to be more verbose - Splitted conditional builders to separate files - Updated tests

---
+++ src/engine/source/builder/builders/opBuilderConditionReference.cpp
@@ -0,0 +1,41 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include "opBuilderConditionReference.hpp"
+
+#include <algorithm>
+#include <string>
+
+#include "syntax.hpp"
+
+namespace builder::internals::builders
+{
+
+types::Lifter opBuilderConditionReference(const types::DocumentValue & def)
+{
+    // Estract field and reference
+    std::string field = def.MemberBegin()->name.GetString();
+    std::string reference = def.MemberBegin()->value.GetString();
+    reference = reference.substr(1, std::string::npos);
+
+    // Return Lifter
+    return [=](types::Observable o)
+    {
+        // Append rxcpp operation
+        return o.filter(
+            [=](types::Event e)
+            {
+                //TODO: implemente proper json check reference
+                auto v = e.get("/" + reference);
+                return e.check("/" + field, v);
+            });
+    };
+}
+
+} // namespace builder::internals::builders
---
+++ src/engine/test/source/builder/builders/opBuilderConditionValueTest.cpp
@@ -0,0 +1,67 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include <gtest/gtest.h>
+#include <testUtils.hpp>
+
+#include <vector>
+
+#include "opBuilderConditionValue.hpp"
+
+using namespace builder::internals::builders;
+
+TEST(opBuilderConditionValue, Builds)
+{
+    Document doc{R"({
+        "check": [
+            {"string": "value"},
+            {"int": 1},
+            {"bool": true}
+        ]
+    })"};
+    const auto & arr = doc.begin()->value.GetArray();
+    for (auto it = arr.Begin(); it != arr.end(); ++it)
+    {
+        ASSERT_NO_THROW(opBuilderConditionValue(*it));
+    }
+}
+
+TEST(opBuilderConditionValue, BuildsOperatesString)
+{
+    Document doc{R"({
+        "check":
+            {"field": "value"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"values"}
+            )"});
+            s.on_next(Event{R"(
+                {"otherfield":"value"}
+            )"});
+            s.on_next(Event{R"(
+                {"otherfield":1}
+            )"});
+            s.on_completed();
+        });
+    Lifter lift = opBuilderConditionValue(*doc.get("/check"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_STREQ(expected[0].get("/field")->GetString(), doc.get("/check")->MemberBegin()->value.GetString());
+}
+
+//TODO: Add rest of use cases (int, bool, null)
