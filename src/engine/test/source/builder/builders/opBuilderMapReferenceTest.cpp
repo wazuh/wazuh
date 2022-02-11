760300671f1bbe34c0825da67880afb88a3e027f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-11 19:32:50 +0100

    Refactored map builders to use registry and types

---
+++ src/engine/test/source/builder/builders/opBuilderMapReferenceTest.cpp
@@ -0,0 +1,57 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include <gtest/gtest.h>
+#include <testUtils.hpp>
+
+#include <vector>
+
+#include "opBuilderMapReference.hpp"
+
+using namespace builder::internals::builders;
+
+TEST(opBuilderMapReference, Builds)
+{
+    Document doc{R"({
+        "normalize":
+            {"field": "$other_field"}
+    })"};
+    ASSERT_NO_THROW(opBuilderMapReference(*doc.get("/normalize")));
+}
+
+TEST(opBuilderMapReference, BuildsOperates)
+{
+    Document doc{R"({
+        "normalize":
+            {"field": "$other_field"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            // TODO: Fix json to return false instead of throw
+            // s.on_next(Event{R"(
+            //     {"field":"value"}
+            // )"});
+            // s.on_next(Event{R"(
+            //     {"field":"values"}
+            // )"});
+            s.on_next(Event{R"(
+                {"other_field":"referenced"}
+            )"});
+            s.on_completed();
+        });
+    Lifter lift1 = opBuilderMapReference(*doc.get("/normalize"));
+
+    Observable output = lift1(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_STREQ(expected[0].get("/field")->GetString(), "referenced");
+}
