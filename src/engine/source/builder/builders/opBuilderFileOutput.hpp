89a157b323194d96848bc299ed062ad65f50b414
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-14 14:23:59 +0100

    Updated refactored Rules, Filters and Outputs to use Registry

---
+++ src/engine/source/builder/builders/opBuilderFileOutput.hpp
@@ -0,0 +1,28 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _OP_BUILDER_FILE_OUTPUT_H
+#define _OP_BUILDER_FILE_OUTPUT_H
+
+#include "builderTypes.hpp"
+
+namespace builder::internals::builders
+{
+
+/**
+ * @brief Builds file output operation.
+ *
+ * @param def Definition of the operation to be built
+ * @return types::Lifter
+ */
+types::Lifter opBuilderFileOutput(const types::DocumentValue & def);
+
+} // namespace builder::internals::builders
+
+#endif // _OP_BUILDER_FILE_OUTPUT_H
---
+++ src/engine/test/source/builder/builders/opBuilderFileOutputTest.cpp
@@ -0,0 +1,79 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include <gtest/gtest.h>
+#include <testUtils.hpp>
+
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include "opBuilderFileOutput.hpp"
+
+using namespace builder::internals::builders;
+
+TEST(opBuilderFileOutput, Builds)
+{
+    Document doc{R"({
+        "file":
+            {"path": "value"}
+    })"};
+
+    ASSERT_NO_THROW(opBuilderFileOutput(*doc.get("/file")));
+}
+
+TEST(opBuilderFileOutput, BuildsOperates)
+{
+    Document doc{R"({
+        "file":
+            {"path": "/tmp/fileOutputTest.txt"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_completed();
+        });
+    Lifter lift = opBuilderFileOutput(*doc.get("/file"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 4);
+
+    const string expectedWrite =
+R"({"field":"value"}
+{"field":"value"}
+{"field":"value"}
+{"field":"value"}
+)";
+
+    string filepath{"/tmp/fileOutputTest.txt"};
+    std::ifstream ifs(filepath);
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+    const string got = buffer.str();
+    // cerr << got << std::endl;
+    // cerr << expectedWrite << std::endl;
+    std::filesystem::remove(filepath);
+
+    ASSERT_EQ(got, expectedWrite);
+}
