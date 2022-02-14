f38e4669bac848ba5617a48c4533efcb65137e18
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-14 16:04:02 +0100

    Updated builder to use registry, updated main and deleted unused code

--- src/engine/source/builder/register.hpp
+++ src/engine/source/builder/register.hpp
@@ -10,30 +10,73 @@
 #ifndef _REGISTER_HPP
 #define _REGISTER_HPP
 
+#include "builderTypes.hpp"
 #include "registry.hpp"
-#include <variant>
 
 // Add all builders includes here
-#include "buildCheck.hpp"
+#include "assetBuilderDecoder.hpp"
+#include "assetBuilderFilter.hpp"
+#include "assetBuilderOutput.hpp"
+#include "assetBuilderRule.hpp"
+#include "combinatorBuilderBroadcast.hpp"
+#include "combinatorBuilderChain.hpp"
+#include "opBuilderCondition.hpp"
+#include "opBuilderConditionReference.hpp"
+#include "opBuilderConditionValue.hpp"
+#include "opBuilderFileOutput.hpp"
+#include "opBuilderHelperExists.hpp"
+#include "opBuilderMap.hpp"
+#include "opBuilderMapReference.hpp"
+#include "opBuilderMapValue.hpp"
+#include "stageBuilderCheck.hpp"
+#include "stageBuilderNormalize.hpp"
+#include "stageBuilderOutputs.hpp"
 
 namespace builder::internals
 {
 void registerBuilders()
 {
     // Needed to initialize the variant type
-    BuildValue b;
-    BuildType c;
+    types::BuilderVariant c;
 
     // Register all builders
-    // Condition Value
-    b = builders::buildCheckVal;
-    c = b;
+    c = builders::opBuilderMapValue;
+    Registry::registerBuilder("map.value", c);
+    c = builders::opBuilderMapReference;
+    Registry::registerBuilder("map.reference", c);
+    c = builders::opBuilderMap;
+    Registry::registerBuilder("map", c);
+    c = builders::combinatorBuilderChain;
+    Registry::registerBuilder("combinator.chain", c);
+    c = builders::combinatorBuilderBroadcast;
+    Registry::registerBuilder("combinator.broadcast", c);
+    c = builders::opBuilderConditionValue;
     Registry::registerBuilder("condition.value", c);
-
-    // Condition
-    b = builders::buildCheck;
-    c = b;
+    c = builders::opBuilderConditionReference;
+    Registry::registerBuilder("condition.reference", c);
+    c = builders::opBuilderHelperExists;
+    Registry::registerBuilder("helper.exists", c);
+    c = builders::opBuilderHelperNotExists;
+    Registry::registerBuilder("helper.not_exists", c);
+    c = builders::opBuilderFileOutput;
+    Registry::registerBuilder("file", c);
+    c = builders::opBuilderCondition;
     Registry::registerBuilder("condition", c);
+    c = builders::stageBuilderNormalize;
+    Registry::registerBuilder("normalize", c);
+    c = builders::stageBuilderCheck;
+    Registry::registerBuilder("check", c);
+    Registry::registerBuilder("allow", c);
+    c = builders::stageBuilderOutputs;
+    Registry::registerBuilder("outputs", c);
+    c = builders::assetBuilderDecoder;
+    Registry::registerBuilder("decoder", c);
+    c = builders::assetBuilderFilter;
+    Registry::registerBuilder("filter", c);
+    c = builders::assetBuilderRule;
+    Registry::registerBuilder("rule", c);
+    c = builders::assetBuilderOutput;
+    Registry::registerBuilder("output", c);
 }
 } // namespace builder::internals
 
--- src/engine/test/source/builder/registerTest.cpp
+++ src/engine/test/source/builder/registerTest.cpp
@@ -12,4 +12,20 @@ TEST(Register, AllBuildersRegistered)
     // Check all builders have been registered
     ASSERT_NO_THROW(Registry::getBuilder("condition.value"));
     ASSERT_NO_THROW(Registry::getBuilder("condition"));
+    ASSERT_NO_THROW(Registry::getBuilder("condition.reference"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.exists"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.not_exists"));
+    ASSERT_NO_THROW(Registry::getBuilder("map"));
+    ASSERT_NO_THROW(Registry::getBuilder("map.value"));
+    ASSERT_NO_THROW(Registry::getBuilder("map.reference"));
+    ASSERT_NO_THROW(Registry::getBuilder("check"));
+    ASSERT_NO_THROW(Registry::getBuilder("normalize"));
+    ASSERT_NO_THROW(Registry::getBuilder("outputs"));
+    ASSERT_NO_THROW(Registry::getBuilder("combinator.chain"));
+    ASSERT_NO_THROW(Registry::getBuilder("combinator.broadcast"));
+    ASSERT_NO_THROW(Registry::getBuilder("file"));
+    ASSERT_NO_THROW(Registry::getBuilder("decoder"));
+    ASSERT_NO_THROW(Registry::getBuilder("rule"));
+    ASSERT_NO_THROW(Registry::getBuilder("output"));
+    ASSERT_NO_THROW(Registry::getBuilder("filter"));
 }
