5635509aff9a7b5162737854c96547755882c750
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-15 14:44:55 +0100

    Added rxcpp thread tests

---
+++ test/rxcpp/testUtils.hpp
@@ -0,0 +1,18 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _TEST_UTILS_H
+#define _TEST_UTILS_H
+
+#define GTEST_COUT std::cout << "[          ] [ INFO ] "
+
+using namespace std;
+using namespace rxcpp;
+
+#endif // _TEST_UTILS_H
