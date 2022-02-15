9152681a53627fb11bee97761f0d52d711f8a77c
palaciosjeremias <jeremias.palacios@wazuh.com>
2022-02-15 15:23:09 -0800

    Add Json specific parser

--- src/engine/source/hlp/src/LogQLParser.cpp
+++ src/engine/source/hlp/src/LogQLParser.cpp
@@ -132,6 +132,12 @@ static Parser parseCaptureString(Token token) {
         }
     }
     parser.captureOpts = std::move(captureParams);
+    if (!parser.captureOpts.empty()) {
+        auto it = ECSParserMapper.find({ parser.captureOpts[0].c_str(), parser.captureOpts[0].length()});
+        if (it != ECSParserMapper.end()) {
+            parser.parserType = it->second;
+        }
+    }
 
     return parser;
 }
