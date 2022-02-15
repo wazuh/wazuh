a3d4c2628d50416e96edb453e1598d3eebde044b
LucioDonda <donda.lucio@gmail.com>
2022-02-15 11:53:08 -0300

    fixing tests

--- src/engine/test/source/hlp/hlp_test.cpp
+++ src/engine/test/source/hlp/hlp_test.cpp
@@ -3,15 +3,18 @@
 #include "hlp/hlp.hpp"
 
     static const char *logQl =
-        "<source.ip> <source.address> - - [<timestamp/APACHE>] \"<http.request.method> <url> HTTP/<http.version>\" "
+        "<source.address> - - [<timestamp/APACHE>] \"<http.request.method> <url> HTTP/<http.version>\" "
         "<http.response.status_code> <http.response.body.bytes> \"-\" \"<user_agent.original>\"";
     static const char *event =
-        "2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF monitoring-server - - [29/May/2017:19:02:48 +0000] \"GET /status HTTP/1.1\" 200 612 \"-\" "
+        "monitoring-server - - [29/May/2017:19:02:48 +0000] \"GET /status HTTP/1.1\" 200 612 \"-\" "
         "\"Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2\"";
 
     static const char *logQl2 = "<source.ip> - - [<timestamp/APACHE>] \"-\" "
                                 "<http.response.status_code> <http.response.body.bytes> \"-\" \"-\"";
     static const char *event2 = "127.0.0.1 - - [02/Feb/2019:05:38:45 +0100] \"-\" 408 152 \"-\" \"-\"";
+
+    static const char *logQl3 = "<source.ip> - <server.ip> -- <source.nat.ip> \"-\" \"-\"";
+    static const char *event3 = "127.0.0.1 - 2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF -- 0:db8:0:4:CCCC:0:EEEE:FFFF \"-\" \"-\"";
 
 // Test: An asset that fails the check against the schema
 TEST(suite_1, test_1)
@@ -34,6 +37,22 @@ TEST(suite_1, test_1)
     printf("%30s | %s\n", "Key", "Val");
     printf("-------------------------------|------------\n");
     for (auto const &r : result2) { printf("%30s | %s\n", r.first.c_str(), r.second.c_str()); }
+
+    printf("\n--------------\n\n");
+}
+
+// Test: parsing succesfully three different IP addresses 
+TEST(suite_1, test_IP_Parser)
+{
+
+    printf("\n\n---HLP Test- IP parser --\n");
+
+    auto parseOp = getParserOp(logQl3);
+    auto result = parseOp(event3);
+    putchar('\n');
+    printf("%30s | %s\n", "Key", "Val");
+    printf("-------------------------------|------------\n");
+    for (auto const &r : result) { printf("%30s | %s\n", r.first.c_str(), r.second.c_str()); }
 
     printf("\n--------------\n\n");
 }
