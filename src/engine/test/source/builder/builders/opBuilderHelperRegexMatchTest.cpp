7cbc187956b399d939d255671c0d62b0711db8c9
miguemedina11 <miguemedina2000@gmail.com>
2022-02-17 12:35:21 +0000

    Added First Implementation of Regex Match Helper Function using RE2 Added simple tests Implemented use of stringUtils::split function

---
+++ src/engine/test/source/builder/builders/opBuilderHelperRegexMatchTest.cpp
@@ -0,0 +1,59 @@
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include <gtest/gtest.h>
+#include <testUtils.hpp>
+
+#include <vector>
+
+#include "OpBuilderHelperFilter.hpp"
+
+using namespace builder::internals::builders;
+
+TEST(opBuilderHelperRegexMatch, Builds)
+{
+    Document doc{R"({
+        "check":
+            {"field": "+r_match/regexp"}
+    })"};
+    ASSERT_NO_THROW(opBuilderHelperRegexMatch(*doc.get("/check")));
+}
+
+TEST(opBuilderHelperRegexMatch, RegexMatches)
+{
+    Document doc{R"({
+        "check":
+            {"field": "+r_match/exp"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            s.on_next(Event{R"(
+                {"field":"exp"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"expregex"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"this is a test exp"}
+            )"});
+            s.on_next(Event{R"(
+                {"field":"value"}
+            )"});
+            s.on_completed();
+        });
+
+    Lifter lift = opBuilderHelperRegexMatch(*doc.get("/check"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_TRUE(RE2::FullMatch(expected[0].get("/field")->GetString(), "exp"));
+}
