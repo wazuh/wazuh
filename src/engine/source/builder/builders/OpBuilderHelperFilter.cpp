bc0f9e8b18e0cdb1b53026fa4d2a7199dc54e6c3
Julian Morales <jmorales@unc.edu.ar>
2022-02-18 14:20:28 +0000

    Adds str_eq helper function
    Include shared utils in engine project
    Adds str_eq helper function

--- src/engine/source/builder/builders/OpBuilderHelperFilter.cpp
+++ src/engine/source/builder/builders/OpBuilderHelperFilter.cpp
@@ -48,6 +48,15 @@ std::tuple<std::string, opString, opString>  getCompOpParameter(const DocumentVa
 namespace builder::internals::builders
 {
 
+/**
+ * @brief Create `exists` helper function that filters events that contains specified field.
+ *
+ * The filter checks if a field exists in the JSON event `e`.
+ * For example: if def = `{wazuh: +exists}` only events containing `wazuh` field
+ * will continue on the rxcpp pipeline.
+ * @param def The filter definition. i.e : `{wazuh: +exists}`
+ * @return types::Lifter The lifter with the `exists` filter.
+ */
 types::Lifter opBuilderHelperExists(const types::DocumentValue & def)
 {
     // Get field
@@ -61,6 +70,15 @@ types::Lifter opBuilderHelperExists(const types::DocumentValue & def)
     };
 }
 
+/**
+ * @brief Create `notExists` helper function that filters events that not contains specified field.
+ *
+ * The filter checks if a field not exists in the JSON event `e`.
+ * For example: if def = `{wazuh: +not_exists}` only events not containing `wazuh`
+ * field will continue on the rxcpp pipeline.
+ * @param def The filter definition. i.e : `{wazuh: +exists}`
+ * @return types::Lifter The lifter with the `exists` filter.
+ */
 types::Lifter opBuilderHelperNotExists(const types::DocumentValue & def)
 {
     // Get field
@@ -74,4 +92,50 @@ types::Lifter opBuilderHelperNotExists(const types::DocumentValue & def)
     };
 }
 
+// TODO field: +s_eq/str|$ref/
+
+/**
+ * @brief Create `s_eq` helper function that filters events with a string
+ * field equals to a value.
+ *
+ * The filter checks if a field in the JSON event `wazuh` is equal to a value.
+ * @param def The filter definition. i.e : `{wazuh: +s_eq/value}`
+ * @return types::Lifter The lifter with the `s_eq` filter.
+ * @throw std::runtime_error if the parameter is not a string.
+ */
+types::Lifter opBuilderHelperS_eq(const types::DocumentValue & def)
+{
+    // Get field key to check
+    std::string key = def.MemberBegin()->name.GetString();
+
+    // Get the raw value of parameter
+    if (!def.MemberBegin()->value.IsString()) {
+        throw std::runtime_error("Invalid parameter type for s_eq operator (str expected)");
+    }
+
+    // Parse parameters
+    std::string parm {def.MemberBegin()->value.GetString()};
+    auto parametersArr = utils::string::split(parm, '/');
+    if (parametersArr.size() != 2) {
+        throw std::runtime_error("Invalid number of parameters for s_eq operator");
+    }
+
+    std::string expectedStr = parametersArr[1];
+    // Return Lifter
+    return [=](types::Observable o)
+    {
+        // Append rxcpp operation
+        return o.filter([expectedStr, key](types::Event e) {
+            try {
+                    return e.get("/" + key)->IsString() && e.get("/" + key)->GetString() == expectedStr;
+            } catch (std::exception & e) {
+                // TODO Check exception type
+                return false;
+            }
+        });
+    };
+}
+
+// TODO field: +s_gt/str|$ref/
+// TODO field: +s_lt/str|$ref/
 } // namespace builder::internals::builders
