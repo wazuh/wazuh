a1866e31f38346d186556098e3c5f811d33a6545
miguemedina11 <miguemedina2000@gmail.com>
2022-02-18 09:58:38 +0000

    Added Not Match Helper Function and Basic Testing

--- src/engine/source/builder/builders/OpBuilderHelperFilter.cpp
+++ src/engine/source/builder/builders/OpBuilderHelperFilter.cpp
@@ -494,4 +494,23 @@ types::Lifter opBuilderHelperRegexMatch(const types::DocumentValue & def)
     };
 }
 
+types::Lifter opBuilderHelperRegexNotMatch(const types::DocumentValue & def)
+{
+    // Get field
+    std::string field = def.MemberBegin()->name.GetString();
+    std::string value = def.MemberBegin()->value.GetString();
+    std::vector<std::string> parameters = utils::string::split(value, '/');
+    if (parameters.size() != 2){
+        throw std::invalid_argument("Wrong number of arguments passed");
+    }
+    std::string regexp = parameters[1];
+
+    // Return Lifter
+    return [=](types::Observable o)
+    {
+        // Append rxcpp operations
+        return o.filter([=](types::Event e) { return (!RE2::PartialMatch(e.get("/" + field)->GetString(), regexp)); });
+    };
+}
+
 } // namespace builder::internals::builders
