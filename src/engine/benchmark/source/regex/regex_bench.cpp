e82b9d31dc655de336395a74beafb0e95657bb55
miguemedina11 <miguemedina2000@gmail.com>
2022-02-18 11:54:23 +0000

    Added RE2 and STD FullMatch vs PartialMatch benchmarks.

--- src/engine/benchmark/source/regex/regex_bench.cpp
+++ src/engine/benchmark/source/regex/regex_bench.cpp
@@ -1,14 +1,9 @@
 #include <regex>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <algorithm>
-#include <iostream>
 #include <re2/re2.h>
 
 #include "benchmark/benchmark.h"
 
-static void re2_bench(benchmark::State& state)
+static void re2_partial_bench(benchmark::State& state)
 {
     for( auto _ : state) {
         RE2::PartialMatch("protocol://server/path","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
@@ -18,9 +13,9 @@ static void re2_bench(benchmark::State& state)
     }
 }
 
-BENCHMARK(re2_bench);
+BENCHMARK(re2_partial_bench);
 
-static void std_bench(benchmark::State& state)
+static void std_partial_bench(benchmark::State& state)
 {
     for( auto _ : state) {
         std::regex_search("protocol://server/path",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
@@ -30,4 +25,28 @@ static void std_bench(benchmark::State& state)
     }
 }
 
-BENCHMARK(std_bench);
+BENCHMARK(std_partial_bench);
+
+static void re2_full_bench(benchmark::State& state)
+{
+    for( auto _ : state) {
+        RE2::FullMatch("protocol://server/path","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
+        RE2::FullMatch("client@wazuh.com","([^ @]+)@([^ @]+)");
+        RE2::FullMatch("23/11/2015","([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)");
+        RE2::FullMatch("client@wazuh.com","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)");
+    }
+}
+
+BENCHMARK(re2_full_bench);
+
+static void std_full_bench(benchmark::State& state)
+{
+    for( auto _ : state) {
+        std::regex_match("protocol://server/path",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
+        std::regex_match("client@wazuh.com",std::regex("([^ @]+)@([^ @]+)"));
+        std::regex_match("23/11/2015",std::regex("([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)"));
+        std::regex_match("client@wazuh.com",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)"));
+    }
+}
+
+BENCHMARK(std_full_bench);
