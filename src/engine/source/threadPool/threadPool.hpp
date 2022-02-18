d917c49e491f06eb66721474d191ef414a7e3b5d
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-02-18 08:20:27 +0100

    Implemented definitions in cpp and fixed CMakes

--- src/engine/source/threadPool/threadPool.hpp
+++ src/engine/source/threadPool/threadPool.hpp
@@ -10,17 +10,13 @@
 #ifndef _THREAD_POOL_HPP_
 #define _THREAD_POOL_HPP_
 
+#include <chrono>
 #include <rxcpp/rx.hpp>
 
 namespace threadpool
 {
 
-using rxcpp::composite_subscription;
-using rxcpp::schedulers::scheduler_interface;
-using rxcpp::schedulers::thread_factory;
-using rxcpp::schedulers::worker;
-
-struct threadPool : public scheduler_interface
+struct threadPool : public rxcpp::schedulers::scheduler_interface
 {
 private:
     typedef threadPool this_type;
@@ -36,73 +32,40 @@ private:
 
         typedef queue_item_time::item_type item_type;
 
-        composite_subscription lifetime;
-        worker controller;
-        std::shared_ptr<const scheduler_interface> alive;
+        rxcpp::composite_subscription lifetime;
+        rxcpp::schedulers::worker controller;
+        std::shared_ptr<const rxcpp::schedulers::scheduler_interface> alive;
 
     public:
-        virtual ~loop_worker()
-        {
-        }
+        virtual ~loop_worker();
 
-        loop_worker(composite_subscription cs, worker w, std::shared_ptr<const scheduler_interface> alive)
-            : lifetime(cs), controller(w), alive(alive)
-        {
-            auto token = controller.add(cs);
-            cs.add([token, w]() { w.remove(token); });
-        }
+        loop_worker(rxcpp::composite_subscription cs, rxcpp::schedulers::worker w,
+                    std::shared_ptr<const rxcpp::schedulers::scheduler_interface> alive);
 
-        virtual clock_type::time_point now() const
-        {
-            return clock_type::now();
-        }
+        virtual clock_type::time_point now() const;
 
-        virtual void schedule(const rxcpp::schedulers::schedulable & scbl) const
-        {
-            controller.schedule(lifetime, scbl.get_action());
-        }
+        virtual void schedule(const rxcpp::schedulers::schedulable & scbl) const;
 
-        virtual void schedule(clock_type::time_point when, const rxcpp::schedulers::schedulable & scbl) const
-        {
-            controller.schedule(when, lifetime, scbl.get_action());
-        }
+        virtual void schedule(clock_type::time_point when, const rxcpp::schedulers::schedulable & scbl) const;
     };
 
-    mutable thread_factory factory;
+    mutable rxcpp::schedulers::thread_factory factory;
     rxcpp::schedulers::scheduler newthread;
     mutable std::atomic<std::size_t> count;
-    composite_subscription loops_lifetime;
-    std::vector<worker> loops;
+    rxcpp::composite_subscription loops_lifetime;
+    std::vector<rxcpp::schedulers::worker> loops;
     int nThreads;
 
     void initThreads(void);
 
 public:
-    threadPool(int _nThreads = 1)
-        : nThreads(_nThreads), factory([](std::function<void()> start) { return std::thread(std::move(start)); }),
-          newthread(rxcpp::schedulers::make_new_thread()), count(0)
-    {
-        initThreads();
-    }
-    explicit threadPool(int _nThreads, thread_factory tf)
-        : nThreads(_nThreads), factory(tf), newthread(rxcpp::schedulers::make_new_thread(tf)), count(0)
-    {
-        initThreads();
-    }
-    virtual ~threadPool()
-    {
-        loops_lifetime.unsubscribe();
-    }
+    threadPool(int _nThreads = 1);
+    explicit threadPool(int _nThreads, rxcpp::schedulers::thread_factory tf);
+    virtual ~threadPool();
 
-    virtual clock_type::time_point now() const
-    {
-        return clock_type::now();
-    }
+    virtual clock_type::time_point now() const;
 
-    virtual worker create_worker(composite_subscription cs) const
-    {
-        return worker(cs, std::make_shared<loop_worker>(cs, loops[++count % loops.size()], this->shared_from_this()));
-    }
+    virtual rxcpp::schedulers::worker create_worker(rxcpp::composite_subscription cs) const;
 };
 
 } // namespace threadpool
---
+++ src/engine/test/source/threadPool/threadPoolTest.cpp
@@ -0,0 +1,9 @@
+#include <chrono>
+#include <gtest/gtest.h>
+#include <rxcpp/rx.hpp>
+#include "threadPool.hpp"
+
+TEST(SchedulerTest, test)
+{
+
+}
