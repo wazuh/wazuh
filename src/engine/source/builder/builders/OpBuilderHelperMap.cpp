b652660eade108650cbe028f16367f118319e658
Juan Cabrera <jcabre88@gmail.com>
2022-02-21 17:32:16 +0100

    Added opBuilderHelperIntCalc function

--- src/engine/source/builder/builders/OpBuilderHelperMap.cpp
+++ src/engine/source/builder/builders/OpBuilderHelperMap.cpp
@@ -9,10 +9,12 @@
 
 #include <algorithm>
 #include <string>
+#include <optional>
 
 #include "OpBuilderHelperMap.hpp"
 #include "stringUtils.hpp"
 
+using namespace std;
 namespace {
 
     using DocumentValue = builder::internals::types::DocumentValue;
@@ -21,7 +23,7 @@ namespace {
 /**
  * @brief Tranform the string in `field` path in the event `e` according to the
  * `op` definition and the `value` or the `refValue`
- * 
+ *
  * @param field The field path to transform
  * @param op The operator to use:
  * - `u`: Upper case
@@ -104,8 +106,6 @@ namespace builder::internals::builders
 //*************************************************
 //*           String tranform                     *
 //*************************************************
-
-
 
 // <field>: +s_up/<str>|$<ref>
 types::Lifter opBuilderHelperStringUP(const types::DocumentValue & def)
@@ -305,6 +305,135 @@ types::Lifter opBuilderHelperStringTrim(const types::DocumentValue & def)
                     return e;
                 }
 
+                return e;
+            });
+    };
+}
+
+//*************************************************
+//*           Int tranform                        *
+//*************************************************
+
+types::Lifter opBuilderHelperIntCalc(const types::DocumentValue & def)
+{
+    // Get field
+    std::string field = def.MemberBegin()->name.GetString();
+    std::string rawValue = def.MemberBegin()->value.GetString();
+
+    std::vector<std::string> parameters = utils::string::split(rawValue, '/');
+
+    if (parameters.size() != 3)
+    {
+        throw std::runtime_error("Invalid parameters");
+    }
+
+    std::optional<std::string> refValue;
+    std::optional<int> value;
+
+    // Take operator parameter
+    if (parameters[1].size() != 1)
+    {
+        throw std::runtime_error("Invalid operator");
+    }
+
+    char op = parameters[1][0];
+
+    switch (op)
+    {
+        case '+':
+        case '-':
+        case '*':
+        case '%':
+            break;
+        default:
+            throw std::runtime_error("Invalid operator");
+    }
+
+    if (parameters[2][0] == '$')
+    {
+        // Check case `+i_calc/op/$`
+        refValue = parameters[2].substr(1, std::string::npos);
+    }
+    else
+    {
+        value = std::stoi(parameters[2]);
+    }
+
+    // Return Lifter
+    return [op, refValue, value, field](types::Observable o)
+    {
+        // Append rxcpp operation
+        return o.map(
+            [=](types::Event e)
+            {
+                if (e.exists("/" + field))
+                {
+                    auto fieldToCheck = e.getObject().FindMember(field.c_str());
+                    if (fieldToCheck->value.IsInt())
+                    {
+                        if (value.has_value())
+                        {
+                            switch (op)
+                            {
+                                case '+':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() + value.value());
+                                    return e;
+                                case '-':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() - value.value());
+                                    return e;
+                                case '*':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() * value.value());
+                                    return e;
+                                case '%': // TODO: Check if this is correct
+                                    if (value.value() != 0)
+                                    {
+                                        fieldToCheck->value.SetInt(
+                                            fieldToCheck->value.GetInt() / value.value());
+                                    }
+                                    return e;
+                                default:
+                                    throw std::runtime_error("Invalid operator");
+                            }
+                            return e;
+                        }
+                        auto refValueToCheck =
+                            e.getObject().FindMember(refValue.value().c_str());
+                        if (refValueToCheck->value.IsInt())
+                        {
+                            switch (op)
+                            {
+                                case '+':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() +
+                                        refValueToCheck->value.GetInt());
+                                    return e;
+                                case '-':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() -
+                                        refValueToCheck->value.GetInt());
+                                    return e;
+                                case '*':
+                                    fieldToCheck->value.SetInt(
+                                        fieldToCheck->value.GetInt() *
+                                        refValueToCheck->value.GetInt());
+                                    return e;
+                                case '%': // TODO: divide by zero
+                                    if (refValueToCheck->value.GetInt() != 0)
+                                    {
+                                        fieldToCheck->value.SetInt(
+                                            fieldToCheck->value.GetInt() /
+                                            refValueToCheck->value.GetInt());
+                                    }
+                                    return e;
+                                default:
+                                    throw std::runtime_error("Invalid operator");
+                            }
+                        }
+                    }
+                }
                 return e;
             });
     };
