c81ae1f20454de97efc8f34b990789800836428f
miguemedina11 <miguemedina2000@gmail.com>
2022-02-21 13:18:09 +0000

    Style Changes on cpp and hpp files

--- benchmark/source/regex/regex_bench.cpp
+++ benchmark/source/regex/regex_bench.cpp
@@ -1,51 +1,57 @@
-#include <regex>
 #include <re2/re2.h>
+#include <regex>
 
 #include "benchmark/benchmark.h"
 
-static void re2_partial_bench(benchmark::State& state)
+static void re2_partial_bench(benchmark::State & state)
 {
-    for( auto _ : state) {
-        RE2::PartialMatch("protocol://server/path","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
-        RE2::PartialMatch("client@wazuh.com","([^ @]+)@([^ @]+)");
-        RE2::PartialMatch("23/11/2015","([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)");
-        RE2::PartialMatch("client@wazuh.com","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)");
+    for (auto _ : state)
+    {
+        RE2::PartialMatch("protocol://server/path", "([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
+        RE2::PartialMatch("client@wazuh.com", "([^ @]+)@([^ @]+)");
+        RE2::PartialMatch("23/11/2015", "([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)");
+        RE2::PartialMatch("client@wazuh.com", "([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)");
     }
 }
 
 BENCHMARK(re2_partial_bench);
 
-static void std_partial_bench(benchmark::State& state)
+static void std_partial_bench(benchmark::State & state)
 {
-    for( auto _ : state) {
-        std::regex_search("protocol://server/path",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
-        std::regex_search("client@wazuh.com",std::regex("([^ @]+)@([^ @]+)"));
-        std::regex_search("23/11/2015",std::regex("([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)"));
-        std::regex_search("client@wazuh.com",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)"));
+    for (auto _ : state)
+    {
+        std::regex_search("protocol://server/path", std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
+        std::regex_search("client@wazuh.com", std::regex("([^ @]+)@([^ @]+)"));
+        std::regex_search("23/11/2015", std::regex("([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)"));
+        std::regex_search("client@wazuh.com",
+                          std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)"));
     }
 }
 
 BENCHMARK(std_partial_bench);
 
-static void re2_full_bench(benchmark::State& state)
+static void re2_full_bench(benchmark::State & state)
 {
-    for( auto _ : state) {
-        RE2::FullMatch("protocol://server/path","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
-        RE2::FullMatch("client@wazuh.com","([^ @]+)@([^ @]+)");
-        RE2::FullMatch("23/11/2015","([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)");
-        RE2::FullMatch("client@wazuh.com","([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)");
+    for (auto _ : state)
+    {
+        RE2::FullMatch("protocol://server/path", "([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?");
+        RE2::FullMatch("client@wazuh.com", "([^ @]+)@([^ @]+)");
+        RE2::FullMatch("23/11/2015", "([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)");
+        RE2::FullMatch("client@wazuh.com", "([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)");
     }
 }
 
 BENCHMARK(re2_full_bench);
 
-static void std_full_bench(benchmark::State& state)
+static void std_full_bench(benchmark::State & state)
 {
-    for( auto _ : state) {
-        std::regex_match("protocol://server/path",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
-        std::regex_match("client@wazuh.com",std::regex("([^ @]+)@([^ @]+)"));
-        std::regex_match("23/11/2015",std::regex("([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)"));
-        std::regex_match("client@wazuh.com",std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)"));
+    for (auto _ : state)
+    {
+        std::regex_match("protocol://server/path", std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?"));
+        std::regex_match("client@wazuh.com", std::regex("([^ @]+)@([^ @]+)"));
+        std::regex_match("23/11/2015", std::regex("([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]([0-9][0-9])?)"));
+        std::regex_match("client@wazuh.com",
+                         std::regex("([a-zA-Z][a-zA-Z0-9]*)://([^ /]+)(/[^ ]*)?|([^ @]+)@([^ @]+)"));
     }
 }
 
