c174571ac5c1c190fe53a0d0d89721fb1e4260fd
Julian Morales <jmorales@unc.edu.ar>
2022-02-22 22:26:52 +0000

    String le and lt helper functions added

---
+++ src/engine/test/source/builder/builders/opBuilderHelperStringLt_test.cpp
@@ -0,0 +1,177 @@
+/* Copyright (C) 2015-2022, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+
+#include <gtest/gtest.h>
+#include <testUtils.hpp>
+#include <vector>
+
+#include "OpBuilderHelperFilter.hpp"
+
+using namespace builder::internals::builders;
+
+// Build ok
+TEST(opBuilderHelperString_lt, Builds)
+{
+    Document doc{R"({
+        "check":
+            {"field2check": "+s_lt/abcd"}
+    })"};
+    ASSERT_NO_THROW(opBuilderHelperString_lt(*doc.get("/check")));
+}
+
+// Build incorrect number of arguments
+TEST(opBuilderHelperString_lt, BuildsIncorrectNumberOfArguments)
+{
+    Document doc{R"({
+        "check":
+            {"field2check": "+s_lt/test_value/test_value2"}
+    })"};
+    ASSERT_THROW(opBuilderHelperString_lt(*doc.get("/check")), std::runtime_error);
+}
+
+// Test ok: static values
+TEST(opBuilderHelperString_lt, staticStringOk)
+{
+    Document doc{R"({
+        "check":
+            {"field2check": "+s_lt/ABCD"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            // less
+            s.on_next(Event{R"(
+                {"field2check":"ABC"}
+            )"});
+            // Equal
+            s.on_next(Event{R"(
+                {"field2check":"ABCD"}
+            )"});
+            // Greater
+            s.on_next(Event{R"(
+                {"field2check":"ABCDE"}
+            )"});
+
+            // Greater with different case
+            s.on_next(Event{R"(
+                {"field2check":"BBBB"}
+            )"});
+            // Less with different case
+            s.on_next(Event{R"(
+                {"field2check":"AABCD"}
+            )"});
+
+            // lower case are greater
+            s.on_next(Event{R"(
+                {"field2check":"abc"}
+            )"});
+            s.on_next(Event{R"(
+                {"field2check":"abcd"}
+            )"});
+            s.on_next(Event{R"(
+                {"field2check":"abcde"}
+            )"});
+
+            // Other fields will be ignored
+            s.on_next(Event{R"(
+                {"otherfield":"abcd"}
+            )"});
+            s.on_next(Event{R"(
+                {"otherfield":"abcd"}
+            )"});
+            s.on_completed();
+        });
+
+    Lifter lift = opBuilderHelperString_lt(*doc.get("/check"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) {expected.push_back(e);});
+    ASSERT_EQ(expected.size(), 2);
+    ASSERT_STREQ(expected[0].get("/field2check")->GetString(), "ABC");
+    ASSERT_STREQ(expected[1].get("/field2check")->GetString(), "AABCD");
+}
+
+// Test ok: static values (numbers, compare as string)
+TEST(opBuilderHelperString_lt, staticNumberOk)
+{
+
+    Document doc{R"({
+        "check":
+            {"field2check": "+s_lt/50"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            // Less
+            s.on_next(Event{R"(
+                {"field2check":"499"}
+            )"});
+            // Equal
+            s.on_next(Event{R"(
+                {"field2check":"50"}
+            )"});
+            // Greater
+            s.on_next(Event{R"(
+                {"otherfield":"51"}
+            )"});
+            s.on_completed();
+        });
+
+    Lifter lift = opBuilderHelperString_lt(*doc.get("/check"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_STREQ(expected[0].get("/field2check")->GetString(), "499");
+}
+
+// Test ok: dynamic values (string)
+TEST(opBuilderHelperString_lt, dynamicsStringOk) {
+    Document doc{R"({
+        "check":
+            {"field2check": "+s_lt/$ref_key"}
+    })"};
+
+    Observable input = observable<>::create<Event>(
+        [=](auto s)
+        {
+            // Less
+            s.on_next(Event{R"(
+                {
+                    "field2check":"ABCD",
+                    "ref_key":"abcd"
+                }
+            )"});
+            // Equal
+            s.on_next(Event{R"(
+                {
+                    "field2check":"ABCD",
+                    "ref_key":"ABCD"
+                }
+            )"});
+            // GREATER
+            s.on_next(Event{R"(
+                {
+                    "otherfield":"abcd",
+                    "ref_key":"ABCD"
+                }
+            )"});
+            s.on_completed();
+        });
+
+    Lifter lift = opBuilderHelperString_lt(*doc.get("/check"));
+    Observable output = lift(input);
+    vector<Event> expected;
+    output.subscribe([&](Event e) { expected.push_back(e); });
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_STREQ(expected[0].get("/field2check")->GetString(), "ABCD");
+}
