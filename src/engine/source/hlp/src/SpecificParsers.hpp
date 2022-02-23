71e1c0877dda5f596814fc91a819fbdec2277b5e
Santiago Belluzzo <santiago.belluzzo@wazuh.com>
2022-02-23 18:01:54 -0300

    HLP - Timestamp parser refactor

--- src/engine/source/hlp/src/SpecificParsers.hpp
+++ src/engine/source/hlp/src/SpecificParsers.hpp
@@ -2,6 +2,7 @@
 #define _FILE_PATH_PARSER_H
 
 #include <string>
+#include <vector>
 
 struct URLResult{
     std::string domain;   //"url.domain": "keyword",
@@ -15,28 +16,6 @@ struct URLResult{
     std::string username; //"url.username": "keyword",
 };
 
-enum class TimeStampFormat {
-    ANSIC,
-    Layout,
-    UnixDate,
-    ANSICM,
-    APACHE,
-    RubyDate,
-    RFC822,
-    RFC822Z,
-    RFC850,
-    RFC1123Z,
-    RFC1123,
-    RFC3339Nano,
-    RFC3339,
-    StampNano,
-    StampMicro,
-    StampMilli,
-    Stamp,
-    Kitchen,
-    NONE,
-};
-
 struct TimeStampResult {
     std::string year;
     std::string month;
@@ -44,6 +23,7 @@ struct TimeStampResult {
     std::string hour;
     std::string minutes;
     std::string seconds;
+    std::string timezone;
 };
 
 bool parseFilePath(const char **it, char endToken);
@@ -58,7 +38,7 @@ std::string parseMap(const char **it, char endToken, std::vector<std::string> co
 
 std::string parseIPaddress(const char **it, char endToken);
 
-bool parseTimeStamp(const char **it, char endToken, TimeStampResult &tsr);
+bool parseTimeStamp(const char **it, std::vector<std::string> const& opts, char endToken, TimeStampResult &tsr);
 
 bool parseURL(const char **it, char endToken, URLResult &result);
 
