dc2f72a3b1e8d59a16ecfbf4f8e396735452ee98
Juan Cabrera <jcabre88@gmail.com>
2022-02-23 14:20:46 +0100

    Added less than function helper for integer

--- test/source/builder/builders/opBuilderHelperIntLessThan_test.cpp
+++ test/source/builder/builders/opBuilderHelperIntLessThan_test.cpp
@@ -16,38 +16,38 @@
 
 using namespace builder::internals::builders;
 
-TEST(opBuilderHelperIntEqual, Builds)
+TEST(opBuilderHelperIntLessThan, Builds)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/10"}
+            {"field_test": "+i_lt/10"}
     })"};
-    ASSERT_NO_THROW(opBuilderHelperIntEqual(*doc.get("/check")));
+    ASSERT_NO_THROW(opBuilderHelperIntLessThan(*doc.get("/check")));
 }
 
-TEST(opBuilderHelperIntEqual, Builds_error_bad_parameter)
+TEST(opBuilderHelperIntLessThan, Builds_error_bad_parameter)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/test"}
+            {"field_test": "+i_lt/test"}
     })"};
-    ASSERT_THROW(opBuilderHelperIntEqual(*doc.get("/check")), std::invalid_argument);
+    ASSERT_THROW(opBuilderHelperIntLessThan(*doc.get("/check")), std::invalid_argument);
 }
 
-TEST(opBuilderHelperIntEqual, Builds_error_more_parameters)
+TEST(opBuilderHelperIntLessThan, Builds_error_more_parameters)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/10/10"}
+            {"field_test": "+i_lt/10/10"}
     })"};
-    ASSERT_THROW(opBuilderHelperIntEqual(*doc.get("/check")), std::runtime_error);
+    ASSERT_THROW(opBuilderHelperIntLessThan(*doc.get("/check")), std::runtime_error);
 }
 
-TEST(opBuilderHelperIntEqual, Exec_equal_ok)
+TEST(opBuilderHelperIntLessThan, Exec_less_than_ok)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/10"}
+            {"field_test": "+i_lt/10"}
     })"};
 
     Observable input = observable<>::create<Event>(
@@ -67,21 +67,20 @@ TEST(opBuilderHelperIntEqual, Exec_equal_ok)
             )"});
             s.on_completed();
         });
-    Lifter lift = opBuilderHelperIntEqual(*doc.get("/check"));
+    Lifter lift = opBuilderHelperIntLessThan(*doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 2);
-    ASSERT_EQ(expected[0].get("/field_test")->GetInt(),10);
-    ASSERT_EQ(expected[1].get("/field_test")->GetInt(),10);
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_LT(expected[0].get("/field_test")->GetInt(),10);
 
 }
 
-TEST(opBuilderHelperIntEqual, Exec_equal_true)
+TEST(opBuilderHelperIntLessThan, Exec_less_than_true)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/10"}
+            {"field_test": "+i_lt/10"}
     })"};
 
     Observable input = observable<>::create<Event>(
@@ -101,28 +100,27 @@ TEST(opBuilderHelperIntEqual, Exec_equal_true)
             )"});
             s.on_completed();
         });
-    Lifter lift = opBuilderHelperIntEqual(*doc.get("/check"));
+    Lifter lift = opBuilderHelperIntLessThan(*doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 2);
-    ASSERT_EQ(expected[0].get("/field_test")->GetInt(),10);
-    ASSERT_EQ(expected[1].get("/field_test")->GetInt(),10);
+    ASSERT_EQ(expected.size(), 1);
+    ASSERT_LT(expected[0].get("/field_test")->GetInt(), 10);
 
 }
 
-TEST(opBuilderHelperIntEqual, Exec_equal_false)
+TEST(opBuilderHelperIntLessThan, Exec_less_than_false)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/10"}
+            {"field_test": "+i_lt/10"}
     })"};
 
     Observable input = observable<>::create<Event>(
         [=](auto s)
         {
             s.on_next(Event{R"(
-                {"field_test":9}
+                {"field_test":20}
             )"});
             s.on_next(Event{R"(
                 {"field_test2":10}
@@ -135,7 +133,7 @@ TEST(opBuilderHelperIntEqual, Exec_equal_false)
             )"});
             s.on_completed();
         });
-    Lifter lift = opBuilderHelperIntEqual(*doc.get("/check"));
+    Lifter lift = opBuilderHelperIntLessThan(*doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
@@ -143,43 +141,43 @@ TEST(opBuilderHelperIntEqual, Exec_equal_false)
 
 }
 
-TEST(opBuilderHelperIntEqual, Exec_equal_ref_true)
+TEST(opBuilderHelperIntLessThan, Exec_less_than_ref_true)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/$field_src"}
+            {"field_test": "+i_lt/$field_src"}
     })"};
 
     Observable input = observable<>::create<Event>(
         [=](auto s)
         {
             s.on_next(Event{R"(
-                {"field_test": 9,"field_src": 10}
+                {"field_test": 10,"field_src": 10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src":10}
+                {"field_test":9,"field_src":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src":10}
+                {"field_test":9,"field_src":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src":"10"}
+                {"field_test":9,"field_src":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test":"10","field_src":10}
+                {"field_test":"9","field_src":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":"10","field_src":"10"}
+                {"field_test":"9","field_src":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test":11,"field_src":"10"}
+                {"field_test":9,"field_src":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src":"test"}
+                {"field_test":9,"field_src":"test"}
             )"});
             s.on_completed();
         });
-    Lifter lift = opBuilderHelperIntEqual(*doc.get("/check"));
+    Lifter lift = opBuilderHelperIntLessThan(*doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
@@ -189,18 +187,18 @@ TEST(opBuilderHelperIntEqual, Exec_equal_ref_true)
     };
 
     ASSERT_EQ(expected.size(), 2);
-    ASSERT_EQ(expected[0].get("/field_test")->GetInt(),
+    ASSERT_LT(expected[0].get("/field_test")->GetInt(),
               expected[0].get("/field_src")->GetInt());
-    ASSERT_EQ(expected[1].get("/field_test")->GetInt(),
-              expected[0].get("/field_src")->GetInt());
+    ASSERT_LT(expected[1].get("/field_test")->GetInt(),
+              expected[1].get("/field_src")->GetInt());
 
 }
 
-TEST(opBuilderHelperIntEqual, Exec_equal_ref_false)
+TEST(opBuilderHelperIntLessThan, Exec_less_than_ref_false)
 {
     Document doc{R"({
         "check":
-            {"field_test": "+i_eq/$field_src"}
+            {"field_test": "+i_lt/$field_src"}
     })"};
 
     Observable input = observable<>::create<Event>(
@@ -210,29 +208,29 @@ TEST(opBuilderHelperIntEqual, Exec_equal_ref_false)
                 {"field_test2":9,"field_src":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src3":10}
+                {"field_test":9,"field_src3":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test":10,"field_src4":10}
+                {"field_test":9,"field_src4":10}
             )"});
             s.on_next(Event{R"(
-                {"field_test5":10,"field_src":"10"}
+                {"field_test5":9,"field_src":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test6":"10","field_src2":10}
+                {"field_test6":"9","field_src2":10}
             )"});
             s.on_next(Event{R"(
-                {"field_":"10","field_src":"10"}
+                {"field_":"9","field_src":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test":11,"field_src2":"10"}
+                {"field_test":9,"field_src2":"10"}
             )"});
             s.on_next(Event{R"(
-                {"field_test2":10,"field_src2":"test"}
+                {"field_test2":9,"field_src2":"test"}
             )"});
             s.on_completed();
         });
-    Lifter lift = opBuilderHelperIntEqual(*doc.get("/check"));
+    Lifter lift = opBuilderHelperIntLessThan(*doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
