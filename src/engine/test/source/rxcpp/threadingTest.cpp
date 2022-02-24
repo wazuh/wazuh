b08f6c9719a8f6dcbb1ec6a715e25324ee2308a4
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-02-24 00:34:58 +0000

    Code improved

--- src/engine/test/source/rxcpp/threadingTest.cpp
+++ src/engine/test/source/rxcpp/threadingTest.cpp
@@ -21,7 +21,7 @@ using threadpool::ThreadPool;
 using rxcpp::composite_subscription;
 using rxcpp::observable;
 
-#define WAIT_FOR_WORKERS_TIME_MS    50
+#define WAIT_FOR_WORKERS_TIME_MS 50
 
 TEST(RxcppThreading, testSchedulerCustomFactoryWithPrints)
 {
@@ -277,10 +277,12 @@ TEST(RxcppThreading, testWithFactory)
 using namespace rxcpp::operators;
 TEST(RxcppThreading, SubscriberSubjectx100000)
 {
-    struct event{
+    struct event
+    {
         std::thread::id pipeId;
         int e;
-        void check(std::thread::id other){
+        void check(std::thread::id other)
+        {
             std::stringstream s1;
             std::stringstream s2;
             s1 << this->pipeId;
@@ -291,135 +293,137 @@ TEST(RxcppThreading, SubscriberSubjectx100000)
 
     printsafe("START MAIN");
 
-
     // SETTINGS
-    int P{5};
-    static std::map<std::thread::id, int> threads;
-    auto sc = rxcpp::schedulers::make_scheduler<ThreadPool>(P, [](auto f){
-        auto t = std::thread{f};
-        threads[t.get_id()] = 0;
-        return t;
-    });
+    int nThreads{5};
+    const int N_OPS{1000};
+    const int N_EVTS{2550};
+    const std::string inputString{"asdfgh"};
 
-    static auto scW = rxcpp::observe_on_one_worker(sc);
-    int N_OPS{1000};
-    int N_EVTS{250000};
-    std::string inputString{"asdfgh"};
+    static std::map<std::thread::id, int> threadMap;
+
     int inputInt{0};
-    for (auto c : inputString){
+    for (auto c : inputString)
+    {
         inputInt += int(c);
     }
-    int expectedProcessed{N_OPS+inputInt};
+    int expectedProcessed{N_OPS + inputInt};
 
     // Fake ProtocolHandler + Enviroment + Router
     rxcpp::subjects::subject<std::string> pipeline;
     auto pipelineIn = pipeline.get_subscriber();
 
     std::atomic<int> total{0};
-    auto pipeBuilder = [&total, N_OPS, N_EVTS, expectedProcessed](rxcpp::observable<std::string> input) -> rxcpp::composite_subscription {
+    auto pipeBuilder = [&total, &N_OPS, &N_EVTS,
+                        expectedProcessed](rxcpp::observable<std::string> input) -> rxcpp::composite_subscription
+    {
         printsafe("PipeBuilder builds");
-        rxcpp::observable<event> innerPipe = input
-            | map([](std::string e){
-                event evt;
-                int sum = 0;
-                for (char c : e){
-                    sum += int(c);
-                }
-                evt.pipeId = std::this_thread::get_id();
-                evt.e = sum;
-                return evt;
-            });
 
-        for (auto i = 0; i < N_OPS; ++i){
-            innerPipe = innerPipe.map(
-                [](event e) -> event {
-                    e.check(std::this_thread::get_id());
-                    e.e += 1;
-                    return e;
-                }
-            );
+        auto mapFunctionAddOnce = [](std::string e)
+        {
+            event evt;
+            int sum = 0;
+            for (char c : e)
+            {
+                sum += int(c);
+            }
+            evt.pipeId = std::this_thread::get_id();
+            evt.e = sum;
+            return evt;
+        };
+        rxcpp::observable<event> innerPipe = input | map(mapFunctionAddOnce);
+
+        auto mapFunctionAddMany = [](event e) -> event
+        {
+            e.check(std::this_thread::get_id());
+            e.e += 1;
+            return e;
+        };
+        for (auto i = 0; i < N_OPS; ++i)
+        {
+            innerPipe = innerPipe | map(mapFunctionAddMany);
         }
 
-        return innerPipe
-            .subscribe(
-                [&total, expectedProcessed](event e){
-                    ++total;
-                    printsafe("Pipeline processed(" + std::to_string(e.e) + ") iter: " + std::to_string(total));
-                    threads[std::this_thread::get_id()]++;
-                    //ASSERT_EQ(e, expectedProcessed);
-                    ASSERT_EQ(e.pipeId, std::this_thread::get_id());
-                },
-                [](auto eptr){
-                    printsafe("Pipeline got error: " + rxcpp::util::what(eptr));
-                    FAIL();
-                },
-                [&total, N_EVTS](){
-                    printsafe("Pipeline completed: " + std::to_string(total));
-                    ASSERT_EQ(total, N_EVTS);
-                }
-            );
+        return innerPipe.subscribe(
+            [&total, expectedProcessed](event e)
+            {
+                ++total;
+                printsafe("Pipeline processed(" + std::to_string(e.e) + ") iter: " + std::to_string(total));
+                threadMap[std::this_thread::get_id()]++;
+
+                ASSERT_EQ(e.pipeId, std::this_thread::get_id());
+            },
+            [](auto eptr)
+            {
+                printsafe("Pipeline got error: " + rxcpp::util::what(eptr));
+                FAIL();
+            },
+            [&total, &N_EVTS]()
+            {
+                printsafe("Pipeline completed: " + std::to_string(total));
+                ASSERT_EQ(total, N_EVTS);
+            });
     };
 
     auto pipelineSubscription = pipeBuilder(pipeline.get_observable());
 
     // Fake Server | input
-    auto fakeServer = rxcpp::observable<>::create<rxcpp::observable<std::string>>([=](auto s){
-        for(auto i = 0; i < N_EVTS; ++i) {
-            //printsafe("Producer sent observable<" + input_line + ">");
-            s.on_next(rxcpp::observable<>::just<std::string>(inputString));
-        }
-        printsafe("Producer completed");
-        s.on_completed();
-    });
-
-    // Main
-    std::atomic<int> control{0};
-    fakeServer
-        .subscribe(
-            [=, &control](rxcpp::observable<std::string> o){
-                //printsafe("Control subscriber got o");
-                ++control;
-                o
-                    .observe_on(scW)
-                    .subscribe([=](auto event){
-                        pipelineIn.on_next(event);
-                    },
-                    [=](auto eptr){
-                        printsafe("inner got error: " + rxcpp::util::what(eptr));
-                        //pipelineIn.on_error(eptr);
-                    },
-                    [=](){
-                        // pipelineIn.on_completed();
-                    });
-                    // .tap([=](std::string e){
-                    //     pipelineIn.on_next(e);
-                    // })
-                    // .subscribe();
-            },
-            [=](auto eptr){
-                printsafe("Control subscriber got error: " + rxcpp::util::what(eptr));
-                pipelineIn.on_error(eptr);
-                // wait until is unsubscribed
-            },
-            [=, &control](){
-                printsafe("Control subscriber completed: " + std::to_string(control));
-                // rxcpp::observable<>::empty<std::string>()
-                //     .observe_on(rxcpp::observe_on_one_worker(sc))
-                //     .tap([](std::string event){},[](auto eptr){},[=](){
-                //         pipelineIn.on_completed();
-                //     })
-                //     .subscribe();
-
-                // wait until is unsubscribed
-                ASSERT_EQ(N_EVTS, control);
+    auto fakeServer = rxcpp::observable<>::create<rxcpp::observable<std::string>>(
+        [&inputString](auto s)
+        {
+            for (auto i = 0; i < N_EVTS; ++i)
+            {
+                s.on_next(rxcpp::observable<>::just<std::string>(inputString));
             }
-        );
-    while(total != N_EVTS){
+            printsafe("Producer completed");
+            s.on_completed();
+        });
+
+    auto threadFactory = [](auto f)
+    {
+        auto t = std::thread{f};
+        threadMap[t.get_id()] = 0;
+        return t;
+    };
+    auto sc = rxcpp::schedulers::make_scheduler<ThreadPool>(nThreads, threadFactory);
+    static auto scW = rxcpp::observe_on_one_worker(sc);
+
+    std::atomic<int> eventsCounted{0};
+
+    fakeServer.subscribe(
+        [pipelineIn, &eventsCounted](rxcpp::observable<std::string> o)
+        {
+            ++eventsCounted;
+            o.observe_on(scW).subscribe([pipelineIn](auto event) { pipelineIn.on_next(event); },
+                                        [](auto eptr) { printsafe("inner got error: " + rxcpp::util::what(eptr)); },
+                                        []() {});
+        },
+        [pipelineIn](auto eptr)
+        {
+            printsafe("Control subscriber got error: " + rxcpp::util::what(eptr));
+            pipelineIn.on_error(eptr);
+            // TODO: is the following message a TODO?
+            // wait until is unsubscribed
+        },
+        [&N_EVTS, &eventsCounted]()
+        {
+            printsafe("Control subscriber completed: " + std::to_string(eventsCounted));
+
+            // TODO: is the following message a TODO?
+            // wait until is unsubscribed
+            ASSERT_EQ(N_EVTS, eventsCounted);
+        });
+
+    while (total != N_EVTS)
+    {
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
+
     pipelineIn.on_completed();
-    for (auto i : threads){
-        ASSERT_EQ(N_EVTS/P, i.second);
+
+    for (auto thread : threadMap)
+    {
+        ASSERT_EQ(N_EVTS / nThreads, thread.second);
     }
+
     printsafe("END MAIN");
 }
