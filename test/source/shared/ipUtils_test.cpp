a4dd530672e2c941effb8469925a8dbec921eacc
Julian Morales <jmorales@unc.edu.ar>
2022-03-03 14:47:37 +0000

    Add ipUtils test

--- test/source/shared/ipUtils_test.cpp
+++ test/source/shared/ipUtils_test.cpp
@@ -10,3 +10,36 @@
 #include <gtest/gtest.h>
 
 #include "ipUtils.hpp"
+
+TEST(IPv4ToUInt, Invalid_format)
+{
+    EXPECT_THROW(utils::ip::IPv4ToUInt(""), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.2"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.2.3"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.2.3.4."), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.2.3.255."), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.2.3.4.5"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt(" 1.1.1.1 "), std::invalid_argument);
+}
+
+TEST(IPv4ToUInt, Invalid_range)
+{
+    EXPECT_THROW(utils::ip::IPv4ToUInt("-1.1.1.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.-1.1.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.1.-1.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.1.1.-1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("256.1.1.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.256.1.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.1.256.1"), std::invalid_argument);
+    EXPECT_THROW(utils::ip::IPv4ToUInt("1.1.1.256"), std::invalid_argument);
+
+}
+
+TEST(IPv4ToUInt, Valid_range)
+{
+    EXPECT_EQ(utils::ip::IPv4ToUInt("0.0.0.0"), 0x0);
+    EXPECT_EQ(utils::ip::IPv4ToUInt("127.0.0.1"), 0x7F'00'00'01);
+    EXPECT_EQ(utils::ip::IPv4ToUInt("192.168.0.1"), 0b11000000'10101000'00000000'00000001);
+    EXPECT_EQ(utils::ip::IPv4ToUInt("255.255.255.255"), 0xFFFFFFFF);
+}
