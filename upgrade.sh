4e18a55daca28093ac37cdcedc9f588555e9bbe3
MiguelCasaresRobles <miguel_27793@hotmail.com>
2022-03-13 14:35:32 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -16,4 +16,17 @@ if [ "X${WAZUH_VERSION}" = "X" ] ; then
     fi
 fi
 
+printf "\n#Toggles Logcollector to accept remote commands from the manager or not.\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+printf "\nlogcollector.remote_commands=1\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+printf "\n#Enable the execution of commands in policy files received from the manager (Files in etc/shared).\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+printf "\nsca.remote_commands=1\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+printf "\n#Toggles whether Command Module should accept commands defined in the shared configuration or not.\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+printf "\nwazuh_command.remote_commands=1\n" >> ${WAZUH_HOME}/etc/local_internal_options.conf
+
+
 (sleep 5 && chmod +x ${WAZUH_HOME}/var/upgrade/src/init/*.sh && ${WAZUH_HOME}/var/upgrade/src/init/pkg_installer.sh ${WAZUH_HOME} ${WAZUH_VERSION} && find ${WAZUH_HOME}/var/upgrade/* -not -name upgrade_result -delete) >/dev/null 2>&1 &
