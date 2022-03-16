6b05fc4e37a28ca9721971f99dd43198d25a0d0d
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-03-16 14:21:57 +0100

    Updated conditions and maps operation builders to new json

--- src/engine/test/source/builder/builders/opBuilderMapValue_test.cpp
+++ src/engine/test/source/builder/builders/opBuilderMapValue_test.cpp
@@ -59,9 +59,9 @@ TEST(opBuilderMapValue, BuildsOperates)
             )"));
             s.on_completed();
         });
-    Lifter lift1 = opBuilderMapValue(*doc.get("/normalize/0"));
-    Lifter lift2 = opBuilderMapValue(*doc.get("/normalize/1"));
-    Lifter lift3 = opBuilderMapValue(*doc.get("/normalize/2"));
+    Lifter lift1 = opBuilderMapValue(doc.get("/normalize/0"));
+    Lifter lift2 = opBuilderMapValue(doc.get("/normalize/1"));
+    Lifter lift3 = opBuilderMapValue(doc.get("/normalize/2"));
 
     Observable output = lift3(lift2(lift1(input)));
     vector<Event> expected;
@@ -69,9 +69,9 @@ TEST(opBuilderMapValue, BuildsOperates)
     ASSERT_EQ(expected.size(), 4);
     for (auto got : expected)
     {
-        ASSERT_STREQ(got->get("/mapped/string")->GetString(), "value");
-        ASSERT_EQ(got->get("/mapped/int")->GetInt(), 1);
-        ASSERT_TRUE(got->get("/mapped/bool")->GetBool());
+        ASSERT_STREQ(got->get("/mapped/string").GetString(), "value");
+        ASSERT_EQ(got->get("/mapped/int").GetInt(), 1);
+        ASSERT_TRUE(got->get("/mapped/bool").GetBool());
     }
 }
 
