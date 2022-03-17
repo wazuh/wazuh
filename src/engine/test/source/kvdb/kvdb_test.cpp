1cf468b65a582e727273c9ec0e5ad93d04c6a37c
LucioDonda <donda.lucio@gmail.com>
2022-03-17 11:03:54 -0300

    adding read and write to CF functions, making a generic proccess for all modifcations

--- src/engine/test/source/kvdb/kvdb_test.cpp
+++ src/engine/test/source/kvdb/kvdb_test.cpp
@@ -17,3 +17,27 @@ TEST(kvdbTests, column_family_creation_deletion)
     ret = DestroyKVDB();
     ASSERT_TRUE(ret);
 }
+
+TEST(kvdbTests, read_write_column_family)
+{
+    bool ret = CreateKVDB();
+    ASSERT_TRUE(ret);
+    ret = CreateColumnFamily("IP_BLACKLIST");
+    ASSERT_TRUE(ret);
+    ret = WriteToColumnFamily("IP_BLACKLIST", "someKey", "192.168.10.2");
+    ASSERT_TRUE(ret);
+    ret = WriteToColumnFamily("IP_BLACKLIST", "someKey", "127.0.0.1");
+    ASSERT_TRUE(ret);
+    std::string val;
+    ret = ReadToColumnFamily("IP_BLACKLIST", "someKey", val);
+    ASSERT_TRUE(ret);
+    ASSERT_EQ(val,"127.0.0.1");
+    ret = DeleteKeyInColumnFamily("IP_BLACKLIST", "someKey");
+    ASSERT_TRUE(ret);
+    ret = ReadToColumnFamily("IP_BLACKLIST", "someKey", val);
+    ASSERT_TRUE(!ret);
+    ret = DeleteColumnFamily("IP_BLACKLIST");
+    ASSERT_TRUE(ret);
+    ret = DestroyKVDB();
+    ASSERT_TRUE(ret);
+}
