eabc5ed9a780672fbcf2aa0495e959cf87fb3da3
LucioDonda <donda.lucio@gmail.com>
2022-03-17 11:03:54 -0300

    adding read and write to CF functions, making a generic proccess for all modifcations

--- src/engine/source/kvdb/include/kvdb/kvdb.hpp
+++ src/engine/source/kvdb/include/kvdb/kvdb.hpp
@@ -4,6 +4,14 @@
 #include <string>
 #include <vector>
 
+enum ACTION_ON_CF {
+    WRITE = 0,
+    READ,
+    DELETE,
+    READ_VALUE_COPY,
+    READ_WITHOUT_VALUE_COPY,
+};
+
 bool CreateKVDB();
 
 bool DestroyKVDB();
@@ -11,5 +19,14 @@ bool DestroyKVDB();
 bool CreateColumnFamily(std::string const column_family_name);
 
 bool DeleteColumnFamily(std::string const column_family_name);
+
+bool ReadToColumnFamily(std::string const &columnFamily,
+    std::string const &key, std::string &value);
+
+bool WriteToColumnFamily(std::string const &columnFamily,
+    std::string const &key, std::string const &value);
+
+bool DeleteKeyInColumnFamily(std::string const &columnFamily,
+    std::string const &key, std::string const &value = std::string());
 
 #endif // _KVDB_H
