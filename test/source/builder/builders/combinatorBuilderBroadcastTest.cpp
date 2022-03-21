3a199f17a579a05ab2567ab0ef963b6d2cec48ab
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-03-21 16:21:00 +0100

    Added todos and combinator placeholder tests

---
+++ test/source/builder/builders/combinatorBuilderBroadcastTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "combinatorBuilderBroadcast.hpp"
+#include "testUtils.hpp"
+
+TEST(CombinatorBuilderBroadcastTest, NotImplemented)
+{
+    // TODO: combinatorBuilderBroadcast tests
+    GTEST_SKIP();
+}
