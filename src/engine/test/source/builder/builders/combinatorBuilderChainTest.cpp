02352f9639f12fac995802904cfce4ecb234ac10
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-03-21 16:21:00 +0100

    Added todos and combinator placeholder tests

---
+++ src/engine/test/source/builder/builders/combinatorBuilderChainTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "combinatorBuilderChain.hpp"
+#include "testUtils.hpp"
+
+TEST(CombinatorBuilderChainTest, NotImplemented)
+{
+    // TODO: combinatorBuilderChain tests
+    GTEST_SKIP();
+}
