5ac275585b706bfdaacc27b4e753a0aa3e78d7f1
Santiago Belluzzo <santiago.belluzzo@wazuh.com>
2022-03-21 11:24:24 -0300

    Engine - fixing compilation with profile disabled

--- source/base/profile/profile.hpp
+++ source/base/profile/profile.hpp
@@ -1,14 +1,17 @@
-#include <Tracy.hpp>
-
+#ifndef _WPROFILE_H
+#define _WPROFILE_H
 #ifndef WAZUH_PROFILING_ENABLED
 
 #define WAZUH_TRACE_FUNCTION
+#define WAZUH_TRACE_FUNCTION_S(n)
 #define WAZUH_TRACE_SCOPE(name)
 
 #else
+#include <Tracy.hpp>
 
-#define WAZUH_TRACE_FUNCTION ZoneScoped;
+#define WAZUH_TRACE_FUNCTION      ZoneScoped;
 #define WAZUH_TRACE_FUNCTION_S(n) ZoneScopedS(n);
-#define WAZUH_TRACE_SCOPE(name) ZoneScopedN(name);
+#define WAZUH_TRACE_SCOPE(name)   ZoneScopedN(name);
 
+#endif
 #endif
