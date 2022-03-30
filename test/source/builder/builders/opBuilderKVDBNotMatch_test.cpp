0b16ecfd60f2379a8c23dc8de527d67448948b5a
palaciosjeremias <jeremias.palacios@wazuh.com>
2022-03-30 16:22:11 -0700

    Implement KVDB Function Helpers and UT

--- test/source/builder/builders/opBuilderKVDBNotMatch_test.cpp
+++ test/source/builder/builders/opBuilderKVDBNotMatch_test.cpp
@@ -18,15 +18,15 @@
 using namespace builder::internals::builders;
 
 namespace {
-class opBuilderKVDBMatchTest : public ::testing::Test {
+class opBuilderKVDBNotMatchTest : public ::testing::Test {
 
 protected:
     KVDBManager& kvdbManager = KVDBManager::getInstance();
 
-    opBuilderKVDBMatchTest() {
+    opBuilderKVDBNotMatchTest() {
     }
 
-    virtual ~opBuilderKVDBMatchTest() {
+    virtual ~opBuilderKVDBNotMatchTest() {
     }
 
     virtual void SetUp() {
@@ -39,38 +39,39 @@ protected:
 };
 
 // Build ok
-TEST_F(opBuilderKVDBMatchTest, Builds)
+TEST_F(opBuilderKVDBNotMatchTest, Builds)
 {
     Document doc{R"({
         "check":
-            {"field2match": "+kvdb_match/TEST_DB"}
+            {"field2match": "+kvdb_not_match/TEST_DB"}
     })"};
-    ASSERT_NO_THROW(opBuilderKVDBMatch(doc.get("/check")));
+    ASSERT_NO_THROW(opBuilderKVDBNotMatch(doc.get("/check")));
 }
 
 // Build incorrect number of arguments
-TEST_F(opBuilderKVDBMatchTest, Builds_incorrect_number_of_arguments)
+TEST_F(opBuilderKVDBNotMatchTest, Builds_incorrect_number_of_arguments)
 {
     Document doc{R"({
         "check":
-            {"field2match": "+kvdb_match"}
+            {"field2match": "+kvdb_not_match"}
     })"};
-    ASSERT_THROW(opBuilderKVDBMatch(doc.get("/check")), std::runtime_error);
+    ASSERT_THROW(opBuilderKVDBNotMatch(doc.get("/check")), std::runtime_error);
 }
 
 // Build invalid DB
-TEST_F(opBuilderKVDBMatchTest, Builds_incorrect_invalid_db)
+TEST_F(opBuilderKVDBNotMatchTest, Builds_incorrect_invalid_db)
 {
     Document doc{R"({
         "check":
-            {"field2match": "+kvdb_match/INVALID_DB"}
+            {"field2match": "+kvdb_not_match/INVALID_DB"}
     })"};
-    ASSERT_THROW(opBuilderKVDBMatch(doc.get("/check")), std::runtime_error);
+    ASSERT_THROW(opBuilderKVDBNotMatch(doc.get("/check")), std::runtime_error);
 }
 
-// Single level
-TEST_F(opBuilderKVDBMatchTest, Single_level_target_ok)
+// Test ok: static values
+TEST_F(opBuilderKVDBNotMatchTest, Static_string_ok)
 {
+    // Set Up KVDB
     KVDB& kvdb = kvdbManager.getDB("TEST_DB");
     kvdb.write("KEY", "DUMMY"); // TODO: Remove DUMMY Use non-value overload
 
@@ -85,6 +86,9 @@ TEST_F(opBuilderKVDBMatchTest, Single_level_target_ok)
             s.on_next(std::make_shared<json::Document>(R"(
                 {"field2match":"KEY"}
             )"));
+            s.on_next(std::make_shared<json::Document>(R"(
+                {"field2match":"INEXISTENT_KEY"}
+            )"));
             // Other fields will be ignored
             s.on_next(std::make_shared<json::Document>(R"(
                 {"otherfield":"KEY"}
@@ -92,46 +96,50 @@ TEST_F(opBuilderKVDBMatchTest, Single_level_target_ok)
             s.on_completed();
         });
 
-    Lifter lift = opBuilderKVDBMatch(doc.get("/check"));
+    Lifter lift = opBuilderKVDBNotMatch(doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
 
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_STREQ(expected[0]->get("/field2match").GetString(), "KEY");
+    ASSERT_EQ(expected.size(), 2);
+    ASSERT_STREQ(expected[0]->get("/field2match").GetString(), "INEXISTENT_KEY");
+    ASSERT_STREQ(expected[1]->get("/otherfield").GetString(), "KEY");
 }
 
-// Multi level
-TEST_F(opBuilderKVDBMatchTest, Multilevel_target_ok)
+TEST_F(opBuilderKVDBNotMatchTest, Multilevel_target)
 {
     KVDB& kvdb = kvdbManager.getDB("TEST_DB");
     kvdb.write("KEY", "DUMMY"); // TODO: Remove DUMMY Use non-value overload
 
     Document doc{R"({
         "check":
-            {"a.b.field2match": "+kvdb_match/TEST_DB"}
+            {"a.b.field2match": "+kvdb_not_match/TEST_DB"}
     })"};
 
     Observable input = observable<>::create<Event>(
         [=](auto s)
         {
             s.on_next(std::make_shared<json::Document>(R"(
-                {"a":{"b":{"field2match":"KEY"}}}
+                {"a": {"b":{"field2match":"KEY"}}}
             )"));
-            // Other fields will be ignored
             s.on_next(std::make_shared<json::Document>(R"(
-                {"a":{"b":{"otherfield":"KEY"}}}
+                {"a": {"b":{"field2match":"INEXISTENT_KEY"}}}
+            )"));
+            // Other fields will continue
+            s.on_next(std::make_shared<json::Document>(R"(
+                {"a": {"b":{"otherfield":"KEY"}}}
             )"));
             s.on_completed();
         });
 
-    Lifter lift = opBuilderKVDBMatch(doc.get("/check"));
+    Lifter lift = opBuilderKVDBNotMatch(doc.get("/check"));
     Observable output = lift(input);
     vector<Event> expected;
     output.subscribe([&](Event e) { expected.push_back(e); });
 
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_STREQ(expected[0]->get("/a/b/field2match").GetString(), "KEY");
+    ASSERT_EQ(expected.size(), 2);
+    ASSERT_STREQ(expected[0]->get("/a/b/field2match").GetString(), "INEXISTENT_KEY");
+    ASSERT_STREQ(expected[1]->get("/a/b/otherfield").GetString(), "KEY");
 }
 
 } // namespace
