92112a289982245045ddaea029906aae518da130
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-03-30 15:57:39 +0200

    Added usage of tracer in operations

--- src/engine/source/builder/builders/opBuilderConditionValue.cpp
+++ src/engine/source/builder/builders/opBuilderConditionValue.cpp
@@ -9,29 +9,47 @@
 
 #include "opBuilderConditionValue.hpp"
 
+#include <fmt/format.h>
+
 using namespace std;
 
 namespace builder::internals::builders
 {
 
-types::Lifter opBuilderConditionValue(const types::DocumentValue & def, types::TracerFn tr)
+types::Lifter opBuilderConditionValue(const types::DocumentValue &def,
+                                      types::TracerFn tr)
 {
     if (!def.MemberBegin()->name.IsString())
     {
-        throw std::runtime_error("Error building condition value, key of definition must be a string.");
+        throw std::runtime_error("Error building condition value, key of "
+                                 "definition must be a string.");
     }
 
-    std::string field = json::formatJsonPath(def.MemberBegin()->name.GetString());
+    std::string field =
+        json::formatJsonPath(def.MemberBegin()->name.GetString());
     // TODO: build document with value only
-    types::Document value{def};
+    types::Document value {def};
+    std::string successTrace = fmt::format("{} Condition Success", value.str());
+    std::string failureTrace = fmt::format("{} Condition Failure", value.str());
 
     // Return Lifter
     return [=](types::Observable o)
     {
         // Append rxcpp operation
-        return o.filter([=](types::Event e) {
-            return e->equals(field, value.begin()->value);
-        });
+        return o.filter(
+            [=](types::Event e)
+            {
+                if (e->equals(field, value.begin()->value))
+                {
+                    tr(successTrace);
+                    return true;
+                }
+                else
+                {
+                    tr(failureTrace);
+                    return false;
+                }
+            });
     };
 }
 
