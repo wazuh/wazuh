8e2b63cacd4c0a3a8ed2db0c5b39c29395133f66
LucioDonda <donda.lucio@gmail.com>
2022-03-30 16:12:22 -0300

    code format with clang on kvdbmanager

--- src/engine/source/kvdb/include/kvdb/kvdbManager.hpp
+++ src/engine/source/kvdb/include/kvdb/kvdbManager.hpp
@@ -1,29 +1,35 @@
 #ifndef _KVDBMANAGER_H
 #define _KVDBMANAGER_H
 
+#include <filesystem>
 #include <string>
 #include <vector>
-#include <filesystem>
 
 #include "kvdb.hpp"
 
-class KVDBManager {
+class KVDBManager
+{
     KVDBManager();
     ~KVDBManager();
     const std::string FOLDER = "/var/ossec/queue/db/kvdb/";
-    using DBMap = std::unordered_map<std::string, KVDB*>;
+    using DBMap = std::unordered_map<std::string, KVDB *>;
     DBMap available_kvdbs;
-    bool addDB(KVDB* DB);
+    bool addDB(KVDB *DB);
     KVDB invalidDB = KVDB(); // TODO: Make this static?
 
 public:
-    static KVDBManager& getInstance() { static KVDBManager instance; return instance; }
-    KVDBManager(KVDBManager const&)     = delete;
-    void operator=(KVDBManager const&)  = delete;
-    bool createDB(const std::string& Name, bool replace = true);
-    bool createDBfromCDB(const std::filesystem::path& path, bool replace = true);
+    static KVDBManager &getInstance()
+    {
+        static KVDBManager instance;
+        return instance;
+    }
+    KVDBManager(KVDBManager const &) = delete;
+    void operator=(KVDBManager const &) = delete;
+    bool createDB(const std::string &Name, bool replace = true);
+    bool createDBfromCDB(const std::filesystem::path &path,
+                         bool replace = true);
     bool DeleteDB(const std::string &name);
-    KVDB& getDB(const std::string& Name);
+    KVDB &getDB(const std::string &Name);
 };
 
 #endif // _KVDBMANAGER_H
