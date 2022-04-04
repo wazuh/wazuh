fd811192851c3e09a748b1085772b9579ff2e7c6
Ramiro Dapozo <ramiroromero94@gmail.com>
2022-04-04 15:25:55 -0300

    Remove old mkdir

--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -1160,7 +1160,6 @@ InstallAgent()
     ${INSTALL} -m 0750 -o root -g ${WAZUH_GROUP} ../wodles/azure/azure-logs.py ${INSTALLDIR}/wodles/azure/azure-logs
 
     # Install debugging symbols if available
-    mkdir -p 
     # The symbols directory should always exist, if debugging symbols
     # are not supported for this platform, it will simply be empty.
     cp -r symbols ${INSTALLDIR}/.symbols
