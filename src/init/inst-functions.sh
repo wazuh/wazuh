8bc7831f07c4d1761cce303a8cd149c99d4ac1a5
Ramiro Dapozo <ramiroromero94@gmail.com>
2022-04-04 17:33:28 -0300

    remove extra newline

--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -1167,7 +1167,6 @@ InstallAgent()
 
     ${INSTALL} -d -m 0750 -o root -g ${WAZUH_GROUP} ${INSTALLDIR}/wodles/azure
     ${INSTALL} -m 0750 -o root -g ${WAZUH_GROUP} ../wodles/azure/azure-logs.py ${INSTALLDIR}/wodles/azure/azure-logs
-
 }
 
 InstallWazuh()
