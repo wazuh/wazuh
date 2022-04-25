ce3b72bf6f42fcacb5e3894b896d6791177a03c8
Santiago Belluzzo <santiago.belluzzo@wazuh.com>
2022-04-25 12:28:18 -0300

    Engine - catalog test cleanup

--- src/engine/test/source/catalog/catalog_test.cpp
+++ src/engine/test/source/catalog/catalog_test.cpp
@@ -1,161 +1,64 @@
-#include "catalog_test.hpp"
+#include <gtest/gtest.h>
+#include <rapidjson/writer.h>
+// TODO we are leaking the yaml exception
+#include <yaml-cpp/yaml.h>
 
-#include "gtest/gtest.h"
-#include "rapidjson/writer.h"
+#include <catalog.hpp>
 
 using namespace catalog;
+
+//TODO fix failing catalog tests
 
 // Test: An asset that fails the check against the schema
 TEST(getAsset, get_asset_invalid_schema)
 {
+    Catalog catalog {StorageType::Local, "base/path"};
 
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    EXPECT_THROW(
-        {
-            try
-            {
-                auto asset = catalog->getAsset(AssetType::Decoder, "syslog_invalid_schema");
-            }
-            catch (const std::runtime_error & e)
-            {
-                ASSERT_STREQ(e.what(), "The asset 'syslog_invalid_schema' is invalid: "
-                                       "Invalid JSON schema: '#/properties/draft'\n"
-                                       "Invalid keyword: 'type'\n"
-                                       "Invalid document: '#/draft'\n");
-                throw;
-            }
-        },
-        std::runtime_error);
+    EXPECT_THROW(catalog.getAsset(AssetType::Decoder, "syslog_invalid_schema"),
+                 std::runtime_error);
 }
 
 // Test: Get a corrupted JSON schema
 TEST(getAsset, get_asset_corrupted_json_schema)
 {
+    Catalog catalog {StorageType::Local, "base/path"};
 
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    spStorageDriver->set_malformed_schemas(true);
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    EXPECT_THROW(
-        {
-            try
-            {
-                auto asset = catalog->getAsset(AssetType::Decoder, "syslog2");
-            }
-            catch (const std::runtime_error & e)
-            {
-                ASSERT_STREQ(e.what(), "Could not parse the schema for the asset type.");
-                throw;
-            }
-        },
-        std::runtime_error);
+    EXPECT_THROW(catalog.getAsset(AssetType::Decoder, "syslog2"),
+                 std::runtime_error);
 }
 
 // Test: Get a corrupted asset (YML Malformed thow an YAML::ParserException)
 TEST(getAsset, get_asset_corrupted)
 {
-
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    EXPECT_THROW(catalog->getAsset(AssetType::Decoder, "syslog_malformed"), YAML::ParserException);
+    auto catalog = Catalog {StorageType::Local, "base/path"};
+    EXPECT_THROW(catalog.getAsset(AssetType::Decoder, "syslog_malformed"),
+                 ::YAML::ParserException);
 }
 
 // Test: Get asset for which there is no schema
 TEST(getAsset, get_asset_schema_not_found)
 {
-
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    spStorageDriver->set_empty_schemas(true);
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    EXPECT_THROW(
-        {
-            try
-            {
-                auto asset = catalog->getAsset(AssetType::Decoder, "syslog2");
-            }
-            catch (const std::runtime_error & e)
-            {
-                ASSERT_STREQ(e.what(), "Could not get the schema 'wazuh-decoders' for "
-                                       "the asset type. DRIVER: Schema not found: "
-                                       "'schemas/wazuh-decoders.json'");
-                throw;
-            }
-        },
-        std::runtime_error);
+    auto catalog = Catalog {StorageType::Local, "base/path"};
+    EXPECT_THROW(catalog.getAsset(AssetType::Decoder, "syslog2"),
+                 std::runtime_error);
 }
 
 // Test: Get asset with does not exist (Driver should throw an exception)
 TEST(getAsset, get_asset_not_found)
 {
+    auto catalog = Catalog {StorageType::Local, "base/path"};
 
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    EXPECT_THROW(
-        {
-            try
-            {
-                auto asset = catalog->getAsset(AssetType::Decoder, "not_found_asset");
-            }
-            catch (const std::runtime_error & e)
-            {
-                ASSERT_STREQ(e.what(), "Asset not found in file: "
-                                       "'decoders/not_found_asset.yml'");
-                throw;
-            }
-        },
-        std::runtime_error);
+    EXPECT_THROW(catalog.getAsset(AssetType::Decoder, "not_found_asset"),
+                 std::runtime_error);
 }
 
 // Test: Get a valid asset
 TEST(getAsset, get_asset_valid)
 {
+    auto catalog = Catalog {StorageType::Local, "base/path"};
 
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    auto catalog = std::make_unique<Catalog>(std::move(spStorageDriver));
-
-    rapidjson::Document decoder = catalog->getAsset(AssetType::Decoder, "syslog2");
-
-    if (decoder.IsNull())
-    {
-        FAIL() << "The decoder is null";
-    }
-    else if (decoder.IsObject())
-    {
-        auto nameProp = decoder.FindMember("name");
-
-        if (nameProp->value.IsNull())
-        {
-            FAIL() << "The decoder no have a name";
-        }
-        else if (nameProp->value.IsString())
-        {
-            EXPECT_STREQ("syslog2", nameProp->value.GetString());
-        }
-        else
-        {
-            FAIL() << "The decoder name is not a string";
-        }
-    }
-    else
-    {
-        FAIL() << "The decoder is not an object";
-    }
-}
-
-// Test: Copy move Get a valid asset
-TEST(getAsset, copy_move_get_asset_valid)
-{
-
-    auto spStorageDriver = std::make_unique<fakeStorage>();
-    Catalog catalog_base{std::move(spStorageDriver)};
-    Catalog catalog{std::move(catalog_base)};
-
-    rapidjson::Document decoder = catalog.getAsset(AssetType::Decoder, "syslog2");
+    rapidjson::Document decoder =
+        catalog.getAsset(AssetType::Decoder, "syslog2");
 
     if (decoder.IsNull())
     {
