03e1a96e44c6b633e07da855a5878c6d9cdffa6f
Juan Cabrera <jcabre88@gmail.com>
2022-04-27 12:29:46 +0200

    Fixed opBuilderCondition tests

--- src/engine/test/source/builder/builders/opBuilderCondition_test.cpp
+++ src/engine/test/source/builder/builders/opBuilderCondition_test.cpp
@@ -15,10 +15,15 @@
 #include "opBuilderConditionValue.hpp"
 #include "opBuilderHelperFilter.hpp"
 
-using namespace builder::internals::builders;
+using namespace base;
+namespace bld = builder::internals::builders;
 
 using FakeTrFn = std::function<void(std::string)>;
 static FakeTrFn tr = [](std::string msg){};
+
+auto createEvent = [](const char * json){
+    return std::make_shared<EventHandler>(std::make_shared<Document>(json));
+};
 
 TEST(opBuilderCondition, BuildsAllNonRegistered)
 {
@@ -35,13 +40,13 @@ TEST(opBuilderCondition, BuildsAllNonRegistered)
     const auto & arr = doc.begin()->value.GetArray();
     for (auto it = arr.Begin(); it != arr.end(); ++it)
     {
-        ASSERT_THROW(opBuilderCondition(*it, tr), invalid_argument);
+        ASSERT_THROW(bld::opBuilderCondition(*it, tr), invalid_argument);
     }
 }
 
 TEST(opBuilderCondition, BuildsValue)
 {
-    BuilderVariant c = opBuilderConditionValue;
+    BuilderVariant c = bld::opBuilderConditionValue;
     Registry::registerBuilder("condition.value", c);
     Document doc{R"({
         "check": [
@@ -53,30 +58,30 @@ TEST(opBuilderCondition, BuildsValue)
     const auto & arr = doc.begin()->value.GetArray();
     for (auto it = arr.Begin(); it != arr.end(); ++it)
     {
-        ASSERT_NO_THROW(opBuilderCondition(*it, tr));
+        ASSERT_NO_THROW(bld::opBuilderCondition(*it, tr));
     }
 }
 
 TEST(opBuilderCondition, BuildsReference)
 {
-    BuilderVariant c = opBuilderConditionReference;
+    BuilderVariant c = bld::opBuilderConditionReference;
     Registry::registerBuilder("condition.reference", c);
     Document doc{R"({"check": {"ref": "$ref"}})"};
-    ASSERT_NO_THROW(opBuilderCondition(doc.get("/check"), tr));
+    ASSERT_NO_THROW(bld::opBuilderCondition(doc.get("/check"), tr));
 }
 
 TEST(opBuilderCondition, BuildsHelperExists)
 {
-    BuilderVariant c = opBuilderHelperExists;
+    BuilderVariant c = bld::opBuilderHelperExists;
     Registry::registerBuilder("helper.exists", c);
     Document doc{R"({"check": {"ref": "+exists"}})"};
-    ASSERT_NO_THROW(opBuilderCondition(doc.get("/check"), tr));
+    ASSERT_NO_THROW(bld::opBuilderCondition(doc.get("/check"), tr));
 }
 
 TEST(opBuilderCondition, BuildsHelperNotExists)
 {
-    BuilderVariant c = opBuilderHelperNotExists;
+    BuilderVariant c = bld::opBuilderHelperNotExists;
     Registry::registerBuilder("helper.not_exists", c);
     Document doc{R"({"check": {"ref": "+not_exists"}})"};
-    ASSERT_NO_THROW(opBuilderCondition(doc.get("/check"), tr));
+    ASSERT_NO_THROW(bld::opBuilderCondition(doc.get("/check"), tr));
 }
