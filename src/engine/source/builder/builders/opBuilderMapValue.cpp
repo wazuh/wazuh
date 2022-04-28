0202391b2996e7c3930e434dfdb0a1bab8968508
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-04-28 19:47:36 +0000

    Code improved and formatted and cleaned

--- src/engine/source/builder/builders/opBuilderMapValue.cpp
+++ src/engine/source/builder/builders/opBuilderMapValue.cpp
@@ -9,8 +9,6 @@
 
 #include "opBuilderMapValue.hpp"
 
-using namespace std;
-
 namespace builder::internals::builders
 {
 // TODOL DOC and test this
@@ -18,26 +16,27 @@ base::Lifter opBuilderMapValue(const base::DocumentValue &def,
                                 types::TracerFn tr)
 {
     // Make deep copy of value
-    base::Document doc {def};
     std::string field =
         json::formatJsonPath(def.MemberBegin()->name.GetString());
-    std::string defstr {doc.str()};
 
+    base::Document doc {def};
+    std::string defstr {doc.str()};
     // Debug trace
-    std::string successTrace = fmt::format("{} Mapping Success", doc.str());
-    std::string failureTrace = fmt::format("{} Mapping Failure", doc.str());
+    const std::string successTrace =
+        fmt::format("{} Mapping Success", doc.str());
+    const std::string failureTrace =
+        fmt::format("{} Mapping Failure", doc.str());
 
     // Return Lifter
     return [=](base::Observable o)
     {
         // Append rxcpp operation
-        return o.map(
-            [=](base::Event e)
-            {
-                e->getEvent()->set(field, doc.m_doc.MemberBegin()->value);
-                tr(successTrace);
-                return e;
-            });
+        return o.map([=](base::Event e) {
+            e->getEvent()->set(field, doc.m_doc.MemberBegin()->value)
+                ? tr(successTrace)
+                : tr(failureTrace);
+            return e;
+        });
     };
 }
 
