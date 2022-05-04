cd05f8dfe86c328f4108b2798674c64372a0fb2d
Víctor Fernández Poyatos <victor.f.poyatos@wazuh.com>
2022-05-04 14:16:24 +0200

    Update core wdb unit tests

--- framework/wazuh/core/tests/test_wdb.py
+++ framework/wazuh/core/tests/test_wdb.py
@@ -20,20 +20,25 @@ def test_failed_connection():
     """
     Tests an exception is properly raised when it's not possible to connect to wdb
     """
-    # tests the socket path doesn't exists
-    with patch('wazuh.core.common.WDB_SOCKET', '/this/path/doesnt/exist'):
-        with pytest.raises(exception.WazuhException, match=".* 2005 .*"):
-            WazuhDBConnection()
-    # tests an exception is properly raised when a connection error is raised
-    with patch('socket.socket') as socket_patch:
-        with pytest.raises(exception.WazuhException, match=".* 2005 .*"):
-            socket_patch.return_value.connect.side_effect = ConnectionError
-            WazuhDBConnection()
+    # wazuh-db daemon is not running
+    with pytest.raises(exception.WazuhError, match=".* 1017 .*"):
+        WazuhDBConnection()
+    with patch('wazuh.core.manager.check_wazuh_status'):
+        # Tests the socket path doesn't exists
+        with patch('wazuh.core.common.WDB_SOCKET', '/this/path/doesnt/exist'):
+            with pytest.raises(exception.WazuhException, match=".* 2005 .*"):
+                WazuhDBConnection()
+        # Tests an exception is properly raised when a connection error is raised
+        with patch('socket.socket') as socket_patch:
+            with pytest.raises(exception.WazuhException, match=".* 2005 .*"):
+                socket_patch.return_value.connect.side_effect = ConnectionError
+                WazuhDBConnection()
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_wrong_character_encodings_wdb(send_mock, connect_mock):
+def test_wrong_character_encodings_wdb(send_mock, wazuh_status_mock, connect_mock):
     """
     Tests receiving a text with a bad character encoding from wazuh db
     """
@@ -48,8 +53,9 @@ def test_wrong_character_encodings_wdb(send_mock, connect_mock):
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_null_values_are_removed(send_mock, connect_mock):
+def test_null_values_are_removed(send_mock, wazuh_status_mock, connect_mock):
     """
     Tests '(null)' values are removed from the resulting dictionary
     """
@@ -64,8 +70,9 @@ def test_null_values_are_removed(send_mock, connect_mock):
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_failed_send_private(send_mock, connect_mock):
+def test_failed_send_private(send_mock, wazuh_status_mock, connect_mock):
     """
         Tests an exception is properly raised when it's not possible to send a msg to the wdb socket
     """
@@ -91,8 +98,9 @@ def test_failed_send_private(send_mock, connect_mock):
     b'ok {"agents": {"001": "DB not found"}}'
 ])
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_remove_agents_database(send_mock, connect_mock, content):
+def test_remove_agents_database(send_mock, wazuh_status_mock, connect_mock, content):
     """
     Tests delete_agents_db method handle exceptions properly
     """
@@ -114,23 +122,26 @@ def test_remove_agents_database(send_mock, connect_mock, content):
     'global sql delete test ;'
 ])
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_query_input_validation_private(send_mock, connect_mock, error_query):
+def test_query_input_validation_private(send_mock, wazuh_status_mock, connect_mock, error_query):
     mywdb = WazuhDBConnection()
     with pytest.raises(exception.WazuhException, match=".* 2004 .*"):
         mywdb.execute(error_query)
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_query_lower_private(send_mock, connect_mock):
+def test_query_lower_private(send_mock, wazuh_status_mock, connect_mock):
     mywdb = WazuhDBConnection()
     with pytest.raises(exception.WazuhException, match=".* 2004 .*"):
         mywdb.execute("Agent sql select 'test'")
 
 
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.connect")
-def test_run_wdb_command(connect_mock):
+def test_run_wdb_command(connect_mock, wazuh_status_mock):
     """Test `WazuhDBConnection.run_wdb_command` method."""
     send_result = ('status', '["data"]')
     command = "any wdb command"
@@ -160,9 +171,10 @@ def test_run_wdb_command_ko(connect_mock, wdb_response):
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
 @patch("wazuh.core.wdb.WazuhDBConnection._send")
-def test_execute(send_mock, socket_send_mock, connect_mock):
+def test_execute(send_mock, socket_send_mock, wazuh_status_mock, connect_mock):
     def send_mock(obj, msg, raw=False):
         return ['ok', '{"total": 5}'] if raw else [{"total": 5}]
 
@@ -176,8 +188,9 @@ def test_execute(send_mock, socket_send_mock, connect_mock):
 
 
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_execute_pagination(socket_send_mock, connect_mock):
+def test_execute_pagination(socket_send_mock, wazuh_status_mock, connect_mock):
     mywdb = WazuhDBConnection()
 
     # Test pagination
@@ -200,8 +213,10 @@ def test_execute_pagination(socket_send_mock, connect_mock):
     ('agent 000 sql select test from test offset 1 limit 1', Exception, 2007, False, False)
 ])
 @patch("socket.socket.connect")
+@patch('wazuh.core.manager.check_wazuh_status')
 @patch("socket.socket.send")
-def test_failed_execute(send_mock, connect_mock, error_query, error_type, expected_exception, delete, update):
+def test_failed_execute(send_mock, wazuh_status_mock, connect_mock, error_query, error_type, expected_exception, delete,
+                        update):
     mywdb = WazuhDBConnection()
     if not error_type:
         with pytest.raises(exception.WazuhException, match=f'.* {expected_exception} .*'):
