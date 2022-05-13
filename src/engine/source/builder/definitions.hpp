f72b6a0dac8ea99f7d5f20f7d52be07d4c59ce82
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-05-13 11:48:38 +0200

    Added definitions functionality

---
+++ src/engine/source/builder/definitions.hpp
@@ -0,0 +1,16 @@
+#ifndef _DEFINITIONS_H_
+#define _DEFINITIONS_H_
+
+#include "builderTypes.hpp"
+
+namespace builder
+{
+namespace internals
+{
+
+void substituteDefinitions(base::Document& asset);
+
+}
+} // namespace builder
+
+#endif // _DEFINITIONS_H_
