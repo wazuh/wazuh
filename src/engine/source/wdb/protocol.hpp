a5761fb00ad8c4ab46b19c0fd9c7d7c7c1fef51e
Julian Morales <jmorales@unc.edu.ar>
2022-05-19 19:38:59 +0000

    Add base functions send-recv

--- src/engine/source/wdb/protocol.hpp
+++ src/engine/source/wdb/protocol.hpp
@@ -8,7 +8,6 @@
 
 #include <logging/logging.hpp>
 
-
 constexpr int OS_SOCKTERR = -1;
 
 int OS_ConnectUnixDomain(const char* path)
@@ -37,6 +36,9 @@ int OS_ConnectUnixDomain(const char* path)
     /* Connect to the UNIX domain */
     if (connect(ossock, (struct sockaddr*)&n_us, SUN_LEN(&n_us)) < 0)
     {
+         WAZUH_LOG_WARN("Cannot connect: {} ({})",
+                       strerror(errno),
+                       errno);
         close(ossock);
         return (OS_SOCKTERR);
     }
@@ -69,13 +71,92 @@ int OS_ConnectUnixDomain(const char* path)
     // Set close-on-exec
     if (fcntl(ossock, F_SETFD, FD_CLOEXEC) == -1)
     {
-        WAZUH_LOG_ERROR("{}", "Cannot set close-on-exec flag to socket");
-        //mwarn("Cannot set close-on-exec flag to socket: %s (%d)",
-        //      strerror(errno),
-        //      errno);
+        WAZUH_LOG_WARN("Cannot set close-on-exec flag to socket {} {}",
+                       strerror(errno),
+                       errno);
     }
 
     return (ossock);
+}
+
+
+
+/* Receive a message from a stream socket, full message (MSG_WAITALL)
+ * Returns size on success.
+ * Returns -1 on socket error.
+ * Returns 0 on socket disconnected or timeout.
+ */
+ssize_t os_recv_waitall(int sock, void * buf, size_t size) {
+    size_t offset;
+    ssize_t recvb;
+
+    for (offset = 0; offset < size; offset += recvb) {
+        recvb = recv(sock, buf + offset, size - offset, 0);
+
+        if (recvb <= 0) {
+            return recvb;
+        }
+    }
+
+    return offset;
+}
+
+// Send secure TCP message
+int OS_SendSecureTCP(int sock, uint32_t size, const void * msg) {
+    int retval = OS_SOCKTERR;
+    void* buffer = NULL;
+    size_t bufsz = size + sizeof(uint32_t);
+
+    if (sock < 0) {
+        return retval;
+    }
+
+    buffer = malloc(bufsz);
+    *(uint32_t *)buffer = size;
+    memcpy(buffer + sizeof(uint32_t), msg, size);
+    errno = 0;
+    retval = send(sock, buffer, bufsz, 0) == (ssize_t)bufsz ? 0 : OS_SOCKTERR;
+    free(buffer);
+    return retval;
+}
+
+
+/* Receive secure TCP message
+ * This function reads a header containing message size as 4-byte little-endian unsigned integer.
+ * Return recvval on success or OS_SOCKTERR on error.
+ */
+int OS_RecvSecureTCP(int sock, char * ret, uint32_t size) {
+    ssize_t recvval, recvb;
+    uint32_t msgsize;
+
+    /* Get header */
+    recvval = os_recv_waitall(sock, &msgsize, sizeof(msgsize));
+
+    switch(recvval) {
+        case -1:
+            return recvval;
+            break;
+
+        case 0:
+            return recvval;
+            break;
+    }
+
+    if(msgsize > size){
+        /* Error: the payload length is too long */
+        return OS_SOCKTERR;
+    }
+
+    /* Get payload */
+    recvb = os_recv_waitall(sock, ret, msgsize);
+
+    /* Terminate string if there is space left */
+
+    if (recvb == (int32_t) msgsize && msgsize < size) {
+        ret[msgsize] = '\0';
+    }
+
+    return recvb;
 }
 
 #endif
