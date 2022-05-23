ae6977d14003718a3d0f820d6465a10e13f8dee9
Nicolás Mariano Koremblum <nmkoremblum@gmail.com>
2022-05-23 03:30:10 +0000

    Tests added and code improved

--- src/engine/test/source/wdb/protocol_test.cpp
+++ src/engine/test/source/wdb/protocol_test.cpp
@@ -7,25 +7,22 @@
 #include <gtest/gtest.h>
 
 using namespace socketinterface;
-/*
-TODO:
-- Rename FILE_TEST
-- TEST CONEXION OK
-- TEST CONEXION NOT OK (PERMISSION)
-- TEST CONEXION NOT OK (NOT SUCH FILE OR DIRECTORY)
-- TEST SEND OK
-- TEST SEND OUT OF RANGE
-- TEST RECV OK
-- TEST RECV OUT OF RANGE
-- TEST Loopback
-*/
 
-// Test sock stream unix server
-/* Bind to a Unix domain, using DGRAM sockets */
-int OS_BindUnixDomain(const char *path)
+#define MAX_BUFFER_SIZE     1024
+#define MESSAGE_HEADER_SIZE sizeof(uint32_t)
+#define TEST_SEND_MESSAGE   "Test message to be send!\n"
+#define TEST_SOCKET_PATH    "/tmp/test.sock"
+
+/**
+ * @brief Test auxiliar function to bind a datagram UNIX socket
+ *
+ * @param path Socket pathname
+ * @return int Socket file descriptor
+ */
+static int testBindUnixSocket(const char* path)
 {
+    int sockFD = 0;
     struct sockaddr_un n_us;
-    int ossock = 0;
 
     /* Make sure the path isn't there */
     unlink(path);
@@ -34,72 +31,181 @@ int OS_BindUnixDomain(const char *path)
     n_us.sun_family = AF_UNIX;
     strncpy(n_us.sun_path, path, sizeof(n_us.sun_path) - 1);
 
-    if ((ossock = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+    if ((sockFD = socket(PF_UNIX, SOCK_STREAM, 0)) < 0)
+    {
         return (SOCKET_ERROR);
     }
 
-    if (bind(ossock, (struct sockaddr *)&n_us, SUN_LEN(&n_us)) < 0) {
-        close(ossock);
+    if (bind(sockFD, (struct sockaddr*)&n_us, SUN_LEN(&n_us)) < 0)
+    {
+        close(sockFD);
         return (SOCKET_ERROR);
     }
 
     /* Change permissions */
-    if (chmod(path, 0660) < 0) {
-        close(ossock);
+    if (chmod(path, 0660) < 0)
+    {
+        close(sockFD);
         return (SOCKET_ERROR);
     }
 
-    if (listen(ossock, 128) < 0) {
-        close(ossock);
+    if (listen(sockFD, 1) < 0)
+    {
+        close(sockFD);
         return (SOCKET_ERROR);
     }
 
-    return (ossock);
+    return (sockFD);
 }
 
-/* Accept a TCP connection */
-int OS_AcceptTCP(int socket)
+/**
+ * @brief Test auxiliar function to accept the socket connections
+ *
+ * @param serverSocketFD File descriptor of the connection socket
+ * @return int File descriptor of the client socket
+ */
+static int testAcceptConnection(const int serverSocketFD)
 {
-    int clientsocket;
     struct sockaddr_in _nc;
-    socklen_t _ncl;
-
     memset(&_nc, 0, sizeof(_nc));
-    _ncl = sizeof(_nc);
+    socklen_t _ncl = sizeof(_nc);
 
-    if ((clientsocket = accept(socket, (struct sockaddr *) &_nc,
-                               &_ncl)) < 0) {
-        return (-1);
+    int clientSocketFD = accept(serverSocketFD, (struct sockaddr*)&_nc, &_ncl);
+    if (clientSocketFD < 0)
+    {
+        return (SOCKET_ERROR);
     }
 
-    return (clientsocket);
+    return (clientSocketFD);
 }
 
+/*
+TODO:
+- Rename FILE_TEST
+- TEST CONEXION NOT OK (PERMISSION)
+- TEST SEND OUT OF RANGE
+- TEST RECV OUT OF RANGE
+*/
 
-TEST(wdb_procol, init)
+TEST(wdb_procol, testSocketConnectError)
 {
-    const char * hi = "Test mesg send!\n";
-    char reply[1024] = {};
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_EQ(clientSocketFD, SOCKET_ERROR);
+}
+
+TEST(wdb_procol, testSocketConnect)
+{
+    // Create server
+    const int acceptSocketFD = testBindUnixSocket(TEST_SOCKET_PATH);
+    ASSERT_GT(acceptSocketFD, 0);
+
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_GT(clientSocketFD, 0);
+
+    close(acceptSocketFD);
+    close(clientSocketFD);
+
+    unlink(TEST_SOCKET_PATH);
+}
+
+TEST(wdb_procol, testSendMessageError)
+{
+    // Create server
+    const int acceptSocketFD = testBindUnixSocket(TEST_SOCKET_PATH);
+    ASSERT_GT(acceptSocketFD, 0);
+
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_GT(clientSocketFD, 0);
+
+    // Force error
+    close(clientSocketFD);
+    ASSERT_EQ(sendMsg(clientSocketFD, TEST_SEND_MESSAGE, strlen(TEST_SEND_MESSAGE)),
+              SOCKET_ERROR);
+
+    close(acceptSocketFD);
+
+    unlink(TEST_SOCKET_PATH);
+}
+
+TEST(wdb_procol, testSendMessage)
+{
+    // Create server
+    const int acceptSocketFD = testBindUnixSocket(TEST_SOCKET_PATH);
+    ASSERT_GT(acceptSocketFD, 0);
+
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_GT(clientSocketFD, 0);
+
+    const int msgLen = strlen(TEST_SEND_MESSAGE);
+    ASSERT_EQ(sendMsg(clientSocketFD, TEST_SEND_MESSAGE, msgLen),
+              msgLen + MESSAGE_HEADER_SIZE);
+
+    close(acceptSocketFD);
+    close(clientSocketFD);
+
+    unlink(TEST_SOCKET_PATH);
+}
+
+TEST(wdb_procol, testRecvMessage)
+{
+    char msg[MAX_BUFFER_SIZE] = {};
 
     // Create server
-    int fd_server = OS_BindUnixDomain("/root/test.sock");
-    ASSERT_GT(fd_server, 0);
+    const int acceptSocketFD = testBindUnixSocket(TEST_SOCKET_PATH);
+    ASSERT_GT(acceptSocketFD, 0);
 
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_GT(clientSocketFD, 0);
 
-    int fd_client = socketConnect("/root/test.sock");
-    ASSERT_GT(fd_client, 0);
+    const int serverSocketFD = testAcceptConnection(acceptSocketFD);
+    ASSERT_GT(serverSocketFD, 0);
 
-    std::cout << "send: " << hi << std::endl;
-    sendMsg(fd_client, hi, strlen(hi));
-    sendMsg(fd_client, "Hey, how are u?");
-    int srv_client = OS_AcceptTCP(fd_server);
-    int recvBytes = recvMsg(srv_client , reply, 1024);
-    std::cout << "recv (" << recvBytes  << "): "<< reply << std::endl;
-    recvBytes = recvMsg(srv_client , reply, 1024);
-    std::cout << "recv (" << recvBytes  << "): "<< reply << std::endl;
+    const int msgLen = strlen(TEST_SEND_MESSAGE);
+    ASSERT_EQ(sendMsg(serverSocketFD, TEST_SEND_MESSAGE, msgLen),
+              msgLen + MESSAGE_HEADER_SIZE);
 
-    close(fd_client);
-    close(fd_server);
-    close(srv_client);
-    unlink("/root/test.sock");
+    int recvBytes = recvMsg(clientSocketFD, msg, MAX_BUFFER_SIZE);
+    ASSERT_STREQ(msg, TEST_SEND_MESSAGE);
+
+    close(acceptSocketFD);
+    close(serverSocketFD);
+    close(clientSocketFD);
+
+    unlink(TEST_SOCKET_PATH);
+}
+
+TEST(wdb_procol, testSendRecvMessage)
+{
+    char msg[MAX_BUFFER_SIZE] = {};
+
+    // Create server
+    const int acceptSocketFD = testBindUnixSocket(TEST_SOCKET_PATH);
+    ASSERT_GT(acceptSocketFD, 0);
+
+    const int clientSocketFD = socketConnect(TEST_SOCKET_PATH);
+    ASSERT_GT(clientSocketFD, 0);
+
+    const int serverSocketFD = testAcceptConnection(acceptSocketFD);
+    ASSERT_GT(serverSocketFD, 0);
+
+    const int msgLen = strlen(TEST_SEND_MESSAGE);
+    ASSERT_EQ(sendMsg(clientSocketFD, TEST_SEND_MESSAGE, msgLen),
+              msgLen + MESSAGE_HEADER_SIZE);
+
+    int recvBytes = recvMsg(serverSocketFD, msg, MAX_BUFFER_SIZE);
+    ASSERT_STREQ(msg, TEST_SEND_MESSAGE);
+
+    bzero(msg, MAX_BUFFER_SIZE);
+
+    ASSERT_EQ(sendMsg(serverSocketFD, TEST_SEND_MESSAGE, msgLen),
+              msgLen + MESSAGE_HEADER_SIZE);
+
+    recvBytes = recvMsg(clientSocketFD, msg, MAX_BUFFER_SIZE);
+    ASSERT_STREQ(msg, TEST_SEND_MESSAGE);
+
+    close(acceptSocketFD);
+    close(clientSocketFD);
+    close(serverSocketFD);
+
+    unlink(TEST_SOCKET_PATH);
 }
