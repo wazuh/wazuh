06a823cc01a3d082767af0b7394c616b6155dfb2
Julian Morales <jmorales@unc.edu.ar>
2022-05-23 22:37:01 +0000

    Base WDB connector

--- source/wdb/include/wdb/wdb.hpp
+++ source/wdb/include/wdb/wdb.hpp
@@ -2,4 +2,28 @@
 #ifndef _WDB_H
 #define _WDB_H
 
+#include <filesystem>
+
+/**
+ * @brief
+ *
+ * @warning NOT A THREAD SAFE IMPLEMENTATION
+ */
+class WazuhDB
+{
+private:
+    constexpr static const char* WDB_PATH {"queue/db/wdb"};
+    /** @brief Path to the wdb socket (From the chrooted jail directory) */
+    std::filesystem::path path; // Relative by default
+    int fd {-1};                                 ///< File descriptor to the wdb socket
+public:
+    /** @brief Construc from a path */
+    WazuhDB(std::string_view strPath = WDB_PATH);
+    // Create move copy and assign operators
+    /** @brief Destructor */
+    ~WazuhDB();
+    /** @brief Connect to the wdb socket */
+    int connect();
+};
+
 #endif
