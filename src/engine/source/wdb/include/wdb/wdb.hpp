5820ac5c1d9afafaf6c7a06f2898c13098038bf4
Julian Morales <jmorales@unc.edu.ar>
2022-05-24 20:30:54 +0000

    CMake and function definition improvements

--- src/engine/source/wdb/include/wdb/wdb.hpp
+++ src/engine/source/wdb/include/wdb/wdb.hpp
@@ -9,7 +9,8 @@
 
 namespace wazuhdb
 {
-enum class QueryResult
+
+enum class QueryResultCodes
 {
     OK,     ///< Command processed successfully
     DUE,    ///< Command processed successfully with pending data
@@ -18,40 +19,64 @@ enum class QueryResult
     UNKNOWN ///< Unknown status
 };
 
-const std::map<const char*, QueryResult> QueryResultStrings = {
-    {"ok", QueryResult::OK},
-    {"due", QueryResult::DUE },
-    {"err", QueryResult::ERROR },
-    {"ign", QueryResult::IGNORE }};
+/** Assoiates the result (string) of a query with a queryResult enum */
+const std::map<const char*, QueryResultCodes> QueryResStr2Code = {
+    {"ok", QueryResultCodes::OK},
+    {"due", QueryResultCodes::DUE},
+    {"err", QueryResultCodes::ERROR},
+    {"ign", QueryResultCodes::IGNORE}};
+
+
 
 /**
- * @brief
+ * @brief WazuhDB class
  *
- * @warning NOT A THREAD SAFE IMPLEMENTATION
+ * This class is used to interact with the WazuhDB database.
+ *
+ * @warning Not a thread-safe implementation.
  */
 class WazuhDB
 {
 private:
-    constexpr static const char* WDB_PATH {"queue/db/wdb"};
-    constexpr static int SOCKET_NOT_CONNECTED {-1};
-    /** @brief Path to the wdb socket (From the chrooted jail directory) */
-    std::filesystem::path m_path;    // Relative by default
+    constexpr static const char* WDB_PATH {"queue/db/wdb"}; ///< Default wdb socket path
+    constexpr static int SOCKET_NOT_CONNECTED {-1}; ///< Socket not connected (status)
+
+    // State and configuration
+    std::filesystem::path m_path;    ///< WDB socket path
     int m_fd {SOCKET_NOT_CONNECTED}; ///< File descriptor to the wdb socket
+
 public:
-    /** @brief Construc from a path */
+    /** @brief Create a WazuhDB object from a path
+     *
+     * @param path Path to the wdb socket
+     */
     WazuhDB(std::string_view strPath = WDB_PATH)
         : m_path(strPath) {};
-    // Create move copy and assign operators
+
+    // TODO: Create move copy and assign operators
+
     /** @brief Destructor */
     ~WazuhDB();
+
     /**
      * @brief Connect to the wdb socket
      *
      * @throw std::runtime_error if cannot connect to the wdb socket
      */
     void connect();
+
+    /**
+     * @brief Query the wdb socket
+     *
+     * @param query Query to send to the wdb socket
+     * @param [out] response Response buffer
+     * @param length Response buffer length
+     *     *
+     * @throw std::runtime_error if cannot connect to the wdb socket
+     */
     void query(std::string_view query, char* response, int length);
-    QueryResult parseResult(char* result, char** payload);
+
+    QueryResultCodes parseResult(char* result, char** payload);
 };
 } // namespace wazuhdb
 #endif
