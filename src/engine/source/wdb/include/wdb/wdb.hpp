6d6003163763e07cd1afbada922911b5c0232d79
Julian Morales <jmorales@unc.edu.ar>
2022-05-24 16:17:51 +0000

    Adds connect and parse result

--- src/engine/source/wdb/include/wdb/wdb.hpp
+++ src/engine/source/wdb/include/wdb/wdb.hpp
@@ -4,6 +4,26 @@
 
 #include <filesystem>
 
+#include <map>
+#include <string>
+
+namespace wazuhdb
+{
+enum class QueryResult
+{
+    OK,     ///< Command processed successfully
+    DUE,    ///< Command processed successfully with pending data
+    ERROR,  ///< An error occurred
+    IGNORE, ///< Command ignored
+    UNKNOWN ///< Unknown status
+};
+
+const std::map<const char*, QueryResult> QueryResultStrings = {
+    {"ok", QueryResult::OK},
+    {"due", QueryResult::DUE },
+    {"err", QueryResult::ERROR },
+    {"ign", QueryResult::IGNORE }};
+
 /**
  * @brief
  *
@@ -13,17 +33,25 @@ class WazuhDB
 {
 private:
     constexpr static const char* WDB_PATH {"queue/db/wdb"};
+    constexpr static int SOCKET_NOT_CONNECTED {-1};
     /** @brief Path to the wdb socket (From the chrooted jail directory) */
-    std::filesystem::path path; // Relative by default
-    int fd {-1};                                 ///< File descriptor to the wdb socket
+    std::filesystem::path m_path;    // Relative by default
+    int m_fd {SOCKET_NOT_CONNECTED}; ///< File descriptor to the wdb socket
 public:
     /** @brief Construc from a path */
-    WazuhDB(std::string_view strPath = WDB_PATH);
+    WazuhDB(std::string_view strPath = WDB_PATH)
+        : m_path(strPath) {};
     // Create move copy and assign operators
     /** @brief Destructor */
     ~WazuhDB();
-    /** @brief Connect to the wdb socket */
-    int connect();
+    /**
+     * @brief Connect to the wdb socket
+     *
+     * @throw std::runtime_error if cannot connect to the wdb socket
+     */
+    void connect();
+    void query(std::string_view query, char* response, int length);
+    QueryResult parseResult(char* result, char** payload);
 };
-
+} // namespace wazuhdb
 #endif
--- src/engine/source/wdb/src/wdb.cpp
+++ src/engine/source/wdb/src/wdb.cpp
@@ -6,41 +6,144 @@
 
 #include "unixSocketInterface.hpp"
 
-WazuhDB::WazuhDB(std::string_view strPath) : path(strPath)
+namespace wazuhdb
 {
-
-    if (std::filesystem::exists(path) == false)
-    {
-        const std::string msg {"The wdb socket does not exist:" + path.string()};
-        throw std::runtime_error(msg);
-    }
-    else if (std::filesystem::is_socket(path) == false)
-    {
-        const std::string msg {"The wdb socket does not a socket:" + path.string()};
-        throw std::runtime_error(msg);
-    }
-
-}
 
 WazuhDB::~WazuhDB()
 {
-    if (fd != -1)
+    if (this->m_fd > 0)
     {
-        close(fd);
+        close(this->m_fd);
     }
 };
 
-int WazuhDB::connect()
+void WazuhDB::connect()
 {
+    if (std::filesystem::exists(m_path) == false)
+    {
+        const std::string msg {"The wdb socket does not exist:" + m_path.string()};
+        throw std::runtime_error(msg);
+    }
+    else if (std::filesystem::is_socket(m_path) == false)
+    {
+        const std::string msg {"The wdb socket does not a socket:" + m_path.string()};
+        throw std::runtime_error(msg);
+    }
 
-    if (fd > 0)
+    if (this->m_fd != SOCKET_NOT_CONNECTED)
     {
         WAZUH_LOG_DEBUG("Already connected to the wdb socket.. closing it");
-        close(fd);
-        fd = -1;
+        close(this->m_fd);
+        this->m_fd = SOCKET_NOT_CONNECTED;
     }
 
-    // change the name socketConnect to connect
-    fd = socketinterface::socketConnect(path.c_str());
-    return fd;
+    this->m_fd = socketinterface::socketConnect(m_path.c_str());
 };
+
+void WazuhDB::query(std::string_view query, char* response, int length)
+{
+    // Check the query
+    if (query.empty())
+    {
+        throw std::runtime_error("The query is empty");
+    }
+    else if (length < 0)
+    {
+        throw std::runtime_error("The response buffer length is negative");
+    }
+    else if (response == nullptr)
+    {
+        throw std::runtime_error("The response buffer is null");
+    }
+
+    // Check the connection
+    if (this->m_fd == SOCKET_NOT_CONNECTED)
+    {
+        WAZUH_LOG_DEBUG("Not connected to the wdb socket.. connecting");
+        // TODO TRY CATCH
+        this->connect();
+    }
+
+    // Send the query
+    switch (socketinterface::sendMsg(this->m_fd, query.data(), query.size()))
+    {
+
+        case socketinterface::INVALID_SOCKET:
+            WAZUH_LOG_ERROR("The socket is invalid");
+            return; // if reach this point there is an error in logic
+        case socketinterface::NULL_PTR:
+            WAZUH_LOG_ERROR("The pointer to query is null");
+            return; // if reach this point there is an error in logic
+        case socketinterface::SIZE_ZERO:
+            WAZUH_LOG_ERROR("The size of the query is zero");
+            return; // if reach this point there is an error in logic
+        case socketinterface::SIZE_TOO_LONG:
+            throw std::runtime_error("Query size is too long");
+            break;
+        case socketinterface::SOCKET_ERROR:
+        {
+            const std::string errMsg = std::string {"Cannot send the query: "}
+                                       + strerror(errno) + " (" + std::to_string(errno)
+                                       + ")";
+            throw std::runtime_error(errMsg);
+        }
+        default: break;
+    }
+
+    // Go to find the response
+    switch (socketinterface::recvMsg(this->m_fd, response, length))
+    {
+        case socketinterface::INVALID_SOCKET:
+            WAZUH_LOG_ERROR("The socket is invalid");
+            return; // if reach this point there is an error in logic
+        case socketinterface::NULL_PTR:
+            WAZUH_LOG_ERROR("The pointer to response is null");
+            return; // if reach this point there is an error in logic
+        case socketinterface::SIZE_ZERO:
+            WAZUH_LOG_ERROR("The size of the response is zero");
+            return; // if reach this point there is an error in logic
+        case socketinterface::SIZE_TOO_LONG:
+            throw std::runtime_error("Response size is too long");
+            break;
+        case socketinterface::SOCKET_ERROR:
+        {
+            const std::string errMsg = std::string {"Cannot receive the response: "}
+                                       + strerror(errno) + " (" + std::to_string(errno)
+                                       + ")";
+            throw std::runtime_error(errMsg);
+            break;
+        }
+        case 0: throw std::runtime_error("Timeout or remote gracefully closed"); break;
+
+        default: break;
+    }
+}
+
+QueryResult parseResult(char* result, char** payload) {
+
+    // if (result == nullptr)
+
+    // Separete the code result and the payload
+    char * wptr {strchr(result, ' ')};
+
+    if (wptr != nullptr) {
+        *wptr = '\0';
+        wptr++;
+    } else {
+        wptr = result;
+    }
+
+    // Parse payload
+    if (payload) {
+        *payload = wptr;
+    }
+
+    // Parse code
+    auto res = QueryResultStrings.find(result);
+    if (res == QueryResultStrings.end()) {
+        return QueryResult::UNKNOWN;
+    }
+    return res->second;
+}
+
+} // namespace wazuhdb
