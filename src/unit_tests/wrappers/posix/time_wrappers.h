4c0b05f0ba8c56d73dab5f1bb03af9f0612be8e3
Santiago Vendramini <sdvendramini@gmail.com>
2022-06-02 20:42:21 +0200

    wdb global parser tests fixed

--- src/unit_tests/wrappers/posix/time_wrappers.h
+++ src/unit_tests/wrappers/posix/time_wrappers.h
@@ -16,4 +16,6 @@
 time_t __wrap_time(time_t *t);
 char *__wrap_ctime_r(const time_t *timep, char *buf);
 
+void __wrap_gettimeofday(__attribute__((unused))struct timeval *__restrict __tv, __attribute__((unused)) void *__restrict __tz);
+
 #endif // TIME_WRAPPERS_H
