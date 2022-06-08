6e787df41d777539b6894fcfa1e736cf0606457c
Julian Morales <jmorales@unc.edu.ar>
2022-06-08 20:31:37 +0000

    Add base dgram socket interface

---
+++ test/source/base/socketInterface/unixDatagram_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <utils/socketInterface/unixDatagram.hpp>
+
+using namespace base::utils::socketInterface::unixDatagram;
+
+TEST(socketInterface_Datagram, createasd)
+{
+   //GTEST_SKIP();
+   socketConnect("");
+}
