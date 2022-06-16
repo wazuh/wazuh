54b2aa39c9cbf32136cfb6e563f7580560f43803
Matias Pereyra <matias.pereyra@wazuh.com>
2022-06-16 19:57:50 -0300

    Properly deleting agent artifacts

--- src/shared/read-agents.c
+++ src/shared/read-agents.c
@@ -596,6 +596,10 @@ void delete_sqlite(const char *id, const char *name)
 /* Delete diff folders */
 void delete_diff(const char *name)
 {
+    if (!name || *name == '\0') {
+        return;
+    }
+
     char tmp_folder[513];
     tmp_folder[512] = '\0';
     snprintf(tmp_folder, 512, "%s/%s",
