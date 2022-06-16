3d953b671f3e7bd6eb74745edde6e5bdf8b3f79f
Christian Borla <borlach12@gmail.com>
2022-06-16 20:48:27 -0300

    Removed alredy calculated datetime

--- src/wazuh_modules/wm_github.c
+++ src/wazuh_modules/wm_github.c
@@ -355,15 +355,7 @@ STATIC void wm_github_execute_scan(wm_github *github_config, int initial_scan) {
                     wm_github_scan_failure_action(&github_config->fails, current->org_name, event_types[event_types_it], error_msg, github_config->queue_fd);
                 } else {
                     org_state_struc.last_log_time = new_scan_time;
-
-                    if (isDebug()) {
-                        memset(new_scan_time_str, '\0', 80);
-                        gmtime_r(&new_scan_time, &tm_scan);
-                        strftime(new_scan_time_str, sizeof(new_scan_time_str), "%Y-%m-%dT%H:%M:%SZ", &tm_scan);
-
-                        mtdebug1(WM_GITHUB_LOGTAG, "Bookmark updated to '%s', waiting '%ld' seconds to run next scan", new_scan_time_str, github_config->interval);
-                    }
-
+                    mtdebug1(WM_GITHUB_LOGTAG, "Bookmark updated to '%s', waiting '%ld' seconds to run next scan", new_scan_time_str, github_config->interval);
                     if (wm_state_io(org_state_name, WM_IO_WRITE, &org_state_struc, sizeof(org_state_struc)) < 0) {
                         mterror(WM_GITHUB_LOGTAG, "Couldn't save running state.");
                     }
