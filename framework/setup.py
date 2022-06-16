dd99a22806784a5845cf91a6835c652f14bad245
Dwordcito <octa.tala@gmail.com>
2022-06-15 23:12:23 -0300

    Bump version to 4.3.5

--- framework/setup.py
+++ framework/setup.py
@@ -40,7 +40,7 @@ class InstallCommand(install):
 
 
 setup(name='wazuh',
-      version='4.3.4',
+      version='4.3.5',
       description='Wazuh control with Python',
       url='https://github.com/wazuh',
       author='Wazuh',
