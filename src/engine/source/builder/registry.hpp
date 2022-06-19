82f3c2d2197dda5a4cc4ed6f7f13f8b2de7a195f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-06-19 08:29:38 +0000

    Updated Registry to new interface using Expressions

--- src/engine/source/builder/registry.hpp
+++ src/engine/source/builder/registry.hpp
@@ -1,45 +1,101 @@
-/* Copyright (C) 2015-2021, Wazuh Inc.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation.
- */
-
 #ifndef _REGISTRY_H
 #define _REGISTRY_H
 
+#include <any>
+#include <functional>
+#include <memory>
 #include <string>
 #include <unordered_map>
 
-#include "builderTypes.hpp"
+#include <fmt/format.h>
+
+#include "builder/expression.hpp"
 
 namespace builder::internals
 {
 
+using Builder = std::function<Expression(std::any)>;
+
+/**
+ * @brief Registry of builders.
+ *
+ * This class is used to register builders and to get builders by name.
+ */
 class Registry
 {
 private:
-    static std::unordered_map<std::string, types::BuilderVariant> m_registry;
+    std::unordered_map<std::string, Builder> m_builders;
+
+    Registry() = default;
+    Registry(const Registry&) = delete;
+    Registry& operator=(const Registry&) = delete;
+    Registry(Registry&&) = delete;
+    Registry& operator=(Registry&&) = delete;
 
 public:
     /**
-     * @brief
+     * @brief Get Registry instance.
      *
-     * @param builderName
-     * @param builder
+     * @return auto& Registry instance.
      */
-    static void registerBuilder(const std::string &builderName,
-                                const types::BuilderVariant &builder);
+    static auto& instance()
+    {
+        static Registry instance;
+        return instance;
+    }
 
     /**
      * @brief Get the Builder object
      *
-     * @param builderName
-     * @return builder_t
+     * @param name Name of the builder.
+     * @return Builder& Builder object reference.
      */
-    static types::BuilderVariant getBuilder(const std::string &builderName);
+    static Builder& getBuilder(const std::string& name)
+    {
+        if (Registry::instance().m_builders.find(name)
+            == Registry::instance().m_builders.end())
+        {
+            throw std::runtime_error(fmt::format(
+                "[Registry::getBuilder(name)] name not found in the registry: [{}]",
+                name));
+        }
+        return Registry::instance().m_builders.at(name);
+    }
+
+    /**
+     * @brief Register a builder.
+     *
+     * @param builder Builder object.
+     * @param names Names of the builder.
+     */
+    template<typename... Names>
+    static void registerBuilder(Builder builder, Names... names)
+    {
+        for (auto name : {names...})
+        {
+            if (Registry::instance().m_builders.find(name)
+                == Registry::instance().m_builders.end())
+            {
+                Registry::instance().m_builders.insert(std::make_pair(name, builder));
+            }
+            else
+            {
+                throw std::logic_error(fmt::format(
+                    "Error, trying to register a builder with name [{}], but a "
+                    "builder with that name already exists",
+                    name));
+            }
+        }
+    }
+
+    /**
+     * @brief Clear the registry.
+     *
+     */
+    static void clear()
+    {
+        Registry::instance().m_builders.clear();
+    }
 };
 
 } // namespace builder::internals
