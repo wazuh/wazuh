61af10039702464b94665bd0a657b99eb0c7ae1e
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-06-19 08:29:38 +0000

    Updated Registry to new interface using Expressions

--- src/engine/test/source/builder/registry_test.cpp
+++ src/engine/test/source/builder/registry_test.cpp
@@ -1,48 +1,88 @@
 #include <gtest/gtest.h>
-#include <rxcpp/rx.hpp>
 
 #include "registry.hpp"
-#include "testUtils.hpp"
-#include <json.hpp>
 
-using namespace base;
+using namespace builder::internals;
 
-using FakeTrFn = std::function<void(std::string)>;
-FakeTrFn tr = [](std::string msg){};
-
-Lifter builderDummy(const Document &t, FakeTrFn tr)
+Expression builderDummy(std::any)
 {
-    return [](Observable o)
+    Expression expression;
+    return expression;
+}
+
+class RegistryTest : public ::testing::Test
+{
+protected:
+    void TearDown() override
     {
-        return o;
-    };
+        Registry::clear();
+    }
+};
+
+TEST_F(RegistryTest, RegisterBuilder)
+{
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test"));
 }
 
-TEST(Registry_test, RegisterBuilder)
+TEST_F(RegistryTest, RegisterBuilderMultipleNames)
 {
-    ASSERT_NO_THROW(Registry::registerBuilder("test", builderDummy));
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test", "test2", "test3"));
 }
 
-TEST(Registry_test, RegisterDuplicatedBuilder)
+TEST_F(RegistryTest, RegisterBuilderDuplicateName)
 {
-    ASSERT_THROW(Registry::registerBuilder("test", builderDummy),
-                 invalid_argument);
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test"));
+    ASSERT_THROW(Registry::registerBuilder(builderDummy, "test"), std::logic_error);
 }
 
-TEST(Registry_test, GetBuilder)
+TEST_F(RegistryTest, GetBuilder)
 {
-    ASSERT_NO_THROW(auto buildB = Registry::getBuilder("test"));
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test"));
+    ASSERT_NO_THROW(Registry::getBuilder("test"));
 }
 
-TEST(Registry_test, GetNonExistentBuilder)
+TEST_F(RegistryTest, GetBuilderMultipleNames)
 {
-    ASSERT_THROW(auto buildB = Registry::getBuilder("error"), invalid_argument);
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test", "test2", "test3"));
+    ASSERT_NO_THROW(Registry::getBuilder("test"));
+    ASSERT_NO_THROW(Registry::getBuilder("test2"));
+    ASSERT_NO_THROW(Registry::getBuilder("test3"));
 }
 
-TEST(Registry_test, GetBuilderAndBuilds)
+TEST_F(RegistryTest, GetBuilderNotRegistered)
 {
-    auto buildB = std::get<types::OpBuilder>(Registry::getBuilder("test"));
-    base::Observable o = rxcpp::observable<>::empty<base::Event>();
-    base::Observable expected = buildB(Document(R"({"test":1})").get("/test"), tr)(o);
-    ASSERT_EQ(o, expected);
+    ASSERT_THROW(Registry::getBuilder("test"), std::runtime_error);
+}
+
+TEST_F(RegistryTest, Clear)
+{
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test"));
+    ASSERT_NO_THROW(Registry::clear());
+    ASSERT_THROW(Registry::getBuilder("test"), std::runtime_error);
+}
+
+TEST_F(RegistryTest, ClearTwice)
+{
+    ASSERT_NO_THROW(Registry::clear());
+    ASSERT_NO_THROW(Registry::clear());
+}
+
+TEST_F(RegistryTest, UseCase)
+{
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test"));
+    Builder builder;
+    ASSERT_NO_THROW(builder = Registry::getBuilder("test"));
+    ASSERT_NO_THROW(builder(std::any {}));
+
+    ASSERT_THROW(Registry::getBuilder("test2"), std::runtime_error);
+
+    ASSERT_NO_THROW(Registry::registerBuilder(builderDummy, "test2"));
+    ASSERT_NO_THROW(builder = Registry::getBuilder("test2"));
+    ASSERT_NO_THROW(builder(std::any {}));
+
+    ASSERT_THROW(Registry::registerBuilder(builderDummy, "test"), std::logic_error);
+
+    ASSERT_NO_THROW(Registry::clear());
+    ASSERT_THROW(Registry::getBuilder("test"), std::runtime_error);
+    ASSERT_THROW(Registry::getBuilder("test2"), std::runtime_error);
 }
