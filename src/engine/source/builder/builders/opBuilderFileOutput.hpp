845e64dceea96a8dc13df3b8b385871b620b9932
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-06-21 14:02:27 +0000

    Added output builders

--- src/engine/source/builder/builders/opBuilderFileOutput.hpp
+++ src/engine/source/builder/builders/opBuilderFileOutput.hpp
@@ -1,16 +1,9 @@
-/* Copyright (C) 2015-2021, Wazuh Inc.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation.
- */
-
 #ifndef _OP_BUILDER_FILE_OUTPUT_H
 #define _OP_BUILDER_FILE_OUTPUT_H
 
-#include "builderTypes.hpp"
+#include <any>
+
+#include "expression.hpp"
 
 namespace builder::internals::builders
 {
@@ -18,10 +11,10 @@ namespace builder::internals::builders
 /**
  * @brief Builds file output operation.
  *
- * @param def Definition of the operation to be built
- * @return base::Lifter
+ * @param definition Definition of the operation to be built
+ * @return base::Expression
  */
-base::Lifter opBuilderFileOutput(const base::DocumentValue & def, types::TracerFn tr);
+base::Expression opBuilderFileOutput(const std::any& definition);
 
 } // namespace builder::internals::builders
 
