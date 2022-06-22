d4fe0f7cb9e9a9f6631ea260f88ce0fa19137b6f
LucioDonda <donda.lucio@gmail.com>
2022-06-22 13:20:46 -0300

    code style corrections and improovements on WDBSync and Unit tests

--- src/engine/source/builder/builders/opBuilderWdbSync.hpp
+++ src/engine/source/builder/builders/opBuilderWdbSync.hpp
@@ -12,36 +12,28 @@
 
 #include "builderTypes.hpp"
 
+constexpr std::string_view STREAM_SOCK_PATH = "/tmp/testStream.socket";
+
 namespace builder::internals::builders
 {
+
 /**
- * @brief 
- * @param def 
- * @param tr 
- * @return base::Lifter 
+ * @brief Executes query on WDB returning status ok or not ok.
+ * @param def Json Doc
+ * @param tr Tracer
+ * @return base::Lifter true when executes without any problem, false otherwise.
  */
 base::Lifter opBuilderWdbSyncUpdate(const base::DocumentValue& def,
                                 types::TracerFn tr);
 
 /**
- * @brief Builds KVDB extract function helper
- *
- * @param def
- * @return base::Lifter
+ * @brief Executes query on WDB returning status and payload.
+ * @param def Json Doc
+ * @param tr Tracer
+ * @return base::Lifter when true returns string of payload, false none.
  */
 base::Lifter opBuilderWdbSyncQuery(const base::DocumentValue& def,
                                    types::TracerFn tr);
-
-/**
- * @brief 
- * 
- * @param def 
- * @param tr 
- * @param returnPayload 
- * @return base::Lifter 
- */
-base::Lifter opBuilderWdbSyncGenericQuery(const base::DocumentValue& def,
-                                   types::TracerFn tr, bool returnPayload = true);
 } // namespace builder::internals::builders
 
 #endif // _OP_BUILDER_WDB_SYNC_H
