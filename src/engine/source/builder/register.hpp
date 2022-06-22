1ec64390b4ad7561dad914e03d67f35ef054b8b5
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-06-22 13:14:07 +0000

    Fixed header mismatch and added KVDB and helper exists to register

--- src/engine/source/builder/register.hpp
+++ src/engine/source/builder/register.hpp
@@ -9,24 +9,38 @@
 #include "builders/stageBuilderOutputs.hpp"
 #include "builders/stageBuilderParse.hpp"
 #include "builders/opBuilderLogqlParser.hpp"
+#include "builders/opBuilderKVDB.hpp"
+#include "builders/opBuilderHelperFilter.hpp"
 #include "registry.hpp"
 
 namespace builder::internals
 {
 static void registerBuilders()
 {
+    // Basic operations
     Registry::registerBuilder(builders::operationMapBuilder, "operation.map");
     Registry::registerBuilder(builders::operationConditionBuilder, "operation.condition");
 
+    // Stages
     Registry::registerBuilder(builders::stageCheckBuilder, "stage.check", "stage.allow");
     Registry::registerBuilder(builders::stageMapBuilder, "stage.map");
     Registry::registerBuilder(builders::stageNormalizeBuilder, "stage.normalize");
 
+    // Parsers
     Registry::registerBuilder(builders::stageBuilderParse, "stage.parse");
     Registry::registerBuilder(builders::opBuilderLogqlParser, "parser.logql");
 
+    // Outputs
     Registry::registerBuilder(builders::stageBuilderOutputs, "stage.outputs");
     Registry::registerBuilder(builders::opBuilderFileOutput, "output.file");
+
+    // KVDB
+    Registry::registerBuilder(builders::opBuilderKVDBExtract, "helper.kvdb_extract");
+    Registry::registerBuilder(builders::opBuilderKVDBMatch, "helper.kvdb_match");
+    Registry::registerBuilder(builders::opBuilderKVDBNotMatch, "helper.kvdb_not_match");
+
+    // Filter Helpers
+    Registry::registerBuilder(builders::opBuilderHelperExists, "helper.exists");
 }
 } // namespace builder::internals
 
