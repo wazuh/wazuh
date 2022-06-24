d26723caf41c4b19602975eacfcd6fa13795fc82
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-06-24 17:53:49 +0000

    Fix test name

--- src/engine/test/source/builder/builders/opBuilderHelperStringEqN_test.cpp
+++ src/engine/test/source/builder/builders/opBuilderHelperStringEqN_test.cpp
@@ -49,20 +49,20 @@ TEST(opBuilderHelperStringEqN, RawString)
 
     Observable input = observable<>::create<Event>([=](auto s) {
         s.on_next(createSharedEvent(R"(
-                {"sourceField":"not_test_value"}
-            )"));
+                {"sourceField":"sample_value"}
+            )")); // Shall not pass
         s.on_next(createSharedEvent(R"(
                 {"sourceField":"test_value_extended"}
-            )"));
+            )")); // Shall pass
         s.on_next(createSharedEvent(R"(
                 {"otherfield":"value"}
-            )"));
+            )")); // Shall not pass
         s.on_next(createSharedEvent(R"(
                 {"otherfield":"test_value"}
-            )"));
+            )")); // Shall not pass
         s.on_next(createSharedEvent(R"(
                 {"sourceField":"test_value_extended"}
-            )"));
+            )")); // Shall pass
         s.on_completed();
     });
 
@@ -85,7 +85,7 @@ TEST(opBuilderHelperStringEqN, TruncatedRawString)
 
     Observable input = observable<>::create<Event>([=](auto s) {
         s.on_next(createSharedEvent(R"(
-                {"sourceField":"not_test_value"}
+                {"sourceField":"sample_value"}
             )")); // Shall not pass
         s.on_next(createSharedEvent(R"(
                 {"sourceField":"test_value_extended"}
@@ -114,45 +114,6 @@ TEST(opBuilderHelperStringEqN, TruncatedRawString)
     ASSERT_EQ(expected.size(), 3);
 }
 
-// Test ok: static values (numbers, compare as string)
-TEST(opBuilderHelperStringEqN, RawNumber)
-{
-    Document doc {R"({
-        "check":
-            {"sourceField": "+s_eq_n/3/404911"}
-    })"};
-
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {"sourceField":012404}
-            )")); // Shall not pass
-        s.on_next(createSharedEvent(R"(
-                {"sourceField":404911}
-            )")); // Shall pass
-        s.on_next(createSharedEvent(R"(
-                {"otherfield":404}
-            )")); // Shall not pass
-        s.on_next(createSharedEvent(R"(
-                {"otherfield":404}
-            )")); // Shall not pass
-        s.on_next(createSharedEvent(R"(
-                {"sourceField":404}
-            )")); // Shall pass
-        s.on_next(createSharedEvent(R"(
-                {"sourceField":404666}
-            )")); // Shall pass
-        s.on_completed();
-    });
-
-    Lifter lift = [=](Observable input) {
-        return input.filter(opBuilderHelperStringEqN(doc.get("/check"), tr));
-    };
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 3);
-}
-
 // Test ok: dynamic values (string)
 TEST(opBuilderHelperStringEqN, ReferencedString)
 {
@@ -164,7 +125,7 @@ TEST(opBuilderHelperStringEqN, ReferencedString)
     Observable input = observable<>::create<Event>([=](auto s) {
         s.on_next(createSharedEvent(R"(
                 {
-                    "sourceField":"not_test_value",
+                    "sourceField":"sample_value",
                     "ref_key":"test_value"
                 }
             )")); // Shall not pass
@@ -196,8 +157,8 @@ TEST(opBuilderHelperStringEqN, ReferencedString)
                 {
                     "sourceField":"test_value_extra_extended",
                     "ref_key":"test_value_extended"
-                } // Shall pass
-            )"));
+                }
+            )")); // Shall pass
         s.on_completed();
     });
 
@@ -215,7 +176,7 @@ TEST(opBuilderHelperStringEqN, NestedReferencedStrings)
 {
     Document doc {R"({
         "check":
-            {"rootKey1.sourceField": "+s_eq_n/1/$rootKey2.ref_key"}
+            {"rootKey1.sourceField": "+s_eq_n/10/$rootKey2.ref_key"}
     })"};
 
     Observable input = observable<>::create<Event>([=](auto s) {
@@ -233,11 +194,11 @@ TEST(opBuilderHelperStringEqN, NestedReferencedStrings)
             )")); // Shall not pass
         s.on_next(createSharedEvent(R"(
                 {
-                    "parentObjt_2": {
+                    "rootKey2": {
                         "sourceField": "test_value",
                         "ref_key": "sample_value"
                     },
-                    "parentObjt_1": {
+                    "rootKey1": {
                         "sourceField": "sample_value",
                         "ref_key": "test_value"
                     }
@@ -245,11 +206,11 @@ TEST(opBuilderHelperStringEqN, NestedReferencedStrings)
             )")); // Shall pass
         s.on_next(createSharedEvent(R"(
                 {
-                    "parentObjt_2": {
+                    "rootKey2": {
                         "sourceField": "test_value_extended",
                         "ref_key": "sample_value"
                     },
-                    "parentObjt_1": {
+                    "rootKey1": {
                         "sourceField": "sample_value",
                         "ref_key": "test_value"
                     }
@@ -257,82 +218,13 @@ TEST(opBuilderHelperStringEqN, NestedReferencedStrings)
             )")); // Shall pass
         s.on_next(createSharedEvent(R"(
                 {
-                    "parentObjt_2": {
+                    "rootKey2": {
                         "sourceField": "test_value_extended",
                         "ref_key": "sample_value"
                     },
-                    "parentObjt_1": {
+                    "rootKey1": {
                         "sourceField": "sample_value",
                         "ref_key": "test_value_extra_extended"
-                    }
-                }
-            )")); // Shall pass
-        s.on_completed();
-    });
-
-    Lifter lift = [=](Observable input) {
-        return input.filter(opBuilderHelperStringEqN(doc.get("/check"), tr));
-    };
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 3);
-}
-
-// Test ok: multilevel referenced values (int)
-TEST(opBuilderHelperStringEqN, NestedReferencedStrings)
-{
-    Document doc {R"({
-        "check":
-            {"rootKey1.sourceField": "+s_eq_n/1/$rootKey2.ref_key"}
-    })"};
-
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                    "rootKey2": {
-                        "sourceField": 404,
-                        "ref_key": 404
-                    },
-                    "rootKey1": {
-                        "sourceField": 911,
-                        "ref_key": 911
-                    }
-                }
-            )")); // Shall not pass
-        s.on_next(createSharedEvent(R"(
-                {
-                    "parentObjt_2": {
-                        "sourceField": 404,
-                        "ref_key": 911
-                    },
-                    "parentObjt_1": {
-                        "sourceField": 911,
-                        "ref_key": 404
-                    }
-                }
-            )")); // Shall pass
-        s.on_next(createSharedEvent(R"(
-                {
-                    "parentObjt_2": {
-                        "sourceField": 404666,
-                        "ref_key": 911
-                    },
-                    "parentObjt_1": {
-                        "sourceField": 911,
-                        "ref_key": 404
-                    }
-                }
-            )")); // Shall pass
-        s.on_next(createSharedEvent(R"(
-                {
-                    "parentObjt_2": {
-                        "sourceField": 404666,
-                        "ref_key": 911
-                    },
-                    "parentObjt_1": {
-                        "sourceField": 911,
-                        "ref_key": 404123321
                     }
                 }
             )")); // Shall pass
