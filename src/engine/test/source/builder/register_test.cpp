b210e97841e9a65a8b7c5af74f7072c0a4648075
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-07-01 08:08:12 +0000

    Updated register and registry tests

--- src/engine/test/source/builder/register_test.cpp
+++ src/engine/test/source/builder/register_test.cpp
@@ -21,7 +21,45 @@ TEST(RegisterTest, AllBuildersRegistered)
     // Check all builders have been registered
     ASSERT_NO_THROW(Registry::getBuilder("operation.map"));
     ASSERT_NO_THROW(Registry::getBuilder("operation.condition"));
+
     ASSERT_NO_THROW(Registry::getBuilder("stage.check"));
+    ASSERT_NO_THROW(Registry::getBuilder("stage.allow"));
     ASSERT_NO_THROW(Registry::getBuilder("stage.map"));
     ASSERT_NO_THROW(Registry::getBuilder("stage.normalize"));
+
+    ASSERT_NO_THROW(Registry::getBuilder("stage.parse"));
+    ASSERT_NO_THROW(Registry::getBuilder("parser.logql"));
+
+    ASSERT_NO_THROW(Registry::getBuilder("stage.outputs"));
+    ASSERT_NO_THROW(Registry::getBuilder("output.file"));
+
+    ASSERT_NO_THROW(Registry::getBuilder("helper.kvdb_extract"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.kvdb_match"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.kvdb_not_match"));
+
+    ASSERT_NO_THROW(Registry::getBuilder("helper.exists"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.not_exists"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_eq"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_ne"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_gt"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_ge"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_lt"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_le"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_eq"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_ne"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_gt"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_ge"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_lt"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_le"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.r_match"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.r_not_match"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.ip_cidr"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_contains"));
+
+    ASSERT_NO_THROW(Registry::getBuilder("helper.i_calc"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_up"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_lo"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_trim"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.r_ext"));
+    ASSERT_NO_THROW(Registry::getBuilder("helper.s_append"));
 }
