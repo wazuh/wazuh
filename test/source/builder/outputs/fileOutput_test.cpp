1575c4b1fa724f34d844aa9eb652d0eb0198445b
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-07-01 07:15:41 +0000

    Updated file output test

--- test/source/builder/outputs/fileOutput_test.cpp
+++ test/source/builder/outputs/fileOutput_test.cpp
@@ -1,4 +1,3 @@
-#include <rxcpp/rx.hpp>
 #include <gtest/gtest.h>
 
 #include <algorithm>
@@ -11,17 +10,13 @@
 #include <fstream>
 #include <iostream>
 
-#include "json/json.hpp"
-#include "test_utils.hpp"
+#include "baseTypes.hpp"
 
 #include "outputs/file.hpp"
 
-namespace bld = builder::internals;
-
-auto createSharedEvent = [](const char * json){
-    return std::make_shared<base::EventHandler>(std::make_shared<base::Document>(json));
-};
-
+using namespace builder::internals::outputs;
+using namespace base;
+using namespace json;
 
 auto messageStr = R"({
     "event": {
@@ -62,29 +57,47 @@ auto compact_message =
     R"({"event":{"original":"::1 - - [26/Dec/2016:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209\n"},"wazuh":{"agent":{"id":"001","name":"agentSim","version":"PoC"},"event":{"format":"text","id":"9aa69e7b-e1b0-530e-a710-49108e86019b","ingested":"2021-10-26T16:50:34.348945Z","kind":"event"},"host":{"architecture":"x86_64","hostname":"hive","ip":"127.0.1.1","mac":"B0:7D:64:11:B3:13","os":{"kernel":"5.14.14-arch1-1","name":"Linux","type":"posix","version":"#1 SMP PREEMPT Wed, 20 Oct 2021 21:35:18 +0000"}},"module":{"name":"logcollector","source":"apache-access"}}}
 )";
 
-TEST(FileOutput, Create)
+constexpr auto FILE_PATH = "/tmp/file";
+
+class FileOutputTest : public ::testing::Test
 {
-    auto output = bld::outputs::FileOutput("/tmp/file");
-    std::filesystem::remove("/tmp/file");
+    void SetUp() override
+    {
+        if (std::filesystem::exists(FILE_PATH))
+        {
+            std::filesystem::remove(FILE_PATH);
+        }
+    }
+
+    void TearDown() override
+    {
+        if (std::filesystem::exists(FILE_PATH))
+        {
+            std::filesystem::remove(FILE_PATH);
+        }
+    }
+};
+
+TEST_F(FileOutputTest, Create)
+{
+    ASSERT_NO_THROW(FileOutput(std::string {FILE_PATH}));
+    ASSERT_TRUE(std::filesystem::exists(FILE_PATH));
 }
 
-TEST(FileOutput, Unknown_path)
+TEST_F(FileOutputTest, UnknownPath)
 {
-    ASSERT_THROW(bld::outputs::FileOutput("/tmp45/file"), std::invalid_argument);
+    ASSERT_THROW(FileOutput("/tmp45/file"), std::invalid_argument);
 }
 
-TEST(FileOutput, Write)
+TEST_F(FileOutputTest, Write)
 {
-    auto filepath = "/tmp/file";
-    auto msg = createSharedEvent(messageStr);
-    auto output = bld::outputs::FileOutput(filepath);
-    output.write(msg);
+    auto msg = std::make_shared<Json>(messageStr);
+    auto output = FileOutput(FILE_PATH);
+    ASSERT_NO_THROW(output.write(msg));
 
-    std::ifstream ifs(filepath);
+    std::ifstream ifs(FILE_PATH);
     std::stringstream buffer;
     buffer << ifs.rdbuf();
 
-    GTEST_COUT << buffer.str() << std::endl;
-    ASSERT_TRUE(buffer.str() == compact_message);
-    std::filesystem::remove(filepath);
+    ASSERT_EQ(buffer.str(), compact_message);
 }
