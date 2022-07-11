814b6191afe3a1cfee6631408a418c5c9934c48d
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-07-11 02:25:53 +0000

    PR review changes implemented

--- src/engine/source/builder/builders/opBuilderARWrite.cpp
+++ src/engine/source/builder/builders/opBuilderARWrite.cpp
@@ -17,8 +17,6 @@ using base::utils::socketInterface::SendRetval;
 using base::utils::socketInterface::unixDatagram;
 using builder::internals::syntax::REFERENCE_ANCHOR;
 using rapidjson::Value;
-using std::runtime_error;
-using std::string;
 using utils::string::split;
 
 namespace builder::internals::builders
@@ -29,12 +27,12 @@ base::Lifter opBuilderARWrite(const base::DocumentValue& def, types::TracerFn tr
     // The first parameter should be a string
     if (!def.MemberBegin()->value.IsString())
     {
-        throw runtime_error(
+        throw std::runtime_error(
             "Write AR operator: Invalid parameter type (string expected).");
     }
 
     // This is the left side operator, which is set with the operation result.
-    const string resultOperatorKey {
+    const std::string resultOperatorKey {
         json::formatJsonPath(def.MemberBegin()->name.GetString())};
 
     // Parse parameters
@@ -43,109 +41,118 @@ base::Lifter opBuilderARWrite(const base::DocumentValue& def, types::TracerFn tr
     // It is expected to have only two parameters (operation and query)
     if (parametersArr.size() != 2)
     {
-        throw runtime_error(
+        throw std::runtime_error(
             "Write AR operator: Invalid number of parameters (two expected).");
     }
 
     // Check for empty parameter
     if (parametersArr.at(1).empty())
     {
-        throw runtime_error(
+        throw std::runtime_error(
             "Write AR operator: Invalid empty parameters (not empty expected).");
     }
 
     // Assigned to parameter in order to avoid handling array with 1 value
-    const string opParameter = parametersArr.at(1);
+    const std::string opParameter = parametersArr.at(1);
 
     base::Document doc {def};
 
+    std::shared_ptr<unixDatagram> socketAR = nullptr;
+
+    try
+    {
+        socketAR = std::make_shared<unixDatagram>(AR_QUEUE_PATH);
+    }
+    catch (const std::exception& e)
+    {
+        throw std::runtime_error(
+            "Write AR operator: Error when creating the unixDatagram object.");
+    }
+
     // Return Lifter
-    return [&, resultOperatorKey, opParameter, tr = std::move(tr)](base::Observable o) {
-        return o.map([&, resultOperatorKey, opParameter, tr = std::move(tr)](
-                         base::Event e) {
-            string query {};
-            bool messageSent {false};
+    return [&, doc, socketAR, resultOperatorKey, opParameter, tr = std::move(tr)](
+               base::Observable o) {
+        return o.map(
+            [&, doc, socketAR, resultOperatorKey, opParameter, tr = std::move(tr)](
+                base::Event e) {
+                std::string query {};
+                bool messageSent {false};
 
-            // Check if the value comes from a reference
-            if (REFERENCE_ANCHOR == opParameter[0])
-            {
-                // Gets the referenced key (without the reference anchor)
-                const auto key = json::formatJsonPath(opParameter.substr(1));
-
-                try
+                // Check if the value comes from a reference
+                if (REFERENCE_ANCHOR == opParameter[0])
                 {
-                    // Gets the value referenced by the key
-                    auto value =
-                        (opParameter.length() > 1) ? &e->getEventValue(key) : nullptr;
+                    // Gets the referenced key (without the reference anchor)
+                    const auto key = json::formatJsonPath(opParameter.substr(1));
 
-                    if (value && value->IsString())
+                    try
                     {
-                        query = value->GetString();
+                        // Gets the value referenced by the key
+                        // const auto &value = e->getEvent()->get("key");
+                        const auto& value = e->getEventValue(key);
 
-                        if (query.empty())
+                        if (value.IsString())
                         {
-                            const string msg {AR_INVALID_REFERENCE_MSG};
+                            query = value.GetString();
+
+                            if (query.empty())
+                            {
+                                const std::string msg {AR_INVALID_REFERENCE_MSG};
+                                tr(msg);
+                            }
+                        }
+                        else
+                        {
+                            const std::string msg {AR_INVALID_REFERENCE_MSG};
                             tr(msg);
-                            WAZUH_LOG_ERROR(msg);
                         }
                     }
-                    else
+                    catch (std::exception& exception)
                     {
-                        const string msg {AR_INVALID_REFERENCE_MSG};
+                        const std::string msg =
+                            std::string {"Write AR operator exception: "}
+                            + exception.what();
                         tr(msg);
-                        WAZUH_LOG_ERROR(msg);
                     }
                 }
-                catch (std::exception& exception)
+                else // It is a direct value
                 {
-                    const string msg =
-                        string {"Write AR operator exception: "} + exception.what();
-                    tr(msg);
-                    WAZUH_LOG_ERROR(msg);
+                    query = opParameter;
                 }
-            }
-            else // It is a direct value
-            {
-                query = opParameter;
-            }
 
-            if (!query.empty())
-            {
-                try
+                if (!query.empty())
                 {
-                    unixDatagram socketAR(AR_QUEUE_PATH);
-
-                    if (socketAR.sendMsg(query) == SendRetval::SUCCESS)
+                    try
                     {
-                        messageSent = true;
+                        if (socketAR->sendMsg(query) == SendRetval::SUCCESS)
+                        {
+                            messageSent = true;
 
-                        const string msg {
-                            "Write AR operator info: AR message sent. Query: " + query};
-                        tr(msg);
-                        WAZUH_LOG_DEBUG(msg);
+                            const std::string msg {
+                                "Write AR operator info: AR message sent. Query: "
+                                + query};
+                            tr(msg);
+                        }
+                        else
+                        {
+                            const std::string msg {
+                                "Write AR operator error: AR message not sent. Document: "
+                                + doc.str()};
+                            tr(msg);
+                        }
                     }
-                    else
+                    catch (const std::exception& exception)
                     {
-                        const string msg {
-                            "Write AR operator error: AR message not sent. Document: "
-                            + doc.str()};
+                        const std::string msg =
+                            std::string {"Write AR operator sendMsg() exception: "}
+                            + exception.what();
                         tr(msg);
-                        WAZUH_LOG_ERROR(msg);
                     }
                 }
-                catch (const std::exception& exception)
-                {
-                    const string msg = string {"Write AR operator sendMsg() exception: "}
-                                       + exception.what();
-                    tr(msg);
-                    WAZUH_LOG_ERROR(msg);
-                }
-            }
 
-            e->setEventValue(resultOperatorKey, Value(messageSent).Move());
+                e->setEventValue(resultOperatorKey, Value(messageSent).Move());
 
-            return e;
-        });
+                return e;
+            });
     };
 }
 
