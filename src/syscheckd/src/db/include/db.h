0e297060b13abac3f665ef33248c5c12ee5c99c1
Rivero, Franco <francorivero2012@gmail.com>
2022-07-12 12:57:16 -0300

    Add changes requested by Antonio

--- src/syscheckd/src/db/include/db.h
+++ src/syscheckd/src/db/include/db.h
@@ -52,7 +52,7 @@ FIMDBErrorCode fim_db_init(int storage,
                            int value_limit,
                            bool sync_registry_enabled,
                            int sync_thread_pool,
-                           int sync_queue_size);
+                           unsigned int sync_queue_size);
 
 /**
  * @brief Get entry data using path.
