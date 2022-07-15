9f4038e5aa985fae284c6367cda1f4e08b1e8519
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-07-15 10:18:51 +0000

    Added Graph tests and Refactored methods for robustness - Modified environment.hpp to update changes on Graph

--- src/engine/source/base/graph.hpp
+++ src/engine/source/base/graph.hpp
@@ -1,138 +1,315 @@
 #ifndef _GRAPH_H
 #define _GRAPH_H
 
+#include <functional>
+#include <queue>
 #include <sstream>
 #include <stack>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
+#include <vector>
 
+#include <fmt/format.h>
+
+/**
+ * @brief Simple graph class.
+ *
+ * @tparam K Type of the key for the graph's nodes.
+ * @tparam T Type of the value for the graph's nodes.
+ */
 template<typename K, typename T>
 class Graph
 {
-public:
+private:
     K m_root;
     std::unordered_map<K, T> m_nodes;
     std::unordered_map<K, std::vector<K>> m_edges;
 
+    void _addNode(K key, T&& value)
+    {
+        if (m_nodes.end() != m_nodes.find(key))
+        {
+            throw std::runtime_error(fmt::format("Node [{}] already exists", key));
+        }
+        m_nodes[key] = std::move(value);
+    }
+
+    void _addEdge(K from, K to)
+    {
+        if (m_edges.end() == m_edges.find(from))
+        {
+            m_edges[from] = std::vector<K> {};
+            m_edges[from].push_back(to);
+        }
+        else
+        {
+            if (m_edges[from].end()
+                != std::find(m_edges[from].begin(), m_edges[from].end(), to))
+            {
+                throw std::runtime_error(
+                    fmt::format("Edge [{} -> {}] already exists", from, to));
+            }
+            else
+            {
+                m_edges[from].push_back(to);
+            }
+        }
+    }
+
+    void _deleteNode(K key)
+    {
+        if (m_nodes.end() == m_nodes.find(key))
+        {
+            throw std::runtime_error(fmt::format("Node [{}] does not exist", key));
+        }
+        m_nodes.erase(key);
+        m_edges.erase(key);
+    }
+
+    void _preOrder(K key, std::function<void(const K&, const T&)> visitor) const
+    {
+        visitor(key, m_nodes.at(key));
+        if (hasChildren(key))
+        {
+            for (auto child : children(key))
+            {
+                _preOrder(child, visitor);
+            }
+        }
+    }
+
 public:
+    /**
+     * @brief Construct a new Graph empty object
+     *
+     */
     Graph() = default;
 
+    /**
+     * @brief Construct a new Graph object
+     *
+     * @param rootId Root node id.
+     * @param root Root node value.
+     */
     Graph(K rootId, T root)
         : m_root {rootId}
         , m_nodes {std::make_pair(rootId, root)}
     {
     }
 
+    /**
+     * @brief Set the root node.
+     *
+     * @return K Root node id.
+     */
+    void setRoot(K rootId, T rootValue)
+    {
+        if (m_nodes.end() != m_nodes.find(m_root))
+        {
+            _deleteNode(m_root);
+        }
+        m_root = rootId;
+        _addNode(rootId, std::move(rootValue));
+    }
+
+    /**
+     * @brief Add node to graph.
+     *
+     * @param id Node id.
+     * @param node Node value.
+     */
     void addNode(K id, T node)
     {
-        m_nodes.insert(std::make_pair(id, node));
-        if (m_edges.find(id) == m_edges.end())
-        {
-            m_edges.insert(std::make_pair(id, std::vector<K>{}));
-        }
+        _addNode(id, std::move(node));
     }
 
+    /**
+     * @brief Add directed edge to graph.
+     *
+     * @param from Node id.
+     * @param to Node id.
+     */
     void addEdge(K from, K to)
     {
-        m_edges[from].push_back(to);
+        _addEdge(from, to);
     }
 
+    /**
+     * @brief Inject node in graph.
+     *
+     * When injecting a node (id, node) in the graph under parent, all children of parent
+     * will be added as children of the new node, and the parent will have the new node as
+     * child.
+     *
+     * @param id Injected node id.
+     * @param node Injected node value.
+     * @param parent Parent node id.
+     */
     void injectNode(K id, T node, K parent)
     {
-        addNode(id, node);
+        _addNode(id, std::move(node));
         for (auto& child : m_edges[parent])
         {
-            addEdge(id, child);
+            _addEdge(id, child);
         }
         m_edges[parent].clear();
-        m_edges[parent].push_back(id);
+        _addEdge(parent, id);
     }
 
-    const K& root() const
+    /**
+     * @brief Get the root node id.
+     *
+     * @return const K& Root node id.
+     */
+    const K& rootId() const
     {
         return m_root;
     }
 
+    /**
+     * @brief Get the node value.
+     *
+     * @param id Id of the node.
+     * @return const T& Value of the node.
+     */
     const T& node(K id) const
     {
+        if (m_nodes.end() == m_nodes.find(id))
+        {
+            throw std::runtime_error(fmt::format("Node [{}] does not exist", id));
+        }
         return m_nodes.at(id);
     }
 
+    /**
+     * @brief Get the node children.
+     *
+     * @param id Id of the node.
+     * @return const std::vector<K>& Children of the node.
+     *
+     * @throws std::runtime_error if node does not have children.
+     */
+    const std::vector<K>& children(K id) const
+    {
+        if (m_edges.end() == m_edges.find(id))
+        {
+            throw std::runtime_error(fmt::format("Node [{}] has no children", id));
+        }
+        return m_edges.at(id);
+    }
+
+    /**
+     * @brief Get the edges map.
+     *
+     * @return const std::unordered_map<K, std::vector<K>>&
+     */
+    const std::unordered_map<K, std::vector<K>>& edges() const
+    {
+        return m_edges;
+    }
+
+    /**
+     * @brief Get the nodes map.
+     *
+     * @return const std::unordered_map<K, T>&
+     */
+    const std::unordered_map<K, T>& nodes() const
+    {
+        return m_nodes;
+    }
+
+    /**
+     * @brief Check if node exists.
+     *
+     * @param id Node id.
+     * @return true If node exists.
+     * @return false If node does not exist.
+     */
     bool hasNode(K id) const
     {
         return m_nodes.find(id) != m_nodes.end();
     }
 
+    /**
+     * @brief Check if node has children.
+     *
+     * @param id Node id.
+     * @return true If node has children.
+     * @return false If node does not have children.
+     */
     bool hasChildren(K id) const
     {
         auto it = m_edges.find(id);
         return it != m_edges.end() && !it->second.empty();
     }
 
-    // visit pre-order
+    /**
+     * @brief Check if graph is empty.
+     *
+     * @return true
+     * @return false
+     */
+    bool empty() const
+    {
+        return m_nodes.empty();
+    }
+
+    /**
+     * @brief Preorder traversal of the graph.
+     *
+     * @warning If multiple parent nodes have the same child, the child will be visited
+     * multiple times.
+     *
+     * @param visitor Function to call for each node.
+     */
     void visit(const std::function<void(const K&, const T&)>& visitor)
     {
-        std::stack<K> stack;
-        stack.push(m_root);
-
-        while (!stack.empty())
-        {
-            auto id = stack.top();
-            stack.pop();
-
-            visitor(id, m_nodes.at(id));
-
-            for (auto& edge : m_edges.at(id))
-            {
-                stack.push(edge);
-            }
-        }
+        // preorder traversal
+        _preOrder(m_root, visitor);
     }
 
-    void visitLeafs(const std::function<void(const K&, const T&)>& visitor)
+    /**
+     * @brief Preorder traversal of all leaf nodes.
+     *
+     * @warning If multiple parent nodes have the same leaf child, the child will be
+     * visited multiple times.
+     *
+     * @param visitor Function to call for each leaf node.
+     */
+    void visitLeaves(const std::function<void(const K&, const T&)>& visitor)
     {
-        std::stack<K> stack;
-        stack.push(m_root);
-
-        while (!stack.empty())
+        auto _visitor = [&](const K& id, const T& node)
         {
-            auto id = stack.top();
-            stack.pop();
-
-            if (m_edges.at(id).empty())
+            if (!hasChildren(id))
             {
-                visitor(id, m_nodes.at(id));
+                visitor(id, node);
             }
-            else
-            {
-                for (auto& edge : m_edges.at(id))
-                {
-                    stack.push(edge);
-                }
-            }
-        }
+        };
+        _preOrder(m_root, _visitor);
     }
 
-    // Graphivz
+    /**
+     * @brief Get the Graph Str object
+     *
+     * @return std::string Graphviz representation.
+     */
     std::string getGraphStr() const
     {
         std::stringstream ss;
         ss << "strict digraph G {\n";
 
-        auto visit = [&ss, this](const std::string& id, auto& visitRef) -> void
+        auto visitor = [&](const std::string& id, auto& visitRef) -> void
         {
-            if (m_edges.find(id) != m_edges.end())
+            if (hasChildren(id))
             {
-                for (auto& edge : m_edges.at(id))
+                for (auto& child : children(id))
                 {
-                    ss << fmt::format("{} -> {}\n", id, edge);
-                    visitRef(edge, visitRef);
+                    ss << fmt::format("\"{}\" -> \"{}\"\n", id, child);
                 }
             }
         };
 
-        visit(m_root, visit);
+        _preOrder(m_root, visitor);
         ss << "}\n";
         return ss.str();
     }
