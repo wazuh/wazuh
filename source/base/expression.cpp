e3d59d9d76d981d452bf3d3853992cca67be884e
Juan Cabrera <jcabre88@gmail.com>
2022-07-18 18:42:13 +0200

    Splited environment class into .hpp and .cpp file

--- source/base/expression.cpp
+++ source/base/expression.cpp
@@ -2,248 +2,248 @@
 
 namespace base
 {
-    using Expression = std::shared_ptr<Formula>;
+using Expression = std::shared_ptr<Formula>;
 
-    unsigned int Formula::generateId()
-    {
-        unsigned int id = 0;
-        return id++;
-    }
+unsigned int Formula::generateId()
+{
+    unsigned int id = 0;
+    return id++;
+}
 
-    Formula::Formula(std::string name, std::string typeName)
-        : m_id {generateId()}
-        , m_name {name}
-        , m_typeName {typeName}
-    {
-    }
+Formula::Formula(std::string name, std::string typeName)
+    : m_id {generateId()}
+    , m_name {name}
+    , m_typeName {typeName}
+{
+}
 
-    Formula::~Formula() = default;
+Formula::~Formula() = default;
 
-    bool Formula::isTerm() const
-    {
-        return false;
-    }
+bool Formula::isTerm() const
+{
+    return false;
+}
 
-    bool Formula::isOperation() const
-    {
-        return false;
-    }
+bool Formula::isOperation() const
+{
+    return false;
+}
 
-    bool Formula::isAnd() const
-    {
-        return false;
-    }
+bool Formula::isAnd() const
+{
+    return false;
+}
 
-    bool Formula::isOr() const
-    {
-        return false;
-    }
+bool Formula::isOr() const
+{
+    return false;
+}
 
-    bool Formula::isChain() const
-    {
-        return false;
-    }
+bool Formula::isChain() const
+{
+    return false;
+}
 
-    bool Formula::isImplication() const
-    {
-        return false;
-    }
+bool Formula::isImplication() const
+{
+    return false;
+}
 
-    bool Formula::isBroadcast() const
-    {
-        return false;
-    }
+bool Formula::isBroadcast() const
+{
+    return false;
+}
 
-    unsigned int Formula::getId() const
-    {
-        return m_id;
-    }
+unsigned int Formula::getId() const
+{
+    return m_id;
+}
 
-    std::string Formula::getTypeName() const
-    {
-        return m_typeName;
-    }
+std::string Formula::getTypeName() const
+{
+    return m_typeName;
+}
 
-    std::string Formula::getName() const
-    {
-        return m_name;
-    }
+std::string Formula::getName() const
+{
+    return m_name;
+}
 
-    Operation::Operation(std::string name,
-              std::string nameType,
-              std::vector<std::shared_ptr<Formula>> operands)
-        : Formula(name, nameType)
-        , m_operands {operands}
-    {
-    }
+Operation::Operation(std::string name,
+                     std::string nameType,
+                     std::vector<std::shared_ptr<Formula>> operands)
+    : Formula(name, nameType)
+    , m_operands {operands}
+{
+}
 
-    Operation::~Operation() = default;
+Operation::~Operation() = default;
 
-    bool Operation::isOperation() const
-    {
-        return true;
-    }
+bool Operation::isOperation() const
+{
+    return true;
+}
 
-    const std::vector<std::shared_ptr<Formula>>& Operation::getOperands() const
-    {
-        return m_operands;
-    }
+const std::vector<std::shared_ptr<Formula>>& Operation::getOperands() const
+{
+    return m_operands;
+}
 
-    std::vector<std::shared_ptr<Formula>>& Operation::getOperands()
-    {
-        return m_operands;
-    }
+std::vector<std::shared_ptr<Formula>>& Operation::getOperands()
+{
+    return m_operands;
+}
 
-    [[nodiscard]] std::shared_ptr<Implication>
-    Implication::create(std::string name,
-           std::shared_ptr<Formula> leftOperand,
-           std::shared_ptr<Formula> rightOperand)
-    {
-        return std::shared_ptr<Implication>(
-            new Implication {name, leftOperand, rightOperand});
-    }
+[[nodiscard]] std::shared_ptr<Implication>
+Implication::create(std::string name,
+                    std::shared_ptr<Formula> leftOperand,
+                    std::shared_ptr<Formula> rightOperand)
+{
+    return std::shared_ptr<Implication>(
+        new Implication {name, leftOperand, rightOperand});
+}
 
-    Implication::~Implication() = default;
+Implication::~Implication() = default;
 
-    bool Implication::isImplication() const
-    {
-        return true;
-    }
+bool Implication::isImplication() const
+{
+    return true;
+}
 
-    Implication::Implication(std::string name,
-                std::shared_ptr<Formula> leftOperand,
-                std::shared_ptr<Formula> rightOperand)
-        : Operation {name, "Implication", {leftOperand, rightOperand}}
-    {
-    }
+Implication::Implication(std::string name,
+                         std::shared_ptr<Formula> leftOperand,
+                         std::shared_ptr<Formula> rightOperand)
+    : Operation {name, "Implication", {leftOperand, rightOperand}}
+{
+}
 
-    [[nodiscard]] std::shared_ptr<And>
-    And::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-    {
-        return std::shared_ptr<And>(new And(name, operands));
-    }
+[[nodiscard]] std::shared_ptr<And>
+And::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+{
+    return std::shared_ptr<And>(new And(name, operands));
+}
 
-    And::~And() = default;
+And::~And() = default;
 
-    bool And::isAnd() const
-    {
-        return true;
-    }
+bool And::isAnd() const
+{
+    return true;
+}
 
-    And::And(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-        : Operation(name, "And", operands)
-    {
-    }
+And::And(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+    : Operation(name, "And", operands)
+{
+}
 
-    [[nodiscard]] std::shared_ptr<Or>
-    Or::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-    {
-        return std::shared_ptr<Or>(new Or(name, operands));
-    }
+[[nodiscard]] std::shared_ptr<Or>
+Or::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+{
+    return std::shared_ptr<Or>(new Or(name, operands));
+}
 
-    Or::~Or() = default;
+Or::~Or() = default;
 
-    bool Or::isOr() const
-    {
-        return true;
-    }
+bool Or::isOr() const
+{
+    return true;
+}
 
-    Or::Or(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-        : Operation(name, "Or", operands)
-    {
-    }
+Or::Or(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+    : Operation(name, "Or", operands)
+{
+}
 
-    [[nodiscard]] std::shared_ptr<Chain>
-    Chain::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-    {
-        return std::shared_ptr<Chain>(new Chain(name, operands));
-    }
+[[nodiscard]] std::shared_ptr<Chain>
+Chain::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+{
+    return std::shared_ptr<Chain>(new Chain(name, operands));
+}
 
-    Chain::~Chain() = default;
+Chain::~Chain() = default;
 
-    bool Chain::isChain() const
-    {
-        return true;
-    }
+bool Chain::isChain() const
+{
+    return true;
+}
 
-    Chain::Chain(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-        : Operation(name, "Chain", operands)
-    {
-    }
+Chain::Chain(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+    : Operation(name, "Chain", operands)
+{
+}
 
-    [[nodiscard]] std::shared_ptr<Broadcast>
-    Broadcast::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-    {
-        return std::shared_ptr<Broadcast>(new Broadcast(name, operands));
-    }
+[[nodiscard]] std::shared_ptr<Broadcast>
+Broadcast::create(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+{
+    return std::shared_ptr<Broadcast>(new Broadcast(name, operands));
+}
 
-    Broadcast::~Broadcast() = default;
+Broadcast::~Broadcast() = default;
 
-    bool Broadcast::isBroadcast() const
-    {
-        return true;
-    }
+bool Broadcast::isBroadcast() const
+{
+    return true;
+}
 
-    Broadcast::Broadcast(std::string name, std::vector<std::shared_ptr<Formula>> operands)
-        : Operation(name, "Broadcast", operands)
-    {
-    }
+Broadcast::Broadcast(std::string name, std::vector<std::shared_ptr<Formula>> operands)
+    : Operation(name, "Broadcast", operands)
+{
+}
 
-    std::string toGraphvizStr(Expression expression)
-    {
-        auto header = R"(
+std::string toGraphvizStr(Expression expression)
+{
+    auto header = R"(
     compound=true;
     fontname="Helvetica,Arial,sans-serif";
     fontsize=12;
     node [color="#57abff", fontname="Helvetica,Arial,sans-serif", fontsize=10, fontcolor="white"];
     edge [fontname="Helvetica,Arial,sans-serif", fontsize=8];
     )";
-        auto clusterHeader = R"(
+    auto clusterHeader = R"(
     style="rounded,filled";
     color="#57abff";
     )";
 
-        std::stringstream ss;
-        ss << "strict digraph G {" << std::endl << header << std::endl;
+    std::stringstream ss;
+    ss << "strict digraph G {" << std::endl << header << std::endl;
 
-        // Erase " from string
-        auto eraseQuotes = [](std::string str)
+    // Erase " from string
+    auto eraseQuotes = [](std::string str)
+    {
+        while (str.find("\"") != std::string::npos)
         {
-            while (str.find("\"") != std::string::npos)
-            {
-                str.replace(str.find("\""), 1, "'");
-            }
+            str.replace(str.find("\""), 1, "'");
+        }
 
-            return str;
-        };
-        auto visit = [&](Expression current, auto& visitRef) -> void
+        return str;
+    };
+    auto visit = [&](Expression current, auto& visitRef) -> void
+    {
+        ss << "subgraph cluster_" << current->getId() << " {" << std::endl
+           << clusterHeader << std::endl;
+        ss << "label=\"" << current->getTypeName() << "\";" << std::endl;
+        ss << current->getId() << " [label=\"" << eraseQuotes(current->getName()) << " ["
+           << current->getId() << "]\"];" << std::endl;
+        ss << "}" << std::endl;
+        if (current->isOperation())
         {
-            ss << "subgraph cluster_" << current->getId() << " {" << std::endl
-            << clusterHeader << std::endl;
-            ss << "label=\"" << current->getTypeName() << "\";" << std::endl;
-            ss << current->getId() << " [label=\"" << eraseQuotes(current->getName()) << " ["
-            << current->getId() << "]\"];" << std::endl;
-            ss << "}" << std::endl;
-            if (current->isOperation())
+            auto operation = current->getPtr<Operation>();
+            int i = 0;
+            for (auto& child : operation->getOperands())
             {
-                auto operation = current->getPtr<Operation>();
-                int i = 0;
-                for (auto& child : operation->getOperands())
-                {
-                    ss << fmt::format("{} -> {} [ltail=cluster_{} lhead=cluster_{} label={} "
-                                    "fontcolor=\"red\"];\n",
-                                    current->getId(),
-                                    child->getId(),
-                                    current->getId(),
-                                    child->getId(),
-                                    i++);
-                    visitRef(child, visitRef);
-                }
+                ss << fmt::format("{} -> {} [ltail=cluster_{} lhead=cluster_{} label={} "
+                                  "fontcolor=\"red\"];\n",
+                                  current->getId(),
+                                  child->getId(),
+                                  current->getId(),
+                                  child->getId(),
+                                  i++);
+                visitRef(child, visitRef);
             }
-        };
-        visit(expression, visit);
-        ss << "}\n";
-        return ss.str();
-    }
+        }
+    };
+    visit(expression, visit);
+    ss << "}\n";
+    return ss.str();
+}
 
 } // namespace base
--- source/builder/asset.cpp
+++ source/builder/asset.cpp
@@ -2,167 +2,165 @@
 
 namespace builder
 {
-    std::string Asset::typeToString(Asset::Type type)
+std::string Asset::typeToString(Asset::Type type)
+{
+    switch (type)
     {
-        switch (type)
+        case Asset::Type::DECODER: return "decoder";
+        case Asset::Type::RULE: return "rule";
+        case Asset::Type::OUTPUT: return "output";
+        case Asset::Type::FILTER: return "filter";
+        default:
+            throw std::runtime_error(
+                fmt::format("[Asset::typeToString(type)] unknown type: [{}]",
+                            static_cast<int>(type)));
+    }
+}
+
+Asset::Asset(std::string name, Asset::Type type)
+    : m_name {name}
+    , m_type {type}
+{
+}
+
+Asset::Asset(const json::Json& jsonDefinition, Asset::Type type)
+    : m_type {type}
+{
+    if (!jsonDefinition.isObject())
+    {
+        throw std::runtime_error(fmt::format("[Asset::Asset(jsonDefinition, type)] "
+                                             "Asset expects a JSON object, got: [{}]",
+                                             jsonDefinition.typeName()));
+    }
+    // Process definitions
+    json::Json tmpJson {jsonDefinition};
+    internals::substituteDefinitions(tmpJson);
+
+    auto objectDefinition = tmpJson.getObject().value();
+
+    // Get name
+    auto namePos = std::find_if(objectDefinition.begin(),
+                                objectDefinition.end(),
+                                [](auto tuple) { return std::get<0>(tuple) == "name"; });
+    if (objectDefinition.end() != namePos && std::get<1>(*namePos).isString())
+    {
+        m_name = std::get<1>(*namePos).getString().value();
+        objectDefinition.erase(namePos);
+    }
+    else
+    {
+        throw std::runtime_error("[Asset::Asset(jsonDefinition, type)] "
+                                 "Asset definition missing string name");
+    }
+
+    // Get parents
+    auto parentsPos = std::find_if(objectDefinition.begin(),
+                                   objectDefinition.end(),
+                                   [](auto tuple) {
+                                       return std::get<0>(tuple) == "parents"
+                                              || std::get<0>(tuple) == "after";
+                                   });
+    if (objectDefinition.end() != parentsPos)
+    {
+        if (!std::get<1>(*parentsPos).isArray())
         {
-            case Asset::Type::DECODER: return "decoder";
-            case Asset::Type::RULE: return "rule";
-            case Asset::Type::OUTPUT: return "output";
-            case Asset::Type::FILTER: return "filter";
-            default:
-                throw std::runtime_error(
-                    fmt::format("[Asset::typeToString(type)] unknown type: [{}]",
-                                static_cast<int>(type)));
+            throw std::runtime_error(
+                fmt::format("[Asset::Asset(jsonDefinition, type)] "
+                            "Asset definition [parents] expects an array, got: [{}]",
+                            std::get<1>(*parentsPos).typeName()));
+        }
+        auto parents = std::get<1>(*parentsPos).getArray().value();
+        for (auto& parent : parents)
+        {
+            m_parents.insert(parent.getString().value());
+        }
+        objectDefinition.erase(parentsPos);
+    }
+
+    // Get metadata
+    auto metadataPos =
+        std::find_if(objectDefinition.begin(),
+                     objectDefinition.end(),
+                     [](auto tuple) { return std::get<0>(tuple) == "metadata"; });
+    if (objectDefinition.end() != metadataPos)
+    {
+        m_metadata = std::get<1>(*metadataPos);
+        objectDefinition.erase(metadataPos);
+    }
+
+    // Get check
+    auto checkPos = std::find_if(objectDefinition.begin(),
+                                 objectDefinition.end(),
+                                 [](auto tuple) {
+                                     return std::get<0>(tuple) == "check"
+                                            || std::get<0>(tuple) == "allow";
+                                 });
+    if (objectDefinition.end() != checkPos)
+    {
+        try
+        {
+            m_check =
+                internals::Registry::getBuilder("stage.check")({std::get<1>(*checkPos)});
+            objectDefinition.erase(checkPos);
+        }
+        catch (const std::exception& e)
+        {
+            std::throw_with_nested(
+                std::runtime_error("[Asset::Asset(jsonDefinition, type)] failed to "
+                                   "build stage check"));
         }
     }
 
-    Asset::Asset(std::string name, Asset::Type type)
-        : m_name {name}
-        , m_type {type}
+    // Get parse if present
+    auto parsePos =
+        std::find_if(objectDefinition.begin(),
+                     objectDefinition.end(),
+                     [](auto tuple) { return std::get<0>(tuple) == "parse"; });
+    if (objectDefinition.end() != parsePos)
     {
-    }
-
-    Asset::Asset(const json::Json& jsonDefinition, Asset::Type type)
-        : m_type {type}
-    {
-        if (!jsonDefinition.isObject())
+        try
         {
-            throw std::runtime_error(fmt::format("[Asset::Asset(jsonDefinition, type)] "
-                                                 "Asset expects a JSON object, got: [{}]",
-                                                 jsonDefinition.typeName()));
+            auto parseExpression =
+                internals::Registry::getBuilder("stage.parse")({std::get<1>(*parsePos)});
+            objectDefinition.erase(parsePos);
+            m_check = base::And::create("condition", {m_check, parseExpression});
         }
-        // Process definitions
-        json::Json tmpJson {jsonDefinition};
-        internals::substituteDefinitions(tmpJson);
-
-        auto objectDefinition = tmpJson.getObject().value();
-
-        // Get name
-        auto namePos =
-            std::find_if(objectDefinition.begin(),
-                         objectDefinition.end(),
-                         [](auto tuple) { return std::get<0>(tuple) == "name"; });
-        if (objectDefinition.end() != namePos && std::get<1>(*namePos).isString())
+        catch (const std::exception& e)
         {
-            m_name = std::get<1>(*namePos).getString().value();
-            objectDefinition.erase(namePos);
-        }
-        else
-        {
-            throw std::runtime_error("[Asset::Asset(jsonDefinition, type)] "
-                                     "Asset definition missing string name");
-        }
-
-        // Get parents
-        auto parentsPos = std::find_if(objectDefinition.begin(),
-                                       objectDefinition.end(),
-                                       [](auto tuple) {
-                                           return std::get<0>(tuple) == "parents"
-                                                  || std::get<0>(tuple) == "after";
-                                       });
-        if (objectDefinition.end() != parentsPos)
-        {
-            if (!std::get<1>(*parentsPos).isArray())
-            {
-                throw std::runtime_error(
-                    fmt::format("[Asset::Asset(jsonDefinition, type)] "
-                                "Asset definition [parents] expects an array, got: [{}]",
-                                std::get<1>(*parentsPos).typeName()));
-            }
-            auto parents = std::get<1>(*parentsPos).getArray().value();
-            for (auto& parent : parents)
-            {
-                m_parents.insert(parent.getString().value());
-            }
-            objectDefinition.erase(parentsPos);
-        }
-
-        // Get metadata
-        auto metadataPos =
-            std::find_if(objectDefinition.begin(),
-                         objectDefinition.end(),
-                         [](auto tuple) { return std::get<0>(tuple) == "metadata"; });
-        if (objectDefinition.end() != metadataPos)
-        {
-            m_metadata = std::get<1>(*metadataPos);
-            objectDefinition.erase(metadataPos);
-        }
-
-        // Get check
-        auto checkPos = std::find_if(objectDefinition.begin(),
-                                     objectDefinition.end(),
-                                     [](auto tuple) {
-                                         return std::get<0>(tuple) == "check"
-                                                || std::get<0>(tuple) == "allow";
-                                     });
-        if (objectDefinition.end() != checkPos)
-        {
-            try
-            {
-                m_check = internals::Registry::getBuilder("stage.check")(
-                    {std::get<1>(*checkPos)});
-                objectDefinition.erase(checkPos);
-            }
-            catch (const std::exception& e)
-            {
-                std::throw_with_nested(
-                    std::runtime_error("[Asset::Asset(jsonDefinition, type)] failed to "
-                                       "build stage check"));
-            }
-        }
-
-        // Get parse if present
-        auto parsePos =
-            std::find_if(objectDefinition.begin(),
-                         objectDefinition.end(),
-                         [](auto tuple) { return std::get<0>(tuple) == "parse"; });
-        if (objectDefinition.end() != parsePos)
-        {
-            try
-            {
-                auto parseExpression = internals::Registry::getBuilder("stage.parse")(
-                    {std::get<1>(*parsePos)});
-                objectDefinition.erase(parsePos);
-                m_check = base::And::create("condition", {m_check, parseExpression});
-            }
-            catch (const std::exception& e)
-            {
-                std::throw_with_nested(
-                    std::runtime_error("[Asset::Asset(jsonDefinition, type)] failed to "
-                                       "build stage parse"));
-            }
-        }
-
-        // Get stages
-        m_stages = base::And::create("stages", {});
-        auto asOp = m_stages->getPtr<base::Operation>();
-        for (auto& tuple : objectDefinition)
-        {
-            auto stageName = "stage." + std::get<0>(tuple);
-            auto stageDefinition = std::get<1>(tuple);
-            auto stageExpression =
-                internals::Registry::getBuilder(stageName)({stageDefinition});
-            asOp->getOperands().push_back(stageExpression);
+            std::throw_with_nested(
+                std::runtime_error("[Asset::Asset(jsonDefinition, type)] failed to "
+                                   "build stage parse"));
         }
     }
 
-    base::Expression Asset::getExpression() const
+    // Get stages
+    m_stages = base::And::create("stages", {});
+    auto asOp = m_stages->getPtr<base::Operation>();
+    for (auto& tuple : objectDefinition)
     {
-        base::Expression asset;
-        switch (m_type)
-        {
-            case Asset::Type::OUTPUT:
-            case Asset::Type::RULE:
-            case Asset::Type::DECODER:
-                asset = base::Implication::create(m_name, m_check, m_stages);
-                break;
-            case Asset::Type::FILTER: asset = base::And::create(m_name, {m_check}); break;
-            default:
-                throw std::runtime_error("Unknown asset type in Asset::getExpression");
-        }
-
-        return asset;
+        auto stageName = "stage." + std::get<0>(tuple);
+        auto stageDefinition = std::get<1>(tuple);
+        auto stageExpression =
+            internals::Registry::getBuilder(stageName)({stageDefinition});
+        asOp->getOperands().push_back(stageExpression);
     }
+}
+
+base::Expression Asset::getExpression() const
+{
+    base::Expression asset;
+    switch (m_type)
+    {
+        case Asset::Type::OUTPUT:
+        case Asset::Type::RULE:
+        case Asset::Type::DECODER:
+            asset = base::Implication::create(m_name, m_check, m_stages);
+            break;
+        case Asset::Type::FILTER: asset = base::And::create(m_name, {m_check}); break;
+        default: throw std::runtime_error("Unknown asset type in Asset::getExpression");
+    }
+
+    return asset;
+}
 
 } // namespace builder
