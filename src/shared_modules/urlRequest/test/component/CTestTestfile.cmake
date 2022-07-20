23a7dc2d7be27d385846720c15c5e8445eca4088
sebasfalcone <sebastian.falcone@wazuh.com>
2022-07-20 17:13:52 -0300

    fixed /src/Makefile

--- src/shared_modules/urlRequest/test/component/CTestTestfile.cmake
+++ src/shared_modules/urlRequest/test/component/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(urlrequest_component_test "/home/sebas/Documents/wazuh/wazuh/src/shared_modules/urlRequest/test/component/urlrequest_component_test")
-set_tests_properties(urlrequest_component_test PROPERTIES  _BACKTRACE_TRIPLES "/home/sebas/Documents/wazuh/wazuh/src/shared_modules/urlRequest/test/component/CMakeLists.txt;17;add_test;/home/sebas/Documents/wazuh/wazuh/src/shared_modules/urlRequest/test/component/CMakeLists.txt;0;")
+set_tests_properties(urlrequest_component_test PROPERTIES  _BACKTRACE_TRIPLES "/home/sebas/Documents/wazuh/wazuh/src/shared_modules/urlRequest/test/component/CMakeLists.txt;19;add_test;/home/sebas/Documents/wazuh/wazuh/src/shared_modules/urlRequest/test/component/CMakeLists.txt;0;")
