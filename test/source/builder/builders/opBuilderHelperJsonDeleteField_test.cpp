fb8201346ec905fa1d82e2bd04dea91f00f03af6
Juan Cabrera <jcabre88@gmail.com>
2022-07-28 18:31:23 +0200

    Adapted opBuilderHelperJsonDeleteField tests

--- test/source/builder/builders/opBuilderHelperJsonDeleteField_test.cpp
+++ test/source/builder/builders/opBuilderHelperJsonDeleteField_test.cpp
@@ -1,424 +1,139 @@
-#include <vector>
+/* Copyright (C) 2015-2021, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
 
+#include <any>
 #include <gtest/gtest.h>
+#include <vector>
 
 #include <baseTypes.hpp>
 
-#include "opBuilderCondition.hpp"
-#include "opBuilderHelperFilter.hpp"
 #include "opBuilderHelperMap.hpp"
-#include "stageBuilderCheck.hpp"
-#include "stageBuilderNormalize.hpp"
-#include "testUtils.hpp"
 
 using namespace base;
 namespace bld = builder::internals::builders;
 
-using FakeTrFn = std::function<void(std::string)>;
-static FakeTrFn tr = [](std::string msg) {
-};
-
-class opBuilderHelperDeleteFieldTestSuite : public testing::Test
+TEST(opBuilderHelperJsonDeleteField, Builds)
 {
-protected:
-    // Per-test-suite set-up.
-    // Called before the first test in this test suite.
-    static void SetUpTestSuite()
-    {
-        Registry::registerBuilder("helper.delete_field",
-                                  builders::opBuilderHelperDeleteField);
-    }
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    // Per-test-suite tear-down.
-    // Called after the last test in this test suite.
-    static void TearDownTestSuite() { return; }
-};
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, Builds)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "qttyOfDeletedFields": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    ASSERT_NO_THROW(bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr));
+    ASSERT_NO_THROW(bld::opBuilderHelperDeleteField(tuple));
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, CantBuildWithtParameter)
+TEST(opBuilderHelperJsonDeleteField, Builds_bad_parameters)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "qttyOfDeletedFields": "+delete_field/Argument"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {"test", "test"});
 
-    ASSERT_THROW(bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr),
-                 std::runtime_error);
+    ASSERT_THROW(bld::opBuilderHelperDeleteField(tuple), std::runtime_error);
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, CantBuildWithEmptyParameter)
+TEST(opBuilderHelperJsonDeleteField, Exec_json_delete_field_field_not_exist)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "": "+delete_field//"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    ASSERT_THROW(bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr),
-                 std::runtime_error);
+    auto event1 = std::make_shared<json::Json>(R"({"fieldcheck": 10})");
+
+    auto op = bld::opBuilderHelperDeleteField(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, ExecutesWithSimpleDelete)
+TEST(opBuilderHelperJsonDeleteField, Exec_json_delete_field_success)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_1": "+delete_field"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_1": "1",
-                "Field_2": "2",
-                "Third": "Value"
-                }
-            )"));
-        s.on_completed();
-    });
+    auto event1 = std::make_shared<json::Json>(R"({"field2check": 10})");
 
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_2").GetString(), "2");
-    ASSERT_THROW(expected[0]->getEventValue("/Field_1"), std::invalid_argument);
+    auto op = bld::opBuilderHelperDeleteField(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_TRUE(result);
+
+    ASSERT_FALSE(result.payload()->exists("/field2check"));
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteTwoFields)
+TEST(opBuilderHelperJsonDeleteField, Exec_json_delete_field_multilevel_field_not_exist)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_1": "+delete_field",
-                    "Field_2": "+delete_field"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/parentObjt_1/field2check"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    auto normalize = bld::stageBuilderNormalize(doc.get("/normalize"), tr);
-
-    rxcpp::subjects::subject<Event> inputSubject;
-    inputSubject.get_observable().subscribe([](Event e) {});
-    auto inputObservable = inputSubject.get_observable();
-    auto output = normalize(inputObservable);
-
-    std::vector<Event> expected;
-    output.subscribe([&expected](Event e) { expected.push_back(e); });
-
-    auto eventsCount = 1;
-    auto inputObject = createSharedEvent(R"(
-                {
-                "Field_1": 8,
-                "Field_2": "2",
-                "Field_3": 9
-                }
-            )");
-
-    inputSubject.get_subscriber().on_next(inputObject);
-
-    ASSERT_EQ(expected.size(), eventsCount);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_1"), std::invalid_argument);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2"), std::invalid_argument);
-    ASSERT_EQ(expected[0]->getEventValue("/Field_3").GetInt(), 9);
-}
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteTwoTimesSameField)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_1": "+delete_field",
-                    "Field_1": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    auto normalize = bld::stageBuilderNormalize(doc.get("/normalize"), tr);
-
-    rxcpp::subjects::subject<Event> inputSubject;
-    inputSubject.get_observable().subscribe([](Event e) {});
-    auto inputObservable = inputSubject.get_observable();
-    auto output = normalize(inputObservable);
-
-    std::vector<Event> expected;
-    output.subscribe([&expected](Event e) { expected.push_back(e); });
-
-    auto eventsCount = 1;
-    auto inputObject = createSharedEvent(R"(
-                {
-                "Field_1": 8,
-                "Field_2": 9
-                }
-            )");
-
-    inputSubject.get_subscriber().on_next(inputObject);
-
-    ASSERT_EQ(expected.size(), eventsCount);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_1"), std::invalid_argument);
-    ASSERT_EQ(expected[0]->getEventValue("/Field_2").GetInt(), 9);
-}
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteTwoFieldsOneUnexisting)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_3": "+delete_field",
-                    "Field_1": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    auto normalize = bld::stageBuilderNormalize(doc.get("/normalize"), tr);
-
-    rxcpp::subjects::subject<Event> inputSubject;
-    inputSubject.get_observable().subscribe([](Event e) {});
-    auto inputObservable = inputSubject.get_observable();
-    auto output = normalize(inputObservable);
-
-    std::vector<Event> expected;
-    output.subscribe([&expected](Event e) { expected.push_back(e); });
-
-    auto eventsCount = 1;
-    auto inputObject = createSharedEvent(R"(
-                {
-                "Field_1": 8,
-                "Field_2": "2"
-                }
-            )");
-
-    inputSubject.get_subscriber().on_next(inputObject);
-
-    ASSERT_EQ(expected.size(), eventsCount);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_1"), std::invalid_argument);
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_2").GetString(), "2");
-}
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, CantDeleteUnexistentField)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_2": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_1": "Value"
-                }
-            )"));
-        s.on_completed();
-    });
-
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_1").GetString(), "Value");
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2"), std::invalid_argument);
-}
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteFullNestedField)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_2": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_2":
-                    { "a" : "Value_2" },
-                "Third": 9
-                }
-            )"));
-        s.on_completed();
-    });
-
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2"), std::invalid_argument);
-}
-
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteSingleNestedField)
-{
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_2.a": "+delete_field"
-                }
-            }
-        ]
-    })"};
-
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_1": "Value_1",
-                "Field_2":
-                    { "a" : "Value_1_a",
-                      "b" : "Value_1_b"
+    auto event1 = std::make_shared<json::Json>(R"({
+                    "parentObjt_2": {
+                        "field2check": 15,
+                        "ref_key": 10
                     },
-                "Third": 9
-                }
-            )"));
-        s.on_completed();
-    });
+                    "parentObjt_1": {
+                        "fieldcheck": 10,
+                        "ref_key": 11
+                    }
+                    })");
 
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2/a").GetString(),
-                 std::invalid_argument);
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_2/b").GetString(), "Value_1_b");
+    auto op = bld::opBuilderHelperDeleteField(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteSingleArrayField)
+TEST(opBuilderHelperJsonDeleteField, Exec_json_delete_field_multilevel_success)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_2.0.key_a": "+delete_field"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/parentObjt_1/field2check"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_1": "Value",
-                "Field_2":
-                    [
-                        { "key_a" : "Value_A"},
-                        { "key_a" : "Value_B"},
-                        { "key_a" : "Value_C"}
-                    ]
-                }
-            )"));
-        s.on_completed();
-    });
+    auto event1 = std::make_shared<json::Json>(R"({
+                    "parentObjt_2": {
+                        "field2check": 15,
+                        "ref_key": 10
+                    },
+                    "parentObjt_1": {
+                        "field2check": 10,
+                        "ref_key": 11
+                    }
+                    })");
 
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2/0/key_a").GetString(),
-                 std::invalid_argument);
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_2/1/key_a").GetString(), "Value_B");
-    ASSERT_STREQ(expected[0]->getEventValue("/Field_2/2/key_a").GetString(), "Value_C");
+    auto op = bld::opBuilderHelperDeleteField(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_TRUE(result);
+
+    ASSERT_FALSE(result.payload()->exists("/parentObjt_1/field2check"));
 }
 
-TEST_F(opBuilderHelperDeleteFieldTestSuite, DeleteFullArrayField)
+// TODO This test should not be valid since the key must be unique.
+TEST(opBuilderHelperJsonDeleteField, aux)
 {
-    Document doc {R"({
-        "normalize":
-        [
-            {
-                "map":
-                {
-                    "Field_2": "+delete_field"
-                }
-            }
-        ]
-    })"};
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"delete_field"},
+                                 std::vector<std::string> {});
 
-    Observable input = observable<>::create<Event>([=](auto s) {
-        s.on_next(createSharedEvent(R"(
-                {
-                "Field_1": "Value",
-                "Field_2":
-                    [
-                        { "key_a" : "Value_A"},
-                        { "key_a" : "Value_B"},
-                        { "key_a" : "Value_C"}
-                    ]
-                }
-            )"));
-        s.on_completed();
-    });
+    auto event1 =
+        std::make_shared<json::Json>(R"({"field2check": 10,"field2check": 20})");
 
-    Lifter lift = bld::opBuilderHelperDeleteField(doc.get("/normalize/0/map"), tr);
-    Observable output = lift(input);
-    vector<Event> expected;
-    output.subscribe([&](Event e) { expected.push_back(e); });
-    ASSERT_EQ(expected.size(), 1);
-    ASSERT_THROW(expected[0]->getEventValue("/Field_2"), std::invalid_argument);
+    auto op = bld::opBuilderHelperDeleteField(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_TRUE(result);
+
+    ASSERT_TRUE(result.payload()->exists(
+        "/field2check")); // Remove the first occurrence of the path
 }
