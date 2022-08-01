b9a63723e587c3ce00130b6f1f06ae828e3e1395
LucioDonda <donda.lucio@gmail.com>
2022-08-01 15:51:10 -0300

    SHA, StringFromArray and NetInfoAddress helpers and yaml decoders fro syscollector

---
+++ src/engine/source/builder/builders/opBuilderSHAfrom.hpp
@@ -0,0 +1,17 @@
+#ifndef _OP_BUILDER_SHA_FROM_H
+#define _OP_BUILDER_SHA_FROM_H
+
+#include <any>
+
+#include "expression.hpp"
+
+namespace builder::internals::builders
+{
+
+base::Expression opBuilderSHAfrom(const std::any& definition);
+
+} // namespace builder::internals::builders
+
+// namespace builder::internals::builders
+
+#endif // _OP_BUILDER_SHA_FROM_H
