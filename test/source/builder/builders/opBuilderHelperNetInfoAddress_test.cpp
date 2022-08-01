fc72698017fce9a41e20ecb598a3d7fcb7500568
LucioDonda <donda.lucio@gmail.com>
2022-08-01 15:52:59 -0300

    StringFromArray helper added on HelperMap, test and corrections

--- test/source/builder/builders/opBuilderHelperNetInfoAddress_test.cpp
+++ test/source/builder/builders/opBuilderHelperNetInfoAddress_test.cpp
@@ -18,7 +18,7 @@
 using namespace base;
 namespace bld = builder::internals::builders;
 
-TEST(opBuilderHelperNetInfoTest, Builds)
+TEST(opBuilderHelperNetInfoAddressTest, Builds)
 {
     auto tuple = std::make_tuple(std::string {"/field"},
                                  std::string {"netInfoAddress"},
@@ -26,19 +26,3 @@ TEST(opBuilderHelperNetInfoTest, Builds)
 
     ASSERT_NO_THROW(bld::opBuilderHelperNetInfoAddres(tuple));
 }
-
-TEST(opBuilderHelperNetInfoTest, CorrectExecutionWithEvent)
-{
-    auto tuple = std::make_tuple(std::string {"/field"},
-                                 std::string {"netInfoAddress"},
-                                 std::vector<std::string> {"0"});
-
-    auto event1 =
-        std::make_shared<json::Json>(R"({"agent":{"id":"021"},"event":{"original":{"ID":"123456","iface":{"IPv4":{"address":["192.168.10.15","192.168.11.16"],"netmask":["255.255.255.0","255.255.255.0"],"broadcast":["192.168.10.255","192.168.11.255"]}}}}})");
-
-    auto op =
-        bld::opBuilderHelperNetInfoAddres(tuple)->getPtr<Term<EngineOp>>()->getFn();
-    result::Result<Event> result = op(event1);
-    ASSERT_TRUE(result);
-}
-
