bb5235ae14b217be46b72d67f55d9a6fd4a9d8bd
LucioDonda <donda.lucio@gmail.com>
2022-08-02 13:16:52 -0300

    Added UT for SHA, stringFromArray and NetInfoAddress helpers plus yaml decoders fixed

--- src/engine/test/source/builder/builders/opBuilderHelperStringFromArray_test.cpp
+++ src/engine/test/source/builder/builders/opBuilderHelperStringFromArray_test.cpp
@@ -26,7 +26,7 @@ TEST(opBuilderHelperStringFromArray, Builds)
     ASSERT_NO_THROW(bld::opBuilderHelperStringFromArray(tuple));
 }
 
-TEST(opBuilderHelperStringFromArray, Builds_bad_parameters)
+TEST(opBuilderHelperStringFromArray, BuildsBadParameters)
 {
     auto tuple = std::make_tuple(std::string {"/field"},
                                  std::string {"s_from_array"},
@@ -35,7 +35,7 @@ TEST(opBuilderHelperStringFromArray, Builds_bad_parameters)
     ASSERT_THROW(bld::opBuilderHelperStringFromArray(tuple), std::out_of_range);
 }
 
-TEST(opBuilderHelperStringFromArray, Exec_string_from_Array_Success)
+TEST(opBuilderHelperStringFromArray, ExecStringFromArraySuccess)
 {
     auto tuple = std::make_tuple(std::string {"/field2check"},
                                  std::string {"s_from_array"},
@@ -52,7 +52,7 @@ TEST(opBuilderHelperStringFromArray, Exec_string_from_Array_Success)
     ASSERT_EQ("A-B-C-D-E", result.payload()->getString("/field2check").value());
 }
 
-TEST(opBuilderHelperStringFromArray, Exec_string_from_Array_with_ref_Success)
+TEST(opBuilderHelperStringFromArray, ExecStringFromArrayWithRefSuccess)
 {
     auto tuple = std::make_tuple(std::string {"/field2check"},
                                  std::string {"s_from_array"},
@@ -67,4 +67,81 @@ TEST(opBuilderHelperStringFromArray, Exec_string_from_Array_with_ref_Success)
     ASSERT_TRUE(result);
 
     ASSERT_EQ("A,B", result.payload()->getString("/field2check").value());
+}
+
+TEST(opBuilderHelperStringFromArray, FailedNotArray)
+{
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"s_from_array"},
+                                 std::vector<std::string> {"$separator", "$arrayField"});
+
+    auto event1 = std::make_shared<json::Json>(R"({"separator": ",", "arrayField": "A"})");
+
+    auto op = bld::opBuilderHelperStringFromArray(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
+}
+
+TEST(opBuilderHelperStringFromArray, FailedArrayWithoutStrings)
+{
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"s_from_array"},
+                                 std::vector<std::string> {"$separator", "$arrayField"});
+
+    auto event1 = std::make_shared<json::Json>(R"({"separator": ",", "arrayField": [1,150]})");
+
+    auto op = bld::opBuilderHelperStringFromArray(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
+}
+
+TEST(opBuilderHelperStringFromArray, FailedEmptyArray)
+{
+    auto tuple = std::make_tuple(std::string {"/field2check"},
+                                 std::string {"s_from_array"},
+                                 std::vector<std::string> {"$separator", "$arrayField"});
+
+    auto event1 = std::make_shared<json::Json>(R"({"separator": ",", "arrayField": []})");
+
+    auto op = bld::opBuilderHelperStringFromArray(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
+}
+
+TEST(opBuilderHelperStringFromArray, SuccessWithMultilevelAssignment)
+{
+    auto tuple = std::make_tuple(std::string {"/arrayResult/0/field2check"},
+                                 std::string {"s_from_array"},
+                                 std::vector<std::string> {"$separator", "$arrayField"});
+
+    auto event1 = std::make_shared<json::Json>(R"({"separator": ".", "arrayField": ["A","B"]})");
+
+    auto op = bld::opBuilderHelperStringFromArray(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_TRUE(result);
+
+    ASSERT_EQ("A.B", result.payload()->getString("/arrayResult/0/field2check").value());
+}
+
+TEST(opBuilderHelperStringFromArray, FailedWithSeccondParameterNotReference)
+{
+    auto tuple = std::make_tuple(std::string {"/arrayResult/0/field2check"},
+                                 std::string {"s_from_array"},
+                                 std::vector<std::string> {"$separator", "[\"A\",\"B\"]"});
+
+    auto event1 = std::make_shared<json::Json>(R"({"separator": ".", "arrayField": ["A","B"]})");
+
+    auto op = bld::opBuilderHelperStringFromArray(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event1);
+
+    ASSERT_FALSE(result);
 }
