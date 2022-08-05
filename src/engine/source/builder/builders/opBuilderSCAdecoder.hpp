dbda8a43788df56ee29153da28813c5800cf0ac8
Juan Cabrera <jcabre88@gmail.com>
2022-08-05 15:12:05 +0200

    Added sca namespace to allow testing

--- src/engine/source/builder/builders/opBuilderSCAdecoder.hpp
+++ src/engine/source/builder/builders/opBuilderSCAdecoder.hpp
@@ -12,6 +12,7 @@
 
 #include <any>
 
+#include "base/baseTypes.hpp"
 #include "expression.hpp"
 
 namespace builder::internals::builders
@@ -23,6 +24,75 @@ namespace builder::internals::builders
 constexpr std::string_view STREAM_SOCK_PATH = "/tmp/testStream.socket";
 /* Security configuration assessment remoted queue */
 constexpr const char* CFGARQUEUE {"/tmp/cfgar.sock"}; //"queue/alerts/cfgarq"
+
+namespace sca
+{
+
+static std::unordered_map<std::string, std::string> scanInfoKeyValues {{"/policy_id", ""},
+                                                                       {"/hash", ""},
+                                                                       {"/hash_file", ""},
+                                                                       {"/file", ""},
+                                                                       {"/policy", ""}};
+
+bool CheckEventJSON(base::Event& event, const std::string& scaEventPath);
+
+void FillCheckEventInfo(base::Event& event,
+                        const std::string& response,
+                        const std::string& scaEventPath);
+
+bool CheckScanInfoJSON(base::Event& event, const std::string& scaEventPath);
+
+int FindScanInfo(base::Event& event,
+                 const std::string& agentId,
+                 const std::string& scaEventPath,
+                 std::string& hashScanInfo);
+
+int SaveScanInfo(base::Event& event, const std::string& agent_id, int update);
+
+int FindPolicyInfo(base::Event& event, const std::string& agent_id);
+
+bool PushDumpRequest(base::Event& event, const std::string& agentId, int firstScan);
+
+int SavePolicyInfo(const std::string& agent_id,
+                   std::string& description_db,
+                   std::string& references_db);
+
+int FindPolicySHA256(const std::string& agent_id, std::string& old_hash);
+
+int DeletePolicy(const std::string& agent_id);
+
+int DeletePolicyCheck(const std::string& agent_id);
+
+int FindCheckResults(const std::string& agentId, std::string& wdbResponse);
+
+int FindPoliciesIds(const std::string& agentId, std::string& policiesIds);
+
+std::optional<std::string> CheckDumpJSON(base::Event event,
+                                         std::string& elementsSent,
+                                         std::string& policyId,
+                                         std::string& scanId,
+                                         const std::string& scaEventPath);
+
+int DeletePolicyCheckDistinct(const std::string& agentId,
+                              const std::string& policyId,
+                              const std::string& scanId);
+
+std::optional<std::string> HandleCheckEvent(base::Event& event,
+                                            const std::string& agent_id,
+                                            const std::string& scaEventPath);
+
+std::optional<std::string> HandleScanInfo(base::Event& event,
+                                          const std::string& agent_id,
+                                          const std::string& scaEventPath);
+
+std::optional<std::string> HandlePoliciesInfo(base::Event& event,
+                                              const std::string& agentId,
+                                              const std::string& scaEventPath);
+
+std::optional<std::string> HandleDumpEvent(base::Event& event,
+                                           const std::string& agentId,
+                                           const std::string& scaEventPath);
+} // namespace sca
 
 /**
  * @brief Executes query on WDB returning status ok or not ok.
