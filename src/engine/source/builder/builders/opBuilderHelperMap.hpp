3f1ff79d7015ce2669c34389524abceea1256d4a
LucioDonda <donda.lucio@gmail.com>
2022-08-11 11:31:28 -0300

    Syscollector PR corrections for new approach for helpers (netAddress, stringfromArray and NetInfo) and yaml decoders

--- src/engine/source/builder/builders/opBuilderHelperMap.hpp
+++ src/engine/source/builder/builders/opBuilderHelperMap.hpp
@@ -66,9 +66,12 @@ base::Expression opBuilderHelperStringTrim(const std::any& definition);
 base::Expression opBuilderHelperStringConcat(const std::any& definition);
 
 /**
- * @brief Transforms an array of strings into a single string field
+ * @brief Transforms an array of strings into a single string field result of concatenate
+ * them with a separator between (not at the start or the end).
+ * i.e: '<field>: +s_fromArray/<separator>/$<array_reference1>'
  * @param definition The transformation definition.
- * i.e: //field: +s_fromArray/<separator>|<separatorRef>/<array_reference1>
+ * @throw std::runtime_error if the parameter is not a reference or if theres no
+ * Value argument for the separator.
  * @return base::Expression
  */
 base::Expression opBuilderHelperStringFromArray(const std::any& definition);
