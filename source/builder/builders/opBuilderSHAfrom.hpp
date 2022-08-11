4da2792cce928a9f57ff3510cd24745f76a21da2
LucioDonda <donda.lucio@gmail.com>
2022-08-11 11:31:28 -0300

    Syscollector PR corrections for new approach for helpers (netAddress, stringfromArray and NetInfo) and yaml decoders

--- source/builder/builders/opBuilderSHAfrom.hpp
+++ source/builder/builders/opBuilderSHAfrom.hpp
@@ -8,10 +8,15 @@
 namespace builder::internals::builders
 {
 
+/**
+ * @brief Builds helper SHA1 hash calculated from a strings or a set of strings
+ * as parameters.
+ * @param definition Definition of the operation to be built.
+ * @return base::Expression The Lifter with the SHA1 hash.
+ * @throw std::runtime_error if the parameter size is less than one.
+ */
 base::Expression opBuilderSHAfrom(const std::any& definition);
 
 } // namespace builder::internals::builders
-
-// namespace builder::internals::builders
 
 #endif // _OP_BUILDER_SHA_FROM_H
