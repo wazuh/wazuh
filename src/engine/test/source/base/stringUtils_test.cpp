c590683114135c50fe2e85ab04b29994b17f4ce0
LucioDonda <donda.lucio@gmail.com>
2022-08-16 18:14:08 -0300

    Separting to string util join function, removing unnecesary try catchs

--- src/engine/test/source/base/stringUtils_test.cpp
+++ src/engine/test/source/base/stringUtils_test.cpp
@@ -75,3 +75,62 @@ TEST(splitMulti, ThreeDelimiters) {
     std::vector<std::string> result = utils::string::splitMulti(input, utils::string::Delimeter('-', true), utils::string::Delimeter(',', false), utils::string::Delimeter(' ', false));
     ASSERT_EQ(result, expected);
 }
+
+TEST(join, defaut_separator)
+{
+    std::string expected = "test";
+    std::vector<std::string> test = {"test"};
+    std::string result = utils::string::join(test);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, not_defaut_separator_starting_with_it)
+{
+    std::string expected = ",test";
+    std::string separator = ",";
+    std::vector<std::string> test = {"test"};
+    std::string result = utils::string::join(test, separator, true);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, not_defaut_separator_several_strings_starting_with_it)
+{
+    std::string expected = ",test1,test2,test3,test4";
+    std::string separator = ",";
+    std::vector<std::string> test = {"test1", "test2", "test3", "test4"};
+    std::string result = utils::string::join(test, separator, true);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, not_defaut_separator_several_strings)
+{
+    std::string expected = "test1,test2,test3,test4";
+    std::string separator = ",";
+    std::vector<std::string> test = {"test1", "test2", "test3", "test4"};
+    std::string result = utils::string::join(test, separator, false);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, defaut_separator_several_strings)
+{
+    std::string expected = "test1test2test3test4";
+    std::vector<std::string> test = {"test1", "test2", "test3", "test4"};
+    std::string result = utils::string::join(test);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, defaut_separator_none_input)
+{
+    std::string expected = "";
+    std::vector<std::string> test = {};
+    std::string result = utils::string::join(test);
+    ASSERT_EQ(result, expected);
+}
+
+TEST(join, defaut_separator_empty_strings_array_as_input)
+{
+    std::string expected = "";
+    std::vector<std::string> test = {"", "", ""};
+    std::string result = utils::string::join(test);
+    ASSERT_EQ(result, expected);
+}
