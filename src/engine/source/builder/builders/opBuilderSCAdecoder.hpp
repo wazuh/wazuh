531f5dc884bf3c4b97b733a8db3a588d02f2bd11
Julian Morales <jmorales@unc.edu.ar>
2022-08-17 20:22:42 +0000

    Implement InfoEventDecode for all handlers

--- src/engine/source/builder/builders/opBuilderSCAdecoder.hpp
+++ src/engine/source/builder/builders/opBuilderSCAdecoder.hpp
@@ -14,6 +14,7 @@
 #include <memory>
 
 #include <json/json.hpp>
+#include <utils/socketInterface/unixDatagram.hpp>
 #include <wdb/wdb.hpp>
 
 #include "base/baseTypes.hpp"
@@ -25,9 +26,9 @@ namespace builder::internals::builders
 // /* Security configuration assessment remoted queue */
 
 // TODO: remove when undoing set for testing
-constexpr std::string_view STREAM_SOCK_PATH = "/tmp/testStream.socket";
+constexpr std::string_view WDB_SOCK_PATH {"/tmp/testStream.socket"};
 /* Security configuration assessment remoted queue */
-constexpr const char* CFGARQUEUE {"/tmp/cfgar.sock"}; //"queue/alerts/cfgarq"
+constexpr std::string_view CFG_AR_PATH {"/tmp/cfgar.sock"}; //"queue/alerts/cfgarq"
 
 namespace sca
 {
@@ -92,123 +93,256 @@ enum class Name
 Name& operator++(Name& field);
 
 /**
- * @brief Get the Raw Path object
+ * @brief Get the Raw Path of the field.
  *
- * @param field Field to get the Raw Path object
- * @return std::string Raw Path object
+ * @param field Field to get the Raw Path.
+ * @return relative path of the field.
  */
-std::string getRawPath(Name field);
+std::string getRealtivePath(Name field);
 
 } // namespace field
 
 /**
- * @brief Value for a SCA Find Query Operation
+ * @brief Value for a SCA Find Query Operation.
  */
 enum class SearchResult
 {
-    ERROR = -1, ///< Error on wdb or unexpected result
-    NOT_FOUND,  ///< Not found
-    FOUND       ///< Found
+    ERROR = -1, ///< Error on wdb or unexpected result.
+    NOT_FOUND,  ///< Not found.
+    FOUND       ///< Found.
 };
 
 /**
- * @brief Store all decoder information for processing the SCA Event
+ * @brief Store all decoder information and context for processing the SCA Event.
  */
-struct InfoEventDecode
+struct DecodeCxt
 {
-    // TODO Changes names
-    base::Event& event;               ///< Event to be processed
-    const std::string& agentID;       ///< Agent ID of the agent that generated the event
-    const std::string& sourceSCApath; ///< Path to the SCA Event in the incoming event
-    std::shared_ptr<wazuhdb::WazuhDB> wdb; ///< WazuhDB instance
-    const std::unordered_map<sca::field::Name, std::string>& fieldSource;
-    const std::unordered_map<sca::field::Name, std::string>& fieldDest;
+    base::Event& event;         ///< Event to be processed.
+    const std::string& agentID; ///< Agent ID of the agent that generated the event.
+    std::shared_ptr<wazuhdb::WazuhDB> wdb; ///< WazuhDB instance.
+    /** @brief Socket to forward dump request. */
+    std::shared_ptr<base::utils::socketInterface::unixDatagram> forwarderSocket;
+    /** @brief Mapping the field Name to path of the field in the Original Event. */
+    const std::unordered_map<sca::field::Name, std::string>& sourcePath;
+    /** @brief Mapping the field Name to path of the field in the /sca Event. */
+    const std::unordered_map<sca::field::Name, std::string>& destinationPath;
 
-    std::optional<int> getIntFromSrc(sca::field::Name field) const
+    /**
+     * @brief Get int value of a field.
+     * @param field Field to get the value.
+     * @return empty if the field is not found or the value is not an.
+     * int
+     */
+    std::optional<int> getSrcInt(sca::field::Name field) const
     {
-        return event->getInt(fieldSource.at(field));
+        return event->getInt(sourcePath.at(field));
     };
 
-    std::optional<std::string> getStrFromSrc(sca::field::Name field) const
+    /**
+     * @brief Get string value of a field.
+     * @param field Field to get the value.
+     * @return empty if the field is not found or the value is
+     * not a string.
+     */
+    std::optional<std::string> getSrcStr(sca::field::Name field) const
     {
-        return event->getString(fieldSource.at(field));
+        return event->getString(sourcePath.at(field));
     };
 
+    /**
+     * @brief Get int value of a field.
+     * @param field Field to get the value.
+     * @return Empty if the field is not found or the value is not an object.
+     */
     std::optional<std::vector<std::tuple<std::string, json::Json>>>
-    getObjectFromSrc(sca::field::Name field) const
+    getSrcObject(sca::field::Name field) const
     {
-        return event->getObject(fieldSource.at(field));
+        return event->getObject(sourcePath.at(field));
     };
 
-    std::optional<std::vector<json::Json>> getArrayFromSrc(sca::field::Name field) const
+    /**
+     * @brief Get int value of a field.
+     * @param field Field to get the value.
+     * @return Empty if the field is not found or the value is not an array.
+     */
+    std::optional<std::vector<json::Json>> getSrcArray(sca::field::Name field) const
     {
-        return event->getArray(fieldSource.at(field));
+        return event->getArray(sourcePath.at(field));
     };
 
-    bool existsFromSrc(sca::field::Name field) const
+    /**
+     * @brief Check if a field is present in the original event.
+     *
+     * @param field Field to check.
+     * @return true if the field is present.
+     * @return false if the field is not present.
+     */
+    bool existsSrc(sca::field::Name field) const
     {
-        return event->exists(fieldSource.at(field));
+        return event->exists(sourcePath.at(field));
     }
 };
+
 /****************************************************************************************
                                  Check Event
 *****************************************************************************************/
 
 /**
- * @brief Check if the event is a valid check event type
+ * @brief Check if the event is a valid check event type.
  *
- * @param event The event to check
- * @param sourceSCApath The path to sca incomming event (in event)
- * @return true If the event is a valid check event type
- * @return false If the event is not a valid check event type
+ * @param ctx The decoder context, decode info status.
+ * @return true If the event is a valid check event type.
+ * @return false If the event is not a valid check event type.
  */
-bool isValidCheckEvent(const InfoEventDecode& ctx);
+bool isValidCheckEvent(const DecodeCxt& ctx);
 
 /**
- * @brief Fill the /sca object with the check event info
+ * @brief Fill the /sca object with the check event info.
  *
- * @param event The event to get the info from
- * @param previousResult The previous result of scan
- * @param sourceSCApath The path to sca incomming event (in event)
+ * @param ctx The decoder context, decode info status.
+ * @param previousResult The previous result of scan.
  */
-void fillCheckEvent(const InfoEventDecode& ctx, const std::string& previousResult);
+void fillCheckEvent(const DecodeCxt& ctx, const std::string& previousResult);
+
+/**
+ * @brief Insert compliance to wdb.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param checkID The check ID to insert.
+ */
+void insertCompliance(const DecodeCxt& ctx, const int checkID);
+
+/**
+ * @brief Insert rules  to wdb
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param checkID The check ID to insert
+ */
+void insertRules(const DecodeCxt& ctx, const int checkID);
 
 /****************************************************************************************
-                                  Scan Event
+                                  Scan Info Event
 *****************************************************************************************/
+/**
+ * @brief Check if the event is a valid scan event type.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @return true If the event is a valid scan event type
+ * @return false If the event is not a valid scan event type
+ */
+bool isValidScanInfoEvent(const DecodeCxt& ctx);
 
-bool CheckScanInfoJSON(base::Event& event, const std::string& sourceSCApath);
+/**
+ * @brief Requesting dump for policy (db dump) through the cfg ar socket.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to request the dump.
+ * @param firstScan If true, request the first scan dump, otherwise request the other scan
+ * dump.
+ */
+void pushDumpRequest(const DecodeCxt& ctx, const std::string& policyId, bool firstScan);
 
-bool SaveScanInfo(const InfoEventDecode& ctx, bool update);
+/**
+ * @brief Saves or updates the scan info in wdb.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param update If true, update the scan info, otherwise insert it.
+ * @return true If the wdb operation was successful.
+ * @return false If the wdb operation was not successful.
+ */
+bool SaveScanInfo(const DecodeCxt& ctx, bool update);
 
-SearchResult findPolicyInfo(const InfoEventDecode& ctx);
+/**
+ * @brief Insert de policy to wdb.
+ *
+ * @param ctx The decoder context, decode info status.
+ */
+void insertPolicyInfo(const DecodeCxt& ctx);
 
-void pushDumpRequest(const std::string& agentId,
-                     const std::string& policyId,
-                     bool firstScan);
+/**
+ * @brief Update the policy `policyId` in wdb.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to update.
+ */
+void updatePolicyInfo(const DecodeCxt& ctx, const std::string& policyId);
 
-void savePolicyInfo(const InfoEventDecode& ctx);
+/**
+ * @brief Find the check result in wdb and dump the policy if is necessary.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to find.
+ * @param isFirstScan If true, request the first scan
+ * @param eventHash The event hash to compare.
+ */
+void checkResultsAndDump(const DecodeCxt& ctx,
+                         const std::string& policyId,
+                         bool isFirstScan,
+                         const std::string& eventHash);
 
-bool deletePolicyAndCheck(const InfoEventDecode& ctx, const std::string& policyId);
+/**
+ * @brief Delete Policy and check from wdb.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to delete.
+ * @return true If the wdb operation was successful.
+ * @return false If the wdb operation was not successful.
+ */
+bool deletePolicyAndCheck(const DecodeCxt& ctx, const std::string& policyId);
 
-std::tuple<SearchResult, std::string> findCheckResults(const InfoEventDecode& ctx,
+/**
+ * @brief Find the check result in wdb
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to find.
+ * @return std::tuple<SearchResult, std::string> The search result and the event hash.
+ */
+std::tuple<SearchResult, std::string> findCheckResults(const DecodeCxt& ctx,
                                                        const std::string& policyId);
 
+/**
+ * @brief Fill the /sca object with the scan info event.
+ *
+ * @param ctx The decoder context, decode info status.
+ */
+void FillScanInfo(const DecodeCxt& ctx);
+
+/****************************************************************************************
+                                  Dump Event
+*****************************************************************************************/
+
+/**
+ * @brief Check if the event is a valid dump event type.
+ *
+ * @param ctx The decoder context, decode info status.
+ * @return true If the event is a valid dump event type.
+ * @return false If the event is not a valid dump event type.
+ */
 std::tuple<std::optional<std::string>, std::string, int>
-checkDumpJSON(const InfoEventDecode& ctx);
+isValidDumpEvent(const DecodeCxt& ctx);
 
-bool deletePolicyCheckDistinct(const std::string& agentId,
+/**
+ * @brief Delete check distinct policy id
+ *
+ * @param ctx The decoder context, decode info status.
+ * @param policyId The policy ID to delete.
+ * @param scanId The scan ID to delete.
+ */
+void deletePolicyCheckDistinct(const DecodeCxt& ctx,
                                const std::string& policyId,
-                               const int scanId,
-                               std::shared_ptr<wazuhdb::WazuhDB> wdb);
+                               const int scanId);
 
-std::optional<std::string> handleCheckEvent(const InfoEventDecode& ctx);
+/****************************************************************************************
+                                  Handlers
+*****************************************************************************************/
 
-std::optional<std::string> handleScanInfo(const InfoEventDecode& ctx);
+std::optional<std::string> handleCheckEvent(const DecodeCxt& ctx);
 
-std::optional<std::string> handlePoliciesInfo(const InfoEventDecode& ctx);
+std::optional<std::string> handleScanInfo(const DecodeCxt& ctx);
 
-std::optional<std::string> handleDumpEvent(const InfoEventDecode& ctx);
+std::optional<std::string> handlePoliciesInfo(const DecodeCxt& ctx);
+
+std::optional<std::string> handleDumpEvent(const DecodeCxt& ctx);
 
 } // namespace sca
 
