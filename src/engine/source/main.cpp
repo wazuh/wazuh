eace1470415a466cbc4395be4ec1ddfbb883218f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-09-02 08:24:27 +0000

    Added subcommands as separate package. - Added test, run and graph under cmds package. - Removed hyphens when getting graphviz environment graph. - Added getExceptionStack under base.

--- src/engine/source/main.cpp
+++ src/engine/source/main.cpp
@@ -1,58 +1,45 @@
-/* Copyright (C) 2015-2022, Wazuh Inc.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation.
- */
-
-#include <atomic>
-#include <csignal>
 #include <memory>
-#include <sstream>
 #include <string>
-#include <thread>
 #include <vector>
 
 #include <CLI/CLI.hpp>
 
-#include <builder.hpp>
-#include <catalog.hpp>
-#include <engineServer.hpp>
-#include <graph.hpp>
-#include <json/json.hpp>
-#include <kvdb/kvdbManager.hpp>
+#include <cmds/cmdGraph.hpp>
+#include <cmds/cmdRun.hpp>
+#include <cmds/cmdTest.hpp>
 #include <logging/logging.hpp>
-#include <profile/profile.hpp>
-#include <protocolHandler.hpp>
-#include <register.hpp>
-// #include <router.hpp>
-#include <hlp/hlp.hpp>
-#include <rxbk/rxFactory.hpp>
-#include <wdb/wdb.hpp>
 
-constexpr auto WAIT_DEQUEUE_TIMEOUT_USEC = 1 * 1000000;
-
-// Static global variables for handling threads
-static std::atomic<bool> gs_doRun = true;
-static std::vector<std::thread> gs_threadList;
+#include "base/utils/stringUtils.hpp"
 
 // Arguments configuration
 namespace args
 {
 // Subcommand names
 constexpr auto SUBCOMMAND_RUN = "run";
-constexpr auto SUBCOMMAND_LOGTEST = "logtest";
-constexpr auto SUBCOMMAND_GRAPH = "generate_graph";
+constexpr auto SUBCOMMAND_LOGTEST = "test";
+constexpr auto SUBCOMMAND_GRAPH = "graph";
+
+// Graph file names
+constexpr auto ENV_DEF_DIR = ".";
+constexpr auto ENV_GRAPH = "env_graph.dot";
+constexpr auto ENV_EXPR_GRAPH = "env_expr_graph.dot";
+
+// Trace all string
+constexpr auto TRACE_ALL = "ALL";
 
 // Arguments
-static std::string endpoint;
-static std::string file_storage;
-static unsigned int queue_size;
-static unsigned int threads;
-static std::string kvdb_path;
-static std::string environment;
+std::string endpoint;
+std::string file_storage;
+unsigned int queue_size;
+unsigned int threads;
+std::string kvdb_path;
+std::string environment;
+std::string graph_out_dir;
+int protocol_queue;
+std::string protocol_location;
+int debug_level;
+std::string asset_trace;
+int log_level;
 
 void configureSubcommandRun(std::shared_ptr<CLI::App> app)
 {
@@ -93,14 +80,64 @@ void configureSubcommandRun(std::shared_ptr<CLI::App> app)
         ->check(CLI::ExistingDirectory);
 
     // Environment
-    run->add_option("--environment", args::environment, "Environment name.")
-        ->required();
+    run->add_option("--environment", args::environment, "Environment name.")->required();
+
+    // Log level
+    run->add_option("-l, --log_level",
+                    args::log_level,
+                    "Log level. 0 = Debug, 1 = Info, 2 = Warning, 3 = Error")
+        ->default_val(3);
 }
 
 void configureSubcommandLogtest(std::shared_ptr<CLI::App> app)
 {
     CLI::App* logtest = app->add_subcommand(args::SUBCOMMAND_LOGTEST,
                                             "Run the Wazuh engine module in test mode.");
+    // KVDB path
+    logtest->add_option("-k, --kvdb_path", args::kvdb_path, "Path to KVDB folder.")
+        ->default_val("/var/ossec/queue/db/kvdb/")
+        ->check(CLI::ExistingDirectory);
+
+    // File storage
+    logtest
+        ->add_option("-f, --file_storage",
+                     args::file_storage,
+                     "Path to folder where assets are located.")
+        ->required()
+        ->check(CLI::ExistingDirectory);
+
+    // Environment
+    logtest->add_option("--environment", args::environment, "Environment name.")
+        ->required();
+
+    // Protocol queue
+    logtest
+        ->add_option("-q, --protocol_queue",
+                     args::protocol_queue,
+                     "Protocol queue number of the event.")
+        ->default_val(1);
+
+    // Protocol location
+    logtest
+        ->add_option(
+            "-l, --protocol_location", args::protocol_location, "Protocol location.")
+        ->default_val("/dev/stdin");
+
+    // Debug levels
+    auto debug = logtest->add_flag("-d, --debug",
+                                   args::debug_level,
+                                   "Enable debug mode [0-2]. "
+                                   "0: No debug, 1: Asset history, 2: 1 + "
+                                   "Full tracing.");
+
+    // Trace
+    logtest
+        ->add_option(
+            "-t, --trace",
+            args::asset_trace,
+            "Assets to be traced, separated by commas. Only effective if debug=2.")
+        ->needs(debug)
+        ->default_val(args::TRACE_ALL);
 }
 
 void configureSubcommandGraph(std::shared_ptr<CLI::App> app)
@@ -108,6 +145,29 @@ void configureSubcommandGraph(std::shared_ptr<CLI::App> app)
     CLI::App* graph = app->add_subcommand(
         args::SUBCOMMAND_GRAPH,
         "Validate and generate environment graph and expression graph.");
+
+    // KVDB path
+    graph->add_option("-k, --kvdb_path", args::kvdb_path, "Path to KVDB folder.")
+        ->default_val("/var/ossec/queue/db/kvdb/")
+        ->check(CLI::ExistingDirectory);
+
+    // File storage
+    graph
+        ->add_option("-f, --file_storage",
+                     args::file_storage,
+                     "Path to folder where assets are located.")
+        ->required()
+        ->check(CLI::ExistingDirectory);
+
+    // Environment
+    graph->add_option("--environment", args::environment, "Environment name.")
+        ->required();
+
+    // Graph dir
+    graph
+        ->add_option(
+            "-o, --output_dir", args::graph_out_dir, "Directory to save graph files")
+        ->default_str(args::ENV_DEF_DIR);
 }
 
 std::shared_ptr<CLI::App> configureCliApp()
@@ -125,164 +185,8 @@ std::shared_ptr<CLI::App> configureCliApp()
 }
 } // namespace args
 
-static void sigint_handler(const int signum)
-{
-    // Inform threads that they must exit
-    gs_doRun = false;
-
-    for (auto& t : gs_threadList)
-    {
-        t.join();
-    };
-
-    exit(0);
-}
-
-// Get all exceptions nested also
-static std::string getFullException(const std::exception& e, int level = 0)
-{
-    std::stringstream ss;
-    ss << std::string(level, ' ') << "exception: " << e.what() << '\n';
-    try
-    {
-        std::rethrow_if_nested(e);
-    }
-    catch (const std::exception& nestedException)
-    {
-        ss << getFullException(nestedException, level + 1);
-    }
-    catch (...)
-    {
-    }
-
-    return ss.str();
-}
-
-static void run()
-{
-    // Init logging
-    // TODO: add cmd to config logging level
-    logging::LoggingConfig logConfig;
-    logConfig.logLevel = logging::LogLevel::Debug;
-    logging::loggingInit(logConfig);
-
-    KVDBManager::init(args::kvdb_path);
-
-    engineserver::EngineServer server {{args::endpoint},
-                                       static_cast<size_t>(args::queue_size)};
-    if (!server.isConfigured())
-    {
-        WAZUH_LOG_ERROR("Could not configure server for endpoint [{}], engine "
-                        "inizialization aborted.",
-                        args::endpoint);
-        return;
-    }
-
-    catalog::Catalog _catalog(catalog::StorageType::Local, args::file_storage);
-
-    auto hlpParsers =
-        _catalog.getFileContents(catalog::AssetType::Schema, "wazuh-logql-types");
-    // TODO because builders don't have access to the catalog we are configuring
-    // the parser mappings on start up for now
-    hlp::configureParserMappings(hlpParsers);
-
-    try
-    {
-        builder::internals::registerBuilders();
-    }
-    catch (const std::exception& e)
-    {
-        WAZUH_LOG_ERROR("Exception while registering builders: [{}]",
-                        getFullException(e));
-        return;
-    }
-    // TODO: Handle errors on construction
-    builder::Builder<catalog::Catalog> _builder(_catalog);
-    decltype(_builder.buildEnvironment(args::environment)) env;
-    try
-    {
-        env = _builder.buildEnvironment(args::environment);
-    }
-    catch (const std::exception& e)
-    {
-        WAZUH_LOG_ERROR("Exception while building environment: [{}]",
-                        getFullException(e));
-        return;
-    }
-
-    // Processing Workers (Router), Router is replicated in each thread
-    // TODO: handle hot modification of routes
-    for (auto i = 0; i < args::threads; ++i)
-    {
-        std::thread t {
-            [=, &eventBuffer = server.output()]()
-            {
-                auto controller = rxbk::buildRxPipeline(env);
-
-                // else if (trace)
-                // {
-                //     for (auto assetName : traceNames)
-                //     {
-                //         router.subscribeTraceSink(
-                //             "test_environment", assetName, cerrLogger);
-                //     }
-                // }
-
-                // Thread loop
-                while (gs_doRun)
-                {
-                    std::string event;
-
-                    if (eventBuffer.wait_dequeue_timed(event, WAIT_DEQUEUE_TIMEOUT_USEC))
-                    {
-                        WAZUH_TRACE_SCOPE("Router on-next");
-                        try
-                        {
-                            auto result = base::result::makeSuccess(
-                                engineserver::ProtocolHandler::parse(event));
-                            controller.ingestEvent(
-                                std::make_shared<base::result::Result<base::Event>>(
-                                    std::move(result)));
-                        }
-                        catch (const std::exception& e)
-                        {
-                            WAZUH_LOG_ERROR(
-                                "An error ocurred while parsing a message: [{}]",
-                                e.what());
-                        }
-                    }
-                }
-
-                controller.complete();
-                return 0;
-            }};
-
-        gs_threadList.push_back(std::move(t));
-    }
-
-    server.run();
-
-    logging::loggingTerm();
-
-    return;
-}
-
-static void logtest() {}
-
-static void graph() {}
-
 int main(int argc, char* argv[])
 {
-    // Set Crt+C handler
-    sigset_t sig_empty_mask;
-    sigemptyset(&sig_empty_mask);
-
-    struct sigaction sigintAction;
-    sigintAction.sa_handler = sigint_handler;
-    sigintAction.sa_mask = sig_empty_mask;
-
-    sigaction(SIGINT, &sigintAction, NULL);
-
     // Configure argument parsers
     auto app = args::configureCliApp();
     CLI11_PARSE(*app, argc, argv);
@@ -290,15 +194,41 @@ int main(int argc, char* argv[])
     // Launch parsed subcommand
     if (app->get_subcommand(args::SUBCOMMAND_RUN)->parsed())
     {
-        run();
+        cmd::run(args::kvdb_path,
+                 args::endpoint,
+                 args::queue_size,
+                 args::threads,
+                 args::file_storage,
+                 args::environment,
+                 args::log_level);
     }
     else if (app->get_subcommand(args::SUBCOMMAND_LOGTEST)->parsed())
     {
-        logtest();
+        std::vector<std::string> assetTrace;
+        bool TraceAll = false;
+
+        if (args::TRACE_ALL == args::asset_trace)
+        {
+            TraceAll = true;
+        }
+        else
+        {
+            assetTrace = utils::string::split(args::asset_trace, ',');
+        }
+
+        cmd::test(args::kvdb_path,
+                  args::file_storage,
+                  args::environment,
+                  args::debug_level,
+                  TraceAll,
+                  assetTrace,
+                  args::protocol_queue,
+                  args::protocol_location);
     }
     else if (app->get_subcommand(args::SUBCOMMAND_GRAPH)->parsed())
     {
-        graph();
+        cmd::graph(
+            args::kvdb_path, args::file_storage, args::environment, args::graph_out_dir);
     }
     else
     {
