aa76f04a392358996f776bbffb55f03d2fb894b4
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-09-02 16:17:46 +0000

    s_fromHexa helper function and tests added

--- src/engine/test/source/builder/builders/opBuilderHelperStringFromHexa_test.cpp
+++ src/engine/test/source/builder/builders/opBuilderHelperStringFromHexa_test.cpp
@@ -9,18 +9,48 @@
 using namespace base;
 using namespace builder::internals::builders;
 
-const std::string targetField {"/output"};
+
 const std::string helperFunctionName {"s_fromHexa"};
+
+const std::string targetField {"/output"};
+
 
 TEST(opBuilderHelperStringFromHexa, build)
 {
-    const std::vector<std::string> arguments {"48656C6C6F20776F726C6421"};
+    const std::vector<std::string> arguments {"$dummy"};
     const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
 
     ASSERT_NO_THROW(opBuilderHelperStringFromHexa(tuple));
 }
 
-TEST(opBuilderHelperStringFromHexa, test)
+TEST(opBuilderHelperStringFromHexa, buildNoArgumentsError)
+{
+    const std::vector<std::string> arguments {};
+    const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
+
+    ASSERT_THROW(opBuilderHelperStringFromHexa(tuple), std::runtime_error);
+}
+
+TEST(opBuilderHelperStringFromHexa, buildNoTargetError)
+{
+    const std::vector<std::string> arguments {};
+    const auto tuple = std::make_tuple("", helperFunctionName, arguments);
+
+    ASSERT_THROW(opBuilderHelperStringFromHexa(tuple), std::runtime_error);
+}
+
+TEST(opBuilderHelperStringFromHexa, buildHexaValueError)
+{
+    const std::vector<std::string> arguments {"48656C6C6F20776F726C6421"};
+    const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
+
+    auto event =
+        std::make_shared<json::Json>(R"({"dummy_field": "dummy_value"})");
+
+    ASSERT_THROW(opBuilderHelperStringFromHexa(tuple), std::runtime_error);
+}
+
+TEST(opBuilderHelperStringFromHexa, hexaReference)
 {
     const std::vector<std::string> arguments {"$sourceField"};
     const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
@@ -34,7 +64,43 @@ TEST(opBuilderHelperStringFromHexa, test)
 
     ASSERT_TRUE(result);
 
-    std::cout << result.payload()->prettyStr() << std::endl;
-
     ASSERT_EQ("Hello world!", result.payload()->getString("/output").value());
+}
+
+TEST(opBuilderHelperStringFromHexa, invalidHexaReference)
+{
+    const std::vector<std::string> arguments {"$sourceField"};
+    const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
+
+    auto event =
+        std::make_shared<json::Json>(R"({"sourceField": "48656C6C6F20X776F726C6421"})");
+
+    auto op = opBuilderHelperStringFromHexa(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event);
+
+    ASSERT_FALSE(result);
+}
+
+TEST(opBuilderHelperStringFromHexa, allThePrintableCharacters)
+{
+    const std::vector<std::string> arguments {"$sourceField"};
+    const auto tuple = std::make_tuple(targetField, helperFunctionName, arguments);
+
+    auto event =
+        std::make_shared<json::Json>("{\"sourceField\": "
+                                     "\"202122232425262728292A2B2C2D2E2F30313233343536373"
+                                     "8393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F5051"
+                                     "52535455565758595A5B5C5D5E5F606162636465666768696A6"
+                                     "B6C6D6E6F707172737475767778797A7B7C7D7E\"}");
+
+    auto op = opBuilderHelperStringFromHexa(tuple)->getPtr<Term<EngineOp>>()->getFn();
+
+    result::Result<Event> result = op(event);
+
+    ASSERT_TRUE(result);
+
+    ASSERT_EQ(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`"
+              "abcdefghijklmnopqrstuvwxyz{|}~",
+              result.payload()->getString("/output").value());
 }
