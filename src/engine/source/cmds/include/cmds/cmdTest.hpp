d81768131b26615dbc9b5b7ac493002beea4f87f
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-09-06 07:54:11 +0000

    Added missing docs and QoL changes

--- src/engine/source/cmds/include/cmds/cmdTest.hpp
+++ src/engine/source/cmds/include/cmds/cmdTest.hpp
@@ -7,9 +7,24 @@
 namespace cmd
 {
 
+/**
+ * @brief Run environment in test mode. Inputs from stdin, outputs event to stdout and
+ * debug to stderr.
+ *
+ * @param kvdbPath Path to KVDB folder.
+ * @param fileStorage Path to asset folders.
+ * @param environment Name of the environment to be loaded.
+ * @param logLevel Log level.
+ * @param debugLevel Debug level.
+ * @param traceAll Trace all assets.
+ * @param assetTrace Trace specific assets.
+ * @param protocolQueue Queue of the protocol.
+ * @param protocolLocation Location of the protocol.
+ */
 void test(const std::string& kvdbPath,
           const std::string& fileStorage,
           const std::string& environment,
+          int logLevel,
           int debugLevel,
           bool traceAll,
           const std::vector<std::string>& assetTrace,
