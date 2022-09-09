f2205edc9662727e01c7fff0cda69db260b62207
Christian Borla <borlach12@gmail.com>
2022-09-09 20:10:07 -0300

    Added unit test to windows SendMSGAction function

--- src/win32/win_utils.c
+++ src/win32/win_utils.c
@@ -18,6 +18,12 @@
 #include "sym_load.h"
 #include "../os_net/os_net.h"
 
+#ifdef WAZUH_UNIT_TESTING
+#include "unit_tests/wrappers/windows/libc/kernel32_wrappers.h"
+#include "unit_tests/win32/test_win_utils.c"
+#endif
+
+
 HANDLE hMutex;
 int win_debug_level;
 
@@ -322,7 +328,7 @@ int SendMSGAction(__attribute__((unused)) int queue, const char *message, const 
 
     if (OS_INVALID == wstr_escape(loc_buff, sizeof(loc_buff), (char *) locmsg, '|', ':')) {
         merror(FORMAT_ERROR);
-        return (retval);
+        return retval;
     }
 
     snprintf(tmpstr, OS_MAXSTR, "%c:%s:%s", loc, loc_buff, message);
