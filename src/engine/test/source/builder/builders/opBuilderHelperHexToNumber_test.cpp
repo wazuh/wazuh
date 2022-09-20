6e893f8b9ea782e5f62e8bb0566585b5d748752d
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-09-20 09:50:08 +0000

    Added helper hex to dec, updated win-security

---
+++ src/engine/test/source/builder/builders/opBuilderHelperHexToNumber_test.cpp
@@ -0,0 +1,101 @@
+#include <any>
+#include <gtest/gtest.h>
+#include <vector>
+
+#include <baseTypes.hpp>
+
+#include "opBuilderHelperMap.hpp"
+
+using namespace base;
+namespace bld = builder::internals::builders;
+
+TEST(OpBuilderHelperHexToNumber, Builds)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}});
+    ASSERT_NO_THROW(bld::opBuilderHelperHexToNumber(tuple));
+}
+
+TEST(OpBuilderHelperHexToNumber, WrongParametersSize)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}, {"invalid"}});
+    ASSERT_THROW(bld::opBuilderHelperHexToNumber(tuple), std::runtime_error);
+
+    tuple = std::make_tuple(std::string {"/field"},
+                            std::string {"+s_hex_to_number"},
+                            std::vector<std::string> {});
+    ASSERT_THROW(bld::opBuilderHelperHexToNumber(tuple), std::runtime_error);
+}
+
+TEST(OpBuilderHelperHexToNumber, WrongParameterType)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"invalid"}});
+    ASSERT_THROW(bld::opBuilderHelperHexToNumber(tuple), std::runtime_error);
+}
+
+TEST(OpBuilderHelperHexToNumber, Success)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}});
+
+    auto op = bld::opBuilderHelperHexToNumber(tuple)->getPtr<Term<EngineOp>>()->getFn();
+    auto event = std::make_shared<json::Json>(R"({
+        "ref": "0x1234"
+    })");
+    auto result = op(event);
+    ASSERT_TRUE(result);
+    ASSERT_EQ(result.payload()->getInt("/field"), 4660);
+}
+
+TEST(OpBuilderHelperHexToNumber, FailureBadHex)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}});
+
+    auto op = bld::opBuilderHelperHexToNumber(tuple)->getPtr<Term<EngineOp>>()->getFn();
+    auto event1 = std::make_shared<json::Json>(R"({
+        "ref": "0tx1234g"
+    })");
+    auto event2 = std::make_shared<json::Json>(R"({
+        "ref": "0Notx1234g"
+    })");
+    auto result = op(event1);
+    ASSERT_FALSE(result);
+    result = op(event2);
+    ASSERT_FALSE(result);
+}
+
+TEST(OpBuilderHelperHexToNumber, FailureRefNotFound)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}});
+
+    auto op = bld::opBuilderHelperHexToNumber(tuple)->getPtr<Term<EngineOp>>()->getFn();
+    auto event = std::make_shared<json::Json>(R"({
+        "Notref": "0x1234"
+    })");
+    auto result = op(event);
+    ASSERT_FALSE(result);
+}
+
+TEST(OpBuilderHelperHexToNumber, FailureRefNotString)
+{
+    auto tuple = std::make_tuple(std::string {"/field"},
+                                 std::string {"+s_hex_to_number"},
+                                 std::vector<std::string> {{"$ref"}});
+
+    auto op = bld::opBuilderHelperHexToNumber(tuple)->getPtr<Term<EngineOp>>()->getFn();
+    auto event = std::make_shared<json::Json>(R"({
+        "ref": 1234
+    })");
+    auto result = op(event);
+    ASSERT_FALSE(result);
+}
