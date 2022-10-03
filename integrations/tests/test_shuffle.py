2213ca4318a22cf93bf86db2a3da26f74d9eec6b
Facundo Dalmau <facundo.dalmau@wazuh.com>
2022-10-03 11:15:20 -0300

    Modify test parameters

--- integrations/tests/test_shuffle.py
+++ integrations/tests/test_shuffle.py
@@ -9,14 +9,13 @@ import sys
 import os
 import pytest
 import shuffle
-import random
 
 sys.path.append(os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '..', '..'))
 
 alert_template = {'timestamp': 'year-month-dayThours:minuts:seconds+0000',
                   'rule': {'level': 0, 'description': 'alert description',
-                           'id': 'rule-id',
+                           'id': '',
                            'firedtimes': 1},
                   'id': 'alert_id',
                   'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
@@ -35,7 +34,7 @@ def test_generate_msg(alert, expected_msg, rule_id):
 
     Parameters
     ----------
-    json_alert : json
+    alert : json
         json data that simulates the values that could have been obtained from alert_file_location.
 
     msg : str
@@ -45,43 +44,32 @@ def test_generate_msg(alert, expected_msg, rule_id):
     assert shuffle.generate_msg(alert) == expected_msg
 
 
-@pytest.mark.parametrize('json_alert', [({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
-                                          'rule': {'level': 0, 'description': 'alert description',
-                                                   'id': 'rule-id',
-                                                   'firedtimes': 1},
-                                          'id': 'alert_id',
-                                          'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
-                                          'location': 'wazuh-X'})])
-def test_filtered_msg(json_alert):
+@pytest.mark.parametrize('alert, rule_ids', [(alert_template, shuffle.SKIP_RULE_IDS)])
+def test_filtered_msg(alert, rule_ids):
     """
     Test that the alerts with certain rule ids are filtered.
 
     Parameters
     ----------
-    json_alert : json
+    alert : json
         json data that simulates the values that could have been obtained from alert_file_location.
 
     """
-    json_alert['rule']['id'] = random.choice(shuffle.SKIP_RULE_IDS)
+    for skip_id in rule_ids:
+        alert['rule']['id'] = skip_id
+        assert not shuffle.filter_msg(alert)
 
-    assert not shuffle.filter_msg(json_alert)
 
-
-@pytest.mark.parametrize('json_alert', [({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
-                                          'rule': {'level': 0, 'description': 'alert description',
-                                                   'id': 'rule-id',
-                                                   'firedtimes': 1},
-                                          'id': 'alert_id',
-                                          'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
-                                          'location': 'wazuh-X'})])
-def test_not_filtered_msg(json_alert):
+@pytest.mark.parametrize('alert, rule_id', [(alert_template, 'rule-id')])
+def test_not_filtered_msg(alert, rule_id):
     """
     Test that the alerts that not contain certain rule ids are not filtered.
 
     Parameters
     ----------
-    json_alert : json
+    alert : json
         json data that simulates the values that could have been obtained from alert_file_location.
 
     """
-    assert shuffle.filter_msg(json_alert)
+    alert['rule']['id'] = rule_id
+    assert shuffle.filter_msg(alert)
