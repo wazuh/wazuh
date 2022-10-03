7acf491ab1b1bffd0dbd93150bc3ead2a6b7ce5c
Facundo Dalmau <facundo.dalmau@wazuh.com>
2022-10-03 10:59:46 -0300

    Modify parameterized events

--- integrations/tests/test_shuffle.py
+++ integrations/tests/test_shuffle.py
@@ -14,46 +14,44 @@ import random
 sys.path.append(os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '..', '..'))
 
+alert_template = {'timestamp': 'year-month-dayThours:minuts:seconds+0000',
+                  'rule': {'level': 0, 'description': 'alert description',
+                           'id': 'rule-id',
+                           'firedtimes': 1},
+                  'id': 'alert_id',
+                  'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
+                  'location': 'wazuh-X'}
 
-@pytest.mark.parametrize('json_alert, msg', [({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
-                                               'rule': {'level': 0, 'description': 'alert description',
-                                                        'id': 'rule-id',
-                                                        'firedtimes': 1},
-                                               'id': 'alert_id',
-                                               'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
-                                               'location': 'wazuh-X'},
-                                              '{"severity": 1, "pretext": "WAZUH Alert", "title": "alert description", "text": "full log.", "rule_id": "rule-id", "timestamp": "year-month-dayThours:minuts:seconds+0000", "id": "alert_id", "all_fields": {"timestamp": "year-month-dayThours:minuts:seconds+0000", "rule": {"level": 0, "description": "alert description", "id": "rule-id", "firedtimes": 1}, "id": "alert_id", "full_log": "full log.", "decoder": {"name": "decoder-name"}, "location": "wazuh-X"}}'),
-                                             ({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
-                                               'rule': {'level': 0, 'description': 'alert description',
-                                                        'id': random.choice(shuffle.SKIP_RULE_IDS),
-                                                        'firedtimes': 1},
-                                               'id': 'alert_id',
-                                               'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
-                                               'location': 'wazuh-X'},
-                                              '')])
-def test_generate_msg(json_alert, msg):
+msg_template = '{"severity": 1, "pretext": "WAZUH Alert", "title": "alert description", "text": "full log.", "rule_id": "rule-id", "timestamp": "year-month-dayThours:minuts:seconds+0000", "id": "alert_id", "all_fields": {"timestamp": "year-month-dayThours:minuts:seconds+0000", "rule": {"level": 0, "description": "alert description", "id": "rule-id", "firedtimes": 1}, "id": "alert_id", "full_log": "full log.", "decoder": {"name": "decoder-name"}, "location": "wazuh-X"}}'
+
+
+@pytest.mark.parametrize('alert, expected_msg, rule_id', [
+    (alert_template, msg_template, 'rule-id'),
+    (alert_template, "", shuffle.SKIP_RULE_IDS[0])
+])
+def test_generate_msg(alert, expected_msg, rule_id):
     """
     Test that the expected message is generated when json_alert received.
 
     Parameters
     ----------
     json_alert : json
-        json data that simulates the values that could have
-        been obtained from alert_file_location.
+        json data that simulates the values that could have been obtained from alert_file_location.
 
     msg : str
         message that should be returned by the generate_msg function
     """
-    assert shuffle.generate_msg(json_alert) == msg
+    alert['rule']['id'] = rule_id
+    assert shuffle.generate_msg(alert) == expected_msg
 
 
 @pytest.mark.parametrize('json_alert', [({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
-                                                         'rule': {'level': 0, 'description': 'alert description',
-                                                                  'id': 'rule-id',
-                                                                  'firedtimes': 1},
-                                                         'id': 'alert_id',
-                                                         'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
-                                                         'location': 'wazuh-X'})])
+                                          'rule': {'level': 0, 'description': 'alert description',
+                                                   'id': 'rule-id',
+                                                   'firedtimes': 1},
+                                          'id': 'alert_id',
+                                          'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
+                                          'location': 'wazuh-X'})])
 def test_filtered_msg(json_alert):
     """
     Test that the alerts with certain rule ids are filtered.
@@ -61,8 +59,7 @@ def test_filtered_msg(json_alert):
     Parameters
     ----------
     json_alert : json
-        json data that simulates the values that could have
-        been obtained from alert_file_location.
+        json data that simulates the values that could have been obtained from alert_file_location.
 
     """
     json_alert['rule']['id'] = random.choice(shuffle.SKIP_RULE_IDS)
@@ -84,8 +81,7 @@ def test_not_filtered_msg(json_alert):
     Parameters
     ----------
     json_alert : json
-        json data that simulates the values that could have
-        been obtained from alert_file_location.
+        json data that simulates the values that could have been obtained from alert_file_location.
 
     """
     assert shuffle.filter_msg(json_alert)
