53c0bbf86d0813fdefc9bfe05ac8a44d4152ce2c
Facundo Dalmau <facundo.dalmau@wazuh.com>
2022-10-03 10:41:56 -0300

    Add constant from shuffle.py

--- integrations/tests/test_shuffle.py
+++ integrations/tests/test_shuffle.py
@@ -14,10 +14,6 @@ import random
 sys.path.append(os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '..', '..'))
 
-filter_values = ["87924", "87900", "87901", "87902", "87903", "87904", "86001",
-                 "86002", "86003", "87932", "80710", "87929", "87928",
-                 "5710"]
-
 
 @pytest.mark.parametrize('json_alert, msg', [({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
                                                'rule': {'level': 0, 'description': 'alert description',
@@ -29,7 +25,7 @@ filter_values = ["87924", "87900", "87901", "87902", "87903", "87904", "86001",
                                               '{"severity": 1, "pretext": "WAZUH Alert", "title": "alert description", "text": "full log.", "rule_id": "rule-id", "timestamp": "year-month-dayThours:minuts:seconds+0000", "id": "alert_id", "all_fields": {"timestamp": "year-month-dayThours:minuts:seconds+0000", "rule": {"level": 0, "description": "alert description", "id": "rule-id", "firedtimes": 1}, "id": "alert_id", "full_log": "full log.", "decoder": {"name": "decoder-name"}, "location": "wazuh-X"}}'),
                                              ({'timestamp': 'year-month-dayThours:minuts:seconds+0000',
                                                'rule': {'level': 0, 'description': 'alert description',
-                                                        'id': random.choice(filter_values),
+                                                        'id': random.choice(shuffle.SKIP_RULE_IDS),
                                                         'firedtimes': 1},
                                                'id': 'alert_id',
                                                'full_log': 'full log.', 'decoder': {'name': 'decoder-name'},
@@ -46,7 +42,7 @@ def test_generate_msg(json_alert, msg):
         been obtained from alert_file_location.
 
     msg : str
-        message that should be retourned by the generate_msg function
+        message that should be returned by the generate_msg function
     """
     assert shuffle.generate_msg(json_alert) == msg
 
@@ -69,7 +65,7 @@ def test_filtered_msg(json_alert):
         been obtained from alert_file_location.
 
     """
-    json_alert['rule']['id'] = random.choice(filter_values)
+    json_alert['rule']['id'] = random.choice(shuffle.SKIP_RULE_IDS)
 
     assert not shuffle.filter_msg(json_alert)
 
