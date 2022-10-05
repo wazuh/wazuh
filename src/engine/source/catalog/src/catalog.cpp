54e8fdadc925844876b204b350219aaa4cb829d2
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-10-05 10:34:41 +0000

    Refactored Catalog - Added IValidator interface to Builder - Refactored Catalog to be the public interface of the Engine of Environments, Assets and Schemas manipulation. Previous Catalog responsibilities are implemented by the Store. - Catalog is composed only of it's core functionality, to be improved upon once API is finished.

---
+++ src/engine/source/catalog/src/catalog.cpp
@@ -0,0 +1,216 @@
+#include "catalog/catalog.hpp"
+
+#include <fmt/format.h>
+
+#include "yml2Json.hpp"
+
+namespace catalog
+{
+void Config::validate() const
+{
+    if (!store)
+    {
+        throw std::runtime_error("[Catalog::Config] Store is not set");
+    }
+    if (!validator)
+    {
+        throw std::runtime_error("[Catalog::Config] Validator is not set");
+    }
+    if (!apiReg)
+    {
+        throw std::runtime_error("[Catalog::Config] API Registry is not set");
+    }
+}
+
+Catalog::Catalog(const Config& config)
+{
+    config.validate();
+    m_store = config.store;
+    m_validator = config.validator;
+    m_apiReg = config.apiReg;
+
+    // Json handling
+    m_outFormat[Format::JSON] = [](const json::Json& json)
+    {
+        return json.str();
+    };
+    m_inFormat[Format::JSON] = [](const std::string& str)
+    {
+        std::variant<json::Json, base::Error> result;
+        try
+        {
+            result = json::Json {str.c_str()};
+        }
+        catch (const std::exception& e)
+        {
+            result = base::Error {e.what()};
+        }
+
+        return result;
+    };
+
+    // Yaml handling
+    m_outFormat[Format::YAML] = [](const json::Json& json)
+    {
+        std::variant<std::string, base::Error> result;
+        try
+        {
+            // TODO: Expose internals on json::Json ??
+            rapidjson::Document doc;
+            doc.Parse(json.str().c_str());
+            auto yaml = yml2json::internal::json2yaml(doc);
+            YAML::Emitter out;
+            out << yaml;
+            result = out.c_str();
+        }
+        catch (const std::exception& e)
+        {
+            result = base::Error {e.what()};
+        }
+
+        return result;
+    };
+    m_inFormat[Format::YAML] = [](const std::string& str)
+    {
+        std::variant<json::Json, base::Error> result;
+        try
+        {
+            result = json::Json {yml2json::loadYMLfromString(str)};
+        }
+        catch (const std::exception& e)
+        {
+            result = base::Error {e.what()};
+        }
+
+        return result;
+    };
+}
+
+std::variant<std::string, base::Error> Catalog::get(const base::Name& name,
+                                                    Format format) const
+{
+    auto type = stringToType(name.m_type);
+    if (type == Type::ERROR_TYPE)
+    {
+        return base::Error {fmt::format("[Catalog::get] Invalid type [{}]", name.m_type)};
+    }
+
+    auto outFormat = m_outFormat.find(format);
+    if (outFormat == m_outFormat.end())
+    {
+        return base::Error {fmt::format("[Catalog::get] Format [{}] is not supported",
+                                        formatToString(format))};
+    }
+
+    auto content = m_store->get(name);
+    if (std::holds_alternative<base::Error>(content))
+    {
+        return base::Error {fmt::format("[Catalog::get] Cannot get [{}], {}",
+                                        name.fullName(),
+                                        std::get<base::Error>(content).message)};
+    }
+
+    // TODO: Validate content depending on type
+    auto contentJson = std::move(std::get<json::Json>(content));
+
+    auto formatResult = outFormat->second(contentJson);
+    if (std::holds_alternative<base::Error>(formatResult))
+    {
+        return base::Error {fmt::format("[Catalog::get] Cannot format to type [{}], {}",
+                                        formatToString(format),
+                                        std::get<base::Error>(formatResult).message)};
+    }
+
+    return std::get<std::string>(formatResult);
+}
+
+std::optional<base::Error>
+Catalog::add(const base::Name& name, const std::string& content, Format format)
+{
+    auto type = stringToType(name.m_type);
+    if (type == Type::ERROR_TYPE)
+    {
+        return base::Error {fmt::format("[Catalog::add] Invalid type [{}]", name.m_type)};
+    }
+
+    auto inFormat = m_inFormat.find(format);
+    if (inFormat == m_inFormat.end())
+    {
+        return base::Error {fmt::format("[Catalog::add] Format [{}] is not supported",
+                                        formatToString(format))};
+    }
+
+    auto json = inFormat->second(content);
+    if (std::holds_alternative<base::Error>(json))
+    {
+        return base::Error {
+            fmt::format("[Catalog::add] Cannot transform type [{}] to JSON, {}",
+                        formatToString(format),
+                        std::get<base::Error>(json).message)};
+    }
+    auto strJson = std::get<json::Json>(json).str();
+    // TODO: Validate content depending on type
+    auto contentJson = std::move(std::get<json::Json>(json));
+
+    auto error = m_store->add(name, contentJson);
+    if (error)
+    {
+        return base::Error {fmt::format("[Catalog::add] Cannot add [{}], {}",
+                                        name.fullName(),
+                                        error.value().message)};
+    }
+
+    return std::nullopt;
+}
+
+std::optional<base::Error> Catalog::del(const base::Name& name)
+{
+    auto type = stringToType(name.m_type);
+    if (type == Type::ERROR_TYPE)
+    {
+        return base::Error {fmt::format("[Catalog::del] Invalid type [{}]", name.m_type)};
+    }
+
+    auto error = m_store->del(name);
+    if (error)
+    {
+        return base::Error {fmt::format("[Catalog::del] Cannot delete [{}], {}",
+                                        name.fullName(),
+                                        error.value().message)};
+    }
+
+    return std::nullopt;
+}
+
+std::optional<base::Error>
+Catalog::validateEnvironment(const json::Json& environment) const
+{
+    // TODO: Validate against appropriate schema first
+
+    std::optional<base::Error> error;
+    error = m_validator->validateEnvironment(environment);
+    if (error)
+    {
+        return base::Error {
+            fmt::format("[Catalog::validateEnvironment] Validation failed, {}",
+                        error.value().message)};
+    }
+
+    return std::nullopt;
+}
+
+std::optional<base::Error> Catalog::validateAsset(const json::Json& asset) const
+{
+    // TODO: Validate against appropriate schema first
+
+    std::optional<base::Error> error;
+    error = m_validator->validateAsset(asset);
+    if (error)
+    {
+        return base::Error {fmt::format("[Catalog::validateAsset] Validation failed, {}",
+                                        error.value().message)};
+    }
+
+    return std::nullopt;
+}
+} // namespace catalog
