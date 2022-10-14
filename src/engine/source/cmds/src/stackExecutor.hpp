daa3c50ba97b7a5f0c5fb8c277ee5d7862074b50
Julian Morales <jmorales@unc.edu.ar>
2022-10-14 10:18:28 +0000

    Create a stack executor for exit handlers

---
+++ src/engine/source/cmds/src/stackExecutor.hpp
@@ -0,0 +1,41 @@
+#ifndef _STACK_EXECUTOR_HPP
+#define _STACK_EXECUTOR_HPP
+
+#include <functional>
+#include <deque>
+
+namespace cmd
+{
+
+/**
+ * @brief Class to execute a stack of functions
+ * 
+ * The functions are executed in reverse order of insertion (LIFO)
+ */
+class StackExecutor
+{
+private:
+    std::deque<std::function<void()>> m_stack; ///< Stack of functions
+
+public:
+    StackExecutor() : m_stack() {}; ///< Default constructor
+    ~StackExecutor() = default;
+
+    /**
+     * @brief Add a function to the stack
+     * 
+     * @param func Function to add
+     */
+    void add(std::function<void()> func); 
+
+    /**
+     * @brief Execute the stack of functions. 
+     * 
+     * The functions are executed in reverse order of insertion (LIFO), and the stack is cleared.
+     */
+    void execute();
+};
+
+} // namespace cmd
+
+#endif // _STACK_EXECUTOR_HPP
