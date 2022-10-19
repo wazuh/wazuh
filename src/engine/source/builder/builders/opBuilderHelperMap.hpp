da9ea8ebfd0b24533a4a64b6dd3d19674f517442
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2022-10-19 21:48:46 +0000

    Helper functions renamed

--- src/engine/source/builder/builders/opBuilderHelperMap.hpp
+++ src/engine/source/builder/builders/opBuilderHelperMap.hpp
@@ -68,7 +68,7 @@ base::Expression opBuilderHelperStringConcat(const std::any& definition);
 /**
  * @brief Transforms an array of strings into a single string field result of concatenate
  * them with a separator between (not at the start or the end).
- * i.e: '<field>: +s_fromArray/$<array_reference1>/<separator>'
+ * i.e: '<field>: +s_from_array/$<array_reference1>/<separator>'
  * @param definition The transformation definition.
  * @throw std::runtime_error if the parameter is not a reference or if theres no
  * Value argument for the separator.
@@ -78,7 +78,7 @@ base::Expression opBuilderHelperStringFromArray(const std::any& definition);
 
 /**
  * @brief Transforms a string of hexa digits into an ASCII string
- * i.e: 'targetField: +s_fromHexa/48656C6C6F20776F726C6421' then 'targetField' would be
+ * i.e: 'targetField: +s_from_hexa/48656C6C6F20776F726C6421' then 'targetField' would be
  * 'Hello world!'
  * @param definition The transformation definition.
  * @throw std::runtime_error if the parameter is not a reference
@@ -88,7 +88,7 @@ base::Expression opBuilderHelperStringFromHexa(const std::any& definition);
 
 /**
  * @brief Transforms a string of hexadecimal digits into a number
- * i.e: 'targetField: +s_hex_to_number/0x1234' then 'targetField' would be 4660
+ * i.e: 'targetField: +s_hex_to_num/0x1234' then 'targetField' would be 4660
  * Fail if the string is not a valid hexadecimal number or the reference is not found.
  *
  * @param definition The transformation definition.
@@ -132,13 +132,13 @@ base::Expression opBuilderHelperIntCalc(const std::any& definition);
 //*             JSON tranform                     *
 //*************************************************
 
-// <field>: +delete_field/
+// <field>: +ef_delete/
 /**
  * @brief Delete a field of the json event
  *
  * @param def The transformation definition.
- * i.e : '<field>: +delete_field
- * @return base::Expression The lifter with the `delete_field` transformation.
+ * i.e : '<field>: +ef_delete
+ * @return base::Expression The lifter with the `ef_delete` transformation.
  */
 base::Expression opBuilderHelperDeleteField(const std::any& definition);
 
@@ -146,8 +146,8 @@ base::Expression opBuilderHelperDeleteField(const std::any& definition);
  * @brief Renames a field of the json event
  *
  * @param def The transformation definition.
- * i.e : '<field>: +rename_field/$<sourceField>
- * @return base::Expression The lifter with the `rename_field` transformation.
+ * i.e : '<field>: +ef_rename/$<sourceField>
+ * @return base::Expression The lifter with the `ef_rename` transformation.
  */
 base::Expression opBuilderHelperRenameField(const std::any& definition);
 
@@ -201,7 +201,7 @@ base::Expression opBuilderHelperAppendSplitString(const std::any& definition);
  * - If source or target are not arrays or objects
  *
  * @param definition Definition of the operation to be built
- * @return base::Expression The lifter with the `merge` transformation.
+ * @return base::Expression The lifter with the `ef_merge` transformation.
  *
  * @throw std::runtime_error if the parameters size is not 1 or is not a reference.
  */
@@ -235,7 +235,7 @@ base::Expression opBuilderHelperEpochTimeFromSystem(const std::any& definition);
 
 /**
  * @brief Builds helper SHA1 hash calculated from a strings or a reference.
- * <field>: +hash_sha1/<string1>|$<string_reference1>
+ * <field>: +h_sha1/<string1>|$<string_reference1>
  *
  * @param definition Definition of the operation to be built.
  * @return base::Expression The Lifter with the SHA1 hash.
