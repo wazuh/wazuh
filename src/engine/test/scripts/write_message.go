535ff74777e8e71d63e8bb22f90d78abef22475b
Julian Morales <jmorales@unc.edu.ar>
2022-10-26 15:56:04 +0000

    Code-server and config added to vagrant file

--- src/engine/test/scripts/write_message.go
+++ src/engine/test/scripts/write_message.go
@@ -1,32 +1,60 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/binary"
-	"flag"
 	"fmt"
 	"net"
 	"os"
+	"bufio"
+    "log"
+	"flag"
 	"strconv"
-	"strings"
 )
 
+func main() {
+
+	var sockPath string = "/var/ossec/queue/sockets/queue" // Path to unix socket
+	var conn net.Conn
+	var logFile string
+	var logMessage string
+	var loops uint
+
+	flag.StringVar(&logFile, "f", "test_logs_base.txt", "Path to dataset of logs")
+	flag.StringVar(&logMessage, "m", "", "Only log")
+	flag.UintVar(&loops,"l", 1, "Number of times we send all the logs of the file")
+	flag.Parse()
+
+
+	conn = connectSockunix(sockPath)
+	defer conn.Close()
+
+	if logMessage == "" {
+		lines, err := readLines(logFile)
+		if err != nil {
+			log.Fatalf("readLines: %s", err)
+		}
+		for i := uint(0); i < loops; i++ {
+			j := 1000 * int(i+1);
+			for _, line := range lines {
+				sockQuery(conn, line, j);
+				j++;
+			}		
+		}
+	} else {
+		sockQuery(conn, logMessage, 000);
+	}
+
+
+
+
+}
+
 // Exit on fail
-func sendLogSock(conn net.Conn, header bool, message string, queue int, location string) {
+func sockQuery(conn net.Conn, message string, agentid int) {
 	var payload []byte
 
-	payload = []byte(strconv.Itoa(queue) + ":" + location + ":" + message)
-
-	if header {
-		secMsg := new(bytes.Buffer)
-		err := binary.Write(secMsg, binary.LittleEndian, int32(len(payload)))
-		if err != nil {
-			fmt.Println("binary.Write failed:", err)
-			os.Exit(1)
-		}
-		payload = append(secMsg.Bytes(), payload...)
-	}
+	agentStr := strconv.Itoa(agentid)
+	ret := '\r'
+	payload = []byte( "1:[" + agentStr + "] (hostname" + agentStr + ") any->/var/cosas:" + message + string(ret))
 
 	if _, err := conn.Write(payload); err != nil {
 		fmt.Printf("Error: %v\n", err)
@@ -35,60 +63,31 @@ func sendLogSock(conn net.Conn, header bool, message string, queue int, location
 	return
 }
 
-func main() {
+// Exit on fail
+func connectSockunix(socket string) net.Conn {
 
-	/*   Arguments	*/
-	// Path/Adress to the sockets
-	var sockPath string
-	// Path/Adress to the sockets
-	var sockProto string
-	// Queue
-	var queue int
-	// Location
-	var location string
-
-	// Parser arguments
-	flag.StringVar(&sockPath, "s", "/var/ossec/queue/sockets/queue", "Path/Adress to the sockets")
-	flag.StringVar(&sockProto, "p", "unixgram", `Known networks are "tcp", "tcp4" (IPv4-only), `+
-		`"tcp6" (IPv6-only), "udp", "udp4" (IPv4-only), `+
-		`"udp6" (IPv6-only), "ip", "ip4" (IPv4-only),`+
-		`"ip6" (IPv6-only), "unix", "unixgram" and "unixpacket". `)
-	flag.IntVar(&queue, "q", 0, "Queue")
-	flag.StringVar(&location, "l", "[123] (hostname_test_bench) any->/var/some_location", "Location")
-	flag.Parse()
-	var inputs []string
-	var input string
-	if flag.NArg() == 0 {
-		reader := bufio.NewReader(os.Stdin)
-		var err error
-		for err == nil {
-			input, err = reader.ReadString('\n')
-			if err == nil {
-				input = strings.TrimSuffix(input, "\n")
-				inputs = append(inputs, input)
-			}
-		}
-
-	} else {
-		for i := range flag.Args() {
-			inputs = append(inputs, flag.Arg(i))
-		}
-	}
-
-	for i := range inputs {
-		fmt.Println(inputs[i])
-	}
-
-	// Connect to the socket
-	conn, err := net.Dial(sockProto, sockPath)
+	conn, err := net.Dial("unixgram", socket)
 	if err != nil {
 		fmt.Printf("Failed to dial: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close()
 
-	// Send the messages
-	for i := range inputs {
-		sendLogSock(conn, true, inputs[i], queue, location)
-	}
+	return conn
+}
+
+// Exit on fail
+func readLines(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+		fmt.Printf("Failed on read: %v\n", err)
+		os.Exit(1)
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
 }
