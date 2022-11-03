a84ad2bd3866fd3a0f39f56e31e3c44df5000869
Santiago Vendramini <sdvendramini@gmail.com>
2022-11-04 00:20:40 +0100

    bat file changed

--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -6,11 +6,11 @@ COPY upgrade\upgrade.bat . > NUL
 COPY upgrade\do_upgrade.ps1 . > NUL
 COPY upgrade\wazuh-agent-*.msi . > NUL
 
-START /B upgrade.bat B
+START upgrade.bat B
 GOTO end
 
 :background
 SLEEP 5 2> NUL || ping -n 5 127.0.0.1 > NUL
-powershell -noprofile -c "Start-Process powershell.exe '.\do_upgrade.ps1'"
+powershell -ExecutionPolicy ByPass -File do_upgrade.ps1
 
 :end
