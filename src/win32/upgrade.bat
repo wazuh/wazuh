d4b62601f602a1af420c5e42ab4680cf5d6fdb6f
Santiago Vendramini <sdvendramini@gmail.com>
2022-11-04 22:17:57 +0100

    change param on update.bat

--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -11,7 +11,7 @@ GOTO end
 
 :background
 SLEEP 5 2> NUL || ping -n 5 127.0.0.1 > NUL
-powershell -ExecutionPolicy ByPass -File do_upgrade.ps1
+powershell -ExecutionPolicy ByPass -File startprocess.ps1
 
 DEL do_upgrade.ps1
 DEL wazuh-agent-*.msi
