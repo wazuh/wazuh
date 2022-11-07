4937d46797e3356a1cd5c3fe894c6294808a4e9d
Santiago Vendramini <sdvendramini@gmail.com>
2022-11-07 22:28:21 +0100

    change  update.bat

--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -11,10 +11,6 @@ GOTO end
 
 :background
 SLEEP 5 2> NUL || ping -n 5 127.0.0.1 > NUL
-powershell -noprofile -c Start-Process C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe '-ExecutionPolicy Bypass -File \"%programfiles(x86)%\ossec-agent\do_upgrade.ps1\"'
-
-DEL do_upgrade.ps1
-DEL wazuh-agent-*.msi
-DEL upgrade.bat
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell '-File \".\do_upgrade.ps1\"'}"
 
 :end
