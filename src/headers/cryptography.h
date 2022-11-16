c466908cae2adf9001fa05ee2bbf8fefbc730bab
Dwordcito <octa.tala@gmail.com>
2022-11-16 13:17:13 -0300

    Changes based on PR review comments.

--- src/headers/cryptography.h
+++ src/headers/cryptography.h
@@ -18,7 +18,7 @@
 /**
  * @brief Verify the signature of a PE file.
  *
- * @param file_name PE file name.
+ * @param path PE file name.
  * @return ERROR_SUCCESS if the signature is valid, ERROR_INVALID_PARAMETER if the file is not a PE file,
  *         ERROR_FILE_NOT_FOUND if the file does not exist, or another error code.
  */
@@ -28,9 +28,7 @@ DWORD verify_pe_signature(const wchar_t *path);
  * @brief Calculate the SHA256 hash of a file.
  *
  * @param path Path to the file.
- * @param hash Buffer to store the hash.
- * @param hash_size Size of the hash buffer.
- * @return int 0 on success otherwise error code.
+ * @return int ERROR_SUCCESS on success otherwise error code.
  */
 DWORD verify_hash_catalog(wchar_t *path);
 
@@ -38,7 +36,9 @@ DWORD verify_hash_catalog(wchar_t *path);
  * @brief Verify the signature of a file using the Windows Catalog.
  *
  * @param path Path to the file.
- * @return int 0 on success otherwise error code.
+ * @param hash Buffer to store the hash.
+ * @param hash_size Size of the hash buffer.
+ * @return int ERROR_SUCCESS on success otherwise error code.
  */
 DWORD get_file_hash(const wchar_t *path, BYTE **hash, DWORD *hash_size);
 
