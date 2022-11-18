4a85e349473c7e20ed03bf339a2c5c8a69891c5d
Juan Cabrera <jcabre88@gmail.com>
2022-11-18 11:43:12 +0100

    Fixed ISO8601 and ISO8601_Z formatting

--- src/engine/source/hlp/src/specificParsers.cpp
+++ src/engine/source/hlp/src/specificParsers.cpp
@@ -131,8 +131,8 @@ static const std::unordered_map<std::string_view, std::tuple<const char*, const 
         {"RFC3154_TIMEZONE", {"%b %d %T %Z", "Mar  1 18:48:50 UTC"}},
         {"24H_LOCALTIME", {"%T", "15:16:01"}},
         {"SYSLOG", {"%b %d %T", "Jun 14 15:16:01"}}, // TODO specify the RCF, rfc3164?
-        {"ISO8601", {"%Y-%d-%mT%T%z", "2018-08-14T14:30:02.203151+02:00"}},
-        {"ISO8601_Z", {"%Y-%d-%mT%TZ", "2018-08-14T14:30:02.203151Z"}},
+        {"ISO8601", {"%Y-%m-%dT%T%z", "2018-08-14T14:30:02.203151+02:00"}},
+        {"ISO8601_Z", {"%Y-%m-%dT%TZ", "2018-08-14T14:30:02.203151Z"}},
         {"HTTPDATE", {"%d/%b/%Y:%T %z", "26/Dec/2016:16:22:14 +0000"}},
         {"NGINX_ERROR", {"%Y/%m/%d %T", "2016/10/25 14:49:34"}},
         {"APACHE_ERROR", {"%a %b %d %T %Y", "Mon Dec 26 16:15:55.103786 2016"}},
