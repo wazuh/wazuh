f089c2c69adea902a4b9f8a7c119a9a51477aa0f
Gabriel Diaz <gdiaz@qswarm.com>
2022-11-27 22:50:23 +0100

    Update parsers to comply with the spec
    All parsers must honor the stop string and the index advancement

--- src/engine/source/hlp2/src/json.cpp
+++ src/engine/source/hlp2/src/json.cpp
@@ -1,11 +1,8 @@
 #include "fmt/format.h"
 #include <hlp/parsec.hpp>
 #include <rapidjson/document.h>
-#include <rapidjson/error/en.h>
 #include <rapidjson/stringbuffer.h>
-#include <rapidjson/writer.h>
 #include <optional>
-#include <list>
 #include <json/json.hpp>
 
 using Stop = std::optional<std::string>;
@@ -18,13 +15,9 @@ parsec::Parser<json::Json> getJSONParser(Stop str, Options lst)
 {
     return [str](std::string_view text, int index)
     {
-        std::string_view fp;
-
-        unsigned long pos;
-        if (!str.has_value()) {
-            fp = text;
-        }
-        else
+        size_t pos = text.size();
+        std::string_view fp = text;
+        if (str.has_value() && ! str.value().empty())
         {
             pos = text.find(str.value(), index);
             if (pos == std::string::npos)
@@ -39,8 +32,6 @@ parsec::Parser<json::Json> getJSONParser(Stop str, Options lst)
         rapidjson::StringStream ss {fp.data()};
         rapidjson::Document doc;
 
-        // Parse the json and stop at the end of the json object, if error return false
-        // TODO: see if there is a way to specify the root JSON type
         doc.ParseStream<rapidjson::kParseStopWhenDoneFlag>(ss);
         if (doc.HasParseError())
         {
