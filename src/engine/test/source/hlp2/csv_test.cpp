96c74c4e8a6286a0196db29b4327b4f09f66f99c
Gabriel Diaz <gdiaz@qswarm.com>
2022-11-27 22:51:33 +0100

    Add more test cases for all parsers

--- src/engine/test/source/hlp2/csv_test.cpp
+++ src/engine/test/source/hlp2/csv_test.cpp
@@ -4,27 +4,54 @@
 #include <string>
 #include <vector>
 #include "run_test.hpp"
+#include <fmt/format.h>
+#include <json/json.hpp>
+
+
+#define GTEST_COUT std::cerr << "[          ] [ DEBUG ] "
 
 TEST(HLP2, CSVParser)
 {
     auto fn = [](std::string in) -> json::Json {
         json::Json doc;
-        try {
         doc = json::Json(in.c_str());
-        } catch ( std::exception & e) {
-            std::cout << "AQUI!! "<< std::endl;
-        }
         return doc;
     };
 
     std::vector<TestCase> testCases {
-        TestCase {"val1,val2,val3,val4",  true, {},Options{"f1","f2","f3","f4" },fn(R"({"f1":"val1","f2":"val2","f3":"val3","f4":"val4"}))"), 0},
-        TestCase {"invalid string",  false,{},Options{"f1","f2","f3","f4" }, fn(R"({"f1":"val1","f2":"val2","f3":"val3","f4":"val4"})"), 12},
+        // A single field CSV is just a field, use other parsers for it
+        TestCase {R"(hi)",  false, {},Options{",","\"","field_1"},fn(R"({"field_1":"hi"})"), 3},
+        TestCase {R"(hi,hi2)",  true,{},Options{",","\"","field_1","field_2"}, fn(R"({"field_1":"hi","field_2":"hi2"})"), 6},
+        TestCase {R"(hi,hi2 bye)",  true, " ",Options{",","\"","field_1","field_2"},fn(R"({"field_1":"hi","field_2":"hi2"})"), 6},
+        TestCase {R"(,)",  true, {},Options{",","\"","field_1","field_2"},fn(R"({"field_1":null,"field_2":null})"), 2},
+        TestCase {R"(, bye)",  true, " ",Options{",","\"","field_1","field_2"},fn(R"({"field_1":null,"field_2":null})"), 2},
+        // An empty field must have its delimiter
+        // pos != end
+        TestCase {R"(hi1,hi2,hi3,hi4 bye)",  true, {},Options{",","\"","field_1","field_2"},fn(R"({"field_1":"hi1","field_2":"hi2"})"), 7},
+        // should we unescape CSV?
+        // pos != end
+        TestCase {R"(,,hi,"semicolon scaped'"",""' <-- other here <,>",other value,,,value new,,)",  true, {},Options{",","\"","null_1","null_2","word","escaped_1","no_escape,null_3","null_4","new","null_5","null_6","null_7"},fn(R"({"null_1":null,"null_2":null,"word":"hi","escaped_1":"semicolon scaped'\",\"' <-- other here <,>","no_escape,null_3":"other value","null_4":null,"new":null,"null_5":"value new","null_6":null,"null_7":null})"), 76},
+        TestCase {R"(f1,f2,f3)",  true, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2", "field_3": "f3"})"), 8},
+        TestCase {R"(f1,f2,f3,f4,f5)",  true, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2", "field_3": "f3", "field_4": "f4"})"), 11},
+        TestCase {R"(f1,f2,f3,"f4,f5")",  true, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3","field_4":"f4,f5"})"), 16},
+        TestCase {R"(f1,f2,f3,"f4,f5)",  false, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3"})"), 8},
+        // A quote can be escaped using another quote, so " must be encoded as "" in a written CSV
+        // if there string contains """, it would be invalid, as there is a single quote
+        TestCase {R"(f1,f2,f3,f4""")",  false, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3"})"), 8},
+        // https://www.rfc-editor.org/rfc/rfc4180 sect 2.5
+        TestCase {R"(f1,f2,f3,f4"""")",  false, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3"})"), 8},
+        TestCase {R"(f1,f2,f3,"--""--")",  true, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3","field_4":"--\"--"})"), 17},
+        TestCase {R"(f1,f2,f3,"--""--",)",  true, {},Options{",","\"","field_1","field_2","field_3","field_4", "field_5"},fn(R"({"field_1":"f1","field_2":"f2","field_3":"f3","field_4":"--\"--","field_5":null})"), 19},
+        TestCase {R"(f1,f2;asd)",  true, ";",Options{",","\"","field_1","field_2"},fn(R"({"field_1":"f1","field_2":"f2"})"), 5},
+        TestCase {R"(f1,"f2;wazuh";asd)",  true, ";asd",Options{",","\"","field_1","field_2"},fn(R"({"field_1":"f1","field_2":"f2;wazuh"})"), 13},
+        TestCase {R"(f1,f2;a,sd)",  true, {},Options{",","\"","field_1","field_2"},fn(R"({"field_1":"f1","field_2":"f2;a"})"), 7},
+        TestCase {R"(0,1.0,,"")",  true, {},Options{",","\"","field_1","field_2","field_3","field_4"},fn(R"({"field_1":0,"field_2":1.0,"field_3":null,"field_4":null})"), 9},
+        TestCase {R"("v1","v2","v3")", true, {}, Options{",","\"","f1","f2","f3"}, fn(R"({"f1":"v1","f2":"v2","f3":"v3"})"), 14}
     };
-
 
     for (auto t : testCases)
     {
         runTest(t, hlp::getCSVParser);
     }
+
 }
