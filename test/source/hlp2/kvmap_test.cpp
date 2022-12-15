6b33f2f49c4459c5549ed6c33214babc142e5936
Julian Morales <jmorales@unc.edu.ar>
2022-12-15 14:27:19 +0000

    Fix IP parser, KVMap and add test

--- test/source/hlp2/kvmap_test.cpp
+++ test/source/hlp2/kvmap_test.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <vector>
 
-TEST(KVParser, KVParser)
+TEST(KVParser, parser)
 {
     auto fn = [](std::string in) -> json::Json
     {
@@ -14,69 +14,75 @@ TEST(KVParser, KVParser)
     };
 
     std::vector<TestCase> testCases {
+        TestCase {R"(f1=v1 f2=v2 f3=v3)",
+                  true,
+                  {""},
+                  Options {"=", " ", "\"", "\\"},
+                  fn(R"({"f1":"v1","f2":"v2","f3":"v3"})"),
+                  17},
         TestCase {R"(f1=v1 f2=v2 f3=v3###)",
                   true,
                   {"###"},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"f1":"v1","f2":"v2","f3":"v3"})"),
                   17},
         TestCase {R"(key1=Value1 Key2=Value2-dummy)",
                   true,
                   {"-dummy"},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"key1":"Value1","Key2":"Value2"})"),
                   23},
         // fail if parser do not reach stop or end
         TestCase {R"(key1=Value1 Key2=)",
                   true,
                   {""},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"key1":"Value1","Key2":null})"),
                   17},
         TestCase {R"(key1=Value1 Key2)",
                   false,
                   {""},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"key1":"Value1"})"),
                   11},
         TestCase {R"(key1=Value1 =Value2)",
                   false,
                   {},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"key1":"Value1"})"),
                   19},
         TestCase {R"(=Value1 =Value2)",
                   false,
                   {},
-                  Options {"=", " ", "'", ""},
+                  Options {"=", " ", "'", "\\"},
                   fn(R"({"key1":"Value1"})"),
                   19},
         //      // should we support multi chars sep or delim?
         //      // TestCase {R"(key1: Value1 Key2: Value2 hi!)", true, {}, Options {":", "
-        //      ", "'", ""}, fn(R"({"key1":"Value1","Key2":"Value2"})"), 19},
+        //      ", "'", "\\"}, fn(R"({"key1":"Value1","Key2":"Value2"})"), 19},
         TestCase {R"(keyX=valueX)",
                   false,
                   {},
-                  Options {":", ",", "'", ""},
+                  Options {":", ",", "'", "\\"},
                   fn(R"({"keyX":"valueX"})"),
                   19},
         TestCase {R"(keyX=valueX)",
                   true,
                   {},
-                  Options {"=", ",", "'", ""},
+                  Options {"=", ",", "'", "\\"},
                   fn(R"({"keyX":"valueX"})"),
                   11},
         TestCase {R"(keyX|valueX)",
                   true,
                   {},
-                  Options {"|", ",", "'", ""},
+                  Options {"|", ",", "'", "\\"},
                   fn(R"({"keyX":"valueX"})"),
                   11},
         // fail if parser do not reach stop or end
         TestCase {R"(keyX:"valueX;";)",
                   false,
                   {},
-                  Options {":", ";", "\"", ""},
+                  Options {":", ";", "\"", "\\"},
                   fn(R"({"keyX":"valueX;"})"),
                   14},
         // fail if parser do not reach stop or end
@@ -86,20 +92,20 @@ TEST(KVParser, KVParser)
                   Options {"=", " ", "\"", "\\"},
                   fn(R"({"key1":null,"key2":null,"key3":null})"),
                   20},
-        TestCase {R"(: ;)", false, {}, Options {":", " ", "'", ""}, fn(R"({})"), 0},
-        TestCase {R"(: valueX;)", false, {}, Options {":", " ", "'", ""}, fn(R"({})"), 0},
-        TestCase {R"(: valueX)", false, {}, Options {":", " ", "'", ""}, fn(R"({})"), 0},
-        TestCase {R"(:valueX)", false, {}, Options {":", " ", "'", ""}, fn(R"({})"), 0},
+        TestCase {R"(: ;)", false, {}, Options {":", " ", "'", "\\"}, fn(R"({})"), 0},
+        TestCase {R"(: valueX;)", false, {}, Options {":", " ", "'", "\\"}, fn(R"({})"), 0},
+        TestCase {R"(: valueX)", false, {}, Options {":", " ", "'", "\\"}, fn(R"({})"), 0},
+        TestCase {R"(:valueX)", false, {}, Options {":", " ", "'", "\\"}, fn(R"({})"), 0},
         TestCase {R"(key1:value1,:value2)",
                   false,
                   {},
-                  Options {":", ",", "'", ""},
+                  Options {":", ",", "'", "\\"},
                   fn(R"({})"),
                   0},
         TestCase {R"(key1:value1,key2:value2,:value3)",
                   false,
                   {},
-                  Options {":", ",", "'", ""},
+                  Options {":", ",", "'", "\\"},
                   fn(R"({})"),
                   0},
         // best effort parsing, index returns up to the most complete kv pair or
@@ -107,13 +113,13 @@ TEST(KVParser, KVParser)
         TestCase {R"(key1:value1,key2:value2,value3)",
                   false,
                   {},
-                  Options {":", ",", "'", ""},
+                  Options {":", ",", "'", "\\"},
                   fn(R"({"key1":"value1","key2":"value2"})"),
                   23},
         TestCase {R"(key1:value1,key2:value2,:)",
                   false,
                   {},
-                  Options {":", ",", "'", ""},
+                  Options {":", ",", "'", "\\"},
                   fn(R"({})"),
                   0},
         // different escape char
@@ -169,26 +175,31 @@ TEST(KVParser, KVParser)
     }
 }
 
-TEST(KVParser, getKVParserConfigErrors)
+TEST(KVParser, build)
 {
-    auto fn = [](std::string in) -> json::Json
-    {
-        json::Json doc;
-        doc.setString(in);
-        return doc;
-    };
+        // ok
+        ASSERT_NO_THROW(hlp::getKVParser({}, {}, Options {"=", " ", "'", "\\"}));
 
-    std::vector<TestCase> testCases {
-        TestCase {"f1=v1 f2=v2", false, {}, Options {}, fn("{}"), 0},
-        TestCase {"f1=v1 f2=v2", false, {}, Options {"="}, fn("{}"), 0},
-        TestCase {"f1=v1 f2=v2", false, {}, Options {"=", " "}, fn("{}"), 0},
-        TestCase {"f1=v1 f2=v2", false, {}, Options {"=", " ", "'"}, fn("{}"), 0},
-        TestCase {"f1=v1 f2=v2", false, {}, Options {"=", " ", "'", "", ""}, fn("{}"), 0},
-        TestCase {
-            "f1=v1 f2=v2", false, {}, Options {"=", " ", "'", "", "", ""}, fn("{}"), 0}};
+        // Missing params
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "'"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " "}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"="}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {}), std::runtime_error);
+        // Exeeds params
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "'", "\\", ""}), std::runtime_error);
 
-    for (auto t : testCases)
-    {
-        runTest(t, hlp::getKVParser);
-    }
+        // Empty params
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"", " ", "'", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", "", "'", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "'", ""}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "'", "\\", ""}), std::runtime_error);
+
+        // Param with more than one char
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"==", " ", "'", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", "  ", "'", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "''", "\\"}), std::runtime_error);
+        ASSERT_THROW(hlp::getKVParser({}, {""}, Options {"=", " ", "'", "\\\\"}), std::runtime_error);
+        
+
 }
