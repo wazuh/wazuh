1de96ca37f9ce96431542fd91db27c6fd588c587
Javier Bejar Mendez <javier.bejar@wazuh.com>
2022-12-15 11:27:24 +0000

    Added missing enum types and QoL changes

--- src/engine/source/hlp2/include/hlp/registerParsers.hpp
+++ src/engine/source/hlp2/include/hlp/registerParsers.hpp
@@ -8,28 +8,33 @@ namespace hlp
 {
 inline void registerParsers(std::shared_ptr<logpar::Logpar> logpar)
 {
-    logpar->registerBuilder(ParserType::P_BOOL, hlp::getBoolParser);
-    logpar->registerBuilder(ParserType::P_BYTE, hlp::getByteParser);
+    // Numeric
     logpar->registerBuilder(ParserType::P_LONG, hlp::getLongParser);
-    logpar->registerBuilder(ParserType::P_FLOAT, hlp::getFloatParser);
     logpar->registerBuilder(ParserType::P_DOUBLE, hlp::getDoubleParser);
+    logpar->registerBuilder(ParserType::P_FLOAT, hlp::getFloatParser);
     logpar->registerBuilder(ParserType::P_SCALED_FLOAT, hlp::getScaledFloatParser);
+    logpar->registerBuilder(ParserType::P_BYTE, hlp::getByteParser);
+    // String
     logpar->registerBuilder(ParserType::P_TEXT, hlp::getTextParser);
-    logpar->registerBuilder(ParserType::P_BINARY, hlp::getBinaryParser);
-    logpar->registerBuilder(ParserType::P_DATE, hlp::getDateParser);
-    logpar->registerBuilder(ParserType::P_IP, hlp::getIPParser);
-    logpar->registerBuilder(ParserType::P_URI, hlp::getUriParser);
-    logpar->registerBuilder(ParserType::P_FQDN, hlp::getFQDNParser);
-    logpar->registerBuilder(ParserType::P_FILE, hlp::getFilePathParser);
     logpar->registerBuilder(ParserType::P_LITERAL, hlp::getLiteralParser);
-    logpar->registerBuilder(ParserType::P_IGNORE, hlp::getIgnoreParser);
     logpar->registerBuilder(ParserType::P_QUOTED, hlp::getQuotedParser);
+    // Enconding
+    logpar->registerBuilder(ParserType::P_BINARY, hlp::getBinaryParser);
+    // Format
     logpar->registerBuilder(ParserType::P_CSV, hlp::getCSVParser);
     logpar->registerBuilder(ParserType::P_DSV, hlp::getDSVParser);
     logpar->registerBuilder(ParserType::P_JSON, hlp::getJSONParser);
-    logpar->registerBuilder(ParserType::P_USER_AGENT, hlp::getUAParser);
     logpar->registerBuilder(ParserType::P_XML, hlp::getXMLParser);
     logpar->registerBuilder(ParserType::P_KV, hlp::getKVParser);
+    // Other types
+    logpar->registerBuilder(ParserType::P_BOOL, hlp::getBoolParser);
+    logpar->registerBuilder(ParserType::P_USER_AGENT, hlp::getUAParser);
+    logpar->registerBuilder(ParserType::P_IP, hlp::getIPParser);
+    logpar->registerBuilder(ParserType::P_DATE, hlp::getDateParser);
+    logpar->registerBuilder(ParserType::P_URI, hlp::getUriParser);
+    logpar->registerBuilder(ParserType::P_FQDN, hlp::getFQDNParser);
+    logpar->registerBuilder(ParserType::P_FILE, hlp::getFilePathParser);
+    logpar->registerBuilder(ParserType::P_IGNORE, hlp::getIgnoreParser);
 }
 
 } // namespace hlp
