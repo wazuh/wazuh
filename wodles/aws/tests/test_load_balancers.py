43e2f1562ffcb7307c7e51ccc1f11a717913e194
CarlosRS <carlos.ridao@wazuh.com>
2022-12-16 12:26:58 +0100

    Add test templates for every bucket and service

---
+++ wodles/aws/tests/test_load_balancers.py
@@ -0,0 +1,49 @@
+import pytest
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket__init__():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket_get_base_prefix():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket_get_service_prefix():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket_iter_regions_and_accounts():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket_get_full_prefix():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSLBBucket_mark_complete():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSALBBucket__init__():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSALBBucket_load_information_from_file():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSCLBBucket__init__():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSCLBBucket_load_information_from_file():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSNLBBucket___init__():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSNLBBucket_load_information_from_file():
+    pass
---
+++ wodles/aws/tests/test_server_access.py
@@ -0,0 +1,33 @@
+import pytest
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess___init__():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess__key_is_old():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_iter_files_in_bucket():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_marker_only_logs_after():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_check_bucket():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_load_information_from_file():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_parse_line():
+    pass
+
+@pytest.mark.skip("Not implemented yet")
+def test_AWSServerAccess_merge_values():
+    pass
