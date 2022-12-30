9733a3aef33c56afc91624c297fbb01c935e318e
pereyra-m <matias.pereyra@wazuh.com>
2022-12-30 13:17:12 -0300

    Improving error message for libraries loading

--- src/shared/sym_load.c
+++ src/shared/sym_load.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "sym_load.h"
+#include "debug_op.h"
 
 void* so_get_module_handle_on_path(const char *path, const char *so){
 #ifdef WIN32
@@ -8,12 +9,17 @@ void* so_get_module_handle_on_path(const char *path, const char *so){
     return LoadLibrary(file_name);
 #else
     char file_name[4096] = { 0 };
+    void* result = NULL;
 #if defined(__MACH__)
     snprintf(file_name, 4096-1, "%slib%s.dylib", path, so);
 #else
     snprintf(file_name, 4096-1, "%slib%s.so", path, so);
 #endif
-    return dlopen(file_name, RTLD_LAZY);
+    result = dlopen(file_name, RTLD_LAZY);
+    if(result == NULL){
+        mdebug1("Unable to load shared library '%s': %s", file_name, dlerror());
+    }
+    return result;
 #endif
 }
 
@@ -24,18 +30,27 @@ void* so_get_module_handle(const char *so){
     return LoadLibrary(file_name);
 #else
     char file_name[4096] = { 0 };
+    void* result = NULL;
 #if defined(__MACH__)
     snprintf(file_name, 4096-1, "lib%s.dylib", so);
 #else
     snprintf(file_name, 4096-1, "lib%s.so", so);
 #endif
-    return dlopen(file_name, RTLD_LAZY);
+    result = dlopen(file_name, RTLD_LAZY);
+    if(result == NULL){
+        mdebug1("Unable to load shared library '%s': %s", file_name, dlerror());
+    }
+    return result;
 #endif
 }
 
 void* so_get_function_sym(void *handle, const char *function_name){
 #ifndef WIN32
-    return dlsym(handle, function_name);
+    void* result = dlsym(handle, function_name);
+    if(result == NULL){
+        mdebug1("Unable to load symbol '%s': %s", function_name, dlerror());
+    }
+    return result;
 #else
     return (void *)(intptr_t)GetProcAddress((HINSTANCE)handle, function_name);
 #endif
