eb8aefbfeeb87072b700c2037b22099ac8055ef2
Chema Martinez <chema.martinez@wazuh.com>
2023-01-10 17:30:06 +0100

    Improve SQL actions to migrate SCA DB

--- src/wazuh_db/schema_upgrade_v10.sql
+++ src/wazuh_db/schema_upgrade_v10.sql
@@ -8,31 +8,7 @@
  * and/or modify it under the terms of GPLv2.
 */
 
-CREATE TABLE IF NOT EXISTS _sca_check (
-   scan_id INTEGER REFERENCES sca_scan_info (id),
-   id INTEGER PRIMARY KEY,
-   policy_id TEXT REFERENCES sca_policy (id),
-   title TEXT,
-   description TEXT,
-   rationale TEXT,
-   remediation TEXT,
-   file TEXT,
-   process TEXT,
-   directory TEXT,
-   registry TEXT,
-   command TEXT,
-   `references` TEXT,
-   result TEXT,
-   reason TEXT,
-   condition TEXT
-);
-
-INSERT INTO _sca_check SELECT scan_id, id, policy_id, title, description, rationale, remediation, file, process, directory, registry, command, `references`, CASE WHEN result <> '' THEN result ELSE 'not applicable' END AS result, reason, condition FROM sca_check;
-
-DROP TABLE IF EXISTS sca_check;
-
-ALTER TABLE _sca_check RENAME TO sca_check;
-
-CREATE INDEX IF NOT EXISTS policy_id_index ON sca_check (policy_id);
+UPDATE sca_check SET result='not applicable' WHERE status='Not applicable';
+ALTER TABLE sca_check DROP COLUMN status;
 
 INSERT OR REPLACE INTO metadata (key, value) VALUES ('db_version', 10);
