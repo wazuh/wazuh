68747c62843b70ca2c7cc12ff77664c2413e88fc
Julian Morales <jmorales@unc.edu.ar>
2023-01-16 22:10:03 +0000

    Change runtimeEnvironment, delete thread, queue and add method topush envent

--- src/engine/source/router/include/router/router.hpp
+++ src/engine/source/router/include/router/router.hpp
@@ -15,6 +15,19 @@
 #include <expression.hpp>
 #include <registry.hpp>
 
+/*************************************
+TODO:
+- Add list route
+- delete route
+- Refresh routes (pipeline)
+- Check if destination is valid ?
+- check if destination is in the router and his route is up
+- check routes aviable and the states
+- check if the route is valid
+- Implement all api callbacks
+
+*************************************/
+
 namespace router
 {
 
@@ -24,13 +37,14 @@ class Router
 private:
     using concurrentQueue = moodycamel::BlockingConcurrentQueue<base::Event>;
 
-    std::size_t m_numThreads; // Todo const?
-    std::shared_mutex m_mutexRoutes;
-    std::atomic_bool m_isRunning;
-    std::vector<std::thread> m_threads;
+    std::size_t m_numThreads;        ///< Number of threads for the router
+    std::shared_mutex m_mutexRoutes; ///< Mutex to protect the routes map
 
     std::unordered_map<std::string, std::vector<base::Expression>> m_routes;
-    std::shared_ptr<builder::internals::Registry> m_registry;
+
+    std::shared_ptr<builder::internals::Registry> m_registry; ///< Registry for builders
+    std::atomic_bool m_isRunning;       ///< Flag to know if the router is running
+    std::vector<std::thread> m_threads; ///< Vector of threads for the router
 
 public:
     Router(std::shared_ptr<builder::internals::Registry> registry, std::size_t threads = 1)
@@ -43,7 +57,12 @@ public:
     {
         if (threads == 0)
         {
-            throw std::runtime_error("Number of threads can't be 0");
+            throw std::runtime_error("Number of threads of the router can't be 0");
+        }
+
+        if (registry == nullptr)
+        {
+            throw std::runtime_error("Registry can't be null");
         }
     };
 
--- src/engine/source/router/include/router/runtimeEnvironment.hpp
+++ src/engine/source/router/include/router/runtimeEnvironment.hpp
@@ -1,15 +1,12 @@
 #ifndef _ROUTER_RUNTIME_ENVIRONMENT_HPP
 #define _ROUTER_RUNTIME_ENVIRONMENT_HPP
 
-#include <atomic>
 #include <optional>
 #include <string>
 
-#include <api/api.hpp>
 #include <builder.hpp>
 #include <error.hpp>
-
-#include <blockingconcurrentqueue.h>
+#include <rxbk/rxFactory.hpp>
 
 namespace router
 {
@@ -17,60 +14,50 @@ namespace router
 /**
  * @brief Runtime environment represent an environment in memory, ready to be builed and
  * run
+ *
+ * TODO: Should be a thread safe class? (I think not).
+ *  Mutex not only protect the environment, but also the pipeline. 1 event por pipeline at a time.
  */
 class RuntimeEnvironment
 {
 private:
-    using concurrentQueue = moodycamel::BlockingConcurrentQueue<std::string>;
-
     std::string m_asset;
-    std::size_t m_numThreads;
-    std::vector<builder::Environment> m_environments;
-    // Internal state
-    std::atomic<bool> m_isRunning;
-    std::vector<std::thread> m_threads;
+    std::shared_ptr<rxbk::Controller> m_controller;
 
 public:
     /**
      * @brief Construct a new Runtime Environment object
      *
-     * @param asset Asset to be loaded
-     * @param threads Number of threads to be used
-     * @param queue Queue to be used
+     * @param asset Asset of the environment
      */
-    RuntimeEnvironment(std::string asset,
-                       std::size_t threads,
-                       std::shared_ptr<concurrentQueue> queue)
+    RuntimeEnvironment(std::string asset)
         : m_asset {asset}
-        , m_numThreads {threads}
-        , m_environments {}
-        , m_isRunning {false}
-        , m_threads {}
+        , m_controller {}
     {
     }
 
-    ~RuntimeEnvironment() { stop(); m_environments.clear(); }
+    ~RuntimeEnvironment() = default;
 
     /**
-     * @brief Start the environment
+     * @brief Build the environment and instantiate the controller.
      *
      * @param builder Builder to be used for environment creation
-     * @return Error message if any
+     * @return Error message if creation fails
+     *
+     * @note: This function is not thread safe. Only one environment can be built at a time.
      */
     std::optional<base::Error> build(std::shared_ptr<builder::Builder> builder);
 
     /**
-     * @brief Run the environment
+     * @brief Inyect an event into the environment
      *
-     * @param queue Queue to be used
-     * @return Error message if any
+     * @param event Event to be inyect
+     * @return std::optional<base::Error>
+     *
+     * @note This function is not thread safe. Only one event at a time, because the expression tree (helper
+     * functions) are not thread safe.
      */
-    std::optional<base::Error> run(std::shared_ptr<concurrentQueue> queue);
-
-    /**
-     * @brief Stop the environment
-     */
-    void stop();
+    std::optional<base::Error> pushEvent(base::Event event);
 };
 
 } // namespace router
