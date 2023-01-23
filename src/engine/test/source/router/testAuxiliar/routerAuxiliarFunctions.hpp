eacfaecd1245644a8edd58616088883564f1efda
Julian Morales <jmorales@unc.edu.ar>
2023-01-23 16:47:46 +0000

    Base API for router added

--- src/engine/test/source/router/testAuxiliar/routerAuxiliarFunctions.hpp
+++ src/engine/test/source/router/testAuxiliar/routerAuxiliarFunctions.hpp
@@ -2,13 +2,16 @@
 #define _ROUTER_AUX_FUNCTIONS_H
 
 #include "utils/stringUtils.hpp"
-#include <registry.hpp>
 #include <builder.hpp>
-
+#include <registry.hpp>
 
 namespace aux
 {
-    std::shared_ptr<builder::Builder> getFakeBuilder();
-}
+std::shared_ptr<builder::Builder> getFakeBuilder();
+
+const std::vector<std::string> sampleEventsStr {
+    R"(2:10.0.0.1:Test Event - deco_1 )", R"(4:10.0.0.1:Test Event - deco_2 )", R"(8:10.0.0.1:Test Event - deco_3 )"};
+
+} // namespace aux
 
 #endif // _ROUTER_AUX_FUNCTIONS_H
