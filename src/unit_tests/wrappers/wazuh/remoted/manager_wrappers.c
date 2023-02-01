818f5420f2badbb230af4e901e876fb9e7ff2fc0
Tomas Turina <tomas.turina@wazuh.com>
2023-02-01 16:29:38 +0000

    Add UT for new functions assign_group_to_agent and assign_group_to_agent_worker

---
+++ src/unit_tests/wrappers/wazuh/remoted/manager_wrappers.c
@@ -0,0 +1,22 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
+
+
+#include <stddef.h>
+#include <stdarg.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "manager_wrappers.h"
+
+cJSON *__wrap_assign_group_to_agent(const char *agent_id, const char *md5) {
+    check_expected(agent_id);
+    check_expected(md5);
+
+    return mock_type(cJSON *);
+}
