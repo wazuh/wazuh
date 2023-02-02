76812cb944569fc0651076d680f9b1d1dcdc9e6c
Javier Bejar Mendez <javier.bejar@wazuh.com>
2023-02-02 11:13:03 +0000

    Updated vagrant deploy scripts

--- src/engine/test/scripts/wazuh-bin-engine.sh
+++ src/engine/test/scripts/wazuh-bin-engine.sh
@@ -6,11 +6,11 @@ ENGINE_DIR=$WAZUH_DIR/engine
 OUTPUT_FILE_PATH=/tmp/engine.log
 
 # wazuh-engine configuration parameters
-LOG_LEVEL=0
-KVDB_PATH=$WAZUH_DIR/etc/kvdb/
-STORE_PATH=$ENGINE_DIR/store/
-EVENTS_QUEUE_PATH=$WAZUH_DIR/queue/sockets/queue
-API_QUEUE_PATH=$WAZUH_DIR/queue/sockets/engine-api
+export WZE_LOG_LEVEL=0
+export WZE_KVDB_PATH=$WAZUH_DIR/etc/kvdb/
+export WZE_STORE_PATH=$ENGINE_DIR/store/
+export WZE_EVENT_SOCK=$WAZUH_DIR/queue/sockets/queue
+export WZE_API_SOCK=$WAZUH_DIR/queue/sockets/engine-api
 
 # TODO: improve the config testing section
 if [ "$1" = "-t" ]; then
@@ -18,7 +18,7 @@ if [ "$1" = "-t" ]; then
 fi
 
 # Start Engine daemon
-nohup $ENGINE_DIR/wazuh-engine start -e $EVENTS_QUEUE_PATH -a $API_QUEUE_PATH -f $STORE_PATH -k $KVDB_PATH -l $LOG_LEVEL >> $OUTPUT_FILE_PATH 2>&1 &
+nohup $ENGINE_DIR/wazuh-engine server start >> $OUTPUT_FILE_PATH 2>&1 &
 
 # Control file for wazuh-control, this file contains the wazuh-engine pid
 echo $! > $WAZUH_DIR/var/run/wazuh-engine-$!.pid
@@ -26,7 +26,7 @@ echo $! > $WAZUH_DIR/var/run/wazuh-engine-$!.pid
 sleep 1 # Sleep to give time for the queues to be created
 
 # Queues ownership setup
-chown wazuh:wazuh $EVENTS_QUEUE_PATH
-chown wazuh:wazuh $API_QUEUE_PATH
+chown wazuh:wazuh $WZE_EVENT_SOCK
+chown wazuh:wazuh $WZE_API_SOCK
 
 echo "" >> /tmp/engine.log
