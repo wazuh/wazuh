118ff5a9389a8cae15ce6554bcabde68a42216ce
Facundo Dalmau <facundo.dalmau@wazuh.com>
2023-02-14 17:36:35 -0300

    Add __all__ keyword in __init__ files

--- wodles/aws/buckets_s3/__init__.py
+++ wodles/aws/buckets_s3/__init__.py
@@ -7,3 +7,15 @@ from buckets_s3 import server_access
 from buckets_s3 import umbrella
 from buckets_s3 import vpcflow
 from buckets_s3 import waf
+
+__all__ = [
+    "aws_bucket",
+    "cloudtrail",
+    "config",
+    "guardduty",
+    "load_balancers",
+    "server_access",
+    "umbrella",
+    "vpcflow",
+    "waf"
+]
