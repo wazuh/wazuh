29e8e048af61b5718409c59e818ab745d20ec366
Julian Morales <jmorales@unc.edu.ar>
2023-02-16 19:53:46 +0000

    Make a proto message a library

--- src/engine/source/proto/generateCode.sh
+++ src/engine/source/proto/generateCode.sh
@@ -9,13 +9,14 @@ fi
 # Move to the directory of this script
 OLD_PWD=`pwd`
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-cd $SCRIPT_DIR
-PROTODIR=.
-CPP_DIR=../api/src/messages
-PROTO_DEPS_SRC_DIR=../../build/_deps/protobuf-src/src
 
-# Generate CPP code
-protoc --proto_path=$PROTODIR --cpp_out=$CPP_DIR $PROTODIR/*.proto
+SRC_PROTO_DIR="${SCRIPT_DIR}/src"
+CPP_DIR="${SCRIPT_DIR}/include/eMessages/"
+PROTO_DEPS_SRC_DIR=../../../build/_deps/protobuf-src/src
+
+# Generate CPP code (Move for relative paths)
+cd "${SRC_PROTO_DIR}"
+protoc --proto_path=$PROTO_DEPS_SRC_DIR --proto_path="${SRC_PROTO_DIR}" --cpp_out=$CPP_DIR *.proto
 
 # Go back
 cd $OLD_PWD
