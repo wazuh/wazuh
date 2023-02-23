5a6b95b72968fbd793e640f80aba3c8d764e8fcb
Manuel <manuel.cano@wazuh.com>
2023-02-23 12:00:08 +0100

    Improved code format

--- src/os_auth/main-server.c
+++ src/os_auth/main-server.c
@@ -289,13 +289,13 @@ int main(int argc, char **argv)
                         merror_exit("-%c needs an argument", c);
                     }
 
-                    if(w_str_is_number(optarg)){
+                    if (w_str_is_number(optarg)) {
                         generate_certificate = true;
                         if (snprintf(cert_val, OS_SIZE_32 + 1, "%s", optarg) > OS_SIZE_32) {
                             mwarn("-%c argument exceeds %d bytes. Certificate validity info truncated", c, OS_SIZE_32);
                         }
                     }
-                    else{
+                    else {
                         merror_exit("-%c needs a numeric argument", c);   
                     }
                     break;
@@ -305,13 +305,13 @@ int main(int argc, char **argv)
                         merror_exit("-%c needs an argument", c);
                     }
                     
-                    if(w_str_is_number(optarg)){
+                    if (w_str_is_number(optarg)) {
                         generate_certificate = true;
                         if (snprintf(cert_key_bits, OS_SIZE_32 + 1, "%s", optarg) > OS_SIZE_32) {
                             mwarn("-%c argument exceeds %d bytes. Certificate key size info truncated", c, OS_SIZE_32);
                         }
                     }
-                    else{
+                    else {
                         merror_exit("-%c needs a numeric argument", c);
                     }
                     break;
@@ -354,7 +354,6 @@ int main(int argc, char **argv)
                     break;
             }
         }
-
 
         if (generate_certificate) {
             // Sanitize parameters
