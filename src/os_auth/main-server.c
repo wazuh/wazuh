cfda51f4caeaa15adee3cbc7e967034c94de1131
Manuel Cano <119335479+mjcr99@users.noreply.github.com>
2023-02-23 09:56:47 +0100

    Improve error message
    Co-authored-by: Chema Martínez <chema.martinez@wazuh.com>

--- src/os_auth/main-server.c
+++ src/os_auth/main-server.c
@@ -237,7 +237,7 @@ int main(int argc, char **argv)
                     }
                     else {
                         if (w_str_is_number(optarg)) {
-                            merror_exit("-%c needs a valid SSL cipher", c); 
+                            merror_exit("-%c needs a valid list of SSL ciphers", c); 
                         }
                         ciphers = optarg;
                     }
