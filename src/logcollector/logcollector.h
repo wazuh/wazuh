1bd323dc975c79c1f610e03b82f1b3fee0e6d4b1
Nicolás Mariano Koremblum <nmkoremblum@gmail.com>
2023-02-27 12:01:23 -0500

    Logcollector modified to collect normal logs as json objects

--- src/logcollector/logcollector.h
+++ src/logcollector/logcollector.h
@@ -36,6 +36,8 @@
 #define OS_LOGCOLLECTOR_JSON_HASH       "hash"
 #define OS_LOGCOLLECTOR_JSON_OFFSET     "offset"
 
+///< JSON field name which will contain a log that is originally not a json object
+#define LOGCOLLECTOR_JSON_EVENT_FIELD   "log"
 
 #include "shared.h"
 #include "config/localfile-config.h"
