1df3fa13aa36e00483ba8308a09276086ba91a5b
Jorge Marino <jorge.marino@wazuh.com>
2023-03-02 15:54:30 +0000

    Adding Opentelemetry Library

---
+++ src/engine/source/metrics/include/metrics.hpp
@@ -0,0 +1,15 @@
+#ifndef _METRICS_H
+#define _METRICS_H
+
+#include <any>
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+namespace metrics
+{
+
+bool metricsInitialize(void);
+
+} // namespace metrics
+#endif // _METRICS_H
