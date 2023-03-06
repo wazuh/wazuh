09697fa23ca06f3c597250b3b2c64882f9d2128c
Jorge Marino <jorge.marino@wazuh.com>
2023-03-06 20:06:02 +0000

    Adding Metrics API

--- src/engine/source/cmds/include/cmds/metrics.hpp
+++ src/engine/source/cmds/include/cmds/metrics.hpp
@@ -5,23 +5,31 @@
 
 #include <CLI/CLI.hpp>
 
+#include <api/wazuhRequest.hpp>
+#include <api/wazuhResponse.hpp>
+#include <json/json.hpp>
+
 
 namespace cmd::metrics
 {
 
-/**
- * @brief Interface to the Metrics Module
- *
- * @param resourceName Name of the resource
- */
-struct Options
+namespace details
 {
-    std::string resourceName;
-};
+constexpr auto ORIGIN_NAME = "engine_integrated_metrics_api";
+
+constexpr auto API_METRICS_DUMP_SUBCOMMAND {"dump"};
+
+std::string commandName(const std::string& command);
+
+json::Json getParameters(const std::string& action);
+
+void processResponse(const api::WazuhResponse& response);
+void singleRequest(const api::WazuhRequest& request, const std::string& socketPath);
+} // namespace details
 
 void configure(CLI::App_p app);
 
-void runDump();
+void runDump(const std::string& socketPath);
 
 } // namespace cmd::metrics
 
