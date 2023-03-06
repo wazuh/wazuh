efa6f9a97952ea71b89e5bc03f34d61357ff2f51
Nicolas Bertoldo <nicobertoldo@gmail.com>
2023-03-06 13:23:23 +0000

    Binary and alphanumeric parser fixed. Some tests improved

--- src/engine/test/source/hlp2/run_test.hpp
+++ src/engine/test/source/hlp2/run_test.hpp
@@ -24,7 +24,6 @@ using namespace hlp;
 //   [3] std::list<std::string> options,
 //   [4] std::string which will be inside the json::Json returned value,
 //   [5] int index position
-//   [6] ReturnDoc a funcion wihch receives [4] and returns a json::Json document
 using TestCase = std::tuple<std::string, bool, Stop, Options, json::Json, size_t>;
 
 namespace
@@ -64,22 +63,27 @@ std::string to_string(const TestCase& testCase)
  *
  * @param t The tuple containing the test case parameters
  * @param parserBuilder The parser builder
+ * @header The header to be appended to the input string
+ * @tail The tail to be appended to the input string
+ * @strictStop The end token is not modified and no tail is added
  */
-static void runTest(TestCase t, std::function<parsec::Parser<json::Json>(std::string, Stop, Options)> parserBuilder)
+static void runTest(TestCase t,
+                    std::function<parsec::Parser<json::Json>(std::string, Stop, Options)> parserBuilder,
+                    const std::string& header = "header ",
+                    const std::string& tail = " tail",
+                    bool strictStop = false)
 {
     parsec::Parser<json::Json> parser;
     auto expectedSuccess = std::get<1>(t);
     auto expectedDoc = std::get<4>(t);
 
-    const std::string& headerString = "header";
-    const std::string& tailString = "tail";
-    std::string token[4][2] = {{"", ""}, {headerString, ""}, {"", tailString}, {headerString, tailString}};
+    std::string token[4][2] = {{"", ""}, {header, ""}, {"", tail}, {header, tail}};
 
     for (int i = 0; i < 4; i++)
     {
         auto stopString = printStop(std::get<2>(t));
         std::list<std::string> tokenList = {token[i][1]};
-        std::list<std::string> stopStringList = stopString.empty() ? tokenList : std::get<2>(t);
+        std::list<std::string> stopStringList = (strictStop || !stopString.empty()) ? std::get<2>(t) : tokenList;
         try
         {
             parser = parserBuilder({}, stopStringList, std::get<3>(t));
@@ -90,7 +94,7 @@ static void runTest(TestCase t, std::function<parsec::Parser<json::Json>(std::st
                 << fmt::format("Error building parser: {}", e.what());
             return;
         }
-        auto fullEvent = token[i][0] + std::get<0>(t) + token[i][1];
+        auto fullEvent = token[i][0] + std::get<0>(t) + (strictStop ? "" : token[i][1]);
         auto r = parser(fullEvent, token[i][0].size());
 
         ASSERT_EQ(r.success(), expectedSuccess)
