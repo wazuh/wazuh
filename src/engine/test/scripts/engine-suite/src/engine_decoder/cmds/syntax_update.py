67a0b1ca318ae2e32b445113affa7cdf517620fe
LucioDonda <donda.lucio@gmail.com>
2023-03-07 21:42:49 +0000

    Adding file input feature, correcting changes and adding plainText handling

--- src/engine/test/scripts/engine-suite/src/engine_decoder/cmds/syntax_update.py
+++ src/engine/test/scripts/engine-suite/src/engine_decoder/cmds/syntax_update.py
@@ -2,38 +2,45 @@ from pathlib import Path
 import shared.resource_handler as rs
 
 
-def apply_in_decoders(resource_handler, directory, method):
+def apply_in_each_decoder(resource_handler, directory, method):
     modified_files = []
     path = Path(directory)
     for p in path.rglob('*.yml'):
-        # get dict from file
-        base_file = resource_handler.load_file(str(p), rs.Format.YML)
-        file_replaced = method(str(base_file))
-        # print('base_file: ' + str(base_file))
-        # print('file_replaced: ' + file_replaced)
-        # compare both dicts
-        if file_replaced != base_file:
-            print('modified ' + str(p))
-            modified_files.append(p)
+        # get string from file
+        base_file = resource_handler.read_plain_text_file(str(p))
+        new_file_content = method(str(base_file))
+        # check if anything has changed
+        if len(new_file_content):
+            modified_files.append(p.name)
+            resource_handler.save_plain_text_file(
+                str(p.parent), p.name, new_file_content)
     return modified_files
 
 
+def name_replace_helper_function(oldName, newName):
+    def modification(file_string):
+        new_file_string = file_string.replace(oldName, newName)
+        if new_file_string != file_string:
+            return new_file_string
+        else:
+            return ""
+    return modification
+
+
 def helper_function_name_change(resource_handler, directory, oldName, newName):
-    # TODO: check before if it has the sign to let the user choose what's better
-    oldNameHelper = '+' + oldName
-    newNameHelper = '+' + newName
-    print('From ' + oldNameHelper + ' to ' + newNameHelper)
+    # check before if it starts with '+' if not add it
+    if oldName[0] != '+':
+        oldName = '+' + oldName
+    if newName[0] != '+':
+        newName = '+' + newName
+    print('Replacing ' + oldName + ' with ' + newName)
 
-    # Instance of modification function
-    def name_replace_helper_function(oldName, newName):
-        def modification(file_string):
-            #replace all string on any key of dict
-            file_string = file_string.replace(oldName, newName)
-            return file_string
-        return modification
+    modifiedFiles = apply_in_each_decoder(
+        resource_handler, directory, name_replace_helper_function(oldName, newName))
 
-    modifiedFiles = apply_in_decoders(
-        resource_handler, directory, name_replace_helper_function(oldNameHelper, newNameHelper))
+    for name in modifiedFiles:
+        print('Modified ' + str(name))
+
     return len(modifiedFiles)
 
 
@@ -41,19 +48,34 @@ def run(args, resource_handler: rs.ResourceHandler):
     oldName = args['old-name']
     newName = args['new-name']
     directory = args['directory']
+    list_file = args['list-file']
 
-    if not oldName or not newName:
-        print("old-name and new-name paramaters are needed.")
+    if list_file:
+        list_file_content = resource_handler.load_file(
+            list_file, rs.Format.JSON)
+        if not list_file_content:
+            print("File must not be empty if used.")
+            exit(1)
+        for key in list_file_content:
+            files_modified = helper_function_name_change(
+                resource_handler, directory, key, list_file_content[key])
+        if files_modified > 0:
+            print('Success ' + str(files_modified) + ' decoders updated.')
+        else:
+            print('None decoder was updated.')
+    elif not oldName or not newName:
+        print("If no file is used then old-name and new-name paramaters are needed.")
         exit(1)
-
-    print(f'Replacing Helper Function Name')
-    files_modified = helper_function_name_change(
-        resource_handler, directory, oldName, newName)
-    if files_modified > 0:
-        print('Success ' + str(files_modified) + ' decoders updated.')
-        print('Update decoders to make changes effective.')
     else:
-        print('None decoder was updated.')
+        # TODO: couldn't unify in same dict
+        files_modified = helper_function_name_change(
+            resource_handler, directory, oldName, newName)
+        if files_modified > 0:
+            print('Success ' + str(files_modified) + ' decoders updated.')
+        else:
+            print('None decoder was updated.')
+
+    print('Update decoders to make changes effective.')
 
 
 def configure(subparsers):
@@ -65,6 +87,9 @@ def configure(subparsers):
 
     parser_update_syntax.add_argument(
         '-n', '--new-name', help=f'New Helper Function Name', type=str, dest='new-name')
+
+    parser_update_syntax.add_argument(
+        '-l', '--list-file', help=f'Path to list of {{"old":"new"}} helper functions names', type=str, dest='list-file')
 
     parser_update_syntax.add_argument('-d', '--directory', help=f'Directory where to look for decoders', type=str, dest='directory',
                                       default='/home/vagrant/engine/wazuh/src/engine/ruleset/decoders')
