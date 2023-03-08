567809705976370fd92817e40de89825c8246934
soynof <nof@live.com.ar>
2023-03-08 20:38:03 +0000

    Fix the command  get from metrics module

--- src/engine/source/cmds/include/cmds/metrics.hpp
+++ src/engine/source/cmds/include/cmds/metrics.hpp
@@ -18,6 +18,8 @@ namespace details
 constexpr auto ORIGIN_NAME = "engine_integrated_metrics_api";
 constexpr auto API_METRICS_DUMP_SUBCOMMAND {"dump"};
 constexpr auto API_METRICS_GET_SUBCOMMAND {"get"};
+constexpr auto API_METRICS_ENABLE_SUBCOMMAND {"enable"};
+constexpr auto API_METRICS_LIST_SUBCOMMAND {"list"};
 
 std::string commandName(const std::string& command);
 json::Json getParameters(const std::string& action);
@@ -29,7 +31,9 @@ void singleRequest(const api::WazuhRequest& request, const std::string& socketPa
 
 void configure(CLI::App_p app);
 void runDump(const std::string& socketPath);
-void runGet(const std::string& socketPath, const std::string& name);
+void runGetInstrument(const std::string& socketPath, const std::string& name);
+void runEnableInstrument(const std::string& socketPath, const std::string& nameInstrument, bool enableState = true);
+void runListInstruments(const std::string& socketPath);
 } // namespace cmd::metrics
 
 #endif // _CMD_METRICS_HPP
