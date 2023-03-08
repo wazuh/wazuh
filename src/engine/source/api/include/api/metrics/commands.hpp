2bf3b53261c514fec3bec4de7840d5ca2f654cbf
soynof <nof@live.com.ar>
2023-03-08 20:02:18 +0000

    Add get command to metrics

--- src/engine/source/api/include/api/metrics/commands.hpp
+++ src/engine/source/api/include/api/metrics/commands.hpp
@@ -7,13 +7,21 @@
 
 namespace api::metrics::cmds
 {
+constexpr char METRICS_NAME_MISSING[] {"Metrics \"Name\" parameter is missing"};
+constexpr char METRICS_NAME_EMPTY[] {"Metrics \"Name\" parameter cannot be empty"};
 
+/**
+ * @brief Get the Metrics's name from the params or return an error
+ *
+ * @param params The json /data from the request
+ * @return [bool, std::string] True if the name is valid, false otherwise
+ *                             The name if it's valid, the error message otherwise.
+ */
+std::tuple<bool, std::string> getNameOrError(const json::Json& params);
 api::CommandFn metricsDumpCmd();
-api::CommandFn metricsEnableCmd();
-api::CommandFn metricsListCmd();
+api::CommandFn metricsGetCmd();
 
 void registerAllCmds(std::shared_ptr<api::Registry> registry);
 } // namespace api::metrics::cmds
 
 #endif // _METRICS_COMMANDS_HPP
-
