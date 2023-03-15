44b1b7f6c4b9db28b254ce4d33e0121b7aeef9d6
pereyra-m <matias.pereyra@wazuh.com>
2023-03-14 21:50:19 -0300

    Adding a parallel execution for RTR

--- rtr.sh
+++ rtr.sh
@@ -14,6 +14,7 @@ GROUPID=$(id -g $USERNAME)
 VERBOSE=''
 BUILD_DOCKER="yes"
 THREADS=""
+PARALLEL="no"
 
 build() {
 
@@ -37,28 +38,43 @@ Help() {
     echo "v     Verbose. Enables debug mode for RTR."
     echo "b     Build docker. By default, it builds the docker image. Set to 'no' to skip the build."
     echo "t     Threads. Sets the number of threads for all the steps, overwriting the value of the json configuration files."
+    echo "p     Parallel. Runs all the steps in parallel. The output directory will contain a folder for each step. The input file will be ignored."
     echo
-    echo "      Example: ./rtr.sh -i ./rtr_inputs/rtr_ut.json"
-    echo "      Example: ./rtr.sh -o /tmp/rtr -s /home/user/wazuh/src/engine -i ./rtr_inputs/rtr_ut.json -v -b no -t 8"
+    echo "      Examples:"
+    echo "      Run all the steps in single mode with default params "
+    echo "          ./rtr.sh"
+    echo "      Run a single step with all default parameters in single mode "
+    echo "          ./rtr.sh -i ./rtr_inputs/rtr_ut.json"
+    echo "      Run a single step in a fixed folder forcing 8 threads and not building the docker image with verbose output: "
+    echo "          ./rtr.sh -o /tmp/rtr -s /home/user/wazuh/src/engine -i ./rtr_inputs/rtr_ut.json -v -b no -t 8"
+    echo "      Run all steps in parallel mode forcing 8 threads in a fixed folder: "
+    echo "          ./rtr.sh -o /tmp/rtr -s /home/user/wazuh/src/engine -p -t 8"
 }
 
 run() {
-    echo "Running RTR on '$SRC_DIR'. Output directory: '$OUTPUT_DIR'. RTR input file: '$RTR_CONFIG'"
-    jq -c '.steps[]' $RTR_CONFIG | while read step
+    LOCAL_SRC_DIR=$1
+    LOCAL_OUTPUT_DIR=$2
+    LOCAL_RTR_CONFIG=$3
+    echo "Running RTR on '$LOCAL_SRC_DIR'. Output directory: '$LOCAL_OUTPUT_DIR'. RTR input file: '$LOCAL_RTR_CONFIG'"
+    jq -c '.steps[]' $LOCAL_RTR_CONFIG | while read step
     do
         STEP_DESC=$(echo $step | jq -r '.description')
         STEP_PARAMS=$(echo $step | jq -r '.parameters | join(" ")')
-        echo "->Step: $STEP_DESC"
-        docker run --rm -v $SRC_DIR:/source -v $OUTPUT_DIR:/output -v $RTR_DIR:/rtr $CONTAINER_NAME /rtr/rtr.py $VERBOSE -u $USERID -g $GROUPID -o /output -s /source $STEP_PARAMS $THREADS
+        echo "->Step: '$STEP_DESC'"
+        STEP_DESC_TRIM=$(echo "${STEP_DESC// /-}" | tr '[:upper:]' '[:lower:]')
+        STEP_DESC_TRIM=$(echo "${STEP_DESC_TRIM//\(/-}")
+        STEP_DESC_TRIM=$(echo "${STEP_DESC_TRIM//\)/-}")
+        RANDOM_VALUE=$RANDOM
+        docker run --rm --name $STEP_DESC_TRIM-$RANDOM_VALUE --hostname $STEP_DESC_TRIM-$RANDOM_VALUE -v $LOCAL_SRC_DIR:/source -v $LOCAL_OUTPUT_DIR:/output -v $RTR_DIR:/rtr $CONTAINER_NAME /rtr/rtr.py $VERBOSE -u $USERID -g $GROUPID -o /output -s /source $STEP_PARAMS $THREADS
         docker_exit_code=$?
         if [ $docker_exit_code -ne 0 ]; then
-            echo "Execution fail, RTR step: $STEP_DESC."
+            echo "Execution fail for '$LOCAL_RTR_CONFIG', RTR step: '$STEP_DESC'."
             return $docker_exit_code
         fi
     done
 }
 
-while getopts ":ho:s:i::vb:t:" option
+while getopts ":ho:s:i::vb:t::p" option
 do
     case "${option}" in
         h) # display Help
@@ -76,6 +92,8 @@ do
             BUILD_DOCKER=${OPTARG};;
         t) # threads
             THREADS="--threads "${OPTARG};;
+        p) # parallel
+            PARALLEL="yes";;
         :) # missing argument
             echo "Error: Missing argument for option '$OPTARG'."
             Help
@@ -91,11 +109,41 @@ if [[ "$BUILD_DOCKER" == "yes" ]]; then
     build
 fi
 
-run
-if [ $? -ne 0 ]; then
-    echo "RTR failed. Results on $OUTPUT_DIR directory."
-    exit 1
+if [[ "$PARALLEL" == "no" ]]; then
+    echo "Running RTR in single mode."
+    run $SRC_DIR $OUTPUT_DIR $RTR_CONFIG
+    if [ $? -ne 0 ]; then
+        echo "RTR failed. Results on $OUTPUT_DIR directory."
+        exit 1
+    else
+        echo "RTR was succesfull. Results on $OUTPUT_DIR directory."
+        exit 0
+    fi
 else
-    echo "RTR was succesfull. Results on $OUTPUT_DIR directory."
-    exit 0
+    echo "Running RTR in parallel mode."
+    PARALLEL_STEPS=$( jq -cr '.steps | join(" ")' $SRC_DIR/rtr_inputs/rtr_parallel.json )
+    PIDS_ARRAY=()
+    FINAL_EXIT_CODE=0
+
+    for step in ${PARALLEL_STEPS[@]}; do
+        echo "Launching RTR step: '$step'"
+        FOLDER_SUFFIX=${step%.*}
+        run $SRC_DIR "$OUTPUT_DIR/$FOLDER_SUFFIX" "$SRC_DIR/rtr_inputs/$step" &
+        PIDS_ARRAY+=( $! )
+    done
+
+    i=0
+    for step in ${PARALLEL_STEPS[@]}; do
+        echo "Waiting for RTR step: $step. PID: ${PIDS_ARRAY[$i]}"
+        wait ${PIDS_ARRAY[$i]} || let "FINAL_EXIT_CODE=1"
+        i=$((i+1))
+    done
+
+    if [ $FINAL_EXIT_CODE -ne 0 ]; then
+        echo "RTR failed. Results on $OUTPUT_DIR directory."
+        exit 1
+    else
+        echo "RTR was succesfull. Results on $OUTPUT_DIR directory."
+        exit 0
+    fi
 fi
