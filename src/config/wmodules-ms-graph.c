3338323665c97ac0e12367b2170c827ca2f199ba
Bryce Shurts <bryce.shurts@infodefense.com>
2023-03-15 16:29:55 -0500

    Framework MS Graph XML read function

--- src/config/wmodules-ms-graph.c
+++ src/config/wmodules-ms-graph.c
@@ -8,3 +8,33 @@
  * License (version 2) as published by the FSF - Free Software
  * Foundation.
  */
+
+#if defined WIN32 || defined __linux__ || defined __MACH__
+
+#include "wazuh_modules/wmodules.h"
+
+static const char* XML_ENABLED = "enabled";
+static const char* XML_RUN_ON_START = "run_on_start";
+
+static const char* XML_VERSION = "version";
+static const char* XML_RESOURCE = "resource";
+
+static const char* XML_CLIENT_ID = "client_id";
+static const char* XML_TENANT_ID = "tenant_id";
+static const char* XML_SECRET_VALUE = "secret_value";
+
+static const char* XML_RESOURCE_NAME = "name";
+static const char* XML_RESOURCE_RELATIONSHIP = "relationship";
+
+int wm_ms_graph_read(const OS_XML* xml, xml_node** nodes, wmodule* module) {
+
+	int i = 0;
+	wm_ms_graph* ms_graph;
+	wm_ms_graph_auth* auth_config;
+	wm_ms_graph_resource** resources;
+
+
+	return 0;
+}
+
+#endif // WIN32 || defined __linux__ || defined __MACH__
