075b29dff85bc17761f87c2856e25397fe22bcd4
Julian Morales <jmorales@unc.edu.ar>
2023-03-20 17:15:44 +0000

    Create API as object

--- src/engine/test/source/server/engineServer_test.cpp
+++ src/engine/test/source/server/engineServer_test.cpp
@@ -1,91 +1,69 @@
-/* Copyright (C) 2015-2021, Wazuh Inc.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation.
- */
-
-#include <string>
-#include <vector>
-
 #include <gtest/gtest.h>
-#include <rxcpp/rx.hpp>
 
-#include <uvw/pipe.hpp>
-#include <uvw/tcp.hpp>
-#include <uvw/udp.hpp>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <thread>
+#include <unistd.h>
 
-//#include "engineServer.hpp"
+#include <server/engineServer.hpp>
 
-//using namespace engineserver;
-using namespace std;
-using namespace rxcpp;
-
-#define GTEST_COUT cerr << "[          ] [ INFO ]"
-
-TEST(ServerTest, InitializesTcp)
+class EngineServerTest : public ::testing::Test
 {
-    //vector<string> config = {"tcp:localhost:5054"};
-    //ASSERT_NO_THROW(EngineServer server(config));
-    GTEST_SKIP();
+protected:
+    void SetUp() override { test_socket_path = "/tmp/gtest_engine_server.sock"; }
+
+    void TearDown() override { unlink(test_socket_path.c_str()); }
+
+    std::string test_socket_path;
+};
+
+TEST_F(EngineServerTest, TestAddEndpointAndStart)
+{
+    engineserver::EngineServer engine_server;
+
+    std::atomic<bool> callback_called(false);
+    std::string test_message = "Test message";
+
+    engine_server.addEndpoint_UnixDatagram_woResponse(test_socket_path,
+                                                      [&callback_called, &test_message](std::string&& received_message)
+                                                      {
+                                                          ASSERT_EQ(test_message, received_message);
+                                                          callback_called.store(true);
+                                                      });
+
+    std::thread server_thread([&engine_server]() { engine_server.start(); });
+
+    // Wait for the server to start
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    int client_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+    ASSERT_NE(client_socket, -1);
+
+    sockaddr_un server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, test_socket_path.c_str(), sizeof(server_addr.sun_path) - 1);
+
+    ssize_t bytes_sent = sendto(client_socket,
+                                test_message.c_str(),
+                                test_message.length(),
+                                0,
+                                reinterpret_cast<sockaddr*>(&server_addr),
+                                sizeof(server_addr));
+    ASSERT_EQ(bytes_sent, static_cast<ssize_t>(test_message.length()));
+
+    // Wait for the callback to be called
+    int retries = 0;
+    while (!callback_called.load() && retries++ < 10)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    close(client_socket);
+
+    ASSERT_TRUE(callback_called.load());
+
+    // Stop the server and join the server thread
+    engine_server.request_stop();
+    server_thread.join();
 }
-
-// TEST(ServerTest, InitializesUdp)
-// {
-//     vector<string> config = {"udp:localhost:5054"};
-//     ASSERT_NO_THROW(EngineServer server(config));
-// }
-
-// TEST(ServerTest, InitializesSocket)
-// {
-//     vector<string> config = {"socket:/tmp/testsocket"};
-//     ASSERT_NO_THROW(EngineServer server(config));
-// }
-
-// TEST(ServerTest, InitializesErrorEndpointType)
-// {
-//     vector<string> config = {"error:localhost:5054"};
-//     ASSERT_THROW(EngineServer server(config), invalid_argument);
-// }
-
-// TEST(ServerTest, RunStopTcp)
-// {
-//     vector<string> config = {"tcp:localhost:5054"};
-//     EngineServer server(config);
-//     ASSERT_NO_THROW(server.run());
-//     // Give time to initialize before closing
-//     this_thread::sleep_for(chrono::milliseconds(5));
-//     ASSERT_NO_THROW(server.close());
-// }
-
-// TEST(ServerTest, RunStopUdp)
-// {
-//     vector<string> config = {"udp:localhost:5054"};
-//     EngineServer server(config);
-//     ASSERT_NO_THROW(server.run());
-//     // Give time to initialize before closing
-//     this_thread::sleep_for(chrono::milliseconds(5));
-//     ASSERT_NO_THROW(server.close());
-// }
-
-// TEST(ServerTest, RunStopSocket)
-// {
-//     vector<string> config = {"socket:/tmp/testsocket"};
-//     EngineServer server(config);
-//     ASSERT_NO_THROW(server.run());
-//     // Give time to initialize before closing
-//     this_thread::sleep_for(chrono::milliseconds(5));
-//     ASSERT_NO_THROW(server.close());
-// }
-
-// TEST(ServerTest, RunStopTcpUdpSocket)
-// {
-//     vector<string> config = {"socket:/tmp/testsocket", "udp:localhost:5054", "tcp:localhost:5054"};
-//     EngineServer server(config);
-//     ASSERT_NO_THROW(server.run());
-//     // Give time to initialize before closing
-//     this_thread::sleep_for(chrono::milliseconds(5));
-//     ASSERT_NO_THROW(server.close());
-// }
