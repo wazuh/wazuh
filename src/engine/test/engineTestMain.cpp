4cd06f559f2a316b0a557566fba334ab62626297
cborla <borlach12@gmail.com>
2023-03-27 15:13:36 +0000

    Rollback engineTestMain

--- src/engine/test/engineTestMain.cpp
+++ src/engine/test/engineTestMain.cpp
@@ -3,17 +3,12 @@
 #include <kvdb/kvdbManager.hpp>
 #include <logging/logging.hpp>
 
-#include "metrics.hpp"
-
 int main(int argc, char** argv)
 {
-    Metrics::instance().initMetrics("metrics_unit_tests", "/root/repos/wazuh/src/engine/build/source/metrics/input_files/metrics-config.json");
     logging::LoggingConfig logConfig;
     logConfig.logLevel = logging::LogLevel::Off;
     logging::loggingInit(logConfig);
 
     ::testing::InitGoogleTest(&argc, argv);
-    auto result = RUN_ALL_TESTS();
-    Metrics::instance().clean();
-    return result;
+    return RUN_ALL_TESTS();
 }
