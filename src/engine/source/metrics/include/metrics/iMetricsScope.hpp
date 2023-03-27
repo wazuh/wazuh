67118eb713df5bbd16af9b73928c83d89dd110c6
Jorge Marino <jorge.marino@wazuh.com>
2023-03-27 18:31:52 +0000

    Tidy up namespaces

--- src/engine/source/metrics/include/metrics/iMetricsScope.hpp
+++ src/engine/source/metrics/include/metrics/iMetricsScope.hpp
@@ -9,40 +9,12 @@ namespace metrics_manager
 class IMetricsScope 
 {
 public:
-    virtual std::shared_ptr<instruments::iCounter<double>> getCounterDouble(const std::string& name) = 0;
-    virtual std::shared_ptr<instruments::iCounter<uint64_t>> getCounterInteger(const std::string& name) = 0;
-    virtual std::shared_ptr<instruments::iCounter<double>> getUpDownCounterDouble(const std::string& name) = 0;
-    virtual std::shared_ptr<instruments::iCounter<int64_t>> getUpDownCounterInteger(const std::string& name) = 0;
-    virtual std::shared_ptr<instruments::iHistogram<double>> getHistogramDouble(const std::string& name) = 0;
-    virtual std::shared_ptr<instruments::iHistogram<uint64_t>> getHistogramInteger(const std::string& name) = 0;
-
-/*
-// Opcion 1
-    virtual InstrumentHandler getCounterDouble(const std::string& name) = 0;
-    virtual InstrumentHandler getCounterInteger(const std::string& name) = 0;
-    virtual InstrumentHandler getHistogram(const std::string& name) = 0;    
-
-
-// Opcion 2
-    virtual InstrumentHandler<CounterDouble> getCounterDouble(const std::string& name) = 0;
-    virtual InstrumentHandler<CounterInteger> getCounterInteger(const std::string& name) = 0;
-    virtual InstrumentHandler<Histogram> getHistogram(const std::string& name) = 0;    
-
-
-// Opcion 3
-    virtual CounterDouble   getCounterDouble(const std::string& name) = 0;
-    virtual CounterInteger  getCounterInteger(const std::string& name) = 0;
-    virtual Histogram       getHistogram(const std::string& name) = 0;    
-
-
-// Opcion 4
-CounterDouble:public InstrumentHandler;
-CounterInteger:public InstrumentHandler;
-Histogram::public InstrumentHandler;
-    virtual CounterDouble getCounterDouble(const std::string& name) = 0;
-    virtual CounterInteger getCounterInteger(const std::string& name) = 0;
-    virtual Histogram getHistogram(const std::string& name) = 0;    
-*/
+    virtual std::shared_ptr<iCounter<double>> getCounterDouble(const std::string& name) = 0;
+    virtual std::shared_ptr<iCounter<uint64_t>> getCounterInteger(const std::string& name) = 0;
+    virtual std::shared_ptr<iCounter<double>> getUpDownCounterDouble(const std::string& name) = 0;
+    virtual std::shared_ptr<iCounter<int64_t>> getUpDownCounterInteger(const std::string& name) = 0;
+    virtual std::shared_ptr<iHistogram<double>> getHistogramDouble(const std::string& name) = 0;
+    virtual std::shared_ptr<iHistogram<uint64_t>> getHistogramInteger(const std::string& name) = 0;
 };
 
 } // namespace metrics_manager
