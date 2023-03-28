c5b22e1775f8a1c36a6b779f0aa422eed30eab31
Javier Bejar Mendez <javier.bejar@wazuh.com>
2023-03-28 09:34:08 +0000

    Added documentation

--- src/engine/source/api/include/api/integration/integration.hpp
+++ src/engine/source/api/include/api/integration/integration.hpp
@@ -11,18 +11,42 @@
 namespace api::integration
 {
 
+/**
+ * @brief Integration API endpoint
+ *
+ */
 class Integration
 {
 private:
     std::shared_ptr<api::catalog::Catalog> m_catalog;
 
 public:
+    /**
+     * @brief Construct a new Integration object
+     *
+     * @param catalog Catalog API dependency
+     */
     Integration(std::shared_ptr<api::catalog::Catalog> catalog)
         : m_catalog(catalog)
     {
     }
+
+    /**
+     * @brief Add an integration to a policy
+     *
+     * @param policy Policy resource
+     * @param integration Integration resource
+     * @return std::optional<base::Error> Error if any
+     */
     std::optional<base::Error> addTo(const api::catalog::Resource& policy, const api::catalog::Resource& integration);
 
+    /**
+     * @brief Remove an integration from a policy
+     *
+     * @param policy Policy resource
+     * @param integration Integration resource
+     * @return std::optional<base::Error> Error if any
+     */
     std::optional<base::Error> removeFrom(const api::catalog::Resource& policy,
                                           const api::catalog::Resource& integration);
 };
