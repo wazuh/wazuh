a7e091e55db1bf444a4410259fe85f24d36b2011
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2023-03-28 15:48:09 +0000

    Logging level type modified

--- test/source/store/drivers/fileDriver/fileDriver_test.cpp
+++ test/source/store/drivers/fileDriver/fileDriver_test.cpp
@@ -20,7 +20,7 @@ protected:
     {
         // Logging setup
         logging::LoggingConfig logConfig;
-        logConfig.logLevel = spdlog::level::off;
+        logConfig.logLevel = "off";
         logConfig.filePath = logging::DEFAULT_TESTS_LOG_PATH;
         logging::loggingInit(logConfig);
 
