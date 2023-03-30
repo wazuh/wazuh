4a2228b2b65bbd05a120e0e21afe34cfd2f335ed
root <root@DESKTOP-THP4I5R>
2023-03-30 09:28:17 -0300

    AbstractClient.connection_result exception handling corrected. Close #16494 AbstractClient.connection_made remove Handler.log_exceptions from asyncio.gather. Close #16495

--- framework/wazuh/core/cluster/client.py
+++ framework/wazuh/core/cluster/client.py
@@ -163,18 +163,21 @@ class AbstractClient(common.Handler):
     def connection_result(self, future_result):
         """Callback function called when the master sends a response to the hello command sent by the worker.
 
+            If the coroutine raises an exception, future_result.result() will raise the exception in this 
+            callback
+
         Parameters
         ----------
         future_result : asyncio.Future object
             Result of the hello request.
         """
-        response_msg = future_result.result()[0]
-        if isinstance(response_msg, Exception):
-            self.logger.error(f"Could not connect to master: {response_msg}.")
-            self.transport.close()
-        else:
+        try:
+            future_result.result()
             self.logger.info("Successfully connected to master.")
             self.connected = True
+        except Exception as e:        
+            self.logger.error(f"Could not connect to master: {str(e)}.")
+            self.transport.close()
 
     def connection_made(self, transport):
         """Define process of connecting to the server.
@@ -185,7 +188,7 @@ class AbstractClient(common.Handler):
             Socket to write data on.
         """
         self.transport = transport
-        future = asyncio.gather(self.log_exceptions(self.send_request(command=b'hello', data=self.client_data)))
+        future = asyncio.gather(self.send_request(command=b'hello', data=self.client_data))
         future.add_done_callback(self.connection_result)
 
     def connection_lost(self, exc):
