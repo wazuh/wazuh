6fc3ff67600052a6ad68aa7bad144897d7e7f515
Nicolas Bertoldo <nicobertoldo@gmail.com>
2023-03-31 20:37:32 +0000

    Test logging fixed

---
+++ src/engine/test/source/common/testsCommon.hpp
@@ -0,0 +1,27 @@
+#ifndef _H_TESTS_COMMON
+#define _H_TESTS_COMMON
+
+#include <logging/logging.hpp>
+
+constexpr char DEFAULT_TESTS_LOG_PATH[] {"/tmp/engine_code_tests.log"};
+
+/**
+ * @brief Initializes the logging module for tests
+ *
+ */
+void inline initLogging(void)
+{
+    static bool initialized = false;
+
+    if (!initialized)
+    {
+        // Logging setup
+        logging::LoggingConfig logConfig;
+        logConfig.logLevel = "off";
+        logConfig.filePath = DEFAULT_TESTS_LOG_PATH;
+        logging::loggingInit(logConfig);
+        initialized = true;
+    }
+}
+
+#endif // _H_TESTS_COMMON
