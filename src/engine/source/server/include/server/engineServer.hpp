1fcda9913d03c4627bea22170d00a349bc1fcf3c
Julian Morales <jmorales@unc.edu.ar>
2023-04-03 14:18:08 +0000

    Adds the Stream protocol handler and test

--- src/engine/source/server/include/server/engineServer.hpp
+++ src/engine/source/server/include/server/engineServer.hpp
@@ -5,29 +5,48 @@
 #include <memory>
 #include <string>
 
-#include <metrics/iMetricsManager.hpp>
-#include <metrics/iMetricsScope.hpp>
+#include <uvw.hpp>
+#include <uvw/async.hpp>
+
+#include <server/endpoint.hpp>
 
 namespace engineserver
 {
-
+/**
+ * @brief The EngineServer class is the main class of the server. It is responsible for managing the endpoints and the
+ * main loop.
+ *
+ * The EngineServer class is the main class of the server. It is responsible for managing the endpoints and the main
+ * loop. The main loop is implemented using uvw, a C++ wrapper for libuv. The loop is the default loop of the
+ * application.
+ *
+ * @warning the default loop is a singleton. This means that the loop is shared between all the classes of the
+ */
 class EngineServer
 {
+    enum class Status
+    {
+        STOPPED,
+        RUNNING,
+        STOPPING
+    };
+private:
+    std::shared_ptr<uvw::Loop> m_loop;
+    Status m_status;
+    std::shared_ptr<uvw::AsyncHandle> m_stopHandle;
+    std::unordered_map<std::string, std::shared_ptr<Endpoint>> m_endpoints;
+
+    void stop();
+
 public:
     EngineServer();
     ~EngineServer();
 
-    // No response is expected
-    void addEndpoint_UnixDatagram_woResponse(const std::string& address, std::function<void(std::string&&)> callback);
+    void addEndpoint(const std::string& name, std::shared_ptr<Endpoint> endpoint);
 
     void start();
     void request_stop();
 
-private:
-    void stop();
-
-    class Impl;                  // Declaración adelantada
-    std::unique_ptr<Impl> pimpl; // Puntero único a la implementación
 };
 
 } // namespace server
