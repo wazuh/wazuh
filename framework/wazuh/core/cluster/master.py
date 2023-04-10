ee9e8e241fe7026338f23c736d2349add0df0da3
RamosFe <federico.ramos@wazuh.com>
2023-04-10 12:25:27 -0300

    Fixed error with initial date in master node.

--- framework/wazuh/core/cluster/master.py
+++ framework/wazuh/core/cluster/master.py
@@ -261,11 +261,11 @@ class MasterHandler(server.AbstractServerHandler, c_common.WazuhCommon):
             return self.process_sync_error_from_worker(data)
         elif command == b'syn_w_g_e':
             logger = self.task_loggers['Agent-groups send']
-            start_time = datetime.strptime(self.send_agent_groups_status['date_start'], DECIMALS_DATE_FORMAT).timestamp()
+            start_time = self.send_agent_groups_status['date_start'].timestamp()
             return c_common.end_sending_agent_information(logger, start_time, data.decode())
         elif command == b'syn_wgc_e':
             logger = self.task_loggers['Agent-groups send full']
-            start_time = datetime.strptime(self.send_agent_groups_status['date_start'], DECIMALS_DATE_FORMAT).timestamp()
+            start_time = self.send_agent_groups_status['date_start'].timestamp()
             return c_common.end_sending_agent_information(logger, start_time, data.decode())
         elif command == b'syn_w_g_err':
             logger = self.task_loggers['Agent-groups send']
