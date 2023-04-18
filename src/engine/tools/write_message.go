b6c09adfab16ae27cd189c69df5ad28e99bb8bc8
LucioDonda <donda.lucio@gmail.com>
2023-04-18 21:12:36 +0000

    Allowing the generation of logcollector and saving to file, modifying write_message to allow queue and location

--- src/engine/tools/write_message.go
+++ src/engine/tools/write_message.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os"
-	"bufio"
-    "log"
-	"flag"
 	"strconv"
 )
 
@@ -16,13 +16,14 @@ func main() {
 	var conn net.Conn
 	var logFile string
 	var logMessage string
+	var isRawMessage bool
 	var loops uint
 
 	flag.StringVar(&logFile, "f", "test_logs_base.txt", "Path to dataset of logs")
 	flag.StringVar(&logMessage, "m", "", "Only log")
-	flag.UintVar(&loops,"l", 1, "Number of times we send all the logs of the file")
+	flag.BoolVar(&isRawMessage, "r", false, "Use raw message")
+	flag.UintVar(&loops, "l", 1, "Number of times we send all the logs of the file")
 	flag.Parse()
-
 
 	conn = connectSockunix(sockPath)
 	defer conn.Close()
@@ -33,29 +34,28 @@ func main() {
 			log.Fatalf("readLines: %s", err)
 		}
 		for i := uint(0); i < loops; i++ {
-			j := 1000 * int(i+1);
+			j := 1000 * int(i+1)
 			for _, line := range lines {
-				sockQuery(conn, line, j);
-				j++;
+				sockQuery(conn, line, j, isRawMessage)
+				j++
 			}
 		}
 	} else {
-		sockQuery(conn, logMessage, 000);
+		sockQuery(conn, logMessage, 000, isRawMessage)
 	}
-
-
-
 
 }
 
 // Exit on fail
-func sockQuery(conn net.Conn, message string, agentid int) {
+func sockQuery(conn net.Conn, message string, agentid int, is_raw bool) {
 	var payload []byte
 
 	agentStr := strconv.Itoa(agentid)
-	//ret := '\r'
-	//payload = []byte( "1:[" + agentStr + "] (hostname" + agentStr + ") any->/var/cosas:" + message + string(ret))
-	payload = []byte( "2:[" + agentStr + "] (hostname" + agentStr + ") any->/var/cosas:" + message)
+	if is_raw {
+		payload = []byte(message)
+	} else {
+		payload = []byte("2:[" + agentStr + "] (hostname" + agentStr + ") any->/var/cosas:" + message)
+	}
 
 	if _, err := conn.Write(payload); err != nil {
 		fmt.Printf("Error: %v\n", err)
@@ -78,17 +78,17 @@ func connectSockunix(socket string) net.Conn {
 
 // Exit on fail
 func readLines(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
+	file, err := os.Open(path)
+	if err != nil {
 		fmt.Printf("Failed on read: %v\n", err)
 		os.Exit(1)
-    }
-    defer file.Close()
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
