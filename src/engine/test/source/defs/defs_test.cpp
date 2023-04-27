586773aed2f5f6edf62b8d788db3867ec479fc25
Javier Bejar Mendez <javier.bejar@wazuh.com>
2023-04-27 10:15:25 +0000

    Created basic defs module

---
+++ src/engine/test/source/defs/defs_test.cpp
@@ -0,0 +1,40 @@
+#include <defs/defs.hpp>
+#include <gtest/gtest.h>
+
+class DefsBuildsTest : public ::testing::TestWithParam<std::tuple<json::Json, bool>>
+{
+};
+
+TEST_P(DefsBuildsTest, Builds) {
+    auto [definitions, shouldPass] = GetParam();
+
+    if (shouldPass)
+    {
+        ASSERT_NO_THROW(auto def = defs::Definitions(definitions));
+    }
+    else
+    {
+        ASSERT_THROW(auto def = defs::Definitions(definitions), std::runtime_error);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Builds,
+    DefsBuildsTest,
+    ::testing::Values(
+        std::make_tuple(json::Json(), false),
+        std::make_tuple(json::Json(R"([])"), false),
+        std::make_tuple(json::Json(R"(["a"])"), false),
+        std::make_tuple(json::Json(R"({})"), false),
+        std::make_tuple(json::Json(R"({"a": 1})"), true),
+        std::make_tuple(json::Json(R"({"a": "1"})"), true),
+        std::make_tuple(json::Json(R"({"a": true})"), true),
+        std::make_tuple(json::Json(R"({"a": false})"), true),
+        std::make_tuple(json::Json(R"({"a": null})"), true),
+        std::make_tuple(json::Json(R"({"a": []})"), true),
+        std::make_tuple(json::Json(R"({"a": {}})"), true),
+        std::make_tuple(json::Json(R"({"a": 1, "b":"1", "c":true, "d":false, "e":null, "f":[], "g":{}})"), true),
+        std::make_tuple(json::Json(R"({"$a": 1})"), false),
+        std::make_tuple(json::Json(R"({"schema.field": "value"})"), false)
+    )
+);
