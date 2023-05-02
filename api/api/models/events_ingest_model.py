cd972a3f93b472113be34c0a2d493ef11778467b
Nicolas Stefani <nicolas.stefi@wazuh.com>
2023-05-02 17:43:15 -0300

    #16068: Improved model and request behavior

--- api/api/models/events_ingest_model.py
+++ api/api/models/events_ingest_model.py
@@ -1,10 +1,13 @@
+from typing import Optional, List, Dict
+
 from api.models.base_model_ import Body
 
 
 class EventsIngestModel(Body):
-    def __init__(self, events: list) -> None:
+
+    def __init__(self, events: Optional[list] = None) -> None:
         self.swagger_types = {
-            'events': list,
+            'events': List[dict],
         }
 
         self.attribute_map = {
@@ -14,7 +17,7 @@ class EventsIngestModel(Body):
         self._events = events
 
     @property
-    def events(self) -> list:
+    def events(self) -> Optional[list]:
         """
         :return: Events list
         :rtype: list
