7a964e962042b6751d54e69423eb03640336adad
Eduardo Leon Wazuh <eduardo.leon@wazuh.com>
2023-05-04 11:35:55 -0300

    Fixed gcp logger tests

--- wodles/gcloud/tests/test_gcp_log_strategy.py
+++ wodles/gcloud/tests/test_gcp_log_strategy.py
@@ -1,6 +1,6 @@
 import logging
 import pytest
-
+from unittest.mock import patch
 from gcp_logger import GCPLogStrategy
 
 
@@ -11,30 +11,34 @@ def gcp_strategy():
 
 def test_info(gcp_strategy, caplog):
     with caplog.at_level(logging.INFO, logger='TestGCPLogStrategy'):
-        gcp_strategy.info('Test info message')
+        with patch.object(gcp_strategy, 'logger', logging.getLogger('TestGCPLogStrategy')):
+            gcp_strategy.info('Test info message')
     assert 'Test info message' in caplog.text
 
 
 def test_debug(gcp_strategy, caplog):
     with caplog.at_level(logging.DEBUG, logger='TestGCPLogStrategy'):
-        gcp_strategy.debug('Test debug message')
+        with patch.object(gcp_strategy, 'logger', logging.getLogger('TestGCPLogStrategy')):
+            gcp_strategy.debug('Test debug message')
     assert 'Test debug message' in caplog.text
 
 
 def test_warning(gcp_strategy, caplog):
     with caplog.at_level(logging.WARNING, logger='TestGCPLogStrategy'):
-        gcp_strategy.warning('Test warning message')
+        with patch.object(gcp_strategy, 'logger', logging.getLogger('TestGCPLogStrategy')):
+            gcp_strategy.warning('Test warning message')
     assert 'Test warning message' in caplog.text
 
 
 def test_error(gcp_strategy, caplog):
     with caplog.at_level(logging.ERROR, logger='TestGCPLogStrategy'):
-        gcp_strategy.error('Test error message')
+        with patch.object(gcp_strategy, 'logger', logging.getLogger('TestGCPLogStrategy')):
+            gcp_strategy.error('Test error message')
     assert 'Test error message' in caplog.text
 
 
 def test_critical(gcp_strategy, caplog):
     with caplog.at_level(logging.CRITICAL, logger='TestGCPLogStrategy'):
-        gcp_strategy.critical('Test critical message')
+        with patch.object(gcp_strategy, 'logger', logging.getLogger('TestGCPLogStrategy')):
+            gcp_strategy.critical('Test critical message')
     assert 'Test critical message' in caplog.text
-
