29b6333730bdb9ea98557db553bb8961573f604f
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2023-05-08 18:12:15 +0000

    Graph command modified to be an API client

--- src/engine/source/cmds/include/cmds/graph.hpp
+++ src/engine/source/cmds/include/cmds/graph.hpp
@@ -1,33 +1,54 @@
 #ifndef _CMD_GRAPH_HPP
 #define _CMD_GRAPH_HPP
 
-#include <memory>
 #include <string>
 
 #include <CLI/CLI.hpp>
+#include <cmds/apiclnt/client.hpp>
+#include <base/utils/wazuhProtocol/wazuhProtocol.hpp>
 
 namespace cmd::graph
 {
+
+namespace details
+{
+
+constexpr auto ORIGIN_NAME = "engine_integrated_graph_api";
+
+/* GRAPH api command (endpoints) */
+constexpr auto API_GRAPH_SUBCOMMAND {"graph"};
+
+} // namespace details
+
 /**
- * @brief Load and build policy to generate policy graph and policy
- * expression graph.
+ * @brief Graph configuration parameters.
  *
- * @param kvdbPath Path to KVDB folder.
- * @param fileStorage Path to asset folders.
- * @param policy Name of the policy to be loaded.
- * @param graphOutDir Directory where the graphs will be saved.
+ * @param graphType Type of graph to get.
+ * @param policyName Name of the policy to get the graph from.
+ * @param serverApiSock Path to the server API socket.
  */
 struct Options
 {
-    std::string kvdbPath;
-    std::string fileStorage;
-    std::string policy;
-    std::string graph;
-    std::string logLevel;
+    std::string graphType;
+    std::string policyName;
+    std::string serverApiSock;
 };
-void run(const Options& options);
 
+/**
+ * @brief Get the graph from the API.
+ *
+ * @param options Graph options.
+ */
+void getGraph(std::shared_ptr<apiclnt::Client> client, const Options& options);
+
+/**
+ * @brief Register all handlers for the graph API.
+ *
+ * @param app CLI app.
+ * @throw std::runtime_error If the command registration fails for any reason and at any
+ */
 void configure(CLI::App_p app);
+
 } // namespace cmd::graph
 
 #endif // _CMD_GRAPH_HPP
