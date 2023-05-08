a51b2799ea5cbcd8f6e7a3f4d22e4cf43efc3c25
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2023-05-08 15:28:15 +0000

    Graph API command added and working

---
+++ source/api/include/api/graph/handlers.hpp
@@ -0,0 +1,39 @@
+#ifndef _API_GRAPH_HANDLERS_HPP
+#define _API_GRAPH_HANDLERS_HPP
+
+#include <api/api.hpp>
+#include <store/drivers/fileDriver.hpp>
+#include <kvdb/kvdbManager.hpp>
+
+namespace api::graph::handlers
+{
+
+/**
+ * @brief Graph configuration parameters.
+ *
+ */
+struct Config
+{
+    std::shared_ptr<store::FileDriver> store;
+
+    std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager;
+};
+
+/** @brief Handler for the resource endpoint get command.
+ *
+ * @param config Graph configuration.
+ * @return api::Handler
+ */
+api::Handler resourceGet(const Config& config);
+
+/** @brief Register all handlers for the graph API.
+ *
+ * @param config Graph configuration.
+ * @param api API instance.
+ * @throw std::runtime_error If the command registration fails for any reason and at any
+ */
+void registerHandlers(const Config& config, std::shared_ptr<api::Api> api);
+
+} // namespace api::graph::handlers
+
+#endif // _API_GRAPH_HANDLERS_HPP
