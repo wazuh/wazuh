83ca6cf7a07877a7b833442ac14b4f9daeeaf380
nof <nof@live.com.ar>
2023-05-09 20:33:51 +0000

    Adding space and scope logical to kvdb}

--- src/engine/source/kvdb2/src/kvdbHandlerCollection.cpp
+++ src/engine/source/kvdb2/src/kvdbHandlerCollection.cpp
@@ -3,7 +3,7 @@
 namespace kvdbManager
 {
 
-std::shared_ptr<IKVDBHandler> KVDBHandlerCollection::getKVDBHandler(rocksdb::ColumnFamilyHandle* cfHandle, const std::string& dbName, const std::string& scopeName)
+std::shared_ptr<IKVDBHandler> KVDBHandlerCollection::getKVDBHandler(rocksdb::DB* db, rocksdb::ColumnFamilyHandle* cfHandle, const std::string& dbName, const std::string& scopeName)
 {
     std::lock_guard<std::mutex> lock(m_mutex);
     auto it = m_mapInstances.find(dbName);
@@ -15,7 +15,7 @@ std::shared_ptr<IKVDBHandler> KVDBHandlerCollection::getKVDBHandler(rocksdb::Col
     }
     else
     {
-        auto spHandler = std::make_shared<KVDBSpace>(m_handleManager, cfHandle, dbName, scopeName);
+        auto spHandler = std::make_shared<KVDBSpace>(m_handleManager, db, cfHandle, dbName, scopeName);
         auto spInstance = std::make_shared<KVDBHandlerInstance>(spHandler);
         spInstance->addScope(scopeName);
         m_mapInstances.insert(std::make_pair(dbName, spInstance));
