6db83877d4b2836c019d8dbb97a5d727e6700cd2
nof <nof@live.com.ar>
2023-05-09 20:33:51 +0000

    Adding space and scope logical to kvdb}

--- src/engine/source/kvdb2/include/kvdb2/kvdbSpace.hpp
+++ src/engine/source/kvdb2/include/kvdb2/kvdbSpace.hpp
@@ -14,19 +14,21 @@ class IKVDBHandlerManager;
 class KVDBSpace : public IKVDBHandler, public KVDBManagedHandler
 {
 public:
-    KVDBSpace(IKVDBHandlerManager* manager, rocksdb::ColumnFamilyHandle* cfHandle, const std::string& spaceName, const std::string& scopeName) :
+    KVDBSpace(IKVDBHandlerManager* manager, rocksdb::DB* db, rocksdb::ColumnFamilyHandle* cfHandle, const std::string& spaceName, const std::string& scopeName) :
         KVDBManagedHandler(manager, scopeName),
         m_spaceName(spaceName),
+        m_pRocksDB(db),
         m_pCFhandle(cfHandle) {}
     ~KVDBSpace();
     std::variant<bool, base::Error> set(const std::string& key, const std::string& value) override;
-    bool add(const std::string& key) override;
-    bool remove(const std::string& key) override;
+    std::variant<bool, base::Error> add(const std::string& key) override;
+    std::variant<bool, base::Error> remove(const std::string& key) override;
     std::variant<bool, base::Error> contains(const std::string& key) override;
     std::variant<std::string, base::Error> get(const std::string& key) override;
 protected:
     std::string m_spaceName;
     rocksdb::ColumnFamilyHandle* m_pCFhandle;
+    rocksdb::DB* m_pRocksDB;
 };
 
 } // namespace kvdbManager
