38172ad4db3c0c0744f1500ffcd8426357bdb743
nof <nof@live.com.ar>
2023-05-09 13:45:09 +0000

    Adding Column Family Handlers and Descriptors to manager

--- src/engine/test/source/kvdb2/kvdb2_test.cpp
+++ src/engine/test/source/kvdb2/kvdb2_test.cpp
@@ -10,7 +10,7 @@ using namespace metricsManager;
 namespace
 {
 
-const std::string KVDB_PATH {"/tmp/kvdbTestSuitePath/"};
+const std::string KVDB_PATH {"/tmp/"};
 const std::string KVDB_DB_FILENAME {"testDB"};
 
 class KVDB2Test : public ::testing::Test
@@ -34,10 +34,14 @@ protected:
         kvdbManager::KVDBManagerOptions kvdbManagerOptions { KVDB_PATH, KVDB_DB_FILENAME };
 
         m_spKVDBManager = std::make_shared<kvdbManager::KVDBManager>(kvdbManagerOptions, spMetrics);
+
+        m_spKVDBManager->initialize();
     };
 
     void TearDown() override
     {
+        m_spKVDBManager->finalize();
+
         if (std::filesystem::exists(KVDB_PATH))
         {
             std::filesystem::remove_all(KVDB_PATH);
@@ -48,6 +52,24 @@ protected:
 TEST_F(KVDB2Test, Startup)
 {
 
+}
+
+TEST_F(KVDB2Test, ScopeTest)
+{
+    auto scope = m_spKVDBManager->getKVDBScope("scope1");
+    auto handler = scope->getKVDBHandler("db_test");
+    bool result = handler->add("key1");
+    ASSERT_TRUE(result);
+
+    auto result1 = handler->contains("key1");
+    ASSERT_TRUE(std::holds_alternative<bool>(result1));
+
+    auto result2 = handler->set("key1", "value");
+    ASSERT_TRUE(std::holds_alternative<bool>(result2));
+
+    auto result3 = handler->get("key1");
+    ASSERT_TRUE(std::holds_alternative<std::string>(result3));
+    ASSERT_EQ(std::get<std::string>(result3), "value");
 }
 
 } // namespace
