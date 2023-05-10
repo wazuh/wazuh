35120993613d0e6cec8f960e3f990272390f3687
Nicolas Stefani <nicolas.stefi@wazuh.com>
2023-05-10 17:19:57 -0300

    #16068: Added docstring to forward_event

--- api/api/controllers/event_controller.py
+++ api/api/controllers/event_controller.py
@@ -16,7 +16,23 @@ from api.util import raise_if_exc, remove_nones_to_dict
 logger = logging.getLogger('wazuh-api')
 
 
-async def forward_event(request, pretty: bool = False, wait_for_complete: bool = False) -> web.Response:
+async def forward_event(request: web.Request, pretty: bool = False, wait_for_complete: bool = False) -> web.Response:
+    """Forward events to analysisd.
+
+    Parameters
+    ----------
+    request : web.Request
+        API Request.
+    pretty : bool, optional
+        Show results in human-readable format, by default False.
+    wait_for_complete : bool, optional
+        Disable timeout response, by default False.
+
+    Returns
+    -------
+    web.Response
+        API Response.
+    """
     Body.validate_content_type(request, expected_content_type='application/json')
     f_kwargs = await EventsIngestModel.get_kwargs(request)
 
