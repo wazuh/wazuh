36e14f357841eaad3551a26f980cf5401ca3402b
LucioDonda <donda.lucio@gmail.com>
2023-05-11 19:06:33 +0000

    Adding default fields for documentation and corrections on the script

--- src/engine/tools/engine-suite/src/engine_integration/cmds/create.py
+++ src/engine/tools/engine-suite/src/engine_integration/cmds/create.py
@@ -1,6 +1,19 @@
 import shared.resource_handler as rs
 
 
+def fill_documentation_base(path, name, resource_handler: rs.ResourceHandler):
+    doc = {}
+    doc["title"] = '<title>'
+    doc["overview"] = '<overview>'
+    doc["compatibility"] = '<compatibility>'
+    doc["configuration"] = '<configuration>'
+    doc["event"] = {'module': '<event.module>', 'dataset':'<event.dataset>'}
+    resource_handler.save_file(path, name, doc, rs.Format.YML)
+
+def fill_changelog_base(path, name, resource_handler: rs.ResourceHandler):
+    doc = {'items' : ['version', 'short', 'pr']}
+    resource_handler.save_file(path, name, doc, rs.Format.YML)
+
 def run(args, resource_handler: rs.ResourceHandler):
     integration_name = args['name']
 
@@ -14,7 +27,10 @@ def run(args, resource_handler: rs.ResourceHandler):
     resource_handler.create_file(f'{integration_name}/fields.yml')
     resource_handler.create_file(f'{integration_name}/logpar.yml')
     resource_handler.create_file(f'{integration_name}/documentation.yml')
+    fill_documentation_base(integration_name, 'documentation.yml', resource_handler)
     resource_handler.create_dir(f'{integration_name}/kvdbs')
+    fill_changelog_base(integration_name, 'changelog.yml', resource_handler)
+
 
 def configure(subparsers):
     parser_create = subparsers.add_parser(
