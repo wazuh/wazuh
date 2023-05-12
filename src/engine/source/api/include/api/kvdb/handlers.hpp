04a38327e7fb2b0ae5d2e3f7b49abf1db9ffcb56
nof <nof@live.com.ar>
2023-05-12 20:07:51 +0000

    Add API commmands: delete and list.

--- src/engine/source/api/include/api/kvdb/handlers.hpp
+++ src/engine/source/api/include/api/kvdb/handlers.hpp
@@ -4,22 +4,24 @@
 #include <memory>
 
 #include <api/api.hpp>
+// TODO: KVDB: remove when refactor ut
 #include <kvdb/kvdbManager.hpp>
+#include <kvdb2/iKVDBManager.hpp>
 
 namespace api::kvdb::handlers
 {
 
 // New commands
-api::Handler managerGet(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
-api::Handler managerPost(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
-api::Handler managerDelete(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
-api::Handler managerDump(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
+api::Handler managerGet(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager);
+api::Handler managerPost(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager);
+api::Handler managerDelete(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager);
+api::Handler managerDump(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager);
 
-api::Handler dbGet(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
-api::Handler dbDelete(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
-api::Handler dbPut(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager);
+api::Handler dbGet(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager, std::shared_ptr<kvdbManager::IKVDBScope> kvdbScope);
+api::Handler dbDelete(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager, std::shared_ptr<kvdbManager::IKVDBScope> kvdbScope);
+api::Handler dbPut(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager, std::shared_ptr<kvdbManager::IKVDBScope> kvdbScope);
 
-void registerHandlers(std::shared_ptr<kvdb_manager::KVDBManager> kvdbManager, std::shared_ptr<api::Api>);
+void registerHandlers(std::shared_ptr<kvdbManager::IKVDBManager> kvdbManager, std::shared_ptr<kvdbManager::IKVDBScope> kvdbScope, std::shared_ptr<api::Api>);
 } // namespace api::kvdb::handlers
 
 #endif // _API_KVDB_HANDLERS_HPP
