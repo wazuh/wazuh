2a901232a8d668fbda1aa9f12b899507e20e1789
Bryce Shurts <bryce.shurts@infodefense.com>
2023-05-15 13:19:52 -0500

    Replace copyright placeholders

--- src/wazuh_modules/wm_ms_graph.h
+++ src/wazuh_modules/wm_ms_graph.h
@@ -1,6 +1,6 @@
 /*
  * Wazuh Module for Microsoft Graph
- * Copyright (C) 2015, Wazuh Inc.
+ * Copyright (C) 2023, InfoDefense Inc.
  * March, 2023.
  *
  * This program is free software; you can redistribute it
