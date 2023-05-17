af0a45b41197f2e077b5884025699a1b50609fc1
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-05-17 18:40:29 +0000

    Parameterize the tests and add mock from the store

---
+++ src/engine/test/source/mocks/include/mocks/store.hpp
@@ -0,0 +1,22 @@
+#ifndef _STORE_HPP
+#define _STORE_HPP
+
+#include <store/istore.hpp>
+#include <gmock/gmock.h>
+
+class MockStoreRead : public store::IStoreRead
+{
+public:
+    MOCK_CONST_METHOD1(get, std::variant<json::Json, base::Error>(const base::Name& name));
+};
+
+class MockStore : public store::IStore
+{
+public:
+    MOCK_METHOD2(add, std::optional<base::Error>(const base::Name& name, const json::Json& content));
+    MOCK_METHOD1(del, std::optional<base::Error>(const base::Name& name));
+    MOCK_METHOD2(update, std::optional<base::Error>(const base::Name& name, const json::Json& content));
+    MOCK_CONST_METHOD1(get, std::variant<json::Json, base::Error>(const base::Name& name));
+};
+
+#endif // _STORE_HPP
