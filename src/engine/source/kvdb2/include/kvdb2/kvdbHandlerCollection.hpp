7da4009cc5343b03587263bcbfd4f0441ec5c5a8
Jorge Marino <jorge.marino@wazuh.com>
2023-05-18 18:51:26 +0000

    Small changes, cleaning up interfaces

--- src/engine/source/kvdb2/include/kvdb2/kvdbHandlerCollection.hpp
+++ src/engine/source/kvdb2/include/kvdb2/kvdbHandlerCollection.hpp
@@ -20,20 +20,28 @@ class KVDBHandlerCollection
 {
 public:
     KVDBHandlerCollection(IKVDBHandlerManager* handleManager) : m_handleManager(handleManager) {}
-    KVDBHandler getKVDBHandler(rocksdb::DB* db, rocksdb::ColumnFamilyHandle* cfHandle, const std::string& dbName, const std::string& scopeName);
-    void removeKVDBHandler(const std::string& dbName, const std::string& scopeName, bool &isRemoved);
+
+    KVDBHandler getKVDBHandler(rocksdb::DB* db,
+                               rocksdb::ColumnFamilyHandle* cfHandle,
+                               const std::string& dbName,
+                               const std::string& scopeName);
+
+    void removeKVDBHandler(const std::string& dbName,
+                           const std::string& scopeName,
+                           bool &isRemoved);
+
     std::vector<std::string> getDBNames();
     std::map<std::string, int> getRefMap(const std::string& dbName);
-
 private:
+
     class KVDBHandlerInstance
     {
     public:
-        void addScope(const std::string& scopeName) { m_scopeCounter.addRef(scopeName); }
-        void removeScope(const std::string& scopeName) { m_scopeCounter.removeRef(scopeName); }
-        bool emptyScopes() const { return m_scopeCounter.empty(); }
-        std::vector<std::string> getRefNames() const { return m_scopeCounter.getRefNames(); }
-        std::map<std::string, int> getRefMap() const { return m_scopeCounter.getRefMap(); }
+        void addScope(const std::string& scopeName);
+        void removeScope(const std::string& scopeName);
+        bool emptyScopes() const;
+        std::vector<std::string> getRefNames() const;
+        std::map<std::string, int> getRefMap() const;
     private:
         RefCounter m_scopeCounter;
     };
--- src/engine/source/kvdb2/src/kvdbScope.cpp
+++ src/engine/source/kvdb2/src/kvdbScope.cpp
@@ -1,29 +1,12 @@
 #include <kvdb2/kvdbScope.hpp>
-#include <fmt/format.h>
-#include <logging/logging.hpp>
+#include <kvdb2/iKVDBHandlerManager.hpp>
 
 namespace kvdbManager
 {
 
-KVDBScope::KVDBScope(IKVDBHandlerManager* handlerManager, const std::string& name)
-    : m_handlerManager(handlerManager)
-{
-    setName(name);
-}
-
-KVDBScope::~KVDBScope()
-{
-}
-
-bool KVDBScope::initialize()
-{
-    m_initialized = true;
-    return m_initialized;
-}
-
 KVDBHandler KVDBScope::getKVDBHandler(const std::string& dbName)
 {
-    return m_handlerManager->getKVDBHandler(dbName, getName());
+    return m_handlerManager->getKVDBHandler(dbName, m_name);
 }
 
 } // namespace kvdbManager
