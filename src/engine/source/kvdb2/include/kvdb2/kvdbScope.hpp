babf370651984a2d79322420662f0cec394458c4
Jorge Marino <jorge.marino@wazuh.com>
2023-05-18 18:51:26 +0000

    Small changes, cleaning up interfaces

--- src/engine/source/kvdb2/include/kvdb2/kvdbScope.hpp
+++ src/engine/source/kvdb2/include/kvdb2/kvdbScope.hpp
@@ -1,29 +1,20 @@
 #ifndef _KVDBSCOPE_H
 #define _KVDBSCOPE_H
 
-#include <memory>
-
-#include <kvdb2/iKVDBHandlerManager.hpp>
 #include <kvdb2/iKVDBScope.hpp>
 
 namespace kvdbManager
 {
+class IKVDBHandlerManager;
 
 class KVDBScope : public IKVDBScope
 {
 public:
-    KVDBScope(IKVDBHandlerManager* handlerManager, const std::string& name);
-    ~KVDBScope();
-
-    bool initialize();
-
-    std::string getName() const override { return m_name; }
-    void setName(const std::string& name) override { m_name = name; }
-
+    KVDBScope(IKVDBHandlerManager* handlerManager, const std::string& name): 
+        m_handlerManager(handlerManager), 
+        m_name(name) {}
     KVDBHandler getKVDBHandler(const std::string& dbName) override;
-
 private:
-    bool m_initialized { false };
     IKVDBHandlerManager* m_handlerManager { nullptr };
     std::string m_name;
 };
