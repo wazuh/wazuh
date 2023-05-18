6775c4d094f3d4f814a1d17f6b0a4070b044d442
nof <nof@live.com.ar>
2023-05-18 19:41:59 +0000

    Remove unused function finalize() in API.

--- src/engine/source/cmds/src/start.cpp
+++ src/engine/source/cmds/src/start.cpp
@@ -234,7 +234,6 @@ void runStart(ConfHandler confManager)
                 [kvdbManager]()
                 {
                     LOG_INFO("KVDB terminated.");
-                    kvdbManager->finalize();
                 });
 
             auto kvdbScope = kvdbManager->getKVDBScope("api");
