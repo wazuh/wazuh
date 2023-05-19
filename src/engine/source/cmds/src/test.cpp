54c28dbac2d010a165eaaa65f0081b6d5aff7b28
LucioDonda <donda.lucio@gmail.com>
2023-05-19 12:27:06 +0000

    Fixes limitation of console test buffer size

--- src/engine/source/cmds/src/test.cpp
+++ src/engine/source/cmds/src/test.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <memory>
 #include <thread>
+#include <termios.h>
 
 #include <re2/re2.h>
 
@@ -38,6 +39,33 @@ void sigint_handler(const int signum)
 
 namespace cmd::test
 {
+
+int clear_icanon(bool flag)
+{
+    struct termios settings;
+
+    if (tcgetattr(STDIN_FILENO, &settings) < 0)
+    {
+        return 0;
+    }
+
+    if (true == flag)
+    {
+        settings.c_lflag &= ~ICANON;
+    }
+    else
+    {
+        settings.c_lflag |= ICANON;
+    }
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &settings) < 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
 void run(const Options& options)
 {
     // Logging init
@@ -226,6 +254,12 @@ void run(const Options& options)
 
     // Stdin loop
     std::string line;
+
+    if(!clear_icanon(true))
+    {
+        std::cout << "WARNING: Failed to set non-canonical mode, only logs shorter than 4095 characters will be processed correctly." << std::endl << std::endl;
+    }
+
     while (gs_doRun && std::getline(std::cin, line))
     {
         if (!line.empty())
@@ -313,6 +347,7 @@ void run(const Options& options)
 
     }
 
+    clear_icanon(false);
     g_exitHanlder.execute();
 }
 
