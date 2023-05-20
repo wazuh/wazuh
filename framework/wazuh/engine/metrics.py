4dfbebb792ddf6beb6a8eae78144e97c90c4caf8
RamosFe <federico.ramos@wazuh.com>
2023-05-20 20:00:55 -0300

    Added WazuhMockedEngine and added it to the controllers of the Metrics module.
    - Changed name from EngineRequestBuilder to EngineRequestMessage.
    - Added new method for creating a message to EnginaRequestMessage.
    - Added WazuhMockedEngine with hardcoded responses for the Metric module.
    - Added WazuhMockedEngine to the Metric module controllers.

--- framework/wazuh/engine/metrics.py
+++ framework/wazuh/engine/metrics.py
@@ -4,19 +4,27 @@
 
 from typing import Optional
 
-from wazuh.engine.request_builder import EngineRequestBuilder
+from wazuh.engine.request_message import EngineRequestMessage
 from wazuh.engine.commands import MetricCommand
+from wazuh.engine.wazuh_engine import WazuhMockedEngine
 
 
 # TODO Redefine HARDCODED values
+HARDCODED_SOCKET_PATH = "som/path/"
 HARDCODED_ORIGIN_NAME = "metric"
 HARDCODED_ORIGIN_MODULE = "metric"
 ENGINE_METRICS_VERSION = 1
 
 
-def get_metrics(limit: int, scope_name: Optional[str] = None, instrument_name: Optional[str] = None,
-                select: Optional[str] = None, sort: Optional[str] = None, search: Optional[str] = None,
-                offset: int = 0):
+def get_metrics(
+    limit: int,
+    scope_name: Optional[str] = None,
+    instrument_name: Optional[str] = None,
+    select: Optional[str] = None,
+    sort: Optional[str] = None,
+    search: Optional[str] = None,
+    offset: int = 0,
+):
     """
     Retrieves metrics based on the specified parameters.
 
@@ -33,8 +41,12 @@ def get_metrics(limit: int, scope_name: Optional[str] = None, instrument_name: O
         None
     """
 
-    request_builder = EngineRequestBuilder(ENGINE_METRICS_VERSION)
-    request_builder.add_origin(name=HARDCODED_ORIGIN_NAME, module=HARDCODED_ORIGIN_MODULE)
+    wazuh_engine = WazuhMockedEngine(socket_path=HARDCODED_SOCKET_PATH)
+
+    request_builder = EngineRequestMessage(ENGINE_METRICS_VERSION)
+    request_builder.add_origin(
+        name=HARDCODED_ORIGIN_NAME, module=HARDCODED_ORIGIN_MODULE
+    )
 
     if scope_name is None and instrument_name is None:
         request_builder.add_command(command=MetricCommand.DUMP)
@@ -46,11 +58,20 @@ def get_metrics(limit: int, scope_name: Optional[str] = None, instrument_name: O
         return
     else:
         request_builder.add_command(command=MetricCommand.LIST)
-        request_builder.add_parameters(parameters={"scopeName": scope_name, "instrumentName": instrument_name})
+        request_builder.add_parameters(
+            parameters={"scopeName": scope_name, "instrumentName": instrument_name}
+        )
+
+    result = wazuh_engine.send_command(command=request_builder.to_dict())
 
 
-def get_instruments(limit: int, select: Optional[str] = None, sort: Optional[str] = None,
-                    search: Optional[str] = None, offset: int = 0):
+def get_instruments(
+    limit: int,
+    select: Optional[str] = None,
+    sort: Optional[str] = None,
+    search: Optional[str] = None,
+    offset: int = 0,
+):
     """
     Retrieves information about instruments.
 
@@ -64,14 +85,22 @@ def get_instruments(limit: int, select: Optional[str] = None, sort: Optional[str
     Returns:
         None
     """
+    wazuh_engine = WazuhMockedEngine(socket_path=HARDCODED_SOCKET_PATH)
 
-    request_builder = EngineRequestBuilder(version=ENGINE_METRICS_VERSION)
-    request_builder.add_origin(name=HARDCODED_ORIGIN_NAME, module=HARDCODED_ORIGIN_MODULE)
-    request_builder.add_command(command=MetricCommand.LIST)
+    msg = EngineRequestMessage(version=ENGINE_METRICS_VERSION).create_message(
+        origin_name=HARDCODED_ORIGIN_NAME,
+        module=HARDCODED_ORIGIN_MODULE,
+        command=MetricCommand.LIST,
+    )
+
+    result = wazuh_engine.send_command(command=msg)
 
 
-def enable_instrument(scope_name: Optional[str] = None, instrument_name: Optional[str] = None,
-                  enable: bool = True):
+def enable_instrument(
+    scope_name: Optional[str] = None,
+    instrument_name: Optional[str] = None,
+    enable: bool = True,
+):
     """
     Enables or disables a specified metric.
 
@@ -83,15 +112,19 @@ def enable_instrument(scope_name: Optional[str] = None, instrument_name: Optiona
     Returns:
         None
     """
+    wazuh_engine = WazuhMockedEngine(socket_path=HARDCODED_SOCKET_PATH)
+    msg = EngineRequestMessage(version=ENGINE_METRICS_VERSION).create_message(
+        origin_name=HARDCODED_ORIGIN_NAME,
+        module=HARDCODED_ORIGIN_MODULE,
+        command=MetricCommand.ENABLE,
+        parameters={
+            "scopeName": scope_name,
+            "instrumentName": instrument_name,
+            "status": enable,
+        },
+    )
 
-    request_builder = EngineRequestBuilder(version=ENGINE_METRICS_VERSION)
-    request_builder.add_origin(name=HARDCODED_ORIGIN_NAME, module=HARDCODED_ORIGIN_MODULE)
-    request_builder.add_command(command=MetricCommand.ENABLE)
-    request_builder.add_parameters({
-        "scopeName": scope_name,
-        "instrumentName": instrument_name,
-        "status": enable
-    })
+    result = wazuh_engine.send_command(command=msg)
 
 
 def get_test_dummy_metric():
@@ -101,7 +134,11 @@ def get_test_dummy_metric():
     Returns:
         None
     """
+    wazuh_engine = WazuhMockedEngine(socket_path=HARDCODED_SOCKET_PATH)
+    msg = EngineRequestMessage(version=ENGINE_METRICS_VERSION).create_message(
+        origin_name=HARDCODED_ORIGIN_NAME,
+        module=HARDCODED_ORIGIN_MODULE,
+        command=MetricCommand.TEST,
+    )
 
-    request_builder = EngineRequestBuilder(version=ENGINE_METRICS_VERSION)
-    request_builder.add_origin(name=HARDCODED_ORIGIN_NAME, module=HARDCODED_ORIGIN_MODULE)
-    request_builder.add_command(command=MetricCommand.TEST)
+    result = wazuh_engine.send_command(command=msg)
--- framework/wazuh/engine/request_message.py
+++ framework/wazuh/engine/request_message.py
@@ -2,11 +2,11 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from commands import EngineCommand
 
 
-class EngineRequestBuilder:
+class EngineRequestMessage:
     """ Represents a request to the engine"""
     def __init__(self, version: int):
         """
@@ -18,6 +18,37 @@ class EngineRequestBuilder:
             The version of the engine request.
         """
         self.internal_dict: Dict[str, Any] = {"version": version}
+
+    def create_message(self, origin_name: str, module: str, command: EngineCommand,
+                       parameters: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        """
+        Create a message for the engine request.
+
+        Parameters
+        ----------
+        origin_name : str
+            The name of the origin.
+        module : str
+            The module of the origin.
+        command : EngineCommand
+            The command for the engine request.
+        parameters : Dict[str, Any], optional
+            Dictionary with the parameters to add, by default None
+
+        Returns
+        -------
+        Dict[str, Any]
+            The created engine request message.
+
+        """
+
+        self.add_origin(name=origin_name, module=module)
+        self.add_command(command=command)
+
+        if parameters:
+            self.add_parameters(parameters=parameters)
+
+        return self.to_dict()
 
     def add_origin(self, name: str, module: str):
         """
