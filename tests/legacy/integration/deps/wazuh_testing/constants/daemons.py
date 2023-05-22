6ca6c8926e10fc54b32963a10dbf9300346cd36d
quebim <kevin.ledesma@wazuh.com>
2023-05-22 14:48:36 -0300

    Removed unused variables and docstrings added

--- tests/legacy/integration/deps/wazuh_testing/constants/daemons.py
+++ tests/legacy/integration/deps/wazuh_testing/constants/daemons.py
@@ -1,33 +1,51 @@
-"""
-Global variables related to wazuh-daemons
-"""
+# Copyright (C) 2015-2023, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-LOGCOLLECTOR_DAEMON = 'wazuh-logcollector'
+
+AGENT_DAEMON = 'wazuh-agentd'
 AGENTLESS_DAEMON = 'wazuh-agentlessd'
-CSYSLOG_DAEMON = 'wazuh-csyslogd'
-REMOTE_DAEMON = 'wazuh-remoted'
 ANALYSISD_DAEMON = 'wazuh-analysisd'
 API_DAEMON = 'wazuh-apid'
-MAIL_DAEMON = 'wazuh-maild'
-SYSCHECK_DAEMON = 'wazuh-syscheckd'
-EXEC_DAEMON = 'wazuh-execd'
-MODULES_DAEMON = 'wazuh-modulesd'
 CLUSTER_DAEMON = 'wazuh-clusterd'
+CSYSLOG_DAEMON = 'wazuh-csyslogd'
+EXEC_DAEMON = 'wazuh-execd'
 INTEGRATOR_DAEMON = 'wazuh-integratord'
+MAIL_DAEMON = 'wazuh-maild'
+MODULES_DAEMON = 'wazuh-modulesd'
 MONITOR_DAEMON = 'wazuh-monitord'
+LOGCOLLECTOR_DAEMON = 'wazuh-logcollector'
+REMOTE_DAEMON = 'wazuh-remoted'
+SYSCHECK_DAEMON = 'wazuh-syscheckd'
 WAZUH_DB_DAEMON = 'wazuh-db'
-AGENT_DAEMON = 'wazuh-agentd'
 
+WAZUH_AGENT_DAEMONS = [AGENT_DAEMON,
+                       EXEC_DAEMON,
+                       MODULES_DAEMON,
+                       LOGCOLLECTOR_DAEMON,
+                       SYSCHECK_DAEMON]
 
-WAZUH_AGENT_DAEMONS = [LOGCOLLECTOR_DAEMON, SYSCHECK_DAEMON,
-                       EXEC_DAEMON, MODULES_DAEMON, AGENT_DAEMON]
-WAZUH_MANAGER_DAEMONS = [LOGCOLLECTOR_DAEMON, AGENTLESS_DAEMON, CSYSLOG_DAEMON, REMOTE_DAEMON, ANALYSISD_DAEMON,
-                         API_DAEMON, MAIL_DAEMON, SYSCHECK_DAEMON, EXEC_DAEMON, MODULES_DAEMON, CLUSTER_DAEMON,
-                         INTEGRATOR_DAEMON, MONITOR_DAEMON, WAZUH_DB_DAEMON]
+WAZUH_MANAGER_DAEMONS = [AGENTLESS_DAEMON,
+                         ANALYSISD_DAEMON,
+                         API_DAEMON,
+                         CLUSTER_DAEMON,
+                         CSYSLOG_DAEMON,
+                         EXEC_DAEMON,
+                         INTEGRATOR_DAEMON,
+                         LOGCOLLECTOR_DAEMON,
+                         MAIL_DAEMON,
+                         MODULES_DAEMON,
+                         MONITOR_DAEMON,
+                         REMOTE_DAEMON,
+                         SYSCHECK_DAEMON,
+                         WAZUH_DB_DAEMON]
 
-API_DAEMONS_REQUIREMENTS = [API_DAEMON, WAZUH_DB_DAEMON,
-                            EXEC_DAEMON, ANALYSISD_DAEMON, REMOTE_DAEMON, MODULES_DAEMON]
-
+API_DAEMONS_REQUIREMENTS = [ANALYSISD_DAEMON,
+                            API_DAEMON,
+                            EXEC_DAEMON,
+                            MODULES_DAEMON,
+                            REMOTE_DAEMON,
+                            WAZUH_DB_DAEMON]
 
 WAZUH_UNIX_USER = 'wazuh'
 WAZUH_UNIX_GROUP = 'wazuh'
