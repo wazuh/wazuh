bc4b7e220f4d29a5b780cebca00dcb632198436b
quebim <kevin.ledesma@wazuh.com>
2023-05-24 12:55:56 -0300

    Migrate deps to qa-integration-framework repo

--- tests/legacy/integration/deps/wazuh_testing/logger.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2015-2023, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-import logging
-
-
-logger = logging.getLogger('wazuh_testing')
-logger.setLevel(logging.DEBUG)
