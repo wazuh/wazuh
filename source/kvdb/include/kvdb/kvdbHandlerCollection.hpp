3b6668ba3fa41ddaa45a1954bb8d487d9d2da169
Jorge Marino <jorge.marino@wazuh.com>
2023-06-02 11:18:57 +0000

    Adding documentation

--- source/kvdb/include/kvdb/kvdbHandlerCollection.hpp
+++ source/kvdb/include/kvdb/kvdbHandlerCollection.hpp
@@ -1,5 +1,5 @@
-#ifndef _KVDBHANDLERCOLLECTION_H
-#define _KVDBHANDLERCOLLECTION_H
+#ifndef _KVDB_HANDLER_COLLECTION_H
+#define _KVDB_HANDLER_COLLECTION_H
 
 #include <map>
 #include <memory>
@@ -16,45 +16,142 @@ namespace kvdbManager
 class KVDBScope;
 class IKVDBHandlerManager;
 
+/**
+ * @brief Collection of KVDB Handlers for a given DB and the Scopes referencing them.
+ *
+ */
 class KVDBHandlerCollection
 {
 public:
+    /**
+     * @brief Construct a new KVDBHandlerCollection object
+     *
+     * @param handleManager Pointer to the Manager that deals with handlers.
+     *
+     */
     KVDBHandlerCollection(IKVDBHandlerManager* handleManager)
         : m_handleManager(handleManager)
     {
     }
 
+    /**
+     * @brief Gets a KVDB Handler given the provided DB name and scope name.
+     * This automatically manage the reference counters and mappings.
+     *
+     * @param db Pointer to the RocksDB DB Internals.
+     * @param cfHandle Pointer to the RocksDB Column Family Handle.
+     * @param dbName Name of the DB.
+     * @param scopeName Name of the Scope.
+     * @return std::unique_ptr<IKVDBHandler> A KVDBHandler.
+     *
+     */
     std::unique_ptr<IKVDBHandler> getKVDBHandler(rocksdb::DB* db,
                                rocksdb::ColumnFamilyHandle* cfHandle,
                                const std::string& dbName,
                                const std::string& scopeName);
 
+    /**
+     * @brief Removes a KVDB Handler given the provided DB name and scope name.
+     * This automatically manage the reference counters and mappings.
+     * If the handler is the last one, it will trigger the Column Family to be removed.
+     *
+     * @param dbName Name of the DB.
+     * @param scopeName Name of the Scope.
+     * @param isRemoved True if the DB can be removed.
+     *
+     */
     void removeKVDBHandler(const std::string& dbName, const std::string& scopeName, bool& isRemoved);
 
+    /**
+     * @brief Returns all the registered Database names.
+     *
+     * @return std::vector<std::string> List of DB names.
+     *
+     */
     std::vector<std::string> getDBNames();
+
+    /**
+     * @brief Get the Reference Count of scopes for a given DB.
+     *
+     * @param dbName Name of the DB.
+     * @return std::map<std::string, int> Map of Scope names and their reference count.
+     */
     std::map<std::string, int> getRefMap(const std::string& dbName);
 
 private:
+    /**
+     * @brief Helper class to manage the reference counters for a given DB.
+     * This is used to track how many scopes are using a given DB.
+     *
+     */
     class KVDBHandlerInstance
     {
     public:
+        /**
+         * @brief Add a new scope to the reference counter.
+         *
+         * @param scopeName Name of the Scope.
+         *
+         */
         void addScope(const std::string& scopeName);
+
+        /**
+         * @brief Remove a scope from the reference counter.
+         *
+         * @param scopeName Name of the Scope.
+         */
         void removeScope(const std::string& scopeName);
+
+        /**
+         * @brief Returns if there are no scopes referencing this DB.
+         *
+         * @return true No scopes are referencing this DB.
+         * @return false Some scopes are referencing this DB.
+         */
         bool emptyScopes() const;
+
+        /**
+         * @brief Get the list of scopes referencing this DB.
+         *
+         * @return std::vector<std::string> List of Scope names.
+         */
         std::vector<std::string> getRefNames() const;
+
+        /**
+         * @brief Get the Ref Map object
+         *
+         * @return std::map<std::string, int> Map of DB names and their reference count.
+         */
         std::map<std::string, int> getRefMap() const;
 
     private:
+        /**
+         * @brief Basically a wrapper to std::map<std::string, int> with some helper functions.
+         *
+         */
         RefCounter m_scopeCounter;
     };
 
 private:
+    /**
+     * @brief Internal map holding DB names and their KVDBHandlerInstance with extra info.
+     *
+     */
     std::map<std::string, std::shared_ptr<KVDBHandlerInstance>> m_mapInstances;
 
+    /**
+     * @brief Pointer to the Manager that deals with handlers.
+     *
+     */
     IKVDBHandlerManager* m_handleManager {nullptr};
+
+    /**
+     * @brief Mutex to protect the internal map.
+     *
+     */
     std::mutex m_mutex;
 };
 
 } // namespace kvdbManager
 
-#endif // _KVDBHANDLERCOLLECTION_H
+#endif // _KVDB_HANDLER_COLLECTION_H
--- source/kvdb/include/kvdb/refCounter.hpp
+++ source/kvdb/include/kvdb/refCounter.hpp
@@ -1,5 +1,5 @@
-#ifndef _REFCOUNTER_H
-#define _REFCOUNTER_H
+#ifndef _REF_COUNTER_H
+#define _REF_COUNTER_H
 
 #include <map>
 #include <string>
@@ -8,23 +8,85 @@
 namespace kvdbManager
 {
 
+/**
+ * @brief Basic Helper wrapping std::map<std::string, int> to count references.
+ *
+ */
 class RefCounter
 {
 public:
+    /**
+     * @brief Construct a new RefCounter object
+     *
+     */
     RefCounter() = default;
+
+    /**
+     * @brief Construct a new RefCounter object by copy.
+     *
+     * @param refMap object to copy.
+     *
+     */
     RefCounter(const RefCounter&);
+
+    /**
+     * @brief Destroy the RefCounter object
+     *
+     */
     ~RefCounter() { m_refMap.clear(); }
+
+    /**
+     * @brief Add n reference(s) to the counter.
+     *
+     * @param name Reference name to increase
+     * @param times How many times to increase the reference. Default is 1.
+     */
     void addRef(const std::string& name, const unsigned int& times = 1);
+
+    /**
+     * @brief Remove 1 reference from the counter.
+     *
+     * @param name Reference name to decrease
+     */
     void removeRef(const std::string& name);
+
+    /**
+     * @brief Get the number of references for a given name.
+     *
+     * @param name Reference name to get the count.
+     * @return int Number of references for the given name.
+     */
     int count(const std::string& name) const;
+
+    /**
+     * @brief Checks if there are no references to any name.
+     *
+     * @return true If there are no references.
+     */
     bool empty() const;
+
+    /**
+     * @brief Get the names of the references.
+     *
+     * @return std::vector<std::string> Names of the references.
+     */
     std::vector<std::string> getRefNames() const;
+
+    /**
+     * @brief Get the map of references with the counters also.
+     *
+     * @return std::map<std::string, int> Map of references.
+     */
     std::map<std::string, int> getRefMap() const;
 
 private:
+    /**
+     * @brief Map of references with the counters.
+     *
+     */
     std::map<std::string, int> m_refMap;
 };
 
 } // namespace kvdbManager
 
-#endif // _REFCOUNTER_H
+#endif // _REF_COUNTER_H
