8b14807442b1ebfd493921252f6f7e9fe80dd75a
Nicolas Mariano Koremblum <nmkoremblum@gmail.com>
2023-06-05 19:11:09 +0000

    Session manager and test handlers improved

--- src/engine/source/api/include/api/test/sessionManager.hpp
+++ src/engine/source/api/include/api/test/sessionManager.hpp
@@ -3,8 +3,8 @@
 
 #include <ctime>
 #include <iostream>
-#include <mutex>
 #include <optional>
+#include <shared_mutex>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -25,14 +25,6 @@ constexpr auto ROUTE_NAME_FORMAT = "{}_route";          ///< Route name format, 
 struct Session
 {
 public:
-    const std::string m_filterName;
-    const std::string m_policyName;
-    const std::string m_routeName;
-    const std::string m_sessionID;
-    const std::string m_sessionName;
-    const std::time_t m_creationDate;
-    const uint32_t m_lifespan; ///< Session m_lifespan in seconds. 0 means no expiration.
-
     Session(const std::string& name, const std::string& policy, const std::string& route, const uint32_t lifespan = 0)
         : m_creationDate(std::time(nullptr))
         , m_filterName(fmt::format(FILTER_NAME_FORMAT, name))
@@ -44,16 +36,69 @@ public:
     {
     }
 
+    /**
+     * @brief Get the filter name.
+     *
+     * @return std::string
+     */
     std::string getFilterName(void) const { return m_filterName; };
+
+    /**
+     * @brief Get the policy name.
+     *
+     * @return std::string
+     */
     std::string getPolicyName(void) const { return m_policyName; };
+
+    /**
+     * @brief Get the route name.
+     *
+     * @return std::string
+     */
     std::string getRouteName(void) const { return m_routeName; };
+
+    /**
+     * @brief Get the session ID.
+     *
+     * @return std::string
+     */
     std::string getSessionID(void) const { return m_sessionID; };
+
+    /**
+     * @brief Get the session name.
+     *
+     * @return std::string
+     */
     std::string getSessionName(void) const { return m_sessionName; };
+
+    /**
+     * @brief Get the session creation date.
+     *
+     * @return std::time_t
+     */
     std::time_t getCreationDate(void) const { return m_creationDate; };
+
+    /**
+     * @brief Get the session lifespan.
+     *
+     * @return uint32_t
+     */
     uint32_t getLifespan(void) const { return m_lifespan; };
 
 private:
-    /// Generates a session ID based on the creation date.
+    const std::string m_filterName;
+    const std::string m_policyName;
+    const std::string m_routeName;
+    const std::string m_sessionID;
+    const std::string m_sessionName;
+    const std::time_t m_creationDate;
+    const uint32_t m_lifespan; ///< Session m_lifespan in seconds. 0 means no expiration.
+
+    /**
+     * @brief Generates a session ID based on the creation date.
+     *
+     * @return std::string
+     */
     std::string generateSessionID(void) { return std::to_string(m_creationDate); }
 };
 
@@ -64,25 +109,67 @@ private:
     std::unordered_map<std::string, std::string> m_policyMap;
     std::unordered_map<std::string, std::string> m_routeMap;
 
-    std::mutex m_sessionMutex;
+    std::shared_mutex m_sessionMutex;
 
 public:
     SessionManager() = default;
     SessionManager(const SessionManager&) = delete;
     SessionManager& operator=(const SessionManager&) = delete;
 
+    /**
+     * @brief Get a Session Manager instance.
+     *
+     * @return SessionManager&
+     */
     static SessionManager& getInstance(void);
 
+    /**
+     * @brief Create a new session.
+     *
+     * @param sessionName Name of the session.
+     * @param routeName Name of the route.
+     * @param policyName Name of the policy.
+     * @param lifespan Lifespan of the session in seconds. 0 means no expiration.
+     * @return std::optional<base::Error>
+     */
     std::optional<base::Error> createSession(const std::string& sessionName,
                                              const std::string& routeName,
                                              const std::string& policyName,
                                              uint32_t lifespan = 0);
 
+    /**
+     * @brief Get the list of active sessions.
+     *
+     * @return std::vector<std::string>
+     */
     std::vector<std::string> getSessionsList(void);
+
+    /**
+     * @brief Get the session object.
+     *
+     * @param sessionName Name of the session.
+     * @return std::optional<Session>
+     */
     std::optional<Session> getSession(const std::string& sessionName);
+
+    /**
+     * @brief Check if a session exists.
+     *
+     * @param sessionName Name of the session.
+     * @return true the session exists.
+     * @return false the session does not exist.
+     */
+    bool doesSessionExist(const std::string& sessionName);
 
     bool deleteSessions(const bool removeAll, const std::string sessionName = "");
     bool deleteAllSessions(void);
+    /**
+     * @brief Delete a session.
+     *
+     * @param sessionName Name of the session.
+     * @return true if the session was deleted.
+     * @return false if the session could not be deleted.
+     */
     bool deleteSession(const std::string& sessionName);
 };
 
