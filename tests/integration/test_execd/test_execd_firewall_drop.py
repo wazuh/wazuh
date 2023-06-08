9e9b9a272de6b2886c9b4cc84996759a0ffc7ba2
quebim <kevin.ledesma@wazuh.com>
2023-06-08 11:37:38 -0300

    feat(#17426): add active response config

--- tests/integration/test_execd/test_execd_firewall_drop.py
+++ tests/integration/test_execd/test_execd_firewall_drop.py
@@ -3,6 +3,7 @@ import pytest
 from pathlib import Path
 
 from wazuh_testing.constants.paths.logs import OSSEC_LOG_PATH
+from wazuh_testing.modules.execd import EXECD_DEBUG_CONFIG
 from wazuh_testing.tools import file_monitor
 from wazuh_testing.utils import config, callbacks
 
@@ -10,7 +11,7 @@ from . import CONFIGS_PATH, TEST_CASES_PATH
 
 
 # Set pytest marks.
-pytestmark = [pytest.mark.server, pytest.mark.tier(level=1)]
+pytestmark = [pytest.mark.server, pytest.mark.tier(level=0)]
 
 # Configuration and cases data.
 configs_path = Path(CONFIGS_PATH, 'config_execd.yaml')
@@ -20,10 +21,18 @@ cases_path = Path(TEST_CASES_PATH, 'cases_execd_firewall_drop.yaml')
 config_parameters, metadata, cases_ids = config.get_test_cases_data(cases_path)
 configuration = config.load_configuration_template(configs_path, config_parameters, metadata)
 
+# Test internal options.
+local_internal_options = EXECD_DEBUG_CONFIG
+
+# Test Active Response configuration
+active_response_configuration = '''restart-wazuh0 - restart-wazuh - 0\n
+                                   restart-wazuh0 - restart-wazuh.exe - 0\n
+                                   firewall-drop0 - firewall-drop - 0\n
+                                   firewall-drop5 - firewall-drop - 5'''
 
 # Test function.
 @pytest.mark.parametrize('configuration, metadata', zip(configuration, metadata), ids=cases_ids)
-def test_execd_firewall_drop(configuration, metadata):
+def test_execd_firewall_drop(configuration, metadata, configure_local_internal_options, set_active_response_configuration):
     '''
     description: Check if 'firewall-drop' command of 'active response' is executed correctly.
                  For this purpose, a simulated agent is used and the 'active response'
