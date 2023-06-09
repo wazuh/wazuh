da82ea788d27cef667687cf343d7705dd3c19a6b
Facundo Dalmau <facundo.dalmau@wazuh.com>
2023-06-09 12:01:29 -0300

    Fix renaming

--- wodles/aws/subscribers/__init__.py
+++ wodles/aws/subscribers/__init__.py
@@ -1,8 +1,7 @@
 from subscribers import sqs_queue
 from subscribers import sl_subscriber_bucket
 
-
 __all__ = [
-  "sqs_queue.py",
-    "sl_subscriber_bucket.py"
+    "sqs_queue",
+    "sl_subscriber_bucket"
 ]
