2b57a25b8db96260002396884473107207bbbf36
Julian Morales <jmorales@unc.edu.ar>
2023-06-12 20:44:57 +0000

    Helper +merge_value added

--- src/engine/source/builder/register.hpp
+++ src/engine/source/builder/register.hpp
@@ -104,6 +104,7 @@ static void registerHelperBuilders(std::shared_ptr<Registry<HelperBuilder>> help
     helperRegistry->registerBuilder(builders::opBuilderHelperEpochTimeFromSystem, "system_epoch");
     // Map helpers: Definition functions
     helperRegistry->registerBuilder(builders::getOpBuilderHelperGetValue(dependencies.schema), "get_value");
+    helperRegistry->registerBuilder(builders::getOpBuilderHelperMergeValue(dependencies.schema), "merge_value");
 
     // Special helpers
 
