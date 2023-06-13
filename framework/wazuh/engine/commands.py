444c269c5615326ccee1c1450866356484ea4dde
RamosFe <federico.ramos@wazuh.com>
2023-06-13 11:30:56 -0300

    Added all the commands of the Engine to the EngineCommand hierarchy.

--- framework/wazuh/engine/commands.py
+++ framework/wazuh/engine/commands.py
@@ -19,3 +19,48 @@ class MetricCommand(EngineCommand):
     TEST = 'metrics.manager/test'
 
 
+class CatalogCommand(EngineCommand):
+    """Enum class representing catalog commands."""
+    POST = 'catalog.resource/post'
+    GET = 'catalog.resource/get'
+    PUT = 'catalog.resource/put'
+    DELETE = 'catalog.resource/delete'
+    VALIDATE = 'catalog.resource/validate'
+
+
+class ConfigCommand(EngineCommand):
+    """Enum class representing config commands."""
+    GET = 'config.runtime/get'
+    PUT = 'config.runtime/put'
+    SAVE = 'config.runtime/save'
+
+
+class IntegrationCommand(EngineCommand):
+    """Enum class representing integration commands."""
+    ADD_TO = 'integration.policy/add_to'
+    REMOVE_FROM = 'integration.policy/remove_from'
+
+
+class KvdbDBCommand(EngineCommand):
+    """Enum class representing KVDB DB commands."""
+    GET = 'kvdb.db/get'
+    DELETE = 'kvdb.db/delete'
+    PUT = 'kvdb.db/put'
+
+
+class KvdbManagerCommand(EngineCommand):
+    """Enum class representing KVDB Manager commands."""
+    GET = 'kvdb.manager/get'
+    POST = 'kvdb.manager/post'
+    DELETE = 'kvdb.manager/delete'
+    DUMP = 'kvdb.manager/dump'
+
+
+class RouterCommand(EngineCommand):
+    """Enum class representing router commands."""
+    GET = 'router.route/get'
+    POST = 'router.route/post'
+    PATCH = 'router.route/patch'
+    DELETE = 'router.route/delete'
+    GET_TABLE = 'router.table/get'
+    POST_QUEUE = 'router.queue/post'
