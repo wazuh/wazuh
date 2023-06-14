de41218ffec931779cec3513b185cde4cd077486
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-06-14 20:52:01 +0000

    Add UT for Sessions and API Commands Test

--- src/engine/source/api/include/api/test/sessionManager.hpp
+++ src/engine/source/api/include/api/test/sessionManager.hpp
@@ -1,10 +1,15 @@
 #ifndef _SESSIONS_MANAGER_HPP
 #define _SESSIONS_MANAGER_HPP
 
-#include "iSessionManager.hpp"
-
+#include <optional>
 #include <shared_mutex>
+#include <string>
 #include <unordered_map>
+#include <vector>
+
+#include <error.hpp>
+
+#include "session.hpp"
 
 namespace api::sessionManager
 {
@@ -13,12 +18,13 @@ namespace api::sessionManager
  * @brief Session Manager class. Implements the ISessionManager interface.
  *
  */
-class SessionManager final : public ISessionManager
+class SessionManager
 {
 private:
     std::unordered_map<std::string, Session> m_activeSessions; ///< Map of active sessions
     std::unordered_map<std::string, std::string> m_policyMap;  ///< Map of active policies
     std::unordered_map<std::string, std::string> m_routeMap;   ///< Map of active routes
+    std::unordered_map<std::string, std::string> m_filterMap;   ///< Map of active filter
 
     std::shared_mutex m_sessionMutex;                          ///< Mutex to protect the sessions resources usage
 
@@ -40,32 +46,32 @@ public:
                                              uint32_t lifespan = 0,
                                              const std::string& description = "",
                                              const std::time_t creationDate = std::time(nullptr),
-                                             const std::string& sessionID = "") override;
+                                             const std::string& sessionID = "");
 
     /**
      * @copydoc ISessionManager::getSessionsList
      */
-    std::vector<std::string> getSessionsList(void) override;
+    std::vector<std::string> getSessionsList(void);
 
     /**
      * @copydoc ISessionManager::getSession
      */
-    std::optional<Session> getSession(const std::string& sessionName) override;
+    std::optional<Session> getSession(const std::string& sessionName);
 
     /**
      * @copydoc ISessionManager::doesSessionExist
      */
-    bool doesSessionExist(const std::string& sessionName) override;
+    bool doesSessionExist(const std::string& sessionName);
 
     /**
      * @copydoc ISessionManager::deleteSessions
      */
-    bool deleteSessions(const bool removeAll, const std::string sessionName = "") override;
+    bool deleteSessions(const bool removeAll, const std::string sessionName = "");
 
     /**
      * @copydoc ISessionManager::deleteSession
      */
-    bool deleteSession(const std::string& sessionName) override;
+    bool deleteSession(const std::string& sessionName);
 };
 
 } // namespace api::sessionManager
