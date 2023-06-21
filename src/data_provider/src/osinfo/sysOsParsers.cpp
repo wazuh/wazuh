a2b1bb51475983fa829296f1062e737c73c193f6
Gabriel Valenzuela <gabriel.valenzuela@wazuh.com>
2023-06-21 08:04:57 -0300

    Add: New macOS version

--- src/data_provider/src/osinfo/sysOsParsers.cpp
+++ src/data_provider/src/osinfo/sysOsParsers.cpp
@@ -438,6 +438,7 @@ bool MacOsParser::parseUname(const std::string& in, nlohmann::json& output)
         {"20", "Big Sur"},
         {"21", "Monterey"},
         {"22", "Ventura"},
+        {"23", "Sonoma"},
     };
     constexpr auto PATTERN_MATCH{"[0-9]+"};
     std::string match;
