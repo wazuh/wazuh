f97742cd7e84ad43f0a1b11f64cbcec61c114270
David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
2023-06-23 17:27:15 +0200

    Bump version 4.6.0

--- src/init/wazuh-client.sh
+++ src/init/wazuh-client.sh
@@ -11,8 +11,8 @@ PWD=`pwd`
 DIR=`dirname $PWD`;
 
 # Installation info
-VERSION="v4.5.0"
-REVISION="40500"
+VERSION="v4.6.0"
+REVISION="40600"
 TYPE="agent"
 
 ###  Do not modify below here ###
--- src/init/wazuh-server.sh
+++ src/init/wazuh-server.sh
@@ -13,8 +13,8 @@ DIR=`dirname $PWD`;
 PLIST=${DIR}/bin/.process_list;
 
 # Installation info
-VERSION="v4.5.0"
-REVISION="40500"
+VERSION="v4.6.0"
+REVISION="40600"
 TYPE="server"
 
 ###  Do not modify below here ###
