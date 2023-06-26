54113b3015d3719fb330e70025693a5c14ced90d
Manuel <manuel.cano@wazuh.com>
2023-06-26 14:50:16 +0200

    Fixed and added usage of wait_for_github_start fixture

--- tests/integration/test_github/conftest.py
+++ tests/integration/test_github/conftest.py
@@ -5,12 +5,15 @@
 import pytest
 
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
+from wazuh_testing.modules.modulesd import patterns
 from wazuh_testing.tools.file_monitor import FileMonitor
-from wazuh_testing.modules.integrations.event_monitors import detect_integration_start
-
+from wazuh_testing.utils import callbacks
 
 @pytest.fixture()
 def wait_for_github_start():
     # Wait for module github starts
-    file_monitor = FileMonitor(WAZUH_LOG_PATH)
-    detect_integration_start("GitHub",file_monitor)
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(patterns.MODULESD_STARTED, {
+                              'integration': 'GitHub'
+                          }))     
+    assert (wazuh_log_monitor.callback_result == None), f'Error invalid configuration event not detected'
