6664ced82602f32fc4cab8d95a37334592c944ff
kevin-mcm <kevincardenasmiranda9@gmail.com>
2023-07-05 09:54:00 -0400

    CL: - Add message when the output folder is deleted. - Add unit tests to the ExecutionContext class

--- src/shared_modules/content_manager/tests/unit/executionContext_test.cpp
+++ src/shared_modules/content_manager/tests/unit/executionContext_test.cpp
@@ -12,6 +12,23 @@
 #include "executionContext_test.hpp"
 #include "executionContext.hpp"
 #include "updaterContext.hpp"
+#include <filesystem>
+#include <memory>
+#include <string>
+
+/**
+ * @brief Removes the directory if it exists.
+ *
+ * @param outputFolder Folder to be removed.
+ */
+void removeOutputFolderIfExists(const std::string& outputFolder)
+{
+    if (std::filesystem::exists(outputFolder))
+    {
+        // Delete the output folder.
+        std::filesystem::remove_all(outputFolder);
+    }
+}
 
 /*
  * @brief Tests the instantiation of the ExecutionContext class
@@ -20,4 +37,107 @@ TEST_F(ExecutionContextTest, instantiation)
 {
     // Check that the ExecutionContext class can be instantiated
     EXPECT_NO_THROW(std::make_shared<ExecutionContext>());
+}
+
+/*
+ * @brief Test default folder when there is no configuration for the output folder.
+ */
+TEST_F(ExecutionContextTest, TestDefaultFolderWhenThereIsNoConfigurationForTheOutputFolder)
+{
+    // Remove the output folder if exists
+    removeOutputFolderIfExists(GENERIC_OUTPUT_FOLDER_PATH);
+
+    const auto expectedCapturedOutput = "Created output folder: \"" + GENERIC_OUTPUT_FOLDER_PATH + "\"\n";
+
+    m_spUpdaterBaseContext->configData.erase("outputFolder");
+
+    testing::internal::CaptureStdout();
+
+    m_spExecutionContext->handleRequest(m_spUpdaterBaseContext);
+
+    const auto capturedOutput {testing::internal::GetCapturedStdout()};
+
+    EXPECT_EQ(capturedOutput, expectedCapturedOutput);
+
+    EXPECT_EQ(m_spUpdaterBaseContext->outputFolder, GENERIC_OUTPUT_FOLDER_PATH);
+
+    EXPECT_TRUE(std::filesystem::exists(m_spUpdaterBaseContext->outputFolder));
+}
+
+/*
+ * @brief Test default folder when the output folder path is empty.
+ */
+TEST_F(ExecutionContextTest, TestDefaultFolderWhenTheOutputFolderPathIsEmpty)
+{
+    // Remove the output folder if exists
+    removeOutputFolderIfExists(GENERIC_OUTPUT_FOLDER_PATH);
+
+    const auto expectedCapturedOutput = "Created output folder: \"" + GENERIC_OUTPUT_FOLDER_PATH + "\"\n";
+
+    m_spUpdaterBaseContext->configData["outputFolder"] = "";
+
+    testing::internal::CaptureStdout();
+
+    m_spExecutionContext->handleRequest(m_spUpdaterBaseContext);
+
+    const auto capturedOutput {testing::internal::GetCapturedStdout()};
+
+    EXPECT_EQ(capturedOutput, expectedCapturedOutput);
+
+    EXPECT_EQ(m_spUpdaterBaseContext->outputFolder, GENERIC_OUTPUT_FOLDER_PATH);
+
+    EXPECT_TRUE(std::filesystem::exists(m_spUpdaterBaseContext->outputFolder));
+}
+
+/*
+ * @brief Test valid case when the output folder path is not empty.
+ */
+TEST_F(ExecutionContextTest, TestValidCaseWhenTheOutputFolderPathIsNotEmpty)
+{
+    const auto expectedOutputFolder {m_spUpdaterBaseContext->configData.at("outputFolder").get<const std::string>()};
+    const auto expectedCapturedOutput = "Created output folder: \"" + expectedOutputFolder + "\"\n";
+
+    // Remove the output folder if exists
+    removeOutputFolderIfExists(expectedOutputFolder);
+
+    testing::internal::CaptureStdout();
+
+    m_spExecutionContext->handleRequest(m_spUpdaterBaseContext);
+
+    const auto capturedOutput {testing::internal::GetCapturedStdout()};
+
+    EXPECT_EQ(capturedOutput, expectedCapturedOutput);
+
+    EXPECT_EQ(m_spUpdaterBaseContext->outputFolder, expectedOutputFolder);
+
+    EXPECT_TRUE(std::filesystem::exists(m_spUpdaterBaseContext->outputFolder));
+}
+
+/*
+ * @brief Test valid case when the output folder path is not empty and already exists.
+ */
+TEST_F(ExecutionContextTest, TestValidCaseWhenTheOutputFolderPathIsNotEmptyAndExists)
+{
+    m_spUpdaterBaseContext->configData["outputFolder"] = "/tmp/output-folder";
+    const auto expectedOutputFolder {m_spUpdaterBaseContext->configData.at("outputFolder").get<const std::string>()};
+    const auto expectedCapturedOutput = "The previous output folder: \"" + expectedOutputFolder +
+                                        "\" will be removed.\nCreated output folder: \"" + expectedOutputFolder +
+                                        "\"\n";
+
+    // Remove the output folder if exists
+    removeOutputFolderIfExists(expectedOutputFolder);
+    // Create the output folder.
+    std::filesystem::create_directory(expectedOutputFolder);
+
+    testing::internal::CaptureStdout();
+
+    m_spExecutionContext->handleRequest(m_spUpdaterBaseContext);
+
+    const auto capturedOutput {testing::internal::GetCapturedStdout()};
+
+    EXPECT_EQ(capturedOutput, expectedCapturedOutput);
+
+    EXPECT_EQ(m_spUpdaterBaseContext->outputFolder, expectedOutputFolder);
+
+    EXPECT_TRUE(std::filesystem::exists(m_spUpdaterBaseContext->outputFolder));
 }
--- src/shared_modules/content_manager/tests/unit/executionContext_test.hpp
+++ src/shared_modules/content_manager/tests/unit/executionContext_test.hpp
@@ -12,7 +12,10 @@
 #ifndef _EXECUTION_CONTEXT_TEST_HPP
 #define _EXECUTION_CONTEXT_TEST_HPP
 
+#include "executionContext.hpp"
+#include "updaterContext.hpp"
 #include "gtest/gtest.h"
+#include <memory>
 
 /**
  * @brief Runs unit tests for ExecutionContext
@@ -22,6 +25,30 @@ class ExecutionContextTest : public ::testing::Test
 protected:
     ExecutionContextTest() = default;
     ~ExecutionContextTest() override = default;
+
+    std::shared_ptr<UpdaterContext> m_spUpdaterContext; ///< UpdaterContext used on the merge pipeline.
+
+    std::shared_ptr<UpdaterBaseContext> m_spUpdaterBaseContext; ///< UpdaterBaseContext used on the merge pipeline.
+
+    std::shared_ptr<ExecutionContext> m_spExecutionContext; ///< ExecutionContext.
+
+    /**
+     * @brief Sets initial conditions for each test case.
+     *
+     */
+    // cppcheck-suppress unusedFunction
+    void SetUp() override
+    {
+        m_spExecutionContext = std::make_shared<ExecutionContext>();
+        // Create a updater context
+        m_spUpdaterContext = std::make_shared<UpdaterContext>();
+        m_spUpdaterBaseContext = std::make_shared<UpdaterBaseContext>();
+        m_spUpdaterBaseContext->configData = R"(
+            {
+                "outputFolder": "/tmp/content_manager"
+            }
+        )"_json;
+    }
 };
 
 #endif //_EXECUTION_CONTEXT_TEST_HPP
