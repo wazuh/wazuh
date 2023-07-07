aed02494b20721ac4e27868bd4b67977312ab60b
GGP1 <gaston.palomeque@wazuh.com>
2023-07-07 08:20:09 -0300

    Revert "Remove the q parameter from GET /groups"
    This reverts commit 5b6d4be601f236bf657d7e08c86b59f66521438b.

--- api/api/controllers/test/test_agent_controller.py
+++ api/api/controllers/test/test_agent_controller.py
@@ -751,7 +751,8 @@ async def test_get_list_group(mock_exc, mock_dapi, mock_remove, mock_dfunc, mock
                 'group_list': None,
                 'sort': None,
                 'search': None,
-                'hash_algorithm': hash_
+                'hash_algorithm': hash_,
+                'q': None
                 }
     mock_dapi.assert_called_once_with(f=agent.get_agent_groups,
                                       f_kwargs=mock_remove.return_value,
