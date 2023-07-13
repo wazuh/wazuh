cca9939d85ee7ca9eebf117c06276ba7ededa261
quebim <kevin.ledesma@wazuh.com>
2023-07-13 18:52:11 -0300

    feat(#17881): add whodata thread scenario working with its cases and config

--- tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_thread.py
+++ tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_thread.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 from wazuh_testing.constants.platforms import WINDOWS
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG, AGENTD_WINDOWS_DEBUG
+from wazuh_testing.modules.fim.patterns import REALTIME_WHODATA_ENGINE_STARTED
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
 from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
@@ -19,8 +20,8 @@ from . import TEST_CASES_PATH, CONFIGS_PATH
 pytestmark = [pytest.mark.linux, pytest.mark.win32, pytest.mark.tier(level=2)]
 
 # Test metadata, configuration and ids.
-cases_path = Path(TEST_CASES_PATH, 'cases_ignore_works_over_restrict.yaml')
-config_path = Path(CONFIGS_PATH, 'configuration_ignore_works_over_restrict.yaml')
+cases_path = Path(TEST_CASES_PATH, 'cases_whodata_thread.yaml')
+config_path = Path(CONFIGS_PATH, 'configuration_whodata_thread.yaml')
 test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
 test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
 
@@ -30,10 +31,11 @@ local_internal_options = {SYSCHECK_DEBUG: 2, AGENTD_DEBUG: 2, MONITORD_ROTATE_LO
 if sys.platform == WINDOWS: local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
 
 
-@pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_ignore_works_over_restrict(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
-                                    truncate_monitored_files, folder_to_monitor, daemons_handler, file_to_monitor):
-    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=generate_callback(test_metadata['regex']))
 
-    assert wazuh_log_monitor.callback_result
+@pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
+def test_whodata_thread(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
+                        truncate_monitored_files, daemons_handler):
+    monitor = FileMonitor(WAZUH_LOG_PATH)
+    monitor.start(callback=generate_callback(REALTIME_WHODATA_ENGINE_STARTED))
+
+    assert bool(monitor.callback_result) == test_metadata['whodata']
