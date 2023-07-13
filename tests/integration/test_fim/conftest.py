e9d90f2295778a6ff0c1d8c6a1ddfc9ec29145b8
quebim <kevin.ledesma@wazuh.com>
2023-07-13 13:22:21 -0300

    feat(#17881): remove un-used fixture, rename metadata

--- tests/integration/test_fim/conftest.py
+++ tests/integration/test_fim/conftest.py
@@ -18,25 +18,8 @@ def file_to_monitor(test_metadata: dict) -> Any:
 @pytest.fixture()
 def folder_to_monitor(test_metadata: dict) -> None:
     path = test_metadata.get('folder_to_monitor')
-    file.create_folder(folder_to_monitor) if path else None
+    file.create_folder(path) if path else None
 
     yield
 
     file.delete_path_recursively(path) if path else None
-
-
-@pytest.fixture()
-def path_to_monitor(test_metadata: dict) -> None:
-    # Get the folder to monitor
-    path = test_metadata.get('path_to_monitor')
-    file.create_folder(path)
-
-    # If it is a specific file, get and create it.
-    # if file_path := test_metadata.get('file_to_monitor'):
-    #     file_path = Path(path, file_path)
-    #     file.write_file(file_path)
-
-    yield path
-
-    # file.remove_file(file_path) if file_path else None
-    file.delete_path_recursively(path)
