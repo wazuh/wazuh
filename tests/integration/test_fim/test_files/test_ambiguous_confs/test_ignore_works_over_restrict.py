e7c3a07a01e63dfeef551b58e45d9ec7eb559bff
quebim <kevin.ledesma@wazuh.com>
2023-07-13 13:12:16 -0300

    feat(#17881): scenario ignore_works_over_restrict working properly

--- tests/integration/test_fim/test_files/test_ambiguous_confs/test_ignore_works_over_restrict.py
+++ tests/integration/test_fim/test_files/test_ambiguous_confs/test_ignore_works_over_restrict.py
@@ -1,13 +1,40 @@
 
+from pathlib import Path
 import sys
+
+import pytest
+from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 
 from wazuh_testing.constants.platforms import WINDOWS
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG, AGENTD_WINDOWS_DEBUG
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
 from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
+from wazuh_testing.tools.monitors.file_monitor import FileMonitor
+from wazuh_testing.utils.callbacks import generate_callback
+from wazuh_testing.utils.configuration import get_test_cases_data, load_configuration_template
+
+from . import TEST_CASES_PATH, CONFIGS_PATH
 
 
-# Set local internal options
+# Pytest marks to run on any service type on linux or windows.
+pytestmark = [pytest.mark.linux, pytest.mark.win32, pytest.mark.tier(level=1)]
+
+# Test metadata, configuration and ids.
+cases_path = Path(TEST_CASES_PATH, 'cases_ignore_works_over_restrict.yaml')
+config_path = Path(CONFIGS_PATH, 'configuration_ignore_works_over_restrict.yaml')
+test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
+test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
+
+# Set configurations required by the fixtures.
+daemons_handler_configuration = {'all_daemons': True}
 local_internal_options = {SYSCHECK_DEBUG: 2, AGENTD_DEBUG: 2, MONITORD_ROTATE_LOG: 0}
-if sys.platform == WINDOWS:
-    local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
+if sys.platform == WINDOWS: local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
+
+
+@pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
+def test_ignore_works_over_restrict(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
+                                    truncate_monitored_files, path_to_monitor, daemons_handler, file_to_monitor):
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=generate_callback(test_metadata['regex']))
+
+    assert wazuh_log_monitor.callback_result
