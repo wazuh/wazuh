676fae3de43bda01703f488faa883093fd3972bc
quebim <kevin.ledesma@wazuh.com>
2023-07-14 17:08:04 -0300

    docs(#17881): add docstrings to fim ambiguous_confs scenarios

--- tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_works_over_realtime.py
+++ tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_works_over_realtime.py
@@ -1,3 +1,61 @@
+'''
+copyright: Copyright (C) 2015-2023, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: These tests will check if the 'who-data' feature of the File Integrity Monitoring (FIM) system
+       works properly. 'who-data' information contains the user who made the changes on the monitored
+       files and also the program name or process used to carry them out. In particular, it will be
+       verified that the value of the 'whodata' attribute prevails over the 'realtime' one.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks configured files
+       for changes to the checksums, permissions, and ownership.
+
+components:
+    - fim
+
+suite: files_ambiguous_complex
+
+targets:
+    - manager
+
+daemons:
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Debian Buster
+    - Red Hat 8
+    - Ubuntu Focal
+    - Ubuntu Bionic
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/auditing-whodata/who-linux.html
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_ambiguous_confs
+'''
 import sys
 import pytest
 
@@ -35,6 +93,72 @@ if sys.platform == WINDOWS: local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
 def test_whodata_works_over_realtime(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
                                      truncate_monitored_files, folder_to_monitor, daemons_handler):
+    '''
+    description: Check if when using the options who-data and real-time at the same time
+                 the value of 'whodata' is the one used. For example, when using 'whodata=yes'
+                 and 'realtime=no' on the same directory, real-time file monitoring
+                 will be enabled, as who-data requires it.
+                 For this purpose, the configuration is applied and it is verified that when
+                 'who-data' is set to 'yes', the 'realtime' value is not taken into account,
+                 enabling in this case the real-time file monitoring.
+
+    test_phases:
+        - setup:
+            - Set wazuh configuration and local_internal_options.
+            - Create custom folder for monitoring
+            - Clean logs files and restart wazuh to apply the configuration.
+        - test:
+            - Create file and detect event creation event
+            - Validate mode is whodata
+            - Delete file and detect event deletion event
+            - Validate mode is whodata
+        - teardown:
+            - Delete custom monitored folder
+            - Restore configuration
+            - Stop wazuh
+            - Clear logs
+
+    wazuh_min_version: 4.2.0
+
+    tier: 2
+
+    parameters:
+        - test_configuration:
+            type: dict
+            brief: Configuration values for ossec.conf.
+        - test_metadata:
+            type: dict
+            brief: Test case data.
+        - set_wazuh_configuration:
+            type: fixture
+            brief: Set ossec.conf configuration.
+        - configure_local_internal_options:
+            type: fixture
+            brief: Set local_internal_options.conf file.
+        - truncate_monitored_files:
+            type: fixture
+            brief: Truncate all the log files and json alerts files before and after the test execution.
+        - daemons_handler:
+            type: fixture
+            brief: Handler of Wazuh daemons.
+        - folder_to_monitor:
+            type: str
+            brief: Folder created for monitoring.
+
+    assertions:
+        - Verify that real-time whodata thread active.
+
+    input_description: The file 'configuration_whodata_prevails_over_realtime.yaml' provides the configuration
+                       template.
+                       The file 'cases_whodata_prevails_over_realtime.yaml' provides the tes cases configuration
+                       details for each test case.
+
+    expected_output:
+        - r'.*Sending FIM event: (.+)$'
+
+    tags:
+        - who_data
+    '''
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
     test_file = Path(folder_to_monitor, test_metadata['test_file'])
     
