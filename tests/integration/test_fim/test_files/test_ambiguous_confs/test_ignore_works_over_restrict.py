5ab0eb3071f7c7651b5bdf348f231b33c8be1b58
quebim <kevin.ledesma@wazuh.com>
2023-07-14 17:08:04 -0300

    docs(#17881): add docstrings to fim ambiguous_confs scenarios

--- tests/integration/test_fim/test_files/test_ambiguous_confs/test_ignore_works_over_restrict.py
+++ tests/integration/test_fim/test_files/test_ambiguous_confs/test_ignore_works_over_restrict.py
@@ -1,3 +1,65 @@
+'''
+copyright: Copyright (C) 2015-2023, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts when these
+       files are modified. All these tests will be performed using ambiguous directory configurations,
+       such as directories and subdirectories with opposite monitoring settings. In particular, it
+       will be verified that the value of the 'ignore' attribute prevails over the 'restrict' one.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks configured files
+       for changes to the checksums, permissions, and ownership.
+
+components:
+    - fim
+
+suite: files_ambiguous_complex
+
+targets:
+    - agent
+
+daemons:
+    - wazuh-agentd
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+    - windows
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Debian Buster
+    - Red Hat 8
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Windows 10
+    - Windows Server 2019
+    - Windows Server 2016
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_ambiguous_confs
+'''
 import sys
 import pytest
 
@@ -33,6 +95,71 @@ if sys.platform == WINDOWS: local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
 def test_ignore_works_over_restrict(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
                                     truncate_monitored_files, folder_to_monitor, daemons_handler, file_to_monitor):
+    '''
+    description: Check if the 'ignore' tag prevails over the 'restrict' one when using both in the same directory.
+                 For example, when a directory is ignored and at the same time monitoring is restricted to a file
+                 that is in that directory, no FIM events should be generated when that file is modified.
+                 For this purpose, the test case configuration is applied, and it is checked if FIM events
+                 are generated when required.
+
+    test_phases:
+        - setup:
+            - Set wazuh configuration and local_internal_options.
+            - Create custom folder and file for monitoring
+        - test:
+            - Create file and detect event creation event
+            - Validate ignored event is generated with matching regex
+        - teardown:
+            - Delete custom monitored folder
+            - Restore configuration
+            - Stop Wazuh
+            - Clean logs
+
+    wazuh_min_version: 4.2.0
+
+    tier: 2
+
+    parameters:
+        - test_configuration:
+            type: dict
+            brief: Configuration values for ossec.conf.
+        - test_metadata:
+            type: dict
+            brief: Test case data.
+        - set_wazuh_configuration:
+            type: fixture
+            brief: Set ossec.conf configuration.
+        - configure_local_internal_options:
+            type: fixture
+            brief: Set local_internal_options.conf file.
+        - truncate_monitored_files:
+            type: fixture
+            brief: Truncate all the log files and json alerts files before and after the test execution.
+        - daemons_handler:
+            type: fixture
+            brief: Handler of Wazuh daemons.
+        - folder_to_monitor:
+            type: str
+            brief: Folder created for monitoring.
+        - file_to_monitor:
+            type: str
+            brief: File created for monitoring.
+
+    assertions:
+        - Verify that when a directory is ignored, the 'restrict' attribute
+          is not taken into account to generate FIM events.
+
+    input_description: The file 'configuration_works_over_restrict.yaml' provides the configuration
+                       template.
+                       The file 'cases_ignore_works_over_restrict.yaml' provides the tes cases configuration
+                       details for each test case.
+
+    expected_output:
+        - r".*Ignoring '.*?' '(.*?)' due to (sregex|pattern)? '.*?'" (When the FIM event should be ignored)
+
+    tags:
+        - fim
+    '''
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
     wazuh_log_monitor.start(callback=generate_callback(test_metadata['regex']))
 
--- tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_thread.py
+++ tests/integration/test_fim/test_files/test_ambiguous_confs/test_whodata_thread.py
@@ -1,3 +1,64 @@
+'''
+copyright: Copyright (C) 2015-2023, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: These tests will check if the 'who-data' feature of the File Integrity Monitoring (FIM)
+       system works properly. 'who-data' information contains the user who made the changes
+       on the monitored files and also the program name or process used to carry them out.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks
+       configured files for changes to the checksums, permissions, and ownership.
+
+components:
+    - fim
+
+suite: files_ambiguous_complex
+
+targets:
+    - manager
+
+daemons:
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+    - windows
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Debian Buster
+    - Red Hat 8
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Windows 10
+    - Windows Server 2019
+    - Windows Server 2016
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/auditing-whodata/who-linux.html
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_ambiguous_confs
+'''
 import sys
 import pytest
 
@@ -35,6 +96,61 @@ if sys.platform == WINDOWS: local_internal_options += {AGENTD_WINDOWS_DEBUG: 2}
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
 def test_whodata_thread(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
                         truncate_monitored_files, daemons_handler):
+    '''
+    description: Check if the 'wazuh-syscheckd' daemon starts the 'whodata' thread when the configuration
+                 is ambiguous. For example, when using 'whodata' on the same directory using conflicting
+                 values ('yes' and 'no'). For this purpose, the configuration is applied and it checks
+                 that the last value detected for 'whodata' in the 'ossec.conf' file is the one used.
+
+    test_phases:
+        - setup:
+            - Set wazuh configuration and local_internal_options.
+            - Clean logs files and restart wazuh to apply the configuration.
+        - test:
+            - Detect if real-time whodata thread has been started
+        - teardown:
+            - Restore configuration
+            - Stop wazuh
+            - Clean logs
+
+    wazuh_min_version: 4.2.0
+
+    tier: 2
+
+    parameters:
+        - test_configuration:
+            type: dict
+            brief: Configuration values for ossec.conf.
+        - test_metadata:
+            type: dict
+            brief: Test case data.
+        - set_wazuh_configuration:
+            type: fixture
+            brief: Set ossec.conf configuration.
+        - configure_local_internal_options:
+            type: fixture
+            brief: Set local_internal_options.conf file.
+        - truncate_monitored_files:
+            type: fixture
+            brief: Truncate all the log files and json alerts files before and after the test execution.
+        - daemons_handler:
+            type: fixture
+            brief: Handler of Wazuh daemons.
+
+    assertions:
+        - Verify that 'whodata' thread is started when the last 'whodata' value detected is set to 'yes'.
+        - Verify that 'whodata' thread is not started when the last 'whodata' value detected is set to 'no'.
+
+    input_description: Two test cases are contained in external YAML file (cases_whodata_thread.yaml)
+                       which includes configuration settings for the 'wazuh-syscheckd' daemon and testing
+                       directories to monitor.
+
+    expected_output:
+        - r'File integrity monitoring real-time Whodata engine started'
+
+    tags:
+        - who_data
+    '''
     monitor = FileMonitor(WAZUH_LOG_PATH)
     monitor.start(callback=generate_callback(REALTIME_WHODATA_ENGINE_STARTED))
 
