ffa66c34698cb12b52161b022f69e3f5f02a1229
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2023-07-17 08:09:01 +0200

    Bump version to 4.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## [v4.8.0]
+
+
 ## [v4.7.0]
 
 
--- src/init/wazuh-client.sh
+++ src/init/wazuh-client.sh
@@ -11,8 +11,8 @@ PWD=`pwd`
 DIR=`dirname $PWD`;
 
 # Installation info
-VERSION="v4.7.0"
-REVISION="40700"
+VERSION="v4.8.0"
+REVISION="40800"
 TYPE="agent"
 
 ###  Do not modify below here ###
