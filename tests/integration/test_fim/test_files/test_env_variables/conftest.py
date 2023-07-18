8044d2bb8bf37d448471bda78c5dec47d99a082b
quebim <kevin.ledesma@wazuh.com>
2023-07-18 18:45:03 -0300

    feat(#17881): modify and add fixtures for test_env_variables IT

--- tests/integration/test_fim/test_files/test_env_variables/conftest.py
+++ tests/integration/test_fim/test_files/test_env_variables/conftest.py
@@ -9,19 +9,21 @@ import pytest
 from wazuh_testing.constants.platforms import WINDOWS
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def set_environment_variables(test_metadata: dict) -> None:
     """
     Create environment variables
     """
-    environment_variables = test_metadata.get('environment_variables')
-    for env, value in environment_variables:
+    environment_variables: dict = test_metadata.get('environment_variables')
+    print(environment_variables)
+    for env, value in environment_variables.items():
         if sys.platform == WINDOWS:
             subprocess.call(['setx.exe', env, value, '/m'])
         else:
-            os.putenv(env, value)
+            # subprocess.run(f"export {env}='{value}'", shell=True)
+            os.environ[env] = value
 
     yield
 
-    for env in environment_variables:
-        os.environ.pop[env]
+    for env in environment_variables.keys():
+        os.environ.pop(env)
--- tests/integration/test_fim/test_files/test_env_variables/test_cud_events.py
+++ tests/integration/test_fim/test_files/test_env_variables/test_cud_events.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG
-from wazuh_testing.modules.fim.patterns import SENDING_FIM_EVENT, WHODATA_NOT_STARTED
+from wazuh_testing.modules.fim.patterns import SENDING_FIM_EVENT, WHODATA_ADDED_EVENT, WHODATA_DELETED_EVENT, WHODATA_NOT_STARTED
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
 from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
+from wazuh_testing.utils import file
 from wazuh_testing.utils.callbacks import generate_callback
 from wazuh_testing.utils.configuration import get_test_cases_data, load_configuration_template
 
@@ -18,8 +19,8 @@ from . import TEST_CASES_PATH, CONFIGS_PATH
 pytestmark = [pytest.mark.linux, pytest.mark.win32, pytest.mark.tier(level=1)]
 
 # Test metadata, configuration and ids.
-cases_path = Path(TEST_CASES_PATH, 'cases_remove_audit.yaml')
-config_path = Path(CONFIGS_PATH, 'configuration_remove_audit.yaml')
+cases_path = Path(TEST_CASES_PATH, 'cases_cud_events.yaml')
+config_path = Path(CONFIGS_PATH, 'configuration_cud_events.yaml')
 test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
 test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
 
@@ -29,11 +30,17 @@ local_internal_options = {SYSCHECK_DEBUG: 2,AGENTD_DEBUG: 2, MONITORD_ROTATE_LOG
 
 
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_cud_events(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
-                    set_environment_variables, truncate_monitored_files, daemons_handler, folder_to_monitor,
-                    file_to_monitor):
+def test_cud_events(test_configuration, test_metadata, folder_to_monitor, set_environment_variables, set_wazuh_configuration,
+                    configure_local_internal_options, truncate_monitored_files , daemons_handler):
 
-    monitor = FileMonitor(WAZUH_LOG_PATH)
-    monitor.start(callback=generate_callback(SENDING_FIM_EVENT))
-
-    assert monitor.callback_result
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    test_file = Path(folder_to_monitor, test_metadata['test_file'])
+    print(test_file )
+    file.write_file(test_file)
+    file.write_file(test_file,'asdasd')
+    wazuh_log_monitor.start(callback=generate_callback(SENDING_FIM_EVENT))
+    assert wazuh_log_monitor.callback_result
+    
+    file.remove_file(test_file)
+    wazuh_log_monitor.start(callback=generate_callback(SENDING_FIM_EVENT))
+    assert wazuh_log_monitor.callback_result
