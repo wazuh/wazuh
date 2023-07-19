5881d7fb0227f391e6c424196f0e19dc8f8a75b9
quebim <kevin.ledesma@wazuh.com>
2023-07-19 16:14:39 -0300

    feat(#17881): add fim workflows

--- tests/integration/test_fim/test_files/test_env_variables/conftest.py
+++ tests/integration/test_fim/test_files/test_env_variables/conftest.py
@@ -7,21 +7,26 @@ import sys
 import pytest
 
 from wazuh_testing.constants.platforms import WINDOWS
+from wazuh_testing.utils import file
 
 
-@pytest.fixture()
-def set_environment_variables(test_metadata: dict) -> None:
+@pytest.fixture(scope='module')
+def set_environment_variables() -> None:
     """
     Create environment variables
     """
-    environment_variables: dict = test_metadata.get('environment_variables')
+    # environment_variables: dict = test_metadata.get('environment_variables')
+    file.create_folder('/test_dir1')
+    environment_variables: dict = {'TESTING': '/test_dir1'}
     print(environment_variables)
     for env, value in environment_variables.items():
         if sys.platform == WINDOWS:
             subprocess.call(['setx.exe', env, value, '/m'])
         else:
-            # subprocess.run(f"export {env}='{value}'", shell=True)
+            print(f"export {env}={value}")
+            subprocess.call(f'export {env}=/test_dir1/', shell=True)
             os.environ[env] = value
+            os.putenv(env, value)
 
     yield
 
