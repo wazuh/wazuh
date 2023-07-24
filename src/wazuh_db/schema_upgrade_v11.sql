12d7212b8f9e92a68642364e6e8bb3f5a94b2523
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2023-07-24 13:52:04 -0300

    Upgrade schema v12

--- src/wazuh_db/schema_upgrade_v11.sql
+++ src/wazuh_db/schema_upgrade_v11.sql
@@ -34,45 +34,6 @@ INSERT INTO _sys_hwinfo (scan_id, scan_time, board_serial, cpu_name, cpu_cores,
 DROP TABLE sys_hwinfo;
 ALTER TABLE _sys_hwinfo RENAME TO sys_hwinfo;
 
-CREATE TABLE IF NOT EXISTS _sys_programs (
-    scan_id INTEGER,
-    scan_time TEXT,
-    format TEXT NOT NULL CHECK (format IN ('pacman', 'deb', 'rpm', 'win', 'pkg', 'apk', 'macports')),
-    name TEXT,
-    priority TEXT,
-    section TEXT,
-    size INTEGER CHECK (size >= 0),
-    vendor TEXT,
-    install_time TEXT,
-    version TEXT,
-    architecture TEXT,
-    multiarch TEXT,
-    source TEXT,
-    description TEXT,
-    location TEXT,
-    triaged INTEGER(1),
-    cpe TEXT,
-    msu_name TEXT,
-    checksum TEXT NOT NULL CHECK (checksum <> ''),
-    item_id TEXT,
-    PRIMARY KEY (scan_id, name, version, architecture)
-);
-
-INSERT INTO _sys_programs SELECT scan_id, scan_time, format, name, priority, section, size, vendor, install_time, version, architecture, multiarch, source, description, location, triaged, cpe, msu_name, CASE WHEN checksum <> '' THEN checksum ELSE 'legacy' END AS checksum, item_id FROM sys_programs;
-DROP TABLE IF EXISTS sys_programs;
-ALTER TABLE _sys_programs RENAME TO sys_programs;
-CREATE INDEX IF NOT EXISTS programs_id ON sys_programs (scan_id);
-
-CREATE TRIGGER obsolete_vulnerabilities
-    AFTER DELETE ON sys_programs
-    WHEN (old.checksum = 'legacy' AND NOT EXISTS (SELECT 1 FROM sys_programs
-                                                  WHERE item_id = old.item_id
-                                                  AND scan_id != old.scan_id ))
-    OR old.checksum != 'legacy'
-    BEGIN
-        UPDATE vuln_cves SET status = 'OBSOLETE' WHERE vuln_cves.reference = old.item_id;
-END;
-
 INSERT OR REPLACE INTO metadata (key, value) VALUES ('db_version', 11);
 
 COMMIT;
