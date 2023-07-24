1abdb84739b3fe85e4ef9c58e6805234f59ca4cb
quebim <kevin.ledesma@wazuh.com>
2023-07-24 18:25:03 -0300

    feat(#17881): add assertions to create_after_delete cases

--- tests/integration/test_fim/test_files/test_basic_usage/test_create_after_delete_dir.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_create_after_delete_dir.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG
-from wazuh_testing.modules.fim.patterns import WHODATA_NOT_STARTED
+from wazuh_testing.modules.fim.patterns import SENDING_FIM_EVENT
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
 from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
+from wazuh_testing.utils import file
 from wazuh_testing.utils.callbacks import generate_callback
 from wazuh_testing.utils.configuration import get_test_cases_data, load_configuration_template
 
@@ -18,17 +19,28 @@ from . import TEST_CASES_PATH, CONFIGS_PATH
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0)]
 
 # Test metadata, configuration and ids.
-cases_path = Path(TEST_CASES_PATH, 'cases_remove_audit.yaml')
-config_path = Path(CONFIGS_PATH, 'configuration_remove_audit.yaml')
+cases_path = Path(TEST_CASES_PATH, 'cases_create_after_delete.yaml')
+config_path = Path(CONFIGS_PATH, 'configuration_basic.yaml')
 test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
 test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
 
 # Set configurations required by the fixtures.
 daemons_handler_configuration = {'all_daemons': True}
-local_internal_options = {SYSCHECK_DEBUG: 2,AGENTD_DEBUG: 2, MONITORD_ROTATE_LOG: 0}
-
+local_internal_options = {SYSCHECK_DEBUG: 2, AGENTD_DEBUG: 2, MONITORD_ROTATE_LOG: 0}
 
 
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_create_after_delete(test_configuration, test_metadata):
-    pass
+def test_create_after_delete(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
+                             truncate_monitored_files, daemons_handler, folder_to_monitor, file_to_monitor):
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+
+    file.remove_folder(folder_to_monitor)
+    wazuh_log_monitor.start(generate_callback(SENDING_FIM_EVENT))
+    print(wazuh_log_monitor.callback_result)
+    assert wazuh_log_monitor.callback_result
+
+    file.create_folder(folder_to_monitor)
+    file.write_file(file_to_monitor)
+    wazuh_log_monitor.start(generate_callback(SENDING_FIM_EVENT))
+    print(wazuh_log_monitor.callback_result)
+    assert wazuh_log_monitor.callback_result
