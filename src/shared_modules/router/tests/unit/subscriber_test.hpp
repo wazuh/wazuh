fb8cd2ad75e6bee9b064449651cb2277391692db
kevin-mcm <kevincardenasmiranda9@gmail.com>
2023-07-25 08:45:04 -0400

    CL: - Add tests for publisher in Router module - Add tests for subscriber in Router module

---
+++ src/shared_modules/router/tests/unit/subscriber_test.hpp
@@ -0,0 +1,24 @@
+/*
+ * Wazuh router - Subscriber tests
+ * Copyright (C) 2015, Wazuh Inc.
+ * July 17, 2023.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _SUBSCRIBER_TEST_HPP
+#define _SUBSCRIBER_TEST_HPP
+
+#include <gtest/gtest.h>
+
+class SubscriberTest : public ::testing::Test
+{
+protected:
+    SubscriberTest() = default;
+    ~SubscriberTest() override = default;
+};
+
+#endif //_SUBSCRIBER_TEST_HPP
