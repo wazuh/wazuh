4b7805c467ea7a822c5506ba7f1b99bfb60f6eb6
Julian Morales <jmorales@unc.edu.ar>
2023-07-28 14:50:59 +0000

    Fix tokenizer

--- src/engine/source/logicexpr/interface/logicexpr/parser.hpp
+++ src/engine/source/logicexpr/interface/logicexpr/parser.hpp
@@ -153,7 +153,7 @@ std::stack<Token> infixToPostfix(std::queue<Token>& infix)
         }
         else
         {
-            while (!operatorStack.empty() && !operatorStack.top()->isParenthesisOpen() && operatorStack.top() >= token)
+            while (!operatorStack.empty() && !operatorStack.top()->isParenthesisOpen() && *(operatorStack.top()) >= *token)
             {
                 postfix.push(std::move(operatorStack.top()));
                 operatorStack.pop();
