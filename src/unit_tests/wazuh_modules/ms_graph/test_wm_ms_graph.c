570468de901791dd5030ebd4641ee6583908e6b0
Nicolas Gomez Palacios <nicogp2@gmail.com>
2023-08-01 20:24:56 -0300

    Fix compilation of ms_graph UTs.

--- src/unit_tests/wazuh_modules/ms_graph/test_wm_ms_graph.c
+++ src/unit_tests/wazuh_modules/ms_graph/test_wm_ms_graph.c
@@ -17,9 +17,9 @@
 #include <time.h>
 
 #include "shared.h"
-#include "wazuh_modules/wmodules.h"
-#include "wazuh_modules/wm_ms_graph.h"
-#include "wazuh_modules/wm_ms_graph.c"
+#include "../../wazuh_modules/wmodules.h"
+#include "../../wazuh_modules/wm_ms_graph.h"
+#include "../../wazuh_modules/wm_ms_graph.c"
 
 #include "../scheduling/wmodules_scheduling_helpers.h"
 #include "../../wrappers/common.h"
