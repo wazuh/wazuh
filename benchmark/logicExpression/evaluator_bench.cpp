d3fa9fa815386c844ddf6a6608db83d9b63d4e5e
Julian Morales <jmorales@unc.edu.ar>
2023-08-01 19:00:51 +0000

    Adapt the benchmark for evaluator

--- benchmark/logicExpression/evaluator_bench.cpp
+++ benchmark/logicExpression/evaluator_bench.cpp
@@ -1,32 +1,33 @@
 #include <benchmark/benchmark.h>
+
 #include <logicexpr/logicexpr.hpp>
 
 static void BM_DijkstraEvaluator(benchmark::State& state)
 {
-    auto fakeTermBuilder = [](std::string s) -> std::function<bool(int)>
+     auto fakeTermBuilder = [](std::string s) -> std::function<bool(int)>
     {
-        if (s == "EVEN")
+        if (s == "even")
         {
             return [](int i)
             {
                 return i % 2 == 0;
             };
         }
-        else if (s == "ODD")
+        else if (s == "odd")
         {
             return [](int i)
             {
                 return i % 2 != 0;
             };
         }
-        else if (s == "GREAT5")
+        else if (s == "great5")
         {
             return [](int i)
             {
                 return i > 5;
             };
         }
-        else if (s == "GREAT1")
+        else if (s == "great1")
         {
             return [](int i)
             {
@@ -40,9 +41,24 @@ static void BM_DijkstraEvaluator(benchmark::State& state)
         }
     };
 
-    // Build function
-    auto evaluator = logicExpression::buildDijstraEvaluator<int>(
-        "(EVEN OR ODD AND NOT GREAT5) AND GREAT1", fakeTermBuilder);
+    parsec::Parser<std::string> termP = [](std::string_view text, size_t pos) -> parsec::Result<std::string>
+    {
+        // Until space, ( or ) without including it
+        auto end = text.find_first_of(" ()", pos);
+        if (end == std::string_view::npos)
+        {
+            end = text.size();
+        }
+        // the keyword cannot be a operator, so we check it here
+        if (std::isupper(text[pos]) || text[pos] == '(' || text[pos] == ')')
+        {
+            return parsec::makeError<std::string>("Unexpected token", pos);
+        }
+        return parsec::makeSuccess<std::string>(std::string {text.substr(pos, end - pos)}, end);
+    };
+
+    auto expression = "(even OR odd AND NOT great5) AND great1";
+    std::function<bool(int)> evaluator = logicexpr::buildDijstraEvaluator<int, std::string>(expression, fakeTermBuilder, termP);
 
     // Benchamark
     for (auto _ : state)
