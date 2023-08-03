506d92da6459b3b9b83fdf8d198487c37e5360bc
javier <javier.sanchez@wazuh.com>
2023-08-03 10:55:47 +0200

    Modify test test_get_ossec_logst

--- framework/wazuh/core/tests/test_manager.py
+++ framework/wazuh/core/tests/test_manager.py
@@ -116,14 +116,13 @@ def test_get_ossec_logs(log_format):
     logs = get_logs(json_log=log_format == LoggingFormat.json).splitlines()
 
     with patch("wazuh.core.manager.get_wazuh_active_logging_format", return_value=log_format):
-        if log_format == LoggingFormat.plain:
-            with pytest.raises(WazuhInternalError, match=".*1020.*"):
-                get_ossec_logs()
-        else:
-            with patch('wazuh.core.manager.exists', return_value=True):
-                with patch('wazuh.core.manager.tail', return_value=logs):
-                    result = get_ossec_logs()
-                    assert all(key in log for key in ('timestamp', 'tag', 'level', 'description') for log in result)
+        with pytest.raises(WazuhInternalError, match=".*1020.*"):
+            get_ossec_logs()
+
+        with patch('wazuh.core.manager.exists', return_value=True):
+            with patch('wazuh.core.manager.tail', return_value=logs):
+                result = get_ossec_logs()
+                assert all(key in log for key in ('timestamp', 'tag', 'level', 'description') for log in result)
 
 
 @patch("wazuh.core.manager.get_wazuh_active_logging_format", return_value=LoggingFormat.plain)
