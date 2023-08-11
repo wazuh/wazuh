3262fdfd3f03dfbdcfaa60b7ab0ae1fcba2144c5
Julian Morales <jmorales@unc.edu.ar>
2023-08-11 19:46:38 +0000

    Add double link cache db and creation from store

--- src/engine/source/rnm/src/resourceNamespaceManager.cpp
+++ src/engine/source/rnm/src/resourceNamespaceManager.cpp
@@ -1,5 +1,7 @@
 #include <rnm/resourceNamespaceManager.hpp>
 
+#include <list>
+
 /* lazy std::hash specialization for base::Name */
 namespace std
 {
@@ -18,6 +20,9 @@ struct hash<base::Name>
     }
 };
 } // namespace std
+
+constexpr std::size_t PARTS_NAMESPACE = 1;        ///< Number of parts in the namespace
+constexpr std::size_t PARTS_VIRTUAL_RESOURCE = 3; ///< Number of parts in the virtual resource
 
 namespace rnm
 {
@@ -38,100 +43,179 @@ inline bool isVSNameValid(const VSName& vsname)
     {
         return false;
     }
-    return name.parts().size() == 1;
-}
-
-inline bool isRoleNameValid(const RoleName& roleName)
-{
-    return !roleName.empty();
+    return name.parts().size() == PARTS_NAMESPACE;
 }
 
 inline bool isResourceCollection(const base::Name& resourceName)
 {
-    return resourceName.parts().size() < 2;
+    return resourceName.parts().size() < PARTS_VIRTUAL_RESOURCE;
+}
+
+inline bool isNameInCollection(const base::Name& name, const base::Name& collection)
+{
+    auto nameParts = name.parts();
+    auto collectionParts = collection.parts();
+    if (collectionParts.size() > nameParts.size())
+    {
+        return false;
+    }
+    for (size_t i = 0; i < collectionParts.size(); ++i)
+    {
+        if (collectionParts[i] != nameParts[i])
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 /*************************************************************************
  *                        ResourceNamespaceManager
  ************************************************************************/
-
-// TODO: Implement thread safety for the DBResourceNames
+/**
+ * @brief ResourceNamespaceManager::DBResourceNames
+ *
+ * This class is used to store the resource names and their associated VSName.
+ * It is used to cache the resource names and their associated VSName.
+ * It is NOT thread safe.
+ */
 class ResourceNamespaceManager::DBResourceNames
 {
 private:
-    std::unordered_map<base::Name, VSName> nameToVS;
+    std::unordered_map<base::Name, VSName> nameToVS;       ///< Map from resource name to VSName
+    std::unordered_multimap<VSName, base::Name> vsToNames; ///< Map from VSName to resource names
 
 public:
-    DBResourceNames()
-        : nameToVS()
-    {
-    }
+    DBResourceNames() = default;
 
     /**
-     * @brief Get the VSName for a resource.
+     * @brief Retrieve the VSName associated with a given resource name.
      *
-     * @param resourceName The name of the resource.
-     * @return std::optional<VSName> The name of the virtual space or an empty optional if the resource does not exist.
+     * @param resourceName Name of the resource.
+     * @return The VSName if found; otherwise, std::nullopt.
      */
     std::optional<VSName> getVSName(const base::Name& resourceName) const
     {
         auto it = nameToVS.find(resourceName);
         if (it == nameToVS.end())
-        {
             return std::nullopt;
-        }
+
         return it->second;
     }
 
     /**
-     * @brief Set the VSName for a resource.
-     * @param resourceName The name of the resource.
-     * @param vsname The name of the virtual space.
-     * @return true if the operation was successful. False if the resource does not exist.
+     * @brief Retrieve the all the resource names associated with a given VSName.
+     *
+     * @param vsname VSName to search for.
+     * @return A vector with all the resource names associated with the VSName.
      */
-    bool setVSName(const base::Name& resourceName, const VSName& vsname)
+    std::vector<base::Name> getResourceNames(const VSName& vsname) const
     {
-        auto it = nameToVS.find(resourceName);
-        if (it == nameToVS.end())
+        auto range = vsToNames.equal_range(vsname);
+        std::vector<base::Name> resourceNames;
+
+        resourceNames.reserve(std::distance(range.first, range.second));
+        for (auto it = range.first; it != range.second; ++it)
+        {
+            resourceNames.push_back(it->second);
+        }
+        return resourceNames;
+    }
+
+    /**
+     * @brief Retrieve the all VSNames.
+     *
+     * @return A vector with all the VSNames.
+     */
+    std::vector<VSName> getVSNames() const
+    {
+        std::vector<VSName> vsnames;
+        for (const auto& [name, vsname] : nameToVS)
+        {
+            vsnames.push_back(vsname);
+        }
+        return vsnames;
+    }
+
+    /**
+     * @brief Change the VSName associated with a given resource.
+     *
+     * @param resourceName Name of the resource.
+     * @param vsname New VSName to associate.
+     * @return true if successfully changed; false otherwise.
+     */
+    bool changeVSName(const base::Name& resourceName, const VSName& vsname)
+    {
+        auto itNameToVS = nameToVS.find(resourceName);
+        if (itNameToVS == nameToVS.end())
         {
             return false;
         }
-        it->second = vsname;
+        auto oldVSName = itNameToVS->second;
+        itNameToVS->second = vsname;
+
+        // Remove previous association from vsToNames
+        auto range = vsToNames.equal_range(oldVSName);
+        for (auto it = range.first; it != range.second; ++it)
+        {
+            if (it->second == resourceName)
+            {
+                vsToNames.erase(it);
+                break;
+            }
+        }
+
+        // Add new association to vsToNames
+        vsToNames.insert({vsname, resourceName});
+
         return true;
     }
 
     /**
-     * @brief Add a resource to the namespace.
+     * @brief Add a new resource to the namespace with an associated VSName.
      *
-     * @param resourceName The name of the resource.
-     * @param vsname The name of the virtual space.
-     * @return true if the operation was successful. False if the resource already exists.
+     * @param resourceName Name of the resource.
+     * @param vsname VSName to associate.
+     * @return true if successfully added; false if resource name already exists.
      */
     bool add(const base::Name& resourceName, const VSName& vsname)
     {
-        auto it = nameToVS.find(resourceName);
-        if (it != nameToVS.end())
+        if (nameToVS.find(resourceName) != nameToVS.end())
         {
             return false;
         }
         nameToVS.insert({resourceName, vsname});
+        vsToNames.insert({vsname, resourceName});
+
         return true;
     }
 
     /**
-     * @brief Delete a resource from the namespace.
+     * @brief Remove a resource from the namespace.
      *
-     * @param resourceName The name of the resource.
-     * @return true if the operation was successful. False if the resource does not exist.
+     * @param resourceName Name of the resource.
+     * @return true if successfully removed; false otherwise.
      */
     bool del(const base::Name& resourceName)
     {
-        auto it = nameToVS.find(resourceName);
-        if (it == nameToVS.end())
+        auto itNameToVS = nameToVS.find(resourceName);
+        if (itNameToVS == nameToVS.end())
         {
             return false;
         }
-        nameToVS.erase(it);
+        auto vsname = itNameToVS->second;
+        nameToVS.erase(itNameToVS);
+
+        // Remove association from vsToNames
+        auto range = vsToNames.equal_range(vsname);
+        for (auto it = range.first; it != range.second; ++it)
+        {
+            if (it->second == resourceName)
+            {
+                vsToNames.erase(it);
+                break;
+            }
+        }
         return true;
     }
 };
@@ -145,13 +229,96 @@ ResourceNamespaceManager::ResourceNamespaceManager(std::weak_ptr<store::IStore> 
     , m_mutex()
     , m_prefix(prefix)
 {
-    // TODO: Add check integrity
-    // Implement the internal shared db resource against the store.
+
+    // Load the cache
+    auto storePtr = m_store.lock();
+    if (!storePtr)
+    {
+        throw std::runtime_error("Store is not available");
+    }
+
+    // TODO: Contemplate the case where the store is empty
+    // TODO: Analice add or change the store interface to return a vector of names whens get a collection
+    std::list<base::Name> fullResouceName {};
+    auto visitor = [&storePtr, &fullResouceName](const base::Name& name, int depth, auto& visitorRef) -> void
+    {
+        if (depth < 0){
+            return;
+        }
+        depth--;
+
+
+        auto resultStore = storePtr->get(name);
+        if(const auto err = std::get_if<base::Error>(&resultStore))
+        {
+            throw std::runtime_error(fmt::format("Error loading namespaces: {}", err->message));
+        }
+
+        const auto& jChildNames = std::get<json::Json>(resultStore).getArray();
+        if (!jChildNames)
+        {
+            throw std::runtime_error("Resource list is not an array");
+        }
+
+        for (const auto& jChildName : jChildNames.value())
+        {
+            const auto& jChildNameStr = jChildName.getString();
+            if (!jChildNameStr)
+            {
+                throw std::runtime_error("Resource name is not a string");
+            }
+            if (depth == 0){
+                fullResouceName.emplace_back(jChildNameStr.value());
+            }
+            else{
+                visitorRef(jChildNameStr.value(), depth, visitorRef);
+            }
+
+        }
+
+    };
+
+    // Get the list of all resources in the store
+    visitor(m_prefix, PARTS_VIRTUAL_RESOURCE, visitor);
+
+    // Get
+
 }
 
-base::Name ResourceNamespaceManager::translateName(const base::Name& resourceName, const VSName& vsname) const
+base::Name ResourceNamespaceManager::virtualToRealName(const base::Name& resourceName, const VSName& vsname) const
 {
     return m_prefix + base::Name(vsname) + resourceName;
+}
+
+// Unused function
+inline std::optional<std::pair<VSName, base::Name>> ResourceNamespaceManager::realToVirtualName(const base::Name& realName) const
+{
+    // Remove de prefix
+    const auto& partsRN = realName.parts();
+    const auto& partsPrefix = m_prefix.parts();
+    VSName vsname;
+
+    // The realname must have more parts than the prefix + namespace
+    if (partsRN.size() + PARTS_NAMESPACE > partsRN.size() && std::equal(partsRN.begin(), partsRN.end(), partsRN.begin()))
+    {
+        auto it = partsRN.begin() + partsRN.size();
+        if (it != partsRN.end())
+        {
+            vsname = *it;
+            // Check if the VSName is valid
+            if (!isVSNameValid(vsname))
+            {
+                return std::nullopt;
+            }
+            // Remove the VSName from the real name
+            it++;
+            base::Name resourceName(std::vector<std::string>(it, partsRN.end()));
+
+            return std::make_pair(std::move(vsname), std::move(resourceName));
+        }
+    }
+
+    return std::nullopt;
 }
 
 std::optional<VSName> ResourceNamespaceManager::getVSName(const base::Name& resourceName) const
@@ -168,10 +335,35 @@ std::optional<VSName> ResourceNamespaceManager::getVSName(const base::Name& reso
 }
 
 std::variant<json::Json, base::Error> ResourceNamespaceManager::getCollection(const base::Name& collection,
-
                                                                               const RoleName& role) const
 {
-    throw std::runtime_error("Not implemented");
+    // Step 1: Get all namespaces
+    std::shared_lock<std::shared_mutex> lock(m_mutex);
+    auto vsnames = m_cache->getVSNames();
+
+    // Step 2: Remove namespaces that the role does not have access to
+    vsnames.erase(std::remove_if(vsnames.begin(),
+                                 vsnames.end(),
+                                 [&](const auto& vsname) { return !m_authFn(vsname, role, VSOperation::LIST); }),
+                  vsnames.end());
+
+    // Step 3: Get all name resources from the namespaces
+    json::Json jCollection;
+    jCollection.setArray();
+    for (const auto& vsname : vsnames)
+    {
+        auto names = m_cache->getResourceNames(vsname);
+        // Insert the names that are in the collection
+        for (const auto& name : names)
+        {
+            if (isNameInCollection(name, collection))
+            {
+                jCollection.appendString(name.fullName());
+            }
+        }
+    }
+
+    return jCollection;
 }
 
 std::variant<json::Json, base::Error> ResourceNamespaceManager::getItem(const base::Name& item,
@@ -198,7 +390,7 @@ std::variant<json::Json, base::Error> ResourceNamespaceManager::getItem(const ba
         return base::Error {"Store is not available"};
     }
 
-    auto name = translateName(item, *vsname);
+    auto name = virtualToRealName(item, *vsname);
     auto resource = store->get(name);
 
     // Step 5: check if error
@@ -251,7 +443,7 @@ std::optional<base::Error> ResourceNamespaceManager::add(const base::Name& resou
     {
         return base::Error {"Store is not available"};
     }
-    auto name = translateName(resourceName, vsname);
+    auto name = virtualToRealName(resourceName, vsname);
     auto error = store->add(name, content);
 
     // Step 5: check if error
@@ -304,7 +496,7 @@ ResourceNamespaceManager::update(const base::Name& resourceName, const json::Jso
         return base::Error {"Store is not available"};
     }
 
-    auto name = translateName(resourceName, *vsname);
+    auto name = virtualToRealName(resourceName, *vsname);
     return store->update(name, content);
 }
 
@@ -337,7 +529,7 @@ std::optional<base::Error> ResourceNamespaceManager::del(const base::Name& resou
     }
 
     m_cache->del(resourceName);
-    auto name = translateName(resourceName, *vsname);
+    auto name = virtualToRealName(resourceName, *vsname);
 
     return store->del(name);
 }
