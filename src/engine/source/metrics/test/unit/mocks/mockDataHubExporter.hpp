c1ef3c432f1e33f30a7b595abe46a7ad87be4c6c
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-08-14 19:01:25 +0000

    Add mocks for metrics module

---
+++ src/engine/source/metrics/test/unit/mocks/mockDataHubExporter.hpp
@@ -0,0 +1,13 @@
+#ifndef _MOCK_DATA_HUB_EXPORTER_HPP
+#define _MOCK_DATA_HUB_EXPORTER_HPP
+
+#include <gmock/gmock.h>
+
+#include <metrics/iDataHub.hpp>
+
+class MockDataHubExporter : public metricsManager::IDataHub
+{
+    MOCK_METHOD(void, setResource, (const std::string&, const json::Json&), (override));
+};
+
+#endif //_MOCK_DATA_HUB_EXPORTER_HPP
