9625eb73b9a5baa1b6a6cf97da22ed9e7d768f35
quebim <kevin.ledesma@wazuh.com>
2023-08-16 14:30:57 -0300

    feature(#17881): Add new pattern and apply it to test, rename scenario

--- tests/integration/test_fim/test_files/test_basic_usage/test_empty_directories_tag.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_empty_directories_tag.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 from wazuh_testing.constants.platforms import WINDOWS
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG, AGENTD_WINDOWS_DEBUG
-from wazuh_testing.modules.fim.patterns import ADDED_EVENT, DELETED_EVENT
+from wazuh_testing.modules.fim.patterns import ADDED_EVENT, DELETED_EVENT, EMPTY_DIRECTORIES_TAG
 from wazuh_testing.modules.fim.utils import get_fim_event_data
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
 from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
@@ -22,8 +22,8 @@ from . import TEST_CASES_PATH, CONFIGS_PATH
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0)]
 
 # Test metadata, configuration and ids.
-cases_path = Path(TEST_CASES_PATH, 'cases_no_directory_in_tag.yaml')
-config_path = Path(CONFIGS_PATH, 'configuration_no_directory_in_tag.yaml')
+cases_path = Path(TEST_CASES_PATH, 'cases_empty_directories_tag.yaml')
+config_path = Path(CONFIGS_PATH, 'configuration_empty_directories_tag.yaml')
 test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
 test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
 
@@ -34,11 +34,11 @@ if sys.platform == WINDOWS: local_internal_options.update({AGENTD_WINDOWS_DEBUG:
 
 
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_no_directory_in_tag(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
-                             truncate_monitored_files, daemons_handler):
+def test_empty_directories_tag(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
+                               truncate_monitored_files, daemons_handler):
     # Arrange
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
 
     # Assert
-    wazuh_log_monitor.start(generate_callback(r'.*Empty directories tag found in the configuration.*'))
+    wazuh_log_monitor.start(generate_callback(EMPTY_DIRECTORIES_TAG))
     assert wazuh_log_monitor.callback_result
