5a8ff2e86394418618f97194bc4d9ab48ad85f3f
quebim <kevin.ledesma@wazuh.com>
2023-08-17 11:21:17 -0300

    refactor(#17881): merge move_dir and move_file scenarios into just one with different cases

--- tests/integration/test_fim/test_files/test_basic_usage/test_move.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_move.py
@@ -22,7 +22,7 @@ from . import TEST_CASES_PATH, CONFIGS_PATH
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0)]
 
 # Test metadata, configuration and ids.
-cases_path = Path(TEST_CASES_PATH, 'cases_move_dir.yaml')
+cases_path = Path(TEST_CASES_PATH, 'cases_move.yaml')
 config_path = Path(CONFIGS_PATH, 'configuration_basic.yaml')
 test_configuration, test_metadata, cases_ids = get_test_cases_data(cases_path)
 test_configuration = load_configuration_template(config_path, test_configuration, test_metadata)
@@ -34,21 +34,13 @@ if sys.platform == WINDOWS: local_internal_options.update({AGENTD_WINDOWS_DEBUG:
 
 
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_move_dir(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
-                  truncate_monitored_files, folder_to_monitor, daemons_handler, start_monitoring):
-    # Arrange
+def test_move(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
+              truncate_monitored_files, folder_to_monitor, daemons_handler, start_monitoring, path_to_edit):
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
     fim_mode = test_metadata.get('fim_mode')
-    folder_to_move = test_metadata.get('folder_to_move')
-    folder_moved_path = test_metadata.get('folder_moved_path')
-
-    # Act
-    file.create_folder(folder_to_move)
-    file.write_file(Path(folder_to_move, 'newfile'), 'test')
-    wazuh_log_monitor.start(generate_callback(ADDED_EVENT))
 
     # Assert
-    file.move(folder_to_move, folder_moved_path)
+    file.move(path_to_edit, Path(folder_to_monitor, 'test'))
     wazuh_log_monitor.start(generate_callback(DELETED_EVENT))
     assert wazuh_log_monitor.callback_result
     assert get_fim_event_data(wazuh_log_monitor.callback_result)['mode'] == fim_mode
