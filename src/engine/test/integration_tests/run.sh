d8c0bf48bd9dd9e499801f11df698f96ec41e192
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-08-21 17:18:49 +0000

    Add integration test in github actions

--- src/engine/test/integration_tests/run.sh
+++ src/engine/test/integration_tests/run.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
 fi
 
 BINARY_PATH="$1"
-ROOT_FOLDER="integration_tests"
+ROOT_FOLDER="/home/runner/work/wazuh/wazuh/src/engine/test/integration_tests"
 
 # Execute the binary with the argument "server start"
 "$BINARY_PATH" server start &
@@ -15,17 +15,18 @@ ROOT_FOLDER="integration_tests"
 # Capture the process ID of the binary
 BINARY_PID=$!
 
-# TODO: Investigate some way to evaluate if the server is started
-sleep 1
+# Wait for the server to start
+# You might need to replace this with a more reliable check
+sleep 2
 
-find "$ROOT_FOLDER" -type d -name "features" -exec sh -c '
-    features_dir="$0"
+# Find "features" folders and execute Behave
+find "$ROOT_FOLDER" -type d -name "features" | while read features_dir; do
     steps_dir=$(dirname "$features_dir")/steps
     if [ -d "$steps_dir" ]; then
         echo "Running Behave in $features_dir"
         behave "$features_dir"
     fi
-' {} \;
+done
 
 # Terminate the binary process by sending a termination signal
 kill $BINARY_PID
