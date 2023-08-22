5227ee1e8e2fb73d6a71032ae05d2fa9715acfcf
kevin-mcm <kevincardenasmiranda9@gmail.com>
2023-08-21 21:23:36 -0400

    Fix clang format

--- src/shared_modules/router/tests/unit/main.cpp
+++ src/shared_modules/router/tests/unit/main.cpp
@@ -13,6 +13,6 @@
 
 int main(int argc, char** argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
