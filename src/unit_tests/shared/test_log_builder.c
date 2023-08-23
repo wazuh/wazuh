73c3edd102c051482745feb7cc3b902465b10d77
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2023-08-23 13:44:25 +0200

    Add unit tests to log_builder library to check the IP update interval

--- src/unit_tests/shared/test_log_builder.c
+++ src/unit_tests/shared/test_log_builder.c
@@ -17,6 +17,10 @@
 
 #include "../../headers/shared.h"
 #include "../wrappers/common.h"
+#include "../wrappers/wazuh/client-agent/notify_wrappers.h"
+
+extern int g_ip_update_interval;
+int log_builder_update_host_ip(log_builder_t * builder);
 
 // Tests
 
@@ -40,9 +44,49 @@ void test_log_builder(void **state)
     log_builder_destroy(builder);
 }
 
+void test_log_builder_update(void **state)
+{
+    will_return(__wrap_getDefine_Int, 1);
+    log_builder_t * builder = log_builder_init(false);
+    assert_int_equal(g_ip_update_interval, 1);
+    assert_non_null(builder);
+
+#ifdef WIN32
+    char * return_ip = "1.2.3.4";
+    time_mock_value = 1000;
+    will_return(wrap_get_agent_ip_legacy_win32, strdup(return_ip));
+#elif defined __linux__ || defined __MACH__ || defined sun || defined FreeBSD || defined OpenBSD
+    will_return(__wrap_control_check_connection, 16);
+    will_return(__wrap_send, 7);
+    will_return(__wrap_recv, 0);
+#endif
+
+    int r = log_builder_update_host_ip(builder);
+    assert_int_equal(r, 0);
+#ifdef WIN32
+    assert_string_equal(builder->host_ip, "1.2.3.4");
+#endif
+    log_builder_destroy(builder);
+}
+
+void test_log_builder_not_update(void **state) {
+    will_return(__wrap_getDefine_Int, 0);
+
+    log_builder_t * builder = log_builder_init(false);
+    assert_int_equal(g_ip_update_interval, 0);
+    assert_non_null(builder);
+
+    int r = log_builder_update_host_ip(builder);
+    assert_int_equal(r, 0);
+
+    log_builder_destroy(builder);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(test_log_builder),
+            cmocka_unit_test(test_log_builder_update),
+            cmocka_unit_test(test_log_builder_not_update),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
