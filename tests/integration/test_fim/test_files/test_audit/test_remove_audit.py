af7585f90e0c0dcecc0a15e564e9d394b2f627f2
quebim <kevin.ledesma@wazuh.com>
2023-08-24 10:53:27 -0300

    feat(#17881): improve patterns naming and update imports

--- tests/integration/test_fim/test_files/test_audit/test_remove_audit.py
+++ tests/integration/test_fim/test_files/test_audit/test_remove_audit.py
@@ -68,7 +68,7 @@ from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
 from wazuh_testing.modules.agentd.configuration import AGENTD_DEBUG
 from wazuh_testing.modules.fim.patterns import WHODATA_NOT_STARTED
 from wazuh_testing.modules.monitord.configuration import MONITORD_ROTATE_LOG
-from wazuh_testing.modules.syscheck.configuration import SYSCHECK_DEBUG
+from wazuh_testing.modules.fim.configuration import SYSCHECK_DEBUG
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
 from wazuh_testing.utils.callbacks import generate_callback
 from wazuh_testing.utils.configuration import get_test_cases_data, load_configuration_template
