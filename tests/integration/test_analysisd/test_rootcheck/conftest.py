b57b7ca559471ed33f5a5d8f748f640d2df11c3d
Luis Enrique Chico Capistrano <luis.chico@wazuh.com>
2023-08-25 17:21:48 -0300

    Address Tomas and Kevin suggestions

--- tests/integration/test_analysisd/test_rootcheck/conftest.py
+++ tests/integration/test_analysisd/test_rootcheck/conftest.py
@@ -6,30 +6,19 @@ import pytest
 import time
 
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
+from wazuh_testing.constants.paths.sockets import QUEUE_DB_PATH
 from wazuh_testing.modules.modulesd import patterns
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
 from wazuh_testing.utils import callbacks
-from wazuh_testing.utils.network import TCP
+from wazuh_testing.utils.manage_agents import remove_agents
 from wazuh_testing.utils.services import control_service
+from wazuh_testing.tools.db_administrator import DatabaseAdministrator
 from wazuh_testing.tools.simulators.agent_simulator import connect, create_agents
 
-@pytest.fixture()
-def wait_for_rootcheck_start():
-    # Wait for module rootcheck starts
-    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(patterns.MODULESD_STARTED, {
-                              'integration': 'rootcheck'
-                          }))
-    assert (wazuh_log_monitor.callback_result == None), f'Error invalid configuration event not detected'
-
-@pytest.fixture(scope="function")
-def load_agents(request):
+def inject_rootcheck_events(agents):
     injectors = []
-    agents = []
-    agents_number = request.getfixturevalue("test_metadata")["agents_number"]
 
-    for index in range(agents_number):
-        agent = create_agents(1, 'localhost')[0]
+    for agent in agents:
         agent.modules['rootcheck']['status'] = 'enabled'
         _, injector = connect(agent)
         injectors.append(injector)
@@ -43,4 +32,34 @@ def load_agents(request):
 
     # Service needs to be stopped otherwise db lock will be held by Wazuh db
     control_service('stop')
-    return agents
+
+def retrieve_rootcheck_rows(agent_id):
+    db_connection = DatabaseAdministrator(os.path.join(QUEUE_DB_PATH, f'{agent_id}.db'))
+    rows = db_connection.select("pm_event")
+    db_connection.cursor.close()
+    db_connection.connection.close()
+    return rows
+
+@pytest.fixture()
+def wait_for_rootcheck_start():
+    # Wait for module rootcheck starts
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(patterns.MODULESD_STARTED, {
+                              'integration': 'rootcheck'
+                          }))
+    assert (wazuh_log_monitor.callback_result == None), f'Error invalid configuration event not detected'
+
+@pytest.fixture(scope="function")
+def load_agents(request):
+    agents = []
+    for _ in range(request.getfixturevalue("test_metadata")["agents_number"]):
+       agent = create_agents(1, 'localhost')[0]
+
+    inject_rootcheck_events(agents)
+
+    yield agents
+    # Stop and delete simulated agent
+    control_service('start')
+    for agent in agents:
+        remove_agents(agent.id,'manage_agents')
+    control_service('stop')
--- tests/integration/test_analysisd/test_rootcheck/test_configuration/test_rootcheck_update.py
+++ tests/integration/test_analysisd/test_rootcheck/test_configuration/test_rootcheck_update.py
@@ -41,34 +41,17 @@ tags:
     - rootcheck
 '''
 
-import json
-import os
 import pytest
+import time
 from pathlib import Path
 
-from wazuh_testing.constants.paths.logs import ALERTS_JSON_PATH
-from wazuh_testing.constants.paths.sockets import QUEUE_DB_PATH, WAZUH_DB_SOCKET_PATH
-from wazuh_testing.tools.db_administrator import DatabaseAdministrator
-from wazuh_testing.tools.socket_controller import SocketController
 from wazuh_testing.utils import configuration
+from conftest import inject_rootcheck_events, retrieve_rootcheck_rows
 
 from . import CONFIGS_PATH, TEST_CASES_PATH
 
 # Marks
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0), pytest.mark.server]
-
-def retrieve_rootcheck_rows(agent_id):
-    db_connection = DatabaseAdministrator(os.path.join(QUEUE_DB_PATH, f'{agent_id}.db'))
-    rows = db_connection.select("pm_event")
-    db_connection.cursor.close()
-    db_connection.connection.close()
-    return rows
-
-def send_delete_table_request(agent_id):
-    controller = SocketController(WAZUH_DB_SOCKET_PATH)
-    controller.send(f'agent {agent_id} rootcheck delete', size=True)
-    response = controller.receive(size=True)
-    return response
 
 # Configuration and cases data.
 test_configs_path = Path(CONFIGS_PATH, 'config_template.yaml')
@@ -84,7 +67,7 @@ daemons_handler_configuration = {'all_daemons': True}
 # Test function.
 @pytest.mark.parametrize('test_configuration, test_metadata',
                          zip(test_configuration, test_metadata), ids=test_cases_ids)
-def test_rootcheck(test_configuration, test_metadata, set_wazuh_configuration,
+def test_rootcheck_update(test_configuration, test_metadata, set_wazuh_configuration,
                    daemons_handler, wait_for_rootcheck_start, truncate_monitored_files,
                    load_agents):
     '''
@@ -130,91 +113,29 @@ def test_rootcheck(test_configuration, test_metadata, set_wazuh_configuration,
     assertions:
         - Verify that rootcheck events are added into the database
         - Verify that the rootcheck events are updated on the database
-        - Verify that the rootcheck events are deletet from the database
-    input_description: Different test cases are contained in an external YAML file (wazuh_manager_conf.yaml)
+    input_description: Different test cases are contained in an external YAML file (cases_configuration.yaml)
                        which includes configuration settings for the 'rootcheck' module.
     expected_output:
         - r'.*not found in Database'
         - r'.*not found in alerts file'
-        - r'.*not found in Database'
         - First time in log was updated after insertion
         - Updated time in log was not updated
-        - Wazuh DB returned an error trying to delete the agent
-        - Rootcheck events were not deleted
-
     '''
-    # Check that logs have been added to the sql database
     agents = load_agents
+    update_threshold = time.time()
+
+    inject_rootcheck_events(agents)
+
+    # Check that logs have been updated
     for agent in agents:
         rows = retrieve_rootcheck_rows(agent.id)
-        db_string = [row[3] for row in rows]
+
         logs_string = [':'.join(x.split(':')[2:]) for x in
-                      agent.rootcheck.messages_list]
-
-        alerts_description = []
-        with open(ALERTS_JSON_PATH, 'r') as f:
-            for line in f:
-                if '"decoder":{"name":"rootcheck"}' in line:
-                    try:
-                        json_lines = json.loads(line.strip())
-                        alerts_description.append(json_lines['full_log'])
-                    except json.JSONDecodeError:
-                        print(f"Invalid json {line.strip()}")
-
-        for log in logs_string:
-            assert log in db_string, f"Log: \"{log}\" not found in Database"
-            if log not in ['Starting rootcheck scan.',
-                           'Ending rootcheck scan.']:
-                assert log in alerts_description, f"Log: \"{log}\" " \
-                                                  "not found in alerts file"
-
-    # if test_metadata["check_updates"]:
-    #     # Service needs to be restarted
-    #     control_service('start')
-
-    #     update_threshold = time.time()
-
-    #     injectors = create_injectors(agents)
-
-    #     # Let rootcheck events to be sent for 60 seconds
-    #     time.sleep(60)
-
-    #     for injector in injectors:
-    #         injector.stop_receive()
-
-    #     # Service needs to be stopped otherwise db lock will be held by Wazuh db
-    #     control_service('stop')
-
-    #     # Check that logs have been updated
-    #     for agent in agents:
-    #         rows = retrieve_rootcheck_rows(agent.id)
-
-    #         logs_string = [':'.join(x.split(':')[2:]) for x in
-    #                        agent.rootcheck.messages_list]
-    #         for row in rows:
-    #             assert row[1] < update_threshold, \
-    #                 f'First time in log was updated after insertion'
-    #             assert row[2] > update_threshold, \
-    #                 f'Updated time in log was not updated'
-    #             assert row[3] in logs_string, \
-    #                 f"Log: \"{log}\" not found in Database"
-
-    # if test_metadata["check_delete"]:
-    #     # Service needs to be restarted
-    #     control_service('start')
-
-    #     for agent in agents:
-    #         response = send_delete_table_request(agent.id)
-    #         assert response.startswith(b'ok'), "Wazuh DB returned an error " \
-    #                                            "trying to delete the agent"
-
-    #     # Wait 5 seconds
-    #     time.sleep(5)
-
-    #     # Service needs to be stopped otherwise db lock will be held by Wazuh db
-    #     control_service('stop')
-
-    #     # Check that logs have been deleted
-    #     for agent in agents:
-    #         rows = retrieve_rootcheck_rows(agent.id)
-    #         assert len(rows) == 0, 'Rootcheck events were not deleted'
+                        agent.rootcheck.messages_list]
+        for row in rows:
+            assert row[1] < update_threshold, \
+                f'First time in log was updated after insertion'
+            assert row[2] > update_threshold, \
+                f'Updated time in log was not updated'
+            assert row[3] in logs_string, \
+                f"Log: \"{row[3]}\" not found in Database"
