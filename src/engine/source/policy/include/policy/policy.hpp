6f43d0780528d6c95e8280a316dac29a2e8d4bd2
Julian Morales <jmorales@unc.edu.ar>
2023-08-28 21:03:42 +0000

    Add base for policy static lib

---
+++ src/engine/source/policy/include/policy/policy.hpp
@@ -0,0 +1,10 @@
+#ifndef _POLICY_POLICY_HPP
+#define _POLICY_POLICY_HPP
+
+#include <policy/iPolicyAPI.hpp>
+
+namespace policy {
+
+} // namespace policy
+
+#endif // _POLICY_POLICY_HPP
---
+++ src/engine/source/policy/test/src/component/policy_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(PolicyCTest, cTest) {
+  GTEST_SKIP();
+}
