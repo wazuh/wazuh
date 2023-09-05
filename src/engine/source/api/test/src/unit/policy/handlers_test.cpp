f11d5b9240e27b1d4ccaa3401045f17a584cd1e3
Julian Morales <jmorales@unc.edu.ar>
2023-09-05 21:41:34 +0000

    Unify handler policy test

--- src/engine/source/api/test/src/unit/policy/handlers_test.cpp
+++ src/engine/source/api/test/src/unit/policy/handlers_test.cpp
@@ -7,21 +7,15 @@
 using namespace api::policy::mocks;
 using namespace api::policy::handlers;
 
-template <typename T>
+template<typename T>
 class PolicyHandlersTest : public ::testing::TestWithParam<T>
 {
 protected:
     std::shared_ptr<MockPolicy> m_policy;
 
-    void SetUp() override
-    {
-        m_policy = std::make_shared<MockPolicy>();
-    }
+    void SetUp() override { m_policy = std::make_shared<MockPolicy>(); }
 
-    void TearDown() override
-    {
-        m_policy.reset();
-    }
+    void TearDown() override { m_policy.reset(); }
 };
 
 using ExpectedFn = std::function<api::wpResponse(const std::shared_ptr<MockPolicy>&)>;
@@ -33,7 +27,8 @@ const std::string STATUS_ERROR = "ERROR";
 
 ExpectedFn success(const Behaviour& behaviour = {}, const std::string& data = {})
 {
-    return [behaviour, data](const std::shared_ptr<MockPolicy>& policy) -> api::wpResponse {
+    return [behaviour, data](const std::shared_ptr<MockPolicy>& policy) -> api::wpResponse
+    {
         if (behaviour)
         {
             behaviour(policy);
@@ -41,10 +36,13 @@ ExpectedFn success(const Behaviour& behaviour = {}, const std::string& data = {}
         json::Json dataR;
         if (!data.empty())
         {
-            try {
+            try
+            {
                 dataR = json::Json {data.c_str()};
-            } catch (const std::exception& e) {
-                std::cerr << "Error parsing data: " << e.what() << std::endl;
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << "Error parsing data: [" << e.what() << "] " << data << std::endl;
                 throw;
             }
         }
@@ -55,7 +53,8 @@ ExpectedFn success(const Behaviour& behaviour = {}, const std::string& data = {}
 
 ExpectedFn failure(const Behaviour& behaviour = {})
 {
-    return [behaviour](const std::shared_ptr<MockPolicy>& policy) -> api::wpResponse {
+    return [behaviour](const std::shared_ptr<MockPolicy>& policy) -> api::wpResponse
+    {
         if (behaviour)
         {
             behaviour(policy);
@@ -66,20 +65,29 @@ ExpectedFn failure(const Behaviour& behaviour = {})
     };
 }
 
-/*******************************************************************************
- * [storePost] Test create policy
- ******************************************************************************/
-using StorePostT = std::tuple<json::Json, ExpectedFn>;
-using StorePostHandler = PolicyHandlersTest<StorePostT>;
+using GetHandlerToTest = std::function<api::Handler(const std::shared_ptr<api::policy::IPolicy>&)>;
+using TestPolT = std::tuple<GetHandlerToTest, std::string, ExpectedFn>;
+using PolicyHandlerTest = PolicyHandlersTest<TestPolT>;
 
-TEST_P(StorePostHandler, createPolicy)
+TEST_P(PolicyHandlerTest, processRequest)
 {
-    const auto [params, expectedFn] = GetParam();
+    const auto [getHandlerFn, params, expectedFn] = GetParam();
+
+    json::Json jParams;
+    try
+    {
+        jParams = json::Json {params.c_str()};
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error parsing params: [" << e.what() << "] " << params << std::endl;
+        throw;
+    }
 
     auto expectedResponse = expectedFn(m_policy);
-    auto request = api::wpRequest::create("policy.store/post", "test" , params);
+    auto request = api::wpRequest::create("policy.command", "test", jParams);
 
-    auto response = storePost(m_policy)(request);
+    auto response = getHandlerFn(m_policy)(request);
 
     if (expectedResponse.data().getString(STATUS_PATH) == STATUS_ERROR)
     {
@@ -93,20 +101,79 @@ TEST_P(StorePostHandler, createPolicy)
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(HandlerPolicyTest,
-                         StorePostHandler,
-                         testing::Values(StorePostT(json::Json {R"( { "name": "test" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "other/name/version" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "pol/name/version" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "policy/name/version/ext" } )"},
-                                                    failure()),
-                                         StorePostT(json::Json {R"( { "policy": "policy/name" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "policy/" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "policy//0" } )"}, failure()),
-                                         StorePostT(json::Json {R"( { "policy": "policy/name/0" } )"}, failure([](auto policy){
-                                                EXPECT_CALL(*policy, create(base::Name {"policy/name/0"})).WillOnce(::testing::Return(base::Error {}));
-                                         })),
-                                         StorePostT(json::Json {R"( { "policy": "policy/name/0" } )"}, success([](auto policy){
-                                                EXPECT_CALL(*policy, create(base::Name {"policy/name/0"})).WillOnce(::testing::Return(std::nullopt));
-                                         }))));
+INSTANTIATE_TEST_SUITE_P(
+    HandlerPolicyTest,
+    PolicyHandlerTest,
+    testing::Values( // [storePost] Test create policy
+        TestPolT(storePost, R"( { "name": "test" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "other/name/version" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "pol/name/version" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "policy/name/version/ext" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "policy/name" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "policy/" } )", failure()),
+        TestPolT(storePost, R"( { "policy": "policy//0" } )", failure()),
+        TestPolT(storePost,
+                   R"( { "policy": "policy/name/0" } )",
+                   failure(
+                       [](auto policy) {
+                           EXPECT_CALL(*policy, create(base::Name {"policy/name/0"}))
+                               .WillOnce(::testing::Return(base::Error {}));
+                       })),
+        TestPolT(storePost,
+                   R"( { "policy": "policy/name/0" } )",
+                   success(
+                       [](auto policy) {
+                           EXPECT_CALL(*policy, create(base::Name {"policy/name/0"}))
+                               .WillOnce(::testing::Return(std::nullopt));
+                       })),
+        // [storeDelete] Test delete policy
+        TestPolT(storeDelete, R"( { "name": "test" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "other/name/version" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "pol/name/version" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "policy/name/version/ext" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "policy/name" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "policy/" } )", failure()),
+        TestPolT(storeDelete, R"( { "policy": "policy//0" } )", failure()),
+        TestPolT(storeDelete,
+                   R"( { "policy": "policy/name/0" } )",
+                   failure(
+                       [](auto policy) {
+                           EXPECT_CALL(*policy, del(base::Name {"policy/name/0"}))
+                               .WillOnce(::testing::Return(base::Error {}));
+                       })),
+        TestPolT(storeDelete,
+                   R"( { "policy": "policy/name/0" } )",
+                   success(
+                       [](auto policy) {
+                           EXPECT_CALL(*policy, del(base::Name {"policy/name/0"}))
+                               .WillOnce(::testing::Return(std::nullopt));
+                       })),
+        // [storeGet] Test get policy
+        TestPolT(storeGet, R"( { "name": "test" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "other/name/version" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "pol/name/version" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "policy/name/version/ext" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "policy/name" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "policy/" } )", failure()),
+        TestPolT(storeGet, R"( { "policy": "policy//0" } )", failure()),
+        TestPolT(storeGet,
+                   R"( { "policy": "policy/name/0" } )",
+                   failure(
+                       [](auto policy)
+                       {
+                           EXPECT_CALL(*policy, get(base::Name {"policy/name/0"}, std::vector<store::NamespaceId> {}))
+                               .WillOnce(::testing::Return(base::Error {}));
+                       })),
+        TestPolT(storeGet,
+                   R"( { "policy": "policy/name/0" } )",
+                   success(
+                       [](auto policy)
+                       {
+                           EXPECT_CALL(*policy, get(base::Name {"policy/name/0"}, std::vector<store::NamespaceId> {}))
+                               .WillOnce(::testing::Return("Dump of policy"));
+                       }))
+        // End
+        ));
