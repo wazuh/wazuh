fa8c8567c4353c6994f3a1f1d982841469390edb
Manuel <manuel.cano@wazuh.com>
2023-09-14 17:07:21 +0200

    Added functions to different FileMonitor recurrent uses

---
+++ tests/integration/test_agentd/__init__.py
@@ -0,0 +1,61 @@
+from wazuh_testing.constants.paths.configurations import WAZUH_CLIENT_KEYS_PATH
+from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
+from wazuh_testing.modules.agentd.patterns import * 
+from wazuh_testing.tools.monitors.file_monitor import FileMonitor
+from wazuh_testing.utils import callbacks
+
+def add_custom_key() -> None:
+    """Set test client.keys file"""
+    with open(WAZUH_CLIENT_KEYS_PATH, 'w+') as client_keys:
+        client_keys.write("100 ubuntu-agent any TopSecret")
+
+def wait_keepalive():
+    """
+        Watch ossec.log until "Sending keep alive" message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_SENDING_KEEP_ALIVE))
+    assert (wazuh_log_monitor.callback_result != None), f'Sending keep alive not found'
+
+def wait_connect():
+    """
+        Watch ossec.log until received "Connected to the server" message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_CONNECTED_TO_SERVER))
+    assert (wazuh_log_monitor.callback_result != None), f'Connected to the server message not found'
+
+
+def wait_ack():
+    """
+        Watch ossec.log until received ack message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_RECEIVED_ACK))
+    assert (wazuh_log_monitor.callback_result != None), f'Received ack message not found'
+
+
+def wait_state_update():
+    """
+        Watch ossec.log until "Updating state file" message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_UPDATING_STATE_FILE))
+    assert (wazuh_log_monitor.callback_result != None), f'State file update not found'
+
+def wait_enrollment():
+    """
+        Watch ossec.log until "Updating state file" message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_RECEIVED_VALID_KEY))
+    assert (wazuh_log_monitor.callback_result != None), 'Agent never enrolled'
+
+def wait_enrollment_try():
+    """
+        Watch ossec.log until "Updating state file" message is found
+    """
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_REQUESTING_KEY))
+    assert (wazuh_log_monitor.callback_result != None), f'Enrollment retry was not sent'
+
