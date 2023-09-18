d8206dcf880f5580b100a939989d7dbd87f4a721
Javier Bejar Mendez <javier.bejar@wazuh.com>
2023-09-18 15:30:56 +0200

    Added windows sidlist decoding helper

---
+++ source/builder/builders/windowsHelper.hpp
@@ -0,0 +1,32 @@
+#ifndef _BUILDER_BUILDERS_WINDOWSHELPER_HPP
+#define _BUILDER_BUILDERS_WINDOWSHELPER_HPP
+
+#include <baseTypes.hpp>
+#include <defs/idefinitions.hpp>
+#include <kvdb/iKVDBManager.hpp>
+#include <schemf/ischema.hpp>
+
+#include "expression.hpp"
+#include "registry.hpp"
+
+namespace builder::internals::builders
+{
+/**
+ * @brief Get the Windows Helper Builder object
+ * 
+ * Obtains the helper that maps A SidList to a SidListDesc, where Sid identifiers for common Windows SIDs are replaced
+ * by their names.
+ * This helpers needs a kvdb where the mappings between the different parts of the SID and the SID names are stored.
+ * 
+ * @param kvdb kvdb manager to obtain the kvdb handler
+ * @param kvdbScopeName kvdb scope name
+ * @param schema schema
+ * @return HelperBuilder 
+ */
+HelperBuilder getWindowsSidListDescHelperBuilder(std::shared_ptr<kvdbManager::IKVDBManager> kvdb,
+                                      const std::string& kvdbScopeName,
+                                      std::shared_ptr<schemf::ISchema> schema);
+
+}
+
+#endif // _BUILDER_BUILDERS_WINDOWSHELPER_HPP
