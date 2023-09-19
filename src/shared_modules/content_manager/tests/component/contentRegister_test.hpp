92f187214cf2c4cca6e7e51e84cf42ac3643b644
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2023-09-19 15:52:40 -0400

    Add tests to the content_manager module (#18441)
    * CL:
    - Add component tests for Action class
    - Add component tests for ActionOrchestrator class
    - Add component tests for ContentManager class
    - Add component tests for ContentModuleFacade class
    - Add component tests for ContentProvider class
    - Update CMakeLists.txt for component tests
    
    * CL:
    - Fix clang format
    - Remove unnecessary header
    - Add missing header
    
    * CL:
    - Add tests for ContentRegister
    - Update descriptions on Component tests
    - Add new tests for the Action class
    - Add new tests for the ActionOrchestrator class
    - Add new tests for the ContentManager class
    - Add new tests for the ContentModuleFacade class
    - Add new tests for the ContentProvider class
    
    * Update the description of the component tests
    
    * Change name of the function to clear endpoints
    
    * Add documentation
    
    * Add documentation to the component tests
    
    * CL:
    - Add FakeServer
    - Add LCOV exclusions
    - Refactor tests based on the changes for xz content
    - Add missing headers
    
    * Fix clang format
    
    * Add missing headers

---
+++ src/shared_modules/content_manager/tests/component/contentRegister_test.hpp
@@ -0,0 +1,80 @@
+/*
+ * Wazuh content manager - Component Tests
+ * Copyright (C) 2015, Wazuh Inc.
+ * July 26, 2023.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _CONTENT_REGISTER_TEST_HPP
+#define _CONTENT_REGISTER_TEST_HPP
+
+#include "fakeServer.hpp"
+#include "gtest/gtest.h"
+#include <external/nlohmann/json.hpp>
+#include <memory>
+
+/**
+ * @brief Runs component tests for ContentRegister
+ */
+class ContentRegisterTest : public ::testing::Test
+{
+protected:
+    ContentRegisterTest() = default;
+    ~ContentRegisterTest() override = default;
+
+    nlohmann::json m_parameters; ///< Parameters used to create the ContentRegister
+
+    inline static std::unique_ptr<FakeServer> fakeServer; ///< pointer to FakeServer class
+
+    /**
+     * @brief Sets initial conditions for each test case.
+     */
+    // cppcheck-suppress unusedFunction
+    void SetUp() override
+    {
+        m_parameters = R"(
+            {
+                "topicName": "content-register-tests",
+                "interval": 1,
+                "ondemand": false,
+                "configData": {
+                    "contentSource": "api",
+                    "compressionType": "raw",
+                    "versionedContent": "false",
+                    "deleteDownloadedContent": false,
+                    "url": "http://localhost:4444/raw",
+                    "outputFolder": "/tmp/content-register-tests",
+                    "dataFormat": "json",
+                    "contentFileName": "sample.json"
+                }
+            }
+        )"_json;
+    }
+
+    /**
+     * @brief Creates the fakeServer for the runtime of the test suite
+     */
+    // cppcheck-suppress unusedFunction
+    static void SetUpTestSuite()
+    {
+        if (!fakeServer)
+        {
+            fakeServer = std::make_unique<FakeServer>("localhost", 4444);
+        }
+    }
+
+    /**
+     * @brief Resets fakeServer causing the shutdown of the test server.
+     */
+    // cppcheck-suppress unusedFunction
+    static void TearDownTestSuite()
+    {
+        fakeServer.reset();
+    }
+};
+
+#endif //_CONTENT_REGISTER_TEST_HPP
