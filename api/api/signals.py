04cee1caa4cae781a924c9b3070e8532e4c6af00
Nicolas Stefani <nicolas.stefi@wazuh.com>
2023-09-20 17:22:55 -0300

    Add task for query remote API in background

--- api/api/signals.py
+++ api/api/signals.py
@@ -1,8 +1,69 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+import asyncio
+import logging
+import ssl
+import uuid
+
+import aiohttp
+import certifi
+
+from wazuh.core.security import load_spec
+
+logger = logging.getLogger('wazuh-api')
+
+
+def cancel_signal_handler(func):
+    async def wrapper(*args, **kwargs):
+        try:
+            await func(*args, **kwargs)
+        except asyncio.CancelledError:
+            pass
+    return wrapper
+
+
+def get_connector():
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    return aiohttp.TCPConnector(ssl=ssl_context)
+
+
+def get_current_version():
+    spec = load_spec()
+    return spec['info']['version']
+
+
+def get_installation_uid():
+    return str(uuid.uuid4())
 
 
 async def modify_response_headers(request, response):
     # Delete 'Server' entry
     response.headers.pop('Server', None)
+
+
+@cancel_signal_handler
+async def get_update_information(app):
+    # Validate if api is on master node or not
+
+    headers = {
+        'wazuh-uid': get_installation_uid(),
+        'wazuh-tag': get_current_version()
+    }
+
+    async with aiohttp.ClientSession(connector=get_connector()) as session:
+        while True:
+            logger.info('Getting updates information')
+            async with session.get('https://httpbin.org/get', headers=headers) as response:
+                logger.debug("Response status %s", response.status)
+                logger.debug("Response data: %s", await response.json())
+            await asyncio.sleep(60*60*24)
+
+
+async def start_background_tasks(app):
+    app['get_update_information'] = asyncio.create_task(get_update_information(app))
+
+
+async def cleanup_background_tasks(app):
+    app['get_update_information'].cancel()
+    await app['get_update_information']
--- api/scripts/wazuh-apid.py
+++ api/scripts/wazuh-apid.py
@@ -106,6 +106,8 @@ def start():
 
     # Add application signals
     app.app.on_response_prepare.append(modify_response_headers)
+    app.app.on_startup.append(start_background_tasks)
+    app.app.on_cleanup.append(cleanup_background_tasks)
 
     # API configuration logging
     logger.debug(f'Loaded API configuration: {api_conf}')
@@ -247,7 +249,7 @@ if __name__ == '__main__':
     # noinspection PyUnresolvedReferences
     from api.constants import CONFIG_FILE_PATH
     from api.middlewares import security_middleware, response_postprocessing, request_logging, set_secure_headers
-    from api.signals import modify_response_headers
+    from api.signals import modify_response_headers, start_background_tasks, cleanup_background_tasks
     from api.uri_parser import APIUriParser
     from api.util import to_relative_path
     from wazuh.rbac.orm import check_database_integrity
