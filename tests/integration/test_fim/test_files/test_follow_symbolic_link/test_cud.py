24f7af8180ebcff409ab0084cafa4be26fa70b9e
quebim <kevin.ledesma@wazuh.com>
2023-09-20 11:01:06 -0300

    feat(#17881): improve fill directory fixture | clean imports

--- tests/integration/test_fim/test_files/test_follow_symbolic_link/test_cud.py
+++ tests/integration/test_fim/test_files/test_follow_symbolic_link/test_cud.py
@@ -94,9 +94,9 @@ if sys.platform == WINDOWS: local_internal_options.update({AGENTD_WINDOWS_DEBUG:
 
 
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
-def test_change_target(test_configuration, test_metadata, set_wazuh_configuration, truncate_monitored_files,
-                       configure_local_internal_options, symlink_target, symlink, daemons_handler,
-                       start_monitoring):
+def test_cud(test_configuration, test_metadata, set_wazuh_configuration, truncate_monitored_files,
+             configure_local_internal_options, symlink_target, symlink, daemons_handler,
+             start_monitoring):
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
     testfile_name = 'testie.txt'
 
