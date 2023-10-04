5d73f390f7bfa9495792ae276697fe48fdecfa15
thejbte <thejbte@gmail.com>
2023-10-04 19:09:08 +0000

    add test_configuration for invalid connections

---
+++ tests/integration/test_remoted/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from wazuh_testing.utils import services
+
+
+@pytest.fixture
+def restart_wazuh_expect_error() -> None:
+    try:
+        services.control_service('restart')
+    except:
+        pass
+
+    yield
+    
+    services.control_service('stop')
