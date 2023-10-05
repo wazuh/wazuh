daf3cb1a9f27636356dae1fe8a8d09b50c6fccdb
matias-braida <matias.braida@wazuh.com>
2023-10-05 18:13:25 -0300

    DEBUG 10

--- src/data_provider/src/sysInfoMac.mm
+++ src/data_provider/src/sysInfoMac.mm
@@ -22,21 +22,18 @@ void getAppsPathsFromLaunchServices(std::deque<std::string>& apps)
   CFBundleRef bundleRef = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.LaunchServices"));
   if (bundleRef == nullptr)
   {
-    std::cout << "DEBUG. LaunchServices bundle not found" << std::endl;
     throw std::runtime_error("LaunchServices bundle not found");
   }
 
   auto pFunc = (OSStatus(*)(CFArrayRef*)) CFBundleGetFunctionPointerForName(bundleRef, CFSTR("_LSCopyAllApplicationURLs"));
   if (pFunc == nullptr)
   {
-    std::cout << "DEBUG. _LSCopyAllApplicationURLs function not found" << std::endl;
     throw std::runtime_error("_LSCopyAllApplicationURLs function not found");
   }
 
   CFArrayRef appsList = nullptr;
   if (pFunc(&appsList) != noErr || appsList == nullptr)
   {
-    std::cout << "DEBUG. Could not list LaunchServices applications" << std::endl;
     throw std::runtime_error("Could not list LaunchServices applications");
   }
 
@@ -45,7 +42,6 @@ void getAppsPathsFromLaunchServices(std::deque<std::string>& apps)
     if (app != nil && [app isKindOfClass:[NSURL class]])
     {
       apps.push_back(std::string([[app path] UTF8String]));
-      //apps.insert(std::string([[app path] UTF8String]) + "/Contents/Info.plist");
     }
   }
 
