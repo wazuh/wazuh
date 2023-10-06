c1f459dd5d080a38f956c9a997c6337d515404ca
soynof <nof@live.com.ar>
2023-10-06 02:49:43 +0000

    Improve design. Add delete and add commands. Configuratioin file parametrized. Add none interative mode to CLI.

--- src/engine/tools/engine-suite/src/engine_test/cmds/add.py
+++ src/engine/tools/engine-suite/src/engine_test/cmds/add.py
@@ -1,16 +1,22 @@
 from engine_test.crud_integration import CrudIntegration
+from engine_test.command import Command
 
-def run(args):
-    integration = CrudIntegration()
-    integration.save_integration(args['integration-name'], args['format'], args['origin'])
+class AddCommand(Command):
+    def __init__(self):
+        pass
 
-def configure(subparsers):
-    parser_add = subparsers.add_parser("add", help='Add an integration test')
-    parser_add.add_argument('integration-name', type=str, help=f'Integration test name')
+    def run(self, args):
+        super().run(args)
+        integration = CrudIntegration()
+        integration.save_integration(args['integration-name'], args['format'], args['origin'])
 
-    parser_add.add_argument('-f', '--format', help='Format of integration. Example: syslog',
-                            dest='format')
-    parser_add.add_argument('-o', '--origin', help='Origin of integration. Example: /tmp/events.log',
-                            dest='origin')
+    def configure(self, subparsers):
+        parser_add = subparsers.add_parser("add", help='Add integration')
+        parser_add.add_argument('integration-name', type=str, help=f'Integration test name')
 
-    parser_add.set_defaults(func=run)
+        parser_add.add_argument('-f', '--format', help='Format of integration. Example: syslog',
+                                dest='format')
+        parser_add.add_argument('-o', '--origin', help='Origin of integration. Example: /tmp/events.log',
+                                dest='origin')
+
+        parser_add.set_defaults(func=self.run)
