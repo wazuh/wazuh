1d1ef762830d4395c55336f30d8f10c4a1b6ec75
Julian Morales <jmorales@unc.edu.ar>
2023-10-19 21:05:14 +0000

    Create a base tool for evtx converter

---
+++ src/engine/tools/evtx2xml/README.md
@@ -0,0 +1,44 @@
+# evtx2xml
+
+A Python tool to convert Windows Event Log (EVTX) files to XML format.
+
+## Installation
+
+You can install `evtx2xml` via pip:
+
+```bash
+pip install ./evtx2xml
+```
+
+## Usage
+
+Once installed, you can use `evtx2xml` from the command line as follows:
+
+```bash
+evtx2xml <evtx_file_path_or_url> [xml_output_path]
+```
+
+- `<evtx_file_path_or_url>`: The path to the EVTX file or a URL to download the EVTX file.
+- `[xml_output_path]`: (Optional) The path where the XML output will be saved. If omitted, the output will be printed to stdout.
+
+## Dependencies
+
+- `evtx`
+- `requests`
+
+## License
+
+This project is licensed under the AGPLv3 License.
+
+## Contributing
+
+If you would like to contribute to this project, feel free to fork the repo, make your changes, and submit a pull request. All contributions are welcome!
+
+## Contact
+
+If you have any questions or issues, please open an issue on GitHub.
+
+## Links
+
+- [GitHub Repository](https://github.com/wazuh/wazuh)
+
---
+++ src/engine/tools/evtx2xml/evtx2xml/evtx_to_xml.py
@@ -0,0 +1,62 @@
+#! python3
+import os
+import sys
+import re
+import requests
+from evtx import PyEvtxParser # https://github.com/omerbenamram/evtx/
+from urllib.parse import urlparse
+
+def evtx_to_xml(evtx_file_path, xml_output_path=None):
+    parser = PyEvtxParser(evtx_file_path, number_of_threads=0)
+    xml_records = []
+    try:
+        for record in parser.records():
+            cleaned_data = re.sub(r'<\?xml version="1.0" encoding="utf-8"\?>\n?', '', record['data'])
+            xml_records.append(cleaned_data)
+    except RuntimeError as e:
+        print(f'Error: {e}')
+        exit(0)
+
+    final_xml = f'<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n<Events>\n'
+    final_xml += '\n'.join(xml_records)
+    final_xml += '\n</Events>'
+
+    if xml_output_path:
+        with open(xml_output_path, "w") as xml_output:
+            xml_output.write(final_xml)
+    else:
+        print(final_xml)
+
+
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python evtx_to_xml.py <evtx_file_path_or_url> [xml_output_path]")
+        sys.exit(1)
+
+    evtx_file_path_or_url = sys.argv[1]
+    xml_output_path = sys.argv[2] if len(sys.argv) == 3 else None
+
+    # Check if the evtx_file_path_or_url is a URL
+    parsed_url = urlparse(evtx_file_path_or_url)
+    isUrl = parsed_url.scheme in ['http', 'https']
+    if isUrl:
+        evtx_file_path_or_url = download_file(evtx_file_path_or_url, 'tmp_downloaded.evtx')
+
+    evtx_to_xml(evtx_file_path_or_url, xml_output_path)
+
+    # Delete the downloaded file
+    if isUrl:
+        os.remove(evtx_file_path_or_url)
+
+
+if __name__ == "__main__":
+    main()
