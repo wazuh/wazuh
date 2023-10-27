0fed15a177a0ac705f8a5f24754ce3a27d5ef00d
soynof <nof@live.com.ar>
2023-10-27 02:59:26 +0000

    Add mock in api-kvdb UT.

--- src/engine/source/kvdb/test/mocks/kvdb/mockKvdbHandlerCollection.hpp
+++ src/engine/source/kvdb/test/mocks/kvdb/mockKvdbHandlerCollection.hpp
@@ -10,22 +10,9 @@ namespace kvdb::mocks
 {
 
 /******************************************************************************/
-// Helper functions to mock method responses
-/******************************************************************************/
-inline base::OptError kvdbError()
-{
-    return base::Error {"Mocked kvdb error"};
-}
-
-inline base::OptError kvdbOk()
-{
-    return std::nullopt;
-}
-
-/******************************************************************************/
 // Mock classes
 /******************************************************************************/
-class MockKVDBHandlerCollection : public kvdb::IKVDBHandlerCollection
+class MockKVDBHandlerCollection : public kvdbManager::IKVDBHandlerCollection
 {
 public:
     MOCK_METHOD((void), addKVDBHandler, (const std::string& dbName, const std::string& scopeName), (override));
