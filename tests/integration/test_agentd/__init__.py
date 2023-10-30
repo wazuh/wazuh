18cd5f2dc00847aae9ade908164a18e22f57bb52
Manuel <manuel.cano@wazuh.com>
2023-10-30 13:02:25 +0100

    Improved helper functions

--- tests/integration/test_agentd/__init__.py
+++ tests/integration/test_agentd/__init__.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 
 from wazuh_testing.constants.paths.configurations import WAZUH_CLIENT_KEYS_PATH
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
@@ -12,13 +13,33 @@ def add_custom_key() -> None:
     with open(WAZUH_CLIENT_KEYS_PATH, 'w+') as client_keys:
         client_keys.write("100 ubuntu-agent any TopSecret")
 
+def kill_server(server):
+    if server:
+        server.clear()
+        server.shutdown()
+
+def parse_time_from_log_line(log_line):
+    """Create a datetime object from a date in a string.
+
+    Args:
+        log_line (str): String with date.
+
+    Returns:
+        datetime: datetime object with the parsed time.
+    """
+    data = log_line.split(" ")
+    (year, month, day) = data[0].split("/")
+    (hour, minute, second) = data[1].split(":")
+    log_time = datetime(year=int(year), month=int(month), day=int(day), hour=int(hour), minute=int(minute),
+                        second=int(second))
+    return log_time
+
 def wait_keepalive():
     """
         Watch ossec.log until "Sending keep alive" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_SENDING_KEEP_ALIVE))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_SENDING_KEEP_ALIVE))
     assert (wazuh_log_monitor.callback_result != None), f'Sending keep alive not found'
 
 def wait_connect():
@@ -26,17 +47,15 @@ def wait_connect():
         Watch ossec.log until received "Connected to the server" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_CONNECTED_TO_SERVER))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_CONNECTED_TO_SERVER))
     assert (wazuh_log_monitor.callback_result != None), f'Connected to the server message not found'
 
 def wait_ack():
     """
-        Watch ossec.log until received ack message is found
+        Watch ossec.log until "Received ack message" is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_RECEIVED_ACK))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_RECEIVED_ACK))
     assert (wazuh_log_monitor.callback_result != None), f'Received ack message not found'
 
 def wait_state_update():
@@ -44,8 +63,7 @@ def wait_state_update():
         Watch ossec.log until "Updating state file" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_UPDATING_STATE_FILE))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_UPDATING_STATE_FILE))
     assert (wazuh_log_monitor.callback_result != None), f'State file update not found'
 
 def wait_enrollment():
@@ -53,8 +71,7 @@ def wait_enrollment():
         Watch ossec.log until "Valid key received" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_RECEIVED_VALID_KEY))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_RECEIVED_VALID_KEY))
     assert (wazuh_log_monitor.callback_result != None), 'Agent never enrolled'
 
 def wait_enrollment_try():
@@ -62,8 +79,7 @@ def wait_enrollment_try():
         Watch ossec.log until "Requesting a key" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_REQUESTING_KEY), timeout = 50)
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_REQUESTING_KEY), timeout = 50)
     assert (wazuh_log_monitor.callback_result != None), f'Enrollment retry was not sent'
 
 def wait_agent_notification(current_value):
@@ -71,8 +87,7 @@ def wait_agent_notification(current_value):
         Watch ossec.log until "Sending agent notification" message is found current_value times
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_SENDING_AGENT_NOTIFICATION), accumulations = int(current_value))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_SENDING_AGENT_NOTIFICATION), accumulations = int(current_value))
     assert (wazuh_log_monitor.callback_result != None), f'Sending agent notification message not found'
 
 def wait_server_rollback():
@@ -80,8 +95,7 @@ def wait_server_rollback():
         Watch ossec.log until "Unable to connect to any server" message is found'
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_UNABLE_TO_CONNECT))
-    truncate_wazuh_logs()
+    wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_UNABLE_TO_CONNECT))
     assert (wazuh_log_monitor.callback_result != None), f'Unable to connect to any server message not found'
 
 def delete_keys_file():
@@ -95,30 +109,13 @@ def check_module_stop():
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
     wazuh_log_monitor.start(callback=callbacks.generate_callback(AGENTD_MODULE_STOPPED))
-    truncate_wazuh_logs()
     assert (wazuh_log_monitor.callback_result == None), f'Unable to access queue message found'
 
-def check_connection_try(interval, log_timeout):
+def check_connection_try():
     """
         Watch ossec.log until "Trying to connect to server" message is found
     """
     wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
-    matched_line = wazuh_log_monitor.start(timeout = interval + log_timeout, callback=callbacks.generate_callback(AGENTD_TRYING_CONNECT), return_matched_line = True)
-    assert (wazuh_log_monitor.callback_result != None), f'Connected to the server message not found'
-    truncate_wazuh_logs()
+    matched_line = wazuh_log_monitor.start(only_new_events = True, callback=callbacks.generate_callback(AGENTD_TRYING_CONNECT), return_matched_line = True)
+    assert (wazuh_log_monitor.callback_result != None), f'Trying to connect to server message not found'
     return matched_line
-
-def truncate_wazuh_logs():
-    """
-    Truncate a file to reset its content.
-
-    Args:
-        file_path (str): Path of the file to be truncated.
-    """
-    with open(WAZUH_LOG_PATH, 'w'):
-        pass
-
-def kill_server(server):
-    if server:
-        server.clear()
-        server.shutdown()
