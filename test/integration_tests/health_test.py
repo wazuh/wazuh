6407dde60a5a6387d73e740bd6d0843dd17edafe
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-10-31 03:32:20 +0000

    Modify input and expected files to windows integrations

--- test/integration_tests/health_test.py
+++ test/integration_tests/health_test.py
@@ -5,8 +5,7 @@ import os
 
 def run_command(command):
     try:
-        result = subprocess.run(
-            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         return result
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}")
@@ -16,19 +15,50 @@ def check_or_create_session(github_working_dir, session_name):
     session_get = f"cd {github_working_dir}/src/engine/build && ./main test --api_socket {github_working_dir}/environment/queue/sockets/engine-api session get {session_name}"
     result = run_command(session_get)
     print(result)
+
     if result.returncode != 0:
         print(f"Session '{session_name}' does not exist. Creating it...")
-        session_post = f"cd {github_working_dir}/src/engine/build && ./main test --api_socket {github_working_dir}/environment/queue/sockets/engine-api session create {session_name}"
+        session_post = f"cd {github_working_dir}/src/engine/build && ./main test --client_timeout 100000 --api_socket {github_working_dir}/environment/queue/sockets/engine-api session create {session_name}"
         result = run_command(session_post)
         print(result)
+
         if result.returncode != 0:
             sys.exit(1)
         print(f"Session '{session_name}' created successfully.")
     else:
         print(f"Session '{session_name}' already exists.")
 
-def process_integration_tests(github_working_dir, allowed_integrations, session_name):
+def execute_integration_test(github_working_dir, os_path, input_file_path):
     main_command = "cat"
+    # Ejecutar el comando y obtener la salida
+    output = subprocess.check_output(f"{main_command} {input_file_path} | engine-test run {os.path.basename(os_path)} --api-socket {github_working_dir}/environment/queue/sockets/engine-api -n wazuh system -j", shell=True, stderr=subprocess.STDOUT)
+
+    # Dividir la salida en cadenas JSON individuales
+    output_str = output.decode('utf-8')
+    json_strings = output_str.strip().split('\n')
+
+    parsed_results = []
+
+    for json_string in json_strings:
+        try:
+            parsed_json = json.loads(json_string)
+            parsed_results.append(parsed_json)
+        except json.JSONDecodeError as e:
+            print(f"Error al parsear JSON: {e}")
+
+    return parsed_results
+
+def compare_results(parsed_results, expected_json, input_file_name):
+    for event_json in parsed_results:
+        if 'TestSessionID' in event_json:
+            del event_json['TestSessionID']
+        try:
+            if event_json not in expected_json:
+                print(f"JSON events in '{input_file_name}' do not match the 'expected.json' file.")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON event: {e}")
+
+def process_integration_tests(github_working_dir, allowed_integrations, session_name):
     integrations_directory = os.path.join(github_working_dir, "src/engine/ruleset/integrations")
 
     for os_dir in os.listdir(integrations_directory):
@@ -39,30 +69,18 @@ def process_integration_tests(github_working_dir, allowed_integrations, session_
 
             if os.path.isdir(test_directory):
                 for input_file in os.listdir(test_directory):
-                    if input_file.endswith("input.txt"):
+                    if input_file.endswith("input.xml"):
                         input_file_path = os.path.join(test_directory, input_file)
-                        expected_file = input_file.replace("input.txt", "expected.json")
+                        expected_file = input_file.replace("input.xml", "expected.json")
                         expected_file_path = os.path.join(test_directory, expected_file)
 
                         if os.path.isfile(expected_file_path):
-                            output = subprocess.check_output(f"{main_command} {input_file_path} | engine-test run {os.path.basename(os_path)} --api-socket {github_working_dir}/environment/queue/sockets/engine-api -n wazuh system -j", shell=True, stderr=subprocess.STDOUT)
-                            output = output.decode('utf-8').strip().split("\n")
-                            first_brace_position = output.find('{')
-                            if first_brace_position != -1:
-                                output = output[first_brace_position:]
-
-                            event_json = output.decode('utf-8').strip().split("\n")
-
+                            expected_json = {}
                             with open(expected_file_path, 'r') as file:
                                 expected_json = json.load(file)
 
-                            for event_json in json_events:
-                                try:
-                                    event = json.loads(event_json)
-                                    if event not in expected_json:
-                                        print("JSON events do not match the 'expected.json' file.")
-                                except json.JSONDecodeError as e:
-                                    print(f"Error decoding JSON event: {e}")
+                            parsed_results = execute_integration_test(github_working_dir, os_path, input_file_path)
+                            compare_results(parsed_results, expected_json, input_file)
                         else:
                             print(f"Expected file 'expected.json' corresponding to '{input_file}' in '{test_directory}' was not found.")
 
@@ -71,10 +89,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     github_working_dir = sys.argv[1]
-    allowed_integrations = ["windows", "syslog", "suricata", "apache-http", "system"]
+    allowed_integrations = ["windows"]
     session_name = "HealthTest"
 
-    #check_or_create_session(github_working_dir, session_name)
+    check_or_create_session(github_working_dir, session_name)
     process_integration_tests(github_working_dir, allowed_integrations, session_name)
 
 print("Process completed.")
