6d3b51ba760fb8f1ae6fd87e0edd593e6909628c
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2023-11-01 14:34:41 +0000

    Add priority queues and patch to not use catalog

--- source/cmds/src/start.cpp
+++ source/cmds/src/start.cpp
@@ -31,6 +31,7 @@
 #include <logpar/registerParsers.hpp>
 #include <metrics/metricsManager.hpp>
 #include <parseEvent.hpp> // Event
+#include <queue/concurrentQueue.hpp> // Queue
 #include <rbac/rbac.hpp>
 #include <router/router.hpp>
 #include <schemf/schema.hpp>
@@ -193,6 +194,7 @@ void runStart(ConfHandler confManager)
     std::shared_ptr<api::Api> api;
     std::shared_ptr<engineserver::EngineServer> server;
     std::shared_ptr<store::Store> store;
+    std::shared_ptr<builder::internals::Registry<builder::internals::Builder>> registry;
     std::shared_ptr<builder::Builder> builder;
     std::shared_ptr<api::catalog::Catalog> catalog;
     std::shared_ptr<router::Router> router;
@@ -286,29 +288,23 @@ void runStart(ConfHandler confManager)
 
         // Builder and registry
         {
-            // auto registry = std::make_shared<builder::internals::Registry<builder::internals::Builder>>();
-            // builder::internals::dependencies deps;
-            // deps.logparDebugLvl = 0;
-            // deps.logpar = logpar;
-            // deps.helperRegistry =
-            // std::make_shared<builder::internals::Registry<builder::internals::HelperBuilder>>(); deps.schema =
-            // schema; deps.forceFieldNaming = false; deps.sockFactory =
-            // std::make_shared<sockiface::UnixSocketFactory>(); deps.wdbManager =
-            //     std::make_shared<wazuhdb::WDBManager>(std::string(wazuhdb::WDB_SOCK_PATH), deps.sockFactory);
-            // builder::internals::registerHelperBuilders(deps.helperRegistry, deps);
-            // builder::internals::registerBuilders(registry, deps);
-            // LOG_DEBUG("Builders registered.");
-            builder::BuilderDeps builderDeps;
-            builderDeps.logparDebugLvl = 0;
-            builderDeps.logpar = logpar;
-            builderDeps.kvdbScopeName = "builder";
-            builderDeps.kvdbManager = kvdbManager;
-            builderDeps.sockFactory = std::make_shared<sockiface::UnixSocketFactory>();
-            builderDeps.wdbManager =
-                std::make_shared<wazuhdb::WDBManager>(std::string(wazuhdb::WDB_SOCK_PATH), builderDeps.sockFactory);
-            auto defs = std::make_shared<defs::DefinitionsBuilder>();
-            auto schemaValidator = std::make_shared<schemval::Validator>(schema);
-            builder = std::make_shared<builder::Builder>(store, schema, defs, schemaValidator, builderDeps);
+            registry = std::make_shared<builder::internals::Registry<builder::internals::Builder>>();
+            builder::internals::dependencies deps;
+            deps.logparDebugLvl = 0;
+            deps.logpar = logpar;
+            deps.kvdbScopeName = "builder";
+            deps.kvdbManager = kvdbManager;
+            deps.helperRegistry = std::make_shared<builder::internals::Registry<builder::internals::HelperBuilder>>();
+            deps.schema = schema;
+            deps.forceFieldNaming = false;
+            deps.sockFactory = std::make_shared<sockiface::UnixSocketFactory>();
+            deps.wdbManager =
+                std::make_shared<wazuhdb::WDBManager>(std::string(wazuhdb::WDB_SOCK_PATH), deps.sockFactory);
+            builder::internals::registerHelperBuilders(deps.helperRegistry, deps);
+            builder::internals::registerBuilders(registry, deps);
+            LOG_DEBUG("Builders registered.");
+
+            builder = std::make_shared<builder::Builder>(store, registry);
             LOG_INFO("Builder initialized.");
         }
 
@@ -332,12 +328,12 @@ void runStart(ConfHandler confManager)
             // If the router table is empty or the force flag is passed, load from the command line
             if (router->getRouteTable().empty())
             {
-                router->addRoute(routeName, routePriority, routeFilter, routePolicy);
+                router->addRoute(routeName, routePriority, std::make_pair(routeFilter, std::nullopt), routePolicy);
             }
             else if (forceRouterArg)
             {
                 router->clear();
-                router->addRoute(routeName, routePriority, routeFilter, routePolicy);
+                router->addRoute(routeName, routePriority, std::make_pair(routeFilter, std::nullopt), routePolicy);
             }
         }
 
@@ -346,16 +342,16 @@ void runStart(ConfHandler confManager)
             sessionManager = std::make_shared<api::sessionManager::SessionManager>();
 
             // Try to load the sessions from the store
-            const auto strJsonSessions = store->readInternalDoc(api::test::handlers::API_SESSIONS_TABLE_NAME);
+            const auto strJsonSessions = store->readInternalDoc(router::API_SESSIONS_TABLE_NAME);
             if (std::holds_alternative<base::Error>(strJsonSessions))
             {
                 LOG_WARNING("Could not retreive configuration file [{}] needed by the 'Test' module: {}",
-                            api::test::handlers::API_SESSIONS_TABLE_NAME,
+                            router::API_SESSIONS_TABLE_NAME,
                             std::get<base::Error>(strJsonSessions).message);
 
                 // Create the sessions table
                 const auto storeSetSessionsTable =
-                    store->createInternalDoc(api::test::handlers::API_SESSIONS_TABLE_NAME, json::Json("[]"));
+                    store->createInternalDoc(router::API_SESSIONS_TABLE_NAME, json::Json("[]"));
                 if (storeSetSessionsTable.has_value())
                 {
                     LOG_ERROR("API sessions table could not be created: {}", storeSetSessionsTable.value().message);
@@ -366,7 +362,7 @@ void runStart(ConfHandler confManager)
             else
             {
                 const auto loadError = api::test::handlers::loadSessionsFromJson(
-                    sessionManager, catalog, router, std::get<json::Json>(strJsonSessions));
+                    sessionManager, router, std::get<json::Json>(strJsonSessions));
                 if (loadError.has_value())
                 {
                     LOG_ERROR("API sessions loading could not be completed: {}", loadError.value().message);
@@ -412,7 +408,7 @@ void runStart(ConfHandler confManager)
 
             // Test
             {
-                api::test::handlers::Config testConfig {sessionManager, router, catalog, store, policyManager};
+                api::test::handlers::Config testConfig {sessionManager, router, registry, store, policyManager};
                 api::test::handlers::registerHandlers(testConfig, api);
                 LOG_DEBUG("Test API registered.");
             }
---
+++ source/queue/test/mocks/queue/mockQueue.hpp
@@ -0,0 +1,19 @@
+#ifndef _MOCK_QUEUE_HPP
+#define _MOCK_QUEUE_HPP
+
+#include <gmock/gmock.h>
+#include <queue/iBlockingConcurrentQueue.hpp>
+
+using namespace base::queue;
+
+template <typename T>
+class MockQueue : public iBlockingConcurrentQueue<T>
+{
+public:
+    MOCK_METHOD((void), push, (T&& element, bool priority), (override));
+    MOCK_METHOD((void), waitPop, (T& element, bool priority), (override));
+    MOCK_METHOD((bool), empty, (bool priority), (const, override));
+    MOCK_METHOD((size_t), size, (bool priority), (const, override));
+};
+
+#endif // _MOCK_QUEUE_HPP
