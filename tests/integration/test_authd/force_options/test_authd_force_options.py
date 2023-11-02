480bf527b0025d775a8ba842853141695ee85fa4
Mateo Cervilla <mateo.cervilla@wazuh.com>
2023-11-02 15:24:50 -0300

    Migrate authd_force_options

--- tests/integration/test_authd/force_options/test_authd_force_options.py
+++ tests/integration/test_authd/force_options/test_authd_force_options.py
@@ -40,61 +40,87 @@ tags:
     - enrollment
     - authd
 '''
-import os
 import time
 import pytest
+import re
+from pathlib import Path
 
-from wazuh_testing.tools.configuration import load_wazuh_configurations
-from wazuh_testing.tools.file import read_yaml
-from wazuh_testing.authd import create_authd_request, validate_authd_response, validate_authd_logs, \
-                                AUTHD_KEY_REQUEST_TIMEOUT
+from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
+from wazuh_testing.utils.configuration import load_configuration_template, get_test_cases_data
+from wazuh_testing.modules.authd.utils import create_authd_request, validate_authd_response
+from wazuh_testing.tools.monitors.file_monitor import FileMonitor
+from wazuh_testing.utils import callbacks
+from wazuh_testing.modules.authd import PREFIX
 
+from . import CONFIGURATIONS_FOLDER_PATH, TEST_CASES_FOLDER_PATH
 
-# Data paths
-data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(data_path, 'template_configuration.yaml')
-tests_path = os.path.join(data_path, 'test_cases', 'valid_config')
+AUTHD_KEY_REQUEST_TIMEOUT = 10
 
-# Configurations
-configurations = load_wazuh_configurations(configurations_path, __name__)
-local_internal_options = {'authd.debug': '2'}
+# Configurations 1
+test_paths_t1 = [Path(TEST_CASES_FOLDER_PATH, 'cases_after_registration_time.yaml'),
+                 Path(TEST_CASES_FOLDER_PATH, 'cases_disconnected_time.yaml'),
+                 Path(TEST_CASES_FOLDER_PATH, 'cases_key_mismatch.yaml'),
+                 Path(TEST_CASES_FOLDER_PATH, 'cases_force_options.yaml') ]
 
-# Tests
-tests = []
-test_case_ids = []
-for file in os.listdir(tests_path):
-    group_name = file.split('.')[0]
-    file_tests = read_yaml(os.path.join(tests_path, file))
-    tests = tests + file_tests
-    test_case_ids = test_case_ids + [f"{group_name} {test_case['name']}" for test_case in file_tests]
+test_configuration_t1 = []
+test_metadata_t1 = []
+test_cases_ids_t1 = []
+
+for test_path in test_paths_t1:
+    test_configuration_tmp, test_metadata_tmp, test_cases_ids_tmp = get_test_cases_data(test_path)
+    test_configuration_t1.extend(test_configuration_tmp)
+    test_metadata_t1.extend(test_metadata_tmp)
+    test_cases_ids_t1.extend(test_cases_ids_tmp)
+
+test_configuration_path_t1 = Path(CONFIGURATIONS_FOLDER_PATH, 'config_authd_force_options.yaml')
+test_configuration_t1 = load_configuration_template(test_configuration_path_t1, test_configuration_t1, test_metadata_t1)
+
+# Configurations 2
+test_configuration_path_t2 = Path(CONFIGURATIONS_FOLDER_PATH, 'config_force_insert.yaml')
+test_cases_path_t2 = Path(TEST_CASES_FOLDER_PATH, 'cases_force_insert.yaml')
+test_configuration_t2, test_metadata_t2, test_cases_ids_t2 = get_test_cases_data(test_cases_path_t2)
+test_configuration_t2 = load_configuration_template(test_configuration_path_t2, test_configuration_t2, test_metadata_t2)
+
+# Configurations 3
+test_configuration_path_t3 = Path(CONFIGURATIONS_FOLDER_PATH, 'config_force_insert_only.yaml')
+test_cases_path_t3 = Path(TEST_CASES_FOLDER_PATH, 'cases_force_insert_only.yaml')
+test_configuration_t3, test_metadata_t3, test_cases_ids_t3 = get_test_cases_data(test_cases_path_t3)
+test_configuration_t3 = load_configuration_template(test_configuration_path_t3, test_configuration_t3, test_metadata_t3)
 
 # Variables
-log_monitor_paths = []
-
+local_internal_options = {'authd.debug': '2'}
 receiver_sockets_params = [(('localhost', 1515), 'AF_INET', 'SSL_TLSv1_2')]
 monitored_sockets_params = [('wazuh-authd', None, True), ('wazuh-db', None, True)]
-receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
+receiver_sockets, monitored_sockets = None, None  # Set in the fixtures
+
+# Functions
+def check_options(test_metadata):
+    authd_sock = receiver_sockets[0]
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    for stage in test_metadata['test_case']:
+        # Reopen socket (socket is closed by manager after sending message with client key)
+        authd_sock.open()
+        authd_sock.send(create_authd_request(stage['input']), size=False)
+        timeout = time.time() + AUTHD_KEY_REQUEST_TIMEOUT
+        response = ''
+        while response == '':
+            response = authd_sock.receive().decode()
+            if time.time() > timeout:
+                raise ConnectionResetError('Manager did not respond to sent message!')
+        result, err_msg = validate_authd_response(response, stage['output'])
+        assert result == 'success', f"Failed stage '{stage['description']}': {err_msg} Complete response: '{response}'"
+
+        for log in stage['log']:
+            log = re.escape(log)
+            wazuh_log_monitor.start(callback=callbacks.generate_callback(fr'{PREFIX}{log}'), timeout=10, encoding='utf-8')
+            assert wazuh_log_monitor.callback_result, f'Error event not detected'
 
 
-# Fixtures
-@pytest.fixture(scope='module')
-def get_configuration(request):
-    """
-    Get configurations from the module
-    """
-    return request.param
 
-
-@pytest.fixture(scope='function', params=tests, ids=test_case_ids)
-def get_current_test_case(request):
-    """
-    Get current test case from the module
-    """
-    return request.param
-
-
-def test_authd_force_options(get_current_test_case, configure_local_internal_options_module, override_authd_force_conf,
-                             insert_pre_existent_agents, file_monitoring, restart_authd_function,
+# Tests
+@pytest.mark.parametrize('test_configuration,test_metadata', zip(test_configuration_t1, test_metadata_t1), ids=test_cases_ids_t1)
+def test_authd_force_options(test_configuration, test_metadata, set_wazuh_configuration,
+                             configure_local_internal_options, insert_pre_existent_agents, restart_authd_function,
                              wait_for_authd_startup_function, connect_to_sockets_function, tear_down):
     '''
     description:
@@ -146,19 +172,134 @@ def test_authd_force_options(get_current_test_case, configure_local_internal_opt
         - Registration request responses on Authd socket.
     '''
 
-    authd_sock = receiver_sockets[0]
-    validate_authd_logs(get_current_test_case.get('log', []))
+    check_options(test_metadata)
 
-    for stage in get_current_test_case['test_case']:
-        # Reopen socket (socket is closed by manager after sending message with client key)
-        authd_sock.open()
-        authd_sock.send(create_authd_request(stage['input']), size=False)
-        timeout = time.time() + AUTHD_KEY_REQUEST_TIMEOUT
-        response = ''
-        while response == '':
-            response = authd_sock.receive().decode()
-            if time.time() > timeout:
-                raise ConnectionResetError('Manager did not respond to sent message!')
-        result, err_msg = validate_authd_response(response, stage['output'])
-        assert result == 'success', f"Failed stage '{stage['description']}': {err_msg} Complete response: '{response}'"
-        validate_authd_logs(stage.get('log', []))
+
+
+@pytest.mark.parametrize('test_configuration,test_metadata', zip(test_configuration_t2, test_metadata_t2), ids=test_cases_ids_t2)
+def test_authd_force_insert(test_configuration, test_metadata, set_wazuh_configuration,
+                             configure_local_internal_options, insert_pre_existent_agents, restart_authd_function,
+                             wait_for_authd_startup_function, connect_to_sockets_function, tear_down):
+    '''
+    description:
+        Checks that every input message in authd port generates the adequate output.
+
+    wazuh_min_version:
+        4.3.0
+
+    tier: 0
+
+    parameters:
+        - get_current_test_case:
+            type: fixture
+            brief: gets the current test case from the tests' list
+        - configure_local_internal_options_module:
+            type: fixture
+            brief: Configure the local internal options file.
+        - override_authd_force_conf:
+            type: fixture
+            brief: Modified the authd configuration options.
+        - insert_pre_existent_agents:
+            type: fixture
+            brief: adds the required agents to the client.keys and global.db
+        - file_monitoring:
+            type: fixture
+            brief: Handle the monitoring of a specified file.
+        - restart_authd_function:
+            type: fixture
+            brief: stops the wazuh-authd daemon.
+        - wait_for_authd_startup_function:
+            type: fixture
+            brief: Waits until Authd is accepting connections.
+        - connect_to_sockets_function:
+            type: fixture
+            brief: Bind to the configured sockets at function scope.
+        - tear_down:
+            type: fixture
+            brief: Roll back the daemon and client.keys state after the test ends.
+
+    assertions:
+        - The received output must match with expected.
+        - Verifies the registration responses.
+
+    input_description:
+        Different test cases are contained in external YAML files (valid_config folder) which includes
+        different possible values for the current authd settings.
+
+    expected_output:
+        - Registration request responses on Authd socket.
+    '''
+
+
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    for log in test_metadata['log']:
+        log = re.escape(log)
+        wazuh_log_monitor.start(callback=callbacks.generate_callback(fr'{PREFIX}{log}'), timeout=10, encoding='utf-8')
+        assert wazuh_log_monitor.callback_result, f'Error event not detected'
+
+    check_options(test_metadata)
+
+
+
+@pytest.mark.parametrize('test_configuration,test_metadata', zip(test_configuration_t3, test_metadata_t3), ids=test_cases_ids_t3)
+def test_authd_force_insert_only(test_configuration, test_metadata, set_wazuh_configuration,
+                             configure_local_internal_options, insert_pre_existent_agents, restart_authd_function,
+                             wait_for_authd_startup_function, connect_to_sockets_function, tear_down):
+    '''
+    description:
+        Checks that every input message in authd port generates the adequate output.
+
+    wazuh_min_version:
+        4.3.0
+
+    tier: 0
+
+    parameters:
+        - get_current_test_case:
+            type: fixture
+            brief: gets the current test case from the tests' list
+        - configure_local_internal_options_module:
+            type: fixture
+            brief: Configure the local internal options file.
+        - override_authd_force_conf:
+            type: fixture
+            brief: Modified the authd configuration options.
+        - insert_pre_existent_agents:
+            type: fixture
+            brief: adds the required agents to the client.keys and global.db
+        - file_monitoring:
+            type: fixture
+            brief: Handle the monitoring of a specified file.
+        - restart_authd_function:
+            type: fixture
+            brief: stops the wazuh-authd daemon.
+        - wait_for_authd_startup_function:
+            type: fixture
+            brief: Waits until Authd is accepting connections.
+        - connect_to_sockets_function:
+            type: fixture
+            brief: Bind to the configured sockets at function scope.
+        - tear_down:
+            type: fixture
+            brief: Roll back the daemon and client.keys state after the test ends.
+
+    assertions:
+        - The received output must match with expected.
+        - Verifies the registration responses.
+
+    input_description:
+        Different test cases are contained in external YAML files (valid_config folder) which includes
+        different possible values for the current authd settings.
+
+    expected_output:
+        - Registration request responses on Authd socket.
+    '''
+
+
+    wazuh_log_monitor = FileMonitor(WAZUH_LOG_PATH)
+    for log in test_metadata['log']:
+        log = re.escape(log)
+        wazuh_log_monitor.start(callback=callbacks.generate_callback(fr'{PREFIX}{log}'), timeout=10, encoding='utf-8')
+        assert wazuh_log_monitor.callback_result, f'Error event not detected'
+
+    check_options(test_metadata)
