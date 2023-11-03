451a6d432dd791e06bf1ad92e24cf0db3a63e2d2
thejbte <thejbte@gmail.com>
2023-11-03 13:48:22 +0000

    fixes minor syntax changes

--- tests/integration/test_remoted/test_configurations/test_syslog_valid_allowed_denied_ips_multiple.py
+++ tests/integration/test_remoted/test_configurations/test_syslog_valid_allowed_denied_ips_multiple.py
@@ -1,3 +1,9 @@
+"""
+ Copyright (C) 2015-2021, Wazuh Inc.
+ Created by Wazuh, Inc. <info@wazuh.com>.
+ This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+"""
+
 import pytest
 
 from pathlib import Path
