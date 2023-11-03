dc3b8610d0e158df1d6c0df1dd957ac73354c2cb
Julian Morales <jmorales@unc.edu.ar>
2023-11-03 12:21:01 +0000

    Add environment base for new router

--- src/engine/source/router2/interface/router/irouter.hpp
+++ src/engine/source/router2/interface/router/irouter.hpp
@@ -16,8 +16,12 @@ public:
     // An environment is a session
     virtual base::OptError postEnvironment(const EntryPost& environment) = 0;
     virtual base::OptError patchEnvironment(const EntryPost& environment) = 0;
-    virtual base::OptError deleteEnvironment(const env::Id& name) = 0;              // Name or id
-    virtual base::RespOrError<Entry> getEnvironment(const env::Id& name) const = 0; // Name or id
+
+    // postTestEnvironment
+    // patchTestEnvironment
+
+    virtual base::OptError deleteEnvironment(const std::string& name) = 0;              // Name or id
+    virtual base::RespOrError<Entry> getEnvironment(const std::string& name) const = 0; // Name or id
 
     // Table entries
     virtual base::RespOrError<std::list<Entry>> getEntries() const = 0;
