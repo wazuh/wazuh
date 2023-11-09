30ac806a8a89e095368282c59c06d7b3cb03b689
Julian Morales <jmorales@unc.edu.ar>
2023-11-09 19:55:35 +0000

    Templetizate controller builder

--- src/engine/source/bk/src/rx/controller.cpp
+++ src/engine/source/bk/src/rx/controller.cpp
@@ -9,16 +9,21 @@ class Controller::TracerImpl final : public detail::Tracer
 {
 };
 
-Controller::Controller(base::Expression expression,
+
+void Controller::build(base::Expression expression,
                        std::unordered_set<std::string> traceables,
                        std::function<void()> endCallback)
-    : m_traceables {traceables}
-    , m_expression {expression}
-    , m_policyInput {m_policySubject.get_subscriber()}
 {
+    if (m_isBuilt)
+    {
+        throw std::runtime_error {"The backend is already built"};
+    }
+    m_traceables = std::move(traceables);
+    m_expression = std::move(expression);
+
     detail::ExprBuilder builder;
     std::unordered_map<std::string, std::shared_ptr<detail::Tracer>> traces;
-    m_policyOutput = builder.build(expression, traces, m_traceables, m_policySubject.get_observable());
+    m_policyOutput = builder.build(m_expression, traces, m_traceables, m_policySubject.get_observable());
     for (auto& [name, trace] : traces)
     {
         m_traces.emplace(name, std::static_pointer_cast<TracerImpl>(trace));
@@ -31,7 +36,9 @@ Controller::Controller(base::Expression expression,
     {
         m_policyOutput.subscribe();
     }
+    m_isBuilt = true;
 }
+
 
 base::RespOrError<Subscription> Controller::subscribe(const std::string& traceable, const Subscriber& subscriber)
 {
