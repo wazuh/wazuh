d6083d97f9ab4bd3bbdb2cda7cf8185d757c2bd4
Julian Morales <jmorales@unc.edu.ar>
2023-11-10 14:05:07 +0000

    Adapt the buildEnvironment

--- src/engine/source/router2/src/buildEnvironment.hpp
+++ src/engine/source/router2/src/buildEnvironment.hpp
@@ -12,12 +12,13 @@ constexpr auto JSON_PATH_SESSION_FILTER {"/TestSessionID"};
 /**
  * @brief BuildEnvironment class for creating environments based on policies and filters.
  *
+ * @typedef T The type of the controller.
  */
-template <typename T, typename = std::enable_if_t<std::is_base_of<bk::IController, T>::value>>
+template<typename T, typename = std::enable_if_t<std::is_base_of<bk::IController, T>::value>>
 class BuildEnvironment
 {
 private:
-    std::shared_ptr<IBuilder> m_builder;
+    std::shared_ptr<IBuilder> m_builder; ///< The builder used to construct the policy and filter.
 
     /**
      * @brief Get the Controller object for a given policy.
@@ -25,9 +26,14 @@ private:
      * @param policyName The name of the policy.
      * @param builder The builder used to construct the policy.
      * @return std::shared_ptr<bk::IController> The constructed controller.
+     * @throws std::runtime_error if the policy has no assets or if the backend cannot be built.
      */
     std::shared_ptr<T> getController(base::Name& policyName)
     {
+        if (policyName.parts().size() == 0 || policyName.parts()[0] != "policy")
+        {
+            throw std::runtime_error {"The asset name is empty or it is not a policy"};
+        }
         // Build the policy and create the pipeline
         auto newPolicy = m_builder->buildPolicy(policyName);
         if (base::isError(newPolicy))
@@ -48,7 +54,7 @@ private:
                        std::inserter(assetNames, assetNames.begin()),
                        [](const auto& name) { return name.toStr(); });
 
-        auto controller =  std::make_shared<T>();
+        auto controller = std::make_shared<T>();
         controller->build(policy->expression(), assetNames);
         return controller;
     }
@@ -58,6 +64,7 @@ private:
      *
      * @param filterName The name of the filter.
      * @return base::Expression The constructed filter expression.
+     * @throws std::runtime_error if the filter cannot be built.
      */
     base::Expression getExpression(base::Name& filterName)
     {
@@ -66,9 +73,8 @@ private:
         {
             throw std::runtime_error {"The asset name is empty or it is not a filter"};
         }
-
         auto filter = m_builder->buildAsset(filterName);
-        if(base::isError(filter))
+        if (base::isError(filter))
         {
             throw std::runtime_error {base::getError(filter).message};
         }
@@ -77,8 +83,21 @@ private:
     }
 
 public:
+    /**
+     * @brief Construct a new BuildEnvironment object
+     *
+     * @param builder The builder used to construct the policy and filter.
+     */
+    BuildEnvironment(std::shared_ptr<IBuilder> builder)
+        : m_builder(builder)
+    {
+        if (m_builder == nullptr)
+        {
+            throw std::runtime_error {"Cannot create BuildEnvironment with a null builder"};
+        }
+    }
 
-    BuildEnvironment(std::shared_ptr<IBuilder> builder) : m_builder (builder){}
+    BuildEnvironment() = delete;
 
     /**
      * @brief Create an environment based on a policy and a filter.
@@ -86,49 +105,70 @@ public:
      * @param policyName The name of the policy.
      * @param filterName The name of the filter.
      * @return Environment The created environment.
+     * @throws std::runtime_error if failed to create the environment.
      */
     Environment create(base::Name& policyName, base::Name& filterName)
     {
-        auto controller = getController(policyName);
-        auto expression = getExpression(filterName);
-        return Environment (std::move(expression), std::move(controller));
+        try
+        {
+            auto controller = getController(policyName);
+            auto expression = getExpression(filterName);
+            return Environment(std::move(expression), std::move(controller));
+        }
+        catch (const std::exception& e)
+        {
+            throw std::runtime_error {fmt::format(
+                "Failed to create environment with policy '{}' and filter '{}': {}", policyName, filterName, e.what())};
+        }
     }
 
     /**
      * @brief Create an environment based on a policy and a filter ID.
      *
+     * The filter ID is used to create a filter expression that checks if the session ID is equal to the filter ID.
      * @param policyName The name of the policy.
      * @param filterId The filter ID.
      * @param builder The builder used to construct the policy and filter.
      * @return Environment The created environment.
+     * @throw std::runtime_error if failed to create the environment.
      */
     Environment create(base::Name& policyName, const uint32_t filterId)
     {
-        auto controller = getController(policyName);
+        if (policyName.parts().size() == 0 || policyName.parts()[0] != "policy")
+        {
+            throw std::runtime_error {"The asset name is empty or it is not a policy"};
+        }
+
+        std::shared_ptr<T> controller {};
+        try
+        {
+            controller = getController(policyName);
+        }
+        catch (std::runtime_error e)
+        {
+            throw std::runtime_error {fmt::format(
+                "Failed to create environment with policy '{}' and filter '{}': {}", policyName, filterId, e.what())};
+        }
 
         json::Json value {std::to_string(filterId).c_str()};
 
-        const auto name {fmt::format("condition.value[{}=={}]", JSON_PATH_SESSION_FILTER, value.str())};
-        const auto successTrace {fmt::format("[{}] -> Success", name)};
-        const auto failureTrace {fmt::format("[{}] -> Failure", name)};
+        const auto name {fmt::format("Router filter {} -> {}", JSON_PATH_SESSION_FILTER, value.str())};
 
-        auto expression = base::Term<base::EngineOp>::create(name,
-                                    [=](base::Event event)
-                                    {
-                                        if (event->equals(JSON_PATH_SESSION_FILTER, value))
-                                        {
-                                            return base::result::makeSuccess(std::move(event), successTrace);
-                                        }
-                                        else
-                                        {
-                                            return base::result::makeFailure(std::move(event), failureTrace);
-                                        }
-                                    });
+        auto expression =
+            base::Term<base::EngineOp>::create(name,
+                                               [value](const base::Event& event) -> base::result::Result<base::Event>
+                                               {
+                                                   if (event->equals(JSON_PATH_SESSION_FILTER, value))
+                                                   {
+                                                       return base::result::makeSuccess(event);
+                                                   }
+                                                   return base::result::makeFailure(event);
+                                               });
 
-        return Environment (std::move(expression), std::move(controller));
+        return Environment(std::move(expression), std::move(controller));
     }
 };
 
-}
+} // namespace router
 
 #endif //_ROUTER2_BUILD_ENVIRONMENT_HPP
