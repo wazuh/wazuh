337a6a1c2070f1d4d24070616df3c4319df39d8f
RamosFe <federico.ramos@wazuh.com>
2023-12-01 11:41:35 -0300

    Adds copyright headers to /framework files.

--- framework/wazuh/core/exception.py
+++ framework/wazuh/core/exception.py
@@ -2,7 +2,6 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-
 from copy import deepcopy
 from typing import Union
 
