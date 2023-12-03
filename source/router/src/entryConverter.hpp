9f3cb1ad1eff133dacb9b26c259275e8929d7d38
Julian Morales <jmorales@unc.edu.ar>
2023-12-03 22:11:59 +0000

    Test of Entry converter added

--- source/router/src/entryConverter.hpp
+++ source/router/src/entryConverter.hpp
@@ -2,27 +2,44 @@
 #define ROUTER_ENTRY_CONVERTER_HPP
 
 #include <exception>
+#include <list>
 #include <optional>
 #include <string>
+#include <vector>
 
+#include <json/json.hpp>
 #include <router/types.hpp>
 
 namespace router
 {
-// Dumpers and loaders configuration
+
 class EntryConverter
 {
-private:
-    constexpr static auto NAME_PATH = "/name";
-    constexpr static auto POLICY_PATH = "/policy";
-    constexpr static auto DESCRIPTION_PATH = "/description";
-    // Tester
-    constexpr static auto LIFETIME_PATH = "/lifetime";
-    constexpr static auto LAST_USE_PATH = "/lastUse";
-    // Router
-    constexpr static auto FILTER_PATH = "/filter";
-    constexpr static auto PRIORITY_PATH = "/priority";
+public:
+    EntryConverter() = delete;
 
+    explicit EntryConverter(const test::Entry& entry); ///< Converts from test::Entry to EntryConverter
+    explicit EntryConverter(const prod::Entry& entry); ///< Converts from prod::Entry to EntryConverter
+    explicit EntryConverter(const json::Json& jEntry); ///< Converts from json::Json to EntryConverter
+
+    const std::string& name() const;                       ///< Returns the name of the entry
+    const std::string& policy() const;                     ///< Returns the policy of the entry
+    const std::optional<std::string>& description() const; ///< Returns the description of the entry
+    const std::optional<int64_t>& lifetime() const;        ///< Returns the lifetime of the entry
+    const std::optional<int64_t>& lastUse() const;         ///< Returns the lastUse of the entry
+
+    explicit operator json::Json() const;      ///< Converts from EntryConverter to json::Json
+    explicit operator test::EntryPost() const; ///< Converts from EntryConverter to test::EntryPost
+    explicit operator prod::EntryPost() const; ///< Converts from EntryConverter to prod::EntryPost
+
+    static std::vector<EntryConverter>
+    fromJsonArray(const json::Json& json); ///< Converts from json::Json to std::vector<EntryConverter>
+
+    template<typename EntryType>
+    static json::Json
+    toJsonArray(const std::list<EntryType>& entries); ///< Converts from std::list<EntryType> to json::Json
+
+private:
     std::string m_name;
     std::string m_policy;
     std::optional<std::string> m_description;
@@ -31,150 +48,15 @@ private:
     std::optional<std::string> m_filter;
     std::optional<size_t> m_priority;
 
-public:
-    EntryConverter() = delete;
-
-    explicit EntryConverter(const test::Entry& entry)
-        : m_name {entry.name()}
-        , m_policy {entry.policy()}
-        , m_description {entry.description()}
-        , m_lifetime {entry.lifetime()}
-        , m_lastUse {entry.lastUse()}
-    {
-    }
-
-    explicit EntryConverter(const prod::Entry& entry)
-        : m_name {entry.name()}
-        , m_policy {entry.policy()}
-        , m_description {entry.description()}
-        , m_filter {entry.filter()}
-        , m_priority {entry.priority()}
-    {
-    }
-
-    explicit EntryConverter(const json::Json& jEntry)
-    {
-        auto name = jEntry.getString(NAME_PATH);
-        auto policy = jEntry.getString(POLICY_PATH);
-        if (!name || !policy)
-        {
-            throw std::runtime_error {"Cannot load the entry: name or policy is missing"};
-        }
-
-        m_name = name.value();
-        m_policy = policy.value();
-        m_description = jEntry.getString(DESCRIPTION_PATH);
-        m_lifetime = jEntry.getInt64(LIFETIME_PATH);
-        m_lastUse = jEntry.getInt64(LAST_USE_PATH);
-        m_filter = jEntry.getString(FILTER_PATH);
-        m_priority = jEntry.getInt64(PRIORITY_PATH);
-    }
-
-    const std::string& name() const { return m_name; }
-    const std::string& policy() const { return m_policy; }
-    const std::optional<std::string>& description() const { return m_description; }
-    const std::optional<int64_t>& lifetime() const { return m_lifetime; }
-    const std::optional<int64_t>& lastUse() const { return m_lastUse; }
-
-    explicit operator json::Json() const
-    {
-        json::Json jEntry {};
-
-        jEntry.setString(m_name, NAME_PATH);
-        jEntry.setString(m_policy, POLICY_PATH);
-
-        if (m_description)
-        {
-            jEntry.setString(m_description.value(), DESCRIPTION_PATH);
-        }
-
-        if (m_lifetime)
-        {
-            jEntry.setInt64(m_lifetime.value(), LIFETIME_PATH);
-        }
-
-        if (m_lastUse)
-        {
-            jEntry.setInt64(m_lastUse.value(), LAST_USE_PATH);
-        }
-
-        if (m_filter)
-        {
-            jEntry.setString(m_filter.value(), FILTER_PATH);
-        }
-
-        if (m_priority)
-        {
-            jEntry.setInt64(static_cast<int64_t>(m_priority.value()), PRIORITY_PATH);
-        }
-
-        return jEntry;
-    }
-
-    explicit operator test::EntryPost() const
-    {
-        if (!m_lifetime)
-        {
-            throw std::runtime_error {"Cannot load the entry: lifetime is missing"};
-        }
-        auto entryPost = test::EntryPost(m_name, m_policy, m_lifetime.value());
-        if (m_description)
-        {
-            entryPost.description(m_description.value());
-        }
-
-        return entryPost;
-    }
-
-    explicit operator prod::EntryPost() const
-    {
-        if (!m_filter)
-        {
-            throw std::runtime_error {"Cannot load the entry: filter is missing"};
-        }
-        if (!m_priority)
-        {
-            throw std::runtime_error {"Cannot load the entry: priority is missing"};
-        }
-        auto entryPost = prod::EntryPost(m_name, m_policy, m_filter.value(), m_priority.value());
-        if (m_description)
-        {
-            entryPost.description(m_description.value());
-        }
-
-        return entryPost;
-    }
-
-    static std::vector<EntryConverter> fromJsonArray(const json::Json& json)
-    {
-        std::optional<std::vector<json::Json>> jArrayEntries = json.getArray();
-        if (!jArrayEntries)
-        {
-            throw std::runtime_error {"Cannot get routes table from store. Invalid table format"};
-        }
-
-        std::vector<EntryConverter> entries {};
-        for (const auto& jEntry : jArrayEntries.value())
-        {
-            entries.emplace_back(jEntry);
-        }
-
-        return entries;
-    }
-
-    template<typename EntryType>
-    static json::Json toJsonArray(const std::list<EntryType>& entries)
-    {
-        json::Json jArray;
-        jArray.setArray();
-
-        for (const auto& entry : entries)
-        {
-            jArray.appendJson(json::Json(EntryConverter(entry)));
-        }
-        return jArray;
-    }
+    static constexpr auto NAME_PATH = "/name";
+    static constexpr auto POLICY_PATH = "/policy";
+    static constexpr auto DESCRIPTION_PATH = "/description";
+    static constexpr auto LIFETIME_PATH = "/lifetime";
+    static constexpr auto LAST_USE_PATH = "/lastUse";
+    static constexpr auto FILTER_PATH = "/filter";
+    static constexpr auto PRIORITY_PATH = "/priority";
 };
+
 } // namespace router
 
 #endif // ROUTER_ENTRY_CONVERTER_HPP
