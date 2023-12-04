6c108a90fdc013b19d627d5e125281144b53b895
Eduardo <eduardo.leon@wazuh.com>
2023-12-04 15:29:08 -0300

    Move configurator and local conf to utils and fix import

--- tests/integration/test_aws/test_path.py
+++ tests/integration/test_aws/test_path.py
@@ -1,43 +1,37 @@
-import os
+# Copyright (C) 2015, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+"""
+This module will contain all cases for the path test suite
+"""
 import pytest
 
 # qa-integration-framework imports
 from wazuh_testing import session_parameters
-from wazuh_testing.constants.paths.configurations import TEMPLATE_DIR, TEST_CASES_DIR
-from wazuh_testing.modules.aws import event_monitor, local_internal_options  # noqa: F401
-from wazuh_testing.modules.aws.db_utils import (
-    get_s3_db_row,
-    s3_db_exists,
-    table_exists_or_has_values,
-)
-from wazuh_testing.utils.configuration import (
-    get_test_cases_data,
-    load_configuration_template,
-)
+from wazuh_testing.constants.paths.aws import S3_CLOUDTRAIL_DB_PATH
+from wazuh_testing.utils.db_queries.aws_db import get_s3_db_row, table_exists_or_has_values
+from wazuh_testing.modules.aws.utils import path_exist
+
 # Local module imports
-from .utils import ERROR_MESSAGES, TIMEOUTS
+from . import event_monitor
+from .utils import ERROR_MESSAGE, TIMEOUT, TestConfigurator, local_internal_options
 
 pytestmark = [pytest.mark.server]
 
-
-# Generic vars
-MODULE = 'path_test_module'
-TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-CONFIGURATIONS_PATH = os.path.join(TEST_DATA_PATH, TEMPLATE_DIR, MODULE)
-TEST_CASES_PATH = os.path.join(TEST_DATA_PATH, TEST_CASES_DIR, MODULE)
+# Set test configurator for the module
+configurator = TestConfigurator(module='path_test_module')
 
 # ---------------------------------------------------- TEST_PATH -------------------------------------------------------
-configurations_path = os.path.join(CONFIGURATIONS_PATH, 'configuration_path.yaml')
-cases_path = os.path.join(TEST_CASES_PATH, 'cases_path.yaml')
-
-configuration_parameters, configuration_metadata, case_ids = get_test_cases_data(cases_path)
-configurations = load_configuration_template(
-    configurations_path, configuration_parameters, configuration_metadata
-)
+# Configure T1 test
+configurator.configure_test(configuration_file='configuration_path.yaml',
+                            cases_file='cases_path.yaml')
 
 
 @pytest.mark.tier(level=0)
-@pytest.mark.parametrize('configuration, metadata', zip(configurations, configuration_metadata), ids=case_ids)
+@pytest.mark.parametrize('configuration, metadata',
+                         zip(configurator.test_configuration_template, configurator.metadata),
+                         ids=configurator.cases_ids)
 def test_path(
     configuration, metadata, load_wazuh_basic_configuration, set_wazuh_configuration, clean_s3_cloudtrail_db,
     configure_local_internal_options_function, truncate_monitored_files, restart_wazuh_function, file_monitoring
@@ -122,7 +116,7 @@ def test_path(
         callback=event_monitor.callback_detect_aws_module_start
     )
 
-    assert log_monitor.callback_result is not None, ERROR_MESSAGES['failed_start']
+    assert log_monitor.callback_result is not None, ERROR_MESSAGE['failed_start']
 
     # Check command was called correctly
     log_monitor.start(
@@ -130,25 +124,25 @@ def test_path(
         callback=event_monitor.callback_detect_aws_module_called(parameters)
     )
 
-    assert log_monitor.callback_result is not None, ERROR_MESSAGES['incorrect_parameters']
+    assert log_monitor.callback_result is not None, ERROR_MESSAGE['incorrect_parameters']
 
     if expected_results:
         log_monitor.start(
-            timeout=TIMEOUTS[20],
+            timeout=TIMEOUT[20],
             callback=event_monitor.callback_detect_event_processed,
         )
 
-        assert log_monitor.callback_result is not None, ERROR_MESSAGES['incorrect_event_number']
+        assert log_monitor.callback_result is not None, ERROR_MESSAGE['incorrect_event_number']
 
     else:
         log_monitor.start(
-            timeout=TIMEOUTS[10],
+            timeout=TIMEOUT[10],
             callback=event_monitor.make_aws_callback(pattern),
         )
 
-        assert log_monitor.callback_result is not None, ERROR_MESSAGES['incorrect_empty_path_message']
+        assert log_monitor.callback_result is not None, ERROR_MESSAGE['incorrect_empty_path_message']
 
-    assert s3_db_exists()
+    assert path_exist(path=S3_CLOUDTRAIL_DB_PATH)
 
     if expected_results:
         data = get_s3_db_row(table_name=table_name)
@@ -163,4 +157,4 @@ def test_path(
         callback=event_monitor.callback_detect_all_aws_err
     )
 
-    assert log_monitor.callback_result is None, ERROR_MESSAGES['error_found']
+    assert log_monitor.callback_result is None, ERROR_MESSAGE['error_found']
