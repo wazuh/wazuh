8148ba6b8b3f12aef03bd038f48341716fac27c2
Mateo Cervilla <mateo.cervilla@wazuh.com>
2023-12-05 12:12:05 -0300

    Migrate logtest IT remove_session

--- tests/integration/test_logtest/test_remove_session/test_remove_session.py
+++ tests/integration/test_logtest/test_remove_session/test_remove_session.py
@@ -49,29 +49,29 @@ tags:
     - logtest_configuration
 '''
 import json
-import os
+from pathlib import Path
 
 import pytest
-import yaml
-from wazuh_testing.constants.paths import WAZUH_PATH
+from wazuh_testing.constants.paths.sockets import LOGTEST_SOCKET_PATH
 from wazuh_testing.tools.socket_controller import SocketController
-from conftest import close_sockets
+from wazuh_testing.utils import configuration
+
+from . import TEST_CASES_FOLDER_PATH
 
 
 # Marks
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0), pytest.mark.server]
 
-# Configurations
-test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/config_templates')
-messages_path = os.path.join(test_data_path, 'config_remove_session.yaml')
-
-with open(messages_path) as f:
-    test_cases = yaml.safe_load(f)
+# Configuration
+t_cases_path = Path(TEST_CASES_FOLDER_PATH, 'cases_remove_session.yaml')
+t_config_parameters, t_config_metadata, t_case_ids = configuration.get_test_cases_data(t_cases_path)
 
 # Variables
-logtest_path = os.path.join(os.path.join(WAZUH_PATH, 'queue', 'sockets', 'logtest'))
-receiver_sockets_params = [(logtest_path, 'AF_UNIX', 'TCP')]
+receiver_sockets_params = [(LOGTEST_SOCKET_PATH, 'AF_UNIX', 'TCP')]
 receiver_sockets = None  # Set in the fixtures
+
+# Test daemons to restart.
+daemons_handler_configuration = {'daemons': ['wazuh-analysisd', 'wazuh-db']}
 
 
 def create_session():
@@ -84,20 +84,27 @@ def create_session():
     token = json.loads(receiver_sockets[0].receive(size=True).rstrip(b'\x00').decode())['data']['token']
 
     # Close socket
-    close_sockets(receiver_sockets)
+    for socket in receiver_sockets:
+        try:
+            # We flush the buffer before closing the connection if the protocol is TCP:
+            if socket.protocol == 1:
+                socket.sock.settimeout(5)
+                socket.receive()  # Flush buffer before closing connection
+            socket.close()
+        except OSError as e:
+            if e.errno == 9:
+                # Do not try to close the socket again if it was reused or closed already
+                pass
 
     # Renew socket for future connections
-    receiver_sockets[0] = SocketController(address=logtest_path, family='AF_UNIX', connection_protocol='TCP')
+    receiver_sockets[0] = SocketController(address=LOGTEST_SOCKET_PATH, family='AF_UNIX', connection_protocol='TCP')
     return token
 
 
 # Tests
-
-@pytest.mark.parametrize('test_case',
-                         [test_case['test_case'] for test_case in test_cases],
-                         ids=[test_case['name'] for test_case in test_cases])
-def test_remove_session(restart_required_logtest_daemons, wait_for_logtest_startup, connect_to_sockets_function,
-                        test_case: list):
+@pytest.mark.parametrize('test_metadata', t_config_metadata, ids=t_case_ids)
+def test_remove_session(test_metadata, daemons_handler_module, 
+                        wait_for_logtest_startup, connect_to_sockets):
     '''
     description: Check if 'wazuh-logtest' correctly detects and removes the sessions under pre-defined scenarios.
                  To do this, the session input is sent and the output is received, then it checks if the received data
@@ -108,18 +115,18 @@ def test_remove_session(restart_required_logtest_daemons, wait_for_logtest_start
     tier: 0
 
     parameters:
-        - restart_required_logtest_daemons:
+        - daemons_handler_module:
             type: fixture
             brief: Wazuh logtests daemons handler.
         - wait_for_logtest_startup:
             type: fixture
             brief: Wait until logtest has begun.
-        - connect_to_sockets_function:
+        - connect_to_sockets:
             type: fixture
             brief: Function scope version of 'connect_to_sockets' which connects to the specified sockets for the test.
-        - test_case:
+        - test_metadata:
             type: list
-            brief: List of test_case stages. (dicts with input, output and stage keys)
+            brief: List of metadata values. (dicts with input, output and stage keys)
 
     assertions:
         - Verify that every test case output matches with the actual received.
@@ -133,17 +140,15 @@ def test_remove_session(restart_required_logtest_daemons, wait_for_logtest_start
     tags:
         - analysisd
     '''
-    stage = test_case[0]
-
-    if stage["stage"] != 'Remove session OK':
-        receiver_sockets[0].send(stage['input'], size=True)
+    if test_metadata["stage"] != 'Remove session OK':
+        receiver_sockets[0].send(test_metadata['input'], size=True)
         reply = receiver_sockets[0].receive(size=True).rstrip(b'\x00').decode()
-        expected = json.loads(stage['output'])
+        expected = json.loads(test_metadata['output'])
     else:
         session_token = create_session()
-        receiver_sockets[0].send(stage['input'].format(session_token), size=True)
+        receiver_sockets[0].send(test_metadata['input'].format(session_token), size=True)
         reply = receiver_sockets[0].receive(size=True).rstrip(b'\x00').decode()
-        expected = json.loads(stage['output'].format(session_token))
+        expected = json.loads(test_metadata['output'].format(session_token))
 
     result = json.loads(reply)
     if 'messages' in expected['data'] and 'messages' in result['data']:
@@ -152,6 +157,4 @@ def test_remove_session(restart_required_logtest_daemons, wait_for_logtest_start
         if message_recieved.split(': ')[-1] == message_expected.split(': ')[-1]:
             expected['data']['messages'][0] = result['data']['messages'][0]
 
-    assert expected == result, 'Failed test case stage {}: {}'.format(
-        test_case.index(stage) + 1,
-        stage['stage'])
+    assert expected == result, 'Failed test case stage {}'.format(test_metadata['stage'])
