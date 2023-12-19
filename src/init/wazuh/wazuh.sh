ce463ec5b18fa59d33f927d3e22744c32db4bda2
Kevin Michael Cardenas Miranda <kevincardenasmiranda9@gmail.com>
2023-12-19 20:46:43 +0000

    CL: - Remove replace_vulnerability_detector.sh script - Add update-indexer.sh script - Implement function that appends the Indexer template to the ossec.conf

--- src/init/wazuh/wazuh.sh
+++ src/init/wazuh/wazuh.sh
@@ -224,14 +224,12 @@ WazuhUpgrade()
         rm -f $DIRECTORY/queue/alerts/execa
     fi
 
-    # Update versions previous to Wazuh 4.8
+    # Ensure that the 'Indexer' is configured
     if [ "X$1" = "Xserver" ]; then
         local OSSEC_CONF_PATH="$PREINSTALLEDDIR/etc/ossec.conf"
-        local VULN_TEMPLATE_PATH="./etc/templates/config/generic/wodle-vulnerability-detection.manager.template"
         local INDEXER_TEMPLATE_PATH="./etc/templates/config/generic/wodle-indexer.manager.template"
 
-        # Load vulnerability detector replacer commands
-        . ./src/init/replace_vulnerability_detector.sh
-        updateVulnerabilityDetector $OSSEC_CONF_PATH $VULN_TEMPLATE_PATH $INDEXER_TEMPLATE_PATH
+        . ./src/init/update-indexer.sh
+        updateIndexerTemplate "$OSSEC_CONF_PATH" "$INDEXER_TEMPLATE_PATH"
     fi
 }
