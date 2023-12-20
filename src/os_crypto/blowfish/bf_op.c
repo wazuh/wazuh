8ca1aef9ad15576ce5c67f7055f7ad91c662b65a
Santiago Vendramini <sdvendramini@gmail.com>
2023-12-20 16:08:34 -0300

    Revert "Migrate deprecated OpenSSL functions BF_set_key  and BF_cbc_encrypt."
    This reverts commit 5dfd1b40707078feb7ad1565bda9dcf5c5fbb4e0.

--- src/os_crypto/blowfish/bf_op.c
+++ src/os_crypto/blowfish/bf_op.c
@@ -16,50 +16,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <openssl/provider.h>
-#include <openssl/evp.h>
+#include <openssl/blowfish.h>
 #include "bf_op.h"
 
 typedef unsigned char uchar;
 
 
-int OS_BF_Str(const char *input, char *output, const char *charkey, long size, short int action)
+int OS_BF_Str(const char *input, char *output, const char *charkey,
+              long size, short int action)
 {
-    int len, final_len;
-    static unsigned char iv[8] = {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+    BF_KEY key = {.P = {0}};
+    static unsigned char cbc_iv [8] = {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+    unsigned char iv[8];
 
-    // As of OpenSSL 3.0, Blowfish has been moved to the legacy provider
-    OSSL_PROVIDER *legacy = OSSL_PROVIDER_load(NULL, "legacy");
-    if (!legacy) {
-        return 0;
-    }
+    memcpy(iv, cbc_iv, sizeof(iv));
 
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (!ctx) {
-        return 0;
-    }
+    BF_set_key(&key, (int)strlen(charkey), (const uchar *)charkey);
 
-    EVP_CIPHER_CTX_set_padding(ctx, 0);
+    BF_cbc_encrypt((const uchar *)input, (uchar *)output, (long)size,
+                   &key, iv, action);
 
-    if (!EVP_CipherInit_ex(ctx, EVP_bf_cbc(), NULL, (const unsigned char *)charkey, iv, action)) {
-        EVP_CIPHER_CTX_free(ctx);
-        OSSL_PROVIDER_unload(legacy);
-        return 0;
-    }
-
-    if (!EVP_CipherUpdate(ctx, (unsigned char *)output, &len, (const unsigned char *)input, size)) {
-        EVP_CIPHER_CTX_free(ctx);
-        OSSL_PROVIDER_unload(legacy);
-        return 0;
-    }
-
-    if (!EVP_CipherFinal_ex(ctx, output + len, &final_len)) {
-        EVP_CIPHER_CTX_free(ctx);
-        OSSL_PROVIDER_unload(legacy);
-        return 0;
-    }
-
-    EVP_CIPHER_CTX_free(ctx);
-    OSSL_PROVIDER_unload(legacy);
-    return 1; // Success
+    return (1);
 }
