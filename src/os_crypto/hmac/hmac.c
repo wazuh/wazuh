35c2101bb92ecb57489fc7bff127ec7693faadc9
Joaquin Romera <joaquin.romera@wazuh.com>
2023-12-21 09:12:14 -0300

    Migrate OS_HMAC_SHA1_File to use newew Open SSL EVP functions

--- src/os_crypto/hmac/hmac.c
+++ src/os_crypto/hmac/hmac.c
@@ -16,6 +16,8 @@
 #include "headers/defs.h"
 #include "../sha1/sha1_op.h"
 #include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/core_names.h>
 #include "hmac.h"
 
 int OS_HMAC_SHA1_Str(const char *key, const char *text, os_sha1 output)
@@ -77,66 +79,80 @@ int OS_HMAC_SHA1_Str(const char *key, const char *text, os_sha1 output)
 
 int OS_HMAC_SHA1_File(const char *key, const char *file_path, os_sha1 output, int mode)
 {
-    unsigned char o_key_pad[HMAC_SHA1_BLOCKSIZE + 1];
-    unsigned char i_key_pad[HMAC_SHA1_BLOCKSIZE + 1];
-    unsigned char key_temp[HMAC_SHA1_BLOCKSIZE + 1];
-    unsigned char result[SHA_DIGEST_LENGTH + 1];
+    unsigned char result[EVP_MAX_MD_SIZE];
     unsigned char buffer[2048 + 2];
 
     int i;
-    size_t key_length;
-    SHA_CTX context;
+    int ret = -1;
     FILE *fp;
-
-    key_length = strlen(key);
-
-    if (key_length > HMAC_SHA1_BLOCKSIZE){
-        os_sha1 sha_key;
-        OS_SHA1_Str(key, key_length, sha_key);
-        key_length = strlen(sha_key);
-        memcpy(key_temp, sha_key, key_length);
-    } else {
-      memcpy(key_temp, key, key_length);
-    }
-
-    memset(o_key_pad, 0, sizeof(o_key_pad));
-    memset(i_key_pad, 0, sizeof(i_key_pad));
-    memcpy(o_key_pad, key_temp, key_length);
-    memcpy(i_key_pad, key_temp, key_length);
-
-    for (i = 0; i < HMAC_SHA1_BLOCKSIZE; i++){
-        o_key_pad[i] ^= 0x5c;
-        i_key_pad[i] ^= 0x36;
-    }
+    size_t result_len, key_length;
+    os_sha1 sha_key;
+    EVP_MAC *mac = NULL;
+    EVP_MAC_CTX *ctx = NULL;
+    OSSL_PARAM params[3];
 
     fp = fopen(file_path, mode == OS_BINARY ? "rb" : "r");
     if (!fp) {
         return -1;
     }
 
-    SHA1_Init(&context);
-
-    SHA1_Update(&context, i_key_pad, HMAC_SHA1_BLOCKSIZE);
-
-    while ((i = fread(buffer, 1, 2048, fp)) > 0) {
-        buffer[i] = '\0';
-        SHA1_Update(&context, buffer, i);
+    mac = EVP_MAC_fetch(NULL, "HMAC", NULL);
+    if (!mac) {
+        goto cleanup;
     }
 
-    SHA1_Final(result, &context);
+    ctx = EVP_MAC_CTX_new(mac);
+    if (!ctx) {
+        goto cleanup;
+    }
 
-    SHA1_Init(&context);
+    key_length = strlen(key);
+    if (key_length > HMAC_SHA1_BLOCKSIZE){
+        OS_SHA1_Str(key, key_length, sha_key);
+        key = sha_key;
+        key_length = SHA_DIGEST_LENGTH;
+    }
 
-    SHA1_Update(&context, o_key_pad, HMAC_SHA1_BLOCKSIZE);
-    SHA1_Update(&context, result, SHA_DIGEST_LENGTH);
+    params[0] = OSSL_PARAM_construct_utf8_string("digest", "SHA1", 0);
+    params[1] = OSSL_PARAM_construct_octet_string(OSSL_MAC_PARAM_KEY, key, key_length);
+    params[2] = OSSL_PARAM_construct_end();
 
-    SHA1_Final(result, &context);
+    if (!EVP_MAC_init(ctx, (unsigned char *)key, strlen(key), params)) {
+        goto cleanup;
+    }
 
-    for (i = 0; i < SHA_DIGEST_LENGTH; i++) {
+    while ((i = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+        if (!EVP_MAC_update(ctx, buffer, i)) {
+            goto cleanup;
+        }
+    }
+
+    result_len = sizeof(result);
+    if (!EVP_MAC_final(ctx, result, &result_len, sizeof(result))) {
+        goto cleanup;
+    }
+
+    if ((result_len * 2) + 1 > sizeof(os_sha1)) {
+        goto cleanup;
+    }
+
+    for (i = 0; i < result_len; i++) {
         snprintf(output + i * 2, 3, "%02x", result[i]);
     }
 
+    output[result_len * 2] = '\0';
+    ret = 0;
+
+cleanup:
     fclose(fp);
 
-    return 0;
+    if (ctx) {
+        EVP_MAC_CTX_free(ctx);
+    }
+
+    if (mac) {
+        EVP_MAC_free(mac);
+    }
+
+    return ret;
 }
