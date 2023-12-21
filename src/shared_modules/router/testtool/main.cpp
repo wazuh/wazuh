69dccc504a362d82e60b9f5d81aa85937c6ef3f3
Dwordcito <octa.tala@gmail.com>
2023-12-21 00:28:04 -0300

    Fix test tool memory handling and catch errors.

--- src/shared_modules/router/testtool/main.cpp
+++ src/shared_modules/router/testtool/main.cpp
@@ -12,99 +12,119 @@
 
 std::unique_ptr<RouterProvider> PROVIDER;
 std::unique_ptr<RouterSubscriber> SUBSCRIPTOR;
+std::string MODE;
+std::atomic<size_t> COUNT = 0;
+
+static void clean()
+{
+    if (MODE.compare("publisher") == 0)
+    {
+        if (PROVIDER)
+        {
+            PROVIDER->stop();
+            PROVIDER.reset();
+        }
+    }
+    else if (MODE.compare("subscriber") == 0)
+    {
+        if (SUBSCRIPTOR)
+        {
+            SUBSCRIPTOR.reset();
+        }
+    }
+    else if (MODE.compare("broker") == 0)
+    {
+        RouterModule::instance().stop();
+    }
+}
 
 int main(int argc, const char* argv[])
 {
-    CmdLineArgs args(argc, argv);
+    try
+    {
+        CmdLineArgs args(argc, argv);
 
-    atexit(
-        []()
+        MODE = args.mode();
+        if (MODE == "publisher")
         {
+            PROVIDER = std::make_unique<RouterProvider>(args.topic(), false);
+            PROVIDER->start();
+        }
+        else if (MODE == "subscriber")
+        {
+            SUBSCRIPTOR = std::make_unique<RouterSubscriber>(args.topic(), args.subscriberId(), false);
+
+            SUBSCRIPTOR->subscribe(
+                [&](const std::vector<char>& message)
+                {
+                    std::cout << "Received message #" << ++COUNT << ": ";
+                    auto fbsPath = args.fbsPath();
+
+                    if (fbsPath.empty())
+                    {
+                        std::cout << std::string(message.begin(), message.end()) << std::endl;
+                    }
+                    else
+                    {
+                        flatbuffers::IDLOptions options;
+                        options.strict_json = true;
+                        flatbuffers::Parser parser(options);
+                        std::string schemaStr;
+
+                        if (!flatbuffers::LoadFile(fbsPath.c_str(), false, &schemaStr))
+                        {
+                            throw std::runtime_error("Unable to load schema file.");
+                        }
+                        if (!parser.Parse(schemaStr.c_str()))
+                        {
+                            throw std::runtime_error("Unable to parse schema file.");
+                        }
+
+                        std::string strData;
+                        flatbuffers::GenText(parser, reinterpret_cast<const uint8_t*>(message.data()), &strData);
+                        std::cout << strData << std::endl;
+                    }
+                });
+        }
+        else if (MODE == "broker")
+        {
+            RouterModule::instance().start();
+        }
+        else
+        {
+            CmdLineArgs::showHelp();
+            clean();
+            return 1;
+        }
+
+        std::cout << "Press 'q' to exit." << std::endl;
+        std::string data;
+        while (std::getline(std::cin, data))
+        {
+            const auto message = std::vector<char>(data.begin(), data.end());
+
+            if (message.empty())
+            {
+                continue;
+            }
+
+            if (message[0] == 'q')
+            {
+                break;
+            }
+
             if (PROVIDER)
             {
-                PROVIDER->stop();
-                PROVIDER.reset();
+                std::cout << "Sending message: " << data << std::endl;
+                PROVIDER->send(message);
             }
-            if (SUBSCRIPTOR)
-            {
-                SUBSCRIPTOR.reset();
-            }
-            RouterModule::instance().stop();
-        });
-
-    if (args.mode() == "publisher")
-    {
-        PROVIDER = std::make_unique<RouterProvider>(args.topic());
-        PROVIDER->start();
+        }
+        clean();
     }
-    else if (args.mode() == "subscriber")
-    {
-        SUBSCRIPTOR = std::make_unique<RouterSubscriber>(args.topic(), args.subscriberId(), false);
-        std::atomic<size_t> count = 0;
-
-        SUBSCRIPTOR->subscribe(
-            [&](const std::vector<char>& message)
-            {
-                std::cout << "Received message #" << ++count << ": ";
-                auto fbsPath = args.fbsPath();
-
-                if (fbsPath.empty())
-                {
-                    std::cout << std::string(message.begin(), message.end()) << std::endl;
-                }
-                else
-                {
-                    flatbuffers::IDLOptions options;
-                    options.strict_json = true;
-                    flatbuffers::Parser parser(options);
-                    std::string schemaStr;
-
-                    if (!flatbuffers::LoadFile(fbsPath.c_str(), false, &schemaStr))
-                    {
-                        throw std::runtime_error("Unable to load schema file.");
-                    }
-                    if (!parser.Parse(schemaStr.c_str()))
-                    {
-                        throw std::runtime_error("Unable to parse schema file.");
-                    }
-
-                    std::string strData;
-                    flatbuffers::GenText(parser, reinterpret_cast<const uint8_t*>(message.data()), &strData);
-                    std::cout << strData << std::endl;
-                }
-            });
-    }
-    else if (args.mode() == "broker")
-    {
-        RouterModule::instance().start();
-    }
-    else
+    catch (const std::exception& e)
     {
         CmdLineArgs::showHelp();
         return 1;
-    }
-
-    std::cout << "Press 'q' to exit." << std::endl;
-    std::string data;
-    while (std::getline(std::cin, data))
-    {
-        const auto message = std::vector<char>(data.begin(), data.end());
-
-        if (message.empty())
-        {
-            continue;
-        }
-
-        if (message[0] == 'q')
-        {
-            break;
-        }
-
-        if (PROVIDER)
-        {
-            std::cout << "Sending message: " << data << std::endl;
-            PROVIDER->send(message);
-        }
     }
     return 0;
 }
