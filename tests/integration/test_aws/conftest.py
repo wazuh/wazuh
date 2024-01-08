afc511c6a218d3c5f16ed1bd4deea49e54986cd4
Eduardo <eduardo.leon@wazuh.com>
2024-01-08 13:12:41 -0300

    Modify fixtures and data for test basic suite

--- tests/integration/test_aws/conftest.py
+++ tests/integration/test_aws/conftest.py
@@ -3,31 +3,27 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 """
-This module contain all necessary components (fixtures, classes, methods)to configure the test for its execution.
+This module contain all necessary components (fixtures, classes, methods) to configure the test for its execution.
 """
-import botocore
 import pytest
 from uuid import uuid4
+from time import time
+from botocore.exceptions import ClientError
 
 # qa-integration-framework imports
 from wazuh_testing.logger import logger
-from wazuh_testing.constants.aws import (
-    FAKE_CLOUDWATCH_LOG_GROUP,
-    PERMANENT_CLOUDWATCH_LOG_GROUP,
-)
 from wazuh_testing.modules.aws.utils import (
-    #create_bucket,
-    create_log_events,
+    create_bucket,
+    upload_log_events,
     create_log_group,
     create_log_stream,
-    #delete_bucket,
+    delete_bucket,
     delete_log_group,
-    delete_log_stream,
-    delete_file,
     delete_s3_db,
     delete_services_db,
-    file_exists,
-    upload_file
+    upload_bucket_file,
+    delete_resources,
+    generate_file
 )
 from wazuh_testing.utils.services import control_service
 
@@ -51,167 +47,208 @@ def restart_wazuh_function_without_exception(daemon=None):
     control_service('stop', daemon=daemon)
 
 
-# S3 fixtures
+"""S3 fixtures"""
 
-@pytest.fixture(scope="session", autouse=True)
-def create_session_uuid():
-    uuid = str(uuid4())[:8]
-    return uuid
-
-
-@pytest.fixture(scope="session", autouse=True)
-def delete_buckets():
-    bucket_list = []
-
-    yield bucket_list
-
-    for bucket in bucket_list:
-        #delete_bucket(bucket)
-        pass
-
-
-@pytest.fixture()
-def create_bucket(create_session_uuid, bucket_list, metadata):
-    """
-
-    Parameters
-    ----------
-        bucket_list
-        create_session_uuid
-        metadata
-
-    Returns
-    -------
-
-    """
-    bucket_name = metadata['bucket_name']
-    bucket_name += f"-{create_session_uuid}"
-
-    create_bucket(bucket_name=bucket_name)
-    metadata['bucket_name'] = bucket_name
-
-    yield
-
-    bucket_list.append(bucket_name)
 
 @pytest.fixture
-def upload_and_delete_file_to_s3(metadata):
+def upload_file_to_bucket(metadata):
     """Upload a file to S3 bucket and delete after the test ends.
 
     Args:
         metadata (dict): Metadata to get the parameters.
     """
+    # Get bucket name
     bucket_name = metadata['bucket_name']
-    filename = upload_file(bucket_type=metadata['bucket_type'], bucket_name=metadata['bucket_name'])
-    if filename != '':
+
+    # Get bucket type
+    bucket_type = metadata['bucket_type']
+
+    # Generate file
+    data, filename = generate_file(bucket_type=bucket_type,
+                                   bucket_name=bucket_name)
+
+    try:
+        # Upload file to bucket
+        upload_bucket_file(bucket_name=bucket_name,
+                           data=data,
+                           filename=filename)
+
         logger.debug('Uploaded file: %s to bucket "%s"', filename, bucket_name)
+
+        # Set filename for test execution
         metadata['uploaded_file'] = filename
 
-    yield
+    except ClientError as error:
+        logger.error({
+            "message": "Client error uploading file to bucket",
+            "bucket_name": bucket_name,
+            "filename": filename,
+            "error": str(error)
+        })
+        pass
 
-    if file_exists(filename=filename, bucket_name=bucket_name):
-        delete_file(filename=filename, bucket_name=bucket_name)
-        logger.debug('Deleted file: %s from bucket %s', filename, bucket_name)
+    except Exception as error:
+        logger.error({
+            "message": "Broad error uploading file to bucket",
+            "bucket_name": bucket_name,
+            "filename": filename,
+            "error": str(error)
+        })
+        pass
 
 
-@pytest.fixture
-def delete_file_from_s3(metadata):
-    """Delete a file from S3 bucket after the test ends.
+"""CloudWatch fixtures"""
 
-    Args:
-        metadata (dict): Metadata to get the parameters.
+
+@pytest.fixture()
+def create_test_log_group(create_session_id: str, create_and_delete_resources_list: list, metadata: dict):
+    """Create a bucket.
+
+    Parameters
+    ----------
+        create_session_id (str): Test session id.
+        create_and_delete_resources_list (list): Resources list.
+        metadata (dict): Log group information.
+
+    Returns
+    -------
+        None
     """
-    yield
+    # Set variables from fixture
+    test_session_id = create_session_id
+    resources_list = create_and_delete_resources_list
 
-    bucket_name = metadata['bucket_name']
-    filename = metadata.get('filename')
-    if filename is not None:
-        delete_file(filename=filename, bucket_name=bucket_name)
-        logger.debug('Deleted file: %s from bucket %s', filename, bucket_name)
+    # Get log group information and add session id
+    log_group_name = metadata["log_group_name"] + f"-{test_session_id}"
+
+    try:
+        # Create log group
+        create_log_group(log_group_name=log_group_name)
+        logger.debug(f"Created log group: {log_group_name}")
+
+        # Create resource dict
+        resource = {
+            "type": "log_group",
+            "name": log_group_name
+        }
+
+        # Append created log group to resources list
+        resources_list.append(log_group_name)
+
+    except ClientError as error:
+        logger.error({
+            "message": "Client error creating log group",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        raise
+
+    except Exception as error:
+        logger.error({
+            "message": "Broad error creating log group",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        raise
 
 
-# CloudWatch fixtures
+@pytest.fixture()
+def create_test_log_stream(metadata: dict):
+    """Create a log stream.
 
-@pytest.fixture(name='create_log_stream')
-def fixture_create_log_stream(metadata):
-    """Create a log stream with events and delete after the execution.
+    Parameters
+    ----------
+        metadata (dict): Log group information.
 
-    Args:
-        metadata (dict): Metadata to get the parameters.
+    Returns
+    -------
+        None
     """
-
-    SKIP_LOG_GROUP_CREATION = [PERMANENT_CLOUDWATCH_LOG_GROUP, FAKE_CLOUDWATCH_LOG_GROUP]
-    print(PERMANENT_CLOUDWATCH_LOG_GROUP, FAKE_CLOUDWATCH_LOG_GROUP)
-    log_group_names = [item.strip() for item in metadata['log_group_name'].split(',')]
-    for log_group_name in log_group_names:
-        if log_group_name in SKIP_LOG_GROUP_CREATION:
-            continue
-
-        import random
-        log_group_name += f"-{random.randint(10**3, 10**4 - 1)}"
-
-        logger.debug('Creating log group: %s', log_group_name)
-        try:
-            create_log_group(log_group_name)
-        except botocore.ResourceAlreadyExistsException as e:
-            pass
-        log_stream = create_log_stream(log_group_name)
-        logger.debug('Created log stream "%s" within log group "%s"', log_stream, log_group_name)
-        try:
-            create_log_events(
-                log_stream=log_stream, log_group=log_group_name, event_number=metadata.get('expected_results', 1)
-            )
-        except botocore.errorfactory.ResourceAlreadyExistsException as e:
-            pass
-        except Exception as e:
-            print(e)
-            pass
-        logger.debug('Created log events')
-        metadata['log_stream'] = log_stream
-
-    yield
-
-    for log_group_name in log_group_names:
-        if log_group_name in SKIP_LOG_GROUP_CREATION:
-            continue
-        delete_log_group(log_group_name)
-        logger.debug('Deleted log group: %s', log_group_name)
-
-
-@pytest.fixture
-def create_log_stream_in_existent_group(metadata):
-    """Create a log stream with events and delete after the execution.
-
-    Args:
-        metadata (dict): Metadata to get the parameters.
-    """
+    # Get log group
     log_group_name = metadata['log_group_name']
-    log_stream = create_log_stream(log_group_name)
-    logger.debug('Created log stream "%s" within log group "%s"', log_stream, log_group_name)
-    create_log_events(log_stream=log_stream, log_group=log_group_name)
-    logger.debug('Created log events')
-    metadata['log_stream'] = log_stream
 
-    yield
+    # Create random stream name
+    log_stream_name = str(uuid4())
 
-    delete_log_stream(log_stream=log_stream, log_group=log_group_name)
-    logger.debug('Deleted log stream: %s', log_stream)
+    try:
+        # Create log stream
+        create_log_stream(log_group=log_group_name,
+                          log_stream=log_stream_name)
+        logger.debug(f'Created log stream {log_stream_name} within log group {log_group_name}')
+
+        metadata['log_stream'] = log_stream_name
+
+    except ClientError as error:
+        logger.error({
+            "message": "Client error creating log stream",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        raise
+
+    except Exception as error:
+        logger.error({
+            "message": "Broad error creating log stream",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        raise
 
 
-@pytest.fixture(name='delete_log_stream')
-def fixture_delete_log_stream(metadata):
-    """Create a log stream with events and delete after the execution.
+@pytest.fixture()
+def create_test_events(metadata: dict):
+    """Create a log event in a log stream.
 
-    Args:
-        metadata (dict): Metadata to get the parameters.
+    Parameters
+    ----------
+        metadata (dict): Log group information.
+
+    Returns
+    -------
+        None
     """
-    yield
-    log_stream = metadata['log_stream']
-    delete_log_stream(log_stream=log_stream)
-    logger.debug('Deleted log stream: %s', log_stream)
-    
+    # Get log group name
+    log_group_name = metadata["log_group_name"]
 
-# DB fixtures
+    # Get log stream name
+    log_stream_name = metadata["log_stream_name"]
+
+    # Get number of events
+    event_number = metadata["expected_results"]
+
+    # Generate event information
+    events = [
+        {'timestamp': int(time() * 1000), 'message': f"Test event number {i}"} for i in range(event_number)
+    ]
+
+    try:
+        # Insert log events in log group
+        upload_log_events(
+            log_stream=log_stream_name,
+            log_group=log_group_name,
+            events=events
+        )
+
+    except ClientError as error:
+        logger.error({
+            "message": "Client error creating log stream",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        pass
+
+    except Exception as error:
+        logger.error({
+            "message": "Broad error creating log stream",
+            "log_group": log_group_name,
+            "error": str(error)
+        })
+        pass
+
+
+"""DB fixtures"""
+
+
 @pytest.fixture
 def clean_s3_cloudtrail_db():
     """Delete the DB file before and after the test execution"""
