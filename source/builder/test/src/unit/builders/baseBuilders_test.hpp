f95685cea9f92681213b87553d53dfed1126c858
Javier Bejar Mendez <javier.bejar@wazuh.com>
2024-01-15 12:23:10 +0100

    Added startsWith and exists helpers test

--- source/builder/test/src/unit/builders/baseBuilders_test.hpp
+++ source/builder/test/src/unit/builders/baseBuilders_test.hpp
@@ -7,10 +7,12 @@
 
 #include "builders/types.hpp"
 #include "mockBuildCtx.hpp"
+#include <schemf/mockSchema.hpp>
 
 using namespace base::test;
 using namespace builder::builders;
 using namespace builder::builders::mocks;
+using namespace schemf::mocks;
 
 template<typename Builder, typename Expected>
 using BuilderT = std::tuple<std::vector<OpArg>, Builder, Expected>;
@@ -18,17 +20,36 @@ using BuilderT = std::tuple<std::vector<OpArg>, Builder, Expected>;
 template<typename Builder, typename Expected>
 using OperationT = std::tuple<std::string, Builder, std::string, std::vector<OpArg>, Expected>;
 
+struct Mocks
+{
+    std::shared_ptr<const MockBuildCtx> ctx;
+    std::shared_ptr<const RunState> runState;
+    std::shared_ptr<MockSchema> schema;
+    Context context;
+};
+
 template<typename T>
 class BaseBuilderTest : public testing::TestWithParam<T>
 {
 protected:
-    std::shared_ptr<const MockBuildCtx> ctx;
-    Context context;
-    std::shared_ptr<const RunState> runState;
+    std::shared_ptr<Mocks> mocks;
+
     void SetUp() override
     {
-        ctx = std::make_shared<const MockBuildCtx>();
-        runState = std::make_shared<const RunState>();
+        mocks = std::make_shared<Mocks>();
+        mocks->ctx = std::make_shared<const MockBuildCtx>();
+        mocks->runState = std::make_shared<const RunState>();
+        mocks->schema = std::make_shared<MockSchema>();
+
+        ON_CALL(*mocks->ctx, context()).WillByDefault(testing::ReturnRef(mocks->context));
+        ON_CALL(*mocks->ctx, runState()).WillByDefault(testing::Return(mocks->runState));
+        ON_CALL(*mocks->ctx, schema()).WillByDefault(testing::ReturnRef(*(mocks->schema)));
+    }
+
+    void expectBuildSuccess()
+    {
+        EXPECT_CALL(*mocks->ctx, context()).Times(testing::AtLeast(1));
+        EXPECT_CALL(*mocks->ctx, runState()).Times(testing::AtLeast(1));
     }
 };
 
@@ -59,8 +80,8 @@ auto makeRef(Args&&... args)
 
 namespace filterbuildtest
 {
-using SuccessExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<None, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
@@ -74,8 +95,8 @@ class FilterBuilderTest : public BaseBuilderTest<FilterT>
 
 namespace filteroperatestest
 {
-using SuccessExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<None, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
@@ -89,8 +110,8 @@ class FilterOperationTest : public BaseBuilderTest<FilterT>
 
 namespace mapbuildtest
 {
-using SuccessExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<None, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
@@ -104,8 +125,8 @@ class MapBuilderTest : public BaseBuilderTest<MapT>
 
 namespace mapoperatestest
 {
-using SuccessExpected = InnerExpected<json::Json, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<json::Json, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
@@ -119,8 +140,8 @@ class MapOperationTest : public BaseBuilderTest<MapT>
 
 namespace transformbuildtest
 {
-using SuccessExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<None, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
@@ -134,8 +155,8 @@ class TransformBuilderTest : public BaseBuilderTest<TransformT>
 
 namespace transformoperatestest
 {
-using SuccessExpected = InnerExpected<base::Event, const std::shared_ptr<const MockBuildCtx>&>;
-using FailureExpected = InnerExpected<None, const std::shared_ptr<const MockBuildCtx>&>;
+using SuccessExpected = InnerExpected<base::Event, const Mocks&>;
+using FailureExpected = InnerExpected<None, const Mocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
