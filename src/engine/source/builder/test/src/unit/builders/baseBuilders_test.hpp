b6d1c0832579196ee6dccb1b64af1ba939e3e83d
Javier Bejar Mendez <javier.bejar@wazuh.com>
2024-01-29 11:50:54 +0100

    Renamed helper tests Mocks struct to avoid runtime issues with gtest_main

--- src/engine/source/builder/test/src/unit/builders/baseBuilders_test.hpp
+++ src/engine/source/builder/test/src/unit/builders/baseBuilders_test.hpp
@@ -22,7 +22,7 @@ using BuilderT = std::tuple<std::vector<OpArg>, Builder, Expected>;
 template<typename Builder, typename Expected>
 using OperationT = std::tuple<std::string, Builder, std::string, std::vector<OpArg>, Expected>;
 
-struct Mocks
+struct BuildersMocks
 {
     std::shared_ptr<const MockBuildCtx> ctx;
     std::shared_ptr<const RunState> runState;
@@ -31,15 +31,14 @@ struct Mocks
     Context context;
 };
 
-template<typename T>
-class BaseBuilderTest : public testing::TestWithParam<T>
+class BaseBuilderTest : public testing::Test
 {
 protected:
-    std::shared_ptr<Mocks> mocks;
+    std::shared_ptr<BuildersMocks> mocks;
 
     void SetUp() override
     {
-        mocks = std::make_shared<Mocks>();
+        mocks = std::make_shared<BuildersMocks>();
         mocks->ctx = std::make_shared<const MockBuildCtx>();
         mocks->runState = std::make_shared<const RunState>();
         mocks->schema = std::make_shared<MockSchema>();
@@ -65,9 +64,6 @@ protected:
         EXPECT_CALL(*mocks->ctx, context()).Times(testing::AtLeast(1));
         EXPECT_CALL(*mocks->ctx, runState()).Times(testing::AtLeast(1));
     }
-
-public:
-    virtual ~BaseBuilderTest() = default;
 };
 
 template<typename TestClass>
@@ -97,82 +93,104 @@ auto makeRef(Args&&... args)
 
 namespace filterbuildtest
 {
-using SuccessExpected = InnerExpected<None, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<None, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using FilterT = BuilderT<FilterBuilder, Expc>;
 
-using FilterBuilderTest = BaseBuilderTest<FilterT>;
+class FilterBuilderTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<FilterT>
+{
+};
 } // namespace filterbuildtest
 
 namespace filteroperatestest
 {
-using SuccessExpected = InnerExpected<None, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<None, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using FilterT = OperationT<FilterBuilder, Expc>;
 
-using FilterOperationTest = BaseBuilderTest<FilterT>;
+class FilterOperationTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<FilterT>
+{
+};
 } // namespace filteroperatestest
 
 namespace mapbuildtest
 {
-using SuccessExpected = InnerExpected<None, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<None, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using MapT = BuilderT<MapBuilder, Expc>;
 
-using MapBuilderTest = BaseBuilderTest<MapT>;
+class MapBuilderTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<MapT>
+{
+};
 } // namespace mapbuildtest
 
 namespace mapoperatestest
 {
-using SuccessExpected = InnerExpected<json::Json, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<json::Json, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using MapT = std::tuple<std::string, MapBuilder, std::vector<OpArg>, Expc>;
 
-class MapOperationTest : public BaseBuilderTest<MapT>
+class MapOperationTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<MapT>
 {
 };
 } // namespace mapoperatestest
 
 namespace transformbuildtest
 {
-using SuccessExpected = InnerExpected<None, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<None, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using TransformT = BuilderT<TransformBuilder, Expc>;
 
-using TransformBuilderTest = BaseBuilderTest<TransformT>;
+class TransformBuilderTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<TransformT>
+{
+};
 } // namespace transformbuildtest
 
 namespace transformoperatestest
 {
-using SuccessExpected = InnerExpected<base::Event, const Mocks&>;
-using FailureExpected = InnerExpected<None, const Mocks&>;
+using SuccessExpected = InnerExpected<base::Event, const BuildersMocks&>;
+using FailureExpected = InnerExpected<None, const BuildersMocks&>;
 using Expc = Expected<SuccessExpected, FailureExpected>;
 static auto SUCCESS = Expc::success();
 static auto FAILURE = Expc::failure();
 
 using TransformT = OperationT<TransformBuilder, Expc>;
 
-using TransformOperationTest = BaseBuilderTest<TransformT>;
+class TransformOperationTest
+    : public BaseBuilderTest
+    , public testing::WithParamInterface<TransformT>
+{
+};
 } // namespace transformoperatestest
 
 #endif // _BUILDER_TEST_BASEBUILDERS_HPP
