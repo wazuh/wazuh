0ee1a6505b6c68d2389b8d3f00fcfbfcf1516c61
Jotacarma90 <jose.carreras@wazuh.com>
2024-02-01 11:35:46 +0100

    Fix UT test_fim_diff_registry_tmp_default_type to add new warning msg

--- src/unit_tests/syscheckd/test_fim_diff_changes.c
+++ src/unit_tests/syscheckd/test_fim_diff_changes.c
@@ -1215,7 +1215,9 @@ void test_fim_diff_registry_tmp_default_type(void **state) {
 
     expect_fopen(diff->file_origin, "w", fp);
 
-    expect_string(__wrap__mwarn, formatted_msg, FIM_REG_VAL_WRONG_TYPE);
+    char warn_message[OS_SIZE_1024];
+    snprintf(warn_message, OS_SIZE_1024, FIM_REG_VAL_WRONG_TYPE, diff->file_origin);
+    expect_string(__wrap__mwarn, formatted_msg, warn_message);
 
     expect_fclose(fp, 0);
 
