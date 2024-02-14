adc430fcb99e2b5d7d3da20d1900717e7d703e7d
Nico Stefani <nicolas.stefi@wazuh.com>
2024-02-14 17:12:16 -0300

    Make async HAProxy helper code

--- framework/wazuh/core/cluster/hap_helper/hap_helper.py
+++ framework/wazuh/core/cluster/hap_helper/hap_helper.py
@@ -1,12 +1,13 @@
 import logging
 import time
+from asyncio import sleep
 from math import ceil, floor
 from os import path
 
 from wazuh.core import common
 from wazuh.core.cluster.hap_helper.configuration import parse_configuration
 from wazuh.core.cluster.hap_helper.custom_logging import CustomLogger
-from wazuh.core.cluster.hap_helper.exception import CoordinatorError, ProxyError, WazuhError
+from wazuh.core.cluster.hap_helper.exception import HAPHelperError, ProxyError, WazuhError
 from wazuh.core.cluster.hap_helper.proxy import Proxy, ProxyAPI, ProxyServerState
 from wazuh.core.cluster.hap_helper.wazuh import WazuhAgent, WazuhAPI
 
@@ -28,33 +29,33 @@ class HAPHelper:
         self.agent_tolerance: float = options['agent_tolerance']
         self.remove_disconnected_node_after: int = options['remove_disconnected_node_after']
 
-    def initialize_components(self):
+    async def initialize_components(self):
         try:
-            self.wazuh_api.initialize()
-            self.proxy.initialize()
+            # self.wazuh_api.initialize()
+            await self.proxy.initialize()
             self.logger.info('Main components were initialized')
         except (WazuhError, ProxyError) as init_exc:
             self.logger.critical('Cannot initialize main components')
             self.logger.critical(init_exc)
             exit(1)
 
-    def initialize_wazuh_cluster_configuration(self):
-        if not self.proxy.exists_backend(self.proxy.wazuh_backend):
+    async def initialize_wazuh_cluster_configuration(self):
+        if not await self.proxy.exists_backend(self.proxy.wazuh_backend):
             self.logger.info(f"Could not find Wazuh backend '{self.proxy.wazuh_backend}'")
-            self.proxy.add_new_backend(name=self.proxy.wazuh_backend)
+            await self.proxy.add_new_backend(name=self.proxy.wazuh_backend)
             self.logger.info('Added Wazuh backend')
 
-        if not self.proxy.exists_frontend(f'{self.proxy.wazuh_backend}_front'):
+        if not await self.proxy.exists_frontend(f'{self.proxy.wazuh_backend}_front'):
             self.logger.info(f"Could not find Wazuh frontend '{self.proxy.wazuh_backend}_front'")
-            self.proxy.add_new_frontend(
+            await self.proxy.add_new_frontend(
                 name=f'{self.proxy.wazuh_backend}_front',
                 port=self.proxy.wazuh_connection_port,
                 backend=self.proxy.wazuh_backend,
             )
             self.logger.info('Added Wazuh frontend')
 
-    def check_node_to_delete(self, node_name: str) -> bool:
-        node_downtime = self.proxy.get_wazuh_server_stats(server_name=node_name)['lastchg']
+    async def check_node_to_delete(self, node_name: str) -> bool:
+        node_downtime = (await self.proxy.get_wazuh_server_stats(server_name=node_name))['lastchg']
         self.logger.trace(f"Server '{node_name}' has been disconnected for {node_downtime}s")
 
         if node_downtime < self.remove_disconnected_node_after * 60:
@@ -65,21 +66,24 @@ class HAPHelper:
         )
         return True
 
-    def check_proxy_processes(self, auto_mode: bool = False, warn: bool = True) -> bool:
-        if not self.proxy.is_proxy_process_single():
+    # TODO: This must be deprecated
+    async def check_proxy_processes(self, auto_mode: bool = False, warn: bool = True) -> bool:
+        if not await self.proxy.is_proxy_process_single():
             warn and self.logger.warning('Detected more than one Proxy processes')
             if not auto_mode and input('  Do you wish to fix them? (y/N): ').lower() != 'y':
                 return False
-            self.manage_proxy_processes()
+            await self.manage_proxy_processes()
             return True
 
-    def backend_servers_state_healthcheck(self):
-        for server in self.proxy.get_current_backend_servers().keys():
-            if self.proxy.is_server_drain(server_name=server):
+    async def backend_servers_state_healthcheck(self):
+        for server in (await self.proxy.get_current_backend_servers()).keys():
+            if await self.proxy.is_server_drain(server_name=server):
                 self.logger.warning(f"Server '{server}' was found {ProxyServerState.DRAIN.value.upper()}. Fixing it")
-                self.proxy.allow_server_new_connections(server_name=server)
+                await self.proxy.allow_server_new_connections(server_name=server)
 
-    def obtain_nodes_to_configure(self, wazuh_cluster_nodes: dict, proxy_backend_servers: dict) -> tuple[list, list]:
+    async def obtain_nodes_to_configure(
+        self, wazuh_cluster_nodes: dict, proxy_backend_servers: dict
+    ) -> tuple[list, list]:
         add_nodes, remove_nodes = [], []
 
         for node_name, node_address in wazuh_cluster_nodes.items():
@@ -91,7 +95,7 @@ class HAPHelper:
         for node_name in proxy_backend_servers.keys() - wazuh_cluster_nodes.keys():
             if node_name in self.wazuh_api.excluded_nodes:
                 self.logger.info(f"Server '{node_name}' has been excluded but is currently active. Removing it")
-            elif self.check_node_to_delete(node_name):
+            elif await self.check_node_to_delete(node_name):
                 pass
             else:
                 continue
@@ -99,59 +103,60 @@ class HAPHelper:
 
         return add_nodes, remove_nodes
 
-    def update_agent_connections(self, agent_list: list[str]):
+    async def update_agent_connections(self, agent_list: list[str]):
         self.logger.debug('Reconnecting agents')
         self.logger.debug(
             f'Agent reconnection chunk size is set to {self.agent_reconnection_chunk_size}. '
             f'Total iterations: {ceil(len(agent_list) / self.agent_reconnection_chunk_size)}'
         )
         for index in range(0, len(agent_list), self.agent_reconnection_chunk_size):
-            self.wazuh_api.reconnect_agents(agent_list[index : index + self.agent_reconnection_chunk_size])
+            await self.wazuh_api.reconnect_agents(agent_list[index : index + self.agent_reconnection_chunk_size])
             self.logger.debug(f'Delay between agent reconnections. Sleeping {self.agent_reconnection_time}s...')
-            time.sleep(self.agent_reconnection_time)
+            await sleep(self.agent_reconnection_time)
 
-    def force_agent_reconnection_to_server(self, chosen_server: str, agents_list: list[dict]):
-        current_servers = self.proxy.get_current_backend_servers().keys()
+    async def force_agent_reconnection_to_server(self, chosen_server: str, agents_list: list[dict]):
+        current_servers = (await self.proxy.get_current_backend_servers()).keys()
         affected_servers = current_servers - {chosen_server}
         for server_name in affected_servers:
-            self.proxy.restrain_server_new_connections(server_name=server_name)
-        time.sleep(self.SERVER_ADMIN_STATE_DELAY)
+            await self.proxy.restrain_server_new_connections(server_name=server_name)
+        await sleep(self.SERVER_ADMIN_STATE_DELAY)
         eligible_agents = WazuhAgent.get_agents_able_to_reconnect(agents_list=agents_list)
         if len(eligible_agents) != len(agents_list):
             self.logger.warning(
                 f"Some agents from '{chosen_server}' are not compatible with the reconnection endpoint."
                 ' Those connections will be balanced afterwards'
             )
-        self.update_agent_connections(agent_list=eligible_agents)
+        await self.update_agent_connections(agent_list=eligible_agents)
         for server_name in affected_servers:
-            self.proxy.allow_server_new_connections(server_name=server_name)
-        time.sleep(self.SERVER_ADMIN_STATE_DELAY)
+            await self.proxy.allow_server_new_connections(server_name=server_name)
+        await sleep(self.SERVER_ADMIN_STATE_DELAY)
 
-    def manage_proxy_processes(self):
-        current_proxy_pid = self.proxy.api.get_runtime_info()['pid']
-        response = self.proxy.api.kill_proxy_processes(pid_to_exclude=current_proxy_pid)
+    # TODO: This must be deprecated
+    async def manage_proxy_processes(self):
+        current_proxy_pid = (await self.proxy.api.get_runtime_info())['pid']
+        response = await self.proxy.api.kill_proxy_processes(pid_to_exclude=current_proxy_pid)
 
         if response['error'] > 0:
             self.logger.error("Could not manage all proxy processes: " f"{response['data']}")
         elif len(response['data']) > 0:
             self.logger.info('Managed proxy processes')
 
-    def migrate_old_connections(self, new_servers: list[str], deleted_servers: list[str]):
+    async def migrate_old_connections(self, new_servers: list[str], deleted_servers: list[str]):
         wazuh_backend_stats = {}
         backend_stats_iteration = 1
         while any([server not in wazuh_backend_stats for server in new_servers]):
             if backend_stats_iteration > self.UPDATED_BACKEND_STATUS_TIMEOUT:
                 self.logger.error(f'Some of the new servers did not go UP: {set(new_servers) - wazuh_backend_stats}')
-                raise CoordinatorError(100)
+                raise HAPHelperError(100)
 
             self.logger.debug('Waiting for new servers to go UP')
             time.sleep(1)
             backend_stats_iteration += 1
-            wazuh_backend_stats = self.proxy.get_wazuh_backend_stats().keys()
+            wazuh_backend_stats = (await self.proxy.get_wazuh_backend_stats()).keys()
 
         self.logger.debug('All new servers are UP')
-        previous_agent_distribution = self.wazuh_api.get_agents_node_distribution()
-        previous_connection_distribution = self.proxy.get_wazuh_backend_server_connections() | {
+        previous_agent_distribution = await self.wazuh_api.get_agents_node_distribution()
+        previous_connection_distribution = await self.proxy.get_wazuh_backend_server_connections() | {
             server: len(previous_agent_distribution[server])
             for server in previous_agent_distribution
             if server not in new_servers
@@ -173,17 +178,17 @@ class HAPHelper:
                 pass
 
             self.logger.info(f"Migrating {len(agents)} connections from server '{wazuh_worker}'")
-            self.force_agent_reconnection_to_server(chosen_server=wazuh_worker, agents_list=agents)
+            await self.force_agent_reconnection_to_server(chosen_server=wazuh_worker, agents_list=agents)
 
         if agents_to_balance:
             self.logger.info('Balancing exceeding connections after changes on the Wazuh backend')
-            self.update_agent_connections(agent_list=agents_to_balance)
+            await self.update_agent_connections(agent_list=agents_to_balance)
 
-        self.check_proxy_processes(auto_mode=True, warn=False)
+        await self.check_proxy_processes(auto_mode=True, warn=False)
 
         self.logger.info('Waiting for agent connections stability')
         self.logger.debug(f'Sleeping {self.agent_reconnection_stability_time}s...')
-        time.sleep(self.agent_reconnection_stability_time)
+        await sleep(self.agent_reconnection_stability_time)
 
     def check_for_balance(self, current_connections_distribution: dict) -> dict:
         if not current_connections_distribution:
@@ -217,10 +222,10 @@ class HAPHelper:
 
         return unbalanced_connections
 
-    def calculate_agents_to_balance(self, affected_servers: dict) -> dict:
+    async def calculate_agents_to_balance(self, affected_servers: dict) -> dict:
         agents_to_balance = {}
         for server_name, n_agents in affected_servers.items():
-            agent_candidates = self.wazuh_api.get_agents_belonging_to_node(node_name=server_name, limit=n_agents)
+            agent_candidates = await self.wazuh_api.get_agents_belonging_to_node(node_name=server_name, limit=n_agents)
             eligible_agents = WazuhAgent.get_agents_able_to_reconnect(agents_list=agent_candidates)
             if len(eligible_agents) != len(agent_candidates):
                 self.logger.warning(
@@ -231,22 +236,22 @@ class HAPHelper:
 
         return agents_to_balance
 
-    def balance_agents(self, affected_servers: dict):
+    async def balance_agents(self, affected_servers: dict):
         self.logger.info('Attempting to balance agent connections')
-        agents_to_balance = self.calculate_agents_to_balance(affected_servers)
+        agents_to_balance = await self.calculate_agents_to_balance(affected_servers)
         for node_name, agent_ids in agents_to_balance.items():
             self.logger.info(f"Balancing {len(agent_ids)} agents from '{node_name}'")
-            self.update_agent_connections(agent_list=agent_ids)
+            await self.update_agent_connections(agent_list=agent_ids)
 
-    def manage_wazuh_cluster_nodes(self):
+    async def manage_wazuh_cluster_nodes(self):
         while True:
             try:
-                self.backend_servers_state_healthcheck()
-                self.check_proxy_processes(auto_mode=True) and time.sleep(self.AGENT_STATUS_SYNC_TIME)
-                current_wazuh_cluster = self.wazuh_api.get_cluster_nodes()
-                current_proxy_backend = self.proxy.get_current_backend_servers()
+                await self.backend_servers_state_healthcheck()
+                await self.check_proxy_processes(auto_mode=True) and await sleep(self.AGENT_STATUS_SYNC_TIME)
+                current_wazuh_cluster = await self.wazuh_api.get_cluster_nodes()
+                current_proxy_backend = await self.proxy.get_current_backend_servers()
 
-                nodes_to_add, nodes_to_remove = self.obtain_nodes_to_configure(
+                nodes_to_add, nodes_to_remove = await self.obtain_nodes_to_configure(
                     current_wazuh_cluster, current_proxy_backend
                 )
                 if nodes_to_add or nodes_to_remove:
@@ -256,47 +261,46 @@ class HAPHelper:
                     self.logger.info('Attempting to update proxy backend')
 
                     for node_to_remove in nodes_to_remove:
-                        self.proxy.remove_wazuh_manager(manager_name=node_to_remove)
+                        await self.proxy.remove_wazuh_manager(manager_name=node_to_remove)
 
                     for node_to_add in nodes_to_add:
-                        self.proxy.add_wazuh_manager(
+                        await self.proxy.add_wazuh_manager(
                             manager_name=node_to_add,
                             manager_address=current_wazuh_cluster[node_to_add],
                             resolver=self.proxy.resolver,
                         )
-                    self.migrate_old_connections(new_servers=nodes_to_add, deleted_servers=nodes_to_remove)
+                    await self.migrate_old_connections(new_servers=nodes_to_add, deleted_servers=nodes_to_remove)
                     continue
 
                 self.logger.info('Load balancer backend is up to date')
                 unbalanced_connections = self.check_for_balance(
-                    current_connections_distribution=self.proxy.get_wazuh_backend_server_connections()
+                    current_connections_distribution=await self.proxy.get_wazuh_backend_server_connections()
                 )
                 if not unbalanced_connections:
-                    if self.logger.level <= logging.DEBUG:
-                        self.logger.debug(
-                            'Current backend stats: ' f'{self.proxy.get_wazuh_backend_server_connections()}'
-                        )
+                    self.logger.debug(
+                        'Current backend stats: ' f'{await self.proxy.get_wazuh_backend_server_connections()}'
+                    )
                     self.logger.info('Load balancer backend is balanced')
                 else:
                     self.logger.info('Agent imbalance detected. Waiting for agent status sync...')
-                    time.sleep(self.AGENT_STATUS_SYNC_TIME)
-                    self.balance_agents(affected_servers=unbalanced_connections)
+                    await sleep(self.AGENT_STATUS_SYNC_TIME)
+                    await self.balance_agents(affected_servers=unbalanced_connections)
 
                 self.logger.debug(f'Sleeping {self.sleep_time}s...')
-                time.sleep(self.sleep_time)
-            except (CoordinatorError, ProxyError, WazuhError) as handled_exc:
+                await sleep(self.sleep_time)
+            except (HAPHelperError, ProxyError, WazuhError) as handled_exc:
                 self.logger.error(str(handled_exc))
                 self.logger.warning(
                     f'Tasks may not perform as expected. Sleeping {self.sleep_time}s ' 'before continuing...'
                 )
-                time.sleep(self.sleep_time)
+                await sleep(self.sleep_time)
 
     @classmethod
     async def run(cls):
         try:
             configuration = parse_configuration()
             main_logger, proxy_logger, wazuh_api_logger = setup_loggers(
-                log_level=configuration['hap_helper'].get('log_level', logging.INFO)
+                log_level=configuration['hap_helper'].get('log_level', 'INFO')
             )
 
             proxy_api = ProxyAPI(
@@ -324,24 +328,26 @@ class HAPHelper:
 
             helper = cls(proxy=proxy, wazuh_api=wazuh_api, logger=main_logger, options=configuration['hap_helper'])
 
-            helper.initialize_components()
-            helper.initialize_wazuh_cluster_configuration()
+            await helper.initialize_components()
+            await helper.initialize_wazuh_cluster_configuration()
 
-            main_logger.info('Starting HAProxy Helper on auto mode')
+            helper.logger.info('Starting HAProxy Helper on auto mode')
             await helper.manage_wazuh_cluster_nodes()
-        except (CoordinatorError, ProxyError) as main_exc:
-            main_logger.error(str(main_exc))
+        except (HAPHelperError, ProxyError) as main_exc:
+            helper.logger.error(str(main_exc))
         except KeyboardInterrupt:
             pass
         except Exception as unexpected_exc:
-            main_logger.critical(f'Unexpected exception: {unexpected_exc}', exc_info=True)
+            helper.logger.critical(f'Unexpected exception: {unexpected_exc}', exc_info=True)
         finally:
-            main_logger.info('Process ended')
+            helper.logger.info('Process ended')
 
 
-def setup_loggers(str, log_level: int) -> tuple[logging.Logger, logging.Logger, logging.Logger]:
+def setup_loggers(log_level: str) -> tuple[logging.Logger, logging.Logger, logging.Logger]:
+    log_level = logging.getLevelName(log_level.upper())
+
     log_file_path = path.join(common.WAZUH_LOGS, 'hap_helper.log')
-    main_logger = CustomLogger('wazuh-coordinator', file_path=log_file_path, level=log_level).get_logger()
+    main_logger = CustomLogger('wazuh-haphelper', file_path=log_file_path, level=log_level).get_logger()
     proxy_logger = CustomLogger('proxy-logger', file_path=log_file_path, level=log_level, tag='Proxy').get_logger()
     wazuh_api_logger = CustomLogger(
         'wazuh-api-logger', file_path=log_file_path, level=log_level, tag='Wazuh API'
--- framework/wazuh/core/cluster/hap_helper/wazuh.py
+++ framework/wazuh/core/cluster/hap_helper/wazuh.py
@@ -1,14 +1,12 @@
 import logging
 import re
-import time
 from collections import defaultdict
 from enum import Enum
 
-import requests
-import urllib3
-from wazuh_coordinator.exception import WazuhError
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # type: ignore
+from wazuh.agent import get_agents, reconnect_agents
+from wazuh.cluster import get_nodes_info
+from wazuh.core.cluster.control import get_system_nodes
+from wazuh.core.cluster.dapi.dapi import DistributedAPI
 
 
 class WazuhAPIMethod(Enum):
@@ -56,130 +54,65 @@ class WazuhAPI:
 
         self.token = ''
 
-    def initialize(self):
-        try:
-            requests.get(f'https://{self.address}:{self.port}/', verify=False)
-        except requests.ConnectionError:
-            raise WazuhError(99, extra_msg='Check connectivity and the configuration file')
-        except requests.RequestException as req_exc:
-            raise WazuhError(99, extra_msg=req_exc)
+    async def get_cluster_nodes(self) -> dict:
+        data = await DistributedAPI(
+            f=get_nodes_info,
+            request_type='local_master',
+            is_async=True,
+            logger=self.logger,
+            local_client_arg='lc',
+            nodes=await get_system_nodes(),
+        ).distribute_function()
 
-    def _obtain_token(self, token_endpoint_method: WazuhAPIMethod = WazuhAPIMethod.GET):
-        endpoint = f'https://{self.address}:{self.port}/security/user/authenticate'
-        response = getattr(requests, str(token_endpoint_method.value))(
-            endpoint, auth=(self.username, self.password), verify=False
-        )
-        if response.status_code == 200:
-            self.token = response.json()['data']['token']
-            self.logger.debug(f'Requested API token ({self.username})')
-        elif response.status_code == 405:
-            self._obtain_token(token_endpoint_method=WazuhAPIMethod.POST)
-        elif response.status_code == 401:
-            raise WazuhError(102)
-        else:
-            raise WazuhError(100, extra_msg=f'Full response: {response.status_code} | {response.json()}')
+        return {item['name']: item['ip'] for item in data.affected_items if item['name'] not in self.excluded_nodes}
 
-    def _security_headers(self):
-        if not self.token:
-            self._obtain_token()
+    async def reconnect_agents(self, agent_list: list = None) -> dict:
+        data = await DistributedAPI(
+            f=reconnect_agents,
+            f_kwargs={'agent_list': agent_list},
+            request_type='distributed_master',
+            logger=self.logger,
+        ).distribute_function()
 
-        return {'Authorization': f'Bearer {self.token}'}
+        return data.affected_items
 
-    def _make_request(
-        self,
-        endpoint: str,
-        method: WazuhAPIMethod = WazuhAPIMethod.GET,
-        query_parameters: dict = None,
-        json_body: dict = None,
-    ) -> dict:
-        response = self._retry_request_if_failed(endpoint, method, query_parameters, json_body)
-        if response.status_code == 200:
-            return response.json()
-        elif response.status_code == 401:
-            self._obtain_token()
-            return self._make_request(endpoint, method=method, query_parameters=query_parameters, json_body=json_body)
-        elif response.status_code == 403:
-            raise WazuhError(103, extra_msg=f"Endpoint '{endpoint}'")
-        else:
-            raise WazuhError(101, extra_msg=f'Full response: {response.status_code} | {response.json()}')
-
-    def _retry_request_if_failed(
-        self,
-        endpoint: str,
-        method: WazuhAPIMethod = WazuhAPIMethod.GET,
-        query_parameters: dict = None,
-        json_body: dict = None,
-    ) -> requests.Response:
-        last_handled_exception = ''
-        uri = f'https://{self.address}:{self.port}{endpoint}'
-
-        for _ in range(self.API_RETRIES):
-            try:
-                response = getattr(requests, str(method.value))(
-                    uri, headers=self._security_headers(), json=json_body, params=query_parameters, verify=False
-                )
-                self.logger.trace(
-                    f"{method.value.upper()} '{endpoint}' - Parameters: {query_parameters or {} }"
-                    f' - JSON body: {json_body or {} } [{response.status_code}]'
-                )
-                if response.status_code == 500:
-                    if response.json().get('error', '') == self.TIMEOUT_ERROR_CODE:
-                        last_handled_exception = TimeoutError(response.json()['detail'])
-                        self.logger.debug('Timeout executing API request')
-                    else:
-                        last_handled_exception = WazuhError(101, extra_msg=str(response.json()))
-                        self.logger.debug('Unexpected error executing API request')
-                    time.sleep(1)
-                else:
-                    return response
-            except requests.ConnectionError as request_err:
-                last_handled_exception = request_err
-                self.logger.debug(f'Could not connect to Wazuh API')
-                time.sleep(1)
-        else:
-            raise WazuhError(104, str(last_handled_exception))
-
-    def get_cluster_nodes(self) -> dict:
-        api_response = self._make_request('/cluster/nodes')
-        return {
-            item['name']: item['ip']
-            for item in api_response['data']['affected_items']
-            if item['name'] not in self.excluded_nodes
-        }
-
-    def reconnect_agents(self, agent_list: list = None) -> dict:
-        query_params = None
-        if agent_list is not None:
-            query_params = {'agents_list': ','.join(agent_list)}
-
-        return self._make_request('/agents/reconnect', method=WazuhAPIMethod.PUT, query_parameters=query_params)
-
-    def get_agents_node_distribution(self) -> dict:
+    async def get_agents_node_distribution(self) -> dict:
         agent_distribution = defaultdict(list)
 
-        query_params = {
-            'select': 'node_name,version',
-            'sort': '-version,id',
-            'status': 'active',
+        f_kwargs = {
+            'select': ['node_name', 'version'],
+            'sort': {'fields': ['version', 'id'], 'order': 'desc'},
+            'filters': {'status': 'active'},
             'q': 'id!=000',
             'limit': self.AGENTS_MAX_LIMIT,
         }
-        api_response = self._make_request('/agents', query_parameters=query_params)
 
-        for agent in api_response['data']['affected_items']:
+        data = await DistributedAPI(
+            f=get_agents,
+            f_kwargs=f_kwargs,
+            request_type='local_master',
+            logger=self.logger,
+        ).distribute_function()
+
+        for agent in data.affected_items:
             agent_distribution[agent['node_name']].append({'id': agent['id'], 'version': agent['version']})
 
         return agent_distribution
 
-    def get_agents_belonging_to_node(self, node_name: str, limit: int = None) -> list[dict]:
-        query_params = {
-            'select': 'version',
-            'sort': '-version,id',
-            'status': 'active',
+    async def get_agents_belonging_to_node(self, node_name: str, limit: int = None) -> list[dict]:
+        f_kwargs = {
+            'select': ['version'],
+            'sort': {'fields': ['version', 'id'], 'order': 'desc'},
+            'filters': {'status': 'active', 'node_name': node_name},
             'q': 'id!=000',
-            'node_name': node_name,
             'limit': limit or self.AGENTS_MAX_LIMIT,
         }
-        api_response = self._make_request('/agents', query_parameters=query_params)
 
-        return api_response['data']['affected_items']
+        data = await DistributedAPI(
+            f=get_agents,
+            f_kwargs=f_kwargs,
+            request_type='local_master',
+            logger=self.logger,
+        ).distribute_function()
+
+        return data.affected_items
