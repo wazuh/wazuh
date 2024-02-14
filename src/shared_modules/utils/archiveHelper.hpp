879bc86f999698f7bc5d169468b5fb261862c0c8
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2024-02-13 23:23:25 -0300

    CL:
    * Improve function documentation.
    * Use proper function argument and variable names.
    * Improve if-else statements.
    * Add check for EOF
    * Exception if multiple files that match the vd regex are found

--- src/shared_modules/utils/archiveHelper.hpp
+++ src/shared_modules/utils/archiveHelper.hpp
@@ -28,14 +28,13 @@ struct Deleter
     }
 };
 
+// C++ smart pointers usage to avoid manually free allocated memory.
 using ArchiveReadPtr = std::unique_ptr<
     struct archive,
     Deleter<decltype(&archive_read_close), decltype(&archive_read_free), archive_read_close, archive_read_free>>;
 using ArchiveWritePtr = std::unique_ptr<
     struct archive,
     Deleter<decltype(&archive_write_close), decltype(&archive_write_free), archive_write_close, archive_write_free>>;
-
-using ArchiveEntryPtr = std::unique_ptr<struct archive_entry>;
 
 namespace Utils
 {
@@ -46,23 +45,28 @@ namespace Utils
     class ArchiveHelper
     {
     private:
-        static void copyData(struct archive* ar, struct archive* aw)
+        static void copyData(struct archive* archiveRead, struct archive* archiveWrite)
         {
-            int r;
+            int retVal;
             const void* buff;
             size_t size;
             int64_t offset;
 
             for (;;)
             {
-                r = archive_read_data_block(ar, &buff, &size, &offset);
-                if (r == ARCHIVE_EOF)
-                    return;
-                if (r != ARCHIVE_OK)
-                    throw std::runtime_error("Couldn't read file");
-                if (archive_write_data_block(aw, buff, size, offset) != ARCHIVE_OK)
+                retVal = archive_read_data_block(archiveRead, &buff, &size, &offset);
+                if (retVal == ARCHIVE_EOF)
                 {
-                    throw std::runtime_error(archive_error_string(aw));
+                    break;
+                }
+                else if (retVal != ARCHIVE_OK)
+                {
+                    throw std::runtime_error("Couldn't read file");
+                }
+
+                if (archive_write_data_block(archiveWrite, buff, size, offset) != ARCHIVE_OK)
+                {
+                    throw std::runtime_error(archive_error_string(archiveWrite));
                 }
             }
         }
@@ -76,64 +80,70 @@ namespace Utils
         /**
          * @brief Uncompress TAR file.
          *
-         * @param filePath Compressed (.tar) file path.
+         * @param filename Compressed (.tar) file name.
+         * @param outputDir Destination path.
+         * @param extractOnly Compressed element to extract.
+         * @param flags Extraction flags.
          */
         static void decompress(const std::string& filename,
-                               const std::string& output = "",
+                               const std::string& outputDir = "",
                                const std::vector<std::string>& extractOnly = {},
-                               int flags = ARCHIVE_EXTRACT_UNLINK)
+                               int flags = 0)
         {
             struct archive_entry* entry;
-            int r;
+            int retVal;
             ArchiveReadPtr archiveRead(archive_read_new());
             ArchiveWritePtr archiveWrite(archive_write_disk_new());
 
             archive_write_disk_set_options(archiveRead.get(), flags);
             archive_read_support_format_tar(archiveRead.get());
 
-            r = archive_read_open_filename(archiveRead.get(), filename.c_str(), 0);
+            retVal = archive_read_open_filename(archiveRead.get(), filename.c_str(), 0);
 
-            if (r != ARCHIVE_OK)
+            if (retVal == ARCHIVE_EOF)
+            {
+                return;
+            }
+            else if (retVal != ARCHIVE_OK)
             {
                 throw std::runtime_error("Couldn't open file");
             }
 
             for (;;)
             {
-                r = archive_read_next_header(archiveRead.get(), &entry);
-                if (r == ARCHIVE_EOF)
-                    break;
-                if (r != ARCHIVE_OK)
+                retVal = archive_read_next_header(archiveRead.get(), &entry);
+                if (retVal == ARCHIVE_EOF)
+                {
+                    return;
+                }
+                else if (retVal != ARCHIVE_OK)
                 {
                     throw std::runtime_error("Couldn't read file");
                 }
 
-                std::filesystem::path outputDir(std::filesystem::current_path() / output /
-                                                archive_entry_pathname(entry));
+                std::filesystem::path outputDirPath(std::filesystem::current_path() / outputDir /
+                                                    archive_entry_pathname(entry));
 
                 if (std::find_if(extractOnly.cbegin(),
                                  extractOnly.cend(),
-                                 [&outputDir](const std::string& path)
+                                 [&outputDirPath](const std::string& path)
                                  {
-                                     size_t pos = outputDir.string().find(path);
+                                     size_t pos = outputDirPath.string().find(path);
                                      return pos != std::string::npos;
                                  }) != extractOnly.cend() ||
                     extractOnly.empty())
                 {
-                    archive_entry_set_pathname(entry, outputDir.c_str());
-                    r = archive_write_header(archiveWrite.get(), entry);
-                    if (r != ARCHIVE_OK)
+                    archive_entry_set_pathname(entry, outputDirPath.c_str());
+                    if (archive_write_header(archiveWrite.get(), entry) != ARCHIVE_OK)
                     {
                         throw std::runtime_error(archive_error_string(archiveWrite.get()));
                     }
-                    else
+
+                    copyData(archiveRead.get(), archiveWrite.get());
+                    retVal = archive_write_finish_entry(archiveWrite.get());
+                    if (retVal != ARCHIVE_OK)
                     {
-                        copyData(archiveRead.get(), archiveWrite.get());
-                        r = archive_write_finish_entry(archiveWrite.get());
-                        if (r != ARCHIVE_OK)
-                        {
-                            throw std::runtime_error(archive_error_string(archiveWrite.get()));
-                        }
+                        throw std::runtime_error(archive_error_string(archiveWrite.get()));
                     }
                 }
             }
