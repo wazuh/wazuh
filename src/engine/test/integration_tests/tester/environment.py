8fa10aa7a47d0d2500c6dcd4d3574c1df53e625e
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2024-02-20 16:38:39 +0000

    Add utils librery for integration test

--- src/engine/test/integration_tests/tester/environment.py
+++ src/engine/test/integration_tests/tester/environment.py
@@ -1,45 +1,9 @@
-import os
-import signal
-import subprocess
-import shlex
-import time
+from handler_engine_instance import up_down
 
-BINARY_PATH = os.environ.get("WAZUH_DIR", "") + "/src/engine/build/main"
-CONF_FILE = os.environ.get("CONF_FILE", "")
-
-def send_interrupt_signal(pid):
-    """
-    Sends an interrupt signal (SIGINT) to a specified process.
-    """
-    try:
-        os.kill(pid, signal.SIGINT)
-        print(f"Interrupt signal sent to the process with PID {pid}")
-    except ProcessLookupError:
-        print(f"Could not find the process with PID {pid}")
+up_down_engine = up_down.UpDownEngine()
 
 def before_all(context):
-    """
-    Executes actions before the start of all scenarios.
-    """
-    # Command to execute the binary in the background
-    command = f"{BINARY_PATH} --config {CONF_FILE} server -l error --api_timeout 1000 start"
-
-    # Split the command into a list of arguments using shlex
-    args = shlex.split(command)
-
-    # Execute the process in the background with subprocess.Popen
-    process = subprocess.Popen(args)
-
-    # Wait for a moment to ensure the process has started
-    time.sleep(2)
-
-    # Get the PID of the process and store it in the context
-    context.pid = process.pid
+    up_down_engine.send_start_command()
 
 def after_all(context):
-    """
-    Executes actions after the completion of all scenarios.
-    """
-    # Send an interrupt signal to the background process using the stored PID
-    send_interrupt_signal(context.pid)
-    time.sleep(2)
+    up_down_engine.send_stop_command()
