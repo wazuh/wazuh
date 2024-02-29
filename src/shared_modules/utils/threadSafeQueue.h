8ffb3e995b9509c163653f1b4f59f057ca8db962
Dwordcito <octa.tala@gmail.com>
2024-02-29 10:45:19 -0300

    Fix invalid iteration.

--- src/shared_modules/utils/threadSafeQueue.h
+++ src/shared_modules/utils/threadSafeQueue.h
@@ -11,184 +11,180 @@
 
 #ifndef THREAD_SAFE_QUEUE_H
 #define THREAD_SAFE_QUEUE_H
-#include <queue>
-#include <mutex>
-#include <memory>
 #include <atomic>
 #include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <queue>
 
 namespace Utils
 {
 
-    template<typename T, typename U, typename Tq=std::queue<T>>
+    template<typename T, typename U, typename Tq = std::queue<T>>
     class TSafeQueue
     {
-        public:
-            TSafeQueue()
-                : m_canceled{ false }
-            {}
-            TSafeQueue& operator=(const TSafeQueue&) = delete;
-            TSafeQueue(TSafeQueue& other)
-                : TSafeQueue{}
+    public:
+        TSafeQueue()
+            : m_canceled {false}
+        {
+        }
+        TSafeQueue& operator=(const TSafeQueue&) = delete;
+        TSafeQueue(TSafeQueue& other)
+            : TSafeQueue {}
+        {
+            std::lock_guard<std::mutex> lock {other.m_mutex};
+            m_queue = other.m_queue;
+        }
+        explicit TSafeQueue(Tq&& queue)
+            : m_queue {std::move(queue)}
+            , m_canceled {false}
+        {
+        }
+        ~TSafeQueue()
+        {
+            cancel();
+        }
+
+        void push(const T& value)
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
+
+            if (!m_canceled)
             {
-                std::lock_guard<std::mutex> lock{ other.m_mutex };
-                m_queue = other.m_queue;
+                m_queue.push(value);
+                m_cv.notify_one();
             }
-            explicit TSafeQueue(Tq&& queue)
-                : m_queue{ std::move(queue) }
-                , m_canceled{ false }
-            {}
-            ~TSafeQueue()
+        }
+
+        bool pop(U& value, const bool wait = true)
+        {
+            std::unique_lock<std::mutex> lock {m_mutex};
+
+            if (wait)
             {
-                cancel();
-            }
-
-            void push(const T& value)
-            {
-                std::lock_guard<std::mutex> lock{ m_mutex };
-
-                if (!m_canceled)
-                {
-                    m_queue.push(value);
-                    m_cv.notify_one();
-                }
-            }
-
-            bool pop(U& value, const bool wait = true)
-            {
-                std::unique_lock<std::mutex> lock{ m_mutex };
-
-                if (wait)
-                {
-                    m_cv.wait(lock, [this]()
-                    {
-                        return !m_queue.empty() || m_canceled;
-                    });
-                }
-
-                const bool ret {!m_canceled&& !m_queue.empty()};
-
-                if (ret)
-                {
-                    value = std::move(m_queue.front());
-                    m_queue.pop();
-                }
-
-                return ret;
+                m_cv.wait(lock, [this]() { return !m_queue.empty() || m_canceled; });
             }
 
-            std::shared_ptr<U> pop(const bool wait = true)
+            const bool ret {!m_canceled && !m_queue.empty()};
+
+            if (ret)
             {
-                std::unique_lock<std::mutex> lock{ m_mutex };
-
-                if (wait)
-                {
-                    m_cv.wait(lock, [this]()
-                    {
-                        return !m_queue.empty() || m_canceled;
-                    });
-                }
-
-                const bool ret {!m_canceled&& !m_queue.empty()};
-
-                if (ret)
-                {
-                    const auto spData{ std::make_shared<U>(m_queue.front()) };
-                    m_queue.pop();
-                    return spData;
-                }
-
-                return nullptr;
+                value = std::move(m_queue.front());
+                m_queue.pop();
             }
 
-            std::queue<U> getBulk(const uint64_t elementsQuantity,
-                                  const std::chrono::seconds& timeout = std::chrono::seconds(5))
+            return ret;
+        }
+
+        std::shared_ptr<U> pop(const bool wait = true)
+        {
+            std::unique_lock<std::mutex> lock {m_mutex};
+
+            if (wait)
             {
-                std::unique_lock<std::mutex> lock{ m_mutex };
-                std::queue<U> bulkQueue;
-
-                auto timeoutReached = false;
-
-                // Lambda to check if the condition is met
-                // (queue size is greater than or equal to the number of elements to be extracted)
-                // or if the queue is not empty and the thread has been canceled.
-                auto condition = [this, elementsQuantity]()
-                {
-                    return m_queue.size() >= elementsQuantity || (!m_queue.empty() && m_canceled);
-                };
-
-                // Wait until the condition is met or the timeout is reached.
-                // If the condition is met, extract the elements from the queue.
-                // If the timeout is reached, extract the elements from the queue
-                while (!condition() && !timeoutReached)
-                {
-                    if (m_cv.wait_for(lock, timeout, condition))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        timeoutReached = true;
-                    }
-                }
-
-                // If the timeout is reached or the queue is not empty and the thread has been canceled,
-                // extract the elements from the queue.
-                if (!timeoutReached || (!m_queue.empty() && !m_canceled))
-                {
-                    for (auto i = 0; i < elementsQuantity && !m_queue.empty(); ++i)
-                    {
-                        bulkQueue.push(std::move(m_queue.front()));
-                    }
-                }
-
-                return bulkQueue;
+                m_cv.wait(lock, [this]() { return !m_queue.empty() || m_canceled; });
             }
 
-            void popBulk(const uint64_t elementsQuantity)
-            {
-                std::lock_guard<std::mutex> lock{ m_mutex };
+            const bool ret {!m_canceled && !m_queue.empty()};
 
+            if (ret)
+            {
+                const auto spData {std::make_shared<U>(m_queue.front())};
+                m_queue.pop();
+                return spData;
+            }
+
+            return nullptr;
+        }
+
+        std::queue<U> getBulk(const uint64_t elementsQuantity,
+                              const std::chrono::seconds& timeout = std::chrono::seconds(5))
+        {
+            std::unique_lock<std::mutex> lock {m_mutex};
+            std::queue<U> bulkQueue;
+
+            auto timeoutReached = false;
+
+            // Lambda to check if the condition is met
+            // (queue size is greater than or equal to the number of elements to be extracted)
+            // or if the queue is not empty and the thread has been canceled.
+            auto condition = [this, elementsQuantity]()
+            {
+                return m_queue.size() >= elementsQuantity || (!m_queue.empty() && m_canceled);
+            };
+
+            // Wait until the condition is met or the timeout is reached.
+            // If the condition is met, extract the elements from the queue.
+            // If the timeout is reached, extract the elements from the queue
+            while (!condition() && !timeoutReached)
+            {
+                if (m_cv.wait_for(lock, timeout, condition))
+                {
+                    break;
+                }
+                else
+                {
+                    timeoutReached = true;
+                }
+            }
+
+            // If the timeout is reached or the queue is not empty and the thread has been canceled,
+            // extract the elements from the queue.
+            if (!timeoutReached || (!m_queue.empty() && !m_canceled))
+            {
                 for (auto i = 0; i < elementsQuantity && !m_queue.empty(); ++i)
                 {
-                    m_queue.pop();
+                    bulkQueue.push(std::move(m_queue.at(i)));
                 }
             }
 
-            bool empty() const
-            {
-                std::lock_guard<std::mutex> lock{ m_mutex };
-                return m_queue.empty();
-            }
+            return bulkQueue;
+        }
 
-            size_t size() const
-            {
-                std::lock_guard<std::mutex> lock{ m_mutex };
-                return m_queue.size();
-            }
+        void popBulk(const uint64_t elementsQuantity)
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
 
-            void cancel()
+            for (auto i = 0; i < elementsQuantity && !m_queue.empty(); ++i)
             {
-                std::lock_guard<std::mutex> lock{ m_mutex };
-                m_canceled = true;
-                m_cv.notify_all();
+                m_queue.pop();
             }
+        }
 
-            bool cancelled() const
-            {
-                std::lock_guard<std::mutex> lock{ m_mutex };
-                return m_canceled;
-            }
+        bool empty() const
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
+            return m_queue.empty();
+        }
 
-        private:
-            mutable std::mutex m_mutex;
-            std::condition_variable m_cv;
-            std::atomic<bool> m_canceled{};
-            Tq m_queue;
+        size_t size() const
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
+            return m_queue.size();
+        }
+
+        void cancel()
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
+            m_canceled = true;
+            m_cv.notify_all();
+        }
+
+        bool cancelled() const
+        {
+            std::lock_guard<std::mutex> lock {m_mutex};
+            return m_canceled;
+        }
+
+    private:
+        mutable std::mutex m_mutex;
+        std::condition_variable m_cv;
+        std::atomic<bool> m_canceled {};
+        Tq m_queue;
     };
 
     template<typename T, typename Tq = std::queue<T>>
     using SafeQueue = TSafeQueue<T, T, Tq>;
-}//namespace Utils
+} // namespace Utils
 
-#endif //THREAD_SAFE_QUEUE_H
+#endif // THREAD_SAFE_QUEUE_H
