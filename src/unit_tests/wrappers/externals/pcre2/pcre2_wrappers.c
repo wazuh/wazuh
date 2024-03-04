aa648aaedc1e903bef36ed60240517accb8338ba
Julian Morales <jmorales@unc.edu.ar>
2024-03-04 18:01:30 +0000

    Implemente enable-diable PCRE2 wrappers

--- src/unit_tests/wrappers/externals/pcre2/pcre2_wrappers.c
+++ src/unit_tests/wrappers/externals/pcre2/pcre2_wrappers.c
@@ -14,23 +14,53 @@
 #include <cmocka.h>
 #include "pcre2_wrappers.h"
 
-pcre2_match_data_8 * wrap_pcre2_match_data_create_from_pattern(__attribute__((unused))pcre2_code_8 * code,
-                                                               __attribute__((unused))void* aux) {
-    return mock_type(pcre2_match_data_8 *);
+static bool g_enable_pcre2 = false;
+
+void w_test_pcre2_wrappers(bool enable)
+{
+    g_enable_pcre2 = !enable;
 }
 
-int wrap_pcre2_match(__attribute__((unused))pcre2_code_8 * code_match_data,
-                __attribute__((unused))const PCRE2_UCHAR8 * str_test,
-                __attribute__((unused))size_t strlen,
-                __attribute__((unused))int a, __attribute__((unused))int b,
-                __attribute__((unused))pcre2_match_data_8 * match_data, __attribute__((unused))void * aux) {
+pcre2_match_data_8* wrap_pcre2_match_data_create_from_pattern(pcre2_code_8* code, void* aux)
+{
+
+    if (g_enable_pcre2)
+    {
+        // Print macro
+        return pcre2_match_data_create_from_pattern(code, aux);
+    }
+    return mock_type(pcre2_match_data_8*);
+}
+
+int wrap_pcre2_match(pcre2_code_8* code_match_data,
+                     const PCRE2_UCHAR8* str_test,
+                     size_t strlen,
+                     int a,
+                     int b,
+                     pcre2_match_data_8* match_data,
+                     void* aux)
+{
+    if (g_enable_pcre2)
+    {
+        return pcre2_match(code_match_data, str_test, strlen, a, b, match_data, aux);
+    }
     return mock();
 }
 
-void wrap_pcre2_match_data_free(__attribute__((unused))pcre2_match_data_8 * match_data) {
+void wrap_pcre2_match_data_free(pcre2_match_data_8* match_data)
+{
+    if (g_enable_pcre2)
+    {
+        pcre2_match_data_free(match_data);
+    }
     return;
 }
 
-size_t* wrap_pcre2_get_ovector_pointer(__attribute__((unused))pcre2_match_data_8 * match_data) {
+size_t* wrap_pcre2_get_ovector_pointer(pcre2_match_data_8* match_data)
+{
+    if (g_enable_pcre2)
+    {
+        return pcre2_get_ovector_pointer(match_data);
+    }
     return mock_type(size_t*);
 }
