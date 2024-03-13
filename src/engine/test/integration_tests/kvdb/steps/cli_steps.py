aee16ce66d43b3990094078655890580e3eed669
Nahuel <nahuelfigueroa978@hotmail.com>
2024-03-13 05:50:39 +0000

    Improve integration test for API and CLI kvdb

--- src/engine/test/integration_tests/kvdb/steps/cli_steps.py
+++ src/engine/test/integration_tests/kvdb/steps/cli_steps.py
@@ -14,102 +14,39 @@ CLI_KVDB = communication.CLIClient(EXEC_PATH, SOCKET_PATH)
 # First Scenario
 @given('I have access to the KVDB CLI')
 def step_impl(context):
-    kvdbs_available_json = CLI_KVDB.execute_command("kvdb list")
-    assert len(kvdbs_available_json) != 0, f'leng: {len(kvdbs_available_json)}'
+    code, output = CLI_KVDB.execute_command("kvdb list")
+    assert code == 0, f"{output}"
+    kvdbs = output.split('\n')
+
+    for kvdb in kvdbs:
+        code, output = CLI_KVDB.execute_command(f"kvdb delete -n {kvdb}")
 
 @when('I run the command "{command}"')
 def step_impl(context, command: str):
-    context.code, context.result = CLI_KVDB.execute_command(command)
-    CLI_KVDB.execute_command("kvdb delete --name TestDB1")
+    context.code, context.output = CLI_KVDB.execute_command(command)
 
-@then('I should receive a {response} message with the new database information')
-def step_impl(context, response: str):
-    if response == "success":
-        assert 0 == context.code
-    else:
-        assert 7 == context.code
-
-
-# Second Scenario
-@given('I have already created a database named "{database_name}" using the KVDB CLI')
-def step_impl(context, database_name: str):
-    CLI_KVDB.execute_command(f"kvdb create --name {database_name}")
-
-@when('I execute the command "{command}"')
-def step_impl(context, command: str):
-    context.result = CLI_KVDB.execute_command(command)
-    CLI_KVDB.execute_command("kvdb delete --name TestDB")
-
-@then('I should receive an error message indicating that "{response}"')
-def step_impl(context, response: str):
-    context.result == response
-
-
-# Third Scenario
-@given('I have a database named "{database_name}" created using the KVDB CLI')
-def step_impl(context, database_name: str):
-    CLI_KVDB.execute_command(f"kvdb create --name {database_name}")
-
-@when('I run the command "{command}')
-def step_impl(context, command: str):
-    context.code, context.result = CLI_KVDB.execute_command(command)
-
-@then('I should receive a {response} message indicating that "The database TestDB has been deleted."')
-def step_impl(context, response: str):
-    assert context.result != response
-    if response == "success":
-        assert 0 == context.code
-    else:
-        assert 7 == context.code
-
-# Fourth Scenario
-@then('I should receive a {response} message with the new key-value pair information')
-def step_impl(context, response: str):
-    if response == "success":
-        assert 0 == context.code
-    else:
-        assert 7 == context.code
-
-
-# Fifth Scenario
-@given('I have already added a key-value pair with the key "{key_name}"')
-def step_impl(context, key_name: str):
-    CLI_KVDB.execute_command(f"kvdb insert -n TestDB -k {key_name} -v sampleValue")
-
-@then('I should receive for CLI a {result} indicating that the key value has been updated')
-def step_impl(context, result: str):
-    if result == "success":
-        assert 0 == context.code
-    else:
-        assert 7 == context.code
-
-
-# Sixth Scenario
-@then('I should receive a {response} message indicating that the key-value pair with the key "sampleKey" has been deleted')
-def step_impl(context, response: str):
-    if response == "success":
-        assert 0 == context.code
-    else:
-        assert 7 == context.code
-
-
-# Seventh Scenario
 @when('I add using CLI in the database "{database_name}" {i} key-value pairs with the key called "{key_name}"_id and another {j} key-value pairs with the key called "{other_key_name}"_id')
 def step_impl(context, database_name:str , i: str, key_name: str, j: str, other_key_name: str):
     CLI_KVDB.execute_command(f"kvdb create -n {database_name}")
     for first in range(int(i)):
         name = key_name + "_" + str(first)
-        CLI_KVDB.execute_command(f"kvdb insert -n {database_name} -k {name} -v sampleValue")
+        context.code, context.output = CLI_KVDB.execute_command(f"kvdb insert -n {database_name} -k {name} -v sampleValue")
     for second in range(int(j)):
         name = other_key_name + "_" + str(second)
-        CLI_KVDB.execute_command(f"kvdb insert -n {database_name} -k {name} -v sampleValue")
+        context.code, context.output = CLI_KVDB.execute_command(f"kvdb insert -n {database_name} -k {name} -v sampleValue")
 
-@when('I run from CLI the command "{command}"')
-def step_impl(context, command: str):
-    context.result = CLI_KVDB.execute_command(command)
-    CLI_KVDB.execute_command(f"kvdb delete -n TestDBsearch")
+@then('I should receive a {response} message')
+def step_impl(context, response: str):
+    if response == "success":
+        assert 0 == context.code, f"{context.output}"
+    else:
+        assert 7 == context.code, f"{context.output}"
+
+@then('I should receive an error message indicating that "{response}"')
+def step_impl(context, response: str):
+    assert context.output.strip("\n") == response, f"{repr(context.output)}"
 
 @then('I should receive a JSON of entries with the {size} key-value pairs whose keyname contains the prefix.')
 def step_impl(context, size: str):
-    print(context.result)
-
+    kvdbs = eval(context.output)
+    assert len(kvdbs) == int(size), f"{context.output}"
