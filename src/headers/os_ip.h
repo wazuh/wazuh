0224b16af62ceadc217e995721fb3278d31e9cad
Julian Morales <jmorales@unc.edu.ar>
2024-03-14 19:10:19 +0000

    Improve code style

--- src/headers/os_ip.h
+++ src/headers/os_ip.h
@@ -2,14 +2,10 @@
 #define SHARED_OS_IP_H
 
 #define w_free_os_ip(x)                                                                                                \
-    if (x)                                                                                                             \
-    {                                                                                                                  \
-        if (x->is_ipv6)                                                                                                \
-        {                                                                                                              \
+    if (x) {                                                                                                           \
+        if (x->is_ipv6) {                                                                                              \
             os_free(x->ipv6)                                                                                           \
-        }                                                                                                              \
-        else                                                                                                           \
-        {                                                                                                              \
+        } else {                                                                                                       \
             os_free(x->ipv4)                                                                                           \
         };                                                                                                             \
         os_free(x->ip);                                                                                                \
@@ -17,27 +13,23 @@
     }
 
 /* IPv4 structure */
-typedef struct _os_ipv4
-{
+typedef struct _os_ipv4 {
     unsigned int ip_address;
     unsigned int netmask;
 } os_ipv4;
 
 /* IPv6 structure */
-typedef struct _os_ipv6
-{
+typedef struct _os_ipv6 {
     uint8_t ip_address[16];
     uint8_t netmask[16];
 } os_ipv6;
 
 /* IP structure */
-typedef struct _os_ip
-{
-    char* ip;
-    union
-    {
-        os_ipv4* ipv4;
-        os_ipv6* ipv6;
+typedef struct _os_ip {
+    char * ip;
+    union {
+        os_ipv4 * ipv4;
+        os_ipv6 * ipv6;
     };
     bool is_ipv6;
 } os_ip;
