4d1f0eb32e599f95d1070aa54f847ba4f724679d
sebasfalcone <sebastian.falcone@wazuh.com>
2024-03-25 10:16:34 +0100

    CL: - socketWrapper   - Added UT   - Improved throw message

--- src/shared_modules/utils/socketWrapper.hpp
+++ src/shared_modules/utils/socketWrapper.hpp
@@ -84,7 +84,7 @@ public:
         m_unixAddr.sun_family = AF_UNIX;
         if (path.size() >= sizeof(m_unixAddr.sun_path))
         {
-            throw std::runtime_error {"Error setting socket path too long"};
+            throw std::runtime_error {"Error setting socket path (too long)"};
         }
 
         std::copy(path.begin(), path.end(), m_unixAddr.sun_path);
