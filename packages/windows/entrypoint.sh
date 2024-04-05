31d82ca7d6bf0d8ce8fb675b7a56b6dd3ed56918
Jose Luis Carreras Marín <jotacarma90@gmail.com>
2024-04-05 16:14:09 +0200

    Migrate and adapt Windows wazuh agent packages subsystem (#22631)
    * Modified windows generate package script and create workflow to use it
    
    * Introduce the new changes and fixes
    
    * Fix zip name
    
    * Generate and push msi to s3
    
    * Check download step
    
    * Adapted Windows package workflow to generate .zip as artifact
    
    * Modified generate msi script to set msi name with parameter
    
    * Added smoke test install after msi creation
    
    * Fix some jobs name and inputs descriptions
    
    ---------
    
    Co-authored-by: Luis Enrique Chico Capistrano <luis.chico@wazuh.com>

--- packages/windows/entrypoint.sh
+++ packages/windows/entrypoint.sh
@@ -2,23 +2,27 @@
 
 set -ex
 
-BRANCH=$1
-JOBS=$2
-DEBUG=$3
-REVISION=$4
-TRUST_VERIFICATION=$5
-CA_NAME=$6
-
-URL_REPO=https://github.com/wazuh/wazuh/archive/${BRANCH}.zip
-
-# Download the wazuh repository
-wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
+JOBS=$1
+DEBUG=$2
+ZIP_NAME=$3
+TRUST_VERIFICATION=$4
+CA_NAME=$5
 
 # Compile the wazuh agent for Windows
 FLAGS="-j ${JOBS} IMAGE_TRUST_CHECKS=${TRUST_VERIFICATION} CA_NAME=\"${CA_NAME}\" "
 
 if [[ "${DEBUG}" = "yes" ]]; then
-    FLAGS+="-d "
+    FLAGS+="DEBUG=1 "
+fi
+
+if [ -z "${BRANCH}"]; then
+    mkdir /wazuh-local-src
+    cp -r /local-src/* /wazuh-local-src
+else
+    URL_REPO=https://github.com/wazuh/wazuh/archive/${BRANCH}.zip
+
+    # Download the wazuh repository
+    wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
 fi
 
 bash -c "make -C /wazuh-*/src deps TARGET=winagent ${FLAGS}"
@@ -26,10 +30,4 @@ bash -c "make -C /wazuh-*/src TARGET=winagent ${FLAGS}"
 
 rm -rf /wazuh-*/src/external
 
-# Zip the compiled agent and move it to the shared folder
-short_commit_hash="$(curl -s https://api.github.com/repos/wazuh/wazuh/commits/${BRANCH} \
-                          | grep '"sha"' | head -n 1| cut -d '"' -f 4 | cut -c 1-7)"
-version="$(cat /wazuh-*/src/VERSION| cut -d 'v' -f 2)"
-ZIP_NAME="wazuh-agent_${version}-${REVISION}_windows_${short_commit_hash}.zip"
-zip -r ${ZIP_NAME} wazuh-*
-cp ${ZIP_NAME} /shared
+zip -r /shared/${ZIP_NAME} /wazuh-*
