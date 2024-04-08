9f0d0c488cd932af0c36db7e8d53898f2baf67e1
JCabreraC <jcabre88@gmail.com>
2024-04-08 08:18:15 +0000

    Added load_and_validate_function unit test

--- src/unit_tests/logcollector/test_journal_log.c
+++ src/unit_tests/logcollector/test_journal_log.c
@@ -21,6 +21,21 @@
 #include "../wrappers/externals/pcre2/pcre2_wrappers.h"
 
 bool is_owned_by_root(const char * library_path);
+bool load_and_validate_function(void * handle, const char * name, void ** func);
+
+// Mock dlsym function to simulate the loading of valid and invalid functions
+void *__wrap_dlsym(void *handle, const char *name) {
+    if (strcmp(name, "valid_function") == 0) {
+        return (void *)1; // Valid function
+    } else {
+        return NULL; // Invalid function
+    }
+}
+
+// Mock dlerror
+char* __wrap_dlerror() {
+    return "ERROR";
+}
 
 /* setup/teardown */
 
@@ -38,6 +53,8 @@ static int group_teardown(void ** state) {
 
 }
 
+// Test is_owned_by_root
+
 void test_is_owned_by_root_root_owned(void **state) {
     (void)state;
 
@@ -46,13 +63,13 @@ void test_is_owned_by_root_root_owned(void **state) {
     struct stat mock_stat;
     mock_stat.st_uid = 0;
 
-    // Assert
     expect_value(__wrap_stat, __file, library_path);
     will_return(__wrap_stat, &mock_stat);
     will_return(__wrap_stat, 0);
 
     bool result = is_owned_by_root(library_path);
 
+    // Assert
     assert_true(result);
 }
 
@@ -64,13 +81,13 @@ void test_is_owned_by_root_not_root_owned(void **state) {
     struct stat mock_stat;
     mock_stat.st_uid = 1000;
 
-    // Assert
     expect_value(__wrap_stat, __file, library_path);
     will_return(__wrap_stat, &mock_stat);
     will_return(__wrap_stat, 0);
 
     bool result = is_owned_by_root(library_path);
 
+    // Assert
     assert_false(result);
 }
 
@@ -82,16 +99,47 @@ void test_is_owned_by_root_stat_fails(void **state) {
     struct stat mock_stat;
     mock_stat.st_uid = 1000;
 
-    // Assert
     expect_string(__wrap_stat, __file, library_path);
     will_return(__wrap_stat, &mock_stat);
     will_return(__wrap_stat, -1);
 
     bool result = is_owned_by_root(library_path);
 
+    // Assert
     assert_false(result);
 }
 
+// Test load_and_validate_function
+
+static void test_load_and_validate_function_success(void **state) {
+    // Arrange
+    void *handle = (void *)1; // Simulate handle
+    const char *function_name = "valid_function";
+    void *function_pointer;
+    
+    // Act
+    bool result = load_and_validate_function(handle, function_name, &function_pointer);
+    
+    // Assert
+    assert_true(result);
+    assert_non_null(function_pointer);
+}
+
+static void test_load_and_validate_function_failure(void **state) {
+    // Arrange
+    void *handle = NULL;  // Simulate invalid handle
+    const char *function_name = "invalid_function";
+    void *function_pointer = (void *)1;
+
+    expect_string(__wrap__mwarn, formatted_msg, "(8008): Failed to load 'invalid_function': 'ERROR'.");
+    
+    // Act
+    bool result = load_and_validate_function(handle, function_name, &function_pointer);
+    
+    // Assert
+    assert_false(result);
+    assert_null(function_pointer);
+}
 
 int main(void) {
 
@@ -99,7 +147,8 @@ int main(void) {
         cmocka_unit_test(test_is_owned_by_root_root_owned),
         cmocka_unit_test(test_is_owned_by_root_not_root_owned),
         cmocka_unit_test(test_is_owned_by_root_stat_fails),
-        //cmocka_unit_test(test_w_journald_poc),
+        cmocka_unit_test(test_load_and_validate_function_success),
+        cmocka_unit_test(test_load_and_validate_function_failure)
     };
 
     return cmocka_run_group_tests(tests, group_setup, group_teardown);
