f6ae89702dfcb9da62e89cd1606f6a140ac2cc2c
Manuel Cano <119335479+mjcr99@users.noreply.github.com>
2024-04-09 20:44:19 +0200

    Migrate and adapt Wazuh manager subsystem from wazuh-packages to wazuh/wazuh repository (#22435)
    * Add packages for deb, rpm and windows
    
    * Add macos to packages
    
    * Add build.sh and generate_package.sh
    
    * Add short hash for package_names and clean generate_packages.sh
    
    * Adds short hash to package_name
    
    * Fix rpm package name to follow the name convention
    
    * Rename rpm arch from x86_64 to amd64
    
    * Added arch name fix in rpm packages creation
    
    * Added Dockerfiles for debs managers
    
    * Added Dockerfiles for rpm managers
    
    * Standarized arch naming
    
    * Updated SPECS
    
    * Updated package naming
    
    * Removed unused Vagrantfile
    
    * Fixed rpm packages names
    
    * Migrated ghcr scripts
    
    * Added upload-deb-manager-images.yml workflow
    
    * Updated packages-upload-images.yml and helper scripts
    
    * Added packages-build-amd64.yml
    
    * Added on-demand package generation
    
    * Added _pkg prefix to CONTAINER_NAME
    
    * Added runner selector by arch
    
    * Fix metadata problem in rpm packages
    
    * Improved manager builder workflows
    
    * Sync with agent migration development
    
    * Fix manager package buiding via workflow call
    
    * Uploaded variables name and added package build condition
    
    * Updates deb and rpm SPECS
    
    * Updated packages naming procedure
    
    * Improved workflows parametrization
    
    * Fixed rpm --src option
    
    * Added workflow abort if package already exists
    
    * Updated docker tag management
    
    * Migrated testing tools from wazuh-packages
    
    * Added install test before package upload
    
    * Fixed paths for docker test
    
    * Updated scripts and workflows with required changes
    
    * Updated multi server builder workflow
    
    * Updated inputs
    
    * Addressed comments
    
    * Fixed dockerfile path for arm64 and ppc64le
    
    * Removed unused if in manager package builder
    
    * Use dpkg in test install
    
    * Fix: removed arm64/aarch64 architecture option
    
    * Fix: applied minor fixes and added git as dependency in debs/amd64 Dockerfile
    
    * Fix: removed non manager-related files
    
    * Fix: fixed some problems regarding previous rebase
    
    * Address some minors comments
    
    ---------
    
    Co-authored-by: Luis Enrique Chico Capistrano <luis.chico@wazuh.com>

---
+++ .github/actions/test-install-components/install_component.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+package_name=$1
+target=$2
+
+echo "Installing Wazuh $target."
+
+if [ -n "$(command -v yum)" ]; then
+    install="yum install -y --nogpgcheck"
+    installed_log="/var/log/yum.log"
+elif [ -n "$(command -v dpkg)" ]; then
+    install="dpkg --install"
+    installed_log="/var/log/dpkg.log"
+else
+    common_logger -e "Couldn't find type of system"
+    exit 1
+fi
+
+if [ "${ARCH}" = "i386" ] || [ "${ARCH}" = "armv7hl" ]; then
+    linux="linux32"
+fi
+
+WAZUH_MANAGER="10.0.0.2" $linux $install "/packages/$package_name"| tee /packages/status.log
+grep -i " installed.*wazuh-$target" $installed_log| tee -a /packages/status.log
---
+++ packages/rpms/utils/helper_function.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# RPM helper functions
+
+# Wazuh package builder
+# Copyright (C) 2015, Wazuh Inc.
+#
+# This program is a free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation.
+
+rpmbuild="rpmbuild"
+rpm_build_dir="" # To be define on setup_build
+
+setup_build(){
+    sources_dir="$1"
+    specs_path="$2"
+    build_dir="$3"
+    package_name="$4"
+
+    rpm_build_dir=${build_dir}/rpmbuild
+    file_name="$package_name-${REVISION}"
+    src_file="${file_name}.src.rpm"
+
+    mkdir -p ${rpm_build_dir}/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+
+    cp ${specs_path}/wazuh-${BUILD_TARGET}.spec ${rpm_build_dir}/SPECS/${package_name}.spec
+
+    # Generating source tar.gz
+    cd ${build_dir}/${BUILD_TARGET} && tar czf "${rpm_build_dir}/SOURCES/${package_name}.tar.gz" "${package_name}"
+}
+
+set_debug(){
+    local debug="$1"
+    if [[ "${debug}" == "no" ]]; then
+        echo '%debug_package %{nil}' > /etc/rpm/macros
+    fi
+}
+
+build_deps(){
+    local legacy="$1"
+    if [ "${legacy}" = "no" ]; then
+        echo "%_source_filedigest_algorithm 8" >> /root/.rpmmacros
+        echo "%_binary_filedigest_algorithm 8" >> /root/.rpmmacros
+        if [ "${BUILD_TARGET}" = "agent" ]; then
+            echo " %rhel 6" >> /root/.rpmmacros
+            echo " %centos 6" >> /root/.rpmmacros
+            echo " %centos_ver 6" >> /root/.rpmmacros
+            echo " %dist .el6" >> /root/.rpmmacros
+            echo " %el6 1" >> /root/.rpmmacros
+        fi
+        rpmbuild="/usr/local/bin/rpmbuild"
+    fi
+}
+
+build_package(){
+    package_name="$1"
+    debug="$2"
+    short_commit_hash="$3"
+
+    if [ "${ARCHITECTURE_TARGET}" = "i386" ] || [ "${ARCHITECTURE_TARGET}" = "armhf" ]; then
+        linux="linux32"
+    fi
+
+    if [ "${ARCHITECTURE_TARGET}" = "armhf" ]; then
+        ARCH="armv7hl"
+    elif [ "${ARCHITECTURE_TARGET}" = "arm64" ]; then
+        ARCH="aarch64"
+    elif [ "${ARCHITECTURE_TARGET}" = "amd64" ]; then
+        ARCH="x86_64"
+    elif [[ "${ARCHITECTURE_TARGET}" == "i386" ]] || [[ "${ARCHITECTURE_TARGET}" == "ppc64le" ]]; then
+        ARCH=${ARCHITECTURE_TARGET}
+    else
+        echo "Invalid architecture selected. Choose: [armhf, arm64, amd64, i386, ppc64le]"
+        return 1
+    fi
+
+    if [[ "${IS_STAGE}" == "yes" ]]; then
+        rpm_file="${file_name}.${ARCH}.rpm"
+    else
+        # Replace "-" with "_" between BUILD_TARGET and Version
+        base_name="$(sed 's/-/_/2' <<< "$file_name")"
+        rpm_file="${base_name}_${ARCH}_${short_commit_hash}.rpm"
+    fi
+
+    $linux $rpmbuild --define "_sysconfdir /etc" --define "_topdir ${rpm_build_dir}" \
+        --define "_threads ${JOBS}" --define "_release ${REVISION}" \
+        --define "_localstatedir ${INSTALLATION_PATH}" --define "_debugenabled ${debug}" \
+        --define "_rpmfilename ${rpm_file}" \
+        --target $ARCH -ba ${rpm_build_dir}/SPECS/${package_name}.spec
+    return 0
+}
+
+get_checksum(){
+    src="$3"
+    if [[ "${checksum}" == "yes" ]]; then
+        cd "${rpm_build_dir}/RPMS" && sha512sum ${rpm_file} > /var/local/wazuh/${rpm_file}.sha512
+        if [[ "${src}" == "yes" ]]; then
+            cd "${rpm_build_dir}/SRPMS" && sha512sum ${src_file} > /var/local/wazuh/${src_file}.sha512
+        fi
+    fi
+
+    if [[ "${src}" == "yes" ]]; then
+        mv ${rpm_build_dir}/SRPMS/${src_file} /var/local/wazuh
+    else
+        mv ${rpm_build_dir}/RPMS/${rpm_file} /var/local/wazuh
+    fi
+}
