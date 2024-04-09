91d6d609ac23c2714a82e3935fd51fec3eb3b43b
Julian Morales <jmorales@unc.edu.ar>
2024-04-09 20:51:53 +0000

    Add read test cases for journal monitoring

--- tests/integration/test_logcollector/test_read/__init__.py
+++ tests/integration/test_logcollector/test_read/__init__.py
@@ -4,9 +4,7 @@ Created by Wazuh, Inc. <info@wazuh.com>.
 This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 """
 from pathlib import Path
-from wazuh_testing.tools.monitors import file_monitor
-from wazuh_testing.utils import callbacks
-from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
+
 
 # Constants & base paths
 TEST_DATA_PATH = Path(Path(__file__).parent, 'data')
