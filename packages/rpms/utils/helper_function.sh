66e3d9efad7b1f500880741eeeb7426b309c9882
Jotacarma90 <jose.carreras@wazuh.com>
2024-04-11 13:16:09 +0200

    Modify manager and agent specs in rpm package generation to use metadata and allow no stage nomenclature

--- packages/rpms/utils/helper_function.sh
+++ packages/rpms/utils/helper_function.sh
@@ -20,8 +20,6 @@ setup_build(){
     package_name="$4"
 
     rpm_build_dir=${build_dir}/rpmbuild
-    file_name="$package_name-${REVISION}"
-    src_file="${file_name}.src.rpm"
 
     mkdir -p ${rpm_build_dir}/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 
@@ -58,6 +56,7 @@ build_package(){
     package_name="$1"
     debug="$2"
     short_commit_hash="$3"
+    wazuh_version="$4"
 
     if [ "${ARCHITECTURE_TARGET}" = "i386" ] || [ "${ARCHITECTURE_TARGET}" = "armhf" ]; then
         linux="linux32"
@@ -76,18 +75,10 @@ build_package(){
         return 1
     fi
 
-    if [[ "${IS_STAGE}" == "yes" ]]; then
-        rpm_file="${file_name}.${ARCH}.rpm"
-    else
-        # Replace "-" with "_" between BUILD_TARGET and Version
-        base_name="$(sed 's/-/_/2' <<< "$file_name")"
-        rpm_file="${base_name}_${ARCH}_${short_commit_hash}.rpm"
-    fi
-
     $linux $rpmbuild --define "_sysconfdir /etc" --define "_topdir ${rpm_build_dir}" \
-        --define "_threads ${JOBS}" --define "_release ${REVISION}" \
+        --define "_threads ${JOBS}" --define "_release ${REVISION}" --define "_isstage ${IS_STAGE}" \
         --define "_localstatedir ${INSTALLATION_PATH}" --define "_debugenabled ${debug}" \
-        --define "_rpmfilename ${rpm_file}" \
+        --define "_version ${wazuh_version}" --define "_hashcommit ${short_commit_hash}" \
         --target $ARCH -ba ${rpm_build_dir}/SPECS/${package_name}.spec
     return 0
 }
@@ -95,15 +86,15 @@ build_package(){
 get_checksum(){
     src="$3"
     if [[ "${checksum}" == "yes" ]]; then
-        cd "${rpm_build_dir}/RPMS" && sha512sum ${rpm_file} > /var/local/wazuh/${rpm_file}.sha512
+        cd "${rpm_build_dir}/RPMS" && sha512sum *.rpm > /var/local/wazuh/*.rpm.sha512
         if [[ "${src}" == "yes" ]]; then
-            cd "${rpm_build_dir}/SRPMS" && sha512sum ${src_file} > /var/local/wazuh/${src_file}.sha512
+            cd "${rpm_build_dir}/SRPMS" && sha512sum *.src.rpm > /var/local/wazuh/*.src.rpm.sha512
         fi
     fi
 
     if [[ "${src}" == "yes" ]]; then
-        mv ${rpm_build_dir}/SRPMS/${src_file} /var/local/wazuh
+        mv ${rpm_build_dir}/SRPMS/*.src.rpm /var/local/wazuh
     else
-        mv ${rpm_build_dir}/RPMS/${rpm_file} /var/local/wazuh
+        mv ${rpm_build_dir}/RPMS/*.rpm /var/local/wazuh
     fi
 }
