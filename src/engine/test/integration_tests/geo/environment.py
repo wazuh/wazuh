4d9b446b65d3326abf229378c81cb2a6c65fd7ab
Javier Bejar Mendez <javier.bejar@wazuh.com>
2024-05-03 09:26:06 +0200

    Added geo integration tests and QoL on integration scripts

---
+++ src/engine/test/integration_tests/geo/environment.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import shutil
+
+from handler_engine_instance import up_down
+
+up_down_engine = up_down.UpDownEngine()
+dbs_path = Path(__file__).resolve().parent / "data" / "dbs"
+
+
+def before_feature(context, feature):
+    up_down_engine.send_start_command()
+
+
+def after_feature(context, feature):
+    up_down_engine.send_stop_command()
+
+    # Clean up data directory
+    for child in dbs_path.iterdir():
+        if child.is_dir():
+            shutil.rmtree(child)
+        else:
+            child.unlink()
