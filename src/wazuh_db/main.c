8d1f4095b5c9c7627953a5ff444ce1cfc282433d
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-05-16 12:56:53 +0200

    fix: create wazuh-db agent template on startup

--- src/wazuh_db/main.c
+++ src/wazuh_db/main.c
@@ -137,6 +137,7 @@ int main(int argc, char ** argv)
     snprintf(path_template, sizeof(path_template), "%s/%s/%s", home_path, WDB2_DIR, WDB_PROF_NAME);
     unlink(path_template);
     mdebug1("Template file removed: %s", path_template);
+    wdb_create_profile();
 
     // Set max open files limit
     struct rlimit rlimit = { nofile, nofile };
