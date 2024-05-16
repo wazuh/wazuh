2ad034c65767bf2d23987ab54d21174799f35dc9
Julian Bustamante N <julianbustamantenarvaez@gmail.com>
2024-05-16 19:01:25 +0000

    syntax changes

--- tests/integration/test_fim/test_files/test_restrict/test_restrict.py
+++ tests/integration/test_fim/test_files/test_restrict/test_restrict.py
@@ -1,7 +1,63 @@
 """
- Copyright (C) 2015-2024, Wazuh Inc.
- Created by Wazuh, Inc. <info@wazuh.com>.
- This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+ copyright: Copyright (C) 2015-2024, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts when
+       these files are modified. Specifically, these tests will verify that FIM generates events
+       only for file operations in monitored directories that do not match the 'restrict' attribute.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks configured
+       files for changes to the checksums, permissions, and ownership.
+
+components:
+    - fim
+
+suite: files_restrict
+
+targets:
+    - agent
+    - manager
+
+daemons:
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Debian Buster
+    - Red Hat 8
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Windows 10
+    - Windows Server 2019
+    - Windows Server 2016
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#directories
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_restrict
 """
 
 import sys
@@ -23,8 +79,8 @@ from wazuh_testing.utils.configuration import get_test_cases_data, load_configur
 
 from . import TEST_CASES_PATH, CONFIGS_PATH
 
-# Pytest marks to run on any service type on linux or windows.
-pytestmark = [ pytest.mark.linux, pytest.mark.tier(level=2)]
+# Pytest marks to run on any service type on linux.
+pytestmark = [pytest.mark.agent, pytest.mark.linux, pytest.mark.tier(level=2)]
 
 # Test metadata, configuration and ids.
 cases_path = Path(TEST_CASES_PATH, 'cases_restrict.yaml')
@@ -36,7 +92,6 @@ test_configuration = load_configuration_template(config_path, test_configuration
 local_internal_options = {configuration.SYSCHECK_DEBUG: 2, AGENTD_DEBUG: 2, MONITORD_ROTATE_LOG: 0}
 if sys.platform == WINDOWS: local_internal_options.update({AGENTD_WINDOWS_DEBUG: 2})
 
-@pytest.mark.skipif(sys.platform=='win32', reason="Blocked by #4077.")
 @pytest.mark.parametrize('test_configuration, test_metadata', zip(test_configuration, test_metadata), ids=cases_ids)
 def test_restrict(test_configuration, test_metadata, set_wazuh_configuration, configure_local_internal_options,
                   truncate_monitored_files, folder_to_monitor, daemons_handler, file_to_monitor):
@@ -67,6 +122,9 @@ def test_restrict(test_configuration, test_metadata, set_wazuh_configuration, co
         - truncate_monitored_files:
             type: fixture
             brief: Truncate all the log files and json alerts files before and after the test execution.
+        - folder_to_monitor:
+            type: str
+            brief: Folder created for monitoring.
         - file_to_monitor:
             type: str
             brief: File created for monitoring.
@@ -96,8 +154,6 @@ def test_restrict(test_configuration, test_metadata, set_wazuh_configuration, co
 
     monitor = FileMonitor(WAZUH_LOG_PATH)
     
-    time.sleep(3)
-
     if test_metadata['data'][1] == True:
         monitor.start(generate_callback(EVENT_TYPE_ADDED))
         print(monitor.callback_result)
