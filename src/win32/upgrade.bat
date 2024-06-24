82a761b1f87c7465431406b80bee5246fb24075a
Marcel Kemp <marcel.kemp@wazuh.com>
2024-06-24 18:07:24 +0200

    fix: adaptation of the package name to the new nomenclature

--- src/win32/upgrade.bat
+++ src/win32/upgrade.bat
@@ -4,7 +4,7 @@ IF "%1"=="B" GOTO background
 
 COPY upgrade\upgrade.bat . > NUL
 COPY upgrade\do_upgrade.ps1 . > NUL
-COPY upgrade\wazuh-agent-*.msi . > NUL
+COPY upgrade\wazuh-agent*.msi . > NUL
 
 START /B upgrade.bat B
 GOTO end
