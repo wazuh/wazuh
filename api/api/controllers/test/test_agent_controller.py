469e5bb796c5318e5a506757914a0356e2d9d6ae
RamosFe <federico.ramos@wazuh.com>
2024-07-22 17:13:06 -0300

    Added UT for the functions related to GET /agents/uninstall.

--- api/api/controllers/test/test_agent_controller.py
+++ api/api/controllers/test/test_agent_controller.py
@@ -20,7 +20,7 @@ with patch('wazuh.common.wazuh_uid'):
             delete_single_agent_multiple_groups,
             delete_single_agent_single_group, get_agent_config,
             get_agent_fields, get_agent_key, get_agent_no_group,
-            get_agent_outdated, get_agent_summary_os, get_agent_summary_status,
+            get_agent_outdated, get_agent_summary_os, get_agent_uninstall_permission, get_agent_summary_status,
             get_agent_upgrade, get_agents, get_agents_in_group, get_daemon_stats,
             get_component_stats, get_group_config, get_group_file, get_group_files,
             get_list_group, get_sync_agent, insert_agent, post_group,
@@ -1174,6 +1174,29 @@ async def test_get_agent_summary_os(mock_exc, mock_dapi, mock_remove, mock_dfunc
     """Verify 'get_agent_summary_os' endpoint is working as expected."""
     result = await get_agent_summary_os()
     mock_dapi.assert_called_once_with(f=agent.get_agents_summary_os,
+                                      f_kwargs=mock_remove.return_value,
+                                      request_type='local_master',
+                                      is_async=False,
+                                      wait_for_complete=False,
+                                      logger=ANY,
+                                      rbac_permissions=mock_request.context['token_info']['rbac_policies']
+                                      )
+    mock_exc.assert_called_once_with(mock_dfunc.return_value)
+    mock_remove.assert_called_once_with({})
+    assert isinstance(result, ConnexionResponse)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("mock_request", ["agent_controller"], indirect=True)
+@patch('api.configuration.api_conf')
+@patch('api.controllers.agent_controller.DistributedAPI.distribute_function', return_value=AsyncMock())
+@patch('api.controllers.agent_controller.remove_nones_to_dict')
+@patch('api.controllers.agent_controller.DistributedAPI.__init__', return_value=None)
+@patch('api.controllers.agent_controller.raise_if_exc', return_value=CustomAffectedItems())
+async def test_get_agent_uninstall_permission(mock_exc, mock_dapi, mock_remove, mock_dfunc, mock_exp, mock_request):
+    """Verify 'get_agent_uninstall_permission' endpoint is working as expected."""
+    result = await get_agent_uninstall_permission()
+    mock_dapi.assert_called_once_with(f=agent.agent_check_uninstall_permission,
                                       f_kwargs=mock_remove.return_value,
                                       request_type='local_master',
                                       is_async=False,
