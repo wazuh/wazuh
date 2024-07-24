d7c2396eb9e5a4f6707d8cd8cc041af8fa8329fd
Julian Morales <jmorales@unc.edu.ar>
2024-07-24 20:49:37 +0000

    Prepares the branch of Engine to merge into master

--- src/logcollector/read_json.c
+++ src/logcollector/read_json.c
@@ -18,8 +18,11 @@
 void *read_json(logreader *lf, int *rc, int drop_it) {
     int __ms = 0;
     int __ms_reported = 0;
+    int i;
+    char *jsonParsed;
     char str[OS_MAXSTR + 1];
     int lines = 0;
+    cJSON * obj;
     int64_t offset = 0;
     int64_t rbytes = 0;
 
@@ -98,38 +101,21 @@ void *read_json(logreader *lf, int *rc, int drop_it) {
             continue;
         }
 
-        const char * jsonErrPtr;
-        cJSON * obj = cJSON_ParseWithOpts(str, &jsonErrPtr, 0);
+        const char *jsonErrPtr;
+        if (obj = cJSON_ParseWithOpts(str, &jsonErrPtr, 0), obj && cJSON_IsObject(obj)) {
+          for (i = 0; lf->labels && lf->labels[i].key; i++) {
+              W_JSON_AddField(obj, lf->labels[i].key, lf->labels[i].value);
+          }
 
-        /* If the parsed event is not an object then it is embedded into the `LOGCOLLECTOR_JSON_EVENT_FIELD` one */
-        if (!cJSON_IsObject(obj)) {
-            mdebug2("Input is not a JSON object.");
-            if (NULL != obj) {
-                /* `obj` is deleted as its current format is not the expected one */
-                cJSON_Delete(obj);
-            }
-            obj = cJSON_CreateObject();
-            W_JSON_AddField(obj, LOGCOLLECTOR_JSON_EVENT_FIELD, str);
+          jsonParsed = cJSON_PrintUnformatted(obj);
+          cJSON_Delete(obj);
+        } else {
+          cJSON_Delete(obj);
+          mdebug1("Line '%.*s'%s read from '%s' is not a JSON object.", sample_log_length, str, rbytes > sample_log_length ? "..." : "", lf->file);
+          continue;
         }
 
-        /* At this point `obj` shouldn't be NULL */
-        if (NULL == obj) {
-            mdebug1("Line '%.*s'%s read from '%s' could not be converted to a JSON object.",
-                                            sample_log_length, str, rbytes > sample_log_length ? "..." : "", lf->file);
-            continue;
-        }
-
-        /* Labels defined in the configuration are added to the json object */
-        for (int i = 0; lf->labels && lf->labels[i].key; i++) {
-            W_JSON_AddField(obj, lf->labels[i].key, lf->labels[i].value);
-        }
-
-        char * jsonParsed = cJSON_PrintUnformatted(obj);
-        /* `obj` is not used anymore */
-        cJSON_Delete(obj);
-
-        mdebug2("Reading json message: '%.*s'%s",
-                            sample_log_length, jsonParsed, strlen(jsonParsed) > (size_t)sample_log_length ? "..." : "");
+        mdebug2("Reading json message: '%.*s'%s", sample_log_length, jsonParsed, strlen(jsonParsed) > (size_t)sample_log_length ? "..." : "");
 
         /* Send message to queue */
         if (drop_it == 0) {
