2b94bc1d7709e1945269ebed56a7d04ab2b6b026
GGP1 <gaston.palomeque@wazuh.com>
2024-08-01 11:05:18 -0300

    Update parent class

--- apis/comms_api/models/error.py
+++ apis/comms_api/models/error.py
@@ -1,15 +1,15 @@
 import json
 from typing import Any
 
-from fastapi import Response
+from fastapi.responses import JSONResponse
 
-class ErrorResponse(Response):
+class ErrorResponse(JSONResponse):
     """Error response model."""
 
-    def __init__(self, message: str, code: int = None, status_code = 500):
+    def __init__(self, message: str, code: int = None, status_code: int = 500):
         self.message = message
         self.code = code if code is not None else status_code
         super().__init__(content=None, status_code=status_code)
 
     def render(self, content: Any = None) -> str:
-        return json.dumps({'message': self.message, 'code': self.code})
+        return json.dumps({'error': {'message': self.message, 'code': self.code}})
