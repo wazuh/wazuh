dbe3dfb54e2abcbca215447ca85593dd44be9671
GGP1 <gaston.palomeque@wazuh.com>
2024-08-01 09:39:30 -0300

    Use fastapi status codes

--- apis/comms_api/models/error.py
+++ apis/comms_api/models/error.py
@@ -1,13 +1,14 @@
 import json
 from typing import Any
 
+from fastapi import status
 from fastapi.responses import JSONResponse
 
 
 class ErrorResponse(JSONResponse):
     """Error response model."""
 
-    def __init__(self, message: str, code: int = None, status_code: int = 500):
+    def __init__(self, message: str, code: int = None, status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR):
         self.message = message
         self.code = code if code is not None else status_code
         super().__init__(content=None, status_code=status_code)
