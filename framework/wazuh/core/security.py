b1d2f5f4e89823fac8477aa19e89d806fe033628
GGP1 <gaston.palomeque@wazuh.com>
2024-08-01 12:37:32 -0300

    Take curve from the parameters

--- framework/wazuh/core/security.py
+++ framework/wazuh/core/security.py
@@ -11,6 +11,7 @@ import api.middlewares as middlewares
 from api import __path__ as api_path
 from api.authentication import change_keypair
 from api.constants import SECURITY_CONFIG_PATH
+from cryptography.hazmat.primitives.asymmetric import ec
 from wazuh import WazuhInternalError, WazuhError
 from wazuh.rbac.orm import RolesManager, TokenManager, check_database_integrity, DB_FILE
 
@@ -115,7 +116,7 @@ def revoke_tokens() -> dict:
     dict
         Confirmation message.
     """
-    change_keypair()
+    change_keypair(ec.SECP521R1())
     with TokenManager() as tm:
         tm.delete_all_rules()
 
