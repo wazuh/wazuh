949bbc2c6c7628e3ceb89bb9c18ac2c176cb7ef3
GGP1 <gaston.palomeque@wazuh.com>
2024-08-01 15:08:24 -0300

    Add http exception handler

--- apis/comms_api/routers/router.py
+++ apis/comms_api/routers/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, status
-from fastapi.responses import Response
+from fastapi import APIRouter, Request, status
+from fastapi.responses import Response, JSONResponse
+
+from comms_api.models.error import HTTPError
 
 router = APIRouter(prefix='/api/v1')
 
@@ -7,3 +9,9 @@ router = APIRouter(prefix='/api/v1')
 @router.get("/")
 async def home():
     return Response(status_code=status.HTTP_200_OK)
+
+async def http_error_handler(request: Request, exc: HTTPError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={'message': exc.message, 'code': exc.code},
+    )
