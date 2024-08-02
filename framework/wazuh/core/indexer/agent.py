f2a8c2514e590cd6b9576538a1816b155db793d5
Nico Stefani <nicolas.stefi@wazuh.com>
2024-08-02 17:04:30 -0300

    Implemented basic delete method

--- framework/wazuh/core/indexer/agent.py
+++ framework/wazuh/core/indexer/agent.py
@@ -9,12 +9,38 @@ from .models import Agent
 NAME_KEY = 'name'
 PASSWORD_KEY = 'password'
 SOURCE_KEY = '_source'
+QUERY_KEY = 'query'
+TERMS_KEY = 'terms'
+INDEX_KEY = 'index'
+BODY_KEY = 'body'
+ID_KEY = 'id'
+HITS_KEY = 'hits'
+TOTAL_KEY = 'total'
+DELETED_KEY = 'deleted'
+FAILURES_KEY = 'failures'
+
+
+def _get_source_items(search_result: dict) -> list:
+    """Extract the elements from a search query.
+
+    Parameters
+    ----------
+    search_result : dict
+        Data to extract the elements.
+
+    Returns
+    -------
+    list
+        Obtained items.
+    """
+    return [item[SOURCE_KEY] for item in search_result[HITS_KEY][HITS_KEY]]
 
 
 class AgentsIndex(BaseIndex):
     """Set of methods to interact with `agents` index."""
 
     INDEX = 'agents'
+    SECONDARY_INDEXES = []
 
     async def create(self, id: uuid7, key: str, name: str) -> Agent:
         """Create a new agent.
@@ -41,11 +67,7 @@ class AgentsIndex(BaseIndex):
         doc = Agent(id=id, key=key, name=name)
         try:
             await self._client.index(
-                index=self.INDEX,
-                id=doc.id,
-                body=asdict(doc),
-                op_type='create',
-                refresh='wait_for'
+                index=self.INDEX, id=doc.id, body=asdict(doc), op_type='create', refresh='wait_for'
             )
         except exceptions.ConflictError:
             raise WazuhError(1708, extra_message=id)
@@ -60,8 +82,35 @@ class AgentsIndex(BaseIndex):
         finally:
             return Agent(uuid=uuid, **data[SOURCE_KEY])
 
-    async def delete(self, uuid: uuid7):
-        try:
-            self._client.delete(id=uuid)
-        except exceptions.NotFoundError:
-            raise WazuhResourceNotFound(1701)
+    async def delete(self, ids: list[str]) -> list:
+        """Delete multiple agents that match with the given parameters.
+
+        Parameters
+        ----------
+        ids : list[uuid7]
+            Agent ids to delete.
+
+        Returns
+        -------
+        list
+            Ids of the deleted agents.
+
+        """
+        indexes = ','.join([self.INDEX, *self.SECONDARY_INDEXES])
+        body = {QUERY_KEY: {TERMS_KEY: {'_id': ids}}}
+        parameters = {INDEX_KEY: indexes, BODY_KEY: body}
+
+        deleted_ids = []
+
+        response = await self._client.delete_by_query(**parameters, refresh='true')
+
+        if len(ids) == response[DELETED_KEY]:
+            deleted_ids = ids
+        elif response[FAILURES_KEY]:
+            ids_after_delete = {
+                item[ID_KEY]
+                for item in _get_source_items(await self._client.search(**parameters, _source_includes=ID_KEY))
+            }
+            deleted_ids = list(set(ids) & ids_after_delete)
+
+        return deleted_ids
