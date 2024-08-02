8eb52913bd6c0474a277bdceff85bd8876f4c6a6
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2024-08-02 20:57:08 +0000

    Improve management of documentation by helper type and keywords

--- src/engine/test/helper_tests/engine_helper_test/src/documentation_generator/__main__.py
+++ src/engine/test/helper_tests/engine_helper_test/src/documentation_generator/__main__.py
@@ -7,12 +7,11 @@ from test_cases_generator.parser import Parser
 from .documentation import *
 from .exporter import IExporter
 from .types import *
-import tempfile
 import subprocess
 
 
 def parse_arguments() -> argparse.Namespace:
-    arg_parser = argparse.ArgumentParser(description="Generates files containing test cases for a given helper")
+    arg_parser = argparse.ArgumentParser(description="Generates files containing documentation for a given helper")
     arg_parser.add_argument(
         "--input_file_path",
         help="Absolute or relative path where the description of the helper function is located",
@@ -28,7 +27,7 @@ def parse_arguments() -> argparse.Namespace:
     arg_parser.add_argument(
         "-o",
         "--output_path",
-        help="Absolute or relative path of the directory where the generated test files will be located",
+        help="Absolute or relative path of the directory where the generated documentation files will be located",
     )
 
     args = arg_parser.parse_args()
@@ -39,26 +38,18 @@ def parse_arguments() -> argparse.Namespace:
     return args
 
 
-def is_temp_path(path: Path):
-    path = path.resolve()
-    temp_dir = Path(tempfile.gettempdir()).resolve()
-    return str(path).startswith(str(temp_dir))
-
-
-def generate_documentation(parser: Parser, exporter: IExporter, file: Path, output_dir: Path):
+def generate_documentation(parser: Parser, exporter: IExporter, file: Path, output_path: Path):
     parser.load_yaml_from_file(file)
     documentation = parse_yaml_to_documentation(parser)
     exporter.create_document(documentation)
-    exporter.save(output_dir)
+    exporter.save(output_path)
 
 
 def main():
     args = parse_arguments()
     parser = Parser()
     exporter_type = args.exporter if args.exporter else "mark_down"
-    output_dir = Path(args.output_path if args.output_path else "/tmp/documentation")
-    if not is_temp_path(output_dir):
-        sys.exit("the output directory must be a temporary one")
+    output_path = Path(args.output_path if args.output_path else "/tmp/documentation")
     exporter = ExporterFactory.get_exporter(exporter_type)
     if args.input_file_path:
         command = f'engine-helper-test-validator --input_file_path {args.input_file_path}'
@@ -66,15 +57,22 @@ def main():
             subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE)
         except subprocess.CalledProcessError as e:
             sys.exit(e.stderr)
-        generate_documentation(parser, exporter, Path(args.input_file_path), output_dir)
+        generate_documentation(parser, exporter, Path(args.input_file_path), output_path)
     elif args.folder_path:
-        for file in Path(args.folder_path).iterdir():
-            if file.is_file() and (file.suffix in ['.yml', '.yaml']):
-                command = f'engine-helper-test-validator --folder_path {args.folder_path}'
-                try:
-                    subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE)
-                except subprocess.CalledProcessError as e:
-                    sys.exit(e.stderr)
-                generate_documentation(parser, exporter, file, output_dir)
+        folder_path = Path(args.folder_path)
+        yaml_files = folder_path.rglob('*.yml')
+        yaml_files = list(yaml_files) + list(folder_path.rglob('*.yaml'))
+
+        if not yaml_files:
+            sys.exit("No YAML files found in the specified directory.")
+
+        for file in yaml_files:
+            command = f'engine-helper-test-validator --input_file_path {file}'
+            try:
+                subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE)
+            except subprocess.CalledProcessError as e:
+                sys.exit(e.stderr)
+            generate_documentation(parser, exporter, file, output_path)
+
     else:
         sys.exit("It is necessary to indicate a file or directory that contains a configuration yaml")
