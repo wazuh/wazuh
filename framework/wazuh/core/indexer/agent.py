dcbd26c779f8241787679ef0aa0dcfba6f75bd28
GGP1 <gaston.palomeque@wazuh.com>
2024-08-07 13:00:42 -0300

    Add agents index update method

--- framework/wazuh/core/indexer/agent.py
+++ framework/wazuh/core/indexer/agent.py
@@ -2,9 +2,8 @@ from dataclasses import asdict
 from typing import List
 
 from opensearchpy import exceptions
-from wazuh.core.exception import WazuhError, WazuhResourceNotFound
 
-from .base import BaseIndex
+from .base import BaseIndex, remove_empty_values
 from .constants import (
     BODY_KEY,
     INDEX_KEY,
@@ -13,10 +12,11 @@ from .constants import (
     TERMS_KEY,
 )
 from .models import Agent
+from wazuh.core.exception import WazuhError, WazuhResourceNotFound
 
 
 class AgentsIndex(BaseIndex):
-    """Set of methods to interact with `agents` index."""
+    """Set of methods to interact with the `agents` index."""
 
     INDEX = 'agents'
     SECONDARY_INDEXES = []
@@ -40,10 +40,10 @@ class AgentsIndex(BaseIndex):
 
         Raises
         ------
-        WazuhError (1708)
+        WazuhError(1708)
             When already exists an agent with the provided id.
         """
-        agent = Agent(id=id, key=key, name=name)
+        doc = Agent(id=id, raw_key=key, name=name)
         try:
             await self._client.index(
                 index=self.INDEX,
@@ -94,23 +94,50 @@ class AgentsIndex(BaseIndex):
         parameters = {INDEX_KEY: self.INDEX, BODY_KEY: query}
         return await self._client.search(**parameters)
 
-    async def get(self, id: str) -> Agent:
-        """Get an agent.
+    async def get(self, uuid: str) -> Agent:
+        """Retrieve an agent information.
 
         Parameters
         ----------
-        id : str
-            Agent ID.
-
+        uuid : str
+            Agent unique identifier.
+        
+        Raises
+        ------
+        WazuhResourceNotFound(1701)
+            If no agents exist with the uuid provided.
+        
         Returns
         -------
         Agent
-            Agent instance.
+            Agent object.
         """
         try:
-            data = self._client.get(index=self.INDEX, id=self.id)
+            data = await self._client.get(index=self.INDEX, id=uuid)
         except exceptions.NotFoundError:
             raise WazuhResourceNotFound(1701)
-        finally:
-            return Agent(uuid=id, **data[SOURCE_KEY])
 
+        return Agent(**data[SOURCE_KEY])
+
+    async def update(self, uuid: str, agent: Agent) -> None:
+        """Update an agent.
+
+        Parameters
+        ----------
+        uuid : str
+            Agent unique identifier.
+        agent : Agent
+            Agent fields. Only specified fields are updated.
+
+        Raises
+        ------
+        WazuhResourceNotFound(1701)
+            If no agents exist with the uuid provided.
+        """
+        try:
+            # Convert to a dictionary removing empty values to avoid updating them
+            agent_dict = asdict(agent, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})
+            body = {'doc': agent_dict}
+            await self._client.update(index=self.INDEX, id=uuid, body=body)
+        except exceptions.NotFoundError:
+            raise WazuhResourceNotFound(1701)
--- framework/wazuh/core/indexer/models.py
+++ framework/wazuh/core/indexer/models.py
@@ -36,45 +36,13 @@ def _hash_key(key: str, salt: bytes) -> str:
     """
     return hashlib.pbkdf2_hmac(HASH_ALGO, key.encode('utf-8'), salt, ITERATIONS)
 
-ITERATIONS = 100_000
-HASH_ALGO = 'sha256'
-
-
-def _generate_salt() -> bytes:
-    """Generate a random salt value.
-
-    Returns
-    -------
-    bytes
-        Random salt.
-    """
-    return os.urandom(16)
-
-
-def _hash_key(key: str, salt: bytes) -> str:
-    """Hash the given key using the provided salt.
-
-    Parameters
-    ----------
-    key : str
-        Value to hash.
-    salt : bytes
-        Value to use within derivation function.
-
-    Returns
-    -------
-    str
-        The hashed key.
-    """
-    return hashlib.pbkdf2_hmac(HASH_ALGO, key.encode('utf-8'), salt, ITERATIONS)
-
 
 @dataclass
 class Agent:
     """Representation of a Wazuh Agent."""
 
-    id: str
-    name: str
+    id: str = None
+    name: str = None
     key: str = None
     groups: str = None
     type: str = None
@@ -121,5 +89,4 @@ class Agent:
         """
         stored_key = self.key.encode('latin-1')
         salt, key_hash = stored_key[:16], stored_key[16:]
-
         return compare_digest(key_hash, _hash_key(key, salt))
