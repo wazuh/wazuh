589133255abfcf56c5f8f51143c21eac61fcadb0
GGP1 <gaston.palomeque@wazuh.com>
2024-08-13 14:46:10 -0300

    Extract lambda to a separate function

--- framework/wazuh/core/indexer/agent.py
+++ framework/wazuh/core/indexer/agent.py
@@ -43,7 +43,7 @@ class AgentsIndex(BaseIndex):
         WazuhError(1708)
             When already exists an agent with the provided id.
         """
-        doc = Agent(id=id, raw_key=key, name=name)
+        agent = Agent(id=id, raw_key=key, name=name)
         try:
             await self._client.index(
                 index=self.INDEX,
@@ -136,7 +136,7 @@ class AgentsIndex(BaseIndex):
         """
         try:
             # Convert to a dictionary removing empty values to avoid updating them
-            agent_dict = asdict(agent, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})
+            agent_dict = asdict(agent, dict_factory=remove_empty_values)
             body = {'doc': agent_dict}
             await self._client.update(index=self.INDEX, id=uuid, body=body)
         except exceptions.NotFoundError:
--- framework/wazuh/core/indexer/base.py
+++ framework/wazuh/core/indexer/base.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Dict, List, Tuple
 
 from opensearchpy import AsyncOpenSearch
 
@@ -11,3 +12,24 @@ class BaseIndex:
     def __init__(self, client: AsyncOpenSearch) -> None:
         self._client = client
         self._logger = logging.getLogger('wazuh')
+
+
+def remove_empty_values(items: List[Tuple[str, Any]]) -> Dict[str, Any]:
+    """Remove empty values from a dictionary.
+
+    Parameters
+    ----------
+    items
+        List of tuples to evaluate.
+    
+    Returns
+    -------
+    Dict[str, Any]
+        Dictionary without None values.
+    """
+    new_dict = {}
+    for (k, v) in items:
+        if v is not None:
+            new_dict[k] = v
+
+    return new_dict
