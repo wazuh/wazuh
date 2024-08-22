4ca5728bf867e83e6e28370a82c44ccf36eeeca2
Julian Morales <jmorales@unc.edu.ar>
2024-08-22 16:50:16 +0000

    Fixes and eliminates dependency on protobuf installation

--- src/engine/source/proto/generateCode.sh
+++ src/engine/source/proto/generateCode.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
+# Search for the protoc compiler and source from the vcpkg directory
+set_proto_config() {
+    if [ -z "$VCPKG_INSTALLED_DIR" ]; then
+        echo "Error: VCPKG_INSTALLED_DIR is not set, build the code using cmake." >&2
+        echo "i.e: cd $ENGINE_DIR && cmake --preset debug && cmake --build ./build --target generate_protobuf_code" >&2
+        exit 1
+    fi
 
-# Function to check if protoc is installed
-check_protoc_installed() {
-    if ! [ -x "$(command -v protoc)" ]; then
-        echo 'Error: protoc is not installed.' >&2
+    VCPKG_PROTO=${VCPKG_INSTALLED_DIR}/tools/protobuf/protoc
+    INCLUDE_PROTO_DIR=${VCPKG_INSTALLED_DIR}/include/
+
+    if [ ! -f "$VCPKG_PROTO" ]; then
+        echo "Error: protoc compiler not found in VCPKG_INSTALLED_DIR." >&2
+        exit 1
+    fi
+
+    if [ ! -d "$INCLUDE_PROTO_DIR/google/protobuf" ]; then
+        echo "Error: include directory not found in VCPKG_INSTALLED_DIR." >&2
         exit 1
     fi
 }
 
 # Function to set up paths and variables
 initialize_variables() {
-    OLD_PWD=$(pwd)
-    PROTO_DEPS_SRC_DIR=../../../build/vcpkg_installed/x64-linux/include/
-    SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-    SRC_PROTO_DIR="${SCRIPT_DIR}/src"
-    CLANG_DIR="${SCRIPT_DIR}/../../"
-    CPP_DIR="${SCRIPT_DIR}/include/eMessages/"
-    PYTHON_DIR="${SCRIPT_DIR}/../../tools/api-communication/src/api_communication/proto"
+    # Parameters of protoc
+    OUTPUT_CPP_DIR="${SCRIPT_DIR}/include/eMessages/"
+    ENGINE_SRC_PROTO="${SCRIPT_DIR}/src"
+    OUTPUT_PYTHON_DIR="${ENGINE_DIR}/tools/api-communication/src/api_communication/proto"
+
+    # Prepare clang-format for pre-processing
+    CLANG_DIR="${ENGINE_DIR}"
 }
 
 # Function to select the appropriate clang-format version
@@ -36,7 +49,7 @@ select_clang_format_version() {
 # Function to format code using clang-format
 format_code() {
     cd "$CLANG_DIR" || exit 1
-    $CLANG_FORMAT -i -style=file "${SRC_PROTO_DIR}"/*.proto
+    $CLANG_FORMAT -i -style=file "${ENGINE_SRC_PROTO}"/*.proto
 }
 
 # Function to clean up unnecessary files
@@ -50,28 +63,33 @@ clean_up_files() {
         fi
     }
 
-    clean_directory "$CPP_DIR" "eMessage.h" "readme.md"
-    clean_directory "$PYTHON_DIR" "__init__.py"
+    clean_directory "$OUTPUT_CPP_DIR" "eMessage.h" "readme.md"
+    clean_directory "$OUTPUT_PYTHON_DIR" "__init__.py"
 }
 
 # Function to generate code using protoc
 generate_code() {
-    cd "${SRC_PROTO_DIR}" || exit 1
-    protoc --proto_path="$PROTO_DEPS_SRC_DIR" --proto_path="${SRC_PROTO_DIR}" --cpp_out="$CPP_DIR" *.proto
-    protoc --proto_path="$PROTO_DEPS_SRC_DIR" --proto_path="${SRC_PROTO_DIR}" --python_out="$PYTHON_DIR" --pyi_out="$PYTHON_DIR" *.proto
+    cd "${ENGINE_SRC_PROTO}" || exit 1
+    $VCPKG_PROTO --proto_path="$INCLUDE_PROTO_DIR" --proto_path="${ENGINE_SRC_PROTO}" --cpp_out="$OUTPUT_CPP_DIR" *.proto
+    $VCPKG_PROTO --proto_path="$INCLUDE_PROTO_DIR" --proto_path="${ENGINE_SRC_PROTO}" --python_out="$OUTPUT_PYTHON_DIR" --pyi_out="$OUTPUT_PYTHON_DIR" *.proto
 }
 
-# Function to modify Python imports
+# Necessary modifications for python imports
 modify_python_imports() {
-    python_files=$(grep -rl '^import .*_pb2' --include="*.py" "$PYTHON_DIR")
+    python_files=$(grep -rl '^import .*_pb2' --include="*.py" "$OUTPUT_PYTHON_DIR")
 
     for file in $python_files; do
         sed -i '/^import .*_pb2/s/import \(.*\) as \(.*\)/import api_communication.proto.\1 as _\1/' "$file"
     done
 }
 
+# Global constants
+OLD_PWD=$(pwd)  
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+ENGINE_DIR="${SCRIPT_DIR}/../.." 
+
 # Main script starts here
-check_protoc_installed
+set_proto_config
 initialize_variables
 select_clang_format_version
 format_code
