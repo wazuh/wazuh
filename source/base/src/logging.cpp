a049c6fc0b95871620d6fa739fae690cdd29e9d5
Nahuel Figueroa <nahuelfigueroa978@hotmail.com>
2024-08-28 19:08:22 +0000

    Revert the removal of options from the logger library

--- source/base/src/logging.cpp
+++ source/base/src/logging.cpp
@@ -9,9 +9,65 @@
  */
 
 #include <base/logging.hpp>
+#include <spdlog/pattern_formatter.h>
 
 namespace logging
 {
+
+class CustomSink : public spdlog::sinks::sink
+{
+public:
+    CustomSink()
+        : m_upFormatter(std::make_unique<spdlog::pattern_formatter>())
+    {
+    }
+
+    void log(const spdlog::details::log_msg& message) override
+    {
+        if (should_log(message.level))
+        {
+            m_level = message.level;
+            spdlog::memory_buf_t buf;
+            m_upFormatter->format(message, buf);
+            std::string formatted_message(buf.data(), buf.size());
+
+            if (message.level >= spdlog::level::warn)
+            {
+                std::cerr << formatted_message;
+            }
+            else
+            {
+                std::cout << formatted_message;
+            }
+        }
+    }
+
+    void flush() override
+    {
+        if (m_level >= spdlog::level::warn)
+        {
+            std::cerr << std::flush;
+        }
+        else
+        {
+            std::cout << std::flush;
+        }
+    }
+
+    void set_pattern(const std::string& pattern) override
+    {
+        m_upFormatter = std::make_unique<spdlog::pattern_formatter>(pattern, spdlog::pattern_time_type::local);
+    }
+
+    void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override
+    {
+        m_upFormatter = std::move(sink_formatter);
+    }
+
+private:
+    spdlog::level::level_enum m_level;
+    std::unique_ptr<spdlog::formatter> m_upFormatter;
+};
 
 std::shared_ptr<spdlog::logger> getDefaultLogger()
 {
@@ -47,9 +103,20 @@ void start(const LoggingConfig& cfg)
         spdlog::init_thread_pool(cfg.queueSize, cfg.dedicatedThreads);
     }
 
-    logger = spdlog::stdout_color_mt("default");
+    if (cfg.filePath == STD_ERR_PATH || cfg.filePath == STD_OUT_PATH || cfg.filePath.empty())
+    {
+        auto custumSink = std::make_shared<CustomSink>();
+        custumSink->set_level(spdlog::level::info);
+        custumSink->set_pattern(DEFAULT_LOG_HEADER);
 
-    setLevel(cfg.level);
+        logger = std::make_shared<spdlog::logger>("default", custumSink);
+        spdlog::set_default_logger(logger);
+    }
+    else
+    {
+        logger = spdlog::basic_logger_mt("default", cfg.filePath, cfg.truncate);
+        setLevel(cfg.level);
+    }
 
     logger->flush_on(spdlog::level::trace);
 }
