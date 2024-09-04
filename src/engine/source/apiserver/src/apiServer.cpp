593061e601ce57b6a6a37761d17c069f14d8e047
Dwordcito <octa.tala@gmail.com>
2024-09-04 15:55:23 -0300

    Add apidoc/openapi documentation and generation.

--- src/engine/source/apiserver/src/apiServer.cpp
+++ src/engine/source/apiserver/src/apiServer.cpp
@@ -1,6 +1,22 @@
+// Copyright (C) 2024 Wazuh Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <apiserver/apiServer.hpp>
 #include <base/json.hpp>
 #include <base/logging.hpp>
+#include <stdexcept>
 
 using namespace apiserver;
 
@@ -13,35 +29,37 @@ ApiServer::ApiServer()
         });
 
     m_svr.set_error_handler(
-        [](const auto& req, auto& res)
+        [](const auto& /*req*/, auto& res)
         {
             // TODO: Add metrics here.
             json::Json response {};
-            response.appendString("Invalid request", "/error");
+            response.appendString("Service Unavailable", "/error");
             response.setInt(res.status, "/code");
             res.set_content(response.str(), "application/json");
         });
 }
 
-void ApiServer::stop()
+ApiServer::~ApiServer()
 {
-    if (m_thread.joinable())
-    {
-        if (m_svr.is_running())
-        {
-            m_svr.stop();
-        }
-        m_thread.join();
-    }
+    stop();
 }
 
 void ApiServer::start(const std::filesystem::path& socketPath)
 {
+    if (socketPath.empty())
+    {
+        throw std::invalid_argument("Socket path cannot be empty");
+    }
+
+    if (m_svr.is_running())
+    {
+        throw ServerAlreadyRunningException();
+    }
 
     m_svr.set_address_family(AF_UNIX);
 
     // Create parent directory if it does not exist
-    if (!std::filesystem::exists(socketPath.parent_path()))
+    if (socketPath.has_parent_path() && !std::filesystem::exists(socketPath.parent_path()))
     {
         std::filesystem::create_directories(socketPath.parent_path());
     }
@@ -52,6 +70,18 @@ void ApiServer::start(const std::filesystem::path& socketPath)
     }
 
     m_thread = std::thread([this, socketPath]() { m_svr.listen(socketPath, true); });
+
+    m_svr.wait_until_ready();
+}
+
+void ApiServer::stop()
+{
+    m_svr.stop();
+
+    if (m_thread.joinable())
+    {
+        m_thread.join();
+    }
 }
 
 void ApiServer::addRoute(const Method method,
@@ -66,4 +96,9 @@ void ApiServer::addRoute(const Method method,
         case Method::DELETE: m_svr.Delete(route, handler); break;
         default: LOG_ERROR("Invalid method: {}", static_cast<int>(method)); break;
     }
+}
+
+bool ApiServer::isRunning() const
+{
+    return m_svr.is_running();
 }
