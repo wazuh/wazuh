be7c54d0139e7e251cdcd85c3b37416f6eaaeccf
sebasfalcone <sebastian.falcone@wazuh.com>
2024-09-13 22:46:59 -0300

    CL: - Aligned testing with changes on monitoring class

--- src/shared_modules/indexer_connector/tests/unit/fakeOpenSearchServer.hpp
+++ src/shared_modules/indexer_connector/tests/unit/fakeOpenSearchServer.hpp
@@ -73,19 +73,32 @@ public:
      */
     void run()
     {
-        m_server.Get(
-            "/_cat/health",
-            [this](const httplib::Request& req, httplib::Response& res)
-            {
-                // Simulate a slow server
-                std::this_thread::sleep_for(std::chrono::milliseconds(m_forcedDelay));
+        m_server.Get("/_cat/health",
+                     [this](const httplib::Request& req, httplib::Response& res)
+                     {
+                         // Simulate a slow server
+                         std::this_thread::sleep_for(std::chrono::milliseconds(m_forcedDelay));
 
-                std::stringstream ss;
-                ss << "epoch      timestamp cluster            status node.total node.data discovered_cluster_manager "
-                      "shards pri relo init unassign pending_tasks max_task_wait_time active_shards_percent\n";
-                ss << "1694645550 22:52:30 opensearch-cluster " << m_health << " 2 2 true 14 7 0 0 0 0 - 100.0%\n";
-                res.set_content(ss.str(), "text/plain");
-            });
+                         const auto response = nlohmann::json::array({{{"epoch", "1726271464"},
+                                                                       {"timestamp", "23:51:04"},
+                                                                       {"cluster", "wazuh-cluster"},
+                                                                       {"status", m_health},
+                                                                       {"node.total", "1"},
+                                                                       {"node.data", "1"},
+                                                                       {"discovered_cluster_manager", "true"},
+                                                                       {"shards", "166"},
+                                                                       {"pri", "166"},
+                                                                       {"relo", "0"},
+                                                                       {"init", "0"},
+                                                                       {"unassign", "0"},
+                                                                       {"pending_tasks", "0"},
+                                                                       {"max_task_wait_time", "-"},
+                                                                       {"active_shards_percent", "100.0%"}}});
+
+                         std::stringstream ss;
+                         ss << response.dump();
+                         res.set_content(ss.str(), "application/json");
+                     });
         m_server.set_keep_alive_max_count(1);
         m_server.listen(m_host.c_str(), m_port);
     }
