11a616502bef4fe27b53b4eb4898864b9f7204e2
Javier Bejar Mendez <javier.bejar@wazuh.com>
2024-09-24 17:07:36 +0200

    Simplified metric and manager relation

---
+++ source/metrics/src/metric/allMetrics.hpp
@@ -0,0 +1,7 @@
+#ifndef _METRICS_ALLMETRIC_HPP
+#define _METRICS_ALLMETRIC_HPP
+
+#include "uIntCounter.hpp"
+#include "uIntHistogram.hpp"
+
+#endif // _METRICS_ALLMETRIC_HPP
--- source/metrics/test/src/unit/metrics/metrics_test.hpp
+++
@@ -1,7 +0,0 @@
-
-
-namespace metrics
-{
-
-
-}
