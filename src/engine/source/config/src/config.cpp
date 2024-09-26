85936226cd2d969433dcb3221f8568330f36aa7e
Julian Morales <jmorales@unc.edu.ar>
2024-09-26 23:03:37 +0000

    Add new start to server

--- src/engine/source/config/src/config.cpp
+++ src/engine/source/config/src/config.cpp
@@ -1,9 +1,23 @@
 #include <config/config.hpp>
 
+#include <unistd.h>
+
 #include <fmt/format.h>
 
-
-
+namespace {
+std::string getExecutablePath()
+{
+    char path[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", path, PATH_MAX);
+    if (count != -1)
+    {
+        path[count] = '\0';
+        std::string pathStr(path);
+        return pathStr.substr(0, pathStr.find_last_of('/'));
+    }
+    return {};
+}
+}
 
 namespace config
 {
@@ -13,21 +27,11 @@ using namespace internal;
 Config::Config()
 {
 
-    // Register all configurations 
+    // Register aviation configuration units
 
-    // OLD Server module
-    // TODO Deprecate this configuration after the migration to the new httplib server
-    m_units["/server/thread_pool_size"] = UConf<int>::make("WAZUH_SERVER_THREAD_POOL_SIZE", 1);
-    m_units["/server/event_socket"] =
-        UConf<std::string>::make("WAZUH_SERVER_EVENT_SOCKET", "/var/wazuh/sockets/old-queue.sock");
-    m_units["/server/event_queue_size"] = UConf<int>::make("WAZUH_SERVER_EVENT_QUEUE_SIZE", 0);
-    m_units["/server/api_socket"] =
-        UConf<std::string>::make("WAZUH_SERVER_API_SOCKET", "/var/wazuh/sockets/old-api.sock");
-    m_units["/server/api_queue_size"] = UConf<int>::make("WAZUH_SERVER_API_QUEUE_SIZE", 50);
-    m_units["/server/client_timeout"] = UConf<int>::make("WAZUH_SERVER_CLIENT_TIMEOUT", 1000);
+    // Logging module
+    m_units["/logging/level"] = UConf<std::string>::make("WAZUH_LOGGING_LEVEL", "info");
 
-    // New API Server module
-    m_units["/api_server/socket"] = UConf<std::string>::make("WAZUH_API_SERVER_SOCKET", "/var/wazuh/sockets/api.sock");
 
     // Store module
     m_units["/store/path"] = UConf<std::string>::make("WAZUH_STORE_PATH", "/var/wazuh/store");
@@ -35,8 +39,15 @@ Config::Config()
     // KVDB module
     m_units["/kvdb/path"] = UConf<std::string>::make("WAZUH_KVDB_PATH", "/var/wazuh/kvdb");
 
-    // Orchestrator module
-    m_units["/orchestrator/threads"] = UConf<int>::make("WAZUH_ORCHESTRATOR_THREADS", 1);
+    // Indexer connector
+    m_units["/indexer/index"] = UConf<std::string>::make("WAZUH_INDEXER_INDEX", "wazuh-alerts-5x");
+    m_units["/indexer/host"] = UConf<std::vector<std::string>>::make("WAZUH_INDEXER_HOST", {"http://127.0.0.1:9200"});
+    m_units["/indexer/user"] = UConf<std::string>::make("WAZUH_INDEXER_USER", "admin");
+    m_units["/indexer/password"] = UConf<std::string>::make("WAZUH_INDEXER_PASSWORD", "WazuhEngine5+");
+    m_units["/indexer/ssl/certificate_authorities"] =
+        UConf<std::string>::make("WAZUH_INDEXER_SSL_CERTIFICATE_AUTHORITIES", "");
+    m_units["/indexer/ssl/certificate"] = UConf<std::string>::make("WAZUH_INDEXER_SSL_CERTIFICATE", "");
+    m_units["/indexer/ssl/key"] = UConf<std::string>::make("WAZUH_INDEXER_SSL_KEY", "");
 
     // Queue module
     m_units["/queue/size"] = UConf<int>::make("WAZUH_QUEUE_SIZE", 1000000);
@@ -50,22 +61,27 @@ Config::Config()
     // If enabled, the queue will drop the flood events instead of storing them in the file.
     m_units["/queue/drop_on_flood"] = UConf<bool>::make("WAZUH_QUEUE_DROP_ON_FLOOD", false);
 
-    // Indexer connector
-    m_units["/indexer/index"] = UConf<std::string>::make("WAZUH_INDEXER_INDEX", "wazuh-alerts-5x");
-    m_units["/indexer/host"] = UConf<std::vector<std::string>>::make("WAZUH_INDEXER_HOST", {"http://127.0.0.1:9200"});
-    m_units["/indexer/user"] = UConf<std::string>::make("WAZUH_INDEXER_USER", "admin");
-    m_units["/indexer/password"] = UConf<std::string>::make("WAZUH_INDEXER_PASSWORD", "WazuhEngine5+");
-    m_units["/indexer/ssl/certificate_authorities"] =
-        UConf<std::string>::make("WAZUH_INDEXER_SSL_CERTIFICATE_AUTHORITIES", "");
-    m_units["/indexer/ssl/certificate"] = UConf<std::string>::make("WAZUH_INDEXER_SSL_CERTIFICATE", "");
-    m_units["/indexer/ssl/key"] = UConf<std::string>::make("WAZUH_INDEXER_SSL_KEY", "");
+    // Orchestrator module
+    m_units["/orchestrator/threads"] = UConf<int>::make("WAZUH_ORCHESTRATOR_THREADS", 1);
 
-    // Logging module
-    m_units["/logging/level"] = UConf<std::string>::make("WAZUH_LOGGING_LEVEL", "info");
+    // OLD Server module
+    // TODO Deprecate this configuration after the migration to the new httplib server
+    m_units["/server/thread_pool_size"] = UConf<int>::make("WAZUH_SERVER_THREAD_POOL_SIZE", 1);
+    m_units["/server/event_socket"] =
+        UConf<std::string>::make("WAZUH_SERVER_EVENT_SOCKET", "/var/wazuh/sockets/old-queue.sock");
+    m_units["/server/event_queue_size"] = UConf<int>::make("WAZUH_SERVER_EVENT_QUEUE_SIZE", 0);
+
+    m_units["/server/api_socket"] =
+        UConf<std::string>::make("WAZUH_SERVER_API_SOCKET", "/var/wazuh/sockets/old-api.sock");
+    m_units["/server/api_queue_size"] = UConf<int>::make("WAZUH_SERVER_API_QUEUE_SIZE", 50);
+    m_units["/server/api_timeout"] = UConf<int>::make("WAZUH_SERVER_API_TIMEOUT", 5000);
+
+    // New API Server module
+    m_units["/api_server/socket"] = UConf<std::string>::make("WAZUH_API_SERVER_SOCKET", getExecutablePath() + "/sockets/api.sock");
 
     // TZDB module
-    m_units["/tzdb/path"] = UConf<std::string>::make("WAZUH_TZDB_PATH", "/var/wazuh/tzdb/");
-    m_units["/tzdb/auto_update"] = UConf<bool>::make("WAZUH_TZDB_AUTO_UPDATE", true);
+    m_units["/tzdb/path"] = UConf<std::string>::make("WAZUH_TZDB_PATH", getExecutablePath() + "/tzdb");
+    m_units["/tzdb/auto_update"] = UConf<bool>::make("WAZUH_TZDB_AUTO_UPDATE", false);
 };
 
 json::Json Config::loadFromAPI() const
