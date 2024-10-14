c3a66efea9c382fa30fcf6fd7bcd8be5fcde96b9
GGP1 <gaston.palomeque@wazuh.com>
2024-10-14 14:38:07 -0300

    Add get commands unit test

--- framework/wazuh/core/indexer/tests/test_commands.py
+++ framework/wazuh/core/indexer/tests/test_commands.py
@@ -2,14 +2,14 @@ from dataclasses import asdict
 from unittest import mock
 
 import pytest
-from wazuh.core.indexer.commands import CommandsIndex
-from wazuh.core.indexer.base import POST_METHOD, remove_empty_values
-from wazuh.core.indexer.models.commands import Command, Source, CreateCommandResponse
+from wazuh.core.indexer.commands import CommandsIndex, STATUS_KEY, TARGET_ID_KEY
+from wazuh.core.indexer.base import IndexerKey, POST_METHOD, remove_empty_values
+from wazuh.core.indexer.models.commands import Command, Source, Status, Type, CreateCommandResponse
 
 
 class TestCommandsIndex:
     index_class = CommandsIndex
-    create_command = Command(source=Source.ENGINE, user='0191480e-7f67-7fd3-8c52-f49a3176360b')
+    create_command = Command(source=Source.ENGINE)
 
     @pytest.fixture
     def client_mock(self) -> mock.AsyncMock:
@@ -20,7 +20,7 @@ class TestCommandsIndex:
         return self.index_class(client=client_mock)
 
     async def test_create(self, index_instance: CommandsIndex, client_mock: mock.AsyncMock):
-        """Check the correct function of `create` method."""
+        """Check the correct functionality of the `create` method."""
         response = await index_instance.create(self.create_command)
 
         assert isinstance(response, CreateCommandResponse)
@@ -28,4 +28,39 @@ class TestCommandsIndex:
             method=POST_METHOD,
             url=index_instance.COMMAND_MANAGER_PLUGIN_URL,
             body=asdict(self.create_command, dict_factory=remove_empty_values),
+        )
+
+    async def test_get(self, index_instance: CommandsIndex, client_mock: mock.AsyncMock):
+        """Check the correct functionality of the `get` method."""
+        uuid = '0191dd54-bd16-7025-80e6-ae49bc101c7a'
+        status = Status.PENDING
+        query = {
+            IndexerKey.QUERY: {
+                IndexerKey.BOOL: {
+                    IndexerKey.MUST: [
+                        {IndexerKey.MATCH: {TARGET_ID_KEY: uuid}},
+                        {IndexerKey.MATCH: {STATUS_KEY: status}},
+                    ]
+                }
+            }
+        }
+        search_result = {IndexerKey.HITS: {IndexerKey.HITS: [
+            {
+                IndexerKey._ID: 'pBjePGfvgm',
+                IndexerKey._SOURCE: {'target': {'id': uuid, 'type': Type.AGENT}, 'status': status}
+            },
+            {
+                IndexerKey._ID: 'pBjePGfvgn',
+                IndexerKey._SOURCE: {'target': {'id': '001', 'type': Type.AGENT}, 'status': status}
+            },
+        ]}}
+        client_mock.search.return_value = search_result
+
+        result = await index_instance.get(uuid=uuid, status=status)
+
+        hits = search_result[IndexerKey.HITS][IndexerKey.HITS]
+        assert result == [Command.from_dict(data[IndexerKey._ID], data[IndexerKey._SOURCE]) for data in hits]
+        client_mock.search.assert_called_once_with(
+            index=index_instance.INDEX,
+            body=query,
         )
