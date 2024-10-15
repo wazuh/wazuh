fe3cfc768c8d1b18683d75ddac94434a2f1516ec
Julian Morales <jmorales@unc.edu.ar>
2024-10-15 20:05:46 +0000

    First aproach engine-catalog

---
+++ src/engine/tools/engine-suite/src/engine_catalog/cmds/delete.py
@@ -0,0 +1,28 @@
+import shared.resource_handler as rs
+
+
+def run(args, resource_handler: rs.ResourceHandler):
+
+    # Get the params
+    api_socket: str = args['api_socket']
+    namespace: str = args['namespace']
+    asset: str = args['asset']
+
+    # Create the api request
+    try:
+        resource_handler.delete_catalog_file(api_socket, '', asset, namespace)
+    except Exception as e:
+        print(f'Error deleting asset or collection: {e}')
+        exit(1)
+
+    return 0
+
+
+def configure(subparsers):
+    parser_delete = subparsers.add_parser(
+        'delete', help='delete asset-type[/asset-name[/version]]: Delete an asset or a collection.')
+
+    parser_delete.add_argument('asset', type=str,
+                               help=f'asset or collection to delete: asset-type[/asset-name[/version]]')
+
+    parser_delete.set_defaults(func=run)
