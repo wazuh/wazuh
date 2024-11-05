9eb7486baf8a7fd6aa501408af645b08634fa788
Julian Morales <jmorales@unc.edu.ar>
2024-11-05 14:38:44 +0000

    Add manage session commands

--- src/engine/tools/engine-suite/src/engine_test/cmds/session_delete.py
+++ src/engine/tools/engine-suite/src/engine_test/cmds/session_delete.py
@@ -4,39 +4,40 @@ from google.protobuf.json_format import ParseDict
 from shared.dumpers import dict_to_yml
 
 from api_communication.client import APIClient
-import api_communication.proto.tester_pb2 as etester
 import api_communication.proto.engine_pb2 as engine
+import api_communication.proto.tester_pb2 as etester
+
 
 
 def run(args):
 
     # Get the params
     api_socket: str = args['api_socket']
+    name: str = args['name']
 
     # Create API client
     client = APIClient(api_socket)
 
     # Create the request
-    request = etester.TableGet_Request()
+    request = etester.SessionDelete_Request()
+    request.name = name
 
     # Send the request
     error, response = client.send_recv(request)
     if error:
-        sys.exit(f'Error getting session list: {error}')
+        sys.exit(f'Error deleting session: {error}')
 
     # Parse the response
-    parsed_response = ParseDict(response, etester.TableGet_Response())
+    parsed_response = ParseDict(response, engine.GenericStatus_Response())
     if parsed_response.status == engine.ERROR:
-        sys.exit(f'Error getting session list: {parsed_response.error}')
-
-    # Print the response
-    data: str = dict_to_yml(response)
-
-    print(data)
+        sys.exit(f'Error deleting session: {parsed_response.error}')
 
     return 0
 
 
 def configure(subparsers):
-    parser = subparsers.add_parser('list', help='List all sessions')
+    parser = subparsers.add_parser(
+        'delete', help='Delete a session for testing')
+    parser.add_argument(
+        'name', type=str, help='Name of the session to delete')
     parser.set_defaults(func=run)
