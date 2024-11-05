5066ddce6b7a2ad6a0aa6b8fc8e1551ac29bd255
Nico Stefani <nicolas.stefi@wazuh.com>
2024-11-05 11:34:08 -0300

    Make installation FHS compliant

--- framework/wrappers/generic_wrapper.sh
+++ framework/wrappers/generic_wrapper.sh
@@ -4,7 +4,7 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 WPYTHON_BIN="framework/python/bin/python3"
-WAZUH_PATH="/var/lib/wazuh-server"
+WAZUH_PATH="/usr/share/wazuh-server"
 
 SCRIPT_PATH_NAME="$0"
 SCRIPT_NAME="$(basename ${SCRIPT_PATH_NAME})"
