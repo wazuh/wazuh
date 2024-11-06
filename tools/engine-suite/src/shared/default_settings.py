7a381c95dfbbf58e53f35f09adbc229ef729f45a
Julian Morales <jmorales@unc.edu.ar>
2024-11-06 19:09:13 +0000

    Adapt the code of new tools after rebase

--- tools/engine-suite/src/shared/default_settings.py
+++ tools/engine-suite/src/shared/default_settings.py
@@ -3,3 +3,4 @@ class Constants:
     SOCKET_PATH: str = '/var/run/wazuh/api.sock'
     DEFAULT_POLICY: str = 'policy/wazuh/0'
     DEFAULT_NS: str = 'user'
+    INDEX_PATTERN: str = 'wazuh-alerts-5x'
