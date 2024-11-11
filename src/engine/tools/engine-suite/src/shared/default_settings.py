61387810c5180a9813f316a33892056b6d8e1672
Julian Morales <jmorales@unc.edu.ar>
2024-11-11 13:09:18 +0000

    Add api-communication as dep and change default socket api

--- src/engine/tools/engine-suite/src/shared/default_settings.py
+++ src/engine/tools/engine-suite/src/shared/default_settings.py
@@ -1,6 +1,6 @@
 
 class Constants:
-    SOCKET_PATH: str = '/var/run/wazuh/api.sock'
+    SOCKET_PATH: str = '/run/wazuh-server/engine-api.socket'
     DEFAULT_POLICY: str = 'policy/wazuh/0'
     DEFAULT_NS: str = 'user'
     INDEX_PATTERN: str = 'wazuh-alerts-5x'
