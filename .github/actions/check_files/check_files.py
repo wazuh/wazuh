6581ddbff78508dd6deea82042b789580885abb1
sebasfalcone <sebastian.falcone@wazuh.com>
2024-11-12 13:23:26 -0300

    CL: - Added flag for file size check - Refactored check_files script to improve legibility - Updated manager expected csv (removed file size) - Added file size flag to the manager package build workflow

--- .github/actions/check_files/check_files.py
+++ .github/actions/check_files/check_files.py
@@ -1,8 +1,8 @@
 import argparse
 import csv
 from datetime import datetime
-import hashlib
-import mimetypes
+import grp
+import pwd
 import os
 import pandas as pd
 import platform
@@ -15,72 +15,63 @@ wazuh_uid = -1
 HEADERS = ['full_filename', 'owner_name', 'group_name', 'mode',
            'type', 'prot_permissions', 'size_bytes', 'size_error']
 
-_filemode_table = (
-    ((stat.S_IFLNK, "l"),
-     (stat.S_IFREG, "-"),
-     (stat.S_IFBLK, "b"),
-     (stat.S_IFDIR, "d"),
-     (stat.S_IFCHR, "c"),
-     (stat.S_IFIFO, "p")),
 
-    ((stat.S_IRUSR, "r"),),
-    ((stat.S_IWUSR, "w"),),
-    ((stat.S_IXUSR | stat.S_ISUID, "s"),
-     (stat.S_ISUID, "S"),
-     (stat.S_IXUSR, "x")),
+class helper:
+    @staticmethod
+    def format_timestamp(timestamp):
+        """
+        Convert a timestamp to a formatted string.
+        """
+        return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
-    ((stat.S_IRGRP, "r"),),
-    ((stat.S_IWGRP, "w"),),
-    ((stat.S_IXGRP | stat.S_ISGID, "s"),
-     (stat.S_ISGID, "S"),
-     (stat.S_IXGRP, "x")),
+    @staticmethod
+    def filemode(mode: str) -> str:
+        """ Convert a file's mode to a string of the form '-rwxrwxrwx'.
 
-    ((stat.S_IROTH, "r"),),
-    ((stat.S_IWOTH, "w"),),
-    ((stat.S_IXOTH | stat.S_ISVTX, "t"),
-     (stat.S_ISVTX, "T"),
-     (stat.S_IXOTH, "x"))
-)
+        Args:
+            mode (str): The mode of the file.
 
-# ---------------------------------------------------------------------------------------------------------------
+        Returns:
+            str: The string representation of the file's mode.
+        """
 
-"""
-    ACTION
+        _filemode_table = (
+            ((stat.S_IFLNK, "l"),
+             (stat.S_IFREG, "-"),
+                (stat.S_IFBLK, "b"),
+                (stat.S_IFDIR, "d"),
+                (stat.S_IFCHR, "c"),
+                (stat.S_IFIFO, "p")),
 
-    Parameters:
-        - param1: 
-    Return:
+            ((stat.S_IRUSR, "r"),),
+            ((stat.S_IWUSR, "w"),),
+            ((stat.S_IXUSR | stat.S_ISUID, "s"),
+             (stat.S_ISUID, "S"),
+                (stat.S_IXUSR, "x")),
 
-    Example:
+            ((stat.S_IRGRP, "r"),),
+            ((stat.S_IWGRP, "w"),),
+            ((stat.S_IXGRP | stat.S_ISGID, "s"),
+             (stat.S_ISGID, "S"),
+                (stat.S_IXGRP, "x")),
 
-"""
+            ((stat.S_IROTH, "r"),),
+            ((stat.S_IWOTH, "w"),),
+            ((stat.S_IXOTH | stat.S_ISVTX, "t"),
+             (stat.S_ISVTX, "T"),
+                (stat.S_IXOTH, "x"))
+        )
 
+        perm = []
+        for table in _filemode_table:
+            for bit, char in table:
+                if mode & bit == bit:
+                    perm.append(char)
+                    break
+            else:
+                perm.append("-")
+        return "".join(perm)
 
-def filemode(mode):
-
-    perm = []
-    for table in _filemode_table:
-        for bit, char in table:
-            if mode & bit == bit:
-                perm.append(char)
-                break
-        else:
-            perm.append("-")
-    return "".join(perm)
-
-# ---------------------------------------------------------------------------------------------------------------
-
-
-"""
-    ACTION
-
-    Parameters:
-        - param1: 
-    Return:
-
-    Example:
-
-"""
 
 def translate_uid(id):
     # If wazuh_uid was not set, use the default behavior
@@ -90,6 +81,7 @@ def translate_uid(id):
         return "wazuh"
     elif id == 0:
         return "root"
+
 
 def translate_gid(id):
     # If wazuh_gid was not set, use the default behavior
@@ -101,79 +93,107 @@ def translate_gid(id):
         return "root"
 
 
-def get_data(item):
+def get_data(item, size_check=False):
+    """
+    Main function to retrieve file metadata based on the operating system.
+    """
     result = {}
-
-    # Common attributes
     stat_info = os.stat(item)
+
+    # Populate common attributes
+    populate_common_attributes(result, stat_info, size_check)
+
+    # Platform-specific attributes
+    if platform.system() != 'Windows':
+        populate_unix_attributes(result, item, stat_info)
+    elif platform.system() == 'Windows':
+        populate_windows_attributes(result, item)
+
+    # Special cases
+    handle_special_cases(result, item)
+
+    return result
+
+
+def populate_common_attributes(result, stat_info, size_check):
+    """
+    Populate common attributes for any operating system.
+    """
     result['mode'] = oct(stat.S_IMODE(stat_info.st_mode))[2:]
     result['type'] = "file"
     result['prot_permissions'] = stat.filemode(stat_info.st_mode)
-    result['size_error'] = 0.2
 
-    #if 'win32' or 'cygwin' or 'msys' in sys.platform:
-    #    print("Im windows")
+    if size_check:
+        result['size_error'] = 0.2
+        result['size_bytes'] = stat_info.st_size
+    else:
+        result['size_error'] = ''
+        result['size_bytes'] = ''
+
+
+def populate_unix_attributes(result, item, stat_info):
+    """
+    Populate attributes specific to Unix-like systems (Linux, macOS).
+    """
+    result['group_name'] = translate_gid(stat_info.st_gid)
+    result['owner_name'] = translate_uid(stat_info.st_uid)
+    result['last_modified'] = helper.format_timestamp(stat_info.st_mtime)
+    result['last_accessed'] = helper.format_timestamp(stat_info.st_atime)
+
     if platform.system() == 'Linux' and os.path.isfile(item):
-        import grp
-        import pwd
-        result['group_name'] = translate_gid(stat_info.st_gid)
-        result['owner_name'] = translate_uid(stat_info.st_uid)
-        result['size_bytes'] = stat_info.st_size
-        result['last_modified'] = datetime.fromtimestamp(
-            stat_info.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
-        result['last_accessed'] = datetime.fromtimestamp(
-            stat_info.st_atime).strftime('%Y-%m-%d %H:%M:%S')
-        result['created_time'] = datetime.fromtimestamp(
-            stat_info.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+        result['created_time'] = helper.format_timestamp(stat_info.st_ctime)
     elif platform.system() == 'Darwin':  # macOS
-        import grp
-        result['group_name'] = translate_gid(stat_info.st_gid)
-        result['owner_name'] = translate_uid(stat_info.st_uid)
-        result['size_bytes'] = stat_info.st_size
-        result['last_modified'] = datetime.fromtimestamp(
-            stat_info.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
-        result['last_accessed'] = datetime.fromtimestamp(
-            stat_info.st_atime).strftime('%Y-%m-%d %H:%M:%S')
-        result['created_time'] = datetime.fromtimestamp(
-            stat_info.st_birthtime).strftime('%Y-%m-%d %H:%M:%S')
+        result['created_time'] = helper.format_timestamp(
+            stat_info.st_birthtime)
         result['readonly_flag'] = bool(stat.S_ISREG(
             stat_info.st_mode) and not stat.S_IWUSR & stat_info.st_mode)
-    elif platform.system() == 'Windows':
-        import win32file
-        import pywintypes
-        # TODO: rework on owner and group for windows
-        result['group_name'] = ''
-        result['owner_name'] = ''
-        result['last_modified'] = datetime.fromtimestamp(
-            os.path.getmtime(item)).strftime('%Y-%m-%d %H:%M:%S')
-        result['last_accessed'] = datetime.fromtimestamp(
-            os.path.getatime(item)).strftime('%Y-%m-%d %H:%M:%S')
-        result['created_time'] = datetime.fromtimestamp(
-            os.path.getctime(item)).strftime('%Y-%m-%d %H:%M:%S')
-        result['size_bytes'] = stat_info.st_size
-        try:
-            file_attributes = win32file.GetFileAttributesW(item)
-            result['is_hidden'] = bool(
-                file_attributes & win32file.FILE_ATTRIBUTE_HIDDEN)
-            result['extension'] = os.path.splitext(item)[1][1:]
-            # File attributes as string
-            result['attributes'] = oct(file_attributes)[-4:]
-            result['readonly_flag'] = bool(
-                file_attributes & win32file.FILE_ATTRIBUTE_READONLY)
-            result['system_flag'] = bool(
-                file_attributes & win32file.FILE_ATTRIBUTE_SYSTEM)
-            result['archive_flag'] = bool(
-                file_attributes & win32file.FILE_ATTRIBUTE_ARCHIVE)
-        except pywintypes.error as e:
-            printf(f"Error processing Windows file {item}: {str(e)}")
-            # Set default values for Windows-specific attributes
-            result['is_hidden'] = False
-            result['attributes'] = ''
-            result['readonly_flag'] = False
-            result['system_flag'] = False
-            result['archive_flag'] = False
 
-    # Special case for htpasswd file
+
+def populate_windows_attributes(result, item):
+    """
+    Populate attributes specific to Windows.
+    """
+    import win32file
+    import pywintypes
+
+    result['group_name'] = ''
+    result['owner_name'] = ''
+    result['last_modified'] = helper.format_timestamp(os.path.getmtime(item))
+    result['last_accessed'] = helper.format_timestamp(os.path.getatime(item))
+    result['created_time'] = helper.format_timestamp(os.path.getctime(item))
+
+    try:
+        file_attributes = win32file.GetFileAttributesW(item)
+        result['is_hidden'] = bool(
+            file_attributes & win32file.FILE_ATTRIBUTE_HIDDEN)
+        result['extension'] = os.path.splitext(item)[1][1:]
+        result['attributes'] = oct(file_attributes)[-4:]
+        result['readonly_flag'] = bool(
+            file_attributes & win32file.FILE_ATTRIBUTE_READONLY)
+        result['system_flag'] = bool(
+            file_attributes & win32file.FILE_ATTRIBUTE_SYSTEM)
+        result['archive_flag'] = bool(
+            file_attributes & win32file.FILE_ATTRIBUTE_ARCHIVE)
+    except pywintypes.error as e:
+        print(f"Error processing Windows file {item}: {str(e)}")
+        set_default_windows_attributes(result)
+
+
+def set_default_windows_attributes(result):
+    """
+    Set default values for Windows-specific attributes in case of an error.
+    """
+    result['is_hidden'] = False
+    result['attributes'] = ''
+    result['readonly_flag'] = False
+    result['system_flag'] = False
+    result['archive_flag'] = False
+
+
+def handle_special_cases(result, item):
+    """
+    Handle special cases, such as specific files requiring different metadata.
+    """
     if item == '/var/ossec/api/configuration/auth/htpasswd':
         result['group_name'] = 'root'
         result['mode'] = '0777'
@@ -181,31 +201,24 @@ def get_data(item):
         result['owner_name'] = 'root'
         result['prot_permissions'] = 'lrwxrwxrwx'
 
-    return result
 
-# ---------------------------------------------------------------------------------------------------------------
+def get_current_items(scan_path='/var/ossec', ignore_names=[]):
+    """ Get all the files in the specified directory and its subdirectories.
 
+    Args:
+        scan_path (str, optional): Directory to be scanned. Defaults to '/var/ossec'.
+        ignore_names (list, optional): List of files to be ignored. Defaults to [].
 
-"""
-    ACTION
-
-    Parameters:
-        - param1: 
-    Return:
-
-    Example:
-
-"""
-
-
-def get_current_items(ossec_path='/var/ossec', ignore_names=[]):
+    Returns:
+        list: List of dictionaries with the file metadata.
+    """
     c_items = []
 
-    for (dirpath, dirnames, filenames) in os.walk(ossec_path, followlinks=False):
+    for (dirpath, dirnames, filenames) in os.walk(scan_path, followlinks=False):
         if dirpath not in ignore_names:
             for filename in filenames:
                 file_path = "{0}/{1}".format(dirpath, filename)
-                if not file_path.endswith('.pyc') and not file_path in ignore_names:
+                if not file_path.endswith('.pyc') and file_path not in ignore_names:
                     try:
                         item = {'full_filename': file_path}
                         item.update(get_data(file_path))
@@ -260,13 +273,13 @@ def csv_to_dict(file_path, key_column):
 """
 
 
-def file_diff(mandatory_item, current_items):
+def file_diff(mandatory_item, current_items, size_check):
     differences = {}
 
     for head_type in HEADERS:
         if head_type == 'size_bytes':
             continue
-        elif head_type == 'size_error':
+        elif head_type == 'size_error' and size_check:
             expected_error = float(mandatory_item[head_type])
             mandatory_size_bytes = float(mandatory_item['size_bytes'])
             difference_bytes = abs(
@@ -301,13 +314,13 @@ def printReport(mandatory_items, not_listed, not_fully_match, current_items, rep
     left_mandatory_items = len(mandatory_items)
     unregistered_files = len(not_listed)
     qtty_not_fully_matched = len(not_fully_match)
-    baseReport = ""
+    base_report = ""
     failed = True
 
     if (left_mandatory_items == 0 and unregistered_files == 0 and qtty_not_fully_matched == 0):
         failed = False
 
-    baseReport += f"""
+    base_report += f"""
 # Checkfiles Test
 
 ## Result
@@ -316,7 +329,7 @@ def printReport(mandatory_items, not_listed, not_fully_match, current_items, rep
     """
 
     if failed:
-        baseReport += f"""
+        base_report += f"""
 ## Result Summary
 
 * Items found: {len(current_items)}
@@ -324,7 +337,7 @@ def printReport(mandatory_items, not_listed, not_fully_match, current_items, rep
 """
 
         if qtty_not_fully_matched != 0:
-            baseReport += f"""
+            base_report += f"""
 
 ## differences
 
@@ -334,10 +347,10 @@ def printReport(mandatory_items, not_listed, not_fully_match, current_items, rep
 
 """
             for item in not_fully_match:
-                baseReport += f"* `{item}` : {not_fully_match[item]}\n"
+                base_report += f"* `{item}` : {not_fully_match[item]}\n"
 
         if left_mandatory_items != 0:
-            baseReport += f"""
+            base_report += f"""
 
 ### Mandatory Files Not Found
 
@@ -345,23 +358,23 @@ def printReport(mandatory_items, not_listed, not_fully_match, current_items, rep
 
 """
             for item in mandatory_items:
-                baseReport += f"* `{item}`\n"
+                base_report += f"* `{item}`\n"
 
         if unregistered_files != 0:
-            baseReport += f"""
+            base_report += f"""
 ### Found Files Not Expected
 
 {unregistered_files} unregistered files in mandatory list
 
 """
             for item in not_listed:
-                baseReport += f"* `{item}`\n"
+                base_report += f"* `{item}`\n"
 
     if report_path != '':
         with open(report_path, 'w') as file:
-            file.write(baseReport)
+            file.write(base_report)
     else:
-        print(baseReport)
+        print(base_report)
 
 # ---------------------------------------------------------------------------------------------------------------
 
@@ -390,8 +403,12 @@ if __name__ == "__main__":
                             help="Directory to scan and check, '/var/ossec' by default")
     arg_parser.add_argument("-b", "--base_file", type=str, default="",
                             help="Creates a base csv in path, not to be used with --report")
-    arg_parser.add_argument("-wg", "--wazuh_gid", type=int, help="The group id for wazuh", default=-1)
-    arg_parser.add_argument("-wu", "--wazuh_uid", type=int, help="The user id for wazuh", default=-1)
+    arg_parser.add_argument("-wg", "--wazuh_gid", type=int,
+                            help="The group id for wazuh", default=-1)
+    arg_parser.add_argument("-wu", "--wazuh_uid", type=int,
+                            help="The user id for wazuh", default=-1)
+    arg_parser.add_argument("-s", "--size_check", action="store_false",
+                            help="Disable size validation", default=True)
 
     # TODO:
     # arg_parser.add_argument("-i", "--ignore", type=str, help="Ignore path: /var/ossec/wodles/oscap/content,/var/ossec/api.")
@@ -400,13 +417,13 @@ if __name__ == "__main__":
     wazuh_gid = args.wazuh_gid
     wazuh_uid = args.wazuh_uid
     installed_dir = args.directory
-
+    size_check = args.size_check
     base_file_path = args.base_file
 
-    if (base_file_path != '' and args.report != ''):
-        sys.exit('Do not set csv file creation alongside report creation')
-    elif base_file_path != '':
-        print("starting base csv")
+    if (base_file_path != ''):
+        if args.report != '':
+            sys.exit('Do not set csv file creation alongside report creation')
+        print("Starting base csv creation...")
         result = get_current_items(installed_dir)
         df = pd.DataFrame(result)
         df = df.reindex(columns=HEADERS)
@@ -431,7 +448,8 @@ if __name__ == "__main__":
                 # File name matched
                 mandatory_item_fields = mandatory_items[key_name]
                 mandatory_items.pop(key_name)
-                difference_dict = file_diff(mandatory_item_fields, file_object)
+                difference_dict = file_diff(
+                    mandatory_item_fields, file_object, size_check)
                 if len(difference_dict) != 0:
                     failed = True
                     not_fully_match[key_name] = difference_dict
