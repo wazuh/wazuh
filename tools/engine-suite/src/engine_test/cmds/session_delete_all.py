347a61e805e700dd449f7267f074d6612961ffc3
Julian Morales <jmorales@unc.edu.ar>
2024-11-15 17:39:34 +0000

    Fixed script after rebase

--- tools/engine-suite/src/engine_test/cmds/session_delete_all.py
+++ tools/engine-suite/src/engine_test/cmds/session_delete_all.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
 from google.protobuf.json_format import ParseDict
-from shared.dumpers import dict_to_yml
+from shared.dumpers import dict_to_str_json
 
 from api_communication.client import APIClient
 import api_communication.proto.tester_pb2 as etester
