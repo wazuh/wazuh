83cf02c9ae846a5b5783bc42e6f992638af2d1b9
RamosFe <federico.ramos@wazuh.com>
2024-11-25 10:18:46 -0300

    Add query parameter to filter sections

--- apis/comms_api/core/unix_server/config.py
+++ apis/comms_api/core/unix_server/config.py
@@ -1,22 +1,34 @@
-from fastapi import Request, Response, status
+from fastapi import Response, status, Query, HTTPException
 from starlette.responses import JSONResponse
+from typing import Optional
 
-from wazuh.core.config.client import CentralizedConfig
+from wazuh.core.config.client import CentralizedConfig, ConfigSections
 
 
-async def get_config(request: Request) -> Response:
+async def get_config(sections: Optional[str] = Query(default=None)) -> Response:
     """Retrieve the current configuration from the Server.
 
     Parameters
     ----------
-    request : Request
-        Incoming HTTP request.
+    sections: str
+        String with list of sections separated by comma. If None, all sections are included.
 
     Returns
     -------
     JSONResponse
         HTTP OK response with the configuration as content.
     """
-    config = CentralizedConfig.get_config_dic()
+    if sections:
+        section_list = sections.split(",")
+        try:
+            validated_sections = [ConfigSections(section) for section in section_list]
+        except ValueError as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Invalid section(s): {str(e)}",
+            )
+    else:
+        validated_sections = None
+    config = CentralizedConfig.get_config_dict(sections=validated_sections)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=config)
