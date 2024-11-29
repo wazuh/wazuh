396ecaec924d3087dab178aa84d7ba83811695f7
Nahuel Figueroa <63238635+NahuFigueroa97@users.noreply.github.com>
2024-11-29 17:20:42 -0300

    Update upload wazuh server images

--- .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
@@ -13,15 +13,10 @@ GITHUB_OWNER="wazuh"
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
-# Extract the architecture from DOCKER_IMAGE_NAME
-ARCHITECTURE=$(echo $DOCKER_IMAGE_NAME | awk -F'_' '{print $NF}')
-
 # Login to GHCR
 echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u $GITHUB_USER --password-stdin
 
 # Build image for the specified architecture
 echo build -t ${IMAGE_ID} -f ${DOCKERFILE_PATH} ${BUILD_CONTEXT}
-docker build --platform linux/$ARCHITECTURE -t ${IMAGE_ID} -f ${DOCKERFILE_PATH} ${BUILD_CONTEXT}
-
-# Push the built image
+docker build -t ${IMAGE_ID} -f ${DOCKERFILE_PATH} ${BUILD_CONTEXT}
 docker push ${IMAGE_ID}
