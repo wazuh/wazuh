876fc3ba91abfde71622592caac155f694d1cdf8
GGP1 <gaston.palomeque@wazuh.com>
2024-11-29 10:27:48 -0300

    Rename wazuh-manager folder to wazuh-server

--- apis/tools/env/5.0/wazuh-server/entrypoint.sh
+++ apis/tools/env/5.0/wazuh-server/entrypoint.sh
@@ -2,9 +2,11 @@
 
 cp /tmp/wazuh-server.yml /etc/wazuh-server/wazuh-server.yml
 
+# Set node's name
+sed -i "s:name\: server_01:name\: $2:g" /etc/wazuh-server/wazuh-server.yml
+
 if [ "$3" != "manager" ]
 then
-    sed -i "s:name\: server_01:name\: $2:g" /etc/wazuh-server/wazuh-server.yml
     # TODO: rename type name to 'manager' once we do the name migration
     sed -i "s:type\: master:type\: worker:g" /etc/wazuh-server/wazuh-server.yml
 fi
