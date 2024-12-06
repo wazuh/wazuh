6dca0c1aa7bf267732a6a101d80a336ec7e71c5c
GGP1 <gaston.palomeque@wazuh.com>
2024-12-06 10:14:21 -0300

    Move constants to a common place

--- api/api/constants.py
+++ api/api/constants.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
-
 from wazuh.core import common
 
 API = 'api'
@@ -11,7 +10,10 @@ CONFIG_FILE_PATH = CONFIG_PATH / 'api.yaml'
 SECURITY_PATH = CONFIG_PATH / 'security'
 SECURITY_CONFIG_PATH = SECURITY_PATH / 'security.yaml'
 
-API_SSL_PATH = CONFIG_PATH / 'ssl'
 INSTALLATION_UID_PATH = common.WAZUH_LIB / 'installation_uid'
 INSTALLATION_UID_KEY = 'installation_uid'
 UPDATE_INFORMATION_KEY = 'update_information'
+
+API_SSL_PATH = CONFIG_PATH / 'ssl'
+API_CERT_PATH = API_SSL_PATH / 'server.crt'
+API_KEY_PATH = API_SSL_PATH / 'server.key'
