16553ce5a478a04ee39b32aa086ff9dc34feb6a9
pereyra-m <matias.pereyra@wazuh.com>
2024-12-07 02:24:29 +0000

    Restore username-password configuration parameters to IndexerConnector

--- src/engine/source/indexerconnector/include/indexerConnector/indexerConnector.hpp
+++ src/engine/source/indexerconnector/include/indexerConnector/indexerConnector.hpp
@@ -36,6 +36,8 @@ struct IndexerConnectorOptions
 {
     std::string name;               ///< The name of the index to push the data.
     std::vector<std::string> hosts; ///< The list of hosts to connect to. i.e. ["https://localhost:9200"]
+    std::string username;           ///< The username to authenticate with OpenSearch.
+    std::string password;           ///< The password to authenticate with OpenSearch.
     std::string keystorePath;       ///< The path to the keystore.
     struct
     {
