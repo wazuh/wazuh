5c333c45ad56a53364d614d97bbe82626d5b75a2
sebasfalcone <sebastian.falcone@wazuh.com>
2024-12-09 15:26:50 -0300

    CL: - Ported changes from #26922

--- src/engine/source/base/include/base/utils/threadSafeQueue.hpp
+++ src/engine/source/base/include/base/utils/threadSafeQueue.hpp
@@ -17,7 +17,6 @@
 #include <memory>
 #include <mutex>
 #include <queue>
-#include <thread>
 
 namespace base::utils::queue
 {
@@ -116,9 +115,13 @@ public:
         // If the queue is not canceled, get the elements.
         if (!m_canceled)
         {
-            for (auto i = 0; i < elementsQuantity && i < m_queue.size(); ++i)
+            try
             {
-                bulkQueue.push(std::move(m_queue.at(i)));
+                m_queue.frontQueue(bulkQueue, m_queue.size() > elementsQuantity ? elementsQuantity : m_queue.size());
+            }
+            catch (const std::exception& e)
+            {
+                bulkQueue = {};
             }
         }
 
@@ -127,10 +130,13 @@ public:
 
     void popBulk(const uint64_t elementsQuantity)
     {
-        std::scoped_lock lock {m_mutex};
-        for (auto i = 0; i < elementsQuantity && !m_queue.empty(); ++i)
+        std::lock_guard<std::mutex> lock {m_mutex};
+        auto counter = 0ULL;
+
+        while (counter < elementsQuantity && !m_queue.empty())
         {
             m_queue.pop();
+            ++counter;
         }
     }
 
