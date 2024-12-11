97231b1f792c10d23f030e407de183274f9f2535
Javier Bejar <javier.bejar@wazuh.com>
2024-12-11 13:55:56 +0100

    Added new api handlers registration, and removed old api endpoints and server implementation

--- source/api/geo/include/api/geo/handlers.hpp
+++ source/api/geo/include/api/geo/handlers.hpp
@@ -12,5 +12,14 @@ adapter::RouteHandler delDb(const std::shared_ptr<::geo::IManager>& geoManager);
 adapter::RouteHandler listDb(const std::shared_ptr<::geo::IManager>& geoManager);
 adapter::RouteHandler remoteUpsertDb(const std::shared_ptr<::geo::IManager>& geoManager);
 
+inline void registerHandlers(const std::shared_ptr<::geo::IManager>& geoManager,
+                             const std::shared_ptr<httpsrv::Server>& server)
+{
+    server->addRoute(httpsrv::Method::POST, "/geo/db/add", addDb(geoManager));
+    server->addRoute(httpsrv::Method::POST, "/geo/db/del", delDb(geoManager));
+    server->addRoute(httpsrv::Method::POST, "/geo/db/list", listDb(geoManager));
+    server->addRoute(httpsrv::Method::POST, "/geo/db/remoteUpsert", remoteUpsertDb(geoManager));
+}
+
 } // namespace api::geo::handlers
 #endif // _API_GEO_HANDLERS_HPP
--- source/api/router/include/api/router/handlers.hpp
+++ source/api/router/include/api/router/handlers.hpp
@@ -23,6 +23,26 @@ adapter::RouteHandler changeEpsSettings(const std::shared_ptr<::router::IRouterA
 adapter::RouteHandler getEpsSettings(const std::shared_ptr<::router::IRouterAPI>& router);
 adapter::RouteHandler activateEpsLimiter(const std::shared_ptr<::router::IRouterAPI>& router);
 adapter::RouteHandler deactivateEpsLimiter(const std::shared_ptr<::router::IRouterAPI>& router);
+
+inline void registerHandlers(const std::shared_ptr<::router::IRouterAPI>& router,
+                             const std::shared_ptr<api::policy::IPolicy>& policy,
+                             const std::shared_ptr<httpsrv::Server>& server)
+{
+    server->addRoute(httpsrv::Method::POST, "/router/route/post", routePost(router));
+    server->addRoute(httpsrv::Method::POST, "/router/route/delete", routeDelete(router));
+    server->addRoute(httpsrv::Method::POST, "/router/route/get", routeGet(router, policy));
+    server->addRoute(httpsrv::Method::POST, "/router/route/reload", routeReload(router));
+    server->addRoute(httpsrv::Method::POST, "/router/route/patchPriority", routePatchPriority(router));
+
+    server->addRoute(httpsrv::Method::POST, "/router/table/get", tableGet(router, policy));
+    server->addRoute(httpsrv::Method::POST, "/router/queue/post", queuePost(router));
+
+    server->addRoute(httpsrv::Method::POST, "/router/eps/changeSettings", changeEpsSettings(router));
+    server->addRoute(httpsrv::Method::POST, "/router/eps/getSettings", getEpsSettings(router));
+    server->addRoute(httpsrv::Method::POST, "/router/eps/activate", activateEpsLimiter(router));
+    server->addRoute(httpsrv::Method::POST, "/router/eps/deactivate", deactivateEpsLimiter(router));
+}
+
 } // namespace api::router::handlers
 
 #endif // _API_ROUTER_HANDLERS_HPP
