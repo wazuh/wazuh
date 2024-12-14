b8863770dc3bf0f356ca4747c8eebf8cee54bc6d
pereyra-m <matias.pereyra@wazuh.com>
2024-12-14 03:42:56 +0000

    Tracy POC

--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -241,7 +241,7 @@ WriteLogs()
 {
   LOCALFILES_TMP=`cat ${LOCALFILES_TEMPLATE}`
   HAS_JOURNALD=`command -v journalctl`
-  
+
   # If has journald, add journald to the configuration file
   if [ "X$HAS_JOURNALD" != "X" ]; then
     if [ "$1" = "echo" ]; then
@@ -290,14 +290,14 @@ WriteLogs()
           continue
         fi
       fi
-  
+
       # If log file present or skip file
       if [ -f "$FILE" ] || [ "X$SKIP_CHECK_FILE" = "Xyes" ]; then
         # Print
         if [ "$1" = "echo" ]; then
             echo "    -- $FILE"
         # Add to the configuration file
-        elif [ "$1" = "add" ]; then         
+        elif [ "$1" = "add" ]; then
           echo "  <localfile>" >> $NEWCONFIG
           if [ "$FILE" = "snort" ]; then
             head -n 1 $FILE|grep "\[**\] "|grep -v "Classification:" > /dev/null
@@ -1222,6 +1222,14 @@ InstallServer()
 
         if ([ "X${DIST_NAME}" = "Xrhel" ] || [ "X${DIST_NAME}" = "Xcentos" ] || [ "X${DIST_NAME}" = "XCentOS" ]); then
             chcon -t textrel_shlib_t ${INSTALLDIR}/lib/librocksdb.so.8
+        fi
+    fi
+    if [ -f external/tracy-0.11.1/build/libTracyClient.so.0.11.1 ]
+    then
+        ${INSTALL} -m 0750 -o root -g ${WAZUH_GROUP} external/tracy-0.11.1/build/libTracyClient.so.0.11.1 ${INSTALLDIR}/lib
+
+        if ([ "X${DIST_NAME}" = "Xrhel" ] || [ "X${DIST_NAME}" = "Xcentos" ] || [ "X${DIST_NAME}" = "XCentOS" ]); then
+            chcon -t textrel_shlib_t ${INSTALLDIR}/lib/libTracyClient.so.0.11.1
         fi
     fi
 
