a47eba72780f1412db42f3b06ca89a3e0e27964e
GGP1 <gaston.palomeque@wazuh.com>
2024-12-18 14:39:46 -0300

    Fix socket path typo

--- framework/wazuh/core/cluster/common.py
+++ framework/wazuh/core/cluster/common.py
@@ -976,7 +976,7 @@ class Handler(asyncio.Protocol):
         """
         self.logger.info('Sending orders to the Communications API')
 
-        transport = httpx.AsyncHTTPTransport(uds=common.COMMS_API_SOCKET)
+        transport = httpx.AsyncHTTPTransport(uds=common.COMMS_API_SOCKET_PATH)
         client = httpx.AsyncClient(transport=transport, timeout=httpx.Timeout(10))
 
         orders_list = json.loads(orders)
