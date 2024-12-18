72c3217fceff00469a0f492e604f73ba4b9f378e
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-12-18 13:25:55 +0100

    chore: purge deprecated modules' support files

--- wodles/aws/tests/data/schema_empty_table.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE 'cloudtrail' (
-    bucket_path 'text' NOT NULL,
-    aws_account_id 'text' NOT NULL,
-    aws_region 'text' NOT NULL,
-    log_key 'text' NOT NULL,
-    processed_date 'text' NOT NULL,
-    created_date 'integer' NOT NULL,
-    PRIMARY KEY (bucket_path, aws_account_id, aws_region, log_key));
