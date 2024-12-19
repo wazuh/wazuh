b8f5025a44643ff014b62c25bcef3416f13f2637
GGP1 <gaston.palomeque@wazuh.com>
2024-12-19 13:13:57 -0300

    Replace sync manager events with asyncio events

--- apis/comms_api/core/test/test_commands.py
+++ apis/comms_api/core/test/test_commands.py
@@ -1,4 +1,4 @@
-from unittest.mock import call, patch
+from unittest.mock import call, patch, AsyncMock
 from multiprocessing import Event
 
 import pytest
@@ -36,25 +36,13 @@ def test_commands_manager_add_commands(sync_manager_mock):
 
 
 @patch('comms_api.core.commands.SyncManager')
-def test_commands_manager_get_commands(sync_manager_mock):
+async def test_commands_manager_get_commands(sync_manager_mock):
     """Check that the `get_commands` method works as expected."""
     commands_manager = CommandsManager()
     commands_manager._commands = {AGENT_ID: [COMMAND]}
-    commands = commands_manager.get_commands(AGENT_ID)
+    commands = await commands_manager.get_commands(AGENT_ID)
 
     assert commands == [COMMAND]
-
-
-@patch('comms_api.core.commands.SyncManager')
-def test_commands_manager_get_commands_timeout(sync_manager_mock):
-    """Check that the `get_commands` method works as expected if the timeout is reached."""
-    commands_manager = CommandsManager()
-    commands_manager._subscriptions = {}
-    commands_manager._commands = {}
-    commands = commands_manager.get_commands(AGENT_ID, 0.01)
-
-    assert commands is None
-    assert commands_manager._subscriptions == {}
 
 
 @patch('comms_api.core.commands.SyncManager')
@@ -75,7 +63,8 @@ def test_commands_manager_shutdown(sync_manager_mock):
 @pytest.mark.asyncio
 @patch('comms_api.core.commands.CommandsManager')
 async def test_pull_commands(commands_manager_mock):
-    commands_manager_mock.get_commands.return_value = [COMMAND]
+    get_commands_mock = AsyncMock(return_value=[COMMAND])
+    commands_manager_mock.get_commands = get_commands_mock
     commands = await pull_commands(commands_manager_mock, AGENT_ID)
 
     assert commands == [COMMAND]
