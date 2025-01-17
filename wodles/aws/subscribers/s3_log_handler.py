512d8dc2df89a0855d2d47321691addd67501d9c
javier <javier.sanchez@wazuh.com>
2025-01-17 16:58:22 +0100

    Change process logic

--- wodles/aws/subscribers/s3_log_handler.py
+++ wodles/aws/subscribers/s3_log_handler.py
@@ -224,10 +224,10 @@ class AWSSubscriberBucket(wazuh_integration.WazuhIntegration, AWSS3LogHandler):
                 if re.match(self.discard_regex, log['full_log']):
                     aws_tools.debug(f'+++ The "{self.discard_regex.pattern}" regex found a match. '
                                     f'The event will be skipped.', 2)
+                    continue
                 else:
                     print(f'WARNING: The "{self.discard_regex.pattern}" regex did not find a match. '
                           f'The event will be processed.')
-                    continue
             elif self.event_should_be_skipped(log):
                 aws_tools.debug(f'+++ The "{self.discard_regex.pattern}" regex found a match '
                                 f'in the "{self.discard_field}" '
