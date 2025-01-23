3b7c0c51ced8dc4f5cc7f9fce31e23a2b9ab2652
Javier Bejar <javier.bejar@wazuh.com>
2025-01-23 17:13:59 +0100

    Updated engine protocol parser and python tester to align with agent sent fields

--- source/main.cpp
+++ source/main.cpp
@@ -648,17 +648,17 @@ int main(int argc, char* argv[])
              * **Example NDJSON Payload:**
              * ```
              * {"agent":{"id":"2887e1cf-9bf2-431a-b066-a46860080f56","name":"javier","type":"endpoint","version":"5.0.0","groups":["group1","group2"],"host":{"hostname":"myhost","os":{"name":"Amazon Linux 2","platform":"Linux"},"ip":["192.168.1.2"],"architecture":"x86_64"}}}
-             * {"module": "logcollector", "type": "file"}
-             * {"log": {"file": {"path": "/var/log/apache2/access.log"}}, "tags": ["production-server"], "event": {"original": "::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209", "created": "2023-12-26T09:22:14.000Z", "module": "apache-access", "provider": "file"}}
-             * {"log":{"file":{"path":"/var/log/apache2/error.log"}},"tags":["production-server"],"event":{"original":"::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209","created":"2023-12-26T09:22:14.000Z","module":"apache-error","provider":"file"}}
-             * {"log":{"file":{"path":"/var/log/syslog"}},"tags":["production-server"],"event":{"original":"::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209","created":"2023-12-26T09:22:14.000Z","module":"syslog","provider":"file"}}
-             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","module":"$CHANEL_NAME","provider":"windows-evenchannel"}}
-             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","module":"Microsoft-Windows-Security-Auditing","provider":"windows-evenchannel"}}
-             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","module":"Windows PowerShell","provider":"windows-evenchannel"}}
-             * {"tags":["production-server"],"event":{"original":"JSON EVENT OF JOURNALD AS STRING","created":"2023-12-26T09:22:14.000Z","module":"$UNIT_NAME","provider":"journald"}}
-             * {"tags":["production-server"],"event":{"original":"JSON EVENT OF JOURNALD AS STRING","created":"2023-12-26T09:22:14.000Z","module":"ssh.service","provider":"journald"}}
-             * {"module": "inventory", "type": "package"}
-             * {"tags": ["string"], "event": {"original": "string", "created": "string", "module": "string", "provider": "string"}}
+             * {"module": "logcollector", "collector": "file"}
+             * {"log": {"file": {"path": "/var/log/apache2/access.log"}}, "tags": ["production-server"], "event": {"original": "::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209", "created": "2023-12-26T09:22:14.000Z"}}
+             * {"log":{"file":{"path":"/var/log/apache2/error.log"}},"tags":["production-server"],"event":{"original":"::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209","created":"2023-12-26T09:22:14.000Z"}}
+             * {"log":{"file":{"path":"/var/log/syslog"}},"tags":["production-server"],"event":{"original":"::1 - - [26/Jun/2020:16:16:29 +0200] \"GET /favicon.ico HTTP/1.1\" 404 209","created":"2023-12-26T09:22:14.000Z"}}
+             * {"module": "logcollector", "collector": "windows-eventlog"}
+             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","provider":"$CHANEL_NAME"}}
+             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","provider":"Microsoft-Windows-Security-Auditing"}}
+             * {"tags":["production-server"],"event":{"original":"XML EVENT AS STRING","created":"2023-12-26T09:22:14.000Z","provider":"Windows PowerShell"}}
+             * {"module": "logcollector", "collector": "journald"}
+             * {"tags":["production-server"],"event":{"original":"JSON EVENT OF JOURNALD AS STRING","created":"2023-12-26T09:22:14.000Z","provider":"$UNIT_NAME"}
+             * {"tags":["production-server"],"event":{"original":"JSON EVENT OF JOURNALD AS STRING","created":"2023-12-26T09:22:14.000Z","provider":"ssh.service"}}
              * ```
              *
              * @apiHeader {String} Content-Type=application/x-ndjson The content type of the request.
@@ -677,10 +677,6 @@ int main(int argc, char* argv[])
              * @apiBody (Agent Information) {Array} agent.host.ip Array of IP addresses of the agent. (e.g ["192.168.1.2"])
              * @apiBody (Agent Information) {String} agent.host.architecture Architecture of the agent, e.g., "x86_64".
              *
-             * @apiBody (Module Information) {Object} module Module information.
-             * @apiBody (Module Information) {String} module.module Name of the module, e.g., "logcollector" or "inventory".
-             * @apiBody (Module Information) {String} module.type Type of module, e.g., "file" or "package".
-             *
              * @apiBody (Log Information) {Object} log Log information.
              * @apiBody (Log Information) {Object} log.file File information.
              * @apiBody (Log Information) {String} log.file.path Path to the file, "/path/to/source". Exist only if is recolected from a file.
@@ -688,8 +684,8 @@ int main(int argc, char* argv[])
              * @apiBody (Log Information) {Object} event Details of the event itself.
              * @apiBody (Log Information) {String} event.original The original message collected from the agent.
              * @apiBody (Log Information) {String} event.created Timestamp when an event is recollected in '%Y-%m-%dT%H:%M:%SZ' format.
-             * @apiBody (Log Information) {String} event.module Name of the module this data is coming from. (e.g. apache, windows-eventchannel,
-             * journald, etc)
+             * @apiBody (Log Information) {String} event.module Name of the module this data is coming from. (e.g logcollector)
+             * @apiBody (Log Information) {String} event.collector Name of the collector this data is coming from. (e.g file, windows-eventlog, journald, macos-uls)
              * @apiBody (Log Information) {String} event.provider Source of the event. (e.g channel, file, journald unit, etc)
              *
              * @apiSuccessExample Success-Response:
--- tools/engine-suite/src/engine_test/cmds/add.py
+++ tools/engine-suite/src/engine_test/cmds/add.py
@@ -14,47 +14,64 @@ def check_positive(value):
     except ValueError:
         raise argparse.ArgumentTypeError(f"{value} is not a valid integer")
     if ivalue <= 0:
-        raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value")
+        raise argparse.ArgumentTypeError(
+            f"{value} is an invalid positive int value")
     return ivalue
 
 
 def check_args(args):
-    # Windows ignores module are automatically set to windows-eventchannel
-    if args['format'] == Formats.WINDOWS_EVENTCHANNEL.value:
-        if args['module'] != None:
-            print(f"Argument -m/--module is ignored for format {args['format']}, setting to windows-eventchannel")
-            args['module'] = 'windows-eventchannel'
-
-        # TODO: Move to constant in shared module
-        args['module'] = 'windows-eventchannel'
-
     # If multi-line format, lines are required
     if args['format'] == Formats.MULTI_LINE.value:
         if args['lines'] == None:
-            raise argparse.ArgumentTypeError(f"Argument -l/--lines is required for multi-line format")
+            raise argparse.ArgumentTypeError(
+                f"Argument -l/--lines is required for multi-line format")
 
-    # Module and provider are required for all formats
+    # Module and collector are required for all formats
     if args['module'] == None:
         raise argparse.ArgumentTypeError(f"Argument -m/--module is required")
-    if args['provider'] == None:
-        raise argparse.ArgumentTypeError(f"Argument -p/--provider is required")
+
+    if args['collector'] == None:
+        raise argparse.ArgumentTypeError(f"Argument -c/--collector is required")
+
+    # If collector is file, provider is not allowed and log_file_path is required
+    if args['collector'] == "file":
+        if args['provider'] != None:
+            raise argparse.ArgumentTypeError(
+                f"Argument -p/--provider is not allowed for file collector")
+        if args['log_file_path'] == None:
+            raise argparse.ArgumentTypeError(
+                f"Argument --log-file-path is required for file collector")
+
+    # If provider is set, log_file_path is not allowed
+    if args['provider'] != None:
+        if args['log_file_path'] != None:
+            raise argparse.ArgumentTypeError(
+                f"Argument --log-file-path is not allowed if provider is set")
+
+    # If collector is not file, provider is required
+    if args['collector'] != "file" and args['provider'] is None:
+        raise argparse.ArgumentTypeError(
+            f"Argument -p/--provider is required if collector is not file")
+
+
 
 
 def run(args):
-
     try:
         # Check the args
         args['post_parse'](args)
 
-
         # Create integration configuration
-        iconf = IntegrationConf(args['integration_name'], args['format'], args['module'],
+        iconf = IntegrationConf(args['integration_name'], args['format'], args['module'], args['collector'],
                                 args['provider'], args['event_created'], args['lines'])
 
         # If provider is not `file` then set the log.file.path
-        if args['provider'] == "file":
-            iconf.get_template().add_field(SubTempleType.EVENT, "log.file.path", args['log_file_path'])
-        else:
+        if args['collector'] == "file" and args['provider'] is None:
+            iconf.get_template().add_field(SubTempleType.EVENT,
+                                           "log.file.path", args['log_file_path'])
+            iconf.get_template().remove_field(SubTempleType.EVENT, "event.provider")
+
+        if args['provider'] is not None:
             iconf.get_template().remove_field(SubTempleType.EVENT, "log.file.path")
 
         # Get the configuration database
@@ -75,13 +92,15 @@ def configure(subparsers):
     parser.add_argument('-f', '--format', help=f'Format in which events should be handled by engine-test.',
                         choices=Formats.get_formats(), dest='format', required=True)
     parser.add_argument(
-        '-m', '--module', help='Name of the module this data is coming from (i.g. apache-error, apache-access, eventchannel, journald, macos-uls)', dest='module')
+        '-m', '--module', help='Name of the module this data is coming from (i.g. logcollector)', dest='module')
     parser.add_argument(
-        '-p', '--provider', help='Name of the provider, source of data (i.g. file, channel name of eventchannel, unit name of journald, program-name of macos-uls, etc)', dest='provider')
+        '-c', '--collector', help='Name of the collector, source of data (i.g. file, windows-eventlog, journald, macos-uls)', dest='collector')
+    parser.add_argument(
+        '-p', '--provider', help='Name of the provider, source of data (i.g. channel name of eventchannel, unit name of journald, program-name of macos-uls)', dest='provider')
     parser.add_argument(
         '-l', '--lines', help='Fixed number of lines for each event. Only for multi-line format.', dest='lines', type=check_positive)
-    parser.add_argument('--log-file-path', help='Path to the log file. Only for file provider.',
-                        dest='log_file_path', type=str, default="/var/log/test.log")
+    parser.add_argument('--log-file-path', help='Path to the log file. Only for file collector.',
+                        dest='log_file_path', type=str)
     parser.add_argument('--force-event-created', help='Force the event.created date to a specific date. Format: YYYY-MM-DDTHH:MM:SSZ',
                         dest='event_created', type=str, default="auto")
 
