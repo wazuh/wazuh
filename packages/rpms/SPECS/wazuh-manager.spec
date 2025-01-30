0c588b85711aa6baa94f246ff662c2998d546333
Dwordcito <octa.tala@gmail.com>
2025-01-29 23:43:09 -0300

    Changes after rebase with 4.10.2

--- packages/rpms/SPECS/wazuh-manager.spec
+++ packages/rpms/SPECS/wazuh-manager.spec
@@ -736,7 +736,17 @@ rm -fr %{buildroot}
 %attr(660, wazuh, wazuh) %ghost %{_localstatedir}/logs/ossec.json
 %dir %attr(440, root, wazuh) %{_localstatedir}/templates
 %attr(0440, root, wazuh) %{_localstatedir}/templates/vd_states_template.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/files_states_template.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/registries_states_template.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/packages_states_template.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/processes_states_template.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/system_states_template.json
 %attr(0440, root, wazuh) %{_localstatedir}/templates/vd_states_update_mappings.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/files_states_update_mappings.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/registries_states_update_mappings.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/packages_states_update_mappings.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/processes_states_update_mappings.json
+%attr(0440, root, wazuh) %{_localstatedir}/templates/system__states_update_mappings.json
 %dir %attr(750, wazuh, wazuh) %{_localstatedir}/logs/api
 %dir %attr(750, wazuh, wazuh) %{_localstatedir}/logs/archives
 %dir %attr(750, wazuh, wazuh) %{_localstatedir}/logs/alerts
--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -1129,11 +1129,11 @@ InstallLocal()
     ${INSTALL} -d -m 0440 -o root -g ${WAZUH_GROUP} ${INSTALLDIR}/templates
     ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/vulnerability_scanner/indexer/template/index-template.json ${INSTALLDIR}/templates/vd_states_template.json
     ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/vulnerability_scanner/indexer/template/update-mappings.json ${INSTALLDIR}/templates/vd_states_update_mappings.json
-    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/files-index-template.json ${INSTALLDIR}/templates/files_states_template.json
-    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/registries-index-template.json ${INSTALLDIR}/templates/registries_states_template.json
-    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/packages-index-template.json ${INSTALLDIR}/templates/packages_states_template.json
-    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/processes-index-template.json ${INSTALLDIR}/templates/processes_states_template.json
-    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/system-index-template.json ${INSTALLDIR}/templates/system_states_template.json
+    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/files-update-mappings.json ${INSTALLDIR}/templates/files_states_update_mappings.json
+    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/registries-update-mappings.json ${INSTALLDIR}/templates/registries_states_update_mappings.json
+    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/packages-update-mappings.json ${INSTALLDIR}/templates/packages_states_update_mappings.json
+    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/processes-update-mappings.json ${INSTALLDIR}/templates/processes_states_update_mappings.json
+    ${INSTALL} -m 0440 -o root -g ${WAZUH_GROUP} wazuh_modules/inventory_harvester/indexer/template/system-update-mappings.json ${INSTALLDIR}/templates/system_states_update_mappings.json
 
     # Install Task Manager files
     ${INSTALL} -d -m 0770 -o ${WAZUH_USER} -g ${WAZUH_GROUP} ${INSTALLDIR}/queue/tasks
