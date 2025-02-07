5c59bf26ae959bbb040083beda88f85fb86f34bb
Dwordcito <octa.tala@gmail.com>
2025-02-07 01:40:59 -0300

    Changes based on PR review.

--- src/shared_modules/indexer_connector/src/indexerConnector.cpp
+++ src/shared_modules/indexer_connector/src/indexerConnector.cpp
@@ -339,8 +339,8 @@ void IndexerConnector::sendBulkReactive(const std::vector<std::pair<std::string,
             }
             else if (statusCode == HTTP_VERSION_CONFLICT)
             {
-                logDebug2(IC_NAME, "Document version conflict, sync omitted");
-                throw std::runtime_error("Version conflict, retrying in 1 second.");
+                logDebug2(IC_NAME, "Document version conflict, sync omitted.");
+                throw std::runtime_error("Document version conflict, sync omitted.");
             }
             else
             {
@@ -698,7 +698,7 @@ IndexerConnector::IndexerConnector(
                     else if (statusCode == HTTP_VERSION_CONFLICT)
                     {
                         logDebug2(IC_NAME, "Document version conflict, retrying in 1 second.");
-                        throw std::runtime_error("Version conflict, retrying in 1 second.");
+                        throw std::runtime_error("Document version conflict, retrying in 1 second.");
                     }
                     else
                     {
