a14410f42162b2faa23dbeb162761c6e8e98365e
Javier Bejar <javier.bejar@wazuh.com>
2025-02-10 11:57:23 +0100

    Added indexer connector utest case and updated documentation

--- source/indexerconnector/interface/indexerConnector/iindexerconnector.hpp
+++ source/indexerconnector/interface/indexerConnector/iindexerconnector.hpp
@@ -13,7 +13,12 @@ public:
      * @brief Publishes a message (in JSON string format) to a persistent queue.
      * This method returns immediately without waiting for the message to be processed.
      *
-     * @param message The message to be published (must be in JSON string format).
+     * @param message The message to be published (must be in JSON string format). The message is a JSON string that
+     * includes the following fields:
+     * - operation: The operation to be performed. Currently, the only supported operations are ADD and DELETE.
+     * - index: The name of the index to which the data will be sent. Only applicable for the ADD operation.
+     * - data: The data to be sent to the index. Only applicable for the ADD operation.
+     * - id: The unique identifier of the element.
      */
     virtual void publish(const std::string& message) = 0;
 };
