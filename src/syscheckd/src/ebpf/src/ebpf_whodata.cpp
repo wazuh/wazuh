f42c84d53cbe6c4e65945cd186960f3d9d753147
Jose Luis Carreras Marin <jose.carreras@wazuh.com>
2025-02-12 15:59:56 +0100

    feat: Added functionality to support ebpf driver in the configuration and FIM whodata flow

--- src/syscheckd/src/ebpf/src/ebpf_whodata.cpp
+++ src/syscheckd/src/ebpf/src/ebpf_whodata.cpp
@@ -1,272 +1,215 @@
-// SPDX-License-Identifier: Apache-2.0
-/*
-Copyright (C) 2023 The Falco Authors.
+/* Copyright (C) 2015, Wazuh Inc.
+ * Copyright (C) 2009 Trend Micro Inc.
+ * All right reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied. See the License for the specific language governing
-permissions and limitations under the License.
-*/
-
-#include <cstdlib>
 #include <cstdio>
-#include <iostream>
-#include <csignal>
-#include <libsinsp/sinsp.h>
-#include <libscap/scap_engines.h>
-#include <unordered_set>
-#include <functional>
-#include <memory>
-#include <thread>
-#include <chrono>
-#include "ebpf_whodata.h"
+#include <cstdlib>
+#include <unistd.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "syscheck.h"
 
-using namespace std;
+#define TASK_COMM_LEN 32
+#define EBPF_HC_FILE "tmp/ebpf_hc"
 
-static bool interrupted = false;
-static string monitored_directory = "";
-static std::unique_ptr<filter_check_list> filter_list;
-static std::shared_ptr<sinsp_filter_factory> filter_factory;
+static volatile bool epbf_hc_created = false;
+static volatile bool event_received = false;
 
-#define EVENT_HEADER                                                \
-	"%evt.num %evt.time cat=%evt.category container=%container.id " \
-	"proc=%proc.name(%proc.pid.%thread.tid) "
-#define EVENT_TRAILER "%evt.dir %evt.type %evt.args"
+// Structure must match the one in the BPF program
+struct file_event {
+    __u32 pid;                   // Process ID
+    __u32 uid;                   // User ID
+    __u32 gid;                   // Group ID
+    char comm[TASK_COMM_LEN];    // Process command/name
+    char filename[PATH_MAX];     // Filename passed to the syscall
+    char cwd[PATH_MAX];          // Process working directory
+    char event_type[16];         // Event type ("create", "delete", "mkdir")
+    __u64 inode;                 // Inode number
+    __u64 dev;                   // Device number
+};
 
-#define EVENT_DEFAULTS EVENT_HEADER EVENT_TRAILER
-#define PROCESS_DEFAULTS \
-	EVENT_HEADER "ppid=%proc.ppid exe=%proc.exe args=[%proc.cmdline] " EVENT_TRAILER
+// Callback function to process events from the ring buffer.
+// This function is called each time a new event is received.
+int handle_event(void *ctx, void *data, size_t data_sz) {
+    file_event *e = static_cast<file_event*>(data);
+    char full_path[PATH_MAX];
 
-#define PLUGIN_DEFAULTS "%evt.num %evt.time [%evt.pluginname] %evt.plugininfo"
+    if (e->filename[0] == '/') {
+        std::snprintf(full_path, PATH_MAX, "%s", e->filename);
+    } elseif (e->cwd[0] == '/') {
+        std::snprintf(full_path, PATH_MAX, "%s/%s", e->cwd, e->filename);
+    } else {
+        return 1;
+    }
 
-#define JSON_PROCESS_DEFAULTS                                                                   \
-	"*%evt.num %evt.time %evt.category %container.id %proc.ppid %proc.pid %evt.type %proc.exe " \
-	"%proc.cmdline %evt.args"
+    // Check if path is configured with whodata eBPF driver
+    directory_t *config = fim_configuration_directory(full_path);
+    if ((config->options & WHODATA_ACTIVE) && (config->options & EBPF_DRIVER)) {
+        whodata_evt *w_evt;
 
-std::string default_output = EVENT_DEFAULTS;
-std::string process_output = PROCESS_DEFAULTS;
-std::string net_output = PROCESS_DEFAULTS " %fd.name";
-std::string plugin_output = PLUGIN_DEFAULTS;
+        w_evt->path = full_path;
+        w_evt->process_name = e->comm;
+        w_evt->user_id = e->uid;
+        w_evt->user_name = get_user(e->uid);
+        w_evt->group_id = e->gid;
+        w_evt->group_name = get_group(e->gid);process_id
+        w_evt->inode = e->inode;
+        w_evt->dev = e->dev;
+        w_evt->process_id = e->pid;
 
-static std::unique_ptr<sinsp_evt_formatter> default_formatter = nullptr;
-static std::unique_ptr<sinsp_evt_formatter> process_formatter = nullptr;
-static std::unique_ptr<sinsp_evt_formatter> net_formatter = nullptr;
-static std::unique_ptr<sinsp_evt_formatter> plugin_evt_formatter = nullptr;
+        fim_whodata_event(w_evt);
+    }
 
-// ** Signals handler to stop monitoring gracefully **
-static void sigint_handler(int) {
-    interrupted = true;
+    return 0;
 }
 
-std::string get_field_as_string(sinsp_evt* evt,
-                                sinsp& inspector,
-                                std::string_view field_name) {
-	if(evt == nullptr) {
-		throw sinsp_exception("The event class is NULL");
-	}
+int healthcheck_event(void *ctx, void *data, size_t data_sz) {
+    file_event *e = static_cast<file_event*>(data);
 
-	std::unique_ptr<sinsp_filter_check> chk(
-	        filter_list->new_filter_check_from_fldname(field_name, &inspector, false));
-	if(chk == nullptr) {
-		throw sinsp_exception("The field " + std::string(field_name) + " is not a valid field.");
-	}
-	// we created a filter check starting from the field name so if we arrive here we will find it
-	// for sure
-	chk->parse_field_name(field_name, true, false);
-
-	const char* result = chk->tostring(evt);
-	if(result == nullptr) {
-		return "";
-	}
-
-	return result;
+    if (strstr(e->filename, EBPF_HC_FILE)) {
+        event_received = true;
+    }
+    return 0;
 }
 
-// ** Prints event details for debugging **
-void formatted_dump(sinsp&, sinsp_evt* ev) {
-	std::string output;
-	if(ev->get_category() == EC_PROCESS) {
-		process_formatter->tostring(ev, output);
-	} else if(ev->get_category() == EC_NET || ev->get_category() == EC_IO_READ ||
-	          ev->get_category() == EC_IO_WRITE) {
-		net_formatter->tostring(ev, output);
-	} else if(ev->get_info()->category & EC_PLUGIN) {
-		plugin_evt_formatter->tostring(ev, output);
-	} else {
-		default_formatter->tostring(ev, output);
-	}
+int initialize_bpf_object(ring_buffer *rb, ring_buffer_sample_fn sample_cb) {
+    bpf_object *obj = nullptr;
+    int err;
+    int rb_fd;
 
-	cout << output << std::endl;
-}
+    // Open the BPF object file (assumes it is named "detect_mod.bpf.o")
+    obj = bpf_object__open_file("/var/ossec/bin/modern.bpf.o", nullptr);
+    if (!obj) {
+        loggingFunction(LOG_ERROR, "Opening BPF object file failed.");
+        return 1;
+    }
 
-static std::string determine_fim_type(sinsp_evt* evt, sinsp& inspector)
-{
-	// Retrieve some fields relevant to creation/writing
-	std::string evt_type_str    = get_field_as_string(evt, inspector, "evt.type");
+    // Load the BPF program into the kernel
+    err = bpf_object__load(obj);
+    if (err) {
+        loggingFunction(LOG_ERROR, "Loading BPF object file failed.");
+        bpf_object__close(obj);
+        return 1;
+    }
 
-	// A simple lookup approach for "add", "modify", "delete"
-	if(evt_type_str == "mkdir" ||
-	   evt_type_str == "mkdirat" ||
-	   evt_type_str == "creat" ||
-	   evt_type_str == "openat")
-	{
-		return "add";
-	}
-	else if(evt_type_str == "unlink" ||
-			evt_type_str == "unlinkat" ||
-			evt_type_str == "rmdir")
-	{
-		return "delete";
-	}
-	else if(evt_type_str == "write" ||
-			evt_type_str == "fchmodat")
-	{
-		return "modify";
-	}
+    // Attach all BPF programs contained in the object
+    bpf_program *prog;
+    bpf_object__for_each_program(prog, obj) {
+        if (!bpf_program__attach(prog)) {
+            loggingFunction(LOG_ERROR, "Attaching BPF program failed.");
+            bpf_object__close(obj);
+            return 1;
+        }
+    }
 
-	return "unknown";
-}
+    // Retrieve the file descriptor for the ring buffer map
+    rb_fd = bpf_object__find_map_fd_by_name(obj, "rb");
+    if (rb_fd < 0) {
+        loggingFunction(LOG_ERROR, "Finding ring buffer map failed.");
+        bpf_object__close(obj);
+        return 1;
+    }
 
-void print_event_details(sinsp_evt* evt, sinsp& inspector)
-{
-	if(!evt)
-	{
-		std::cerr << "Null event pointer!" << std::endl;
-		return;
-	}
+    // Create the ring buffer to receive events from the BPF program
+    rb = ring_buffer__new(rb_fd, sample_cb, nullptr, nullptr);
+    if (!rb) {
+        loggingFunction(LOG_ERROR, "Creating ring buffer failed.");
+        bpf_object__close(obj);
+        return 1;
+    }
 
-	// Basic event info
-	std::string evt_time       = get_field_as_string(evt, inspector, "evt.time");
-	std::string evt_type       = get_field_as_string(evt, inspector, "evt.type");
-	std::string evt_category   = get_field_as_string(evt, inspector, "evt.category");
-	std::string proc_name      = get_field_as_string(evt, inspector, "proc.name");
-	std::string inode          = get_field_as_string(evt, inspector, "fd.ino");
-
-	// File path fields (Falco unifying fields)
-	std::string fs_name        = get_field_as_string(evt, inspector, "fs.path.name");
-
-	// User/group info
-	std::string user_uid       = get_field_as_string(evt, inspector, "user.uid");
-	std::string user_name      = get_field_as_string(evt, inspector, "user.name");
-	std::string group_gid      = get_field_as_string(evt, inspector, "group.gid");
-	std::string group_name     = get_field_as_string(evt, inspector, "group.name");
-
-	// All syscall args (if you want to see them)
-	std::string evt_args       = get_field_as_string(evt, inspector, "evt.args");
-
-	// Our custom "fim_type_event"
-	std::string fim_type_event       = determine_fim_type(evt, inspector);
-
-	// Format them into one line (or many lines, JSON, etc. as you prefer)
-	std::cout
-		<< "[" << evt_time << "] "
-		<< "fim_type_event=" << fim_type_event << " "
-		<< "user=" << user_name << "(" << user_uid << ") "
-		<< "group=" << group_name << "(" << group_gid << ") "
-		<< "type=" << evt_type << " "
-		<< "cat=" << evt_category << " "
-		<< "proc=" << proc_name << " "
-		<< "fs.name=" << fs_name << " "
-		<< "inode=" << inode << " "
-		<< "args=" << evt_args
-		<< std::endl;
-}
-
-libsinsp::events::set<ppm_sc_code> extract_filter_sc_codes(sinsp& inspector) {
-	auto ast = inspector.get_filter_ast();
-	if(ast != nullptr) {
-		return libsinsp::filter::ast::ppm_sc_codes(ast.get());
-	}
-
-	return {};
+    return 0;
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-int ebpf_whodata(char * monitored_path) {
-    monitored_directory = monitored_path;
 
-    cout << "Starting file monitoring for directory: " << monitored_directory << endl;
+int ebpf_whodata_healthcheck() {
+    int err;
+    ring_buffer *rb = nullptr;
 
-    sinsp inspector;
-
-    // ** Try to open BPF engine **
-    try {
-        cout << "-- Trying to open BPF engine..." << endl;
-        auto events_sc_codes = extract_filter_sc_codes(inspector);
-        inspector.open_modern_bpf(16 * 4096, DEFAULT_CPU_FOR_EACH_BUFFER, false, events_sc_codes);
-        cout << "BPF engine opened successfully!" << endl;
-    } catch (const sinsp_exception& e) {
-        cerr << "Error opening BPF engine: " << e.what() << endl;
+    if (initialize_bpf_object(rb, healthcheck_event)) {
         return 1;
     }
 
-    // ** Set filter to capture only file-related events ** evt.type in (open, openat, openat2, creat, unlink, rename, write) and
-    filter_list.reset(new sinsp_filter_check_list());
-    filter_factory.reset(new sinsp_filter_factory(&inspector, *filter_list.get()));
-
-    string filter_string = "(evt.abspath contains " + monitored_directory + ")";
-
-    try {
-        sinsp_filter_compiler compiler(filter_factory, filter_string);
-        std::unique_ptr<sinsp_filter> s = compiler.compile();
-        inspector.set_filter(std::move(s), filter_string);
-    } catch (const sinsp_exception& e) {
-        cerr << "Unable to set filter: " << e.what() << endl;
-    }
-
-	inspector.start_capture();
-    cout << "Monitoring started... (Press Ctrl+C to stop)" << endl;
-
-	default_formatter =
-	        std::make_unique<sinsp_evt_formatter>(&inspector, default_output, *filter_list.get());
-	process_formatter =
-	        std::make_unique<sinsp_evt_formatter>(&inspector, process_output, *filter_list.get());
-	net_formatter =
-	        std::make_unique<sinsp_evt_formatter>(&inspector, net_output, *filter_list.get());
-	plugin_evt_formatter =
-	        std::make_unique<sinsp_evt_formatter>(&inspector, plugin_output, *filter_list.get());
-
-
-    // ** Capture events loop with more debug logs **
-    while (!interrupted) {
-        sinsp_evt* evt = nullptr;
-        int res = inspector.next(&evt);
-
-        if (res == SCAP_SUCCESS) {
-            if (evt == nullptr) {
-                cerr << "Unexpected: Event pointer is null despite SCAP_SUCCESS." << endl;
-                continue;
+    time_t start_time = time(NULL);
+    while (!event_received) {
+        if (!epbf_hc_created) {
+            char ebpf_hc_abs_path[PATH_MAX] = {'\0'};
+            if (abspath(EBPF_HC_FILE, ebpf_hc_abs_path, sizeof(ebpf_hc_abs_path)) == NULL) {
+                merror(FIM_ERROR_GET_ABSOLUTE_PATH, EBPF_HC_FILE, strerror(errno), errno);
+                break;
             }
 
-            formatted_dump(inspector, evt);
-            print_event_details(evt, inspector);
+            std::ofstream file(ebpf_hc_abs_path);
+            if (!file.is_open()) {
+                std::cerr << "ERROR: No se pudo crear el archivo: " << ebpf_hc_abs_path << std::endl;
+                break;
+            }
+            file << "Testing eBPF healthcheck\n";
+            file.close();
+
+            epbf_hc_created = true;
         }
-        else if (res == SCAP_EOF) {
-            cout << "End of capture." << endl;
+
+        err = ring_buffer__poll(rb, 100);
+        if (err < 0) {
+            loggingFunction(LOG_ERROR, "Polling ring buffer failed.");
             break;
         }
-        else if (res == SCAP_TIMEOUT) {
-            this_thread::sleep_for(chrono::milliseconds(1000));  // Sleep to prevent CPU usage spike
-        }
-        else if (res == SCAP_FILTERED_EVENT) {
-        }
-        else {
-            cerr << "Error capturing event: " << inspector.getlasterr() << endl;
+        if (time(NULL) - start_time >= 10) {
+            loggingFunction(LOG_ERROR, "Timeout healthcheck.");
+            break;
         }
     }
 
-    cout << "Monitoring stopped." << endl;
+    if (std::remove(ebpf_hc_abs_path) != 0) {
+        loggingFunction(LOG_ERROR, "Healthcheck file can't be removed.");
+    }
+    ring_buffer__free(rb);
+    bpf_object__close(obj);
+
+    if (!event_received) {
+        return 1;
+    }
+
+    loggingFunction(LOG_INFO, "Healthcheck for eBPF FIM whodata module success.");
     return 0;
 }
+
+// Function that initializes and executes the eBPF logic.
+// This function will be called from another file and runs indefinitely,
+// so termination should be handled externally.
+int ebpf_whodata() {
+    int err;
+    ring_buffer *rb = nullptr;
+
+    if (initialize_bpf_object(rb, handle_event)) {
+        return 1;
+    }
+
+    // Poll the ring buffer in an infinite loop.
+    // This loop runs indefinitely; termination should be handled externally.
+    while (true) {
+        err = ring_buffer__poll(rb, 100 /* timeout in ms */);
+        if (err < 0) {
+            loggingFunction(LOG_INFO, "Polling ring buffer failed.");
+            break;
+        }
+    }
+
+    ring_buffer__free(rb);
+    bpf_object__close(obj);
+    return 0;
+}
+
 #ifdef __cplusplus
 }
-#endif
+#endif // _cplusplus
