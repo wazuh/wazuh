260fe492f5f7b7e84e1373a3cca315fb4fc7b8e3
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2025-02-21 13:03:45 -0300

    Implement clearElements tests

--- src/wazuh_modules/inventory_harvester/tests/unit/systemInventoryDeleteAllEntries_test.cpp
+++ src/wazuh_modules/inventory_harvester/tests/unit/systemInventoryDeleteAllEntries_test.cpp
@@ -9,14 +9,14 @@
  * Foundation.
  */
 
-#include "mockFimContext.hpp"
+#include "common/clearElements.hpp"
+#include "mockIndexerConnector.hpp"
 #include "mockSystemContext.hpp"
-#include "systemInventory/deleteElement.hpp"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-class SystemInventoryDeleteAllEntries : public ::testing::Test
+class SystemInventoryDeleteAllEntries : public ::testing::TestWithParam<MockSystemContext::AffectedComponentType>
 {
 protected:
     // LCOV_EXCL_START
@@ -25,5 +25,92 @@ protected:
     // LCOV_EXCL_STOP
 };
 
-TEST_F(SystemInventoryDeleteAllEntries, test)
-{}
+class SystemInventoryDeleteAllEntriesNoAgent : public ::testing::TestWithParam<MockSystemContext::AffectedComponentType>
+{
+protected:
+    // LCOV_EXCL_START
+    SystemInventoryDeleteAllEntriesNoAgent() = default;
+    ~SystemInventoryDeleteAllEntriesNoAgent() override = default;
+    // LCOV_EXCL_STOP
+};
+
+TEST_P(SystemInventoryDeleteAllEntries, ClearElementsDifferentComponents)
+{
+    std::map<MockSystemContext::AffectedComponentType, std::unique_ptr<MockIndexerConnector>, std::less<>>
+        indexerConnectorInstancesSystem;
+
+    auto component = GetParam();
+
+    // indexerConnectorInstances is not initialized with an invalid component.
+    if (component != MockSystemContext::AffectedComponentType::Invalid)
+    {
+        indexerConnectorInstancesSystem.emplace(component, std::make_unique<MockIndexerConnector>());
+    }
+
+    auto context = std::make_shared<MockSystemContext>();
+    auto deleteElement =
+        std::make_shared<ClearElements<MockSystemContext, MockIndexerConnector>>(indexerConnectorInstancesSystem);
+
+    EXPECT_CALL(*context, agentId()).WillOnce(testing::Return("001"));
+    EXPECT_CALL(*context, affectedComponentType()).WillOnce(testing::Return(component));
+
+    if (!indexerConnectorInstancesSystem.empty())
+    {
+        EXPECT_CALL(*indexerConnectorInstancesSystem[component],
+                    publish("{\"id\":\"001\",\"operation\":\"DELETED_BY_QUERY\"}"))
+            .Times(1);
+        EXPECT_NO_THROW(deleteElement->handleRequest(context));
+    }
+    else
+    {
+        // Exception will be captured in inventoryHarvesterFacade
+        EXPECT_ANY_THROW(deleteElement->handleRequest(context));
+    }
+}
+
+TEST_P(SystemInventoryDeleteAllEntriesNoAgent, ClearElementsDifferentComponentsNoAgent)
+{
+    std::map<MockSystemContext::AffectedComponentType, std::unique_ptr<MockIndexerConnector>, std::less<>>
+        indexerConnectorInstancesSystem;
+
+    auto component = GetParam();
+
+    // indexerConnectorInstances is not initialized with an invalid component.
+    if (component != MockSystemContext::AffectedComponentType::Invalid)
+    {
+        indexerConnectorInstancesSystem.emplace(component, std::make_unique<MockIndexerConnector>());
+    }
+
+    auto context = std::make_shared<MockSystemContext>();
+    auto deleteElement =
+        std::make_shared<ClearElements<MockSystemContext, MockIndexerConnector>>(indexerConnectorInstancesSystem);
+
+    EXPECT_CALL(*context, agentId()).WillOnce(testing::Return(""));
+    EXPECT_CALL(*context, affectedComponentType()).WillOnce(testing::Return(component));
+
+    if (!indexerConnectorInstancesSystem.empty())
+    {
+        EXPECT_CALL(*indexerConnectorInstancesSystem[component], publish("{\"operation\":\"DELETED_BY_QUERY\"}"))
+            .Times(1);
+        EXPECT_NO_THROW(deleteElement->handleRequest(context));
+    }
+    else
+    {
+        // Exception will be captured in inventoryHarvesterFacade
+        EXPECT_ANY_THROW(deleteElement->handleRequest(context));
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(ClearElementsDifferentComponents,
+                         SystemInventoryDeleteAllEntries,
+                         ::testing::Values(MockSystemContext::AffectedComponentType::Package,
+                                           MockSystemContext::AffectedComponentType::Process,
+                                           MockSystemContext::AffectedComponentType::System,
+                                           MockSystemContext::AffectedComponentType::Invalid));
+
+INSTANTIATE_TEST_SUITE_P(ClearElementsDifferentComponentsNoAgent,
+                         SystemInventoryDeleteAllEntriesNoAgent,
+                         ::testing::Values(MockSystemContext::AffectedComponentType::Package,
+                                           MockSystemContext::AffectedComponentType::Process,
+                                           MockSystemContext::AffectedComponentType::System,
+                                           MockSystemContext::AffectedComponentType::Invalid));
