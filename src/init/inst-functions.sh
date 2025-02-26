e3b30b8ff948dd4f456d16248f6eb580ff88086c
Javier Bejar <javier.bejar@wazuh.com>
2025-02-26 13:47:01 +0100

    Added VERSION.json

--- src/init/inst-functions.sh
+++ src/init/inst-functions.sh
@@ -241,7 +241,7 @@ WriteLogs()
 {
   LOCALFILES_TMP=`cat ${LOCALFILES_TEMPLATE}`
   HAS_JOURNALD=`command -v journalctl`
-  
+
   # If has journald, add journald to the configuration file
   if [ "X$HAS_JOURNALD" != "X" ]; then
     if [ "$1" = "echo" ]; then
@@ -290,14 +290,14 @@ WriteLogs()
           continue
         fi
       fi
-  
+
       # If log file present or skip file
       if [ -f "$FILE" ] || [ "X$SKIP_CHECK_FILE" = "Xyes" ]; then
         # Print
         if [ "$1" = "echo" ]; then
             echo "    -- $FILE"
         # Add to the configuration file
-        elif [ "$1" = "add" ]; then         
+        elif [ "$1" = "add" ]; then
           echo "  <localfile>" >> $NEWCONFIG
           if [ "$FILE" = "snort" ]; then
             head -n 1 $FILE|grep "\[**\] "|grep -v "Classification:" > /dev/null
@@ -775,6 +775,7 @@ InstallCommon()
     ./init/adduser.sh ${WAZUH_USER} ${WAZUH_GROUP} ${INSTALLDIR}
 
   ${INSTALL} -d -m 0750 -o root -g ${WAZUH_GROUP} ${INSTALLDIR}/
+  ${INSTALL} -m 440 -o ${WAZUH_USER} -g ${WAZUH_GROUP} ../VERSION.json ${INSTALLDIR}/VERSION.json
   ${INSTALL} -d -m 0770 -o ${WAZUH_USER} -g ${WAZUH_GROUP} ${INSTALLDIR}/logs
   ${INSTALL} -d -m 0750 -o ${WAZUH_USER} -g ${WAZUH_GROUP} ${INSTALLDIR}/logs/wazuh
   ${INSTALL} -m 0660 -o ${WAZUH_USER} -g ${WAZUH_GROUP} /dev/null ${INSTALLDIR}/logs/ossec.log
