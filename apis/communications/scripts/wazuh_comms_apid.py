e1d08de94a45c37a8405edb0a0acad73d1b5d137
GGP1 <gaston.palomeque@wazuh.com>
2025-02-28 15:06:12 -0300

    Lint fixes

--- apis/communications/scripts/wazuh_comms_apid.py
+++ apis/communications/scripts/wazuh_comms_apid.py
@@ -204,17 +204,21 @@ def get_script_arguments() -> Namespace:
 
 
 class StandaloneApplication(BaseApplication):
+    """Communications API standalone application."""
+
     def __init__(self, app: Callable, options: dict[str, Any] = None):
         self.options = options or {}
         self.app = app
         super().__init__()
 
     def load_config(self):
+        """Load the API configuration."""
         config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
     def load(self):
+        """Load the API standalone application."""
         return self.app
 
 
