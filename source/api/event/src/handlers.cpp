3683b64c651fc1214d824665d40792dbd3c0879c
Javier Bejar <javier.bejar@wazuh.com>
2025-03-05 11:20:38 +0100

    Changed engine stateless response to be 400 error if failing to ingest any  ndjson lines

--- source/api/event/src/handlers.cpp
+++ source/api/event/src/handlers.cpp
@@ -1,4 +1,5 @@
 #include <api/event/handlers.hpp>
+#include <base/logging.hpp>
 
 // TODO add metrics
 // TODO add fallback file for events that could not be parsed ??
@@ -8,12 +9,18 @@ namespace api::event::handlers
 adapter::RouteHandler pushEvent(const std::shared_ptr<::router::IRouterAPI>& orchestrator,
                                 ProtolHandler protocolHandler)
 {
-    return [weakOrchestrator = std::weak_ptr(orchestrator), protocolHandler](const auto& req, auto& res)
+    return [lambdaName = logging::getLambdaName(__FUNCTION__, "apiHandler"),
+            weakOrchestrator = std::weak_ptr(orchestrator),
+            protocolHandler](const auto& req, auto& res)
     {
+        LOG_TRACE_L(lambdaName.c_str(), fmt::format("Recieved request {}", req.body));
+
         auto orchestrator = weakOrchestrator.lock();
         if (!orchestrator)
         {
+            LOG_ERROR_L(lambdaName.c_str(), "Recieved request but orchestrator is not available");
             res.status = httplib::StatusCode::InternalServerError_500;
+            res.set_content("{\"error\": \"Internal server error\", \"code\": 500}", "application/json");
             return;
         }
 
@@ -24,13 +31,15 @@ adapter::RouteHandler pushEvent(const std::shared_ptr<::router::IRouterAPI>& orc
         }
         catch (const std::exception& e)
         {
-            // Silenty ignore invalid events as per the spec
-            res.status = httplib::StatusCode::OK_200;
+            LOG_ERROR_L(lambdaName.c_str(), "Failed to parse request: '{}'", e.what());
+            res.status = httplib::StatusCode::BadRequest_400;
+            res.set_content(fmt::format("{{\"error\": \"{}\", \"code\": 400}}", e.what()), "application/json");
             return;
         }
 
         while (!events.empty())
         {
+            LOG_TRACE_L(lambdaName.c_str(), "Posting event to orchestrator: {}", events.front()->str());
             orchestrator->postEvent(std::move(events.front()));
             events.pop();
         }
