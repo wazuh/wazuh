884b42f127a1ddb47ac65f460ad89b9d6da68024
GGP1 <gaston.palomeque@wazuh.com>
2025-03-12 12:23:51 -0300

    Commands manager refactor

---
+++ framework/wazuh/core/commands_manager.py
@@ -0,0 +1,89 @@
+import asyncio
+from multiprocessing.managers import SyncManager
+from multiprocessing.synchronize import Event
+from typing import Dict, List, Optional
+
+from uuid6 import UUID
+from wazuh.core.indexer.models.commands import Command
+
+
+class CommandsManager:
+    """Expose commands received from the local server to the APIs processes."""
+
+    def __init__(self):
+        self._manager: SyncManager = SyncManager()
+        self._manager.start()
+        self._commands: Dict[str, List[Command]] = self._manager.dict()
+        self._subscriptions: Dict[str, Event] = self._manager.dict()
+
+    def add_commands(self, commands: List[Command]) -> List[Command]:
+        """Add a command to the dictionary and call the corresponding subscribers callbacks.
+
+        Parameters
+        ----------
+        commands : List[Command]
+            Commands list.
+
+        Returns
+        -------
+        List[str]
+            List of the processed commands.
+        """
+        processed_commands = []
+        for command in commands:
+            target_id = command.target.id
+
+            if target_id not in self._subscriptions:
+                continue
+
+            if target_id not in self._commands:
+                self._commands[target_id] = [command]
+                processed_commands.append(command)
+                continue
+
+            # Using self._commands[agent_id].append() doesn't work because
+            # it doesn't hold the reference of nested objects
+            command_list = self._commands[target_id]
+            command_list.append(command)
+            self._commands[target_id] = command_list
+            processed_commands.append(command)
+
+        for target_id in self._subscriptions.keys():
+            if target_id in self._subscriptions:
+                self._subscriptions[target_id].set()
+
+        return processed_commands
+
+    async def get_commands(self, target_id: UUID) -> Optional[List[Command]]:
+        """Get commands from the manager.
+
+        It returns immediately if there are commands for the target specified, otherwise it waits for new commands until
+        the timeout is reached.
+
+        Parameters
+        ----------
+        target_id : UUID
+            Target ID.
+        timeout : float
+            Timeout in seconds.
+
+        Returns
+        -------
+        Optional[List[Command]]
+            Commands list or None if the timeout is reached.
+        """
+        if target_id not in self._commands:
+            event = asyncio.Event()
+            self._subscriptions.update({target_id: event})
+
+            signaled = await event.wait()
+
+            self._subscriptions.pop(target_id, None)
+            if not signaled:
+                return
+
+        return self._commands.pop(target_id, None)
+
+    def shutdown(self):
+        """Shutdown sync manager."""
+        self._manager.shutdown()
---
+++ framework/wazuh/core/tests/test_commands_manager.py
@@ -0,0 +1,54 @@
+from multiprocessing import Event
+from unittest.mock import call, patch
+
+from wazuh.core.commands_manager import CommandsManager
+from wazuh.core.indexer.models.commands import Command, Status, Target, TargetType
+
+DOCUMENT_ID = 'UB2jVpEBYSr9jxqDgXAD'
+AGENT_ID = '01915801-4b34-7131-9d88-ff06ff05aefd'
+COMMAND = Command(document_id=DOCUMENT_ID, status=Status.PENDING, target=Target(id=AGENT_ID, type=TargetType.AGENT))
+
+
+@patch('wazuh.core.commands_manager.SyncManager')
+def test_commands_manager_initialization(sync_manager_mock):
+    """Check that the `CommandsManager.__init___` method works as expected."""
+    CommandsManager()
+
+    sync_manager_mock.assert_has_calls(
+        [
+            call(),
+            call().start(),
+            call().dict(),
+            call().dict(),
+        ]
+    )
+
+
+@patch('wazuh.core.commands_manager.SyncManager')
+def test_commands_manager_add_commands(sync_manager_mock):
+    """Check that the `add_commands` method works as expected."""
+    commands_manager = CommandsManager()
+    commands_manager._subscriptions = {AGENT_ID: Event()}
+    commands_manager._commands = {}
+    commands_manager.add_commands([COMMAND])
+
+    assert commands_manager._commands[AGENT_ID] == [COMMAND]
+
+
+@patch('wazuh.core.commands_manager.SyncManager')
+async def test_commands_manager_get_commands(sync_manager_mock):
+    """Check that the `get_commands` method works as expected."""
+    commands_manager = CommandsManager()
+    commands_manager._commands = {AGENT_ID: [COMMAND]}
+    commands = await commands_manager.get_commands(AGENT_ID)
+
+    assert commands == [COMMAND]
+
+
+@patch('wazuh.core.commands_manager.SyncManager')
+def test_commands_manager_shutdown(sync_manager_mock):
+    """Check that the `shutdown` method works as expected."""
+    commands_manager = CommandsManager()
+    commands_manager.shutdown()
+
+    sync_manager_mock.assert_has_calls([call(), call().start(), call().dict(), call().dict(), call().shutdown()])
