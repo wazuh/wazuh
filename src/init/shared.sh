5957596280b3cd7152c684da4c03727775a3fcca
Nico Stefani <nicolas.stefi@wazuh.com>
2025-03-20 11:56:14 -0300

    Remove old REVISION and VERSION files and the references

--- src/init/shared.sh
+++ src/init/shared.sh
@@ -5,8 +5,6 @@
 # Author: Daniel B. Cid <daniel.cid@gmail.com>
 
 ### Setting up variables
-VERSION_FILE="./src/VERSION"
-REVISION_FILE="./src/REVISION"
 VERSION="v$(grep '"version"' VERSION.json | sed -E 's/.*"version": *"([^"]+)".*/\1/')"
 REVISION=`grep '"stage"' VERSION.json | sed -E 's/.*"stage": *"([^"]+)".*/\1/'`
 UNAME=`uname -snr`
