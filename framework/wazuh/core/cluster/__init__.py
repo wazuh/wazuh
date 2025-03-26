390795f9859bb9b72931dce369542a8ff367ead5
Nico Stefani <nicolas.stefi@wazuh.com>
2025-03-26 13:34:08 -0300

    Bump revision to 41122

--- framework/wazuh/core/cluster/__init__.py
+++ framework/wazuh/core/cluster/__init__.py
@@ -5,7 +5,7 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 __version__ = '4.11.2'
-__revision__ = '41121'
+__revision__ = '41122'
 __author__ = "Wazuh Inc"
 __wazuh_name__ = "Wazuh"
 __licence__ = "\
