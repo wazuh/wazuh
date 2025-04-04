807bbb027e86e8f48538c5e8adf4dc64426456ee
pereyra-m <matias.pereyra@wazuh.com>
2025-04-04 23:37:22 +0000

    Create new agent schema version to clear tables

--- src/wazuh_db/schema_agents.sql
+++ src/wazuh_db/schema_agents.sql
@@ -339,7 +339,7 @@ CREATE TABLE IF NOT EXISTS sync_info (
 
 BEGIN;
 
-INSERT INTO metadata (key, value) VALUES ('db_version', '14');
+INSERT INTO metadata (key, value) VALUES ('db_version', '15');
 INSERT INTO scan_info (module) VALUES ('fim');
 INSERT INTO scan_info (module) VALUES ('syscollector');
 INSERT INTO sync_info (component) VALUES ('fim');
