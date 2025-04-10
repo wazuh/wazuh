75832b8325e9cb1ceeb74aae34b30f08115c5c05
Nico Stefani <nicolas.stefi@wazuh.com>
2025-04-10 14:24:37 -0300

    Create log file if not exits

--- .github/actions/test-install-components/install_component.sh
+++ .github/actions/test-install-components/install_component.sh
@@ -7,6 +7,11 @@ echo "Installing Wazuh $target."
 if [ -n "$(command -v yum)" ]; then
     install="yum install -y --nogpgcheck"
     installed_log="/var/log/yum.log"
+
+    if [ ! -f "$installed_log" ]; then
+        installed_log="/var/log/dnf.log"
+    fi
+
 elif [ -n "$(command -v dpkg)" ]; then
     install="dpkg --install"
     installed_log="/var/log/dpkg.log"
@@ -18,6 +23,7 @@ fi
 if [ "${ARCH}" = "i386" ] || [ "${ARCH}" = "armv7hl" ]; then
     linux="linux32"
 fi
+
 
 WAZUH_MANAGER="10.0.0.2" $linux $install "/packages/$package_name"| tee /packages/status.log
 grep -i " installed.*wazuh-$target" $installed_log| tee -a /packages/status.log
