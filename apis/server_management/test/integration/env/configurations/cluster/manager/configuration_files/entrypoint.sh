ca7ab22b7ef727332409385fe8a717dad88d6fe5
GGP1 <gaston.palomeque@wazuh.com>
2025-04-10 09:15:03 -0300

    Remove cluster test data

--- apis/server_management/test/integration/env/configurations/cluster/manager/configuration_files/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i -e "/<\/ossec_config>/i   <reports>\n  <title>Auth_Report</title>\n  <group>authentication_failed,</group>\n  <srcip>192.168.1.10</srcip>\n  <email_to>recipient@example.wazuh.com</email_to>\n  <showlogs>yes</showlogs>\n  </reports>" /var/ossec/etc/ossec.conf
-mkdir -p /var/ossec/stats/totals/2019/Aug/
-cp -rf /tmp_volume/configuration_files/ossec-totals-27.log /var/ossec/stats/totals/2019/Aug/ossec-totals-27.log
-chown -R wazuh:wazuh /var/ossec/stats/totals/2019/Aug/ossec-totals-27.log
