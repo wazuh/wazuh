484b6bd1d3c804e94175fc51dfebd8bee63eeca5
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2025-04-25 09:49:31 +0200

    feat(upgrade): infer Rocky Linux package type

--- src/wazuh_modules/agent_upgrade/manager/wm_agent_upgrade_validate.c
+++ src/wazuh_modules/agent_upgrade/manager/wm_agent_upgrade_validate.c
@@ -50,7 +50,8 @@ static const char* rpm_platforms[] = {
     "opensuse-tumbleweed",
     "rhel",
     "sles",
-    "suse"
+    "suse",
+    "rocky",
 };
 
 int wm_agent_upgrade_validate_id(int agent_id) {
