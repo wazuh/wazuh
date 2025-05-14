9a556077643c53639290d029c36bf56d3edf2413
Nahuel <nahuelfigueroa978@hotmail.com>
2025-05-14 00:28:38 +0000

    Add syn parser for key_value

--- src/engine/source/hlp/src/parsers/parse_field.hpp
+++ src/engine/source/hlp/src/parsers/parse_field.hpp
@@ -59,18 +59,6 @@ std::optional<Field>
 getField(std::string_view input, const char delimiter, const char quote, const char escape, bool strict);
 
 /**
- * @brief Parses a field from a string using the given delimiter, quote, and escape characters.
- *
- * @param input The string to parse the field from.
- * @param delimiter The delimiter character used to separate fields.
- * @param quote The quote character used to enclose fields that contain delimiter characters.
- * @param escape The escape character used to escape quote or escape characters inside a field.
- * @param strict Whether strict parsing should be used. If true, fields not enclosed in quotes cannot contain quotes.
- * @return An optional Field object containing the parsed field, or std::nullopt if the input is invalid.
- */
-std::optional<Field>
-getFieldKeyValue(std::string_view input, const char delimiter, const char quote, const char escape, bool strict);
-/**
  * @brief Unescapes a string
  *
  * @param is_escaped Whether the string is escaped.
