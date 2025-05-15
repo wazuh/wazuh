668ff62e5769d7a8187e9a29ed9350ec77b67d15
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-05-15 01:40:14 +0000

    feat: create user_groups collector

---
+++ src/data_provider/src/extended_sources/groups/include/iuser_groups_wrapper.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <pwd.h>
+#include <grp.h>
+#include <set>
+#include <memory>
+
+class IUserGroupsWrapper
+{
+    public:
+        virtual ~IUserGroupsWrapper() = default;
+
+        virtual long sysconf(int name) const = 0;
+        virtual int getpwuid_r(uid_t uid, struct passwd* pwd, char* buf, size_t buflen, struct passwd** result) const = 0;
+        virtual int getpwent_r(struct passwd* pwd, char* buf, size_t buflen, struct passwd** result) const = 0;
+        virtual void setpwent() const = 0;
+        virtual void endpwent() const = 0;
+        virtual int getgrouplist(const char* user, gid_t group, gid_t* groups, int* ngroups) const = 0;
+};
