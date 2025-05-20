05b10adf022dc87d9b66b705c700597b45affdd0
Luis Enrique Chico Capistrano <luis.chico@wazuh.com>
2025-05-20 17:05:19 -0300

    feat: Use reload instead of restart and fix execd reconection.

--- src/init/wazuh-client.sh
+++ src/init/wazuh-client.sh
@@ -313,8 +313,11 @@ restart)
     restart_service
     ;;
 reload)
-    DAEMONS=$(echo $DAEMONS | sed 's/wazuh-execd//')
+    DAEMONS=$(echo $DAEMONS | sed 's/wazuh-agentd//')
     restart_service
+    # Signal agentd to reload (reconnects execd)
+    pid=`cat ${DIR}/var/run/wazuh-agentd-*.pid`
+    kill -HUP $pid
     ;;
 status)
     lock
