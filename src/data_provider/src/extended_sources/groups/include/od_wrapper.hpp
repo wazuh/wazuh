d3ca4d1d589a842ac481991c85621da2061f1d2d
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-05-20 09:54:53 -0700

    feat: add darwin collector unit tests

--- src/data_provider/src/extended_sources/groups/include/od_wrapper.hpp
+++ src/data_provider/src/extended_sources/groups/include/od_wrapper.hpp
@@ -3,7 +3,9 @@
 #include <map>
 #include <string>
 
+namespace od
+{
 
-void genODEntries(const std::string& recordType,
-                  const std::string* record,
-                  std::map<std::string, bool>& names);
+    void genEntries(const std::string& record_type, const std::string* record, std::map<std::string, bool>& names);
+
+} // namespace od
