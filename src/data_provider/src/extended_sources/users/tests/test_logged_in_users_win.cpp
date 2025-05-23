782daadb14740dc20ea5316be156567f3d67e1dd
LucioDonda <donda.lucio@gmail.com>
2025-05-23 15:12:59 +0000

    feat: license and header documentation addition

--- src/data_provider/src/extended_sources/users/tests/test_logged_in_users_win.cpp
+++ src/data_provider/src/extended_sources/users/tests/test_logged_in_users_win.cpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
 #include "iwinapi_wrappers.hpp"
 #include "logged_in_users_win.hpp"
 #include "gtest/gtest.h"
