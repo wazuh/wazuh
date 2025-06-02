6df7f513d877f05e702736a968c84a5d6f9c9a27
Nicolas Gomez Palacios <nicogp2@gmail.com>
2025-06-02 19:33:27 -0300

    feat(syscollector): added configuration field for groups in syscollector

--- src/wazuh_modules/syscollector/include/syscollector.h
+++ src/wazuh_modules/syscollector/include/syscollector.h
@@ -52,7 +52,8 @@ EXPORTED void syscollector_start(const unsigned int inverval,
                                  const bool ports,
                                  const bool portsAll,
                                  const bool processes,
-                                 const bool hotfixes);
+                                 const bool hotfixes,
+                                 const bool groups);
 
 EXPORTED void syscollector_stop();
 
@@ -79,7 +80,8 @@ typedef void(*syscollector_start_func)(const unsigned int inverval,
                                        const bool ports,
                                        const bool portsAll,
                                        const bool processes,
-                                       const bool hotfixes);
+                                       const bool hotfixes,
+                                       const bool groups);
 
 typedef void(*syscollector_stop_func)();
 
