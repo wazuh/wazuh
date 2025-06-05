1fb4d200a548dcdd2ca77e1a58e49363f2225c45
jnasselle <jnasselle@gmail.com>
2025-06-05 15:19:49 -0300

    Change new ruleset block approach to list insert

--- src/init/update-ruleset-lists.sh
+++ src/init/update-ruleset-lists.sh
@@ -20,17 +20,48 @@ updateRulesetLists()
 
     if [ -n "$LISTS_TO_INSERT" ]; then
 
-        XML_BLOCK="<ossec_config>\n"
-        XML_BLOCK+="  <ruleset>\n"
+        for list in $LISTS_TO_INSERT; do
+        LISTS_BLOCK="$LISTS_BLOCK\n    <list>$list</list>"
+        done
 
-        while IFS= read -r list; do
-            XML_BLOCK+="    <list>$list</list>\n"
-        done <<< "$LISTS_TO_INSERT"
 
-        XML_BLOCK+="  </ruleset>\n"
-        XML_BLOCK+="</ossec_config>"
+        # Insert after the latest <list> at the first uncommented <ruleset> entry
+        awk -v insert_lines="$LISTS_BLOCK" '
+            BEGIN {
+            in_comment=0
+            in_ruleset=0
+            last_list_line=0
+            line_count=0
+            }
+            {
+            line_count++
+            lines[line_count] = $0
 
-        echo "$XML_BLOCK" >> $CONFIG
+            if ($0 ~ /<!--/) in_comment=1
+            if ($0 ~ /-->/) in_comment=0
+
+            if (!in_comment && $0 ~ /<ruleset>/ && in_ruleset==0) {
+                in_ruleset=1
+            }
+
+            if (in_ruleset && !in_comment && $0 ~ /<list>/) {
+                last_list_line = line_count
+            }
+
+            if (!in_comment && $0 ~ /<\/ruleset>/ && in_ruleset==1) {
+                in_ruleset=0
+            }
+            }
+            END {
+            for (i=1; i<=line_count; i++) {
+                print lines[i]
+                if (i == last_list_line) {
+                printf("%s\n", insert_lines)
+                }
+            }
+            }
+        ' "$CONFIG" > "$CONFIG.tmp" && mv "$CONFIG.tmp" "$CONFIG"
+
     fi
 
 }
