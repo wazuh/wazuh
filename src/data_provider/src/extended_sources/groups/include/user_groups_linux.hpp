95dbc96f537a7ca9e84b7712b492ef38df3c5335
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-06-05 21:39:27 +0000

    feat: add method to get groupnames from uids (linux)

--- src/data_provider/src/extended_sources/groups/include/user_groups_linux.hpp
+++ src/data_provider/src/extended_sources/groups/include/user_groups_linux.hpp
@@ -11,27 +11,52 @@
 
 #include <set>
 
-#include "json.hpp"
 #include "igroup_wrapper.hpp"
 #include "ipasswd_wrapper.hpp"
 #include "isystem_wrapper.hpp"
+#include "json.hpp"
 
 #define EXPECTED_GROUPS_MAX 64
 
+// @brief Class for collecting user groups information on Linux systems.
+//
+// This class provides methods to collect user groups information from the
+// Linux operating system. It uses the system's group and passwd databases to retrieve
+// user details such as username, UID, GID, and their associated groups. The collected
+// data is returned in JSON format.
 class UserGroupsProvider
 {
     public:
+        /// @brief Constructs a UserGroupsProvider with specific wrappers.
+        /// @param groupWrapper A shared pointer to an IGroupWrapperLinux instance for group operations.
+        /// @param passwdWrapper A shared pointer to an IPasswdWrapperLinux instance for passwd operations.
+        /// @param sysWrapper A shared pointer to an ISystemWrapper instance for system operations.
         explicit UserGroupsProvider(std::shared_ptr<IGroupWrapperLinux> groupWrapper,
                                     std::shared_ptr<IPasswdWrapperLinux> passwdWrapper,
                                     std::shared_ptr<ISystemWrapper> sysWrapper);
+
+        /// @brief Default constructor that initializes the UserGroupsProvider with default wrappers.
+        /// @note This constructor uses default implementations of IGroupWrapperLinux, IPasswdWrapperLinux, and ISystemWrapper.
         UserGroupsProvider();
+
+        /// @brief Collects user groups information.
+        /// @param uids A set of user IDs (UIDs) to filter the results. If empty, all users are collected.
+        /// @return A JSON array containing user groups information, where each entry includes UID, GID, and group details.
         nlohmann::json collect(const std::set<uid_t>& uids = {});
+
+        /// @brief Retrieves group names associated with the specified UIDs.
+        /// @param uids A set of user IDs (UIDs) for which to retrieve group names.
+        /// @return A JSON object where keys are UIDs and values are arrays of group names associated with those UIDs.
+        /// If a UID has no associated groups, the value will be an empty array.
+        /// @note This method is useful for quickly mapping UIDs to their group names without retrieving full group details.
+        nlohmann::json getGroupNamesByUid(const std::set<uid_t>& uids = {});
 
     private:
         std::shared_ptr<IGroupWrapperLinux> m_groupWrapper;
         std::shared_ptr<IPasswdWrapperLinux> m_passwdWrapper;
         std::shared_ptr<ISystemWrapper> m_sysWrapper;
 
+        /// @brief Structure to hold user information.
         struct UserInfo
         {
             const char* name;
@@ -39,6 +64,18 @@ class UserGroupsProvider
             gid_t gid;
         };
 
-        void getGroupsForUser(nlohmann::json& results, const UserInfo& user);
+        /// @brief Retrieves groups for each user and returns a vector of pairs containing UID and their associated groups.
+        /// @param uids A set of user IDs (UIDs) to filter the results. If empty, all users are processed.
+        /// @return A vector of pairs, where each pair contains a UID and a vector of GIDs representing the groups associated with that UID.
+        /// @note This method is used internally to gather user-group associations before formatting the results into JSON.
+        /// @note If a user has no associated groups, the vector of GIDs will be empty.
+        std::vector<std::pair<uid_t, std::vector<gid_t>>> getUserGroups(const std::set<uid_t>& uids);
+
+        /// @brief Adds groups to the results JSON array for a specific user.
+        /// @param results A reference to the JSON array where the group information will be added.
+        /// @param uid The user ID for which the groups are being added.
+        /// @param groups A pointer to an array of group IDs (GIDs) associated with the user.
+        /// @param ngroups The number of groups in the `groups` array.
+        /// @note This method formats the group information into JSON objects and appends them to the results array.
         void addGroupsToResults(nlohmann::json& results, uid_t uid, const gid_t* groups, int ngroups);
 };
--- src/data_provider/src/extended_sources/groups/src/user_groups_linux.cpp
+++ src/data_provider/src/extended_sources/groups/src/user_groups_linux.cpp
@@ -7,11 +7,11 @@
  * Foundation.
  */
 
-#include <iostream>
 #include "user_groups_linux.hpp"
 #include "group_wrapper.hpp"
 #include "passwd_wrapper.hpp"
 #include "system_wrapper.hpp"
+#include <iostream>
 
 // Reasonable upper bound for getpw_r buffer
 constexpr size_t MAX_GETPW_R_BUF_SIZE = 16 * 1024;
@@ -35,11 +35,55 @@ UserGroupsProvider::UserGroupsProvider()
 nlohmann::json UserGroupsProvider::collect(const std::set<uid_t>& uids)
 {
     nlohmann::json results = nlohmann::json::array();
-    struct passwd pwd;
-    struct passwd* pwdResults
+    auto usersGroups = getUserGroups(uids);
+
+    for (const auto& [uid, groups] : usersGroups)
     {
-        nullptr
-    };
+        addGroupsToResults(results, uid, groups.data(), static_cast<int>(groups.size()));
+    }
+
+    return results;
+}
+
+nlohmann::json UserGroupsProvider::getGroupNamesByUid(const std::set<uid_t>& uids)
+{
+    nlohmann::json result = nlohmann::json::object();
+    auto usersGroups = getUserGroups(uids);
+
+    size_t bufSize = m_sysWrapper->sysconf(_SC_GETGR_R_SIZE_MAX);
+
+    if (bufSize > MAX_GETPW_R_BUF_SIZE)
+    {
+        bufSize = MAX_GETPW_R_BUF_SIZE;
+    }
+
+    for (const auto& [uid, groups] : usersGroups)
+    {
+        nlohmann::json groupNames = nlohmann::json::array();
+
+        for (const auto& gid : groups)
+        {
+            struct group grp;
+            struct group* grpResult = nullptr;
+            auto groupBuf = std::make_unique<char[]>(bufSize);
+
+            if (m_groupWrapper->getgrgid_r(gid, &grp, groupBuf.get(), bufSize, &grpResult) == 0 && grpResult != nullptr)
+            {
+                groupNames.push_back(grpResult->gr_name);
+            }
+        }
+
+        result[std::to_string(uid)] = groupNames;
+    }
+
+    return result;
+}
+
+std::vector<std::pair<uid_t, std::vector<gid_t>>> UserGroupsProvider::getUserGroups(const std::set<uid_t>& uids)
+{
+    std::vector<std::pair<uid_t, std::vector<gid_t>>> userGroups;
+    struct passwd pwd;
+    struct passwd* pwdResults = nullptr;
 
     size_t bufSize = m_sysWrapper->sysconf(_SC_GETPW_R_SIZE_MAX);
 
@@ -50,15 +94,48 @@ nlohmann::json UserGroupsProvider::collect(const std::set<uid_t>& uids)
 
     auto buf = std::make_unique<char[]>(bufSize);
 
+    auto processUser = [&](const struct passwd * pwdInfo)
+    {
+        UserInfo user {pwdInfo->pw_name, pwdInfo->pw_uid, pwdInfo->pw_gid};
+
+        gid_t groupsBuffer[EXPECTED_GROUPS_MAX];
+        gid_t* groups = groupsBuffer;
+        int nGroups = EXPECTED_GROUPS_MAX;
+
+        if (m_groupWrapper->getgrouplist(user.name, user.gid, groups, &nGroups) < 0)
+        {
+            groups = new gid_t[nGroups];
+
+            if (groups == nullptr)
+            {
+                // std::cerr << "Could not allocate memory to get user groups" << std::endl;
+                return;
+            }
+
+            if (m_groupWrapper->getgrouplist(user.name, user.gid, groups, &nGroups) < 0)
+            {
+                // std::cerr << "Could not get user's group list" << std::endl;
+            }
+            else
+            {
+                userGroups.emplace_back(user.uid, std::vector<gid_t>(groups, groups + nGroups));
+            }
+
+            delete[] groups;
+        }
+        else
+        {
+            userGroups.emplace_back(user.uid, std::vector<gid_t>(groups, groups + nGroups));
+        }
+    };
+
     if (!uids.empty())
     {
         for (const auto& uid : uids)
         {
-            if (m_passwdWrapper->getpwuid_r(uid, &pwd, buf.get(), bufSize, &pwdResults) == 0 &&
-                    pwdResults != nullptr)
+            if (m_passwdWrapper->getpwuid_r(uid, &pwd, buf.get(), bufSize, &pwdResults) == 0 && pwdResults != nullptr)
             {
-                UserInfo user{pwdResults->pw_name, pwdResults->pw_uid, pwdResults->pw_gid};
-                getGroupsForUser(results, user);
+                processUser(pwdResults);
             }
         }
     }
@@ -71,65 +148,25 @@ nlohmann::json UserGroupsProvider::collect(const std::set<uid_t>& uids)
         {
             if (processed_uids.insert(pwdResults->pw_uid).second)
             {
-                UserInfo user{pwdResults->pw_name, pwdResults->pw_uid, pwdResults->pw_gid};
-                getGroupsForUser(results, user);
+                processUser(pwdResults);
             }
         }
 
         m_passwdWrapper->endpwent();
     }
 
-    return results;
-}
-
-void UserGroupsProvider::getGroupsForUser(nlohmann::json& results, const UserInfo& user)
-{
-    gid_t groupsBuffer[EXPECTED_GROUPS_MAX];
-    gid_t* groups = groupsBuffer;
-    int nGroups = EXPECTED_GROUPS_MAX;
-
-    if (!m_groupWrapper)
-    {
-        // std::cerr << "UserGroupsProvider: user groups wrapper is not initialized" << std::endl;
-        return;
-    }
-
-    // GLIBC version before 2.3.3 may have a buffer overrun:
-    // http://man7.org/linux/man-pages/man3/getgrouplist.3.html
-    if (m_groupWrapper->getgrouplist(user.name, user.gid, groups, &nGroups) < 0)
-    {
-        groups = new gid_t[nGroups];
-
-        if (groups == nullptr)
-        {
-            // std::cerr << "Could not allocate memory to get user groups" << std::endl;
-            return;
-        }
-
-        if (m_groupWrapper->getgrouplist(user.name, user.gid, groups, &nGroups) < 0)
-        {
-            // std::cerr << "Could not get user's group list" << std::endl;
-        }
-        else
-        {
-            addGroupsToResults(results, user.uid, groups, nGroups);
-        }
-
-        delete[] groups;
-    }
-    else
-    {
-        addGroupsToResults(results, user.uid, groups, nGroups);
-    }
+    return userGroups;
 }
 
 void UserGroupsProvider::addGroupsToResults(nlohmann::json& results, uid_t uid, const gid_t* groups, int nGroups)
 {
     for (int i = 0; i < nGroups; i++)
     {
-        nlohmann::json row;
-        row["uid"] = uid;
-        row["gid"] = groups[i];
-        results.push_back(row);
+        nlohmann::json groupJson;
+
+        groupJson["uid"] = uid;
+        groupJson["gid"] = groups[i];
+
+        results.push_back(groupJson);
     }
 }
