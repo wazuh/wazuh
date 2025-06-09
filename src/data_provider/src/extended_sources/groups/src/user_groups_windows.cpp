b25e805a298e8c70ac8615e681713d5977587140
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-06-09 17:14:53 +0000

    feat: add groupname and username getters (windows)

--- src/data_provider/src/extended_sources/groups/src/user_groups_windows.cpp
+++ src/data_provider/src/extended_sources/groups/src/user_groups_windows.cpp
@@ -31,88 +31,180 @@ UserGroupsProvider::UserGroupsProvider()
 {
 }
 
-void UserGroupsProvider::processLocalUserGroups(const User& user, const std::vector<Group>& groups, nlohmann::json& results)
+nlohmann::json UserGroupsProvider::collect(const std::set<std::uint32_t>& uids)
 {
-    DWORD groupInfoLevel = 0;
-    DWORD numGroups = 0;
-    DWORD totalGroups = 0;
-    localgroup_users_info_0_ptr groupInfo;
 
-    DWORD ret = 0;
+    nlohmann::json results;
 
-    std::wstring username = m_usersHelper->stringToWstring(user.username);
+    auto localUsers = getLocalUsers(uids);
+    auto groups = getLocalGroups();
 
-    ret = m_winapiWrapper->NetUserGetLocalGroupsWrapper(nullptr,
-                                                        username.c_str(),
-                                                        groupInfoLevel,
-                                                        1,
-                                                        reinterpret_cast<LPBYTE*>(groupInfo.get_new_ptr()),
-                                                        MAX_PREFERRED_LENGTH,
-                                                        &numGroups,
-                                                        &totalGroups);
-
-    if (ret == ERROR_MORE_DATA)
+    for (const auto& user : localUsers)
     {
-        // std::cerr << "User " << user.username << " group membership exceeds buffer limits, processing " << numGroups
-        //           << " our of " << totalGroups << " groups" << std::endl;
-    }
-    else if (ret != NERR_Success || groupInfo == nullptr)
-    {
-        // std::cerr << " NetUserGetLocalGroups failed for user " << user.username << " with " << ret << std::endl;
-        return;
+        processLocalUserGroups(user, groups, results);
     }
 
-    for (std::size_t i = 0; i < numGroups; i++)
-    {
-        std::string groupname = Utils::EncodingWindowsHelper::wstringToStringUTF8(groupInfo.get()[i].lgrui0_name);
+    return results;
+}
 
-        auto optGroup = std::find_if(groups.begin(), groups.end(),
-                                     [&groupname](const Group & group)
+nlohmann::json UserGroupsProvider::getGroupNamesByUid(const std::set<std::uint32_t>& uids)
+{
+    auto localUsers = getLocalUsers(uids);
+    auto groups = getLocalGroups();
+
+    const bool singleUid = uids.size() == 1;
+    nlohmann::json result = singleUid ? nlohmann::json::array() : nlohmann::json::object();
+
+    for (const auto& user : localUsers)
+    {
+        const auto groupNames = getLocalGroupNamesForUser(user.username);
+
+        if (singleUid)
+        {
+            for (const auto& group : groupNames)
+            {
+                result.push_back(group);
+            }
+        }
+        else
+        {
+            result[std::to_string(user.uid)] = groupNames;
+        }
+    }
+
+    return result;
+}
+
+nlohmann::json UserGroupsProvider::getUserNamesByGid(const std::set<std::uint32_t>& gids)
+{
+    const bool singleGid = gids.size() == 1;
+    auto localUsers = getLocalUsers({});
+    auto groups = getLocalGroups();
+
+    std::unordered_map<uint32_t, std::vector<std::string>> gidToUsers;
+
+    for (const auto& user : localUsers)
+    {
+        const auto userGroupNames = getLocalGroupNamesForUser(user.username);
+
+        for (const auto& groupname : userGroupNames)
+        {
+            auto it = std::find_if(groups.begin(),
+                                   groups.end(),
+                                   [&groupname](const Group & group)
+            {
+                return group.groupname == groupname;
+            });
+
+            if (it != groups.end())
+            {
+                if (gids.empty() || gids.find(it->gid) != gids.end())
+                {
+                    gidToUsers[it->gid].emplace_back(user.username);
+                }
+            }
+        }
+    }
+
+    if (singleGid)
+    {
+        uint32_t onlyGid = *gids.begin();
+
+        if (gidToUsers.find(onlyGid) != gidToUsers.end())
+        {
+            return gidToUsers[onlyGid];
+        }
+    }
+    else
+    {
+        nlohmann::json result = nlohmann::json::object();
+
+        for (const auto& [gid, usernames] : gidToUsers)
+        {
+            result[std::to_string(gid)] = usernames;
+        }
+
+        return result;
+    }
+}
+
+void UserGroupsProvider::processLocalUserGroups(const User& user,
+                                                const std::vector<Group>& groups,
+                                                nlohmann::json& results)
+{
+    const auto groupNames = getLocalGroupNamesForUser(user.username);
+
+    for (const auto& groupname : groupNames)
+    {
+        auto optGroup = std::find_if(
+                            groups.begin(), groups.end(), [&groupname](const Group & group)
         {
             return group.groupname == groupname;
         });
 
         if (optGroup == groups.end())
         {
-            // std::cerr << "Group " << groupname << " not found in local groups" << std::endl;
             continue;
         }
 
-        nlohmann::json row;
-
-        row["uid"] = user.uid;
-        row["gid"] = optGroup->gid;
-
-        results.push_back(std::move(row));
+        results.push_back({{"uid", user.uid}, {"gid", optGroup->gid}});
     }
 }
 
-nlohmann::json UserGroupsProvider::collect(const std::set<std::uint32_t>& uids)
+std::vector<User> UserGroupsProvider::getLocalUsers(const std::set<std::uint32_t>& uids)
 {
-
-    nlohmann::json results;
-
     std::set<std::string> processedSids;
     auto localUsers = m_usersHelper->processLocalAccounts(processedSids);
-    auto groups = m_groupsHelper->processLocalGroups();
 
-    auto filteringUsers = [&](const std::vector<User>& users)
+    std::vector<User> filteredUsers;
+
+    for (const auto& user : localUsers)
     {
-        for (const auto& user : users)
+        if (uids.empty() || uids.find(user.uid) != uids.end())
         {
-            if (uids.empty() || uids.find(user.uid) != uids.end())
+            if (user.username == "LOCAL SERVICE" || user.username == "SYSTEM" || user.username == "NETWORK SERVICE")
             {
-                if (user.username == "LOCAL SERVICE" || user.username == "SYSTEM" || user.username == "NETWORK SERVICE")
-                {
-                    continue;
-                }
-
-                processLocalUserGroups(user, groups, results);
+                continue;
             }
+
+            filteredUsers.push_back(user);
         }
-    };
+    }
 
-    filteringUsers(localUsers);
+    return filteredUsers;
+}
 
-    return results;
+std::vector<Group> UserGroupsProvider::getLocalGroups()
+{
+    return m_groupsHelper->processLocalGroups();
+}
+
+std::vector<std::string> UserGroupsProvider::getLocalGroupNamesForUser(const std::string& username)
+{
+    std::wstring wUsername = m_usersHelper->stringToWstring(username);
+    DWORD groupInfoLevel = 0;
+    DWORD numGroups = 0;
+    DWORD totalGroups = 0;
+    localgroup_users_info_0_ptr groupInfo;
+
+    DWORD ret = m_winapiWrapper->NetUserGetLocalGroupsWrapper(nullptr,
+                                                              wUsername.c_str(),
+                                                              groupInfoLevel,
+                                                              1,
+                                                              reinterpret_cast<LPBYTE*>(groupInfo.get_new_ptr()),
+                                                              MAX_PREFERRED_LENGTH,
+                                                              &numGroups,
+                                                              &totalGroups);
+
+    std::vector<std::string> groupNames;
+
+    if (ret == NERR_Success && groupInfo != nullptr)
+    {
+        for (std::size_t i = 0; i < numGroups; ++i)
+        {
+            groupNames.emplace_back(Utils::EncodingWindowsHelper::wstringToStringUTF8(groupInfo.get()[i].lgrui0_name));
+        }
+    }
+
+    return groupNames;
 }
