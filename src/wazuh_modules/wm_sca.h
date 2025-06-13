36ef1532265a9cedd0bc321faa3b6e207571e453
Ariel Martin <ariel.martin@wazuh.com>
2025-06-13 18:43:20 -0300

    feat: Removes old module

--- src/wazuh_modules/wm_sca.h
+++ src/wazuh_modules/wm_sca.h
@@ -12,7 +12,10 @@
 #ifndef WM_SCA_H
 #define WM_SCA_H
 
-#define WM_SCA_LOGTAG SCA_WM_NAME
+#include "../os_xml/os_xml.h"
+#include "wmodules_def.h"
+
+#define WM_SCA_LOGTAG ARGV0 ":sca"
 
 typedef struct wm_sca_t
 {
@@ -20,24 +23,9 @@ typedef struct wm_sca_t
     int scan_on_start : 1;
 } wm_sca_t;
 
-// typedef struct cis_db_info_t {
-//     char *result;
-//     cJSON *event;
-//     int id;
-// } cis_db_info_t;
-
-// typedef struct cis_db_hash_info_t {
-//     cis_db_info_t **elem;
-// } cis_db_hash_info_t;
-
 extern const wm_context WM_SCA_CONTEXT;
 
 // Read configuration and return a module (if enabled) or NULL (if disabled)
 int wm_sca_read(const OS_XML* xml, xml_node** nodes, wmodule* module);
-// char* wm_sca_hash_integrity_file(const char* file);
-// char** wm_sort_variables(const cJSON* const variables);
-// #ifdef WIN32
-// void wm_sca_push_request_win(char* msg);
-// #endif
 
 #endif // WM_SCA_H
