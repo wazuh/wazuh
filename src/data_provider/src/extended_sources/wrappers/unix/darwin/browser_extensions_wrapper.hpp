f4554f78567fc78941ebf4f2bf96c7303ab53dae
Rodrigo Causarano <rjcausarano@gmail.com>
2025-06-18 17:10:51 -0300

    feat: add wrapper and implement unit test.

---
+++ src/data_provider/src/extended_sources/wrappers/unix/darwin/browser_extensions_wrapper.hpp
@@ -0,0 +1,23 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#pragma once
+
+#include <string>
+#include "ibrowser_extensions_wrapper.hpp"
+
+#define kAppPath "/Applications/"
+
+class BrowserExtensionsWrapper : public IBrowserExtensionsWrapper
+{
+  public:
+  std::string getApplicationsPath() override {
+    return std::string(kAppPath);
+  }
+};
