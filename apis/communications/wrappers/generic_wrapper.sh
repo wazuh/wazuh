06603b950ef1391897cd3a5fc906b35684fad755
Dev0 <xadminx@mail.ru>
2025-06-26 01:20:34 +0300

    --

--- apis/communications/wrappers/generic_wrapper.sh
+++ apis/communications/wrappers/generic_wrapper.sh
@@ -3,12 +3,12 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-WPYTHON_BIN="framework/python/bin/python3"
 WAZUH_SHARE="/usr/share/wazuh-server"
+WPYTHON_BIN="${WAZUH_SHARE}/framework/python/bin/python3"
 
 SCRIPT_PATH_NAME="$0"
 SCRIPT_NAME="$(basename ${SCRIPT_PATH_NAME})"
 
 PYTHON_SCRIPT="${WAZUH_SHARE}/apis/scripts/${SCRIPT_NAME}.py"
 
-${PYTHON_SCRIPT} $@
+${WPYTHON_BIN} ${PYTHON_SCRIPT} $@
--- framework/wrappers/generic_wrapper.sh
+++ framework/wrappers/generic_wrapper.sh
@@ -3,12 +3,12 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-WAZUH_PATH="/usr/share/wazuh-server"
-WPYTHON_BIN="${WAZUH_PATH}/framework/python/bin/python3"
+WAZUH_SHARE="/usr/share/wazuh-server"
+WPYTHON_BIN="${WAZUH_SHARE}/framework/python/bin/python3"
 
 SCRIPT_PATH_NAME="$0"
 SCRIPT_NAME="$(basename ${SCRIPT_PATH_NAME})"
 
-PYTHON_SCRIPT="${WAZUH_PATH}/framework/scripts/${SCRIPT_NAME}.py"
+PYTHON_SCRIPT="${WAZUH_SHARE}/framework/scripts/${SCRIPT_NAME}.py"
 
 ${WPYTHON_BIN} ${PYTHON_SCRIPT} "$@"
