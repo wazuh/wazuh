0f8a41cc95af5248e78263761a0bfd05be9b4326
jr0me <joaquin.romera@wazuh.com>
2025-07-09 12:34:57 -0300

    fix: compilation with test and debug flags

--- src/data_provider/tests/sysInfoPackages/sysInfoPackagesNPM_test.hpp
+++ src/data_provider/tests/sysInfoPackages/sysInfoPackagesNPM_test.hpp
@@ -13,23 +13,53 @@
 #define _NPMTEST_HPP
 
 #include "gtest/gtest.h"
-#include "MockFileSystem.hpp"
-#include "MockJsonIO.hpp"
+#include "gmock/gmock.h"
 #include "packagesNPM.hpp"
+#include "json.hpp"
+#include <filesystem>
+#include <ifilesystem_wrapper.hpp>
+
+class MockFileSystemWrapper : public IFileSystemWrapper
+{
+public:
+    MOCK_METHOD(bool, exists, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(bool, is_regular_file, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(bool, is_directory, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(bool, is_socket, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(bool, is_symlink, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(std::filesystem::path, canonical, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(std::vector<std::filesystem::path>, list_directory, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(bool, remove, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(std::uintmax_t, remove_all, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(std::filesystem::path, temp_directory_path, (), (const, override));
+    MOCK_METHOD(bool, create_directories, (const std::filesystem::path&), (const, override));
+    MOCK_METHOD(void, rename, (const std::filesystem::path&, const std::filesystem::path&), (const, override));
+    MOCK_METHOD(int, open, (const char*, int, int), (const, override));
+    MOCK_METHOD(int, flock, (int, int), (const, override));
+    MOCK_METHOD(int, close, (int), (const, override));
+};
+
+class MockJsonIO
+{
+public:
+    MOCK_METHOD(nlohmann::json, readJson, (const std::filesystem::path&), ());
+};
 
 class NPMTest : public ::testing::Test
 {
     protected:
-        std::unique_ptr<NPM<MockFileSystem<std::vector<std::filesystem::path>>, MockJsonIO>> npm;
+        MockFileSystemWrapper* mockFileSystem; // Raw pointer - npm will own it
+        std::unique_ptr<NPM<MockJsonIO>> npm;
 
         void SetUp() override
         {
-            npm = std::make_unique<NPM<MockFileSystem<std::vector<std::filesystem::path>>, MockJsonIO>>();
+            mockFileSystem = new MockFileSystemWrapper();
+            npm = std::make_unique<NPM<MockJsonIO>>(std::unique_ptr<IFileSystemWrapper>(mockFileSystem));
         }
 
         void TearDown() override
         {
-            npm.reset();
+            npm.reset(); // This will delete mockFileSystem
         }
 };
 
