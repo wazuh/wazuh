2cdc6340aecf75fd0d4211ac96342b6f4e504fe0
jr0me <joaquin.romera@wazuh.com>
2025-07-10 16:35:06 -0300

    refactor: remove redundant MockFileSystemWrapper definition from sysInfoPackagesNPM_test and sysInfoPackagesPYPI_test

--- src/data_provider/tests/sysInfoPackages/sysInfoPackagesNPM_test.hpp
+++ src/data_provider/tests/sysInfoPackages/sysInfoPackagesNPM_test.hpp
@@ -18,26 +18,7 @@
 #include "json.hpp"
 #include <filesystem>
 #include <ifilesystem_wrapper.hpp>
-
-class MockFileSystemWrapper : public IFileSystemWrapper
-{
-public:
-    MOCK_METHOD(bool, exists, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(bool, is_regular_file, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(bool, is_directory, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(bool, is_socket, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(bool, is_symlink, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(std::filesystem::path, canonical, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(std::vector<std::filesystem::path>, list_directory, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(bool, remove, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(std::uintmax_t, remove_all, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(std::filesystem::path, temp_directory_path, (), (const, override));
-    MOCK_METHOD(bool, create_directories, (const std::filesystem::path&), (const, override));
-    MOCK_METHOD(void, rename, (const std::filesystem::path&, const std::filesystem::path&), (const, override));
-    MOCK_METHOD(int, open, (const char*, int, int), (const, override));
-    MOCK_METHOD(int, flock, (int, int), (const, override));
-    MOCK_METHOD(int, close, (int), (const, override));
-};
+#include "../shared_modules/file_helper/filesystem/mock_filesystem_wrapper.hpp"
 
 class MockJsonIO
 {
