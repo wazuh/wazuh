f32cee2b2aca8ed368204a895e95cbcbcff11bbb
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-07-15 21:16:29 +0000

    feat: created winsvc wrapper

--- src/data_provider/src/extended_sources/services/src/services_windows.cpp
+++ src/data_provider/src/extended_sources/services/src/services_windows.cpp
@@ -8,8 +8,10 @@
 #include <string>
 #include <vector>
 
-#include "encodingWindowsHelper.h"
 #include "services_windows.hpp"
+#include "winsvc_wrapper.hpp"
+#include "windows_api_wrapper.hpp"
+#include "encodingWindowsHelper.h"
 
 using json = nlohmann::json;
 
@@ -33,9 +35,9 @@ std::optional<std::wstring> expandEnvStringW(const std::wstring& input)
     return result;
 }
 
-const std::string kSvcStartType[] = {"BOOT_START", "SYSTEM_START", "AUTO_START", "DEMAND_START", "DISABLED"};
+const std::string K_SERVICE_START_TYPE[] = {"BOOT_START", "SYSTEM_START", "AUTO_START", "DEMAND_START", "DISABLED"};
 
-const std::string kSvcStatus[] =
+const std::string K_SERVICE_STATUS[] =
 {
     "UNKNOWN", "STOPPED", "START_PENDING", "STOP_PENDING", "RUNNING", "CONTINUE_PENDING", "PAUSE_PENDING", "PAUSED"
 };
@@ -44,7 +46,7 @@ const std::string kSvcStatus[] =
  * https://learn.microsoft.com/en-us/windows/win32/api/winsvc/ns-winsvc-service_status
  * https://learn.microsoft.com/en-us/windows/win32/api/winsvc/ns-winsvc-query_service_configw
  */
-const std::map<int, std::string> kServiceType = {{0x00000001, "KERNEL_DRIVER"},
+const std::map<int, std::string> K_SERVICE_TYPE = {{0x00000001, "KERNEL_DRIVER"},
     {0x00000002, "FILE_SYSTEM_DRIVER"},
     {0x00000010, "OWN_PROCESS"},
     {0x00000020, "SHARE_PROCESS"},
@@ -97,53 +99,62 @@ std::optional<std::wstring> readServiceDllFromParameters(const std::wstring& ser
     return buffer;
 }
 
-ServicesProvider::ServicesProvider() {}
+ServicesProvider::ServicesProvider(std::shared_ptr<IWinSvcWrapper> winSvcWrapper,
+                                   std::shared_ptr<IWindowsApiWrapper> winApiWrapper)
+    : m_winSvcWrapper(std::move(winSvcWrapper))
+    , m_winApiWrapper(std::move(winApiWrapper))
+{}
+
+ServicesProvider::ServicesProvider()
+    : m_winSvcWrapper(std::make_shared<WinSvcWrapper>())
+    , m_winApiWrapper(std::make_shared<WindowsApiWrapper>())
+{}
 
 bool ServicesProvider::getService(SC_HANDLE scmHandle, const ENUM_SERVICE_STATUS_PROCESSW& svc, json& results)
 {
 
-    auto svcHandle = OpenServiceW(scmHandle, svc.lpServiceName, SERVICE_QUERY_CONFIG);
+    auto svcHandle = m_winSvcWrapper->OpenServiceWWrapper(scmHandle, svc.lpServiceName, SERVICE_QUERY_CONFIG);
 
     if (!svcHandle)
     {
-        std::cerr << "Error opening service handle: " << GetLastError() << "\n";
+        std::cerr << "Error opening service handle: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
         return false;
     }
 
     DWORD cbBufSize = 0;
-    QueryServiceConfigW(svcHandle, nullptr, 0, &cbBufSize);
+    m_winSvcWrapper->QueryServiceConfigWWrapper(svcHandle, nullptr, 0, &cbBufSize);
 
-    if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+    if (m_winApiWrapper->GetLastErrorWrapper() != ERROR_INSUFFICIENT_BUFFER)
     {
-        std::cerr << "Error getting config size: " << GetLastError() << "\n";
-        CloseServiceHandle(svcHandle);
+        std::cerr << "Error getting config size: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
+        m_winSvcWrapper->CloseServiceHandleWrapper(svcHandle);
         return false;
     }
 
     auto configBuf = std::make_unique<wchar_t[]>(cbBufSize);
     auto config = reinterpret_cast<LPQUERY_SERVICE_CONFIGW>(configBuf.get());
 
-    if (!QueryServiceConfigW(svcHandle, config, cbBufSize, &cbBufSize))
+    if (!m_winSvcWrapper->QueryServiceConfigWWrapper(svcHandle, config, cbBufSize, &cbBufSize))
     {
-        std::cerr << "Error reading service config: " << GetLastError() << "\n";
-        CloseServiceHandle(svcHandle);
+        std::cerr << "Error reading service config: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
+        m_winSvcWrapper->CloseServiceHandleWrapper(svcHandle);
         return false;
     }
 
     json item;
     item["name"] = Utils::EncodingWindowsHelper::wstringToStringUTF8(svc.lpServiceName);
     item["display_name"] = Utils::EncodingWindowsHelper::wstringToStringUTF8(svc.lpDisplayName);
-    item["status"] = kSvcStatus[svc.ServiceStatusProcess.dwCurrentState];
+    item["status"] = K_SERVICE_STATUS[svc.ServiceStatusProcess.dwCurrentState];
     item["pid"] = svc.ServiceStatusProcess.dwProcessId;
     item["win32_exit_code"] = svc.ServiceStatusProcess.dwWin32ExitCode;
     item["service_exit_code"] = svc.ServiceStatusProcess.dwServiceSpecificExitCode;
-    item["start_type"] = kSvcStartType[config->dwStartType];
+    item["start_type"] = K_SERVICE_START_TYPE[config->dwStartType];
     item["path"] = Utils::EncodingWindowsHelper::wstringToStringUTF8(config->lpBinaryPathName);
     item["user_account"] = Utils::EncodingWindowsHelper::wstringToStringUTF8(config->lpServiceStartName);
 
-    if (kServiceType.count(config->dwServiceType) > 0)
+    if (K_SERVICE_TYPE.count(config->dwServiceType) > 0)
     {
-        item["service_type"] = kServiceType.at(config->dwServiceType);
+        item["service_type"] = K_SERVICE_TYPE.at(config->dwServiceType);
     }
     else
     {
@@ -153,14 +164,14 @@ bool ServicesProvider::getService(SC_HANDLE scmHandle, const ENUM_SERVICE_STATUS
     item["description"] = "";
 
     DWORD descBufSize = 0;
-    QueryServiceConfig2W(svcHandle, SERVICE_CONFIG_DESCRIPTION, nullptr, 0, &descBufSize);
+    m_winSvcWrapper->QueryServiceConfig2WWrapper(svcHandle, SERVICE_CONFIG_DESCRIPTION, nullptr, 0, &descBufSize);
 
-    if (GetLastError() == ERROR_INSUFFICIENT_BUFFER && descBufSize > 0)
+    if (m_winApiWrapper->GetLastErrorWrapper() == ERROR_INSUFFICIENT_BUFFER && descBufSize > 0)
     {
         auto descBuf = std::make_unique<BYTE[]>(descBufSize);
         auto desc = reinterpret_cast<LPSERVICE_DESCRIPTIONW>(descBuf.get());
 
-        if (QueryServiceConfig2W(svcHandle, SERVICE_CONFIG_DESCRIPTION, descBuf.get(), descBufSize, &descBufSize))
+        if (m_winSvcWrapper->QueryServiceConfig2WWrapper(svcHandle, SERVICE_CONFIG_DESCRIPTION, descBuf.get(), descBufSize, &descBufSize))
         {
             if (desc->lpDescription != nullptr)
             {
@@ -187,59 +198,58 @@ bool ServicesProvider::getService(SC_HANDLE scmHandle, const ENUM_SERVICE_STATUS
     }
 
     results.push_back(item);
-    CloseServiceHandle(svcHandle);
+    m_winSvcWrapper->CloseServiceHandleWrapper(svcHandle);
     return true;
 }
 
 nlohmann::json ServicesProvider::collect()
 {
-
     json results = json::array();
 
-    auto scmHandle = OpenSCManager(nullptr, nullptr, GENERIC_READ);
+    auto scmHandle = m_winSvcWrapper->OpenSCManagerWrapper(nullptr, nullptr, GENERIC_READ);
 
     if (!scmHandle)
     {
-        std::cerr << "Failed to connect to Service Connection Manager: " << GetLastError() << "\n";
+        std::cerr << "Failed to connect to Service Connection Manager: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
         return results;
     }
 
     DWORD bytesNeeded = 0;
     DWORD serviceCount = 0;
-    EnumServicesStatusExW(scmHandle,
-                          SC_ENUM_PROCESS_INFO,
-                          SERVICE_WIN32 | SERVICE_DRIVER,
-                          SERVICE_STATE_ALL,
-                          nullptr,
-                          0,
-                          &bytesNeeded,
-                          &serviceCount,
-                          nullptr,
-                          nullptr);
+    m_winSvcWrapper->EnumServicesStatusExWWrapper(scmHandle,
+                                                  SC_ENUM_PROCESS_INFO,
+                                                  SERVICE_WIN32 | SERVICE_DRIVER,
+                                                  SERVICE_STATE_ALL,
+                                                  nullptr,
+                                                  0,
+                                                  &bytesNeeded,
+                                                  &serviceCount,
+                                                  nullptr,
+                                                  nullptr);
 
-    if (GetLastError() != ERROR_MORE_DATA)
+    if (m_winApiWrapper->GetLastErrorWrapper() != ERROR_MORE_DATA)
     {
-        std::cerr << "Error querying buffer size: " << GetLastError() << "\n";
-        CloseServiceHandle(scmHandle);
+        std::cerr << "Error querying buffer size: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
+        m_winSvcWrapper->CloseServiceHandleWrapper(scmHandle);
         return results;
     }
 
     auto buffer = std::make_unique<BYTE[]>(bytesNeeded);
     auto services = reinterpret_cast<ENUM_SERVICE_STATUS_PROCESSW*>(buffer.get());
 
-    if (!EnumServicesStatusExW(scmHandle,
-                               SC_ENUM_PROCESS_INFO,
-                               SERVICE_WIN32 | SERVICE_DRIVER,
-                               SERVICE_STATE_ALL,
-                               buffer.get(),
-                               bytesNeeded,
-                               &bytesNeeded,
-                               &serviceCount,
-                               nullptr,
-                               nullptr))
+    if (!m_winSvcWrapper->EnumServicesStatusExWWrapper(scmHandle,
+                                                       SC_ENUM_PROCESS_INFO,
+                                                       SERVICE_WIN32 | SERVICE_DRIVER,
+                                                       SERVICE_STATE_ALL,
+                                                       buffer.get(),
+                                                       bytesNeeded,
+                                                       &bytesNeeded,
+                                                       &serviceCount,
+                                                       nullptr,
+                                                       nullptr))
     {
-        std::cerr << "Error enumerating services: " << GetLastError() << "\n";
-        CloseServiceHandle(scmHandle);
+        std::cerr << "Error enumerating services: " << m_winApiWrapper->GetLastErrorWrapper() << "\n";
+        m_winSvcWrapper->CloseServiceHandleWrapper(scmHandle);
         return results;
     }
 
@@ -251,6 +261,6 @@ nlohmann::json ServicesProvider::collect()
         }
     }
 
-    CloseServiceHandle(scmHandle);
+    m_winSvcWrapper->CloseServiceHandleWrapper(scmHandle);
     return results;
 }
