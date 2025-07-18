2c512fc5e628452b5fb805ee6a033c400e6b30d9
jnasselle <jnasselle@gmail.com>
2025-07-18 10:29:09 -0300

    Remove deprecated CLIs compilation

--- src/active-response/restart.sh
+++ src/active-response/restart.sh
@@ -35,13 +35,6 @@ PWD=`pwd`
 # Logging the call
 echo "`date` $0 $1 $2 $3 $4 $5" >> ${PWD}/logs/active-responses.log
 
-# Rules and decoders test
-if [ "$TYPE" = "manager" ]; then
-    if !(${PWD}/bin/wazuh-logtest-legacy -t > /dev/null 2>&1); then
-        exit 1;
-    fi
-fi
-
 if command -v systemctl >/dev/null 2>&1; then
     systemctl $PARAM_ACTION wazuh-$TYPE
 else
