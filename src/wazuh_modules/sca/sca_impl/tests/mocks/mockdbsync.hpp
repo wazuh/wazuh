791ee2c5c0aa7ee055b6fec6436c952f1f5b81d8
jr0me <joaquin.romera@wazuh.com>
2025-07-21 10:10:40 -0300

    feat: add unit tests and stubs for process rule evaluator and command execution

--- src/wazuh_modules/sca/sca_impl/tests/mocks/mockdbsync.hpp
+++ src/wazuh_modules/sca/sca_impl/tests/mocks/mockdbsync.hpp
@@ -10,13 +10,13 @@ public:
     MOCK_METHOD(void, addTableRelationship, (const nlohmann::json& jsInput), (override));
     MOCK_METHOD(void, insertData, (const nlohmann::json& jsInsert), (override));
     MOCK_METHOD(void, setTableMaxRow, (const std::string& table, const long long maxRows), (override));
-    MOCK_METHOD(void, syncRow, (const nlohmann::json& jsInput, ResultCallbackData& callbackData), (override));
-    MOCK_METHOD(void, selectRows, (const nlohmann::json& jsInput, ResultCallbackData& callbackData), (override));
+    MOCK_METHOD(void, syncRow, (const nlohmann::json& jsInput, ResultCallbackData callbackData), (override));
+    MOCK_METHOD(void, selectRows, (const nlohmann::json& jsInput, ResultCallbackData callbackData), (override));
     MOCK_METHOD(void, deleteRows, (const nlohmann::json& jsInput), (override));
     MOCK_METHOD(void, updateWithSnapshot, (const nlohmann::json& jsInput, nlohmann::json& jsResult), (override));
     MOCK_METHOD(void,
                 updateWithSnapshot,
-                (const nlohmann::json& jsInput, ResultCallbackData& callbackData),
+                (const nlohmann::json& jsInput, ResultCallbackData callbackData),
                 (override));
     MOCK_METHOD(DBSYNC_HANDLE, handle, (), (override));
 };
