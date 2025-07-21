d3f01dfaff6d37a3d6816bdec48ee2c5237392d8
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-07-21 14:28:38 +0000

    feat: created 'systemd_units' collector

---
+++ src/data_provider/src/extended_sources/services/include/systemd_units_linux.hpp
@@ -0,0 +1,75 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#pragma once
+
+#include <systemd/sd-bus.h>
+#include <string>
+#include <vector>
+#include "json.hpp"
+
+/// @brief Class to collect systemd units information from the systemd service manager.
+/// This class uses D-Bus to query systemd for unit information and formats it into a
+/// JSON object for easy consumption.
+class SystemdUnitsProvider
+{
+    public:
+        /// @brief Default constructor.
+        SystemdUnitsProvider() = default;
+
+        /// @brief Collects systemd units information.
+        /// @return A JSON object containing the collected systemd units information.
+        nlohmann::json collect();
+
+    private:
+        /// @brief Structure to hold information about a systemd unit.
+        struct SystemdUnit
+        {
+            std::string id;
+            std::string description;
+            std::string loadState;
+            std::string activeState;
+            std::string subState;
+            std::string following;
+            std::string objectPath;
+            uint32_t jobId;
+            std::string jobType;
+            std::string jobPath;
+
+            // Extra properties
+            std::string fragmentPath;
+            std::string sourcePath;
+            std::string user;
+            std::string unitFileState;
+        };
+
+        /// @brief Structure to describe a property query for D-Bus.
+        struct PropertyQueryDesc
+        {
+            std::string propertyName;
+            std::string columnName;
+            std::string interface;
+        };
+
+        /// @brief List of property queries to be executed on D-Bus.
+        /// Each entry specifies the property name, the column name to use in the output,
+        /// and the D-Bus interface to query.
+        const std::vector<PropertyQueryDesc> m_propertyQueryList =
+        {
+            {"FragmentPath", "fragment_path", "org.freedesktop.systemd1.Unit"},
+            {"SourcePath", "source_path", "org.freedesktop.systemd1.Unit"},
+            {"User", "user", "org.freedesktop.systemd1.Service"},
+            {"UnitFileState", "unit_file_state", "org.freedesktop.systemd1.Unit"},
+        };
+
+        /// @brief Retrieves systemd units from the system using D-Bus.
+        /// @param output A vector to store the retrieved systemd units.
+        /// @return True if the units were successfully retrieved, false otherwise.
+        bool getSystemdUnits(std::vector<SystemdUnit>& output);
+};
