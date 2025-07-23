ce0b181297e50076cf6dcbff107b93fa996c3d80
jr0me <joaquin.romera@wazuh.com>
2025-07-23 00:27:52 -0300

    test: if compilation files
    there seems to be an error in how the SCA module is being included and
    compiled. this should have been addressed at the beginning of the
    branch.

--- src/wazuh_modules/sca/src/sca.cpp
+++ src/wazuh_modules/sca/src/sca.cpp
@@ -1,5 +1,6 @@
 #include <sca.hpp>
 #include <sca_impl.hpp>
+
 #include <iostream>
 
 #ifdef __cplusplus
@@ -10,66 +11,72 @@ extern "C"
 #include "../../wm_sca.h"
 #include "../wazuh_modules/wmodules_def.h"
 
-    void sca_start(log_callback_t callbackLog)
-    {
-        std::function<void(const modules_log_level_t, const std::string&)> callbackLogWrapper {
-            [callbackLog](const modules_log_level_t level, const std::string& data)
-            {
-                callbackLog(level, data.c_str(), WM_SCA_LOGTAG);
-            }};
-
-        std::function<void(const std::string&)> callbackErrorLogWrapper {
-            [callbackLog](const std::string& data)
-            {
-                callbackLog(LOG_ERROR, data.c_str(), WM_SCA_LOGTAG);
-            }};
-
-        try
+void sca_start2(log_callback_t callbackLog)
+{
+    std::function<void(const modules_log_level_t, const std::string&)> callbackLogWrapper {
+        [callbackLog](const modules_log_level_t level, const std::string& data)
         {
-            SCA::instance().init(std::move(callbackLogWrapper));
-        }
-        catch (const std::exception& ex)
+            callbackLog(level, data.c_str(), WM_SCA_LOGTAG);
+        }};
+
+    std::function<void(const std::string&)> callbackErrorLogWrapper {
+        [callbackLog](const std::string& data)
         {
-            callbackErrorLogWrapper(ex.what());
-        }
-    }
-    void sca_stop()
+            callbackLog(LOG_ERROR, data.c_str(), WM_SCA_LOGTAG);
+        }};
+
+    try
     {
-        SCA::instance().destroy();
+        SCA::instance().init(std::move(callbackLogWrapper));
     }
-
-    int sca_sync_message(const char* data)
+    catch (const std::exception& ex)
     {
-        int ret {-1};
-
-        try
-        {
-            SCA::instance().push(data);
-            ret = 0;
-        }
-        catch (...)
-        {
-        }
-
-        return ret;
+        callbackErrorLogWrapper(ex.what());
     }
+}
 
-    SCA::SCA()
-        : m_logFunction {nullptr}
+void sca_stop()
+{
+    SCA::instance().destroy();
+}
+
+int sca_sync_message(const char* data)
+{
+    int ret {-1};
+
+    try
+    {
+        SCA::instance().push(data);
+        ret = 0;
+    }
+    catch (...)
     {
     }
 
-    void SCA::init(const std::function<void(const modules_log_level_t, const std::string&)> logFunction)
-    {
-        // TODO Start doing whatever the module does
-    }
+    return ret;
+}
 
-    void SCA::destroy()
-    {
-        // TODO Stop doing whatever the module is doing and clean up
-    }
+SCA::SCA()
+: m_logFunction {nullptr}
+{
+}
 
-    void SCA::push(const std::string& data) {}
+void SCA::init(const std::function<void(const modules_log_level_t, const std::string&)> logFunction)
+{
+    // TODO Start doing whatever the module does
+    m_sca = std::make_unique<SecurityConfigurationAssessment>(".", "agent-uuid-placeholder");
+}
+
+void SCA::destroy()
+{
+    // TODO Stop doing whatever the module is doing and clean up
+    m_sca.reset();
+}
+
+void SCA::push(const std::string& data)
+{
+}
+
 #ifdef __cplusplus
 }
 #endif
