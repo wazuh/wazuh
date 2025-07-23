64fdbb00359ddb9dc553a2384edd393920b3c872
jr0me <joaquin.romera@wazuh.com>
2025-07-23 00:42:41 -0300

    fix: cmocka fail

--- src/unit_tests/syscheckd/test_syscheck.c
+++ src/unit_tests/syscheckd/test_syscheck.c
@@ -10,6 +10,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <stdint.h>
 #include <cmocka.h>
 #include <stdio.h>
 #include <string.h>
--- src/unit_tests/wrappers/wazuh/shared/rootcheck_op_wrappers.c
+++ src/unit_tests/wrappers/wazuh/shared/rootcheck_op_wrappers.c
@@ -11,6 +11,7 @@
 #include <stddef.h>
 #include <stdarg.h>
 #include <setjmp.h>
+#include <stdint.h>
 #include <cmocka.h>
 #include <stdio.h>
 
