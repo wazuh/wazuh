9b236ca962bdb4f19b4b2e4c558ced0291ef93d1
Manuel Cano <manuelcanorojo@gmail.com>
2025-07-23 12:40:32 +0200

    clean: renamed mange_agents.h to validate.h and removed additional defines

--- src/addagent/validate.h
+++ src/addagent/validate.h
@@ -8,16 +8,12 @@
  * Foundation.
  */
 
-#ifndef MANAGE_AGENTS_H
-#define MANAGE_AGENTS_H
+#ifndef VALIDATE_AGENT_H
+#define VALIDATE_AGENT_H
 
 #include "shared.h"
 #include "sec.h"
 #include <cJSON.h>
-
-#ifndef ARGV0
-#define ARGV0 "manage_agents"
-#endif
 
 /** Prototypes **/
 
@@ -42,7 +38,6 @@ int print_agents(int print_status, int active_only, int inactive_only, int csv_o
 extern char shost[];
 
 /* Internal defines */
-#define USER_SIZE       514
 #define FILE_SIZE       257
 #define STR_SIZE        66
 #define VALID_AGENT_NAME_CHARS "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.-"
@@ -51,13 +46,5 @@ extern char shost[];
 #define PRINT_AVAILABLE     "\nAvailable agents: \n"
 #define PRINT_AGENT         "   ID: %s, Name: %s, IP: %s\n"
 #define PRINT_AGENT_STATUS  "   ID: %s, Name: %s, IP: %s, %s\n"
-#define PRESS_ENTER     "** Press ENTER to return to the main menu.\n"
-
-/* Add errors */
-#define NO_AGENT        "\n** No agent available. You need to add one first.\n"
-#define SYNTAX_ERROR    "\n** Syntax error at file %s\n\n"
-
-/* Common errors */
-#define INPUT_LARGE     ARGV0 ": Input too large. Not adding it.\n"
 
 #endif
