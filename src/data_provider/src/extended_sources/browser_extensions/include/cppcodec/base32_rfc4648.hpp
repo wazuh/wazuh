29c64a1c82019f7054d97d2416855bde37b03851
Rodrigo Causarano <rjcausarano@gmail.com>
2025-07-25 15:56:07 -0300

    test: adds new unit test

--- src/data_provider/src/extended_sources/browser_extensions/include/cppcodec/base32_rfc4648.hpp
+++ src/data_provider/src/extended_sources/browser_extensions/include/cppcodec/base32_rfc4648.hpp
@@ -27,49 +27,71 @@
 #include "detail/codec.hpp"
 #include "detail/base32.hpp"
 
-namespace cppcodec {
-
-namespace detail {
-
-// RFC 4648 uses a simple alphabet: A-Z starting at index 0, then 2-7 starting at index 26.
-static constexpr const char base32_rfc4648_alphabet[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // at index 26
-    '2', '3', '4', '5', '6', '7'
-};
-
-class base32_rfc4648
+namespace cppcodec
 {
-public:
-    template <typename Codec> using codec_impl = stream_codec<Codec, base32_rfc4648>;
 
-    static CPPCODEC_ALWAYS_INLINE constexpr size_t alphabet_size() {
-        static_assert(sizeof(base32_rfc4648_alphabet) == 32, "base32 alphabet must have 32 values");
-        return sizeof(base32_rfc4648_alphabet);
-    }
-    static CPPCODEC_ALWAYS_INLINE constexpr char symbol(alphabet_index_t idx)
+    namespace detail
     {
-        return base32_rfc4648_alphabet[idx];
-    }
-    static CPPCODEC_ALWAYS_INLINE constexpr char normalized_symbol(char c)
-    {
-        // Lower-case letters are accepted, though not generally expected.
-        return (c >= 'a' && c <= 'z') ? (c - 'a' + 'A') : c;
-    }
 
-    static CPPCODEC_ALWAYS_INLINE constexpr bool generates_padding() { return true; }
-    static CPPCODEC_ALWAYS_INLINE constexpr bool requires_padding() { return true; }
-    static CPPCODEC_ALWAYS_INLINE constexpr char padding_symbol() { return '='; }
-    static CPPCODEC_ALWAYS_INLINE constexpr bool is_padding_symbol(char c) { return c == '='; }
-    static CPPCODEC_ALWAYS_INLINE constexpr bool is_eof_symbol(char c) { return c == '\0'; }
+        // RFC 4648 uses a simple alphabet: A-Z starting at index 0, then 2-7 starting at index 26.
+        static constexpr const char base32_rfc4648_alphabet[] =
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // at index 26
+            '2', '3', '4', '5', '6', '7'
+        };
 
-    // RFC4648 does not specify any whitespace being allowed in base32 encodings.
-    static CPPCODEC_ALWAYS_INLINE constexpr bool should_ignore(char) { return false; }
-};
+        class base32_rfc4648
+        {
+            public:
+                template <typename Codec> using codec_impl = stream_codec<Codec, base32_rfc4648>;
 
-} // namespace detail
+                static CPPCODEC_ALWAYS_INLINE constexpr size_t alphabet_size()
+                {
+                    static_assert(sizeof(base32_rfc4648_alphabet) == 32, "base32 alphabet must have 32 values");
+                    return sizeof(base32_rfc4648_alphabet);
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr char symbol(alphabet_index_t idx)
+                {
+                    return base32_rfc4648_alphabet[idx];
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr char normalized_symbol(char c)
+                {
+                    // Lower-case letters are accepted, though not generally expected.
+                    return (c >= 'a' && c <= 'z') ? (c - 'a' + 'A') : c;
+                }
 
-using base32_rfc4648 = detail::codec<detail::base32<detail::base32_rfc4648>>;
+                static CPPCODEC_ALWAYS_INLINE constexpr bool generates_padding()
+                {
+                    return true;
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr bool requires_padding()
+                {
+                    return true;
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr char padding_symbol()
+                {
+                    return '=';
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr bool is_padding_symbol(char c)
+                {
+                    return c == '=';
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr bool is_eof_symbol(char c)
+                {
+                    return c == '\0';
+                }
+
+                // RFC4648 does not specify any whitespace being allowed in base32 encodings.
+                static CPPCODEC_ALWAYS_INLINE constexpr bool should_ignore(char)
+                {
+                    return false;
+                }
+        };
+
+    } // namespace detail
+
+    using base32_rfc4648 = detail::codec<detail::base32<detail::base32_rfc4648>>;
 
 } // namespace cppcodec
 
--- src/data_provider/src/extended_sources/browser_extensions/include/cppcodec/hex_upper.hpp
+++ src/data_provider/src/extended_sources/browser_extensions/include/cppcodec/hex_upper.hpp
@@ -27,48 +27,67 @@
 #include "detail/codec.hpp"
 #include "detail/hex.hpp"
 
-namespace cppcodec {
-
-namespace detail {
-
-static constexpr const char hex_upper_alphabet[] = {
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        'A', 'B', 'C', 'D', 'E', 'F'
-};
-
-class hex_upper
+namespace cppcodec
 {
-public:
-    template <typename Codec> using codec_impl = stream_codec<Codec, hex_upper>;
 
-    static CPPCODEC_ALWAYS_INLINE constexpr size_t alphabet_size() {
-        static_assert(sizeof(hex_upper_alphabet) == 16, "hex alphabet must have 16 values");
-        return sizeof(hex_upper_alphabet);
-    }
-    static CPPCODEC_ALWAYS_INLINE constexpr char symbol(alphabet_index_t index)
+    namespace detail
     {
-        return hex_upper_alphabet[index];
-    }
-    static CPPCODEC_ALWAYS_INLINE constexpr char normalized_symbol(char c)
-    {
-        // Hex decoding is always case-insensitive (even in RFC 4648), the question
-        // is only for encoding whether to use upper-case or lower-case letters.
-        return (c >= 'a' && c <= 'f') ? (c - 'a' + 'A') : c;
-    }
 
-    static CPPCODEC_ALWAYS_INLINE constexpr bool generates_padding() { return false; }
-    // FIXME: doesn't require padding, but requires a multiple of the encoded block size (2)
-    static CPPCODEC_ALWAYS_INLINE constexpr bool requires_padding() { return false; }
-    static CPPCODEC_ALWAYS_INLINE constexpr bool is_padding_symbol(char) { return false; }
-    static CPPCODEC_ALWAYS_INLINE constexpr bool is_eof_symbol(char c) { return c == '\0'; }
+        static constexpr const char hex_upper_alphabet[] =
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F'
+        };
 
-    // Sometimes hex strings include whitespace, but this variant forbids it.
-    static CPPCODEC_ALWAYS_INLINE constexpr bool should_ignore(char) { return false; }
-};
+        class hex_upper
+        {
+            public:
+                template <typename Codec> using codec_impl = stream_codec<Codec, hex_upper>;
 
-} // namespace detail
+                static CPPCODEC_ALWAYS_INLINE constexpr size_t alphabet_size()
+                {
+                    static_assert(sizeof(hex_upper_alphabet) == 16, "hex alphabet must have 16 values");
+                    return sizeof(hex_upper_alphabet);
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr char symbol(alphabet_index_t index)
+                {
+                    return hex_upper_alphabet[index];
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr char normalized_symbol(char c)
+                {
+                    // Hex decoding is always case-insensitive (even in RFC 4648), the question
+                    // is only for encoding whether to use upper-case or lower-case letters.
+                    return (c >= 'a' && c <= 'f') ? (c - 'a' + 'A') : c;
+                }
 
-using hex_upper = detail::codec<detail::hex<detail::hex_upper>>;
+                static CPPCODEC_ALWAYS_INLINE constexpr bool generates_padding()
+                {
+                    return false;
+                }
+                // FIXME: doesn't require padding, but requires a multiple of the encoded block size (2)
+                static CPPCODEC_ALWAYS_INLINE constexpr bool requires_padding()
+                {
+                    return false;
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr bool is_padding_symbol(char)
+                {
+                    return false;
+                }
+                static CPPCODEC_ALWAYS_INLINE constexpr bool is_eof_symbol(char c)
+                {
+                    return c == '\0';
+                }
+
+                // Sometimes hex strings include whitespace, but this variant forbids it.
+                static CPPCODEC_ALWAYS_INLINE constexpr bool should_ignore(char)
+                {
+                    return false;
+                }
+        };
+
+    } // namespace detail
+
+    using hex_upper = detail::codec<detail::hex<detail::hex_upper>>;
 
 } // namespace cppcodec
 
