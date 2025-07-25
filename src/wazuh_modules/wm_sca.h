2015ccde19ffdbeade36880889dd96dc68b230c3
jr0me <joaquin.romera@wazuh.com>
2025-07-25 01:13:43 -0300

    refactor: bring back SCA code for reading configuration

--- src/wazuh_modules/wm_sca.h
+++ src/wazuh_modules/wm_sca.h
@@ -14,15 +14,43 @@
 
 #include "../os_xml/os_xml.h"
 #include "wmodules_def.h"
+#include "schedule_scan.h"
 
 #define WM_SCA_LOGTAG ARGV0 ":sca"
 
-typedef struct wm_sca_t
-{
-    int enabled : 1;
-    int scan_on_start : 1;
-    unsigned int timeout;
+typedef struct wm_sca_policy_t {
+    unsigned int enabled:1;
+    unsigned int remote:1;
+    char *policy_path;
+    char *policy_id;
+    char *policy_regex_type;
+} wm_sca_policy_t;
+
+typedef struct wm_sca_t {
+    int enabled;
+    int scan_on_start;
+    int skip_nfs;
+    int msg_delay;
+    unsigned int summary_delay;
+    unsigned int request_db_interval;
+    char* scan_time;
+    wm_sca_policy_t** policies;
+    char **alert_msg;
+    int queue;
+    int remote_commands:1;
+    int commands_timeout;
+    sched_scan_config scan_config;
 } wm_sca_t;
+
+typedef struct cis_db_info_t {
+    char *result;
+    cJSON *event;
+    int id;
+} cis_db_info_t;
+
+typedef struct cis_db_hash_info_t {
+    cis_db_info_t **elem;
+} cis_db_hash_info_t;
 
 extern const wm_context WM_SCA_CONTEXT;
 
