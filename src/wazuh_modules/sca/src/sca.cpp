9efb637bafafe737f82e85b96f2701aea246dca7
jr0me <joaquin.romera@wazuh.com>
2025-07-25 00:12:01 -0300

    feat: pass sca_config to setup, placeholders

--- src/wazuh_modules/sca/src/sca.cpp
+++ src/wazuh_modules/sca/src/sca.cpp
@@ -11,7 +11,7 @@ extern "C"
 #include "../../wm_sca.h"
 #include "../wazuh_modules/wmodules_def.h"
 
-void sca_start(log_callback_t callbackLog)
+void sca_start(log_callback_t callbackLog, const struct wm_sca_t* sca_config)
 {
     std::function<void(const modules_log_level_t, const std::string&)> callbackLogWrapper {
         [callbackLog](const modules_log_level_t level, const std::string& data)
@@ -28,6 +28,8 @@ void sca_start(log_callback_t callbackLog)
     try
     {
         SCA::instance().init(std::move(callbackLogWrapper));
+        SCA::instance().setup(sca_config);
+        SCA::instance().run();
     }
     catch (const std::exception& ex)
     {
@@ -70,12 +72,53 @@ void SCA::init(const std::function<void(const modules_log_level_t, const std::st
 {
     // TODO Start doing whatever the module does
     m_sca = std::make_unique<SecurityConfigurationAssessment>(".", "agent-uuid-placeholder");
+    m_logFunction = logFunction;
     // logFunction(LOG_INFO, "SCA module initialized successfully.");
+}
+
+void SCA::setup(const struct wm_sca_t* sca_config)
+{
+    if (m_sca && sca_config) {
+        // Extract configuration values from wm_sca_t
+        bool enabled = sca_config->enabled != 0;
+        bool scan_on_start = sca_config->scan_on_start != 0;
+
+        // This should come from the configuration perhaps
+        // Convert scan_config.interval to scan interval (default to 3600 seconds if not set)
+        // const std::time_t scanInterval = sca_config->scan_config.interval > 0 ? sca_config->scan_config.interval : 3600;
+        const std::time_t scanInterval = 3600;
+
+        // Extract policy paths if available
+        std::vector<std::string> policies;
+        std::vector<std::string> disabledPolicies;
+
+        // TODO
+        // if (sca_config->policies) {
+        //     wm_sca_policy_t* policy = *sca_config->policies;
+        //     while (policy) {
+        //         if (policy->policy_path) {
+        //             policies.push_back(std::string(policy->policy_path));
+        //         }
+        //         policy = policy->next;
+        //     }
+        // }
+        m_sca->Setup(enabled, scan_on_start, scanInterval, policies, disabledPolicies);
+    }
+}
+
+void SCA::run()
+{
+    if (m_sca) {
+        m_sca->Run();
+    }
 }
 
 void SCA::destroy()
 {
-    // TODO Stop doing whatever the module is doing and clean up
+    if (!m_sca) {
+        return;
+    }
+    m_sca->Stop();
     m_sca.reset();
 }
 
