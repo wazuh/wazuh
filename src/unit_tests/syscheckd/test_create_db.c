2bf73b5d8cedd1813ebfccd4d07c932d5f6f6355
chema <chema.fernandez@wazuh.com>
2025-07-29 01:37:39 +0200

    added new .conf for fim regex

--- src/unit_tests/syscheckd/test_create_db.c
+++ src/unit_tests/syscheckd/test_create_db.c
@@ -271,6 +271,31 @@ static int setup_group(void **state) {
     return 0;
 }
 
+static int setup_fim_regex_group(void **state) {
+    if(setup_fim_data(state) != 0)
+        return -1;
+
+    test_mode = 0;
+    expect_any_always(__wrap__mdebug1, formatted_msg);
+
+    expect_function_call_any(__wrap_pthread_rwlock_wrlock);
+    expect_function_call_any(__wrap_pthread_rwlock_unlock);
+    expect_function_call_any(__wrap_pthread_mutex_lock);
+    expect_function_call_any(__wrap_pthread_mutex_unlock);
+    expect_function_call_any(__wrap_pthread_rwlock_rdlock);
+
+    // Read and setup global values.
+    Read_Syscheck_Config("test_syscheck4.conf");
+
+    syscheck.rt_delay = 1;
+    syscheck.max_depth = 256;
+    syscheck.file_max_size = 1024;
+
+    test_mode = 1;
+
+    return 0;
+}
+
 static int setup_wildcards(void **state) {
     expect_function_call_any(__wrap_pthread_rwlock_wrlock);
     expect_function_call_any(__wrap_pthread_mutex_lock);
@@ -4318,12 +4343,6 @@ int main(void) {
         /* fim_audit_json */
         cmocka_unit_test_teardown(test_fim_audit_json, teardown_delete_json),
 
-        /* fim_check_ignore */
-        cmocka_unit_test(test_fim_check_ignore_strncasecmp),
-        cmocka_unit_test(test_fim_check_ignore_regex_file),
-        cmocka_unit_test(test_fim_check_ignore_regex_directory),
-        cmocka_unit_test(test_fim_check_ignore_failure),
-
         /* fim_check_restrict */
         cmocka_unit_test(test_fim_check_restrict_success),
         cmocka_unit_test(test_fim_check_restrict_failure),
@@ -4477,7 +4496,13 @@ int main(void) {
         /* dbsync_attributes_json */
         cmocka_unit_test_setup_teardown(test_dbsync_attributes_json, setup_json_event_attributes, teardown_json_event_attributes),
     };
-
+    const struct CMUnitTest fim_regex_tests[] = { 
+        /* fim_check_ignore */
+        cmocka_unit_test(test_fim_check_ignore_strncasecmp),
+        cmocka_unit_test(test_fim_check_ignore_regex_file),
+        cmocka_unit_test(test_fim_check_ignore_regex_directory),
+        cmocka_unit_test(test_fim_check_ignore_failure),
+    }; 
     const struct CMUnitTest root_monitor_tests[] = {
         cmocka_unit_test(test_fim_checker_root_ignore_file_under_recursion_level),
         cmocka_unit_test(test_fim_checker_root_file_within_recursion_level),
@@ -4491,6 +4516,7 @@ int main(void) {
     int retval;
 
     retval = cmocka_run_group_tests(tests, setup_group, teardown_group);
+    retval += cmocka_run_group_tests(fim_regex_tests, setup_fim_regex_group, teardown_group);
     retval += cmocka_run_group_tests(root_monitor_tests, setup_root_group, teardown_group);
     retval += cmocka_run_group_tests(wildcards_tests, setup_wildcards, teardown_wildcards);
 
