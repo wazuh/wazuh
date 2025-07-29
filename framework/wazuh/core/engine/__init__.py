6f9b1fdd7a538863e12b3c9e4448f0396b21730d
Gastón Palomeque <gaston.palomeque@wazuh.com>
2025-07-29 11:04:09 +0200

    Add engine HTTP API client

---
+++ framework/wazuh/core/engine/__init__.py
@@ -0,0 +1,54 @@
+from contextlib import asynccontextmanager
+from logging import getLogger
+from typing import AsyncIterator
+
+from httpx import AsyncClient, AsyncHTTPTransport, ConnectError, Timeout, TimeoutException, UnsupportedProtocol
+from wazuh.core.exception import WazuhEngineError
+
+logger = getLogger('wazuh')
+
+ENGINE_API_SOCKET_PATH = '/run/wazuh-server/engine-api.socket'
+DEFAULT_RETRIES = 5
+DEFAULT_TIMEOUT = 1
+
+
+class Engine:
+    """Wazuh Engine API client."""
+
+    def __init__(
+        self,
+        socket_path: str = ENGINE_API_SOCKET_PATH,
+        retries: int = DEFAULT_RETRIES,
+        timeout: float = DEFAULT_TIMEOUT,
+    ) -> None:
+        transport = AsyncHTTPTransport(uds=socket_path, retries=retries)
+        self._client = AsyncClient(transport=transport, timeout=Timeout(timeout))
+
+        # Register Engine modules here
+
+    async def close(self) -> None:
+        """Close the Engine client."""
+        await self._client.aclose()
+
+
+@asynccontextmanager
+async def get_engine_client() -> AsyncIterator[Engine]:
+    """Create and return the engine client.
+
+    Returns
+    -------
+    AsyncIterator[Engine]
+        Engine client iterator.
+    """
+    client = Engine()
+
+    try:
+        yield client
+    except TimeoutException:
+        raise WazuhEngineError(2800)
+    except UnsupportedProtocol:
+        raise WazuhEngineError(2801)
+    except ConnectError:
+        raise WazuhEngineError(2802)
+    finally:
+        await client.close()
