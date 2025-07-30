1deab895b707c421424fb90bb2f4c5618c585b09
Ariel Martin <ariel.martin@wazuh.com>
2025-07-30 15:37:52 -0300

    refactor: removes unused config settings

--- src/config/wmodules-sca.c
+++ src/config/wmodules-sca.c
@@ -16,7 +16,6 @@ static const char *XML_ENABLED = "enabled";
 static const char *XML_SCAN_ON_START= "scan_on_start";
 static const char *XML_POLICIES = "policies";
 static const char *XML_POLICY = "policy";
-static const char *XML_SKIP_NFS = "skip_nfs";
 
 #ifdef WAZUH_UNIT_TESTING
 /* Remove static qualifier when testing */
@@ -89,9 +88,6 @@ int wm_sca_read(const OS_XML *xml,xml_node **nodes, wmodule *module)
         sca->enabled = 1;
         sca->scan_on_start = 1;
         sched_scan_init(&(sca->scan_config));
-        sca->skip_nfs = 1;
-        sca->alert_msg = NULL;
-        sca->queue = -1;
         sca->policies = NULL;
         module->context = &WM_SCA_CONTEXT;
         module->tag = strdup(module->context->name);
@@ -179,11 +175,6 @@ int wm_sca_read(const OS_XML *xml,xml_node **nodes, wmodule *module)
         closedir(ruleset_dir);
     } else {
         minfo("Could not open the default SCA ruleset folder '%s': %s", ruleset_path, strerror(open_dir_errno));
-    }
-
-    if(!sca->alert_msg) {
-        /* We store up to 255 alerts */
-        os_calloc(256, sizeof(char *), sca->alert_msg);
     }
 
     if (!nodes) {
@@ -318,17 +309,6 @@ int wm_sca_read(const OS_XML *xml,xml_node **nodes, wmodule *module)
                 }
             }
             OS_ClearNode(children);
-        }
-        else if (!strcmp(nodes[i]->element, XML_SKIP_NFS))
-        {
-            int skip_nfs = eval_bool(nodes[i]->content);
-
-            if(skip_nfs == OS_INVALID){
-                merror("Invalid content for tag '%s'", XML_SKIP_NFS);
-                return OS_INVALID;
-            }
-
-            sca->skip_nfs = skip_nfs;
         }
         else if (is_sched_tag(nodes[i]->element)) {
             // Do nothing
