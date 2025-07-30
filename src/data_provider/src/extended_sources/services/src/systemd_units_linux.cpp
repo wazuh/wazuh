68ca1d26aa1d43f65c4c62a4e715dea7e5c5eb68
Nicolas Bertoldo <nicobertoldo@gmail.com>
2025-07-30 21:16:02 +0000

    feat: replaced libsystemd/sd-bus by libdbus-1

--- src/data_provider/src/extended_sources/services/src/systemd_units_linux.cpp
+++ src/data_provider/src/extended_sources/services/src/systemd_units_linux.cpp
@@ -8,20 +8,16 @@
  */
 
 #include <iostream>
-#include "sdbus_wrapper.hpp"
-#include "system_wrapper.hpp"
+#include "dbus_wrapper.hpp"
 #include "systemd_units_linux.hpp"
 
-SystemdUnitsProvider::SystemdUnitsProvider(std::shared_ptr<ISDBusWrapper> dbusWrapper,
-                                           std::shared_ptr<ISystemWrapper> systemWrapper)
+SystemdUnitsProvider::SystemdUnitsProvider(std::shared_ptr<IDBusWrapper> dbusWrapper)
     : m_dbusWrapper(std::move(dbusWrapper))
-    , m_systemWrapper(std::move(systemWrapper))
 {
 }
 
 SystemdUnitsProvider::SystemdUnitsProvider()
-    : m_dbusWrapper(std::make_shared<SDBusWrapper>())
-    , m_systemWrapper(std::make_shared<SystemWrapper>())
+    : m_dbusWrapper(std::make_shared<DBusWrapper>())
 {
 }
 
@@ -41,18 +37,18 @@ nlohmann::json SystemdUnitsProvider::collect()
         nlohmann::json row;
         row["id"] = unit.id;
         row["description"] = unit.description;
-        row["loadState"] = unit.loadState;
-        row["activeState"] = unit.activeState;
-        row["subState"] = unit.subState;
+        row["load_state"] = unit.loadState;
+        row["active_state"] = unit.activeState;
+        row["sub_state"] = unit.subState;
         row["following"] = unit.following;
-        row["objectPath"] = unit.objectPath;
-        row["jobId"] = unit.jobId;
-        row["jobType"] = unit.jobType;
-        row["jobPath"] = unit.jobPath;
-        row["fragmentPath"] = unit.fragmentPath;
-        row["sourcePath"] = unit.sourcePath;
+        row["object_path"] = unit.objectPath;
+        row["job_id"] = unit.jobId;
+        row["job_type"] = unit.jobType;
+        row["job_path"] = unit.jobPath;
+        row["fragment_path"] = unit.fragmentPath;
+        row["source_path"] = unit.sourcePath;
         row["user"] = unit.user;
-        row["unitFileState"] = unit.unitFileState;
+        row["unit_file_state"] = unit.unitFileState;
 
         result.push_back(std::move(row));
     }
@@ -62,99 +58,132 @@ nlohmann::json SystemdUnitsProvider::collect()
 
 bool SystemdUnitsProvider::getSystemdUnits(std::vector<SystemdUnit>& output)
 {
-    sd_bus* bus = nullptr;
-    int ret = m_dbusWrapper->sd_bus_open_system(&bus);
+    DBusError err;
+    m_dbusWrapper->error_init(&err);
 
-    if (ret < 0)
+    DBusConnection* conn = m_dbusWrapper->bus_get(DBUS_BUS_SYSTEM, &err);
+
+    if (!conn || m_dbusWrapper->error_is_set(&err))
     {
-        std::cerr << "Failed to connect to system bus: " << m_systemWrapper->strerror(-ret) << std::endl;
+        std::cerr << "Failed to connect to system bus: " << err.message << std::endl;
+        m_dbusWrapper->error_free(&err);
         return false;
     }
 
-    sd_bus_message* reply = nullptr;
-    ret = m_dbusWrapper->callListUnits(bus, &reply, nullptr);
+    DBusMessage* msg = m_dbusWrapper->message_new_method_call(
+                           "org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", "ListUnits");
 
-    if (ret < 0)
+    if (!msg)
     {
-        std::cerr << "Failed to call ListUnits: " << m_systemWrapper->strerror(-ret) << std::endl;
-        m_dbusWrapper->sd_bus_unref(bus);
+        std::cerr << "Failed to create message" << std::endl;
         return false;
     }
 
-    ret = m_dbusWrapper->sd_bus_message_enter_container(reply, SD_BUS_TYPE_ARRAY, "(ssssssouso)");
+    DBusMessage* reply = m_dbusWrapper->connection_send_with_reply_and_block(conn, msg, -1, &err);
+    m_dbusWrapper->message_unref(msg);
 
-    if (ret < 0)
+    if (!reply || m_dbusWrapper->error_is_set(&err))
     {
-        std::cerr << "Failed to enter array: " << m_systemWrapper->strerror(-ret) << std::endl;
-        m_dbusWrapper->sd_bus_message_unref(reply);
-        m_dbusWrapper->sd_bus_unref(bus);
+        std::cerr << "Failed to send message: " << err.message << std::endl;
+        m_dbusWrapper->error_free(&err);
         return false;
     }
 
-    while ((ret = m_dbusWrapper->sd_bus_message_enter_container(reply, SD_BUS_TYPE_STRUCT, "ssssssouso")) > 0)
+    DBusMessageIter iter;
+
+    if (!m_dbusWrapper->message_iter_init(reply, &iter))
     {
+        std::cerr << "Reply has no arguments." << std::endl;
+        m_dbusWrapper->message_unref(reply);
+        return false;
+    }
+
+    // Array of structs
+    if (DBUS_TYPE_ARRAY != m_dbusWrapper->message_iter_get_arg_type(&iter))
+    {
+        std::cerr << "Expected an array." << std::endl;
+        m_dbusWrapper->message_unref(reply);
+        return false;
+    }
+
+    DBusMessageIter arrayIter;
+    m_dbusWrapper->message_iter_recurse(&iter, &arrayIter);
+
+    while (m_dbusWrapper->message_iter_get_arg_type(&arrayIter) == DBUS_TYPE_STRUCT)
+    {
+        DBusMessageIter structIter;
+        m_dbusWrapper->message_iter_recurse(&arrayIter, &structIter);
+
         SystemdUnit unit;
-        ret = m_dbusWrapper->parseSystemdUnit(reply, unit);
+        const char* str = nullptr;
+        uint32_t jobId;
 
-        if (ret < 0)
-        {
-            std::cerr << "Failed to read unit struct: " << m_systemWrapper->strerror(-ret) << std::endl;
-            break;
-        }
+        // ssssssou → 8 elements
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.id = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.description = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.loadState = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.activeState = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.subState = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.following = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.objectPath = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &jobId);
+        unit.jobId = jobId;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.jobType = str;
+        m_dbusWrapper->message_iter_next(&structIter);
+        m_dbusWrapper->message_iter_get_basic(&structIter, &str);
+        unit.jobPath = str;
 
-        // Reading additional properties via D-Bus
+        // Extra properties
         for (const auto& query : m_propertyQueryList)
         {
-            char* propValueCstring = nullptr;
-            ret = m_dbusWrapper->sd_bus_get_property_string(
-                      bus,
-                      "org.freedesktop.systemd1",
-                      unit.objectPath.c_str(),
-                      query.interface.c_str(),
-                      query.propertyName.c_str(),
-                      nullptr,
-                      &propValueCstring);
+            std::string value;
 
-            if (ret < 0 && query.propertyName != "User")
+            if (m_dbusWrapper->getProperty(
+                        conn, "org.freedesktop.systemd1", unit.objectPath, query.interface, query.propertyName, value))
             {
-                std::cerr << "Error reading property "
-                          << query.propertyName << " from "
-                          << unit.objectPath << ": " << m_systemWrapper->strerror(-ret) << std::endl;
+                if (query.columnName == "fragment_path")
+                {
+                    unit.fragmentPath = value;
+                }
+                else if (query.columnName == "source_path")
+                {
+                    unit.sourcePath = value;
+                }
+                else if (query.columnName == "user")
+                {
+                    unit.user = value;
+                }
+                else if (query.columnName == "unit_file_state")
+                {
+                    unit.unitFileState = value;
+                }
             }
-
-            std::string propValue = "";
-
-            if (ret >= 0)
+            else if (query.propertyName != "User")
             {
-                propValue = propValueCstring;
-                free(propValueCstring);
-            }
-
-            if (query.columnName == "fragment_path")
-            {
-                unit.fragmentPath = propValue;
-            }
-            else if (query.columnName == "source_path")
-            {
-                unit.sourcePath = propValue;
-            }
-            else if (query.columnName == "user")
-            {
-                unit.user = propValue;
-            }
-            else if (query.columnName == "unit_file_state")
-            {
-                unit.unitFileState = propValue;
+                std::cerr << "Failed to get property " << query.propertyName << " for " << unit.objectPath << std::endl;
             }
         }
 
         output.emplace_back(std::move(unit));
-        m_dbusWrapper->sd_bus_message_exit_container(reply);
+        m_dbusWrapper->message_iter_next(&arrayIter);
     }
 
-    m_dbusWrapper->sd_bus_message_exit_container(reply);
-    m_dbusWrapper->sd_bus_message_unref(reply);
-    m_dbusWrapper->sd_bus_unref(bus);
-
+    m_dbusWrapper->message_unref(reply);
     return true;
 }
