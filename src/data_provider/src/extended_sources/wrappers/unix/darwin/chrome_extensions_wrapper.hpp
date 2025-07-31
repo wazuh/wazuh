654346032deadf68a0e916e69b50ed08051bad05
Rodrigo Causarano <rjcausarano@gmail.com>
2025-07-31 18:49:33 -0300

    fix: fix unit tests on MacOs

---
+++ src/data_provider/src/extended_sources/wrappers/unix/darwin/chrome_extensions_wrapper.hpp
@@ -0,0 +1,22 @@
+/* Copyright (C) 2015, Wazuh Inc.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#pragma once
+
+#include <filesystem>
+#include "ichrome_extensions_wrapper.hpp"
+
+class ChromeExtensionsWrapper : public IChromeExtensionsWrapper
+{
+    public:
+        std::filesystem::path getHomePath() override
+        {
+            return std::filesystem::path("/Users");
+        }
+};
