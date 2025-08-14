393ce3bdef92e12155f6f36b7041991a00d8a324
cborla <borlach12@gmail.com>
2025-08-14 12:55:36 -0300

    feat: include service table for linux

--- src/data_provider/src/sysInfoLinux.cpp
+++ src/data_provider/src/sysInfoLinux.cpp
@@ -31,11 +31,11 @@
 #include "linuxInfoHelper.h"
 #include "groups_linux.hpp"
 #include "user_groups_linux.hpp"
-
 #include "logged_in_users_linux.hpp"
 #include "shadow_linux.hpp"
 #include "sudoers_unix.hpp"
 #include "users_linux.hpp"
+#include "systemd_units_linux.hpp"
 
 using ProcessInfo = std::unordered_map<int64_t, std::pair<int32_t, std::string>>;
 
@@ -846,10 +846,39 @@ nlohmann::json SysInfo::getUsers() const
 
 nlohmann::json SysInfo::getServices() const
 {
-    nlohmann::json result;
+    nlohmann::json result = nlohmann::json::array();
 
-    // ServicesProvider usersProvider;
-    // auto collectedServices = servicesProvider.collect();
+    SystemdUnitsProvider servicesProvider;
+    auto collectedServices = servicesProvider.collect();
+
+    for (auto& svc : collectedServices)
+    {
+        nlohmann::json serviceItem{};
+
+        // ECS mapping based on the provided table
+        serviceItem["service_id"]              = svc.value("id",                UNKNOWN_VALUE);
+        serviceItem["service_name"]            = UNKNOWN_VALUE;
+        serviceItem["service_description"]     = svc.value("description",       UNKNOWN_VALUE);
+        serviceItem["service_state"]           = svc.value("active_state",      UNKNOWN_VALUE);
+        serviceItem["service_sub_state"]       = svc.value("sub_state",         UNKNOWN_VALUE);
+        serviceItem["service_start_type"]      = UNKNOWN_VALUE;
+        serviceItem["service_type"]            = UNKNOWN_VALUE;
+        serviceItem["process_pid"]             = 0;
+        serviceItem["service_exit_code"]       = 0;
+        serviceItem["service_win32_exit_code"] = 0;
+        serviceItem["process_executable"]      = svc.value("fragment_path",     UNKNOWN_VALUE);
+        serviceItem["service_address"]         = UNKNOWN_VALUE;
+        serviceItem["user_name"]               = svc.value("user",              UNKNOWN_VALUE);
+        serviceItem["service_enabled"]         = svc.value("unit_file_state",   UNKNOWN_VALUE);
+        serviceItem["service_following"]       = svc.value("following",         UNKNOWN_VALUE);
+        serviceItem["service_object_path"]     = svc.value("object_path",       UNKNOWN_VALUE);
+        serviceItem["service_target_ephemeral_id"] = svc.value("job_id",        0);
+        serviceItem["service_target_type"]     = svc.value("job_type",          UNKNOWN_VALUE);
+        serviceItem["service_target_address"]  = svc.value("job_path",          UNKNOWN_VALUE);
+        serviceItem["file_path"]               = svc.value("source_path",       UNKNOWN_VALUE);
+
+        result.push_back(std::move(serviceItem));
+    }
 
     return result;
 }
