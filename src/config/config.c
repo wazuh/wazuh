5dffeeb82d06eea45923fa7057b942a6e7ed7305
LucioDonda <donda.lucio@gmail.com>
2025-08-15 13:01:59 +0000

    feat: removing csyslog daemon and related code

--- src/config/config.c
+++ src/config/config.c
@@ -28,7 +28,6 @@ static int read_main_elements(const OS_XML *xml, int modules,
     const char *osalerts = "alerts";                            /* Server Config */
     const char *osemailalerts = "email_alerts";                 /* Server Config */
     const char *osdbd = "database_output";                      /* Server Config */
-    const char *oscsyslogd = "syslog_output";                   /* Server Config */
     const char *oslocalfile = "localfile";                      /* Agent Config  */
     const char *osremote = "remote";                            /* Agent Config  */
     const char *osclient = "client";                            /* Agent Config  */
@@ -81,10 +80,6 @@ static int read_main_elements(const OS_XML *xml, int modules,
             }
         } else if (chld_node && (strcmp(node[i]->element, osemailalerts) == 0)) {
             if ((modules & CMAIL) && (Read_EmailAlerts(chld_node, d1, d2) < 0)) {
-                goto fail;
-            }
-        } else if (chld_node && (strcmp(node[i]->element, oscsyslogd) == 0)) {
-            if ((modules & CSYSLOGD) && (Read_CSyslog(chld_node, d1, d2) < 0)) {
                 goto fail;
             }
         } else if (strcmp(node[i]->element, ossyscheck) == 0) {
