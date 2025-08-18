dc9525a7258e57b6d4261823835fbb51e5160c08
Dwordcito <octa.tala@gmail.com>
2025-08-18 09:35:07 -0300

    Add response logic.

--- src/wazuh_modules/inventory_sync/src/responseDispatcher.hpp
+++ src/wazuh_modules/inventory_sync/src/responseDispatcher.hpp
@@ -12,19 +12,44 @@
 #ifndef _RESPONSE_DISPATCHER_HPP
 #define _RESPONSE_DISPATCHER_HPP
 
+#include "asyncValueDispatcher.hpp"
 #include "context.hpp"
 #include "flatbuffers/include/inventorySync_generated.h"
+#include "loggerHelper.h"
 #include "socketClient.hpp"
+#include "stringHelper.h"
 #include "threadDispatcher.h"
+#include <algorithm>
 #include <cstdint>
 #include <memory>
 #include <vector>
 
-using ResponseQueue =
-    Utils::AsyncDispatcher<std::shared_ptr<flatbuffers::FlatBufferBuilder>,
-                           std::function<void(const std::shared_ptr<flatbuffers::FlatBufferBuilder>&)>>;
+struct ResponseMessage
+{
+    flatbuffers::FlatBufferBuilder builder;
+    std::string agentId;
+    std::string moduleName;
 
-constexpr auto EXECQUEUE {"queue/alerts/execq"};
+    // Default constructor
+    ResponseMessage() = default;
+
+    // Move constructor
+    ResponseMessage(ResponseMessage&& other) noexcept = default;
+
+    // Move assignment operator
+    ResponseMessage& operator=(ResponseMessage&& other) noexcept = default;
+
+    // Delete copy constructor and copy assignment operator
+    ResponseMessage(const ResponseMessage&) = delete;
+    ResponseMessage& operator=(const ResponseMessage&) = delete;
+
+    // Destructor
+    ~ResponseMessage() = default;
+};
+
+using ResponseQueue = Utils::AsyncValueDispatcher<ResponseMessage, std::function<void(ResponseMessage&&)>>;
+
+constexpr auto ARQUEUE {"queue/alerts/ar"};
 
 template<typename TQueue>
 class ResponseDispatcherImpl
@@ -36,30 +61,37 @@ public:
     explicit ResponseDispatcherImpl()
     {
         auto responseSocketClient =
-            std::make_shared<SocketClient<Socket<OSPrimitives, AppendHeaderProtocol>, EpollWrapper>>(EXECQUEUE);
+            std::make_shared<SocketClient<Socket<OSPrimitives, NoHeaderProtocol>, EpollWrapper>>(ARQUEUE);
         responseSocketClient->connect(
             [](const char*, uint32_t, const char*, uint32_t)
             {
-                std::cout << "OnRead to " << EXECQUEUE << std::endl;
+                logDebug2(LOGGER_DEFAULT_TAG, "OnRead to %s", ARQUEUE);
                 // Not used
             },
             []()
             {
-                std::cout << "Connected to " << EXECQUEUE << std::endl;
+                logDebug2(LOGGER_DEFAULT_TAG, "Connected to %s", ARQUEUE);
                 // Not used
-            });
+            },
+            SOCK_DGRAM);
 
         m_responseDispatcher = std::make_unique<ResponseQueue>(
-            [responseSocketClient](const std::shared_ptr<flatbuffers::FlatBufferBuilder>& data)
+            [responseSocketClient](const ResponseMessage& data)
             {
-                std::cout << "Sending data: " << data->GetSize() << std::endl;
-                responseSocketClient->send(reinterpret_cast<const char*>(data->GetBufferPointer()), data->GetSize());
-                // We wait to keep the maximum number of events per second
-                // if (reportsWait > 0)
-                // {
-                //     std::this_thread::sleep_for(std::chrono::microseconds(reportsWait));
-                // }
-                // logDebug2(WM_VULNSCAN_LOGTAG, "Report sent: %s", data.c_str());
+                thread_local std::vector<uint8_t> messageVector;
+                constexpr auto header = "(msg_to_agent) [] N!S ";
+                constexpr auto headerSize = 22;
+                messageVector.clear();
+                messageVector.reserve(headerSize + 5 + data.builder.GetSize());
+                messageVector.assign(header, header + headerSize);
+                std::ranges::copy(data.agentId, std::back_inserter(messageVector));
+                messageVector.push_back(' ');
+                std::ranges::copy(data.moduleName, std::back_inserter(messageVector));
+                messageVector.push_back(' ');
+                std::ranges::copy(data.builder.GetBufferPointer(),
+                                  data.builder.GetBufferPointer() + data.builder.GetSize(),
+                                  std::back_inserter(messageVector));
+                responseSocketClient->send(reinterpret_cast<const char*>(messageVector.data()), messageVector.size());
             });
     }
 
@@ -70,46 +102,51 @@ public:
 
     void sendStartAck(const Wazuh::SyncSchema::Status status, const std::shared_ptr<Context> ctx) const
     {
-        auto fbBuilder = std::make_shared<flatbuffers::FlatBufferBuilder>();
-        auto startAckOffset =
-            Wazuh::SyncSchema::CreateStartAckDirect(*fbBuilder, status, ctx->sessionId, ctx->moduleName.c_str());
+        ResponseMessage responseMessage;
+        responseMessage.builder.Clear();
+        responseMessage.agentId = Utils::padString(std::to_string(ctx->agentId), '0', 3);
+        responseMessage.moduleName = ctx->moduleName;
+        auto startAckOffset = Wazuh::SyncSchema::CreateStartAck(responseMessage.builder, status, ctx->sessionId);
 
         auto messageOffset = Wazuh::SyncSchema::CreateMessage(
-            *fbBuilder, Wazuh::SyncSchema::MessageType_StartAck, startAckOffset.Union());
-        fbBuilder->Finish(messageOffset);
+            responseMessage.builder, Wazuh::SyncSchema::MessageType_StartAck, startAckOffset.Union());
+        responseMessage.builder.Finish(messageOffset);
 
-        m_responseDispatcher->push(fbBuilder);
+        m_responseDispatcher->push(std::move(responseMessage));
     }
 
     void sendEndAck(const Wazuh::SyncSchema::Status status, const std::shared_ptr<Context> ctx) const
     {
-        auto fbBuilder = std::make_shared<flatbuffers::FlatBufferBuilder>();
-        auto startAckOffset =
-            Wazuh::SyncSchema::CreateEndAckDirect(*fbBuilder, status, ctx->sessionId, ctx->moduleName.c_str());
+        ResponseMessage responseMessage;
+        responseMessage.builder.Clear();
+        responseMessage.agentId = std::to_string(ctx->agentId);
+        responseMessage.moduleName = ctx->moduleName;
+        auto startAckOffset = Wazuh::SyncSchema::CreateEndAck(responseMessage.builder, status, ctx->sessionId);
 
-        auto messageOffset =
-            Wazuh::SyncSchema::CreateMessage(*fbBuilder, Wazuh::SyncSchema::MessageType_EndAck, startAckOffset.Union());
-        fbBuilder->Finish(messageOffset);
+        auto messageOffset = Wazuh::SyncSchema::CreateMessage(
+            responseMessage.builder, Wazuh::SyncSchema::MessageType_EndAck, startAckOffset.Union());
+        responseMessage.builder.Finish(messageOffset);
 
-        m_responseDispatcher->push(fbBuilder);
+        m_responseDispatcher->push(std::move(responseMessage));
     }
 
     void sendEndMissingSeq(const uint64_t sessionId, const std::vector<std::pair<uint64_t, uint64_t>>& ranges) const
     {
-        auto fbBuilder = std::make_shared<flatbuffers::FlatBufferBuilder>();
+        // auto fbBuilder = std::make_shared<flatbuffers::FlatBufferBuilder>();
+        ResponseMessage responseMessage;
         std::vector<flatbuffers::Offset<Wazuh::SyncSchema::Pair>> convertedRanges;
         for (const auto& [first, second] : ranges)
         {
-            auto offset = Wazuh::SyncSchema::CreatePair(*fbBuilder, first, second);
+            auto offset = Wazuh::SyncSchema::CreatePair(responseMessage.builder, first, second);
             convertedRanges.push_back(offset);
         }
 
-        auto endOffset = Wazuh::SyncSchema::CreateReqRetDirect(*fbBuilder, &convertedRanges, sessionId);
-        auto messageOffset =
-            Wazuh::SyncSchema::CreateMessage(*fbBuilder, Wazuh::SyncSchema::MessageType_ReqRet, endOffset.Union());
-        fbBuilder->Finish(messageOffset);
+        auto endOffset = Wazuh::SyncSchema::CreateReqRetDirect(responseMessage.builder, &convertedRanges, sessionId);
+        auto messageOffset = Wazuh::SyncSchema::CreateMessage(
+            responseMessage.builder, Wazuh::SyncSchema::MessageType_ReqRet, endOffset.Union());
+        responseMessage.builder.Finish(messageOffset);
 
-        m_responseDispatcher->push(fbBuilder);
+        m_responseDispatcher->push(std::move(responseMessage));
     }
 };
 
