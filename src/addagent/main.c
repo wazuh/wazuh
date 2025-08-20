ed5827dc3660c207f9cc9625561b0e59d5b1fdb0
Manuel Cano <manuelcanorojo@gmail.com>
2025-08-20 12:54:32 +0200

    clean: deprecate manager related code in manage_agents binary

--- src/addagent/main.c
+++ src/addagent/main.c
@@ -34,21 +34,11 @@ static int setenv(const char *name, const char *val, __attribute__((unused)) int
 __attribute__((noreturn)) static void helpmsg()
 {
     print_header();
-    print_out("  %s -[Vhlj] [-a <ip> -n <name>] [-F sec] [-e id] [-r id] [-i id] [-f file]", ARGV0);
+    print_out("  %s -[Vhj] [-i id]", ARGV0);
     print_out("    -V          Version and license message.");
     print_out("    -h          This help message.");
     print_out("    -j          Use JSON output.");
-    print_out("    -l          List available agents.");
-    print_out("    -L          Disable agents limit.");
-    print_out("    -a <ip>     Add new agent.");
-    print_out("    -n <name>   Name for new agent.");
-    print_out("    -e <id>     Extracts key for an agent (Manager only).");
-    print_out("    -r <id>     Remove an agent (Manager only).");
     print_out("    -i <key>    Import authentication key (Agent only).");
-    print_out("    -R <sec>    Replace agents that were registered at least <sec> seconds.");
-    print_out("    -D <sec>    Replace agents that were disconnected at least <sec> seconds.");
-    print_out("    -f <file>   Bulk generate client keys from file (Manager only).");
-    print_out("                <file> contains lines in IP,NAME format.");
     exit(1);
 }
 
@@ -57,12 +47,7 @@ static void print_banner()
     printf("\n");
     printf(BANNER, __ossec_name, __ossec_version, (int)(21 - strlen(__ossec_name) - strlen(__ossec_version)), "                     ");
 
-#ifdef CLIENT
     printf(BANNER_CLIENT);
-#else
-    printf(BANNER_OPT);
-#endif
-
     return;
 }
 
@@ -86,14 +71,9 @@ int main(int argc, char **argv)
     enable_dll_verification();
 #endif
 
-    int c = 0, cmdlist = 0, json_output = 0;
-    int disconnected_time;
-    int after_registration_time;
+    int c = 0, json_output = 0;
     char *user_msg;
-    char *end;
-    const char *cmdexport = NULL;
     const char *cmdimport = NULL;
-    const char *cmdbulk = NULL;
 #ifndef WIN32
     const char *group = GROUPGLOBAL;
     gid_t gid;
@@ -113,7 +93,7 @@ int main(int argc, char **argv)
     }
 #endif
 
-    while ((c = getopt(argc, argv, "Vhle:r:i:f:ja:n:R:D:L")) != -1) {
+    while ((c = getopt(argc, argv, "Vhji:")) != -1) {
         switch (c) {
             case 'V':
                 print_version();
@@ -121,95 +101,14 @@ int main(int argc, char **argv)
             case 'h':
                 helpmsg();
                 break;
-            case 'e':
-#ifdef CLIENT
-                merror_exit("Key export only available on a master.");
-#endif
-                if (!optarg) {
-                    merror_exit("-e needs an argument.");
-                }
-                cmdexport = optarg;
-                break;
-            case 'r':
-#ifdef CLIENT
-                merror_exit("Key removal only available on a master.");
-#endif
-                if (!optarg) {
-                    merror_exit("-r needs an argument.");
-                }
-
-                /* Use environment variables already available to remove_agent() */
-                setenv("OSSEC_ACTION", "r", 1);
-                setenv("OSSEC_AGENT_ID", optarg, 1);
-                setenv("OSSEC_ACTION_CONFIRMED", "y", 1);
-                break;
             case 'i':
-#ifndef CLIENT
-                merror_exit("Key import only available on an agent.");
-#endif
                 if (!optarg) {
                     merror_exit("-i needs an argument.");
                 }
                 cmdimport = optarg;
                 break;
-            case 'f':
-#ifdef CLIENT
-                merror_exit("Bulk generate keys only available on a master.");
-#endif
-                if (!optarg) {
-                    merror_exit("-f needs an argument.");
-                }
-                cmdbulk = optarg;
-                printf("Bulk load file: %s\n", cmdbulk);
-                break;
-            case 'l':
-                cmdlist = 1;
-                break;
             case 'j':
                 json_output = 1;
-                break;
-            case 'a':
-#ifdef CLIENT
-                merror_exit("Agent adding only available on a master.");
-#endif
-                if (!optarg)
-                    merror_exit("-a needs an argument.");
-                setenv("OSSEC_ACTION", "a", 1);
-                setenv("OSSEC_ACTION_CONFIRMED", "y", 1);
-                setenv("OSSEC_AGENT_IP", optarg, 1);
-                setenv("OSSEC_AGENT_ID", "0", 1);
-            break;
-            case 'n':
-                if (!optarg)
-                    merror_exit("-n needs an argument.");
-                setenv("OSSEC_AGENT_NAME", optarg, 1);
-                break;
-            case 'D':
-                if (!optarg)
-                    merror_exit("-D needs an argument.");
-
-                disconnected_time = strtol(optarg, &end, 10);
-
-                if (optarg == end || disconnected_time < 0)
-                    merror_exit("Invalid number for -D");
-
-                setenv("DISCONNECTED_TIME", optarg, 1);
-                break;
-            case 'R':
-                if (!optarg)
-                    merror_exit("-R needs an argument.");
-
-                after_registration_time = strtol(optarg, &end, 10);
-
-                if (optarg == end || after_registration_time < 0)
-                    merror_exit("Invalid number for -R");
-
-                setenv("AFTER_REGISTRATION_TIME", optarg, 1);
-                break;
-            case 'L':
-#ifndef CLIENT
-                mwarn("This option no longer applies. The agent limit has been removed.");
-#endif
                 break;
             default:
                 helpmsg();
@@ -223,22 +122,6 @@ int main(int argc, char **argv)
     /* Before chroot */
     srandom_init();
     getuname();
-
-#ifndef CLIENT
-    int is_worker = w_is_worker();
-    char *master;
-
-    switch (is_worker) {
-        case -1:
-            merror("Invalid option at cluster configuration");
-            return 0;
-        case 1:
-            master = get_master_node();
-            merror("Wazuh is running in cluster mode: %s is not available in worker nodes. Please, try again in the master node: %s.", ARGV0, master);
-            free(master);
-            return 0;
-    }
-#endif
 
 #ifndef WIN32
     if (gethostname(shost, sizeof(shost) - 1) < 0) {
@@ -282,17 +165,8 @@ int main(int argc, char **argv)
     }
 #endif
 
-    if (cmdlist == 1) {
-        list_agents(cmdlist);
-        exit(0);
-    } else if (cmdimport) {
+    if (cmdimport) {
         k_import(cmdimport);
-        exit(0);
-    } else if (cmdexport) {
-        k_extract(cmdexport, json_output);
-        exit(0);
-    } else if (cmdbulk) {
-        k_bulkload(cmdbulk);
         exit(0);
     }
 
@@ -314,41 +188,9 @@ int main(int argc, char **argv)
 
         /* All the allowed actions */
         switch (user_msg[0]) {
-            case 'A':
-            case 'a':
-#ifdef CLIENT
-                printf("\n ** Agent adding only available on a master ** \n\n");
-#else
-                add_agent(json_output);
-#endif
-                break;
-            case 'e':
-            case 'E':
-#ifdef CLIENT
-                printf("\n ** Key export only available on a master ** \n\n");
-                break;
-#endif
-                k_extract(NULL, json_output);
-                break;
             case 'i':
             case 'I':
-#ifdef CLIENT
                 k_import(NULL);
-#else
-                printf("\n ** Key import only available on an agent ** \n\n");
-#endif
-                break;
-            case 'l':
-            case 'L':
-                list_agents(0);
-                break;
-            case 'r':
-            case 'R':
-#ifdef CLIENT
-                printf("\n ** Key removal only available on a master ** \n\n");
-                break;
-#endif
-                remove_agent(json_output);
                 break;
             case 'q':
             case 'Q':
