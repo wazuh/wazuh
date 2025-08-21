122b96a8a9860112fd5292b07e88b3324269359b
cborla <borlach12@gmail.com>
2025-08-21 13:05:58 -0300

    feat: the primary key of the browser extensions table is changed to a composite key

--- src/wazuh_modules/syscollector/src/syscollectorImp.cpp
+++ src/wazuh_modules/syscollector/src/syscollectorImp.cpp
@@ -838,6 +838,7 @@ nlohmann::json Syscollector::getBrowserExtensionsData()
         {
             sanitizeJsonValue(extension);
             extension["checksum"] = getItemChecksum(extension);
+            extension["item_id"] = getItemId(extension, BROWSER_EXTENSIONS_ITEM_ID_FIELDS);
             ret.push_back(std::move(extension));
         }
     }
