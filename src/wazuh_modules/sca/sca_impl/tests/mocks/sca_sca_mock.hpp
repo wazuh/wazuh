6b397f3c028fca6614248861337cc5a89d0ce3af
Ariel Martin <ariel.martin@wazuh.com>
2025-08-21 15:27:17 -0300

    feat: adds or modifies mocks

---
+++ src/wazuh_modules/sca/sca_impl/tests/mocks/sca_sca_mock.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <sca_impl.hpp>
+
+#include <mock_dbsync.hpp>
+#include <mock_filesystem_wrapper.hpp>
+
+class SCAMock : public SecurityConfigurationAssessment
+{
+    public:
+        SCAMock(std::shared_ptr<IDBSync> dBSync = nullptr, std::shared_ptr<IFileSystemWrapper> fileSystemWrapper = nullptr)
+            : SecurityConfigurationAssessment("db_path"
+                                              , "agent_uuid"
+                                              , dBSync
+            , fileSystemWrapper) {}
+
+        std::vector<std::unique_ptr<ISCAPolicy>>& GetPolicies()
+        {
+            return m_policies;
+        }
+};
