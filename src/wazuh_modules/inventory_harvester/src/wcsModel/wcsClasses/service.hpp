e59633522921b35b1ee0e079c4ecc5fc675c3c37
Juan Cabrera Carabaza <jcabre88@gmail.com>
2025-08-22 13:21:49 +0000

    Updated development to use the new services fields table

--- src/wazuh_modules/inventory_harvester/src/wcsModel/wcsClasses/service.hpp
+++ src/wazuh_modules/inventory_harvester/src/wcsModel/wcsClasses/service.hpp
@@ -19,56 +19,132 @@ struct Service final
 {
     struct ServiceInfo final
     {
-        std::string_view id;          // service.id (ECS)
-        std::string_view name;        // service.name (ECS)
-        std::string_view description; // service.description (Custom)
-        std::string_view state;       // service.state (ECS)
-        std::string_view sub_state;   // service.sub_state (Custom)
-        std::string_view start_type;  // service.start_type (Custom)
-        std::string_view type;        // service.type (ECS)
-        long exit_code;               // service.exit_code (Custom)
-        std::string_view enabled;     // service.enabled (Custom)
+        std::string_view id;
+        std::string_view name;
+        std::string_view description;
+        std::string_view type;
+        std::string_view state;
+        std::string_view sub_state;
+        std::string_view enabled;
+        std::string_view start_type;
+        std::string_view restart;
+        long frequency;
+        bool starts_on_mount;
+        std::string_view starts_on_path_modified;
+        std::string_view starts_on_not_empty_directory;
+        bool inetd_compatibility;
+        std::string_view address;
+        long exit_code;
+        long win32_exit_code;
+        std::string_view following;
+        std::string_view object_path;
 
         REFLECTABLE(MAKE_FIELD("id", &ServiceInfo::id),
                     MAKE_FIELD("name", &ServiceInfo::name),
                     MAKE_FIELD("description", &ServiceInfo::description),
+                    MAKE_FIELD("type", &ServiceInfo::type),
                     MAKE_FIELD("state", &ServiceInfo::state),
                     MAKE_FIELD("sub_state", &ServiceInfo::sub_state),
+                    MAKE_FIELD("enabled", &ServiceInfo::enabled),
                     MAKE_FIELD("start_type", &ServiceInfo::start_type),
-                    MAKE_FIELD("type", &ServiceInfo::type),
+                    MAKE_FIELD("restart", &ServiceInfo::restart),
+                    MAKE_FIELD("frequency", &ServiceInfo::frequency),
+                    MAKE_FIELD("starts_on_mount", &ServiceInfo::starts_on_mount),
+                    MAKE_FIELD("starts_on_path_modified", &ServiceInfo::starts_on_path_modified),
+                    MAKE_FIELD("starts_on_not_empty_directory", &ServiceInfo::starts_on_not_empty_directory),
+                    MAKE_FIELD("inetd_compatibility", &ServiceInfo::inetd_compatibility),
+                    MAKE_FIELD("address", &ServiceInfo::address),
                     MAKE_FIELD("exit_code", &ServiceInfo::exit_code),
-                    MAKE_FIELD("enabled", &ServiceInfo::enabled));
+                    MAKE_FIELD("win32_exit_code", &ServiceInfo::win32_exit_code),
+                    MAKE_FIELD("following", &ServiceInfo::following),
+                    MAKE_FIELD("object_path", &ServiceInfo::object_path));
     };
 
     struct Process final
     {
-        long pid;                           // process.pid (ECS)
-        std::string_view executable;        // process.executable (ECS)
-        std::string_view args;              // process.args (ECS)
-        std::string_view working_directory; // process.working_directory (ECS)
+        long pid;
+        std::string_view executable;
+        std::string_view args;
+        std::string_view working_directory;
+        std::string_view root_directory;
+
+        struct User final
+        {
+            std::string_view name;
+
+            REFLECTABLE(MAKE_FIELD("name", &User::name));
+        };
+
+        struct Group final
+        {
+            std::string_view name;
+
+            REFLECTABLE(MAKE_FIELD("name", &Group::name));
+        };
+
+        User user;
+        Group group;
 
         REFLECTABLE(MAKE_FIELD("pid", &Process::pid),
                     MAKE_FIELD("executable", &Process::executable),
                     MAKE_FIELD("args", &Process::args),
-                    MAKE_FIELD("working_directory", &Process::working_directory));
-    };
-
-    struct User final
-    {
-        std::string_view name; // user.name (ECS)
-
-        REFLECTABLE(MAKE_FIELD("name", &User::name));
+                    MAKE_FIELD("working_directory", &Process::working_directory),
+                    MAKE_FIELD("root_directory", &Process::root_directory),
+                    MAKE_FIELD("user", &Process::user),
+                    MAKE_FIELD("group", &Process::group));
     };
 
     struct File final
     {
-        std::string_view path;           // file.path (ECS)
-        std::string_view log_path;       // file.log.path (Custom)
-        std::string_view error_log_path; // file.error_log.path (Custom)
+        std::string_view path;
 
-        REFLECTABLE(MAKE_FIELD("path", &File::path),
-                    MAKE_FIELD("log_path", &File::log_path),
-                    MAKE_FIELD("error_log_path", &File::error_log_path));
+        REFLECTABLE(MAKE_FIELD("path", &File::path));
+    };
+
+    struct Log final
+    {
+        struct File final
+        {
+            std::string_view path;
+
+            REFLECTABLE(MAKE_FIELD("path", &File::path));
+        };
+
+        File file;
+
+        REFLECTABLE(MAKE_FIELD("file", &Log::file));
+    };
+
+    struct Error final
+    {
+        struct Log final
+        {
+            struct File final
+            {
+                std::string_view path;
+
+                REFLECTABLE(MAKE_FIELD("path", &File::path));
+            };
+
+            File file;
+
+            REFLECTABLE(MAKE_FIELD("file", &Log::file));
+        };
+
+        Log log;
+
+        REFLECTABLE(MAKE_FIELD("log", &Error::log));
+    };
+
+    struct Target final
+    {
+        long ephemeral_id;
+        std::string_view type;
+        std::string_view address;
+
+        REFLECTABLE(MAKE_FIELD("ephemeral_id", &Target::ephemeral_id),
+                    MAKE_FIELD("type", &Target::type),
+                    MAKE_FIELD("address", &Target::address));
     };
 };
 
