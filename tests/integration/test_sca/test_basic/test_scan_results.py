fff1b774811c5fdb6579dd3a7eb3a74a5ed25ad6
Fabio <fabio.contreras@wazuh.com>
2025-08-21 23:32:16 -0500

    test(test_sca): Added wait for new scan before checking remediation and small rule fix for windows

--- tests/integration/test_sca/test_basic/test_scan_results.py
+++ tests/integration/test_sca/test_basic/test_scan_results.py
@@ -149,13 +149,13 @@ def test_sca_scan_results(test_configuration, test_metadata, prepare_cis_policie
     assert log_monitor.callback_result is not None and log_monitor.callback_result[0] == expected_policy
 
     # Wait for the SCA scan checks to start for the specific policy
-    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_STARTED_CHECK), timeout=200 if sys.platform == WINDOWS else 30)
+    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_STARTED_CHECK), timeout=200 if sys.platform == WINDOWS else 20)
     assert log_monitor.callback_result is not None and log_monitor.callback_result[0] == expected_policy
 
     # Get the results for the checks obtained in the SCA scan
-    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_RESULT), timeout=200 if sys.platform == WINDOWS else 30, accumulations=int(test_metadata['results']))
+    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_RESULT), timeout=200 if sys.platform == WINDOWS else 20, only_new_events=True, accumulations=int(test_metadata['results']))
     assert log_monitor.callback_result is not None and all(result[1] == expected_policy for result in log_monitor.callback_result)
 
     # Wait for the SCA scan checks to end for the specific policy
-    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_ENDED_CHECK), timeout=200 if sys.platform == WINDOWS else 30)
+    log_monitor.start(callback=callbacks.generate_callback(patterns.SCA_SCAN_ENDED_CHECK), timeout=200 if sys.platform == WINDOWS else 20)
     assert log_monitor.callback_result is not None and log_monitor.callback_result[0] == expected_policy
