23fda6c26276930c0f9f112653b1bc7cba636252
Facundo Dalmau <facundo.dalmau@wazuh.com>
2025-08-25 14:55:04 -0300

    Fix manager tests

--- framework/wazuh/tests/test_manager.py
+++ framework/wazuh/tests/test_manager.py
@@ -76,17 +76,17 @@ def test_get_status(mock_status):
 
 
 @pytest.mark.parametrize('tag, level, total_items, sort_by, sort_ascending', [
-    (None, None, 13, None, None),
+    (None, None, 11, None, None),
     ('wazuh-modulesd:database', None, 2, None, None),
     ('wazuh-modulesd:syscollector', None, 2, None, None),
     ('wazuh-modulesd:syscollector', None, 2, None, None),
     ('wazuh-modulesd:aws-s3', None, 5, None, None),
     ('wazuh-execd', None, 1, None, None),
     ('random', None, 0, ['timestamp'], True),
-    (None, 'info', 7, ['timestamp'], False),
+    (None, 'info', 5, ['timestamp'], False),
     (None, 'error', 2, ['level'], True),
     (None, 'debug', 2, ['level'], False),
-    (None, None, 13, ['tag'], True),
+    (None, None, 11, ['tag'], True),
     (None, 'random', 0, None, True),
     (None, 'warning', 2, None, False)
 ])
