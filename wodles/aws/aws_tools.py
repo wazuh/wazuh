0d44d1854d01be509e1fec2ccefa546c587d5a84
javier <javier.sanchez@wazuh.com>
2025-08-27 10:41:43 +0200

    Delete aws_profile

--- wodles/aws/aws_tools.py
+++ wodles/aws/aws_tools.py
@@ -290,12 +290,7 @@ def args_valid_sqs_name(sqs_name):
     return sqs_name
 
 
-def arg_validate_security_lake_auth_params(
-        external_id: Optional[str],
-        name: Optional[str],
-        iam_role_arn: Optional[str],
-        profile: Optional[str]
-):
+def arg_validate_security_lake_auth_params(external_id: Optional[str], name: Optional[str], iam_role_arn: Optional[str]):
     """
     Validate the Securit Lake authentication arguments.
 
@@ -307,8 +302,6 @@ def arg_validate_security_lake_auth_params(
         Name of the SQS Queue.
     iam_role_arn : Optional[str]
         IAM Role.
-    profile : Optional[str]
-        AWS Profile.
     """
 
     if iam_role_arn is None:
@@ -320,8 +313,6 @@ def arg_validate_security_lake_auth_params(
     if external_id is None:
         error('Used a subscriber but no --external_id provided.')
         sys.exit(21)
-    if profile is None:
-        debug('Used a subscriber but no --aws_profile provided, the "default" value will be used.', 2)
 
 
 def get_aws_config_params() -> configparser.RawConfigParser:
