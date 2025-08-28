5406cd3ffefcbb9394696534706778477f1d3279
Julian Morales <jmorales@unc.edu.ar>
2025-08-28 14:22:16 +0000

    Rename engine daemon to analysisd

--- src/os_execd/wcom.c
+++ src/os_execd/wcom.c
@@ -381,7 +381,7 @@ error:
 
 size_t wcom_check_manager_config(char **output) {
     static const char *daemons[] = {"bin/wazuh-authd", "bin/wazuh-remoted",
-                                    "bin/wazuh-execd", "bin/wazuh-engine", "bin/wazuh-logcollector",
+                                    "bin/wazuh-execd", "bin/wazuh-analysisd", "bin/wazuh-logcollector",
                                     "bin/wazuh-syscheckd", "bin/wazuh-modulesd", "bin/wazuh-clusterd",
                                     NULL
                                     };
