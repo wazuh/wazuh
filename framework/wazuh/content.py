0bf601c25271af7357f45cc6c2fc67edaa4a657d
Federico Ramos <federico.ramos@wazuh.com>
2025-09-05 19:41:32 -0300

    Add new Engine endpoints

---
+++ framework/wazuh/content.py
@@ -0,0 +1,66 @@
+# Copyright (C) 2015, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+from wazuh.rbac.decorators import expose_resources
+from wazuh.core.results import WazuhResult
+
+@expose_resources(actions=['content:delete'], resources=['content:file:{filename}'])
+async def delete_content() -> WazuhResult:
+    """
+    Delete a specific content file.
+
+    Returns
+    -------
+    WazuhResult
+        Result object with the operation outcome.
+    """
+    return WazuhResult({'message': 'Not Implemented'})
+
+@expose_resources(actions=['content:status'], resources=['*:*:*'])
+async def get_content_status() -> WazuhResult:
+    """
+    Get the status of all available content.
+
+    Returns
+    -------
+    WazuhResult
+        Result object with the operation outcome.
+    """
+    return WazuhResult({'message': 'Not Implemented'})
+
+@expose_resources(actions=['content:reload'], resources=['*:*:*'])
+async def reload_contents() -> WazuhResult:
+    """
+    Reload all content files.
+
+    Returns
+    -------
+    WazuhResult
+        Result object with the operation outcome.
+    """
+    return WazuhResult({'message': 'Not Implemented'})
+
+@expose_resources(actions=['decoders:validate'], resources=['*:*:*'])
+async def validate_contents() -> WazuhResult:
+    """
+    Validate content file.
+
+    Returns
+    -------
+    WazuhResult
+        Result object with the operation outcome.
+    """
+    return WazuhResult({'message': 'Not Implemented'})
+
+@expose_resources(actions=['decoders:validate'], resources=['*:*:*'])
+async def log_tests() -> WazuhResult:
+    """
+    Run log test for content files.
+
+    Returns
+    -------
+    WazuhResult
+        Result object with the operation outcome.
+    """
+    return WazuhResult({'message': 'Not Implemented'})
