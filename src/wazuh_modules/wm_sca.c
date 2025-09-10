b79ddbaaa34c4b14182afd815a78467c1d821b86
jr0me <joaquin.romera@wazuh.com>
2025-09-10 15:30:21 -0300

    fix: wrong string

--- src/wazuh_modules/wm_sca.c
+++ src/wazuh_modules/wm_sca.c
@@ -393,7 +393,7 @@ void * wm_sca_sync_module(__attribute__((unused)) void * args) {
     }
 
     while (sca_sync_module_running) {
-        mdebug1("Running inventory synchronization.");
+        mdebug1("Running SCA synchronization.");
 
         if (sca_sync_module_ptr) {
             sca_sync_module_ptr(MODE_DELTA, sca_sync_response_timeout, SCA_SYNC_RETRIES, sca_sync_max_eps);
@@ -401,7 +401,7 @@ void * wm_sca_sync_module(__attribute__((unused)) void * args) {
             mdebug1("Sync function not available");
         }
 
-        mdebug1("Inventory synchronization finished, waiting for %d seconds before next run.", sca_sync_interval);
+        mdebug1("SCA synchronization finished, waiting for %d seconds before next run.", sca_sync_interval);
 
         for (uint32_t i = 0; i < sca_sync_interval && sca_sync_module_running; i++) {
             sleep(1);
