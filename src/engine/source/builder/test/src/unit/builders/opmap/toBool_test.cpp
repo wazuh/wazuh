7208a43739f8549260e70a12ce9727f10b5c295a
jam300 <javieradan1490@gmail.com>
2025-09-16 20:09:39 -0600

    fixed clang format

--- src/engine/source/builder/test/src/unit/builders/opmap/toBool_test.cpp
+++ src/engine/source/builder/test/src/unit/builders/opmap/toBool_test.cpp
@@ -5,85 +5,83 @@ using namespace builder::builders;
 
 namespace
 {
-    auto customRefExpected(bool times = false)
+auto customRefExpected(bool times = false)
+{
+    return [=](const BuildersMocks& mocks)
     {
-        return [=](const BuildersMocks& mocks)
+        if (times)
         {
-            if (times)
-            {
-                EXPECT_CALL(*mocks.ctx, validator()).Times(testing::AtLeast(1));
-                EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillRepeatedly(testing::Return(false));
-            }
-            else
-            {
-                EXPECT_CALL(*mocks.ctx, validator());
-                EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillOnce(testing::Return(false));
-            }
-            return None {};
-        };
-    }
-
-    auto customRefExpected(json::Json jValue)
-    {
-        return [=](const BuildersMocks& mocks)
+            EXPECT_CALL(*mocks.ctx, validator()).Times(testing::AtLeast(1));
+            EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillRepeatedly(testing::Return(false));
+        }
+        else
         {
             EXPECT_CALL(*mocks.ctx, validator());
             EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillOnce(testing::Return(false));
-            return jValue;
-        };
-    }
-
-    auto schemaRefExpected(schemf::Type sType)
-    {
-        return [=](const BuildersMocks& mocks)
-        {
-            EXPECT_CALL(*mocks.ctx, validator()).Times(testing::AtLeast(1));
-            EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillOnce(testing::Return(true));
-            EXPECT_CALL(*mocks.validator, getType(DotPath("ref"))).WillOnce(testing::Return(sType));
-            return None {};
-        };
-    }
+        }
+        return None {};
+    };
 }
+
+auto customRefExpected(json::Json jValue)
+{
+    return [=](const BuildersMocks& mocks)
+    {
+        EXPECT_CALL(*mocks.ctx, validator());
+        EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillOnce(testing::Return(false));
+        return jValue;
+    };
+}
+
+auto schemaRefExpected(schemf::Type sType)
+{
+    return [=](const BuildersMocks& mocks)
+    {
+        EXPECT_CALL(*mocks.ctx, validator()).Times(testing::AtLeast(1));
+        EXPECT_CALL(*mocks.validator, hasField(DotPath("ref"))).WillOnce(testing::Return(true));
+        EXPECT_CALL(*mocks.validator, getType(DotPath("ref"))).WillOnce(testing::Return(sType));
+        return None {};
+    };
+}
+} // namespace
 
 namespace mapbuildtest
 {
-    INSTANTIATE_TEST_SUITE_P(
-        Builders,
-        MapBuilderTest,
-        testing::Values(
-            MapT({}, opBuilderHelperToBool, FAILURE()),
-            MapT({makeValue(R"("true")")}, opBuilderHelperToBool, FAILURE()),
-            MapT({makeRef("ref")}, opBuilderHelperToBool, SUCCESS()),
-            MapT({makeRef("ref"), makeRef("ref")}, opBuilderHelperToBool, FAILURE())),
-        testNameFormatter<MapBuilderTest>("ToBool"));
+INSTANTIATE_TEST_SUITE_P(Builders,
+                         MapBuilderTest,
+                         testing::Values(MapT({}, opBuilderHelperToBool, FAILURE()),
+                                         MapT({makeValue(R"("true")")}, opBuilderHelperToBool, FAILURE()),
+                                         MapT({makeRef("ref")}, opBuilderHelperToBool, SUCCESS()),
+                                         MapT({makeRef("ref"), makeRef("ref")}, opBuilderHelperToBool, FAILURE())),
+                         testNameFormatter<MapBuilderTest>("ToBool"));
 }
 
 namespace mapoperatestest
 {
-    INSTANTIATE_TEST_SUITE_P(
-        Builders,
-        MapOperationTest,
-        testing::Values(
-            /*** to_bool ***/
-            /*** invalid type reference field ***/
-            MapT(R"({"ref": "some"})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
-            MapT(R"({"ref": "[1,2,3,4]"})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
-            MapT(R"({"ref": {"key": "value"}})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
-            /*** success cases ***/
-            MapT(R"({"ref": 1})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("true")))),
-            MapT(R"({"ref": 1.0})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("true")))),
-            MapT(R"({"ref": 0})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("false")))),
-            MapT(R"({"ref": 0.0})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("false")))),
-            /*** additional numeric values now supported ***/
-            MapT(R"({"ref": 2})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("true")))),
-            MapT(R"({"ref": -1})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("true")))),
-            MapT(R"({"ref": 0.5})", opBuilderHelperToBool, {makeRef("ref")},
-                SUCCESS(customRefExpected(json::Json("true"))))),
-        testNameFormatter<MapOperationTest>("ToBool"));
+INSTANTIATE_TEST_SUITE_P(
+    Builders,
+    MapOperationTest,
+    testing::Values(
+        /*** to_bool ***/
+        /*** invalid type reference field ***/
+        MapT(R"({"ref": "some"})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
+        MapT(R"({"ref": "[1,2,3,4]"})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
+        MapT(R"({"ref": {"key": "value"}})", opBuilderHelperToBool, {makeRef("ref")}, FAILURE(customRefExpected())),
+        /*** success cases ***/
+        MapT(R"({"ref": 1})", opBuilderHelperToBool, {makeRef("ref")}, SUCCESS(customRefExpected(json::Json("true")))),
+        MapT(
+            R"({"ref": 1.0})", opBuilderHelperToBool, {makeRef("ref")}, SUCCESS(customRefExpected(json::Json("true")))),
+        MapT(R"({"ref": 0})", opBuilderHelperToBool, {makeRef("ref")}, SUCCESS(customRefExpected(json::Json("false")))),
+        MapT(R"({"ref": 0.0})",
+             opBuilderHelperToBool,
+             {makeRef("ref")},
+             SUCCESS(customRefExpected(json::Json("false")))),
+        /*** additional numeric values now supported ***/
+        MapT(R"({"ref": 2})", opBuilderHelperToBool, {makeRef("ref")}, SUCCESS(customRefExpected(json::Json("true")))),
+        MapT(R"({"ref": -1})", opBuilderHelperToBool, {makeRef("ref")}, SUCCESS(customRefExpected(json::Json("true")))),
+        MapT(R"({"ref": 0.5})",
+             opBuilderHelperToBool,
+             {makeRef("ref")},
+             SUCCESS(customRefExpected(json::Json("true"))))),
+    testNameFormatter<MapOperationTest>("ToBool"));
 }
