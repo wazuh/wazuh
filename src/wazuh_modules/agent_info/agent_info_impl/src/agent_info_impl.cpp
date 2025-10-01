52f6b1baf99b71568c996c56b791ecfbc512fdca
jr0me <13784077+jr0me@users.noreply.github.com>
2025-10-01 10:13:39 -0300

    feat: add files for new agent info module

---
+++ src/wazuh_modules/agent_info/agent_info_impl/src/agent_info_impl.cpp
@@ -0,0 +1,13 @@
+#include "agent_info_impl.hpp"
+
+
+AgentInfoImpl::AgentInfoImpl() {}
+
+AgentInfoImpl::~AgentInfoImpl()
+{
+    stop();
+}
+
+void AgentInfoImpl::start() {}
+
+void AgentInfoImpl::stop() {}
