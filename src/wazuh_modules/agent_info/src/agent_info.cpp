33793e606d1b72f70b06198b686b081b8f681f39
jr0me <13784077+jr0me@users.noreply.github.com>
2025-10-01 16:32:00 -0300

    feat: add logs

--- src/wazuh_modules/agent_info/src/agent_info.cpp
+++ src/wazuh_modules/agent_info/src/agent_info.cpp
@@ -1,7 +1,19 @@
 #include "agent_info.h"
 #include "agent_info_impl.hpp"
+#include "logging_helper.hpp"
 
 #include <memory>
+
+/* Agent Info db directory */
+#ifndef WAZUH_UNIT_TESTING
+#define AGENT_INFO_DB_DISK_PATH "queue/agent_info/agent_info.db"
+#else
+#ifndef WIN32
+#define AGENT_INFO_DB_DISK_PATH    "./agent_info.db"
+#else
+#define AGENT_INFO_DB_DISK_PATH    ".\\agent_info.db"
+#endif // WIN32
+#endif // WAZUH_UNIT_TESTING
 
 #ifdef __cplusplus
 extern "C"
@@ -13,9 +25,11 @@ extern "C"
 
     void agent_info_start(const struct wm_agent_info_t* agent_info_config)
     {
+        (void)agent_info_config; // Mark as unused for now
+
         if (!g_agent_info_impl)
         {
-            g_agent_info_impl = std::make_unique<AgentInfoImpl>();
+            g_agent_info_impl = std::make_unique<AgentInfoImpl>(AGENT_INFO_DB_DISK_PATH);
         }
         g_agent_info_impl->start();
     }
@@ -26,6 +40,16 @@ extern "C"
         {
             g_agent_info_impl->stop();
             g_agent_info_impl.reset();
+        }
+    }
+
+    void agent_info_set_log_function(log_callback_t log_callback)
+    {
+        if (log_callback)
+        {
+            LoggingHelper::setLogCallback([log_callback](const modules_log_level_t level, const char* log) {
+                log_callback(level, log, "agent-info");
+            });
         }
     }
 
