d3d28bf4252e7dd111735f18f2626c9feab55394
jr0me <13784077+jr0me@users.noreply.github.com>
2025-10-01 16:32:00 -0300

    feat: add logs

--- src/wazuh_modules/agent_info/agent_info_impl/src/agent_info_impl.cpp
+++ src/wazuh_modules/agent_info/agent_info_impl/src/agent_info_impl.cpp
@@ -1,30 +1,27 @@
 #include "agent_info_impl.hpp"
 
 #include <dbsync.hpp>
+#include "logging_helper.hpp"
 
-constexpr auto AGENT_METADATA_SQL_STATEMENT
-{
-    R"(CREATE TABLE IF NOT EXISTS agent_metadata (
-    agent_id          TEXT NOT NULL PRIMARY KEY,
-    agent_name        TEXT,
-    agent_version     TEXT,
-    host_architecture TEXT,
-    host_hostname     TEXT,
-    host_os_name      TEXT,
-    host_os_type      TEXT,
-    host_os_platform  TEXT,
-    host_os_version   TEXT,
-    checksum          TEXT NOT NULL);)"
-};
+const char* AGENT_METADATA_SQL_STATEMENT =
+    "CREATE TABLE IF NOT EXISTS agent_metadata ("
+    "agent_id          TEXT NOT NULL PRIMARY KEY,"
+    "agent_name        TEXT,"
+    "agent_version     TEXT,"
+    "host_architecture TEXT,"
+    "host_hostname     TEXT,"
+    "host_os_name      TEXT,"
+    "host_os_type      TEXT,"
+    "host_os_platform  TEXT,"
+    "host_os_version   TEXT,"
+    "checksum          TEXT NOT NULL);";
 
-constexpr auto AGENT_GROUPS_SQL_STATEMENT
-{
-    R"(CREATE TABLE IF NOT EXISTS agent_groups (
-    agent_id          TEXT NOT NULL,
-    group_name        TEXT NOT NULL,
-    PRIMARY KEY (agent_id, group_name),
-    FOREIGN KEY (agent_id) REFERENCES agent_metadata(agent_id) ON DELETE CASCADE);)"
-};
+const char* AGENT_GROUPS_SQL_STATEMENT =
+    "CREATE TABLE IF NOT EXISTS agent_groups ("
+    "agent_id          TEXT NOT NULL,"
+    "group_name        TEXT NOT NULL,"
+    "PRIMARY KEY (agent_id, group_name),"
+    "FOREIGN KEY (agent_id) REFERENCES agent_metadata(agent_id) ON DELETE CASCADE);";
 
 AgentInfoImpl::AgentInfoImpl(std::string dbPath,
                              std::shared_ptr<IDBSync> dbSync)
@@ -36,16 +33,24 @@ AgentInfoImpl::AgentInfoImpl(std::string dbPath,
                    GetCreateStatement(),
                    DbManagement::PERSISTENT))
 {
+    LoggingHelper::getInstance().log(LOG_INFO, "AgentInfo initialized.");
 }
 
 AgentInfoImpl::~AgentInfoImpl()
 {
     stop();
+    LoggingHelper::getInstance().log(LOG_INFO, "AgentInfo destroyed.");
 }
 
-void AgentInfoImpl::start() {}
+void AgentInfoImpl::start()
+{
+    LoggingHelper::getInstance().log(LOG_INFO, "AgentInfo module started.");
+}
 
-void AgentInfoImpl::stop() {}
+void AgentInfoImpl::stop()
+{
+    LoggingHelper::getInstance().log(LOG_INFO, "AgentInfo module stopped.");
+}
 
 std::string AgentInfoImpl::GetCreateStatement() const
 {
--- src/wazuh_modules/agent_info/include/agent_info.h
+++ src/wazuh_modules/agent_info/include/agent_info.h
@@ -19,12 +19,18 @@ extern "C"
 {
 #endif
 
+#include "logging_helper.h"
+
     // Forward declarations
     struct wm_agent_info_t;
+
+    typedef void (*log_callback_t)(const modules_log_level_t level, const char* log, const char* tag);
 
     EXPORTED void agent_info_start(const struct wm_agent_info_t* agent_info_config);
 
     EXPORTED void agent_info_stop();
+
+    EXPORTED void agent_info_set_log_function(log_callback_t log_callback);
 
 #ifdef __cplusplus
 }
@@ -32,5 +38,6 @@ extern "C"
 
 typedef void (*agent_info_start_func)(const struct wm_agent_info_t* agent_info_config);
 typedef void (*agent_info_stop_func)();
+typedef void (*agent_info_set_log_function_func)(log_callback_t log_callback);
 
 #endif //_AGENT_INFO_H
