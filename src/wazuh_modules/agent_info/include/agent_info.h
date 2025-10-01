c3d04fe5f710f7122e8f2bac69efb7c4cc63fee0
jr0me <13784077+jr0me@users.noreply.github.com>
2025-10-01 10:13:39 -0300

    Add files for new agent info module

---
+++ src/wazuh_modules/agent_info/include/agent_info.h
@@ -0,0 +1,36 @@
+#ifndef _AGENT_INFO_H
+#define _AGENT_INFO_H
+
+// Define EXPORTED for any platform
+#ifdef _WIN32
+#ifdef WIN_EXPORT
+#define EXPORTED __declspec(dllexport)
+#else
+#define EXPORTED __declspec(dllimport)
+#endif
+#elif __GNUC__ >= 4
+#define EXPORTED __attribute__((visibility("default")))
+#else
+#define EXPORTED
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    // Forward declarations
+    struct wm_agent_info_t;
+
+    EXPORTED void agent_info_start(const struct wm_agent_info_t* agent_info_config);
+
+    EXPORTED void agent_info_stop();
+
+#ifdef __cplusplus
+}
+#endif
+
+typedef void (*agent_info_start_func)(const struct wm_agent_info_t* agent_info_config);
+typedef void (*agent_info_stop_func)();
+
+#endif //_AGENT_INFO_H
