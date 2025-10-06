3209f82e484b00939618b83e764896add1d2005e
Tomas Turina <tomas.turina@wazuh.com>
2025-10-06 21:23:17 +0000

    fix: remove deprecated getRpmInfoLegacy related code

--- src/data_provider/src/packages/packageLinuxDataRetriever.h
+++ src/data_provider/src/packages/packageLinuxDataRetriever.h
@@ -39,12 +39,6 @@ void getRpmInfo(std::function<void(nlohmann::json&)> callback);
 void getRpmPythonPackages(std::unordered_set<std::string>& pythonPackages);
 
 /**
- * @brief Fills a JSON object with all available rpm-related information for legacy Linux.
- * @param callback Callback to be called for every single element being found
- */
-void getRpmInfoLegacy(std::function<void(nlohmann::json&)> callback);
-
-/**
  * @brief Fills a JSON object with all available dpkg-related information
  * @param libPath  Path to dpkg's database directory
  * @param callback Callback to be called for every single element being found
@@ -64,30 +58,14 @@ void getDpkgPythonPackages(std::unordered_set<std::string>& pythonPackages);
  */
 void getApkInfo(const std::string& libPath, std::function<void(nlohmann::json&)> callback);
 
-
 /**
  * @brief Fills a JSON object with all available snap-related information
  * @param callback Callback to be called for every single element being found
  */
 void getSnapInfo(std::function<void(nlohmann::json&)> callback);
 
-// Exception template
-template <LinuxType linuxType>
+// Extract package information in fully compatible Linux systems
 class FactoryPackagesCreator final
-{
-    public:
-        static void getPackages(std::function<void(nlohmann::json&)> /*callback*/)
-        {
-            throw std::runtime_error
-            {
-                "Error creating package data retriever."
-            };
-        }
-};
-
-// Standard template to extract package information in fully compatible Linux systems
-template <>
-class FactoryPackagesCreator<LinuxType::STANDARD> final
 {
     public:
         static void getPackages(std::function<void(nlohmann::json&)> callback)
@@ -134,23 +112,6 @@ class FactoryPackagesCreator<LinuxType::STANDARD> final
                 getRpmPythonPackages(pythonPackages);
             }
         }
-};
-
-// Template to extract package information in partially incompatible Linux systems
-template <>
-class FactoryPackagesCreator<LinuxType::LEGACY> final
-{
-    public:
-        static void getPackages(std::function<void(nlohmann::json&)> callback)
-        {
-            const file_system::FileSystemWrapper fs;
-
-            if (fs.is_directory(RPM_PATH))
-            {
-                getRpmInfoLegacy(callback);
-            }
-        }
-        static void getPythonPackages(std::unordered_set<std::string>&) {};
 };
 
 #endif // _PACKAGE_LINUX_DATA_RETRIEVER_H
