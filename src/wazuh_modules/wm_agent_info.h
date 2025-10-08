5e2b2434b135381aa85b4bb7fe8a5070cc0d1e4b
jr0me <13784077+jr0me@users.noreply.github.com>
2025-10-08 11:31:22 -0300

    feat: add synchronization functions and header for agent-info module

--- src/wazuh_modules/wm_agent_info.h
+++ src/wazuh_modules/wm_agent_info.h
@@ -32,6 +32,8 @@ extern const wm_context WM_AGENT_INFO_CONTEXT;
 void* wm_agent_info_main(wm_agent_info_t* agent_info);
 void wm_agent_info_destroy(wm_agent_info_t* agent_info);
 cJSON* wm_agent_info_dump(const wm_agent_info_t* agent_info);
+void wm_agent_info_stop();
+int wm_agent_info_sync_message(const char* command, size_t command_len);
 
 // Configuration reading function
 int wm_agent_info_read(const OS_XML* xml, xml_node** nodes, wmodule* module);
