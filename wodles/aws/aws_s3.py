256d10d02f7ec2c7088ac0752bd58967b65b16fe
javier <javier.sanchez@wazuh.com>
2025-10-13 13:02:40 +0200

    Remove spaces

--- wodles/aws/aws_s3.py
+++ wodles/aws/aws_s3.py
@@ -177,7 +177,7 @@ def main(argv):
                 external_id=options.external_id,
                 iam_role_arn=options.iam_role_arn,
                 iam_role_duration=options.iam_role_duration,
-                profile = options.aws_profile,
+                profile=options.aws_profile,
                 sts_endpoint=options.sts_endpoint,
                 service_endpoint=options.service_endpoint,
                 name=options.queue,
