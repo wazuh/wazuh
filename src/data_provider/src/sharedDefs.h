967872557ef80001391f08ec6574127f45b5a6ee
Luis Enrique Chico Capistrano <luis.chico@wazuh.com>
2025-10-28 20:25:46 -0300

    fix: Remove duplicate packages caused by path overlap in Library/Python

--- src/data_provider/src/sharedDefs.h
+++ src/data_provider/src/sharedDefs.h
@@ -113,7 +113,6 @@ static const std::set<std::string> UNIX_PYPI_DEFAULT_BASE_DIRS
     "/home/*/.local/lib/python*/*-packages",
     "/root/.local/lib/python*/*-packages",
     "/opt/homebrew/lib",
-    "/Library/Python",
     "/Library/Frameworks/Python.framework/Versions/*/lib/python*/*-packages",
     "/root/.pyenv/versions/*/lib/python*/*-packages",
     "/home/*/.pyenv/versions/*/lib/python*/*-packages"
