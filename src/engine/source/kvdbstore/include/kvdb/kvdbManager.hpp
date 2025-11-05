ae2eec4654e8528d37f02f8b052dfb5e2a146999
Matias Garcia <matias.garcia@wazuh.com>
2025-11-05 20:44:53 +0000

    implement static kvdb

--- src/engine/source/kvdbstore/include/kvdb/kvdbManager.hpp
+++ src/engine/source/kvdbstore/include/kvdb/kvdbManager.hpp
@@ -2,17 +2,42 @@
 #ifndef _KVDB_STORE_MANAGER_H
 #define _KVDB_STORE_MANAGER_H
 
+#include <memory>
+#include <shared_mutex>
+#include <string>
+#include <unordered_map>
+
+#include <cmstore/icmstore.hpp>
+#include <cmstore/types.hpp>
+
 #include <kvdb/ikvdbhandler.hpp>
 #include <kvdb/ikvdbmanager.hpp>
+#include <kvdb/kvdbHandler.hpp>
 
 namespace kvdbStore
 {
+
+using DBMap = std::unordered_map<std::string /*dbName*/, std::weak_ptr<const KVMap>>;
+using Registry = std::unordered_map<cm::store::NamespaceId, DBMap>;
 
 class KVDBManager final : public IKVDBManager
 {
 public:
     KVDBManager() = default;
     ~KVDBManager() = default;
+
+    // Non-copyable / non-movable
+    KVDBManager(const KVDBManager&) = delete;
+    KVDBManager& operator=(const KVDBManager&) = delete;
+    KVDBManager(KVDBManager&&) = delete;
+    KVDBManager& operator=(KVDBManager&&) = delete;
+
+    std::shared_ptr<IKVDBHandler> getKVDBHandler(const cm::store::ICMStoreNSReader& nsReader,
+                                                 const std::string& dbName) override;
+
+private:
+    std::shared_mutex m_mutex;
+    Registry m_registry;
 };
 
 } // namespace kvdbStore
