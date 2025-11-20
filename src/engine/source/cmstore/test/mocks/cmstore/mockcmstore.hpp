71249633091e484bd1eb082b012a7cc3658e8c3e
Matias Garcia <matias.garcia@wazuh.com>
2025-11-20 19:01:18 +0000

    feat: add cmcrud interface

--- src/engine/source/cmstore/test/mocks/cmstore/mockcmstore.hpp
+++ src/engine/source/cmstore/test/mocks/cmstore/mockcmstore.hpp
@@ -12,8 +12,15 @@ class MockICMStoreNSReader : public ICMStoreNSReader
 {
 public:
     MOCK_METHOD(const NamespaceId&, getNamespaceId, (), (const, override));
-    MOCK_METHOD((std::vector<std::tuple<std::string, std::string>>), getCollection, (ResourceType type), (const, override));
-    MOCK_METHOD((std::tuple<std::string, ResourceType>), resolveNameFromUUID, (const std::string& uuid), (const, override));
+    MOCK_METHOD((std::vector<std::tuple<std::string, std::string>>),
+                getCollection,
+                (ResourceType type),
+                (const, override));
+    MOCK_METHOD((std::tuple<std::string, ResourceType>),
+                resolveNameFromUUID,
+                (const std::string& uuid),
+                (const, override));
+    MOCK_METHOD(std::string, resolveHashFromUUID, (const std::string& uuid), (const, override));
     MOCK_METHOD(std::string, resolveUUIDFromName, (const std::string& name, ResourceType type), (const, override));
     MOCK_METHOD(bool, assetExistsByName, (const base::Name& name), (const, override));
     MOCK_METHOD(bool, assetExistsByUUID, (const std::string& uuid), (const, override));
@@ -31,8 +38,15 @@ class MockICMstoreNS : public ICMstoreNS
 public:
     // ICMStoreNSReader methods
     MOCK_METHOD(const NamespaceId&, getNamespaceId, (), (const, override));
-    MOCK_METHOD((std::vector<std::tuple<std::string, std::string>>), getCollection, (ResourceType type), (const, override));
-    MOCK_METHOD((std::tuple<std::string, ResourceType>), resolveNameFromUUID, (const std::string& uuid), (const, override));
+    MOCK_METHOD((std::vector<std::tuple<std::string, std::string>>),
+                getCollection,
+                (ResourceType type),
+                (const, override));
+    MOCK_METHOD((std::tuple<std::string, ResourceType>),
+                resolveNameFromUUID,
+                (const std::string& uuid),
+                (const, override));
+    MOCK_METHOD(std::string, resolveHashFromUUID, (const std::string& uuid), (const, override));
     MOCK_METHOD(std::string, resolveUUIDFromName, (const std::string& name, ResourceType type), (const, override));
     MOCK_METHOD(bool, assetExistsByName, (const base::Name& name), (const, override));
     MOCK_METHOD(bool, assetExistsByUUID, (const std::string& uuid), (const, override));
@@ -45,8 +59,14 @@ public:
     MOCK_METHOD(json::Json, getAssetByUUID, (const std::string& uuid), (const, override));
 
     // ICMstoreNS methods
-    MOCK_METHOD(std::string, createResource, (const std::string& name, ResourceType type, const std::string& ymlContent), (override));
-    MOCK_METHOD(void, updateResourceByName, (const std::string& name, ResourceType type, const std::string& ymlContent), (override));
+    MOCK_METHOD(std::string,
+                createResource,
+                (const std::string& name, ResourceType type, const std::string& ymlContent),
+                (override));
+    MOCK_METHOD(void,
+                updateResourceByName,
+                (const std::string& name, ResourceType type, const std::string& ymlContent),
+                (override));
     MOCK_METHOD(void, updateResourceByUUID, (const std::string& uuid, const std::string& ymlContent), (override));
     MOCK_METHOD(void, deleteResourceByName, (const std::string& name, ResourceType type), (override));
     MOCK_METHOD(void, deleteResourceByUUID, (const std::string& uuid), (override));
@@ -57,14 +77,12 @@ public:
 class MockICMstore : public ICMStore
 {
 public:
-
     MOCK_METHOD(std::shared_ptr<ICMStoreNSReader>, getNSReader, (const NamespaceId& nsId), (const, override));
     MOCK_METHOD(std::shared_ptr<ICMstoreNS>, getNS, (const NamespaceId& nsId), (override));
 
     MOCK_METHOD(std::shared_ptr<ICMstoreNS>, createNamespace, (const NamespaceId& nsId), (override));
     MOCK_METHOD(void, deleteNamespace, (const NamespaceId& nsId), (override));
     MOCK_METHOD(std::vector<NamespaceId>, getNamespaces, (), (const, override));
-
 };
 
 } // namespace cm::store
