abc1700c856f8615b31dd185ab7a993c7e3638ce
jr0me <13784077+jr0me@users.noreply.github.com>
2025-11-26 23:12:35 -0300

    feat: expose SCA integrity interval configuration

--- src/wazuh_modules/sca/include/sca.h
+++ src/wazuh_modules/sca/include/sca.h
@@ -62,6 +62,8 @@ EXPORTED void sca_set_push_functions(push_stateless_func stateless_func, push_st
 EXPORTED void sca_set_sync_parameters(const char* module_name, const char* sync_db_path, const MQ_Functions* mq_funcs, unsigned int sync_end_delay, unsigned int timeout, unsigned int retries,
                                       size_t maxEps);
 
+EXPORTED void sca_set_integrity_interval(unsigned int integrity_interval);
+
 // Sync protocol C wrapper functions
 EXPORTED bool sca_sync_module(Mode_t mode);
 EXPORTED void sca_persist_diff(const char* id, Operation_t operation, const char* index, const char* data, uint64_t version);
@@ -94,6 +96,8 @@ typedef void (*sca_set_push_functions_func)(push_stateless_func stateless_func, 
 
 typedef void (*sca_set_sync_parameters_func)(const char* module_name, const char* sync_db_path, const MQ_Functions* mq_funcs, unsigned int sync_end_delay, unsigned int timeout,
                                              unsigned int retries, size_t maxEps);
+
+typedef void (*sca_set_integrity_interval_func)(unsigned int integrity_interval);
 
 // Sync protocol C wrapper functions
 typedef bool(*sca_sync_module_func)(Mode_t mode);
