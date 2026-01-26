ef3ae58c5898ca1c1b83d071876b779e878b1e90
jnasselle <jnasselle@gmail.com>
2026-01-26 16:11:00 -0300

    Improve Python local generation scripts

--- framework/cpython/compile.sh
+++ framework/cpython/compile.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+set -euo pipefail
+
+WAZUH_HOST_DIR=/wazuh_host
 WAZUH_ROOT_DIR=/wazuh #Important: Do not change this path
 WAZUH_INSTALLDIR=/var/ossec
 CPYTHON_DIR=$WAZUH_ROOT_DIR/src/external/cpython
-PYTHON_VERSION=$(cat "$WAZUH_ROOT_DIR/framework/.python-version")
-PIP_VERSION=$(cat "$WAZUH_ROOT_DIR/framework/.pip-version" 2>/dev/null)
+OUTPUT_DIR=/output
 
 main() {
     # Parse script arguments
     parse_args "$@" || exit 1
+    # Get wazuh repository
+    get_wazuh_repo
     # Download wazuh precompiled dependencies
     make -C "$WAZUH_ROOT_DIR/src" PYTHON_SOURCE=y deps -j
+
+    PYTHON_VERSION=$(cat $WAZUH_ROOT_DIR/framework/.python-version)
+
     if $BUILD_CPYTHON; then
         # Build CPython from sources
         rm -rf "$CPYTHON_DIR"
         download_cpython
         customize_cpython
-        # Replace the default pip version if specified
-        if [ -n "$PIP_VERSION" ]; then
-            replace_default_pip_version $PIP_VERSION
-        fi
         build_cpython
     fi
 
-    if $BUILD_DEPS; then
-        # Build wheels in arg flag was set or python was built
+    if $BUILD_DEPS || $BUILD_CPYTHON; then
         download_wheels
     fi
 
@@ -32,49 +34,57 @@ main() {
     generate_artifacts
 }
 
-generate_artifacts() {
-    # Compress built cpython
-    cd $WAZUH_ROOT_DIR/src/external && tar -zcf cpython_$ARCH.tar.gz --owner=0 --group=0 cpython
-    # Compress ready-to-use CPython
-    cd /var/ossec/framework/python && tar -zcf cpython.tar.gz --owner=0 --group=0 .
+get_wazuh_repo() {
+    if [ -z "${WAZUH_BRANCH:-}" ]; then
+        cp -rf $WAZUH_HOST_DIR $WAZUH_ROOT_DIR
+        # Clean previous builds
+        rm -rf $WAZUH_ROOT_DIR/src/external/*
+        make clean -j -C "$WAZUH_ROOT_DIR/src"
+    else
+        git clone --branch "$WAZUH_BRANCH" --depth 1 https://github.com/wazuh/wazuh.git  "$WAZUH_ROOT_DIR"
+    fi
 }
+
 download_cpython() {
     git clone --branch "v$PYTHON_VERSION" --depth 1 https://github.com/python/cpython.git "$CPYTHON_DIR"
 }
 
-replace_default_pip_version() {
-    local pip_version=$1
-    # Replace pip version in ensurepip
-    sed -i "s/^_PIP_VERSION = .*/_PIP_VERSION = \"$pip_version\"/" "$CPYTHON_DIR/Lib/ensurepip/__init__.py"
-    # Remove existing pip wheel
-    rm -f "$CPYTHON_DIR/Lib/ensurepip/_bundled/pip-"*.whl
-    # Download specified pip wheel
-    wget -O "$CPYTHON_DIR/Lib/ensurepip/_bundled/pip-$pip_version-py3-none-any.whl" "https://files.pythonhosted.org/packages/py3/p/pip/pip-$pip_version-py3-none-any.whl"
-}
-
 customize_cpython() {
-    # Apply customizations
-    # WIP
+    cp -f $WAZUH_ROOT_DIR/framework/cpython/custom/Setup.local $CPYTHON_DIR/Modules
+    cp -f $WAZUH_ROOT_DIR/framework/cpython/custom/Setup.stdlib.in $CPYTHON_DIR/Modules
 }
 
 build_cpython() {
-    make -j -C "$WAZUH_ROOT_DIR/src" external INSTALLDIR=$WAZUH_INSTALLDIR OPTIMIZE_CPYTHON=yes
-    make -j -C "$WAZUH_ROOT_DIR/src" build_python INSTALLDIR=$WAZUH_INSTALLDIR
+    make -j -C "$WAZUH_ROOT_DIR/src" build_python INSTALLDIR=$WAZUH_INSTALLDIR OPTIMIZE_CPYTHON=yes
 }
 
 mimic_full_wazuh_installation() {
+    # Force build of libwazuhext
+    make -j -C "$WAZUH_ROOT_DIR/src" external INSTALLDIR=$WAZUH_INSTALLDIR
     # Install only libwazuhext to avoid full server compilation & installation
-    mkdir -p $WAZUH_INSTALLDIR/lib
-    install -m 0750 -o root -g wazuh libwazuhext.so $WAZUH_INSTALLDIR/lib
+    mkdir -p "$WAZUH_INSTALLDIR/lib"
+    install -m 0750 $WAZUH_ROOT_DIR/src/libwazuhext.so "$WAZUH_INSTALLDIR/lib"
     # Install python interpreter and its dependencies
-    make install_dependencies -j INSTALLDIR=$WAZUH_INSTALLDIR
+    make -j -C "$WAZUH_ROOT_DIR/src" install_dependencies INSTALLDIR=$WAZUH_INSTALLDIR
+}
+
+generate_artifacts() {
+    # Compress built cpython
+    cd $WAZUH_ROOT_DIR/src/external && tar -zcf "$OUTPUT_DIR/cpython_$ARCH.tar.gz" --owner=0 --group=0 cpython
+    # Compress ready-to-use CPython
+    cd $WAZUH_INSTALLDIR/framework/python && tar -zcf "$OUTPUT_DIR/cpython.tar.gz" --owner=0 --group=0 .
 }
 
 download_wheels() {
+    # Install Python3 to download wheels
+    yum install python3 -y
+    python3 -m pip install --upgrade pip
+    # Remove existing dependencies
+    rm -rf "$CPYTHON_DIR/Dependencies"
     # Create dependencies directory
     mkdir -p "$CPYTHON_DIR/Dependencies"
     # Download wheels
-    python3 -m pip download --requirement "$WAZUH_ROOT_DIR/framework/requirements.txt" --only-binary=:all: --dest "$CPYTHON_DIR/Dependencies"  --python-version "$PYTHON_VERSION" --no-cache-dir
+    python3 -m pip download --requirement "$WAZUH_ROOT_DIR/framework/requirements.txt"  --no-deps --dest "$CPYTHON_DIR/Dependencies"  --python-version "$PYTHON_VERSION" --no-cache-dir
     # Create index
     python3 -m pip install piprepo && piprepo build "$CPYTHON_DIR/Dependencies"
 }
@@ -91,8 +101,11 @@ parse_args() {
             --build-deps)
                 BUILD_DEPS=true
                 ;;
+            --wazuh-branch)
+                WAZUH_BRANCH="$2"
+                ;;
             *)
-                echo "Error: parámetro no reconocido: $1" >&2
+                echo "ERROR: Unrecognized parameter: $1" >&2
                 return 1
                 ;;
         esac
--- framework/cpython/generate-cpython.sh
+++ framework/cpython/generate-cpython.sh
@@ -1,53 +1,90 @@
 #!/bin/bash
 
-env_file="./config.env"
-current_arch=$(uname -m)
-wazuh_version=$(jq -r '.version' ../../VERSION.json)
+output_dir="./output"
+wazuh_version=$(jq -r '.version' "$(dirname "${BASH_SOURCE[0]}")/../../VERSION.json")
 wazuh_root_dir=$(dirname "${BASH_SOURCE[0]}")/../../
 
-if [ ! -f "$env_file" ]; then
-  echo "Error: environment file not found: $env_file" >&2
-  exit 1
-fi
+main() {
 
-source $env_file
+    parse_args "$@" || exit 1
+    pull_builder_image
+    run_compilation
+}
 
-required_vars=(
-  "GITHUB_USER"
-  "GHCR_TOKEN"
-)
+run_compilation() {
 
-missing_vars=()
+  mkdir -p $output_dir
+  docker run \
+    --env ARCH="$architecture" \
+    -it -v $wazuh_root_dir:/wazuh_host -v $output_dir:/output \
+    --entrypoint=bash $image /wazuh_host/framework/cpython/compile.sh $compile_parameters
+}
 
-for var in "${required_vars[@]}"; do
-  if [ -z "${!var}" ]; then
-    missing_vars+=("$var")
+pull_builder_image() {
+
+  current_arch=$(uname -m)
+  echo $GHCR_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
+  # Set architecture and image variables
+  if [ "$current_arch" = "arm64" ] || [ "$current_arch" = "aarch64" ]; then
+      architecture="arm64"
+      image="ghcr.io/wazuh/pkg_rpm_manager_builder_arm64:$wazuh_version"
+  elif [ "$current_arch" = "amd64" ] || [ "$current_arch" = "x86_64" ]; then
+      architecture="x86_64"
+      image="ghcr.io/wazuh/pkg_rpm_manager_builder_amd64:$wazuh_version"
+  else
+      echo "Unsupported architecture ($current_arch)" >&2
+      exit 1
   fi
-done
+  docker pull $image
+}
 
-if [ "${#missing_vars[@]}" -ne 0 ]; then
-  echo "Error: the following environment variables are missing:"
-  for var in "${missing_vars[@]}"; do
-    echo "   - $var"
+parse_args() {
+
+  env_file="./config.env"
+  if [ -f "$env_file" ]; then
+    source $env_file
+    echo "Using environment variables from $env_file"
+  else
+    echo "WARNING: $env_file not found. Proceeding without it."
+  fi
+
+  required_vars=(
+    "GITHUB_USER"
+    "GHCR_TOKEN"
+  )
+
+  missing_vars=()
+
+  for var in "${required_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+      missing_vars+=("$var")
+    fi
   done
-  exit 1
-fi
 
-echo $GHCR_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
-
-
-if [ "$current_arch" = "arm64" ] || [ "$current_arch" = "aarch64" ]; then
-    architecture="arm64"
-    docker pull "ghcr.io/wazuh/pkg_rpm_manager_builder_arm64:$wazuh_version"
-elif [ "$current_arch" = "amd64" ] || [ "$current_arch" = "x86_64" ]; then
-    architecture="x86_64"
-    docker pull "ghcr.io/wazuh/pkg_rpm_manager_builder_amd64:$wazuh_version"
-else
-    echo "Unsupported architecture ($current_arch)" >&2
+  if [ "${#missing_vars[@]}" -ne 0 ]; then
+    echo "Error: the following environment variables are missing:"
+    for var in "${missing_vars[@]}"; do
+      echo "   - $var"
+    done
     exit 1
-fi
+  fi
 
-docker run \
-  --env ARCH="$architecture" \
-  -it -v $wazuh_root_dir:/wazuh \
-  --entrypoint=bash ghcr.io/wazuh/pkg_rpm_manager_builder_$architecture:$wazuh_version /wazuh/framework/cpython/compile.sh --build-cpython --build-deps
+  if [ -z "${WAZUH_BRANCH:-}" ]; then
+    echo "No Wazuh branch specified. Using local source code (no modifications will be applied to it)."
+  else
+    echo "Wazuh branch specified: $WAZUH_BRANCH"
+    compile_parameters+=" --wazuh-branch $WAZUH_BRANCH"
+  fi
+
+  if [ "${BUILD_CPYTHON:-false}" = "true" ]; then
+    echo "CPython build enabled."
+    compile_parameters+=" --build-cpython"
+  fi
+
+  if [ "${BUILD_DEPS:-false}" = "true" ]; then
+    echo "Dependencies build enabled."
+    compile_parameters+=" --build-deps"
+  fi
+}
+
+main "$@"
