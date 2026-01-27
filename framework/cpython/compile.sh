b0896c11cffb2a783f9376c8208f125bf03982eb
jnasselle <jnasselle@gmail.com>
2026-01-27 15:21:47 -0300

    Add script headers

--- framework/cpython/compile.sh
+++ framework/cpython/compile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (C) 2015, Wazuh Inc.
+#
+# This program is a free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation.
 
 set -euo pipefail
 
