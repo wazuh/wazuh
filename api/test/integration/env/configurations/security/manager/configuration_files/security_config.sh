09a9c017137e36d0757d12948c03e32b181779dd
Juan Nicolas Asselle <jnasselle@gmail.com>
2026-02-06 09:41:09 -0300

    feat: Rename wazuh user/group to wazuh-manager at API/FW (#34308)

--- api/test/integration/env/configurations/security/manager/configuration_files/security_config.sh
+++ api/test/integration/env/configurations/security/manager/configuration_files/security_config.sh
@@ -3,5 +3,5 @@
 # RBAC configuration
 sqlite3 /var/ossec/api/configuration/security/rbac.db < /tmp_volume/configuration_files/schema_security_test.sql
 sqlite3 /var/ossec/api/configuration/security/rbac.db < /tmp_volume/configuration_files/base_security_test.sql
-chown wazuh:wazuh /var/ossec/api/configuration/security/rbac.db
+chown wazuh-manager:wazuh-manager /var/ossec/api/configuration/security/rbac.db
 chmod 640 /var/ossec/api/configuration/security/rbac.db
