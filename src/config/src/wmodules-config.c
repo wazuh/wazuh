eb1980798ef7bb3f6ace390d9efe81aebd17d034
Antonio Garcia <antonio.garcia@wazuh.com>
2026-02-11 11:37:38 +0100

    fix: reverting osquery comments

--- src/config/src/wmodules-config.c
+++ src/config/src/wmodules-config.c
@@ -71,7 +71,10 @@ int Read_WModule(const OS_XML *xml, xml_node *node, void *d1, void *d2)
     if (children = OS_GetElementsbyNode(xml, node), !children) {
         mdebug1("Empty configuration for module '%s'", node->values[0]);
     }
+    
+    // Select module by name
 
+    //osQuery monitor module
     if (!strcmp(node->values[0], "osquery")) {
         mwarn("Deprecated module 'osquery' is no longer available.");
     }
