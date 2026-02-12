4231e9068f7d942879bef949830010903b49ddc1
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2026-02-12 17:24:36 +0100

    test(logcollector): add unit tests for send_channel_event XML handling

--- src/unit_tests/logcollector/test_read_win_event_channel.c
+++ src/unit_tests/logcollector/test_read_win_event_channel.c
@@ -81,10 +81,50 @@ void test_send_channel_event_render_buffer_size_fail(void ** state) {
     send_channel_event(evt, channel);
 }
 
+void test_send_channel_event_success(void ** state) {
+    os_channel *channel = (os_channel *)*state;
+    EVT_HANDLE evt = (EVT_HANDLE)1;
+    WCHAR xml_wide[] = L"<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'><System><Provider Name='Test'/></System></Event>";
+
+    /* First EvtRender call to get buffer size */
+    expect_value(wrap_EvtRender, Context, NULL);
+    expect_value(wrap_EvtRender, Fragment, evt);
+    expect_value(wrap_EvtRender, Flags, EvtRenderEventXml);
+    expect_value(wrap_EvtRender, BufferSize, 0);
+    will_return(wrap_EvtRender, NULL);
+    will_return(wrap_EvtRender, (wcslen(xml_wide) + 1) * sizeof(WCHAR));
+    will_return(wrap_EvtRender, 0);
+    will_return(wrap_EvtRender, FALSE);
+    will_return(wrap_GetLastError, ERROR_INSUFFICIENT_BUFFER);
+
+    /* Second EvtRender call to get actual data */
+    expect_value(wrap_EvtRender, Context, NULL);
+    expect_value(wrap_EvtRender, Fragment, evt);
+    expect_value(wrap_EvtRender, Flags, EvtRenderEventXml);
+    expect_value(wrap_EvtRender, BufferSize, (wcslen(xml_wide) + 1) * sizeof(WCHAR));
+    will_return(wrap_EvtRender, xml_wide);
+    will_return(wrap_EvtRender, (wcslen(xml_wide) + 1) * sizeof(WCHAR));
+    will_return(wrap_EvtRender, 0);
+    will_return(wrap_EvtRender, TRUE);
+
+    /* convert_windows_string mock */
+    expect_any(__wrap_convert_windows_string, string);
+    will_return(__wrap_convert_windows_string, strdup("<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'><System><Provider Name='Test'/></System></Event>"));
+
+    /* SendMSG mock - this is what we want to verify */
+    expect_string(__wrap_SendMSG, message, "<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'><System><Provider Name='Test'/></System></Event>");
+    expect_string(__wrap_SendMSG, locmsg, "EventChannel");
+    expect_value(__wrap_SendMSG, loc, WIN_EVT_MQ);
+    will_return(__wrap_SendMSG, 0);
+
+    send_channel_event(evt, channel);
+}
+
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test_setup_teardown(test_send_channel_event_render_buffer_size_fail, test_setup, test_teardown)
+        cmocka_unit_test_setup_teardown(test_send_channel_event_render_buffer_size_fail, test_setup, test_teardown),
+        cmocka_unit_test_setup_teardown(test_send_channel_event_success, test_setup, test_teardown)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
