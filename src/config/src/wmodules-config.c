b301e79a14e6ad834f03a053e5c66e4c3eb91243
Antonio Garcia <antonio.garcia@wazuh.com>
2026-02-16 17:32:12 +0100

    feat: remove sca and syscollector from manager

--- src/config/src/wmodules-config.c
+++ src/config/src/wmodules-config.c
@@ -81,19 +81,16 @@ int Read_WModule(const OS_XML *xml, xml_node *node, void *d1, void *d2)
     else if (!strcmp(node->values[0], "open-scap")) {
         mwarn("Deprecated module 'open-scap' is no longer available.");
     }
-#ifdef CLIENT
     else if (!strcmp(node->values[0], WM_SYS_CONTEXT.name)) {
+#ifdef CLIENT
         if (wm_syscollector_read(xml, children, cur_wmodule) < 0) {
             OS_ClearNode(children);
             return OS_INVALID;
         }
-        mdebug1("Syscollector scan is disabled for the manager. Skipping.");
-    }
 #else
-    else if (!strcmp(node->values[0], WM_SYS_CONTEXT.name)) {
         mwarn("The '%s' module only works for the agent", node->values[0]);
-    }
 #endif
+    }
 #ifdef CLIENT
     else if (!strcmp(node->values[0], WM_COMMAND_CONTEXT.name)) {
         if (wm_command_read(children, cur_wmodule, agent_cfg) < 0) {
@@ -225,7 +222,6 @@ int Read_SCA(const OS_XML *xml, xml_node *node, void *d1)
         mwarn("The '%s' module only works for the agent", node->element);
 #endif
     }
-
     OS_ClearNode(children);
     return 0;
 }
