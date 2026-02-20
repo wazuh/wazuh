24310fb702318be1b3431fee3b491551f7696559
Fabio <fabio.contreras@wazuh.com>
2026-02-20 12:33:32 -0500

    fix: Added an small delay between creating the file for the action and the action itself, also an small delay from when realtime mode init

--- tests/integration/test_fim/test_files/test_basic_usage/conftest.py
+++ tests/integration/test_fim/test_files/test_basic_usage/conftest.py
@@ -1,12 +1,15 @@
 # Copyright (C) 2015-2024, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+import sys
+import time
 import pytest
 
 from pathlib import Path
 
 from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH
-from wazuh_testing.modules.fim.patterns import EVENT_TYPE_ADDED
+from wazuh_testing.constants.platforms import WINDOWS
+from wazuh_testing.modules.fim.patterns import EVENT_TYPE_ADDED, PATH_MONITORED_REALTIME
 from wazuh_testing.tools.monitors.file_monitor import FileMonitor
 from wazuh_testing.utils import file
 from wazuh_testing.utils.callbacks import generate_callback
@@ -17,8 +20,17 @@ def path_to_edit(test_metadata: dict) -> str:
     to_edit = test_metadata.get('path_to_edit')
     is_directory = test_metadata.get('is_directory')
 
+    fim_mode = test_metadata.get('fim_mode', '')
+    if sys.platform == WINDOWS and fim_mode == 'realtime':
+        FileMonitor(WAZUH_LOG_PATH).start(
+            callback=generate_callback(PATH_MONITORED_REALTIME),
+            timeout=60
+        )
+
     if is_directory:
         file.recursive_directory_creation(to_edit)
+        if sys.platform == WINDOWS:
+            time.sleep(5)
         file.write_file(Path(to_edit, 'newfile'), 'test')
     else:
         file.write_file(to_edit, 'test')
