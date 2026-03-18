350500cff8cf5583cb0199ba3556da30f590b0a7
Jose Luis Carreras Marin <jose.carreras@wazuh.com>
2026-03-18 11:22:41 +0100

    refactor(api): remove deprecated agent sync endpoint

--- api/api/controllers/agent_controller.py
+++ api/api/controllers/agent_controller.py
@@ -14,7 +14,7 @@ from api.models.agent_added_model import AgentAddedModel
 from api.models.agent_group_added_model import GroupAddedModel
 from api.models.agent_inserted_model import AgentInsertedModel
 from api.models.base_model_ import Body
-from api.util import deprecate_endpoint, parse_api_param, raise_if_exc, remove_nones_to_dict
+from api.util import parse_api_param, raise_if_exc, remove_nones_to_dict
 from api.validator import check_component_configuration_pair
 from wazuh import agent, stats
 from wazuh.core.cluster.control import get_system_nodes
