6cd03c4e57c3891bbfc417291df065382510f9dc
Jose Luis Carreras Marin <jose.carreras@wazuh.com>
2026-03-26 12:26:37 +0100

    fix(tzdb): harden tzdata fetch and stabilize hlp tests

--- src/engine/source/hlp/test/src/unit/date_test.cpp
+++ src/engine/source/hlp/test/src/unit/date_test.cpp
@@ -3,20 +3,27 @@
 #include "hlp_test.hpp"
 #include <base/logging.hpp>
 
+#ifndef HLP_TEST_TZDB_PATH
+#define HLP_TEST_TZDB_PATH "data/tzdb"
+#endif
+
 auto constexpr NAME = "dateParser";
 static const std::string TARGET = "/TargetField";
 auto BUILD_YEAR = std::string(__DATE__).substr(7);
 
 auto initAndGetDateParser() -> hlp::ParserBuilder
 {
-    logging::testInit();
-    static bool hasInitiated = false;
-    if (!hasInitiated)
+    return [](const hlp::Params& params)
     {
-        hlp::initTZDB("data/tzdb", false);
-        hasInitiated = true;
-    }
-    return hlp::parsers::getDateParser;
+        logging::testInit();
+        static bool hasInitiated = false;
+        if (!hasInitiated)
+        {
+            hlp::initTZDB(HLP_TEST_TZDB_PATH, false);
+            hasInitiated = true;
+        }
+        return hlp::parsers::getDateParser(params);
+    };
 };
 
 INSTANTIATE_TEST_SUITE_P(
