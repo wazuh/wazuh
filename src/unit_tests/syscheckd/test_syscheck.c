25e47ce16ee6e9efd166fd444b3184b0a21079d3
Antonio Garcia <antonio.garcia@wazuh.com>
2026-03-27 10:55:32 +0100

    fix(syscheckd): update unit tests to match demoted log levels

--- src/unit_tests/syscheckd/test_syscheck.c
+++ src/unit_tests/syscheckd/test_syscheck.c
@@ -385,19 +385,19 @@ void test_Start_win32_Syscheck_dirs_and_registry(void **state) {
     will_return(__wrap_rootcheck_init, 0);
 
     expect_string(__wrap__minfo, formatted_msg, "(6002): Monitoring registry entry: 'Entry1 [x64]', with options ''");
-    expect_string(__wrap__minfo, formatted_msg, "(6003): Monitoring path: 'c:\\dir1', with options ''.");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6003): Monitoring path: 'c:\\dir1', with options ''.");
 
     expect_string(__wrap__minfo, formatted_msg, FIM_FILE_SIZE_LIMIT_DISABLED);
 
     expect_string(__wrap__minfo, formatted_msg, FIM_DISK_QUOTA_LIMIT_DISABLED);
 
-    expect_string(__wrap__minfo, formatted_msg, "(6206): Ignore 'file' entry 'dir1'");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6206): Ignore 'file' entry 'dir1'");
 
-    expect_string(__wrap__minfo, formatted_msg, "(6207): Ignore 'file' sregex '^regex$'");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6207): Ignore 'file' sregex '^regex$'");
 
-    expect_string(__wrap__minfo, formatted_msg, "(6206): Ignore 'registry' entry 'Entry1'");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6206): Ignore 'registry' entry 'Entry1'");
 
-    expect_string(__wrap__minfo, formatted_msg, "(6004): No diff for file: 'Diff'");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6004): No diff for file: 'Diff'");
 
     expect_wrapper_fim_db_init(0, 100000, 100000);
 
@@ -465,7 +465,7 @@ void test_Start_win32_Syscheck_whodata_active(void **state) {
 
     will_return(__wrap_rootcheck_init, 0);
 
-    expect_string(__wrap__minfo, formatted_msg, "(6003): Monitoring path: 'c:\\dir1', with options 'whodata'.");
+    expect_string(__wrap__mdebug1, formatted_msg, "(6003): Monitoring path: 'c:\\dir1', with options 'whodata'.");
 
     expect_wrapper_fim_db_init(0, 100000, 100000);
 
