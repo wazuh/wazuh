3fd6782098c6d3b6a51303c495159037ec0d00d1
Tomas Turina <tomas.turina@wazuh.com>
2026-07-23 19:37:10 +0000

    fix: fix task manager related unit tests

--- src/unit_tests/wrappers/wazuh/wazuh_modules/wm_task_manager_wrappers.h
+++ src/unit_tests/wrappers/wazuh/wazuh_modules/wm_task_manager_wrappers.h
@@ -15,8 +15,6 @@
 
 cJSON* __wrap_wm_task_manager_parse_message(const char* msg);
 
-cJSON* __wrap_wm_task_manager_process_task(const wm_task_manager_task* task, int* error_code);
-
 cJSON* __wrap_wm_task_manager_parse_data_response(int error_code, int agent_id, int task_id, char* status);
 
 void __wrap_wm_task_manager_parse_data_result(cJSON* response,
@@ -28,5 +26,20 @@ void __wrap_wm_task_manager_parse_data_result(cJSON* response,
                                               int create_time,
                                               int last_update_time,
                                               char* request_command);
+
+void __wrap_wm_task_cache_init(int cache_ttl);
+
+int __wrap_w_create_thread(void *(*function_pointer)(void *), void *data);
+
+// Task cache functions
+cJSON* __wrap_wm_task_cache_get(const char *agent_id);
+
+void __wrap_wm_task_cache_set(const char *agent_id, cJSON *tasks);
+
+void __wrap_wm_task_cache_invalidate(const char *agent_id);
+
+// Task ID generation
+char* __wrap_wm_task_manager_generate_task_id(const char *source_id, const char *agent_id,
+                                               const char *task_type, time_t create_time);
 
 #endif
