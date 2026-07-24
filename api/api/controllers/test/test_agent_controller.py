3544f62e4559a1285ae847fe10b5782f20e8808d
Tomas Turina <tomas.turina@wazuh.com>
2026-07-24 16:15:58 +0000

    feat: integrate remote upgrade endpoints with new task manager

--- api/api/controllers/test/test_agent_controller.py
+++ api/api/controllers/test/test_agent_controller.py
@@ -21,7 +21,7 @@ with patch('wazuh.common.wazuh_uid'):
             delete_single_agent_single_group, get_agent_config,
             get_agent_fields, get_agent_key, get_agent_no_group, get_agent_outdated,
             get_agents_summary, get_agent_summary_os, get_agent_summary_status,
-            get_agent_uninstall_permission, get_agent_upgrade, get_agents, get_agents_in_group,
+            get_agent_uninstall_permission, get_agents, get_agents_in_group,
             get_daemon_stats, get_component_stats, get_group_config, get_group_file,
             get_group_files, get_list_group, insert_agent, post_group,
             post_new_agent, put_agent_single_group, put_group_config,
@@ -444,7 +444,7 @@ async def test_put_upgrade_agents(mock_exc, mock_dapi, mock_remove, mock_dfunc, 
                                       wait_for_complete=False,
                                       logger=ANY,
                                       rbac_permissions=mock_request.context['token_info']['rbac_policies'],
-                                      broadcasting=agents_list == '*'
+                                      broadcasting=True
                                       )
     mock_exc.assert_called_once_with(mock_dfunc.return_value)
     mock_remove.assert_called_once_with(f_kwargs)
@@ -495,7 +495,7 @@ async def test_put_upgrade_custom_agents(mock_exc, mock_dapi, mock_remove, mock_
                                       wait_for_complete=False,
                                       logger=ANY,
                                       rbac_permissions=mock_request.context['token_info']['rbac_policies'],
-                                      broadcasting=agents_list == '*'
+                                      broadcasting=True
                                       )
     mock_exc.assert_called_once_with(mock_dfunc.return_value)
     mock_remove.assert_called_once_with(f_kwargs)
@@ -544,47 +544,6 @@ async def test_get_component_stats(mock_exc, mock_dapi, mock_remove, mock_dfunc,
     mock_dapi.assert_called_once_with(f=stats.get_agents_component_stats_json,
                                       f_kwargs=mock_remove.return_value,
                                       request_type='distributed_master',
-                                      is_async=False,
-                                      wait_for_complete=False,
-                                      logger=ANY,
-                                      rbac_permissions=mock_request.context['token_info']['rbac_policies']
-                                      )
-    mock_exc.assert_called_once_with(mock_dfunc.return_value)
-    mock_remove.assert_called_once_with(f_kwargs)
-    assert isinstance(result, ConnexionResponse)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("mock_request", ["agent_controller"], indirect=True)
-@patch('api.configuration.api_conf')
-@patch('api.controllers.agent_controller.DistributedAPI.distribute_function', return_value=AsyncMock())
-@patch('api.controllers.agent_controller.remove_nones_to_dict')
-@patch('api.controllers.agent_controller.DistributedAPI.__init__', return_value=None)
-@patch('api.controllers.agent_controller.raise_if_exc', return_value=CustomAffectedItems())
-async def test_get_agent_upgrade(mock_exc, mock_dapi, mock_remove, mock_dfunc,
-                                 mock_exp, mock_request):
-    """Verify 'get_agent_upgrade' endpoint is working as expected."""
-    result = await get_agent_upgrade()
-    f_kwargs = {'agent_list': None,
-                'filters': {
-                    'version': None,
-                    'group': None,
-                    'node_name': None,
-                    'name': None,
-                    'ip': None,
-                    'registerIP': mock_request.query_params.get('registerIP', None)
-                },
-                'q': None
-                }
-
-    # Add nested fields to kwargs filters
-    nested = ['os.version', 'os.name', 'os.platform', 'os.type', 'os.major', 'os.minor', 'os.arch']
-    for field in nested:
-        f_kwargs['filters'][field] = mock_request.query_params.get(field, None)
-
-    mock_dapi.assert_called_once_with(f=agent.get_upgrade_result,
-                                      f_kwargs=mock_remove.return_value,
-                                      request_type='local_master',
                                       is_async=False,
                                       wait_for_complete=False,
                                       logger=ANY,
