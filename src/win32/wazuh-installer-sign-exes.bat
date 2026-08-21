7b27bdd60413a73dea7927544aaada5f15a31229
Dario Ortega Leyva <dario.ortega@wazuh.com>
2026-08-21 14:43:36 +0200

    enhancement: support azure artifact signing in msi builder

--- src/win32/wazuh-installer-sign-exes.bat
+++ src/win32/wazuh-installer-sign-exes.bat
@@ -1,20 +1,29 @@
-SETLOCAL
+SETLOCAL EnableExtensions
 SET PATH=%PATH%;C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin
 SET PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin
 
 REM Fix all .exe and .dll files
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "*.exe"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\*.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "*.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\sysinfo.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\dbsync.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\libagent_sync_protocol.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\libhttps_client.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\schema_validator.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\libagent_metadata.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\syscollector.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\sca.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\agent_info.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "..\build\bin\libfimdb.dll"
-signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "InstallerScripts.vbs"
+call :sign "*.exe"
+call :sign "..\*.dll"
+call :sign "*.dll"
+call :sign "..\build\bin\sysinfo.dll"
+call :sign "..\build\bin\dbsync.dll"
+call :sign "..\build\bin\libagent_sync_protocol.dll"
+call :sign "..\build\bin\libhttps_client.dll"
+call :sign "..\build\bin\schema_validator.dll"
+call :sign "..\build\bin\libagent_metadata.dll"
+call :sign "..\build\bin\syscollector.dll"
+call :sign "..\build\bin\sca.dll"
+call :sign "..\build\bin\agent_info.dll"
+call :sign "..\build\bin\libfimdb.dll"
+call :sign "InstallerScripts.vbs"
 pause
+EXIT /B 0
+
+:sign
+signtool.exe sign /a /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 %1
+if %ERRORLEVEL% NEQ 0 (
+    echo SignTool Error: signing failed for %1 with exit code %ERRORLEVEL%
+    EXIT /B 1
+)
+EXIT /B 0
