2f64bf52d4c48dfcf67c5fc421fcb85c6e64f40b
Ezequiel Montero <an.dumu@hotmail.com>
2026-09-01 15:08:44 -0300

    test: no-op comment to verify #38740 wodles/utils.py trigger fix (to be reverted)

--- wodles/utils.py
+++ wodles/utils.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+# wazuh#38740 trigger-verification no-op, to be reverted after evidence is captured
 
 import os
 import subprocess
