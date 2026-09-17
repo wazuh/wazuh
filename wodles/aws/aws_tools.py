319339afdf45b79aef279782de693ceb77fff04d
Miguel Angel De la Vega Rodriguez <miguel.delavega@wazuh.com>
2026-09-17 10:44:52 +0200

    fix: add us-gov-east-1 and us-gov-west-1 to the AWS wodle valid regions

--- wodles/aws/aws_tools.py
+++ wodles/aws/aws_tools.py
@@ -27,7 +27,7 @@ ALL_REGIONS = (
     'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3', 'ap-southeast-4', 'ap-southeast-5', 'ap-southeast-7',
     'ca-central-1', 'ca-west-1', 'eu-central-1', 'eu-central-2', 'eu-north-1', 'eu-south-1', 'eu-south-2', 'eu-west-1',
     'eu-west-2', 'eu-west-3', 'il-central-1', 'me-central-1', 'me-south-1', 'mx-central-1', 'sa-east-1', 'us-east-1',
-    'us-east-2', 'us-west-1', 'us-west-2'
+    'us-east-2', 'us-gov-east-1', 'us-gov-west-1', 'us-west-1', 'us-west-2'
 )
 
 RETRY_ATTEMPTS_KEY: str = "max_attempts"
